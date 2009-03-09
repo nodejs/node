5b08b827f9e41d6b28ebf7799758923bbb2b9a83
Ryan <ry@tinyclouds.org>
2009-03-09 18:21:59 +0100

    report errors from loaded files

--- node.cc
+++ node.cc
@@ -178,8 +178,8 @@ v8::Handle<v8::Value> Load(const v8::Arguments& args) {
     if (source.IsEmpty()) {
       return v8::ThrowException(v8::String::New("Error loading file"));
     }
-    if (!ExecuteString(source, v8::String::New(*file), false, false)) {
-      return v8::ThrowException(v8::String::New("Error executing  file"));
+    if (!ExecuteString(source, v8::String::New(*file), false, true)) {
+      return v8::ThrowException(v8::String::New("Error executing file"));
     }
   }
   return v8::Undefined();
