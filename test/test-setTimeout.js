3886e183fd5f2cf69f84f9829a39e96a905233ec
Ryan <ry@tinyclouds.org>
2009-04-29 14:12:24 +0200

    Add test for setInterval

--- test/test-setTimeout.js
+++ test/test-setTimeout.js
@@ -14,4 +14,18 @@ function onLoad () {
   // this timer shouldn't execute
   var id = setTimeout(function () { assertTrue(false); }, 500);
   clearTimeout(id);
+
+  var count = 0;
+  setInterval(function () {
+      count += 1;
+      var endtime = new Date;
+      var diff = endtime - starttime;
+      if (diff < 0) diff = -diff;
+      puts(diff);
+      var t = count * 1000;
+      assertTrue(t - 100 < diff || diff < t + 100);
+      assertTrue(count <= 3);
+      if (count == 3)
+        clearInterval(this);
+  }, 1000);
 }
