6b9e5eedf2fadbd192e9be522ea51bd8fd02bb71
Urban Hafner <urban@bettong.net>
2009-06-16 08:20:00 +0200

    Test for remote module loading

---
+++ test/test-remote-module-loading.js
@@ -0,0 +1,18 @@
+new node.http.Server(function (req, res) {
+  var body = "exports.A = function() { return 'A';}";
+  res.sendHeader(200, [
+    ["Content-Length", body.length],
+    ["Content-Type", "text/plain"]
+  ]);
+  res.sendBody(body);
+  res.finish();
+}).listen(8000);
+
+include("mjsunit.js");
+var a = require("http://localhost:8000/")
+
+function onLoad() {
+  assertInstanceof(a.A, Function);
+  assertEquals("A", a.A());
+  
+}
