bdad5e7333b15a786cd999b411463ffba5e36592
Ryan <ry@tinyclouds.org>
2009-06-24 13:58:17 +0200

    test-process-kill: start process after onLoad

--- test/mjsunit/test-process-kill.js
+++ test/mjsunit/test-process-kill.js
@@ -1,14 +1,16 @@
 include("mjsunit.js");
 
-var cat = new node.Process("cat");
-
 var exit_status = -1;
 
-cat.onOutput = function (chunk) { assertEquals(null, chunk); };
-cat.onError = function (chunk) { assertEquals(null, chunk); };
-cat.onExit = function (status) { exit_status = status; };
+function onLoad () {
+  var cat = new node.Process("cat");
 
-cat.kill();
+  cat.onOutput = function (chunk) { assertEquals(null, chunk); };
+  cat.onError = function (chunk) { assertEquals(null, chunk); };
+  cat.onExit = function (status) { exit_status = status; };
+
+  cat.kill();
+}
 
 function onExit () {
   assertTrue(exit_status > 0);
