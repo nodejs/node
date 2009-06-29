88ad880556673c06f44acacd5bf1c0eee8a71e52
Ryan <ry@tinyclouds.org>
2009-06-29 14:11:26 +0200

    Add new test for node.fs.stat()

---
+++ test/mjsunit/test-fs-stat.js
@@ -0,0 +1,24 @@
+include("mjsunit.js");
+
+var got_error = false;
+var got_success = false;
+var stats;
+
+var promise = node.fs.stat(".");
+
+promise.addCallback(function (_stats) {
+  stats = _stats;
+  p(stats);
+  got_success = true;
+});
+
+promise.addErrback(function () {
+  got_error = true;
+});
+
+function onExit () {
+  assertTrue(got_success);
+  assertFalse(got_error);
+  assertTrue(stats.mtime instanceof Date);
+}
+
