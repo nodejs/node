9b3baf3d508dbef355b2952b15b5dd3f854f92cb
Ryan <ry@tinyclouds.org>
2009-08-06 13:17:30 +0200

    Bugfix: node.encodeUtf8 was broken. (Connor Dunn)
    http://groups.google.com/group/nodejs/browse_thread/thread/5ad0660a0959d885#

--- src/util.js
+++ src/util.js
@@ -21,7 +21,29 @@ node.inherits = function (ctor, superCtor) {
 
 // This is useful for dealing with raw encodings.
 node.encodeUtf8 = function (array) {
-  return String.fromCharCode.apply(String, array);
+  var string = "";
+  var i = 0;
+  var c = c1 = c2 = 0;
+
+  while (i < array.length) {
+    c = array[i];
+
+    if (c < 128) {
+      string += String.fromCharCode(c);
+      i++;
+    } else if ((c > 191) && (c < 224)) {
+      c2 = array[i+1];
+      string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+      i += 2;
+    } else {
+      c2 = array[i+1];
+      c3 = array[i+2];
+      string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+      i += 3;
+    }
+  }
+
+  return string;
 };
 
 node.cat = function(location, encoding) {
