db42ad959d5fa077e05015d56b0b374faf1d51cd
Ryan <ry@tinyclouds.org>
2009-08-25 16:59:06 +0200

    API: All EventEmitters emit "newListener" when listeners are added.
    The "newListener" event will also be emitted for listeners to "newListener".
    Maybe useful?

--- src/events.js
+++ src/events.js
@@ -6,6 +6,9 @@ node.EventEmitter.prototype.addListener = function (type, listener) {
   if (listener instanceof Function) {
     if (!this._events) this._events = {};
     if (!this._events.hasOwnProperty(type)) this._events[type] = [];
+    // To avoid recursion in the case that type == "newListeners"! Before
+    // adding it to the listeners, first emit "newListeners".
+    this.emit("newListener", [type, listener]);
     this._events[type].push(listener);
   }
   return this;
---
+++ test/mjsunit/test-event-emitter-add-listeners.js
@@ -0,0 +1,31 @@
+include("mjsunit.js");
+
+var e = new node.EventEmitter();
+
+var events_new_listener_emited = [];
+var times_hello_emited = 0;
+
+function onLoad () {
+  e.addListener("newListener", function (event, listener) {
+    puts("newListener: " + event);
+    events_new_listener_emited.push(event);
+  });
+
+  e.addListener("hello", function (a, b) {
+    puts("hello");
+    times_hello_emited += 1
+    assertEquals("a", a);
+    assertEquals("b", b);
+  });
+
+  puts("start");
+
+  e.emit("hello", ["a", "b"]);
+}
+
+function onExit () {
+  assertArrayEquals(["hello"], events_new_listener_emited);
+  assertEquals(1, times_hello_emited);
+}
+
+
