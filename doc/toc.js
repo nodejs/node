5f82c4baf0e5aadb86f6bfc7f54f8e534ae0338a
Ryan Dahl <ry@tinyclouds.org>
2009-10-16 13:46:16 +0200

    Improve webpage
    - add google-analytics
    - add example to front-page

--- doc/toc.js
+++ doc/toc.js
@@ -94,7 +94,16 @@ function generateToc(toclevels) {
   var i = setInterval(function () {
     if (window["sh_highlightDocument"]) {
       sh_highlightDocument();
+
+      try {
+        var pageTracker = _gat._getTracker("UA-10874194-2");
+        pageTracker._trackPageview();
+      } catch(err) {}
+
       clearInterval(i);
     }
   }, 100);
+
+  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 })();
