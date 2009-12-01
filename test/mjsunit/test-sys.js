34c02357ff8e00c35e493754943f599dba0b0f29
Xavier Shay <xavier@rhnh.net>
2009-12-01 21:20:14 +1100

    sys.inspect is totally more awesome now
    - No longer relies on JSON.stringify, so it can output nulls and functions
    - Handles circular references better
    - Has tests

---
+++ test/mjsunit/test-sys.js
@@ -0,0 +1,26 @@
+process.mixin(require("./common"));
+process.mixin(require("sys"));
+
+assert.equal("0", inspect(0));
+assert.equal("1", inspect(1));
+assert.equal("false", inspect(false));
+assert.equal('""', inspect(""));
+assert.equal('"hello"', inspect("hello"));
+assert.equal("[Function]", inspect(function() {}));
+assert.equal('undefined', inspect(undefined));
+assert.equal('null', inspect(null));
+
+assert.equal('[]', inspect([]));
+assert.equal('[\n 1,\n 2\n]', inspect([1, 2]));
+assert.equal('[\n 1,\n [\n  2,\n  3\n ]\n]', inspect([1, [2, 3]]));
+
+assert.equal('{}', inspect({}));
+assert.equal('{\n "a": 1\n}', inspect({a: 1}));
+assert.equal('{\n "a": [Function]\n}', inspect({a: function() {}}));
+assert.equal('{\n "a": 1,\n "b": 2\n}', inspect({a: 1, b: 2}));
+assert.equal('{\n "a": {}\n}', inspect({'a': {}}));
+assert.equal('{\n "a": {\n  "b": 2\n }\n}', inspect({'a': {'b': 2}}));
+
+var value = {}
+value['a'] = value;
+assert.equal('{\n "a": [Circular]\n}', inspect(value));
