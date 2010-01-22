6f738d6e7a90f5c7659074340baaeb098c9e40f7
David Sklar <david.sklar@gmail.com>
2010-01-22 17:03:54 -0500

    Adjust passing-FDs test to wait until socket is really writeable

--- test/mjsunit/test-net-fd-passing.js
+++ test/mjsunit/test-net-fd-passing.js
@@ -12,43 +12,47 @@ function fdPassingTest(path, port) {
   var message = "beep toot";
   var expectedData = ["[greeting] " + greeting, "[echo] " + message];
 
-  puts(fixturesDir);
   var receiverArgs = [fixturesDir + "/net-fd-passing-receiver.js", path, greeting];
   var receiver = process.createChildProcess(process.ARGV[0], receiverArgs);
 
   var initializeSender = function() {
     var fdHighway = new net.Socket();
+
+    fdHighway.addListener("connect", function() {
+      var sender = net.createServer(function(socket) {
+        fdHighway.sendFD(socket);
+        socket.flush();
+        socket.forceClose(); // want to close() the fd, not shutdown()
+      });
+
+      sender.addListener("listening", function() {
+        var client = net.createConnection(port);
+
+        client.addListener("connect", function() {
+          client.send(message);
+        });
+
+        client.addListener("data", function(data) {
+          assert.equal(expectedData[0], data);
+          if (expectedData.length > 1) {
+            expectedData.shift();
+          }
+          else {
+            // time to shut down
+            fdHighway.close();
+            sender.close();
+            client.forceClose();
+          }
+        });
+      });
+
+      tests_run += 1;
+      sender.listen(port);
+    });
+
     fdHighway.connect(path);
 
-    var sender = net.createServer(function(socket) {
-      fdHighway.sendFD(socket);
-      socket.flush();
-      socket.forceClose(); // want to close() the fd, not shutdown()
-    });
 
-    sender.addListener("listening", function() {
-      var client = net.createConnection(port);
-
-      client.addListener("connect", function() {
-        client.send(message);
-      });
-
-      client.addListener("data", function(data) {
-        assert.equal(expectedData[0], data);
-        if (expectedData.length > 1) {
-          expectedData.shift();
-        }
-        else {
-          // time to shut down
-          fdHighway.close();
-          sender.close();
-          client.forceClose();
-        }
-      });
-    });
-
-    tests_run += 1;
-    sender.listen(port);
   };
 
   receiver.addListener("output", function(data) {
