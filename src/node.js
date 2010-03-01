55ab9b4541b2507f4e5491dd07437fab10867425
Felix Geisendörfer <felix@debuggable.com>
2010-03-01 16:05:28 +0100

    Bug fix for deep process.mixin array handling
    process.mixin was throwing an exception when trying to do a deep copy
    of an object that included an array.
    
    This bug was introduced in: 3bb7ad6fea42545e9d84ba5cbef8b48e470790fc

--- src/node.js
+++ src/node.js
@@ -126,7 +126,7 @@ process.mixin = function() {
     if ( (source = arguments[i]) != null ) {
       // Extend the base object
       Object.getOwnPropertyNames(source).forEach(function(k){
-        var d = Object.getOwnPropertyDescriptor(source, k);
+        var d = Object.getOwnPropertyDescriptor(source, k) || {value: source[k]};
         if (d.get) {
           target.__defineGetter__(k, d.get);
           if (d.set) {

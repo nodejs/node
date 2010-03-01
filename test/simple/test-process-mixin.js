nodejs/node

commit 3bb7ad6fea42545e9d84ba5cbef8b48e470790fc

fixed process.mixin to properly copy getters/setters

src/node.js
@@ -105,9 +105,10 @@ process.assert = function (x, msg) {
 // Copyright (c) 2009 John Resig
 // Dual licensed under the MIT and GPL licenses.
 // http://docs.jquery.com/License
+// Modified for node.js (formely for copying properties correctly)
 process.mixin = function() {
   // copy reference to target object
-  var target = arguments[0] || {}, i = 1, length = arguments.length, deep = false, options;
+  var target = arguments[0] || {}, i = 1, length = arguments.length, deep = false, source;
 
   // Handle a deep copy situation
   if ( typeof target === "boolean" ) {
@@ -129,27 +130,31 @@ process.mixin = function() {
 
   for ( ; i < length; i++ ) {
     // Only deal with non-null/undefined values
-    if ( (options = arguments[ i ]) != null ) {
+    if ( (source = arguments[i]) != null ) {
       // Extend the base object
-      for ( var name in options ) {
-        var src = target[ name ], copy = options[ name ];
-
-        // Prevent never-ending loop
-        if ( target === copy )
-          continue;
-
-        // Recurse if we're merging object values
-        if ( deep && copy && typeof copy === "object" ) {
-          target[ name ] = process.mixin( deep,
-            // Never move original objects, clone them
-            src || ( copy.length != null ? [ ] : { } )
-          , copy );
-
-        // Don't bring in undefined values
-        } else {
-          target[ name ] = copy;
+      Object.getOwnPropertyNames(source).forEach(function(k){
+        var d = Object.getOwnPropertyDescriptor(source, k);
+        if (d.get) {
+          target.__defineGetter__(k, d.get);
+          if (d.set)
+            target.__defineSetter__(k, d.set);
         }
-      }
+        else {
+          // Prevent never-ending loop
+          if (target === d.value)
+            continue;
+          
+          if (deep && d.value && typeof d.value === "object") {
+            target[k] = process.mixin(deep,
+              // Never move original objects, clone them
+              source || (d.value.length != null ? [] : {})
+            , d.value);
+          }
+          else {
+            target[k] = d.value;
+          }
+        }
+      });
     }
   }
   // Return the modified object

test/mjsunit/test-process-mixin.js
@@ -20,3 +20,18 @@ target = {};
 
 process.mixin(target, objectWithUndefinedValue);
 assert.ok(target.hasOwnProperty('foo'));
+
+// This test verifies getters and setters being copied correctly
+
+var source = {
+  _foo:'a',
+  get foo(){ return this._foo; },
+  set foo(value){ this._foo = "did set to "+value; }
+};
+var target = {};
+process.mixin(target, source);
+target._foo = 'b';
+assert.equal(source.foo, 'a');
+assert.equal('b', target.foo, 'target.foo != "b" -- value/result was copied instead of getter function');
+source.foo = 'c';
+assert.equal('did set to c', source.foo, 'source.foo != "c" -- value was set instead of calling setter function');

commit 55ab9b4541b2507f4e5491dd07437fab10867425

Bug fix for deep process.mixin array handling
process.mixin was throwing an exception when trying to do a deep copy
of an object that included an array.

This bug was introduced in: 3bb7ad6fea42545e9d84ba5cbef8b48e470790fc

src/node.js
@@ -126,7 +126,7 @@ process.mixin = function() {
     if ( (source = arguments[i]) != null ) {
       // Extend the base object
       Object.getOwnPropertyNames(source).forEach(function(k){
-        var d = Object.getOwnPropertyDescriptor(source, k);
+        var d = Object.getOwnPropertyDescriptor(source, k) || {value: source[k]};
         if (d.get) {
           target.__defineGetter__(k, d.get);
           if (d.set) {

test/simple/test-process-mixin.js
@@ -28,10 +28,19 @@ var source = {
   get foo(){ return this._foo; },
   set foo(value){ this._foo = "did set to "+value; }
 };
-var target = {};
+target = {};
 process.mixin(target, source);
 target._foo = 'b';
 assert.equal(source.foo, 'a');
 assert.equal('b', target.foo, 'target.foo != "b" -- value/result was copied instead of getter function');
 source.foo = 'c';
 assert.equal('did set to c', source.foo, 'source.foo != "c" -- value was set instead of calling setter function');
+
+// Test that nested arrays are handled properly
+target = {};
+process.mixin(true, target, {
+  foo: ['bar'],
+});
+
+assert.notStrictEqual(['bar'], target.foo);
+assert.deepEqual(['bar'], target.foo);