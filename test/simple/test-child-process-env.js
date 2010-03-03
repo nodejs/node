769a35024f399e051f16dd6c8e663f82ed37cbf2
Ryan Dahl <ry@tinyclouds.org>
2010-03-03 10:45:58 -0800

    Allow passing env to child process

---
+++ test/simple/test-child-process-env.js
@@ -0,0 +1,12 @@
+process.mixin(require("../common"));
+child = process.createChildProcess('/usr/bin/env', [], {'HELLO' : 'WORLD'});
+response = "";
+
+child.addListener("output", function (chunk) {
+  puts("stdout: " + JSON.stringify(chunk));
+  if (chunk) response += chunk;
+});
+
+process.addListener('exit', function () {
+ assert.ok(response.indexOf('HELLO=WORLD') >= 0);
+});
