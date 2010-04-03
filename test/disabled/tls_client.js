c55c0a1fca6abec269538ea738749e2129242e1f
Rhys Jones <rhys@wave.to>
2010-04-02 17:10:32 -0700

    Add SSL tests

---
+++ test/disabled/tls_client.js
@@ -0,0 +1,35 @@
+require('../common');
+var sys=require('sys');
+var net=require('net');
+var fs=require('fs');
+
+//var client = net.createConnection(4443, "localhost");
+var client = net.createConnection(443, "www.microsoft.com");
+//var client = net.createConnection(443, "www.google.com");
+
+var caPem = fs.readFileSync(fixturesDir+"/msca.pem");
+//var caPem = fs.readFileSync("ca.pem");
+
+var credentials = net.createCredentials({ca:caPem});
+
+client.setEncoding("UTF8");
+client.addListener("connect", function () {
+  sys.puts("client connected.");
+  client.setSecure(credentials);
+});
+
+client.addListener("secure", function () {
+  sys.puts("client secure : "+JSON.stringify(client.getCipher()));
+  sys.puts(JSON.stringify(client.getPeerCertificate()));
+  sys.puts("verifyPeer : "+client.verifyPeer());
+  client.write("GET / HTTP/1.0\r\n\r\n");
+});
+
+client.addListener("data", function (chunk) {
+  sys.error(chunk);
+});
+
+client.addListener("end", function () {
+  sys.puts("client disconnected.");
+});
+
---
+++ test/disabled/tls_server.js
@@ -0,0 +1,34 @@
+require('../common');
+
+var sys=require('sys');
+var net=require('net');
+var fs=require('fs');
+
+var keyPem = fs.readFileSync(fixturesDir + "/test_key.pem");
+var certPem = fs.readFileSync(fixturesDir + "/cert.pem");
+
+var credentials = net.createCredentials({key:keyPem, cert:certPem});
+var i = 0;
+var server = net.createServer(function (connection) {
+  connection.setSecure(credentials);
+  connection.setEncoding("binary");
+
+  connection.addListener("secure", function () {
+    //sys.puts("Secure");
+  });
+
+  connection.addListener("data", function (chunk) {
+    sys.puts("recved: " + JSON.stringify(chunk));
+    connection.write("HTTP/1.0 200 OK\r\nContent-type: text/plain\r\nContent-length: 9\r\n\r\nOK : "+i+"\r\n\r\n");
+    i=i+1;
+    connection.close();
+  });
+
+  connection.addListener("end", function () {
+    connection.close();
+  });
+
+});
+server.listen(4443);
+
+
