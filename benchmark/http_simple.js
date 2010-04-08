62277ab79b5cbdaba45693b33bfd55d61caa2f4a
Ryan Dahl <ry@tinyclouds.org>
2010-04-08 01:00:29 -0700

    Support both old and new HTTP closing APIs in benchmark program

--- benchmark/http_simple.js
+++ benchmark/http_simple.js
@@ -1,7 +1,7 @@
 path = require("path");
 
 var puts = require("sys").puts;
-var old = false;
+var old = true;
 
 http = require(old ? "http_old" : 'http');
 if (old) puts('old version');
@@ -52,6 +52,10 @@ http.createServer(function (req, res) {
                   , "Content-Length": content_length
                   }
                 );
-  if (old) res.write(body, 'ascii');
-  res.close(body, 'ascii');
+  if (old) {
+    res.write(body, 'ascii');
+    res.close();
+  } else {
+    res.close(body, 'ascii');
+  }
 }).listen(8000);
