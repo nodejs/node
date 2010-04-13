b98cd6753b44563662f60ccf4feb22cfab404858
Ryan Dahl <ry@tinyclouds.org>
2010-04-13 13:44:05 -0700

    process.mixin: deprecation  -> removed

--- src/node.js
+++ src/node.js
@@ -21,6 +21,7 @@ process.debug = removed("process.debug() has moved. Use require('sys') to bring 
 process.error = removed("process.error() has moved. Use require('sys') to bring it back.");
 process.watchFile = removed("process.watchFile() has moved to fs.watchFile()");
 process.unwatchFile = removed("process.unwatchFile() has moved to fs.unwatchFile()");
+process.mixin = removed('process.mixin() has been removed.');
 
 GLOBAL.node = {};
 
@@ -99,73 +100,6 @@ process.assert = function (x, msg) {
   if (!(x)) throw new Error(msg || "assertion error");
 };
 
-
-// From jQuery.extend in the jQuery JavaScript Library v1.3.2
-// Copyright (c) 2009 John Resig
-// Dual licensed under the MIT and GPL licenses.
-// http://docs.jquery.com/License
-// Modified for node.js (formely for copying properties correctly)
-var mixinMessage;
-process.mixin = function() {
-  if (!mixinMessage) {
-    mixinMessage = 'deprecation warning: process.mixin will be removed from node-core future releases.\n'
-    process.binding('stdio').writeError(mixinMessage);
-  }
-  // copy reference to target object
-  var target = arguments[0] || {}, i = 1, length = arguments.length, deep = false, source;
-
-  // Handle a deep copy situation
-  if ( typeof target === "boolean" ) {
-    deep = target;
-    target = arguments[1] || {};
-    // skip the boolean and the target
-    i = 2;
-  }
-
-  // Handle case when target is a string or something (possible in deep copy)
-  if ( typeof target !== "object" && !(typeof target === 'function') )
-    target = {};
-
-  // mixin process itself if only one argument is passed
-  if ( length == i ) {
-    target = GLOBAL;
-    --i;
-  }
-
-  for ( ; i < length; i++ ) {
-    // Only deal with non-null/undefined values
-    if ( (source = arguments[i]) != null ) {
-      // Extend the base object
-      Object.getOwnPropertyNames(source).forEach(function(k){
-        var d = Object.getOwnPropertyDescriptor(source, k) || {value: source[k]};
-        if (d.get) {
-          target.__defineGetter__(k, d.get);
-          if (d.set) {
-            target.__defineSetter__(k, d.set);
-          }
-        }
-        else {
-          // Prevent never-ending loop
-          if (target === d.value) {
-            return;
-          }
-
-          if (deep && d.value && typeof d.value === "object") {
-            target[k] = process.mixin(deep,
-              // Never move original objects, clone them
-              source[k] || (d.value.length != null ? [] : {})
-            , d.value);
-          }
-          else {
-            target[k] = d.value;
-          }
-        }
-      });
-    }
-  }
-  // Return the modified object
-  return target;
-};
 
 // Event
 
