11f673e819d79f07dbb42b75a5b070af66fcb4a5
Ryan Dahl <ry@tinyclouds.org>
2010-04-22 15:48:57 -0700

    curl after server is listening

--- test/simple/test-http-304.js
+++ test/simple/test-http-304.js
@@ -9,11 +9,15 @@ s = http.createServer(function (request, response) {
   response.end();
 })
 s.listen(8000);
+sys.puts('Server running at http://127.0.0.1:8000/')
 
-childProcess.exec('curl http://127.0.0.1:8000/', function (err, stdout, stderr) {
-  if (err) throw err;
-  s.close();
-  sys.puts('curled response correctly');
+s.addListener('listening', function () {
+
+  childProcess.exec('curl http://127.0.0.1:8000/', function (err, stdout, stderr) {
+    if (err) throw err;
+    s.close();
+    sys.puts('curled response correctly');
+  });
+
 });
 
-sys.puts('Server running at http://127.0.0.1:8000/')
