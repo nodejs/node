264e540d008a251a7e37f367611677414c2591d1
Ryan Dahl <ry@tinyclouds.org>
2010-05-08 22:11:55 -0700

    Fix error reporting in child_process callbacks
    Issue 120, test case by Nathan Ostgard

--- test/simple/test-child-process-exit-code.js
+++ test/simple/test-child-process-exit-code.js
@@ -1,11 +1,30 @@
 require("../common");
-var spawn = require('child_process').spawn
-  , path = require('path')
-  , sub = path.join(fixturesDir, 'exit.js')
-  , child = spawn(process.argv[0], [sub, 23])
-  ; 
+spawn = require('child_process').spawn,
+path = require('path');
 
-child.addListener('exit', function(code, signal) {
+exits = 0;
+
+exitScript = path.join(fixturesDir, 'exit.js')
+exitChild = spawn(process.argv[0], [exitScript, 23]);
+exitChild.addListener('exit', function(code, signal) {
   assert.strictEqual(code, 23);
   assert.strictEqual(signal, null);
+
+  exits++;
+});
+
+
+
+errorScript = path.join(fixturesDir, 'child_process_should_emit_error.js')
+errorChild = spawn(process.argv[0], [errorScript]);
+errorChild.addListener('exit', function(code, signal) {
+  assert.ok(code !== 0);
+  assert.strictEqual(signal, null);
+
+  exits++;
+});
+
+
+process.addListener('exit', function () {
+  assert.equal(2, exits);
 });
