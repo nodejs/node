fea6f829bf6cc0d2f3f698a786832797d5ddae8e
Jonas Pfenniger <jonas@pfenniger.name>
2010-05-14 13:12:41 +0200

    FIX: EventEmitter#removeListener logic
    In the case of one defined listener, the function should only remove it
    if it is the same as the passed listener.

---
+++ test/simple/test-event-emitter-remove-listeners.js
@@ -0,0 +1,39 @@
+require("../common");
+var events = require('events');
+
+
+count = 0;
+
+function listener1 () {
+  puts('listener1');
+  count++;
+}
+
+function listener2 () {
+  puts('listener2');
+  count++;
+}
+
+function listener3 () {
+  puts('listener3');
+  count++;
+}
+
+e1 = new events.EventEmitter();
+e1.addListener("hello", listener1);
+e1.removeListener("hello", listener1);
+assert.deepEqual([], e1.listeners('hello'));
+
+e2 = new events.EventEmitter();
+e2.addListener("hello", listener1);
+e2.removeListener("hello", listener2);
+assert.deepEqual([listener1], e2.listeners('hello'));
+
+e3 = new events.EventEmitter();
+e3.addListener("hello", listener1);
+e3.addListener("hello", listener2);
+e3.removeListener("hello", listener1);
+assert.deepEqual([listener2], e3.listeners('hello'));
+
+
+
