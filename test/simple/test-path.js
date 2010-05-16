36a45c4e0def497e3d1446057c1d91680d8bb259
rentzsch <jwr.git@redshed.net>
2010-05-16 14:29:29 -0500

    FIX path.dirname('/tmp') => '/'.
    Previously path.dirname('/tmp') incorrectly returned '.'.
    
    Unfortunately module.js incorrectly thinks dirname('/a/b/') should
    yield '/a/b', so I can't strip trailing slashes yet. Once module.js
    is fixed, then the commented-out code should be activated and a test
    written for it.

--- test/simple/test-path.js
+++ test/simple/test-path.js
@@ -7,6 +7,9 @@ assert.equal(path.basename(f), "test-path.js");
 assert.equal(path.basename(f, ".js"), "test-path");
 assert.equal(path.extname(f), ".js");
 assert.equal(path.dirname(f).substr(-11), "test/simple");
+assert.equal(path.dirname("/a/b"), "/a");
+assert.equal(path.dirname("/a"), "/");
+assert.equal(path.dirname("/"), "/");
 path.exists(f, function (y) { assert.equal(y, true) });
 
 assert.equal(path.join(".", "fixtures/b", "..", "/b/c.js"), "fixtures/b/c.js");
