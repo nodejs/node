fe3d8f2411209b202b7fe74cb48b582ffc5cfc5f
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-05-21 12:05:55 -0700

    Add assert.ifError

--- doc/api.markdown
+++ doc/api.markdown
@@ -2546,6 +2546,9 @@ Expects `block` to throw an error.
 
 Expects `block` not to throw an error.
 
+### assert.ifError(value)
+
+Tests if value is not a false value, throws if it is a true value. Useful when testing the first argument, `error` in callbacks.
 
 ## Path
 
--- test/simple/test-assert.js
+++ test/simple/test-assert.js
@@ -154,3 +154,6 @@ try {
 }
 assert.equal(true,threw,'a.doesNotThrow is not catching type matching errors');
 
+assert.throws(function () {assert.ifError(new Error('test error'))});
+assert.doesNotThrow(function(){assert.ifError(null)});
+assert.doesNotThrow(function(){assert.ifError()});
