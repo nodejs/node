19f475c5734714554c311b0f7e60ab40204d8917
Ryan Dahl <ry@tinyclouds.org>
2010-05-24 15:47:40 -0700

    fs.ReadStream should emit Buffers
    And do proper utf8 encoding.

--- test/simple/test-file-read-stream.js
+++ test/simple/test-file-read-stream.js
@@ -1,54 +1,65 @@
 require('../common');
 
-var
-  path = require('path'),
-  fs = require('fs'),
-  fn = path.join(fixturesDir, 'test_ca.pem'),
-  file = fs.createReadStream(fn),
+// TODO Improved this test. test_ca.pem is too small. A proper test would
+// great a large utf8 (with multibyte chars) file and stream it in,
+// performing sanity checks throughout.
 
-  callbacks = {
-    open: -1,
-    end: -1,
-    close: -1,
-    destroy: -1
-  },
+Buffer = require('buffer').Buffer;
+path = require('path');
+fs = require('fs');
+fn = path.join(fixturesDir, 'test_ca.pem');
 
-  paused = false,
+file = fs.createReadStream(fn);
 
-  fileContent = '';
+callbacks = {
+  open: -1,
+  end: -1,
+  data: -1,
+  close: -1,
+  destroy: -1
+};
 
-file
-  .addListener('open', function(fd) {
-    callbacks.open++;
-    assert.equal('number', typeof fd);
-    assert.ok(file.readable);
-  })
-  .addListener('error', function(err) {
-    throw err;
-  })
-  .addListener('data', function(data) {
-    assert.ok(!paused);
-    fileContent += data;
-    
-    paused = true;
-    file.pause();
-    assert.ok(file.paused);
+paused = false;
 
-    setTimeout(function() {
-      paused = false;
-      file.resume();
-      assert.ok(!file.paused);
-    }, 10);
-  })
-  .addListener('end', function(chunk) {
-    callbacks.end++;
-  })
-  .addListener('close', function() {
-    callbacks.close++;
-    assert.ok(!file.readable);
+fileContent = '';
 
-    assert.equal(fs.readFileSync(fn), fileContent);
-  });
+file.addListener('open', function(fd) {
+  callbacks.open++;
+  assert.equal('number', typeof fd);
+  assert.ok(file.readable);
+});
+
+file.addListener('error', function(err) {
+  throw err;
+});
+
+file.addListener('data', function(data) {
+  callbacks.data++;
+  assert.ok(data instanceof Buffer);
+  assert.ok(!paused);
+  fileContent += data;
+  
+  paused = true;
+  file.pause();
+  assert.ok(file.paused);
+
+  setTimeout(function() {
+    paused = false;
+    file.resume();
+    assert.ok(!file.paused);
+  }, 10);
+});
+
+file.addListener('end', function(chunk) {
+  callbacks.end++;
+});
+
+file.addListener('close', function() {
+  callbacks.close++;
+  assert.ok(!file.readable);
+
+  assert.equal(fs.readFileSync(fn), fileContent);
+});
 
 var file2 = fs.createReadStream(fn);
 file2.destroy(function(err) {
