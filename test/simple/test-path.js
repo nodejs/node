b05daaaf69c5445dd28ad3efab118ce92cd23afb
Micheil Smith <micheil@brandedcode.com>
2010-06-16 23:51:19 +1000

    Adding path.existsSync (with tests.)

--- test/simple/test-path.js
+++ test/simple/test-path.js
@@ -12,6 +12,8 @@ assert.equal(path.dirname("/a"), "/");
 assert.equal(path.dirname("/"), "/");
 path.exists(f, function (y) { assert.equal(y, true) });
 
+assert.equal(path.existsSync(f), true);
+
 assert.equal(path.extname(""), "");
 assert.equal(path.extname("/path/to/file"), "");
 assert.equal(path.extname("/path/to/file.ext"), ".ext");
