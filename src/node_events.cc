192234c68958bbc2489d3bb3b7a020fb4393e144
Ryan Dahl <ry@tinyclouds.org>
2010-07-01 23:36:53 -0700

    Remove unnecessary defines in node_events.cc

--- src/node_events.cc
+++ src/node_events.cc
@@ -1,19 +1,6 @@
 // Copyright 2009 Ryan Dahl <ry@tinyclouds.org>
 #include <node_events.h>
-
-#include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h> /* inet_ntop */
-#include <netinet/in.h> /* sockaddr_in, sockaddr_in6 */
-
 #include <node.h>
-#include <ev.h>
-#include <v8.h>
 
 namespace node {
 
