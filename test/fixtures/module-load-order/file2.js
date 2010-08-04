0a3eff8021de38f11411369da3ea93bd9dbddd31
Nick Stenning <nick@whiteink.com>
2010-08-04 22:55:24 +0100

    Standardise module load order for native and registered file extensions.
    This patch standardises the load order for modules. Highest priority is trying to load exactly the file the user specified, followed by native extensions, followed by registered extra extensions, etc.
    
    In full, if we require('foo') having registered '.coffee' as an alternative extension, we try and load the following files in order:
    
        foo
        foo.js
        foo.node
        foo.coffee
        foo/index.js
        foo/index.node
        foo/index.coffee

---
+++ test/fixtures/module-load-order/file2.js
@@ -0,0 +1,1 @@
+exports.file2 = 'file2.js';
--- test/simple/test-module-loading.js
+++ test/simple/test-module-loading.js
@@ -123,6 +123,36 @@ require.async("../fixtures/empty", function (err, a) {
   }
 });
 
+// Check load order is as expected
+common.debug('load order');
+
+var loadOrder = '../fixtures/module-load-order/',
+    msg       = "Load order incorrect.";
+
+require.registerExtension('.reg',  function(content) { return content; });
+require.registerExtension('.reg2', function(content) { return content; });
+
+assert.equal(require(loadOrder + 'file1').file1, 'file1',            msg);
+assert.equal(require(loadOrder + 'file2').file2, 'file2.js',         msg);
+try {
+  require(loadOrder + 'file3');
+} catch (e) {
+  // Not a real .node module, but we know we require'd the right thing.
+  assert.ok(e.message.match(/^dlopen/));
+  assert.ok(e.message.match(/file3\.node/));
+}
+assert.equal(require(loadOrder + 'file4').file4, 'file4.reg',        msg);
+assert.equal(require(loadOrder + 'file5').file5, 'file5.reg2',       msg);
+assert.equal(require(loadOrder + 'file6').file6, 'file6/index.js',   msg);
+try {
+  require(loadOrder + 'file7');
+} catch (e) {
+  assert.ok(e.message.match(/^dlopen/));
+  assert.ok(e.message.match(/file7\/index\.node/));
+}
+assert.equal(require(loadOrder + 'file8').file8, 'file8/index.reg',  msg);
+assert.equal(require(loadOrder + 'file9').file9, 'file9/index.reg2', msg);
+
 process.addListener("exit", function () {
   assert.equal(true, a.A instanceof Function);
   assert.equal("A done", a.A());
