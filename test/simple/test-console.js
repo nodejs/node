8d2e79451ed0f76ee491df0f6287208d2c0dccb8
Johan Euphrosine <proppy@aminche.com>
2010-08-17 19:07:45 +0200

    add tests for console.log arguments handling

---
+++ test/simple/test-console.js
@@ -0,0 +1,17 @@
+common = require("../common");
+assert = common.assert;
+
+var stdout_write = global.process.stdout.write;
+var strings = [];
+global.process.stdout.write = function(string) {
+    strings.push(string);
+};
+
+console.log('foo');
+console.log('foo', 'bar');
+console.log('%s %s', 'foo', 'bar', 'hop');
+
+global.process.stdout.write = stdout_write;
+assert.equal('foo\n', strings.shift());
+assert.equal('foo bar\n', strings.shift());
+assert.equal('foo bar hop\n', strings.shift());
