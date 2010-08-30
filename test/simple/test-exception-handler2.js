ead58b9a938647287925f2dd6bd6cafa889efe09
Ryan Dahl <ry@tinyclouds.org>
2010-08-30 12:02:01 -0700

    Add failing uncaughtException test
    FIXME

---
+++ test/simple/test-exception-handler2.js
@@ -0,0 +1,11 @@
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
+
+setTimeout(function () {
+  console.log('This will still run.');
+}, 500);
+
+// Intentionally cause an exception, but don't catch it.
+nonexistentFunc();
+console.log('This will not run.');
