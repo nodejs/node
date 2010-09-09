c099e274d2e428e93227ce11ae0f81cddc1204f4
isaacs <i@izs.me>
2010-09-08 18:42:32 -0700

    Better temporary directory handling for tests.
    Add a setUp and tearDown function to the test case class, and use it to
    create and remove the test/tmp directory for each test.
    
    TODO: amend other tests.

--- test/common.js
+++ test/common.js
@@ -3,6 +3,7 @@ var path = require("path");
 exports.testDir = path.dirname(__filename);
 exports.fixturesDir = path.join(exports.testDir, "fixtures");
 exports.libDir = path.join(exports.testDir, "../lib");
+exports.tmpDir = path.join(exports.testDir, "tmp");
 exports.PORT = 12346;
 
 exports.assert = require('assert');
