ee9af669905aeddd6da32712e711aeee9f9d36b3
Ryan Dahl <ry@tinyclouds.org>
2010-09-30 11:45:27 -0700

    Add test for getting parse error from HTTP client
    Made this test in response to this thread:
    http://groups.google.com/group/nodejs/browse_thread/thread/f82835007a277de2/
    But Node appears to be working correctly.

---
+++ test/simple/test-http-client-parse-error.js
@@ -0,0 +1,34 @@
+var common = require("../common");
+var assert = require('assert');
+
+var http = require('http');
+var net = require('net');
+
+// Create a TCP server
+var srv = net.createServer(function(c) {
+  c.write('bad http - should trigger parse error\r\n');
+
+  console.log("connection");
+
+  c.addListener('end', function() { c.end(); });
+});
+srv.listen(common.PORT, '127.0.0.1');
+
+var hc = http.createClient(common.PORT, '127.0.0.1');
+hc.request('GET', '/').end();
+
+var parseError = false;
+
+hc.on('error', function (e) {
+  console.log("got error from client");
+  srv.close();
+  assert.ok(e.message.indexOf("Parse Error") >= 0);
+  parseError = true;
+});
+
+
+process.addListener('exit', function() {
+  assert.ok(parseError);
+});
+
+
