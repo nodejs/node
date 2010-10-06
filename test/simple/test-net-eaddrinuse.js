5d400cfd3a2a9faf27a88bf82f33a57f78fa65af
Johan Euphrosine <proppy@aminche.com>
2010-10-06 19:18:08 +0200

    net listen should emit eaddrinuse error

---
+++ test/simple/test-net-eaddrinuse.js
@@ -0,0 +1,14 @@
+common = require('../common');
+assert = common.assert
+net = require('net');
+
+var server1 = net.createServer(function (socket) {
+});
+var server2 = net.createServer(function (socket) {
+});
+server1.listen(31337);
+server2.addListener('error', function(error) {
+  assert.equal(true, error.message.indexOf('EADDRINUSE') >= 0);
+  server1.close();
+});
+server2.listen(31337);
