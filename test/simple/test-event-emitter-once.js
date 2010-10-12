a6ee3bac85c84c1a24c1a2aa621f77ba5d1c0bb9
Peteris Krumins <peteris.krumins@gmail.com>
2010-10-12 23:52:26 +0300

    Add EventEmitter.prototype.once

---
+++ test/simple/test-event-emitter-once.js
@@ -0,0 +1,20 @@
+common = require("../common");
+assert = common.assert
+var events = require('events');
+
+var e = new events.EventEmitter();
+var times_hello_emited = 0;
+
+e.once("hello", function (a, b) {
+  times_hello_emited++;
+});
+
+e.emit("hello", "a", "b");
+e.emit("hello", "a", "b");
+e.emit("hello", "a", "b");
+e.emit("hello", "a", "b");
+
+process.addListener("exit", function () {
+  assert.equal(1, times_hello_emited);
+});
+
