d9a5edb2b076063058a0631c5389b053f649dd1d
Ryan Dahl <ry@tinyclouds.org>
2010-10-23 11:22:24 -0700

    Fix test-net-eaddrinuse

--- test/simple/test-net-eaddrinuse.js
+++ test/simple/test-net-eaddrinuse.js
@@ -6,9 +6,9 @@ var server1 = net.createServer(function (socket) {
 });
 var server2 = net.createServer(function (socket) {
 });
-server1.listen(31337);
+server1.listen(common.PORT);
 server2.addListener('error', function(error) {
   assert.equal(true, error.message.indexOf('EADDRINUSE') >= 0);
   server1.close();
 });
-server2.listen(31337);
+server2.listen(common.PORT);
