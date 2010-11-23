fa8ffaf9b2375f98ac86f887bf76f3aa81fa5aa4
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-11-23 12:16:56 +0530

    cmake: Install v8 headers include/node

--- cmake/libv8.cmake
+++ cmake/libv8.cmake
@@ -1,4 +1,4 @@
-set(V8_INCLUDE_NAMES v8.h v8-debug.h v8-profiler.h)
+set(V8_INCLUDE_NAMES v8.h v8-debug.h v8-profiler.h v8stdint.h)
 set(V8_LIBRARY_NAMES v8)
 
 if(SHARED_V8)
@@ -12,4 +12,14 @@ else()
     set(v8_fn "libv8.a")
   endif()
   set(V8_LIBRARY_PATH "${PROJECT_BINARY_DIR}/deps/v8/${v8_fn}")
+  install(DIRECTORY
+      ## Do NOT remove the trailing slash
+      ## it is required so that v8 headers are
+      ## copied directly into include/node
+      ## rather than in a subdirectory
+      ## See CMake's install(DIRECTORY) manual for details
+      ${V8_INCLUDE_DIR}/
+
+      DESTINATION include/node
+  )
 endif()
