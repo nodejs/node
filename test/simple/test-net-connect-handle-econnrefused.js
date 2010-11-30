1dbbaa7fa09e3bff48c5d7d9f6baf47fee49cc7b
Ryan Dahl <ry@tinyclouds.org>
2010-11-29 17:36:59 -0800

    Add test to show ECONNREFUSED works

---
+++ test/simple/test-net-connect-handle-econnrefused.js
@@ -0,0 +1,24 @@
+var common = require('../common');
+var net = require('net');
+var assert = require('assert');
+
+
+// Hopefully nothing is running on common.PORT
+var c = net.createConnection(common.PORT);
+
+c.on('connect', function () {
+  console.error("connected?!");
+  assert.ok(false);
+})
+
+var gotError = false;
+c.on('error', function (e) {
+  console.error("couldn't connect.");
+  gotError = true;
+  assert.equal(require('constants').ECONNREFUSED, e.errno);
+});
+
+
+process.on('exit', function () {
+  assert.ok(gotError);
+});
