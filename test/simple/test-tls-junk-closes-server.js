a59483bd1d63b8477ec72b44a2386f0a566c04ef
Ryan Dahl <ry@tinyclouds.org>
2010-12-11 02:45:38 -0800

    TLS: server should die on junk

---
+++ test/simple/test-tls-junk-closes-server.js
@@ -0,0 +1,28 @@
+var common = require('../common');
+var tls = require('tls');
+var fs = require('fs');
+var net = require('net');
+
+var options = {
+  key: fs.readFileSync(common.fixturesDir + '/keys/agent2-key.pem'),
+  cert: fs.readFileSync(common.fixturesDir + '/keys/agent2-cert.pem')
+};
+
+var server = tls.createServer(function (s) {
+  s.write("welcome!\n");
+  s.pipe(s);
+});
+
+server.listen(common.PORT, function () {
+  var c = net.createConnection(common.PORT);
+
+  c.on('connect', function () {
+    c.write("blah\nblah\nblah\n");
+  });
+
+  c.on('end', function () {
+    server.close();
+  });
+
+});
+
