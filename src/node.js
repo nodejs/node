nodejs/node

commit fe804d9bc7c4f0cca1f428acab1fd45155c956a9

_Partial_ backslash support in node.js argv directory munging
Doesn't do it when node is started from another drive using a drive-relative path.
E.g:
  d:
  cd nodejs
  c:
  d:node.exe d:test.js

src/node.js
@@ -530,14 +530,16 @@
   var path = requireNative('path');
 
   // Make process.argv[0] and process.argv[1] into full paths.
-  if (process.argv[0].indexOf('/') > 0) {
+  if ('/\\'.indexOf(process.argv[0].charAt(0)) < 0 
+      && process.argv[0].charAt(1) != ':') {
     process.argv[0] = path.join(cwd, process.argv[0]);
   }
 
   if (process.argv[1]) {
     // Load module
-    if (process.argv[1].charAt(0) != '/' &&
-        !(/^http:\/\//).exec(process.argv[1])) {
+    if ('/\\'.indexOf(process.argv[1].charAt(0)) < 0
+        && process.argv[1].charAt(1) != ':'
+        && !(/^http:\/\//).exec(process.argv[1])) {    
       process.argv[1] = path.join(cwd, process.argv[1]);
     }
     // REMOVEME: nextTick should not be necessary. This hack to get

commit 48334dc0b1b8211f07b2fac5a00a41be58c0c3ee

Fix regression introduced in fe804d9b
It breaks argv[0] on posix systems, and makes it so that npm can't
determine whether node was run from an explicit location, or via "node",
so the configs default improperly.

If on windows, don't do this behavior.  On posix, go back to the old
behavior.

src/node.js
@@ -536,10 +536,15 @@
 
   var cwd = process.cwd();
   var path = requireNative('path');
+  var isWindows = process.platform === 'win32';
 
-  // Make process.argv[0] and process.argv[1] into full paths.
-  if ('/\\'.indexOf(process.argv[0].charAt(0)) < 0 
-      && process.argv[0].charAt(1) != ':') {
+  // Make process.argv[0] and process.argv[1] into full paths, but only
+  // touch argv[0] if it's not a system $PATH lookup.
+  // TODO: Make this work on Windows as well.  Note that "node" might
+  // execute cwd\node.exe, or some %PATH%\node.exe on Windows,
+  // and that every directory has its own cwd, so d:node.exe is valid.
+  var argv0 = process.argv[0];
+  if (!isWindows && argv0.indexOf('/') !== -1 && argv0.charAt(0) !== '/') {
     process.argv[0] = path.join(cwd, process.argv[0]);
   }
 