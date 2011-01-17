43d75952ffc01a5edab77f8b4a745ec54ba5f7c1
Bert Belder <bertbelder@gmail.com>
2011-01-17 23:38:34 +0100

    Windows: get rid of most NO_IMPL related macros

--- src/platform_win32.h
+++ src/platform_win32.h
@@ -3,26 +3,12 @@
 
 #include <windows.h>
 
-#define NO_IMPL(type, name, rv, args...)                                    \
-          type  name ( args ) {                                             \
-            HandleScope scope;                                              \
-            fprintf(stderr, "Not implemented: "#type" "#name"("#args")\n"); \
-            return rv;                                                      \
-          }
 
-#define RET_V8INT(value) \
-          scope.Close(Integer::New(value));
-#define RET_V8UNDEFINED \
-          Undefined()
-#define RET_V8TRUE \
-          True()
-#define RET_V8FALSE \
-          False()
-
-#define NO_IMPL_MSG(name...) \
-          fprintf(stderr, "Not implemented: %s\n", #name);
 
 namespace node {
+
+#define NO_IMPL_MSG(name...) \
+    fprintf(stderr, "Not implemented: %s\n", #name);
 
 void winapi_perror(const char* prefix);
 
