33e8e3d799f7b04991728218bb1c411e01ffcfe2
Ryan Dahl <ry@tinyclouds.org>
2011-01-31 16:38:05 -0800

    Add simpler failing tls throttle test

---
+++ test/simple/test-tls-throttle.js
@@ -0,0 +1,61 @@
+// Server sends a large string. Client counts bytes and pauses every few
+// seconds. Makes sure that pause and resume work properly.
+var common = require('../common');
+var assert = require('assert');
+var tls = require('tls');
+var fs = require('fs');
+
+
+var body = '';
+
+process.stdout.write('build body...');
+for (var i = 0; i < 1024*1024; i++) {
+  body += 'hello world\n';
+}
+process.stdout.write('done\n');
+
+
+var options = {
+  key: fs.readFileSync(common.fixturesDir + '/keys/agent2-key.pem'),
+  cert: fs.readFileSync(common.fixturesDir + '/keys/agent2-cert.pem')
+};
+
+var connections = 0;
+
+
+var server = tls.Server(options, function(socket) {
+  socket.end(body);
+  connections++;
+});
+
+var recvCount = 0;
+
+server.listen(common.PORT, function() {
+  var client = tls.connect(common.PORT);
+
+  client.on('data', function(d) {
+    recvCount += d.length;
+
+    client.pause();
+    process.nextTick(function () {
+      client.resume();
+    });
+  });
+
+
+  client.on('close', function() {
+    server.close();
+    clearTimeout(timeout);
+  });
+});
+
+
+var timeout = setTimeout(function() {
+  process.exit(1);
+}, 10*1000);
+
+
+process.on('exit', function() {
+  assert.equal(1, connections);
+  assert.equal(body.length, recvCount);
+});
