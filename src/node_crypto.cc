db993956d61dd1cff86787cf7b8d1661d3d23e30
Stefan Bühler <stbuehler@web.de>
2011-05-08 14:17:21 +0200

    Fix crypto hmac to accept binary keys + add test cases from rfc 2202 and 4231
    Fixes #324.
    Fixes #1027.
    
    Instead of converting buffers to strings and back again to char array
    directly use the buffer data in hmac_init (same as in hmac_update).

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -2386,15 +2386,25 @@ class Hmac : public ObjectWrap {
       return ThrowException(exception);
     }
 
-    char* buf = new char[len];
-    ssize_t written = DecodeWrite(buf, len, args[1], BINARY);
-    assert(written == len);
-
     String::Utf8Value hashType(args[0]->ToString());
 
-    bool r = hmac->HmacInit(*hashType, buf, len);
+    bool r;
 
-    delete [] buf;
+    if( Buffer::HasInstance(args[1])) {
+      Local<Object> buffer_obj = args[1]->ToObject();
+      char* buffer_data = Buffer::Data(buffer_obj);
+      size_t buffer_length = Buffer::Length(buffer_obj);
+
+      r = hmac->HmacInit(*hashType, buffer_data, buffer_length);
+    } else {
+      char* buf = new char[len];
+      ssize_t written = DecodeWrite(buf, len, args[1], BINARY);
+      assert(written == len);
+
+      r = hmac->HmacInit(*hashType, buf, len);
+
+      delete [] buf;
+    }
 
     if (!r) {
       return ThrowException(Exception::Error(String::New("hmac error")));
