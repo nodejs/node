1e6b72e8cbee0837dba3741f3358edf4e22fefb2
Ben Noordhuis <info@bnoordhuis.nl>
2011-06-25 14:53:28 +0200

    Test case for issue #1228: errno masked in fs.openSync().

--- test/simple/test-fs-open.js
+++ test/simple/test-fs-open.js
@@ -19,9 +19,21 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+var constants = require('constants');
 var common = require('../common');
 var assert = require('assert');
 var fs = require('fs');
+
+var caughtException = false;
+try {
+  // should throw ENOENT, not EBADF - see https://github.com/joyent/node/pull/1228
+  fs.openSync('/path/to/file/that/does/not/exist', 'r');
+}
+catch (e) {
+  assert.equal(e.errno, constants.ENOENT);
+  caughtException = true;
+}
+assert.ok(caughtException);
 
 var openFd;
 fs.open(__filename, 'r', function(err, fd) {
