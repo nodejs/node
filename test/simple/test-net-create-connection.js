c60cdcda4e39081fa56568df941aff1a4b56b39c
koichik <koichik@improvement.jp>
2011-07-05 00:34:58 +0900

    Fix net.Socket.connect argument parsing
    Fixes #1251.

--- test/simple/test-net-create-connection.js
+++ test/simple/test-net-create-connection.js
@@ -24,19 +24,27 @@ var assert = require('assert');
 var net = require('net');
 
 var tcpPort = common.PORT;
-var connectHappened = false;
+var clientConnected = 0;
+var serverConnected = 0;
 
 var server = net.createServer(function(socket) {
-  server.close();
   socket.end();
+  if (++serverConnected === 4) {
+    server.close();
+  }
 });
 server.listen(tcpPort, 'localhost', function() {
-  var client = net.createConnection(tcpPort, 'localhost', function() {
-    connectHappened = true;
-  });
+  function cb() {
+    ++clientConnected;
+  }
+
+  net.createConnection(tcpPort).on('connect', cb);
+  net.createConnection(tcpPort, 'localhost').on('connect', cb);
+  net.createConnection(tcpPort, cb);
+  net.createConnection(tcpPort, 'localhost', cb);
 });
 
 process.on('exit', function () {
-  assert.ok(connectHappened);
+  assert.equal(clientConnected, 4);
 });
 
