7fc835afe362ebd30a0dbec81d3360bd24525222
Ben Noordhuis <info@bnoordhuis.nl>
2011-07-12 00:30:24 +0200

    timers: handle negative or non-numeric timeout values
    Follows browser behaviour by scheduling the callback on the next tick.
    
    Fixes #593.

---
+++ test/simple/test-timers.js
@@ -0,0 +1,71 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+
+var inputs = [
+  undefined,
+  null,
+  true,
+  false,
+  '',
+  [],
+  {},
+  NaN,
+  +Infinity,
+  -Infinity,
+  (1.0 / 0.0),      // sanity check
+  parseFloat('x'),  // NaN
+  -10,
+  -1,
+  -0.5,
+  -0.0,
+  0,
+  0.0,
+  0.5,
+  1,
+  1.0,
+  10,
+  2147483648 // browser behaviour: timeouts > 2^31-1 run on next tick
+];
+
+var timeouts = [];
+var intervals = [];
+
+inputs.forEach(function(value, index) {
+  setTimeout(function() {
+    timeouts[index] = true;
+  }, value);
+
+  var handle = setInterval(function() {
+    clearInterval(handle); // disarm timer or we'll never finish
+    intervals[index] = true;
+  }, value);
+});
+
+process.on('exit', function() {
+  // assert that all timers have run
+  inputs.forEach(function(value, index) {
+    assert.equal(true, timeouts[index]);
+    assert.equal(true, intervals[index]);
+  });
+});
