1b3ebc32d7f64cf7a8b8a9cfc315d30bb74c04f6
Felix Geisendörfer <felix@debuggable.com>
2011-07-26 17:19:05 +0200

    Fix test-net-stream.js
    I broke this in 09ee293.

--- test/simple/test-net-stream.js
+++ test/simple/test-net-stream.js
@@ -22,14 +22,16 @@
 var common = require('../common');
 var assert = require('assert');
 
-var Stream = require('net').Stream;
+var net = require('net');
 
-var s = new Stream();
+var s = new net.Stream();
 
 // test that destroy called on a stream with a server only ever decrements the
 // server connection count once
 
-s.server = { connections: 10 }
+s.server = new net.Server();
+s.server.connections = 10;
+
 assert.equal(10, s.server.connections);
 s.destroy()
 assert.equal(9, s.server.connections);
