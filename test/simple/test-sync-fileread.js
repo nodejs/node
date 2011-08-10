c96ef84427e7010f21e547bce12888323e12551d
Bert Belder <bertbelder@gmail.com>
2011-08-10 19:54:36 +0200

    Revert "Make test-sync-fileread pass even when git cr/lf conversion is enabled"
    We'll solve this problem with a .gitattributes file.
    This reverts commit 27ef0b0903e348cc10d0c5d2ee2ef25aa1676440.

--- test/simple/test-sync-fileread.js
+++ test/simple/test-sync-fileread.js
@@ -24,6 +24,6 @@ var assert = require('assert');
 var path = require('path');
 var fs = require('fs');
 
-var fixture = path.join(__dirname, '../fixtures/x.bin');
+var fixture = path.join(__dirname, '../fixtures/x.txt');
 
 assert.equal('xyz\n', fs.readFileSync(fixture));
