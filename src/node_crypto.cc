320cf729db9ca75a17892fb2e3abd21176ff983b
Ben Noordhuis <info@bnoordhuis.nl>
2011-09-21 01:57:36 +0200

    crypto: fix delete of potentially uninitialized pointer

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -2512,6 +2512,7 @@ class Decipher : public ObjectWrap {
     int out_len;
     Local<Value> outString ;
 
+    out_value = NULL;
     int r = cipher->DecipherFinal(&out_value, &out_len, true);
 
     if (out_len == 0 || r == 0) {
