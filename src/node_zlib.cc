f1ca48535788d5e14193ba75e9019e4fd4d55306
Ben Noordhuis <info@bnoordhuis.nl>
2011-10-03 13:40:42 +0200

    zlib: fix assert message

--- src/node_zlib.cc
+++ src/node_zlib.cc
@@ -207,7 +207,7 @@ template <node_zlib_mode mode> class ZCtx : public ObjectWrap {
     HandleScope scope;
 
     assert(args.Length() == 4 &&
-           "init(level, windowBits, memLevel, strategy)");
+           "init(windowBits, level, memLevel, strategy)");
 
     ZCtx<mode> *ctx = ObjectWrap::Unwrap< ZCtx<mode> >(args.This());
 
