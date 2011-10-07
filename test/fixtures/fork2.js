26c08a3f35f251c86a881aa70a8e709216feb03d
Ryan Dahl <ry@tinyclouds.org>
2011-10-07 13:58:49 -0700

    Do load balancing test in test-child-process-fork2.

--- test/fixtures/fork2.js
+++ test/fixtures/fork2.js
@@ -1,9 +1,27 @@
 var assert = require('assert');
+var net = require('net');
+
+var connections = 0;
 
 process.on('message', function(m, server) {
   console.log('CHILD got message:', m);
   assert.ok(m.hello);
+
   assert.ok(server);
-  process.send({ gotHandle: true });
+  assert.ok(server instanceof net.Server);
+
+  // TODO need better API for this.
+  server._backlog = 9;
+
+  server.listen(function() {
+    process.send({ gotHandle: true });
+  });
+
+  server.on('connection', function(c) {
+    connections++;
+    console.log('CHILD got connection');
+    c.destroy();
+    process.send({ childConnections: connections });
+  });
 });
 
--- test/simple/test-child-process-fork2.js
+++ test/simple/test-child-process-fork2.js
@@ -3,25 +3,53 @@ var common = require('../common');
 var fork = require('child_process').fork;
 var net = require('net');
 
+var socketCloses = 0;
+var N = 10;
+
 var n = fork(common.fixturesDir + '/fork2.js');
 
 var messageCount = 0;
 
-var server = new net.Server();
+var server = new net.Server(function(c) {
+  console.log('PARENT got connection');
+  c.destroy();
+});
+
+// TODO need better API for this.
+server._backlog = 9;
+
 server.listen(common.PORT, function() {
   console.log('PARENT send child server handle');
   n.send({ hello: 'world' }, server);
 });
 
+function makeConnections() {
+  for (var i = 0; i < N; i++) {
+    var socket = net.connect(common.PORT, function() {
+      console.log("CLIENT connected");
+    });
+
+    socket.on("close", function() {
+      socketCloses++;
+      console.log("CLIENT closed " + socketCloses);
+      if (socketCloses == N) {
+        n.kill();
+        server.close();
+      }
+    });
+  }
+}
 
 n.on('message', function(m) {
   console.log('PARENT got message:', m);
-  assert.ok(m.gotHandle);
+  if (m.gotHandle) {
+    makeConnections();
+  }
   messageCount++;
-  n.kill();
-  server.close();
 });
 
 process.on('exit', function() {
-  assert.equal(1, messageCount);
+  assert.equal(10, socketCloses);
+  assert.ok(messageCount > 1);
 });
+
