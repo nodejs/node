7b4370e5f8d3a6faf9fc42c48ddd7a2137bea022
Ryan Dahl <ry@tinyclouds.org>
2011-10-12 16:18:42 -0700

    Fix test/pummel/test-watch-file.js

--- test/pummel/test-watch-file.js
+++ test/pummel/test-watch-file.js
@@ -19,9 +19,6 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// libuv-broken
-
-
 var common = require('../common');
 var assert = require('assert');
 
