d9bc8455cbda69df5c477f971176471cde45a5f6
Ryan Dahl <ry@tinyclouds.org>
2011-10-18 16:12:07 -0700

    Fix test-c-ares.js on Windows by disabling PTR test

--- test/simple/test-c-ares.js
+++ test/simple/test-c-ares.js
@@ -49,8 +49,12 @@ dns.lookup('ipv6.google.com', function(error, result, addressType) {
   assert.equal(6, addressType);
 });
 
-dns.resolve('127.0.0.1', 'PTR', function(error, domains) {
-  if (error) throw error;
-  assert.ok(Array.isArray(domains));
-});
-
+// Windows doesn't usually have an entry for localhost 127.0.0.1 in
+// C:\Windows\System32\drivers\etc\hosts
+// so we disable this test on Windows.
+if (process.platform != 'win32') {
+  dns.resolve('127.0.0.1', 'PTR', function(error, domains) {
+    if (error) throw error;
+    assert.ok(Array.isArray(domains));
+  });
+}
