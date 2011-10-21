fafb5848819d3a17ae54955467ed651685d98672
Ryan Dahl <ry@tinyclouds.org>
2011-10-21 10:21:23 -0700

    Fix race condition in test-dgram-pingpong

--- test/simple/test-dgram-pingpong.js
+++ test/simple/test-dgram-pingpong.js
@@ -71,8 +71,7 @@ function pingPongTest(port, host) {
         client.send(buf, 0, buf.length, port, 'localhost');
       } else {
         sent_final_ping = true;
-        client.send(buf, 0, buf.length, port, 'localhost');
-        process.nextTick(function() {
+        client.send(buf, 0, buf.length, port, 'localhost', function() {
           client.close();
         });
       }
