0a5708c3e99d49c46784cea0f579f85d02eaadd1
Igor Zinkovsky <igorzi@microsoft.com>
2011-10-25 13:47:21 -0700

    fix test-http-curl-chunk-problem.js and test-pipe-file-to-http.js on windows

--- test/common.js
+++ test/common.js
@@ -54,12 +54,8 @@ exports.indirectInstanceOf = function(obj, cls) {
 
 exports.ddCommand = function(filename, kilobytes) {
   if (process.platform == 'win32') {
-    // 'fsutil file createnew' cannot be used on an existing file. If it
-    // already exists delete it.
-    if (require('path').existsSync(filename)) {
-      require('fs').unlinkSync(filename);
-    }
-    return 'fsutil.exe file createnew "' + filename + '" ' + (kilobytes * 1024);
+    return '"' + process.argv[0] + '" "' + path.resolve(exports.fixturesDir,
+      'create-file.js') + '" "' + filename + '" ' + (kilobytes * 1024);
   } else {
     return 'dd if=/dev/zero of="' + filename + '" bs=1024 count=' + kilobytes;
   }
