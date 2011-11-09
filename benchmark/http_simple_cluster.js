4fc0fdc6ee8c41320f4d4ea74e4fa8a3ac12ed57
Ben Noordhuis <info@bnoordhuis.nl>
2011-11-09 17:32:39 +0100

    bench: start (NUM_CPUS-1) workers
    The master is a worker too so fork off one less worker.

--- benchmark/http_simple_cluster.js
+++ benchmark/http_simple_cluster.js
@@ -2,7 +2,8 @@ var cluster = require('cluster');
 var os = require('os');
 
 if (cluster.isMaster) {
-  for (var i = 0, n = os.cpus().length; i < n; ++i) cluster.fork();
+  console.log('master running on pid %d', process.pid);
+  for (var i = 1, n = os.cpus().length; i < n; ++i) cluster.fork();
 } else {
   require(__dirname + '/http_simple.js');
 }
