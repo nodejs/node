863f17f68c4cbac9c310603bb174bb641d521a15
Ryan Dahl <ry@tinyclouds.org>
2011-12-13 14:48:36 -0800

    Move uv loop initialization into isolate

--- src/node.cc
+++ src/node.cc
@@ -1891,8 +1891,7 @@ struct ThreadInfo {
 static void RunIsolate(void* arg) {
   ThreadInfo* ti = reinterpret_cast<ThreadInfo*>(arg);
 
-  uv_loop_t* loop = uv_loop_new();
-  Isolate* isolate = Isolate::New(loop);
+  Isolate* isolate = Isolate::New();
 
   StartThread(isolate, ti->argc_, ti->argv_);
   delete ti;
@@ -2780,7 +2779,7 @@ int Start(int argc, char *argv[]) {
 
   // Create the main node::Isolate object
   node::Isolate::Initialize();
-  Isolate* isolate = node::Isolate::New(uv_default_loop());
+  Isolate* isolate = node::Isolate::New();
   StartThread(isolate, argc, argv);
   isolate->Dispose();
 
