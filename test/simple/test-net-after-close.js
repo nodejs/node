a848a3efbfeb70181a95383e022474fdf2c83aae
koichik <koichik@improvement.jp>
2011-12-28 15:13:57 +0900

    net: fix Socket.pause null reference when called on a closed Stream
    Fixes #1980.

---
+++ test/simple/test-net-after-close.js
@@ -0,0 +1,52 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+var net = require('net');
+var closed = false;
+
+var server = net.createServer(function(s) {
+  s.end();
+});
+
+server.listen(common.PORT, function() {
+  var c = net.createConnection(common.PORT);
+  c.on('close', function() {
+    assert.strictEqual(c._handle, null);
+    closed = true;
+    assert.doesNotThrow(function() {
+      c.setNoDelay();
+      c.setKeepAlive();
+      c.bufferSize;
+      c.pause();
+      c.resume();
+      c.address();
+      c.remoteAddress;
+      c.remotePort;
+    });
+    server.close();
+  });
+});
+
+process.on('exit', function() {
+  assert(closed);
+});
