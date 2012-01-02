6f8839d2ac362ced42235a34a023af5e2c656501
Ben Noordhuis <info@bnoordhuis.nl>
2012-01-02 12:02:05 +0100

    crypto: add SecureContext.clearOptions() method
    SecureContext.setOptions() is backed by SSL_CTX_set_options() which, contrary to
    what the name suggests, is additive: it doesn't set options, it adds them to the
    already active options.
    
    Hence the need for SecureContext.clearOptions(), which lets you unset active
    options.

--- src/node_crypto.h
+++ src/node_crypto.h
@@ -66,6 +66,7 @@ class SecureContext : ObjectWrap {
   static v8::Handle<v8::Value> AddRootCerts(const v8::Arguments& args);
   static v8::Handle<v8::Value> SetCiphers(const v8::Arguments& args);
   static v8::Handle<v8::Value> SetOptions(const v8::Arguments& args);
+  static v8::Handle<v8::Value> ClearOptions(const v8::Arguments& args);
   static v8::Handle<v8::Value> SetSessionIdContext(const v8::Arguments& args);
   static v8::Handle<v8::Value> Close(const v8::Arguments& args);
 
