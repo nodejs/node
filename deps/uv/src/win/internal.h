247d0da188e70949a48739fc42dbf5da4d7b8b40
Igor Zinkovsky <igorzi@microsoft.com>
2012-01-11 19:34:56 -0800

    update libuv to 855764406e fixes #2521

--- deps/uv/src/win/internal.h
+++ deps/uv/src/win/internal.h
@@ -284,10 +284,8 @@ void uv_fs_event_endgame(uv_loop_t* loop, uv_fs_event_t* handle);
 
 /* Utils */
 int uv_parent_pid();
-
-
+void uv_filetime_to_time_t(FILETIME* file_time,  time_t* stat_time);
 void uv_fatal_error(const int errorno, const char* syscall);
-
 uv_err_code uv_translate_sys_error(int sys_errno);
 
 #define SET_REQ_STATUS(req, status)                                     \
--- deps/uv/src/win/util.c
+++ deps/uv/src/win/util.c
@@ -23,6 +23,7 @@
 #include <direct.h>
 #include <malloc.h>
 #include <string.h>
+#include <time.h>
 
 #include "uv.h"
 #include "internal.h"
@@ -235,4 +236,26 @@ int uv_parent_pid() {
 
   CloseHandle(handle);
   return parent_pid;
+}
+
+
+void uv_filetime_to_time_t(FILETIME* file_time, time_t* stat_time) {
+  FILETIME local_time;
+  SYSTEMTIME system_time;
+  struct tm time;
+
+  if ((file_time->dwLowDateTime || file_time->dwHighDateTime) &&
+      FileTimeToLocalFileTime(file_time, &local_time)         &&
+      FileTimeToSystemTime(&local_time, &system_time)) {
+    time.tm_year = system_time.wYear - 1900;
+    time.tm_mon = system_time.wMonth - 1;
+    time.tm_mday = system_time.wDay;
+    time.tm_hour = system_time.wHour;
+    time.tm_min = system_time.wMinute;
+    time.tm_sec = system_time.wSecond;
+
+    *stat_time = mktime(&time);
+  } else {
+    *stat_time = 0;
+  }
 }
