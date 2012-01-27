ff0f0aeb401765646fefd9bbdc0f2a68d1ad342c
isaacs <i@izs.me>
2012-01-26 17:14:16 -0800

    Fix #2507 Raise errors less agressively when destroying stdio streams
    Also, if an error is already provided, then raise the provided
    error, rather than throwing it with a less helpful 'stdout cannot
    be closed' message.
    
    This is important for properly handling EPIPEs.

---
+++ test/fixtures/catch-stdout-error.js
@@ -0,0 +1,38 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+function write() {
+  try {
+    process.stdout.write('Hello, world\n');
+  } catch (ex) {
+    throw new Error('this should never happen');
+  }
+  process.nextTick(function () {
+    write();
+  });
+}
+
+process.stdout.on('error', function (er) {
+  console.error(JSON.stringify(er));
+  process.exit(42);
+});
+
+write();
