7543c38dca2b4c64711f5667b82d80ffab3fd6a2
Ben Noordhuis <info@bnoordhuis.nl>
2012-02-05 23:30:14 +0100

    Revert "Add explicit v8 locker"
    This reverts commit 11d1eca9f3c465045828e3a00b103620c8253258.
    
    It sporadically (but reproducibly) triggers an assert inside V8:
    
      Fatal error in /path/to/node/deps/v8/src/isolate.cc, line 1857
      CHECK(CurrentPerIsolateThreadData()->isolate_ == this) failed
    
    Needs further investigation.

--- src/node.cc
+++ src/node.cc
@@ -2633,7 +2633,6 @@ int Start(int argc, char *argv[]) {
   argv = Init(argc, argv);
 
   v8::V8::Initialize();
-  v8::Locker locker;
   v8::HandleScope handle_scope;
 
   // Create the one and only Context.
