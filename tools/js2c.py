bea2e15864fb41c161dd47a0184ae07e83b7ce76
Nathan Rajlich <nathan@tootallnate.net>
2012-03-13 18:31:04 -0700

    js2c: fix to support files other than ones ending with 2 char extensions
    Previously this was basically hard-coded for *.js files, but now we
    need to include the 'config.gypi' file in there as well.

--- tools/js2c.py
+++ tools/js2c.py
@@ -288,7 +288,7 @@ def JS2C(source, target):
     lines = ExpandMacros(lines, macros)
     lines = CompressScript(lines, do_jsmin)
     data = ToCArray(s, lines)
-    id = (os.path.split(str(s))[1])[:-3]
+    id = os.path.basename(str(s)).split('.')[0]
     if delay: id = id[:-6]
     if delay:
       delay_ids.append((id, len(lines)))
