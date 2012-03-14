d2fba2bf35e3d55c55590116e7b7fd6951bb1fdb
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2012-03-14 11:21:14 +0900

    test: Fix path to require hello-world module

--- test/addons/hello-world/test.js
+++ test/addons/hello-world/test.js
@@ -1,4 +1,4 @@
 var assert = require('assert');
-var binding = require('./out/Release/binding');
+var binding = require('./build/Release/binding');
 assert.equal('world', binding.hello());
 console.log('binding.hello() =', binding.hello());
