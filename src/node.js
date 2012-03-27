698e795a5ffb617c5a3e3e79de7dfdd0ce637695
Nathan Rajlich <nathan@tootallnate.net>
2012-03-26 19:05:32 -0700

    repl: fix 'terminal' mode autodetection on global repls
    Fixes test/simple/test-force-repl.js

--- src/node.js
+++ src/node.js
@@ -121,12 +121,14 @@
       // If -i or --interactive were passed, or stdin is a TTY.
       if (process._forceRepl || NativeModule.require('tty').isatty(0)) {
         // REPL
-        var repl = Module.requireRepl().start({
-          prompt: '> ',
-          terminal: !parseInt(process.env['NODE_NO_READLINE'], 10),
+        var opts = {
           useGlobal: true,
           ignoreUndefined: false
-        });
+        };
+        if (parseInt(process.env['NODE_NO_READLINE'], 10)) {
+          opts.terminal = false;
+        }
+        var repl = Module.requireRepl().start(opts);
         repl.on('exit', function() {
           process.exit();
         });
