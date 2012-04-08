70005be4ffdcfed0ac371873fa79a308ae4cc546
ssuda <sambasivarao@gmail.com>
2012-04-08 20:54:53 +0530

    Fixing ClientRequest setTimeout EventEmitter Leak
    This will fix #3068

---
+++ test/simple/test-http-timeout.js
@@ -0,0 +1,63 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+var common = require('../common');
+var assert = require('assert');
+
+var http = require('http');
+
+var port = 12345;
+var server = http.createServer(function(req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('OK');
+});
+
+var agent = new http.Agent({maxSockets: 1});
+
+server.listen(port, function() {
+
+  for (var i = 0; i < 11; ++i) {
+    createRequest().end();
+  }
+
+  function callback(){}
+
+  var count = 0;
+
+  function createRequest() {
+    var req = http.request({port: port, path: '/', agent: agent}, function(res) {
+
+      req.clearTimeout(callback);
+
+      res.on('end', function() {
+        count++;
+
+        if (count == 11) {
+          server.close();
+        }
+      })
+    });
+
+    req.setTimeout(1000, callback);
+    return req;
+  }
+});
