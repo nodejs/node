6cbed959e6dc61991e50bb95b3a9b1f530a50c0d
Ben Noordhuis <info@bnoordhuis.nl>
2012-04-11 00:47:52 +0200

    test: fix openssl tests
    Don't assume that the libcrypto and libssl that we're linked against is the same
    version as the openssl command line tool. This is important because the tool has
    a bug in all pre-1.0.0 versions that makes it unusable for these tests.

--- test/simple/test-tls-securepair-client.js
+++ test/simple/test-tls-securepair-client.js
@@ -19,18 +19,10 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
-
-
-// There is a bug with 'openssl s_server' which makes it not flush certain
-// important events to stdout when done over a pipe. Therefore we skip this
-// test for all openssl versions less than 1.0.0.
-if (!process.versions.openssl ||
-    parseInt(process.versions.openssl[0]) < 1) {
-  console.error('Skipping due to old OpenSSL version.');
+if (!process.versions.openssl) {
+  console.error('Skipping because node compiled without OpenSSL.');
   process.exit(0);
 }
-
 
 var common = require('../common');
 var join = require('path').join;
@@ -39,134 +31,153 @@ var assert = require('assert');
 var fs = require('fs');
 var crypto = require('crypto');
 var tls = require('tls');
+var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 
-// FIXME: Avoid the common PORT as this test currently hits a C-level
-// assertion error with node_g. The program aborts without HUPing
-// the openssl s_server thus causing many tests to fail with
-// EADDRINUSE.
-var PORT = common.PORT + 5;
+maybe(test);
 
-var connections = 0;
-
-var keyfn = join(common.fixturesDir, 'agent.key');
-var key = fs.readFileSync(keyfn).toString();
-
-var certfn = join(common.fixturesDir, 'agent.crt');
-var cert = fs.readFileSync(certfn).toString();
-
-var server = spawn('openssl', ['s_server',
-                               '-accept', PORT,
-                               '-cert', certfn,
-                               '-key', keyfn]);
-server.stdout.pipe(process.stdout);
-server.stderr.pipe(process.stdout);
-
-
-var state = 'WAIT-ACCEPT';
-
-var serverStdoutBuffer = '';
-server.stdout.setEncoding('utf8');
-server.stdout.on('data', function(s) {
-  serverStdoutBuffer += s;
-  console.error(state);
-  switch (state) {
-    case 'WAIT-ACCEPT':
-      if (/ACCEPT/g.test(serverStdoutBuffer)) {
-        // Give s_server half a second to start up.
-        setTimeout(startClient, 500);
-        state = 'WAIT-HELLO';
-      }
-      break;
-
-    case 'WAIT-HELLO':
-      if (/hello/g.test(serverStdoutBuffer)) {
-
-        // End the current SSL connection and exit.
-        // See s_server(1ssl).
-        server.stdin.write('Q');
-
-        state = 'WAIT-SERVER-CLOSE';
-      }
-      break;
-
-    default:
-      break;
-  }
-});
-
-
-var timeout = setTimeout(function() {
-  server.kill();
-  process.exit(1);
-}, 5000);
-
-var gotWriteCallback = false;
-var serverExitCode = -1;
-
-server.on('exit', function(code) {
-  serverExitCode = code;
-  clearTimeout(timeout);
-});
-
-
-function startClient() {
-  var s = new net.Stream();
-
-  var sslcontext = crypto.createCredentials({key: key, cert: cert});
-  sslcontext.context.setCiphers('RC4-SHA:AES128-SHA:AES256-SHA');
-
-  var pair = tls.createSecurePair(sslcontext, false);
-
-  assert.ok(pair.encrypted.writable);
-  assert.ok(pair.cleartext.writable);
-
-  pair.encrypted.pipe(s);
-  s.pipe(pair.encrypted);
-
-  s.connect(PORT);
-
-  s.on('connect', function() {
-    console.log('client connected');
-  });
-
-  pair.on('secure', function() {
-    console.log('client: connected+secure!');
-    console.log('client pair.cleartext.getPeerCertificate(): %j',
-                pair.cleartext.getPeerCertificate());
-    console.log('client pair.cleartext.getCipher(): %j',
-                pair.cleartext.getCipher());
-    setTimeout(function() {
-      pair.cleartext.write('hello\r\n', function() {
-        gotWriteCallback = true;
-      });
-    }, 500);
-  });
-
-  pair.cleartext.on('data', function(d) {
-    console.log('cleartext: %s', d.toString());
-  });
-
-  s.on('close', function() {
-    console.log('client close');
-  });
-
-  pair.encrypted.on('error', function(err) {
-    console.log('encrypted error: ' + err);
-  });
-
-  s.on('error', function(err) {
-    console.log('socket error: ' + err);
-  });
-
-  pair.on('error', function(err) {
-    console.log('secure error: ' + err);
+// There is a bug with 'openssl s_server' which makes it not flush certain
+// important events to stdout when done over a pipe. Therefore we skip this
+// test for all openssl versions less than 1.0.0.
+function maybe(cb) {
+  exec('openssl version', function(err, data) {
+    if (err) throw err;
+    if (/OpenSSL 0\./.test(data)) {
+      console.error('Skipping due to old OpenSSL version.');
+      return;
+    }
+    cb();
   });
 }
 
+function test() {
+  // FIXME: Avoid the common PORT as this test currently hits a C-level
+  // assertion error with node_g. The program aborts without HUPing
+  // the openssl s_server thus causing many tests to fail with
+  // EADDRINUSE.
+  var PORT = common.PORT + 5;
 
-process.on('exit', function() {
-  assert.equal(0, serverExitCode);
-  assert.equal('WAIT-SERVER-CLOSE', state);
-  assert.ok(gotWriteCallback);
-});
+  var connections = 0;
+
+  var keyfn = join(common.fixturesDir, 'agent.key');
+  var key = fs.readFileSync(keyfn).toString();
+
+  var certfn = join(common.fixturesDir, 'agent.crt');
+  var cert = fs.readFileSync(certfn).toString();
+
+  var server = spawn('openssl', ['s_server',
+                                 '-accept', PORT,
+                                 '-cert', certfn,
+                                 '-key', keyfn]);
+  server.stdout.pipe(process.stdout);
+  server.stderr.pipe(process.stdout);
+
+
+  var state = 'WAIT-ACCEPT';
+
+  var serverStdoutBuffer = '';
+  server.stdout.setEncoding('utf8');
+  server.stdout.on('data', function(s) {
+    serverStdoutBuffer += s;
+    console.error(state);
+    switch (state) {
+      case 'WAIT-ACCEPT':
+        if (/ACCEPT/g.test(serverStdoutBuffer)) {
+          // Give s_server half a second to start up.
+          setTimeout(startClient, 500);
+          state = 'WAIT-HELLO';
+        }
+        break;
+
+      case 'WAIT-HELLO':
+        if (/hello/g.test(serverStdoutBuffer)) {
+
+          // End the current SSL connection and exit.
+          // See s_server(1ssl).
+          server.stdin.write('Q');
+
+          state = 'WAIT-SERVER-CLOSE';
+        }
+        break;
+
+      default:
+        break;
+    }
+  });
+
+
+  var timeout = setTimeout(function() {
+    server.kill();
+    process.exit(1);
+  }, 5000);
+
+  var gotWriteCallback = false;
+  var serverExitCode = -1;
+
+  server.on('exit', function(code) {
+    serverExitCode = code;
+    clearTimeout(timeout);
+  });
+
+
+  function startClient() {
+    var s = new net.Stream();
+
+    var sslcontext = crypto.createCredentials({key: key, cert: cert});
+    sslcontext.context.setCiphers('RC4-SHA:AES128-SHA:AES256-SHA');
+
+    var pair = tls.createSecurePair(sslcontext, false);
+
+    assert.ok(pair.encrypted.writable);
+    assert.ok(pair.cleartext.writable);
+
+    pair.encrypted.pipe(s);
+    s.pipe(pair.encrypted);
+
+    s.connect(PORT);
+
+    s.on('connect', function() {
+      console.log('client connected');
+    });
+
+    pair.on('secure', function() {
+      console.log('client: connected+secure!');
+      console.log('client pair.cleartext.getPeerCertificate(): %j',
+                  pair.cleartext.getPeerCertificate());
+      console.log('client pair.cleartext.getCipher(): %j',
+                  pair.cleartext.getCipher());
+      setTimeout(function() {
+        pair.cleartext.write('hello\r\n', function() {
+          gotWriteCallback = true;
+        });
+      }, 500);
+    });
+
+    pair.cleartext.on('data', function(d) {
+      console.log('cleartext: %s', d.toString());
+    });
+
+    s.on('close', function() {
+      console.log('client close');
+    });
+
+    pair.encrypted.on('error', function(err) {
+      console.log('encrypted error: ' + err);
+    });
+
+    s.on('error', function(err) {
+      console.log('socket error: ' + err);
+    });
+
+    pair.on('error', function(err) {
+      console.log('secure error: ' + err);
+    });
+  }
+
+
+  process.on('exit', function() {
+    assert.equal(0, serverExitCode);
+    assert.equal('WAIT-SERVER-CLOSE', state);
+    assert.ok(gotWriteCallback);
+  });
+}
