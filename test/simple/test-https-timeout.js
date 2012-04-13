48d52d85c3e26e91e6e8f084f89af0a7c3f3ec65
ssuda <sambasivarao@gmail.com>
2012-04-14 05:05:28 +0530

    http, https: fix .setTimeout()
    Fixes #3107.

---
+++ test/simple/test-https-timeout.js
@@ -0,0 +1,75 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+if (!process.versions.openssl) {
+  console.error('Skipping because node compiled without OpenSSL.');
+  process.exit(0);
+}
+
+var common = require('../common');
+var assert = require('assert');
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var https = require('https');
+
+var options = {
+  key: fs.readFileSync(common.fixturesDir + '/keys/agent1-key.pem'),
+  cert: fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem')
+};
+
+// a server that never replies
+var server = https.createServer(options, function() {
+  console.log('Got request.  Doing nothing.');
+}).listen(common.PORT);
+
+var req = https.request({
+  host: 'localhost',
+  port: common.PORT,
+  path: '/',
+  method: 'GET'
+});
+req.end();
+req.on('response', function(res) {
+  console.log('got response');
+});
+
+req.on('socket', function() {
+  console.log('got a socket');
+
+  req.socket.on('connect', function() {
+    console.log('socket connected');
+  });
+
+  setTimeout(function() {
+    throw new Error('Did not get timeout event');
+  }, 200);
+});
+
+req.setTimeout(10);
+
+req.on('timeout', function() {
+  console.log('timeout occurred outside');
+  req.destroy();
+  server.close();
+  process.exit(0);
+});
