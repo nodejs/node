9b42d7daaf2685d967b47e56c155225756a8cfec
Ben Noordhuis <info@bnoordhuis.nl>
2012-05-14 17:34:33 +0200

    test: add https + .pfx client/server test

---
+++ test/simple/test-https-pfx.js
@@ -0,0 +1,56 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+var https = require('https');
+var fs = require('fs');
+
+var pfx = fs.readFileSync(common.fixturesDir + '/test_cert.pfx');
+
+var options = {
+  host: '127.0.0.1',
+  port: common.PORT,
+  path: '/',
+  pfx: pfx,
+  passphrase: 'sample',
+  requestCert: true
+};
+
+var server = https.createServer(options, function(req, res) {
+  assert.equal(req.socket.authorized, false); // not a client cert
+  assert.equal(req.socket.authorizationError, 'UNABLE_TO_GET_ISSUER_CERT');
+  res.writeHead(200);
+  res.end('OK');
+});
+
+server.listen(options.port, options.host, function() {
+  var data = '';
+
+  https.get(options, function(res) {
+    res.on('data', function(data_) { data += data_ });
+    res.on('end', function() { server.close() });
+  });
+
+  process.on('exit', function() {
+    assert.equal(data, 'OK');
+  });
+});
