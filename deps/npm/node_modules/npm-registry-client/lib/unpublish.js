427662c6e9264378fccff4b7702cbe3be3f8f4b8
isaacs <i@izs.me>
2012-06-11 09:30:44 -0700

    Do not gitignore npm's node_modules

---
+++ deps/npm/node_modules/npm-registry-client/lib/unpublish.js
@@ -0,0 +1,103 @@
+
+// fetch the data
+// modify to remove the version in question
+// If no versions remaining, then DELETE
+// else, PUT the modified data
+// delete the tarball
+
+module.exports = unpublish
+
+var semver = require("semver")
+  , url = require("url")
+  , chain = require("slide").chain
+
+function unpublish (name, ver, cb) {
+  if (typeof cb !== "function") cb = ver, ver = null
+
+  this.get(name, null, -1, true, function (er, data) {
+    if (er) {
+      this.log.info("unpublish", name+" not published")
+      return cb()
+    }
+    // remove all if no version specified
+    if (!ver) {
+      this.log.info("unpublish", "No version specified, removing all")
+      return this.request("DELETE", name+'/-rev/'+data._rev, cb)
+    }
+
+    var versions = data.versions || {}
+      , versionPublic = versions.hasOwnProperty(ver)
+
+    if (!versionPublic) {
+      this.log.info("unpublish", name+"@"+ver+" not published")
+    } else {
+      var dist = versions[ver].dist
+      this.log.verbose("unpublish", "removing attachments", dist)
+    }
+
+    delete versions[ver]
+    // if it was the only version, then delete the whole package.
+    if (!Object.keys(versions).length) {
+      this.log.info("unpublish", "No versions remain, removing entire package")
+      return this.request("DELETE", name+"/-rev/"+data._rev, cb)
+    }
+
+    if (!versionPublic) return cb()
+
+    var latestVer = data["dist-tags"].latest
+    for (var tag in data["dist-tags"]) {
+      if (data["dist-tags"][tag] === ver) delete data["dist-tags"][tag]
+    }
+
+    if (latestVer === ver) {
+      data["dist-tags"].latest =
+        Object.getOwnPropertyNames(versions).sort(semver.compare).pop()
+    }
+
+    var rev = data._rev
+    delete data._revisions
+    delete data._attachments
+    var cb_ = detacher.call(this, data, dist, cb)
+    this.request("PUT", name+"/-rev/"+rev, data, function (er) {
+      if (er) {
+        this.log.error("unpublish", "Failed to update data")
+      }
+      cb_(er)
+    }.bind(this))
+  }.bind(this))
+}
+
+function detacher (data, dist, cb) {
+  return function (er) {
+    if (er) return cb(er)
+    this.get(data.name, function (er, data) {
+      if (er) return cb(er)
+
+      var tb = url.parse(dist.tarball)
+
+      detach.call(this, data, tb.pathname, data._rev, function (er) {
+        if (er || !dist.bin) return cb(er)
+        chain(Object.keys(dist.bin).map(function (bt) {
+          return function (cb) {
+            var d = dist.bin[bt]
+            detach.call(this, data, url.parse(d.tarball).pathname, null, cb)
+          }.bind(this)
+        }, this), cb)
+      }.bind(this))
+    }.bind(this))
+  }.bind(this)
+}
+
+function detach (data, path, rev, cb) {
+  if (rev) {
+    path += "/-rev/" + rev
+    this.log.info("detach", path)
+    return this.request("DELETE", path, cb)
+  }
+  this.get(data.name, function (er, data) {
+    rev = data._rev
+    if (!rev) return cb(new Error(
+      "No _rev found in "+data._id))
+    detach.call(this, data, path, rev, cb)
+  }.bind(this))
+}
---
+++ deps/npm/node_modules/npmlog/log.js
@@ -0,0 +1,154 @@
+var EE = require('events').EventEmitter
+var log = exports = module.exports = new EE
+var util = require('util')
+
+var ansi = require('ansi')
+log.cursor = ansi(process.stderr)
+log.stream = process.stderr
+
+// by default, let ansi decide based on tty-ness.
+var colorEnabled = undefined
+log.enableColor = function () {
+  colorEnabled = true
+  this.cursor.enabled = true
+}
+log.disableColor = function () {
+  colorEnabled = false
+  this.cursor.enabled = false
+}
+
+// default level
+log.level = 'info'
+
+// temporarily stop emitting, but don't drop
+log.pause = function () {
+  this._paused = true
+}
+
+log.resume = function () {
+  if (!this._paused) return
+  this._paused = false
+
+  var b = this._buffer
+  this._buffer = []
+  b.forEach(function (m) {
+    this.emitLog(m)
+  }, this)
+}
+
+log._buffer = []
+
+var id = 0
+log.record = []
+log.maxRecordSize = 10000
+log.log = function (lvl, prefix, message) {
+  var l = this.levels[lvl]
+  if (l === undefined) {
+    return this.emit('error', new Error(util.format(
+      'Undefined log level: %j', lvl)))
+  }
+
+  var a = new Array(arguments.length - 2)
+  var stack = null
+  for (var i = 2; i < arguments.length; i ++) {
+    var arg = a[i-2] = arguments[i]
+
+    // resolve stack traces to a plain string.
+    if (typeof arg === 'object' && arg &&
+        (arg instanceof Error) && arg.stack) {
+      arg.stack = stack = arg.stack + ''
+    }
+  }
+  if (stack) a.unshift(stack + '\n')
+  message = util.format.apply(util, a)
+
+  var m = { id: id++,
+            level: lvl,
+            prefix: String(prefix || ''),
+            message: message,
+            messageRaw: a }
+
+  this.emit('log', m)
+  this.emit('log.' + lvl, m)
+  if (m.prefix) this.emit(m.prefix, m)
+
+  this.record.push(m)
+  var mrs = this.maxRecordSize
+  var n = this.record.length - mrs
+  if (n > mrs / 10) {
+    var newSize = Math.floor(mrs * 0.9)
+    this.record = this.record.slice(-1 * newSize)
+  }
+
+  this.emitLog(m)
+}
+
+log.emitLog = function (m) {
+  if (this._paused) {
+    this._buffer.push(m)
+    return
+  }
+  var l = this.levels[m.level]
+  if (l === undefined) return
+  if (l < this.levels[this.level]) return
+  if (l > 0 && !isFinite(l)) return
+
+  var style = log.style[m.level]
+  var disp = log.disp[m.level] || m.level
+  m.message.split(/\r?\n/).forEach(function (line) {
+    if (this.heading) {
+      this.write(this.heading, this.headingStyle)
+      this.write(' ')
+    }
+    this.write(disp, log.style[m.level])
+    var p = m.prefix || ''
+    if (p) this.write(' ')
+    this.write(p, this.prefixStyle)
+    this.write(' ' + line + '\n')
+  }, this)
+}
+
+log.write = function (msg, style) {
+  if (!this.cursor) return
+  if (this.stream !== this.cursor.stream) {
+    this.cursor = ansi(this.stream, { enabled: colorEnabled })
+  }
+
+  style = style || {}
+  if (style.fg) this.cursor.fg[style.fg]()
+  if (style.bg) this.cursor.bg[style.bg]()
+  if (style.bold) this.cursor.bold()
+  if (style.underline) this.cursor.underline()
+  if (style.inverse) this.cursor.inverse()
+  if (style.beep) this.cursor.beep()
+  this.cursor.write(msg).reset()
+}
+
+log.addLevel = function (lvl, n, style, disp) {
+  if (!disp) disp = lvl
+  this.levels[lvl] = n
+  this.style[lvl] = style
+  if (!this[lvl]) this[lvl] = function () {
+    var a = new Array(arguments.length + 1)
+    a[0] = lvl
+    for (var i = 0; i < arguments.length; i ++) {
+      a[i + 1] = arguments[i]
+    }
+    return this.log.apply(this, a)
+  }
+  this.disp[lvl] = disp
+}
+
+log.prefixStyle = { fg: 'magenta' }
+log.headingStyle = { fg: 'white', bg: 'black' }
+
+log.style = {}
+log.levels = {}
+log.disp = {}
+log.addLevel('silly', -Infinity, { inverse: true }, 'sill')
+log.addLevel('verbose', 1000, { fg: 'blue', bg: 'black' }, 'verb')
+log.addLevel('info', 2000, { fg: 'green' })
+log.addLevel('http', 3000, { fg: 'green', bg: 'black' })
+log.addLevel('warn', 4000, { fg: 'black', bg: 'red' }, 'WARN')
+log.addLevel('error', 5000, { fg: 'red', bg: 'black' }, 'ERR!')
+log.addLevel('silent', Infinity)
