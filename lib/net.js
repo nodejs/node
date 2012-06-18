nodejs/node

commit de65ba7aba0e48a846e8cad86278f51372d3de23

net.Socket: Delay pause/resume until after connect
Fix #3118

lib/net.js
@@ -236,6 +236,12 @@ Object.defineProperty(Socket.prototype, 'bufferSize', {
 
 
 Socket.prototype.pause = function() {
+  if (this._paused) return;
+  this._paused = true;
+  if (this._connecting) {
+    // will actually pause once the handle is established.
+    return;
+  }
   if (this._handle) {
     this._handle.readStop();
   }
@@ -243,6 +249,12 @@ Socket.prototype.pause = function() {
 
 
 Socket.prototype.resume = function() {
+  if (!this._paused) return;
+  this._paused = false;
+  if (this._connecting) {
+    // will actually resume once the handle is established.
+    return;
+  }
   if (this._handle) {
     this._handle.readStart();
   }
@@ -732,6 +744,12 @@ function afterConnect(status, handle, req, readable, writable) {
 
   assert.ok(self._connecting);
   self._connecting = false;
+
+  // now that we're connected, process any pending pause state.
+  if (self._paused) {
+    self._paused = false;
+    self.pause();
+  }
 
   if (status == 0) {
     self.readable = readable;

test/simple/test-net-socket-pause-resume-immediate.js
@@ -1,0 +1,30 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+var net = require('net');
+
+var sock = net.connect(1234, 'localhost');
+sock.pause();
+sock.resume();
+console.log('ok');
+process.exit(0);

commit 1df222f179f3c7e6181a319ecc90a1e0fb0a2975

Fix breakage introduced in de65ba7

lib/net.js
@@ -236,7 +236,6 @@ Object.defineProperty(Socket.prototype, 'bufferSize', {
 
 
 Socket.prototype.pause = function() {
-  if (this._paused) return;
   this._paused = true;
   if (this._connecting) {
     // will actually pause once the handle is established.
@@ -249,7 +248,6 @@ Socket.prototype.pause = function() {
 
 
 Socket.prototype.resume = function() {
-  if (!this._paused) return;
   this._paused = false;
   if (this._connecting) {
     // will actually resume once the handle is established.