3bab12197fe6d25f1bd0f40d5e5e83ad0d9276c8
Bert Belder <bertbelder@gmail.com>
2012-06-22 15:11:17 +0200

    windows: fix/improve nodejsvars.bat
    - Fix: it didn't work for the x86 version of node
    - Also add NPM to path
    - Also print node version
    - Don't change dir to home dir if not started from the start menu

--- tools/msvs/nodejsvars.bat
+++ tools/msvs/nodejsvars.bat
@@ -1,13 +1,16 @@
 @echo off
 
-@rem Ensure this Node.js is first in the PATH
-set PATH=%~dp0;%PATH%
+rem Ensure this Node.js and NPM are first in the PATH
+set PATH=%APPDATA%\npm;%~dp0;%PATH%
 
-@rem Figure out architecture and print it.
+rem Figure out node version and architecture and print it.
 setlocal
-for /F "usebackq delims=" %%v in (`"%~dp0"node.exe -p -e process.arch`) do set arch=%%v
-echo Your environment has been set up for using Node.js (%arch%) and NPM
+pushd "%~dp0"
+set print_version=.\node.exe -p -e "process.versions.node + ' (' + process.arch + ')'"
+for /F "usebackq delims=" %%v in (`%print_version%`) do set version=%%v
+echo Your environment has been set up for using Node.js %version% and NPM
+popd
 endlocal
 
-@rem Go to the user's home directory
-cd /d %HOMEDRIVE%"%HOMEPATH%"
+rem If we're in the node.js directory, change to the user's home dir.
+if "%CD%\"=="%~dp0" cd /d "%HOMEDRIVE%%HOMEPATH%"
