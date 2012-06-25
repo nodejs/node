6003912f81c4658f2bb77b33c1421a3207615744
Ben Noordhuis <info@bnoordhuis.nl>
2012-06-25 16:34:59 +0200

    test: use RC4-MD5 cipher in tls test
    NULL-MD5 is not always compiled into openssl but RC4-MD5 should always be
    available.
    
    Fixes #3531.

--- test/simple/test-tls-set-ciphers.js
+++ test/simple/test-tls-set-ciphers.js
@@ -33,7 +33,7 @@ if (process.platform === 'win32') {
 var options = {
   key: fs.readFileSync(common.fixturesDir + '/keys/agent2-key.pem'),
   cert: fs.readFileSync(common.fixturesDir + '/keys/agent2-cert.pem'),
-  ciphers: 'NULL-MD5' // it's ultra-fast!
+  ciphers: 'RC4-MD5'
 };
 
 var reply = 'I AM THE WALRUS'; // something recognizable
@@ -51,8 +51,8 @@ var server = tls.createServer(options, function(conn) {
 });
 
 server.listen(common.PORT, '127.0.0.1', function() {
-  var cmd = 'openssl s_client -cipher NULL-MD5 -connect 127.0.0.1:' +
-            common.PORT;
+  var cmd = 'openssl s_client -cipher ' + options.ciphers +
+            ' -connect 127.0.0.1:' + common.PORT;
 
   exec(cmd, function(err, stdout, stderr) {
     if (err) throw err;
