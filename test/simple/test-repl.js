9126dd2d903f3cf842f0c11a8349d794dde68e98
Nathan Rajlich <nathan@tootallnate.net>
2012-07-04 11:51:24 -0700

    repl: fix passing an empty line inserting "undefined" into the buffer
    There was a possiblity of insering the string "undefined" into the repl's
    command buffer, which would cause interesting results while evaluating.

--- test/simple/test-repl.js
+++ test/simple/test-repl.js
@@ -152,7 +152,9 @@ function error_test() {
     { client: client_unix, send: 'npm install foobar',
       expect: expect_npm },
     { client: client_unix, send: '(function () {\n\nreturn 1;\n})()',
-      expect: '1' }
+      expect: '1' },
+    { client: client_unix, send: '{\n\na: 1\n}',
+      expect: '{ a: 1 }' }
   ]);
 }
 
