8bec26122d6de0b230f74731c1d09da267c95add
Ben Noordhuis <info@bnoordhuis.nl>
2012-08-29 22:53:00 +0200

    tls, https: throw exception on missing key/cert
    Throw an exception in the tls.Server constructor when the options object
    doesn't contain either a PFX or a key/certificate combo.
    
    Said change exposed a bug in simple/test-tls-junk-closes-server. Addressed.
    
    Fixes #3941.

--- test/simple/test-tls-server-missing-options.js
+++ test/simple/test-tls-server-missing-options.js
@@ -25,30 +25,14 @@ if (!process.versions.openssl) {
 }
 
 var common = require('../common');
+var assert = require('assert');
+var https = require('https');
 var tls = require('tls');
-var fs = require('fs');
-var net = require('net');
 
-var options = {
-  key: fs.readFileSync(common.fixturesDir + '/keys/agent2-key.pem'),
-  cert: fs.readFileSync(common.fixturesDir + '/keys/agent2-cert.pem')
-};
+assert.throws(function() {
+  tls.createServer({ /* empty */}).listen(0);
+}, /missing.+certificate/i);
 
-var server = tls.createServer(options, function(s) {
-  s.write('welcome!\n');
-  s.pipe(s);
-});
-
-server.listen(common.PORT, function() {
-  var c = net.createConnection(common.PORT);
-
-  c.on('connect', function() {
-    c.write('blah\nblah\nblah\n');
-  });
-
-  c.on('end', function() {
-    server.close();
-  });
-
-});
-
+assert.throws(function() {
+  https.createServer({ /* empty */}).listen(0);
+}, /missing.+certificate/i);
