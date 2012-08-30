b0d2795fe9501218dd8c953252fffe2d05d59c2a
Bert Belder <bertbelder@gmail.com>
2012-08-30 16:13:27 +0200

    child process: fix processes with IPC channel don't emit 'close'
    With this patch the IPC socket is no longer available in the
    ChildProcess.stdio array. This shouldn't be very problematic, since
    this socket was effectively non-functional; it would never emit any
    events.

---
+++ test/simple/test-child-process-fork-close.js
@@ -0,0 +1,57 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var assert = require('assert'),
+    common = require('../common'),
+    fork = require('child_process').fork,
+    fork = require('child_process').fork;
+
+var cp = fork(common.fixturesDir + '/child-process-message-and-exit.js');
+
+var gotMessage = false,
+    gotExit = false,
+    gotClose = false;
+
+cp.on('message', function(message) {
+  assert(!gotMessage);
+  assert(!gotClose);
+  assert.strictEqual(message, 'hello');
+  gotMessage = true;
+});
+
+cp.on('exit', function() {
+  assert(!gotExit);
+  assert(!gotClose);
+  gotExit = true;
+});
+
+cp.on('close', function() {
+  assert(gotMessage);
+  assert(gotExit);
+  assert(!gotClose);
+  gotClose = true;
+});
+
+process.on('exit', function() {
+  assert(gotMessage);
+  assert(gotExit);
+  assert(gotClose);
+});
