709e935239764dac5a121d47bd4a05c70e121bcb
Bert Belder <bertbelder@gmail.com>
2012-09-11 18:57:47 +0200

    openssl: use dummy OPENSSL_cpuid_setup function
    Use a empty implementation for function OPENSSL_cpuid_setup to resolve link
    error. We should figure out how to geenrate platform specific implementation
    of OPENSSL_cpuid_setup by leveraging crypto/*cpuid.pl.
    
    This patch is taken from Chromium.

--- deps/openssl/openssl/crypto/cryptlib.c
+++ deps/openssl/openssl/crypto/cryptlib.c
@@ -718,7 +718,7 @@ void OPENSSL_cpuid_setup(void)
 unsigned long *OPENSSL_ia32cap_loc(void) { return NULL; }
 #endif
 int OPENSSL_NONPIC_relocated = 0;
-#if !defined(OPENSSL_CPUID_SETUP) && !defined(OPENSSL_CPUID_OBJ)
+#if !defined(OPENSSL_CPUID_SETUP)
 void OPENSSL_cpuid_setup(void) {}
 #endif
 
