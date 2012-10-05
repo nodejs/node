3d3a0b30466b19a7879ff6f92512861404ca6c74
isaacs <i@izs.me>
2012-10-05 08:26:49 -0700

    test: Writable stream end() method doesn't take a callback

--- test/simple/test-fs-write-stream-end.js
+++ test/simple/test-fs-write-stream-end.js
@@ -31,7 +31,8 @@ var writeEndOk = false;
   var file = path.join(common.tmpDir, 'write-end-test.txt');
   var stream = fs.createWriteStream(file);
 
-  stream.end('a\n', 'utf8', function() {
+  stream.end('a\n', 'utf8');
+  stream.on('close', function() {
     var content = fs.readFileSync(file, 'utf8');
     assert.equal(content, 'a\n');
     writeEndOk = true;
