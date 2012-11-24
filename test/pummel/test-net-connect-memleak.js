4cb17cb219b3a2540b40458ed253695b3626a803
Ben Noordhuis <info@bnoordhuis.nl>
2012-11-24 15:24:36 +0100

    net: fix net.connect() resource leak
    The 'connect' event listener was attached with .on(), which blocked it from
    getting garbage collected. Use .once() instead.
    
    Fixes #4308.

---
+++ test/pummel/test-net-connect-memleak.js
@@ -0,0 +1,50 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Flags: --expose-gc
+
+var common = require('../common');
+var assert = require('assert');
+var net = require('net');
+
+assert(typeof gc === 'function', 'Run this test with --expose-gc');
+net.createServer(function() {}).listen(common.PORT);
+
+(function() {
+  // 2**26 == 64M entries
+  for (var i = 0, junk = [123.456]; i < 26; ++i) junk = junk.concat(junk);
+
+  net.createConnection(common.PORT, '127.0.0.1', function() {
+    assert(junk.length != 0);  // keep reference alive
+    setTimeout(done, 10);
+    gc();
+  });
+})();
+
+function done() {
+  var before = process.memoryUsage().rss;
+  gc();
+  var after = process.memoryUsage().rss;
+  var reclaimed = (before - after) / 1024;
+  console.log('%d kB reclaimed', reclaimed);
+  assert(reclaimed > 256 * 1024);  // it's more like 512M on x64
+  process.exit();
+}
