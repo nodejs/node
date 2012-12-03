9d02bfbc252ec24c3f8b26f6eaac2f28d8df3a6f
Ben Noordhuis <info@bnoordhuis.nl>
2012-12-03 03:08:17 +0100

    test: fix simple/test-setproctitle on freebsd

--- test/simple/test-setproctitle.js
+++ test/simple/test-setproctitle.js
@@ -40,6 +40,10 @@ assert.equal(process.title, title);
 exec('ps -p ' + process.pid + ' -o args=', function(error, stdout, stderr) {
   assert.equal(error, null);
   assert.equal(stderr, '');
+
+  // freebsd always add ' (procname)' to the process title
+  if (process.platform === 'freebsd') title += ' (node)';
+
   // omitting trailing whitespace and \n
   assert.equal(stdout.replace(/\s+$/, ''), title);
 });
