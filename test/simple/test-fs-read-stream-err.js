6e97b2cc17851a3b96cd01bac40c82573650ce6b
Ben Noordhuis <info@bnoordhuis.nl>
2012-12-11 14:45:45 +0100

    fs: fix ReadStream fd leak
    Close the file descriptor when a read operation fails.
    
    Fixes #4387.

---
+++ test/simple/test-fs-read-stream-err.js
@@ -0,0 +1,50 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+var fs = require('fs');
+
+var stream = fs.createReadStream(__filename, { bufferSize: 64 });
+var err = new Error('BAM');
+
+stream.on('data', function(buf) {
+  var fd = stream.fd;
+
+  fs.read = function() {
+    var cb = arguments[arguments.length - 1];
+    process.nextTick(function() {
+      cb(err);
+    });
+  };
+
+  fs.close = common.mustCall(function(fd_, cb) {
+    assert.equal(fd_, fd);
+    process.nextTick(cb);
+  });
+
+  stream.on('error', common.mustCall(function(err_) {
+    assert.equal(stream.fd, null);
+    assert.equal(err_, err);
+  }));
+
+  stream.on('data', assert.fail);  // no more 'data' events should follow
+});
