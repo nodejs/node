7b2ef2de208130f6cfe4775357eae0290a621653
Ben Noordhuis <info@bnoordhuis.nl>
2012-12-18 15:45:33 +0100

    deps: upgrade libuv to dc559a5

--- deps/uv/src/unix/tcp.c
+++ deps/uv/src/unix/tcp.c
@@ -233,7 +233,7 @@ int uv_tcp_listen(uv_tcp_t* tcp, int backlog, uv_connection_cb cb) {
 
   if (single_accept == -1) {
     const char* val = getenv("UV_TCP_SINGLE_ACCEPT");
-    single_accept = (val == NULL) || (atoi(val) != 0); /* on by default */
+    single_accept = (val != NULL && atoi(val) != 0);  /* Off by default. */
   }
 
   if (single_accept)
