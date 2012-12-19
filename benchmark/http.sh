825af451fbd5216ee44ecdf5f9f68244743b2e6b
isaacs <i@izs.me>
2012-12-19 09:19:06 -0800

    benchmark: Make http.sh more useful

--- benchmark/http.sh
+++ benchmark/http.sh
@@ -5,8 +5,25 @@ sudo sysctl -w net.inet.tcp.msl=1000
 sudo sysctl -w kern.maxfiles=1000000 kern.maxfilesperproc=1000000
 ulimit -n 100000
 
-./node benchmark/http_simple.js || exit 1 &
+k=${KEEPALIVE}
+if [ "$k" = "no" ]; then
+  k=""
+else
+  k="-k"
+fi
+node=${NODE:-./node}
+
+$node benchmark/http_simple.js &
+npid=$!
 sleep 1
 
-ab -n 30000 -c 100 http://127.0.0.1:8000/${TYPE:-bytes}/${LENGTH:-1024} | grep Req
-killall node
+if [ "$k" = "-k" ]; then
+  echo "using keepalive"
+fi
+
+for i in a a a a a a a a a a a a a a a a a a a a; do
+  ab $k -t 10 -c 100 http://127.0.0.1:8000/${TYPE:-bytes}/${LENGTH:-1024} \
+    2>&1 | grep Req | egrep -o '[0-9\.]+'
+done
+
+kill $npid
