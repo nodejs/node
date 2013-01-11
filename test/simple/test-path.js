6a91eab0970a87431743c79ecb8e3efaed1796d9
Bert Belder <bertbelder@gmail.com>
2013-01-11 18:42:46 +0100

    path: make basename and extname ignore trailing slashes
    Fixes #4536

--- test/simple/test-path.js
+++ test/simple/test-path.js
@@ -30,6 +30,29 @@ var f = __filename;
 
 assert.equal(path.basename(f), 'test-path.js');
 assert.equal(path.basename(f, '.js'), 'test-path');
+assert.equal(path.basename(''), '');
+assert.equal(path.basename('/dir/basename.ext'), 'basename.ext');
+assert.equal(path.basename('/basename.ext'), 'basename.ext');
+assert.equal(path.basename('basename.ext'), 'basename.ext');
+assert.equal(path.basename('basename.ext/'), 'basename.ext');
+assert.equal(path.basename('basename.ext//'), 'basename.ext');
+
+if (isWindows) {
+  // On Windows a backslash acts as a path separator.
+  assert.equal(path.basename('\\dir\\basename.ext'), 'basename.ext');
+  assert.equal(path.basename('\\basename.ext'), 'basename.ext');
+  assert.equal(path.basename('basename.ext'), 'basename.ext');
+  assert.equal(path.basename('basename.ext\\'), 'basename.ext');
+  assert.equal(path.basename('basename.ext\\\\'), 'basename.ext');
+
+} else {
+  // On unix a backslash is just treated as any other character.
+  assert.equal(path.basename('\\dir\\basename.ext'), '\\dir\\basename.ext');
+  assert.equal(path.basename('\\basename.ext'), '\\basename.ext');
+  assert.equal(path.basename('basename.ext'), 'basename.ext');
+  assert.equal(path.basename('basename.ext\\'), 'basename.ext\\');
+  assert.equal(path.basename('basename.ext\\\\'), 'basename.ext\\\\');
+}
 
 // POSIX filenames may include control characters
 // c.f. http://www.dwheeler.com/essays/fixing-unix-linux-filenames.html
@@ -46,7 +69,9 @@ assert.equal(path.dirname(f).substr(-11),
 assert.equal(path.dirname('/a/b/'), '/a');
 assert.equal(path.dirname('/a/b'), '/a');
 assert.equal(path.dirname('/a'), '/');
+assert.equal(path.dirname(''), '.');
 assert.equal(path.dirname('/'), '/');
+assert.equal(path.dirname('////'), '/');
 
 if (isWindows) {
   assert.equal(path.dirname('c:\\'), 'c:\\');
@@ -114,18 +139,34 @@ assert.equal(path.extname('..file..'), '.');
 assert.equal(path.extname('...'), '.');
 assert.equal(path.extname('...ext'), '.ext');
 assert.equal(path.extname('....'), '.');
-assert.equal(path.extname('file.ext/'), '');
+assert.equal(path.extname('file.ext/'), '.ext');
+assert.equal(path.extname('file.ext//'), '.ext');
+assert.equal(path.extname('file/'), '');
+assert.equal(path.extname('file//'), '');
+assert.equal(path.extname('file./'), '.');
+assert.equal(path.extname('file.//'), '.');
 
 if (isWindows) {
   // On windows, backspace is a path separator.
   assert.equal(path.extname('.\\'), '');
   assert.equal(path.extname('..\\'), '');
-  assert.equal(path.extname('file.ext\\'), '');
+  assert.equal(path.extname('file.ext\\'), '.ext');
+  assert.equal(path.extname('file.ext\\\\'), '.ext');
+  assert.equal(path.extname('file\\'), '');
+  assert.equal(path.extname('file\\\\'), '');
+  assert.equal(path.extname('file.\\'), '.');
+  assert.equal(path.extname('file.\\\\'), '.');
+
 } else {
   // On unix, backspace is a valid name component like any other character.
   assert.equal(path.extname('.\\'), '');
   assert.equal(path.extname('..\\'), '.\\');
   assert.equal(path.extname('file.ext\\'), '.ext\\');
+  assert.equal(path.extname('file.ext\\\\'), '.ext\\\\');
+  assert.equal(path.extname('file\\'), '');
+  assert.equal(path.extname('file\\\\'), '');
+  assert.equal(path.extname('file.\\'), '.\\');
+  assert.equal(path.extname('file.\\\\'), '.\\\\');
 }
 
 // path.join tests
