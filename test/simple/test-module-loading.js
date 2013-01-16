47f3fc9a2671bca3f70726f9bbbb8f2b56ac1dc3
isaacs <i@izs.me>
2013-01-16 12:27:53 -0800

    test: add module-loading test with empty string

--- test/simple/test-module-loading.js
+++ test/simple/test-module-loading.js
@@ -270,6 +270,17 @@ assert.deepEqual(children, {
 });
 
 
+// require() must take string, and must be truthy
+assert.throws(function() {
+  console.error('require non-string');
+  require({ foo: 'bar' });
+}, 'path must be a string');
+
+assert.throws(function() {
+  console.error('require empty string');
+  require('');
+}, 'missing path');
+
 process.on('exit', function() {
   assert.ok(common.indirectInstanceOf(a.A, Function));
   assert.equal('A done', a.A());
@@ -295,12 +306,3 @@ process.on('exit', function() {
 // #1440 Loading files with a byte order marker.
 assert.equal(42, require('../fixtures/utf8-bom.js'));
 assert.equal(42, require('../fixtures/utf8-bom.json'));
-
-// require() must take string, and must be truthy
-assert.throws(function() {
-  require({ foo: 'bar' });
-}, 'path must be a string');
-
-assert.throws(function() {
-  require(false);
-}, 'missing path');
