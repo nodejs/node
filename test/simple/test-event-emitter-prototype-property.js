b48e303af023dc60b6f6590694ba94d9b8108702
isaacs <i@izs.me>
2013-01-16 11:26:28 -0800

    events: Don't crash on events named __proto__
    This prefixes all event names internally with 'ev'.

--- test/simple/test-event-emitter-prototype-property.js
+++ test/simple/test-event-emitter-prototype-property.js
@@ -25,34 +25,13 @@ var events = require('events');
 
 var e = new events.EventEmitter();
 
-// default
-for (var i = 0; i < 10; i++) {
-  e.on('default', function() {});
-}
-assert.ok(!e._events.evdefault.hasOwnProperty('warned'));
-e.on('default', function() {});
-assert.ok(e._events.evdefault.warned);
+var emited = false;
+e.on('__proto__', function() {
+  emited = true;
+});
 
-// specific
-e.setMaxListeners(5);
-for (var i = 0; i < 5; i++) {
-  e.on('specific', function() {});
-}
-assert.ok(!e._events.evspecific.hasOwnProperty('warned'));
-e.on('specific', function() {});
-assert.ok(e._events.evspecific.warned);
+e.emit('__proto__');
 
-// only one
-e.setMaxListeners(1);
-e.on('only one', function() {});
-assert.ok(!e._events['evonly one'].hasOwnProperty('warned'));
-e.on('only one', function() {});
-assert.ok(e._events['evonly one'].hasOwnProperty('warned'));
-
-// unlimited
-e.setMaxListeners(0);
-for (var i = 0; i < 1000; i++) {
-  e.on('unlimited', function() {});
-}
-assert.ok(!e._events['evunlimited'].hasOwnProperty('warned'));
-
+process.on('exit', function() {
+  assert.equal(true, emited);
+});
