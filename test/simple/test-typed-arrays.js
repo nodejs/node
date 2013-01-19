nodejs/node

commit 9fe3734f15c822d6efc5d0e07d2a9dfd41632d7c

TypedArrays: Improve dataview perf without endian param
V8 seems to be particularly slow converting an undefined value to false
in BooleanValue.

Revert this when we upgrade to V8 3.17, or whenever the fix discussed
in http://code.google.com/p/v8/issues/detail?id=2487 lands in V8.

src/v8_typed_array.cc
@@ -673,7 +673,10 @@ class DataView {
       return ThrowError("Wrong number of arguments.");
 
     unsigned int index = args[0]->Uint32Value();
-    bool little_endian = args[1]->BooleanValue();
+    // NOTE(deanm): args[1]->BooleanValue when the argument was not passed in
+    // gives us the right answer, but seems to be very slow.  This seems to be
+    // the cost of calling BooleanValue() on undefined.
+    bool little_endian = args.Length() > 1 ? args[1]->BooleanValue() : false;
     // TODO(deanm): All of these things should be cacheable.
     int element_size = v8_typed_array::SizeOfArrayElementForType(
         args.This()->GetIndexedPropertiesExternalArrayDataType());
@@ -708,7 +711,10 @@ class DataView {
       return ThrowError("Wrong number of arguments.");
 
     unsigned int index = args[0]->Int32Value();
-    bool little_endian = args[2]->BooleanValue();
+    // NOTE(deanm): args[1]->BooleanValue when the argument was not passed in
+    // gives us the right answer, but seems to be very slow.  This seems to be
+    // the cost of calling BooleanValue() on undefined.
+    bool little_endian = args.Length() > 1 ? args[1]->BooleanValue() : false;
     // TODO(deanm): All of these things should be cacheable.
     int element_size = v8_typed_array::SizeOfArrayElementForType(
         args.This()->GetIndexedPropertiesExternalArrayDataType());

commit 2dd373894f76f768a72c9c08096c2598c0b8485b

typed arrays: fix DataView endianness regression
Fix an off-by-one error introduced in 9fe3734 that caused a regression
in the default endianness used for writes in DataView::setGeneric().

Fixes #4626.

src/v8_typed_array.cc
@@ -714,7 +714,7 @@ class DataView {
     // NOTE(deanm): args[1]->BooleanValue when the argument was not passed in
     // gives us the right answer, but seems to be very slow.  This seems to be
     // the cost of calling BooleanValue() on undefined.
-    bool little_endian = args.Length() > 1 ? args[1]->BooleanValue() : false;
+    bool little_endian = args.Length() > 2 ? args[2]->BooleanValue() : false;
     // TODO(deanm): All of these things should be cacheable.
     int element_size = v8_typed_array::SizeOfArrayElementForType(
         args.This()->GetIndexedPropertiesExternalArrayDataType());

test/simple/test-typed-arrays.js
@@ -192,3 +192,12 @@ assert.throws(function() {
   var buf = new DataView(new ArrayBuffer(8));
   buf.setFloat64(0xffffffff, 0.0, true);
 }, /Index out of range/);
+
+// DataView::setGeneric() default endianness regression test,
+// see https://github.com/joyent/node/issues/4626
+(function() {
+  var buf = new Uint8Array(2);
+  var view = new DataView(buf);
+  view.setUint16(0, 1);
+  assert.equal(view.getUint16(0), 1);
+})();