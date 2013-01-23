4a7a98fd0ab3d39685888f15bb3a844e863be255
Ben Noordhuis <info@bnoordhuis.nl>
2013-01-23 13:47:29 +0100

    http: close connection on 204 and chunked encoding
    This is similar to commit 2cbf458 but this time for 204 No Content
    instead of 304 Not Modified responses.
    
    When the user sends a 204 response with a Transfer-Encoding: chunked
    header, suppress sending the zero chunk and force the connection to
    close.

--- test/simple/test-http-chunked-304.js
+++ test/simple/test-http-chunked-304.js
@@ -24,32 +24,40 @@ var assert = require('assert');
 var http = require('http');
 var net = require('net');
 
-// RFC 2616, section 10.3.5:
+// RFC 2616, section 10.2.5:
 //
-//   The 304 response MUST NOT contain a message-body, and thus is always
+//   The 204 response MUST NOT contain a message-body, and thus is always
 //   terminated by the first empty line after the header fields.
 //
-// Verify that no empty chunk is sent when the user explicitly sets
-// a Transfer-Encoding header.
-var server = http.createServer(function(req, res) {
-  res.writeHead(304, { 'Transfer-Encoding': 'chunked' });
-  res.end();
-  server.close();
+// Likewise for 304 responses. Verify that no empty chunk is sent when
+// the user explicitly sets a Transfer-Encoding header.
+
+test(204, function() {
+  test(304);
 });
 
-server.listen(common.PORT, function() {
-  var conn = net.createConnection(common.PORT, function() {
-    conn.write('GET / HTTP/1.1\r\n\r\n');
-
-    var resp = '';
-    conn.setEncoding('utf8');
-    conn.on('data', function(data) {
-      resp += data;
-    });
-
-    conn.on('end', common.mustCall(function() {
-      assert.equal(/^Connection: close\r\n$/m.test(resp), true);
-      assert.equal(/^0\r\n$/m.test(resp), false);
-    }));
+function test(statusCode, next) {
+  var server = http.createServer(function(req, res) {
+    res.writeHead(statusCode, { 'Transfer-Encoding': 'chunked' });
+    res.end();
+    server.close();
   });
-});
+
+  server.listen(common.PORT, function() {
+    var conn = net.createConnection(common.PORT, function() {
+      conn.write('GET / HTTP/1.1\r\n\r\n');
+
+      var resp = '';
+      conn.setEncoding('utf8');
+      conn.on('data', function(data) {
+        resp += data;
+      });
+
+      conn.on('end', common.mustCall(function() {
+        assert.equal(/^Connection: close\r\n$/m.test(resp), true);
+        assert.equal(/^0\r\n$/m.test(resp), false);
+        if (next) process.nextTick(next);
+      }));
+    });
+  });
+}
