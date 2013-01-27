nodejs/node

commit 00b4b7bb97c84d19c644a4491fe98170ab9675a6

buffer: remove minor Buffer::Copy deoptimizations
* Omit ToObject() call. Buffer::Data() and Buffer::Length() know how
  to deal with Values.

* Don't check if the argument is undefined because it realistically
  never is and undefined->integer coercion achieves the same thing.

src/node_buffer.cc
@@ -398,13 +398,12 @@ Handle<Value> Buffer::Copy(const Arguments &args) {
     return ThrowTypeError("First arg should be a Buffer");
   }
 
-  Local<Object> target = args[0]->ToObject();
+  Local<Value> target = args[0];
   char* target_data = Buffer::Data(target);
   size_t target_length = Buffer::Length(target);
-  size_t target_start = args[1]->IsUndefined() ? 0 : args[1]->Uint32Value();
-  size_t source_start = args[2]->IsUndefined() ? 0 : args[2]->Uint32Value();
-  size_t source_end = args[3]->IsUndefined() ? source->length_
-                                              : args[3]->Uint32Value();
+  size_t target_start = args[1]->Uint32Value();
+  size_t source_start = args[2]->Uint32Value();
+  size_t source_end = args[3]->Uint32Value();
 
   if (source_end < source_start) {
     return ThrowRangeError("sourceEnd < sourceStart");

commit 2e371b8f926a6a0ae07a18c61c773dbd8cef7aeb

buffer: fix Buffer::Copy regression from 00b4b7b
If the end argument is omitted or not a number, make it default to
the end of the buffer, not zero.

Ideally, it should not matter what it defaults to because the JS shim
in lib/buffer.js should handle that but there are still several places
in node.js core that secrete SlowBuffers, hence Buffer::Copy() gets
called without going through Buffer.prototype.copy() first.

src/node_buffer.cc
@@ -403,7 +403,8 @@ Handle<Value> Buffer::Copy(const Arguments &args) {
   size_t target_length = Buffer::Length(target);
   size_t target_start = args[1]->Uint32Value();
   size_t source_start = args[2]->Uint32Value();
-  size_t source_end = args[3]->Uint32Value();
+  size_t source_end = args[3]->IsUint32() ? args[3]->Uint32Value()
+                                          : source->length_;
 
   if (source_end < source_start) {
     return ThrowRangeError("sourceEnd < sourceStart");