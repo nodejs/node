c7b84a1d01fa4fd6ccff1ceb917a5aaf59694244
Ben Noordhuis <info@bnoordhuis.nl>
2013-02-11 14:01:18 +0100

    fs: fix immediate WriteStream#end()
    Fix an exception that was raised when the WriteStream was closed
    immediately after creating it:
    
      TypeError: Cannot read property 'fd' of undefined
          at WriteStream.close (fs.js:1537:18)
          <snip>
    
    Avoid the TypeError and make sure the file descriptor is closed.
    
    Fixes #4745.

--- test/simple/test-fs-write-stream-end.js
+++ test/simple/test-fs-write-stream-end.js
@@ -21,27 +21,22 @@
 
 var common = require('../common');
 var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
 
-var path = require('path'),
-    fs = require('fs');
-
-var writeEndOk = false;
 (function() {
-  debugger;
-  var file = path.join(common.tmpDir, 'write-end-test.txt');
+  var file = path.join(common.tmpDir, 'write-end-test0.txt');
   var stream = fs.createWriteStream(file);
-
-  stream.end('a\n', 'utf8');
-  stream.on('close', function() {
-    var content = fs.readFileSync(file, 'utf8');
-    assert.equal(content, 'a\n');
-    writeEndOk = true;
-  });
-
+  stream.end();
+  stream.on('close', common.mustCall(function() { }));
 })();
 
-
-process.on('exit', function() {
-  assert.ok(writeEndOk);
-});
-
+(function() {
+  var file = path.join(common.tmpDir, 'write-end-test1.txt');
+  var stream = fs.createWriteStream(file);
+  stream.end('a\n', 'utf8');
+  stream.on('close', common.mustCall(function() {
+    var content = fs.readFileSync(file, 'utf8');
+    assert.equal(content, 'a\n');
+  }));
+})();
