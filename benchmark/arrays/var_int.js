e87ed91faceb1c8418973f3c3a06edfc063f46cf
isaacs <i@izs.me>
2013-02-11 13:32:39 -0800

    bench: Arrays

--- benchmark/arrays/var_int.js
+++ benchmark/arrays/var_int.js
@@ -1,15 +1,20 @@
-var types = 'Array Buffer Int8Array Uint8Array Int16Array Uint16Array Int32Array Uint32Array Float32Array Float64Array'.split(' ');
+var common = require('../common.js');
+var bench = common.createBenchmark(main, {
+  type: 'Array Buffer Int8Array Uint8Array Int16Array Uint16Array Int32Array Uint32Array Float32Array Float64Array'.split(' '),
+  n: [25]
+});
 
-var type = types[types.indexOf(process.argv[2])];
-if (!type)
-  type = types[0];
+function main(conf) {
+  var type = conf.type;
+  var clazz = global[type];
+  var n = +conf.n;
 
-console.error('Benchmarking', type);
-var clazz = global[type];
-
-var arr = new clazz(25 * 10e5);
-for (var i = 0; i < 10; ++i) {
-  for (var j = 0, k = arr.length; j < k; ++j) {
-    arr[j] = (j ^ k) & 127;
+  bench.start();
+  var arr = new clazz(n * 1e6);
+  for (var i = 0; i < 10; ++i) {
+    for (var j = 0, k = arr.length; j < k; ++j) {
+      arr[j] = (j ^ k) & 127;
+    }
   }
+  bench.end(n);
 }
