cc38528acf976c6f10da45b1d824f86444067fb1
isaacs <i@izs.me>
2013-02-11 13:37:24 -0800

    bench: buffer-base64-encode

--- benchmark/buffers/buffer-base64-encode.js
+++ benchmark/buffers/buffer-base64-encode.js
@@ -19,9 +19,18 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var N = 64*1024*1024
-var b = Buffer(N);
-var s = '';
-for (var i = 0; i < 256; ++i) s += String.fromCharCode(i);
-for (var i = 0; i < N; i += 256) b.write(s, i, 256, 'ascii');
-for (var i = 0; i < 32; ++i) b.toString('base64');
+var common = require('../common.js');
+
+var bench = common.createBenchmark(main, {});
+
+function main(conf) {
+  var N = 64 * 1024 * 1024;
+  var b = Buffer(N);
+  var s = '';
+  for (var i = 0; i < 256; ++i) s += String.fromCharCode(i);
+
+  bench.start();
+  for (var i = 0; i < N; i += 256) b.write(s, i, 256, 'ascii');
+  for (var i = 0; i < 32; ++i) b.toString('base64');
+  bench.end(64);
+}
