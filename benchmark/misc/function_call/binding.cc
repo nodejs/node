4e1bcdcab9f1d1eda72724b80d99cf6a564a3d52
isaacs <i@izs.me>
2013-02-11 18:22:43 -0800

    bench: Add function_call to bench-misc

--- benchmark/misc/function_call/binding.cc
+++ benchmark/misc/function_call/binding.cc
@@ -1,6 +1,5 @@
 #include <v8.h>
 #include <node.h>
-#include <time.h>
 
 using namespace v8;
 
@@ -8,12 +7,12 @@ static int c = 0;
 
 static Handle<Value> Hello(const Arguments& args) {
   HandleScope scope;
-  //time_t tv = time(NULL);
   return scope.Close(Integer::New(c++));
 }
 
 extern "C" void init (Handle<Object> target) {
   HandleScope scope;
-  //target->Set(String::New("hello"), String::New("World"));
   NODE_SET_METHOD(target, "hello", Hello);
 }
+
+NODE_MODULE(binding, init);
