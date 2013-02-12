844b33205c77fe32d2aae76209eb444044b9aeff
isaacs <i@izs.me>
2013-02-11 23:10:12 -0800

    bench: Move v8_bench into misc

--- benchmark/misc/v8_bench.js
+++ benchmark/misc/v8_bench.js
@@ -3,9 +3,12 @@ var fs = require('fs');
 var path = require('path');
 var vm = require('vm');
 
-var dir = path.join(__dirname, '..', 'deps', 'v8', 'benchmarks');
+var dir = path.join(__dirname, '..', '..', 'deps', 'v8', 'benchmarks');
 
-global.print = console.log;
+global.print = function(s) {
+  if (s === '----') return;
+  console.log('misc/v8_bench.js %s', s);
+};
 
 global.load = function (x) {
   var source = fs.readFileSync(path.join(dir, x), 'utf8');
