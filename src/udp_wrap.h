b1b7b4e4e806faafebfad85e828d7e6ed492b44a
Ben Noordhuis <info@bnoordhuis.nl>
2013-02-13 15:44:35 +0100

    udp_wrap: don't use namespace imports in header
    It's not really harmful but it's inconsistent with the other header
    files.

--- src/udp_wrap.h
+++ src/udp_wrap.h
@@ -7,43 +7,36 @@
 
 namespace node {
 
-using v8::Object;
-using v8::Handle;
-using v8::Local;
-using v8::Value;
-using v8::String;
-using v8::Arguments;
-
 class UDPWrap: public HandleWrap {
  public:
-  static void Initialize(Handle<Object> target);
-  static Handle<Value> New(const Arguments& args);
-  static Handle<Value> Bind(const Arguments& args);
-  static Handle<Value> Send(const Arguments& args);
-  static Handle<Value> Bind6(const Arguments& args);
-  static Handle<Value> Send6(const Arguments& args);
-  static Handle<Value> RecvStart(const Arguments& args);
-  static Handle<Value> RecvStop(const Arguments& args);
-  static Handle<Value> GetSockName(const Arguments& args);
-  static Handle<Value> AddMembership(const Arguments& args);
-  static Handle<Value> DropMembership(const Arguments& args);
-  static Handle<Value> SetMulticastTTL(const Arguments& args);
-  static Handle<Value> SetMulticastLoopback(const Arguments& args);
-  static Handle<Value> SetBroadcast(const Arguments& args);
-  static Handle<Value> SetTTL(const Arguments& args);
-  static UDPWrap* Unwrap(Local<Object> obj);
+  static void Initialize(v8::Handle<v8::Object> target);
+  static v8::Handle<v8::Value> New(const v8::Arguments& args);
+  static v8::Handle<v8::Value> Bind(const v8::Arguments& args);
+  static v8::Handle<v8::Value> Send(const v8::Arguments& args);
+  static v8::Handle<v8::Value> Bind6(const v8::Arguments& args);
+  static v8::Handle<v8::Value> Send6(const v8::Arguments& args);
+  static v8::Handle<v8::Value> RecvStart(const v8::Arguments& args);
+  static v8::Handle<v8::Value> RecvStop(const v8::Arguments& args);
+  static v8::Handle<v8::Value> GetSockName(const v8::Arguments& args);
+  static v8::Handle<v8::Value> AddMembership(const v8::Arguments& args);
+  static v8::Handle<v8::Value> DropMembership(const v8::Arguments& args);
+  static v8::Handle<v8::Value> SetMulticastTTL(const v8::Arguments& args);
+  static v8::Handle<v8::Value> SetMulticastLoopback(const v8::Arguments& args);
+  static v8::Handle<v8::Value> SetBroadcast(const v8::Arguments& args);
+  static v8::Handle<v8::Value> SetTTL(const v8::Arguments& args);
+  static UDPWrap* Unwrap(v8::Local<v8::Object> obj);
 
-  static Local<Object> Instantiate();
+  static v8::Local<v8::Object> Instantiate();
   uv_udp_t* UVHandle();
 
  private:
-  UDPWrap(Handle<Object> object);
+  UDPWrap(v8::Handle<v8::Object> object);
   virtual ~UDPWrap();
 
-  static Handle<Value> DoBind(const Arguments& args, int family);
-  static Handle<Value> DoSend(const Arguments& args, int family);
-  static Handle<Value> SetMembership(const Arguments& args,
-                                     uv_membership membership);
+  static v8::Handle<v8::Value> DoBind(const v8::Arguments& args, int family);
+  static v8::Handle<v8::Value> DoSend(const v8::Arguments& args, int family);
+  static v8::Handle<v8::Value> SetMembership(const v8::Arguments& args,
+                                             uv_membership membership);
 
   static uv_buf_t OnAlloc(uv_handle_t* handle, size_t suggested_size);
   static void OnSend(uv_udp_send_t* req, int status);
