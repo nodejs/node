50c88e0ff24890cadf2f1730a0eb93ebd0a63c28
Nathan Rajlich <nathan@tootallnate.net>
2013-02-21 12:54:13 -0800

    test: modify async native test.js to test for #4820

--- test/addons/async-hello-world/test.js
+++ test/addons/async-hello-world/test.js
@@ -1,7 +1,15 @@
 var assert = require('assert');
 var binding = require('./build/Release/binding');
+var called = false;
+
+process.on('exit', function () {
+  assert(called);
+});
+
 binding(5, function (err, val) {
   assert.equal(null, err);
   assert.equal(10, val);
-  console.error('done :)');
+  process.nextTick(function () {
+    called = true;
+  });
 });
