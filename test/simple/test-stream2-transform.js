312289b791a5924eec5748fbd19ad6867ff37320
isaacs <i@izs.me>
2013-03-03 19:43:46 -0800

    stream: Use class for write buffer entries

--- test/simple/test-stream2-transform.js
+++ test/simple/test-stream2-transform.js
@@ -82,7 +82,7 @@ test('writable side consumption', function(t) {
   t.equal(transformed, 10);
   t.equal(tx._transformState.writechunk.length, 5);
   t.same(tx._writableState.buffer.map(function(c) {
-    return c[0].length;
+    return c.chunk.length;
   }), [6, 7, 8, 9, 10]);
 
   t.end();
