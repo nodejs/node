8c44869f1dc2cdc7337d5a2a4f64e10494b0fb2f
isaacs <i@izs.me>
2013-03-05 22:09:27 -0800

    stream: Increase highWaterMark on large reads
    If the consumer of a Readable is asking for N bytes, and N > hwm,
    then clearly we have set the hwm to low, and ought to increase it.
    
    Fix #4931

---
+++ test/simple/test-stream-readable-flow-recursion.js
@@ -0,0 +1,69 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+
+// this test verifies that passing a huge number to read(size)
+// will push up the highWaterMark, and cause the stream to read
+// more data continuously, but without triggering a nextTick
+// warning or RangeError.
+
+var Readable = require('stream').Readable;
+
+// throw an error if we trigger a nextTick warning.
+process.throwDeprecation = true;
+
+var stream = new Readable({ highWaterMark: 2, bufferSize: 2 });
+var reads = 0;
+stream._read = function(size) {
+  reads++;
+  stream.push(new Buffer(size));
+};
+
+var depth = 0;
+
+function flow(stream, size, callback) {
+  depth += 1;
+  var chunk = stream.read(size);
+
+  if (!chunk)
+    stream.once('readable', flow.bind(null, stream, size, callback));
+  else
+    callback(chunk);
+
+  depth -= 1;
+  console.log('flow(' + depth + '): exit');
+}
+
+flow(stream, 5000, function() {
+  console.log('complete (' + depth + ')');
+});
+
+process.on('exit', function(code) {
+  assert.equal(reads, 5000);
+  // we pushed up the high water mark
+  assert.equal(stream._readableState.highWaterMark, 5000);
+  assert.equal(stream._readableState.length, 5000);
+  assert(!code);
+  assert.equal(depth, 0);
+  console.log('ok');
+});
