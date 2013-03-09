12713c3bb781c854f347ea9a04e344e980bea2f1
isaacs <i@izs.me>
2013-03-09 09:22:00 -0800

    win/msi: Fix typos

--- tools/msvs/nodevars.bat
+++ tools/msvs/nodevars.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem Ensure this Node.js and NPM are first in the PATH
+rem Ensure this Node.js and npm are first in the PATH
 set PATH=%APPDATA%\npm;%~dp0;%PATH%
 
 setlocal enabledelayedexpansion
@@ -12,7 +12,7 @@ for /F "usebackq delims=" %%v in (`%print_version%`) do set version=%%v
 
 rem Print message.
 if exist npm.cmd (
-  echo Your environment has been set up for using Node.js !version! and NPM.
+  echo Your environment has been set up for using Node.js !version! and npm.
 ) else (
   echo Your environment has been set up for using Node.js !version!.
 )
