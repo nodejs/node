418c9bc60429ab4cd46d100fdfd866c66f0a853a
Miroslav Bajtoš <miro.bajtos@gmail.com>
2013-05-03 08:51:51 +0200

    build: vcbuild.bat fix for Visual Studio 2012
    Change vcbuild.bat to ignore VCINSTALLDIR environment variable,
    always check for specific VS version and set GYP_MSVS_VERSION
    accordingly. Otherwise GYP generates project files in format
    that cannot be compiled by VS2012.

--- vcbuild.bat
+++ vcbuild.bat
@@ -99,9 +99,6 @@ ENDLOCAL
 @rem Skip project generation if requested.
 if defined nobuild goto sign
 
-@rem Bail out early if not running in VS build env.
-if defined VCINSTALLDIR goto msbuild-found
-
 @rem Look for Visual Studio 2012
 if not defined VS110COMNTOOLS goto vc-set-2010
 if not exist "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat" goto vc-set-2010
