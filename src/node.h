nodejs/node

commit 0bba59028381c146b90411d43425a18415d85f98

bindings: update api
All compile time warnings about using deprecated APIs have been
suppressed by updating node's API. Though there are still many function
calls that can accept Isolate, and still need to be updated.

node_isolate had to be added as an extern variable in node.h and
node_object_wrap.h

Also a couple small fixes for Error handling.

Before v8 3.16.6 the error stack message was lazily written when it was
needed, which allowed you to change the message after instantiation.
Then the stack would be written with the new message the first time it
was accessed. Though that has changed. Now it creates the stack message
on instantiation. So setting a different message afterwards won't be
displayed.

This is not a complete fix for the problem. Getting error without any
message isn't very useful.

src/node.h
@@ -86,6 +86,8 @@
 
 namespace node {
 
+extern v8::Isolate* node_isolate;
+
 NODE_EXTERN extern bool no_deprecation;
 
 NODE_EXTERN int Start(int argc, char *argv[]);
@@ -96,7 +98,7 @@ void Load(v8::Handle<v8::Object> process);
 void EmitExit(v8::Handle<v8::Object> process);
 
 #define NODE_PSYMBOL(s) \
-  v8::Persistent<v8::String>::New(v8::String::NewSymbol(s))
+  v8::Persistent<v8::String>::New(node_isolate, v8::String::NewSymbol(s))
 
 /* Converts a unixtime to V8 Date */
 #define NODE_UNIXTIME_V8(t) v8::Date::New(1000*static_cast<double>(t))
@@ -153,7 +155,7 @@ v8::Local<v8::Object> BuildStatsObject(const uv_statbuf_t* s);
 static inline v8::Persistent<v8::Function>* cb_persist(
     const v8::Local<v8::Value> &v) {
   v8::Persistent<v8::Function> *fn = new v8::Persistent<v8::Function>();
-  *fn = v8::Persistent<v8::Function>::New(v8::Local<v8::Function>::Cast(v));
+  *fn = v8::Persistent<v8::Function>::New(node_isolate, v8::Local<v8::Function>::Cast(v));
   return fn;
 }
 
@@ -165,7 +167,7 @@ static inline v8::Persistent<v8::Function>* cb_unwrap(void *data) {
 }
 
 static inline void cb_destroy(v8::Persistent<v8::Function> * cb) {
-  cb->Dispose();
+  cb->Dispose(node_isolate);
   delete cb;
 }
 

src/node_object_wrap.h
@@ -37,6 +37,8 @@
 
 namespace node {
 
+extern v8::Isolate* node_isolate;
+
 class NODE_EXTERN ObjectWrap {
  public:
   ObjectWrap ( ) {
@@ -46,10 +48,10 @@ class NODE_EXTERN ObjectWrap {
 
   virtual ~ObjectWrap ( ) {
     if (!handle_.IsEmpty()) {
-      assert(handle_.IsNearDeath());
-      handle_.ClearWeak();
+      assert(handle_.IsNearDeath(node_isolate));
+      handle_.ClearWeak(node_isolate);
       handle_->SetInternalField(0, v8::Undefined());
-      handle_.Dispose();
+      handle_.Dispose(node_isolate);
       handle_.Clear();
     }
   }
@@ -59,7 +61,7 @@ class NODE_EXTERN ObjectWrap {
   static inline T* Unwrap (v8::Handle<v8::Object> handle) {
     assert(!handle.IsEmpty());
     assert(handle->InternalFieldCount() > 0);
-    return static_cast<T*>(handle->GetPointerFromInternalField(0));
+    return static_cast<T*>(handle->GetAlignedPointerFromInternalField(0));
   }
 
 
@@ -69,15 +71,15 @@ class NODE_EXTERN ObjectWrap {
   inline void Wrap (v8::Handle<v8::Object> handle) {
     assert(handle_.IsEmpty());
     assert(handle->InternalFieldCount() > 0);
-    handle_ = v8::Persistent<v8::Object>::New(handle);
-    handle_->SetPointerInInternalField(0, this);
+    handle_ = v8::Persistent<v8::Object>::New(node_isolate, handle);
+    handle_->SetAlignedPointerInInternalField(0, this);
     MakeWeak();
   }
 
 
   inline void MakeWeak (void) {
-    handle_.MakeWeak(this, WeakCallback);
-    handle_.MarkIndependent();
+    handle_.MakeWeak(node_isolate, this, WeakCallback);
+    handle_.MarkIndependent(node_isolate);
   }
 
   /* Ref() marks the object as being attached to an event loop.
@@ -87,7 +89,7 @@ class NODE_EXTERN ObjectWrap {
   virtual void Ref() {
     assert(!handle_.IsEmpty());
     refs_++;
-    handle_.ClearWeak();
+    handle_.ClearWeak(node_isolate);
   }
 
   /* Unref() marks an object as detached from the event loop.  This is its
@@ -101,7 +103,7 @@ class NODE_EXTERN ObjectWrap {
    */
   virtual void Unref() {
     assert(!handle_.IsEmpty());
-    assert(!handle_.IsWeak());
+    assert(!handle_.IsWeak(node_isolate));
     assert(refs_ > 0);
     if (--refs_ == 0) { MakeWeak(); }
   }
@@ -111,13 +113,15 @@ class NODE_EXTERN ObjectWrap {
 
 
  private:
-  static void WeakCallback (v8::Persistent<v8::Value> value, void *data) {
+  static void WeakCallback(v8::Isolate* env,
+                           v8::Persistent<v8::Value> value,
+                           void* data) {
     v8::HandleScope scope;
 
     ObjectWrap *obj = static_cast<ObjectWrap*>(data);
     assert(value == obj->handle_);
     assert(!obj->refs_);
-    assert(value.IsNearDeath());
+    assert(value.IsNearDeath(env));
     delete obj;
   }
 };

lib/fs.js
@@ -63,7 +63,8 @@ function rethrow() {
     var backtrace = new Error;
     return function(err) {
       if (err) {
-        backtrace.message = err.message;
+        backtrace.stack = err.name + ': ' + err.message +
+                          backtrace.stack.substr(backtrace.name.length);
         err = backtrace;
         throw err;
       }

src/cares_wrap.cc
@@ -272,7 +272,7 @@ class QueryWrap {
   QueryWrap() {
     HandleScope scope;
 
-    object_ = Persistent<Object>::New(Object::New());
+    object_ = Persistent<Object>::New(node_isolate, Object::New());
   }
 
   virtual ~QueryWrap() {
@@ -280,7 +280,7 @@ class QueryWrap {
 
     object_->Delete(oncomplete_sym);
 
-    object_.Dispose();
+    object_.Dispose(node_isolate);
     object_.Clear();
   }
 

src/fs_event_wrap.cc
@@ -74,7 +74,8 @@ void FSEventWrap::Initialize(Handle<Object> target) {
   NODE_SET_PROTOTYPE_METHOD(t, "close", Close);
 
   target->Set(String::NewSymbol("FSEvent"),
-              Persistent<FunctionTemplate>::New(t)->GetFunction());
+              Persistent<FunctionTemplate>::New(node_isolate,
+                                                t)->GetFunction());
 }
 
 
@@ -172,7 +173,7 @@ Handle<Value> FSEventWrap::Close(const Arguments& args) {
   // and legal, HandleWrap::Close() deals with them the same way.
   assert(!args.Holder().IsEmpty());
   assert(args.Holder()->InternalFieldCount() > 0);
-  void* ptr = args.Holder()->GetPointerFromInternalField(0);
+  void* ptr = args.Holder()->GetAlignedPointerFromInternalField(0);
   FSEventWrap* wrap = static_cast<FSEventWrap*>(ptr);
 
   if (wrap == NULL || wrap->initialized_ == false) {

src/handle_wrap.cc
@@ -84,7 +84,7 @@ Handle<Value> HandleWrap::Close(const Arguments& args) {
   HandleScope scope;
 
   HandleWrap *wrap = static_cast<HandleWrap*>(
-      args.Holder()->GetPointerFromInternalField(0));
+      args.Holder()->GetAlignedPointerFromInternalField(0));
 
   // guard against uninitialized handle or double close
   if (wrap == NULL || wrap->handle__ == NULL) {
@@ -115,8 +115,8 @@ HandleWrap::HandleWrap(Handle<Object> object, uv_handle_t* h) {
   HandleScope scope;
   assert(object_.IsEmpty());
   assert(object->InternalFieldCount() > 0);
-  object_ = v8::Persistent<v8::Object>::New(object);
-  object_->SetPointerInInternalField(0, this);
+  object_ = v8::Persistent<v8::Object>::New(node_isolate, object);
+  object_->SetAlignedPointerInInternalField(0, this);
   ngx_queue_insert_tail(&handle_wrap_queue, &handle_wrap_queue_);
 }
 
@@ -147,8 +147,8 @@ void HandleWrap::OnClose(uv_handle_t* handle) {
     MakeCallback(wrap->object_, close_sym, 0, NULL);
   }
 
-  wrap->object_->SetPointerInInternalField(0, NULL);
-  wrap->object_.Dispose();
+  wrap->object_->SetAlignedPointerInInternalField(0, NULL);
+  wrap->object_.Dispose(node_isolate);
   wrap->object_.Clear();
 
   delete wrap;

src/handle_wrap.h
@@ -50,7 +50,7 @@ namespace node {
   assert(!args.Holder().IsEmpty());                                         \
   assert(args.Holder()->InternalFieldCount() > 0);                          \
   type* wrap = static_cast<type*>(                                          \
-      args.Holder()->GetPointerFromInternalField(0));
+      args.Holder()->GetAlignedPointerFromInternalField(0));
 
 class HandleWrap {
   public:

src/node.cc
@@ -195,7 +195,7 @@ static void Spin(uv_idle_t* handle, int status) {
       abort();
     }
     Local<Function> cb = cb_v.As<Function>();
-    process_tickFromSpinner = Persistent<Function>::New(cb);
+    process_tickFromSpinner = Persistent<Function>::New(node_isolate, cb);
   }
 
   TryCatch try_catch;
@@ -912,7 +912,7 @@ MakeDomainCallback(const Handle<Object> object,
       abort();
     }
     Local<Function> cb = cb_v.As<Function>();
-    process_tickDomainCallback = Persistent<Function>::New(cb);
+    process_tickDomainCallback = Persistent<Function>::New(node_isolate, cb);
   }
 
   // lazy load domain specific symbols
@@ -1002,7 +1002,7 @@ MakeCallback(const Handle<Object> object,
       abort();
     }
     Local<Function> cb = cb_v.As<Function>();
-    process_tickCallback = Persistent<Function>::New(cb);
+    process_tickCallback = Persistent<Function>::New(node_isolate, cb);
   }
 
   TryCatch try_catch;
@@ -1802,7 +1802,7 @@ v8::Handle<v8::Value> MemoryUsage(const v8::Arguments& args) {
 
   // V8 memory usage
   HeapStatistics v8_heap_stats;
-  V8::GetHeapStatistics(&v8_heap_stats);
+  node_isolate->GetHeapStatistics(&v8_heap_stats);
   info->Set(heap_total_symbol,
             Integer::NewFromUnsigned(v8_heap_stats.total_heap_size()));
   info->Set(heap_used_symbol,
@@ -2021,7 +2021,7 @@ static Handle<Value> Binding(const Arguments& args) {
   node_module_struct* modp;
 
   if (binding_cache.IsEmpty()) {
-    binding_cache = Persistent<Object>::New(Object::New());
+    binding_cache = Persistent<Object>::New(node_isolate, Object::New());
   }
 
   Local<Object> exports;
@@ -2307,7 +2307,8 @@ Handle<Object> SetupProcessObject(int argc, char *argv[]) {
 
   process_template->SetClassName(String::NewSymbol("process"));
 
-  process = Persistent<Object>::New(process_template->GetFunction()->NewInstance());
+  process = Persistent<Object>::New(node_isolate,
+                                process_template->GetFunction()->NewInstance());
 
   process->SetAccessor(String::New("title"),
                        ProcessTitleGetter,
@@ -2317,7 +2318,7 @@ Handle<Object> SetupProcessObject(int argc, char *argv[]) {
   process->Set(String::NewSymbol("version"), String::New(NODE_VERSION));
 
   // process.moduleLoadList
-  module_load_list = Persistent<Array>::New(Array::New());
+  module_load_list = Persistent<Array>::New(node_isolate, Array::New());
   process->Set(String::NewSymbol("moduleLoadList"), module_load_list);
 
   // process.versions
@@ -2984,6 +2985,10 @@ char** Init(int argc, char *argv[]) {
   }
   V8::SetFlagsFromCommandLine(&v8argc, v8argv, false);
 
+  // Fetch a reference to the main isolate, so we have a reference to it
+  // even when we need it to access it from another (debugger) thread.
+  node_isolate = Isolate::GetCurrent();
+
 #ifdef __POSIX__
   // Ignore SIGPIPE
   RegisterSignalHandler(SIGPIPE, SIG_IGN);
@@ -2998,10 +3003,6 @@ char** Init(int argc, char *argv[]) {
   uv_idle_init(uv_default_loop(), &idle_immediate_dummy);
 
   V8::SetFatalErrorHandler(node::OnFatalError);
-
-  // Fetch a reference to the main isolate, so we have a reference to it
-  // even when we need it to access it from another (debugger) thread.
-  node_isolate = Isolate::GetCurrent();
 
   // If the --debug flag was specified then initialize the debug thread.
   if (use_debug_agent) {
@@ -3111,7 +3112,7 @@ int Start(int argc, char *argv[]) {
 
   V8::Initialize();
   {
-    Locker locker;
+    Locker locker(node_isolate);
     HandleScope handle_scope;
 
     // Create the one and only Context.
@@ -3137,7 +3138,7 @@ int Start(int argc, char *argv[]) {
     RunAtExit();
 
 #ifndef NDEBUG
-    context.Dispose();
+    context.Dispose(node_isolate);
 #endif
   }
 

commit 4bb4f734b3dc3cd42ffd7c5d128fec2abd6b97dd

src: unexport node_isolate
Commit 0bba5902 accidentally (or maybe erroneously) added node_isolate
to src/node.h and src/node_object_wrap.h.

Undo that, said variable is not for public consumption. Add-on authors
should use v8::Isolate::GetCurrent() instead.

I missed that while reviewing. Mea culpa.

Fixes #5639.

src/node.h
@@ -86,8 +86,6 @@
 
 namespace node {
 
-extern v8::Isolate* node_isolate;
-
 NODE_EXTERN extern bool no_deprecation;
 
 NODE_EXTERN int Start(int argc, char *argv[]);
@@ -97,8 +95,9 @@ v8::Handle<v8::Object> SetupProcessObject(int argc, char *argv[]);
 void Load(v8::Handle<v8::Object> process);
 void EmitExit(v8::Handle<v8::Object> process);
 
-#define NODE_PSYMBOL(s) \
-  v8::Persistent<v8::String>::New(node_isolate, v8::String::NewSymbol(s))
+#define NODE_PSYMBOL(s)                                                       \
+  v8::Persistent<v8::String>::New(v8::Isolate::GetCurrent(),                  \
+                                  v8::String::NewSymbol(s))
 
 /* Converts a unixtime to V8 Date */
 #define NODE_UNIXTIME_V8(t) v8::Date::New(1000*static_cast<double>(t))
@@ -152,10 +151,10 @@ NODE_EXTERN ssize_t DecodeWrite(char *buf,
 v8::Local<v8::Object> BuildStatsObject(const uv_stat_t* s);
 
 
-static inline v8::Persistent<v8::Function>* cb_persist(
-    const v8::Local<v8::Value> &v) {
-  v8::Persistent<v8::Function> *fn = new v8::Persistent<v8::Function>();
-  *fn = v8::Persistent<v8::Function>::New(node_isolate, v8::Local<v8::Function>::Cast(v));
+static inline v8::Persistent<v8::Function>* cb_persist(v8::Local<v8::Value> v) {
+  v8::Persistent<v8::Function>* fn = new v8::Persistent<v8::Function>();
+  *fn = v8::Persistent<v8::Function>::New(v8::Isolate::GetCurrent(),
+                                          v.As<v8::Function>());
   return fn;
 }
 
@@ -167,7 +166,7 @@ static inline v8::Persistent<v8::Function>* cb_unwrap(void *data) {
 }
 
 static inline void cb_destroy(v8::Persistent<v8::Function> * cb) {
-  cb->Dispose(node_isolate);
+  cb->Dispose(v8::Isolate::GetCurrent());
   delete cb;
 }
 

src/node_object_wrap.h
@@ -37,8 +37,6 @@
 
 namespace node {
 
-extern v8::Isolate* node_isolate;
-
 class NODE_EXTERN ObjectWrap {
  public:
   ObjectWrap ( ) {
@@ -48,10 +46,11 @@ class NODE_EXTERN ObjectWrap {
 
   virtual ~ObjectWrap ( ) {
     if (!handle_.IsEmpty()) {
-      assert(handle_.IsNearDeath(node_isolate));
-      handle_.ClearWeak(node_isolate);
+      v8::Isolate* isolate = v8::Isolate::GetCurrent();
+      assert(handle_.IsNearDeath(isolate));
+      handle_.ClearWeak(isolate);
       handle_->SetAlignedPointerInInternalField(0, 0);
-      handle_.Dispose(node_isolate);
+      handle_.Dispose(isolate);
       handle_.Clear();
     }
   }
@@ -71,15 +70,17 @@ class NODE_EXTERN ObjectWrap {
   inline void Wrap (v8::Handle<v8::Object> handle) {
     assert(handle_.IsEmpty());
     assert(handle->InternalFieldCount() > 0);
-    handle_ = v8::Persistent<v8::Object>::New(node_isolate, handle);
+    v8::Isolate* isolate = v8::Isolate::GetCurrent();
+    handle_ = v8::Persistent<v8::Object>::New(isolate, handle);
     handle_->SetAlignedPointerInInternalField(0, this);
     MakeWeak();
   }
 
 
   inline void MakeWeak (void) {
-    handle_.MakeWeak(node_isolate, this, WeakCallback);
-    handle_.MarkIndependent(node_isolate);
+    v8::Isolate* isolate = v8::Isolate::GetCurrent();
+    handle_.MakeWeak(isolate, this, WeakCallback);
+    handle_.MarkIndependent(isolate);
   }
 
   /* Ref() marks the object as being attached to an event loop.
@@ -89,7 +90,7 @@ class NODE_EXTERN ObjectWrap {
   virtual void Ref() {
     assert(!handle_.IsEmpty());
     refs_++;
-    handle_.ClearWeak(node_isolate);
+    handle_.ClearWeak(v8::Isolate::GetCurrent());
   }
 
   /* Unref() marks an object as detached from the event loop.  This is its
@@ -103,7 +104,7 @@ class NODE_EXTERN ObjectWrap {
    */
   virtual void Unref() {
     assert(!handle_.IsEmpty());
-    assert(!handle_.IsWeak(node_isolate));
+    assert(!handle_.IsWeak(v8::Isolate::GetCurrent()));
     assert(refs_ > 0);
     if (--refs_ == 0) { MakeWeak(); }
   }
@@ -113,18 +114,18 @@ class NODE_EXTERN ObjectWrap {
 
 
  private:
-  static void WeakCallback(v8::Isolate* env,
+  static void WeakCallback(v8::Isolate* isolate,
                            v8::Persistent<v8::Value> value,
                            void* data) {
-    v8::HandleScope scope(node_isolate);
-
+    v8::HandleScope scope(isolate);
     ObjectWrap *obj = static_cast<ObjectWrap*>(data);
     assert(value == obj->handle_);
     assert(!obj->refs_);
-    assert(value.IsNearDeath(env));
+    assert(value.IsNearDeath(isolate));
     delete obj;
   }
 };
 
 } // namespace node
+
 #endif // object_wrap_h