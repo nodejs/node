99a7e78e7789a1a0de31c493605abf0a0f35312a
isaacs <i@izs.me>
2013-06-26 09:13:48 -0700

    http: Dump response when request is aborted
    Fixes #5695

---
+++ test/simple/test-http-abort-stream-end.js
@@ -0,0 +1,61 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+
+var http = require('http');
+
+var maxSize = 1024;
+var size = 0;
+
+var s = http.createServer(function(req, res) {
+  this.close();
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  for (var i = 0; i < maxSize; i++) {
+    res.write('x' + i);
+  }
+  res.end();
+});
+
+var aborted = false;
+s.listen(common.PORT, function() {
+  var req = http.get('http://localhost:' + common.PORT, function(res) {
+    res.on('data', function(chunk) {
+      size += chunk.length;
+      assert(!aborted, 'got data after abort');
+      if (size > maxSize) {
+        aborted = true;
+        req.abort();
+        size = maxSize;
+      }
+    });
+  });
+
+  req.end();
+});
+
+process.on('exit', function() {
+  assert(aborted);
+  assert.equal(size, maxSize);
+  console.log('ok');
+});
