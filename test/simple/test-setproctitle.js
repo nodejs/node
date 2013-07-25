e4363145bad7fe1bd63fbb2d41101de38477e7fb
Ben Noordhuis <info@bnoordhuis.nl>
2013-07-25 12:24:40 +0200

    test: fix simple/test-setproctitle
    The title shouldn't be too long; libuv's uv_set_process_title() out of
    security considerations no longer overwrites envp, only argv, so the
    maximum title length is possibly quite short.
    
    Fixes #5908.

--- test/simple/test-setproctitle.js
+++ test/simple/test-setproctitle.js
@@ -31,7 +31,10 @@ var common = require('../common');
 var assert = require('assert');
 var exec = require('child_process').exec;
 
-var title = 'testTestTESTtest123123123123123123HiHaiJo';
+// The title shouldn't be too long; libuv's uv_set_process_title() out of
+// security considerations no longer overwrites envp, only argv, so the
+// maximum title length is possibly quite short.
+var title = 'testme';
 
 assert.notEqual(process.title, title);
 process.title = title;
