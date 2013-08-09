41ec6d0580d91c4f2e5f30f279225eb4ec07886d
Ben Noordhuis <info@bnoordhuis.nl>
2013-08-09 02:33:40 +0200

    dgram: fix regression in string argument handling
    v0.10 allows strings for the offset, length and port arguments to
    dgram.send() and dgram.sendto() but master before this commit would
    abort with the following assert:
    
        node: ../../src/udp_wrap.cc:227: static void
        node::UDPWrap::DoSend(const v8::FunctionCallbackInfo<v8::Value>&,
        int): Assertion `args[2]->IsUint32()' failed.
    
    Go beyond what v0.10 does and also add range checks: offset and length
    should be >= 0, port should be between 1 and 65535.
    
    That particular change needs to be back-ported to v0.10 because passing
    a negative offset or length number aborts with the following assertions:
    
        node: ../../src/udp_wrap.cc:264: static v8::Handle<v8::Value>
        node::UDPWrap::DoSend(const v8::Arguments&, int): Assertion
        `offset < Buffer::Length(buffer_obj)' failed.
    
    Or:
    
        node: ../../src/udp_wrap.cc:265: static v8::Handle<v8::Value>
        node::UDPWrap::DoSend(const v8::Arguments&, int): Assertion
        `length <= Buffer::Length(buffer_obj) - offset' failed.
    
    Interestingly enough, a negative port number is accepted in v0.10 but
    is silently ignored.
    
    This commit exposed a bug in the simple/test-dgram-close test which
    has also been fixed.

--- test/simple/test-dgram-send-bad-arguments.js
+++ test/simple/test-dgram-send-bad-arguments.js
@@ -19,16 +19,22 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Ensure that if a dgram socket is closed before the DNS lookup completes, it
-// won't crash.
+var common = require('../common');
+var assert = require('assert');
+var dgram = require('dgram');
 
-var assert = require('assert'),
-    common = require('../common'),
-    dgram = require('dgram');
+var buf = Buffer('test');
+var host = '127.0.0.1';
+var sock = dgram.createSocket('udp4');
 
-var buf = new Buffer(1024);
-buf.fill(42);
+assert.throws(function() {
+  sock.send();
+}, TypeError);  // First argument should be a buffer.
 
-var socket = dgram.createSocket('udp4');
-socket.send(buf, 0, buf.length, common.PORT, 'localhost');
-socket.close();
+assert.throws(function() { sock.send(buf, -1, 1, 1, host);    }, RangeError);
+assert.throws(function() { sock.send(buf, 1, -1, 1, host);    }, RangeError);
+assert.throws(function() { sock.send(buf, 1, 1, -1, host);    }, RangeError);
+assert.throws(function() { sock.send(buf, 5, 1, 1, host);     }, RangeError);
+assert.throws(function() { sock.send(buf, 1, 5, 1, host);     }, RangeError);
+assert.throws(function() { sock.send(buf, 1, 1, 0, host);     }, RangeError);
+assert.throws(function() { sock.send(buf, 1, 1, 65536, host); }, RangeError);
