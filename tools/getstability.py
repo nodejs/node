92e43751731e2186e323f20b2ade57ac8adfba05
Timothy J Fontaine <tjfontaine@gmail.com>
2013-08-21 16:55:27 -0700

    tools: script to report [un]stable build

---
+++ tools/getstability.py
@@ -0,0 +1,19 @@
+import os,re
+
+node_version_h = os.path.join(os.path.dirname(__file__), '..', 'src',
+    'node_version.h')
+
+f = open(node_version_h)
+
+for line in f:
+  if re.match('#define NODE_MAJOR_VERSION', line):
+    major = line.split()[2]
+  if re.match('#define NODE_MINOR_VERSION', line):
+    minor = line.split()[2]
+  if re.match('#define NODE_PATCH_VERSION', line):
+    patch = line.split()[2]
+
+if int(minor) % 2 == 0:
+  print 'stable'
+else:
+  print 'unstable'
