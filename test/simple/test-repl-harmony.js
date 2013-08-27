54fbb1da96376688f2bffac28486c61f38bf6eaf
Nathan Rajlich <nathan@tootallnate.net>
2013-08-27 08:57:16 -0700

    repl: treat "Assignment to const" as syntax error
    Adding a new `repl-harmony` test file here because adding the
    `--use_strict --harmony` flags on the main repl test file was causing
    lots of unrelated failures, due to global variable assignments and
    things like that. This new test file is based off of the original
    repl.js test file, but has a lot of the tests stripped out. A test case
    for this commit is included though.
    
    Fixes #6132.

---
+++ test/simple/test-repl-harmony.js
@@ -0,0 +1,47 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+
+var spawn = require('child_process').spawn;
+var args = ['--harmony', '--use-strict', '-i'];
+var child = spawn(process.execPath, args);
+
+var input = 'function x(){const y=1;y=2}\n';
+var expectOut = /^> SyntaxError: Assignment to constant variable.\n/;
+
+child.stderr.setEncoding('utf8');
+child.stderr.on('data', function(c) {
+  throw new Error('child.stderr be silent');
+});
+
+child.stdout.setEncoding('utf8');
+var out = '';
+child.stdout.on('data', function(c) {
+  out += c;
+});
+child.stdout.on('end', function() {
+  assert(expectOut.test(out));
+  console.log('ok');
+});
+
+child.stdin.end(input);
