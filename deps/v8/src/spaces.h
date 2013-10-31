532f9ffca2d9ac01e92a15a46adc0374ef75f4c4
Fedor Indutny <fedor.indutny@gmail.com>
2013-10-31 15:08:17 +0400

    v8: backport b3775cf3 from upstream

--- deps/v8/src/spaces.h
+++ deps/v8/src/spaces.h
@@ -321,7 +321,8 @@ class MemoryChunk {
   Space* owner() const {
     if ((reinterpret_cast<intptr_t>(owner_) & kFailureTagMask) ==
         kFailureTag) {
-      return reinterpret_cast<Space*>(owner_ - kFailureTag);
+      return reinterpret_cast<Space*>(reinterpret_cast<intptr_t>(owner_) -
+                                      kFailureTag);
     } else {
       return NULL;
     }
