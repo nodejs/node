df3537102b9ba335f05315e513e7cb0123321362
Ben Noordhuis <info@bnoordhuis.nl>
2013-11-01 15:44:43 +0100

    benchmark: add http chunked encoding benchmarks
    Test both "Content-Length: xxx" and "Transfer-Encoding: chunked" responses in
    the http/simple benchmark.

--- benchmark/http/simple.js
+++ benchmark/http/simple.js
@@ -5,6 +5,7 @@ var bench = common.createBenchmark(main, {
   // unicode confuses ab on os x.
   type: ['bytes', 'buffer'],
   length: [4, 1024, 102400],
+  chunks: [0, 1, 4],  // chunks=0 means 'no chunked encoding'.
   c: [50, 500]
 });
 
@@ -13,7 +14,7 @@ function main(conf) {
   var spawn = require('child_process').spawn;
   var server = require('../http_simple.js');
   setTimeout(function() {
-    var path = '/' + conf.type + '/' + conf.length; //+ '/' + conf.chunks;
+    var path = '/' + conf.type + '/' + conf.length + '/' + conf.chunks;
     var args = ['-r', 5000, '-t', 8, '-c', conf.c];
 
     bench.http(path, args, function() {
