3421d29d6310f6bf0c90fa70d936d2646ccc9187
Ben Noordhuis <info@bnoordhuis.nl>
2013-11-23 23:05:34 +0100

    v8: unbreak freebsd build
    reland de8c0a5
    
    Fixes #7020 and #7021

--- deps/v8/src/platform-freebsd.cc
+++ deps/v8/src/platform-freebsd.cc
@@ -182,7 +182,7 @@ void OS::LogSharedLibraryAddresses(Isolate* isolate) {
     // There may be no filename in this line.  Skip to next.
     if (start_of_path == NULL) continue;
     buffer[bytes_read] = 0;
-    LOG(isolate SharedLibraryEvent(start_of_path, start, end));
+    LOG(isolate, SharedLibraryEvent(start_of_path, start, end));
   }
   close(fd);
 }
