c5d49ba9c295078173247e11fac3ca635f60a5a6
Alexis Campailla <alexis@janeasystems.com>
2013-12-10 11:32:29 -0800

    test: test-os fix win32 localhost assumption
    The test is expecting an invalid result for the loopback
    interface network mask, but this issue was fixed in
    libuv commit 1d5c61a8b31257733c41fb507762d3eb56eecb2d
    
    Closes #5262 #6673

--- test/simple/test-os.js
+++ test/simple/test-os.js
@@ -101,10 +101,7 @@ switch (platform) {
   case 'win32':
     var filter = function(e) { return e.address == '127.0.0.1'; };
     var actual = interfaces['Loopback Pseudo-Interface 1'].filter(filter);
-    // NOTE: Windows does not set a prefix or netmask on 127.0.0.1, so we
-    //       default to /32 here.  We could put in a special case to force
-    //       to /8 if desired.
-    var expected = [{ address: '127.0.0.1', netmask: '255.255.255.255',
+    var expected = [{ address: '127.0.0.1', netmask: '255.0.0.0',
                       mac: '00:00:00:00:00:00', family: 'IPv4',
                       internal: true }];
     assert.deepEqual(actual, expected);
