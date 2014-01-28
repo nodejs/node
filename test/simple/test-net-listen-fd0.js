3859fbdb7dfa4132a77dbdc446f73f6ab8a10404
Timothy J Fontaine <tjfontaine@gmail.com>
2014-01-27 17:39:45 -0800

    test: fix test-net-listen-fd0 for pipes
    In the case of a pipe'd input, i.e. from the CI the fd will be a PIPE
    and when listen() is called it will return ENOTSOCK instead of EINVAL.
    
    Backport: cd2d3aedaab5cb1a19794ca3ca50d7436ed424cf

--- test/simple/test-net-listen-fd0.js
+++ test/simple/test-net-listen-fd0.js
@@ -26,11 +26,15 @@ var net = require('net');
 var gotError = false;
 
 process.on('exit', function() {
-  assert.equal(gotError, true);
+  assert(gotError instanceof Error);
 });
 
 // this should fail with an async EINVAL error, not throw an exception
 net.createServer(assert.fail).listen({fd:0}).on('error', function(e) {
-  assert.equal(e.code, 'EINVAL');
-  gotError = true;
+  switch(e.code) {
+    case 'EINVAL':
+    case 'ENOTSOCK':
+      gotError = e;
+      break
+  }
 });
