6d15b163b090f7dd218843fdca55f874da5daead
Nathan Rajlich <nathan@tootallnate.net>
2014-03-25 10:48:57 -1000

    http: better client "protocol not supported" error
    Include the "expected protocol" in the Error message
    string, which evaluates to "http:" for the `http`
    core module, and "https:" for the `https` module.
    
    Closes #7355.

--- test/simple/test-http-url.parse-only-support-http-https-protocol.js
+++ test/simple/test-http-url.parse-only-support-http-https-protocol.js
@@ -29,7 +29,7 @@ assert.throws(function() {
   http.request(url.parse('file:///whatever'));
 }, function(err) {
   if (err instanceof Error) {
-    assert.strictEqual(err.message, 'Protocol:file: not supported.');
+    assert.strictEqual(err.message, 'Protocol "file:" not supported. Expected "http:".');
     return true;
   }
 });
@@ -38,7 +38,7 @@ assert.throws(function() {
   http.request(url.parse('mailto:asdf@asdf.com'));
 }, function(err) {
   if (err instanceof Error) {
-    assert.strictEqual(err.message, 'Protocol:mailto: not supported.');
+    assert.strictEqual(err.message, 'Protocol "mailto:" not supported. Expected "http:".');
     return true;
   }
 });
@@ -47,7 +47,7 @@ assert.throws(function() {
   http.request(url.parse('ftp://www.example.com'));
 }, function(err) {
   if (err instanceof Error) {
-    assert.strictEqual(err.message, 'Protocol:ftp: not supported.');
+    assert.strictEqual(err.message, 'Protocol "ftp:" not supported. Expected "http:".');
     return true;
   }
 });
@@ -56,7 +56,7 @@ assert.throws(function() {
   http.request(url.parse('javascript:alert(\'hello\');'));
 }, function(err) {
   if (err instanceof Error) {
-    assert.strictEqual(err.message, 'Protocol:javascript: not supported.');
+    assert.strictEqual(err.message, 'Protocol "javascript:" not supported. Expected "http:".');
     return true;
   }
 });
@@ -65,7 +65,7 @@ assert.throws(function() {
   http.request(url.parse('xmpp:isaacschlueter@jabber.org'));
 }, function(err) {
   if (err instanceof Error) {
-    assert.strictEqual(err.message, 'Protocol:xmpp: not supported.');
+    assert.strictEqual(err.message, 'Protocol "xmpp:" not supported. Expected "http:".');
     return true;
   }
 });
@@ -74,7 +74,7 @@ assert.throws(function() {
   http.request(url.parse('f://some.host/path'));
 }, function(err) {
   if (err instanceof Error) {
-    assert.strictEqual(err.message, 'Protocol:f: not supported.');
+    assert.strictEqual(err.message, 'Protocol "f:" not supported. Expected "http:".');
     return true;
   }
 });
