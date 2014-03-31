cf3f4b5308f35214aaa227b83ca99280f0544b24
Ben Noordhuis <info@bnoordhuis.nl>
2014-03-31 15:07:59 +0200

    deps: fix up v8 postmortem codegen
    Fix up overzealous parses, strip whitespace from class and field names.

--- deps/v8/tools/gen-postmortem-metadata.py
+++ deps/v8/tools/gen-postmortem-metadata.py
@@ -434,9 +434,13 @@ def load_fields():
 # Emit a block of constants.
 #
 def emit_set(out, consts):
-        for ii in range(0, len(consts)):
-                out.write('int v8dbg_%s = %s;\n' %
-                    (consts[ii]['name'], consts[ii]['value']));
+        # Fix up overzealous parses.  This could be done inside the
+        # parsers but as there are several, it's easiest to do it here.
+        ws = re.compile('\s+')
+        for const in consts:
+                name = ws.sub('', const['name'])
+                value = ws.sub('', str(const['value']))  # Can be a number.
+                out.write('int v8dbg_%s = %s;\n' % (name, value))
         out.write('\n');
 
 #
