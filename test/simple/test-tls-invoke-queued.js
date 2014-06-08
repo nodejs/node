eef0715db56f8b9b0207c1b979e4ff9f5acef475
Fedor Indutny <fedor@indutny.com>
2014-06-08 11:58:07 -0700

    tls_wrap: fix interleaving in InvokeQueued
    WriteItem callback may add new item to the `pending_write_items`. Ensure
    that this item won't be called in the same `InvokeQueued` call, as it
    may result in way-to-early `finish` event on js-side.
    
    fix #7733
    
    Signed-off-by: Fedor Indutny <fedor@indutny.com>

---
+++ test/simple/test-tls-invoke-queued.js
@@ -0,0 +1,65 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+if (!process.versions.openssl) {
+  console.error('Skipping because node compiled without OpenSSL.');
+  process.exit(0);
+}
+
+var assert = require('assert');
+var fs = require('fs');
+var net = require('net');
+var tls = require('tls');
+
+var common = require('../common');
+
+var received = '';
+var ended = 0;
+
+var server = tls.createServer({
+  key: fs.readFileSync(common.fixturesDir + '/keys/agent1-key.pem'),
+  cert: fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem')
+}, function(c) {
+  c._write('hello ', null, function() {
+    c._write('world!', null, function() {
+      c.destroy();
+    });
+    c._write(' gosh', null, function() {});
+  });
+
+  server.close();
+}).listen(common.PORT, function() {
+  var c = tls.connect(common.PORT, {
+    rejectUnauthorized: false
+  }, function() {
+    c.on('data', function(chunk) {
+      received += chunk;
+    });
+    c.on('end', function() {
+      ended++;
+    });
+  });
+});
+
+process.on('exit', function() {
+  assert.equal(ended, 1);
+  assert.equal(received, 'hello world! gosh');
+});
