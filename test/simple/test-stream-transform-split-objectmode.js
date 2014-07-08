6046f96ed490e51076b00097240a57f51f4b8c01
Andrew Oppenlander <andrew.oppenlander@zipscene.com>
2014-07-08 18:06:40 -0400

    streams: set default hwm properly for Duplex
    Default highWaterMark is now set properly when using stream Duplex's
    writableObjectMode and readableObjectMode options.
    
    Added condition to the already existing split objectMode test to ensure
    the highWaterMark is being set to the correct default value on both the
    ReadableState and WritableState for readableObjectMode and
    writableObjectMode.
    
    Signed-off-by: Fedor Indutny <fedor@indutny.com>

--- test/simple/test-stream-transform-split-objectmode.js
+++ test/simple/test-stream-transform-split-objectmode.js
@@ -28,6 +28,8 @@ var parser = new Transform({ readableObjectMode : true });
 
 assert(parser._readableState.objectMode);
 assert(!parser._writableState.objectMode);
+assert(parser._readableState.highWaterMark === 16);
+assert(parser._writableState.highWaterMark === (16 * 1024));
 
 parser._transform = function (chunk, enc, callback) {
   callback(null, { val : chunk[0] });
@@ -50,10 +52,12 @@ var serializer = new Transform({ writableObjectMode : true });
 
 assert(!serializer._readableState.objectMode);
 assert(serializer._writableState.objectMode);
+assert(serializer._readableState.highWaterMark === (16 * 1024));
+assert(serializer._writableState.highWaterMark === 16);
 
 serializer._transform = function (obj, _, callback) {
   callback(null, new Buffer([obj.val]));
-}
+};
 
 var serialized;
 
