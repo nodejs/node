dca90e10800e48762593c2c09b1ca32f78239dd9
cjihrig <cjihrig@gmail.com>
2014-09-20 00:07:33 -0400

    child_process: improve spawn() argument handling
    Add stricter argument type checking to normalizeSpawnArguments().
    
    Removes a number of extraneous checks in spawn().
    
    Fix regression in handling of the optional args argument.
    
    Add more thorough testing of spawn() arguments.
    
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

--- test/simple/test-child-process-spawn-typeerror.js
+++ test/simple/test-child-process-spawn-typeerror.js
@@ -22,12 +22,15 @@
 var spawn = require('child_process').spawn,
   assert = require('assert'),
   windows = (process.platform === 'win32'),
-  cmd = (windows) ? 'ls' : 'dir',
+  cmd = (windows) ? 'dir' : 'ls',
+  invalidcmd = (windows) ? 'ls' : 'dir',
+  invalidArgsMsg = /Incorrect value of args option/,
+  invalidOptionsMsg = /options argument must be an object/,
   errors = 0;
 
 try {
   // Ensure this throws a TypeError
-  var child = spawn(cmd, 'this is not an array');
+  var child = spawn(invalidcmd, 'this is not an array');
 
   child.on('error', function (err) {
     errors++;
@@ -36,6 +39,44 @@ try {
 } catch (e) {
   assert.equal(e instanceof TypeError, true);
 }
+
+// verify that valid argument combinations do not throw
+assert.doesNotThrow(function() {
+  spawn(cmd);
+});
+
+assert.doesNotThrow(function() {
+  spawn(cmd, []);
+});
+
+assert.doesNotThrow(function() {
+  spawn(cmd, {});
+});
+
+assert.doesNotThrow(function() {
+  spawn(cmd, [], {});
+});
+
+// verify that invalid argument combinations throw
+assert.throws(function() {
+  spawn();
+}, /Bad argument/);
+
+assert.throws(function() {
+  spawn(cmd, null);
+}, invalidArgsMsg);
+
+assert.throws(function() {
+  spawn(cmd, true);
+}, invalidArgsMsg);
+
+assert.throws(function() {
+  spawn(cmd, [], null);
+}, invalidOptionsMsg);
+
+assert.throws(function() {
+  spawn(cmd, [], 1);
+}, invalidOptionsMsg);
 
 process.on('exit', function() {
   assert.equal(errors, 0);
