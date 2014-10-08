6bea3fc44c36a5f10d76487e9b7b4dcbcf8d87d6
Sam Roberts <sam@strongloop.com>
2014-10-08 14:36:21 -0700

    process: pid can be a string in process.kill()
    Not allowing string was a change from v0.10 behaviour, commented on in
    joyent/node#7991. Allow them again, but still check that argument is
    numberish. Also, simplify the fragile and non-portable test code
    introduced in 832ec1cd507 that required fixups 2a415358ee, and
    ef3c4ed3d.
    
    PR-URL: https://github.com/joyent/node/pull/8531
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

--- src/node.js
+++ src/node.js
@@ -641,8 +641,8 @@
     process.kill = function(pid, sig) {
       var err;
 
-      if (typeof pid !== 'number' || !isFinite(pid)) {
-        throw new TypeError('pid must be a number');
+      if (pid != (pid | 0)) {
+        throw new TypeError('invalid pid');
       }
 
       // preserve null signal
