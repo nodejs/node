nodejs/node

commit f6e574018090ed4d63596b8a3bb614f8f48b6267

path: resolve normalize drive letter to lower case
make path.resolve work the same as path.normalize

lib/path.js
@@ -163,6 +163,11 @@ if (isWindows) {
     resolvedTail = normalizeArray(resolvedTail.split(/[\\\/]+/).filter(f),
                                   !resolvedAbsolute).join('\\');
 
+     // If device is a drive letter, we'll normalize to lower case.
+     if (resolvedDevice && resolvedDevice.charAt(1) === ':')
+       resolvedDevice = resolvedDevice[0].toLowerCase() +
+        resolvedDevice.substr(1);
+
     return (resolvedDevice + (resolvedAbsolute ? '\\' : '') + resolvedTail) ||
            '.';
   };

test/simple/test-module-nodemodulepaths.js
@@ -21,6 +21,7 @@
 
 var common = require('../common');
 var assert = require('assert');
+var path = require('path');
 
 var module = require('module');
 
@@ -29,7 +30,7 @@ var isWindows = process.platform === 'win32';
 var file, delimiter, paths;
 
 if (isWindows) {
-  file = 'C:\\Users\\Rocko Artischocko\\node_stuff\\foo';
+  file = path.normalize('C:\\Users\\Rocko Artischocko\\node_stuff\\foo');
   delimiter = '\\'
 } else {
   file = '/usr/test/lib/node_modules/npm/foo';

test/simple/test-path.js
@@ -321,7 +321,7 @@ if (isWindows) {
        [['c:/ignore', 'd:\\a/b\\c/d', '\\e.exe'], 'd:\\e.exe'],
        [['c:/ignore', 'c:/some/file'], 'c:\\some\\file'],
        [['d:/ignore', 'd:some/dir//'], 'd:\\ignore\\some\\dir'],
-       [['.'], process.cwd()],
+       [['.'], path.normalize(process.cwd())],
        [['//server/share', '..', 'relative\\'], '\\\\server\\share\\relative'],
        [['c:/', '//'], 'c:\\'],
        [['c:/', '//dir'], 'c:\\dir'],

commit 50249daac29e943128aafe8a25cad6a65035cb81

lint: fix whitespace issues
Fixes: f6e5740 "path: resolve normalize drive letter to lower case"
Signed-off-by: Trevor Norris <trev.norris@gmail.com>

lib/path.js
@@ -163,10 +163,11 @@ if (isWindows) {
     resolvedTail = normalizeArray(resolvedTail.split(/[\\\/]+/).filter(f),
                                   !resolvedAbsolute).join('\\');
 
-     // If device is a drive letter, we'll normalize to lower case.
-     if (resolvedDevice && resolvedDevice.charAt(1) === ':')
-       resolvedDevice = resolvedDevice[0].toLowerCase() +
-        resolvedDevice.substr(1);
+    // If device is a drive letter, we'll normalize to lower case.
+    if (resolvedDevice && resolvedDevice.charAt(1) === ':') {
+      resolvedDevice = resolvedDevice[0].toLowerCase() +
+          resolvedDevice.substr(1);
+    }
 
     return (resolvedDevice + (resolvedAbsolute ? '\\' : '') + resolvedTail) ||
            '.';