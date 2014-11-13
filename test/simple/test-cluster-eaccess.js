79cd7fed9e99f1ca75ef63e357331b635caef128
Rod Vagg <rod@vagg.org>
2014-11-13 15:35:20 +1000

    test: fix test-cluster-eaccess to work on windows
    properly create an EADDRINUSE condition rather than
    simulating one with a plain file
    
    PR-URL: https://github.com/node-forward/node/pull/59
    Reviewed-By: Bert Belder <bertbelder@gmail.com>

--- test/simple/test-cluster-eaccess.js
+++ test/simple/test-cluster-eaccess.js
@@ -19,13 +19,16 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// test that errors propagated from cluster children are properly received in their master
+// creates an EADDRINUSE condition by also forking a child process to listen on a socket
+
 var common = require('../common');
 var assert = require('assert');
 var cluster = require('cluster');
+var fork = require('child_process').fork;
 var fs = require('fs');
 var net = require('net');
 
-var socketPath = common.PIPE;
 
 if (cluster.isMaster) {
   var worker = cluster.fork();
@@ -33,28 +36,32 @@ if (cluster.isMaster) {
   worker.on('message', function(err) {
     gotError++;
     console.log(err);
-    if (process.platform === 'win32')
-      assert.strictEqual('EACCES', err.code);
-    else
-      assert.strictEqual('EADDRINUSE', err.code);
+    assert.strictEqual('EADDRINUSE', err.code);
     worker.disconnect();
   });
   process.on('exit', function() {
     console.log('master exited');
     try {
-      fs.unlinkSync(socketPath);
+      fs.unlinkSync(common.PIPE);
     } catch (e) {
     }
     assert.equal(gotError, 1);
   });
 } else {
-  fs.writeFileSync(socketPath, 'some contents');
+  var cp = fork(common.fixturesDir + '/listen-on-socket-and-exit.js', { stdio: 'inherit' });
 
-  var server = net.createServer().listen(socketPath, function() {
-    console.log('here');
-  });
+  // message from the child indicates it's ready and listening
+  cp.on('message', function() {
+    var server = net.createServer().listen(common.PIPE, function() {
+      console.log('parent listening, should not be!');
+    });
 
-  server.on('error', function(err) {
-    process.send(err);
+    server.on('error', function(err) {
+      console.log('parent error, ending');
+      // message to child process tells it to exit
+      cp.send('end');
+      // propagate error to parent
+      process.send(err);
+    });
   });
 }
