b2464063a228df96dd8acc9db31f284eddd16392
Evan Lucas <evan@btc.com>
2014-12-02 21:16:32 -0600

    net: give better error messages
    Add address and/or port to errors where applicable for better reporting.
    In the event the local address and port are accessible, it will also add
    those to the error message.
    
    See joyent/node#7005
    Reviewed-By: Fedor Indutny <fedor@indutny.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    PR-URL: https://github.com/iojs/io.js/pull/35

---
+++ test/simple/test-net-better-error-messages-listen-path.js
@@ -0,0 +1,9 @@
+var common = require('../common');
+var assert = require('assert');
+var net = require('net');
+var fp = '/blah/fadfa';
+var server = net.createServer(assert.fail);
+server.listen(fp, assert.fail);
+server.on('error', common.mustCall(function(e) {
+  assert.equal(e.address, fp)
+}));
