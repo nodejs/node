nodejs/node

commit 0d051238be2e07e671d7d9f4f444e0cc1efadf1b

timers: fix unref() memory leak
The destructor isn't being called for timers that have been unref'd.

Fixes: https://github.com/joyent/node/issues/8364
Signed-off-by: Trevor Norris <trev.norris@gmail.com>

icu_config.gypi
@@ -1,0 +1,2 @@
+# Do not edit. Generated by the configure script.
+{ 'variables': { }}

lib/timers.js
@@ -290,6 +290,14 @@ var Timeout = function(after) {
   this._repeat = false;
 };
 
+
+function unrefdHandle() {
+  this.owner._onTimeout();
+  if (!this.owner.repeat)
+    this.owner.close();
+}
+
+
 Timeout.prototype.unref = function() {
   if (!this._handle) {
 
@@ -303,7 +311,8 @@ Timeout.prototype.unref = function() {
     if (delay < 0) delay = 0;
     exports.unenroll(this);
     this._handle = new Timer();
-    this._handle.ontimeout = this._onTimeout;
+    this._handle.owner = this;
+    this._handle.ontimeout = unrefdHandle;
     this._handle.start(delay, 0);
     this._handle.domain = this.domain;
     this._handle.unref();

commit 813114dab05231b71f3cdc4f5889b9833d9a1d06

src: remove icu_config
ICU support in v0.12 generates a new icu_config.gypi. This was
accidentally committed after switching branches. The file has been removed
and added to .gitignore.

Fixes: 0d051238 "timers: fix unref() memory leak"

.gitignore
@@ -15,6 +15,7 @@ node_g
 .benchmark_reports
 /.project
 /.cproject
+icu_config.gypi
 
 /out
 

icu_config.gypi
@@ -1,2 +1,0 @@
-# Do not edit. Generated by the configure script.
-{ 'variables': { }}