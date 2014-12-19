ba676bfff598a0731da2c9153a11c81ded0e5d18
Julien Gilli <julien.gilli@joyent.com>
2014-12-18 16:51:08 -0800

    timers: don't close interval timers when unrefd
    This change fixes a regression introduced by commit
    0d051238be2e07e671d7d9f4f444e0cc1efadf1b, which contained a typo that
    would cause every unrefd interval to fire only once.
    
    Fixes: https://github.com/joyent/node/issues/8900
    Reviewed-By: Timothy J Fontaine <tjfontaine@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

---
+++ test/parallel/test-timers-unrefd-interval-still-fires.js
@@ -0,0 +1,18 @@
+/*
+ * This test is a regression test for joyent/node#8900.
+ */
+var assert = require('assert');
+
+var N = 5;
+var nbIntervalFired = 0;
+var timer = setInterval(function() {
+  ++nbIntervalFired;
+  if (nbIntervalFired === N)
+    clearInterval(timer);
+}, 1);
+
+timer.unref();
+
+setTimeout(function onTimeout() {
+  assert.strictEqual(nbIntervalFired, N);
+}, 100);
