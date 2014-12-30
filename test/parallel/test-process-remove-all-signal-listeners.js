09653fb7e793c54bc8a75958dc86809c52023923
Sam Roberts <sam@strongloop.com>
2014-12-29 18:00:02 -0800

    process: fix regression in unlistening signals
    When the last signal listener is removed, the signal wrap should be
    closed, restoring the default signal handling behaviour. This is done in
    a (patched) process.removeListener(). However, events.removeAllListeners
    has an optimization to avoid calling removeListener() if there are no
    listeners for the 'removeListener' event, introduced in 56668f54d1. That
    caused the following code to fail to terminate:
    
        process.stdin.resume();
        function listener() {};
        process.on('SIGINT', listener);
        process.removeAllListeners('SIGINT');
        process.kill(process.pid, 'SIGINT')
    
    while the following will terminate:
    
        process.stdin.resume();
        function listener() {};
        process.on('SIGINT', listener);
        process.removeListener('SIGINT', listener);
        process.kill(process.pid, 'SIGINT')
    
    Replace the method patching with use of the 'newListener' and
    'removeListener' events, which will fire no matter which methods are
    used to add or remove listeners.

---
+++ test/parallel/test-process-remove-all-signal-listeners.js
@@ -0,0 +1,40 @@
+if (process.platform === 'win32') {
+  // Win32 doesn't have signals, just a kindof emulation, insufficient
+  // for this test to apply.
+  return;
+}
+
+var assert = require('assert');
+var spawn = require('child_process').spawn;
+var ok;
+
+if (process.argv[2] !== '--do-test') {
+  // We are the master, fork a child so we can verify it exits with correct
+  // status.
+  process.env.DOTEST = 'y';
+  var child = spawn(process.execPath, [__filename, '--do-test']);
+
+  child.once('exit', function(code, signal) {
+    assert.equal(signal, 'SIGINT');
+    ok = true;
+  });
+
+  process.on('exit', function() {
+    assert(ok);
+  });
+
+  return;
+}
+
+process.on('SIGINT', function() {
+  // Remove all handlers and kill ourselves. We should terminate by SIGINT
+  // now that we have no handlers.
+  process.removeAllListeners('SIGINT');
+  process.kill(process.pid, 'SIGINT');
+});
+
+// Signal handlers aren't sufficient to keep node alive, so resume stdin
+process.stdin.resume();
+
+// Demonstrate that signals are being handled
+process.kill(process.pid, 'SIGINT');
