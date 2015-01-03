b636ba8186d191c52ee36f2f2b1aebbbb4d95575
cjihrig <cjihrig@gmail.com>
2015-01-03 18:26:26 -0500

    net: make connect() input validation synchronous
    Socket.prototype.connect() sometimes throws on bad inputs
    after an asynchronous operation. This commit makes the input
    validation synchronous. This commit also removes some hard
    coded IP addresses.
    
    PR-URL: https://github.com/joyent/node/pull/8180
    Fixes: https://github.com/joyent/node/issues/8140
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>
    Reviewed-By: Fedor Indutny <fedor@indutny.com>
    Reviewed-By: Timothy J Fontaine <tjfontaine@gmail.com>

--- test/simple/test-process-active-wraps.js
+++ test/simple/test-process-active-wraps.js
@@ -41,9 +41,16 @@ var handles = [];
 })();
 
 (function() {
+  function onlookup() {
+    setImmediate(function() {
+      assert.equal(process._getActiveRequests().length, 0);
+    });
+  };
+
   expect(1, 0);
   var conn = net.createConnection(common.PORT);
-  conn.on('error', function() { /* ignore */ });
+  conn.on('lookup', onlookup); 
+  conn.on('error', function() { assert(false); });
   expect(2, 1);
   conn.destroy();
   expect(2, 1); // client handle doesn't shut down until next tick
@@ -52,10 +59,15 @@ var handles = [];
 
 (function() {
   var n = 0;
+
   handles.forEach(function(handle) {
     handle.once('close', onclose);
   });
   function onclose() {
-    if (++n === handles.length) setImmediate(expect, 0, 0);
+    if (++n === handles.length) {
+      setImmediate(function() {
+        assert.equal(process._getActiveHandles().length, 0);
+      });
+    }
   }
 })();
