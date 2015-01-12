42ebdcbff1b0846c8e2d291ee320c5e0fcfcb09e
Ben Noordhuis <info@bnoordhuis.nl>
2015-01-12 22:01:27 +0100

    addon: update NODE_MODULE_VERSION macro
    io.js v1.0.0 is not ABI-compatible with joyent/node@v0.12, update
    the NODE_MODULE_VERSION macro to reflect that.  Update to a much
    larger value to avoid future clashes with joyent/node.
    
    PR-URL: https://github.com/iojs/io.js/pull/312
    Reviewed-By: Bert Belder <bertbelder@gmail.com>

--- src/node_version.h
+++ src/node_version.h
@@ -66,6 +66,6 @@
  * an API is broken in the C++ side, including in v8 or
  * other dependencies.
  */
-#define NODE_MODULE_VERSION 14 /* v0.12 */
+#define NODE_MODULE_VERSION 42  /* io.js v1.0.0 */
 
 #endif  /* SRC_NODE_VERSION_H_ */
