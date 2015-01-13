27e9ed6e9819794220fa7bd682125116ae76d3c4
Fedor Indutny <fedor@indutny.com>
2015-01-13 16:28:56 +0300

    test: fix setproctitle on FreeBSD
    PR-URL: https://github.com/iojs/io.js/pull/332
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/sequential/test-setproctitle.js
+++ test/sequential/test-setproctitle.js
@@ -24,7 +24,7 @@ exec('ps -p ' + process.pid + ' -o args=', function(error, stdout, stderr) {
   assert.equal(stderr, '');
 
   // freebsd always add ' (procname)' to the process title
-  if (process.platform === 'freebsd') title += ' (node)';
+  if (process.platform === 'freebsd') title += ' (iojs)';
 
   // omitting trailing whitespace and \n
   assert.equal(stdout.replace(/\s+$/, ''), title);
