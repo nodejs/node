851c9937168e2ff4ed67d4a67977171efc557c80
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2015-02-06 20:48:14 +0300

    src: cleanup `Isolate::GetCurrent()`
    PR-URL: https://github.com/iojs/io.js/pull/807
    Reviewed-by: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

--- src/node.h
+++ src/node.h
@@ -190,7 +190,7 @@ NODE_EXTERN void RunAtExit(Environment* env);
 // Used to be a macro, hence the uppercase name.
 #define NODE_DEFINE_CONSTANT(target, constant)                                \
   do {                                                                        \
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();                         \
+    v8::Isolate* isolate = target->GetIsolate();                              \
     v8::Local<v8::String> constant_name =                                     \
         v8::String::NewFromUtf8(isolate, #constant);                          \
     v8::Local<v8::Number> constant_value =                                    \
