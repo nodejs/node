a97ebc0ccf703e93ebf2b2bd0cd97cc243ab9a76
Evan Lucas <evanlucas@me.com>
2015-02-18 12:02:01 -0600

    net: persist net.Socket options before connect
    Remembers net.Socket options called before connect and retroactively
    applies them after the handle has been created.
    
    This change makes the following function calls more user-friendly:
    
    - setKeepAlive()
    - setNoDelay()
    - ref()
    - unref()
    
    Related: https://github.com/joyent/node/issues/7077 and
    https://github.com/joyent/node/issues/8572
    
    Fixes: https://github.com/joyent/node/issues/7077
    Fixes: https://github.com/joyent/node/issues/8572
    PR-URL: https://github.com/nodejs/io.js/pull/1518
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Roman Reiss <me@silverwind.io>

---
+++ test/parallel/test-net-persistent-keepalive.js
@@ -0,0 +1,33 @@
+'use strict';
+var common = require('../common');
+var assert = require('assert');
+var net = require('net');
+
+var serverConnection;
+var echoServer = net.createServer(function(connection) {
+  serverConnection = connection;
+  connection.setTimeout(0);
+  assert.equal(typeof connection.setKeepAlive, 'function');
+  connection.on('end', function() {
+    connection.end();
+  });
+});
+echoServer.listen(common.PORT);
+
+echoServer.on('listening', function() {
+  var clientConnection = new net.Socket();
+  // send a keepalive packet after 1000 ms
+  // and make sure it persists
+  clientConnection.setKeepAlive(true, 400);
+  clientConnection.connect(common.PORT);
+  clientConnection.setTimeout(0);
+
+  setTimeout(function() {
+    // make sure both connections are still open
+    assert.equal(serverConnection.readyState, 'open');
+    assert.equal(clientConnection.readyState, 'open');
+    serverConnection.end();
+    clientConnection.end();
+    echoServer.close();
+  }, 600);
+});
