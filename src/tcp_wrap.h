a43af39ffc369776f776c69e5287c9632349fc2b
Ben Noordhuis <info@bnoordhuis.nl>
2015-02-25 16:40:40 +0100

    src: DRY getsockname/getpeername code
    Extract the common logic into a template function.  No functional
    changes, just code cleanup.
    
    PR-URL: https://github.com/nodejs/node/pull/956
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- src/tcp_wrap.h
+++ src/tcp_wrap.h
@@ -19,12 +19,16 @@ class TCPWrap : public StreamWrap {
   size_t self_size() const override { return sizeof(*this); }
 
  private:
+  typedef uv_tcp_t HandleType;
+
+  template <typename T,
+            int (*F)(const typename T::HandleType*, sockaddr*, int*)>
+  friend void GetSockOrPeerName(const v8::FunctionCallbackInfo<v8::Value>&);
+
   TCPWrap(Environment* env, v8::Handle<v8::Object> object, AsyncWrap* parent);
   ~TCPWrap();
 
   static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
-  static void GetSockName(const v8::FunctionCallbackInfo<v8::Value>& args);
-  static void GetPeerName(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetNoDelay(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetKeepAlive(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Bind(const v8::FunctionCallbackInfo<v8::Value>& args);
