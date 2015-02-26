b150c9839e2e522dbdaef9f6f6e2863842ca63d9
Ben Noordhuis <info@bnoordhuis.nl>
2015-02-26 21:34:03 +0100

    src: fix -Wempty-body compiler warnings
    Turn counter macros into no-op instructions when counters are disabled.
    Evaluating to nothing makes gcc complain when the macro is used in a
    conditional.  Fixes the following warning:
    
        ../src/tls_wrap.cc:320:5: warning:
        suggest braces around empty body in an 'if' statement [-Wempty-body]
             NODE_COUNT_NET_BYTES_SENT(write_size_);
             ^
    
    PR-URL: https://github.com/iojs/io.js/pull/974
    Reviewed-By: Chris Dickinson <christopher.s.dickinson@gmail.com>

--- src/node_counters.h
+++ src/node_counters.h
@@ -7,18 +7,18 @@
 #include "node_win32_perfctr_provider.h"
 #else
 #define NODE_COUNTER_ENABLED() (false)
-#define NODE_COUNT_HTTP_SERVER_REQUEST()
-#define NODE_COUNT_HTTP_SERVER_RESPONSE()
-#define NODE_COUNT_HTTP_CLIENT_REQUEST()
-#define NODE_COUNT_HTTP_CLIENT_RESPONSE()
-#define NODE_COUNT_SERVER_CONN_OPEN()
-#define NODE_COUNT_SERVER_CONN_CLOSE()
-#define NODE_COUNT_NET_BYTES_SENT(bytes)
-#define NODE_COUNT_NET_BYTES_RECV(bytes)
-#define NODE_COUNT_GET_GC_RAWTIME()
-#define NODE_COUNT_GC_PERCENTTIME(percent)
-#define NODE_COUNT_PIPE_BYTES_SENT(bytes)
-#define NODE_COUNT_PIPE_BYTES_RECV(bytes)
+#define NODE_COUNT_GC_PERCENTTIME(percent) do { } while (false)
+#define NODE_COUNT_GET_GC_RAWTIME() do { } while (false)
+#define NODE_COUNT_HTTP_CLIENT_REQUEST() do { } while (false)
+#define NODE_COUNT_HTTP_CLIENT_RESPONSE() do { } while (false)
+#define NODE_COUNT_HTTP_SERVER_REQUEST() do { } while (false)
+#define NODE_COUNT_HTTP_SERVER_RESPONSE() do { } while (false)
+#define NODE_COUNT_NET_BYTES_RECV(bytes) do { } while (false)
+#define NODE_COUNT_NET_BYTES_SENT(bytes) do { } while (false)
+#define NODE_COUNT_PIPE_BYTES_RECV(bytes) do { } while (false)
+#define NODE_COUNT_PIPE_BYTES_SENT(bytes) do { } while (false)
+#define NODE_COUNT_SERVER_CONN_CLOSE() do { } while (false)
+#define NODE_COUNT_SERVER_CONN_OPEN() do { } while (false)
 #endif
 
 #include "v8.h"
