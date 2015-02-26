b2a0d8f65e16e2299f8dab47cfe89233fee7931b
Ben Noordhuis <info@bnoordhuis.nl>
2015-02-26 11:27:18 +0100

    deps: update libuv to 1.4.2
    PR-URL: https://github.com/iojs/io.js/pull/966
    Reviewed-By: Fedor Indutny <fedor@indutny.com>

--- deps/uv/src/unix/stream.c
+++ deps/uv/src/unix/stream.c
@@ -394,7 +394,8 @@ int uv__stream_open(uv_stream_t* stream, int fd, int flags) {
 #if defined(__APPLE__)
   enable = 1;
   if (setsockopt(fd, SOL_SOCKET, SO_OOBINLINE, &enable, sizeof(enable)) &&
-      errno != ENOTSOCK) {
+      errno != ENOTSOCK &&
+      errno != EINVAL) {
     return -errno;
   }
 #endif
