nodejs/node

commit 1fddc1fee84785d296e37337dcb0dc12c13a049f

http: do not send `0\r\n\r\n` in TE HEAD responses
When replying to a HEAD request, do not attempt to send the trailers and
EOF sequence (`0\r\n\r\n`). The HEAD request MUST not have body.

Quote from RFC:

The presence of a message body in a response depends on both the
request method to which it is responding and the response status code
(Section 3.1.2).  Responses to the HEAD request method (Section 4.3.2
of [RFC7231]) never include a message body because the associated
response header fields (e.g., Transfer-Encoding, Content-Length,
etc.), if present, indicate only what their values would have been if
the request method had been GET (Section 4.3.1 of [RFC7231]).

fix #8361

Reviewed-By: Timothy J Fontaine <tjfontaine@gmail.com>

lib/http.js
@@ -945,6 +945,10 @@ OutgoingMessage.prototype.end = function(data, encoding) {
   if (encoding === 'hex' || encoding === 'base64')
     hot = false;
 
+  // Transfer-encoding: chunked responses to HEAD requests
+  if (this._hasBody && this.chunkedEncoding)
+    hot = false;
+
   if (hot) {
     // Hot path. They're doing
     //   res.writeHead();
@@ -982,7 +986,7 @@ OutgoingMessage.prototype.end = function(data, encoding) {
   }
 
   if (!hot) {
-    if (this.chunkedEncoding) {
+    if (this._hasBody && this.chunkedEncoding) {
       ret = this._send('0\r\n' + this._trailer + '\r\n', 'ascii');
     } else {
       // Force a flush, HACK.

test/simple/test-http-head-request.js
@@ -26,32 +26,44 @@ var util = require('util');
 
 
 var body = 'hello world\n';
+var id = 0;
 
-var server = http.createServer(function(req, res) {
-  common.error('req: ' + req.method);
-  res.writeHead(200, {'Content-Length': body.length});
-  res.end();
-  server.close();
-});
+function test(headers) {
+  var port = common.PORT + id++;
 
-var gotEnd = false;
-
-server.listen(common.PORT, function() {
-  var request = http.request({
-    port: common.PORT,
-    method: 'HEAD',
-    path: '/'
-  }, function(response) {
-    common.error('response start');
-    response.on('end', function() {
-      common.error('response end');
-      gotEnd = true;
-    });
-    response.resume();
+  var server = http.createServer(function(req, res) {
+    console.error('req: %s headers: %j', req.method, headers);
+    res.writeHead(200, headers);
+    res.end();
+    server.close();
   });
-  request.end();
-});
 
-process.on('exit', function() {
-  assert.ok(gotEnd);
+  var gotEnd = false;
+
+  server.listen(port, function() {
+    var request = http.request({
+      port: port,
+      method: 'HEAD',
+      path: '/'
+    }, function(response) {
+      console.error('response start');
+      response.on('end', function() {
+        console.error('response end');
+        gotEnd = true;
+      });
+      response.resume();
+    });
+    request.end();
+  });
+
+  process.on('exit', function() {
+    assert.ok(gotEnd);
+  });
+}
+
+test({
+  'Transfer-Encoding': 'chunked'
+});
+test({
+  'Content-Length': body.length
 });

commit 8bcd0a4c4a8e3d748bea53d036985609ea634b8d

http: fix performance regression for GET requests
A significant performance regressions has been introduced in 1fddc1f for
GET requests which send data through response.end(). The number of
requests per second dropped to somewhere around 6% of their previous
level.

The fix consists of removing a part of the lines added by 1fddc1f,
lines which were supposed to affect only HEAD requests, but interfered
with GET requests instead.

The lines removed would not have affected the behaviour in the case of
a HEAD request as this._hasBody would always be false. Therefore, they
were not required to fix the issue reported in #8361.

Fixes #8940.

PR: #9026
PR-URL: https://github.com/joyent/node/pull/9026
Reviewed-By: Julien Gilli <julien.gilli@joyent.com>

lib/http.js
@@ -946,10 +946,6 @@ OutgoingMessage.prototype.end = function(data, encoding) {
   if (encoding === 'hex' || encoding === 'base64')
     hot = false;
 
-  // Transfer-encoding: chunked responses to HEAD requests
-  if (this._hasBody && this.chunkedEncoding)
-    hot = false;
-
   if (hot) {
     // Hot path. They're doing
     //   res.writeHead();