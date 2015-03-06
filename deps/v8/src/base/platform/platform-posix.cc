38064e4af9cdc9e45abf6727a77faa39ee58a5f5
Johan Bergström <bugs@bergstroem.nu>
2015-03-06 14:50:32 +1100

    v8: fix build on solaris platforms
    `v8/3c7e4403` introduced a different cast which broke building on
    Illumos. Revert to previous behavior for V8_OS_SOLARIS.
    
    Found on SmartOS while building with gcc 4.9.0.

--- deps/v8/src/base/platform/platform-posix.cc
+++ deps/v8/src/base/platform/platform-posix.cc
@@ -260,6 +260,8 @@ int OS::GetCurrentThreadId() {
   return static_cast<int>(syscall(__NR_gettid));
 #elif V8_OS_ANDROID
   return static_cast<int>(gettid());
+#elif V8_OS_SOLARIS
+  return static_cast<int>(pthread_self());
 #else
   return static_cast<int>(reinterpret_cast<intptr_t>(pthread_self()));
 #endif
