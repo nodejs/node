414adfa3f6761fe3b5619f5194d0183ed29791b3
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2015-03-13 09:55:29 +0900

    crypto: add deprecated ValiCert CA for cross cert
    The host of melissadata.net has a cross root certification between
    Starfield Class 2 and ValiCert Class 2. OpenSSL-1.0.1 only looks up
    a cert chain to the deprecated ValiCert Class 2 CA and causes
    untrusted error. We add it for a short-term remedy and it is to be
    removed after upgrading OpenSSSL-1.0.2 and applying private patches
    to support alternative cert chains.
    See #402 and #589.
    
    Fixes: https://github.com/iojs/io.js/issues/923

---
+++ test/internet/test-tls-connnect-melissadata.js
@@ -0,0 +1,7 @@
+// Test for authorized access to the server which has a cross root
+// certification between Starfield Class 2 and ValiCert Class 2
+var tls = require('tls');
+var socket = tls.connect(443, 'address.melissadata.net', function() {
+  socket.resume();
+  socket.destroy();
+});
