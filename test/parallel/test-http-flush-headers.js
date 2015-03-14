b6ad10ed3f8db08a4453a4f990623e8a0fe405a9
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2015-03-14 16:24:07 +0900

    http: add flushHeaders and deprecate flush

---
+++ test/parallel/test-http-flush-headers.js
@@ -0,0 +1,20 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const http = require('http');
+
+const server = http.createServer();
+server.on('request', function(req, res){
+  assert(req.headers['foo'], 'bar');
+  res.end('ok');
+  server.close();
+});
+server.listen(common.PORT, '127.0.0.1', function() {
+  let req = http.request({
+    method: 'GET',
+    host: '127.0.0.1',
+    port: common.PORT,
+  });
+  req.setHeader('foo', 'bar');
+  req.flushHeaders();
+});
