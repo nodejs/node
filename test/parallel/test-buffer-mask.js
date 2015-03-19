7bf9a2dfaef0e7ab2fb09c25c89aa2b509e08d4b
Chris Dickinson <christopher.s.dickinson@gmail.com>
2015-03-19 02:02:58 -0700

    test style fixes

--- test/parallel/test-buffer-mask.js
+++ test/parallel/test-buffer-mask.js
@@ -10,21 +10,16 @@ var tests = [
 
 tests.forEach(Function.prototype.call.bind(Function.prototype.call))
 
-function referenceImplementation(source, maskNum, output, offset, length) {
+function referenceImplementation(source, maskNum, output, offset, start, end) {
   var i = 0;
   var mask = new Buffer(4);
+  var length = end - start;
   mask.writeUInt32LE(maskNum, 0);
-
-  for (; i < length - 3; i += 4) {
-    var num = (maskNum ^ source.readUInt32LE(i, true)) >>> 0;
-    if (num < 0) num = 4294967296 + num;
-    output.writeUInt32LE(num, offset + i, true);
-  }
-
-  switch (length % 4) {
-    case 3: output[offset + i + 2] = source[i + 2] ^ mask[2];
-    case 2: output[offset + i + 1] = source[i + 1] ^ mask[1];
-    case 1: output[offset + i] = source[i] ^ mask[0];
+  var toCopy = Math.min(length, output.length - offset);
+  var maskIdx = 0;
+  for (var i = 0; i < toCopy; ++i) {
+    output[i + offset] = source[i + start] ^ mask[maskIdx];
+    maskIdx = (maskIdx + 1) & 3;
   }
 }
 
@@ -37,32 +32,33 @@ function testBasic() {
   for (var i = 0; i < input.length; ++i)
     input[i] = i;
 
-  referenceImplementation(input, mask, refoutput, 0, 256);
-  input.mask(mask, output, 256);
-
-  for(var i = 0; i < input.length; ++i)
+  output[0] = refoutput[0] = 0;
+  referenceImplementation(input, mask, refoutput, 1, 0, 255);
+  input.mask(mask, output, 1, 0, 255);
+  for (var i = 0; i < input.length; ++i) {
     assert.equal(output[i], refoutput[i]);
+  }
 }
 
 function testBounds() {
-  var input = new Buffer(16)
-  var output = new Buffer(32)
+  var input = new Buffer(16);
+  var output = new Buffer(32);
   try {
-    input.mask(120120, output, 17)
-    assert.fail('expected error')
+    input.mask(120120, output, 0, 0, 17);
+    assert.fail('expected error');
   } catch(err) {
-    assert.ok(err)
+    assert.ok(err);
   }
 }
 
 function testOverflow() {
-  var input = new Buffer(16)
-  var output = new Buffer(15)
+  var input = new Buffer(16);
+  var output = new Buffer(15);
   try {
-    input.mask(120120, output)
-    assert.fail('expected error')
+    input.mask(120120, output);
+    assert.fail('expected error');
   } catch(err) {
-    assert.ok(err)
+    assert.ok(err);
   }
 }
 
@@ -76,10 +72,10 @@ function testNonAligned() {
     input[i] = i;
 
   for (var end = 3; end > 0; --end) {
-    referenceImplementation(input, mask, refoutput, 0, end);
-    input.mask(mask, output, end);
+    referenceImplementation(input, mask, refoutput, 0, 0, end);
+    input.mask(mask, output, 0, 0, end);
 
-    for(var i = 0; i < end; ++i)
+    for (var i = 0; i < end; ++i)
       assert.equal(output[i], refoutput[i]);
   }
 }
