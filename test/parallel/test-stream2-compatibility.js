cc0342a517279c9c52543e3a37e11da3fc6cdb36
Brian White <mscdex@mscdex.net>
2015-03-19 22:04:07 -0400

    streams: update .readable/.writable to false
    These properties were initially used to determine stream status
    back in node v0.8 and earlier. Since streams2 however, these
    properties were *always* true, which can be misleading for
    example if you are trying to immediately determine whether
    a Writable stream is still writable or not (to avoid a "write after
    end" exception).
    
    PR-URL: https://github.com/nodejs/node/pull/4083
    Reviewed-By: Chris Dickinson <christopher.s.dickinson@gmail.com>

--- test/parallel/test-stream2-compatibility.js
+++ test/parallel/test-stream2-compatibility.js
@@ -1,6 +1,7 @@
 'use strict';
 var common = require('../common');
 var R = require('_stream_readable');
+var W = require('_stream_writable');
 var assert = require('assert');
 
 var util = require('util');
@@ -28,5 +29,26 @@ TestReader.prototype._read = function(n) {
 var reader = new TestReader();
 setImmediate(function() {
   assert.equal(ondataCalled, 1);
+  console.log('ok');
+  reader.push(null);
+});
+
+function TestWriter() {
+  W.apply(this);
+  this.write('foo');
+  this.end();
+}
+
+util.inherits(TestWriter, W);
+
+TestWriter.prototype._write = function(chunk, enc, cb) {
+  cb();
+};
+
+var writer = new TestWriter();
+
+process.on('exit', function() {
+  assert.strictEqual(reader.readable, false);
+  assert.strictEqual(writer.writable, false);
   console.log('ok');
 });
