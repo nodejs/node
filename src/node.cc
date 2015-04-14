f99eaefe75b47d4908c806d5c1b61042ff149b02
Julien Gilli <julien.gilli@joyent.com>
2015-04-14 00:21:56 +0000

    src: fix backport for SIGINT crash fix on FreeBSD
    61fe1fe21ba281b05b90a632e2dad29eefb14a0a backported
    b64983d77cc3ed2e4b585f3bfc8ed23802389a52 from io.js, but failed to
    change nullptr to NULL, which lead to a build break on FreeBSD since the
    current build system doesn't enable support for C++11.
    
    This change replaces nullptr by NULL, and has been tested on
    FreeBSD 10.1-RELEASE-p8.
    
    Fixes #9326.
    
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    PR-URL: https://github.com/joyent/node/pull/14819

--- src/node.cc
+++ src/node.cc
@@ -2790,7 +2790,7 @@ static void SignalExit(int signo) {
   struct sigaction sa;
   memset(&sa, 0, sizeof(sa));
   sa.sa_handler = SIG_DFL;
-  CHECK_EQ(sigaction(signo, &sa, nullptr), 0);
+  CHECK_EQ(sigaction(signo, &sa, NULL), 0);
 #endif
   raise(signo);
 }
