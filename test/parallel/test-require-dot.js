5e842104515dc1d560f14c1931e6aeccc2a5fa2a
Roman Reiss <me@silverwind.io>
2015-04-16 19:50:52 +0200

    Fix test and nits

--- test/parallel/test-require-dot.js
+++ test/parallel/test-require-dot.js
@@ -1,5 +1,6 @@
 var common = require('../common');
 var assert = require('assert');
+var module = require('module');
 
 var a = require(common.fixturesDir + '/module-require/relative/dot.js');
 var b = require(common.fixturesDir + '/module-require/relative/dot-slash.js');
@@ -12,4 +13,4 @@ module._initPaths();
 
 var c = require('.');
 
-assert.equal(c, 42, 'require(".") should honor NODE_PATH');
+assert.equal(c.value, 42, 'require(".") should honor NODE_PATH');
