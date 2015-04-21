1d0a5fd1f5404c3f60ae82d4edcaa72cc18ba450
Chris Yip <i@chrisyip.im>
2015-04-21 15:13:27 +0800

    Remove '' from Module.globalPaths

--- test/parallel/test-module-globalpaths-nodepath.js
+++ test/parallel/test-module-globalpaths-nodepath.js
@@ -6,20 +6,22 @@ var module = require('module');
 var isWindows = process.platform === 'win32';
 
 var partA, partB;
+var partC = '';
 
 if (isWindows) {
   partA = 'C:\\Users\\Rocko Artischocko\\AppData\\Roaming\\npm';
   partB = 'C:\\Program Files (x86)\\nodejs\\';
-  process.env['NODE_PATH'] = partA + ';' + partB;
+  process.env['NODE_PATH'] = partA + ';' + partB + ';' + partC;
 } else {
   partA = '/usr/test/lib/node_modules';
   partB = '/usr/test/lib/node';
-  process.env['NODE_PATH'] = partA + ':' + partB;
+  process.env['NODE_PATH'] = partA + ':' + partB + ':' + partC;
 }
 
 module._initPaths();
 
 assert.ok(module.globalPaths.indexOf(partA) !== -1);
 assert.ok(module.globalPaths.indexOf(partB) !== -1);
+assert.ok(module.globalPaths.indexOf(partC) === -1);
 
 assert.ok(Array.isArray(module.globalPaths));
