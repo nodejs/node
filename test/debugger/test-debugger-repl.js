4e94871e03ebf113e8783bfdd88e2769f2c500cd
Jackson Tian <puling.tyq@alibaba-inc.com>
2015-04-22 00:34:52 +0800

    debugger: introduce exec method for debugger
    In debugger, the usage of `repl` very ugly. I'd like there is a `p`
    like gdb. So the `exec` is coming.
    
    Usage:
    
    ```
    $ ./iojs debug ~/git/node_research/server.js
    < Debugger listening on port 5858
    connecting to 127.0.0.1:5858 ... ok
    break in /Users/jacksontian/git/node_research/server.js:1
    > 1 var http = require('http');
      2
      3 http.createServer(function (req, res) {
    debug> exec process.title
    /Users/jacksontian/git/io.js/out/Release/iojs
    debug>
    ```
    
    And the `repl`:
    
    ```
    debug> repl
    Press Ctrl + C to leave debug repl
    > process.title
    '/Users/jacksontian/git/io.js/out/Release/iojs'
    debug>
    (^C again to quit)
    ```
    
    The enter and leave debug repl is superfluous.
    
    R-URL: https://github.com/nodejs/node/pull/1491
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Roman Reiss <me@silverwind.io>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/debugger/test-debugger-repl.js
+++ test/debugger/test-debugger-repl.js
@@ -51,8 +51,18 @@ addTest('sb("setInterval()", "!(setInterval.flag++)")', [
 
 // Continue
 addTest('c', [
-  /break in node.js:\d+/,
+  /break in timers.js:\d+/,
   /\d/, /\d/, /\d/, /\d/, /\d/
+]);
+
+// Execute
+addTest('exec process.title', [
+  /node/
+]);
+
+// Execute
+addTest('exec exec process.title', [
+  /SyntaxError: Unexpected identifier/
 ]);
 
 // REPL and process.env regression
