5b225cd99a5833781a7a61d68b65e0dcd927c5bb
Guilherme Souza <sitegui@sitegui.com.br>
2015-04-21 18:24:13 -0300

    tls_wrap: use localhost if options.host is empty
    tls.connect(options) with no options.host should accept a certificate
    with CN: 'localhost'. Fix Error: Hostname/IP doesn't match
    certificate's altnames: "Host: undefined. is not cert's CN: localhost"
    
    'localhost' is not added directly to defaults because that is not
    always desired (for example, when using options.socket)
    
    See https://github.com/iojs/io.js/issues/1489

---
+++ test/parallel/test-tls-connect-no-host.js
@@ -0,0 +1,31 @@
+var common = require('../common');
+
+if (!common.hasCrypto) {
+  console.log('1..0 # Skipped: missing crypto');
+  process.exit();
+}
+var tls = require('tls');
+
+var fs = require('fs');
+var path = require('path');
+
+var cert = fs.readFileSync(path.join(common.fixturesDir, 'test_cert.pem'));
+var key = fs.readFileSync(path.join(common.fixturesDir, 'test_key.pem'));
+
+// https://github.com/iojs/io.js/issues/1489
+// tls.connect(options) with no options.host should accept a cert with CN:'localhost'
+tls.createServer({
+  key: key,
+  cert: cert
+}).listen(common.PORT);
+
+tls.connect({
+    port: common.PORT,
+    ca: cert,
+    // No host set here. 'localhost' is the default,
+    // but tls.checkServerIdentity() breaks before the fix with:
+    // Error: Hostname/IP doesn't match certificate's altnames: "Host: undefined. is not cert's CN: localhost"
+}, function () {
+    console.log('OK');
+    process.exit();
+});
