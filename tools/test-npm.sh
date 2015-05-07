3416e932ca07c527741841dfd01f7e6535b95fca
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2015-05-07 16:48:12 -0400

    tools: refactor `make test-npm` into test-npm.sh
    Extracts test-npm from Makefile and puts it in tools/test-npm.sh
    Also improves test-npm to use a separate copy of deps/npm for testing.
    
    PR-URL: https://github.com/iojs/io.js/pull/1662
    Reviewed-By: Johan Bergström <bugs@bergstroem.nu>
    Reviewed-By: Evan Lucas <evanlucas@me.com>

---
+++ tools/test-npm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+# always change the working directory to the project's root directory
+cd $(dirname $0)/..
+
+# pass NODE_EXE from something like Makefile
+# it should point to either {./}node or {./}node.exe, depending on the platform
+if [ -z $NODE_EXE ]; then
+  echo "No node executable provided. Bailing." >&2
+  exit 0
+fi
+
+rm -rf test-npm
+mkdir test-npm
+
+# make a copy of deps/npm to run the tests on
+cp -r deps/npm/ test-npm/
+
+cd test-npm
+
+# do a rm first just in case deps/npm contained these
+rm -rf npm-cache npm-tmp npm-prefix
+mkdir npm-cache npm-tmp npm-prefix
+
+# set some npm env varibles to point to our new temporary folders
+export npm_config_cache="npm-cache"
+export npm_config_prefix="npm-prefix"
+export npm_config_tmp="npm-tmp"
+
+# install npm devDependencies and run npm's tests
+../$NODE_EXE cli.js install --ignore-scripts
+../$NODE_EXE cli.js run-script test-all
+
+# clean up everything one single shot
+cd .. && rm -rf test-npm
