bad95b1c2a1e95f7cdaa832032579b0e607393e0
Evan Lucas <evanlucas@me.com>
2015-05-07 16:09:31 -0500

    events: provide better error message for unhandled error
    Previously, in the event of an unhandled error event, if the error is a
    not an actual Error, then a default error is thrown. Now, the argument
    is appended to the error message and added as the `context` property
    of the error.
    
    PR-URL: https://github.com/iojs/io.js/pull/1654
    Reviewed-By: Rod Vagg <rod@vagg.org>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

---
+++ test/parallel/test-event-emitter-errors.js
@@ -0,0 +1,8 @@
+var EventEmitter = require('events');
+var assert = require('assert');
+
+var EE = new EventEmitter();
+
+assert.throws(function() {
+  EE.emit('error', 'Accepts a string');
+}, /Accepts a string/);
