f9688bde15f240ba8e34b2e09e861d516a893632
Roman Klauke <romaaan.git@gmail.com>
2015-05-07 20:27:12 +0200

    v8: add type check to make failed calls visible
    Calling v8.setFlagsFromString with e.g a function as a flag
    argument gave no exception or warning that the function call will
    fail. There is now an exception if the function gets called with
    the wrong flag type (string is required) or that a flag is
    expected.
    Other APIs already provide exceptions if the argument has
    not the expected type.

---
+++ test/parallel/test-v8-flag-type-check.js
@@ -0,0 +1,6 @@
+var common = require('../common');
+var assert = require('assert');
+var v8 = require('v8');
+
+assert.throws(function() {v8.setFlagsFromString(1)}, TypeError);
+assert.throws(function() {v8.setFlagsFromString()}, TypeError);
