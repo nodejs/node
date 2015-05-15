nodejs/node

commit 5b636feaa8550ca39229e7d8805c73c72f7995dc

process: relocate needSpinner calls
It's only necessary to request the spinner once tickDone has been
called, and not on every nextTick.

src/node.js
@@ -355,11 +355,11 @@
         } else {
           nextTickQueue.splice(0, infoBox[index]);
           infoBox[length] = nextTickQueue.length;
-          if (needSpinner) {
-            _needTickCallback();
-            needSpinner = false;
-          }
         }
+      }
+      if (needSpinner) {
+        _needTickCallback();
+        needSpinner = false;
       }
       inTick = false;
       infoBox[index] = 0;
@@ -483,11 +483,6 @@
 
       nextTickQueue.push(obj);
       infoBox[length]++;
-
-      if (needSpinner) {
-        _needTickCallback();
-        needSpinner = false;
-      }
     }
 
     function _nextDomainTick(callback) {
@@ -501,11 +496,6 @@
 
       nextTickQueue.push(obj);
       infoBox[length]++;
-
-      if (needSpinner) {
-        _needTickCallback();
-        needSpinner = false;
-      }
     }
   };
 

commit ed74eaab9b193272791916774685cc900571cfdf

test: eval mode net.listen should emit listening
Server does not emit a listen event when no address is specified and
is called in eval mode.

Regression introduced in 5b636feaa8550ca39229e7d8805c73c72f7995dc.

test/parallel/test-net-listen-anyaddr.js
@@ -1,0 +1,15 @@
+var assert = require('assert');
+var common = require('../common');
+var cp = require('child_process');
+
+var args = [
+  '-e',
+  "require('net').createServer().listen(0, function() { this.close(); });",
+];
+
+// should exit cleanly almost immediately
+var opts = {
+  timeout: common.platformTimeout(1000),
+};
+
+cp.execFile(process.execPath, args, opts, assert.ifError);