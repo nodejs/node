bb1db5112f8136c4014668c35d3a805a594f3ae5
Roman Reiss <me@silverwind.io>
2015-05-19 20:16:54 +0200

    Revert and blacklist test-fs-non-number-arguments-throw

--- test/parallel/test-fs-non-number-arguments-throw.js
+++ test/parallel/test-fs-non-number-arguments-throw.js
@@ -1,24 +1,23 @@
-'use strict';
 var assert = require('assert'),
     fs = require('fs'),
     saneEmitter,
     sanity = 'ire(\'assert\')';
 
-saneEmitter = fs.createReadStream(__filename, { start: 31, end: 43 });
+saneEmitter = fs.createReadStream(__filename, { start: 17, end: 29 });
 
-assert.throws(function() {
-  fs.createReadStream(__filename, { start: '31', end: 43 });
+assert.throws(function () {
+  fs.createReadStream(__filename, { start: "17", end: 29 });
 }, "start as string didn't throw an error for createReadStream");
 
-assert.throws(function() {
-  fs.createReadStream(__filename, { start: 31, end: '43' });
+assert.throws(function () {
+  fs.createReadStream(__filename, { start: 17, end: "29" });
 }, "end as string didn't throw an error");
 
-assert.throws(function() {
-  fs.createWriteStream(__filename, { start: '31' });
+assert.throws(function () {
+  fs.createWriteStream(__filename, { start: "17" });
 }, "start as string didn't throw an error for createWriteStream");
 
-saneEmitter.on('data', function(data) {
+saneEmitter.on('data', function (data) {
   // a sanity check when using numbers instead of strings
   assert.strictEqual(sanity, data.toString('utf8'), 'read ' +
                      data.toString('utf8') + ' instead of ' + sanity);
