f8e5eaa1ed9792ce3c3508b246e2492410ae75eb
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2015-05-20 14:20:26 +0900

    tls: make server not use DHE in less than 1024bits
    DHE key lengths less than 1024bits is already weaken as pointed out in
    https://weakdh.org/ . 1024bits will not be safe in near future. We
    will extend this up to 2048bits somedays later.
    
    PR-URL: https://github.com/nodejs/io.js/pull/1739
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Fedor Indutny <fedor@indutny.com>

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -757,6 +757,12 @@ void SecureContext::SetDHParam(const FunctionCallbackInfo<Value>& args) {
   if (dh == nullptr)
     return;
 
+  const int keylen = BN_num_bits(dh->p);
+  if (keylen < 1024)
+    return env->ThrowError("DH parameter is less than 1024 bits");
+  else if (keylen < 2048)
+    fprintf(stderr, "WARNING: DH parameter is less than 2048 bits\n");
+
   SSL_CTX_set_options(sc->ctx_, SSL_OP_SINGLE_DH_USE);
   int r = SSL_CTX_set_tmp_dh(sc->ctx_, dh);
   DH_free(dh);
