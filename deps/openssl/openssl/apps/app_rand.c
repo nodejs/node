1cbdb1957d982641bb222413d64f4903da89dac9
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2015-05-27 10:33:38 +0900

    deps: add -no_rand_screen to openssl s_client
    In openssl s_client on Windows, RAND_screen() is invoked to initialize
    random state but it takes several seconds in each connection.
    This added -no_rand_screen to openssl s_client on Windows to skip
    RAND_screen() and gets a better performance in the unit test of
    test-tls-server-verify.
    Do not enable this except to use in the unit test.
    
    (cherry picked from commit 9f0f7c38e6df975dd39735d0e9ef968076369c74)
    
    Reviewed-By: James M Snell <jasnell@gmail.com>
    PR-URL: https://github.com/joyent/node/pull/25368

--- deps/openssl/openssl/apps/app_rand.c
+++ deps/openssl/openssl/apps/app_rand.c
@@ -124,10 +124,16 @@ int app_RAND_load_file(const char *file, BIO *bio_e, int dont_warn)
     char buffer[200];
 
 #ifdef OPENSSL_SYS_WINDOWS
-    BIO_printf(bio_e, "Loading 'screen' into random state -");
-    BIO_flush(bio_e);
-    RAND_screen();
-    BIO_printf(bio_e, " done\n");
+    /*
+     * allocate 2 to dont_warn not to use RAND_screen() via
+     * -no_rand_screen option in s_client
+     */
+    if (dont_warn != 2) {
+      BIO_printf(bio_e, "Loading 'screen' into random state -");
+      BIO_flush(bio_e);
+      RAND_screen();
+      BIO_printf(bio_e, " done\n");
+    }
 #endif
 
     if (file == NULL)
