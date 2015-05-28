b4f588551039fb4600de22ed8a6d4646767a38ca
Ben Noordhuis <info@bnoordhuis.nl>
2015-05-28 21:46:54 +0200

    test: add heap profiler add-on regression test
    Add a regression test for https://github.com/nodejs/io.js/pull/1827.
    
    PR-URL: https://github.com/nodejs/io.js/pull/1828
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

---
+++ test/addons/heap-profiler/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const binding = require('./build/Release/binding');
+
+// Create an AsyncWrap object.
+const timer = setTimeout(function() {}, 1);
+timer.unref();
+
+// Stress-test the heap profiler.
+binding.test();
+
+clearTimeout(timer);
