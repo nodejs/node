8606793999979829df002c2ab69235a05605fc4d
Johan Bergström <bugs@bergstroem.nu>
2015-05-30 13:46:12 +1000

    tools: pass constant to logger instead of string
    On a few of our installations (namely CentOS), passing 'INFO'
    resulted in a silent loglevel. Use a logging constant instead.
    
    Fixes: https://github.com/nodejs/build/issues/104
    PR-URL: https://github.com/nodejs/io.js/pull/1842
    Reviewed-By: Rod Vagg <rod@vagg.org>

--- tools/test.py
+++ tools/test.py
@@ -1367,7 +1367,7 @@ def Main():
 
   ch = logging.StreamHandler(sys.stdout)
   logger.addHandler(ch)
-  logger.setLevel('INFO')
+  logger.setLevel(logging.INFO)
   if options.logfile:
     fh = logging.FileHandler(options.logfile)
     logger.addHandler(fh)
