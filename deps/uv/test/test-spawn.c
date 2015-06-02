d7df81f0e375e3bddd91961cdcf0c48934f7986b
sxyizhiren <786647787@qq.com>
2015-06-02 21:40:47 +0800

    Revert " fix for execFile fail when filepath contain "(""
    This reverts commit 09d97ee0ecbbfd340e650ad21734a8f01546c33e.

--- deps/uv/test/test-spawn.c
+++ deps/uv/test/test-spawn.c
@@ -995,7 +995,7 @@ TEST_IMPL(spawn_detect_pipe_name_collisions_on_windows) {
 
 
 int make_program_args(char** args, int verbatim_arguments, WCHAR** dst_ptr);
-WCHAR* quote_cmd_arg(const WCHAR *source, WCHAR *target, BOOL isFilePath);
+WCHAR* quote_cmd_arg(const WCHAR *source, WCHAR *target);
 
 TEST_IMPL(argument_escaping) {
   const WCHAR* test_str[] = {
@@ -1032,7 +1032,7 @@ TEST_IMPL(argument_escaping) {
   ASSERT(test_output != NULL);
   for (i = 0; i < count; ++i) {
     test_output[i] = calloc(2 * (wcslen(test_str[i]) + 2), sizeof(WCHAR));
-    quote_cmd_arg(test_str[i], test_output[i], FALSE);
+    quote_cmd_arg(test_str[i], test_output[i]);
     wprintf(L"input : %s\n", test_str[i]);
     wprintf(L"output: %s\n", test_output[i]);
     total_size += wcslen(test_output[i]) + 1;
