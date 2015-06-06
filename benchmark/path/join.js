acf590bf99b3514ac4744c57e241646aa3dbfae4
Nathan Woltman <nwoltman@outlook.com>
2015-06-05 20:43:48 -0400

    benchmark: Add some path benchmarks for #1778
    Path functions being benchmarked are:
    * format
    * isAbsolute
    * join
    * normalize
    * relative
    * resolve
    
    PR-URL: https://github.com/nodejs/io.js/pull/1778
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>
    Reviewed-By: Roman Reiss <me@silverwind.io>

---
+++ benchmark/path/join.js
@@ -0,0 +1,18 @@
+var common = require('../common.js');
+var path = require('path');
+
+var bench = common.createBenchmark(main, {
+  type: ['win32', 'posix'],
+  n: [1e6],
+});
+
+function main(conf) {
+  var n = +conf.n;
+  var p = path[conf.type];
+
+  bench.start();
+  for (var i = 0; i < n; i++) {
+    p.join('/foo', 'bar', '', 'baz/asdf', 'quux', '..');
+  }
+  bench.end(n);
+}
