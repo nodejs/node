a7990386435c2cf7c21f42e67786f6733195e4a4
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-06-10 11:06:15 -0600

    buffer: fix case of one buffer passed to concat
    Fix Buffer.concat() so a copy is always returned regardless of the
    number of buffers that were passed.
    
    Previously if the array length was one then the same same buffer was
    returned. This created a special case for the user where there was a
    chance mutating the buffer returned by .concat() could mutate the buffer
    passed in.
    
    Also fixes an inconsistency when throwing if an array member was not a
    Buffer instance. For example:
    
        Buffer.concat([42]);      // Returns 42
        Buffer.concat([42, 1]);  // Throws a TypeError
    
    Now .concat() will always throw if an array member is not a Buffer
    instance.
    
    See: https://github.com/nodejs/io.js/issues/1891
    PR-URL: https://github.com/nodejs/io.js/pull/1937
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-buffer-concat.js
+++ test/parallel/test-buffer-concat.js
@@ -14,8 +14,14 @@ var flatLongLen = Buffer.concat(long, 40);
 
 assert(flatZero.length === 0);
 assert(flatOne.toString() === 'asdf');
-assert(flatOne === one[0]);
+// A special case where concat used to return the first item,
+// if the length is one. This check is to make sure that we don't do that.
+assert(flatOne !== one[0]);
 assert(flatLong.toString() === (new Array(10 + 1).join('asdf')));
 assert(flatLongLen.toString() === (new Array(10 + 1).join('asdf')));
+
+assert.throws(function() {
+  Buffer.concat([42]);
+}, TypeError);
 
 console.log('ok');
