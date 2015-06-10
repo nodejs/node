03d2bd381910f3741eed527b9cff1628a6542d2b
Sam Roberts <sam@strongloop.com>
2015-06-10 08:41:03 -0700

    test: make listen-fd-cluster/server more robust
    - eliminate unnecessary intermediate process ("parent")
    - children exit if runner dies unexpectedly (killed on a test timeout,
      for example)
    - use explicit messaging from children to parents to indicate when
      worker is ready to accept http requests, rather than racing to see
      whether runner will make request before worker is listening
    
    PR-URL: https://github.com/nodejs/io.js/pull/1944
    Reviewed-By: Johan Bergstrom <bugs@bergstroem.nu>
    Reviewed-By: Ben Noordhuis <ben@strongloop.com>

--- test/parallel/test-listen-fd-server.js
+++ test/parallel/test-listen-fd-server.js
@@ -13,54 +13,50 @@ if (common.isWindows) {
 
 switch (process.argv[2]) {
   case 'child': return child();
-  case 'parent': return parent();
-  default: return test();
 }
 
-// spawn the parent, and listen for it to tell us the pid of the child.
+var ok;
+
+process.on('exit', function() {
+  assert.ok(ok);
+});
+
 // WARNING: This is an example of listening on some arbitrary FD number
 // that has already been bound elsewhere in advance.  However, binding
 // server handles to stdio fd's is NOT a good or reliable way to do
 // concurrency in HTTP servers!  Use the cluster module, or if you want
 // a more low-level approach, use child process IPC manually.
-function test() {
-  var parent = spawn(process.execPath, [__filename, 'parent'], {
-    stdio: [ 0, 'pipe', 2 ]
-  });
-  var json = '';
-  parent.stdout.on('data', function(c) {
-    json += c.toString();
-    if (json.indexOf('\n') !== -1) next();
-  });
-  function next() {
-    console.error('output from parent = %s', json);
-    var child = JSON.parse(json);
-    // now make sure that we can request to the child, then kill it.
-    http.get({
-      server: 'localhost',
-      port: PORT,
-      path: '/',
-    }).on('response', function(res) {
-      var s = '';
-      res.on('data', function(c) {
-        s += c.toString();
-      });
-      res.on('end', function() {
-        // kill the child before we start doing asserts.
-        // it's really annoying when tests leave orphans!
-        process.kill(child.pid, 'SIGKILL');
-        try {
-          parent.kill();
-        } catch (e) {}
-
+test(function(child) {
+  // now make sure that we can request to the child, then kill it.
+  http.get({
+    server: 'localhost',
+    port: PORT,
+    path: '/',
+  }).on('response', function(res) {
+    var s = '';
+    res.on('data', function(c) {
+      s += c.toString();
+    });
+    res.on('end', function() {
+      child.kill();
+      child.on('exit', function() {
         assert.equal(s, 'hello from child\n');
         assert.equal(res.statusCode, 200);
+        console.log('ok');
+        ok = true;
       });
     });
-  }
-}
+  });
+});
 
 function child() {
+  // Prevent outliving the parent process in case it is terminated before
+  // killing this child process.
+  process.on('disconnect', function() {
+    console.error('exit on disconnect');
+    process.exit(0);
+  });
+
   // start a server on fd=3
   http.createServer(function(req, res) {
     console.error('request on child');
@@ -68,10 +64,11 @@ function child() {
     res.end('hello from child\n');
   }).listen({ fd: 3 }, function() {
     console.error('child listening on fd=3');
+    process.send('listening');
   });
 }
 
-function parent() {
+function test(cb) {
   var server = net.createServer(function(conn) {
     console.error('connection on parent');
     conn.end('hello from parent\n');
@@ -80,7 +77,7 @@ function parent() {
 
     var spawn = require('child_process').spawn;
     var child = spawn(process.execPath, [__filename, 'child'], {
-      stdio: [ 0, 1, 2, server._handle ]
+      stdio: [ 0, 1, 2, server._handle, 'ipc' ]
     });
 
     console.log('%j\n', { pid: child.pid });
@@ -90,13 +87,10 @@ function parent() {
     // be accepted, because the child has the fd open.
     server.close();
 
-    child.on('exit', function(code) {
-      console.error('child exited', code);
+    child.on('message', function(msg) {
+      if (msg === 'listening') {
+        cb(child);
+      }
     });
-
-    child.on('close', function() {
-      console.error('child closed');
-    });
-    console.error('child spawned');
   });
 }
