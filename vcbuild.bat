6e4d30286d7305335b14762271a25f5dbc5c63c0
Roman Reiss <me@silverwind.io>
2015-06-11 19:08:25 +0200

    tools: enable/add additional eslint rules
    Enables the following rules:
    
    - no-undef: Valuable rule to error on usage of undefined variables
    - require-buffer: Custom rule that forbids usage of the global Buffer
      inside lib/ because of REPL issues.
    
    PR-URL: https://github.com/nodejs/io.js/pull/1794
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- vcbuild.bat
+++ vcbuild.bat
@@ -181,7 +181,7 @@ goto jslint
 :jslint
 if not defined jslint goto exit
 echo running jslint
-%config%\iojs tools\eslint\bin\eslint.js src lib test --reset --quiet
+%config%\iojs tools\eslint\bin\eslint.js src lib test --rulesdir tools\eslint-rules --reset --quiet
 goto exit
 
 :create-msvs-files-failed
