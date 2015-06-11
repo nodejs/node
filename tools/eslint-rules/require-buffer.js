98594d022f3878ddd765047254e650744c438601
Roman Reiss <me@silverwind.io>
2015-06-11 20:03:42 +0200

    Fix nits

--- tools/eslint-rules/require-buffer.js
+++ tools/eslint-rules/require-buffer.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const msg = "Use const Buffer = require('buffer').Buffer; on top of this file";
+const msg = 'Use const Buffer = require(\'buffer\').Buffer; ' +
+            'at the beginning of this file';
 
 module.exports = function(context) {
   return {
-    "Program:exit": function() {
+    'Program:exit': function() {
       context.getScope().through.forEach(function(ref) {
         if (ref.identifier.name === 'Buffer') {
             context.report(ref.identifier, msg);
