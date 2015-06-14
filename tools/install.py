4a15e0c31f6df17572e1cdd968361dea1054c4d8
Rod Vagg <rod@vagg.org>
2015-06-14 20:23:43 +1000

    build: add tar-headers target for headers-only tar
    to replace the full src download by node-gyp, using the proper format
    instead of the full source format
    
    PR-URL: https://github.com/nodejs/io.js/pull/1975
    Reviewed-By: William Blankenship <william.jblankenship@gmail.com>
    Reviewed-By: Johan Bergstrom <bugs@bergstroem.nu>

--- tools/install.py
+++ tools/install.py
@@ -156,6 +156,9 @@ def files(action):
 
   if 'true' == variables.get('node_install_npm'): npm_files(action)
 
+  headers(action)
+
+def headers(action):
   action([
     'common.gypi',
     'config.gypi',
@@ -177,7 +180,6 @@ def files(action):
     subdir_files('deps/openssl/openssl/include/openssl', 'include/node/openssl/', action)
     subdir_files('deps/openssl/config/archs', 'include/node/openssl/archs', action)
     action(['deps/openssl/config/opensslconf.h'], 'include/node/openssl/')
-
 
   if 'false' == variables.get('node_shared_zlib'):
     action([
@@ -207,8 +209,14 @@ def run(args):
   install_path = dst_dir + node_prefix + '/'
 
   cmd = args[1] if len(args) > 1 else 'install'
-  if cmd == 'install': return files(install)
-  if cmd == 'uninstall': return files(uninstall)
+
+  if os.environ.get('HEADERS_ONLY'):
+    if cmd == 'install': return headers(install)
+    if cmd == 'uninstall': return headers(uninstall)
+  else:
+    if cmd == 'install': return files(install)
+    if cmd == 'uninstall': return files(uninstall)
+
   raise RuntimeError('Bad command: %s\n' % cmd)
 
 if __name__ == '__main__':
