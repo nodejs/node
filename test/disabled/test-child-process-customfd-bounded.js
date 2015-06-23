856c11f8c8667b39a69455e42f9af493b37816fc
Rich Trott <rtrott@gmail.com>
2015-06-23 14:29:38 -0700

    test: purge stale disabled tests
    Tests in the disabled directory are not used by Makefile nor by the CI.
    Other than a single 2015 commit that puts 'use strict' in each test,
    many of them haven't been touched in years.
    
    This removes all the disabled tests that have been unmodified since
    2011 (with the exception of the 'use strict' modification mentioned
    above).
    
    PR-URL: https://github.com/nodejs/io.js/pull/2045
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Evan Lucas <evanlucas@me.com>

--- test/disabled/test-child-process-customfd-bounded.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var common = require('../common');
-
-var bigish = Array(200);
-
-for (var i = 0, il = bigish.length; i < il; ++i)
-  bigish[i] = -1;
-
-common.spawnPwd({ customFds: bigish });
