b6444eeafd19c2e0eb8462a316e21e1b4b572eea
Fedor Indutny <fedor@indutny.com>
2015-06-28 18:47:32 -0700

    stream_wrap: fix initialization order

--- test/parallel/test-stream-wrap.js
+++ test/parallel/test-stream-wrap.js
@@ -6,19 +6,41 @@ const StreamWrap = require('_stream_wrap');
 const Duplex = require('stream').Duplex;
 const ShutdownWrap = process.binding('stream_wrap').ShutdownWrap;
 
-var stream = new Duplex({
-  read: function() {
-  },
-  write: function(data, enc, callback) {
-    callback(null);
-  }
+function testShutdown(callback) {
+  var stream = new Duplex({
+    read: function() {
+    },
+    write: function(data, enc, callback) {
+      callback(null);
+    }
+  });
+
+  var wrap = new StreamWrap(stream);
+
+  var req = new ShutdownWrap();
+  req.oncomplete = function() {};
+  req.handle = wrap._handle;
+  wrap._handle.shutdown(req);
+
+  wrap.destroy();
+
+  process.nextTick(callback);
+}
+
+function testReadAfterClose(callback) {
+  var stream = new Duplex({
+    read: function() {
+    },
+    write: function(data, enc, callback) {
+      callback(null);
+    }
+  });
+  stream.push('data');
+  stream.push(null);
+
+  var wrap = new StreamWrap(stream);
+}
+
+testShutdown(function() {
+  testReadAfterClose();
 });
-
-var wrap = new StreamWrap(stream);
-
-var req = new ShutdownWrap();
-req.oncomplete = function() {};
-req.handle = wrap._handle;
-wrap._handle.shutdown(req);
-
-wrap.destroy();
