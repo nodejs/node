3d3709bca7864ae69ddcc57b3741955570020376
Fedor Indutny <fedor@indutny.com>
2015-06-29 16:35:56 -0700

    ...

--- test/parallel/test-stream-wrap.js
+++ test/parallel/test-stream-wrap.js
@@ -6,41 +6,34 @@ const StreamWrap = require('_stream_wrap');
 const Duplex = require('stream').Duplex;
 const ShutdownWrap = process.binding('stream_wrap').ShutdownWrap;
 
+var done = false;
+
 function testShutdown(callback) {
   var stream = new Duplex({
     read: function() {
     },
-    write: function(data, enc, callback) {
-      callback(null);
+    write: function() {
     }
   });
 
   var wrap = new StreamWrap(stream);
 
   var req = new ShutdownWrap();
-  req.oncomplete = function() {};
+  req.oncomplete = function(code) {
+    assert(code < 0);
+    callback();
+  };
   req.handle = wrap._handle;
-  wrap._handle.shutdown(req);
 
+  // Close the handle to simulate
   wrap.destroy();
-
-  process.nextTick(callback);
-}
-
-function testReadAfterClose(callback) {
-  var stream = new Duplex({
-    read: function() {
-    },
-    write: function(data, enc, callback) {
-      callback(null);
-    }
-  });
-  stream.push('data');
-  stream.push(null);
-
-  var wrap = new StreamWrap(stream);
+  req.handle.shutdown(req);
 }
 
 testShutdown(function() {
-  testReadAfterClose();
+  done = true;
+});
+
+process.on('exit', function() {
+  assert(done);
 });
