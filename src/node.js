12c8d69624d465f1006abe579bf1b0d71915ceca
Bradley Meck <bradley.meck@gmail.com>
2015-07-16 09:07:02 -0500

    expose a global require for usage in tools like node-inspector

--- src/node.js
+++ src/node.js
@@ -888,6 +888,7 @@
 
     return nativeModule.exports;
   };
+  process._require = NativeModule.require.bind(NativeModule);
 
   NativeModule.getCached = function(id) {
     return NativeModule._cache[id];
