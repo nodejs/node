nodejs/node

commit af249fa8a15bad8996187e73b480b30dcd881bad

net: wrap connect in nextTick
Fixes an edge case regression introduced in
1bef71747678c19c7214048de5b9e3848889248d.

With the lookup being skipped, an error could be emitted before an
error listener has been added.

An example of this was presented by changing the server’s IP address
and then immediately making a request to the old address.

Related: https://github.com/nodejs/io.js/pull/1823
PR-URL: https://github.com/nodejs/io.js/pull/2054
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
Reviewed-by: Trevor Norris <trev.norris@gmail.com>

lib/net.js
@@ -925,7 +925,9 @@ function lookupAndConnect(self, options) {
   // TODO(evanlucas) should we hot path this for localhost?
   var addressType = exports.isIP(host);
   if (addressType) {
-    connect(self, host, port, addressType, localAddress, localPort);
+    process.nextTick(function() {
+      connect(self, host, port, addressType, localAddress, localPort);
+    });
     return;
   }
 

commit 235bb292fb65be6702fd97acdce6b8e819ac84c3

net: don't throw on immediately destroyed socket
Fixes regression introduced in af249fa8a15bad8996187e73b480b30dcd881bad.

With connect being deferred to the next tick, Socket.destroy could be
called before connect. Socket.destroy sets _connecting to false which
would cause an assertion error.

Fixes: https://github.com/nodejs/io.js/issues/2250
PR-URL: https://github.com/nodejs/io.js/pull/2251
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

lib/net.js
@@ -927,7 +927,8 @@ function lookupAndConnect(self, options) {
   var addressType = exports.isIP(host);
   if (addressType) {
     process.nextTick(function() {
-      connect(self, host, port, addressType, localAddress, localPort);
+      if (self._connecting)
+        connect(self, host, port, addressType, localAddress, localPort);
     });
     return;
   }

test/parallel/test-net-connect-immediate-destroy.js
@@ -1,0 +1,8 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const net = require('net');
+
+const socket = net.connect(common.PORT, common.localhostIPv4, assert.fail);
+socket.on('error', assert.fail);
+socket.destroy();