e714b5620e98ca01d176e16d7afdae467935d0a2
Michaël Zasso <mic.besace@gmail.com>
2015-08-02 12:09:38 +0200

    test: remove unreachable code
    There is no way a line can be called after throwing an exception.
    
    PR-URL: https://github.com/nodejs/node/pull/2289
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: Roman Reiss <me@silverwind.io>
    Reviewed-By: Brendan Ashworth <brendan.ashworth@me.com>

--- test/message/throw_custom_error.js
+++ test/message/throw_custom_error.js
@@ -2,9 +2,5 @@
 var common = require('../common');
 var assert = require('assert');
 
-console.error('before');
-
 // custom error throwing
 throw ({ name: 'MyCustomError', message: 'This is a custom message' });
-
-console.error('after');
