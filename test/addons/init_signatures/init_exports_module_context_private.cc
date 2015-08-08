850fb5fab6d7ce1bbf45eaa2662d431cd8bd9428
David Siegel <david.siegel@artcom.de>
2015-08-08 19:23:25 +0200

    [addons] Styled a few more pointers

--- test/addons/init_signatures/init_exports_module_context_private.cc
+++ test/addons/init_signatures/init_exports_module_context_private.cc
@@ -2,7 +2,7 @@
 void init(v8::Local<v8::Object> exports,
           v8::Local<v8::Object> module,
           v8::Local<v8::Context> context,
-          void * priv) {
+          void* priv) {
   node::test::setInitTag(exports);
 }
 NODE_MODULE(NODE_TEST_ADDON_NAME, init)
