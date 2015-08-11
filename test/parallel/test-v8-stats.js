26a2428e091fb340102600816d665ec9bfb1f9ec
Roman Klauke <romaaan.git@gmail.com>
2015-08-11 20:06:41 +0200

    src: add total_available_size to v8 statistics
    v8 introduced the new flag `total_available_size` in version 4.4
    and upwards. This flag is now available on `v8.getHeapStatistics`
    with the name `total_available_size`. It contains the total
    available heap size of v8.
    
    Introduced with commit: 0a1352a716e9af5d861dcede0d7c6580f018a10b

--- test/parallel/test-v8-stats.js
+++ test/parallel/test-v8-stats.js
@@ -6,6 +6,7 @@ var v8 = require('v8');
 var s = v8.getHeapStatistics();
 var keys = [
   'heap_size_limit',
+  'total_available_size',
   'total_heap_size',
   'total_heap_size_executable',
   'total_physical_size',
