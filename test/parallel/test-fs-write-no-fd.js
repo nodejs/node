c86c1eeab56df8a627d3d8da27008221ee295d33
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2015-08-23 01:33:43 +0530

    fs: validate args of truncate functions in js
    This patch
    
     1. moves the basic validation of arguments to `truncate` family
        of functions to the JavaScript layer from the C++ layer.
    
     2. makes sure that the File Descriptors are validated strictly.
    
    PR-URL: https://github.com/nodejs/node/pull/2498
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- test/parallel/test-fs-write-no-fd.js
+++ test/parallel/test-fs-write-no-fd.js
@@ -1,12 +1,13 @@
 'use strict';
+
 require('../common');
 const fs = require('fs');
 const assert = require('assert');
 
 assert.throws(function() {
-  fs.write(null, Buffer.allocUnsafe(1), 0, 1);
-}, /TypeError/);
+  fs.write(null, Buffer.allocUnsafe(1), 0, 1, () => {});
+}, /TypeError: file descriptor must be a unsigned 32-bit integer/);
 
 assert.throws(function() {
-  fs.write(null, '1', 0, 1);
-}, /TypeError/);
+  fs.write(undefined, '1', 0, 1, () => {});
+}, /TypeError: file descriptor must be a unsigned 32-bit integer/);
