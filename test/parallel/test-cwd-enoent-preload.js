nodejs/node

commit 5759722cfacf17cc79651c81801a5e03521db053

src: fix module search path for preload modules
When the preload module is not a abs/relative path, we should use
the standard search mechanism of looking into the node_modules folders
outwards. The current working directory is deemed to be the 'requiring
module', i.e. parent. The search path starts from cwd outwards.

Fixes: https://github.com/nodejs/io.js/issues/1803
PR-URL: https://github.com/nodejs/io.js/pull/1812
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>

lib/module.js
@@ -503,6 +503,20 @@ Module.requireRepl = function() {
   return Module._load('internal/repl', '.');
 };
 
+Module._preloadModules = function(requests) {
+  if (!Array.isArray(requests))
+    return;
+
+  // Preloaded modules have a dummy parent module which is deemed to exist
+  // in the current working directory. This seeds the search path for
+  // preloaded modules.
+  var parent = new Module('internal/preload', null);
+  parent.paths = Module._nodeModulePaths(process.cwd());
+  requests.forEach(function(request) {
+    Module._load(request, parent, false);
+  });
+};
+
 Module._initPaths();
 
 // backwards compatibility

src/node.js
@@ -839,10 +839,7 @@
   // Load preload modules
   startup.preloadModules = function() {
     if (process._preload_modules) {
-      var Module = NativeModule.require('module');
-      process._preload_modules.forEach(function(module) {
-        Module._load(module);
-      });
+      NativeModule.require('module')._preloadModules(process._preload_modules);
     }
   };
 

test/fixtures/cluster-preload.js
@@ -1,3 +1,12 @@
+var assert = require('assert');
+
+// https://github.com/nodejs/io.js/issues/1803
+// this module is used as a preload module. It should have a parent with the
+// module search paths initialized from the current working directory
+assert.ok(module.parent);
+var expectedPaths = require('module')._nodeModulePaths(process.cwd());
+assert.deepEqual(module.parent.paths, expectedPaths);
+
 var cluster = require('cluster');
 cluster.isMaster || process.exit(42 + cluster.worker.id); // +42 to distinguish
 // from exit(1) for other random reasons

commit 02855473e24a24dd7f76bd4076765784bbaee447

module: fix module preloading when cwd is ENOENT
Fixes a regression from 5759722cfacf17cc79651c81801a5e03521db053
that prevented modules from being preloaded if the cwd does not exist.
Absolute and builtin modules now preload correctly again.

Refs: https://github.com/nodejs/node/issues/1803
PR-URL: https://github.com/nodejs/node/pull/2353
Reviewed-By: Jeremiah Senkpiel <fishrock123@rockemail.com>

lib/module.js
@@ -515,9 +515,16 @@ Module._preloadModules = function(requests) {
   // in the current working directory. This seeds the search path for
   // preloaded modules.
   var parent = new Module('internal/preload', null);
-  parent.paths = Module._nodeModulePaths(process.cwd());
+  try {
+    parent.paths = Module._nodeModulePaths(process.cwd());
+  }
+  catch (e) {
+    if (e.code !== 'ENOENT') {
+      throw e;
+    }
+  }
   requests.forEach(function(request) {
-    Module._load(request, parent, false);
+    parent.require(request);
   });
 };
 

test/parallel/test-cwd-enoent-preload.js
@@ -1,0 +1,28 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+
+// Fails with EINVAL on SmartOS, EBUSY on Windows.
+if (process.platform === 'sunos' || common.isWindows) {
+  console.log('1..0 # Skipped: cannot rmdir current working directory');
+  return;
+}
+
+const dirname = common.tmpDir + '/cwd-does-not-exist-' + process.pid;
+const abspathFile = require('path').join(common.fixturesDir, 'a.js');
+common.refreshTmpDir();
+fs.mkdirSync(dirname);
+process.chdir(dirname);
+fs.rmdirSync(dirname);
+
+
+const proc = spawn(process.execPath, ['-r', abspathFile, '-e', '0']);
+proc.stdout.pipe(process.stdout);
+proc.stderr.pipe(process.stderr);
+
+proc.once('exit', common.mustCall(function(exitCode, signalCode) {
+  assert.strictEqual(exitCode, 0);
+  assert.strictEqual(signalCode, null);
+}));