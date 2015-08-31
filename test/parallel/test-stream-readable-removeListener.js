fa013e86e8375516a40dc72b3de7ec9bcf5ba7a1
Philippe Laferriere <laferriere.phil@gmail.com>
2015-08-31 09:24:48 -0400

    stream: Cleaned code in readable stream
    In _stream_readable, removed unused variable EE, a reference to
    EventEmitter. Changed all state variable declarations to const.
    Changed EE.listenerCount(type) call, now deprecated, to
    this.listenerCount(type). Also changed some var declarations to
    const in test-stream-readable-removeListener.js.

--- test/parallel/test-stream-readable-removeListener.js
+++ test/parallel/test-stream-readable-removeListener.js
@@ -1,20 +1,19 @@
 'use strict';
-var assert = require('assert');
-var EE = require('events').EventEmitter;
-var Readable = require('stream').Readable;
+const assert = require('assert');
+const Readable = require('stream').Readable;
 
-var buf = new Buffer(65536);
+const buf = new Buffer(65536);
 buf.fill(0);
 
 var packetsToPush = 10;
-var toUnshift = 16 * 1024;
+const toUnshift = 16 * 1024;
 
 Readable.prototype._read = function() {
   var chunk = --packetsToPush > 0 ? buf.slice() : null;
   setImmediate(this.push.bind(this, chunk));
 };
 
-var rStream = new Readable();
+const rStream = new Readable();
 
 var first = true;
 rStream.on('readable', function onReadable() {
@@ -22,7 +21,7 @@ rStream.on('readable', function onReadable() {
   if (first) {
     first = false;
     rStream.removeListener('readable', onReadable);
-    assert.equal(EE.listenerCount(rStream, 'readable'), 0);
+    assert.equal(this.listenerCount('readable'), 0);
     rStream.unshift(buf.slice(0, toUnshift));
     rStream.on('readable', onReadable);
   }
