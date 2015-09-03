b4a60067ed447b5dba009dfbb5d01ed91970c861
Ali Ijaz Sheikh <ofrobots@google.com>
2015-09-03 07:12:16 -0700

    src: increment NODE_MODULE_VERSION to 46
    V8 4.5 API changes that warrant this:
    * Remove deprecated v8::Object::TurnOnAccessCheck API
    * Remove obsolete options in ScriptCompiler::CompileOptions
    * Remove v8::Private
    Full details at https://docs.google.com/document/d/1g8JFi8T_oAE_7uAri7Njtig7fKaPDfotU6huOa1alds/edit?hl=en&forcehl=1

--- src/node_version.h
+++ src/node_version.h
@@ -40,6 +40,6 @@
  * an API is broken in the C++ side, including in v8 or
  * other dependencies.
  */
-#define NODE_MODULE_VERSION 45  /* io.js v3.0.0 */
+#define NODE_MODULE_VERSION 46  /* v4.0.0 */
 
 #endif  /* SRC_NODE_VERSION_H_ */
