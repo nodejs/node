0a5f80a11f044ef9b6e3a8be1d588ee9217163f2
Karl Skomski <karl@skomski.com>
2015-09-09 18:35:04 +0200

    src: use subarray() in Buffer#slice() for speedup
    Use the built-in Typed Array method subarray() to improve performance of
    Buffer#slice().
    
    Benchmark improvements:
    
        benchmark/buffer-slice: 40%
        benchmark/buffer-creation (pool): 25%
    
    Additional tests also added.
    
    PR-URL: https://github.com/nodejs/node/pull/2777
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>

--- test/parallel/test-buffer.js
+++ test/parallel/test-buffer.js
@@ -1061,14 +1061,46 @@ assert.equal(buf.readInt8(0), -1);
   assert.equal(buf.slice(-10, 10), '0123456789');
   assert.equal(buf.slice(-20, 10), '0123456789');
   assert.equal(buf.slice(-20, -10), '');
+  assert.equal(buf.slice(), '0123456789');
+  assert.equal(buf.slice(0), '0123456789');
+  assert.equal(buf.slice(0, 0), '');
+  assert.equal(buf.slice(undefined), '0123456789');
+  assert.equal(buf.slice('foobar'), '0123456789');
+  assert.equal(buf.slice(undefined, undefined), '0123456789');
+
+  assert.equal(buf.slice(2), '23456789');
+  assert.equal(buf.slice(5), '56789');
+  assert.equal(buf.slice(10), '');
+  assert.equal(buf.slice(5, 8), '567');
+  assert.equal(buf.slice(8, -1), '8');
+  assert.equal(buf.slice(-10), '0123456789');
+  assert.equal(buf.slice(0, -9), '0');
+  assert.equal(buf.slice(0, -10), '');
   assert.equal(buf.slice(0, -1), '012345678');
   assert.equal(buf.slice(2, -2), '234567');
   assert.equal(buf.slice(0, 65536), '0123456789');
   assert.equal(buf.slice(65536, 0), '');
+  assert.equal(buf.slice(-5, -8), '');
+  assert.equal(buf.slice(-5, -3), '56');
+  assert.equal(buf.slice(-10, 10), '0123456789');
   for (var i = 0, s = buf.toString(); i < buf.length; ++i) {
+    assert.equal(buf.slice(i), s.slice(i));
+    assert.equal(buf.slice(0, i), s.slice(0, i));
     assert.equal(buf.slice(-i), s.slice(-i));
     assert.equal(buf.slice(0, -i), s.slice(0, -i));
   }
+
+  var utf16Buf = new Buffer('0123456789', 'utf16le');
+  assert.deepEqual(utf16Buf.slice(0, 6), Buffer('012', 'utf16le'));
+
+  assert.equal(buf.slice('0', '1'), '0');
+  assert.equal(buf.slice('-5', '10'), '56789');
+  assert.equal(buf.slice('-10', '10'), '0123456789');
+  assert.equal(buf.slice('-10', '-5'), '01234');
+  assert.equal(buf.slice('-10', '-0'), '');
+  assert.equal(buf.slice('111'), '');
+  assert.equal(buf.slice('0', '-111'), '');
+
   // try to slice a zero length Buffer
   // see https://github.com/joyent/node/issues/5881
   SlowBuffer(0).slice(0, 1);
