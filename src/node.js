5e25c82d7680ec3061791269279d5fae0d45a968
Tristian Flanagan <tflanagan@datacollaborative.com>
2015-09-14 18:26:59 -0400

    module: fix column offsets in errors
    Because Node modules are wrapped, errors on the first line
    of a file leak the wrapper to the user and report the wrong
    column number. This commit adds a line break to the module
    wrapper so that the first line is treated the same as all
    other lines. To compensate for the additional line, a line
    offset of -1 is also applied to errors.
    
    Fixes: https://github.com/nodejs/node/issues/2860
    PR-URL: https://github.com/nodejs/node/pull/2867
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- src/node.js
+++ src/node.js
@@ -942,7 +942,7 @@
   };
 
   NativeModule.wrapper = [
-    '(function (exports, require, module, __filename, __dirname) { ',
+    '(function (exports, require, module, __filename, __dirname) {\n',
     '\n});'
   ];
 
