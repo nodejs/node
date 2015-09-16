2073c9e512b4c3de82fb02dcd6c8f8b46e79a53c
Evan Lucas <evanlucas@me.com>
2015-09-16 12:44:28 -0500

    src: honor --abort_on_uncaught_exception flag
    Fix regression introduced in 0af4c9ea7434e4f505dbe071357e4bc3b4ab2a8a
    that ignores the --abort-on-uncaught-exception flag. Prior to that
    commit, the flag was passed through to v8. After that commit, the
    process just calls exit(1).
    
    PR-URL: https://github.com/nodejs/node/pull/2776
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Brian White <mscdex@mscdex.net>
    Reviewed-by: Trevor Norris <trev.norris@gmail.com>

--- src/node.cc
+++ src/node.cc
@@ -2180,7 +2180,11 @@ void FatalException(Isolate* isolate,
 
   if (false == caught->BooleanValue()) {
     ReportException(env, error, message);
-    exit(1);
+    if (abort_on_uncaught_exception) {
+      ABORT();
+    } else {
+      exit(1);
+    }
   }
 }
 
