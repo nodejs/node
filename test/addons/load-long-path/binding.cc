450d406649768dc50d9517f02e688eb4810a199a
Justin Chase <justin@evolvelabs.com>
2015-09-19 17:03:32 -0500

    lib,test: Use long paths when loading native addons (windows only)
    When using require to load a native addon the path must be converted
    into a long path, otherwise the addon will fail to be loaded on
    windows if the path is longer than 260 characters.

---
+++ test/addons/load-long-path/binding.cc
@@ -0,0 +1,14 @@
+#include <node.h>
+#include <v8.h>
+
+void Method(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+  v8::HandleScope scope(isolate);
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "world"));
+}
+
+void init(v8::Local<v8::Object> target) {
+  NODE_SET_METHOD(target, "hello", Method);
+}
+
+NODE_MODULE(binding, init);
