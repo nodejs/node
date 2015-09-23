a6aab430936c4c2791dbb29d8adec9d9a12f9376
João Reis <reis@janeasystems.com>
2015-09-23 13:33:09 +0100

    build,win: fix node.exe resource version
    When MSBuild invokes rc.exe, it passes NODE_TAG unstringified, but
    passes it correctly to cl.exe. Hence, this workaround was made to
    apply only to the resource file.
    
    Fixes: https://github.com/nodejs/node/issues/2963
    PR-URL: https://github.com/nodejs/node/pull/3053
    Reviewed-By: Alexis Campailla <orangemocha@nodejs.org>
    Reviewed-By: Johan Bergström <bugs@bergstroem.nu>

--- src/node_version.h
+++ src/node_version.h
@@ -18,12 +18,21 @@
 # else
 #  define NODE_TAG "-pre"
 # endif
+#else
+// NODE_TAG is passed without quotes when rc.exe is run from msbuild
+# define NODE_EXE_VERSION NODE_STRINGIFY(NODE_MAJOR_VERSION) "." \
+                          NODE_STRINGIFY(NODE_MINOR_VERSION) "." \
+                          NODE_STRINGIFY(NODE_PATCH_VERSION)     \
+                          NODE_STRINGIFY(NODE_TAG)
 #endif
 
 # define NODE_VERSION_STRING  NODE_STRINGIFY(NODE_MAJOR_VERSION) "." \
                               NODE_STRINGIFY(NODE_MINOR_VERSION) "." \
                               NODE_STRINGIFY(NODE_PATCH_VERSION)     \
                               NODE_TAG
+#ifndef NODE_EXE_VERSION
+# define NODE_EXE_VERSION NODE_VERSION_STRING
+#endif
 
 #define NODE_VERSION "v" NODE_VERSION_STRING
 
