8d627246f07bf316a97a35531f437e8845c90941
Fedor Indutny <fedor@indutny.com>
2015-09-24 01:30:25 -0400

    child_process: `null` channel handle on close
    `HandleWrap::OnClose` destroys the underlying C++ object and null's the
    internal field pointer to it. Therefore there should be no references to
    the wrapping JavaScript object.
    
    `null` the process' `_channel` field right after closing it, to ensure
    no crashes will happen.
    
    Fix: https://github.com/nodejs/node/issues/2847

---
+++ test/parallel/test-child-process-fork-regr-gh-2847.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const assert = require('assert');
+const common = require('../common');
+
+const cluster = require('cluster');
+const net = require('net');
+const util = require('util');
+
+if (!cluster.isMaster) {
+  // Exit on first received handle to leave the queue non-empty in master
+  process.on('message', function() {
+    process.exit(1);
+  });
+  return;
+}
+
+var server = net.createServer(function(s) {
+  setTimeout(function() {
+    s.destroy();
+  }, 100);
+}).listen(common.PORT, function() {
+  var worker = cluster.fork();
+
+  function send(callback) {
+    var s = net.connect(common.PORT, function() {
+      worker.send({}, s, callback);
+    });
+    s.on('error', function() {
+    });
+  }
+
+  worker.process.once('close', common.mustCall(function() {
+    // Otherwise the crash on `_channel.fd` access may happen
+    assert(worker.process._channel === null);
+    server.close();
+  }));
+
+  // Queue up several handles, to make `process.disconnect()` wait
+  for (var i = 0; i < 100; i++)
+    send();
+});
