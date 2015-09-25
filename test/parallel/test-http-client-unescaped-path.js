5cea15e19f7a056c49eb9507a89b317d6882c879
David D Lowe <daviddlowe.flimm@gmail.com>
2015-09-25 15:49:04 +0000

    http: Reject paths containing non-ASCII characters
    http would previously accept paths with non-ASCII characters. This
    proved problematic, because multi-byte characters were encoded as
    'binary', that is, the first byte was taken and the remaining bytes were
    dropped for that character.
    
    There is no sensible way to fix this without breaking backwards
    compatibility for paths containing U+0080 to U+00FF characters.
    
    We already reject paths with unescaped spaces with an exception. This
    commit does the same for paths with non-ASCII characters too.
    
    The alternative would have been to encode paths in UTF-8, but this would
    cause the behaviour to silently change for paths with single-byte
    non-ASCII characters (eg: the copyright character U+00A9 ©). I find it
    preferable to to add to the existing prohibition of bad paths with
    spaces.
    
    Bug report: https://github.com/nodejs/node/issues/2114

--- test/parallel/test-http-client-unescaped-path.js
+++ test/parallel/test-http-client-unescaped-path.js
@@ -4,6 +4,13 @@ var assert = require('assert');
 var http = require('http');
 
 assert.throws(function() {
-  // Path with spaces in it should throw.
   http.get({ path: 'bad path' }, assert.fail);
-}, /contains unescaped characters/);
+}, /contains unescaped characters/, 'Paths with spaces in it should throw');
+
+for (var path of ['caf\u00e9', '\u0649\u0644\u0649', '💩' /* U+1F4A9 */ ]) {
+  assert.throws(
+    function() { http.get({ path: 'caf\u00e9' }, assert.fail); },
+    /contains unescaped characters/,
+    'Path ' + path + ' with non-ASCII characters should throw'
+  );
+}
