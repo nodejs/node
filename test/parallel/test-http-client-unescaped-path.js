5743cae0b457c91522ee2408cc6f4200a660a1e5
David D Lowe <daviddlowe.flimm@gmail.com>
2015-09-25 16:19:32 +0000

    http: Reject additional whitespace chars in paths

--- test/parallel/test-http-client-unescaped-path.js
+++ test/parallel/test-http-client-unescaped-path.js
@@ -3,14 +3,18 @@ var common = require('../common');
 var assert = require('assert');
 var http = require('http');
 
-assert.throws(function() {
-  http.get({ path: 'bad path' }, assert.fail);
-}, /contains unescaped characters/, 'Paths with spaces in it should throw');
+for (var path of ['bad path', 'line\nfeed', 'carriage\rreturn']) {
+  assert.throws(
+    function() { http.get({ path: path }, assert.fail); },
+    /contains unescaped characters/,
+    'Path "' + path + '" with whitespace should throw'
+  );
+}
 
 for (var path of ['caf\u00e9', '\u0649\u0644\u0649', '💩' /* U+1F4A9 */ ]) {
   assert.throws(
-    function() { http.get({ path: 'caf\u00e9' }, assert.fail); },
+    function() { http.get({ path: path }, assert.fail); },
     /contains unescaped characters/,
-    'Path ' + path + ' with non-ASCII characters should throw'
+    'Path "' + path + '" with non-ASCII characters should throw'
   );
 }
