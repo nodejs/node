fae020ef11d5a9bdaddba64fc34a2877951f274c
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2015-09-25 11:21:40 -0400

    https: throw Error if required params missing
    Throw an error when required parameters are missing. Handles ciphers that requires no auth.
    Does not throw error If pfx option is provided.
    Additional tests added for the same.
    
    Fixes: https://github.com/nodejs/node/issues/3024
    PR-URL: https://github.com/nodejs/node/pull/3064

--- test/parallel/test-https-pfx.js
+++ test/parallel/test-https-pfx.js
@@ -21,12 +21,27 @@ var options = {
   rejectUnauthorized: false
 };
 
+var options1 = {
+  host: '127.0.0.1',
+  port: common.PORT,
+  path: '/',
+  pfx: pfx,
+  passphrase: 'sample',
+  requestCert: true
+};
+
 var server = https.createServer(options, function(req, res) {
   assert.equal(req.socket.authorized, false); // not a client cert
   assert.equal(req.socket.authorizationError, 'DEPTH_ZERO_SELF_SIGNED_CERT');
   res.writeHead(200);
   res.end('OK');
 });
+
+assert.doesNotThrow(() => https.createServer(options1, assert.fail),
+ 'cert is a required parameter for Server.createServer');
+
+assert.doesNotThrow(() => https.createServer(options1, assert.fail),
+  'key is a required parameter for Server.createServer');
 
 server.listen(options.port, options.host, function() {
   var data = '';
