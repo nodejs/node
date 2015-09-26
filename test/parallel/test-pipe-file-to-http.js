e922716192489b9a257cf31cbc356203a95b2b46
Rich Trott <rtrott@gmail.com>
2015-09-26 15:00:51 -0700

    test: remove deprecated error logging
    common.error() is just deprecated util.error() renamed.
    Remove calls to it and some other extraneous console logging
    in tests.
    
    PR-URL: https://github.com/nodejs/node/pull/3079
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/parallel/test-pipe-file-to-http.js
+++ test/parallel/test-pipe-file-to-http.js
@@ -13,19 +13,15 @@ var clientReqComplete = false;
 var count = 0;
 
 var server = http.createServer(function(req, res) {
-  console.error('SERVER request');
   var timeoutId;
   assert.equal('POST', req.method);
   req.pause();
-  common.error('request paused');
 
   setTimeout(function() {
     req.resume();
-    common.error('request resumed');
   }, 1000);
 
   req.on('data', function(chunk) {
-    common.error('recv data! nchars = ' + chunk.length);
     count += chunk.length;
   });
 
@@ -33,7 +29,6 @@ var server = http.createServer(function(req, res) {
     if (timeoutId) {
       clearTimeout(timeoutId);
     }
-    console.log('request complete from server');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end();
   });
@@ -42,12 +37,9 @@ server.listen(common.PORT);
 
 server.on('listening', function() {
   var cmd = common.ddCommand(filename, 10240);
-  console.log('dd command: ', cmd);
 
   cp.exec(cmd, function(err, stdout, stderr) {
     if (err) throw err;
-    console.error('EXEC returned successfully stdout=%d stderr=%d',
-                  stdout.length, stderr.length);
     makeRequest();
   });
 });
@@ -59,27 +51,16 @@ function makeRequest() {
     method: 'POST'
   });
 
-  common.error('pipe!');
-
   var s = fs.ReadStream(filename);
   s.pipe(req);
-  s.on('data', function(chunk) {
-    console.error('FS data chunk=%d', chunk.length);
-  });
-  s.on('end', function() {
-    console.error('FS end');
-  });
   s.on('close', function(err) {
     if (err) throw err;
     clientReqComplete = true;
-    common.error('client finished sending request');
   });
 
   req.on('response', function(res) {
-    console.error('RESPONSE', res.statusCode, res.headers);
     res.resume();
     res.on('end', function() {
-      console.error('RESPONSE end');
       server.close();
     });
   });
