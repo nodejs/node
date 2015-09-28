2e4fb99a869ecd44650db6f0f6e91d0bf2be6de0
Cheng Zhao <zcbenz@gmail.com>
2015-09-28 21:42:25 +0800

    src: add node::FreeEnvironment public API
    Since debugger::Agent's interface is not exported, third party embedders
    will have linking errors if they call Environment's destructor directly.
    
    PR-URL: https://github.com/nodejs/node/pull/3098
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node.cc
+++ src/node.cc
@@ -3226,6 +3226,13 @@ void LoadEnvironment(Environment* env) {
   f->Call(global, 1, &arg);
 }
 
+
+void FreeEnvironment(Environment* env) {
+  CHECK_NE(env, nullptr);
+  env->Dispose();
+}
+
+
 static void PrintHelp();
 
 static bool ParseDebugOpt(const char* arg) {
