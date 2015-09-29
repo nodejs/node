3210cb89fa053cb9168b9ba0c74c5e4dbe230a58
Rich Trott <rtrott@gmail.com>
2015-09-28 20:56:41 -0700

    test: var -> const

--- test/parallel/test-tty-stdout-end.js
+++ test/parallel/test-tty-stdout-end.js
@@ -1,13 +1,13 @@
 'use strict';
 // Can't test this when 'make test' doesn't assign a tty to the stdout.
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
-var shouldThrow = function() {
+const shouldThrow = function() {
   process.stdout.end();
 };
 
-var validateError = function(e) {
+const validateError = function(e) {
   return e instanceof Error &&
     e.message === 'process.stdout cannot be closed.';
 };
