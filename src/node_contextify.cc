e83499ae6289f708be3a5f8ed88b712041815224
Oguz Bastemur <obastemur@gmail.com>
2015-09-30 07:02:12 +0200

    contextify: use CHECK instead of `if`
    I was walking through the vm changes and saw this particular `if` check
    interesting. In case `ctx` is empty it's going to fail later anyways.
    So, instead of putting an `if` check there;
    
    option a - use CHECK
    option b - do nothing
    
    Considering the developer wanted to make sure `ctx` is not empty,
    `CHECK` option looked more convenient.
    
    PR-URL: https://github.com/nodejs/node/pull/3125
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node_contextify.cc
+++ src/node_contextify.cc
@@ -216,8 +216,9 @@ class ContextifyContext {
     object_template->SetHandler(config);
 
     Local<Context> ctx = Context::New(env->isolate(), nullptr, object_template);
-    if (!ctx.IsEmpty())
-      ctx->SetSecurityToken(env->context()->GetSecurityToken());
+
+    CHECK(!ctx.IsEmpty());
+    ctx->SetSecurityToken(env->context()->GetSecurityToken());
 
     env->AssignToContext(ctx);
 
