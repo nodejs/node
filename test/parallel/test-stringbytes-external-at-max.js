3b9e2bee4c1ec328865d5d154d4c225d44560aa7
Rich Trott <rtrott@gmail.com>
2015-10-09 15:34:42 -0700

    fixup: update error message

--- test/parallel/test-stringbytes-external-at-max.js
+++ test/parallel/test-stringbytes-external-at-max.js
@@ -4,27 +4,25 @@ require('../common');
 const assert = require('assert');
 
 // v8 fails silently if string length > v8::String::kMaxLength
-(function() {
-  // v8::String::kMaxLength defined in v8.h
-  const kStringMaxLength = process.binding('buffer').kStringMaxLength;
+// v8::String::kMaxLength defined in v8.h
+const kStringMaxLength = process.binding('buffer').kStringMaxLength;
 
-  try {
-    new Buffer(kStringMaxLength * 3);
-  } catch(e) {
-    assert.equal(e.message, 'Invalid array buffer length');
-    console.log(
-        '1..0 # Skipped: intensive toString tests due to memory confinements');
-    return;
-  }
+try {
+  new Buffer(kStringMaxLength * 3);
+} catch(e) {
+  assert.equal(e.message, 'Invalid typed array length');
+  console.log(
+      '1..0 # Skipped: intensive toString tests due to memory confinements');
+  return;
+}
 
-  const buf = new Buffer(kStringMaxLength);
+const buf = new Buffer(kStringMaxLength);
 
-  var maxString = buf.toString();
-  assert.equal(maxString.length, kStringMaxLength);
-  // Free the memory early instead of at the end of the next assignment
-  maxString = undefined;
+var maxString = buf.toString();
+assert.equal(maxString.length, kStringMaxLength);
+// Free the memory early instead of at the end of the next assignment
+maxString = undefined;
 
-  maxString = buf.toString('binary');
-  assert.equal(maxString.length, kStringMaxLength);
-  maxString = undefined;
-})();
+maxString = buf.toString('binary');
+assert.equal(maxString.length, kStringMaxLength);
+maxString = undefined;
