nodejs/node

commit 63644dd1cd6e6765b52f60d54b0f94f7c69a8f79

lib: remove redundant code, add tests in timers.js
insert() is only called from one place where there is already a check
that msecs is greater than or equal to zero, so do not repeat the check
inside insert().

timers.active() is not documented and should not be exposed, but since
it is exposed for now, let's test it.

PR-URL: https://github.com/nodejs/node/pull/3143
Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

lib/timers.js
@@ -23,13 +23,16 @@ const TIMEOUT_MAX = 2147483647; // 2^31-1
 // value = list
 var lists = {};
 
+
+// call this whenever the item is active (not idle)
+// it will reset its timeout.
 // the main function - creates lists on demand and the watchers associated
 // with them.
-function insert(item, msecs) {
-  item._idleStart = Timer.now();
-  item._idleTimeout = msecs;
-
+exports.active = function(item) {
+  const msecs = item._idleTimeout;
   if (msecs < 0) return;
+
+  item._idleStart = Timer.now();
 
   var list;
 
@@ -48,7 +51,7 @@ function insert(item, msecs) {
 
   L.append(list, item);
   assert(!L.isEmpty(list)); // list is not empty
-}
+};
 
 function listOnTimeout() {
   var msecs = this.msecs;
@@ -153,15 +156,6 @@ exports.enroll = function(item, msecs) {
 
   item._idleTimeout = msecs;
   L.init(item);
-};
-
-
-// call this whenever the item is active (not idle)
-// it will reset its timeout.
-exports.active = function(item) {
-  var msecs = item._idleTimeout;
-  if (msecs >= 0)
-    insert(item, msecs);
 };
 
 

test/parallel/test-timers-active.js
@@ -1,0 +1,33 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const active = require('timers').active;
+
+// active() should create timers for these
+var legitTimers = [
+  { _idleTimeout: 0 },
+  { _idleTimeout: 1 }
+];
+
+legitTimers.forEach(function(legit) {
+  const savedTimeout = legit._idleTimeout;
+  active(legit);
+  // active() should mutate these objects
+  assert(legit._idleTimeout === savedTimeout);
+  assert(Number.isInteger(legit._idleStart));
+  assert(legit._idleNext);
+  assert(legit._idlePrev);
+});
+
+
+// active() should not create a timer for these
+var bogusTimers = [
+  { _idleTimeout: -1 }
+];
+
+bogusTimers.forEach(function(bogus) {
+  const savedTimeout = bogus._idleTimeout;
+  active(bogus);
+  // active() should not mutate these objects
+  assert.deepStrictEqual(bogus, {_idleTimeout: savedTimeout});
+});

commit e2c2b44d34eb4a235a57da5a5b4ef8c758806079

lib: fix undefined timeout regression
63644dd1cd6e introduced a regression caused by everyone's favourite
JavaScript feature: undefined < 0 === undefined >= 0.

Add a case to the existing tests to cover this scenario and then add
the check for undefined that makes the test pass.

lib/timers.js
@@ -30,7 +30,7 @@ var lists = {};
 // with them.
 exports.active = function(item) {
   const msecs = item._idleTimeout;
-  if (msecs < 0) return;
+  if (msecs < 0 || msecs === undefined) return;
 
   item._idleStart = Timer.now();
 

test/parallel/test-timers-active.js
@@ -22,7 +22,8 @@ legitTimers.forEach(function(legit) {
 
 // active() should not create a timer for these
 var bogusTimers = [
-  { _idleTimeout: -1 }
+  { _idleTimeout: -1 },
+  { _idleTimeout: undefined },
 ];
 
 bogusTimers.forEach(function(bogus) {