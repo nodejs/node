676e61872f54dd546e324599c7871c20b798386a
Rich Trott <rtrott@gmail.com>
2015-10-14 21:22:55 -0700

    test: apply correct assert.fail() arguments
    The assert.fail function signature has the message as the third argument
    but, understandably, it is often assumed that it is the first argument
    (or at least the first argument if no other arguments are passed).
    
    This corrects the assert.fail() invocations in the Node.js tests.
    
    Before:
    assert.fail('message');
    // result: AssertionError: 'message' undefined undefined
    
    After:
    assert.fail(null, null, 'message');
    // result: AssertionError: message
    
    PR-URL: https://github.com/nodejs/node/pull/3378
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-spawn-cmd-named-pipe.js
+++ test/parallel/test-spawn-cmd-named-pipe.js
@@ -39,7 +39,7 @@ if (!process.argv[2]) {
 
   const comspec = process.env['comspec'];
   if (!comspec || comspec.length === 0) {
-    assert.fail('Failed to get COMSPEC');
+    assert.fail(null, null, 'Failed to get COMSPEC');
   }
 
   const args = ['/c', process.execPath, __filename, 'child',
