bf7c3dabb470e83e138b5a75508458edb4d8bd9e
Rod Vagg <rod@vagg.org>
2015-10-16 23:10:00 +1100

    src: bump NODE_MODULE_VERSION To 47
    For Node.js v5.0.0, due to upgrade to V8 4.6 which has an incompatible
    ABI
    
    PR-URL: https://github.com/nodejs/node/pull/3400
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_version.h
+++ src/node_version.h
@@ -49,6 +49,6 @@
  * an API is broken in the C++ side, including in v8 or
  * other dependencies.
  */
-#define NODE_MODULE_VERSION 46 /* Node.js v4.0.0 */
+#define NODE_MODULE_VERSION 47 /* Node.js v5.0.0 */
 
 #endif  /* SRC_NODE_VERSION_H_ */
