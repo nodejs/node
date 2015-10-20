55561960833070ada7377efd7afcdd3fbfa46deb
Rich Trott <rtrott@gmail.com>
2015-10-19 21:29:37 -0700

    fixup: handle `unlimited` being coerced to NaN

--- test/sequential/test-child-process-emfile.js
+++ test/sequential/test-child-process-emfile.js
@@ -10,7 +10,7 @@ if (common.isWindows) {
 }
 
 const ulimit = Number(child_process.execSync('ulimit -n'));
-if (ulimit > 64) {
+if (ulimit > 64 || Number.isNaN(ulimit)) {
   // Sorry about this nonsense. It can be replaced if
   // https://github.com/nodejs/node-v0.x-archive/pull/2143#issuecomment-2847886
   // ever happens.
