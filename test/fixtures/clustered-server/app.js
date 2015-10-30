1902aa034651adfcf3ba951d64820752f3d51910
Rich Trott <rtrott@gmail.com>
2015-10-29 22:24:29 -0700

    sched-none

--- test/fixtures/clustered-server/app.js
+++ test/fixtures/clustered-server/app.js
@@ -2,6 +2,8 @@ var http = require('http');
 var cluster = require('cluster');
 var common = require('../../common');
 
+cluster.schedulingPolicy = cluster.SCHED_NONE;
+
 function handleRequest(request, response) {
   response.end('hello world\n');
 }
