e2141cb75eb0f645ea64cb25ee6596af8a22eb7e
Michael Cornacchia <cornacch@ca.ibm.com>
2015-11-06 12:51:48 -0500

    test: skip instead of fail when mem constrained
    The current implementation of tests for strings with length at or
    exceeding kStringMaxLength allocate a temporary buffer inside a try
    block to skip the test if there is insufficient memory. This commit adds
    an invocation of the garbage collector after the temporary buffer is
    allocated so that memory is freed for later allocations.
    
    Change the corresponding catch block to rethrow the original exception
    instead of asserting the exception message to provide more information
    about the exception.
    
    Add an additional check before trying to allocate memory to immediately
    skip the test on machines with insufficient total memory.
    
    PR-URL: https://github.com/nodejs/node/pull/3697
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Michaël Zasso <mic.besace@gmail.com>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>

--- test/parallel/test-stringbytes-external-at-max.js
+++ test/parallel/test-stringbytes-external-at-max.js
@@ -1,22 +1,32 @@
 'use strict';
+// Flags: --expose-gc
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 
 // v8 fails silently if string length > v8::String::kMaxLength
 // v8::String::kMaxLength defined in v8.h
 const kStringMaxLength = process.binding('buffer').kStringMaxLength;
 
-try {
-  new Buffer(kStringMaxLength * 3);
-} catch(e) {
-  assert.equal(e.message, 'Invalid array buffer length');
-  console.log(
-      '1..0 # Skipped: intensive toString tests due to memory confinements');
+const skipMessage =
+  '1..0 # Skipped: intensive toString tests due to memory confinements';
+if (!common.enoughTestMem) {
+  console.log(skipMessage);
   return;
 }
+assert(typeof gc === 'function', 'Run this test with --expose-gc');
 
-const buf = new Buffer(kStringMaxLength);
+try {
+  var buf = new Buffer(kStringMaxLength);
+  // Try to allocate memory first then force gc so future allocations succeed.
+  new Buffer(2 * kStringMaxLength);
+  gc();
+} catch(e) {
+  // If the exception is not due to memory confinement then rethrow it.
+  if (e.message !== 'Invalid array buffer length') throw (e);
+  console.log(skipMessage);
+  return;
+}
 
 const maxString = buf.toString('binary');
 assert.equal(maxString.length, kStringMaxLength);
--- test/parallel/test-stringbytes-external-exceed-max-by-2.js
+++ test/parallel/test-stringbytes-external-exceed-max-by-2.js
@@ -1,22 +1,32 @@
 'use strict';
+// Flags: --expose-gc
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
+
+const skipMessage =
+  '1..0 # Skipped: intensive toString tests due to memory confinements';
+if (!common.enoughTestMem) {
+  console.log(skipMessage);
+  return;
+}
+assert(typeof gc === 'function', 'Run this test with --expose-gc');
 
 // v8 fails silently if string length > v8::String::kMaxLength
 // v8::String::kMaxLength defined in v8.h
 const kStringMaxLength = process.binding('buffer').kStringMaxLength;
 
 try {
-  new Buffer(kStringMaxLength * 3);
+  var buf = new Buffer(kStringMaxLength + 2);
+  // Try to allocate memory first then force gc so future allocations succeed.
+  new Buffer(2 * kStringMaxLength);
+  gc();
 } catch(e) {
-  assert.equal(e.message, 'Invalid array buffer length');
-  console.log(
-      '1..0 # Skipped: intensive toString tests due to memory confinements');
+  // If the exception is not due to memory confinement then rethrow it.
+  if (e.message !== 'Invalid array buffer length') throw (e);
+  console.log(skipMessage);
   return;
 }
 
-const buf2 = new Buffer(kStringMaxLength + 2);
-
-const maxString = buf2.toString('utf16le');
+const maxString = buf.toString('utf16le');
 assert.equal(maxString.length, (kStringMaxLength + 2) / 2);
