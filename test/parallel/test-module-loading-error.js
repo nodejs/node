f99c6363dea907c920565f9a3bdc90a170189ad7
fansworld-claudio <cr@fansworld.tv>
2015-11-16 10:59:39 -0300

    test: module loading error fix solaris #3798
    - refactor test to accept multiple error messages per platform
    - add new message to be found in Solaris 11.3 as per #3798
    
    PR-URL: https://github.com/nodejs/node/pull/3855
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Evan Lucas <evanlucas@me.com>

--- test/parallel/test-module-loading-error.js
+++ test/parallel/test-module-loading-error.js
@@ -5,12 +5,10 @@ var assert = require('assert');
 console.error('load test-module-loading-error.js');
 
 var error_desc = {
-  win32: '%1 is not a valid Win32 application',
-  linux: 'file too short',
-  sunos: 'unknown file type'
+  win32: ['%1 is not a valid Win32 application'],
+  linux: ['file too short', 'Exec format error'],
+  sunos: ['unknown file type', 'not an ELF file']
 };
-var musl_errno_enoexec = 'Exec format error';
-
 var dlerror_msg = error_desc[process.platform];
 
 if (!dlerror_msg) {
@@ -21,11 +19,9 @@ if (!dlerror_msg) {
 try {
   require('../fixtures/module-loading-error.node');
 } catch (e) {
-  if (process.platform === 'linux' &&
-      e.toString().indexOf(musl_errno_enoexec) !== -1) {
-    dlerror_msg = musl_errno_enoexec;
-  }
-  assert.notEqual(e.toString().indexOf(dlerror_msg), -1);
+  assert.strictEqual(dlerror_msg.some((errMsgCase) => {
+    return e.toString().indexOf(errMsgCase) !== -1;
+  }), true);
 }
 
 try {
