781f8c0d1ec00aab3ae73fa3a56623e866b1b48b
Evan Lucas <evanlucas@me.com>
2015-11-18 08:07:15 -0600

    test: add OS X to module loading error test
    Previously, this test was not supported on OS X. This change makes sure
    that it is no longer skipped.
    
    PR-URL: https://github.com/nodejs/node/pull/3901
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-module-loading-error.js
+++ test/parallel/test-module-loading-error.js
@@ -7,7 +7,8 @@ console.error('load test-module-loading-error.js');
 var error_desc = {
   win32: ['%1 is not a valid Win32 application'],
   linux: ['file too short', 'Exec format error'],
-  sunos: ['unknown file type', 'not an ELF file']
+  sunos: ['unknown file type', 'not an ELF file'],
+  darwin: ['file too short']
 };
 var dlerror_msg = error_desc[process.platform];
 
