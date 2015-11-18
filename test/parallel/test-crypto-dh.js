5f0dd690b46d287d339cca1decf855ca84a6600d
Stefan Budeanu <stefan@budeanu.com>
2015-11-18 11:40:32 -0500

    test: avoid test timeouts on rpi
    Generating 1024-bit primes on rpi test machines sometimes
    causes timeouts. Avoid this situation by using 256-bit
    primes when not running in FIPS mode.
    
    PR-URL: https://github.com/nodejs/node/pull/3902
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Brian White <mscdex@mscdex.net>
    Reviewed-By: Ben Noordhuis <ben@strongloop.com>

--- test/parallel/test-crypto-dh.js
+++ test/parallel/test-crypto-dh.js
@@ -11,7 +11,7 @@ var crypto = require('crypto');
 
 // Test Diffie-Hellman with two parties sharing a secret,
 // using various encodings as we go along
-var dh1 = crypto.createDiffieHellman(1024);
+var dh1 = crypto.createDiffieHellman(common.hasFipsCrypto ? 1024 : 256);
 var p1 = dh1.getPrime('buffer');
 var dh2 = crypto.createDiffieHellman(p1, 'buffer');
 var key1 = dh1.generateKeys();
