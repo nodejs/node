8dff4d2166957b49dfd56feb512ba3e5ca383894
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-19 21:49:30 +0100

    Use real OS tmp dir on Linux for long path tests
    Use os.tmpdir() on Linux if it is readable/writable
    in test-fs-long-path.js and test-require-long-path.js
    to avoid failing tests on ecryptfs filesystems - see issue #2255

--- test/parallel/test-require-long-path.js
+++ test/parallel/test-require-long-path.js
@@ -2,16 +2,38 @@
 const common = require('../common');
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
+
+var tmpDir = common.tmpDir;
+var useOsTmpDir = false;
+
+// use real OS tmp dir on Linux when it is readable/writable
+// to avoid failing tests on ecryptfs filesystems:
+// https://github.com/nodejs/node/issues/2255
+if (os.type() == 'Linux') {
+  try {
+    fs.accessSync(os.tmpdir(), fs.R_OK | fs.W_OK);
+    useOsTmpDir = true;
+    tmpDir = path.join(os.tmpdir(),
+      'node-' + process.version + '-test-' + (Math.random() * 1e6 | 0));
+    fs.mkdirSync(tmpDir);
+  } catch (e) {
+    useOsTmpDir = false;
+    tmpDir = common.tmpDir;
+  }
+}
 
 // make a path that is more than 260 chars long.
-const dirNameLen = Math.max(260 - common.tmpDir.length, 1);
-const dirName = path.join(common.tmpDir, 'x'.repeat(dirNameLen));
+const dirNameLen = Math.max(260 - tmpDir.length, 1);
+const dirName = path.join(tmpDir, 'x'.repeat(dirNameLen));
 const fullDirPath = path.resolve(dirName);
 
 const indexFile = path.join(fullDirPath, 'index.js');
 const otherFile = path.join(fullDirPath, 'other.js');
 
-common.refreshTmpDir();
+if (!useOsTmpDir) {
+  common.refreshTmpDir();
+}
 
 fs.mkdirSync(fullDirPath);
 fs.writeFileSync(indexFile, 'require("./other");');
@@ -20,4 +42,11 @@ fs.writeFileSync(otherFile, '');
 require(indexFile);
 require(otherFile);
 
-common.refreshTmpDir();
+if (useOsTmpDir) {
+  fs.unlinkSync(indexFile);
+  fs.unlinkSync(otherFile);
+  fs.rmdirSync(fullDirPath);
+  fs.rmdirSync(tmpDir);
+} else {
+  common.refreshTmpDir();
+}
