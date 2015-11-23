5169311bf9f3ca0a601f9af2e3e732e250dd984b
cjihrig <cjihrig@gmail.com>
2015-11-23 14:58:18 -0500

    util,src: allow lookup of hidden values
    This commit adds an internal util method that makes hidden
    values in the C++ layer visible in JS.
    
    PR-URL: https://github.com/nodejs/node/pull/3988
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

---
+++ test/parallel/test-util-internal.js
@@ -0,0 +1,32 @@
+'use strict';
+// Flags: --expose_internals
+
+const common = require('../common');
+const assert = require('assert');
+const internalUtil = require('internal/util');
+
+function getHiddenValue(obj, name) {
+  return function() {
+    internalUtil.getHiddenValue(obj, name);
+  };
+}
+
+assert.throws(getHiddenValue(), /obj must be an object/);
+assert.throws(getHiddenValue(null, 'foo'), /obj must be an object/);
+assert.throws(getHiddenValue(undefined, 'foo'), /obj must be an object/);
+assert.throws(getHiddenValue('bar', 'foo'), /obj must be an object/);
+assert.throws(getHiddenValue(85, 'foo'), /obj must be an object/);
+assert.throws(getHiddenValue({}), /name must be a string/);
+assert.throws(getHiddenValue({}, null), /name must be a string/);
+assert.throws(getHiddenValue({}, []), /name must be a string/);
+assert.deepEqual(internalUtil.getHiddenValue({}, 'foo'), undefined);
+
+let arrowMessage;
+
+try {
+  require('../fixtures/syntax/bad_syntax');
+} catch (err) {
+  arrowMessage = internalUtil.getHiddenValue(err, 'arrowMessage');
+}
+
+assert(/bad_syntax\.js:1/.test(arrowMessage));
