6a7c9d9293023e298f783057c4bade4ff0c14cc9
Ben Noordhuis <info@bnoordhuis.nl>
2015-11-25 22:37:43 +0100

    util: move .decorateErrorStack to internal/util
    Move the method that was added in commit 8ca412b from earlier this month
    from lib/util.js to lib/internal/util.js.
    
    Avoids exposing a method that we may not wish to expose just yet, seeing
    how it relies on implementation details.
    
    PR-URL: https://github.com/nodejs/node/pull/4026
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-util-decorate-error-stack.js
+++ test/parallel/test-util-decorate-error-stack.js
@@ -1,18 +1,19 @@
+// Flags: --expose_internals
 'use strict';
 const common = require('../common');
 const assert = require('assert');
-const util = require('util');
+const internalUtil = require('internal/util');
 
 assert.doesNotThrow(function() {
-  util.decorateErrorStack();
-  util.decorateErrorStack(null);
-  util.decorateErrorStack(1);
-  util.decorateErrorStack(true);
+  internalUtil.decorateErrorStack();
+  internalUtil.decorateErrorStack(null);
+  internalUtil.decorateErrorStack(1);
+  internalUtil.decorateErrorStack(true);
 });
 
 // Verify that a stack property is not added to non-Errors
 const obj = {};
-util.decorateErrorStack(obj);
+internalUtil.decorateErrorStack(obj);
 assert.strictEqual(obj.stack, undefined);
 
 // Verify that the stack is decorated when possible
@@ -23,7 +24,7 @@ try {
 } catch (e) {
   err = e;
   assert(!/var foo bar;/.test(err.stack));
-  util.decorateErrorStack(err);
+  internalUtil.decorateErrorStack(err);
 }
 
 assert(/var foo bar;/.test(err.stack));
@@ -31,5 +32,5 @@ assert(/var foo bar;/.test(err.stack));
 // Verify that the stack is unchanged when there is no arrow message
 err = new Error('foo');
 const originalStack = err.stack;
-util.decorateErrorStack(err);
+internalUtil.decorateErrorStack(err);
 assert.strictEqual(originalStack, err.stack);
