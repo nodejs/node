127a82ecc950ac73199ded537db5b1161b022855
Dave <dave@jut.io>
2015-11-30 13:12:28 -0800

    test-child-process-flush-stdio: indent 2 spaces

--- test/parallel/test-child-process-flush-stdio.js
+++ test/parallel/test-child-process-flush-stdio.js
@@ -6,8 +6,8 @@ const assert = require('assert');
 const p = cp.spawn('echo');
 
 p.on('close', common.mustCall(function(code, signal) {
-    assert.strictEqual(code, 0);
-    assert.strictEqual(signal, null);
+  assert.strictEqual(code, 0);
+  assert.strictEqual(signal, null);
 }));
 
 p.stdout.read();
