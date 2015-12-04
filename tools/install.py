ce2a393e34e593f24b0027f42423a999e09a6e18
Richard Lau <riclau@uk.ibm.com>
2015-12-03 21:39:19 -0500

    tools: backport tools/install.py for headers
    Backport the tools/install.py changes from 628a3ab that were missed
    when 6fb0b92 backported the corresponding changes to the Makefile to
    build the headers only archive.
    
    PR-URL: https://github.com/nodejs/node/pull/4149
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Johan Bergström <bugs@bergstroem.nu>
    Reviewed-By: Rod Vagg <rod@vagg.org>

--- tools/install.py
+++ tools/install.py
@@ -142,6 +142,9 @@ def files(action):
 
   if 'true' == variables.get('node_install_npm'): npm_files(action)
 
+  headers(action)
+
+def headers(action):
   action([
     'common.gypi',
     'config.gypi',
@@ -194,8 +197,13 @@ def run(args):
   install_path = dst_dir + node_prefix + '/'
 
   cmd = args[1] if len(args) > 1 else 'install'
-  if cmd == 'install': return files(install)
-  if cmd == 'uninstall': return files(uninstall)
+  if os.environ.get('HEADERS_ONLY'):
+    if cmd == 'install': return headers(install)
+    if cmd == 'uninstall': return headers(uninstall)
+  else:
+    if cmd == 'install': return files(install)
+    if cmd == 'uninstall': return files(uninstall)
+
   raise RuntimeError('Bad command: %s\n' % cmd)
 
 if __name__ == '__main__':
