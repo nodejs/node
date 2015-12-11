a1bd3455fce037fad5dfd26145d30b527856c86f
cjihrig <cjihrig@gmail.com>
2015-12-11 14:57:45 -0500

    src: fix line numbers on core errors
    In dfee4e3712ac4673b5fc472a8f77ac65bdc65f87, the module wrapper
    and line offset used when wrapping module code was changed to
    better report errors on the first line of modules. However, that
    commit did not update the runInThisContext() call used to
    execute the core modules, so their error line numbers have been
    off by one. This commit provides the correct lineOffset for core
    modules.
    
    Refs: https://github.com/nodejs/node/pull/2867
    PR-URL: https://github.com/nodejs/node/pull/4254
    Reviewed-By: Brian White <mscdex@mscdex.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Minwoo Jung <jmwsoft@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node.js
+++ src/node.js
@@ -950,7 +950,10 @@
     var source = NativeModule.getSource(this.id);
     source = NativeModule.wrap(source);
 
-    var fn = runInThisContext(source, { filename: this.filename });
+    var fn = runInThisContext(source, {
+      filename: this.filename,
+      lineOffset: -1
+    });
     fn(this.exports, NativeModule.require, this, this.filename);
 
     this.loaded = true;
