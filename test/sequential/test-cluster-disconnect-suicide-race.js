f8a676ed59180f0a61828c0e0441656398016309
Santiago Gimeno <santiago.gimeno@gmail.com>
2015-12-18 19:13:50 +0100

    cluster: fix race condition setting suicide prop
    There is no guarantee that the `suicide` property of a worker in the
    master process is going to be set when the `disconnect` and `exit`
    events are emitted.
    
    To fix it, wait for the ACK of the suicide message from the master
    before disconnecting the worker. Also, there's no need to send the
    suicide message from the worker if the disconnection has been
    initiated in the master.
    
    Add `test-cluster-disconnect-suicide-race` that forks a lot of workers
    to consistently reproduce the issue this patch tries to solve.
    
    Modify `test-regress-GH-3238` so it checks both the `kill` and
    `disconnect` cases. Also take into account that the `disconnect` event
    may be received after the `exit` event.
    
    PR-URL: https://github.com/nodejs/node/pull/4349
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/sequential/test-cluster-disconnect-suicide-race.js
+++ test/sequential/test-cluster-disconnect-suicide-race.js
@@ -2,6 +2,7 @@
 const common = require('../common');
 const assert = require('assert');
 const cluster = require('cluster');
+const os = require('os');
 
 if (cluster.isMaster) {
   function forkWorker(action) {
@@ -15,8 +16,17 @@ if (cluster.isMaster) {
     }));
   }
 
-  forkWorker('disconnect');
-  forkWorker('kill');
+  const cpus = os.cpus().length;
+  const tries = cpus > 8 ? 64 : cpus * 8;
+
+  cluster.on('exit', common.mustCall((worker, code) => {
+    assert.strictEqual(code, 0, 'worker exited with error');
+  }, tries * 2));
+
+  for (let i = 0; i < tries; ++i) {
+    forkWorker('disconnect');
+    forkWorker('kill');
+  }
 } else {
   cluster.worker[process.env.action]();
 }
