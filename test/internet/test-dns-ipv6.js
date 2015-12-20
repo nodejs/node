24b4e1121146bf7c9525f79201aff55224039b38
Brian White <mscdex@mscdex.net>
2015-12-20 04:12:49 -0500

    test: fix internet tests for new dns

--- test/internet/test-dns-ipv6.js
+++ test/internet/test-dns-ipv6.js
@@ -42,7 +42,8 @@ function TEST(f) {
 }
 
 function checkWrap(req) {
-  assert.ok(typeof req === 'object');
+  if (process.oldDNS)
+    assert.ok(typeof req === 'object');
 }
 
 TEST(function test_resolve6(done) {
