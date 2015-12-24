c01bedbadda7cd6fa7a761a1b526307862f8ef01
Rich Trott <rtrott@gmail.com>
2015-12-24 13:00:29 -0800

    child_process: guard against race condition
    It is possible that the internal hnadleMessage() might try to send to
    a channel that has been closed. The result can be an AssertionError.
    Guard against this.
    
    Fixes: https://github.com/nodejs/node/issues/4205

---
+++ test/parallel/test-cluster-disconnect-race.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Ref: https://github.com/nodejs/node/issues/4205
+
+const common = require('../common');
+const net = require('net');
+const cluster = require('cluster');
+cluster.schedulingPolicy = cluster.SCHED_NONE;
+
+if (cluster.isMaster) {
+  var worker1, worker2;
+
+  worker1 = cluster.fork();
+  worker1.on('message', common.mustCall(function() {
+    worker2 = cluster.fork();
+    worker1.disconnect();
+    worker2.on('online', common.mustCall(worker2.disconnect));
+  }));
+
+  return;
+}
+
+var server = net.createServer();
+
+server.listen(common.PORT, function() {
+  process.send('listening');
+});
