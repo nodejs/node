fcc64792aebafd659d4d20387b072e53c1f9e608
Rich Trott <rtrott@gmail.com>
2015-12-24 13:00:29 -0800

    child_process: guard against race condition
    It is possible that the internal handleMessage() might try to send to
    a channel that has been closed. The result can be an AssertionError.
    Guard against this.
    
    Fixes: https://github.com/nodejs/node/issues/4205
    PR-URL: https://github.com/nodejs/node/pull/5153
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

---
+++ test/simple/test-cluster-disconnect-race.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// This code triggers an AssertionError on Linux in Node.js 5.3.0 and earlier.
+// Ref: https://github.com/nodejs/node/issues/4205
+
+var common = require('../common');
+var assert = require('assert');
+var net = require('net');
+var cluster = require('cluster');
+cluster.schedulingPolicy = cluster.SCHED_NONE;
+
+if (cluster.isMaster) {
+  var worker1, worker2;
+
+  worker1 = cluster.fork();
+  worker1.on('message', common.mustCall(function() {
+    worker2 = cluster.fork();
+    worker1.disconnect();
+    worker2.on('online', common.mustCall(worker2.disconnect));
+  }, 2));
+
+  cluster.on('exit', function(worker, code) {
+    assert.strictEqual(code, 0, 'worker exited with error');
+  });
+
+  return;
+}
+
+var server = net.createServer();
+
+server.listen(common.PORT, function retry() {
+  process.send('listening');
+  process.send('listening');
+});
