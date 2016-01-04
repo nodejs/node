04a927643dc772ccba4f1a606330277f2b8b2582
Dave <dave@jut.io>
2016-01-04 10:55:43 -0800

    test: overwrite stderr.write instead of checking e._events

--- test/parallel/test-global-console-exists.js
+++ test/parallel/test-global-console-exists.js
@@ -2,18 +2,28 @@
 
 const assert = require('assert');
 const events = require('events');
+const leak_warning = /EventEmitter memory leak detected. 2 hello listeners/;
+
+var write_calls = 0;
+process.stderr.write = function(data) {
+  if (write_calls === 0)
+    assert.ok(data.match(leak_warning));
+  else if (write_calls === 1)
+    assert.ok(data.match(/Trace/));
+  else
+    assert.ok(false, 'stderr.write should be called only twice');
+
+  write_calls++;
+};
 
 const old_default = events.defaultMaxListeners;
 events.defaultMaxListeners = 1;
 
 const e = new events.EventEmitter();
 e.on('hello', function() {});
-
-assert.ok(!e._events['hello'].hasOwnProperty('warned'));
-
 e.on('hello', function() {});
 
-assert.ok(e._events['hello'].hasOwnProperty('warned'));
+assert.equal(write_calls, 2);
 
 events.defaultMaxListeners = old_default;
 
