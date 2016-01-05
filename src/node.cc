19dfb2adc1424326348fc2216470bf3b003ab1e1
Trevor Norris <trev.norris@gmail.com>
2016-01-05 15:33:21 -0700

    src: add AsyncCallbackScope
    Add a scope that will allow MakeCallback to know whether or not it's
    currently running. This will prevent nextTickQueue and the
    MicrotaskQueue from being processed recursively. It is also required to
    wrap the bootloading stage since it doesn't run within a MakeCallback.
    
    Ref: https://github.com/nodejs/node-v0.x-archive/issues/9245
    PR-URL: https://github.com/nodejs/node/pull/4507
    Reviewed-By: Fedor Indutny <fedor@indutny.com>

--- src/node.cc
+++ src/node.cc
@@ -1132,6 +1132,8 @@ Local<Value> MakeCallback(Environment* env,
   bool ran_init_callback = false;
   bool has_domain = false;
 
+  Environment::AsyncCallbackScope callback_scope(env);
+
   // TODO(trevnorris): Adding "_asyncQueue" to the "this" in the init callback
   // is a horrible way to detect usage. Rethink how detection should happen.
   if (recv->IsObject()) {
@@ -1192,7 +1194,7 @@ Local<Value> MakeCallback(Environment* env,
     }
   }
 
-  if (!env->KickNextTick())
+  if (!env->KickNextTick(&callback_scope))
     return Undefined(env->isolate());
 
   return ret;
@@ -4100,7 +4102,10 @@ static void StartNodeInstance(void* arg) {
     if (instance_data->use_debug_agent())
       StartDebug(env, debug_wait_connect);
 
-    LoadEnvironment(env);
+    {
+      Environment::AsyncCallbackScope callback_scope(env);
+      LoadEnvironment(env);
+    }
 
     env->set_trace_sync_io(trace_sync_io);
 
