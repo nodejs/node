eb2b8c637b34f000e71ca15de80ebbb313215201
Ben Noordhuis <info@bnoordhuis.nl>
2016-01-07 22:36:30 +0100

    module: cache stat() results more aggressively
    Reduce the number of stat() system calls that require() makes by caching
    the results more aggressively.
    
    To avoid unbounded growth without implementing a LRU cache, scope the
    cache to the lifetime of the first call to require().  Recursive calls
    (i.e. require() calls in the included code) transparently profit from
    the cache.
    
    The benchmarked application is the loopback-sample-app[0] and it sees
    the number of stat calls at start-up go down by 40%, from 4736 to 2810.
    
    [0] https://github.com/strongloop/loopback-sample-app
    
    PR-URL: https://github.com/nodejs/node/pull/4575
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/parallel/test-module-require-depth.js
@@ -0,0 +1,13 @@
+// Flags: --expose_internals
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const internalModule = require('internal/module');
+
+// Module one loads two too so the expected depth for two is, well, two.
+assert.strictEqual(internalModule.requireDepth, 0);
+const one = require(common.fixturesDir + '/module-require-depth/one');
+const two = require(common.fixturesDir + '/module-require-depth/two');
+assert.deepStrictEqual(one, { requireDepth: 1 });
+assert.deepStrictEqual(two, { requireDepth: 2 });
+assert.strictEqual(internalModule.requireDepth, 0);
