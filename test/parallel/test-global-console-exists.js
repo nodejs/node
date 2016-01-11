ff4065eab771c8aad104047f0eba9fdf0269c22e
Dave <dave@jut.io>
2016-01-11 12:43:02 -0800

    global console exists spec cleanups eslint-disable required-modules escape regex dot

--- test/parallel/test-global-console-exists.js
+++ test/parallel/test-global-console-exists.js
@@ -1,8 +1,12 @@
+/* eslint-disable required-modules */
+// ordinarily test files must require('common') but that action causes
+// the global console to be compiled, defeating the purpose of this test
+
 'use strict';
 
 const assert = require('assert');
 const events = require('events');
-const leak_warning = /EventEmitter memory leak detected. 2 hello listeners/;
+const leak_warning = /EventEmitter memory leak detected\. 2 hello listeners/;
 
 var write_calls = 0;
 process.stderr.write = function(data) {
@@ -26,6 +30,3 @@ e.on('hello', function() {});
 assert.equal(write_calls, 2);
 
 events.defaultMaxListeners = old_default;
-
-// this caches console, so place it after the test just to appease the linter
-require('../common');
