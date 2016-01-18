8e359da6f437614d31b7a3081d3a2790ff355156
Jackson Tian <shyvo1987@gmail.com>
2016-01-18 13:14:14 +0800

    stream: remove defaultEncoding of ReadableState
    The defaultEncoding of ReadableState can not be set by any API.
    
    Use new Buffer()'s default encoding should be more reasonable.

--- test/parallel/test-stream2-readable-bytesread.js
+++ test/parallel/test-stream2-readable-bytesread.js
@@ -37,6 +37,63 @@ const Transform = require('stream').Transform;
       if (i > this._max)
         this.push(null);
       else
+        this.push('中文', 'utf8');
+    }
+  });
+
+  readable._max = 5;
+  readable._index = 1;
+
+  var total = 0;
+  readable.setEncoding('utf8');
+  readable.on('data', function(chunk) {
+    total += Buffer.byteLength(chunk);
+  });
+
+  readable.on('end', function() {
+    assert.equal(total, readable.bytesRead);
+    assert.equal(30, readable.bytesRead);
+  });
+})();
+
+(function() {
+  // complex case
+  const readable = new Readable({
+    read: function(n) {
+      var i = this._index++;
+      if (i === 3)
+        this.push(null);
+      else if (i === 1) {
+        this.push(new Buffer([0xe4, 0xb8, 0xad, 0xe6, 0x96])); // 5 bytes
+      } else if (i === 2) {
+        this.push('中文'); // 6 bytes
+      }
+    }
+  });
+
+  readable._index = 1;
+
+  var total = 0;
+  readable.setEncoding('utf8');
+  readable.on('data', function(chunk) {
+    console.log(chunk);
+    total += Buffer.byteLength(chunk);
+    console.log(Buffer.byteLength(chunk));
+  });
+
+  readable.on('end', function() {
+    assert.equal(11, readable.bytesRead);
+    assert.equal(total, readable.bytesRead);
+  });
+})();
+
+(function() {
+  const readable = new Readable({
+    read: function(n) {
+      var i = this._index++;
+      if (i > this._max)
+        this.push(null);
+      else
         this.push(new Buffer('a'));
     }
   });
