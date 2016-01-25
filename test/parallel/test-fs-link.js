f90c991749f452c7147e7128182abbc2443f745f
Drew Folta <drew@folta.net>
2016-01-25 12:59:15 -0800

    fixup test: fs.link() test runs on same device

--- test/parallel/test-fs-link.js
+++ test/parallel/test-fs-link.js
@@ -7,19 +7,15 @@ const fs = require('fs');
 common.refreshTmpDir();
 
 // test creating and reading hard link
-const srcPath = path.join(common.fixturesDir, 'cycles', 'root.js');
-const srcContent = fs.readFileSync(srcPath, 'utf8');
-const tmpSrcPath = path.join(common.tmpDir, 'root.js');
+const srcPath = path.join(common.tmpDir, 'hardlink-target.txt');
 const dstPath = path.join(common.tmpDir, 'link1.js');
+fs.writeFileSync(srcPath, 'hello world');
 
 const callback = function(err) {
   if (err) throw err;
   const dstContent = fs.readFileSync(dstPath, 'utf8');
-  assert.strictEqual(srcContent, dstContent);
+  assert.strictEqual('hello world', dstContent);
 };
-
-// copy source file to same directory to avoid cross-filesystem issues
-fs.writeFileSync(tmpSrcPath, srcContent, 'utf8');
 
 fs.link(srcPath, dstPath, common.mustCall(callback));
 
