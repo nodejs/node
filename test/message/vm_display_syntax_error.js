7ba7318db7076a7442d99ca578767cefe7ae1241
cjihrig <cjihrig@gmail.com>
2016-01-25 22:44:17 -0500

    src: attach error to stack on displayErrors
    The vm module's displayErrors option attaches error arrow
    messages as a hidden property. Later, core JavaScript code
    can optionally decorate the error stack with the arrow message.
    However, when user code catches an error, it has no way to
    access the arrow message. This commit changes the behavior of
    displayErrors to mean "decorate the error stack if an error
    occurs."
    
    Fixes: https://github.com/nodejs/node/issues/4835
    PR-URL: https://github.com/nodejs/node/pull/4874
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/message/vm_display_syntax_error.js
+++ test/message/vm_display_syntax_error.js
@@ -4,6 +4,12 @@ var vm = require('vm');
 
 console.error('beginning');
 
+try {
+  vm.runInThisContext('var 4;', { filename: 'foo.vm', displayErrors: true });
+} catch (err) {
+  console.error(err.stack);
+}
+
 vm.runInThisContext('var 5;', { filename: 'test.vm' });
 
 console.error('end');
