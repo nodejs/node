85604d74729d7e1fc8c54947e6e1609867726d55
Rod Vagg <rod@vagg.org>
2016-01-27 17:12:19 +1100

    test: make tick-processor test async

--- test/parallel/test-tick-processor.js
+++ test/parallel/test-tick-processor.js
@@ -1,8 +1,9 @@
 'use strict';
-var fs = require('fs');
-var assert = require('assert');
-var cp = require('child_process');
-var common = require('../common');
+
+const fs = require('fs');
+const assert = require('assert');
+const cp = require('child_process');
+const common = require('../common');
 
 common.refreshTmpDir();
 process.chdir(common.tmpDir);
@@ -16,35 +17,42 @@ runTest(/LazyCompile.*\[eval\]:1|.*%  UNKNOWN/,
      setImmediate(function() { f(); });
    };
    setTimeout(function() { process.exit(0); }, 2000);
-   f();`);
-if (common.isWindows ||
-    common.isSunOS ||
-    common.isAix ||
-    common.isLinuxPPCBE ||
-    common.isFreeBSD) {
-  console.log('1..0 # Skipped: C++ symbols are not mapped for this os.');
-  return;
-}
-runTest(/RunInDebugContext/,
-  `function f() {
-     require(\'vm\').runInDebugContext(\'Debug\');
-     setImmediate(function() { f(); });
-   };
-   setTimeout(function() { process.exit(0); }, 2000);
-   f();`);
+   f();`, () => {
 
-function runTest(pattern, code) {
-  cp.execFileSync(process.execPath, ['-prof', '-pe', code]);
-  var matches = fs.readdirSync(common.tmpDir).filter(function(file) {
-    return /^isolate-/.test(file);
-  });
-  if (matches.length != 1) {
-    assert.fail(null, null, 'There should be a single log file.');
+  if (common.isWindows ||
+      common.isSunOS ||
+      common.isAix ||
+      common.isLinuxPPCBE ||
+      common.isFreeBSD) {
+    console.log('1..0 # Skipped: C++ symbols are not mapped for this os.');
+    return;
   }
-  var log = matches[0];
-  var out = cp.execSync(process.execPath +
-                        ' --prof-process --call-graph-size=10 ' + log,
-                        {encoding: 'utf8'});
-  assert(pattern.test(out));
-  fs.unlinkSync(log);
+  runTest(/RunInDebugContext/,
+    `function f() {
+       require(\'vm\').runInDebugContext(\'Debug\');
+       setImmediate(function() { f(); });
+     };
+     setTimeout(function() { process.exit(0); }, 2000);
+     f();`);
+});
+
+function runTest(pattern, code, callback) {
+  cp.execFile(process.execPath, ['-prof', '-pe', code], (err, stdout, stderr) => {
+    assert.ifError(err)
+    const matches = fs.readdirSync(common.tmpDir).filter((file) => /^isolate-/.test(file));
+
+    if (matches.length != 1)
+      return assert.fail(null, null, 'There should be a single log file.');
+
+    const log = matches[0];
+
+    cp.execFile(process.execPath, ['--prof-process', '--call-graph-size=10', log],
+                          {encoding: 'utf8'}, (err, stdout, stderr) => {
+      assert.ifError(err)
+      assert(pattern.test(stdout.toString()));
+      fs.unlinkSync(log);
+
+      callback && callback()
+    });
+  });
 }
