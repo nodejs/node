7af2c6e073540f6d17652e5d09755e07e69aaf0e
Alan Cohen <alan@ripple.com>
2016-01-27 16:01:19 -0800

    test: Add assertion for TLS peer certificate fingerprint
    PR-URL: https://github.com/nodejs/node/pull/4923
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-tls-peer-certificate.js
+++ test/parallel/test-tls-peer-certificate.js
@@ -38,6 +38,8 @@ server.listen(common.PORT, function() {
     assert.equal(peerCert.subject.emailAddress, 'ry@tinyclouds.org');
     assert.equal(peerCert.serialNumber, '9A84ABCFB8A72AC0');
     assert.equal(peerCert.exponent, '0x10001');
+    assert.equal(peerCert.fingerprint,
+                 '8D:06:3A:B3:E5:8B:85:29:72:4F:7D:1B:54:CD:95:19:3C:EF:6F:AA');
     assert.deepEqual(peerCert.infoAccess['OCSP - URI'],
                      [ 'http://ocsp.nodejs.org/' ]);
 
