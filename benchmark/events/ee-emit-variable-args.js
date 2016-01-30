f65990ecc1a7efeed3ca5c39dfdc17a869efdd9e
Matt Loring <mattloring@google.com>
2016-01-30 11:56:23 -0800

    lib: EventEmitter emit performance improvement
    Previously, there were specialized emit functions used for calls to emit
    with different numbers of arguments. Though originally used to improve
    performance and avoid V8 deopts, they are no longer necessary. Removing
    them results in a substantial peformance increasing benchmark
    performance by as much as 150%. Removing these specialized functions
    also simplifies the code and reduces overall code size.
    
    This also adds a new benchmark which calls emit with different argument
    counts inside a tight loop to expose v8 deopts if present. Emit performs
    better without the specialized emit functions on this new benchmark as
    well.

---
+++ benchmark/events/ee-emit-variable-args.js
@@ -0,0 +1,23 @@
+var common = require('../common.js');
+var EventEmitter = require('events').EventEmitter;
+
+var bench = common.createBenchmark(main, {n: [2e6]});
+
+function main(conf) {
+  var n = conf.n | 0;
+
+  var ee = new EventEmitter();
+  var listeners = [];
+
+  for (var k = 0; k < 10; k += 1)
+    ee.on('dummy', function() {});
+
+  bench.start();
+  for (var i = 0; i < n; i += 1) {
+    ee.emit('dummy');
+    ee.emit('dummy', 5);
+    ee.emit('dummy', 5, true, 'hi');
+    ee.emit('dummy', 5, true, 'hi', {});
+  }
+  bench.end(n);
+}
