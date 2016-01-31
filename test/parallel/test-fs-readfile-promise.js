437166c266d345000533c8036a6089616d7ed67f
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-31 15:53:42 -0200

    Fix lint errors

--- test/parallel/test-fs-readfile-promise.js
+++ test/parallel/test-fs-readfile-promise.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 const emptyFile = path.join(common.fixturesDir, 'empty.txt');
 const fakeFile = path.join('./fakefile');
 
-fs.readFile(emptyFile, 'utf8').then(data => {
+fs.readFile(emptyFile, 'utf8').then((data) => {
   assert.strictEqual(data, '');
 });
 
-fs.readFile(fakeFile, 'utf8').catch(err => {
+fs.readFile(fakeFile, 'utf8').catch((err) => {
   assert(err);
 });
