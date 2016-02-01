be641df04b59d237fe03e8c636c44830d52f8c2c
Andreas Madsen <amwebdk@gmail.com>
2016-02-01 11:30:00 +0100

    benchmark: refactor to use process.send
    This removes the need for parsing stdout from the benchmarks. If the
    process wasn't executed by fork, it will just print like it used to.
    
    This also fixes the parsing of CLI arguments, by inferring the type
    from the options object instead of the value content.
    
    Only two benchmarks had to be changed:
    
    * http/http_server_for_chunky_client.js this previously used a spawn
    now it uses a fork and relays the messages using common.sendResult.
    
    * misc/v8-bench.js this utilized that v8/benchmark/run.js called
    global.print and reformatted the input. It now interfaces directly
    with the benchmark runner global.BenchmarkSuite.
    
    PR-URL: https://github.com/nodejs/node/pull/7094
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
    Reviewed-By: Brian White <mscdex@mscdex.net>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- benchmark/http/http_server_for_chunky_client.js
+++ benchmark/http/http_server_for_chunky_client.js
@@ -3,8 +3,8 @@
 var path = require('path');
 var http = require('http');
 var fs = require('fs');
-var spawn = require('child_process').spawn;
-require('../common.js');
+var fork = require('child_process').fork;
+var common = require('../common.js');
 var test = require('../../test/common.js');
 var pep = path.dirname(process.argv[1]) + '/_chunky_http_client.js';
 var PIPE = test.PIPE;
@@ -30,25 +30,10 @@ server.on('error', function(err) {
   throw new Error('server error: ' + err);
 });
 
-try {
-  var child;
+server.listen(PIPE);
 
-  server.listen(PIPE);
-
-  child = spawn(process.execPath, [pep], { });
-
-  child.on('error', function(err) {
-    throw new Error('spawn error: ' + err);
-  });
-
-  child.stdout.pipe(process.stdout);
-  child.stderr.pipe(process.stderr);
-
-  child.on('close', function(exitCode) {
-    server.close();
-  });
-
-} catch (e) {
-  throw new Error('error: ' + e);
-}
-
+var child = fork(pep, process.argv.slice(2));
+child.on('message', common.sendResult);
+child.on('close', function() {
+  server.close();
+});
