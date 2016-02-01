68837b87097d3ca814ecb38e4226d38b9324a957
Chris Dickinson <christopher.s.dickinson@gmail.com>
2016-01-31 20:42:22 -0800

    dns: promisify

--- test/parallel/test-dns-regress-7070.js
+++ test/parallel/test-dns-regress-7070.js
@@ -5,4 +5,3 @@ var dns = require('dns');
 
 // Should not raise assertion error. Issue #7070
 assert.throws(function() { dns.resolveNs([]); }); // bad name
-assert.throws(function() { dns.resolveNs(''); }); // bad callback
