a29f2391ace64216654a36eda9791b0817502b3e
Natanael Copa <ncopa@alpinelinux.org>
2016-02-05 11:46:47 +0100

    test: fix test-net-* error code check for getaddrinfo(3)
    Replace '...' as invalid hostname with '***', which will give a more
    consisten error message on different systems. The hostname '...' returns
    EAI_AGAIN on musl libc and EAI_NONAME on most other systems.
    
    By changing the testcase we get same restult on all known platforms.
    
    PR-URL: https://github.com/nodejs/node/pull/5099
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Rod Vagg <rod@vagg.org>

--- test/parallel/test-net-connect-immediate-finish.js
+++ test/parallel/test-net-connect-immediate-finish.js
@@ -3,14 +3,14 @@ const common = require('../common');
 const assert = require('assert');
 const net = require('net');
 
-const client = net.connect({host: '...', port: common.PORT});
+const client = net.connect({host: '***', port: common.PORT});
 
 client.once('error', common.mustCall(function(err) {
   assert(err);
   assert.strictEqual(err.code, err.errno);
   assert.strictEqual(err.code, 'ENOTFOUND');
   assert.strictEqual(err.host, err.hostname);
-  assert.strictEqual(err.host, '...');
+  assert.strictEqual(err.host, '***');
   assert.strictEqual(err.syscall, 'getaddrinfo');
 }));
 
