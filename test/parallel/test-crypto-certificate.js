c03582a4a32bac0a0803ab7087a165dc275e23f6
Rich Trott <rtrott@gmail.com>
2016-02-08 10:41:15 -0800

    tools: disallow mixed spaces and tabs for indents
    Enable eslint rule disallowing mixing tabs and spaces for indentation.
    Modify the one file that had been mixing tabs and spaces.
    
    PR-URL: https://github.com/nodejs/node/pull/5135
    Reviewed-By: Michaël Zasso <mic.besace@gmail.com>
    Reviewed-By: Roman Reiss <me@silverwind.io>

--- test/parallel/test-crypto-certificate.js
+++ test/parallel/test-crypto-certificate.js
@@ -22,14 +22,16 @@ var certificate = new crypto.Certificate();
 assert.equal(certificate.verifySpkac(spkacValid), true);
 assert.equal(certificate.verifySpkac(spkacFail), false);
 
-assert.equal(stripLineEndings(certificate.exportPublicKey(spkacValid)
-							  .toString('utf8')),
-			 stripLineEndings(spkacPem.toString('utf8')));
+assert.equal(
+  stripLineEndings(certificate.exportPublicKey(spkacValid).toString('utf8')),
+  stripLineEndings(spkacPem.toString('utf8'))
+);
 assert.equal(certificate.exportPublicKey(spkacFail), '');
 
-assert.equal(certificate.exportChallenge(spkacValid)
-			 .toString('utf8'),
-			 'fb9ab814-6677-42a4-a60c-f905d1a6924d');
+assert.equal(
+  certificate.exportChallenge(spkacValid).toString('utf8'),
+  'fb9ab814-6677-42a4-a60c-f905d1a6924d'
+);
 assert.equal(certificate.exportChallenge(spkacFail), '');
 
 function stripLineEndings(obj) {
