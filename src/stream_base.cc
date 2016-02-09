ac5ca42dfd1bd80a6e30da6619f5c91d06de60e9
Fedor Indutny <fedor@indutny.com>
2016-02-09 16:24:32 -0500

    stream_base: introduce `OnClose` and `close_cb`
    Invoke `close_cb` on `~StreamResource`, it will be used to unconsume
    streams on destruction.

--- src/stream_base.cc
+++ src/stream_base.cc
@@ -444,6 +444,11 @@ Local<Object> StreamBase::GetObject() {
 }
 
 
+StreamResource::~StreamResource() {
+  OnClose();
+}
+
+
 int StreamResource::DoTryWrite(uv_buf_t** bufs, size_t* count) {
   // No TryWrite by default
   return 0;
