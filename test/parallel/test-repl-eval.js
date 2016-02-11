e7ebcda38b66e11ba803e0cf12bb2f89b9a841cc
Rich Trott <rtrott@gmail.com>
2016-02-10 23:16:39 -0800

    fixup

--- test/parallel/test-repl-eval.js
+++ test/parallel/test-repl-eval.js
@@ -4,19 +4,26 @@ const assert = require('assert');
 const repl = require('repl');
 
 {
+  let evalCalledWithExpectedArgs = false;
+
   const options = {
     eval: common.mustCall((cmd, context) => {
-      assert.strictEqual(cmd, 'foo\n');
-      assert.deepStrictEqual(context, {animal: 'Sterrance'});
+      // Assertions here will not cause the test to exit with an error code
+      // so set a boolean that is checked in process.on('exit',...) instead.
+      evalCalledWithExpectedArgs = (cmd === 'foo\n' && context.foo === 'bar');
     })
   };
 
   const r = repl.start(options);
-  r.context = {animal: 'Sterrance'};
+  r.context = {foo: 'bar'};
 
   try {
     r.write('foo\n');
   } finally {
     r.write('.exit\n');
   }
+
+  process.on('exit', () => {
+    assert(evalCalledWithExpectedArgs);
+  });
 }
