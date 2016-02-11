e7cf4a42cae76495dc4e3710a5cfb081d976007a
Roman Reiss <me@silverwind.io>
2016-02-11 20:11:09 +0100

    doc: improve scrolling, various CSS tweaks
    - Made scrolling of the sidebar not affect the main page by using a
      a combination of overflow:hidden and overflow:scroll.
    - Changed the scrollbar of the sidebar to be visible again for
      accessibilty reason and removed the indication gradient because it
      was causing too much issues.
    - The main content is no longer limited to 702px, but now uses all
      available space.
    - In WebKit based browsers, the scrollbar received custom styling
      making it appear to be outside of the sidebar.
    - Changed the background of the code blocks to a neutral color and
      made inline blocks the same color.
    - Made inline code blocks inside italic sections not italic.
    - Simplified styling of api_stability classes by introducing a common
      class name for the stability levels.
    - Fixed various issues related to the green hover background on links.
    - Various minor tweaks to paddings and margins.
    - Cleaned up numbers in CSS, removing unnecessary units an decimals.

--- tools/doc/html.js
+++ tools/doc/html.js
@@ -186,8 +186,10 @@ function parseListItem(text) {
 }
 
 function parseAPIHeader(text) {
-  text = text.replace(/(.*:)\s(\d)([\s\S]*)/,
-                      '<pre class="api_stability_$2">$1 $2$3</pre>');
+  text = text.replace(
+    /(.*:)\s(\d)([\s\S]*)/,
+    '<pre class="api_stability api_stability_$2">$1 $2$3</pre>'
+  );
   return text;
 }
 
