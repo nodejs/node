fbfe12168409fc6d5fbdf012240021f5e41ea54a
Stefan Budeanu <stefan@budeanu.com>
2016-02-17 19:04:00 -0500

    WIP: Use sync spawn API to vastly simplify test code

--- test/parallel/test-crypto-fips.js
+++ test/parallel/test-crypto-fips.js
@@ -1,7 +1,7 @@
 'use strict';
 var common = require('../common');
 var assert = require('assert');
-var spawn = require('child_process').spawn;
+var spawnSync = require('child_process').spawnSync;
 var path = require('path');
 
 if (!common.hasCrypto) {
@@ -9,14 +9,12 @@ if (!common.hasCrypto) {
   return;
 }
 
-const EXIT_FAILURE = -1;
 const FIPS_ENABLED = 1;
 const FIPS_DISABLED = 0;
 const FIPS_ERROR_STRING = 'Error: Cannot set FIPS mode';
 const OPTION_ERROR_STRING = 'bad option';
 const CNF_FIPS_ON = path.join(common.fixturesDir, 'openssl_fips_enabled.cnf');
 const CNF_FIPS_OFF = path.join(common.fixturesDir, 'openssl_fips_disabled.cnf');
-var num_children_spawned = 0;
 var num_children_ok = 0;
 
 function compiledWithFips() {
@@ -32,175 +30,153 @@ function addToEnv(newVar, value) {
   return envCopy;
 }
 
-function childOk(child) {
-  console.error('Child ' +  ++num_children_ok + '/' + num_children_spawned +
-      ' [pid:' + child.pid + '] OK.');
-}
-
-function testHelper(requiresFips, args, expectedOutput, cmd, env) {
-  const child = spawn(process.execPath, args.concat(['-i']), {
+function testHelper(stream, args, expectedOutput, cmd, env) {
+  const fullArgs = args.concat(['-e', 'console.log(' + cmd + ')']);
+  const child = spawnSync(process.execPath, fullArgs, {
     cwd: path.dirname(process.execPath),
     env: env
   });
 
   console.error('Spawned child [pid:' + child.pid + '] with cmd ' +
       cmd + ' and args \'' + args + '\'');
-  num_children_spawned++;
 
-  function responseHandler(response, expectedOutput, expectedError) {
+  function childOk(child) {
+    console.error('Child #' +  ++num_children_ok +
+        ' [pid:' + child.pid + '] OK.');
+  }
+
+  function responseHandler(buffer, expectedOutput) {
+    const response = buffer.toString();
     // We won't always have data on both stdout and stderr.
     if (response.length > 0) {
-      if (EXIT_FAILURE === expectedOutput) {
-        if (-1 === response.indexOf(expectedError))
-          assert.notEqual(-1, response.indexOf(expectedError));
+      if (FIPS_ENABLED !== expectedOutput && FIPS_DISABLED !== expectedOutput) {
+        // In the case of expected errors just look for a substring.
+        assert.notEqual(-1, response.indexOf(expectedOutput));
       } else {
+        // Normal path where we expect either FIPS enabled or disabled.
         assert.equal(expectedOutput, response);
       }
       childOk(child);
     }
   }
 
-  // Buffer all data received from children on stderr/stdout.
-  var stderr = '';
-  var stdout = '';
-
-  function filter(data) {
-    return data.toString().replace(/\n|>/g, '').trim();
-  }
-
-  child.stdout.on('data', function(data) {
-    stdout += filter(data);
-  });
-
-  child.stderr.on('data', function(data) {
-    stderr += filter(data);
-  });
-
-  // If using FIPS mode binary, or running a generic test.
-  if (compiledWithFips() || !requiresFips) {
-    child.stdout.on('end', function(data) {
-      responseHandler(stdout,
-          expectedOutput, FIPS_ERROR_STRING);
-    });
-  } else {
-    // If using a non-FIPS binary expect a failure.
-    child.stdout.on('end', function() {
-      responseHandler(stdout,
-          EXIT_FAILURE, FIPS_ERROR_STRING);
-    });
-    /* Failure to start Node executable is reported on stderr */
-    child.stderr.on('end', function() {
-      responseHandler(stderr,
-          EXIT_FAILURE, OPTION_ERROR_STRING);
-    });
-  }
-  // Wait for write to complete before exiting child, but we won't be able to
-  // write to the child's stdin if cmd line args are rejected and process dies
-  // first, resulting in EPIPE. We can avoid EPIPE by  not writing anything.
-  if (null !== cmd) {
-    child.stdin.setEncoding('utf-8');
-    child.stdin.write(cmd + '\n;process.exit()\n');
-  }
+  responseHandler(child[stream], expectedOutput);
 }
 
 // By default FIPS should be off in both FIPS and non-FIPS builds.
-testHelper(false,
+testHelper(
+  'stdout',
   [],
   FIPS_DISABLED,
   'require("crypto").fips',
   process.env);
 
 // --enable-fips should force FIPS mode on
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   ['--enable-fips'],
-  compiledWithFips() ? FIPS_ENABLED : EXIT_FAILURE,
-  compiledWithFips() ? 'require("crypto").fips' : null,
+  compiledWithFips() ? FIPS_ENABLED : OPTION_ERROR_STRING,
+  'require("crypto").fips',
   process.env);
 
 //--disable-fips should force FIPS mode on
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   ['--disable-fips'],
-  compiledWithFips() ? FIPS_DISABLED : EXIT_FAILURE,
-  compiledWithFips() ? 'require("crypto").fips' : null,
+  compiledWithFips() ? FIPS_DISABLED : OPTION_ERROR_STRING,
+  'require("crypto").fips',
   process.env);
 
 // --disable-fips should take precedence over --enable-fips
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   ['--disable-fips', '--enable-fips'],
-  compiledWithFips() ? FIPS_DISABLED : EXIT_FAILURE,
-  compiledWithFips() ? 'require("crypto").fips' : null,
+  compiledWithFips() ? FIPS_DISABLED : OPTION_ERROR_STRING,
+  'require("crypto").fips',
   process.env);
 
 // --disable-fips and --enable-fips order do not matter
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   ['--enable-fips', '--disable-fips'],
-  compiledWithFips() ? FIPS_DISABLED : EXIT_FAILURE,
-  compiledWithFips() ? 'require("crypto").fips' : null,
+  compiledWithFips() ? FIPS_DISABLED : OPTION_ERROR_STRING,
+  'require("crypto").fips',
   process.env);
 
 // OpenSSL config file should be able to turn on FIPS mode
-testHelper(false,
+testHelper(
+  'stdout',
   [],
   compiledWithFips() ? FIPS_ENABLED : FIPS_DISABLED,
   'require("crypto").fips',
   addToEnv('OPENSSL_CONF', CNF_FIPS_ON));
 
 // --disable-fips should take precedence over OpenSSL config file
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   ['--disable-fips'],
-  compiledWithFips() ? FIPS_DISABLED : EXIT_FAILURE,
-  compiledWithFips() ? 'require("crypto").fips' : null,
+  compiledWithFips() ? FIPS_DISABLED : OPTION_ERROR_STRING,
+  'require("crypto").fips',
   addToEnv('OPENSSL_CONF', CNF_FIPS_ON));
 
 // --enable-fips should take precedence over OpenSSL config file
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   ['--enable-fips'],
-  compiledWithFips() ? FIPS_ENABLED : EXIT_FAILURE,
-  compiledWithFips() ? 'require("crypto").fips' : null,
+  compiledWithFips() ? FIPS_ENABLED : OPTION_ERROR_STRING,
+  'require("crypto").fips',
   addToEnv('OPENSSL_CONF', CNF_FIPS_OFF));
 
 // setFipsCrypto should be able to turn FIPS mode on
-testHelper(true, [], FIPS_ENABLED,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
+  [],
+  compiledWithFips() ? FIPS_ENABLED : FIPS_ERROR_STRING,
   '(require("crypto").fips = true,' +
   'require("crypto").fips)',
   process.env);
 
 // setFipsCrypto should be able to turn FIPS mode on and off
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   [],
-  FIPS_DISABLED,
+  compiledWithFips() ? FIPS_DISABLED : FIPS_ERROR_STRING,
   '(require("crypto").fips = true,' +
   'require("crypto").fips = false,' +
   'require("crypto").fips)',
   process.env);
 
 // setFipsCrypto takes precedence over OpenSSL config file, FIPS on
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   [],
-  compiledWithFips() ? FIPS_ENABLED : FIPS_DISABLED,
+  compiledWithFips() ? FIPS_ENABLED : FIPS_ERROR_STRING,
   '(require("crypto").fips = true,' +
   'require("crypto").fips)',
   addToEnv('OPENSSL_CONF', CNF_FIPS_OFF));
 
 // setFipsCrypto takes precedence over OpenSSL config file, FIPS off
-testHelper(true,
+testHelper(
+  compiledWithFips() ? 'stdout' : 'stderr',
   [],
-  FIPS_DISABLED,
+  compiledWithFips() ? FIPS_DISABLED : FIPS_ERROR_STRING,
   '(require("crypto").fips = false,' +
   'require("crypto").fips)',
   addToEnv('OPENSSL_CONF', CNF_FIPS_ON));
 
 // --disable-fips prevents use of setFipsCrypto API
-testHelper(true,
+testHelper(
+  'stderr',
   ['--disable-fips'],
-  EXIT_FAILURE,
-  compiledWithFips() ? 'require("crypto").fips = true' : null,
+  compiledWithFips() ? FIPS_ERROR_STRING : OPTION_ERROR_STRING,
+  'require("crypto").fips = true',
   process.env);
 
 // --enable-fips prevents use of setFipsCrypto API
-testHelper(true,
+testHelper(
+  'stderr',
   ['--enable-fips'],
-  EXIT_FAILURE,
-  compiledWithFips() ? 'require("crypto").fips = false' : null,
+  compiledWithFips() ? FIPS_ERROR_STRING : OPTION_ERROR_STRING,
+  'require("crypto").fips = false',
   process.env);
 
