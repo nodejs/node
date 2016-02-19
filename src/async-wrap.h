6f312b3a9191709511054a8b8b7e663990d5413e
Trevor Norris <trev.norris@gmail.com>
2016-02-19 13:13:04 -0700

    http_parser: use `MakeCallback`
    Make `HTTPParser` an instance of `AsyncWrap` and make it use
    `MakeCallback`. This means that async wrap hooks will be called on
    consumed TCP sockets as well as on non-consumed ones.
    
    Additional uses of `AsyncCallbackScope` are necessary to prevent
    improper state from progressing that triggers failure in the
    test-http-pipeline-flood.js test. Optimally this wouldn't be necessary,
    but for the time being it's the most sure way to allow operations to
    proceed as they have.
    
    Ref: https://github.com/nodejs/node/pull/7048
    Fix: https://github.com/nodejs/node/issues/4416
    PR-URL: https://github.com/nodejs/node/pull/5419
    Reviewed-By: Fedor Indutny <fedor@indutny.com>

--- src/async-wrap.h
+++ src/async-wrap.h
@@ -17,6 +17,7 @@ namespace node {
   V(FSREQWRAP)                                                                \
   V(GETADDRINFOREQWRAP)                                                       \
   V(GETNAMEINFOREQWRAP)                                                       \
+  V(HTTPPARSER)                                                               \
   V(JSSTREAM)                                                                 \
   V(PIPEWRAP)                                                                 \
   V(PIPECONNECTWRAP)                                                          \
