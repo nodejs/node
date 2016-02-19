eee377d9c4b5886a583841f76b66cb469ee6b4d1
Jackson Tian <puling.tyq@alibaba-inc.com>
2016-02-19 11:01:11 +0800

    lib: simplify code with String.prototype.repeat()
    use String.prototype.repeat() to simplify code, less code,
    more semantically.
    
    PR-URL: https://github.com/nodejs/node/pull/5359
    Reviewed-By: Michaël Zasso <mic.besace@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/fixtures/print-chars.js
+++ test/fixtures/print-chars.js
@@ -2,9 +2,4 @@ var assert = require('assert');
 
 var n = parseInt(process.argv[2]);
 
-var s = '';
-for (var i = 0; i < n; i++) {
-  s += 'c';
-}
-
-process.stdout.write(s);
+process.stdout.write('c'.repeat(n));
