991f82b4bd995ef5d8bafc9293ead93ecac2aec2
Rich Trott <rtrott@gmail.com>
2016-02-20 19:30:21 -0800

    test: mitigate flaky test-http-agent
    Reduce number of clients from 100 to 16 as Raspberry Pi in CI starts to
    exhibit flakiness around 22 or so clients.
    
    Fixes: https://github.com/nodejs/node/issues/5184
    PR-URL: https://github.com/nodejs/node/pull/5346
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-http-agent.js
+++ test/parallel/test-http-agent.js
@@ -9,8 +9,8 @@ var server = http.Server(function(req, res) {
 });
 
 var responses = 0;
-var N = 10;
-var M = 10;
+var N = 4;
+var M = 4;
 
 server.listen(common.PORT, function() {
   for (var i = 0; i < N; i++) {
