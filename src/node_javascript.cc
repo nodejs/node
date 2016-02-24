78726a77cfd65efdaeb2c6beeb3b46a1de9c502b
Ben Noordhuis <info@bnoordhuis.nl>
2016-02-24 21:57:34 +0100

    src,tools: allow utf-8 in built-in js source code
    PR-URL: https://github.com/nodejs/node/pull/5418
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- src/node_javascript.cc
+++ src/node_javascript.cc
@@ -13,11 +13,14 @@ namespace node {
 
 using v8::HandleScope;
 using v8::Local;
+using v8::NewStringType;
 using v8::Object;
 using v8::String;
 
 Local<String> MainSource(Environment* env) {
-  return OneByteString(env->isolate(), node_native, sizeof(node_native) - 1);
+  return String::NewFromUtf8(
+      env->isolate(), reinterpret_cast<const char*>(node_native),
+      NewStringType::kNormal, sizeof(node_native) - 1).ToLocalChecked();
 }
 
 void DefineJavaScript(Environment* env, Local<Object> target) {
@@ -26,10 +29,10 @@ void DefineJavaScript(Environment* env, Local<Object> target) {
   for (int i = 0; natives[i].name; i++) {
     if (natives[i].source != node_native) {
       Local<String> name = String::NewFromUtf8(env->isolate(), natives[i].name);
-      Local<String> source = String::NewFromUtf8(env->isolate(),
-                                                  natives[i].source,
-                                                  String::kNormalString,
-                                                  natives[i].source_len);
+      Local<String> source =
+          String::NewFromUtf8(
+              env->isolate(), reinterpret_cast<const char*>(natives[i].source),
+              NewStringType::kNormal, natives[i].source_len).ToLocalChecked();
       target->Set(name, source);
     }
   }
