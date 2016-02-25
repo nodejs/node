nodejs/node

commit 66fc8ca22b05036f88cf457873332e67f5b576b2

cluster: emit 'message' event on cluster master
For consistency with the worker 'exit', 'online', 'disconnect', and
'listening' events which are emitted on worker and cluster, also emit
'message' on cluster.

Reviewed-by: Sam Roberts <vieuxtech@gmail.com>
Reviewed-by: Christian Tellnes <christian@tellnes.no>
Reviewed-by: Stephen Belanger <admin@stephenbelanger.com>
PR-URL: https://github.com/nodejs/io.js/pull/861

doc/api/cluster.markdown
@@ -248,6 +248,16 @@ This can be used to restart the worker by calling `.fork()` again.
 
 See [child_process event: 'exit'](child_process.html#child_process_event_exit).
 
+## Event: 'message'
+
+* `worker` {Worker object}
+* `message` {Object}
+
+Emitted when any worker receives a message.
+
+See
+[child_process event: 'message'](child_process.html#child_process_event_message).
+
 ## Event: 'setup'
 
 * `settings` {Object}
@@ -529,6 +539,8 @@ created. It is disconnected after the `disconnect` event is emitted.
 ### Event: 'message'
 
 * `message` {Object}
+
+Similar to the `cluster.on('message')` event, but specific to this worker.
 
 This event is the same as the one provided by `child_process.fork()`.
 

lib/cluster.js
@@ -319,6 +319,8 @@ function masterInit() {
       process: workerProcess
     });
 
+    worker.on('message', this.emit.bind(this, 'message'));
+
     function removeWorker(worker) {
       assert(worker);
 

test/parallel/test-cluster-message.js
@@ -6,7 +6,7 @@ var net = require('net');
 
 function forEach(obj, fn) {
   Object.keys(obj).forEach(function(name, index) {
-    fn(obj[name], name, index);
+    fn(obj[name], name);
   });
 }
 
@@ -45,6 +45,10 @@ if (cluster.isWorker) {
 else if (cluster.isMaster) {
 
   var checks = {
+    global: {
+      'receive': false,
+      'correct': false
+    },
     master: {
       'receive': false,
       'correct': false
@@ -76,12 +80,15 @@ else if (cluster.isMaster) {
   // Spawn worker
   var worker = cluster.fork();
 
-  // When a IPC message is received form the worker
+  // When a IPC message is received from the worker
   worker.on('message', function(message) {
     check('master', message === 'message from worker');
   });
+  cluster.on('message', function(message) {
+    check('global', message === 'message from worker');
+  });
 
-  // When a TCP connection is made with the worker connect to it
+  // When a TCP server is listening in the worker connect to it
   worker.on('listening', function() {
 
     client = net.connect(common.PORT, function() {

commit 66f4586dd031435984ed77837512531dfc4fe893

cluster: emit worker as first 'message' event arg
It's documented as such but didn't actually behave that way.

Bug introduced in commit 66fc8ca ("cluster: emit 'message' event on
cluster master"), which is the commit that introduced the event.

Fixes: https://github.com/nodejs/node/issues/5126
PR-URL: https://github.com/nodejs/node/pull/5361
Reviewed-By: Ali Ijaz Sheikh <ofrobots@google.com>
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

doc/api/cluster.markdown
@@ -487,10 +487,28 @@ The `addressType` is one of:
 
 * `worker` {cluster.Worker}
 * `message` {Object}
+* `handle` {undefined|Object}
 
 Emitted when any worker receives a message.
 
 See [child_process event: 'message'][].
+
+Before Node.js v6.0, this event emitted only the message and the handle,
+but not the worker object, contrary to what the documentation stated.
+
+If you need to support older versions and don't need the worker object,
+you can work around the discrepancy by checking the number of arguments:
+
+```js
+cluster.on('message', function(worker, message, handle) {
+  if (arguments.length === 2) {
+    handle = message;
+    message = worker;
+    worker = undefined;
+  }
+  // ...
+});
+```
 
 ## Event: 'online'
 

lib/cluster.js
@@ -341,9 +341,9 @@ function masterInit() {
       process: workerProcess
     });
 
-    worker.on('message', (message, handle) =>
-      cluster.emit('message', message, handle)
-    );
+    worker.on('message', function(message, handle) {
+      cluster.emit('message', this, message, handle);
+    });
 
     worker.process.once('exit', function(exitCode, signalCode) {
       /*

test/parallel/test-cluster-message.js
@@ -84,7 +84,8 @@ else if (cluster.isMaster) {
   worker.on('message', function(message) {
     check('master', message === 'message from worker');
   });
-  cluster.on('message', function(message) {
+  cluster.on('message', function(worker_, message) {
+    assert.strictEqual(worker_, worker);
     check('global', message === 'message from worker');
   });
 