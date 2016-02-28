75c1520c84f0972ff8bead5159b66829379554d8
Rich Trott <rtrott@gmail.com>
2016-02-27 21:56:18 -0800

    benchmark: refactor to eliminate redeclared vars
    In order to comply with linting rules used in the rest of the code base,
    eliminate redeclared variables. A conservative approach is used so as to
    avoid unintentional performance issues (for example, as might be seen in
    some situations when using `let` instead of `var`).

--- benchmark/buffers/buffer-base64-encode.js
+++ benchmark/buffers/buffer-base64-encode.js
@@ -7,9 +7,10 @@ function main(conf) {
   var N = 64 * 1024 * 1024;
   var b = Buffer(N);
   var s = '';
-  for (var i = 0; i < 256; ++i) s += String.fromCharCode(i);
-  for (var i = 0; i < N; i += 256) b.write(s, i, 256, 'ascii');
+  var i;
+  for (i = 0; i < 256; ++i) s += String.fromCharCode(i);
+  for (i = 0; i < N; i += 256) b.write(s, i, 256, 'ascii');
   bench.start();
-  for (var i = 0; i < 32; ++i) b.toString('base64');
+  for (i = 0; i < 32; ++i) b.toString('base64');
   bench.end(64);
 }
--- benchmark/compare.js
+++ benchmark/compare.js
@@ -37,18 +37,19 @@ for (var i = 2; i < process.argv.length; i++) {
   }
 }
 
+var start, green, red, reset, end;
 if (!html) {
-  var start = '';
-  var green = '\u001b[1;32m';
-  var red = '\u001b[1;31m';
-  var reset = '\u001b[m';
-  var end = '';
+  start = '';
+  green = '\u001b[1;32m';
+  red = '\u001b[1;31m';
+  reset = '\u001b[m';
+  end = '';
 } else {
-  var start = '<pre style="background-color:#333;color:#eee">';
-  var green = '<span style="background-color:#0f0;color:#000">';
-  var red = '<span style="background-color:#f00;color:#fff">';
-  var reset = '</span>';
-  var end = '</pre>';
+  start = '<pre style="background-color:#333;color:#eee">';
+  green = '<span style="background-color:#0f0;color:#000">';
+  red = '<span style="background-color:#f00;color:#fff">';
+  reset = '</span>';
+  end = '</pre>';
 }
 
 var runBench = process.env.NODE_BENCH || 'bench';
@@ -137,7 +138,7 @@ function compare() {
 
     var r0 = util.format('%s%s: %d%s', g, nodes[0], n0.toPrecision(5), g ? reset : '');
     var r1 = util.format('%s%s: %d%s', r, nodes[1], n1.toPrecision(5), r ? reset : '');
-    var pct = c + pct + '%' + reset;
+    pct = c + pct + '%' + reset;
     var l = util.format('%s: %s %s', bench, r0, r1);
     maxLen = Math.max(l.length + pct.length, maxLen);
     return [l, pct];
