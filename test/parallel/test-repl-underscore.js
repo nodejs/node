1dd5e2a1aa761a03f5874fe78c796f4ab1867401
Lance Ball <lball@redhat.com>
2016-03-03 13:02:31 -0500

    repl: Reset underscore assignment on .clear
    Ensures that, when invoking `.clear` in a repl, the underscore
    assignment behavior is reset, so that its value is again set to the most
    recently evaluated expression.
    
    Additional tests have been added for the behavior of `let` when underscore
    assignment has been disabled.

--- test/parallel/test-repl-underscore.js
+++ test/parallel/test-repl-underscore.js
@@ -4,18 +4,37 @@ require('../common');
 const assert = require('assert');
 const repl = require('repl');
 const stream = require('stream');
-const inputStream = new stream.PassThrough();
-const outputStream = new stream.PassThrough();
-let output = '';
 
-outputStream.on('data', (data) => {
-  output += data;
+var tests = [
+  testSloppyMode,
+  testStrictMode,
+  testResetContext
+];
+
+tests.forEach(function(test) {
+  test();
 });
 
-process.on('exit', () => {
-  const lines = output.trim().split('\n');
+function testSloppyMode() {
+  const r = initRepl(repl.REPL_MODE_SLOPPY);
 
-  assert.deepStrictEqual(lines, [
+  // cannot use `let` in sloppy mode
+  r.write(`_;          // initial value undefined
+          var x = 10;  // evaluates to undefined
+          _;           // still undefined
+          y = 10;      // evaluates to 10
+          _;           // 10 from last eval
+          _ = 20;      // explicitly set to 20
+          _;           // 20 from user input
+          _ = 30;      // make sure we can set it twice and no prompt
+          _;           // 30 from user input
+          y = 40;      // make sure eval doesn't change _
+          _;           // remains 30 from user input
+          `);
+
+  assertOutput(r.output, [
+    'undefined',
+    'undefined',
     'undefined',
     '10',
     '10',
@@ -23,27 +42,88 @@ process.on('exit', () => {
     '20',
     '20',
     '30',
-    '20',
+    '30',
     '40',
-    '40',
-    '40'
+    '30'
   ]);
-});
+}
 
-const r = repl.start({
-  prompt: '',
-  input: inputStream,
-  output: outputStream
-});
+function testStrictMode() {
+  const r = initRepl(repl.REPL_MODE_STRICT);
 
-r.write('_;\n');
-r.write('x = 10;\n');
-r.write('_;\n');
-r.write('_ = 20;\n');
-r.write('_;\n');
-r.write('y = 30;\n');
-r.write('_;\n');
-r.write('_ = 40;\n');
-r.write('_;\n');
-r.resetContext();
-r.write('_;\n');
+  r.write(`_;          // initial value undefined
+          var x = 10;  // evaluates to undefined
+          _;           // still undefined
+          let _ = 20;  // use 'let' only in strict mode - evals to undefined
+          _;           // 20 from user input
+          _ = 30;      // make sure we can set it twice and no prompt
+          _;           // 30 from user input
+          var y = 40;  // make sure eval doesn't change _
+          _;           // remains 30 from user input
+          function f() { let _ = 50; } // undefined
+          f();         // undefined
+          _;           // remains 30 from user input
+          `);
+
+  assertOutput(r.output, [
+    'undefined',
+    'undefined',
+    'undefined',
+    'undefined',
+    '20',
+    '30',
+    '30',
+    'undefined',
+    '30',
+    'undefined',
+    'undefined',
+    '30'
+  ]);
+}
+
+function testResetContext() {
+  const r = initRepl(repl.REPL_MODE_SLOPPY);
+
+  r.write(`_ = 10;     // explicitly set to 10
+          _;           // 10 from user input
+          .clear       // Clearing context...
+          _;           // remains 10
+          x = 20;      // but behavior reverts to last eval
+          _;           // expect 20
+          `);
+
+  assertOutput(r.output, [
+    'expression assignment to _ now disabled',
+    '10',
+    '10',
+    'Clearing context...',
+    '10',
+    '20',
+    '20'
+  ]);
+}
+
+function initRepl(mode, useGlobal) {
+  const inputStream = new stream.PassThrough();
+  const outputStream = new stream.PassThrough();
+  outputStream.accum = '';
+
+  outputStream.on('data', (data) => {
+    outputStream.accum += data;
+  });
+
+  return repl.start({
+    input: inputStream,
+    output: outputStream,
+    useColors: false,
+    terminal: false,
+    prompt: '',
+    useGlobal: !!useGlobal,
+    replMode: mode
+  });
+}
+
+function assertOutput(output, expected) {
+  const lines = output.accum.trim().split('\n');
+  assert.deepStrictEqual(lines, expected);
+}
