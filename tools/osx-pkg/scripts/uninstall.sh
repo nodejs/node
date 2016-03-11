fe8c6c7d7b257bd6c89cad3c6bf3406def168eaa
Rod Vagg <rod@vagg.org>
2016-03-11 16:24:41 +1100

    build: fix osx uninstaller script

--- tools/osx-pkg/scripts/uninstall.sh
+++ tools/osx-pkg/scripts/uninstall.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-DIR_PREFIX=/usr/local
+
+if [ -z "$DIR_PREFIX" ]; then
+  DIR_PREFIX=/usr/local
+fi
+
 if [ -f /tmp/nodejs-run-uninstall ]; then
   rm -f $DIR_PREFIX/bin/node
   rm -f $DIR_PREFIX/bin/npm
-  if [ -L $DIR_PREFIX/bin/node ]; then
-    rm -f $DIR_PREFIX/bin/node
-  fi
   rm -rf $DIR_PREFIX/include/node
   rm -f $DIR_PREFIX/lib/dtrace/node.d
   rm -f $DIR_PREFIX/share/man/man1/node.1
   rm -f $DIR_PREFIX/share/systemtap/tapset/node.stp
+  rm -f $DIR_PREFIX/share/doc/node/gdbinit
 
-  if [ ! "$(ls -A $DIR_PREFIX/lib/node_modules)" ]; then
+  rm -rf $DIR_PREFIX/lib/node_modules/npm
+  if [ ! "$(ls -A $DIR_PREFIX/lib/node_modules 2> /dev/null)" ]; then
     rm -rf $DIR_PREFIX/lib/node_modules
   fi
 
