c86162300d86e0d093917ec3b77d6f04c384af41
Kevin Locke <kevin@kevinlocke.name>
2016-03-15 15:40:32 -0700

    stream: Support writing null to stream.Transform
    As discussed in #5711, writing null to a stream.Transform puts the
    stream into an unrecoverable state.  This commit fixes the issue by
    passing it to ._transform, like the other falsey values.
    
    It updates the falsey value test for Transform to include null (and
    undefined, which was previously omitted) and adds a test for the
    behavior of Writable in objectMode when falsey values are written to
    match Transform.
    
    Fixes: #5711

--- test/parallel/test-stream2-transform.js
+++ test/parallel/test-stream2-transform.js
@@ -131,7 +131,7 @@ test('simple transform', function(t) {
 test('simple object transform', function(t) {
   var pt = new Transform({ objectMode: true });
   pt._transform = function(c, e, cb) {
-    pt.push(JSON.stringify(c));
+    pt.push(JSON.stringify(c) || 'undefined');
     cb();
   };
 
@@ -142,6 +142,8 @@ test('simple object transform', function(t) {
   pt.write('foo');
   pt.write('');
   pt.write({ a: 'b'});
+  pt.write(null);
+  pt.write(undefined);
   pt.end();
 
   t.equal(pt.read(), '1');
@@ -151,6 +153,8 @@ test('simple object transform', function(t) {
   t.equal(pt.read(), '"foo"');
   t.equal(pt.read(), '""');
   t.equal(pt.read(), '{"a":"b"}');
+  t.equal(pt.read(), 'null');
+  t.equal(pt.read(), 'undefined');
   t.end();
 });
 
