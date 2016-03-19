a33a70a1266a012db976c08f9ceec08395f75631
Rich Trott <rtrott@gmail.com>
2016-03-18 20:54:41 -0700

    fixup: test improvement

--- test/parallel/test-zlib-sync-no-event.js
+++ test/parallel/test-zlib-sync-no-event.js
@@ -1,9 +1,21 @@
 'use strict';
 require('../common');
 const zlib = require('zlib');
+const assert = require('assert');
+
+const shouldNotBeCalled = () => { throw new Error('unexpected event'); };
+
+const message = 'Come on, Fhqwhgads.';
 
 const zipper = new zlib.Gzip();
-zipper.on('close', () => { throw new Error('unexpected `close` event'); });
+zipper.on('close', shouldNotBeCalled);
 
-const buffer = new Buffer('hello world');
-zipper._processChunk(buffer, zlib.Z_FINISH);
+const buffer = new Buffer(message);
+const zipped = zipper._processChunk(buffer, zlib.Z_FINISH);
+
+const unzipper = new zlib.Gunzip();
+unzipper.on('close', shouldNotBeCalled);
+
+const unzipped = unzipper._processChunk(zipped, zlib.Z_FINISH);
+assert.notEqual(zipped.toString(), message);
+assert.strictEqual(unzipped.toString(), message);
