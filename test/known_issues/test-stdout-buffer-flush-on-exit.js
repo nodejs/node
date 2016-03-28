b12cf06d0aa3807ad5f93261fa5c17ef7c54ca29
Rich Trott <rtrott@gmail.com>
2016-03-28 14:09:53 -0700

    fixup per mscdex

--- test/known_issues/test-stdout-buffer-flush-on-exit.js
+++ test/known_issues/test-stdout-buffer-flush-on-exit.js
@@ -1,19 +1,21 @@
 'use strict';
 // Refs: https://github.com/nodejs/node/issues/2148
+
 require('../common');
 const assert = require('assert');
 const execSync = require('child_process').execSync;
 
-const longLine = 'foo bar baz quux quuz aaa bbb ccc'.repeat(80);
-const expectedLength = (longLine.length * 999) + 1;
+const longLine = 'foo bar baz quux quuz aaa bbb ccc'.repeat(65536);
 
 if (process.argv[2] === 'child') {
   process.on('exit', () => {
-    console.log(longLine.repeat(999));
+    console.log(longLine);
   });
   process.exit();
 }
 
-const stdout = execSync(`${process.execPath} ${__filename} child`);
+const stdout = execSync(`${process.execPath} ${__filename} child`)
+  .toString()
+  .trim();
 
-assert.strictEqual(stdout.length, expectedLength);
+assert.strictEqual(stdout, longLine);
