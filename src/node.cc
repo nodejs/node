nodejs/node

commit 21d66d621c5b1ce27498fcb1cb846fd34fce4234

lib: remove bootstrap global context indirection
PR-URL: https://github.com/nodejs/node/pull/5881
Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
Reviewed-By: James M Snell <jasnell@gmail.com>
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

src/node.cc
@@ -118,6 +118,7 @@ using v8::Locker;
 using v8::MaybeLocal;
 using v8::Message;
 using v8::Name;
+using v8::Null;
 using v8::Number;
 using v8::Object;
 using v8::ObjectTemplate;
@@ -3321,8 +3322,12 @@ void LoadEnvironment(Environment* env) {
 
   env->SetMethod(env->process_object(), "_rawDebug", RawDebug);
 
+  // Expose the global object as a property on itself
+  // (Allows you to set stuff on `global` from anywhere in JavaScript.)
+  global->Set(FIXED_ONE_BYTE_STRING(env->isolate(), "global"), global);
+
   Local<Value> arg = env->process_object();
-  f->Call(global, 1, &arg);
+  f->Call(Null(env->isolate()), ARRAY_SIZE(&arg), &arg);
 }
 
 static void PrintHelp();

lib/internal/bootstrap_node.js
@@ -8,7 +8,6 @@
 'use strict';
 
 (function(process) {
-  this.global = this;
 
   function startup() {
     var EventEmitter = NativeModule.require('events');
@@ -211,7 +210,6 @@
 
   function setupGlobalVariables() {
     global.process = process;
-    global.global = global;
     const util = NativeModule.require('util');
 
     // Deprecate GLOBAL and root

commit 00c876dec54f9c515aeed78c75ce61a4e0206d95

src: fix ARRAY_SIZE() logic error
Bug introduced in commit 21d66d62 ("lib: remove bootstrap global context
indirection").

PR-URL: https://github.com/nodejs/node/pull/5969
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
Reviewed-By: James M Snell <jasnell@gmail.com>

src/node.cc
@@ -3327,7 +3327,7 @@ void LoadEnvironment(Environment* env) {
   global->Set(FIXED_ONE_BYTE_STRING(env->isolate(), "global"), global);
 
   Local<Value> arg = env->process_object();
-  f->Call(Null(env->isolate()), ARRAY_SIZE(&arg), &arg);
+  f->Call(Null(env->isolate()), 1, &arg);
 }
 
 static void PrintHelp();