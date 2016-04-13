7916d607cb97e008245294bff0f6aa183b2f0a8e
James M Snell <jasnell@gmail.com>
2016-04-12 18:20:06 -0700

    process: on error in stdout/stderr, reopen as null
    On an error in stdout/stderr stream, redirect the error to a new
    `'stdio-error'` event and set the stdout/stderr to a null stream.
    
    Additional refactoring the stdio.js while in the code.

--- test/parallel/test-tty-stdout-end.js
+++ test/parallel/test-tty-stdout-end.js
@@ -3,13 +3,4 @@
 require('../common');
 const assert = require('assert');
 
-const shouldThrow = function() {
-  process.stdout.end();
-};
-
-const validateError = function(e) {
-  return e instanceof Error &&
-    e.message === 'process.stdout cannot be closed.';
-};
-
-assert.throws(shouldThrow, validateError);
+assert.throws(() => process.stdout.end(), /process.stdout cannot be closed./);
