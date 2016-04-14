67f21dd90722fb06dfd0cea70a10da7dd454ff3f
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2016-04-14 18:55:34 -0400

    handle_wrap: IsRefed -> Unrefed, no isAlive check
    This fixes my perceived usability issues with 7d8882b. Which, at the
    time of writing, has not landed in any release except v6 RCs. This
    should not be considered a breaking change due to that.
    
    It is useful if you have a handle, even if it has been closed, to be
    able to inspect whether that handle was unrefed or not. As such, this
    renames the method accordingly. If people need to check a handle's
    aliveness, that is a separate API we should consider exposing.
    
    Refs: https://github.com/nodejs/node/pull/5834
    PR-URL: https://github.com/nodejs/node/pull/6204
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- test/parallel/test-handle-wrap-isrefed.js
+++ test/parallel/test-handle-wrap-isrefed.js
@@ -12,86 +12,92 @@ function makeAssert(message) {
 
 // child_process
 {
-  const assert = makeAssert('isRefed() not working on process_wrap');
+  const assert = makeAssert('unrefed() not working on process_wrap');
   const spawn = require('child_process').spawn;
   const cmd = common.isWindows ? 'rundll32' : 'ls';
   const cp = spawn(cmd);
-  assert(Object.getPrototypeOf(cp._handle).hasOwnProperty('isRefed'), true);
-  assert(cp._handle.isRefed(), true);
+  assert(Object.getPrototypeOf(cp._handle).hasOwnProperty('unrefed'), true);
+  assert(cp._handle.unrefed(), false);
   cp.unref();
-  assert(cp._handle.isRefed(), false);
+  assert(cp._handle.unrefed(), true);
   cp.ref();
-  assert(cp._handle.isRefed(), true);
-  cp.unref();
+  assert(cp._handle.unrefed(), false);
+  cp._handle.close();
+  assert(cp._handle.unrefed(), false);
 }
 
 
 // dgram
 {
-  const assert = makeAssert('isRefed() not working on udp_wrap');
+  const assert = makeAssert('unrefed() not working on udp_wrap');
   const dgram = require('dgram');
 
   const sock4 = dgram.createSocket('udp4');
-  assert(Object.getPrototypeOf(sock4._handle).hasOwnProperty('isRefed'), true);
-  assert(sock4._handle.isRefed(), true);
+  assert(Object.getPrototypeOf(sock4._handle).hasOwnProperty('unrefed'), true);
+  assert(sock4._handle.unrefed(), false);
   sock4.unref();
-  assert(sock4._handle.isRefed(), false);
+  assert(sock4._handle.unrefed(), true);
   sock4.ref();
-  assert(sock4._handle.isRefed(), true);
-  sock4.unref();
+  assert(sock4._handle.unrefed(), false);
+  sock4._handle.close();
+  assert(sock4._handle.unrefed(), false);
 
   const sock6 = dgram.createSocket('udp6');
-  assert(Object.getPrototypeOf(sock6._handle).hasOwnProperty('isRefed'), true);
-  assert(sock6._handle.isRefed(), true);
+  assert(Object.getPrototypeOf(sock6._handle).hasOwnProperty('unrefed'), true);
+  assert(sock6._handle.unrefed(), false);
   sock6.unref();
-  assert(sock6._handle.isRefed(), false);
+  assert(sock6._handle.unrefed(), true);
   sock6.ref();
-  assert(sock6._handle.isRefed(), true);
-  sock6.unref();
+  assert(sock6._handle.unrefed(), false);
+  sock6._handle.close();
+  assert(sock6._handle.unrefed(), false);
 }
 
 
 // pipe
 {
-  const assert = makeAssert('isRefed() not working on pipe_wrap');
+  const assert = makeAssert('unrefed() not working on pipe_wrap');
   const Pipe = process.binding('pipe_wrap').Pipe;
   const handle = new Pipe();
-  assert(Object.getPrototypeOf(handle).hasOwnProperty('isRefed'), true);
-  assert(handle.isRefed(), true);
+  assert(Object.getPrototypeOf(handle).hasOwnProperty('unrefed'), true);
+  assert(handle.unrefed(), false);
   handle.unref();
-  assert(handle.isRefed(), false);
+  assert(handle.unrefed(), true);
   handle.ref();
-  assert(handle.isRefed(), true);
-  handle.unref();
+  assert(handle.unrefed(), false);
+  handle.close();
+  assert(handle.unrefed(), false);
 }
 
 
 // tcp
 {
-  const assert = makeAssert('isRefed() not working on tcp_wrap');
+  const assert = makeAssert('unrefed() not working on tcp_wrap');
   const net = require('net');
   const server = net.createServer(() => {}).listen(common.PORT);
-  assert(Object.getPrototypeOf(server._handle).hasOwnProperty('isRefed'), true);
-  assert(server._handle.isRefed(), true);
+  assert(Object.getPrototypeOf(server._handle).hasOwnProperty('unrefed'), true);
+  assert(server._handle.unrefed(), false);
   assert(server._unref, false);
   server.unref();
-  assert(server._handle.isRefed(), false);
+  assert(server._handle.unrefed(), true);
   assert(server._unref, true);
   server.ref();
-  assert(server._handle.isRefed(), true);
+  assert(server._handle.unrefed(), false);
   assert(server._unref, false);
-  server.unref();
+  server._handle.close();
+  assert(server._handle.unrefed(), false);
 }
 
 
 // timers
 {
-  const assert = makeAssert('isRefed() not working on timer_wrap');
+  const assert = makeAssert('unrefed() not working on timer_wrap');
   const timer = setTimeout(() => {}, 500);
   timer.unref();
-  assert(Object.getPrototypeOf(timer._handle).hasOwnProperty('isRefed'), true);
-  assert(timer._handle.isRefed(), false);
+  assert(Object.getPrototypeOf(timer._handle).hasOwnProperty('unrefed'), true);
+  assert(timer._handle.unrefed(), true);
   timer.ref();
-  assert(timer._handle.isRefed(), true);
-  timer.unref();
+  assert(timer._handle.unrefed(), false);
+  timer.close();
+  assert(timer._handle.unrefed(), false);
 }
