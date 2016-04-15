33c1e8b3d56fc4546a45489e05817725a95b53b9
joelostrowski <jo@craftwork.dk>
2016-04-15 16:49:36 +0200

    tls: implement clientCertEngine option
    Add an option 'clientCertEngine' to `tls.createSecureContext()` which gets
    wired up to OpenSSL function `SSL_CTX_set_client_cert_engine`. The option
    is passed through from `https.request()` as well. This allows using a custom
    OpenSSL engine to provide the client certificate.

--- src/node_crypto.h
+++ src/node_crypto.h
@@ -93,6 +93,9 @@ class SecureContext : public BaseObject {
   SSL_CTX* ctx_;
   X509* cert_;
   X509* issuer_;
+#ifndef OPENSSL_NO_ENGINE
+  bool client_cert_engine_provided_ = false;
+#endif  // !OPENSSL_NO_ENGINE
 
   static const int kMaxSessionSize = 10 * 1024;
 
@@ -135,6 +138,10 @@ class SecureContext : public BaseObject {
       const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Close(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void LoadPKCS12(const v8::FunctionCallbackInfo<v8::Value>& args);
+#ifndef OPENSSL_NO_ENGINE
+  static void SetClientCertEngine(
+      const v8::FunctionCallbackInfo<v8::Value>& args);
+#endif  // !OPENSSL_NO_ENGINE
   static void GetTicketKeys(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetTicketKeys(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetFreeListLength(
