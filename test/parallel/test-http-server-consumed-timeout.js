602c6aeb84c0d5e07c76c9697e75486495546ff7
Fedor Indutny <fedor@indutny.com>
2016-04-19 16:41:13 -0400

    http: unref socket timer on parser execute
    When underlying `net.Socket` instance is consumed in http server - no
    `data` events are emitted, and thus `socket.setTimeout` fires the
    callback even if the data is constantly flowing into the socket.
    
    Fix this by calling `socket._unrefTimer()` on every `onParserExecute`
    call.
    
    Fix: #5899

---
+++ test/parallel/test-http-server-consumed-timeout.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  server.close();
+
+  res.writeHead(200);
+  res.flushHeaders();
+
+  req.setTimeout(100, () => {
+    assert(false, 'Should not happen');
+  });
+  req.resume();
+  req.once('end', () => {
+    res.end();
+  });
+});
+
+server.listen(common.PORT, () => {
+  const req = http.request({
+    port: common.PORT,
+    method: 'POST'
+  }, (res) => {
+    const interval = setInterval(() => {
+      req.write('a');
+    }, 25);
+    setTimeout(() => {
+      clearInterval(interval);
+      req.end();
+    }, 150);
+  });
+  req.write('.');
+});
