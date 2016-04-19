a91471f2ee97d0fc1bce4cb527e35f4767683af1
James M Snell <jasnell@gmail.com>
2016-04-18 21:02:18 -0700

    src: add process.binding('config')
    It turns out that userland likes to override process.config with
    their own stuff. If we want to be able to depend on it in any way,
    we need our own internal mechanism.
    
    This adds a new private process.binding('config') that is
    intended to serve as a container for internal flags and compile
    time configs that need to be passed on to the JS layer.
    
    PR-URL: https://github.com/nodejs/node/pull/6266
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

---
+++ src/node_config.cc
@@ -0,0 +1,36 @@
+#include "node.h"
+#include "env.h"
+#include "env-inl.h"
+#include "util.h"
+#include "util-inl.h"
+
+
+namespace node {
+
+using v8::Context;
+using v8::Local;
+using v8::Object;
+using v8::Value;
+using v8::ReadOnly;
+
+// The config binding is used to provide an internal view of compile or runtime
+// config options that are required internally by lib/*.js code. This is an
+// alternative to dropping additional properties onto the process object as
+// has been the practice previously in node.cc.
+
+#define READONLY_BOOLEAN_PROPERTY(str)                                        \
+  do {                                                                        \
+    target->DefineOwnProperty(env->context(),                                 \
+                              OneByteString(env->isolate(), str),             \
+                              True(env->isolate()), ReadOnly).FromJust();     \
+  } while (0)
+
+void InitConfig(Local<Object> target,
+                Local<Value> unused,
+                Local<Context> context) {
+  // Environment* env = Environment::GetCurrent(context);
+}
+
+}  // namespace node
+
+NODE_MODULE_CONTEXT_AWARE_BUILTIN(config, node::InitConfig)
