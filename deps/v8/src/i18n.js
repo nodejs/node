6291753e38054334efccc29c198f7e5559f2981e
Michaël Zasso <targos@protonmail.com>
2016-04-19 10:00:01 +0200

    deps: backport ddb5c2d from V8's upstream
    Original commit message:
    
        Make NumberFormat use the ICU currency data, fix bug in NumberFormat
    
        NumberFormat previously just used a min of 0 digits after the decimal and a max of 3. This CL changes it so that we use the ICU currency data, and set the min and max to the number of numbers after the decimal point for each currency.
    
        This CL also fixes a small bug where if the minimum fraction digits is above 3 but the maximum fraction digits isn't set, then it returns with only three numbers after the decimal point.
    
        BUG=435465,473104,304722
        LOG=Y
    
        Review URL: https://codereview.chromium.org/1231613006
    
        Cr-Commit-Position: refs/heads/master@{#29734}

--- deps/v8/src/i18n.js
+++ deps/v8/src/i18n.js
@@ -1099,11 +1099,19 @@ function initializeNumberFormat(numberFormat, locales, options) {
   var mnid = getNumberOption(options, 'minimumIntegerDigits', 1, 21, 1);
   defineWEProperty(internalOptions, 'minimumIntegerDigits', mnid);
 
-  var mnfd = getNumberOption(options, 'minimumFractionDigits', 0, 20, 0);
-  defineWEProperty(internalOptions, 'minimumFractionDigits', mnfd);
+  var mnfd = options['minimumFractionDigits'];
+  var mxfd = options['maximumFractionDigits'];
+  if (!IS_UNDEFINED(mnfd) || !internalOptions.style === 'currency') {
+    mnfd = getNumberOption(options, 'minimumFractionDigits', 0, 20, 0);
+    defineWEProperty(internalOptions, 'minimumFractionDigits', mnfd);
+  }
 
-  var mxfd = getNumberOption(options, 'maximumFractionDigits', mnfd, 20, 3);
-  defineWEProperty(internalOptions, 'maximumFractionDigits', mxfd);
+  if (!IS_UNDEFINED(mxfd) || !internalOptions.style === 'currency') {
+    mnfd = IS_UNDEFINED(mnfd) ? 0 : mnfd;
+    fallback_limit = (mnfd > 3) ? mnfd : 3;
+    mxfd = getNumberOption(options, 'maximumFractionDigits', mnfd, 20, fallback_limit);
+    defineWEProperty(internalOptions, 'maximumFractionDigits', mxfd);
+  }
 
   var mnsd = options['minimumSignificantDigits'];
   var mxsd = options['maximumSignificantDigits'];
@@ -1157,8 +1165,6 @@ function initializeNumberFormat(numberFormat, locales, options) {
                                       internalOptions,
                                       resolved);
 
-  // We can't get information about number or currency style from ICU, so we
-  // assume user request was fulfilled.
   if (internalOptions.style === 'currency') {
     ObjectDefineProperty(resolved, 'currencyDisplay', {value: currencyDisplay,
                                                        writable: true});
