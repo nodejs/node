783cae24aa72e6c3cf7a3b2ccaf9b5537be4bd4b
Fedor Indutny <fedor@indutny.com>
2016-04-20 11:55:19 -0400

    fix test

--- test/parallel/test-net-bytes-read.js
+++ test/parallel/test-net-bytes-read.js
@@ -4,8 +4,7 @@ const common = require('../common');
 const assert = require('assert');
 const net = require('net');
 
-const big = new Buffer(1024 * 1024);
-big.fill('-');
+const big = Buffer.alloc(1024 * 1024);
 
 const server = net.createServer((socket) => {
   socket.end(big);
