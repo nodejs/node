cb6c490e64987b4fcb559784195545315d7b8b4e
Ali Ijaz Sheikh <ofrobots@google.com>
2016-04-20 17:29:08 -0700

    deps: upgrade to V8 5.0.71.34
    Pick up the latest bug fix from the V8 5.0 branch.
    
    Original commit message:
    V8-Commit: https://github.com/v8/v8/commit/c36773f
      Version 5.0.71.34 (cherry-pick)
      Merged 9acbca1
    
      [es6] Fix bug in pattern re-writing
    
      BUG=v8:4891
      LOG=N
      R=littledan@chromium.org
    
      Review URL: https://codereview.chromium.org/1906633002 .

--- deps/v8/include/v8-version.h
+++ deps/v8/include/v8-version.h
@@ -11,7 +11,7 @@
 #define V8_MAJOR_VERSION 5
 #define V8_MINOR_VERSION 0
 #define V8_BUILD_NUMBER 71
-#define V8_PATCH_LEVEL 33
+#define V8_PATCH_LEVEL 34
 
 // Use 1 for candidates and 0 otherwise.
 // (Boolean macro values are not supported by all preprocessors.)
