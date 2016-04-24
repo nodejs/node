21b4cbd977498219b7783b91e99296ac12e6c9e8
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-04-24 12:28:37 +0200

    buffer: cpp expose split / join

--- src/node_buffer.cc
+++ src/node_buffer.cc
@@ -1359,6 +1359,9 @@ void Initialize(Local<Object> target,
   env->SetMethod(target, "readFloatBE", ReadFloatBE);
   env->SetMethod(target, "readFloatLE", ReadFloatLE);
 
+  env->SetMethod(target, "split", Split);
+  env->SetMethod(target, "join", Join);
+
   env->SetMethod(target, "writeDoubleBE", WriteDoubleBE);
   env->SetMethod(target, "writeDoubleLE", WriteDoubleLE);
   env->SetMethod(target, "writeFloatBE", WriteFloatBE);
