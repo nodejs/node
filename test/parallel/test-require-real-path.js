37b7b5eeecd5602e1e504ab5bea6d545e902333d
Alexander Gugel <alexander.gugel@gmail.com>
2016-04-29 04:08:56 +0100

    module: Change precedence of _-prefixed modules
    That way stat() only needs to be called once in most cases (assuming non
    _-prefixed symlinks are more common).

--- test/parallel/test-require-real-path.js
+++ test/parallel/test-require-real-path.js
@@ -7,6 +7,9 @@ const fs = require('fs');
 const realModuleSymlinkPath = path.join(common.fixturesDir,
   '/module-require-real-path/node_modules/_real-module');
 
+const linkModuleSymlinkPath = path.join(common.fixturesDir,
+  '/module-require-real-path/node_modules/_link-module');
+
 const localRealModulePath = path.join(common.fixturesDir,
   '/module-require-real-path/real-module');
 
@@ -18,13 +21,15 @@ const _require = require(
 
 process.on('exit', function() {
   fs.unlinkSync(realModuleSymlinkPath);
+  fs.unlinkSync(linkModuleSymlinkPath);
 });
 
 fs.symlinkSync('../real-module', realModuleSymlinkPath);
+fs.symlinkSync('../real-module', linkModuleSymlinkPath);
 
 assert.equal(_require('./real-module'), _require('real-module'));
 assert.equal(_require('real-module').dirname, localRealModulePath);
 
 // When required directly with the _-prefix, resolve to path of symlink.
-assert.notEqual(_require('./real-module'), _require('_real-module'));
-assert.equal(_require('_real-module').dirname, realModuleSymlinkPath);
+assert.notEqual(_require('./real-module'), _require('_link-module'));
+assert.equal(_require('_link-module').dirname, linkModuleSymlinkPath);
