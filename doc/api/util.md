b2ebb7b4b2451b8ce3679730c3479f99a0a1ba7e
James M Snell <jasnell@gmail.com>
2016-04-29 19:24:00 -0700

    Cache the results to avoid duplicate lookups, add option

--- doc/api/util.md
+++ doc/api/util.md
@@ -179,6 +179,9 @@ formatted string:
  - `customInspect` - if `false`, then custom `inspect(depth, opts)` functions
    defined on the objects being inspected won't be called. Defaults to `true`.
 
+ - `showProxy` - if `true`, then objects that are Proxy objects will be
+   introspected to show their `target` and `hander` objects.
+
 Example of inspecting all properties of the `util` object:
 
 ```js
--- test/parallel/test-util-inspect-proxy.js
+++ test/parallel/test-util-inspect-proxy.js
@@ -12,7 +12,7 @@ const handler = {
 const proxyObj = new Proxy(target, handler);
 
 // Inspecting the proxy should not actually walk it's properties
-assert.doesNotThrow(() => util.inspect(proxyObj));
+assert.doesNotThrow(() => util.inspect(proxyObj, {showProxy: true}));
 
 // getProxyDetails is an internal method, not intended for public use.
 // This is here to test that the internals are working correctly.
@@ -20,8 +20,15 @@ const details = processUtil.getProxyDetails(proxyObj);
 assert.deepStrictEqual(target, details.target);
 assert.deepStrictEqual(handler, details.handler);
 
-assert.strictEqual(util.inspect(proxyObj),
+assert.strictEqual(util.inspect(proxyObj, {showProxy: true}),
     'Proxy { handler: { get: [Function] }, target: {} }');
 
 // Using getProxyDetails with non-proxy returns undefined
 assert.strictEqual(processUtil.getProxyDetails({}), undefined);
+
+// This will throw because the showProxy option is not used
+// and the get function on the handler object defined above
+// is actually invoked.
+assert.throws(
+  () => util.inspect(proxyObj)
+);
