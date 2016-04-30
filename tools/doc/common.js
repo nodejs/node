ea137637b78773f3902e936cc7c65469611417b8
Anna Henningsen <anna@addaleax.net>
2016-05-01 01:51:38 +0200

    tools: allow multiple added: version entries
    Allow multiple `added:` version entries, since semver-minors
    can trickle down to previous major versions, and thus
    features may have been added in multiple versions.
    
    Also include `deprecated:` entries and apply the same logic
    to them for consistency.
    
    Stylize the added HTML as `Added in:` and `Deprecated since:`.
    
    PR-URL: https://github.com/nodejs/node/pull/6495
    Reviewed-By: Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>

--- tools/doc/common.js
+++ tools/doc/common.js
@@ -8,6 +8,10 @@ function isYAMLBlock(text) {
 
 exports.isYAMLBlock = isYAMLBlock;
 
+function arrify(value) {
+  return Array.isArray(value) ? value : [value];
+}
+
 function extractAndParseYAML(text) {
   text = text.trim();
 
@@ -15,7 +19,22 @@ function extractAndParseYAML(text) {
              .replace(/-->$/, '');
 
   // js-yaml.safeLoad() throws on error
-  return yaml.safeLoad(text);
+  const meta = yaml.safeLoad(text);
+
+  const added = meta.added || meta.Added;
+  if (added) {
+    // Since semver-minors can trickle down to previous major versions,
+    // features may have been added in multiple versions.
+    meta.added = arrify(added);
+  }
+
+  const deprecated = meta.deprecated || meta.Deprecated;
+  if (deprecated) {
+    // Treat deprecated like added for consistency.
+    meta.deprecated = arrify(deprecated);
+  }
+
+  return meta;
 }
 
 exports.extractAndParseYAML = extractAndParseYAML;
