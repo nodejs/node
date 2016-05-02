12a9699fcf85a693f1d4334607b386a5bf945340
Anna Henningsen <anna@addaleax.net>
2016-05-02 05:31:48 +0200

    buffer: fix needle length misestimation for UCS2
    Use `StringBytes::Size` to determine the needle string length
    instead of assuming latin-1 or UTF-8.
    
    Previously, `Buffer.indexOf` could fail with an assertion failure
    when the needle's byte length, but not its character count,
    exceeded the haystack's byte length.
    
    PR-URL: https://github.com/nodejs/node/pull/6511
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- src/node_buffer.cc
+++ src/node_buffer.cc
@@ -827,9 +827,9 @@ void IndexOfString(const FunctionCallbackInfo<Value>& args) {
   Local<String> needle = args[1].As<String>();
   const char* haystack = ts_obj_data;
   const size_t haystack_length = ts_obj_length;
-  // Extended latin-1 characters are 2 bytes in Utf8.
+
   const size_t needle_length =
-      enc == BINARY ? needle->Length() : needle->Utf8Length();
+      StringBytes::Size(args.GetIsolate(), needle, enc);
 
 
   if (needle_length == 0 || haystack_length == 0) {
