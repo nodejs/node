8357733c3738434310644394d39619cbcc36a0b2
James M Snell <jasnell@gmail.com>
2016-05-02 16:31:20 -0700

    src,module: add --preserve-symlinks command line flag
    Add the `--preserve-symlinks` flag. This makes the changes added
    in #5950 conditional. By default the old behavior is used. With
    the flag set, symlinks are preserved, switching to the new
    behavior. This should be considered to be a temporary solution
    until we figure out how to solve the symlinked peer dependency
    problem in a more general way that does not break everything
    else.
    
    Additional test cases are included.
    
    PR-URL: https://github.com/nodejs/node/pull/6537
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node.cc
+++ src/node.cc
@@ -168,6 +168,11 @@ bool force_fips_crypto = false;
 bool no_process_warnings = false;
 bool trace_warnings = false;
 
+// Set in node.cc by ParseArgs when --preserve-symlinks is used.
+// Used in node_config.cc to set a constant on process.binding('config')
+// that is used by lib/module.js
+bool config_preserve_symlinks = false;
+
 // process-relative uptime base, initialized at start-up
 static double prog_start_time;
 static bool debugger_running;
@@ -3460,6 +3465,8 @@ static void PrintHelp() {
          "                        note: linked-in ICU data is\n"
          "                        present.\n"
 #endif
+         "  --preserve-symlinks   preserve symbolic links when resolving\n"
+         "                        and caching modules.\n"
 #endif
          "\n"
          "Environment variables:\n"
@@ -3589,6 +3596,8 @@ static void ParseArgs(int* argc,
     } else if (strncmp(arg, "--security-revert=", 18) == 0) {
       const char* cve = arg + 18;
       Revert(cve);
+    } else if (strcmp(arg, "--preserve-symlinks") == 0) {
+      config_preserve_symlinks = true;
     } else if (strcmp(arg, "--prof-process") == 0) {
       prof_process = true;
       short_circuit = true;
