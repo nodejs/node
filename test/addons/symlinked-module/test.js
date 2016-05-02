0f5d97e8d3210662d67a6f1c929c06de67309f81
Marcin Cieślak <saper@saper.info>
2016-05-02 23:46:50 +0000

    test: require symlinked binary module twice
    Attempt to load the binary module disguised
    behind two symlinked locations.
    
    Big thanks to Michael Mifsud <xzyfer@gmail.com>
    for preparing a proper test case.

---
+++ test/addons/symlinked-module/test.js
@@ -0,0 +1,32 @@
+'use strict';
+const common = require('../../common');
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+
+common.refreshTmpDir();
+
+var destDir = path.resolve(common.tmpDir);
+
+var location = [path.join(destDir, 'first'), path.join(destDir, 'second')];
+fs.mkdirSync(location[0]);
+try {
+  fs.symlinkSync(location[0], location[1]);
+} catch (EPERM) {
+  common.skip('module identity test (no privs for symlinks)');
+  return;
+}
+
+const addonPath = path.join(__dirname, 'build', 'Release', 'binding.node');
+
+var contents = fs.readFileSync(addonPath);
+fs.writeFileSync(path.join(location[0], 'binding.node'), contents);
+fs.writeFileSync(path.join(location[1], 'binding.node'), contents);
+
+var primary = require(path.join(location[0], 'binding.node'));
+assert(primary != null);
+assert(primary.hello() == 'world');
+var secondary = require(path.join(location[1], 'binding.node'));
+assert(secondary != null);
+assert(secondary.hello() == 'world');
+require('./submodule').test(location[1]);
