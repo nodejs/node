4220cb929cc0d9cc57f6d8503e1481f0275f502d
Anna Henningsen <anna@addaleax.net>
2016-05-02 03:52:46 +0200

    buffer: fix lastIndexOf crash for overlong needle
    Return -1 in `Buffer.lastIndexOf` if the needle is longer than the
    haystack. The previous check only tested the corresponding
    condition for forward searches.
    
    This applies only to Node.js v6, as `lastIndexOf` was added in it.
    
    Fixes: https://github.com/nodejs/node/issues/6510

--- src/node_buffer.cc
+++ src/node_buffer.cc
@@ -1011,7 +1011,8 @@ void IndexOfString(const FunctionCallbackInfo<Value>& args) {
   }
   size_t offset = static_cast<size_t>(opt_offset);
   CHECK_LT(offset, haystack_length);
-  if (is_forward && needle_length + offset > haystack_length) {
+  if ((is_forward && needle_length + offset > haystack_length) ||
+      needle_length > haystack_length) {
     return args.GetReturnValue().Set(-1);
   }
 
@@ -1113,7 +1114,8 @@ void IndexOfBuffer(const FunctionCallbackInfo<Value>& args) {
   }
   size_t offset = static_cast<size_t>(opt_offset);
   CHECK_LT(offset, haystack_length);
-  if (is_forward && needle_length + offset > haystack_length) {
+  if ((is_forward && needle_length + offset > haystack_length) ||
+      needle_length > haystack_length) {
     return args.GetReturnValue().Set(-1);
   }
 
