7ca31e38fbadcea8d2a32cfd6a87729e78316ef0
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2016-05-04 01:06:40 +0900

    test: fix alpn tests for openssl1.0.2h
    cherry-pick 65030c77b7cbab05f73899823288197864a04d64 from v6-staging.
    
    https://github.com/openssl/openssl/commit/af2db04c9979554ada88d969da6332a827a47599
    changed some ALPN behaviors. The tests when ALPN has no selection
    should be fixed because openssl was changed NPN callback to be invoked
    in this case.
    
    Fixes: https://github.com/nodejs/node/issues/6458
    PR-URL: https://github.com/nodejs/node/pull/6550
    Reviewed-By: Myles Borins <myles.borins@gmail.com>
    Reviewed-By: Fedor Indutny <fedor.indutny@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/parallel/test-tls-alpn-server-client.js
+++ test/parallel/test-tls-alpn-server-client.js
@@ -99,8 +99,8 @@ function Test1() {
                   client: {ALPN: 'b', NPN: undefined}});
     // nothing is selected by ALPN
     checkResults(results[2],
-                 {server: {ALPN: false, NPN: false},
-                  client: {ALPN: false, NPN: undefined}});
+                 {server: {ALPN: false, NPN: 'first-priority-unsupported'},
+                  client: {ALPN: false, NPN: false}});
     // execute next test
     Test2();
   });
@@ -132,8 +132,8 @@ function Test2() {
                   client: {ALPN: 'b', NPN: undefined}});
     // nothing is selected by ALPN
     checkResults(results[2],
-                 {server: {ALPN: false, NPN: false},
-                  client: {ALPN: false, NPN: undefined}});
+                 {server: {ALPN: false, NPN: 'http/1.1'},
+                  client: {ALPN: false, NPN: false}});
     // execute next test
     Test3();
   });
@@ -224,8 +224,9 @@ function Test5() {
     checkResults(results[1], {server: {ALPN: 'b', NPN: false},
                               client: {ALPN: 'b', NPN: undefined}});
     // nothing is selected by ALPN
-    checkResults(results[2], {server: {ALPN: false, NPN: false},
-                              client: {ALPN: false, NPN: undefined}});
+    checkResults(results[2], {server: {ALPN: false,
+                                       NPN: 'first-priority-unsupported'},
+                              client: {ALPN: false, NPN: false}});
     // execute next test
     Test6();
   });
@@ -253,8 +254,8 @@ function Test6() {
     checkResults(results[1], {server: {ALPN: 'b', NPN: false},
                               client: {ALPN: 'b', NPN: undefined}});
     // nothing is selected by ALPN
-    checkResults(results[2], {server: {ALPN: false, NPN: false},
-                              client: {ALPN: false, NPN: undefined}});
+    checkResults(results[2], {server: {ALPN: false, NPN: 'http/1.1'},
+                              client: {ALPN: false, NPN: false}});
     // execute next test
     Test7();
   });
