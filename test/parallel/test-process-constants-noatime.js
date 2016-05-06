fcfb5effa88fa4da04764b7de36f1aa1156560da
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2016-05-06 15:36:04 +0530

    change the property existence check

--- test/parallel/test-process-constants-noatime.js
+++ test/parallel/test-process-constants-noatime.js
@@ -5,8 +5,8 @@ const assert = require('assert');
 const constants = process.binding('constants');
 
 if (process.platform === 'linux') {
-  assert(constants.hasOwnProperty('O_NOATIME'));
+  assert('O_NOATIME' in constants);
   assert.strictEqual(constants.O_NOATIME, 0x40000);
 } else {
-  assert(false === constants.hasOwnProperty('O_NOATIME'));
+  assert(!('O_NOATIME' in constants));
 }
