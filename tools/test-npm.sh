390b74c8b9e68794a8e92393f6c69b4f2d204d69
Gibson Fahnestock <gib@uk.ibm.com>
2016-05-06 15:58:09 +0100

    test: make test-npm use tap output
    Pass -p/--progress and --logfile as you would for tools/test.py
    
    Add *test.tap to .gitignore
    
    Make test-npm default to using the node binary in out/Release (like
    tools/test.py). This can still be changed by setting the $NODE
    environment variable (as before).

--- tools/test-npm.sh
+++ tools/test-npm.sh
@@ -1,43 +1,71 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e
+# Handle arguments
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -p|--progress) PROGRESS="$2"; shift ;;
+    -p=*) PROGRESS="${1#-p=}" ;;
+    --progress=*) PROGRESS="${1#--progress=}" ;;
+    --logfile) LOGFILE="$2"; shift ;;
+    --logfile=*) LOGFILE="${1#--logfile=}" ;;
+    *) echo "Unknown parameters $@" && exit 1;;
+  esac
+  shift
+done
 
-# always change the working directory to the project's root directory
+# Set default progress indicator to classic
+PROGRESS=${PROGRESS:-classic}
+
+# Always change the working directory to the project's root directory
 cd $(dirname $0)/..
 
-# pass a $NODE environment variable from something like Makefile
-# it should point to either ./node or ./node.exe, depending on the platform
+# Pass a $NODE environment variable from something like Makefile
+# It should be a relative path to the node binary, e.g. ./node
 if [ -z $NODE ]; then
-  echo "No node executable provided. Bailing." >&2
-  exit 0
+  echo "No \$NODE executable provided, defaulting to out/Release/node." >&2
+  NODE=out/Release/node
 fi
+
+# Ensure npm always uses the local node
+export PATH="$PWD/`dirname $NODE`:$PATH"
+unset NODE
 
 rm -rf test-npm
 
-# make a copy of deps/npm to run the tests on
+# Make a copy of deps/npm to run the tests on
 cp -r deps/npm test-npm
 
 cd test-npm
 
-# do a rm first just in case deps/npm contained these
-rm -rf npm-cache npm-tmp npm-prefix
-mkdir npm-cache npm-tmp npm-prefix
+# Do a rm first just in case deps/npm contained these
+rm -rf npm-cache npm-tmp npm-prefix npm-userconfig npm-home
+mkdir npm-cache npm-tmp npm-prefix npm-userconfig npm-home
 
-# set some npm env variables to point to our new temporary folders
+# Set some npm env variables to point to our new temporary folders
 export npm_config_cache="$(pwd)/npm-cache"
 export npm_config_prefix="$(pwd)/npm-prefix"
 export npm_config_tmp="$(pwd)/npm-tmp"
+export npm_config_userconfig="$(pwd)/npm-userconfig"
+export HOME="$(pwd)/npm-home"
 
-# ensure npm always uses the local node
-export PATH="$(../$NODE -p 'require("path").resolve("..")'):$PATH"
-unset NODE
-
-# make sure the binaries from the non-dev-deps are available
+# Make sure the binaries from the non-dev-deps are available
 node cli.js rebuild
-# install npm devDependencies and run npm's tests
+# Install npm devDependencies and run npm's tests
 node cli.js install --ignore-scripts
-# run the tests
-node cli.js run-script test-node
 
-# clean up everything one single shot
+# Run the tests with logging if set
+if [ -n "$LOGFILE" ]; then
+  echo "node cli.js run test-node -- --reporter=$PROGRESS | tee ../$LOGFILE"
+  node cli.js run test-node -- --reporter=$PROGRESS | tee ../$LOGFILE
+else
+  echo "node cli.js run test-node -- --reporter=$PROGRESS"
+  node cli.js run test-node -- --reporter=$PROGRESS
+fi
+
+# Move npm-debug.log up a directory if it exists
+if [ -f npm-debug.log ]; then
+  mv npm-debug.log ..
+fi
+
+# Clean up everything in one single shot
 cd .. && rm -rf test-npm
