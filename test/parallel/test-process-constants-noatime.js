7d3f5751b34725985286dc028f7b35c876806202
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2016-05-06 11:45:00 +0530

    test: make sure O_NOATIME is present only in Linux
    As it is, the test checks if the return value is `undefined` in other
    platforms. But it should also make sure that the `O_NOATIME` should be
    found only in Linux.
    
    PR-URL: https://github.com/nodejs/node/pull/6614
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-process-constants-noatime.js
+++ test/parallel/test-process-constants-noatime.js
@@ -2,10 +2,11 @@
 
 require('../common');
 const assert = require('assert');
+const constants = process.binding('constants');
 
-const isLinux = process.platform === 'linux';
-
-const O_NOATIME = process.binding('constants').O_NOATIME;
-const expected = isLinux ? 0x40000 : undefined;
-
-assert.strictEqual(O_NOATIME, expected);
+if (process.platform === 'linux') {
+  assert('O_NOATIME' in constants);
+  assert.strictEqual(constants.O_NOATIME, 0x40000);
+} else {
+  assert(!('O_NOATIME' in constants));
+}
