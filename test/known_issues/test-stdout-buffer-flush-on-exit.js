0912b8832050042fdf7098e6fecfae97c71b27de
Rich Trott <rtrott@gmail.com>
2016-05-07 14:07:29 -0700

    test: make stdout buffer test more robust
    test-stdout-buffer-flush-on-exit is unfortunately non-deterministic. It
    will, every so often, pass when it is supposed to fail. This is
    currently guarded against by running the test with three different long
    strings. This change increases it to five to reduce the false negatives.
    
    PR-URL: https://github.com/nodejs/node/pull/6633
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

--- test/known_issues/test-stdout-buffer-flush-on-exit.js
+++ test/known_issues/test-stdout-buffer-flush-on-exit.js
@@ -15,7 +15,7 @@ if (process.argv[2] === 'child') {
   process.exit();
 }
 
-[16, 18, 20].forEach((exponent) => {
+[22, 21, 20, 19, 18, 17, 16, 16, 17, 18, 19, 20, 21, 22].forEach((exponent) => {
   const bigNum = Math.pow(2, exponent);
   const longLine = lineSeed.repeat(bigNum);
   const cmd = `${process.execPath} ${__filename} child ${exponent} ${bigNum}`;
