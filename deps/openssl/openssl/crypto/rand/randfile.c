c9b2baa3e38d9cce05bf5ce02dae0e5e8f9a3dff
Alexander Pyhalov <apyhalov@gmail.com>
2016-05-13 09:15:25 +0300

    deps: fix openssl compilation with gcc 5.3 on illumos

--- deps/openssl/openssl/crypto/rand/randfile.c
+++ deps/openssl/openssl/crypto/rand/randfile.c
@@ -57,7 +57,7 @@
  */
 
 /* We need to define this to get macros like S_IFBLK and S_IFCHR */
-#if !defined(OPENSSL_SYS_VXWORKS)
+#if !defined(OPENSSL_SYS_VXWORKS) && !defined(__sun__)
 # define _XOPEN_SOURCE 500
 #endif
 
