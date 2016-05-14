b732f2bba09bfda7df7ecc9163d42405e01622b0
suryagh <surya.com@gmail.com>
2016-05-14 15:08:12 -0400

    test: added tests for https-agent-getname
    PR-URL: https://github.com/nodejs/node/pull/6762
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Brian White <mscdex@mscdex.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Fedor Indutny <fedor.indutny@gmail.com>
    Reviewed-By: Johan Bergström <bugs@bergstroem.nu>

---
+++ test/parallel/test-https-agent-getname.js
@@ -0,0 +1,32 @@
+'use strict';
+
+require('../common');
+const assert = require('assert');
+const https = require('https');
+
+const agent = new https.Agent();
+
+// empty options
+assert.strictEqual(
+  agent.getName({}),
+  'localhost:::::::::'
+);
+
+// pass all options arguments
+const options = {
+  host: '0.0.0.0',
+  port: 443,
+  localAddress: '192.168.1.1',
+  ca: 'ca',
+  cert: 'cert',
+  ciphers: 'ciphers',
+  key: 'key',
+  pfx: 'pfx',
+  rejectUnauthorized: false,
+  servername: 'localhost',
+};
+
+assert.strictEqual(
+  agent.getName(options),
+  '0.0.0.0:443:192.168.1.1:ca:cert:ciphers:key:pfx:false:localhost'
+);
