4bd410bbbe2863da0baa36b9373d6b1491773102
Anna Henningsen <anna@addaleax.net>
2016-05-20 22:55:37 +0200

    vm: don't abort process when stack space runs out
    Make less assumptions about what objects will be available when
    vm context creation or error message printing fail because V8
    runs out of JS stack space.
    
    Ref: https://github.com/nodejs/node/issues/6899
    PR-URL: https://github.com/nodejs/node/pull/6907
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node.cc
+++ src/node.cc
@@ -1521,8 +1521,8 @@ void AppendExceptionLine(Environment* env,
   // sourceline to 78 characters, and we end up not providing very much
   // useful debugging info to the user if we remove 62 characters.
 
-  int start = message->GetStartColumn(env->context()).FromJust();
-  int end = message->GetEndColumn(env->context()).FromJust();
+  int start = message->GetStartColumn(env->context()).FromMaybe(0);
+  int end = message->GetEndColumn(env->context()).FromMaybe(0);
 
   char arrow[1024];
   int max_off = sizeof(arrow) - 2;
