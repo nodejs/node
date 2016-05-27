44228dfdef9d50bfe77ed0abfad5e322315cc918
Rich Trott <rtrott@gmail.com>
2016-05-26 20:49:51 -0700

    test: remove `common.PORT` from gc tests
    Allow the operating system to provide an arbitrary available port rather
    than using `common.PORT`, as `common.PORT` makes it likely that a test
    will fail with `EADDRINUSE` as a side effect of an earlier test.
    
    PR-URL: https://github.com/nodejs/node/pull/7013
    Reviewed-By: Brian White <mscdex@mscdex.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/gc/test-http-client-timeout.js
+++ test/gc/test-http-client-timeout.js
@@ -12,9 +12,8 @@ function serverHandler(req, res) {
 
 const http = require('http');
 const weak = require('weak');
-const common = require('../common');
+require('../common');
 const assert = require('assert');
-const PORT = common.PORT;
 const todo = 550;
 let done = 0;
 let count = 0;
@@ -23,7 +22,7 @@ let countGC = 0;
 console.log('We should do ' + todo + ' requests');
 
 var server = http.createServer(serverHandler);
-server.listen(PORT, getall);
+server.listen(0, getall);
 
 function getall() {
   if (count >= todo)
@@ -39,7 +38,7 @@ function getall() {
     var req = http.get({
       hostname: 'localhost',
       pathname: '/',
-      port: PORT
+      port: server.address().port
     }, cb);
     req.on('error', cb);
     req.setTimeout(10, function() {
