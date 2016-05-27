30a0d6288d685ac9d0dbb274f011dc65363ea261
Anna Henningsen <anna@addaleax.net>
2016-05-28 00:10:28 +0200

    deps: float gyp patch for long filenames
    Pulling in https://codereview.chromium.org/2019133002/ in its current
    state, as gyp seems to be largely abandoned as a project.
    
    Original commit message:
    
        Hash intermediate file name to avoid ENAMETOOLONG
    
        Hash the intermediate Makefile target used for multi-output rules
        so that it still works when the involved file names are very long.
    
        Since the intermediate file's name is effectively arbitrary, this
        does not come with notable behavioural changes.
    
        The `import hashlib` boilerplate is taken directly
        from `xcodeproj_file.py`.
    
    Concretely, this makes the V8 inspector build currently fail when long
    pathnames are involved, notably when using ecryptfs which has a lower
    file name length limit.
    
    Fixes: https://github.com/nodejs/node/issues/7959
    Ref: https://github.com/nodejs/node/issues/7510

--- tools/gyp/pylib/gyp/generator/make.py
+++ tools/gyp/pylib/gyp/generator/make.py
@@ -31,6 +31,17 @@ import gyp.xcode_emulation
 from gyp.common import GetEnvironFallback
 from gyp.common import GypError
 
+# hashlib is supplied as of Python 2.5 as the replacement interface for sha
+# and other secure hashes.  In 2.6, sha is deprecated.  Import hashlib if
+# available, avoiding a deprecation warning under 2.6.  Import sha otherwise,
+# preserving 2.4 compatibility.
+try:
+  import hashlib
+  _new_sha1 = hashlib.sha1
+except ImportError:
+  import sha
+  _new_sha1 = sha.new
+
 generator_default_variables = {
   'EXECUTABLE_PREFIX': '',
   'EXECUTABLE_SUFFIX': '',
@@ -1743,7 +1754,10 @@ $(obj).$(TOOLSET)/$(TARGET)/%%.o: $(obj)/%%%s FORCE_DO_CMD
       #   actual command.
       # - The intermediate recipe will 'touch' the intermediate file.
       # - The multi-output rule will have an do-nothing recipe.
-      intermediate = "%s.intermediate" % (command if command else self.target)
+
+      # Hash the target name to avoid generating overlong filenames.
+      cmddigest = _new_sha1(command if command else self.target).hexdigest()
+      intermediate = "%s.intermediate" % (cmddigest)
       self.WriteLn('%s: %s' % (' '.join(outputs), intermediate))
       self.WriteLn('\t%s' % '@:');
       self.WriteLn('%s: %s' % ('.INTERMEDIATE', intermediate))
