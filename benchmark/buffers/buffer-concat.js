e62b30cf068ef84d71eae7cd948b5a8642a1066c
Anna Henningsen <anna@addaleax.net>
2016-05-29 22:19:02 +0200

    benchmark: add benchmark for Buffer.concat

---
+++ benchmark/buffers/buffer-concat.js
@@ -0,0 +1,23 @@
+'use strict';
+const common = require('../common.js');
+
+const bench = common.createBenchmark(main, {
+  pieces: [1, 4, 16],
+  pieceSize: [1, 16, 256],
+  n: [1024]
+});
+
+function main(conf) {
+  const n = +conf.n;
+  const size = +conf.pieceSize;
+  const pieces = +conf.pieces;
+
+  const list = new Array(pieces);
+  list.fill(Buffer.allocUnsafe(size));
+
+  bench.start();
+  for (var i = 0; i < n * 1024; i++) {
+    Buffer.concat(list);
+  }
+  bench.end(n);
+}
