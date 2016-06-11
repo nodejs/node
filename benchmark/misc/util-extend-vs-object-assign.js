100a8be9906558ef921151813199c5cb94bd464d
surya panikkal <surya.com@gmail.com>
2016-06-10 20:32:20 -0400

    benchmark: review comments

--- benchmark/misc/util-extend-vs-object-assign.js
+++ benchmark/misc/util-extend-vs-object-assign.js
@@ -5,21 +5,19 @@ const util = require('util');
 const v8 = require('v8');
 
 const bench = common.createBenchmark(main, {
-  type: ['util._extend', 'Object.assign',
-         'util._extend', 'Object.assign'],
+  type: ['extend', 'assign'],
   n: [10e4]
 });
 
-function main(params) {
+function main(conf) {
   let fn;
-  const n = params.n | 0;
+  const n = conf.n | 0;
   let v8command;
 
-  if (params.type == 'util._extend') {
+  if (conf.type === 'extend') {
     fn = util._extend;
     v8command = '%OptimizeFunctionOnNextCall(util._extend)';
-
-  } else if (params.type == 'Object.assign') {
+  } else if (conf.type === 'assign') {
     fn = Object.assign;
     //Object.assign is built-in, cannot be optimized
     v8command = '';
@@ -27,14 +25,16 @@ function main(params) {
 
   // Force-optimize the method to test so that the benchmark doesn't
   // get disrupted by the optimizer kicking in halfway through.
-  for (let i = 0; i < params.type.length * 10; i += 1)
+  for (var i = 0; i < conf.type.length * 10; i += 1)
     fn({}, process.env);
 
   v8.setFlagsFromString('--allow_natives_syntax');
   eval(v8command);
 
+  var obj = {};
+
   bench.start();
-  for (let i = 0; i < n; i += 1)
-    fn({}, process.env);
+  for (var j = 0; j < n; j += 1)
+    fn(obj, process.env);
   bench.end(n);
 }
