f19e968343d136d7b1ce72c7519653f432aed485
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-06-12 13:14:34 +0200

    src: move and update comment about 'node.js' compilation.
    Currently the comment regarding execution of src/node.js seems to refer
    to the file before the refactorings done in ec6af31. Also, it looks like
    the comment itself might have "drifted" a little in the file.
    
    Updated the comment to refer to the lib/internal/bootstrap_node.js file,
    moved it closer to the compilation step, and updated the name that is
    generated in node_natives.h.

--- src/node.cc
+++ src/node.cc
@@ -3247,9 +3247,6 @@ void LoadEnvironment(Environment* env) {
   env->isolate()->SetFatalErrorHandler(node::OnFatalError);
   env->isolate()->AddMessageListener(OnMessage);
 
-  // Compile, execute the src/node.js file. (Which was included as static C
-  // string in node_natives.h. 'native_node' is the string containing that
-  // source code.)
 
   // The node.js file returns a function 'f'
   atexit(AtExit);
@@ -3261,6 +3258,9 @@ void LoadEnvironment(Environment* env) {
   // are not safe to ignore.
   try_catch.SetVerbose(false);
 
+  // Compile, execute the lib/internal/bootstrap_node.js file. (Which was
+  // included as static C string in node_natives.h.
+  // 'internal_bootstrap_node_native is the string containing that source code.)
   Local<String> script_name = FIXED_ONE_BYTE_STRING(env->isolate(), "node.js");
   Local<Value> f_value = ExecuteString(env, MainSource(env), script_name);
   if (try_catch.HasCaught())  {
