74f42352ad7c045fa0201b82b14ad20b62348661
Myles Borins <myles.borins@gmail.com>
2016-06-22 05:21:16 -0700

    deps: cherry-pick 7a88ff3 from V8 upstream
    Original commit message:
      Filter out stale left-trimmed handles for scavenges
    
      The missing part from
        https://codereview.chromium.org/2078403002/
    
      R=jochen@chromium.org
      BUG=chromium:621869
      LOG=N
    
      Review-Url: https://codereview.chromium.org/2077353004
      Cr-Commit-Position: refs/heads/master@{#37184}

--- deps/v8/src/heap/scavenger.cc
+++ deps/v8/src/heap/scavenger.cc
@@ -462,6 +462,9 @@ void ScavengeVisitor::VisitPointers(Object** start, Object** end) {
 void ScavengeVisitor::ScavengePointer(Object** p) {
   Object* object = *p;
   if (!heap_->InNewSpace(object)) return;
+
+  if (heap_->PurgeLeftTrimmedObject(p)) return;
+
   Scavenger::ScavengeObject(reinterpret_cast<HeapObject**>(p),
                             reinterpret_cast<HeapObject*>(object));
 }
