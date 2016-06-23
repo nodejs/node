3c23f12cc6262d6ba40ca3eb972fb7d4daec9e31
Rich Trott <rtrott@gmail.com>
2016-06-22 22:18:51 -0700

    squash: test update

--- test/parallel/test-child-process-exec-data-buffer.js
+++ test/parallel/test-child-process-exec-data-buffer.js
@@ -3,15 +3,11 @@ const common = require('../common');
 const assert = require('assert');
 const exec = require('child_process').exec;
 
-const keepAlive = setInterval(() => {}, 9999);
 var cbCalls = 0;
 const expectedCalls = 2;
 
 const cb = common.mustCall((data) => {
   assert(data instanceof Buffer);
-  if (++cbCalls === expectedCalls) {
-    clearInterval(keepAlive);
-  }
 }, expectedCalls);
 
 const command = common.isWindows ? 'dir' : 'ls';
