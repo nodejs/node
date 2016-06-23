d46fe32e612bed334c7e37620f03e312e4a732fd
Rich Trott <rtrott@gmail.com>
2016-06-22 22:16:21 -0700

    squash: address nits, add test for stderr

--- test/known_issues/test-child-process-exec-stdout-data-string.js
+++ test/known_issues/test-child-process-exec-stdout-data-string.js
@@ -4,12 +4,19 @@ const common = require('../common');
 const assert = require('assert');
 const exec = require('child_process').exec;
 
-const keepAlive = setInterval(() => {}, 9999);
+// const keepAlive = setInterval(() => {}, 9999);
+var cbCalls = 0;
+const expectedCalls = 2;
+
+const cb = common.mustCall((data) => {
+  console.log('foo');
+  assert(data instanceof Buffer);
+  // if (++cbCalls === expectedCalls) {
+  //   clearInterval(keepAlive);
+  // }
+}, expectedCalls);
 
 const command = common.isWindows ? 'dir' : 'ls';
-const e = exec(command);
+exec(command).stdout.on('data', cb);
 
-e.stdout.on('data', function(data) {
-  assert.strictEqual(typeof data, 'string');
-  clearInterval(keepAlive);
-});
+exec('fhqwhgads').stderr.on('data', cb);
