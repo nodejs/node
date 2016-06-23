2fe26f0c6f329ee8390a218ad44d0d5186d5ae3d
Rich Trott <rtrott@gmail.com>
2016-06-22 22:24:23 -0700

    squash: fix lint

--- test/parallel/test-child-process-exec-data-buffer.js
+++ test/parallel/test-child-process-exec-data-buffer.js
@@ -3,7 +3,6 @@ const common = require('../common');
 const assert = require('assert');
 const exec = require('child_process').exec;
 
-var cbCalls = 0;
 const expectedCalls = 2;
 
 const cb = common.mustCall((data) => {
