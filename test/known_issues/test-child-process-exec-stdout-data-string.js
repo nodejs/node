8141cfa19b6dc3e24f929435fd40ce968698fa6b
Rich Trott <rtrott@gmail.com>
2016-06-22 22:23:34 -0700

    squash: lint

--- test/known_issues/test-child-process-exec-stdout-data-string.js
+++ test/known_issues/test-child-process-exec-stdout-data-string.js
@@ -4,7 +4,6 @@ const common = require('../common');
 const assert = require('assert');
 const exec = require('child_process').exec;
 
-var cbCalls = 0;
 const expectedCalls = 2;
 
 const cb = common.mustCall((data) => {
