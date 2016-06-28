ba20ae3b20ebb7c95c0ac5adf4964bc927942256
Ben Noordhuis <info@bnoordhuis.nl>
2016-06-28 21:21:21 +0200

    tools: disable unwanted cpplint rules again
    This commit disables the build/include, build/include_alpha,
    build/include_order and legal/copyright warnings again.
    
    PR-URL: https://github.com/nodejs/node/pull/7462
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- tools/cpplint.py
+++ tools/cpplint.py
@@ -254,7 +254,12 @@ _LEGACY_ERROR_CATEGORIES = [
 # flag. By default all errors are on, so only add here categories that should be
 # off by default (i.e., categories that must be enabled by the --filter= flags).
 # All entries here should start with a '-' or '+', as in the --filter= flag.
-_DEFAULT_FILTERS = ['-build/include_alpha']
+_DEFAULT_FILTERS = [
+    '-build/include',
+    '-build/include_alpha',
+    '-build/include_order',
+    '-legal/copyright',
+    ]
 
 # The default list of categories suppressed for C (not C++) files.
 _DEFAULT_C_SUPPRESSED_CATEGORIES = [
