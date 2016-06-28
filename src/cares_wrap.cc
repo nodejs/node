e5c5a654c816a48612fa73e30014a90dd343e7de
Ben Noordhuis <info@bnoordhuis.nl>
2016-06-28 21:21:21 +0200

    src: fix readability/inheritance cpplint warnings
    PR-URL: https://github.com/nodejs/node/pull/7462
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- src/cares_wrap.cc
+++ src/cares_wrap.cc
@@ -257,7 +257,7 @@ class QueryWrap : public AsyncWrap {
       req_wrap_obj->Set(env->domain_string(), env->domain_array()->Get(0));
   }
 
-  virtual ~QueryWrap() override {
+  ~QueryWrap() override {
     CHECK_EQ(false, persistent().IsEmpty());
     ClearWrap(object());
     persistent().Reset();
