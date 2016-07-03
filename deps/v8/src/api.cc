483aed38aae33fd0afa009dc2ccce10bf3d50715
Anna Henningsen <anna@addaleax.net>
2016-07-03 15:51:47 +0200

    deps: remove extra field from v8::HeapStatistics
    Remove the `_malloced_memory` field from the `HeapStatistics`
    class to achieve full ABI compatibility with V8 5.0.
    
    Ref: https://github.com/nodejs/node/pull/7016
    PR-URL: https://github.com/nodejs/node/pull/7526
    Reviewed-By: Fedor Indutny <fedor.indutny@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- deps/v8/src/api.cc
+++ deps/v8/src/api.cc
@@ -5526,8 +5526,7 @@ HeapStatistics::HeapStatistics()
       total_available_size_(0),
       used_heap_size_(0),
       heap_size_limit_(0),
-      does_zap_garbage_(0),
-      malloced_memory_(0) {}
+      does_zap_garbage_(0) {}
 
 HeapSpaceStatistics::HeapSpaceStatistics(): space_name_(0),
                                             space_size_(0),
@@ -7428,8 +7427,6 @@ void Isolate::GetHeapStatistics(HeapStatistics* heap_statistics) {
   heap_statistics->total_available_size_ = heap->Available();
   heap_statistics->used_heap_size_ = heap->SizeOfObjects();
   heap_statistics->heap_size_limit_ = heap->MaxReserved();
-  heap_statistics->malloced_memory_ =
-      isolate->allocator()->GetCurrentMemoryUsage();
   heap_statistics->does_zap_garbage_ = heap->ShouldZapGarbage();
 }
 
