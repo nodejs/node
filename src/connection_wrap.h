303f4102d309dd17c894a796cd4ce06157275476
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-07-04 19:20:33 +0200

    src: pull OnConnection from pipe_wrap and tcp_wrap
    One of the issues in #4641 concerns OnConnection in pipe_wrap and
    tcp_wrap which are very similar with some minor difference in how
    they are coded. This commit extracts OnConnection so both these
    classes can share the same implementation.
    
    PR-URL: https://github.com/nodejs/node/pull/7547
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

---
+++ src/connection_wrap.h
@@ -0,0 +1,36 @@
+#ifndef SRC_CONNECTION_WRAP_H_
+#define SRC_CONNECTION_WRAP_H_
+
+#if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
+
+#include "env.h"
+#include "stream_wrap.h"
+#include "v8.h"
+
+namespace node {
+
+template <typename WrapType, typename UVType>
+class ConnectionWrap : public StreamWrap {
+ public:
+  UVType* UVHandle() {
+    return &handle_;
+  }
+
+  static void OnConnection(uv_stream_t* handle, int status);
+
+ protected:
+  ConnectionWrap(Environment* env,
+                 v8::Local<v8::Object> object,
+                 ProviderType provider,
+                 AsyncWrap* parent);
+  ~ConnectionWrap() = default;
+
+  UVType handle_;
+};
+
+
+}  // namespace node
+
+#endif  // defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
+
+#endif  // SRC_CONNECTION_WRAP_H_
