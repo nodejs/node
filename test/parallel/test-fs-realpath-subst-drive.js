288cd95c63e8565e9c4c76040687c47f808b12a9
Bartosz Sosnowski <bartosz@janeasystems.com>
2016-07-06 13:00:37 +0200

    win, fs: fix realpath behavior on substed drives
    Before v6 on drives created with subst and network mapped drives
    realpath used to return filename on the mapped drive. With v6 it now
    returns filename on the original device or on the network share. This
    restores the old behavior by using old javascript implementation when
    path returned by new implementation is on a different device than the
    original path.
    
    Fixes: 7294

---
+++ test/parallel/test-fs-realpath-subst-drive.js
@@ -0,0 +1,58 @@
+const common = require('../common');
+const assert = require('assert');
+const fs = require('fs');
+const spawnSync = require('child_process').spawnSync;
+
+if (!common.isWindows) {
+  common.skip("Test for Windows only");
+  return;
+}
+let result;
+
+// create a subst drive
+const driveLetters = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+let driveLetter;
+for (var i = 0; i < driveLetters.length; ++i) {
+  driveLetter = `${driveLetters[i]}:`
+   result = spawnSync('subst', [driveLetter, common.fixturesDir]);
+  if (result.status === 0)
+    break;
+}
+if (i === driveLetters.length) {
+  common.skip("Cannot create subst drive");
+  return;
+}
+
+var asyncCompleted = 0;
+// schedule cleanup (and check if all callbacks where called)
+process.on('exit', function() {
+    spawnSync('subst', ['/d', driveLetter]);
+    assert.equal(asyncCompleted, 2);
+});
+
+
+// test:
+const filename = `${driveLetter}\\empty.js`;
+const filenameBuffer = Buffer.from(filename);
+
+result = fs.realpathSync(filename);
+assert.equal(typeof result, 'string');
+assert.equal(result, filename);
+
+result = fs.realpathSync(filename, 'buffer');
+assert(Buffer.isBuffer(result));
+assert(result.equals(filenameBuffer));
+
+fs.realpath(filename, function(err, result) {
+  ++asyncCompleted;
+  assert(!err);
+  assert.equal(typeof result, 'string');
+  assert.equal(result, filename);
+});
+
+fs.realpath(filename, 'buffer', function(err, result) {
+  ++asyncCompleted;
+  assert(!err);
+  assert(Buffer.isBuffer(result));
+  assert(result.equals(filenameBuffer));
+});
