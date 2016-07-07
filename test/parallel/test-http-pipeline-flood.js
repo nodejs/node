820ad4d67bfd8e5f38080860a7c2a9555f4f48e8
Rich Trott <rtrott@gmail.com>
2016-07-07 15:30:50 -0700

    test: remove unused vars from http/https tests
    Remove handful of variables that are assigned but never used.
    
    (This was missed by the linter in previous versions of ESLint but is
    flagged by the current version. Updating the linter is contingent on
    this change or some similar remedy landing.)
    
    PR-URL: https://github.com/nodejs/node/pull/7598
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Michaël Zasso <mic.besace@gmail.com>

--- test/parallel/test-http-pipeline-flood.js
+++ test/parallel/test-http-pipeline-flood.js
@@ -27,12 +27,10 @@ switch (process.argv[2]) {
 function parent() {
   const http = require('http');
   const bigResponse = new Buffer(10240).fill('x');
-  var requests = 0;
   var connections = 0;
   var backloggedReqs = 0;
 
   const server = http.createServer(function(req, res) {
-    requests++;
     res.setHeader('content-length', bigResponse.length);
     if (!res.write(bigResponse)) {
       if (backloggedReqs === 0) {
