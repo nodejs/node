db8d80928d3983f6ada91102a76626589d33f2ab
Prince J Wesley <princejohnwesley@gmail.com>
2016-07-09 05:21:12 +0530

    Add absolute issue URL

--- test/parallel/test-repl.js
+++ test/parallel/test-repl.js
@@ -328,7 +328,7 @@ function error_test() {
     // or block comment. https://github.com/nodejs/node/issues/3611
     { client: client_unix, send: 'a = 3.5e',
       expect: /^SyntaxError: Invalid or unexpected token/ },
-    // Mitigate #548 issue
+    // Mitigate https://github.com/nodejs/node/issues/548
     { client: client_unix, send: 'function name(){ return "node"; };name()',
       expect: "'node'\n" + prompt_unix },
     { client: client_unix, send: 'function name(){ return "nodejs"; };name()',
