f7ae05a495a2d38bd6b4080b99e8357e3bb31942
Rich Trott <rtrott@gmail.com>
2016-07-07 20:14:51 -0700

    test: fix flaky test-*-connect-address-family
    Skip tests if localhost does not resolve to ::1.
    
    Fixes: https://github.com/nodejs/node/issues/7288
    PR-URL: https://github.com/nodejs/node/pull/7605
    Reviewed-By: Rod Vagg <rod@vagg.org>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

--- test/parallel/test-tls-connect-address-family.js
+++ test/parallel/test-tls-connect-address-family.js
@@ -5,28 +5,41 @@ if (!common.hasCrypto) {
   return;
 }
 
-const assert = require('assert');
-const tls = require('tls');
-
 if (!common.hasIPv6) {
   common.skip('no IPv6 support');
   return;
 }
 
-const ciphers = 'AECDH-NULL-SHA';
-tls.createServer({ ciphers }, function() {
-  this.close();
-}).listen(common.PORT, '::1', function() {
-  const options = {
-    host: 'localhost',
-    port: common.PORT,
-    family: 6,
-    ciphers: ciphers,
-    rejectUnauthorized: false,
-  };
-  // Will fail with ECONNREFUSED if the address family is not honored.
-  tls.connect(options).once('secureConnect', common.mustCall(function() {
-    assert.strictEqual('::1', this.remoteAddress);
-    this.destroy();
+const assert = require('assert');
+const tls = require('tls');
+const dns = require('dns');
+
+function runTest() {
+  const ciphers = 'AECDH-NULL-SHA';
+  tls.createServer({ ciphers }, common.mustCall(function() {
+    this.close();
+  })).listen(common.PORT, '::1', common.mustCall(function() {
+    const options = {
+      host: 'localhost',
+      port: common.PORT,
+      family: 6,
+      ciphers: ciphers,
+      rejectUnauthorized: false,
+    };
+    // Will fail with ECONNREFUSED if the address family is not honored.
+    tls.connect(options).once('secureConnect', common.mustCall(function() {
+      assert.strictEqual('::1', this.remoteAddress);
+      this.destroy();
+    }));
   }));
+}
+
+dns.lookup('localhost', {family: 6, all: true}, (err, addresses) => {
+  if (err)
+    throw err;
+
+  if (addresses.some((val) => val.address === '::1'))
+    runTest();
+  else
+    common.skip('localhost does not resolve to ::1');
 });
