d11f4752d8a8d45a01e52bb2b7e4b4fa39ed1e20
Rich Trott <rtrott@gmail.com>
2016-07-12 16:31:46 -0700

    test: improve error message in test-tick-processor
    Provide additional information about values that indicate test failed.

--- test/parallel/test-tick-processor.js
+++ test/parallel/test-tick-processor.js
@@ -1,8 +1,8 @@
 'use strict';
-var fs = require('fs');
-var assert = require('assert');
-var cp = require('child_process');
-var common = require('../common');
+const fs = require('fs');
+const assert = require('assert');
+const cp = require('child_process');
+const common = require('../common');
 
 // TODO(mhdawson) Currently the test-tick-processor functionality in V8
 // depends on addresses being smaller than a full 64 bits.  Aix supports
@@ -49,12 +49,12 @@ function runTest(pattern, code) {
     return /^isolate-/.test(file);
   });
   if (matches.length != 1) {
-    assert.fail(null, null, 'There should be a single log file.');
+    common.fail('There should be a single log file.');
   }
   var log = matches[0];
   var out = cp.execSync(process.execPath +
                         ' --prof-process --call-graph-size=10 ' + log,
                         {encoding: 'utf8'});
-  assert(pattern.test(out));
+  assert(pattern.test(out), `${pattern} not matching ${out}`);
   fs.unlinkSync(log);
 }
