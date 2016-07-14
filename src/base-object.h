e8ed621525fb66c233e4aca882904c2d77b0d962
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-07-14 18:10:12 +0200

    Renaming objecthandle_ to persistent_handle_

--- src/base-object.h
+++ src/base-object.h
@@ -44,7 +44,7 @@ class BaseObject {
   static inline void WeakCallback(
       const v8::WeakCallbackInfo<Type>& data);
 
-  v8::Persistent<v8::Object> objecthandle_;
+  v8::Persistent<v8::Object> persistent_handle_;
   Environment* env_;
 };
 
