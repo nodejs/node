376ea476de7bcbd5d2fd4d27152edc0c47f9acb7
Vse Mozhet Byt <vsemozhetbyt@gmail.com>
2016-07-15 18:55:42 +0300

    Use common.fixturesDir, add some more assertions

--- test/parallel/test-stream-preprocess.js
+++ test/parallel/test-stream-preprocess.js
@@ -10,7 +10,7 @@ const BOM = '\uFEFF';
 
 // get the data by a non-stream way to compare with the streamed data
 const modelData = fs.readFileSync(
-  path.join(__dirname, '..', 'fixtures', 'file-to-read-without-bom.txt'), 'utf8'
+  path.join(common.fixturesDir, 'file-to-read-without-bom.txt'), 'utf8'
 );
 const modelDataFirstCharacter = modelData[0];
 
@@ -20,21 +20,22 @@ const linesNumber = modelData.match(/\n/g).length;
 // ensure both without-bom and with-bom test files are textwise equal
 assert.strictEqual(
   fs.readFileSync(
-    path.join(__dirname, '..', 'fixtures', 'file-to-read-with-bom.txt'), 'utf8'
+    path.join(common.fixturesDir, 'file-to-read-with-bom.txt'), 'utf8'
   ),
   `${BOM}${modelData}`
 );
 
 // an unjustified BOM stripping with a non-BOM character unshifted to a stream
 const inputWithoutBOM = fs.createReadStream(
-  path.join(__dirname, '..', 'fixtures', 'file-to-read-without-bom.txt'), 'utf8'
+  path.join(common.fixturesDir, 'file-to-read-without-bom.txt'), 'utf8'
 );
 
 inputWithoutBOM.once('readable', common.mustCall(() => {
   const maybeBOM = inputWithoutBOM.read(1);
   assert.strictEqual(maybeBOM, modelDataFirstCharacter);
 
-  if (maybeBOM !== BOM) inputWithoutBOM.unshift(maybeBOM);
+  assert.notStrictEqual(maybeBOM, BOM);
+  inputWithoutBOM.unshift(maybeBOM);
 
   let streamedData = '';
   rl.createInterface({
@@ -48,14 +49,14 @@ inputWithoutBOM.once('readable', common.mustCall(() => {
 
 // a justified BOM stripping
 const inputWithBOM = fs.createReadStream(
-  path.join(__dirname, '..', 'fixtures', 'file-to-read-with-bom.txt'), 'utf8'
+  path.join(common.fixturesDir, 'file-to-read-with-bom.txt'), 'utf8'
 );
 
 inputWithBOM.once('readable', common.mustCall(() => {
   const maybeBOM = inputWithBOM.read(1);
   assert.strictEqual(maybeBOM, BOM);
 
-  if (maybeBOM !== BOM) inputWithBOM.unshift(maybeBOM);
+  assert.strictEqual(maybeBOM, BOM);
 
   let streamedData = '';
   rl.createInterface({
