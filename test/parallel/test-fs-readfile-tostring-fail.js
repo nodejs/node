7fb4794e191931c776327cc8cac1f80af81b7142
Rich Trott <rtrott@gmail.com>
2016-07-17 09:33:35 -0700

    test: increase RAM requirement for intensive tests
    test-fs-read-buffer-tostring-fail and test-fs-readfile-tostring-fail
    have been timing out on Raspberry Pi 3 devices on the continuous
    integration server. These devices have 1 Gb of RAM and the tests are
    memory intensive. Previous checks for memory intensive tests used a 512
    Mb cut-off, but that was probably instituted when we only had Pi 1
    devices.
    
    Consequently, this change increases the threshold for memory-intensive
    tests to 1 Gb and adds that threshold to test-fs-readfile-tostring-fail.
    
    PR-URL: https://github.com/nodejs/node/pull/7772
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/parallel/test-fs-readfile-tostring-fail.js
+++ test/parallel/test-fs-readfile-tostring-fail.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const common = require('../common');
+
+if (!common.enoughTestMem) {
+  const skipMessage = 'intensive toString tests due to memory confinements';
+  common.skip(skipMessage);
+  return;
+}
+
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
