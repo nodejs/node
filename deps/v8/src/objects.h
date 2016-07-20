b6692bafb8604b4d60519c22106dca01c5553ddd
Matt Loring <mattloring@google.com>
2016-07-20 13:37:45 -0700

    deps: backport a76d133 from v8 upstream
    Original commit message:
    
      Fix incorrect parameter to HasSufficientCapacity
    
      It takes the number of additional elements, not the total target
      capacity.
    
      Also, avoid right-shifting a negative integer as this is undefined in
      general
    
      BUG=v8:4909
      R=verwaest@chromium.org
    
    Review-Url: https://codereview.chromium.org/2162333002
    Cr-Commit-Position: refs/heads/master@{#37901}
    
    Fixes: https://github.com/nodejs/node/issues/6180
    Ref: https://github.com/nodejs/node/pull/7883
    PR-URL: https://github.com/nodejs/node/pull/7689
    Reviewed-By: Matt Loring <mattloring@google.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- deps/v8/src/objects.h
+++ deps/v8/src/objects.h
@@ -3114,6 +3114,9 @@ class HashTable : public HashTableBase {
       Key key,
       PretenureFlag pretenure = NOT_TENURED);
 
+  // Returns true if this table has sufficient capacity for adding n elements.
+  bool HasSufficientCapacityToAdd(int number_of_additional_elements);
+
   // Sets the capacity of the hash table.
   void SetCapacity(int capacity) {
     // To scale a computed hash code to fit within the hash table, we
