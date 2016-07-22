c22d90f5121c25e60a7fbec4f5042c5c9fd56bfc
cjihrig <cjihrig@gmail.com>
2016-07-22 12:05:27 -0400

    cluster: support stdio option for workers
    This commit allows setupMaster() to configure the stdio channels
    for worker processes.
    
    Refs: https://github.com/nodejs/node-v0.x-archive/issues/5727
    Refs: https://github.com/nodejs/node/pull/7811
    PR-URL: https://github.com/nodejs/node/pull/7838
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/parallel/test-cluster-fork-stdio.js
@@ -0,0 +1,40 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const cluster = require('cluster');
+const net = require('net');
+
+if (cluster.isMaster) {
+  const buf = Buffer.from('foobar');
+
+  cluster.setupMaster({
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc', 'pipe']
+  });
+
+  const worker = cluster.fork();
+  const channel = worker.process.stdio[4];
+  let response = '';
+
+  worker.on('exit', common.mustCall((code, signal) => {
+    assert.strictEqual(code, 0);
+    assert.strictEqual(signal, null);
+  }));
+
+  channel.setEncoding('utf8');
+  channel.on('data', (data) => {
+    response += data;
+
+    if (response === buf.toString()) {
+      worker.disconnect();
+    }
+  });
+  channel.write(buf);
+} else {
+  const pipe = new net.Socket({ fd: 4 });
+
+  pipe.unref();
+  pipe.on('data', (data) => {
+    assert.ok(data instanceof Buffer);
+    pipe.write(data);
+  });
+}
