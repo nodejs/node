f80d049c6d127cfa53a1acd57fdd00c607aa291d
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2016-07-27 22:25:26 +0530

    fs: warn if no callback is passed to async calls
    This patch issues a deprecation warning, if an asynchronous function
    is called without a callback function.
    
    PR-URL: https://github.com/nodejs/node/pull/7897
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Yorkie Liu <yorkiefixer@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-fs-make-callback.js
+++ test/parallel/test-fs-make-callback.js
@@ -1,7 +1,7 @@
 'use strict';
-require('../common');
-var assert = require('assert');
-var fs = require('fs');
+const common = require('../common');
+const assert = require('assert');
+const fs = require('fs');
 
 function test(cb) {
   return function() {
@@ -13,16 +13,26 @@ function test(cb) {
 // Verify the case where a callback function is provided
 assert.doesNotThrow(test(function() {}));
 
-// Passing undefined calls rethrow() internally, which is fine
-assert.doesNotThrow(test(undefined));
+process.once('warning', common.mustCall((warning) => {
+  assert.strictEqual(
+    warning.message,
+    'Calling an asynchronous function without callback is deprecated.'
+  );
 
-// Anything else should throw
-assert.throws(test(null));
-assert.throws(test(true));
-assert.throws(test(false));
-assert.throws(test(1));
-assert.throws(test(0));
-assert.throws(test('foo'));
-assert.throws(test(/foo/));
-assert.throws(test([]));
-assert.throws(test({}));
+  invalidArgumentsTests();
+}));
+
+// Passing undefined/nothing calls rethrow() internally, which emits a warning
+assert.doesNotThrow(test());
+
+function invalidArgumentsTests() {
+  assert.throws(test(null));
+  assert.throws(test(true));
+  assert.throws(test(false));
+  assert.throws(test(1));
+  assert.throws(test(0));
+  assert.throws(test('foo'));
+  assert.throws(test(/foo/));
+  assert.throws(test([]));
+  assert.throws(test({}));
+}
