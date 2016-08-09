62f0576714cb50e0045b8a9d935d1057a5e9077d
Ben Noordhuis <info@bnoordhuis.nl>
2016-08-09 16:37:21 +0200

    deps: make gtest output tap
    Teach gtest to produce TAP so we can integrate it better with our CI
    tooling.
    
    TAP is printed to stdout but it can also be written to file by passing
    the `--gtest_output=tap:filename.tap` switch to cctest.
    
    PR-URL: https://github.com/nodejs/node/pull/8034
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- deps/gtest/src/gtest_main.cc
+++ deps/gtest/src/gtest_main.cc
@@ -32,7 +32,6 @@
 #include "gtest/gtest.h"
 
 GTEST_API_ int main(int argc, char **argv) {
-  printf("Running main() from gtest_main.cc\n");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
