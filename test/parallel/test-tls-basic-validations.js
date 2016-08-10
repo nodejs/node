c26b9af1e277ef94b9bb33d93e1779b0a35f41e2
Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
2016-08-11 00:46:06 +0530

    tls: copy the Buffer object before using
    `convertNPNProtocols` and `convertALPNProtocols' uses the `protocols`
    buffer object as it is, and if it is modified outside of core, it
    might have an impact. This patch makes a copy of the buffer object,
    before using it.
    
    PR-URL: https://github.com/nodejs/node/pull/8055
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>

--- test/parallel/test-tls-basic-validations.js
+++ test/parallel/test-tls-basic-validations.js
@@ -33,3 +33,21 @@ assert.throws(() => tls.createServer({ticketKeys: new Buffer(0)}),
 
 assert.throws(() => tls.createSecurePair({}),
               /Error: First argument must be a tls module SecureContext/);
+
+{
+  const buffer = Buffer.from('abcd');
+  const out = {};
+  tls.convertALPNProtocols(buffer, out);
+  out.ALPNProtocols.write('efgh');
+  assert(buffer.equals(Buffer.from('abcd')));
+  assert(out.ALPNProtocols.equals(Buffer.from('efgh')));
+}
+
+{
+  const buffer = Buffer.from('abcd');
+  const out = {};
+  tls.convertNPNProtocols(buffer, out);
+  out.NPNProtocols.write('efgh');
+  assert(buffer.equals(Buffer.from('abcd')));
+  assert(out.NPNProtocols.equals(Buffer.from('efgh')));
+}
