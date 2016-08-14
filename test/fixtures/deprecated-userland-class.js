1f9fbade4ccaced763c153ea67f3312c6d98da37
Bryan English <bryan@bryanenglish.com>
2016-08-14 15:27:40 -0700

    util: fix deprecated class prototype
    Ensure the wrapped class prototype is exactly the unwrapped class
    prototype, rather than an object whose prototype is the unwrapped
    class prototype.
    
    This ensures that instances of the unwrapped class are instances
    of the wrapped class. This is useful when both a wrapped class and
    a factory for the unwrapped class are both exposed.
    
    Ref: https://github.com/nodejs/node/pull/8103
    PR-URL: https://github.com/nodejs/node/pull/8105
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Evan Lucas <evanlucas@me.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/fixtures/deprecated-userland-class.js
+++ test/fixtures/deprecated-userland-class.js
@@ -7,6 +7,9 @@ class deprecatedClass {
 const deprecated = util.deprecate(deprecatedClass, 'deprecatedClass is deprecated.');
 
 const instance = new deprecated();
+const deprecatedInstance = new deprecatedClass();
 
 assert(instance instanceof deprecated);
 assert(instance instanceof deprecatedClass);
+assert(deprecatedInstance instanceof deprecated);
+assert(deprecatedInstance instanceof deprecatedClass);
