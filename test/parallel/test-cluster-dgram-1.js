07624cac5afa0164f5ee859335a8ba0aa46f8b89
James M Snell <jasnell@gmail.com>
2016-08-24 13:55:12 -0700

    test: refactor/cleanup a number of cluster tests
    * Move shared code into common
    * Favor use of strictEqual
    * Add some missing common.mustCalls
    * Other general cleanup
    
    PR-URL: https://github.com/nodejs/node/pull/8261
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

--- test/parallel/test-cluster-dgram-1.js
+++ test/parallel/test-cluster-dgram-1.js
@@ -1,11 +1,11 @@
 'use strict';
 const common = require('../common');
-var NUM_WORKERS = 4;
-var PACKETS_PER_WORKER = 10;
+const NUM_WORKERS = 4;
+const PACKETS_PER_WORKER = 10;
 
-var assert = require('assert');
-var cluster = require('cluster');
-var dgram = require('dgram');
+const assert = require('assert');
+const cluster = require('cluster');
+const dgram = require('dgram');
 
 
 if (common.isWindows) {
@@ -28,13 +28,13 @@ function master() {
     cluster.fork();
 
   // Wait until all workers are listening.
-  cluster.on('listening', function() {
+  cluster.on('listening', common.mustCall(() => {
     if (++listening < NUM_WORKERS)
       return;
 
     // Start sending messages.
-    var buf = Buffer.from('hello world');
-    var socket = dgram.createSocket('udp4');
+    const buf = Buffer.from('hello world');
+    const socket = dgram.createSocket('udp4');
     var sent = 0;
     doSend();
 
@@ -47,15 +47,14 @@ function master() {
       if (sent < NUM_WORKERS * PACKETS_PER_WORKER) {
         doSend();
       } else {
-        console.log('master sent %d packets', sent);
         socket.close();
       }
     }
-  });
+  }, NUM_WORKERS));
 
   // Set up event handlers for every worker. Each worker sends a message when
   // it has received the expected number of packets. After that it disconnects.
-  for (var key in cluster.workers) {
+  for (const key in cluster.workers) {
     if (cluster.workers.hasOwnProperty(key))
       setupWorker(cluster.workers[key]);
   }
@@ -63,15 +62,13 @@ function master() {
   function setupWorker(worker) {
     var received = 0;
 
-    worker.on('message', function(msg) {
+    worker.on('message', common.mustCall((msg) => {
       received = msg.received;
-      console.log('worker %d received %d packets', worker.id, received);
-    });
+    }));
 
-    worker.on('disconnect', function() {
-      assert(received === PACKETS_PER_WORKER);
-      console.log('worker %d disconnected', worker.id);
-    });
+    worker.on('disconnect', common.mustCall(() => {
+      assert.strictEqual(received, PACKETS_PER_WORKER);
+    }));
   }
 }
 
@@ -82,15 +79,15 @@ function worker() {
   // Create udp socket and start listening.
   var socket = dgram.createSocket('udp4');
 
-  socket.on('message', function(data, info) {
+  socket.on('message', common.mustCall((data, info) => {
     received++;
 
     // Every 10 messages, notify the master.
-    if (received == PACKETS_PER_WORKER) {
+    if (received === PACKETS_PER_WORKER) {
       process.send({received: received});
       process.disconnect();
     }
-  });
+  }, PACKETS_PER_WORKER));
 
   socket.bind(common.PORT);
 }
