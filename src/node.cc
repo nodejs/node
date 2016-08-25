9f3badd41f7c545a938030ad09dbd221a23fd69e
Ali Ijaz Sheikh <ofrobots@google.com>
2016-08-25 14:14:47 -0700

    src: avoid duplicate AtExit functions
    node.cc had two functions with the name AtExit with entirely different
    purposes:
    
    * node::AtExit(): file static; used to register the atexit(3) handler
      for the Node process.
    * node::AtExit(void (*)(void*), void*): publicly exported symbol that
      addons can use to request callbacks upon exit.
    
    For code readability it is better to avoid the unintentional overload.
    
    PR-URL: https://github.com/nodejs/node/pull/8273
    Reviewed-By: addaleax - Anna Henningsen <anna@addaleax.net>
    Reviewed-By: jasnell - James M Snell <jasnell@gmail.com>
    Reviewed-By: cjihrig - Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: bnoordhuis - Ben Noordhuis <info@bnoordhuis.nl>

--- src/node.cc
+++ src/node.cc
@@ -3326,7 +3326,7 @@ void SetupProcessObject(Environment* env,
 #undef READONLY_PROPERTY
 
 
-static void AtExit() {
+static void AtProcessExit() {
   uv_tty_reset_mode();
 }
 
@@ -3363,7 +3363,7 @@ void LoadEnvironment(Environment* env) {
   env->isolate()->SetFatalErrorHandler(node::OnFatalError);
   env->isolate()->AddMessageListener(OnMessage);
 
-  atexit(AtExit);
+  atexit(AtProcessExit);
 
   TryCatch try_catch(env->isolate());
 
