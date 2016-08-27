2b41bfeda35feb122858f41df3d85dac40782b1f
James M Snell <jasnell@gmail.com>
2016-08-26 22:11:12 -0700

    Address nits

--- test/known_issues/test-path-parse-6229.js
+++ test/known_issues/test-path-parse-6229.js
@@ -5,14 +5,30 @@ require('../common');
 const assert = require('assert');
 const path = require('path');
 
-// The path `/foo/bar` is not the same path as `/foo/bar/`
-const parsed1 = path.parse('/foo/bar');
-const parsed2 = path.parse('/foo/bar/');
+{
+  // The path `/foo/bar` is not the same path as `/foo/bar/`
+  const parsed1 = path.posix.parse('/foo/bar');
+  const parsed2 = path.posix.parse('/foo/bar/');
 
-assert.notDeepStrictEqual(parsed1, parsed2);
+  assert.strictEqual(parsed1.root, '/');
+  assert.strictEqual(parsed1.dir, '/foo');
+  assert.strictEqual(parsed1.base, 'bar');
 
-// parsed1 *should* equal:
-// {root: '/', dir: '/foo', base: 'bar', ext: '', name: 'bar'}
-//
-// parsed2 *should* equal:
-// {root: '/', dir: '/foo/bar', base: '', ext: '', name: ''}
+  assert.strictEqual(parsed2.root, '/');
+  assert.strictEqual(parsed2.dir, '/foo/bar');
+  assert.strictEqual(parsed2.base, '');
+}
+
+{
+  // The path `\\foo\\bar` is not the same path as `\\foo\\bar\\`
+  const parsed1 = path.win32.parse('\\foo\\bar');
+  const parsed2 = path.win32.parse('\\foo\\bar\\');
+
+  assert.strictEqual(parsed1.root, '\\');
+  assert.strictEqual(parsed1.dir, '\\foo');
+  assert.strictEqual(parsed1.base, 'bar');
+
+  assert.strictEqual(parsed2.root, '\\');
+  assert.strictEqual(parsed2.dir, '\\foo\\bar');
+  assert.strictEqual(parsed2.base, '');
+}
