cf380c4bf2dd0782cf731faedde20cfdb7c7ce7d
Paul Grock <paulgrock@gmail.com>
2016-08-27 15:29:13 -0700

    test: cleanup test require symlink
    * Changed `==` to `includes` for clarity.
    * Switched to `assert.strictEqual` from `assert.equal`
    * Changed some `var` to `const`
    * Test cleanup with `common.refreshTmpDir`
    
    PR-URL: https://github.com/nodejs/node/pull/8305
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-require-symlink.js
+++ test/parallel/test-require-symlink.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 const exec = require('child_process').exec;
 const spawn = require('child_process').spawn;
 
+common.refreshTmpDir();
+
 const linkTarget = path.join(common.fixturesDir,
   '/module-require-symlink/node_modules/dep2/');
 
@@ -22,8 +24,8 @@ if (common.isWindows) {
   // On Windows, creating symlinks requires admin privileges.
   // We'll only try to run symlink test if we have enough privileges.
   exec('whoami /priv', function(err, o) {
-    if (err || o.indexOf('SeCreateSymbolicLinkPrivilege') == -1) {
-      console.log('Skipped: insufficient privileges');
+    if (err || !o.includes('SeCreateSymbolicLinkPrivilege')) {
+      common.skip('insufficient privileges');
       return;
     } else {
       test();
@@ -36,21 +38,20 @@ if (common.isWindows) {
 function test() {
   process.on('exit', function() {
     fs.unlinkSync(linkDir);
-    fs.unlinkSync(linkScript);
   });
 
   fs.symlinkSync(linkTarget, linkDir);
   fs.symlinkSync(linkScriptTarget, linkScript);
 
   // load symlinked-module
-  var fooModule =
+  const fooModule =
     require(path.join(common.fixturesDir, '/module-require-symlink/foo.js'));
-  assert.equal(fooModule.dep1.bar.version, 'CORRECT_VERSION');
-  assert.equal(fooModule.dep2.bar.version, 'CORRECT_VERSION');
+  assert.strictEqual(fooModule.dep1.bar.version, 'CORRECT_VERSION');
+  assert.strictEqual(fooModule.dep2.bar.version, 'CORRECT_VERSION');
 
   // load symlinked-script as main
-  var node = process.execPath;
-  var child = spawn(node, ['--preserve-symlinks', linkScript]);
+  const node = process.execPath;
+  const child = spawn(node, ['--preserve-symlinks', linkScript]);
   child.on('close', function(code, signal) {
     assert(!code);
     assert(!signal);
