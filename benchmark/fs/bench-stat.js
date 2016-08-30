82de54182603a3af0bb9b3b83ead6810cc042233
Anna Henningsen <anna@addaleax.net>
2016-08-30 14:52:41 +0200

    benchmark: add benches for fs.stat & fs.statSync
    Add very simple benchmarks for `fs.stat` and `fs.statSync` as
    well as `fs.lstat` and `fs.lstatSync` based on the `readdir`
    benchmarks.

---
+++ benchmark/fs/bench-stat.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const common = require('../common');
+const fs = require('fs');
+
+const bench = common.createBenchmark(main, {
+  n: [1e4],
+  kind: ['lstat', 'stat']
+});
+
+
+function main(conf) {
+  const n = conf.n >>> 0;
+  const fn = fs[conf.kind];
+
+  bench.start();
+  (function r(cntr) {
+    if (cntr-- <= 0)
+      return bench.end(n);
+    fn(__filename, function() {
+      r(cntr);
+    });
+  }(n));
+}
