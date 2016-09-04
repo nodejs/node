e07a3973f32e7e3cbc8a285794d7f649e886cc53
Rich Trott <rtrott@gmail.com>
2016-09-03 22:28:41 -0700

    test: increase _debugger coverage
    The uncaught exception test for `_debugger.js` was not exercising some
    code (particularly concerning `interface_.child`) because of the
    synchronous nature of the test. This adds an asynchronous version to
    increase test coverage.

---
+++ test/fixtures/debug-uncaught-async.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('../common');
+const assert = require('assert');
+const debug = require('_debugger');
+
+function emit() {
+  const error = new Error('fhqwhgads');
+  process.emit('uncaughtException', error);
+}
+
+assert.doesNotThrow(emit);
+
+debug.start(['sterrance']);
+
+setTimeout(emit, 100);
