c1893315bbc48623203c0cd93135a42717ad6d7b
Santiago Gimeno <santiago.gimeno@gmail.com>
2016-09-04 23:01:43 +0200

    src: fix -Wunused-result warning
    PR-URL: https://github.com/nodejs/node/pull/8450
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- src/node_util.cc
+++ src/node_util.cc
@@ -132,7 +132,7 @@ void Initialize(Local<Object> target,
 #define V(name, _)                                                            \
   target->Set(context,                                                        \
               FIXED_ONE_BYTE_STRING(env->isolate(), #name),                   \
-              Integer::NewFromUnsigned(env->isolate(), index++));
+              Integer::NewFromUnsigned(env->isolate(), index++)).FromJust();
   {
     uint32_t index = 0;
     PER_ISOLATE_PRIVATE_SYMBOL_PROPERTIES(V)
