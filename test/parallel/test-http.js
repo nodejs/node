9f3bf4214ec8046bfc1e7f7835e01c0b4c0cfc97
Junshu Okamoto <o_askgulf@icloud.com>
2016-09-09 14:37:18 -0700

    test: favor ’assert.strictEqual’  over  in ’assert.equal’ in http test

--- test/parallel/test-http.js
+++ test/parallel/test-http.js
@@ -11,20 +11,20 @@ var body1 = '';
 
 var server = http.Server(function(req, res) {
   if (responses_sent === 0) {
-    assert.equal('GET', req.method);
-    assert.equal('/hello', url.parse(req.url).pathname);
+    assert.strictEqual('GET', req.method);
+    assert.strictEqual('/hello', url.parse(req.url).pathname);
 
     console.dir(req.headers);
-    assert.equal(true, 'accept' in req.headers);
-    assert.equal('*/*', req.headers['accept']);
+    assert.strictEqual(true, 'accept' in req.headers);
+    assert.strictEqual('*/*', req.headers['accept']);
 
-    assert.equal(true, 'foo' in req.headers);
-    assert.equal('bar', req.headers['foo']);
+    assert.strictEqual(true, 'foo' in req.headers);
+    assert.strictEqual('bar', req.headers['foo']);
   }
 
   if (responses_sent === 1) {
-    assert.equal('POST', req.method);
-    assert.equal('/world', url.parse(req.url).pathname);
+    assert.strictEqual('POST', req.method);
+    assert.strictEqual('/world', url.parse(req.url).pathname);
     this.close();
   }
 
@@ -48,7 +48,7 @@ server.on('listening', function() {
     headers: {'Accept': '*/*', 'Foo': 'bar'},
     agent: agent
   }, function(res) {
-    assert.equal(200, res.statusCode);
+    assert.strictEqual(200, res.statusCode);
     responses_recvd += 1;
     res.setEncoding('utf8');
     res.on('data', function(chunk) { body0 += chunk; });
@@ -62,7 +62,7 @@ server.on('listening', function() {
       path: '/world',
       agent: agent
     }, function(res) {
-      assert.equal(200, res.statusCode);
+      assert.strictEqual(200, res.statusCode);
       responses_recvd += 1;
       res.setEncoding('utf8');
       res.on('data', function(chunk) { body1 += chunk; });
@@ -74,12 +74,11 @@ server.on('listening', function() {
 
 process.on('exit', function() {
   console.error('responses_recvd: ' + responses_recvd);
-  assert.equal(2, responses_recvd);
+  assert.strictEqual(2, responses_recvd);
 
   console.error('responses_sent: ' + responses_sent);
-  assert.equal(2, responses_sent);
+  assert.strictEqual(2, responses_sent);
 
-  assert.equal('The path was /hello', body0);
-  assert.equal('The path was /world', body1);
+  assert.strictEqual('The path was /hello', body0);
+  assert.strictEqual('The path was /world', body1);
 });
-
