e25767ee7a28d5aa2206e75d6a83023bac79c697
Rich Trott <rtrott@gmail.com>
2016-09-10 13:19:01 -0700

    squash: equal->strictEqual, throw->common.fail()

--- test/parallel/test-force-repl.js
+++ test/parallel/test-force-repl.js
@@ -6,13 +6,13 @@ const spawn = require('child_process').spawn;
 // spawn a node child process in "interactive" mode (force the repl)
 const cp = spawn(process.execPath, ['-i']);
 var timeoutId = setTimeout(function() {
-  throw new Error('timeout!');
+  common.fail('timeout!');
 }, common.platformTimeout(5000)); // give node + the repl 5 seconds to start
 
 cp.stdout.setEncoding('utf8');
 
 cp.stdout.once('data', common.mustCall(function(b) {
   clearTimeout(timeoutId);
-  assert.equal(b, '> ');
+  assert.strictEqual(b, '> ');
   cp.kill();
 }));
