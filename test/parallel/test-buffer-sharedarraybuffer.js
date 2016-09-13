ce9aa0bfa2b1bac7f0b7b96bbbc525aee6b349e2
Ojas Shirekar <ojas.shirekar@gmail.com>
2016-09-13 20:08:10 +0530

    tests: update test-buffer-sharedarraybuffer
    Updated test-buffer-sharedarraybuffer.js to have more readable
    variable names.
    Replaced var definitions with const.

--- test/parallel/test-buffer-sharedarraybuffer.js
+++ test/parallel/test-buffer-sharedarraybuffer.js
@@ -7,19 +7,19 @@ const assert = require('assert');
 const Buffer = require('buffer').Buffer;
 
 const sab = new SharedArrayBuffer(24);
-const arr = new Uint16Array(sab);
-const ar = new Uint16Array(12);
-ar[0] = 5000;
-arr[0] = 5000;
-arr[1] = 4000;
-ar[1] = 4000;
+const arr1 = new Uint16Array(sab);
+const arr2 = new Uint16Array(12);
+arr2[0] = 5000;
+arr1[0] = 5000;
+arr1[1] = 4000;
+arr2[1] = 4000;
 
-var arr_buf = Buffer.from(arr.buffer);
-var ar_buf = Buffer.from(ar.buffer);
+const arr_buf = Buffer.from(arr1.buffer);
+const ar_buf = Buffer.from(arr2.buffer);
 
 assert.deepStrictEqual(arr_buf, ar_buf, 0);
 
-arr[1] = 6000;
-ar[1] = 6000;
+arr1[1] = 6000;
+arr2[1] = 6000;
 
 assert.deepStrictEqual(arr_buf, ar_buf, 0);
