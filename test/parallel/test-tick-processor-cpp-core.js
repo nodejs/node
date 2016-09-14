82b7894d3b539a02ad115d7287dcc429f6be5a94
Fedor Indutny <fedor@indutny.com>
2016-09-15 01:26:52 +0200

    test: make test-tick-processor.js non-flaky
    Wait for a sought-for symbol to appear instead of just hard-killing
    subprocesses at 2s timeout.
    
    Fix: #4427
    PR-URL: https://github.com/nodejs/node/pull/8542
    Reviewed-By: Rich Trott <rtrott@gmail.com>

---
+++ test/parallel/test-tick-processor-cpp-core.js
@@ -0,0 +1,33 @@
+'use strict';
+const common = require('../common');
+const path = require('path');
+
+// TODO(mhdawson) Currently the test-tick-processor functionality in V8
+// depends on addresses being smaller than a full 64 bits.  Aix supports
+// the full 64 bits and the result is that it does not process the
+// addresses correctly and runs out of memory
+// Disabling until we get a fix upstreamed into V8
+if (common.isAix) {
+  common.skip('Aix address range too big for scripts.');
+  return;
+}
+
+const base = require(path.join(common.fixturesDir, 'tick-processor-base.js'));
+
+if (common.isWindows ||
+    common.isSunOS ||
+    common.isAix ||
+    common.isLinuxPPCBE ||
+    common.isFreeBSD) {
+  common.skip('C++ symbols are not mapped for this os.');
+  return;
+}
+
+base.runTest({
+  pattern: /RunInDebugContext/,
+  code: `function f() {
+           require(\'vm\').runInDebugContext(\'Debug\');
+           setImmediate(function() { f(); });
+         };
+         f();`
+});
