7561123a9f5f6ee7b48fbfb899b88a9d35d4a57c
Matt Lang <matt@mediasuite.co.nz>
2016-09-17 11:24:05 +0200

    test: update var to const, use arrow functions
    PR-URL: https://github.com/nodejs/node/pull/8595
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-cluster-disconnect-idle-worker.js
+++ test/parallel/test-cluster-disconnect-idle-worker.js
@@ -1,13 +1,13 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var cluster = require('cluster');
-var fork = cluster.fork;
+const common = require('../common');
+const assert = require('assert');
+const cluster = require('cluster');
+const fork = cluster.fork;
 
 if (cluster.isMaster) {
   fork(); // it is intentionally called `fork` instead of
   fork(); // `cluster.fork` to test that `this` is not used
-  cluster.disconnect(common.mustCall(function() {
+  cluster.disconnect(common.mustCall(() => {
     assert.deepStrictEqual(Object.keys(cluster.workers), []);
   }));
 }
