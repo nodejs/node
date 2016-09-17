366495d4e221ccd7080a0a05ce67160905b4a6a5
Dennis Schwartz <dennis@repositive.io>
2016-09-17 11:40:08 +0200

    test: improve child_process tests
    Replaced var keyword with const and let in the tests for child process
    stdin and stdio.
    
    PR-URL: https://github.com/nodejs/node/pull/8617
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ilkka Myller <ilkka.myller@nodefield.com>

--- test/parallel/test-child-process-stdio.js
+++ test/parallel/test-child-process-stdio.js
@@ -1,9 +1,9 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
-var options = {stdio: ['pipe']};
-var child = common.spawnPwd(options);
+let options = {stdio: ['pipe']};
+let child = common.spawnPwd(options);
 
 assert.notEqual(child.stdout, null);
 assert.notEqual(child.stderr, null);
