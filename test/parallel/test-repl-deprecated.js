fb651d3e314b0acdf66058fc8ab905bc7c3c36ca
Michaël Zasso <targos@protonmail.com>
2016-09-17 18:25:03 +0200

    test: add expectWarning to common
    There are multiple tests that use the same boilerplate to test that
    warnings are correctly emitted. This adds a new common function to do that
    and changes the tests to use it.
    
    PR-URL: https://github.com/nodejs/node/pull/8662
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-repl-deprecated.js
+++ test/parallel/test-repl-deprecated.js
@@ -3,18 +3,8 @@ const common = require('../common');
 const assert = require('assert');
 const repl = require('repl');
 
-const expected = [
-  'replServer.convertToContext() is deprecated'
-];
-
-process.on('warning', common.mustCall((warning) => {
-  assert.strictEqual(warning.name, 'DeprecationWarning');
-  assert.notStrictEqual(expected.indexOf(warning.message), -1,
-                        `unexpected error message: "${warning.message}"`);
-  // Remove a warning message after it is seen so that we guarantee that we get
-  // each message only once.
-  expected.splice(expected.indexOf(warning.message), 1);
-}, expected.length));
+common.expectWarning('DeprecationWarning',
+                     'replServer.convertToContext() is deprecated');
 
 // Create a dummy stream that does nothing
 const stream = new common.ArrayStream();
