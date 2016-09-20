5daccd23c71b55f7dd03ef3399138d9f66347c4d
Italo A. Casas <me@italoacasas.com>
2016-09-20 13:42:10 -0400

    cleanup test-stream-big-push.js

--- test/parallel/test-stream-big-push.js
+++ test/parallel/test-stream-big-push.js
@@ -1,17 +1,19 @@
 'use strict';
-require('../common');
-var assert = require('assert');
-var stream = require('stream');
-var str = 'asdfasdfasdfasdfasdf';
 
-var r = new stream.Readable({
+require('../common');
+
+const assert = require('assert');
+const stream = require('stream');
+const str = 'asdfasdfasdfasdfasdf';
+
+const r = new stream.Readable({
   highWaterMark: 5,
   encoding: 'utf8'
 });
 
-var reads = 0;
-var eofed = false;
-var ended = false;
+let reads = 0;
+let eofed = false;
+let ended = false;
 
 r._read = function(n) {
   if (reads === 0) {
@@ -21,7 +23,7 @@ r._read = function(n) {
     reads++;
   } else if (reads === 1) {
     var ret = r.push(str);
-    assert.equal(ret, false);
+    assert.strictEqual(ret, false);
     reads++;
   } else {
     assert(!eofed);
@@ -40,9 +42,9 @@ var ret = r.push(str);
 // should be false.  > hwm
 assert(!ret);
 var chunk = r.read();
-assert.equal(chunk, str);
+assert.strictEqual(chunk, str);
 chunk = r.read();
-assert.equal(chunk, null);
+assert.strictEqual(chunk, null);
 
 r.once('readable', function() {
   // this time, we'll get *all* the remaining data, because
@@ -50,15 +52,15 @@ r.once('readable', function() {
   // us below the hwm, and so it triggered a _read() again,
   // which synchronously added more, which we then return.
   chunk = r.read();
-  assert.equal(chunk, str + str);
+  assert.strictEqual(chunk, str + str);
 
   chunk = r.read();
-  assert.equal(chunk, null);
+  assert.strictEqual(chunk, null);
 });
 
 process.on('exit', function() {
   assert(eofed);
   assert(ended);
-  assert.equal(reads, 2);
+  assert.strictEqual(reads, 2);
   console.log('ok');
 });
