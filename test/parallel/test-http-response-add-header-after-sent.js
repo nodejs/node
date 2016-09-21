a72b6fc05d0bd6bdd42b351d5753ad50a29cd117
Niklas Ingholt <niklas@ingholt.com>
2016-09-21 15:59:49 +0200

    Fix missing semicolon.

--- test/parallel/test-http-response-add-header-after-sent.js
+++ test/parallel/test-http-response-add-header-after-sent.js
@@ -7,15 +7,15 @@ const server = http.createServer((req, res) => {
   assert.doesNotThrow(() => {
     res.setHeader('header1', 1);
   });
-  res.write('abc')
+  res.write('abc');
   assert.throws(() => {
     res.setHeader('header2', 2);
-  }, /Can't set headers after they are sent./)
-  res.end()
+  }, /Can't set headers after they are sent./);
+  res.end();
 });
 
 server.listen(0, () => {
   http.get({port: server.address().port}, () => {
-    server.close()
+    server.close();
   });
 });
--- test/parallel/test-http-response-remove-header-after-sent.js
+++ test/parallel/test-http-response-remove-header-after-sent.js
@@ -7,15 +7,15 @@ const server = http.createServer((req, res) => {
   assert.doesNotThrow(() => {
     res.removeHeader('header1', 1);
   });
-  res.write('abc')
+  res.write('abc');
   assert.throws(() => {
     res.removeHeader('header2', 2);
-  }, /Can't remove headers after they are sent/)
-  res.end()
+  }, /Can't remove headers after they are sent/);
+  res.end();
 });
 
 server.listen(0, () => {
   http.get({port: server.address().port}, () => {
-    server.close()
+    server.close();
   });
 });
