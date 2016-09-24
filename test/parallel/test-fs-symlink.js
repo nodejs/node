0b08e9f8ef164be11c43f53f1f5eaf115dd68f04
Richard Hong <rich.hong@gmail.com>
2016-09-24 16:14:16 -0700

    test:replace indexOf, assert.equal, add mustCall

--- test/parallel/test-fs-symlink.js
+++ test/parallel/test-fs-symlink.js
@@ -12,7 +12,7 @@ if (common.isWindows) {
   // On Windows, creating symlinks requires admin privileges.
   // We'll only try to run symlink test if we have enough privileges.
   exec('whoami /priv', function(err, o) {
-    if (err || o.indexOf('SeCreateSymbolicLinkPrivilege') == -1) {
+    if (err || !o.includes('SeCreateSymbolicLinkPrivilege')) {
       common.skip('insufficient privileges');
       return;
     }
@@ -25,7 +25,7 @@ common.refreshTmpDir();
 const linkData = path.join(common.fixturesDir, '/cycles/root.js');
 const linkPath = path.join(common.tmpDir, 'symlink1.js');
 
-fs.symlink(linkData, linkPath, function(err) {
+fs.symlink(linkData, linkPath, common.mustCall(function(err) {
   if (err) throw err;
 
   fs.lstat(linkPath, common.mustCall(function(err, stats) {
@@ -40,9 +40,9 @@ fs.symlink(linkData, linkPath, function(err) {
 
   fs.readlink(linkPath, common.mustCall(function(err, destination) {
     if (err) throw err;
-    assert.equal(destination, linkData);
+    assert.strictEqual(destination, linkData);
   }));
-});
+}));
 
 
 process.on('exit', function() {
