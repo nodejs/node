c97298dc5778cc4aa87609d76e5508930d7c84dd
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-09-24 22:38:14 -0400

    Uses `common.skip` and removes FIXME comment

--- test/parallel/test-setproctitle.js
+++ test/parallel/test-setproctitle.js
@@ -3,9 +3,8 @@
 const common = require('../common');
 
 require('../common');
-// FIXME add sunos support
 if (!(common.isFreeBSD || common.isOSX || common.isLinux || common.isSunOS)) {
-  console.log(`1..0 # Skipped: Unsupported platform [${process.platform}]`);
+  common.skip(`Unsupported platform [${process.platform}]`);
   return;
 }
 
