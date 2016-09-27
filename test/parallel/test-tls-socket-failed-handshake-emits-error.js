b6941b01411631df4e486697e293c09794d3d9a7
Mariusz 'koder' Chwalba <mariusz.chwalba@cashbill.pl>
2016-09-27 08:20:10 +0200

    tls: TLSSocket emits 'error' on handshake failure
    Removes branch that would make TLSSocket emit '_tlsError' event if
    error occured on handshake and control was not released, as it was
    never happening.  Added test for tls.Server to ensure it still emits
    'tlsClientError' as expected.
    
    Note that 'tlsClientError' does not exist in the v4.x branch so this
    back-port emits 'clientError' instead.  See also pull request #4557.
    
    Fixes: https://github.com/nodejs/node/issues/8803
    PR-URL: https://github.com/nodejs/node/pull/8805
    Refs: https://github.com/nodejs/node/pull/4557
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Fedor Indutny <fedor@indutny.com>

---
+++ test/parallel/test-tls-socket-failed-handshake-emits-error.js
@@ -0,0 +1,38 @@
+'use strict';
+const common = require('../common');
+
+if (!common.hasCrypto) {
+  common.skip('missing crypto');
+  return;
+}
+const tls = require('tls');
+const net = require('net');
+const assert = require('assert');
+
+const bonkers = Buffer.alloc(1024, 42);
+
+const server = net.createServer(function(c) {
+  setTimeout(function() {
+    const s = new tls.TLSSocket(c, {
+      isServer: true,
+      server: server
+    });
+
+    s.on('error', common.mustCall(function(e) {
+      assert.ok(e instanceof Error,
+                'Instance of Error should be passed to error handler');
+      assert.ok(e.message.match(
+        /SSL routines:SSL23_GET_CLIENT_HELLO:unknown protocol/),
+        'Expecting SSL unknown protocol');
+    }));
+
+    s.on('close', function() {
+      server.close();
+      s.destroy();
+    });
+  }, common.platformTimeout(200));
+}).listen(0, function() {
+  const c = net.connect({port: this.address().port}, function() {
+    c.write(bonkers);
+  });
+});
