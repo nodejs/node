b9d5d7de6b76a420c0d899b2619c467613e96c2e
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-09-28 20:24:19 +0200

    use strictEqual instead of equals

--- test/parallel/test-tcp-wrap-connect.js
+++ test/parallel/test-tcp-wrap-connect.js
@@ -10,25 +10,25 @@ function makeConnection() {
 
   var req = new TCPConnectWrap();
   var err = client.connect(req, '127.0.0.1', this.address().port);
-  assert.equal(err, 0);
+  assert.strictEqual(err, 0);
 
   req.oncomplete = function(status, client_, req_, readable, writable) {
-    assert.equal(0, status);
-    assert.equal(client, client_);
-    assert.equal(req, req_);
-    assert.equal(true, readable);
-    assert.equal(true, writable);
+    assert.strictEqual(0, status);
+    assert.strictEqual(client, client_);
+    assert.strictEqual(req, req_);
+    assert.strictEqual(true, readable);
+    assert.strictEqual(true, writable);
 
     console.log('connected');
     var shutdownReq = new ShutdownWrap();
     var err = client.shutdown(shutdownReq);
-    assert.equal(err, 0);
+    assert.strictEqual(err, 0);
 
     shutdownReq.oncomplete = function(status, client_, req_) {
       console.log('shutdown complete');
-      assert.equal(0, status);
-      assert.equal(client, client_);
-      assert.equal(shutdownReq, req_);
+      assert.strictEqual(0, status);
+      assert.strictEqual(client, client_);
+      assert.strictEqual(shutdownReq, req_);
       shutdownCount++;
       client.close();
     };
@@ -56,7 +56,7 @@ var server = require('net').Server(function(s) {
 server.listen(0, makeConnection);
 
 process.on('exit', function() {
-  assert.equal(1, shutdownCount);
-  assert.equal(1, connectCount);
-  assert.equal(1, endCount);
+  assert.strictEqual(1, shutdownCount);
+  assert.strictEqual(1, connectCount);
+  assert.strictEqual(1, endCount);
 });
