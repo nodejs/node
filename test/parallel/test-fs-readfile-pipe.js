fc428255300bf728ea67dcedd356f4a6ae79f2d0
Lydia Kats <llkats@gmail.com>
2016-09-28 18:55:59 -0700

    test: modernize syntax, add strict checks
    Changed `var` to `const`, strings to template literals, and
    assert.equal to assert.strictEqual where appropriate.
    
    PR-URL: https://github.com/nodejs/node/pull/8841
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>

--- test/parallel/test-fs-readfile-pipe.js
+++ test/parallel/test-fs-readfile-pipe.js
@@ -1,6 +1,6 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
 // simulate `cat readfile.js | node readfile.js`
 
@@ -9,9 +9,9 @@ if (common.isWindows || common.isAix) {
   return;
 }
 
-var fs = require('fs');
+const fs = require('fs');
 
-var dataExpected = fs.readFileSync(__filename, 'utf8');
+const dataExpected = fs.readFileSync(__filename, 'utf8');
 
 if (process.argv[2] === 'child') {
   fs.readFile('/dev/stdin', function(er, data) {
@@ -21,10 +21,10 @@ if (process.argv[2] === 'child') {
   return;
 }
 
-var exec = require('child_process').exec;
-var f = JSON.stringify(__filename);
-var node = JSON.stringify(process.execPath);
-var cmd = 'cat ' + f + ' | ' + node + ' ' + f + ' child';
+const exec = require('child_process').exec;
+const f = JSON.stringify(__filename);
+const node = JSON.stringify(process.execPath);
+const cmd = `cat ${f} | ${node} ${f} child`;
 exec(cmd, function(err, stdout, stderr) {
   if (err) console.error(err);
   assert(!err, 'it exits normally');
