1bc9af88d12005bdac79f0b9f8b1d8539237caba
larissayvette <larissayvette55@gmail.com>
2016-10-10 18:07:43 +0100

    test: fixes that do not affect performance
    PR-URL: https://github.com/nodejs/node/pull/9011
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-debugger-pid.js
+++ test/parallel/test-debugger-pid.js
@@ -29,8 +29,8 @@ interfacer.on('line', function(line) {
     switch (++lineCount) {
       case 1:
         line = line.replace(/^(debug> *)+/, '');
-        var msg = 'There was an internal error in Node\'s debugger. ' +
-                  'Please report this bug.';
+        const msg = 'There was an internal error in Node\'s debugger. ' +
+                    'Please report this bug.';
         expected = `(node:${pid}) ${msg}`;
         break;
 
@@ -50,5 +50,5 @@ interfacer.on('line', function(line) {
 });
 
 interfacer.on('exit', function(code, signal) {
-  assert.ok(code == 1, 'Got unexpected code: ' + code);
+  assert.strictEqual(code, 1, `Got unexpected code: ${code}`);
 });
