9cc900124431c3c7afdd91c9539bfeaf92dc151e
Jenna Vuong <hello@jennavuong.com>
2016-10-09 22:28:42 -0700

    test: refactor test-file-*
    * var to const
    * add check that expected error is ENOENT
    * indexOf() to includes()
    
    PR-URL: https://github.com/nodejs/node/pull/8999
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>

--- test/parallel/test-file-read-noexist.js
+++ test/parallel/test-file-read-noexist.js
@@ -1,11 +1,11 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var path = require('path');
-var fs = require('fs');
+const common = require('../common');
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
 
-var filename = path.join(common.fixturesDir, 'does_not_exist.txt');
-
+const filename = path.join(common.fixturesDir, 'does_not_exist.txt');
 fs.readFile(filename, 'latin1', common.mustCall(function(err, content) {
   assert.ok(err);
+  assert.strictEqual(err.code, 'ENOENT');
 }));
