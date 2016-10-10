3d27c89431fb349b37580f9a434a16199cc578d2
Jenna Vuong <hello@jennavuong.com>
2016-10-09 22:33:53 -0700

    change 'var' to 'const' for clarity

--- test/parallel/test-file-read-noexist.js
+++ test/parallel/test-file-read-noexist.js
@@ -1,8 +1,8 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var path = require('path');
-var fs = require('fs');
+const common = require('../common');
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
 
 var filename = path.join(common.fixturesDir, 'does_not_exist.txt');
 fs.readFile(filename, 'latin1', common.mustCall(function(err, content) {
