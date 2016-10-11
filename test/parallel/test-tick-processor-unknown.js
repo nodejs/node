eb690b13351b226f0e687a858e4b0a919adac0ac
Rich Trott <rtrott@gmail.com>
2016-10-10 23:23:18 -0700

    test: move module out of fixture directory
    tick-processor-base.js is a module used by three other tests. It is not
    a test fixture so move it out of the fixture directory. (One downside to
    having it in the fixture directory is that fixture code is not currently
    linted.)
    
    It is possible that the code in tick-processor-base.js should be
    integrated into common.js. This can potentially happen subsequently (and
    might make a reasonable good first contribution for a new contributor).
    
    PR-URL: https://github.com/nodejs/node/pull/9022
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-tick-processor-unknown.js
+++ test/parallel/test-tick-processor-unknown.js
@@ -1,6 +1,5 @@
 'use strict';
 const common = require('../common');
-const path = require('path');
 
 // TODO(mhdawson) Currently the test-tick-processor functionality in V8
 // depends on addresses being smaller than a full 64 bits.  Aix supports
@@ -17,7 +16,7 @@ if (!common.enoughTestCpu) {
   return;
 }
 
-const base = require(path.join(common.fixturesDir, 'tick-processor-base.js'));
+const base = require('./tick-processor-base.js');
 
 // Unknown checked for to prevent flakiness, if pattern is not found,
 // then a large number of unknown ticks should be present
