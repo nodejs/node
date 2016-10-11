393a059697fe6347110ae6229f2ba1d7514b536e
Jenna Vuong <hello@jennavuong.com>
2016-10-10 23:54:07 -0700

    add semicolon after `assert.strictEqual()`

--- test/parallel/test-file-read-noexist.js
+++ test/parallel/test-file-read-noexist.js
@@ -7,5 +7,5 @@ const fs = require('fs');
 const filename = path.join(common.fixturesDir, 'does_not_exist.txt');
 fs.readFile(filename, 'latin1', common.mustCall(function(err, content) {
   assert.ok(err);
-  assert.strictEqual(err.code, 'ENOENT')
+  assert.strictEqual(err.code, 'ENOENT');
 }));
