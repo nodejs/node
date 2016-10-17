92550d1b2d1fe6b45e1f0a85ccd421bc5fc3db58
larissayvette <larissayvette55@gmail.com>
2016-10-17 08:53:49 +0100

    nits: identations

--- test/parallel/test-debug-agent.js
+++ test/parallel/test-debug-agent.js
@@ -4,9 +4,9 @@ const assert = require('assert');
 const debug = require('_debug_agent');
 
 assert.throws(
-  () => { debug.start();},
-   function(err) {
-     return (err instanceof assert.AssertionError &&
-            err.message === 'Debugger agent running without bindings!');
-   }
+  () => { debug.start(); },
+  function(err) {
+    return (err instanceof assert.AssertionError &&
+           err.message === 'Debugger agent running without bindings!');
+  }
 );
