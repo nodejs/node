f140c3f6be29f94fa0d6fa75df8006c1bf826bae
Anna Henningsen <anna@addaleax.net>
2016-10-18 23:14:12 +0200

    [squash] addres nit for checking `_readableState.pipes` before unpiping

--- test/parallel/test-stream-pipe-unpipe-streams.js
+++ test/parallel/test-stream-pipe-unpipe-streams.js
@@ -14,6 +14,10 @@ source.pipe(dest2);
 dest1.on('unpipe', common.mustCall(() => {}));
 dest2.on('unpipe', common.mustCall(() => {}));
 
+assert.strictEqual(source._readableState.pipes[0], dest1);
+assert.strictEqual(source._readableState.pipes[1], dest2);
+assert.strictEqual(source._readableState.pipes.length, 2);
+
 // Should be able to unpipe them in the reverse order that they were piped.
 
 source.unpipe(dest2);
