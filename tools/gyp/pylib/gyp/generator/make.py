64bd16e878fd5b826d5671d8c62a48f0c626b11d
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-10-18 16:41:26 +0200

    build: enable cctest to use generated objects
    This commit tries to make it simpler to add unit tests (cctest) for
    code that needs to test node core funtionality but that might not be
    appropriate as an addon or a JavaScript test. An example of this could
    be adding functionality targeted for situations when Node itself is
    embedded.
    
    Currently it was not as easy, or efficient, as one would have hoped to
    add such tests. The object output directories vary for different
    operating systems which we need to link to so that we don't have an
    additional compilation step.
    
    PR-URL: https://github.com/nodejs/node/pull/11956
    Ref: https://github.com/nodejs/node/pull/9163
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- tools/gyp/pylib/gyp/generator/make.py
+++ tools/gyp/pylib/gyp/generator/make.py
@@ -147,7 +147,7 @@ cmd_alink_thin = rm -f $@ && $(AR.$(TOOLSET)) crsT $@ $(filter %.o,$^)
 # special "figure out circular dependencies" flags around the entire
 # input list during linking.
 quiet_cmd_link = LINK($(TOOLSET)) $@
-cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o $@ -Wl,--start-group $(LD_INPUTS) -Wl,--end-group $(LIBS)
+cmd_link = $(LINK.$(TOOLSET)) $(GYP_LDFLAGS) $(LDFLAGS.$(TOOLSET)) -o $@ -Wl,--start-group $(LD_INPUTS) $(LIBS) -Wl,--end-group
 
 # We support two kinds of shared objects (.so):
 # 1) shared_library, which is just bundling together many dependent libraries
