ce2d434ab646551c9935d70e469f9d016f4643a4
Rich Trott <rtrott@gmail.com>
2016-10-19 15:57:00 -0700

    test: remove arbitrary timer
    `test-dgram-send-callback-buffer-length` was timing out (via the
    200ms timeout in the code) on FreeBSD in CI. The 200ms timeout is
    arbitrary and not necessary. Remove it.
    
    PR-URL: https://github.com/nodejs/node/pull/9197
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-dgram-send-callback-buffer-length.js
+++ test/parallel/test-dgram-send-callback-buffer-length.js
@@ -6,18 +6,13 @@ const assert = require('assert');
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
 
-const timer = setTimeout(function() {
-  throw new Error('Timeout');
-}, common.platformTimeout(200));
-
 const buf = Buffer.allocUnsafe(256);
 const offset = 20;
 const len = buf.length - offset;
 
 const messageSent = common.mustCall(function messageSent(err, bytes) {
-  assert.notEqual(bytes, buf.length);
-  assert.equal(bytes, buf.length - offset);
-  clearTimeout(timer);
+  assert.notStrictEqual(bytes, buf.length);
+  assert.strictEqual(bytes, buf.length - offset);
   client.close();
 });
 
