52670fc09e49eab6db76787c160a18c16b645018
James M Snell <jasnell@gmail.com>
2016-10-23 08:08:39 -0700

    test: skip whatwg url parse and setter tests when icu is missing
    the WHATWG url parser relies on ICU's punycode implementation.
    A handful of the standard tests fail when ICU is not present
    because of the additional checks that are not implemented. For
    now, skip the parse and setter tests if ICU is not present.
    
    PR-URL: https://github.com/nodejs/node/pull/9246
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Roman Reiss <me@silverwind.io>
    Reviewed-By: Johan Bergström <bugs@bergstroem.nu>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/parallel/test-whatwg-url-parsing.js
+++ test/parallel/test-whatwg-url-parsing.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const common = require('../common');
+
+if (!common.hasIntl) {
+  // A handful of the tests fail when ICU is not included.
+  common.skip('missing Intl... skipping test');
+  return;
+}
+
 const URL = require('url').URL;
 const path = require('path');
 const assert = require('assert');
