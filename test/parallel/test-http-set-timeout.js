f6d986723a72d5c95d72470fce75b7c6a5e99025
BethGriggs <Bethany.Griggs@uk.ibm.com>
2016-10-24 23:25:22 +0100

    test: remove err timer from test-http-set-timeout
    Removed the errorTimer from test-http-set-timeout.js, as this timer is
    not necessary to test the setTimeout functionality.
    
    Also edited the console.log message on line 8 to log the correct
    timeout duration. Changed var to const, and added common.mustCall() to
    on timeout and on error callbacks.
    
    Fixes: https://github.com/nodejs/node/issues/9256
    PR-URL: https://github.com/nodejs/node/pull/9264
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-http-set-timeout.js
+++ test/parallel/test-http-set-timeout.js
@@ -1,32 +1,26 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var http = require('http');
-var net = require('net');
+const common = require('../common');
+const assert = require('assert');
+const http = require('http');
+const net = require('net');
 
 var server = http.createServer(function(req, res) {
-  console.log('got request. setting 1 second timeout');
-  var s = req.connection.setTimeout(500);
-  assert.ok(s instanceof net.Socket);
-  req.connection.on('timeout', function() {
+  console.log('got request. setting 500ms timeout');
+  var socket = req.connection.setTimeout(500);
+  assert.ok(socket instanceof net.Socket);
+  req.connection.on('timeout', common.mustCall(function() {
     req.connection.destroy();
     console.error('TIMEOUT');
     server.close();
-  });
+  }));
 });
 
 server.listen(0, function() {
   console.log(`Server running at http://127.0.0.1:${this.address().port}/`);
 
-  var errorTimer = setTimeout(function() {
-    throw new Error('Timeout was not successful');
-  }, common.platformTimeout(2000));
-
-  var x = http.get({port: this.address().port, path: '/'});
-  x.on('error', function() {
-    clearTimeout(errorTimer);
+  var request = http.get({port: this.address().port, path: '/'});
+  request.on('error', common.mustCall(function() {
     console.log('HTTP REQUEST COMPLETE (this is good)');
-  });
-  x.end();
-
+  }));
+  request.end();
 });
