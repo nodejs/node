f0f44bbc5244230d17414c4d90c8eb49f77f37ab
Matt Loring <mattloring@google.com>
2016-10-27 15:24:00 -0700

    Clean up end to end test

--- test/parallel/test-trace-event.js
+++ test/parallel/test-trace-event.js
@@ -1,29 +1,28 @@
 'use strict';
 
 const assert = require('assert');
-const fs = require('fs');
 const cp = require('child_process');
+const common = require('../common.js');
+const fs = require('fs');
 
 const CODE = 'for (var i = 0; i < 100000; i++) { \"test\" + i }';
 const FILE_NAME = 'node_trace.1.log';
 
-fs.access(FILE_NAME, (err) => {
-  if (!err) {
+common.refreshTmpDir();
+process.chdir(common.tmpDir);
+
+const proc = cp.spawn(process.execPath,
+  [ '--enable-tracing', '-e', CODE ]);
+
+proc.once('exit', common.mustCall(() => {
+  assert(common.fileExists(FILE_NAME));
+  fs.readFile(FILE_NAME, (err, data) => {
     fs.unlinkSync(FILE_NAME);
-  }
-
-  const proc = cp.spawn(process.execPath,
-    [ '--enable-tracing', '-e', CODE ]);
-
-  proc.once('exit', () => {
-    fs.readFile(FILE_NAME, (err, data) => {
-      fs.unlinkSync(FILE_NAME);
-      const traces = JSON.parse(data).traceEvents;
-      assert(traces.length > 0);
-      // Values that should be present in all outputs to approximate well-formedness.
-      assert(traces.some((trace) => { return trace.pid === proc.pid; }));
-      assert(traces.some((trace) => { return trace.cat === 'v8'; }));
-      assert(traces.some((trace) => { return trace.name === 'V8.ScriptCompiler'; }));
-    });
+    const traces = JSON.parse(data).traceEvents;
+    assert(traces.length > 0);
+    // Values that should be present in all outputs to approximate well-formedness.
+    assert(traces.some((trace) => { return trace.pid === proc.pid; }));
+    assert(traces.some((trace) => { return trace.cat === 'v8'; }));
+    assert(traces.some((trace) => { return trace.name === 'V8.ScriptCompiler'; }));
   });
-});
+}));
