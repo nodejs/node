c3deb42eaa5558a4603dd1a0098bbc0822b62fea
Jaideep Bajwa <bjaideep@ca.ibm.com>
2016-11-01 02:53:37 -0400

    v8: update make-v8.sh to use git
    google build tool gclient doesn't support
    svn anymore. Updating v8 build script to use
    git instead.

--- tools/make-v8.sh
+++ tools/make-v8.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 
-
-git_origin=$(git config --get remote.origin.url | sed 's/.\+[\/:]\([^\/]\+\/[^\/]\+\)$/\1/')
-git_branch=$(git rev-parse --abbrev-ref HEAD)
-v8ver=${1:-v8} #default v8
-svn_prefix=https://github.com
-svn_path="$svn_prefix/$git_origin/branches/$git_branch/deps/$v8ver"
-#svn_path="$git_origin/branches/$git_branch/deps/$v8ver"
-gclient_string="solutions = [{'name': 'v8', 'url': '$svn_path', 'managed': False}]"
+if [ $# -eq 0 ]; then
+  # Default branch is master
+  BRANCH="master"
+else
+  # eg: 5.4-lkgr
+  BRANCH="$1"
+fi
 
 # clean up if someone presses ctrl-c
 trap cleanup INT
 
 function cleanup() {
   trap - INT
-
   rm .gclient || true
   rm .gclient_entries || true
   rm -rf _bad_scm/ || true
-
-  #if v8ver isn't v8, move the v8 folders
-  #back to what they were
-  if [ "$v8ver" != "v8" ]; then
-    mv v8 $v8ver
-    mv .v8old v8
+  rm -rf .v8old
+  if [ "$BRANCH" == "master" ]; then
+    echo "git cleanup if branch is master"
+    git ls-files -m | xargs git checkout --
+    git clean -fd >/dev/null
   fi
   exit 0
 }
 
 cd deps
-echo $gclient_string > .gclient
-if [ "$v8ver" != "v8" ]; then
-  mv v8 .v8old
-  mv $v8ver v8
+mv v8 .v8old
+
+echo "Fetching v8 from chromium.googlesource.com"
+fetch v8
+if [ "$?" -ne 0 ]; then
+  echo "V8 fetch failed"
+  exit 1
 fi
+echo "V8 fetched"
+
+cd v8
+
+echo "Checking out branch:$BRANCH"
+if [ "$BRANCH" != "master" ]; then
+   git fetch
+   git checkout origin/$BRANCH
+fi
+
+echo "Sync dependencies"
 gclient sync
+
+cd ..
 cleanup
