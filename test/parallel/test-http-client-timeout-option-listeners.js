65790af582998c9c3b820a2dee84da5634455343
Karl Böhlmark <karl.bohlmark@netinsight.se>
2016-11-01 16:59:31 +0100

    http: remove stale timeout listeners
    In order to prevent a memory leak when using keep alive, ensure that the
    timeout listener for the request is removed when the response has ended.

---
+++ test/parallel/test-http-client-timeout-option-listeners.js
@@ -0,0 +1,56 @@
+'use strict';
+require('../common');
+const http = require('http');
+const assert = require('assert');
+
+const agent = new http.Agent({ keepAlive: true });
+
+const server = http.createServer((req, res) => {
+  res.end('');
+});
+
+const options = {
+  agent,
+  method: 'GET',
+  port: undefined,
+  host: '127.0.0.1',
+  path: '/',
+  timeout: 10
+};
+
+server.listen(0, options.host, (e) => {
+  options.port = server.address().port;
+
+  doRequest().then(doRequest)
+    .then((numListeners) => {
+      assert.strictEqual(numListeners, 1,
+                         'Should be a single timeout listener on the socket');
+      server.close();
+      agent.destroy();
+    })
+    .catch((e) => {
+      console.log(e);
+      process.exit(1);
+    });
+});
+
+function doRequest() {
+  return new Promise((resolve, reject) => {
+    http.request(options, (response) => {
+      const sockets = agent.sockets[`127.0.0.1:${options.port}:`];
+      if (sockets.length !== 1) {
+        reject(new Error('Only one socket should be created'));
+      }
+      const socket = sockets[0];
+      const timeoutEvent = socket._events.timeout;
+      let numListeners = 0;
+      if (Array.isArray(timeoutEvent)) {
+        numListeners = timeoutEvent.length;
+      } else if (timeoutEvent) {
+        numListeners = 1;
+      }
+      response.resume();
+      response.on('end', () => resolve(numListeners));
+    }).end();
+  });
+}
