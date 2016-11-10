dfd96af1be8d917f36540527bfdee9289c0eb72f
Rich Trott <rtrott@gmail.com>
2016-11-10 11:26:20 -0800

    test: refactor test-zlib.js
    * minor layout changes for clarity
    * assert.equal() and assert.ok() swapped out for assert.strictEqual()
    * var -> const for modules included via require()
    
    PR-URL: https://github.com/nodejs/node/pull/9544
    Reviewed-By: Evan Lucas <evanlucas@me.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/parallel/test-zlib.js
+++ test/parallel/test-zlib.js
@@ -3,13 +3,17 @@ const common = require('../common');
 const assert = require('assert');
 const zlib = require('zlib');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const stream = require('stream');
 
-var zlibPairs =
-    [[zlib.Deflate, zlib.Inflate],
-     [zlib.Gzip, zlib.Gunzip],
-     [zlib.Deflate, zlib.Unzip],
-     [zlib.Gzip, zlib.Unzip],
-     [zlib.DeflateRaw, zlib.InflateRaw]];
+var zlibPairs = [
+  [zlib.Deflate, zlib.Inflate],
+  [zlib.Gzip, zlib.Gunzip],
+  [zlib.Deflate, zlib.Unzip],
+  [zlib.Gzip, zlib.Unzip],
+  [zlib.DeflateRaw, zlib.InflateRaw]
+];
 
 // how fast to trickle through the slowstream
 var trickle = [128, 1024, 1024 * 1024];
@@ -36,8 +40,6 @@ if (!process.env.PUMMEL) {
   strategy = [0];
 }
 
-var fs = require('fs');
-
 var testFiles = ['person.jpg', 'elipses.txt', 'empty.txt'];
 
 if (process.env.FAST) {
@@ -45,13 +47,10 @@ if (process.env.FAST) {
   testFiles = ['person.jpg'];
 }
 
-var tests = {};
+const tests = {};
 testFiles.forEach(function(file) {
   tests[file] = fs.readFileSync(path.resolve(common.fixturesDir, file));
 });
-
-var util = require('util');
-var stream = require('stream');
 
 
 // stream that saves everything
@@ -197,11 +196,16 @@ Object.keys(tests).forEach(function(file) {
                 ss.pipe(def).pipe(inf).pipe(buf);
                 ss.end(test);
               });
-            }); }); }); }); }); }); // sad stallman is sad.
+            });
+          });
+        });
+      });
+    });
+  });
 });
 
 process.on('exit', function(code) {
   console.log('1..' + done);
-  assert.equal(done, total, (total - done) + ' tests left unfinished');
-  assert.ok(!failures, 'some test failures');
+  assert.strictEqual(done, total, (total - done) + ' tests left unfinished');
+  assert.strictEqual(failures, 0, 'some test failures');
 });
