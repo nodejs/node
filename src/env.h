5a61b99d98c3df4198709445c2ff306068ba1ce1
Fedor Indutny <fedor@indutny.com>
2016-11-12 16:49:40 -0500

    process: add `process.memoryUsage.external`
    PR-URL: https://github.com/nodejs/node/pull/9587
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Johan Bergström <bugs@bergstroem.nu>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Roman Reiss <me@silverwind.io>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/env.h
+++ src/env.h
@@ -103,6 +103,7 @@ namespace node {
   V(exponent_string, "exponent")                                              \
   V(exports_string, "exports")                                                \
   V(ext_key_usage_string, "ext_key_usage")                                    \
+  V(external_string, "external")                                              \
   V(external_stream_string, "_externalStream")                                \
   V(family_string, "family")                                                  \
   V(fatal_exception_string, "_fatalException")                                \
