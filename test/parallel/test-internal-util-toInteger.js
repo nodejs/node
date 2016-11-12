241d32a01e255490231b70ac0ae06a47988a86a0
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2016-11-12 17:03:44 +0530

    simplify implementation and improve tests

--- test/parallel/test-internal-util-toInteger.js
+++ test/parallel/test-internal-util-toInteger.js
@@ -5,15 +5,28 @@ require('../common');
 const assert = require('assert');
 const {toInteger} = require('internal/util');
 
-assert.strictEqual(toInteger(+0), 0);
-assert.strictEqual(toInteger(-0), 0);
-assert.strictEqual(toInteger(NaN), 0);
+const expectZero = [
+  '0', '-0', NaN, {}, [], {'a': 'b'}, [1, 2], '0x', '0o', '0b', false,
+  '', ' ', undefined, null
+];
+expectZero.forEach(function(value) {
+  assert.strictEqual(toInteger(value), 0);
+});
+
 assert.strictEqual(toInteger(Infinity), Infinity);
 assert.strictEqual(toInteger(-Infinity), -Infinity);
 
-assert.strictEqual(toInteger(0.1), 0);
-assert.strictEqual(toInteger(-0.1), 0);
-assert.strictEqual(toInteger(0.5), 0);
-assert.strictEqual(toInteger(-0.5), 0);
-assert.strictEqual(toInteger(0.9), 0);
-assert.strictEqual(toInteger(-0.9), 0);
+const expectSame = [
+  '0x100', '0o100', '0b100', 0x100, -0x100, 0o100, -0o100, 0b100, -0b100, true
+];
+expectSame.forEach(function(value) {
+  assert.strictEqual(toInteger(value), +value, `${value} is not an Integer`);
+});
+
+const expectIntegers = new Map([
+  [[1], 1], [[-1], -1], [['1'], 1], [['-1'], -1],
+  [3.14, 3], [-3.14, -3], ['3.14', 3], ['-3.14', -3],
+]);
+expectIntegers.forEach(function(expected, value) {
+  assert.strictEqual(toInteger(value), expected);
+});
--- test/parallel/test-internal-util-toLength.js
+++ test/parallel/test-internal-util-toLength.js
@@ -4,17 +4,32 @@
 require('../common');
 const assert = require('assert');
 const {toLength} = require('internal/util');
+const maxValue = Number.MAX_SAFE_INTEGER;
 
-assert.strictEqual(toLength(+0), 0);
-assert.strictEqual(toLength(-0), 0);
-assert.strictEqual(toLength(NaN), 0);
-assert.strictEqual(toLength(Math.pow(2, 60)), 9007199254740991);
-assert.strictEqual(toLength(Infinity), 9007199254740991);
-assert.strictEqual(toLength(-Infinity), 0);
+const expectZero = [
+  '0', '-0', NaN, {}, [], {'a': 'b'}, [1, 2], '0x', '0o', '0b', false,
+  '', ' ', undefined, null, -1, -1.25, -1.1, -1.9, -Infinity
+];
+expectZero.forEach(function(value) {
+  assert.strictEqual(toLength(value), 0);
+});
 
-assert.strictEqual(toLength(0.1), 0);
-assert.strictEqual(toLength(-0.1), 0);
-assert.strictEqual(toLength(0.5), 0);
-assert.strictEqual(toLength(-0.5), 0);
-assert.strictEqual(toLength(0.9), 0);
-assert.strictEqual(toLength(-0.9), 0);
+assert.strictEqual(toLength(maxValue - 1), maxValue - 1);
+assert.strictEqual(maxValue, maxValue);
+assert.strictEqual(toLength(Infinity), maxValue);
+assert.strictEqual(toLength(maxValue + 1), maxValue);
+
+
+[
+  '0x100', '0o100', '0b100', 0x100, -0x100, 0o100, -0o100, 0b100, -0b100, true
+].forEach(function(value) {
+  assert.strictEqual(toLength(value), +value > 0 ? +value : 0);
+});
+
+const expectIntegers = new Map([
+  [[1], 1], [[-1], 0], [['1'], 1], [['-1'], 0],
+  [3.14, 3], [-3.14, 0], ['3.14', 3], ['-3.14', 0],
+]);
+expectIntegers.forEach(function(expected, value) {
+  assert.strictEqual(toLength(value), expected);
+});
