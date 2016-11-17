a220170861d62d7a488330896695ed7b4464a59e
Rich Trott <rtrott@gmail.com>
2016-11-17 10:25:57 -0800

    test: refactor test-async-wrap-*
    * `assert.equal()` -> `assert.strictEqual()`
    * add duration to `setTimeout()`
    
    PR-URL: https://github.com/nodejs/node/pull/9663
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Prince John Wesley <princejohnwesley@gmail.com>

--- test/parallel/test-async-wrap-propagate-parent.js
+++ test/parallel/test-async-wrap-propagate-parent.js
@@ -18,12 +18,12 @@ function init(uid, type, parentUid, parentHandle) {
     cntr++;
     // Cannot assert in init callback or will abort.
     process.nextTick(() => {
-      assert.equal(providers[type], 'TCPWRAP');
-      assert.equal(parentUid, server._handle[uidSymbol],
-                   'server uid doesn\'t match parent uid');
-      assert.equal(parentHandle, server._handle,
-                   'server handle doesn\'t match parent handle');
-      assert.equal(this, client._handle, 'client doesn\'t match context');
+      assert.strictEqual(providers[type], 'TCPWRAP');
+      assert.strictEqual(parentUid, server._handle[uidSymbol],
+                         'server uid doesn\'t match parent uid');
+      assert.strictEqual(parentHandle, server._handle,
+                         'server handle doesn\'t match parent handle');
+      assert.strictEqual(this, client._handle, 'client doesn\'t match context');
     });
   }
 }
@@ -47,5 +47,5 @@ const server = net.createServer(function(c) {
 
 process.on('exit', function() {
   // init should have only been called once with a parent.
-  assert.equal(cntr, 1);
+  assert.strictEqual(cntr, 1);
 });
