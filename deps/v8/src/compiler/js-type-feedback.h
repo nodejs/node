c1effb1255a7137316f9d6a8c12263adb40acbdc
Johan Bergström <bugs@bergstroem.nu>
2016-11-23 10:09:53 -0300

    deps: fix build with libc++ 3.8.0
    Make sure the map allocators are of the same type. This fixes
    building Node.js 4.x on libc++ 3.8.0 (for instance FreeBSD 11).
    
    Upstream bug/patch: https://bugs.freebsd.org/208467
    
    PR-URL: https://github.com/nodejs/node/pull/9763
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>
    Reviewed-By: Ali Ijaz Sheikh <ofrobots@google.com>

--- deps/v8/src/compiler/js-type-feedback.h
+++ deps/v8/src/compiler/js-type-feedback.h
@@ -33,9 +33,10 @@ class JSTypeFeedbackTable : public ZoneObject {
  private:
   friend class JSTypeFeedbackSpecializer;
   typedef std::map<NodeId, TypeFeedbackId, std::less<NodeId>,
-                   zone_allocator<TypeFeedbackId> > TypeFeedbackIdMap;
+                   zone_allocator<std::pair<const NodeId, TypeFeedbackId> > >
+      TypeFeedbackIdMap;
   typedef std::map<NodeId, FeedbackVectorICSlot, std::less<NodeId>,
-                   zone_allocator<FeedbackVectorICSlot> >
+                   zone_allocator<std::pair<const NodeId, FeedbackVectorICSlot> > >
       FeedbackVectorICSlotMap;
 
   TypeFeedbackIdMap type_feedback_id_map_;
