b49b496a92118bf786176bc3c763a292b9bdff5d
Trevor Norris <trev.norris@gmail.com>
2016-11-22 22:35:10 -0700

    async_wrap: call destroy() callback in uv_idle_t
    Calling JS during GC is a no-no. So intead create a queue of all ids
    that need to have their destroy() callback called and call them later.
    
    Removed checking destroy() in test-async-wrap-uid because destroy() can
    be called after the 'exit' callback.
    
    Missing a reliable test to reproduce the issue that caused the
    FATAL_ERROR.
    
    PR-URL: https://github.com/nodejs/node/pull/9753
    Fixes: https://github.com/nodejs/node/issues/8216
    Fixes: https://github.com/nodejs/node/issues/9465
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/async-wrap.h
+++ src/async-wrap.h
@@ -4,6 +4,7 @@
 #if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
 
 #include "base-object.h"
+#include "uv.h"
 #include "v8.h"
 
 #include <stdint.h>
@@ -59,6 +60,8 @@ class AsyncWrap : public BaseObject {
   static void Initialize(v8::Local<v8::Object> target,
                          v8::Local<v8::Value> unused,
                          v8::Local<v8::Context> context);
+
+  static void DestroyIdsCb(uv_idle_t* handle);
 
   inline ProviderType provider_type() const;
 
--- src/env-inl.h
+++ src/env-inl.h
@@ -194,6 +194,8 @@ inline Environment::Environment(IsolateData* isolate_data,
 
   RB_INIT(&cares_task_list_);
   AssignToContext(context);
+
+  destroy_ids_list_.reserve(512);
 }
 
 inline Environment::~Environment() {
@@ -245,6 +247,15 @@ inline uv_check_t* Environment::immediate_check_handle() {
 
 inline uv_idle_t* Environment::immediate_idle_handle() {
   return &immediate_idle_handle_;
+}
+
+inline Environment* Environment::from_destroy_ids_idle_handle(
+    uv_idle_t* handle) {
+  return ContainerOf(&Environment::destroy_ids_idle_handle_, handle);
+}
+
+inline uv_idle_t* Environment::destroy_ids_idle_handle() {
+  return &destroy_ids_idle_handle_;
 }
 
 inline void Environment::RegisterHandleCleanup(uv_handle_t* handle,
@@ -299,6 +310,10 @@ inline void Environment::set_trace_sync_io(bool value) {
 
 inline int64_t Environment::get_async_wrap_uid() {
   return ++async_wrap_uid_;
+}
+
+inline std::vector<int64_t>* Environment::destroy_ids_list() {
+  return &destroy_ids_list_;
 }
 
 inline uint32_t* Environment::heap_statistics_buffer() const {
