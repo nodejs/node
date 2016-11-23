dbf626d18a0b2ea5679bdeb71a536fb6341e0790
Christopher J. Brody <brodybits@litehelpers.net>
2016-11-23 11:53:13 +0100

    url: mark ignored return value in node::url::Parse(...)
    This is to resolve an unused result warning in node_url.cc.

--- src/node_url.cc
+++ src/node_url.cc
@@ -1296,7 +1296,7 @@ namespace url {
         argv[ARG_PATH] = Copy(isolate, url.path);
     }
 
-    cb->Call(context, recv, 9, argv);
+    (void)cb->Call(context, recv, 9, argv);
   }
 
   static void Parse(const FunctionCallbackInfo<Value>& args) {
