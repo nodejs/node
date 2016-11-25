60ff0fb12e5ba487ac09e27a6a97b1f03c3986d3
Karl Böhlmark <karl.bohlmark@netinsight.se>
2016-11-25 17:52:14 +0100

    test in callback-style

--- test/parallel/test-http-client-timeout-option-listeners.js
+++ test/parallel/test-http-client-timeout-option-listeners.js
@@ -15,41 +15,35 @@ const options = {
   port: undefined,
   host: common.localhostIPv4,
   path: '/',
-  timeout: common.platformTimeout(20)
+  timeout: common.platformTimeout(100)
 };
-
-process.on('unhandledRejection', function(reason) {
-  common.fail('A promise rejection was unhandled: ' + reason);
-});
 
 server.listen(0, options.host, (e) => {
   options.port = server.address().port;
-
-  doRequest().then(doRequest)
-    .then((numListeners) => {
+  doRequest(() => {
+    doRequest((numListeners) => {
       assert.strictEqual(numListeners, 1);
       server.close();
       agent.destroy();
     });
+  });
 });
 
-function doRequest() {
-  return new Promise((resolve, reject) => {
-    http.request(options, (response) => {
-      const sockets = agent.sockets[`${options.host}:${options.port}:`];
-      if (sockets.length !== 1) {
-        return reject(new Error('Only one socket should be created'));
-      }
-      const socket = sockets[0];
-      const timeoutEvent = socket._events.timeout;
-      let numListeners = 0;
-      if (Array.isArray(timeoutEvent)) {
-        numListeners = timeoutEvent.length;
-      } else if (timeoutEvent) {
-        numListeners = 1;
-      }
-      response.resume();
-      response.on('end', () => resolve(numListeners));
-    }).end();
-  });
+function doRequest(cb) {
+  http.request(options, (response) => {
+    const sockets = agent.sockets[`${options.host}:${options.port}:`];
+    assert.strictEqual(sockets.length, 1)
+    const socket = sockets[0];
+    const timeoutEvent = socket._events.timeout;
+    let numListeners = 0;
+    if (Array.isArray(timeoutEvent)) {
+      numListeners = timeoutEvent.length;
+    } else if (timeoutEvent) {
+      numListeners = 1;
+    }
+    response.resume();
+    response.once('end', () => {
+      process.nextTick(cb, numListeners);
+    });
+  }).end();
 }
