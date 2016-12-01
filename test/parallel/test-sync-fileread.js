98496b6d3e4eaca827450966f443880440bd5bc0
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-12-01 10:36:16 -0600

    test: test: refactor test-sync-fileread
    change equal to strictEqual and var to const
    
    PR-URL: https://github.com/nodejs/node/pull/9941
    Reviewed-By: Prince John Wesley <princejohnwesley@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-sync-fileread.js
+++ test/parallel/test-sync-fileread.js
@@ -1,9 +1,9 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var path = require('path');
-var fs = require('fs');
+const common = require('../common');
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
 
-var fixture = path.join(common.fixturesDir, 'x.txt');
+const fixture = path.join(common.fixturesDir, 'x.txt');
 
-assert.equal('xyz\n', fs.readFileSync(fixture));
+assert.strictEqual(fs.readFileSync(fixture).toString(), 'xyz\n');
