4cc813d8b9cc0377be77320e77f03ffb776c2bd0
Oscar Martinez <oscar.martinez@hautelook.com>
2016-12-01 10:54:07 -0600

    test: refactor test-cluster-setup-master-argv
    - updated vars to const
    - add common.mustCall() to setup callback
    
    PR-URL: https://github.com/nodejs/node/pull/9960
    Reviewed-By: Prince John Wesley <princejohnwesley@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-cluster-setup-master-argv.js
+++ test/parallel/test-cluster-setup-master-argv.js
@@ -1,16 +1,16 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var cluster = require('cluster');
+const common = require('../common');
+const assert = require('assert');
+const cluster = require('cluster');
 
 setTimeout(common.fail.bind(assert, 'setup not emitted'), 1000).unref();
 
-cluster.on('setup', function() {
+cluster.on('setup', common.mustCall(function() {
   var clusterArgs = cluster.settings.args;
   var realArgs = process.argv;
   assert.strictEqual(clusterArgs[clusterArgs.length - 1],
                      realArgs[realArgs.length - 1]);
-});
+}));
 
 assert.notStrictEqual(process.argv[process.argv.length - 1], 'OMG,OMG');
 process.argv.push('OMG,OMG');
