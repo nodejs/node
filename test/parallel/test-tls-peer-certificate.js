c30332daa66e466bdc67e99717899a97f5d4dc2a
Ilya Potuzhnov <ipotuzhnov@gmail.com>
2016-12-01 11:13:38 -0600

    test: Modernize test-tls-peer-certificate.js
    Modernize `test/parallel/test-tls-peer-certificate.js` according to the
    following rules:
    * `var` --> `const`/`let`
    * `assert.equal` --> `assert.strictEqual`
    * `assert.ok(a === b)` --> `assert.strictEqual(a, b)`
    
    PR-URL: https://github.com/nodejs/node/pull/10014
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-by: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/parallel/test-tls-peer-certificate.js
+++ test/parallel/test-tls-peer-certificate.js
@@ -1,32 +1,32 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
 if (!common.hasCrypto) {
   common.skip('missing crypto');
   return;
 }
-var tls = require('tls');
+const tls = require('tls');
 
-var fs = require('fs');
-var util = require('util');
-var join = require('path').join;
+const fs = require('fs');
+const util = require('util');
+const join = require('path').join;
 
-var options = {
+const options = {
   key: fs.readFileSync(join(common.fixturesDir, 'keys', 'agent1-key.pem')),
   cert: fs.readFileSync(join(common.fixturesDir, 'keys', 'agent1-cert.pem')),
   ca: [ fs.readFileSync(join(common.fixturesDir, 'keys', 'ca1-cert.pem')) ]
 };
 
-var server = tls.createServer(options, function(cleartext) {
+const server = tls.createServer(options, function(cleartext) {
   cleartext.end('World');
 });
 server.listen(0, common.mustCall(function() {
-  var socket = tls.connect({
+  const socket = tls.connect({
     port: this.address().port,
     rejectUnauthorized: false
   }, common.mustCall(function() {
-    var peerCert = socket.getPeerCertificate();
+    let peerCert = socket.getPeerCertificate();
     assert.ok(!peerCert.issuerCertificate);
 
     // Verify that detailed return value has all certs
@@ -34,17 +34,19 @@ server.listen(0, common.mustCall(function() {
     assert.ok(peerCert.issuerCertificate);
 
     console.error(util.inspect(peerCert));
-    assert.equal(peerCert.subject.emailAddress, 'ry@tinyclouds.org');
-    assert.equal(peerCert.serialNumber, '9A84ABCFB8A72AC0');
-    assert.equal(peerCert.exponent, '0x10001');
-    assert.equal(peerCert.fingerprint,
-                 '8D:06:3A:B3:E5:8B:85:29:72:4F:7D:1B:54:CD:95:19:3C:EF:6F:AA');
+    assert.strictEqual(peerCert.subject.emailAddress, 'ry@tinyclouds.org');
+    assert.strictEqual(peerCert.serialNumber, '9A84ABCFB8A72AC0');
+    assert.strictEqual(peerCert.exponent, '0x10001');
+    assert.strictEqual(
+      peerCert.fingerprint,
+      '8D:06:3A:B3:E5:8B:85:29:72:4F:7D:1B:54:CD:95:19:3C:EF:6F:AA'
+    );
     assert.deepStrictEqual(peerCert.infoAccess['OCSP - URI'],
                            [ 'http://ocsp.nodejs.org/' ]);
 
-    var issuer = peerCert.issuerCertificate;
-    assert.ok(issuer.issuerCertificate === issuer);
-    assert.equal(issuer.serialNumber, '8DF21C01468AF393');
+    const issuer = peerCert.issuerCertificate;
+    assert.strictEqual(issuer.issuerCertificate, issuer);
+    assert.strictEqual(issuer.serialNumber, '8DF21C01468AF393');
     server.close();
   }));
   socket.end('Hello');
