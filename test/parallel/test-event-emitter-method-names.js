64882a61ab6a33595b12e59c7465256d99fe24fa
Rodrigo Palma <rpalma@mx1.ibm.com>
2016-12-01 11:24:38 -0600

    test: change the equal() by strictEqual()
    Improved test by using strictEqual instead of equal

--- test/parallel/test-event-emitter-method-names.js
+++ test/parallel/test-event-emitter-method-names.js
@@ -4,10 +4,10 @@ var assert = require('assert');
 var events = require('events');
 
 var E = events.EventEmitter.prototype;
-assert.equal(E.constructor.name, 'EventEmitter');
-assert.equal(E.on, E.addListener);  // Same method.
+assert.strictEqual(E.constructor.name, 'EventEmitter');
+assert.strictEqual(E.on, E.addListener);  // Same method.
 Object.getOwnPropertyNames(E).forEach(function(name) {
   if (name === 'constructor' || name === 'on') return;
   if (typeof E[name] !== 'function') return;
-  assert.equal(E[name].name, name);
+  assert.strictEqual(E[name].name, name);
 });
