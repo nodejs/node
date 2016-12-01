91b942ec8a8785dda0d18c15fa9e6740856da450
Johnny Reading <johnny@hellowallet.com>
2016-12-01 10:43:40 -0600

    test: refactor child-process-spawn-error
    Use const instead of var for assignments.
    
    PR-URL: https://github.com/nodejs/node/pull/9951
    Reviewed-By: Prince John Wesley <princejohnwesley@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-child-process-spawn-error.js
+++ test/parallel/test-child-process-spawn-error.js
@@ -1,13 +1,13 @@
 'use strict';
-var common = require('../common');
-var spawn = require('child_process').spawn;
-var assert = require('assert');
+const common = require('../common');
+const spawn = require('child_process').spawn;
+const assert = require('assert');
 
-var enoentPath = 'foo123';
-var spawnargs = ['bar'];
+const enoentPath = 'foo123';
+const spawnargs = ['bar'];
 assert.strictEqual(common.fileExists(enoentPath), false);
 
-var enoentChild = spawn(enoentPath, spawnargs);
+const enoentChild = spawn(enoentPath, spawnargs);
 enoentChild.on('error', common.mustCall(function(err) {
   assert.strictEqual(err.code, 'ENOENT');
   assert.strictEqual(err.errno, 'ENOENT');
