9747c043691c4d42a217fe7d0ab7b81e3c65765b
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-12-01 14:46:38 -0500

    common.mustCall's, follow punctuation and syntax rules

--- test/parallel/test-stream-readable-readingMore.js
+++ test/parallel/test-stream-readable-readingMore.js
@@ -1,31 +1,31 @@
 'use strict';
-const Readable = require('stream').Readable,
-	assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
+const Readable = require('stream').Readable;
 
 const readable = new Readable({
 	read(size) {}
 });
 
-// false initially
+// Starting off with false initially.
 assert.strictEqual(readable._readableState.readingMore, false);
 
-readable.on('data', data => {
-	// still in a flowing state, try to read more
+readable.on('data', common.mustCall((data) => {
+	// Still in a flowing state, should try to read more.
 	assert.strictEqual(readable._readableState.readingMore, true);
-});
+}));
 
-readable.on('end', () => {
-	// end of stream
-	// reading is false, and so should be readingMore
+readable.on('end', common.mustCall(() => {
+	// End of stream; _readableState.reading is false
+	// And so should be readingMore.
 	assert.strictEqual(readable._readableState.readingMore, false);
-});
+}));
 
-
-readable.push("abc");
-readable.push(null);
+readable.push('abc');
+readable.push(null); // end
 
 process.nextTick(() => {
-	// finished reading
-	// reading is false, and so should be readingMore
+	// Finished reading. _readableState.reading is false.
+	// readingMore then should be false as well.
 	assert.strictEqual(readable._readableState.readingMore, false);
 });
