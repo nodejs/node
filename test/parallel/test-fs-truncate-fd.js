f102955b3556264a96261515e3bb5558e9fa444d
Nigel Kibodeaux <nigel@team.about.me>
2016-12-01 11:06:29 -0600

    test: Modernize test-fs-truncate-fd
    - changed `var` to `const` where variables were not reassigned.
    - changed `assert.equal` to `assert.strictEqual` because there was no
    downside to being more rigorous.

--- test/parallel/test-fs-truncate-fd.js
+++ test/parallel/test-fs-truncate-fd.js
@@ -1,17 +1,17 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var path = require('path');
-var fs = require('fs');
-var tmp = common.tmpDir;
+const common = require('../common');
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
+const tmp = common.tmpDir;
 common.refreshTmpDir();
-var filename = path.resolve(tmp, 'truncate-file.txt');
+const filename = path.resolve(tmp, 'truncate-file.txt');
 
 fs.writeFileSync(filename, 'hello world', 'utf8');
-var fd = fs.openSync(filename, 'r+');
+const fd = fs.openSync(filename, 'r+');
 fs.truncate(fd, 5, common.mustCall(function(err) {
   assert.ok(!err);
-  assert.equal(fs.readFileSync(filename, 'utf8'), 'hello');
+  assert.strictEqual(fs.readFileSync(filename, 'utf8'), 'hello');
 }));
 
 process.on('exit', function() {
