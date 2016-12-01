27c91715862e3c986cf5e05f94a503b8e8e22b98
Bidur Adhikari <badhikari@godaddy.com>
2016-12-01 11:51:06 -0600

    test: use assert.strictEqual() cluster test
    Updated test-cluster-shared-handle-bind-privileged-port test
    method to use assert.strictEqual() instead of assert.equal()
    
    PR-URL: https://github.com/nodejs/node/pull/10042
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

--- test/parallel/test-cluster-shared-handle-bind-privileged-port.js
+++ test/parallel/test-cluster-shared-handle-bind-privileged-port.js
@@ -18,13 +18,13 @@ if (cluster.isMaster) {
   // Master opens and binds the socket and shares it with the worker.
   cluster.schedulingPolicy = cluster.SCHED_NONE;
   cluster.fork().on('exit', common.mustCall(function(exitCode) {
-    assert.equal(exitCode, 0);
+    assert.strictEqual(exitCode, 0);
   }));
 } else {
   var s = net.createServer(common.fail);
   s.listen(42, common.fail.bind(null, 'listen should have failed'));
   s.on('error', common.mustCall(function(err) {
-    assert.equal(err.code, 'EACCES');
+    assert.strictEqual(err.code, 'EACCES');
     process.disconnect();
   }));
 }
