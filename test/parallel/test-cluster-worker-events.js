eeabddea4a9a3dd5cb686449ea0a5f19c0816830
fmizzell <fmizzell@1312210.no-reply.drupal.org>
2016-12-01 17:19:34 +0000

    test: refactor test-cluster-worker-events
    Use assert.strictEqual() instead of assert.equal()

--- test/parallel/test-cluster-worker-events.js
+++ test/parallel/test-cluster-worker-events.js
@@ -10,7 +10,7 @@ if (cluster.isMaster) {
   var worker = cluster.fork();
 
   worker.on('exit', function(code) {
-    assert.equal(code, OK);
+    assert.strictEqual(code, OK);
     process.exit(0);
   });
 
@@ -50,7 +50,7 @@ function check(m) {
   assert.deepStrictEqual(messages[0], messages[1]);
 
   cluster.worker.once('error', function(e) {
-    assert.equal(e, 'HI');
+    assert.strictEqual(e, 'HI');
     process.exit(OK);
   });
 
