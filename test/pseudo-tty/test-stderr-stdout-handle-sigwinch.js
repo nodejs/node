1fb5d71ecc492d3ad2185fe5caadcefed31d4653
Sarah Meyer <sarahsaltrick@gmail.com>
2016-12-01 13:16:46 -0600

    test: adding test coverage for SIGWINCH in stdio.js, call original function from wrapper

--- test/pseudo-tty/test-stderr-stdout-handle-sigwinch.js
+++ test/pseudo-tty/test-stderr-stdout-handle-sigwinch.js
@@ -2,13 +2,17 @@
 require('../common');
 
 // if printing, add to out file
+const originalRefreshSizeWrapperStderr = process.stderr._refreshSize;
+const originalRefreshSizeWrapperStdout = process.stdout._refreshSize;
 
 const refreshSizeWrapperStderr = () => {
   console.log('calling stderr._refreshSize');
+  originalRefreshSizeWrapperStderr.call(process.stderr);
 };
 
 const refreshSizeWrapperStdout = () => {
   console.log('calling stdout._refreshSize');
+  originalRefreshSizeWrapperStdout.call(process.stdout);
 };
 
 process.stderr._refreshSize = refreshSizeWrapperStderr;
