e1394eeb162abb5668a9342406986cc89c11a171
Adrian Estrada <edsadr@gmail.com>
2016-12-01 10:54:34 -0600

    test: refactor test-tls-friendly-error-message.js
    Replaces var with const and adds common.mustCall().
    
    PR-URL: https://github.com/nodejs/node/pull/9967
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-tls-friendly-error-message.js
+++ test/parallel/test-tls-friendly-error-message.js
@@ -1,26 +1,26 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
 if (!common.hasCrypto) {
   common.skip('missing crypto');
   return;
 }
-var tls = require('tls');
+const tls = require('tls');
 
-var fs = require('fs');
+const fs = require('fs');
 
-var key = fs.readFileSync(common.fixturesDir + '/keys/agent1-key.pem');
-var cert = fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem');
+const key = fs.readFileSync(common.fixturesDir + '/keys/agent1-key.pem');
+const cert = fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem');
 
-tls.createServer({ key: key, cert: cert }, function(conn) {
+tls.createServer({ key: key, cert: cert }, common.mustCall(function(conn) {
   conn.end();
   this.close();
-}).listen(0, function() {
+})).listen(0, common.mustCall(function() {
   var options = { port: this.address().port, rejectUnauthorized: true };
   tls.connect(options).on('error', common.mustCall(function(err) {
-    assert.equal(err.code, 'UNABLE_TO_VERIFY_LEAF_SIGNATURE');
-    assert.equal(err.message, 'unable to verify the first certificate');
+    assert.strictEqual(err.code, 'UNABLE_TO_VERIFY_LEAF_SIGNATURE');
+    assert.strictEqual(err.message, 'unable to verify the first certificate');
     this.destroy();
   }));
-});
+}));
