afbd8df7fd6b7dcac2d051ffdbdc97bf2e76bebb
Safia Abdalla <safia@safia.rocks>
2016-11-30 22:04:28 -0600

    test: increase coverage for lib/events.js
    Adds tests for the case in which listeners() is invoked on a
    EventEmitter with no events.
    
    Adds a new test case for the situation in which a class
    inherits from the EventEmitter but overrides the constructor
    in the EventEmitter so that the _events is never set.
    
    PR-URL: https://github.com/nodejs/node/pull/9865
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>

--- test/parallel/test-event-emitter-listeners.js
+++ test/parallel/test-event-emitter-listeners.js
@@ -3,9 +3,12 @@
 require('../common');
 const assert = require('assert');
 const events = require('events');
+const util = require('util');
 
 function listener() {}
 function listener2() {}
+class TestStream { constructor() { } }
+util.inherits(TestStream, events.EventEmitter);
 
 {
   const ee = new events.EventEmitter();
@@ -35,4 +38,15 @@ function listener2() {}
   ee.on('foo', listener2);
   assert.deepStrictEqual(ee.listeners('foo'), [listener, listener2]);
   assert.deepStrictEqual(eeListenersCopy, [listener]);
+}
+
+{
+  const ee = new events.EventEmitter();
+  ee._events = undefined;
+  assert.deepStrictEqual(ee.listeners('foo'), []);
+}
+
+{
+  const s = new TestStream();
+  assert.deepStrictEqual(s.listeners('foo'), []);
 }
