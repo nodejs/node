5009de4a53cdfac8f65d6cf2aa115540d6da08a7
Christine Hong <christine@dogvacay.com>
2016-12-01 11:09:07 -0600

    test: refactor test-cluster-setup-master-argv
    Change assert.equal to assert.strictEqual.
    
    PR-URL: https://github.com/nodejs/node/pull/9993
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-cluster-setup-master-argv.js
+++ test/parallel/test-cluster-setup-master-argv.js
@@ -8,8 +8,8 @@ setTimeout(common.fail.bind(assert, 'setup not emitted'), 1000).unref();
 cluster.on('setup', function() {
   var clusterArgs = cluster.settings.args;
   var realArgs = process.argv;
-  assert.equal(clusterArgs[clusterArgs.length - 1],
-               realArgs[realArgs.length - 1]);
+  assert.strictEqual(clusterArgs[clusterArgs.length - 1],
+                     realArgs[realArgs.length - 1]);
 });
 
 assert.notStrictEqual(process.argv[process.argv.length - 1], 'OMG,OMG');
