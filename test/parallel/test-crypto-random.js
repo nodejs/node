cd1a7ea5e5c17fa44ee6402f41e72876c3235c77
Nate <nfoxley@stedwards.edu>
2016-12-01 11:32:14 -0600

    test: add regex to text-crypto-random
    PR-URL: https://github.com/nodejs/node/pull/10020
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/parallel/test-crypto-random.js
+++ test/parallel/test-crypto-random.js
@@ -13,10 +13,11 @@ crypto.DEFAULT_ENCODING = 'buffer';
 // bump, we register a lot of exit listeners
 process.setMaxListeners(256);
 
+const expectedErrorRegexp = /^TypeError: size must be a number >= 0$/;
 [crypto.randomBytes, crypto.pseudoRandomBytes].forEach(function(f) {
   [-1, undefined, null, false, true, {}, []].forEach(function(value) {
-    assert.throws(function() { f(value); }, TypeError);
-    assert.throws(function() { f(value, function() {}); }, TypeError);
+    assert.throws(function() { f(value); }, expectedErrorRegexp);
+    assert.throws(function() { f(value, function() {}); }, expectedErrorRegexp);
   });
 
   [0, 1, 2, 4, 16, 256, 1024].forEach(function(len) {
