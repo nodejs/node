22226fa900d5118e3f5af919ceda4cfbdc426a12
Travis Bretton <bluesailstudio@gmail.com>
2016-12-01 10:45:24 -0700

    test: refactoring test-pipe-head
    - Updated assert.equal to assert.strictEqual
    - Updated 'var' to 'const'
    - Using template literals
    
    PR-URL: https://github.com/nodejs/node/pull/10036
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Italo A. Casas <me@italoacasas.com>

--- test/parallel/test-pipe-head.js
+++ test/parallel/test-pipe-head.js
@@ -1,17 +1,17 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
-var exec = require('child_process').exec;
-var join = require('path').join;
+const exec = require('child_process').exec;
+const join = require('path').join;
 
-var nodePath = process.argv[0];
-var script = join(common.fixturesDir, 'print-10-lines.js');
+const nodePath = process.argv[0];
+const script = join(common.fixturesDir, 'print-10-lines.js');
 
-var cmd = '"' + nodePath + '" "' + script + '" | head -2';
+const cmd = `"${nodePath}" "${script}" | head -2`;
 
 exec(cmd, common.mustCall(function(err, stdout, stderr) {
-  if (err) throw err;
-  var lines = stdout.split('\n');
-  assert.equal(3, lines.length);
+  assert.ifError(err);
+  const lines = stdout.split('\n');
+  assert.strictEqual(3, lines.length);
 }));
