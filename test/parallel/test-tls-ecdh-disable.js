7ba944aa84a5cefc4117fb7c1bd6d458214e3b65
Aaron Williams <aaron.williams@stratfor.com>
2016-12-01 10:38:22 -0600

    test: refactor test-tls-ecdh-disable
    * use common.mustCall()
    * use String.prototype.includes() instead of String.prototype.indexOf()
    
    PR-URL: https://github.com/nodejs/node/pull/9989
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-tls-ecdh-disable.js
+++ test/parallel/test-tls-ecdh-disable.js
@@ -20,7 +20,7 @@ var options = {
 
 var server = tls.createServer(options, common.fail);
 
-server.listen(0, '127.0.0.1', function() {
+server.listen(0, '127.0.0.1', common.mustCall(function() {
   var cmd = '"' + common.opensslCli + '" s_client -cipher ' + options.ciphers +
             ` -connect 127.0.0.1:${this.address().port}`;
 
@@ -28,10 +28,10 @@ server.listen(0, '127.0.0.1', function() {
   if (common.isWindows)
     cmd += ' -no_rand_screen';
 
-  exec(cmd, function(err, stdout, stderr) {
+  exec(cmd, common.mustCall(function(err, stdout, stderr) {
     // Old versions of openssl will still exit with 0 so we
     // can't just check if err is not null.
-    assert.notEqual(stderr.indexOf('handshake failure'), -1);
+    assert(stderr.includes('handshake failure'));
     server.close();
-  });
-});
+  }));
+}));
