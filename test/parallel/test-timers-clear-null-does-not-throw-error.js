3db67892ba4d8ce19625dcb9956b7116922bb780
lrlna <shestak.irina@gmail.com>
2016-12-01 15:02:34 -0600

    add changes requested by @fishrock123

--- test/parallel/test-timers-clear-null-does-not-throw-error.js
+++ test/parallel/test-timers-clear-null-does-not-throw-error.js
@@ -5,14 +5,14 @@ const assert = require('assert');
 // This test makes sure clearing timers with
 // 'null' or no input does not throw error
 
-assert.doesNotThrow(() => { clearInterval(null);});
+assert.doesNotThrow(() => clearInterval(null));
 
-assert.doesNotThrow(() => { clearInterval();});
+assert.doesNotThrow(() => clearInterval());
 
-assert.doesNotThrow(() => { clearTimeout(null);});
+assert.doesNotThrow(() => clearTimeout(null));
 
-assert.doesNotThrow(() => { clearTimeout();});
+assert.doesNotThrow(() => clearTimeout());
 
-assert.doesNotThrow(() => { clearInterval(null);});
+assert.doesNotThrow(() => clearInterval(null));
 
-assert.doesNotThrow(() => { clearInterval();});
+assert.doesNotThrow(() => clearInterval());
