65c44830c2aa9049a28efe67bc8e212b34f2333c
Sean Villars <stvillars8@gmail.com>
2016-12-01 10:15:40 -0600

    test: var to const, assert.equal to assert.strictEqual in net
    * var -> const
    * assert.equal -> assert.strictEqual
    
    PR-URL: https://github.com/nodejs/node/pull/9907
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Prince John Wesley <princejohnwesley@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-net-better-error-messages-port-hostname.js
+++ test/parallel/test-net-better-error-messages-port-hostname.js
@@ -1,14 +1,14 @@
 'use strict';
-var common = require('../common');
-var net = require('net');
-var assert = require('assert');
+const common = require('../common');
+const net = require('net');
+const assert = require('assert');
 
-var c = net.createConnection(common.PORT, '***');
+const c = net.createConnection(common.PORT, '***');
 
 c.on('connect', common.fail);
 
 c.on('error', common.mustCall(function(e) {
-  assert.equal(e.code, 'ENOTFOUND');
-  assert.equal(e.port, common.PORT);
-  assert.equal(e.hostname, '***');
+  assert.strictEqual(e.code, 'ENOTFOUND');
+  assert.strictEqual(e.port, common.PORT);
+  assert.strictEqual(e.hostname, '***');
 }));
