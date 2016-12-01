4d238654460c96d586128d4aa386c95cd2f335a1
Adriana Rios <amrios2817@gmail.com>
2016-12-01 09:54:26 -0600

    test: refactor test-tls-ecdh
    clear out const/let
    change assert.notEqual to assert
    move assert after common.hasCrypto check
    
    PR-URL: https://github.com/nodejs/node/pull/9878
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/parallel/test-tls-ecdh.js
+++ test/parallel/test-tls-ecdh.js
@@ -1,31 +1,31 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
 
 if (!common.hasCrypto) {
   common.skip('missing crypto');
   return;
 }
-var tls = require('tls');
+const assert = require('assert');
+const tls = require('tls');
 
-var exec = require('child_process').exec;
-var fs = require('fs');
+const exec = require('child_process').exec;
+const fs = require('fs');
 
-var options = {
+const options = {
   key: fs.readFileSync(common.fixturesDir + '/keys/agent2-key.pem'),
   cert: fs.readFileSync(common.fixturesDir + '/keys/agent2-cert.pem'),
   ciphers: '-ALL:ECDHE-RSA-AES128-SHA256',
   ecdhCurve: 'prime256v1'
 };
 
-var reply = 'I AM THE WALRUS'; // something recognizable
+const reply = 'I AM THE WALRUS'; // something recognizable
 
-var server = tls.createServer(options, common.mustCall(function(conn) {
+const server = tls.createServer(options, common.mustCall(function(conn) {
   conn.end(reply);
 }));
 
 server.listen(0, '127.0.0.1', common.mustCall(function() {
-  var cmd = '"' + common.opensslCli + '" s_client -cipher ' + options.ciphers +
+  let cmd = '"' + common.opensslCli + '" s_client -cipher ' + options.ciphers +
             ` -connect 127.0.0.1:${this.address().port}`;
 
   // for the performance and stability issue in s_client on Windows
@@ -34,7 +34,7 @@ server.listen(0, '127.0.0.1', common.mustCall(function() {
 
   exec(cmd, common.mustCall(function(err, stdout, stderr) {
     if (err) throw err;
-    assert.notEqual(stdout.indexOf(reply), -1);
+    assert(stdout.includes(reply));
     server.close();
   }));
 }));
