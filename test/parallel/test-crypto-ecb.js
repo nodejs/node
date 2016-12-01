f900753eeb4c507fce04836b5a0c9a67429ad363
michael6 <michaelf614@gmail.com>
2016-12-01 11:18:33 -0600

    test: refactor test-crypto-ecb
    * var -> const/let
    * IIFE to blocks
    
    PR-URL: https://github.com/nodejs/node/pull/10029
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-crypto-ecb.js
+++ test/parallel/test-crypto-ecb.js
@@ -1,6 +1,6 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
 if (!common.hasCrypto) {
   common.skip('missing crypto');
@@ -10,24 +10,25 @@ if (common.hasFipsCrypto) {
   common.skip('BF-ECB is not FIPS 140-2 compatible');
   return;
 }
-var crypto = require('crypto');
+const crypto = require('crypto');
 
 crypto.DEFAULT_ENCODING = 'buffer';
 
 // Testing whether EVP_CipherInit_ex is functioning correctly.
 // Reference: bug#1997
 
-(function() {
-  var encrypt = crypto.createCipheriv('BF-ECB', 'SomeRandomBlahz0c5GZVnR', '');
-  var hex = encrypt.update('Hello World!', 'ascii', 'hex');
+{
+  const encrypt =
+    crypto.createCipheriv('BF-ECB', 'SomeRandomBlahz0c5GZVnR', '');
+  let hex = encrypt.update('Hello World!', 'ascii', 'hex');
   hex += encrypt.final('hex');
   assert.strictEqual(hex.toUpperCase(), '6D385F424AAB0CFBF0BB86E07FFB7D71');
-}());
+}
 
-(function() {
-  var decrypt = crypto.createDecipheriv('BF-ECB', 'SomeRandomBlahz0c5GZVnR',
-      '');
-  var msg = decrypt.update('6D385F424AAB0CFBF0BB86E07FFB7D71', 'hex', 'ascii');
+{
+  const decrypt =
+    crypto.createDecipheriv('BF-ECB', 'SomeRandomBlahz0c5GZVnR', '');
+  let msg = decrypt.update('6D385F424AAB0CFBF0BB86E07FFB7D71', 'hex', 'ascii');
   msg += decrypt.final('ascii');
   assert.strictEqual(msg, 'Hello World!');
-}());
+}
