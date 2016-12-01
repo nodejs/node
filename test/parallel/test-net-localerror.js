4a204ee24c2ec545317821ec4e11b31f3a9283ed
Daniel Flores <dflores009@gmail.com>
2016-12-01 10:12:47 -0600

    test: Update to const and use regex for assertions
    Use const over var.
    
    Assert error message with regex.
    
    PR-URL: https://github.com/nodejs/node/pull/9891
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-net-localerror.js
+++ test/parallel/test-net-localerror.js
@@ -1,19 +1,19 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
-var net = require('net');
+const common = require('../common');
+const assert = require('assert');
+const net = require('net');
 
 connect({
   host: 'localhost',
   port: common.PORT,
   localPort: 'foobar',
-}, 'localPort should be a number: foobar');
+}, /^TypeError: "localPort" option should be a number: foobar$/);
 
 connect({
   host: 'localhost',
   port: common.PORT,
   localAddress: 'foobar',
-}, 'localAddress should be a valid IP: foobar');
+}, /^TypeError: "localAddress" option must be a valid IP: foobar$/);
 
 function connect(opts, msg) {
   assert.throws(function() {
