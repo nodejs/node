e9b2325d68e3c24d90515d1567e34642edc0c64f
Wallace Zhang <zhangwnn1@gmail.com>
2016-12-01 12:07:23 -0600

    test: test error messages in test-dns-regress-7070
    Add a RegExp as a second argument to assert.throws().
    
    PR-URL: https://github.com/nodejs/node/pull/10058
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-dns-regress-7070.js
+++ test/parallel/test-dns-regress-7070.js
@@ -4,5 +4,7 @@ const assert = require('assert');
 const dns = require('dns');
 
 // Should not raise assertion error. Issue #7070
-assert.throws(() => dns.resolveNs([])); // bad name
-assert.throws(() => dns.resolveNs('')); // bad callback
+assert.throws(() => dns.resolveNs([]), // bad name
+              /^Error: "name" argument must be a string$/);
+assert.throws(() => dns.resolveNs(''), // bad callback
+              /^Error: "callback" argument must be a function$/);
