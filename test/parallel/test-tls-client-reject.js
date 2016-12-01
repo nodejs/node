d1e235ac32af00c12f8ea8c0e3f680f44e9bb2c3
rgoodwin <rjngoodwin@gmial.com>
2016-12-01 10:41:34 -0600

    test: update tls test to use const/let and common.mustCall
    Replace variable defs using var with more up to date const/let. Updated tests to use strict equality to ensure type and value comparision
    
    tests: wrap callback functions in common.mustCall to ensure single execution

--- test/parallel/test-tls-client-reject.js
+++ test/parallel/test-tls-client-reject.js
@@ -1,64 +1,64 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
 if (!common.hasCrypto) {
   common.skip('missing crypto');
   return;
 }
-var tls = require('tls');
+const tls = require('tls');
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-var options = {
+const options = {
   key: fs.readFileSync(path.join(common.fixturesDir, 'test_key.pem')),
   cert: fs.readFileSync(path.join(common.fixturesDir, 'test_cert.pem'))
 };
 
-var server = tls.createServer(options, common.mustCall(function(socket) {
+const server = tls.createServer(options, common.mustCall(function(socket) {
   socket.on('data', function(data) {
     console.error(data.toString());
-    assert.equal(data, 'ok');
+    assert.strictEqual(data.toString(), 'ok');
   });
 }, 3)).listen(0, function() {
   unauthorized();
 });
 
 function unauthorized() {
-  var socket = tls.connect({
+  const socket = tls.connect({
     port: server.address().port,
     servername: 'localhost',
     rejectUnauthorized: false
-  }, function() {
+  }, common.mustCall(function() {
     assert(!socket.authorized);
     socket.end();
     rejectUnauthorized();
-  });
+  }));
   socket.on('error', common.fail);
   socket.write('ok');
 }
 
 function rejectUnauthorized() {
-  var socket = tls.connect(server.address().port, {
+  const socket = tls.connect(server.address().port, {
     servername: 'localhost'
   }, common.fail);
-  socket.on('error', function(err) {
+  socket.on('error', common.mustCall(function(err) {
     console.error(err);
     authorized();
-  });
+  }));
   socket.write('ng');
 }
 
 function authorized() {
-  var socket = tls.connect(server.address().port, {
+  const socket = tls.connect(server.address().port, {
     ca: [fs.readFileSync(path.join(common.fixturesDir, 'test_cert.pem'))],
     servername: 'localhost'
-  }, function() {
+  }, common.mustCall(function() {
     assert(socket.authorized);
     socket.end();
     server.close();
-  });
+  }));
   socket.on('error', common.fail);
   socket.write('ok');
 }
