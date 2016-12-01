21ab8129926d9aac72fe0944ad258178da477cf3
Segu Riluvan <riluvan@gmail.com>
2016-12-01 11:53:59 -0600

    test: fixed assert.strictEqual test/parallel/test-cluster-net-listen.js
    10.5 error Please use assert.strictEqual() instead of assert.equal()

--- test/parallel/test-cluster-net-listen.js
+++ test/parallel/test-cluster-net-listen.js
@@ -7,7 +7,7 @@ var net = require('net');
 if (cluster.isMaster) {
   // ensure that the worker exits peacefully
   cluster.fork().on('exit', common.mustCall(function(statusCode) {
-    assert.equal(statusCode, 0);
+    assert.strictEqual(statusCode, 0);
   }));
 } else {
   // listen() without port should not trigger a libuv assert
