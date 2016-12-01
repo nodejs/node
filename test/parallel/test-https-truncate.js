934fca1fdf1c960393837a25dca5a7c31f3d0706
davidmarkclements <david.clements@nearform.com>
2016-12-01 14:23:55 -0600

    test: refactor test-https-truncate
    Changes assert.equal to assert.strictEqual to ensure specificity.
    
    Changes var declarations to const/let where appropriate.
    
    PR-URL: https://github.com/nodejs/node/pull/10074
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

--- test/parallel/test-https-truncate.js
+++ test/parallel/test-https-truncate.js
@@ -1,34 +1,34 @@
 'use strict';
-var common = require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
 if (!common.hasCrypto) {
   common.skip('missing crypto');
   return;
 }
-var https = require('https');
+const https = require('https');
 
-var fs = require('fs');
+const fs = require('fs');
 
-var key = fs.readFileSync(common.fixturesDir + '/keys/agent1-key.pem');
-var cert = fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem');
+const key = fs.readFileSync(common.fixturesDir + '/keys/agent1-key.pem');
+const cert = fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem');
 
 // number of bytes discovered empirically to trigger the bug
-var data = Buffer.allocUnsafe(1024 * 32 + 1);
+const data = Buffer.allocUnsafe(1024 * 32 + 1);
 
 httpsTest();
 
 function httpsTest() {
-  var sopt = { key: key, cert: cert };
+  const sopt = { key: key, cert: cert };
 
-  var server = https.createServer(sopt, function(req, res) {
+  const server = https.createServer(sopt, function(req, res) {
     res.setHeader('content-length', data.length);
     res.end(data);
     server.close();
   });
 
   server.listen(0, function() {
-    var opts = { port: this.address().port, rejectUnauthorized: false };
+    const opts = { port: this.address().port, rejectUnauthorized: false };
     https.get(opts).on('response', function(res) {
       test(res);
     });
@@ -38,14 +38,14 @@ function httpsTest() {
 
 function test(res) {
   res.on('end', function() {
-    assert.equal(res._readableState.length, 0);
-    assert.equal(bytes, data.length);
+    assert.strictEqual(res._readableState.length, 0);
+    assert.strictEqual(bytes, data.length);
     console.log('ok');
   });
 
   // Pause and then resume on each chunk, to ensure that there will be
   // a lone byte hanging out at the very end.
-  var bytes = 0;
+  let bytes = 0;
   res.on('data', function(chunk) {
     bytes += chunk.length;
     this.pause();
