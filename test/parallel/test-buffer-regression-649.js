1277c6a94a6a801c5ce22f03e1dfe898c3143095
joyeecheung <joyeec9h3@gmail.com>
2016-12-05 00:09:40 +0800

    test: squash line for buffer regression #649 test

--- test/parallel/test-buffer-regression-649.js
+++ test/parallel/test-buffer-regression-649.js
@@ -7,13 +7,9 @@ const SlowBuffer = require('buffer').SlowBuffer;
 // Regression test for https://github.com/nodejs/node/issues/649.
 const len = 1422561062959;
 const lenLimitMsg = /^RangeError: Invalid typed array length$/;
-assert.throws(() => Buffer(len).toString('utf8'),
-              lenLimitMsg);
-assert.throws(() => SlowBuffer(len).toString('utf8'),
-              lenLimitMsg);
-assert.throws(() => Buffer.alloc(len).toString('utf8'),
-              lenLimitMsg);
-assert.throws(() => Buffer.allocUnsafe(len).toString('utf8'),
-              lenLimitMsg);
+assert.throws(() => Buffer(len).toString('utf8'), lenLimitMsg);
+assert.throws(() => SlowBuffer(len).toString('utf8'), lenLimitMsg);
+assert.throws(() => Buffer.alloc(len).toString('utf8'), lenLimitMsg);
+assert.throws(() => Buffer.allocUnsafe(len).toString('utf8'), lenLimitMsg);
 assert.throws(() => Buffer.allocUnsafeSlow(len).toString('utf8'),
               lenLimitMsg);
