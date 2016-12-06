f53bf4799a4047d1a66ac1ac7f1f0d427050ace8
Nikolai Vavilov <vvnicholas@gmail.com>
2016-12-06 17:19:04 +0200

    use stat

--- test/parallel/test-fs-readfile-fd-offset.js
+++ test/parallel/test-fs-readfile-fd-offset.js
@@ -3,9 +3,11 @@ const common = require('../common');
 const assert = require('assert');
 const fs = require('fs');
 
+const fileLength = fs.statSync(__filename).size;
+
 ['r', 'a+'].forEach(mode => {
   const fd = fs.openSync(__filename, mode);
-  const fileLength = fs.readFileSync(fd).length;
+  assert.strictEqual(fileLength, fs.readFileSync(fd).length);
 
   // Reading again should result in the same length.
   assert.strictEqual(fileLength, fs.readFileSync(fd).length);
