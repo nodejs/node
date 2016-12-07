de19bc87daafc1c8edca0330df509a26fd6441d0
Paul Lucas <lucaspa@umich.edu>
2016-12-06 20:57:44 -0800

    tools: updating osx uninstall
    Changing code based upon silverwind's review:
    Removed if condition for a temp uninstall file
    Added quotes around DIR_PREFIX file paths incase of space

--- tools/osx-uninstall.sh
+++ tools/osx-uninstall.sh
@@ -8,19 +8,15 @@ if [ -z "$DIR_PREFIX" ]; then
   DIR_PREFIX=/usr/local
 fi
 
-if [ -f /tmp/nodejs-run-uninstall ]; then
-  rm -f $DIR_PREFIX/bin/node
-  rm -f $DIR_PREFIX/bin/npm
-  rm -rf $DIR_PREFIX/include/node
-  rm -f $DIR_PREFIX/lib/dtrace/node.d
-  rm -f $DIR_PREFIX/share/man/man1/node.1
-  rm -f $DIR_PREFIX/share/systemtap/tapset/node.stp
-  rm -f $DIR_PREFIX/share/doc/node/gdbinit
+rm -f "$DIR_PREFIX/bin/node"
+rm -f "$DIR_PREFIX/bin/npm"
+rm -rf "$DIR_PREFIX/include/node"
+rm -f "$DIR_PREFIX/lib/dtrace/node.d"
+rm -f "$DIR_PREFIX/share/man/man1/node.1"
+rm -f "$DIR_PREFIX/share/systemtap/tapset/node.stp"
+rm -f "$DIR_PREFIX/share/doc/node/gdbinit"
+rm -rf "$DIR_PREFIX/lib/node_modules/npm"
 
-  rm -rf $DIR_PREFIX/lib/node_modules/npm
-  if [ ! "$(ls -A $DIR_PREFIX/lib/node_modules 2> /dev/null)" ]; then
-    rm -rf $DIR_PREFIX/lib/node_modules
-  fi
-
-  rm -f /tmp/nodejs-run-uninstall
+if [ ! "$(ls -A "$DIR_PREFIX/lib/node_modules" 2> /dev/null)" ]; then
+    rm -rf "$DIR_PREFIX/lib/node_modules"
 fi
