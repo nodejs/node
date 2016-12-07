7243066bebf1d27d8d7ad7e89e130fecbd60ebac
Troy Connor <troy0820@users.noreply.github.com>
2016-12-07 11:06:30 -0500

    buffer: Add array in createBenchmark object for buffer size & iteration in buffer-base64-encode & buffer-base64-decode.js

--- benchmark/buffers/buffer-base64-encode.js
+++ benchmark/buffers/buffer-base64-encode.js
@@ -1,16 +1,20 @@
 'use strict';
 var common = require('../common.js');
 
-var bench = common.createBenchmark(main, {});
+const bench = common.createBenchmark(main, {
+  N: [64 * 1024 * 1024],
+  n: [32]
+});
 
 function main(conf) {
-  var N = 64 * 1024 * 1024;
+  var n = conf.n;
+  var N = conf.N;
   var b = Buffer.allocUnsafe(N);
   var s = '';
   var i;
   for (i = 0; i < 256; ++i) s += String.fromCharCode(i);
   for (i = 0; i < N; i += 256) b.write(s, i, 256, 'ascii');
   bench.start();
-  for (i = 0; i < 32; ++i) b.toString('base64');
-  bench.end(64);
+  for (i = 0; i < n; ++i) b.toString('base64');
+  bench.end(n);
 }
