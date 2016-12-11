ec2f13fe66c5b65a5b028ea1833dd2091ca90963
Ben Noordhuis <info@bnoordhuis.nl>
2016-12-11 17:28:21 +0100

    src: don't overwrite non-writable vm globals
    Check that the property doesn't have the read-only flag set before
    overwriting it.
    
    Fixes: https://github.com/nodejs/node/issues/10223
    PR-URL: https://github.com/nodejs/node/pull/10227
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>

--- test/parallel/test-vm-context.js
+++ test/parallel/test-vm-context.js
@@ -75,3 +75,14 @@ assert.throws(function() {
 // https://github.com/nodejs/node/issues/6158
 ctx = new Proxy({}, {});
 assert.strictEqual(typeof vm.runInNewContext('String', ctx), 'function');
+
+// https://github.com/nodejs/node/issues/10223
+ctx = vm.createContext();
+vm.runInContext('Object.defineProperty(this, "x", { value: 42 })', ctx);
+assert.strictEqual(ctx.x, undefined);  // Not copied out by cloneProperty().
+assert.strictEqual(vm.runInContext('x', ctx), 42);
+vm.runInContext('x = 0', ctx);                      // Does not throw but x...
+assert.strictEqual(vm.runInContext('x', ctx), 42);  // ...should be unaltered.
+assert.throws(() => vm.runInContext('"use strict"; x = 0', ctx),
+              /Cannot assign to read only property 'x'/);
+assert.strictEqual(vm.runInContext('x', ctx), 42);
