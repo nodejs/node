0403859f38a7f932788b58abe08a6532090a21c4
Rich Trott <rtrott@gmail.com>
2016-12-11 20:34:52 -0800

    test: refactor test-crypto-random
    * specify constructor for assert.throws()
    * load additional modules only if crypto check passes
    * normalize some potentially confusing indentation
    * provided actual first and expected second in assertions
    
    PR-URL: https://github.com/nodejs/node/pull/10232
    Reviewed-By: Prince John Wesley <princejohnwesley@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/parallel/test-crypto-random.js
+++ test/parallel/test-crypto-random.js
@@ -1,11 +1,11 @@
 'use strict';
 const common = require('../common');
-const assert = require('assert');
 
 if (!common.hasCrypto) {
   common.skip('missing crypto');
   return;
 }
+const assert = require('assert');
 const crypto = require('crypto');
 
 crypto.DEFAULT_ENCODING = 'buffer';
@@ -13,25 +13,16 @@ crypto.DEFAULT_ENCODING = 'buffer';
 // bump, we register a lot of exit listeners
 process.setMaxListeners(256);
 
-[crypto.randomBytes,
-  crypto.pseudoRandomBytes
-].forEach(function(f) {
-  [-1,
-    undefined,
-    null,
-    false,
-    true,
-    {},
-    []
-  ].forEach(function(value) {
-    assert.throws(function() { f(value); });
-    assert.throws(function() { f(value, function() {}); });
+[crypto.randomBytes, crypto.pseudoRandomBytes].forEach(function(f) {
+  [-1, undefined, null, false, true, {}, []].forEach(function(value) {
+    assert.throws(function() { f(value); }, TypeError);
+    assert.throws(function() { f(value, function() {}); }, TypeError);
   });
 
   [0, 1, 2, 4, 16, 256, 1024].forEach(function(len) {
     f(len, common.mustCall(function(ex, buf) {
-      assert.strictEqual(null, ex);
-      assert.strictEqual(len, buf.length);
+      assert.strictEqual(ex, null);
+      assert.strictEqual(buf.length, len);
       assert.ok(Buffer.isBuffer(buf));
     }));
   });
