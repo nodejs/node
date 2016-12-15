cb9ca5e4a117b326e344b278b8e426f48e14e9e9
Josh Gavant <josh.gavant@outlook.com>
2016-12-14 20:18:15 -0800

    src, inspector: add --inspect-brk option
    add an --inspect-brk option which breaks on
    first line of user script. same behavior as old
    --debug-brk flag.
    
    PR-URL: https://github.com/nodejs/node/pull/8979
    Reviewed-By: Eugene Ostroukhov <eostroukhov@chromium.org>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@keybase.io>

--- src/node.cc
+++ src/node.cc
@@ -3480,6 +3480,12 @@ static void PrintHelp() {
          "                           does not appear to be a terminal\n"
          "  -r, --require            module to preload (option can be "
          "repeated)\n"
+#if HAVE_INSPECTOR
+         "  --inspect[=host:port] activate inspector on host:port\n"
+         "                        (default: 127.0.0.1:9229)\n"
+         "  --inspect-brk[=host:port]  activate inspector on host:port\n"
+         "                             and break at start of user script\n"
+#endif
          "  --no-deprecation         silence deprecation warnings\n"
          "  --trace-deprecation      show stack traces on deprecations\n"
          "  --throw-deprecation      throw an exception on deprecations\n"
