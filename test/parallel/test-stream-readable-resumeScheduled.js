fd195b47d68e0ea7d170ba3ca28270116a3483f8
Italo A. Casas <me@italoacasas.com>
2016-12-15 20:17:38 -0500

    test: stream readable resumeScheduled state
    PR-URL: https://github.com/nodejs/node/pull/10299
    Ref: https://github.com/nodejs/node/issues/8683
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

---
+++ test/parallel/test-stream-readable-resumeScheduled.js
@@ -0,0 +1,67 @@
+'use strict';
+const common = require('../common');
+
+// Testing Readable Stream resumeScheduled state
+
+const assert = require('assert');
+const stream = require('stream');
+const Readable = stream.Readable;
+const Writable = stream.Writable;
+
+{
+  // pipe() test case
+  const r = new Readable({ read() {} });
+  const w = new Writable();
+
+  // resumeScheduled should start = `false`.
+  assert.strictEqual(r._readableState.resumeScheduled, false);
+
+  // calling pipe() should change the state value = true.
+  r.pipe(w);
+  assert.strictEqual(r._readableState.resumeScheduled, true);
+
+  process.nextTick(common.mustCall(() => {
+    assert.strictEqual(r._readableState.resumeScheduled, false);
+  }));
+}
+
+{
+  // 'data' listener test case
+  const r = new Readable({ read() {} });
+
+  // resumeScheduled should start = `false`.
+  assert.strictEqual(r._readableState.resumeScheduled, false);
+
+  r.push(Buffer.from([1, 2, 3]));
+
+  // adding 'data' listener should change the state value
+  r.on('data', common.mustCall(() => {
+    assert.strictEqual(r._readableState.resumeScheduled, false);
+  }));
+  assert.strictEqual(r._readableState.resumeScheduled, true);
+
+  process.nextTick(common.mustCall(() => {
+    assert.strictEqual(r._readableState.resumeScheduled, false);
+  }));
+}
+
+{
+  // resume() test case
+  const r = new Readable({ read() {} });
+
+  // resumeScheduled should start = `false`.
+  assert.strictEqual(r._readableState.resumeScheduled, false);
+
+  // Calling resume() should change the state value.
+  r.resume();
+  assert.strictEqual(r._readableState.resumeScheduled, true);
+
+  r.on('resume', common.mustCall(() => {
+    // The state value should be `false` again
+    assert.strictEqual(r._readableState.resumeScheduled, false);
+  }));
+
+  process.nextTick(common.mustCall(() => {
+    assert.strictEqual(r._readableState.resumeScheduled, false);
+  }));
+}
