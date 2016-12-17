88086ce802c0dc377132ca77ef96fc30bad4d6ab
Santiago Gimeno <santiago.gimeno@gmail.com>
2016-12-17 09:01:47 +0100

    test: fix flaky test-debug-port
    It can happen that first data chunk received in stdout is not exactly
    `'debug> '`. Make sure the exit condition is met.
    
    PR-URL: https://github.com/nodejs/node/pull/10316
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Italo A. Casas <me@italoacasas.com>

--- test/parallel/test-debug-prompt.js
+++ test/parallel/test-debug-prompt.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const assert = require('assert');
-const common = require('../common');
+require('../common');
 const spawn = require('child_process').spawn;
 
 const proc = spawn(process.execPath, ['debug', 'foo']);
 proc.stdout.setEncoding('utf8');
 
-proc.stdout.once('data', common.mustCall((data) => {
-  assert.strictEqual(data, 'debug> ');
-  proc.kill();
-}));
+let output = '';
+proc.stdout.on('data', (data) => {
+  output += data;
+  if (output.includes('debug> '))
+    proc.kill();
+});
