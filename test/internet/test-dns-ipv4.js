38d9c15edd6871ade6bc181d7093b5fe8a5bcd57
Adrian Estrada <edsadr@gmail.com>
2016-12-19 19:41:27 -0500

    test: refactor the code in test-dns-ipv4
    * remove the manual control for functions execution
    * use common.mustCall to control the functions execution automatically
    * use let and const instead of var
    * use assert.strictEqual instead assert.equal
    
    PR-URL: https://github.com/nodejs/node/pull/10200
    Reviewed-By: Italo A. Casas <me@italoacasas.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

--- test/internet/test-dns-ipv4.js
+++ test/internet/test-dns-ipv4.js
@@ -1,18 +1,16 @@
 'use strict';
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const dns = require('dns');
 const net = require('net');
 const isIPv4 = net.isIPv4;
 
-let expected = 0;
-let completed = 0;
 let running = false;
 const queue = [];
 
 function TEST(f) {
   function next() {
-    var f = queue.shift();
+    const f = queue.shift();
     if (f) {
       running = true;
       console.log(f.name);
@@ -22,11 +20,9 @@ function TEST(f) {
 
   function done() {
     running = false;
-    completed++;
     process.nextTick(next);
   }
 
-  expected++;
   queue.push(f);
 
   if (!running) {
@@ -39,149 +35,150 @@ function checkWrap(req) {
 }
 
 TEST(function test_resolve4(done) {
-  var req = dns.resolve4('www.google.com', function(err, ips) {
-    if (err) throw err;
+  const req = dns.resolve4('www.google.com',
+    common.mustCall((err, ips) => {
+      assert.ifError(err);
 
-    assert.ok(ips.length > 0);
+      assert.ok(ips.length > 0);
 
-    for (var i = 0; i < ips.length; i++) {
-      assert.ok(isIPv4(ips[i]));
-    }
+      for (let i = 0; i < ips.length; i++) {
+        assert.ok(isIPv4(ips[i]));
+      }
 
-    done();
-  });
+      done();
+    }));
 
   checkWrap(req);
 });
 
 TEST(function test_reverse_ipv4(done) {
-  var req = dns.reverse('8.8.8.8', function(err, domains) {
-    if (err) throw err;
+  const req = dns.reverse('8.8.8.8',
+    common.mustCall((err, domains) => {
+      assert.ifError(err);
 
-    assert.ok(domains.length > 0);
+      assert.ok(domains.length > 0);
 
-    for (var i = 0; i < domains.length; i++) {
-      assert.ok(domains[i]);
-      assert.ok(typeof domains[i] === 'string');
-    }
+      for (let i = 0; i < domains.length; i++) {
+        assert.ok(domains[i]);
+        assert.ok(typeof domains[i] === 'string');
+      }
 
-    done();
-  });
+      done();
+    }));
 
   checkWrap(req);
 });
 
 TEST(function test_lookup_ipv4_explicit(done) {
-  var req = dns.lookup('www.google.com', 4, function(err, ip, family) {
-    if (err) throw err;
-    assert.ok(net.isIPv4(ip));
-    assert.strictEqual(family, 4);
+  const req = dns.lookup('www.google.com', 4,
+    common.mustCall((err, ip, family) => {
+      assert.ifError(err);
+      assert.ok(net.isIPv4(ip));
+      assert.strictEqual(family, 4);
 
-    done();
-  });
+      done();
+    }));
 
   checkWrap(req);
 });
 
 TEST(function test_lookup_ipv4_implicit(done) {
-  var req = dns.lookup('www.google.com', function(err, ip, family) {
-    if (err) throw err;
-    assert.ok(net.isIPv4(ip));
-    assert.strictEqual(family, 4);
+  const req = dns.lookup('www.google.com',
+    common.mustCall((err, ip, family) => {
+      assert.ifError(err);
+      assert.ok(net.isIPv4(ip));
+      assert.strictEqual(family, 4);
 
-    done();
-  });
+      done();
+    }));
 
   checkWrap(req);
 });
 
 TEST(function test_lookup_ipv4_explicit_object(done) {
-  var req = dns.lookup('www.google.com', {
+  const req = dns.lookup('www.google.com', {
     family: 4
-  }, function(err, ip, family) {
-    if (err) throw err;
+  }, common.mustCall((err, ip, family) => {
+    assert.ifError(err);
     assert.ok(net.isIPv4(ip));
     assert.strictEqual(family, 4);
 
     done();
-  });
+  }));
 
   checkWrap(req);
 });
 
 TEST(function test_lookup_ipv4_hint_addrconfig(done) {
-  var req = dns.lookup('www.google.com', {
+  const req = dns.lookup('www.google.com', {
     hints: dns.ADDRCONFIG
-  }, function(err, ip, family) {
-    if (err) throw err;
+  }, common.mustCall((err, ip, family) => {
+    assert.ifError(err);
     assert.ok(net.isIPv4(ip));
     assert.strictEqual(family, 4);
 
     done();
-  });
+  }));
 
   checkWrap(req);
 });
 
 TEST(function test_lookup_ip_ipv4(done) {
-  var req = dns.lookup('127.0.0.1', function(err, ip, family) {
-    if (err) throw err;
-    assert.strictEqual(ip, '127.0.0.1');
-    assert.strictEqual(family, 4);
+  const req = dns.lookup('127.0.0.1',
+    common.mustCall((err, ip, family) => {
+      assert.ifError(err);
+      assert.strictEqual(ip, '127.0.0.1');
+      assert.strictEqual(family, 4);
 
-    done();
-  });
+      done();
+    }));
 
   checkWrap(req);
 });
 
 TEST(function test_lookup_localhost_ipv4(done) {
-  var req = dns.lookup('localhost', 4, function(err, ip, family) {
-    if (err) throw err;
-    assert.strictEqual(ip, '127.0.0.1');
-    assert.strictEqual(family, 4);
+  const req = dns.lookup('localhost', 4,
+    common.mustCall((err, ip, family) => {
+      assert.ifError(err);
+      assert.strictEqual(ip, '127.0.0.1');
+      assert.strictEqual(family, 4);
 
-    done();
-  });
+      done();
+    }));
 
   checkWrap(req);
 });
 
 TEST(function test_lookup_all_ipv4(done) {
-  var req = dns.lookup(
+  const req = dns.lookup(
     'www.google.com',
     {all: true, family: 4},
-    function(err, ips) {
-      if (err) throw err;
+    common.mustCall((err, ips) => {
+      assert.ifError(err);
       assert.ok(Array.isArray(ips));
       assert.ok(ips.length > 0);
 
-      ips.forEach(function(ip) {
+      ips.forEach((ip) => {
         assert.ok(isIPv4(ip.address));
         assert.strictEqual(ip.family, 4);
       });
 
       done();
     }
-  );
+  ));
 
   checkWrap(req);
 });
 
 TEST(function test_lookupservice_ip_ipv4(done) {
-  var req = dns.lookupService('127.0.0.1', 80, function(err, host, service) {
-    if (err) throw err;
-    assert.equal(typeof host, 'string');
-    assert(host);
-    assert(['http', 'www', '80'].includes(service));
-    done();
-  });
+  const req = dns.lookupService('127.0.0.1', 80,
+    common.mustCall((err, host, service) => {
+      assert.ifError(err);
+      assert.strictEqual(typeof host, 'string');
+      assert(host);
+      assert(['http', 'www', '80'].includes(service));
+      done();
+    }));
 
   checkWrap(req);
-});
-
-process.on('exit', function() {
-  console.log(completed + ' tests completed');
-  assert.equal(running, false);
-  assert.strictEqual(expected, completed);
 });
