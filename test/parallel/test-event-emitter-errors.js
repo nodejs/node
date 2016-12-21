125afceebf12a18879acfdf86e0c4addfe5b44b8
Chris Burkhart <christide@christide.com>
2016-12-21 09:32:21 -0800

    events: update and clarify error message
    Update error message that's thrown when no error listeners are attached
    to an emitter.
    
    PR-URL: https://github.com/nodejs/node/pull/10387
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Italo A. Casas <me@italoacasas.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/parallel/test-event-emitter-errors.js
+++ test/parallel/test-event-emitter-errors.js
@@ -5,6 +5,10 @@ const assert = require('assert');
 
 const EE = new EventEmitter();
 
-assert.throws(function() {
+assert.throws(() => {
   EE.emit('error', 'Accepts a string');
-}, /Accepts a string/);
+}, /^Error: Unhandled "error" event\. \(Accepts a string\)$/);
+
+assert.throws(() => {
+  EE.emit('error', {message: 'Error!'});
+}, /^Error: Unhandled "error" event\. \(\[object Object\]\)$/);
