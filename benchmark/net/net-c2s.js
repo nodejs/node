91c59a6f7b9434e47ce0395caa44c633e76a5066
Brian White <mscdex@mscdex.net>
2016-12-24 23:29:22 -0500

    benchmark: improve readability of net benchmarks
    PR-URL: https://github.com/nodejs/node/pull/10446
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- benchmark/net/net-c2s.js
+++ benchmark/net/net-c2s.js
@@ -65,8 +65,17 @@ Writer.prototype.emit = function() {};
 Writer.prototype.prependListener = function() {};
 
 
+function flow() {
+  var dest = this.dest;
+  var res = dest.write(chunk, encoding);
+  if (!res)
+    dest.once('drain', this.flow);
+  else
+    process.nextTick(this.flow);
+}
+
 function Reader() {
-  this.flow = this.flow.bind(this);
+  this.flow = flow.bind(this);
   this.readable = true;
 }
 
@@ -74,15 +83,6 @@ Reader.prototype.pipe = function(dest) {
   this.dest = dest;
   this.flow();
   return dest;
-};
-
-Reader.prototype.flow = function() {
-  var dest = this.dest;
-  var res = dest.write(chunk, encoding);
-  if (!res)
-    dest.once('drain', this.flow);
-  else
-    process.nextTick(this.flow);
 };
 
 
