d4cafa0aab1c1099508b1aecbcae2957293d718e
Rich Trott <rtrott@gmail.com>
2016-12-26 22:35:44 -0800

    squash: comment explaining the console.log()

--- test/pseudo-tty/test-stderr-stdout-handle-sigwinch.js
+++ test/pseudo-tty/test-stderr-stdout-handle-sigwinch.js
@@ -1,33 +1,29 @@
 'use strict';
 const common = require('../common');
 
-// if printing, add to out file
-const originalRefreshSizeWrapperStderr = process.stderr._refreshSize;
-const originalRefreshSizeWrapperStdout = process.stdout._refreshSize;
+const originalRefreshSizeStderr = process.stderr._refreshSize;
+const originalRefreshSizeStdout = process.stdout._refreshSize;
 
-const refreshSizeWrapperStderr = () => {
-  console.log('calling stderr._refreshSize');
-  try {
-    originalRefreshSizeWrapperStderr.call(process.stderr);
-  } catch (e) {
-    // EINVAL happens on SmartOS if emulation is incomplete
-    if (!common.isSunOS || e.code !== 'EINVAL')
-      throw e;
-  }
+const wrap = (fn, ioStream, string) => {
+  return () => {
+    // The console.log() call prints a string that is in the .out file. In other
+    // words, the console.log() is part of the test, not extraneous debugging.
+    console.log(string);
+    try {
+      fn.call(ioStream);
+    } catch (e) {
+      // EINVAL happens on SmartOS if emulation is incomplete
+      if (!common.isSunOS || e.code !== 'EINVAL')
+        throw e;
+    }
+  };
 };
 
-const refreshSizeWrapperStdout = () => {
-  console.log('calling stdout._refreshSize');
-  try {
-    originalRefreshSizeWrapperStdout.call(process.stdout);
-  } catch (e) {
-    // EINVAL happens on SmartOS if emulation is incomplete
-    if (!common.isSunOS || e.code !== 'EINVAL')
-      throw e;
-  }
-};
-
-process.stderr._refreshSize = refreshSizeWrapperStderr;
-process.stdout._refreshSize = refreshSizeWrapperStdout;
+process.stderr._refreshSize = wrap(originalRefreshSizeStderr,
+                                   process.stderr,
+                                   'calling stderr._refreshSize');
+process.stdout._refreshSize = wrap(originalRefreshSizeStdout,
+                                   process.stdout,
+                                   'calling stdout._refreshSize');
 
 process.emit('SIGWINCH');
