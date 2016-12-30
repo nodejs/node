e7f72a415de9fc2dd37b410dd2d4edd44cf3c5bf
Stewart X Addison <sxa@uk.ibm.com>
2016-12-30 12:44:46 +0000

    build: don't squash signal handlers with --shared
    An application using node built as a shared library may legitimately
    implement its own signal handling routines. Current behaviour is
    to squash all signal handlers on node startup. This change will
    stop that behaviour when node is built as a shared library.
    
    PR-URL: https://github.com/nodejs/node/pull/10539
    Fixes: https://github.com/nodejs/node/issues/10520
    Refs: https://github.com/nodejs/node/pull/615
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node.cc
+++ src/node.cc
@@ -2187,7 +2187,7 @@ static void WaitForInspectorDisconnect(Environment* env) {
   if (env->inspector_agent()->IsConnected()) {
     // Restore signal dispositions, the app is done and is no longer
     // capable of handling signals.
-#ifdef __POSIX__
+#if defined(__POSIX__) && !defined(NODE_SHARED_MODE)
     struct sigaction act;
     memset(&act, 0, sizeof(act));
     for (unsigned nr = 1; nr < kMaxSignal; nr += 1) {
@@ -4069,6 +4069,7 @@ inline void PlatformInit() {
 
   CHECK_EQ(err, 0);
 
+#ifndef NODE_SHARED_MODE
   // Restore signal dispositions, the parent process may have changed them.
   struct sigaction act;
   memset(&act, 0, sizeof(act));
@@ -4082,6 +4083,7 @@ inline void PlatformInit() {
     act.sa_handler = (nr == SIGPIPE) ? SIG_IGN : SIG_DFL;
     CHECK_EQ(0, sigaction(nr, &act, nullptr));
   }
+#endif  // !NODE_SHARED_MODE
 
   RegisterSignalHandler(SIGINT, SignalExit, true);
   RegisterSignalHandler(SIGTERM, SignalExit, true);
