f3d1b7209d05215fda6a2540bd0b1b0d5fc5be4d
cjihrig <cjihrig@gmail.com>
2016-12-30 11:46:21 -0500

    test: s/ASSERT/assert/
    This commit addresses an inconsistency with eight tests. These
    tests use the assert module, but named the variable ASSERT. This
    goes against the project's typical coding style, and negatively
    impacts global find and replace updates.
    
    PR-URL: https://github.com/nodejs/node/pull/10544
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Jackson Tian <shyvo1987@gmail.com>
    Reviewed-By: Prince John Wesley <princejohnwesley@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-readint.js
+++ test/parallel/test-readint.js
@@ -3,7 +3,7 @@
  * Tests to verify we're reading in signed integers correctly
  */
 require('../common');
-var ASSERT = require('assert');
+const assert = require('assert');
 
 /*
  * Test 8 bit signed integers
@@ -12,19 +12,19 @@ function test8(clazz) {
   var data = new clazz(4);
 
   data[0] = 0x23;
-  ASSERT.equal(0x23, data.readInt8(0));
+  assert.equal(0x23, data.readInt8(0));
 
   data[0] = 0xff;
-  ASSERT.equal(-1, data.readInt8(0));
+  assert.equal(-1, data.readInt8(0));
 
   data[0] = 0x87;
   data[1] = 0xab;
   data[2] = 0x7c;
   data[3] = 0xef;
-  ASSERT.equal(-121, data.readInt8(0));
-  ASSERT.equal(-85, data.readInt8(1));
-  ASSERT.equal(124, data.readInt8(2));
-  ASSERT.equal(-17, data.readInt8(3));
+  assert.equal(-121, data.readInt8(0));
+  assert.equal(-85, data.readInt8(1));
+  assert.equal(124, data.readInt8(2));
+  assert.equal(-17, data.readInt8(3));
 }
 
 
@@ -33,13 +33,13 @@ function test16(clazz) {
 
   buffer[0] = 0x16;
   buffer[1] = 0x79;
-  ASSERT.equal(0x1679, buffer.readInt16BE(0));
-  ASSERT.equal(0x7916, buffer.readInt16LE(0));
+  assert.equal(0x1679, buffer.readInt16BE(0));
+  assert.equal(0x7916, buffer.readInt16LE(0));
 
   buffer[0] = 0xff;
   buffer[1] = 0x80;
-  ASSERT.equal(-128, buffer.readInt16BE(0));
-  ASSERT.equal(-32513, buffer.readInt16LE(0));
+  assert.equal(-128, buffer.readInt16BE(0));
+  assert.equal(-32513, buffer.readInt16LE(0));
 
   /* test offset with weenix */
   buffer[0] = 0x77;
@@ -48,16 +48,16 @@ function test16(clazz) {
   buffer[3] = 0x6e;
   buffer[4] = 0x69;
   buffer[5] = 0x78;
-  ASSERT.equal(0x7765, buffer.readInt16BE(0));
-  ASSERT.equal(0x6565, buffer.readInt16BE(1));
-  ASSERT.equal(0x656e, buffer.readInt16BE(2));
-  ASSERT.equal(0x6e69, buffer.readInt16BE(3));
-  ASSERT.equal(0x6978, buffer.readInt16BE(4));
-  ASSERT.equal(0x6577, buffer.readInt16LE(0));
-  ASSERT.equal(0x6565, buffer.readInt16LE(1));
-  ASSERT.equal(0x6e65, buffer.readInt16LE(2));
-  ASSERT.equal(0x696e, buffer.readInt16LE(3));
-  ASSERT.equal(0x7869, buffer.readInt16LE(4));
+  assert.equal(0x7765, buffer.readInt16BE(0));
+  assert.equal(0x6565, buffer.readInt16BE(1));
+  assert.equal(0x656e, buffer.readInt16BE(2));
+  assert.equal(0x6e69, buffer.readInt16BE(3));
+  assert.equal(0x6978, buffer.readInt16BE(4));
+  assert.equal(0x6577, buffer.readInt16LE(0));
+  assert.equal(0x6565, buffer.readInt16LE(1));
+  assert.equal(0x6e65, buffer.readInt16LE(2));
+  assert.equal(0x696e, buffer.readInt16LE(3));
+  assert.equal(0x7869, buffer.readInt16LE(4));
 }
 
 
@@ -68,15 +68,15 @@ function test32(clazz) {
   buffer[1] = 0x53;
   buffer[2] = 0x16;
   buffer[3] = 0x79;
-  ASSERT.equal(0x43531679, buffer.readInt32BE(0));
-  ASSERT.equal(0x79165343, buffer.readInt32LE(0));
+  assert.equal(0x43531679, buffer.readInt32BE(0));
+  assert.equal(0x79165343, buffer.readInt32LE(0));
 
   buffer[0] = 0xff;
   buffer[1] = 0xfe;
   buffer[2] = 0xef;
   buffer[3] = 0xfa;
-  ASSERT.equal(-69638, buffer.readInt32BE(0));
-  ASSERT.equal(-84934913, buffer.readInt32LE(0));
+  assert.equal(-69638, buffer.readInt32BE(0));
+  assert.equal(-84934913, buffer.readInt32LE(0));
 
   buffer[0] = 0x42;
   buffer[1] = 0xc3;
@@ -84,12 +84,12 @@ function test32(clazz) {
   buffer[3] = 0xa9;
   buffer[4] = 0x36;
   buffer[5] = 0x17;
-  ASSERT.equal(0x42c395a9, buffer.readInt32BE(0));
-  ASSERT.equal(-1013601994, buffer.readInt32BE(1));
-  ASSERT.equal(-1784072681, buffer.readInt32BE(2));
-  ASSERT.equal(-1449802942, buffer.readInt32LE(0));
-  ASSERT.equal(917083587, buffer.readInt32LE(1));
-  ASSERT.equal(389458325, buffer.readInt32LE(2));
+  assert.equal(0x42c395a9, buffer.readInt32BE(0));
+  assert.equal(-1013601994, buffer.readInt32BE(1));
+  assert.equal(-1784072681, buffer.readInt32BE(2));
+  assert.equal(-1449802942, buffer.readInt32LE(0));
+  assert.equal(917083587, buffer.readInt32LE(1));
+  assert.equal(389458325, buffer.readInt32LE(2));
 }
 
 
