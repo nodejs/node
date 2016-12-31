a26d60221e14d3ae5e66a8e07649ceb897eb269a
dpg5000 <dgoussev@gmail.com>
2016-12-30 19:50:35 -0800

    test: remove process.on('exit') with callback wrapper common.mustCall

--- test/parallel/test-stream2-readable-wrap.js
+++ test/parallel/test-stream2-readable-wrap.js
@@ -1,24 +1,22 @@
 'use strict';
 require('../common');
-var assert = require('assert');
+const assert = require('assert');
 
-var Readable = require('_stream_readable');
-var Writable = require('_stream_writable');
-var EE = require('events').EventEmitter;
+const Readable = require('_stream_readable');
+const Writable = require('_stream_writable');
+const EE = require('events').EventEmitter;
 
-var testRuns = 0, completedRuns = 0;
 function runTest(highWaterMark, objectMode, produce) {
-  testRuns++;
 
-  var old = new EE();
-  var r = new Readable({ highWaterMark: highWaterMark,
+  const old = new EE();
+  const r = new Readable({ highWaterMark: highWaterMark,
                          objectMode: objectMode });
-  assert.equal(r, r.wrap(old));
+  assert.strictEqual(r, r.wrap(old));
 
-  var ended = false;
-  r.on('end', function() {
+  let ended = false;
+  r.on('end', common.mustCall(function() {
     ended = true;
-  });
+  }));
 
   old.pause = function() {
     console.error('old.pause()');
@@ -32,14 +30,14 @@ function runTest(highWaterMark, objectMode, produce) {
     flow();
   };
 
-  var flowing;
-  var chunks = 10;
-  var oldEnded = false;
-  var expected = [];
+  let flowing;
+  let chunks = 10;
+  let oldEnded = false;
+  const expected = [];
   function flow() {
     flowing = true;
     while (flowing && chunks-- > 0) {
-      var item = produce();
+      const item = produce();
       expected.push(item);
       console.log('old.emit', chunks, flowing);
       old.emit('data', item);
@@ -52,19 +50,18 @@ function runTest(highWaterMark, objectMode, produce) {
     }
   }
 
-  var w = new Writable({ highWaterMark: highWaterMark * 2,
+ const w = new Writable({ highWaterMark: highWaterMark * 2,
                          objectMode: objectMode });
-  var written = [];
+  const written = [];
   w._write = function(chunk, encoding, cb) {
     console.log('_write', chunk);
     written.push(chunk);
-    setTimeout(cb);
+    setTimeout(cb, 1000);
   };
 
-  w.on('finish', function() {
-    completedRuns++;
+  w.on('finish', common.mustCall(function() {
     performAsserts();
-  });
+  }));
 
   r.pipe(w);
 
@@ -77,14 +74,10 @@ function runTest(highWaterMark, objectMode, produce) {
   }
 }
 
-runTest(100, false, function() { return Buffer.allocUnsafe(100); });
-runTest(10, false, function() { return Buffer.from('xxxxxxxxxx'); });
-runTest(1, true, function() { return { foo: 'bar' }; });
+runTest(100, false, common.mustCall(function() { return Buffer.allocUnsafe(100); });
+runTest(10, false, common.mustCall(function() { return Buffer.from('xxxxxxxxxx'); }));
+runTest(1, true, common.mustCall(function() { return { foo: 'bar' }; }));
 
-var objectChunks = [ 5, 'a', false, 0, '', 'xyz', { x: 4 }, 7, [], 555 ];
-runTest(1, true, function() { return objectChunks.shift(); });
+const objectChunks = [ 5, 'a', false, 0, '', 'xyz', { x: 4 }, 7, [], 555 ];
+runTest(1, true, common.mustCall(function() { return objectChunks.shift(); }));
 
-process.on('exit', function() {
-  assert.equal(testRuns, completedRuns);
-  console.log('ok');
-});
