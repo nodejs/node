17d9a739c18ad067dd5cef3b5cc43e614b4602fd
sivaprasanna <sivaprasanna@gmail.com>
2017-01-07 23:10:16 +0530

    test: refactor test-watch-file.js
    * use const and let instead of var
    * use arrow function
    * removed console.log statements
    
    PR-URL: https://github.com/nodejs/node/pull/10679
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Italo A. Casas <me@italoacasas.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

--- test/pummel/test-watch-file.js
+++ test/pummel/test-watch-file.js
@@ -5,14 +5,11 @@ var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
 
-var f = path.join(common.fixturesDir, 'x.txt');
+const f = path.join(common.fixturesDir, 'x.txt');
 
-console.log('watching for changes of ' + f);
-
-var changes = 0;
+let changes = 0;
 function watchFile() {
-  fs.watchFile(f, function(curr, prev) {
-    console.log(f + ' change');
+  fs.watchFile(f, (curr, prev) => {
     changes++;
     assert.notDeepStrictEqual(curr.mtime, prev.mtime);
     fs.unwatchFile(f);
@@ -24,7 +21,7 @@ function watchFile() {
 watchFile();
 
 
-var fd = fs.openSync(f, 'w+');
+const fd = fs.openSync(f, 'w+');
 fs.writeSync(fd, 'xyz\n');
 fs.closeSync(fd);
 
