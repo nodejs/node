ab2b31c2cf607ce601baa4f7587120d05bf42cb0
abouthiroppy <git@about-hiroppy.com>
2017-01-09 06:54:44 +0900

    test: refactor test-http-abort-stream-end.js

--- test/disabled/test-http-abort-stream-end.js
+++ test/disabled/test-http-abort-stream-end.js
@@ -4,23 +4,23 @@ const assert = require('assert');
 
 const http = require('http');
 
-var maxSize = 1024;
-var size = 0;
+const maxSize = 1024;
+let size = 0;
 
-var s = http.createServer(function(req, res) {
+const s = http.createServer(function(req, res) {
   this.close();
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  for (var i = 0; i < maxSize; i++) {
+  for (let i = 0; i < maxSize; i++) {
     res.write('x' + i);
   }
   res.end();
 });
 
-var aborted = false;
-s.listen(common.PORT, function() {
-  var req = http.get('http://localhost:' + common.PORT, function(res) {
-    res.on('data', function(chunk) {
+let aborted = false;
+s.listen(common.PORT, () => {
+  const req = http.get(`http://localhost:${common.PORT}`, (res) => {
+    res.on('data', (chunk) => {
       size += chunk.length;
       assert(!aborted, 'got data after abort');
       if (size > maxSize) {
@@ -34,7 +34,7 @@ s.listen(common.PORT, function() {
   req.end();
 });
 
-process.on('exit', function() {
+process.on('exit', () => {
   assert(aborted);
   assert.equal(size, maxSize);
   console.log('ok');
