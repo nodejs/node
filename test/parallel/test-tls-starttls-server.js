a1b114ec471959a8da3533e554ec5ba2e7255850
Fedor Indutny <fedor@indutny.com>
2017-01-09 08:38:31 -0500

    tls: do not crash on STARTTLS when OCSP requested
    `TLSSocket` should not have a hard dependency on `tls.Server`, since it
    may be running without it in cases like `STARTTLS`.
    
    Fix: #10704

---
+++ test/parallel/test-tls-starttls-server.js
@@ -0,0 +1,50 @@
+'use strict';
+const common = require('../common');
+
+if (!common.hasCrypto) {
+  common.skip('missing crypto');
+  return;
+}
+
+const assert = require('assert');
+const net = require('net');
+const tls = require('tls');
+const fs = require('fs');
+
+const key = fs.readFileSync(common.fixturesDir + '/keys/agent1-key.pem');
+const cert = fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem');
+
+const server = net.createServer(common.mustCall((s) => {
+  const tlsSocket = new tls.TLSSocket(s, {
+    isServer: true,
+    server: server,
+
+    secureContext: tls.createSecureContext({
+      key: key,
+      cert: cert
+    }),
+
+    SNICallback: common.mustCall((hostname, callback) => {
+      assert.equal(hostname, 'test.test');
+
+      callback(null, null);
+    })
+  });
+
+  tlsSocket.on('secure', common.mustCall(() => {
+    console.log('hello');
+    tlsSocket.end();
+    server.close();
+  }));
+})).listen(0, () => {
+  const opts = {
+    servername: 'test.test',
+    port: server.address().port,
+    rejectUnauthorized: false,
+    requestOCSP: true
+  };
+
+  const client = tls.connect(opts, function() {
+    client.end();
+  });
+});
