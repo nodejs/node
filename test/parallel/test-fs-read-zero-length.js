d481a63149abba3a27f9d7af14f517826bb5e834
abouthiroppy <git@about-hiroppy.com>
2017-01-11 07:54:12 +0900

    test: refactor test-fs-read-zero-length.js
    PR-URL: https://github.com/nodejs/node/pull/10729
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-fs-read-zero-length.js
+++ test/parallel/test-fs-read-zero-length.js
@@ -7,7 +7,7 @@ const filepath = path.join(common.fixturesDir, 'x.txt');
 const fd = fs.openSync(filepath, 'r');
 const expected = '';
 
-fs.read(fd, 0, 0, 'utf-8', common.mustCall(function(err, str, bytesRead) {
+fs.read(fd, 0, 0, 'utf-8', common.mustCall((err, str, bytesRead) => {
   assert.ok(!err);
   assert.equal(str, expected);
   assert.equal(bytesRead, 0);
