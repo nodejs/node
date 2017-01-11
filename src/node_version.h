91ab09fe2a5739e8e6b4133ffa23d8c5afff37d9
Michaël Zasso <targos@protonmail.com>
2017-01-11 09:50:19 +0100

    src: update NODE_MODULE_VERSION to 52
    V8 5.5 is not API/ABI compatible with 5.4.
    This commit increments NODE_MODULE_VERSION by one.
    
    Refs: https://github.com/nodejs/CTC/blob/master/meetings/2016-09-28.md
    PR-URL: https://github.com/nodejs/node/pull/9618
    Reviewed-By: Ali Ijaz Sheikh <ofrobots@google.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node_version.h
+++ src/node_version.h
@@ -52,6 +52,6 @@
  * an API is broken in the C++ side, including in v8 or
  * other dependencies.
  */
-#define NODE_MODULE_VERSION 51 /* Node.js v7.0.0 */
+#define NODE_MODULE_VERSION 52 /* Node.js v8.0.0 */
 
 #endif  // SRC_NODE_VERSION_H_
