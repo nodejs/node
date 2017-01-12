7d6aff4fcef5b45a4c682989b04d24c1e3036669
Rich Trott <rtrott@gmail.com>
2017-01-12 09:53:24 -0800

    squash

--- test/parallel/test-net-server-connections.js
+++ test/parallel/test-net-server-connections.js
@@ -1,10 +1,15 @@
 'use strict';
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 
 const net = require('net');
 
 const server = new net.Server();
+
+const expectedWarning = 'Server.connections property is deprecated. ' +
+                        'Use Server.getConnections method instead.';
+
+common.expectWarning('DeprecationWarning', expectedWarning);
 
 // test that server.connections property is no longer enumerable now that it
 // has been marked as deprecated
