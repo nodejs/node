1d9f9c0dd28b8ca8d95ad3cc1c3e700c567eece0
Rich Trott <rtrott@gmail.com>
2017-01-11 23:20:54 -0800

    test,net: add tests for server.connections
    There were no tests confirming situations where server.connections
    should return `null`. Add a test for that situation.
    
    Expand existing server.connection test slightly to check value.
    
    Refactor (mostly spacing) code for server.connections setter.

--- test/parallel/test-net-server-connections.js
+++ test/parallel/test-net-server-connections.js
@@ -4,9 +4,10 @@ const assert = require('assert');
 
 const net = require('net');
 
-// test that server.connections property is no longer enumerable now that it
-// has been marked as deprecated
-
 const server = new net.Server();
 
+// test that server.connections property is no longer enumerable now that it
+// has been marked as deprecated
 assert.strictEqual(Object.keys(server).indexOf('connections'), -1);
+
+assert.strictEqual(server.connections, 0);
