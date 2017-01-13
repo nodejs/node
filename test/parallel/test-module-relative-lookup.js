c67207731f16a78f6cae90e49c53b10728241ecf
Brian White <mscdex@mscdex.net>
2017-01-13 05:22:27 -0500

    lib: simplify Module._resolveLookupPaths
    This commit consists of two changes:
    
    * Avoids returning request/id *just* for the debug() output
    * Returns `null` instead of an empty array for the list of paths
    
    PR-URL: https://github.com/nodejs/node/pull/10789
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- test/parallel/test-module-relative-lookup.js
+++ test/parallel/test-module-relative-lookup.js
@@ -4,7 +4,12 @@ require('../common');
 const assert = require('assert');
 const _module = require('module'); // avoid collision with global.module
 const lookupResults = _module._resolveLookupPaths('./lodash');
-const paths = lookupResults[1];
+let paths = lookupResults[1];
 
 assert.strictEqual(paths[0], '.',
+                   'Current directory gets highest priority for local modules');
+
+paths = _module._resolveLookupPaths('./lodash', null, true);
+
+assert.strictEqual(paths && paths[0], '.',
                    'Current directory gets highest priority for local modules');
