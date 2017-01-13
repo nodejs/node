af89c995780543b5e4e9e8900826b306fec567d3
Christopher Hiller <boneskull@boneskull.com>
2017-01-12 22:21:11 -0800

    dgram: Improve signature of Socket.prototype.send
    - Do not require presence of `address` parameter to use `callback`
      parameter; `address` is *always* optional
    - Improve exception messaging if `address` is invalid type
    - If `address` is an invalid type, guarantee a synchronously thrown
      exception
    - Update documentation to reflect signature changes

--- test/parallel/test-dgram-send-address-types.js
+++ test/parallel/test-dgram-send-address-types.js
@@ -4,53 +4,48 @@ const assert = require('assert');
 const dgram = require('dgram');
 
 const port = common.PORT;
-const host = common.localhostIPv4;
 const buf = Buffer.from('test');
 const client = dgram.createSocket('udp4');
 
-client.bind(0, common.mustCall(() => {
-  const onMessage = common.mustCall((err, bytes) => {
-    assert.ifError(err);
-    assert.strictEqual(bytes, buf.length);
-  }, 5);
+const onMessage = common.mustCall((err, bytes) => {
+  assert.ifError(err);
+  assert.strictEqual(bytes, buf.length);
+}, 6);
 
-  // valid address: false
-  client.send(buf, port, false, onMessage);
+// valid address: false
+client.send(buf, port, false, onMessage);
 
-  // valid address: empty string
-  client.send(buf, port, '', onMessage);
+// valid address: empty string
+client.send(buf, port, '', onMessage);
 
-  // valid address: null
-  client.send(buf, port, null, onMessage);
+// valid address: null
+client.send(buf, port, null, onMessage);
 
-  // valid address: 0
-  client.send(buf, port, 0, onMessage);
+// valid address: 0
+client.send(buf, port, 0, onMessage);
 
-  // valid address: undefined
-  client.send(buf, port, undefined, onMessage);
+// valid address: undefined
+client.send(buf, port, undefined, onMessage);
 
-  // the socket must be bound before these following assertions;
-  // otherwise the exceptions won't be thrown synchronously.
+// valid address: not provided
+client.send(buf, port, onMessage);
 
-  // invalid address: object
-  assert.throws(() => {
-    client.send(buf, port, []);
-  }, TypeError);
+const expectedError = new RegExp('^TypeError: Invalid arguments: address ' +
+  'must be a nonempty string or falsy$');
 
-  // invalid address: nonzero number
-  assert.throws(() => {
-    client.send(buf, port, 1);
-  }, TypeError);
+// invalid address: object
+assert.throws(() => {
+  client.send(buf, port, []);
+}, expectedError);
 
-  // invalid address: true
-  assert.throws(() => {
-    client.send(buf, port, true);
-  }, TypeError);
+// invalid address: nonzero number
+assert.throws(() => {
+  client.send(buf, port, 1);
+}, expectedError);
 
-  // invalid address: function
-  assert.throws(() => {
-    client.send(buf, port, () => {
-    });
-  }, TypeError);
-}))
-  .unref();
+// invalid address: true
+assert.throws(() => {
+  client.send(buf, port, true);
+}, expectedError);
+
+client.unref();
