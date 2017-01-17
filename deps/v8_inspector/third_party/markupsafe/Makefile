022fbce655a737d1e4acd5a5f26e8c164ad4d053
Ali Ijaz Sheikh <ofrobots@google.com>
2017-01-17 11:49:26 -0800

    deps: switch to v8_inspector in V8
    * Delete deps/v8_inspector
    * Start building v8_inspector from V8
    * Remove licenses related to the deleted code from LICENSE file
    
    PR-URL: https://github.com/nodejs/node/pull/10992
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- deps/v8_inspector/third_party/markupsafe/Makefile
+++
@@ -1,7 +0,0 @@
-test:
-	python setup.py test
-
-tox-test:
-	tox
-
-.PYONY: test tox-test
--- deps/v8_inspector/third_party/markupsafe/bench/bench_long_empty_string.py
+++
@@ -1,6 +0,0 @@
-from markupsafe import escape
-
-
-def run():
-    string = 'Hello World!' * 1000
-    escape(string)
