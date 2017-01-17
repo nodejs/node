c452db66603e0c48d776dae81ec98101920f7d78
Roman Reiss <me@silverwind.io>
2017-01-18 00:55:34 +0100

    module: remove require('.') with NODE_PATH compat
    This removes the hack for the unintended use case of require('.') used
    in conjunction with NODE_PATH. NODE_PATH was never intended to be
    pointed to a module's directory directly, but instead should point to a
    directory containing modules in sub-directories, which can be loaded
    through require('module-name');

--- test/parallel/test-require-dot.js
+++ test/parallel/test-require-dot.js
@@ -10,9 +10,7 @@ const b = require(fixtures.path('module-require', 'relative', 'dot-slash.js'));
 assert.strictEqual(a.value, 42);
 assert.strictEqual(a, b, 'require(".") should resolve like require("./")');
 
+// require('.') should not lookup in NODE_PATH
 process.env.NODE_PATH = fixtures.path('module-require', 'relative');
 m._initPaths();
-
-const c = require('.');
-
-assert.strictEqual(c.value, 42, 'require(".") should honor NODE_PATH');
+assert.throws(() => { require('.'); }, Error, "Cannot find module '.'");
