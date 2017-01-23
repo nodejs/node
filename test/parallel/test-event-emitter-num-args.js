9496494aaab03e67e23caac0fec8a27b8be67956
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2017-01-23 14:38:26 -0300

    test: expand test coverage of events.js
    * test else path in emitMany function
    * test calling removeAllListeners() in a event emitter instance
      with no events at all
    * test calling removeListener() passing a event type that does
      not exist
    * test calling eventNames() in a event emitter instance
      with no events at all
    
    Refs: https://coverage.nodejs.org/coverage-ba776b3a56642d4c/root/events.js.html
    PR-URL: https://github.com/nodejs/node/pull/10947
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>

--- test/parallel/test-event-emitter-num-args.js
+++ test/parallel/test-event-emitter-num-args.js
@@ -4,15 +4,20 @@ var assert = require('assert');
 var events = require('events');
 
 const e = new events.EventEmitter();
-const num_args_emited = [];
+const num_args_emitted = [];
 
 e.on('numArgs', function() {
-  var numArgs = arguments.length;
-  console.log('numArgs: ' + numArgs);
-  num_args_emited.push(numArgs);
+  const numArgs = arguments.length;
+  num_args_emitted.push(numArgs);
 });
 
-console.log('start');
+e.on('foo', function() {
+  num_args_emitted.push(arguments.length);
+});
+
+e.on('foo', function() {
+  num_args_emitted.push(arguments.length);
+});
 
 e.emit('numArgs');
 e.emit('numArgs', null);
@@ -21,6 +26,8 @@ e.emit('numArgs', null, null, null);
 e.emit('numArgs', null, null, null, null);
 e.emit('numArgs', null, null, null, null, null);
 
+e.emit('foo', null, null, null, null);
+
 process.on('exit', function() {
-  assert.deepStrictEqual([0, 1, 2, 3, 4, 5], num_args_emited);
+  assert.deepStrictEqual([0, 1, 2, 3, 4, 5, 4, 4], num_args_emitted);
 });
