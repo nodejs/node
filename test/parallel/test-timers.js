6e25ea5cc13290a36197e9021da43af2b3c65d95
Rich Trott <rtrott@gmail.com>
2017-01-22 21:26:55 -0800

    test: enhance test-timers
    In test-timers, confirm that all input values that should be coerced to
    1 ms are not being coerced to a significantly larger value.
    
    This eliminates the need for the separate test-regress-GH-897.
    
    PR-URL: https://github.com/nodejs/node/pull/10960
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-timers.js
+++ test/parallel/test-timers.js
@@ -1,6 +1,6 @@
 'use strict';
-require('../common');
-var assert = require('assert');
+const common = require('../common');
+const assert = require('assert');
 
 var inputs = [
   undefined,
@@ -18,13 +18,14 @@ var inputs = [
   -10,
   -1,
   -0.5,
+  -0.1,
   -0.0,
   0,
   0.0,
+  0.1,
   0.5,
   1,
   1.0,
-  10,
   2147483648,     // browser behaviour: timeouts > 2^31-1 run on next tick
   12345678901234  // ditto
 ];
@@ -43,10 +44,17 @@ inputs.forEach(function(value, index) {
   }, value);
 });
 
-process.on('exit', function() {
-  // assert that all timers have run
+// All values in inputs array coerce to 1 ms. Therefore, they should all run
+// before a timer set here for 2 ms.
+
+setTimeout(common.mustCall(function() {
+  // assert that all other timers have run
   inputs.forEach(function(value, index) {
-    assert.equal(true, timeouts[index]);
-    assert.equal(true, intervals[index]);
+    assert(timeouts[index]);
+    assert(intervals[index]);
   });
-});
+}), 2);
+
+// Test 10 ms timeout separately.
+setTimeout(common.mustCall(function() {}), 10);
+setInterval(common.mustCall(function() { clearInterval(this); }), 10);
