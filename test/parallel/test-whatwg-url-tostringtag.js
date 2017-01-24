232903b24b0fd266afbce2aad25e59b3491eb6dc
Timothy Gu <timothygu99@gmail.com>
2017-01-23 17:06:58 -0800

    url: make URLSearchParams and its Iterator's properties spec-compliant

--- test/parallel/test-whatwg-url-tostringtag.js
+++ test/parallel/test-whatwg-url-tostringtag.js
@@ -11,11 +11,11 @@ const sp = url.searchParams;
 const test = [
   [toString.call(url), 'URL'],
   [toString.call(sp), 'URLSearchParams'],
-  [toString.call(Object.getPrototypeOf(sp)), 'URLSearchParamsPrototype'],
   // Web IDL spec says we have to return 'URLPrototype', but it is too
   // expensive to implement; therefore, use Chrome's behavior for now, until
   // spec is changed.
-  [toString.call(Object.getPrototypeOf(url)), 'URL']
+  [toString.call(Object.getPrototypeOf(url)), 'URL'],
+  [toString.call(Object.getPrototypeOf(sp)), 'URLSearchParams']
 ];
 
 test.forEach((row) => {
