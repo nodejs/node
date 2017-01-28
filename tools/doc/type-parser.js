e8a3c95629671bd1c9c8df670240825b8f1d7d9e
Timothy Gu <timothygu99@gmail.com>
2017-01-28 12:02:20 -0800

    tools: add MDN link for Iterable

--- tools/doc/type-parser.js
+++ tools/doc/type-parser.js
@@ -36,6 +36,8 @@ const typeMap = {
   'http.IncomingMessage': 'http.html#http_class_http_incomingmessage',
   'http.Server': 'http.html#http_class_http_server',
   'http.ServerResponse': 'http.html#http_class_http_serverresponse',
+  'Iterable': jsDocPrefix +
+              'Reference/Iteration_protocols#The_iterable_protocol',
   'Iterator': jsDocPrefix +
               'Reference/Iteration_protocols#The_iterator_protocol'
 };
