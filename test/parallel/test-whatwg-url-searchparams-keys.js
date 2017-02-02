b5e40a059d88ecf280c7c2629e8acd3873f3f161
Joyee Cheung <joyeec9h3@gmail.com>
2017-02-02 14:13:16 +0800

    test, url: synchronize WPT url tests
    * attributon of WPT in url-setter-tests
    * add WPT test utilities
    * synchronize WPT URLSearchParams tests
    * synchronize WPT url tests
    * split whatwg-url-inspect test
    * port historical url tests from WPT
    * protocol setter and special URLs
    
    Refs: https://github.com/w3c/web-platform-tests/pull/4413
    Refs: https://github.com/whatwg/url/issues/104
    Backport-of: https://github.com/nodejs/node/pull/11079

--- test/parallel/test-whatwg-url-searchparams-keys.js
+++ test/parallel/test-whatwg-url-searchparams-keys.js
@@ -4,6 +4,7 @@ require('../common');
 const assert = require('assert');
 const URLSearchParams = require('url').URLSearchParams;
 
+// Tests below are not from WPT.
 const params = new URLSearchParams('a=b&c=d');
 const keys = params.keys();
 
--- test/parallel/test-whatwg-url-setters.js
+++ test/parallel/test-whatwg-url-setters.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const common = require('../common');
+const path = require('path');
+const URL = require('url').URL;
+const { test, assert_equals } = common.WPT;
 
 if (!common.hasIntl) {
   // A handful of the tests fail when ICU is not included.
@@ -8,25 +11,68 @@ if (!common.hasIntl) {
   return;
 }
 
-const path = require('path');
-const URL = require('url').URL;
-const assert = require('assert');
-const attrs = require(path.join(common.fixturesDir, 'url-setter-tests.json'));
+const request = {
+  response: require(path.join(common.fixturesDir, 'url-setter-tests.json'))
+};
 
-for (const attr in attrs) {
-  if (attr === 'comment')
-    continue;
-  const tests = attrs[attr];
-  let n = 0;
-  for (const test of tests) {
-    if (test.skip) continue;
-    n++;
-    const url = new URL(test.href);
-    url[attr] = test.new_value;
-    for (const test_attr in test.expected) {
-      assert.strictEqual(test.expected[test_attr], url[test_attr],
-                         `${n} ${attr} ${test_attr} ` +
-                         `${test.href} ${test.comment}`);
+/* eslint-disable */
+/* WPT Refs:
+   https://github.com/w3c/web-platform-tests/blob/8791bed/url/url-setters.html
+   License: http://www.w3.org/Consortium/Legal/2008/04-testsuite-copyright.html
+*/
+function startURLSettersTests() {
+//   var setup = async_test("Loading data…")
+//   setup.step(function() {
+//     var request = new XMLHttpRequest()
+//     request.open("GET", "setters_tests.json")
+//     request.send()
+//     request.responseType = "json"
+//     request.onload = setup.step_func(function() {
+         runURLSettersTests(request.response)
+//       setup.done()
+//     })
+//   })
+}
+
+function runURLSettersTests(all_test_cases) {
+  for (var attribute_to_be_set in all_test_cases) {
+    if (attribute_to_be_set == "comment") {
+      continue;
+    }
+    var test_cases = all_test_cases[attribute_to_be_set];
+    for(var i = 0, l = test_cases.length; i < l; i++) {
+      var test_case = test_cases[i];
+      var name = "Setting <" + test_case.href + ">." + attribute_to_be_set +
+                 " = '" + test_case.new_value + "'";
+      if ("comment" in test_case) {
+        name += " " + test_case.comment;
+      }
+      test(function() {
+        var url = new URL(test_case.href);
+        url[attribute_to_be_set] = test_case.new_value;
+        for (var attribute in test_case.expected) {
+          assert_equals(url[attribute], test_case.expected[attribute])
+        }
+      }, "URL: " + name)
+      // test(function() {
+      //   var url = document.createElement("a");
+      //   url.href = test_case.href;
+      //   url[attribute_to_be_set] = test_case.new_value;
+      //   for (var attribute in test_case.expected) {
+      //     assert_equals(url[attribute], test_case.expected[attribute])
+      //   }
+      // }, "<a>: " + name)
+      // test(function() {
+      //   var url = document.createElement("area");
+      //   url.href = test_case.href;
+      //   url[attribute_to_be_set] = test_case.new_value;
+      //   for (var attribute in test_case.expected) {
+      //     assert_equals(url[attribute], test_case.expected[attribute])
+      //   }
+      // }, "<area>: " + name)
     }
   }
 }
+
+startURLSettersTests()
+/* eslint-enable */
