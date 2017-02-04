87725677a30db850abfc4c0aab3f337aa3c5fb73
DavidCai <davidcai1993@yahoo.com>
2017-02-04 21:39:20 +0800

    url: fix error message of url.format

--- test/parallel/test-url-format-invalid-input.js
+++ test/parallel/test-url-format-invalid-input.js
@@ -3,17 +3,20 @@ require('../common');
 const assert = require('assert');
 const url = require('url');
 
-// https://github.com/nodejs/node/pull/1036
-const throws = [
-  undefined,
-  null,
-  true,
-  false,
-  0,
-  function() {}
-];
-for (let i = 0; i < throws.length; i++) {
-  assert.throws(function() { url.format(throws[i]); }, TypeError);
+const throwsObjsAndReportTypes = new Map([
+  [undefined, 'undefined'],
+  [null, 'null'],
+  [true, 'boolean'],
+  [false, 'boolean'],
+  [0, 'number'],
+  [function() {}, 'function'],
+  [Symbol('foo'), 'symbol']
+]);
+
+for (const [obj, type] of throwsObjsAndReportTypes) {
+  const error = new RegExp('^TypeError: Parameter "urlObj" must be an object' +
+                           `, not ${type}$`);
+  assert.throws(function() { url.format(obj); }, error);
 }
 assert.strictEqual(url.format(''), '');
 assert.strictEqual(url.format({}), '');
