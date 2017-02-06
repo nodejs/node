039a813ff059c0787b9f4902f342914cd30a2845
Ben Noordhuis <info@bnoordhuis.nl>
2017-02-06 21:21:16 +0100

    deps: back-port b049d1a from V8 upstream
    Original commit message:
    
        Ensure we align zone memory at 8 byte boundaries on all platforms
    
        BUG=v8:5668
        R=verwaest@chromium.org
    
        Review-Url: https://codereview.chromium.org/2672203002
        Cr-Commit-Position: refs/heads/master@{#42959}
    
    PR-URL: https://github.com/nodejs/node/pull/11204
    Reviewed-By: Ali Ijaz Sheikh <ofrobots@google.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ deps/v8/test/unittests/zone/zone-unittest.cc
@@ -0,0 +1,23 @@
+// Copyright 2017 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "src/zone/zone.h"
+
+#include "src/zone/accounting-allocator.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace v8 {
+namespace internal {
+
+TEST(Zone, 8ByteAlignment) {
+  AccountingAllocator allocator;
+  Zone zone(&allocator);
+
+  for (size_t i = 0; i < 16; ++i) {
+    ASSERT_EQ(reinterpret_cast<intptr_t>(zone.New(i)) % 8, 0);
+  }
+}
+
+}  // namespace internal
+}  // namespace v8
