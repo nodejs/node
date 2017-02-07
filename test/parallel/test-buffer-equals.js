f8cdaaa16ad01b946c7528c473f7fb80cfbeb3a2
Sebastian Roeder <afk@Sebastians-iMac.local>
2017-02-07 06:38:45 -0800

    test: validate error message from buffer.equals
    Adding regular expression as an argument to an assert.throws().
    
    PR-URL: https://github.com/nodejs/node/pull/11215
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Yuta Hiroto <hello@about-hiroppy.com>

--- test/parallel/test-buffer-equals.js
+++ test/parallel/test-buffer-equals.js
@@ -14,4 +14,5 @@ assert.ok(!d.equals(e));
 assert.ok(d.equals(d));
 assert.ok(d.equals(new Uint8Array([0x61, 0x62, 0x63, 0x64, 0x65])));
 
-assert.throws(() => Buffer.alloc(1).equals('abc'));
+assert.throws(() => Buffer.alloc(1).equals('abc'),
+              /^TypeError: Argument must be a Buffer or Uint8Array$/);
