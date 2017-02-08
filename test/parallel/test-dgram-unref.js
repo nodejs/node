b471392f8c6eb8dff24439ded1e51ce6e6710157
cjihrig <cjihrig@gmail.com>
2017-02-08 10:15:49 -0500

    test: increase dgram ref()/unref() coverage
    This commit completes code coverage for dgram's Socket#ref() and
    Socket#unref() methods.
    
    PR-URL: https://github.com/nodejs/node/pull/11240
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-dgram-unref.js
+++ test/parallel/test-dgram-unref.js
@@ -2,8 +2,18 @@
 const common = require('../common');
 const dgram = require('dgram');
 
-const s = dgram.createSocket('udp4');
-s.bind();
-s.unref();
+{
+  // Test the case of unref()'ing a socket with a handle.
+  const s = dgram.createSocket('udp4');
+  s.bind();
+  s.unref();
+}
+
+{
+  // Test the case of unref()'ing a socket with no handle.
+  const s = dgram.createSocket('udp4');
+
+  s.close(common.mustCall(() => s.unref()));
+}
 
 setTimeout(common.mustNotCall(), 1000).unref();
