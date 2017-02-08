ff785fd51792a95281014b5e9b526c6619450dc9
Brian White <mscdex@mscdex.net>
2017-02-08 03:23:03 -0500

    querystring: fix empty pairs and optimize parse()
    This commit fixes handling of empty pairs that occur before the end
    of the query string so that they are also ignored.
    
    Additionally, some optimizations have been made, including:
    
    * Avoid unnecessary code execution where possible
    * Use a lookup table when checking for hex characters
    * Avoid forced decoding when '+' characters are encountered and we
    are using the default decoder
    
    Fixes: https://github.com/nodejs/node/issues/10454
    PR-URL: https://github.com/nodejs/node/pull/11234
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Nicu Micleușanu <micnic90@gmail.com>

--- benchmark/querystring/querystring-parse.js
+++ benchmark/querystring/querystring-parse.js
@@ -12,7 +12,9 @@ var inputs = {
   multivalue: 'foo=bar&foo=baz&foo=quux&quuy=quuz',
   multivaluemany: 'foo=bar&foo=baz&foo=quux&quuy=quuz&foo=abc&foo=def&' +
                   'foo=ghi&foo=jkl&foo=mno&foo=pqr&foo=stu&foo=vwxyz',
-  manypairs: 'a&b&c&d&e&f&g&h&i&j&k&l&m&n&o&p&q&r&s&t&u&v&w&x&y&z'
+  manypairs: 'a&b&c&d&e&f&g&h&i&j&k&l&m&n&o&p&q&r&s&t&u&v&w&x&y&z',
+  manyblankpairs: '&&&&&&&&&&&&&&&&&&&&&&&&',
+  altspaces: 'foo+bar=baz+quux&xyzzy+thud=quuy+quuz&abc=def+ghi'
 };
 
 var bench = common.createBenchmark(main, {
@@ -20,34 +22,38 @@ var bench = common.createBenchmark(main, {
   n: [1e6],
 });
 
+// A deopt followed by a reopt of main() can happen right when the timed loop
+// starts, which seems to have a noticeable effect on the benchmark results.
+// So we explicitly disable optimization of main() to avoid this potential
+// issue.
+v8.setFlagsFromString('--allow_natives_syntax');
+eval('%NeverOptimizeFunction(main)');
+
 function main(conf) {
   var type = conf.type;
   var n = conf.n | 0;
   var input = inputs[type];
-
-  // Force-optimize querystring.parse() so that the benchmark doesn't get
-  // disrupted by the optimizer kicking in halfway through.
-  v8.setFlagsFromString('--allow_natives_syntax');
-  if (type !== 'multicharsep') {
-    querystring.parse(input);
-    eval('%OptimizeFunctionOnNextCall(querystring.parse)');
-    querystring.parse(input);
-  } else {
-    querystring.parse(input, '&&&&&&&&&&');
-    eval('%OptimizeFunctionOnNextCall(querystring.parse)');
-    querystring.parse(input, '&&&&&&&&&&');
-  }
-
   var i;
-  if (type !== 'multicharsep') {
-    bench.start();
+
+  // Note: we do *not* use OptimizeFunctionOnNextCall() here because currently
+  // it causes a deopt followed by a reopt, which could make its way into the
+  // timed loop. Instead, just execute the function a "sufficient" number of
+  // times before the timed loop to ensure the function is optimized just once.
+  if (type === 'multicharsep') {
     for (i = 0; i < n; i += 1)
-      querystring.parse(input);
-    bench.end(n);
-  } else {
+      querystring.parse(input, '&&&&&&&&&&');
+
     bench.start();
     for (i = 0; i < n; i += 1)
       querystring.parse(input, '&&&&&&&&&&');
+    bench.end(n);
+  } else {
+    for (i = 0; i < n; i += 1)
+      querystring.parse(input);
+
+    bench.start();
+    for (i = 0; i < n; i += 1)
+      querystring.parse(input);
     bench.end(n);
   }
 }
