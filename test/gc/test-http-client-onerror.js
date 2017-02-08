a3a6892f9d6e6e8b26d80f8bc80b492281096516
Ben Noordhuis <info@bnoordhuis.nl>
2017-02-08 15:33:52 +0100

    test: don't call process.exit() in gc tests
    Rewrite the tests in test/gc so that they no longer call process.exit().
    Instead they exit gracefully now.
    
    PR-URL: https://github.com/nodejs/node/pull/11239
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>

--- test/gc/test-http-client-onerror.js
+++ test/gc/test-http-client-onerror.js
@@ -12,7 +12,6 @@ function serverHandler(req, res) {
 
 const http = require('http');
 const weak = require('weak');
-const assert = require('assert');
 const todo = 500;
 let done = 0;
 let count = 0;
@@ -31,7 +30,6 @@ function getall() {
     function cb(res) {
       res.resume();
       done += 1;
-      statusLater();
     }
     function onerror(er) {
       throw er;
@@ -59,20 +57,11 @@ function afterGC() {
   countGC++;
 }
 
-let timer;
-function statusLater() {
-  global.gc();
-  if (timer) clearTimeout(timer);
-  timer = setTimeout(status, 1);
-}
+setInterval(status, 100).unref();
 
 function status() {
   global.gc();
   console.log('Done: %d/%d', done, todo);
   console.log('Collected: %d/%d', countGC, count);
-  if (done === todo) {
-    console.log('All should be collected now.');
-    assert.strictEqual(count, countGC);
-    process.exit(0);
-  }
+  if (countGC === todo) server.close();
 }
