01274503490946558b0f3f036b91e0a6082abf96
Joyee Cheung <joyeec9h3@gmail.com>
2017-02-09 23:42:14 +0800

    benchmark: clean up legacy url benchmarks
    Backport-of: https://github.com/nodejs/node/pull/11264

--- benchmark/querystring/querystring-parse.js
+++ benchmark/querystring/querystring-parse.js
@@ -3,17 +3,7 @@ var common = require('../common.js');
 var querystring = require('querystring');
 var v8 = require('v8');
 
-var inputs = {
-  noencode: 'foo=bar&baz=quux&xyzzy=thud',
-  multicharsep: 'foo=bar&&&&&&&&&&baz=quux&&&&&&&&&&xyzzy=thud',
-  encodefake: 'foo=%©ar&baz=%A©uux&xyzzy=%©ud',
-  encodemany: '%66%6F%6F=bar&%62%61%7A=quux&xyzzy=%74h%75d',
-  encodelast: 'foo=bar&baz=quux&xyzzy=thu%64',
-  multivalue: 'foo=bar&foo=baz&foo=quux&quuy=quuz',
-  multivaluemany: 'foo=bar&foo=baz&foo=quux&quuy=quuz&foo=abc&foo=def&' +
-                  'foo=ghi&foo=jkl&foo=mno&foo=pqr&foo=stu&foo=vwxyz',
-  manypairs: 'a&b&c&d&e&f&g&h&i&j&k&l&m&n&o&p&q&r&s&t&u&v&w&x&y&z'
-};
+var inputs = require('../fixtures/url-inputs.js').searchParams;
 
 var bench = common.createBenchmark(main, {
   type: Object.keys(inputs),
