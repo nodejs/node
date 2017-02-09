b9bc94f9fa2d2b58d0b483ef7b366e761b3a8166
Joyee Cheung <joyeec9h3@gmail.com>
2017-02-09 23:41:14 +0800

    benchmark: move url data to fixtures
    PR-URL: https://github.com/nodejs/node/pull/11264
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Brian White <mscdex@mscdex.net>

--- benchmark/url/legacy-vs-whatwg-url-serialize.js
+++ benchmark/url/legacy-vs-whatwg-url-serialize.js
@@ -3,19 +3,7 @@ const common = require('../common.js');
 const url = require('url');
 const URL = url.URL;
 const assert = require('assert');
-
-const inputs = {
-  long: 'http://nodejs.org:89/docs/latest/api/url.html#test?' +
-        'payload1=true&payload2=false&test=1&benchmark=3&' +
-        'foo=38.38.011.293&bar=1234834910480&test=19299&3992&' +
-        'key=f5c65e1e98fe07e648249ad41e1cfdb0',
-  short: 'https://nodejs.org/en/blog/',
-  idn: 'http://你好你好',
-  auth: 'https://user:pass@example.com/path?search=1',
-  special: 'file:///foo/bar/test/node.js',
-  percent: 'https://%E4%BD%A0/foo',
-  dot: 'https://example.org/./a/../b/./c'
-};
+const inputs = require('../fixtures/url-inputs.js').urls;
 
 const bench = common.createBenchmark(main, {
   type: Object.keys(inputs),
