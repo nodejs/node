e571fd4f6ccadd9e77095ca0377a9b07f49bc56b
Joyee Cheung <joyeec9h3@gmail.com>
2017-02-09 23:41:14 +0800

    benchmark: move url data to fixtures
    PR-URL: https://github.com/nodejs/node/pull/11264
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Brian White <mscdex@mscdex.net>

--- benchmark/url/legacy-vs-whatwg-url-searchparams-parse.js
+++ benchmark/url/legacy-vs-whatwg-url-searchparams-parse.js
@@ -2,18 +2,7 @@
 const common = require('../common.js');
 const { URLSearchParams } = require('url');
 const querystring = require('querystring');
-
-const inputs = {
-  noencode: 'foo=bar&baz=quux&xyzzy=thud',
-  encodemany: '%66%6F%6F=bar&%62%61%7A=quux&xyzzy=%74h%75d',
-  encodefake: 'foo=%©ar&baz=%A©uux&xyzzy=%©ud',
-  encodelast: 'foo=bar&baz=quux&xyzzy=thu%64',
-  multicharsep: 'foo=bar&&&&&&&&&&baz=quux&&&&&&&&&&xyzzy=thud',
-  multivalue: 'foo=bar&foo=baz&foo=quux&quuy=quuz',
-  multivaluemany: 'foo=bar&foo=baz&foo=quux&quuy=quuz&foo=abc&foo=def&' +
-                  'foo=ghi&foo=jkl&foo=mno&foo=pqr&foo=stu&foo=vwxyz',
-  manypairs: 'a&b&c&d&e&f&g&h&i&j&k&l&m&n&o&p&q&r&s&t&u&v&w&x&y&z'
-};
+const inputs = require('../fixtures/url-inputs.js').searchParams;
 
 const bench = common.createBenchmark(main, {
   type: Object.keys(inputs),
