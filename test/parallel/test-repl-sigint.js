32512af41cfc8420abe874ee9b9b13061c77250a
Rich Trott <rtrott@gmail.com>
2017-02-10 23:26:25 -0800

    test: refactor test-repl-sigint
    * remove debugging code that prints child stdout
    * indexOf() -> includes()
    * improved messages on assertion failures

--- test/parallel/test-repl-sigint.js
+++ test/parallel/test-repl-sigint.js
@@ -17,17 +17,9 @@ const child = spawn(process.execPath, [ '-i' ], {
 
 let stdout = '';
 child.stdout.setEncoding('utf8');
-child.stdout.pipe(process.stdout);
 child.stdout.on('data', function(c) {
   stdout += c;
 });
-
-child.stdin.write = ((original) => {
-  return (chunk) => {
-    process.stderr.write(chunk);
-    return original.call(child.stdin, chunk);
-  };
-})(child.stdin.write);
 
 child.stdout.once('data', common.mustCall(() => {
   process.on('SIGUSR2', common.mustCall(() => {
@@ -45,6 +37,12 @@ child.stdout.once('data', common.mustCall(() => {
 
 child.on('close', function(code) {
   assert.strictEqual(code, 0);
-  assert.notStrictEqual(stdout.indexOf('Script execution interrupted.\n'), -1);
-  assert.notStrictEqual(stdout.indexOf('42042\n'), -1);
+  assert.ok(
+    stdout.includes('Script execution interrupted.\n'),
+    `Expected stdout to contain "Script execution interrupted.", got ${stdout}`
+  );
+  assert.ok(
+    stdout.includes('42042\n'),
+    `Expected stdout to contain "42042", got ${stdout}`
+  );
 });
