ae9b891a3904cd0b1017453ccec3f3bc18a1f093
Rich Trott <rtrott@gmail.com>
2017-02-15 15:21:12 -0800

    test: use expectsError in require-invalid-package
    Use common.expectsError() in place of inline validation function in
    test-require-invalid-package.
    
    PR-URL: https://github.com/nodejs/node/pull/11409
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-require-invalid-package.js
+++ test/parallel/test-require-invalid-package.js
@@ -1,9 +1,9 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 
 // Should be an invalid package path.
-assert.throws(() => require('package.json'), (err) => {
-  return err && err.code === 'MODULE_NOT_FOUND';
-});
+assert.throws(() => require('package.json'),
+              common.expectsError('MODULE_NOT_FOUND')
+);
