d9deb1fb62798442e557e305e9a395fcb0c80ccd
Rich Trott <rtrott@gmail.com>
2017-02-14 21:55:50 -0800

    test: improve message in net-connect-local-error
    test-net-connect-local-error can fail with messages that report
    `AssertionError: undefined === 12346`. Unfortunately, this doesn't
    provide sufficient information to identify what went wrong with the
    test. Increase information provided.
    
    PR-URL: https://github.com/nodejs/node/pull/11393
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-net-connect-local-error.js
+++ test/parallel/test-net-connect-local-error.js
@@ -10,6 +10,14 @@ var client = net.connect({
 });
 
 client.on('error', common.mustCall(function onError(err) {
-  assert.equal(err.localPort, common.PORT);
-  assert.equal(err.localAddress, common.localhostIPv4);
+  assert.strictEqual(
+    err.localPort,
+    common.PORT,
+    `${err.localPort} !== ${common.PORT} in ${err}`
+  );
+  assert.strictEqual(
+    err.localAddress,
+    common.localhostIPv4,
+    `${err.localAddress} !== ${common.localhostIPv4} in ${err}`
+  );
 }));
