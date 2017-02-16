54e55e05ca46122691879b5e6dd03f34ecaa8659
Michaël Zasso <targos@protonmail.com>
2017-02-16 09:30:50 +0100

    test: make test-intl-no-icu-data more robust
    In V8 5.6, String#toLocaleUpperCase can work even when no ICU data is
    loaded. Use another method to check the --icu-data-dir option pointing
    to an empty directory.
    
    PR-URL: https://github.com/nodejs/node/pull/10992
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/parallel/test-intl-no-icu-data.js
+++ test/parallel/test-intl-no-icu-data.js
@@ -4,6 +4,5 @@ require('../common');
 const assert = require('assert');
 const config = process.binding('config');
 
-// No-op when ICU case mappings are unavailable.
-assert.strictEqual('ç'.toLocaleUpperCase('el'), 'ç');
+assert.deepStrictEqual(Intl.NumberFormat.supportedLocalesOf('en'), []);
 assert.strictEqual(config.icuDataDir, 'test/fixtures/empty/');
