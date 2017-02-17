0a3b5f319a5b45416d01cb73f3ed006c1101a9fb
Anna Henningsen <anna@addaleax.net>
2017-02-17 18:15:09 +0100

    console: fixup `console.dir()` error handling
    Apply the `console: do not emit error events` changes properly
    to `console.dir()`.
    
    This was overlooked in f18e08d820dde161788d95a5603546ceca021e90
    (https://github.com/nodejs/node/pull/9744).
    
    Ref: https://github.com/nodejs/node/commit/f18e08d820dde161788d95a5603546ceca021e90#commitcomment-20934407
    PR-URL: https://github.com/nodejs/node/pull/11443
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-console-sync-write-error.js
+++ test/parallel/test-console-sync-write-error.js
@@ -4,44 +4,46 @@ const { Console } = require('console');
 const { Writable } = require('stream');
 const assert = require('assert');
 
-{
-  const out = new Writable({
-    write: common.mustCall((chunk, enc, callback) => {
-      callback(new Error('foobar'));
-    })
-  });
+for (const method of ['dir', 'log', 'warn']) {
+  {
+    const out = new Writable({
+      write: common.mustCall((chunk, enc, callback) => {
+        callback(new Error('foobar'));
+      })
+    });
 
-  const c = new Console(out, out, true);
+    const c = new Console(out, out, true);
 
-  assert.doesNotThrow(() => {
-    c.log('abc');
-  });
-}
+    assert.doesNotThrow(() => {
+      c[method]('abc');
+    });
+  }
 
-{
-  const out = new Writable({
-    write: common.mustCall((chunk, enc, callback) => {
-      throw new Error('foobar');
-    })
-  });
+  {
+    const out = new Writable({
+      write: common.mustCall((chunk, enc, callback) => {
+        throw new Error('foobar');
+      })
+    });
 
-  const c = new Console(out, out, true);
+    const c = new Console(out, out, true);
 
-  assert.doesNotThrow(() => {
-    c.log('abc');
-  });
-}
+    assert.doesNotThrow(() => {
+      c[method]('abc');
+    });
+  }
 
-{
-  const out = new Writable({
-    write: common.mustCall((chunk, enc, callback) => {
-      setImmediate(() => callback(new Error('foobar')));
-    })
-  });
+  {
+    const out = new Writable({
+      write: common.mustCall((chunk, enc, callback) => {
+        setImmediate(() => callback(new Error('foobar')));
+      })
+    });
 
-  const c = new Console(out, out, true);
+    const c = new Console(out, out, true);
 
-  assert.doesNotThrow(() => {
-    c.log('abc');
-  });
+    assert.doesNotThrow(() => {
+      c[method]('abc');
+    });
+  }
 }
