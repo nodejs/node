b13d561e708e84280ccb98e00cf8e77ff708a8d0
Anna Henningsen <anna@addaleax.net>
2017-02-17 18:15:09 +0100

    console: fixup `console.dir()` error handling
    Apply the `console: do not emit error events` changes properly
    to `console.dir()`.
    
    This was overlooked in f18e08d820dde161788d95a5603546ceca021e90
    (https://github.com/nodejs/node/pull/9744).
    
    Ref: https://github.com/nodejs/node/commit/f18e08d820dde161788d95a5603546ceca021e90#commitcomment-20934407

--- test/parallel/test-console-async-write-error.js
+++ test/parallel/test-console-async-write-error.js
@@ -4,14 +4,16 @@ const { Console } = require('console');
 const { Writable } = require('stream');
 const assert = require('assert');
 
-const out = new Writable({
-  write: common.mustCall((chunk, enc, callback) => {
-    process.nextTick(callback, new Error('foobar'));
-  })
-});
+for (const method of ['dir', 'log', 'warn']) {
+  const out = new Writable({
+    write: common.mustCall((chunk, enc, callback) => {
+      process.nextTick(callback, new Error('foobar'));
+    })
+  });
 
-const c = new Console(out, out, true);
+  const c = new Console(out, out, true);
 
-assert.doesNotThrow(() => {
-  c.log('abc');
-});
+  assert.doesNotThrow(() => {
+    c[method]('abc');
+  });
+}
