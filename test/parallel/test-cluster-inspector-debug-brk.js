e2c86af1d3940ff777f8ebaadff66c4fab2b31a9
liusi <siyuan.liu@autodesk.com>
2017-02-21 17:15:23 +0800

    test: test clusters --inspect-brk and --debug-brk
    This test ensures that flag --inspect-brk and --debug-brk works
    properly for clusters.

--- test/parallel/test-cluster-inspector-debug-brk.js
+++ test/parallel/test-cluster-inspector-debug-brk.js
@@ -1,10 +1,12 @@
 'use strict';
+// Flags: --inspect={PORT}
 // This test ensures that flag --inspect-brk and --debug-brk works properly
-
 const common = require('../common');
 const assert = require('assert');
 const cluster = require('cluster');
+const net = require('net');
 
+const debuggerPort = common.PORT;
 const script = common.fixturesDir + '/empty.js';
 
 function fail() {
@@ -13,26 +15,29 @@ function fail() {
 
 if (cluster.isMaster) {
 
-  function fork(execArgv) {
-    cluster.setupMaster({execArgv});
+  function fork(offset, execArgv) {
+    cluster.setupMaster(execArgv);
     let worker = cluster.fork();
 
     worker.on('exit', fail);
 
     // give node time to start up the debugger
-    setTimeout(() => {
+    let socket = net.connect(debuggerPort + offset, () => {
+      socket.end();
       worker.removeListener('exit', fail);
       worker.kill();
-    }, 2000);
+    });
 
     return worker;
   }
   
+  assert.strictEqual(process.debugPort, debuggerPort);
+
   let workers = [
-    fork([script, '--inspect-brk']),
-    fork([script, `--inspect-brk=5959`]),
-    fork([script, '--debug-brk']),
-    fork([script, `--debug-brk=9230`])
+    fork(1, [`--inspect-brk`, script]),
+    fork(2, [`--inspect-brk=${debuggerPort}`, script]),
+    fork(3, [`--debug-brk`, script]),
+    fork(4, [`--debug-brk=${debuggerPort}`, script])
   ];
 
   process.on('exit', function() {
