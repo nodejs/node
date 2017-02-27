cd3c478f70dd71b8aeb02e041f686fa44742d81b
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2017-02-28 00:15:58 +0530

    test: skip the test with proper TAP message
    On Windows, the test simply returns which will be counted as passed.
    The test actually skips the rest of the test. So proper TAP message has
    to be included while skipping.
    
    This patch uses `common.skip` rather than `console.log` to print the
    skip messages.
    
    PR-URL: https://github.com/nodejs/node/pull/11584
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-setproctitle.js
+++ test/parallel/test-setproctitle.js
@@ -4,8 +4,7 @@ const common = require('../common');
 
 // FIXME add sunos support
 if (common.isSunOS) {
-  console.log(`1..0 # Skipped: Unsupported platform [${process.platform}]`);
-  return;
+  return common.skip(`Unsupported platform [${process.platform}]`);
 }
 
 const assert = require('assert');
@@ -22,8 +21,9 @@ process.title = title;
 assert.strictEqual(process.title, title);
 
 // Test setting the title but do not try to run `ps` on Windows.
-if (common.isWindows)
-  return;
+if (common.isWindows) {
+  return common.skip('Windows does not have "ps" utility');
+}
 
 exec(`ps -p ${process.pid} -o args=`, function callback(error, stdout, stderr) {
   assert.ifError(error);
