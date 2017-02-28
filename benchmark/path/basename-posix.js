f1f899ae0eb21cd7798db0c5ed6dfd6f4e8c9c3e
Bartosz Sosnowski <bartosz@janeasystems.com>
2017-02-28 18:40:53 +0100

    benchmark: remove benchmarks forced optimizations
    Removes all instances of %OptimizeFunctionOnNextCall from benchmarks
    
    Refs: https://github.com/nodejs/node/pull/9615
    Refs: https://github.com/nodejs/node/pull/11720

--- benchmark/path/basename-posix.js
+++ benchmark/path/basename-posix.js
@@ -1,7 +1,6 @@
 'use strict';
 var common = require('../common.js');
 var path = require('path');
-var v8 = require('v8');
 
 var bench = common.createBenchmark(main, {
   pathext: [
@@ -29,12 +28,6 @@ function main(conf) {
     ext = input.slice(extIdx + 1);
     input = input.slice(0, extIdx);
   }
-
-  // Force optimization before starting the benchmark
-  p.basename(input, ext);
-  v8.setFlagsFromString('--allow_natives_syntax');
-  eval('%OptimizeFunctionOnNextCall(p.basename)');
-  p.basename(input, ext);
 
   bench.start();
   for (var i = 0; i < n; i++) {
