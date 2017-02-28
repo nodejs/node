54adf3705b989a574a6ede96ac3ed9550bc2f773
Sam Roberts <vieuxtech@gmail.com>
2017-02-28 10:46:29 -0800

    deps: fix CLEAR_HASH macro to be usable as a single statement
    Apply unreleased (as of zlib v1.2.11) patch from upstream:
    - https://github.com/madler/zlib/commit/38e8ce32afbaa82f67d992b9f3056f281fe69259
    
    Original commit message:
      Fix CLEAR_HASH macro to be usable as a single statement.
    
      As it is used in deflateParams().
    
    PR-URL: https://github.com/nodejs/node/pull/11616
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- deps/zlib/deflate.c
+++ deps/zlib/deflate.c
@@ -190,8 +190,11 @@ local const config configuration_table[10] = {
  * prev[] will be initialized on the fly.
  */
 #define CLEAR_HASH(s) \
-    s->head[s->hash_size-1] = NIL; \
-    zmemzero((Bytef *)s->head, (unsigned)(s->hash_size-1)*sizeof(*s->head));
+    do { \
+        s->head[s->hash_size-1] = NIL; \
+        zmemzero((Bytef *)s->head, \
+                 (unsigned)(s->hash_size-1)*sizeof(*s->head)); \
+    } while (0)
 
 /* ===========================================================================
  * Slide the hash table when sliding the window down (could be avoided with 32
