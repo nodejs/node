d01a309f9083a9d37688ed6c54bd36f464e2de1f
Bartosz Sosnowski <bartosz@janeasystems.com>
2017-02-28 18:45:24 +0100

    benchmark: remove forced optimization from es
    This removes all instances of %OptimizeFunctionOnNextCall from es
    benchmarks
    
    PR-URL: https://github.com/nodejs/node/pull/9615
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- benchmark/es/defaultparams-bench.js
+++ benchmark/es/defaultparams-bench.js
@@ -22,8 +22,6 @@ function defaultParams(x = 1, y = 2) {
 
 function runOldStyleDefaults(n) {
 
-  common.v8ForceOptimization(oldStyleDefaults);
-
   var i = 0;
   bench.start();
   for (; i < n; i++)
@@ -32,8 +30,6 @@ function runOldStyleDefaults(n) {
 }
 
 function runDefaultParams(n) {
-
-  common.v8ForceOptimization(defaultParams);
 
   var i = 0;
   bench.start();
