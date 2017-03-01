eba2c62bb10fb65785050600212f70d38f1ffba3
Bartosz Sosnowski <bartosz@janeasystems.com>
2017-03-01 12:37:12 +0100

    benchmark: remove forced optimization from path
    This removes all instances of %OptimizeFunctionOnNextCall from path
    benchmarks
    
    PR-URL: https://github.com/nodejs/node/pull/9615
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- benchmark/path/isAbsolute-posix.js
+++ benchmark/path/isAbsolute-posix.js
@@ -1,7 +1,6 @@
 'use strict';
 var common = require('../common.js');
 var path = require('path');
-var v8 = require('v8');
 
 var bench = common.createBenchmark(main, {
   path: [
@@ -18,12 +17,6 @@ function main(conf) {
   var n = +conf.n;
   var p = path.posix;
   var input = '' + conf.path;
-
-  // Force optimization before starting the benchmark
-  p.isAbsolute(input);
-  v8.setFlagsFromString('--allow_natives_syntax');
-  eval('%OptimizeFunctionOnNextCall(p.isAbsolute)');
-  p.isAbsolute(input);
 
   bench.start();
   for (var i = 0; i < n; i++) {
