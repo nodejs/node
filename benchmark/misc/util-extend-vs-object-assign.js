3adff2a794a92c03ff3b8f3280ad505bbd55bdba
Bartosz Sosnowski <bartosz@janeasystems.com>
2017-03-01 12:36:27 +0100

    benchmark: remove forced optimization from misc
    This removes all instances of %OptimizeFunctionOnNextCall from misc
    benchmarks
    
    PR-URL: https://github.com/nodejs/node/pull/9615
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- benchmark/misc/util-extend-vs-object-assign.js
+++ benchmark/misc/util-extend-vs-object-assign.js
@@ -2,7 +2,6 @@
 
 const common = require('../common.js');
 const util = require('util');
-const v8 = require('v8');
 
 const bench = common.createBenchmark(main, {
   type: ['extend', 'assign'],
@@ -12,24 +11,17 @@ const bench = common.createBenchmark(main, {
 function main(conf) {
   let fn;
   const n = conf.n | 0;
-  let v8command;
 
   if (conf.type === 'extend') {
     fn = util._extend;
-    v8command = '%OptimizeFunctionOnNextCall(util._extend)';
   } else if (conf.type === 'assign') {
     fn = Object.assign;
-    // Object.assign is built-in, cannot be optimized
-    v8command = '';
   }
 
   // Force-optimize the method to test so that the benchmark doesn't
   // get disrupted by the optimizer kicking in halfway through.
   for (var i = 0; i < conf.type.length * 10; i += 1)
     fn({}, process.env);
-
-  v8.setFlagsFromString('--allow_natives_syntax');
-  eval(v8command);
 
   var obj = new Proxy({}, { set: function(a, b, c) { return true; } });
 
