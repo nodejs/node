3ae58acd29fcbde16206f238a2a4cc935892b2de
AnnaMag <AnnaMag@users.noreply.github.com>
2017-03-02 16:03:54 +0000

    test: failing behaviour on sandboxed Proxy
    CopyProperties() causes sandboxed Proxy to throw error
    when in fact no code has been run. The function will
    be removed with the updates to the V8 API.
    
    Here, failing Proxy test case is moved to known_issues.
    
    PR-URL: https://github.com/nodejs/node/pull/11671
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>

---
+++ test/known_issues/test-vm-proxy-failure-CP.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Sandbox throws in CopyProperties() despite no code being run
+// Issue: https://github.com/nodejs/node/issues/11902
+
+
+require('../common');
+const assert = require('assert');
+const vm = require('vm');
+
+const handler = {
+    getOwnPropertyDescriptor: (target, prop) => {
+      throw new Error('whoops');
+    }
+};
+const sandbox = new Proxy({foo: 'bar'}, handler);
+const context = vm.createContext(sandbox);
+
+
+assert.doesNotThrow(() => vm.runInContext('', context));
