821d713a38a139c82f1406d27a8455e3d1ff209c
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-03-03 11:02:20 +0100

    src: remove outdated FIXME in node_crypto.cc
    Issue 4641 contains a FIXME regarding the InitCrypto function. After
    discussing this with bnoordhuis it seems to be an outdated comment.
    
    Refs: https://github.com/nodejs/node/issues/4641
    PR-URL: https://github.com/nodejs/node/pull/11669
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -5998,7 +5998,6 @@ void SetFipsCrypto(const FunctionCallbackInfo<Value>& args) {
 #endif /* NODE_FIPS_MODE */
 }
 
-// FIXME(bnoordhuis) Handle global init correctly.
 void InitCrypto(Local<Object> target,
                 Local<Value> unused,
                 Local<Context> context,
