bc820d5636c452634817abeb5000d303d0e67b3c
Teddy Katz <teddy.katz@gmail.com>
2017-03-05 21:01:09 -0500

    squash: ensure the filename is still used

--- test/parallel/test-cli-syntax.js
+++ test/parallel/test-cli-syntax.js
@@ -52,6 +52,9 @@ const syntaxArgs = [
     // no stdout should be produced
     assert.strictEqual(c.stdout, '', 'stdout produced');
 
+    // stderr should include the filename
+    assert(c.stderr.startsWith(file), "stderr doesn't start with the filename");
+
     // stderr should have a syntax error message
     const match = c.stderr.match(/^SyntaxError: Unexpected identifier$/m);
     assert(match, 'stderr incorrect');
@@ -94,4 +97,23 @@ syntaxArgs.forEach(function(args) {
   assert.strictEqual(c.stderr, '', 'stderr produced');
 
   assert.strictEqual(c.status, 0, 'code == ' + c.status);
+});
+
+// should should throw if code piped from stdin with --check has bad syntax
+// loop each possible option, `-c` or `--check`
+syntaxArgs.forEach(function(args) {
+  const stdin = 'var foo bar;';
+  const c = spawnSync(node, args, {encoding: 'utf8', input: stdin});
+
+  // stderr should include '[stdin]' as the filename
+  assert(c.stderr.startsWith('[stdin]'), "stderr doesn't start with [stdin]");
+
+  // no stdout or stderr should be produced
+  assert.strictEqual(c.stdout, '', 'stdout produced');
+
+  // stderr should have a syntax error message
+  const match = c.stderr.match(/^SyntaxError: Unexpected identifier$/m);
+  assert(match, 'stderr incorrect');
+
+  assert.strictEqual(c.status, 1, 'code == ' + c.status);
 });
