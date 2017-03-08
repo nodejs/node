040b11e92991f69f5b0f80fa57b701b37e82181e
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-03-08 13:39:57 +0100

    test: add hasCrypto check to tls-socket-close
    Currently test-tls-socket-close will fail if node
    was built using --without-ssl. This commit adds a check to
    verify is crypto support exists and if not skip this test.

--- test/parallel/test-tls-socket-close.js
+++ test/parallel/test-tls-socket-close.js
@@ -1,5 +1,9 @@
 'use strict';
 const common = require('../common');
+if (!common.hasCrypto) {
+  common.skip('missing crypto');
+  return;
+}
 const assert = require('assert');
 
 const tls = require('tls');
