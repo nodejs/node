nodejs/node

commit b610a4db1c2919f88711962f5797f25ecb1cd36b

url: enforce valid UTF-8 in WHATWG parser
This commit implements the Web IDL USVString conversion, which mandates
all unpaired Unicode surrogates be turned into U+FFFD REPLACEMENT
CHARACTER. It also disallows Symbols to be used as USVString per spec.

Certain functions call into C++ methods in the binding that use the
Utf8Value class to access string arguments. Utf8Value already does the
normalization using V8's String::Write, so in those cases, instead of
doing the full USVString normalization, only a symbol check is done
(`'' + val`, which uses ES's ToString, versus `String()` which has
special provisions for symbols).

PR-URL: https://github.com/nodejs/node/pull/11436
Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
Reviewed-By: James M Snell <jasnell@gmail.com>

lib/internal/url.js
@@ -23,6 +23,18 @@ const IteratorPrototype = Object.getPrototypeOf(
   Object.getPrototypeOf([][Symbol.iterator]())
 );
 
+const unpairedSurrogateRe =
+    /([^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])/;
+function toUSVString(val) {
+  const str = '' + val;
+  // As of V8 5.5, `str.search()` (and `unpairedSurrogateRe[@@search]()`) are
+  // slower than `unpairedSurrogateRe.exec()`.
+  const match = unpairedSurrogateRe.exec(str);
+  if (!match)
+    return str;
+  return binding.toUSVString(str, match.index);
+}
+
 class OpaqueOrigin {
   toString() {
     return 'null';
@@ -104,7 +116,6 @@ function onParseComplete(flags, protocol, username, password,
 
 // Reused by URL constructor and URL#href setter.
 function parse(url, input, base) {
-  input = String(input);
   const base_context = base ? base[context] : undefined;
   url[context] = new StorageObject();
   binding.parse(input.trim(), -1,
@@ -206,8 +217,10 @@ function onParseHashComplete(flags, protocol, username, password,
 
 class URL {
   constructor(input, base) {
+    // toUSVString is not needed.
+    input = '' + input;
     if (base !== undefined && !(base instanceof URL))
-      base = new URL(String(base));
+      base = new URL(base);
     parse(this, input, base);
   }
 
@@ -315,6 +328,8 @@ Object.defineProperties(URL.prototype, {
       return this[kFormat]({});
     },
     set(input) {
+      // toUSVString is not needed.
+      input = '' + input;
       parse(this, input);
     }
   },
@@ -332,7 +347,8 @@ Object.defineProperties(URL.prototype, {
       return this[context].scheme;
     },
     set(scheme) {
-      scheme = String(scheme);
+      // toUSVString is not needed.
+      scheme = '' + scheme;
       if (scheme.length === 0)
         return;
       binding.parse(scheme, binding.kSchemeStart, null, this[context],
@@ -346,7 +362,8 @@ Object.defineProperties(URL.prototype, {
       return this[context].username || '';
     },
     set(username) {
-      username = String(username);
+      // toUSVString is not needed.
+      username = '' + username;
       if (!this.hostname)
         return;
       const ctx = this[context];
@@ -366,7 +383,8 @@ Object.defineProperties(URL.prototype, {
       return this[context].password || '';
     },
     set(password) {
-      password = String(password);
+      // toUSVString is not needed.
+      password = '' + password;
       if (!this.hostname)
         return;
       const ctx = this[context];
@@ -391,7 +409,8 @@ Object.defineProperties(URL.prototype, {
     },
     set(host) {
       const ctx = this[context];
-      host = String(host);
+      // toUSVString is not needed.
+      host = '' + host;
       if (this[cannotBeBase] ||
           (this[special] && host.length === 0)) {
         // Cannot set the host if cannot-be-base is set or
@@ -415,7 +434,8 @@ Object.defineProperties(URL.prototype, {
     },
     set(host) {
       const ctx = this[context];
-      host = String(host);
+      // toUSVString is not needed.
+      host = '' + host;
       if (this[cannotBeBase] ||
           (this[special] && host.length === 0)) {
         // Cannot set the host if cannot-be-base is set or
@@ -439,11 +459,12 @@ Object.defineProperties(URL.prototype, {
       return port === undefined ? '' : String(port);
     },
     set(port) {
+      // toUSVString is not needed.
+      port = '' + port;
       const ctx = this[context];
       if (!ctx.host || this[cannotBeBase] ||
           this.protocol === 'file:')
         return;
-      port = String(port);
       if (port === '') {
         ctx.port = undefined;
         return;
@@ -462,9 +483,11 @@ Object.defineProperties(URL.prototype, {
       return ctx.path !== undefined ? `/${ctx.path.join('/')}` : '';
     },
     set(path) {
+      // toUSVString is not needed.
+      path = '' + path;
       if (this[cannotBeBase])
         return;
-      binding.parse(String(path), binding.kPathStart, null, this[context],
+      binding.parse(path, binding.kPathStart, null, this[context],
                     onParsePathComplete.bind(this));
     }
   },
@@ -477,7 +500,7 @@ Object.defineProperties(URL.prototype, {
     },
     set(search) {
       const ctx = this[context];
-      search = String(search);
+      search = toUSVString(search);
       if (!search) {
         ctx.query = null;
         ctx.flags &= ~binding.URL_FLAGS_HAS_QUERY;
@@ -509,7 +532,8 @@ Object.defineProperties(URL.prototype, {
     },
     set(hash) {
       const ctx = this[context];
-      hash = String(hash);
+      // toUSVString is not needed.
+      hash = '' + hash;
       if (this.protocol === 'javascript:')
         return;
       if (!hash) {
@@ -652,19 +676,22 @@ class URLSearchParams {
           if (pair.length !== 2) {
             throw new TypeError('Each query pair must be a name/value tuple');
           }
-          this[searchParams].push(String(pair[0]), String(pair[1]));
+          const key = toUSVString(pair[0]);
+          const value = toUSVString(pair[1]);
+          this[searchParams].push(key, value);
         }
       } else {
         // record<USVString, USVString>
         this[searchParams] = [];
-        for (const key of Object.keys(init)) {
-          const value = String(init[key]);
+        for (var key of Object.keys(init)) {
+          key = toUSVString(key);
+          const value = toUSVString(init[key]);
           this[searchParams].push(key, value);
         }
       }
     } else {
       // USVString
-      init = String(init);
+      init = toUSVString(init);
       if (init[0] === '?') init = init.slice(1);
       initSearchParams(this, init);
     }
@@ -743,8 +770,8 @@ defineIDLClass(URLSearchParams.prototype, 'URLSearchParams', {
       throw new TypeError('"name" and "value" arguments must be specified');
     }
 
-    name = String(name);
-    value = String(value);
+    name = toUSVString(name);
+    value = toUSVString(value);
     this[searchParams].push(name, value);
     update(this[context], this);
   },
@@ -758,7 +785,7 @@ defineIDLClass(URLSearchParams.prototype, 'URLSearchParams', {
     }
 
     const list = this[searchParams];
-    name = String(name);
+    name = toUSVString(name);
     for (var i = 0; i < list.length;) {
       const cur = list[i];
       if (cur === name) {
@@ -779,7 +806,7 @@ defineIDLClass(URLSearchParams.prototype, 'URLSearchParams', {
     }
 
     const list = this[searchParams];
-    name = String(name);
+    name = toUSVString(name);
     for (var i = 0; i < list.length; i += 2) {
       if (list[i] === name) {
         return list[i + 1];
@@ -798,7 +825,7 @@ defineIDLClass(URLSearchParams.prototype, 'URLSearchParams', {
 
     const list = this[searchParams];
     const values = [];
-    name = String(name);
+    name = toUSVString(name);
     for (var i = 0; i < list.length; i += 2) {
       if (list[i] === name) {
         values.push(list[i + 1]);
@@ -816,7 +843,7 @@ defineIDLClass(URLSearchParams.prototype, 'URLSearchParams', {
     }
 
     const list = this[searchParams];
-    name = String(name);
+    name = toUSVString(name);
     for (var i = 0; i < list.length; i += 2) {
       if (list[i] === name) {
         return true;
@@ -834,8 +861,8 @@ defineIDLClass(URLSearchParams.prototype, 'URLSearchParams', {
     }
 
     const list = this[searchParams];
-    name = String(name);
-    value = String(value);
+    name = toUSVString(name);
+    value = toUSVString(value);
 
     // If there are any name-value pairs whose name is `name`, in `list`, set
     // the value of the first such name-value pair to `value` and remove the
@@ -1098,11 +1125,13 @@ function originFor(url, base) {
 }
 
 function domainToASCII(domain) {
-  return binding.domainToASCII(String(domain));
+  // toUSVString is not needed.
+  return binding.domainToASCII('' + domain);
 }
 
 function domainToUnicode(domain) {
-  return binding.domainToUnicode(String(domain));
+  // toUSVString is not needed.
+  return binding.domainToUnicode('' + domain);
 }
 
 // Utility function that converts a URL object into an ordinary
@@ -1188,11 +1217,14 @@ function getPathFromURL(path) {
   return isWindows ? getPathFromURLWin32(path) : getPathFromURLPosix(path);
 }
 
-exports.getPathFromURL = getPathFromURL;
-exports.URL = URL;
-exports.URLSearchParams = URLSearchParams;
-exports.domainToASCII = domainToASCII;
-exports.domainToUnicode = domainToUnicode;
-exports.urlToOptions = urlToOptions;
-exports.formatSymbol = kFormat;
-exports.searchParamsSymbol = searchParams;
+module.exports = {
+  toUSVString,
+  getPathFromURL,
+  URL,
+  URLSearchParams,
+  domainToASCII,
+  domainToUnicode,
+  urlToOptions,
+  formatSymbol: kFormat,
+  searchParamsSymbol: searchParams
+};

test/parallel/test-whatwg-url-searchparams-append.js
@@ -57,4 +57,13 @@ test(function() {
   assert.throws(() => {
     params.set('a');
   }, /^TypeError: "name" and "value" arguments must be specified$/);
+
+  const obj = { toString() { throw new Error('toString'); } };
+  const sym = Symbol();
+  assert.throws(() => params.set(obj, 'b'), /^Error: toString$/);
+  assert.throws(() => params.set('a', obj), /^Error: toString$/);
+  assert.throws(() => params.set(sym, 'b'),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
+  assert.throws(() => params.set('a', sym),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
 }

test/parallel/test-whatwg-url-searchparams-constructor.js
@@ -207,3 +207,19 @@ test(() => {
   assert.throws(() => new URLSearchParams([{ [Symbol.iterator]: 42 }]),
                 /^TypeError: Each query pair must be iterable$/);
 }
+
+{
+  const obj = { toString() { throw new Error('toString'); } };
+  const sym = Symbol();
+
+  assert.throws(() => new URLSearchParams({ a: obj }), /^Error: toString$/);
+  assert.throws(() => new URLSearchParams([['a', obj]]), /^Error: toString$/);
+  assert.throws(() => new URLSearchParams(sym),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
+  assert.throws(() => new URLSearchParams({ a: sym }),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
+  assert.throws(() => new URLSearchParams([[sym, 'a']]),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
+  assert.throws(() => new URLSearchParams([['a', sym]]),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
+}

test/parallel/test-whatwg-url-searchparams-delete.js
@@ -51,6 +51,12 @@ test(function() {
   assert.throws(() => {
     params.delete();
   }, /^TypeError: "name" argument must be specified$/);
+
+  const obj = { toString() { throw new Error('toString'); } };
+  const sym = Symbol();
+  assert.throws(() => params.delete(obj), /^Error: toString$/);
+  assert.throws(() => params.delete(sym),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
 }
 
 // https://github.com/nodejs/node/issues/10480

test/parallel/test-whatwg-url-searchparams-get.js
@@ -42,4 +42,10 @@ test(function() {
   assert.throws(() => {
     params.get();
   }, /^TypeError: "name" argument must be specified$/);
+
+  const obj = { toString() { throw new Error('toString'); } };
+  const sym = Symbol();
+  assert.throws(() => params.get(obj), /^Error: toString$/);
+  assert.throws(() => params.get(sym),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
 }

test/parallel/test-whatwg-url-searchparams-getall.js
@@ -46,4 +46,10 @@ test(function() {
   assert.throws(() => {
     params.getAll();
   }, /^TypeError: "name" argument must be specified$/);
+
+  const obj = { toString() { throw new Error('toString'); } };
+  const sym = Symbol();
+  assert.throws(() => params.getAll(obj), /^Error: toString$/);
+  assert.throws(() => params.getAll(sym),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
 }

test/parallel/test-whatwg-url-searchparams-has.js
@@ -45,4 +45,10 @@ test(function() {
   assert.throws(() => {
     params.has();
   }, /^TypeError: "name" argument must be specified$/);
+
+  const obj = { toString() { throw new Error('toString'); } };
+  const sym = Symbol();
+  assert.throws(() => params.has(obj), /^Error: toString$/);
+  assert.throws(() => params.has(sym),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
 }

test/parallel/test-whatwg-url-searchparams-set.js
@@ -43,4 +43,13 @@ test(function() {
   assert.throws(() => {
     params.set('a');
   }, /^TypeError: "name" and "value" arguments must be specified$/);
+
+  const obj = { toString() { throw new Error('toString'); } };
+  const sym = Symbol();
+  assert.throws(() => params.append(obj, 'b'), /^Error: toString$/);
+  assert.throws(() => params.append('a', obj), /^Error: toString$/);
+  assert.throws(() => params.append(sym, 'b'),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
+  assert.throws(() => params.append('a', sym),
+                /^TypeError: Cannot convert a Symbol value to a string$/);
 }

commit 32eb72318cdbcc1f39ad917abd20b208d123fb7a

url: prioritize toString when stringifying
The ES addition operator calls the ToPrimitive() abstract operation
without hint String, leading a subsequent OrdinaryToPrimitive() to call
valueOf() first on an object rather than the desired toString().

Instead, use template literals which directly call ToString() abstract
operation, per Web IDL spec.

PR-URL: https://github.com/nodejs/node/pull/11737
Fixes: b610a4db1c2919f887119 "url: enforce valid UTF-8 in WHATWG parser"
Refs: https://github.com/nodejs/node/commit/b610a4db1c2919f88711962f5797f25ecb1cd36b#commitcomment-21200056
Refs: https://tc39.github.io/ecma262/#sec-addition-operator-plus-runtime-semantics-evaluation
Refs: https://tc39.github.io/ecma262/#sec-template-literals-runtime-semantics-evaluation
Reviewed-By: Michaël Zasso <targos@protonmail.com>
Reviewed-By: Daijiro Wachi <daijiro.wachi@gmail.com>
Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
Reviewed-By: James M Snell <jasnell@gmail.com>

lib/internal/url.js
@@ -26,7 +26,7 @@ const IteratorPrototype = Object.getPrototypeOf(
 const unpairedSurrogateRe =
     /([^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])/;
 function toUSVString(val) {
-  const str = '' + val;
+  const str = `${val}`;
   // As of V8 5.5, `str.search()` (and `unpairedSurrogateRe[@@search]()`) are
   // slower than `unpairedSurrogateRe.exec()`.
   const match = unpairedSurrogateRe.exec(str);
@@ -218,7 +218,7 @@ function onParseHashComplete(flags, protocol, username, password,
 class URL {
   constructor(input, base) {
     // toUSVString is not needed.
-    input = '' + input;
+    input = `${input}`;
     if (base !== undefined && !(base instanceof URL))
       base = new URL(base);
     parse(this, input, base);
@@ -329,7 +329,7 @@ Object.defineProperties(URL.prototype, {
     },
     set(input) {
       // toUSVString is not needed.
-      input = '' + input;
+      input = `${input}`;
       parse(this, input);
     }
   },
@@ -348,7 +348,7 @@ Object.defineProperties(URL.prototype, {
     },
     set(scheme) {
       // toUSVString is not needed.
-      scheme = '' + scheme;
+      scheme = `${scheme}`;
       if (scheme.length === 0)
         return;
       binding.parse(scheme, binding.kSchemeStart, null, this[context],
@@ -363,7 +363,7 @@ Object.defineProperties(URL.prototype, {
     },
     set(username) {
       // toUSVString is not needed.
-      username = '' + username;
+      username = `${username}`;
       if (!this.hostname)
         return;
       const ctx = this[context];
@@ -384,7 +384,7 @@ Object.defineProperties(URL.prototype, {
     },
     set(password) {
       // toUSVString is not needed.
-      password = '' + password;
+      password = `${password}`;
       if (!this.hostname)
         return;
       const ctx = this[context];
@@ -410,7 +410,7 @@ Object.defineProperties(URL.prototype, {
     set(host) {
       const ctx = this[context];
       // toUSVString is not needed.
-      host = '' + host;
+      host = `${host}`;
       if (this[cannotBeBase] ||
           (this[special] && host.length === 0)) {
         // Cannot set the host if cannot-be-base is set or
@@ -435,7 +435,7 @@ Object.defineProperties(URL.prototype, {
     set(host) {
       const ctx = this[context];
       // toUSVString is not needed.
-      host = '' + host;
+      host = `${host}`;
       if (this[cannotBeBase] ||
           (this[special] && host.length === 0)) {
         // Cannot set the host if cannot-be-base is set or
@@ -460,7 +460,7 @@ Object.defineProperties(URL.prototype, {
     },
     set(port) {
       // toUSVString is not needed.
-      port = '' + port;
+      port = `${port}`;
       const ctx = this[context];
       if (!ctx.host || this[cannotBeBase] ||
           this.protocol === 'file:')
@@ -484,7 +484,7 @@ Object.defineProperties(URL.prototype, {
     },
     set(path) {
       // toUSVString is not needed.
-      path = '' + path;
+      path = `${path}`;
       if (this[cannotBeBase])
         return;
       binding.parse(path, binding.kPathStart, null, this[context],
@@ -533,7 +533,7 @@ Object.defineProperties(URL.prototype, {
     set(hash) {
       const ctx = this[context];
       // toUSVString is not needed.
-      hash = '' + hash;
+      hash = `${hash}`;
       if (this.protocol === 'javascript:')
         return;
       if (!hash) {
@@ -1125,12 +1125,12 @@ function originFor(url, base) {
 
 function domainToASCII(domain) {
   // toUSVString is not needed.
-  return binding.domainToASCII('' + domain);
+  return binding.domainToASCII(`${domain}`);
 }
 
 function domainToUnicode(domain) {
   // toUSVString is not needed.
-  return binding.domainToUnicode('' + domain);
+  return binding.domainToUnicode(`${domain}`);
 }
 
 // Utility function that converts a URL object into an ordinary

test/parallel/test-whatwg-url-searchparams-append.js
@@ -58,7 +58,10 @@ test(function() {
     params.set('a');
   }, /^TypeError: "name" and "value" arguments must be specified$/);
 
-  const obj = { toString() { throw new Error('toString'); } };
+  const obj = {
+    toString() { throw new Error('toString'); },
+    valueOf() { throw new Error('valueOf'); }
+  };
   const sym = Symbol();
   assert.throws(() => params.set(obj, 'b'), /^Error: toString$/);
   assert.throws(() => params.set('a', obj), /^Error: toString$/);

test/parallel/test-whatwg-url-searchparams-constructor.js
@@ -209,7 +209,10 @@ test(() => {
 }
 
 {
-  const obj = { toString() { throw new Error('toString'); } };
+  const obj = {
+    toString() { throw new Error('toString'); },
+    valueOf() { throw new Error('valueOf'); }
+  };
   const sym = Symbol();
 
   assert.throws(() => new URLSearchParams({ a: obj }), /^Error: toString$/);

test/parallel/test-whatwg-url-searchparams-delete.js
@@ -52,7 +52,10 @@ test(function() {
     params.delete();
   }, /^TypeError: "name" argument must be specified$/);
 
-  const obj = { toString() { throw new Error('toString'); } };
+  const obj = {
+    toString() { throw new Error('toString'); },
+    valueOf() { throw new Error('valueOf'); }
+  };
   const sym = Symbol();
   assert.throws(() => params.delete(obj), /^Error: toString$/);
   assert.throws(() => params.delete(sym),

test/parallel/test-whatwg-url-searchparams-get.js
@@ -43,7 +43,10 @@ test(function() {
     params.get();
   }, /^TypeError: "name" argument must be specified$/);
 
-  const obj = { toString() { throw new Error('toString'); } };
+  const obj = {
+    toString() { throw new Error('toString'); },
+    valueOf() { throw new Error('valueOf'); }
+  };
   const sym = Symbol();
   assert.throws(() => params.get(obj), /^Error: toString$/);
   assert.throws(() => params.get(sym),

test/parallel/test-whatwg-url-searchparams-getall.js
@@ -47,7 +47,10 @@ test(function() {
     params.getAll();
   }, /^TypeError: "name" argument must be specified$/);
 
-  const obj = { toString() { throw new Error('toString'); } };
+  const obj = {
+    toString() { throw new Error('toString'); },
+    valueOf() { throw new Error('valueOf'); }
+  };
   const sym = Symbol();
   assert.throws(() => params.getAll(obj), /^Error: toString$/);
   assert.throws(() => params.getAll(sym),

test/parallel/test-whatwg-url-searchparams-has.js
@@ -46,7 +46,10 @@ test(function() {
     params.has();
   }, /^TypeError: "name" argument must be specified$/);
 
-  const obj = { toString() { throw new Error('toString'); } };
+  const obj = {
+    toString() { throw new Error('toString'); },
+    valueOf() { throw new Error('valueOf'); }
+  };
   const sym = Symbol();
   assert.throws(() => params.has(obj), /^Error: toString$/);
   assert.throws(() => params.has(sym),

test/parallel/test-whatwg-url-searchparams-set.js
@@ -44,7 +44,10 @@ test(function() {
     params.set('a');
   }, /^TypeError: "name" and "value" arguments must be specified$/);
 
-  const obj = { toString() { throw new Error('toString'); } };
+  const obj = {
+    toString() { throw new Error('toString'); },
+    valueOf() { throw new Error('valueOf'); }
+  };
   const sym = Symbol();
   assert.throws(() => params.append(obj, 'b'), /^Error: toString$/);
   assert.throws(() => params.append('a', obj), /^Error: toString$/);

test/parallel/test-whatwg-url-setters.js
@@ -107,7 +107,10 @@ startURLSettersTests()
 
 {
   const url = new URL('http://example.com/');
-  const obj = { toString() { throw new Error('toString'); } };
+  const obj = {
+    toString() { throw new Error('toString'); },
+    valueOf() { throw new Error('valueOf'); }
+  };
   const sym = Symbol();
   const props = Object.getOwnPropertyDescriptors(Object.getPrototypeOf(url));
   for (const [name, { set }] of Object.entries(props)) {