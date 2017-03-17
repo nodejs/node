0839ba9547044cc70895576cebfa68a0bcc23fd1
Lucas Lago <lucasbenderlago@gmail.com>
2017-03-17 17:58:53 -0300

    benchmark: remove v8ForceOptimization calls
    This removes common.v8ForceOptimization calls from url and vm benchmark
    files.
    
    PR-URL: https://github.com/nodejs/node/pull/11908
    Fixes: https://github.com/nodejs/node/issues/11895
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- benchmark/url/whatwg-url-idna.js
+++ benchmark/url/whatwg-url-idna.js
@@ -37,8 +37,6 @@ function main(conf) {
   const input = inputs[conf.input][to];
   const method = to === 'ascii' ? domainToASCII : domainToUnicode;
 
-  common.v8ForceOptimization(method, input);
-
   bench.start();
   for (var i = 0; i < n; i++) {
     method(input);
