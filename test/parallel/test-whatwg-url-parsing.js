990db85e79b11ef75fe94cd4d1fd7495da52c7ad
Joyee Cheung <joyeec9h3@gmail.com>
2017-03-20 14:18:37 +0800

    url: show input in parse error message
    PR-URL: https://github.com/nodejs/node/pull/11934
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Daijiro Wachi <daijiro.wachi@gmail.com>

--- test/parallel/test-whatwg-url-parsing.js
+++ test/parallel/test-whatwg-url-parsing.js
@@ -13,15 +13,30 @@ if (!common.hasIntl) {
 
 // Tests below are not from WPT.
 const tests = require(path.join(common.fixturesDir, 'url-tests'));
+const failureTests = tests.filter((test) => test.failure).concat([
+  { input: '' },
+  { input: 'test' },
+  { input: undefined },
+  { input: 0 },
+  { input: true },
+  { input: false },
+  { input: null },
+  { input: new Date() },
+  { input: new RegExp() },
+  { input: () => {} }
+]);
 
-for (const test of tests) {
-  if (typeof test === 'string')
-    continue;
-
-  if (test.failure) {
-    assert.throws(() => new URL(test.input, test.base),
-                  /^TypeError: Invalid URL$/);
-  }
+for (const test of failureTests) {
+  assert.throws(
+    () => new URL(test.input, test.base),
+    (error) => {
+      // The input could be processed, so we don't do strict matching here
+      const match = (error + '').match(/^TypeError: Invalid URL: (.*)$/);
+      if (!match) {
+        return false;
+      }
+      return error.input === match[1];
+    });
 }
 
 const additional_tests = require(
