59ccc634f40da978a569df446e909f61a5e23f2b
Rich Trott <rtrott@gmail.com>
2017-03-21 17:07:23 -0700

    test: add minimal test for net benchmarks
    Currently, benchmark code is not exercised at all in CI. This adds a
    minimal test for net benchmarks. If this is deemed acceptable, similar
    minimal tests for other benchmarks can be written. Additionally, as
    issues and edge cases are uncovered, checks for them can be added.
    
    PR-URL: https://github.com/nodejs/node/pull/11979
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

---
+++ test/sequential/test-benchmark-net.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('../common');
+
+// Minimal test for net benchmarks. This makes sure the benchmarks aren't
+// horribly broken but nothing more than that.
+
+// Because the net benchmarks use hardcoded ports, this should be in sequential
+// rather than parallel to make sure it does not conflict with tests that choose
+// random available ports.
+
+const assert = require('assert');
+const fork = require('child_process').fork;
+const path = require('path');
+
+const runjs = path.join(__dirname, '..', '..', 'benchmark', 'run.js');
+
+const child = fork(runjs, ['--set', 'dur=0', 'net']);
+child.on('exit', (code, signal) => {
+  assert.strictEqual(code, 0);
+  assert.strictEqual(signal, null);
+});
