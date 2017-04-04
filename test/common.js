dae94ffb7187b3c307f3bcc9b7c4410181492d13
Timothy Gu <timothygu99@gmail.com>
2017-04-03 18:29:41 -0700

    test: add common.getArrayBufferViews(buf)

--- test/common.js
+++ test/common.js
@@ -654,3 +654,29 @@ exports.skipIfInspectorDisabled = function skipIfInspectorDisabled() {
     process.exit(0);
   }
 };
+
+const arrayBufferViews = [
+  Int8Array,
+  Uint8Array,
+  Uint8ClampedArray,
+  Int16Array,
+  Uint16Array,
+  Int32Array,
+  Uint32Array,
+  Float32Array,
+  Float64Array,
+  DataView
+];
+
+exports.getArrayBufferViews = function getArrayBufferViews(buf) {
+  const { buffer, byteOffset, byteLength } = buf;
+
+  const out = [];
+  for (const type of arrayBufferViews) {
+    const { BYTES_PER_ELEMENT = 1 } = type;
+    if (Number.isInteger(byteLength % BYTES_PER_ELEMENT)) {
+      out.push(new type(buffer, byteOffset, byteLength / BYTES_PER_ELEMENT));
+    }
+  }
+  return out;
+};
