a9d72e1a5d4f390abaf5d78384eb9508dec010c6
Evan Torrie <evantorrie@users.noreply.github.com>
2017-04-04 20:03:10 -0700

    test: Skip intl-no-icu-data test when not (Intl and smallICU)
    The --icu-data-dir flag applies only when the code was originally
    compiled with --with-intl=small-icu.

--- test/parallel/test-intl-no-icu-data.js
+++ test/parallel/test-intl-no-icu-data.js
@@ -1,8 +1,12 @@
 // Flags: --icu-data-dir=test/fixtures/empty/
 'use strict';
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const config = process.binding('config');
 
+if (!(common.hasIntl && common.hasSmallICU)) {
+  common.skip('not (Intl AND SmallICU)');
+  return;
+}
 assert.deepStrictEqual(Intl.NumberFormat.supportedLocalesOf('en'), []);
 assert.strictEqual(config.icuDataDir, 'test/fixtures/empty/');
