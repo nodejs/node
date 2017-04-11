d55392d5fea965150426d93f0a708f8d2fc798d2
cjihrig <cjihrig@gmail.com>
2017-04-11 16:37:07 -0400

    child_process: improve ChildProcess validation
    This commit improves input validation for the ChildProcess
    internals. It became officially supported API a while back, but
    never had any validation.
    
    Refs: https://github.com/nodejs/node/issues/12177
    PR-URL: https://github.com/nodejs/node/pull/12348
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

--- test/parallel/test-child-process-constructor.js
+++ test/parallel/test-child-process-constructor.js
@@ -5,6 +5,50 @@ const assert = require('assert');
 const { ChildProcess } = require('child_process');
 assert.strictEqual(typeof ChildProcess, 'function');
 
+{
+  // Verify that invalid options to spawn() throw.
+  const child = new ChildProcess();
+
+  [undefined, null, 'foo', 0, 1, NaN, true, false].forEach((options) => {
+    assert.throws(() => {
+      child.spawn(options);
+    }, /^TypeError: "options" must be an object$/);
+  });
+}
+
+{
+  // Verify that spawn throws if file is not a string.
+  const child = new ChildProcess();
+
+  [undefined, null, 0, 1, NaN, true, false, {}].forEach((file) => {
+    assert.throws(() => {
+      child.spawn({ file });
+    }, /^TypeError: "file" must be a string$/);
+  });
+}
+
+{
+  // Verify that spawn throws if envPairs is not an array or undefined.
+  const child = new ChildProcess();
+
+  [null, 0, 1, NaN, true, false, {}, 'foo'].forEach((envPairs) => {
+    assert.throws(() => {
+      child.spawn({ envPairs, stdio: ['ignore', 'ignore', 'ignore', 'ipc'] });
+    }, /^TypeError: "envPairs" must be an array$/);
+  });
+}
+
+{
+  // Verify that spawn throws if args is not an array or undefined.
+  const child = new ChildProcess();
+
+  [null, 0, 1, NaN, true, false, {}, 'foo'].forEach((args) => {
+    assert.throws(() => {
+      child.spawn({ file: 'foo', args });
+    }, /^TypeError: "args" must be an array$/);
+  });
+}
+
 // test that we can call spawn
 const child = new ChildProcess();
 child.spawn({
