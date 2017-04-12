2bf9bd940c6ac82a400d2084aae21248ad087192
cjihrig <cjihrig@gmail.com>
2017-04-12 13:52:36 -0400

    test: complete coverage of lib/child_process.js
    This commit adds a test which brings coverage of
    lib/child_process.js to 100%. It adds coverage for the call to
    uv.errname() in execFile()'s exithandler() function.
    
    PR-URL: https://github.com/nodejs/node/pull/12367
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Yuta Hiroto <hello@about-hiroppy.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

--- test/parallel/test-child-process-execfile.js
+++ test/parallel/test-child-process-execfile.js
@@ -1,9 +1,9 @@
 'use strict';
-
 const common = require('../common');
 const assert = require('assert');
 const execFile = require('child_process').execFile;
 const path = require('path');
+const uv = process.binding('uv');
 
 const fixture = path.join(common.fixturesDir, 'exit.js');
 
@@ -18,4 +18,23 @@ const fixture = path.join(common.fixturesDir, 'exit.js');
       assert.strictEqual(e.code, 42);
     })
   );
+}
+
+{
+  // Verify that negative exit codes can be translated to UV error names.
+  const errorString = `Error: Command failed: ${process.execPath}`;
+  const code = -1;
+  const callback = common.mustCall((err, stdout, stderr) => {
+    assert.strictEqual(err.toString().trim(), errorString);
+    assert.strictEqual(err.code, uv.errname(code));
+    assert.strictEqual(err.killed, true);
+    assert.strictEqual(err.signal, null);
+    assert.strictEqual(err.cmd, process.execPath);
+    assert.strictEqual(stdout.trim(), '');
+    assert.strictEqual(stderr.trim(), '');
+  });
+  const child = execFile(process.execPath, callback);
+
+  child.kill();
+  child.emit('close', code, null);
 }
