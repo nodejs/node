0f290f24141d9dbcb522ad20723abf0da0caac31
Rich Trott <rtrott@gmail.com>
2017-04-13 12:47:55 -0700

    test: enable setuid/setgid test
    Refactor test for situations where it was expected to fail.
    Move from disabled directory to parallel.
    
    PR-URL: https://github.com/nodejs/node/pull/12403
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Yuta Hiroto <hello@about-hiroppy.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

---
+++ test/parallel/test-process-setuid-setgid.js
@@ -0,0 +1,47 @@
+'use strict';
+const common = require('../common');
+
+const assert = require('assert');
+
+if (common.isWindows) {
+  // uid/gid functions are POSIX only
+  assert.strictEqual(process.getuid, undefined);
+  assert.strictEqual(process.setuid, undefined);
+  assert.strictEqual(process.getgid, undefined);
+  assert.strictEqual(process.setgid, undefined);
+  return;
+}
+
+assert.throws(() => {
+  process.setuid('fhqwhgadshgnsdhjsdbkhsdabkfabkveybvf');
+}, /^Error: setuid user id does not exist$/);
+
+// If we're not running as super user...
+if (process.getuid() !== 0) {
+  assert.doesNotThrow(() => {
+    process.getgid();
+    process.getuid();
+  });
+
+  assert.throws(
+    () => { process.setgid('nobody'); },
+    /^Error: (EPERM, .+|setgid group id does not exist)$/
+  );
+
+  assert.throws(
+    () => { process.setuid('nobody'); },
+    /^Error: EPERM, /
+  );
+  return;
+}
+
+// If we are running as super user...
+const oldgid = process.getgid();
+process.setgid('nobody');
+const newgid = process.getgid();
+assert.notStrictEqual(newgid, oldgid);
+
+const olduid = process.getuid();
+process.setuid('nobody');
+const newuid = process.getuid();
+assert.notStrictEqual(newuid, olduid);
