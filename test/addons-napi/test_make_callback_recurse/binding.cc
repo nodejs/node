046f8a637d2f821c3fff5d28f96f6f94ae91d592
Hitesh Kanwathirtha <hiteshk@microsoft.com>
2017-04-13 20:21:52 -0700

    test: port test for make_callback to n-api
    Improved test coverage for napi_make_callback by porting the
    existing addons/make_callback test to n-api
    
    PR-URL: https://github.com/nodejs/node/pull/12409
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/addons-napi/test_make_callback_recurse/binding.cc
@@ -0,0 +1,32 @@
+#include <node_api.h>
+#include "../common.h"
+#include <vector>
+
+namespace {
+
+napi_value MakeCallback(napi_env env, napi_callback_info info) {
+  size_t argc = 2;
+  napi_value args[2];
+  NAPI_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
+
+  napi_value recv = args[0];
+  napi_value func = args[1];
+
+  napi_make_callback(env,
+    recv, func, 0 /* argc */, nullptr /* argv */, nullptr /* result */);
+
+  return recv;
+}
+
+void Init(napi_env env, napi_value exports, napi_value module, void* priv) {
+  napi_value fn;
+  NAPI_CALL_RETURN_VOID(env,
+    napi_create_function(env, NULL, MakeCallback, NULL, &fn));
+  NAPI_CALL_RETURN_VOID(env,
+    napi_set_named_property(env, exports, "makeCallback", fn));
+}
+
+
+}  // namespace
+
+NAPI_MODULE(binding, Init)
