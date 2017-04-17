f5aead066ec0ab7dfa4a9c45a11d4e10c9e2e1ef
Aditya Anand <aditya.anandmc@gmail.com>
2017-04-17 17:20:17 +0530

    test: use dynamic port instead of common.PORT
    Remove common.PORT from, test-net-connect-immediate-destroy,
    test-net-options-lookup, test-net-connect-local-error,
    test-net-connect-handle-econnrefused, test-net-socket-destroy-twice,
    test-net-better-error-messages-port-hostname, test-net-localerror,
    to reduce possibility that a dynamic port used in another test will
    collide with common.PORT.
    
    Moved test-net-listen-shared-ports, test-net-better-error-messages-port
    from tests/parallel to test/sequential
    
    Refs: #12376

--- test/parallel/test-net-connect-immediate-finish.js
+++ test/parallel/test-net-connect-immediate-finish.js
@@ -26,7 +26,7 @@ const net = require('net');
 
 const client = net.connect({host: '***', port: common.PORT});
 
-client.once('error', common.mustCall(function(err) {
+client.once('error', common.mustCall((err) => {
   assert(err);
   assert.strictEqual(err.code, err.errno);
   assert.strictEqual(err.code, 'ENOTFOUND');
