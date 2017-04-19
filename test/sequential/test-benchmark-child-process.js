32f77ff8ce3b67ba14943738b28e6912b44a1454
Rich Trott <rtrott@gmail.com>
2017-04-19 10:45:21 -0700

    test: minimize time for child_process benchmark
    test-benchmark-child-process sometimes times out on Windows in CI.
    Minimize the number of configurations that run so it will (hopefully)
    not time out anymore. Set dur=0.
    
    PR-URL: https://github.com/nodejs/node/pull/12518
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

--- test/sequential/test-benchmark-child-process.js
+++ test/sequential/test-benchmark-child-process.js
@@ -8,9 +8,11 @@ const path = require('path');
 
 const runjs = path.join(__dirname, '..', '..', 'benchmark', 'run.js');
 
-const child = fork(runjs, ['--set', 'dur=0.1',
+const child = fork(runjs, ['--set', 'dur=0',
                            '--set', 'n=1',
                            '--set', 'len=1',
+                           '--set', 'params=1',
+                           '--set', 'methodName=execSync',
                            'child_process'],
                    {env: {NODEJS_BENCHMARK_ZERO_ALLOWED: 1}});
 child.on('exit', (code, signal) => {
