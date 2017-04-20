dc49d916c0ceb869f8e747ba12f0a63e451b6725
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2017-04-20 11:43:51 -0400

    [Squash] remove comment

--- test/parallel/test-timers-unref-reuse-no-exposed-list.js
+++ test/parallel/test-timers-unref-reuse-no-exposed-list.js
@@ -10,7 +10,5 @@ assert.strictEqual(timer1._handle._list, undefined,
 // Check that everything works even if the handle was not re-used.
 setTimeout(() => {}, 1);
 const timer2 = setTimeout(() => {}, 1).unref();
-// Note: It is unlikely that this assertion will be hit in a failure.
-// Instead, timers.js will likely throw a TypeError internally.
 assert.strictEqual(timer2._handle._list, undefined,
                    'timer2._handle._list should be undefined');
