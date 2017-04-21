a73a99192c007414c0a625dbd4d20adcaa5aa0c8
Michael Dawson <michael_dawson@ca.ibm.com>
2017-04-21 18:10:39 -0400

    test: add coverage for napi_cancel_async_work
    adding test coverage for napi_cancel_async_work based
    on coverage report

--- test/addons-napi/test_async/test.js
+++ test/addons-napi/test_async/test.js
@@ -3,8 +3,11 @@ const common = require('../../common');
 const assert = require('assert');
 const test_async = require(`./build/${common.buildType}/test_async`);
 
-test_async(5, common.mustCall(function(err, val) {
+test_async.Test(5, common.mustCall(function(err, val) {
   assert.strictEqual(err, null);
   assert.strictEqual(val, 10);
   process.nextTick(common.mustCall(function() {}));
 }));
+
+const cancelSuceeded = function() {};
+test_async.TestCancel(common.mustCall(cancelSuceeded));
