6eb7161016bb352ae322f1b618dd39cc56fb8d30
Zahidul Islam <zahidsharp@outlook.com>
2017-04-22 13:36:18 -0700

    test: fixing lint error in test-times-clearImmediate.js

--- test/parallel/test-timers-clearImmediate.js
+++ test/parallel/test-timers-clearImmediate.js
@@ -1,13 +1,14 @@
 'use strict';
 const common = require('../common');
-const assert = require('assert');
 
 const N = 3;
 
 function next() {
-  const immediate = setImmediate(common.mustCall(() => clearImmediate(immediate)));
+  const immediate = setImmediate(
+                        common.mustCall(() => clearImmediate(immediate))
+                    );
 }
 
-for(let i = 0; i < N; i++) {
+for (let i = 0; i < N; i++) {
   next();
 }
