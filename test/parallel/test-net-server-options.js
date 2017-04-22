e8004dbdd5dc12389a67479f195d240871f37ba4
coreybeaumont <coreybeaumont@gmail.com>
2017-04-22 11:07:32 -0700

    test: improved type checking with regex
    Replaced TypeError with a regular expression of the actual error.
    
    PR-URL: https://github.com/nodejs/node/pull/12591
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-net-server-options.js
+++ test/parallel/test-net-server-options.js
@@ -3,5 +3,7 @@ require('../common');
 const assert = require('assert');
 const net = require('net');
 
-assert.throws(function() { net.createServer('path'); }, TypeError);
-assert.throws(function() { net.createServer(0); }, TypeError);
+assert.throws(function() { net.createServer('path'); },
+              /^TypeError: options must be an object$/);
+assert.throws(function() { net.createServer(0); },
+              /^TypeError: options must be an object$/);
