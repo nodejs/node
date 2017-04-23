9d813f7e1432c7cde805caebc6c64f375486a253
Daniel Pihlstrom <sciolist.se@gmail.com>
2017-04-23 01:55:35 -0400

    src,fs: calculate fs times without truncation
    also added some missing bits that didn't make it into #12818
    
    PR-URL: https://github.com/nodejs/node/pull/12607
    Refs: https://github.com/nodejs/node/pull/12818
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_file.cc
+++ src/node_file.cc
@@ -479,9 +479,9 @@ void FillStatsArray(double* fields, const uv_stat_t* s) {
   fields[9] = -1;
 #endif
   // Dates.
-#define X(idx, name)                                                          \
-  fields[idx] = (static_cast<double>(s->st_##name.tv_sec) * 1000) +           \
-                (static_cast<double>(s->st_##name.tv_nsec / 1000000));        \
+#define X(idx, name)                          \
+  fields[idx] = (s->st_##name.tv_sec * 1e3) + \
+                (s->st_##name.tv_nsec / 1e6); \
 
   X(10, atim)
   X(11, mtim)
