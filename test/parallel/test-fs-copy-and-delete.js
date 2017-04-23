bad81e79fd77a2298814e6c2a5f168c92859f499
Jacob O'Toole <jacob.otoole1@gmail.com>
2017-04-23 18:16:58 +0100

    Made sure that the test cannot fluke with timings.

--- test/parallel/test-fs-copy-and-delete.js
+++ test/parallel/test-fs-copy-and-delete.js
@@ -27,138 +27,142 @@ function setUpDir(path) {
 }
 
 setUpDir(testDir + '/test1');
-fs.copySync(testDir + '/test1', testDir + '/test1-copied');
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1`), true);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test2`), true);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3`), true);
-assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/file1`).toString(),
-                   'Contents of file\r\n1');
-assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/file2`).toString(),
-                   'Contents of file 2');
-assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test1/file3`)
-  .toString(),
-                   'Contents of file\r\n3');
-assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test3/file4`)
-  .toString(),
-                   'Contents of file 4');
-assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test3/file5`)
-  .toString(),
-                   'Contents of file 5');
 
-function checkProps(p1, p2) {
+setTimeout(() => {
 
-  assert.strictEqual(fs.statSync(p1).mode, fs.statSync(p2).mode);
-  assert.strictEqual(fs.statSync(p1).uid, fs.statSync(p2).uid);
-  assert.strictEqual(fs.statSync(p1).gid, fs.statSync(p2).gid);
-  assert.strictEqual(fs.statSync(p1).size, fs.statSync(p2).size);
-  if (Math.abs(fs.statSync(p1).mtime.getTime() -
-               fs.statSync(p2).mtime.getTime()) > 1000) {
+  fs.copySync(testDir + '/test1', testDir + '/test1-copied');
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1`), true);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test2`), true);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3`), true);
+  assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/file1`)
+    .toString(), 'Contents of file\r\n1');
+  assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/file2`)
+    .toString(), 'Contents of file 2');
+  assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test1/file3`)
+    .toString(), 'Contents of file\r\n3');
+  assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test3/file4`)
+    .toString(), 'Contents of file 4');
+  assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test3/file5`)
+    .toString(), 'Contents of file 5');
 
-    assert.fail(Math.abs(fs.statSync(p1).mtime.getTime() -
-                         fs.statSync(p2).mtime.getTime()), 1000,
-                'mtimes are not correct.',
-                '<');
+  function checkProps(p1, p2) {
 
-  }
+    assert.strictEqual(fs.statSync(p1).mode, fs.statSync(p2).mode);
+    assert.strictEqual(fs.statSync(p1).uid, fs.statSync(p2).uid);
+    assert.strictEqual(fs.statSync(p1).gid, fs.statSync(p2).gid);
+    assert.strictEqual(fs.statSync(p1).size, fs.statSync(p2).size);
+    if (process.platform !== 'aix' &&
+    Math.abs(fs.statSync(p1).mtime.getTime() -
+    fs.statSync(p2).mtime.getTime()) > 1000) {
 
-}
+      assert.fail(Math.abs(fs.statSync(p1).mtime.getTime() -
+                          fs.statSync(p2).mtime.getTime()), 1000,
+                  'mtimes are not correct.',
+                  '<');
 
-checkProps(`${testDir}/test1/test1`, `${testDir}/test1-copied/test1`);
-checkProps(`${testDir}/test1/test2`, `${testDir}/test1-copied/test2`);
-checkProps(`${testDir}/test1/test3`, `${testDir}/test1-copied/test3`);
-checkProps(`${testDir}/test1/file1`, `${testDir}/test1-copied/file1`);
-checkProps(`${testDir}/test1/file2`, `${testDir}/test1-copied/file2`);
-checkProps(`${testDir}/test1/test1/file3`,
-           `${testDir}/test1-copied/test1/file3`);
-checkProps(`${testDir}/test1/test3/file4`,
-           `${testDir}/test1-copied/test3/file4`);
-checkProps(`${testDir}/test1/test3/file5`,
-           `${testDir}/test1-copied/test3/file5`);
-
-fs.deleteSync(testDir + '/test1-copied');
-
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied`), false);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1`), false);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test2`), false);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3`), false);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/file1`), false);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/file2`), false);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1/file3`),
-                   false);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3/file4`),
-                   false);
-assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3/file5`),
-                   false);
-
-fs.copy(testDir + '/test1', testDir + '/test1-copied', (err) => {
-
-  if (err) {
-
-    assert.ifError(err);
-
-  } else {
-
-    assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1`),
-                       true);
-    assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test2`),
-                       true);
-    assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3`),
-                       true);
-    assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/file1`)
-      .toString(), 'Contents of file\r\n1');
-    assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/file2`)
-      .toString(), 'Contents of file 2');
-    assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test1/file3`)
-      .toString(), 'Contents of file\r\n3');
-    assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test3/file4`)
-      .toString(), 'Contents of file 4');
-    assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test3/file5`)
-      .toString(), 'Contents of file 5');
-
-    checkProps(`${testDir}/test1/test1`, `${testDir}/test1-copied/test1`);
-    checkProps(`${testDir}/test1/test2`, `${testDir}/test1-copied/test2`);
-    checkProps(`${testDir}/test1/test3`, `${testDir}/test1-copied/test3`);
-    checkProps(`${testDir}/test1/file1`, `${testDir}/test1-copied/file1`);
-    checkProps(`${testDir}/test1/file2`, `${testDir}/test1-copied/file2`);
-    checkProps(`${testDir}/test1/test1/file3`,
-               `${testDir}/test1-copied/test1/file3`);
-    checkProps(`${testDir}/test1/test3/file4`,
-               `${testDir}/test1-copied/test3/file4`);
-    checkProps(`${testDir}/test1/test3/file5`,
-               `${testDir}/test1-copied/test3/file5`);
-
-    fs.delete(testDir + '/test1-copied', (err) => {
-
-      if (err) {
-
-        assert.ifError(err);
-
-      } else {
-
-        assert.strictEqual(common.fileExists(`${testDir}/test1-copied`), false);
-        assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1`),
-                           false);
-        assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test2`),
-                           false);
-        assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3`),
-                           false);
-        assert.strictEqual(common.fileExists(`${testDir}/test1-copied/file1`),
-                           false);
-        assert.strictEqual(common.fileExists(`${testDir}/test1-copied/file2`),
-                           false);
-        assert.strictEqual(common
-          .fileExists(`${testDir}/test1-copied/test1/file3`), false);
-        assert.strictEqual(common
-          .fileExists(`${testDir}/test1-copied/test3/file4`), false);
-        assert.strictEqual(common
-          .fileExists(`${testDir}/test1-copied/test3/file5`), false);
-
-        fs.deleteSync(`${testDir}/test1`);
-
-      }
-
-    });
+    }
 
   }
 
-});
+  checkProps(`${testDir}/test1/test1`, `${testDir}/test1-copied/test1`);
+  checkProps(`${testDir}/test1/test2`, `${testDir}/test1-copied/test2`);
+  checkProps(`${testDir}/test1/test3`, `${testDir}/test1-copied/test3`);
+  checkProps(`${testDir}/test1/file1`, `${testDir}/test1-copied/file1`);
+  checkProps(`${testDir}/test1/file2`, `${testDir}/test1-copied/file2`);
+  checkProps(`${testDir}/test1/test1/file3`,
+             `${testDir}/test1-copied/test1/file3`);
+  checkProps(`${testDir}/test1/test3/file4`,
+             `${testDir}/test1-copied/test3/file4`);
+  checkProps(`${testDir}/test1/test3/file5`,
+             `${testDir}/test1-copied/test3/file5`);
+
+  fs.deleteSync(testDir + '/test1-copied');
+
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied`), false);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1`), false);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test2`), false);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3`), false);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/file1`), false);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/file2`), false);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1/file3`),
+                     false);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3/file4`),
+                     false);
+  assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3/file5`),
+                     false);
+
+  fs.copy(testDir + '/test1', testDir + '/test1-copied', (err) => {
+
+    if (err) {
+
+      assert.ifError(err);
+
+    } else {
+
+      assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1`),
+                         true);
+      assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test2`),
+                         true);
+      assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3`),
+                         true);
+      assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/file1`)
+        .toString(), 'Contents of file\r\n1');
+      assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/file2`)
+        .toString(), 'Contents of file 2');
+      assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test1/file3`)
+        .toString(), 'Contents of file\r\n3');
+      assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test3/file4`)
+        .toString(), 'Contents of file 4');
+      assert.strictEqual(fs.readFileSync(`${testDir}/test1-copied/test3/file5`)
+        .toString(), 'Contents of file 5');
+
+      checkProps(`${testDir}/test1/test1`, `${testDir}/test1-copied/test1`);
+      checkProps(`${testDir}/test1/test2`, `${testDir}/test1-copied/test2`);
+      checkProps(`${testDir}/test1/test3`, `${testDir}/test1-copied/test3`);
+      checkProps(`${testDir}/test1/file1`, `${testDir}/test1-copied/file1`);
+      checkProps(`${testDir}/test1/file2`, `${testDir}/test1-copied/file2`);
+      checkProps(`${testDir}/test1/test1/file3`,
+                 `${testDir}/test1-copied/test1/file3`);
+      checkProps(`${testDir}/test1/test3/file4`,
+                 `${testDir}/test1-copied/test3/file4`);
+      checkProps(`${testDir}/test1/test3/file5`,
+                 `${testDir}/test1-copied/test3/file5`);
+
+      fs.delete(testDir + '/test1-copied', (err) => {
+
+        if (err) {
+
+          assert.ifError(err);
+
+        } else {
+
+          assert.strictEqual(common.fileExists(`${testDir}/test1-copied`),
+                             false);
+          assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test1`)
+                             , false);
+          assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test2`)
+                             , false);
+          assert.strictEqual(common.fileExists(`${testDir}/test1-copied/test3`)
+                             , false);
+          assert.strictEqual(common.fileExists(`${testDir}/test1-copied/file1`)
+                             , false);
+          assert.strictEqual(common.fileExists(`${testDir}/test1-copied/file2`)
+                             , false);
+          assert.strictEqual(common
+            .fileExists(`${testDir}/test1-copied/test1/file3`), false);
+          assert.strictEqual(common
+            .fileExists(`${testDir}/test1-copied/test3/file4`), false);
+          assert.strictEqual(common
+            .fileExists(`${testDir}/test1-copied/test3/file5`), false);
+
+          fs.deleteSync(`${testDir}/test1`);
+
+        }
+
+      });
+
+    }
+
+  });
+
+}, 1500);
