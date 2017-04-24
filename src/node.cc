nodejs/node

commit 43c1b625a693db933ea7d132718e2f77b9cdb180

src: add fcntl.h include to node.cc
https://github.com/nodejs/node/pull/11863 adds _O_RDWR to node.cc
which is defined in fcntl.h. This adds this include directly to
node.cc.

PR-URL: https://github.com/nodejs/node/pull/12540
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
Reviewed-By: James M Snell <jasnell@gmail.com>
Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
Reviewed-By: Anna Henningsen <anna@addaleax.net>
Reviewed-By: Myles Borins <myles.borins@gmail.com>

src/node.cc
@@ -73,6 +73,7 @@
 #endif
 
 #include <errno.h>
+#include <fcntl.h>. // _O_RDWR
 #include <limits.h>  // PATH_MAX
 #include <locale.h>
 #include <signal.h>

commit 04579b7568f91d2bf83a776c316d56060fdb2464

src: fix linting error
43c1b625a6 introduced a minor c linting issue

src/node.cc
@@ -73,7 +73,7 @@
 #endif
 
 #include <errno.h>
-#include <fcntl.h>. // _O_RDWR
+#include <fcntl.h>.  // _O_RDWR
 #include <limits.h>  // PATH_MAX
 #include <locale.h>
 #include <signal.h>