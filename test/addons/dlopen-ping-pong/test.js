f69e7291f668100fe92722f57573d1ecf373728d
Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
2017-04-29 16:06:22 -0400

    src: add support to pass flags to dlopen
    * add constants for dlopen flags, which are needed
      for dlopen's flag passing.
    
    * introduce an optional parameter for process.dlopen(),
      allowing to pass dlopen flags (using values from os.constants.dlopen).
    
    If no flags are passed, the default behavior is to load the library
    with RTLD_LAZY (perform lazy binding) and RTLD_LOCAL (symbols are
    available only locally).
    
    PR-URL: https://github.com/nodejs/node/pull/12794
    Refs: https://github.com/nodejs/node/pull/4105
    Refs: https://github.com/libuv/libuv/pull/1331
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

---
+++ test/addons/dlopen-ping-pong/test.js
@@ -0,0 +1,23 @@
+'use strict';
+const common = require('../../common');
+
+if (common.isWindows)
+  common.skip('dlopen global symbol loading is not supported on this os.');
+
+if (common.isAIX)
+  common.skip('this test does not pass on AIX.');
+
+const assert = require('assert');
+const path = require('path');
+const os = require('os');
+
+const bindingPath = require.resolve(`./build/${common.buildType}/binding`);
+process.dlopen(module, bindingPath,
+               os.constants.dlopen.RTLD_NOW | os.constants.dlopen.RTLD_GLOBAL);
+module.exports.load(path.dirname(bindingPath) + '/ping.so');
+assert.strictEqual(module.exports.ping(), 'pong');
+
+// Check that after the addon is loaded with
+// process.dlopen() a require() call fails.
+const re = /^Error: Module did not self-register\.$/;
+assert.throws(() => require(`./build/${common.buildType}/binding`), re);
