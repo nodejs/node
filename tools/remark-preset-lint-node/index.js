31153fd15f5d121a1ba0b17397ee687c41bbd0e3
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-04-30 18:08:50 +0200

    tools: introduce remark-preset-lint-node
    PR-URL: https://github.com/nodejs/node/pull/12756
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Vse Mozhet Byt <vsemozhetbyt@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

---
+++ tools/remark-preset-lint-node/index.js
@@ -0,0 +1,44 @@
+// @see https://github.com/nodejs/node/blob/master/doc/STYLE_GUIDE.md
+
+'use strict';
+
+module.exports.plugins = [
+  require('remark-lint'),
+  require('remark-lint-checkbox-content-indent'),
+  require('remark-lint-definition-spacing'),
+  require('remark-lint-fenced-code-flag'),
+  require('remark-lint-final-definition'),
+  require('remark-lint-final-newline'),
+  require('remark-lint-hard-break-spaces'),
+  require('remark-lint-no-auto-link-without-protocol'),
+  require('remark-lint-no-blockquote-without-caret'),
+  require('remark-lint-no-duplicate-definitions'),
+  require('remark-lint-no-file-name-articles'),
+  require('remark-lint-no-file-name-consecutive-dashes'),
+  require('remark-lint-no-file-name-outer-dashes'),
+  require('remark-lint-no-heading-content-indent'),
+  require('remark-lint-no-heading-indent'),
+  require('remark-lint-no-inline-padding'),
+  require('remark-lint-no-multiple-toplevel-headings'),
+  require('remark-lint-no-shell-dollars'),
+  require('remark-lint-no-shortcut-reference-image'),
+  require('remark-lint-no-table-indentation'),
+  require('remark-lint-no-tabs'),
+  require('remark-lint-no-unused-definitions'),
+  require('remark-lint-rule-style'),
+  require('remark-lint-table-pipes'),
+  [require('remark-lint-blockquote-indentation'), 2],
+  [
+    require('remark-lint-checkbox-character-style'),
+    {
+      'checked': 'x', 'unchecked': ' '
+    }
+  ],
+  [require('remark-lint-code-block-style'), 'fenced'],
+  [require('remark-lint-fenced-code-marker'), '`'],
+  [require('remark-lint-file-extension'), 'md'],
+  [require('remark-lint-first-heading-level'), 1],
+  [require('remark-lint-heading-style'), 'atx'],
+  [require('remark-lint-strong-marker'), '*'],
+  [require('remark-lint-table-cell-padding'), 'padded']
+];
