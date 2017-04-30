e488857402d6fc243baaddf8ebceb0a355583b68
Tony Rice <tony@bestellensoftware.com>
2017-04-30 12:07:37 -0500

    build: add static option to vcbuild.bat
    Passes --enable-static to ./configure.
    
    PR-URL: https://github.com/nodejs/node/pull/12764
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>

--- vcbuild.bat
+++ vcbuild.bat
@@ -40,6 +40,7 @@ set enable_vtune_arg=
 set configure_flags=
 set build_addons=
 set dll=
+set enable_static=
 set build_addons_napi=
 set test_node_inspect=
 set test_check_deopts=
@@ -92,6 +93,7 @@ if /i "%1"=="download-all"  set download_arg="--download=all"&goto arg-ok
 if /i "%1"=="ignore-flaky"  set test_args=%test_args% --flaky-tests=dontcare&goto arg-ok
 if /i "%1"=="enable-vtune"  set enable_vtune_arg=1&goto arg-ok
 if /i "%1"=="dll"           set dll=1&goto arg-ok
+if /i "%1"=="static"           set enable_static=1&goto arg-ok
 if /i "%1"=="no-NODE-OPTIONS"	set no_NODE_OPTIONS=1&goto arg-ok
 
 echo Error: invalid command line option `%1`.
@@ -124,6 +126,7 @@ if defined release_urlbase set configure_flags=%configure_flags% --release-urlba
 if defined download_arg set configure_flags=%configure_flags% %download_arg%
 if defined enable_vtune_arg set configure_flags=%configure_flags% --enable-vtune-profiling
 if defined dll set configure_flags=%configure_flags% --shared
+if defined enable_static set configure_flags=%configure_flags% --enable-static
 if defined no_NODE_OPTIONS set configure_flags=%configure_flags% --without-node-options
 
 if "%i18n_arg%"=="full-icu" set configure_flags=%configure_flags% --with-intl=full-icu
