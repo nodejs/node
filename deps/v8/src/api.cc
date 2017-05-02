9936eb916844cf2094fbc2de3096409065eb2cb7
daniel.bevenius <daniel.bevenius@gmail.com>
2017-05-02 03:28:17 -0700

    deps: cherry-pick f5fad6d from upstream v8
    Original commit message:
    
        This commit adds a getter for the private is_verbose_ member.
        The use case for this comes from Node.js where the ability to avoid
        calling FatalException if the TryCatch is verbose would be nice
        to have.
    
        BUG=
    
        Review-Url: https://codereview.chromium.org/2840803002
        Cr-Commit-Position: refs/heads/master@{#45018}
    
    PR-URL: https://github.com/nodejs/node/pull/12826
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- deps/v8/src/api.cc
+++ deps/v8/src/api.cc
@@ -2709,6 +2709,10 @@ void v8::TryCatch::SetVerbose(bool value) {
   is_verbose_ = value;
 }
 
+bool v8::TryCatch::IsVerbose() const {
+  return is_verbose_;
+}
+
 
 void v8::TryCatch::SetCaptureMessage(bool value) {
   capture_message_ = value;
