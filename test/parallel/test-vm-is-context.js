6c199caf896e865a1776dc50704d129645e50c6a
jeyanthinath <jeyanthinath10@gmail.com>
2017-05-02 16:16:53 +0530

    vm: changed the error from constructor to regular expression
    assert.throws initally throws the TypeError constructor and now replacing that with regular expression

--- test/parallel/test-vm-is-context.js
+++ test/parallel/test-vm-is-context.js
@@ -26,7 +26,7 @@ const vm = require('vm');
 
 assert.throws(function() {
   vm.isContext('string is not supported');
-}, /TypeError/);
+}, /^TypeError: sandbox must be an object$/);
 
 assert.strictEqual(vm.isContext({}), false);
 assert.strictEqual(vm.isContext([]), false);
@@ -34,8 +34,6 @@ assert.strictEqual(vm.isContext([]), false);
 assert.strictEqual(vm.isContext(vm.createContext()), true);
 assert.strictEqual(vm.isContext(vm.createContext([])), true);
 
-{
   const sandbox = { foo: 'bar' };
   vm.createContext(sandbox);
   assert.strictEqual(vm.isContext(sandbox), true);
-}
