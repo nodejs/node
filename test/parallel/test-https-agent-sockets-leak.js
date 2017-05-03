fb39a61c15b3e8469e0243d4a7b6f1b830247405
Vse Mozhet Byt <vsemozhetbyt@gmail.com>
2017-05-03 06:21:47 +0300

    fixup: parallel/https* – https-drain

--- test/parallel/test-https-agent-sockets-leak.js
+++ test/parallel/test-https-agent-sockets-leak.js
@@ -11,9 +11,9 @@ const https = require('https');
 const assert = require('assert');
 
 const options = {
-  key: fs.readFileSync(common.fixturesDir + '/keys/agent1-key.pem'),
-  cert: fs.readFileSync(common.fixturesDir + '/keys/agent1-cert.pem'),
-  ca: fs.readFileSync(common.fixturesDir + '/keys/ca1-cert.pem')
+  key: fs.readFileSync(`${common.fixturesDir}/keys/agent1-key.pem`),
+  cert: fs.readFileSync(`${common.fixturesDir}/keys/agent1-cert.pem`),
+  ca: fs.readFileSync(`${common.fixturesDir}/keys/ca1-cert.pem`)
 };
 
 const server = https.Server(options, common.mustCall((req, res) => {
