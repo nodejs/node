edb98468846e1d0fcbca84d8002669a800b9d4dd
Calvin Metcalf <cmetcalf@appgeo.com>
2017-05-05 15:57:57 +0200

    stream: remove usage of *State.highWaterMark
    Replaced _readableState.highWaterMark with a .readableHighWaterMark
    getter and _writableState.highWaterMark with a .writableHighWaterMark
    getter.
    The getters are non-enumerable because they break some prototype
    manipulation that happen in the ecosystem.
    
    Ref: https://github.com/nodejs/node/issues/445.
    
    PR-URL: https://github.com/nodejs/node/pull/12860
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-stream-transform-split-objectmode.js
+++ test/parallel/test-stream-transform-split-objectmode.js
@@ -29,8 +29,12 @@ const parser = new Transform({ readableObjectMode: true });
 
 assert(parser._readableState.objectMode);
 assert(!parser._writableState.objectMode);
-assert.strictEqual(parser._readableState.highWaterMark, 16);
-assert.strictEqual(parser._writableState.highWaterMark, 16 * 1024);
+assert.strictEqual(parser.readableHighWaterMark, 16);
+assert.strictEqual(parser.writableHighWaterMark, 16 * 1024);
+assert.strictEqual(parser.readableHighWaterMark,
+                   parser._readableState.highWaterMark);
+assert.strictEqual(parser.writableHighWaterMark,
+                   parser._writableState.highWaterMark);
 
 parser._transform = function(chunk, enc, callback) {
   callback(null, { val: chunk[0] });
@@ -53,8 +57,12 @@ const serializer = new Transform({ writableObjectMode: true });
 
 assert(!serializer._readableState.objectMode);
 assert(serializer._writableState.objectMode);
-assert.strictEqual(serializer._readableState.highWaterMark, 16 * 1024);
-assert.strictEqual(serializer._writableState.highWaterMark, 16);
+assert.strictEqual(serializer.readableHighWaterMark, 16 * 1024);
+assert.strictEqual(serializer.writableHighWaterMark, 16);
+assert.strictEqual(parser.readableHighWaterMark,
+                   parser._readableState.highWaterMark);
+assert.strictEqual(parser.writableHighWaterMark,
+                   parser._writableState.highWaterMark);
 
 serializer._transform = function(obj, _, callback) {
   callback(null, Buffer.from([obj.val]));
