129b5b005482f1f618c2153c373dd080a8a4b6fd
Matteo Collina <hello@matteocollina.com>
2017-05-06 14:20:52 +0200

    stream: add destroy and _destroy methods.
    Adds destroy() and _destroy() methods to Readable, Writable, Duplex
    and Transform. It also standardizes the behavior and the implementation
    of destroy(), which has been inconsistent in userland and core.
    This PR also updates all the subsystems of core to use the new
    destroy().
    
    PR-URL: https://github.com/nodejs/node/pull/12925
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Calvin Metcalf <calvin.metcalf@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-tls-writewrap-leak.js
+++ test/parallel/test-tls-writewrap-leak.js
@@ -16,7 +16,7 @@ const server = net.createServer(common.mustCall((c) => {
   const c = tls.connect({ port: server.address().port });
   c.on('error', () => {
     // Otherwise `.write()` callback won't be invoked.
-    c.destroyed = false;
+    c._undestroy();
   });
 
   c.write('hello', common.mustCall((err) => {
