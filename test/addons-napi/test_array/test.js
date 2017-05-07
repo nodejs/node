183b5c5d53ec023c1975d46c1698902ea6c5af85
Michael Dawson <mdawson@devrus.com>
2017-05-07 19:40:58 -0400

    test: improve n-api array func coverage
    - add coverage for napi_has_element
    - add coverage for napi_create_array_with_length

--- test/addons-napi/test_array/test.js
+++ test/addons-napi/test_array/test.js
@@ -19,19 +19,26 @@ const array = [
   ]
 ];
 
-assert.strictEqual(test_array.Test(array, array.length + 1),
+assert.strictEqual(test_array.TestGetElement(array, array.length + 1),
                    'Index out of bound!');
 
 assert.throws(
   () => {
-    test_array.Test(array, -2);
+    test_array.TestGetElement(array, -2);
   },
   /Invalid index\. Expects a positive integer\./
 );
 
 array.forEach(function(element, index) {
-  assert.strictEqual(test_array.Test(array, index), element);
+  assert.strictEqual(test_array.TestGetElement(array, index), element);
 });
 
 
 assert.deepStrictEqual(test_array.New(array), array);
+
+assert(test_array.TestHasElement(array, 0));
+assert.strictEqual(false, test_array.TestHasElement(array, array.length + 1));
+
+assert(test_array.NewWithLength(0) instanceof Array);
+assert(test_array.NewWithLength(1) instanceof Array);
+assert(test_array.NewWithLength(2 ^ 32 - 1) instanceof Array);
