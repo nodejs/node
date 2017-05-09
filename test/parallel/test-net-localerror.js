60c6fbafe4e9f211257cc46e58f24ba3d4963676
akopcz2 <adrian.kopczewski@designory.com>
2017-05-09 10:21:06 -0500

    test: replacing common.PORT with value of 0 inside connection functions within test-net-localerror.js

--- test/parallel/test-net-localerror.js
+++ test/parallel/test-net-localerror.js
@@ -26,13 +26,13 @@ const net = require('net');
 
 connect({
   host: 'localhost',
-  port: common.PORT,
+  port: 0,
   localPort: 'foobar',
 }, /^TypeError: "localPort" option should be a number: foobar$/);
 
 connect({
   host: 'localhost',
-  port: common.PORT,
+  port: 0,
   localAddress: 'foobar',
 }, /^TypeError: "localAddress" option must be a valid IP: foobar$/);
 
