e597b392c2e59db609fb1e43bbaac20c3ada102d
Vse Mozhet Byt <vsemozhetbyt@gmail.com>
2017-05-10 07:53:18 +0300

    test: make a test path-independent
    parallel/test-spawn-cmd-named-pipe.js failed with spaces
    both in node.exe and test paths.

--- test/parallel/test-spawn-cmd-named-pipe.js
+++ test/parallel/test-spawn-cmd-named-pipe.js
@@ -37,15 +37,10 @@ if (!process.argv[2]) {
   });
   stdoutPipeServer.listen(stdoutPipeName);
 
-  const comspec = process.env['comspec'];
-  if (!comspec || comspec.length === 0) {
-    assert.fail('Failed to get COMSPEC');
-  }
+  const args =
+    [`"${__filename}"`, 'child', '<', stdinPipeName, '>', stdoutPipeName];
 
-  const args = ['/c', process.execPath, __filename, 'child',
-                '<', stdinPipeName, '>', stdoutPipeName];
-
-  const child = spawn(comspec, args);
+  const child = spawn(`"${process.execPath}"`, args, { shell: true });
 
   child.on('exit', common.mustCall(function(exitCode) {
     stdinPipeServer.close();
