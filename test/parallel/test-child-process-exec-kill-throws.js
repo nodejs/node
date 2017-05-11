945f20808143d954fea73e4ac3a0d71cf73c9b2c
Vse Mozhet Byt <vsemozhetbyt@gmail.com>
2017-05-11 15:10:00 +0300

    test: make the rest of tests path-independent
    Permit spaces in paths to a Node.js executable and test scripts.
    
    PR-URL: https://github.com/nodejs/node/pull/12972
    Fixes: https://github.com/nodejs/node/issues/12773
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-child-process-exec-kill-throws.js
+++ test/parallel/test-child-process-exec-kill-throws.js
@@ -18,7 +18,7 @@ if (process.argv[2] === 'child') {
     throw new Error('mock error');
   };
 
-  const cmd = `${process.execPath} ${__filename} child`;
+  const cmd = `"${process.execPath}" "${__filename}" child`;
   const options = { maxBuffer: 0 };
   const child = cp.exec(cmd, options, common.mustCall((err, stdout, stderr) => {
     // Verify that if ChildProcess#kill() throws, the error is reported.
