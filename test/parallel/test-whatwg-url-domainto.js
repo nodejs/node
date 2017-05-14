a13c377af82b6d1ba8daa3c6864bf9278dffe5c7
Rajaram Gaunker <zimbabao@gmail.com>
2017-05-13 18:08:48 -0700

    url: fast path ascii domains, do not run ToASCII
    To match browser behavior fast path ascii only domains and
    do not run ToASCII on them.
    
    Fixes: https://github.com/nodejs/node/issues/12965
    Refs: https://github.com/nodejs/node/pull/12966
    Refs: https://github.com/whatwg/url/pull/309

--- test/parallel/test-whatwg-url-domainto.js
+++ test/parallel/test-whatwg-url-domainto.js
@@ -11,7 +11,6 @@ const { domainToASCII, domainToUnicode } = require('url');
 
 // Tests below are not from WPT.
 const tests = require('../fixtures/url-idna.js');
-const testsHyphenDomains = require('../fixtures/url-domains-with-hyphens.js');
 
 {
   const expectedError = common.expectsError(
@@ -36,10 +35,16 @@ const testsHyphenDomains = require('../fixtures/url-domains-with-hyphens.js');
 }
 
 {
-  for (const [i, { ascii, unicode }] of testsHyphenDomains.valid.entries()) {
-    assert.strictEqual(ascii, domainToASCII(unicode),
-                       `domainToASCII(${i + 1})`);
-  }
+  [
+    'r4---sn-a5mlrn7s.gevideo.com',
+    '-sn-a5mlrn7s.gevideo.com',
+    'sn-a5mlrn7s-.gevideo.com',
+    '-sn-a5mlrn7s-.gevideo.com',
+    '-sn--a5mlrn7s-.gevideo.com'
+  ].forEach((domain) => {
+    assert.strictEqual(domain, domainToASCII(domain),
+                       `domainToASCII(${domain})`);
+  })
 }
 
 {
