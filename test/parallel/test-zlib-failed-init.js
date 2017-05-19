e5c39bd9079ec7533a1eeaef2bd36b347777588a
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-05-19 21:31:36 +0300

    zlib: fix bugs in the validation logic

--- test/parallel/test-zlib-failed-init.js
+++ test/parallel/test-zlib-failed-init.js
@@ -11,3 +11,27 @@ const zlib = require('zlib');
 assert.throws(() => {
   zlib.createDeflateRaw({ windowBits: 8 });
 }, /^Error: Init error$/);
+
+// Regression tests for bugs in the validation logic.
+
+assert.throws(() => {
+  zlib.createGzip({ chunkSize: 0 });
+}, /^RangeError: Invalid chunk size: 0$/);
+
+assert.throws(() => {
+  zlib.createGzip({ windowBits: 0 });
+}, /^RangeError: Invalid windowBits: 0$/);
+
+assert.throws(() => {
+  zlib.createGzip({ memLevel: 0 });
+}, /^RangeError: Invalid memLevel: 0$/);
+
+{
+  const stream = zlib.createGzip({ level: NaN });
+  assert.ok(!Number.isNaN(stream._level));
+}
+
+{
+  const stream = zlib.createGzip({ strategy: NaN });
+  assert.ok(!Number.isNaN(stream._strategy));
+}
