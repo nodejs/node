d3e3ade0bf0017f429c6b09573c640537a8546bb
Brian White <mscdex@mscdex.net>
2017-05-23 08:44:47 -0400

    events: improve on() performance

--- benchmark/events/ee-add-remove.js
+++ benchmark/events/ee-add-remove.js
@@ -2,24 +2,26 @@
 var common = require('../common.js');
 var events = require('events');
 
-var bench = common.createBenchmark(main, {n: [25e4]});
+var bench = common.createBenchmark(main, {n: [25e4], listeners: [10]});
 
 function main(conf) {
   var n = conf.n | 0;
+  var listeners = conf.listeners | 0;
 
   var ee = new events.EventEmitter();
-  var listeners = [];
+  ee.setMaxListeners(listeners + 1);
+  var fns = [];
 
   var k;
-  for (k = 0; k < 10; k += 1)
-    listeners.push(function() {});
+  for (k = 0; k < listeners; k += 1)
+    fns.push(function() { return 0; });
 
   bench.start();
   for (var i = 0; i < n; i += 1) {
-    for (k = listeners.length; --k >= 0; /* empty */)
-      ee.on('dummy', listeners[k]);
-    for (k = listeners.length; --k >= 0; /* empty */)
-      ee.removeListener('dummy', listeners[k]);
+    for (k = listeners; --k >= 0; /* empty */)
+      ee.on('dummy', fns[k]);
+    for (k = listeners; --k >= 0; /* empty */)
+      ee.removeListener('dummy', fns[k]);
   }
   bench.end(n);
 }
