f4691bb2f37661cfd481b46461c1a483b99f63b6
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-05-24 14:37:29 +0200

    crypto: remove root_cert_store from node_crypto.h
    root_cert_store is defined as extern in node_crypto.h but only used in
    node_crypto.cc. It is then set using SSL_CTX_set_cert_store. The only
    usages of SSL_CTX_get_cert_store are in node_crypto.cc which would all
    be accessing the same X509_STORE through the root_cert_store pointer as
    far as I can tell. Am I missing something here?
    
    This commit suggests removing it from the header and making it static
    in node_crypto.cc.
    
    PR-URL: https://github.com/nodejs/node/pull/13194
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>

--- src/node_crypto.h
+++ src/node_crypto.h
@@ -63,8 +63,6 @@ enum CheckResult {
 
 extern int VerifyCallback(int preverify_ok, X509_STORE_CTX* ctx);
 
-extern X509_STORE* root_cert_store;
-
 extern void UseExtraCaCerts(const std::string& file);
 
 // Forward declaration
