36de54af58904dd9eb8abe1db7920fd5909f73b5
Michael Dawson <michael_dawson@ca.ibm.com>
2017-05-30 17:47:05 -0400

    test: consolidate n-api test addons
    It takes time to build each of the addons used to test n-api.
    Consolidate a few of the smaller ones to save build time.
    
    PR-URL: https://github.com/nodejs/node/pull/13317
    Reviewed-By: Jason Ginchereau <jasongin@microsoft.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/addons-napi/test_general/testInstanceOf.js
+++ test/addons-napi/test_general/testInstanceOf.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 
 // addon is referenced through the eval expression in testFile
 // eslint-disable-next-line no-unused-vars
-const addon = require(`./build/${common.buildType}/test_instanceof`);
+const addon = require(`./build/${common.buildType}/test_general`);
 const path = require('path');
 
 // The following assert functions are referenced by v8's unit tests
--- test/addons-napi/test_general/test_general.c
+++ test/addons-napi/test_general/test_general.c
@@ -33,11 +33,40 @@ napi_value testGetVersion(napi_env env, napi_callback_info info) {
   return result;
 }
 
+napi_value doInstanceOf(napi_env env, napi_callback_info info) {
+  size_t argc = 2;
+  napi_value args[2];
+  NAPI_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
+
+  bool instanceof;
+  NAPI_CALL(env, napi_instanceof(env, args[0], args[1], &instanceof));
+
+  napi_value result;
+  NAPI_CALL(env, napi_get_boolean(env, instanceof, &result));
+
+  return result;
+}
+
+napi_value getNull(napi_env env, napi_callback_info info) {
+  napi_value result;
+  NAPI_CALL(env, napi_get_null(env, &result));
+  return result;
+}
+
+napi_value getUndefined(napi_env env, napi_callback_info info) {
+  napi_value result;
+  NAPI_CALL(env, napi_get_undefined(env, &result));
+  return result;
+}
+
 void Init(napi_env env, napi_value exports, napi_value module, void* priv) {
   napi_property_descriptor descriptors[] = {
     DECLARE_NAPI_PROPERTY("testStrictEquals", testStrictEquals),
     DECLARE_NAPI_PROPERTY("testGetPrototype", testGetPrototype),
     DECLARE_NAPI_PROPERTY("testGetVersion", testGetVersion),
+    DECLARE_NAPI_PROPERTY("doInstanceOf", doInstanceOf),
+    DECLARE_NAPI_PROPERTY("getUndefined", getUndefined),
+    DECLARE_NAPI_PROPERTY("getNull", getNull),
   };
 
   NAPI_CALL_RETURN_VOID(env, napi_define_properties(
