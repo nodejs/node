06cf9480d393adae4eaf698719836f07d6121ea0
Michael Dawson <michael_dawson@ca.ibm.com>
2017-05-30 17:47:05 -0400

    test: consolidate n-api test addons
    It takes time to build each of the addons used to test n-api.
    Consolidate a few of the smaller ones to save build time.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/19447
    PR-URL: https://github.com/nodejs/node/pull/13317
    Reviewed-By: Jason Ginchereau <jasongin@microsoft.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/addons-napi/test_general/testGlobals.js
+++ test/addons-napi/test_general/testGlobals.js
@@ -2,7 +2,7 @@
 const common = require('../../common');
 const assert = require('assert');
 
-const test_globals = require(`./build/${common.buildType}/test_globals`);
+const test_globals = require(`./build/${common.buildType}/test_general`);
 
 assert.strictEqual(test_globals.getUndefined(), undefined);
 assert.strictEqual(test_globals.getNull(), null);
