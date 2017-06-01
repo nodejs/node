5a13d1afd79dc58ee68ba7263e1ad58a7e14122b
Timothy Gu <timothygu99@gmail.com>
2017-05-31 17:13:43 -0700

    url: update IDNA handling
    Remove custom tests for invalid IDNA domains in url-idna.js in favor of
    the more comprehensive official set.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/17365
    PR-URL: https://github.com/nodejs/node/pull/13362
    Refs: https://github.com/whatwg/url/pull/309
    Refs: https://github.com/w3c/web-platform-tests/pull/5976
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Daijiro Wachi <daijiro.wachi@gmail.com>

--- src/node_i18n.h
+++ src/node_i18n.h
@@ -16,6 +16,30 @@ namespace i18n {
 
 bool InitializeICUDirectory(const std::string& path);
 
+enum idna_mode {
+  // Default mode for maximum compatibility.
+  IDNA_DEFAULT,
+  // Ignore all errors in IDNA conversion, if possible.
+  IDNA_LENIENT,
+  // Enforce STD3 rules (UseSTD3ASCIIRules) and DNS length restrictions
+  // (VerifyDnsLength). Corresponds to `beStrict` flag in the "domain to ASCII"
+  // algorithm.
+  IDNA_STRICT
+};
+
+// Implements the WHATWG URL Standard "domain to ASCII" algorithm.
+// https://url.spec.whatwg.org/#concept-domain-to-ascii
+int32_t ToASCII(MaybeStackBuffer<char>* buf,
+                const char* input,
+                size_t length,
+                enum idna_mode mode = IDNA_DEFAULT);
+
+// Implements the WHATWG URL Standard "domain to Unicode" algorithm.
+// https://url.spec.whatwg.org/#concept-domain-to-unicode
+int32_t ToUnicode(MaybeStackBuffer<char>* buf,
+                  const char* input,
+                  size_t length);
+
 }  // namespace i18n
 }  // namespace node
 
