48cad9cde6dbc2b684c889ffd06ad3bf43486083
Brian White <mscdex@mscdex.net>
2017-06-02 02:07:58 -0400

    tools: fix node args passing in test runner
    This fixes a regression from 53c88fa4111 so that special arguments
    can once again be passed to the node executable when running tests.
    
    PR-URL: https://github.com/nodejs/node/pull/13384
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- tools/test.py
+++ tools/test.py
@@ -817,6 +817,10 @@ class TestRepository(TestSuite):
       (file, pathname, description) = imp.find_module('testcfg', [ self.path ])
       module = imp.load_module('testcfg', file, pathname, description)
       self.config = module.GetConfiguration(context, self.path)
+      if hasattr(self.config, 'additional_flags'):
+        self.config.additional_flags += context.node_args
+      else:
+        self.config.additional_flags = context.node_args
     finally:
       if file:
         file.close()
