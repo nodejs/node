a430541b51dbef50525e9c4ea0b8f47f2d8b5930
Rich Trott <rtrott@gmail.com>
2017-06-06 09:53:32 -0700

    test: refactor test-fs-read-*
    * Use `common.mustNotCall()` in place of `common.noop` where appropriate
    * Increase specificity of regular expressions (that is, make them match
      the whole error string rather than part of the error string) in
      `assert.throws()` calls

--- test/parallel/test-fs-read-type.js
+++ test/parallel/test-fs-read-type.js
@@ -13,9 +13,9 @@ assert.throws(() => {
           expected.length,
           0,
           'utf-8',
-          common.noop);
-}, /Second argument needs to be a buffer/);
+          common.mustNotCall());
+}, /^TypeError: Second argument needs to be a buffer$/);
 
 assert.throws(() => {
   fs.readSync(fd, expected.length, 0, 'utf-8');
-}, /Second argument needs to be a buffer/);
+}, /^TypeError: Second argument needs to be a buffer$/);
