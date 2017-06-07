58e177cde1d7b97cbf7229fdfdc1e9331d1e23c0
Sam Roberts <vieuxtech@gmail.com>
2017-06-07 12:49:00 -0700

    doc: document and test that methods return this
    Also, add tests to ensure they will always return this, and to confirm
    they return this when these doc changes are back-ported to earlier
    release lines.
    
    PR-URL: https://github.com/nodejs/node/pull/13531
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-net-server-close.js
+++ test/parallel/test-net-server-close.js
@@ -44,7 +44,7 @@ const server = net.createServer(function(c) {
   sockets.push(c);
 
   if (sockets.length === 2) {
-    server.close();
+    assert.strictEqual(server.close(), server);
     sockets.forEach(function(c) { c.destroy(); });
   }
 });
@@ -53,7 +53,7 @@ server.on('close', function() {
   events.push('server');
 });
 
-server.listen(0, function() {
+assert.strictEqual(server, server.listen(0, function() {
   net.createConnection(this.address().port);
   net.createConnection(this.address().port);
-});
+}));
