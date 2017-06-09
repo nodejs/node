dd8e09a2d22db9d6c8b12b07e954b49e234de6f2
Luigi Pinca <luigipinca@gmail.com>
2017-06-09 18:44:59 +0200

    fixup! http: handle cases where socket.server is null

--- test/parallel/test-cluster-send-socket-to-worker-http-server.js
+++ test/parallel/test-cluster-send-socket-to-worker-http-server.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Regression test for https://github.com/nodejs/node/issues/13435
+// Tests that `socket.server` is correctly set when a socket is sent to a worker
+// and the `'connection'` event is emitted manually on an HTTP server.
+
 const common = require('../common');
 const assert = require('assert');
 const cluster = require('cluster');
@@ -8,13 +12,11 @@ const net = require('net');
 
 if (cluster.isMaster) {
   const worker = cluster.fork();
-  const server = net.createServer({
-    pauseOnConnect: true
-  }, common.mustCall((socket) => {
+  const server = net.createServer(common.mustCall((socket) => {
     worker.send('socket', socket);
   }));
 
-  worker.on('exit', common.mustCall((code, signal) => {
+  worker.on('exit', common.mustCall((code) => {
     assert.strictEqual(code, 0);
     server.close();
   }));
@@ -25,13 +27,13 @@ if (cluster.isMaster) {
 } else {
   const server = http.createServer();
 
-  server.setTimeout(100, common.mustCall((socket) => {
+  server.on('connection', common.mustCall((socket) => {
+    assert.strictEqual(socket.server, server);
     socket.destroy();
-    cluster.worker.kill();
+    cluster.worker.disconnect();
   }));
 
   process.on('message', common.mustCall((message, socket) => {
     server.emit('connection', socket);
-    socket.resume();
   }));
 }
