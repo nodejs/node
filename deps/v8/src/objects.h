66c1197f3aefd5b9f36533e6b617f2046acc2c0f
Zuzana Svetlikova <zsvetlik@redhat.com>
2017-06-09 14:07:19 +0200

    v8: fix build errors with g++ 7
    This is a local patch because upstream fixed it differently by moving
    large chunks of code out of objects.h.  We cannot easily back-port
    those changes due to their size and invasiveness.
    
    Fixes: https://github.com/nodejs/node/issues/10388
    PR-URL: https://github.com/nodejs/node/pull/12392
    Backport-PR-URL: https://github.com/nodejs/node/pull/13574
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- deps/v8/src/objects.h
+++ deps/v8/src/objects.h
@@ -3261,22 +3261,10 @@ class HashTableBase : public FixedArray {
 template <typename Derived, typename Shape, typename Key>
 class HashTable : public HashTableBase {
  public:
-  // Wrapper methods
-  inline uint32_t Hash(Key key) {
-    if (Shape::UsesSeed) {
-      return Shape::SeededHash(key, GetHeap()->HashSeed());
-    } else {
-      return Shape::Hash(key);
-    }
-  }
-
-  inline uint32_t HashForObject(Key key, Object* object) {
-    if (Shape::UsesSeed) {
-      return Shape::SeededHashForObject(key, GetHeap()->HashSeed(), object);
-    } else {
-      return Shape::HashForObject(key, object);
-    }
-  }
+  // Wrapper methods.  Defined in src/objects-inl.h
+  // to break a cycle with src/heap/heap.h.
+  inline uint32_t Hash(Key key);
+  inline uint32_t HashForObject(Key key, Object* object);
 
   // Returns a new HashTable object.
   MUST_USE_RESULT static Handle<Derived> New(
