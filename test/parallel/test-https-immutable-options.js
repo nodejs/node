26d4efb02d17cf7d07df0af87a0e34fb9b8c5f27
XadillaX <admin@xcoder.in>
2017-06-11 02:09:35 +0800

    https: make opts optional & immutable when create
    `opts` in `createServer` will be immutable that won't change origional
    opts value. What's more, it's optional which can make `requestListener`
    be the first argument.
    
    Refs: https://github.com/nodejs/node/issues/13584

---
+++ test/parallel/test-https-immutable-options.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const common = require('../common');
+
+const assert = require('assert');
+const https = require('https');
+const tls = require('tls');
+
+const dftProtocol = {};
+tls.convertNPNProtocols([ 'http/1.1', 'http/1.0' ], dftProtocol);
+
+const opts = { foo: 'bar' };
+const server1 = https.createServer(opts);
+
+assert.deepStrictEqual(opts, { foo: 'bar' });
+assert.strictEqual(server1.NPNProtocols.compare(dftProtocol.NPNProtocols), 0);
+
+const mustNotCall = common.mustNotCall('dummy callback');
+const server2 = https.createServer(mustNotCall);
+
+assert.strictEqual(server2.NPNProtocols.compare(dftProtocol.NPNProtocols), 0);
+assert.strictEqual(mustNotCall, server2._events.request);
