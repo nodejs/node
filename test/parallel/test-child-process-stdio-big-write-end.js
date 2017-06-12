19a793a34c38b7a02a6c312b36c2aab62c4426ac
Rich Trott <rtrott@gmail.com>
2017-06-11 21:10:56 -0700

    test: increase bufsize in child process write test
    test-child-process-stdio-big-write-end was failing on ubuntu1604-arm64
    because the while loop that was supposed to fill up the buffer ended up
    being an infinite loop.
    
    This increases the size of the writes in the loop by 1K until the buffer
    fills up.
    
    PR-URL: https://github.com/nodejs/node/pull/13626
    Fixes: https://github.com/nodejs/node/issues/13603
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Alexey Orlenko <eaglexrlnk@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-child-process-stdio-big-write-end.js
+++ test/parallel/test-child-process-stdio-big-write-end.js
@@ -22,7 +22,7 @@
 'use strict';
 require('../common');
 const assert = require('assert');
-const BUFSIZE = 1024;
+let bufsize = 0;
 
 switch (process.argv[2]) {
   case undefined:
@@ -51,14 +51,15 @@ function parent() {
   // Write until the buffer fills up.
   let buf;
   do {
-    buf = Buffer.alloc(BUFSIZE, '.');
-    sent += BUFSIZE;
+    bufsize += 1024;
+    buf = Buffer.alloc(bufsize, '.');
+    sent += bufsize;
   } while (child.stdin.write(buf));
 
   // then write a bunch more times.
   for (let i = 0; i < 100; i++) {
-    const buf = Buffer.alloc(BUFSIZE, '.');
-    sent += BUFSIZE;
+    const buf = Buffer.alloc(bufsize, '.');
+    sent += bufsize;
     child.stdin.write(buf);
   }
 
