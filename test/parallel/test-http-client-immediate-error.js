fcd82e760106ed7bd7aea634b65fe9faac7c8e7c
Brian White <mscdex@mscdex.net>
2017-06-15 03:28:29 -0400

    test: improve http test reliability
    Fake the socket handle to ensure an immediate error is returned
    uniformly across all platforms.
    
    PR-URL: https://github.com/nodejs/node/pull/13693
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-http-client-immediate-error.js
+++ test/parallel/test-http-client-immediate-error.js
@@ -5,8 +5,36 @@
 
 const common = require('../common');
 const assert = require('assert');
+const net = require('net');
 const http = require('http');
-const req = http.get({ host: '127.0.0.1', port: 1 });
-req.on('error', common.mustCall((err) => {
-  assert.strictEqual(err.code, 'ECONNREFUSED');
+const uv = process.binding('uv');
+const { async_id_symbol } = process.binding('async_wrap');
+const { newUid } = require('async_hooks');
+
+const agent = new http.Agent();
+agent.createConnection = common.mustCall((cfg) => {
+  const sock = new net.Socket();
+
+  // Fake the handle so we can enforce returning an immediate error
+  sock._handle = {
+    connect: common.mustCall((req, addr, port) => {
+      return uv.UV_ENETUNREACH;
+    }),
+    readStart() {},
+    close() {}
+  };
+
+  // Simulate just enough socket handle initialization
+  sock[async_id_symbol] = newUid();
+
+  sock.connect(cfg);
+  return sock;
+});
+
+http.get({
+  host: '127.0.0.1',
+  port: 1,
+  agent
+}).on('error', common.mustCall((err) => {
+  assert.strictEqual(err.code, 'ENETUNREACH');
 }));
