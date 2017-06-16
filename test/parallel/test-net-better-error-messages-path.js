f40caf728247ce78a1fe0a0673c4893fe299fafc
Ruben Bridgewater <ruben@bridgewater.de>
2017-06-16 15:47:48 -0400

    net: fix abort on bad address input
    PR-URL: https://github.com/nodejs/node/pull/13726
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-net-better-error-messages-path.js
+++ test/parallel/test-net-better-error-messages-path.js
@@ -2,12 +2,21 @@
 const common = require('../common');
 const net = require('net');
 const assert = require('assert');
-const fp = '/tmp/fadagagsdfgsdf';
-const c = net.connect(fp);
 
-c.on('connect', common.mustNotCall());
+{
+  const fp = '/tmp/fadagagsdfgsdf';
+  const c = net.connect(fp);
 
-c.on('error', common.mustCall(function(e) {
-  assert.strictEqual(e.code, 'ENOENT');
-  assert.strictEqual(e.message, `connect ENOENT ${fp}`);
-}));
+  c.on('connect', common.mustNotCall());
+  c.on('error', common.expectsError({
+    code: 'ENOENT',
+    message: `connect ENOENT ${fp}`
+  }));
+}
+
+{
+  assert.throws(
+    () => net.createConnection({ path: {} }),
+    common.expectsError({ code: 'ERR_INVALID_ARG_TYPE' })
+  );
+}
