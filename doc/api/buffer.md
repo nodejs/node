22cf25cf2d251371590ea35c740793a5f19d4623
James M Snell <jasnell@gmail.com>
2017-06-16 12:35:15 -0700

    buffer: support boxed strings and toPrimitive
    Add support for `Buffer.from(new String('...'))` and
    `Buffer.from({[Symbol.toPrimitive]() { return '...'; }})`
    
    PR-URL: https://github.com/nodejs/node/pull/13725
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- doc/api/buffer.md
+++ doc/api/buffer.md
@@ -908,6 +908,44 @@ console.log(buf2.toString());
 
 A `TypeError` will be thrown if `str` is not a string.
 
+### Class Method: Buffer.from(object[, offsetOrEncoding[, length]])
+<!-- YAML
+added: REPLACEME
+-->
+
+* `object` {Object} An object supporting `Symbol.toPrimitive` or `valueOf()`
+* `offsetOrEncoding` {number|string} A byte-offset or encoding, depending on
+  the value returned either by `object.valueOf()` or
+  `object[Symbol.toPrimitive]()`.
+* `length` {number} A length, depending on the value returned either by
+  `object.valueOf()` or `object[Symbol.toPrimitive]()`.
+
+For objects whose `valueOf()` function returns a value not strictly equal to
+`object`, returns `Buffer.from(object.valueOf(), offsetOrEncoding, length)`.
+
+For example:
+
+```js
+const buf = Buffer.from(new String('this is a test'));
+// <Buffer 74 68 69 73 20 69 73 20 61 20 74 65 73 74>
+```
+
+For objects that support `Symbol.toPrimitive`, returns
+`Buffer.from(object[Symbol.toPrimitive](), offsetOrEncoding, length)`.
+
+For example:
+
+```js
+class Foo {
+  [Symbol.toPrimitive]() {
+    return 'this is a test';
+  }
+}
+
+const buf = Buffer.from(new Foo(), 'utf8');
+// <Buffer 74 68 69 73 20 69 73 20 61 20 74 65 73 74>
+```
+
 ### Class Method: Buffer.isBuffer(obj)
 <!-- YAML
 added: v0.1.101
---
+++ test/parallel/test-buffer-from.js
@@ -0,0 +1,57 @@
+'use strict';
+
+require('../common');
+const { deepStrictEqual, throws } = require('assert');
+const { Buffer } = require('buffer');
+const { runInNewContext } = require('vm');
+
+const checkString = 'test';
+
+const check = Buffer.from(checkString);
+
+class MyString extends String {
+  constructor() {
+    super(checkString);
+  }
+}
+
+class MyPrimitive {
+  [Symbol.toPrimitive]() {
+    return checkString;
+  }
+}
+
+class MyBadPrimitive {
+  [Symbol.toPrimitive]() {
+    return 1;
+  }
+}
+
+deepStrictEqual(Buffer.from(new String(checkString)), check);
+deepStrictEqual(Buffer.from(new MyString()), check);
+deepStrictEqual(Buffer.from(new MyPrimitive()), check);
+deepStrictEqual(Buffer.from(
+                  runInNewContext('new String(checkString)', {checkString})),
+                check);
+
+const err = new RegExp('^TypeError: First argument must be a string, Buffer, ' +
+                       'ArrayBuffer, Array, or array-like object\\.$');
+
+[
+  {},
+  new Boolean(true),
+  { valueOf() { return null; } },
+  { valueOf() { return undefined; } },
+  { valueOf: null },
+  Object.create(null)
+].forEach((input) => {
+  throws(() => Buffer.from(input), err);
+});
+
+[
+  new Number(true),
+  new MyBadPrimitive()
+].forEach((input) => {
+  throws(() => Buffer.from(input),
+         /^TypeError: "value" argument must not be a number$/);
+});
