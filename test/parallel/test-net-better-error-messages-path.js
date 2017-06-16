7a6f4144cfd4034567b6ce3c5a32d5c55471c4fb
Ruben Bridgewater <ruben@bridgewater.de>
2017-06-17 00:20:53 +0200

    address comments

--- test/parallel/test-net-better-error-messages-path.js
+++ test/parallel/test-net-better-error-messages-path.js
@@ -8,14 +8,15 @@ const assert = require('assert');
   const c = net.connect(fp);
 
   c.on('connect', common.mustNotCall());
-  c.on('error', common.mustCall(function(e) {
-    assert.strictEqual(e.code, 'ENOENT');
-    assert.strictEqual(e.message, `connect ENOENT ${fp}`);
+  c.on('error', common.expectsError({
+    code: 'ENOENT',
+    message: `connect ENOENT ${fp}`
   }));
 }
 
 {
-  assert.throws(() => {
-    net.createConnection({ path: {} });
-  }, TypeError);
+  assert.throws(
+    () => net.createConnection({ path: {} }),
+    common.expectsError({ code: 'ERR_INVALID_ARG_TYPE' })
+  );
 }
