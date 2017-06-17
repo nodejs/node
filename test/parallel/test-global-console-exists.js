4885982ee270429e1d54602d6abb7ee1b174cb1b
Rich Trott <rtrott@gmail.com>
2017-06-17 13:25:51 -0700

    test: remove common module from test it thwarts
    test-global-console-exists cannot use the common module as explained in
    a comment but it was included later anyway. This change removes it.
    
    PR-URL: https://github.com/nodejs/node/pull/13748
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-global-console-exists.js
+++ test/parallel/test-global-console-exists.js
@@ -1,39 +1,46 @@
 /* eslint-disable required-modules */
-// ordinarily test files must require('common') but that action causes
-// the global console to be compiled, defeating the purpose of this test
 
 'use strict';
 
-const common = require('../common');
+// Ordinarily test files must require('common') but that action causes
+// the global console to be compiled, defeating the purpose of this test.
+
 const assert = require('assert');
 const EventEmitter = require('events');
 const leakWarning = /EventEmitter memory leak detected\. 2 hello listeners/;
 
-common.hijackStderr(common.mustCall(function(data) {
-  if (process.stderr.writeTimes === 0) {
-    assert.ok(leakWarning.test(data));
-  } else {
-    assert.fail('stderr.write should be called only once');
-  }
-}));
-
-process.on('warning', function(warning) {
+let writeTimes = 0;
+let warningTimes = 0;
+process.on('warning', () => {
   // This will be called after the default internal
   // process warning handler is called. The default
   // process warning writes to the console, which will
   // invoke the monkeypatched process.stderr.write
   // below.
-  assert.strictEqual(process.stderr.writeTimes, 1);
+  assert.strictEqual(writeTimes, 1);
   EventEmitter.defaultMaxListeners = oldDefault;
-  // when we get here, we should be done
+  warningTimes++;
 });
+
+process.on('exit', () => {
+  assert.strictEqual(warningTimes, 1);
+});
+
+process.stderr.write = (data) => {
+  if (writeTimes === 0)
+    assert.ok(leakWarning.test(data));
+  else
+    assert.fail('stderr.write should be called only once');
+
+  writeTimes++;
+};
 
 const oldDefault = EventEmitter.defaultMaxListeners;
 EventEmitter.defaultMaxListeners = 1;
 
 const e = new EventEmitter();
-e.on('hello', common.noop);
-e.on('hello', common.noop);
+e.on('hello', () => {});
+e.on('hello', () => {});
 
 // TODO: Figure out how to validate console. Currently,
 // there is no obvious way of validating that console
