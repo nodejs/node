cc68bc27f8acc62fb8b2145865c995415648735a
Refael Ackermann <refack@gmail.com>
2017-06-19 15:24:04 -0400

    test: add more asserts to `test-internal-errors`
    Backport-PR-URL: https://github.com/nodejs/node/pull/19579
    PR-URL: https://github.com/nodejs/node/pull/13686
    Fixes: https://github.com/nodejs/node/issues/13682
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/parallel/test-internal-errors.js
+++ test/parallel/test-internal-errors.js
@@ -175,6 +175,7 @@ common.expectsError(() => {
        message: /^Error for testing 2/ });
 }, {
   code: 'ERR_ASSERTION',
+  type: assert.AssertionError,
   message: /.+ does not match \S/
 });
 
@@ -225,6 +226,7 @@ common.expectsError(
   () => errors.message('ERR_INVALID_URL_SCHEME', [[]]),
   {
     code: 'ERR_ASSERTION',
+    type: assert.AssertionError,
     message: /^At least one expected value needs to be specified$/
   });
 
@@ -239,6 +241,7 @@ common.expectsError(
   () => errors.message('ERR_MISSING_ARGS'),
   {
     code: 'ERR_ASSERTION',
+    type: assert.AssertionError,
     message: /^At least one arg needs to be specified$/
   });
 
