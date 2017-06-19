ea12038ffe0573a1eff9daf7190b2931b352a8f8
Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
2017-06-19 20:18:12 -0300

    test: remove unneeded HandleScope usage
    These methods are Javascript-accessible so they get an implicit
    HandleScope. The extra scope is unneeded and can be dropped.
    
    PR-URL: https://github.com/nodejs/node/pull/13859
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Signed-off-by: Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>

--- test/addons/repl-domain-abort/binding.cc
+++ test/addons/repl-domain-abort/binding.cc
@@ -25,14 +25,12 @@
 using v8::Function;
 using v8::FunctionCallbackInfo;
 using v8::Local;
-using v8::HandleScope;
 using v8::Isolate;
 using v8::Object;
 using v8::Value;
 
 void Method(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
-  HandleScope scope(isolate);
   node::MakeCallback(isolate,
                      isolate->GetCurrentContext()->Global(),
                      args[0].As<Function>(),
