e42b1b16b72904ca5d9abc2a4023303c809ec561
Steven Winston <swinston100@hotmail.com>
2017-06-21 23:53:52 +0300

    test: remove unnecessary Buffer import
    Removed require('buffer') from
    
    - test/disabled/test-sendfd.js
    - test/internet/test-dgram-broadcast-multi-process.js
    - test/pummel/test-https-no-reader.js
    
    PR-URL: https://github.com/nodejs/node/pull/13860
    Refs: https://github.com/nodejs/node/issues/13836
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Alexey Orlenko <eaglexrlnk@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Vse Mozhet Byt <vsemozhetbyt@gmail.com>

--- test/disabled/test-sendfd.js
+++ test/disabled/test-sendfd.js
@@ -50,7 +50,6 @@
 const common = require('../common');
 const assert = require('assert');
 
-const buffer = require('buffer');
 const child_process = require('child_process');
 const fs = require('fs');
 const net = require('net');
@@ -112,7 +111,7 @@ var srv = net.createServer(function(s) {
   var str = JSON.stringify(DATA) + '\n';
 
   DATA.ord = DATA.ord + 1;
-  var buf = buffer.Buffer.allocUnsafe(str.length);
+  var buf = Buffer.allocUnsafe(str.length);
   buf.write(JSON.stringify(DATA) + '\n', 'utf8');
 
   s.write(str, 'utf8', pipeFDs[1]);
