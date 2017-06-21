dda959d5cbc3f559671b6134dba46918265cbdc5
sallen450 <jqh101@sina.com>
2017-06-22 00:49:58 +0800

    test: remove require('buffer') on 6 fs test files
    * test/parallel/test-fs-mkdtemp.js
    * test/parallel/test-fs-read-zero-length.js
    * test/parallel/test-fs-read.js
    * test/parallel/test-fs-whatwg-url.js
    * test/parallel/test-fs-write-string-coerce.js
    * test/parallel/test-fs-write.js
    
    PR-URL: https://github.com/nodejs/node/pull/13845
    Refs: https://github.com/nodejs/node/issues/13836
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Alexey Orlenko <eaglexrlnk@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Vse Mozhet Byt <vsemozhetbyt@gmail.com>

--- test/parallel/test-fs-write-string-coerce.js
+++ test/parallel/test-fs-write-string-coerce.js
@@ -2,7 +2,6 @@
 const common = require('../common');
 const assert = require('assert');
 const path = require('path');
-const Buffer = require('buffer').Buffer;
 const fs = require('fs');
 
 common.refreshTmpDir();
