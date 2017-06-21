58b9436ee8fde735ac05416ba3b24afa48c2d6b2
Zongmin Lei <leizongmin@gmail.com>
2017-06-22 01:22:33 +0800

    test: remove `require('buffer')` from 4 test files
    Remove superfluous import from:
    
    * test/parallel/test-buffer-fakes.js
    * test/parallel/test-buffer-includes.js
    * test/parallel/test-buffer-indexof.js
    * test/parallel/test-buffer-new.js
    
    Refs: https://github.com/nodejs/node/issues/13836
    PR-URL: https://github.com/nodejs/node/pull/13846
    Reviewed-By: Vse Mozhet Byt <vsemozhetbyt@gmail.com>
    Reviewed-By: Alexey Orlenko <eaglexrlnk@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Jackson Tian <shyvo1987@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- test/parallel/test-buffer-new.js
+++ test/parallel/test-buffer-new.js
@@ -2,6 +2,5 @@
 
 require('../common');
 const assert = require('assert');
-const Buffer = require('buffer').Buffer;
 
 assert.throws(() => new Buffer(42, 'utf8'), /first argument must be a string/);
