e903a963441c803363058a9af66f42805d1246e5
Steven Winston <swinston100@hotmail.com>
2017-06-22 00:15:31 +0300

    Update test-sendfd.js

--- test/disabled/test-sendfd.js
+++ test/disabled/test-sendfd.js
@@ -8,7 +8,7 @@
 // persons to whom the Software is furnished to do so, subject to the
 // following conditions:
 //
-// The above copnotice and this permission notice shall be included
+// The above copyright notice and this permission notice shall be included
 // in all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
