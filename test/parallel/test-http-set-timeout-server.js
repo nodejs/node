92656e51590a93f6563437feae7b97ded1b5cc4c
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-06-26 21:41:28 +0300

    test: refactor test-http(s)-set-timeout-server
    * Make changes to `test-https-set-timeout-server` to resolve
      inconsistencies with its http counterpart:
    
      - Apply the changes analogous to those in GH-13802 to the https test.
      - Add a missing `common.mustCall()` wrapper.
      - Make small stylistic changes (e.g., remove unnecessary line breaks
        in comments) to make it visually consistent with the http test.
    
    * Use arrow functions.
    
    PR-URL: https://github.com/nodejs/node/pull/13935
    Fixes: https://github.com/nodejs/node/issues/13588
    Refs: https://github.com/nodejs/node/pull/13802
    Refs: https://github.com/nodejs/node/pull/13625
    Refs: https://github.com/nodejs/node/pull/13822
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Yuta Hiroto <hello@about-hiroppy.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-http-set-timeout-server.js
+++ test/parallel/test-http-set-timeout-server.js
@@ -42,11 +42,11 @@ function run() {
 }
 
 test(function serverTimeout(cb) {
-  const server = http.createServer(common.mustCall(function(req, res) {
+  const server = http.createServer(common.mustCall((req, res) => {
     // just do nothing, we should get a timeout event.
   }));
-  server.listen(common.mustCall(function() {
-    const s = server.setTimeout(50, common.mustCall(function(socket) {
+  server.listen(common.mustCall(() => {
+    const s = server.setTimeout(50, common.mustCall((socket) => {
       socket.destroy();
       server.close();
       cb();
@@ -59,16 +59,16 @@ test(function serverTimeout(cb) {
 });
 
 test(function serverRequestTimeout(cb) {
-  const server = http.createServer(common.mustCall(function(req, res) {
+  const server = http.createServer(common.mustCall((req, res) => {
     // just do nothing, we should get a timeout event.
-    const s = req.setTimeout(50, common.mustCall(function(socket) {
+    const s = req.setTimeout(50, common.mustCall((socket) => {
       socket.destroy();
       server.close();
       cb();
     }));
     assert.ok(s instanceof http.IncomingMessage);
   }));
-  server.listen(common.mustCall(function() {
+  server.listen(common.mustCall(() => {
     const req = http.request({
       port: server.address().port,
       method: 'POST'
@@ -80,16 +80,16 @@ test(function serverRequestTimeout(cb) {
 });
 
 test(function serverResponseTimeout(cb) {
-  const server = http.createServer(common.mustCall(function(req, res) {
+  const server = http.createServer(common.mustCall((req, res) => {
     // just do nothing, we should get a timeout event.
-    const s = res.setTimeout(50, common.mustCall(function(socket) {
+    const s = res.setTimeout(50, common.mustCall((socket) => {
       socket.destroy();
       server.close();
       cb();
     }));
     assert.ok(s instanceof http.OutgoingMessage);
   }));
-  server.listen(common.mustCall(function() {
+  server.listen(common.mustCall(() => {
     http.get({
       port: server.address().port
     }).on('error', common.mustCall());
@@ -97,18 +97,18 @@ test(function serverResponseTimeout(cb) {
 });
 
 test(function serverRequestNotTimeoutAfterEnd(cb) {
-  const server = http.createServer(common.mustCall(function(req, res) {
+  const server = http.createServer(common.mustCall((req, res) => {
     // just do nothing, we should get a timeout event.
     const s = req.setTimeout(50, common.mustNotCall());
     assert.ok(s instanceof http.IncomingMessage);
     res.on('timeout', common.mustCall());
   }));
-  server.on('timeout', common.mustCall(function(socket) {
+  server.on('timeout', common.mustCall((socket) => {
     socket.destroy();
     server.close();
     cb();
   }));
-  server.listen(common.mustCall(function() {
+  server.listen(common.mustCall(() => {
     http.get({
       port: server.address().port
     }).on('error', common.mustCall());
@@ -118,31 +118,31 @@ test(function serverRequestNotTimeoutAfterEnd(cb) {
 test(function serverResponseTimeoutWithPipeline(cb) {
   let caughtTimeout = '';
   let secReceived = false;
-  process.on('exit', function() {
+  process.on('exit', () => {
     assert.strictEqual(caughtTimeout, '/2');
   });
-  const server = http.createServer(function(req, res) {
+  const server = http.createServer((req, res) => {
     if (req.url === '/2')
       secReceived = true;
-    const s = res.setTimeout(50, function() {
+    const s = res.setTimeout(50, () => {
       caughtTimeout += req.url;
     });
     assert.ok(s instanceof http.OutgoingMessage);
     if (req.url === '/1') res.end();
   });
-  server.on('timeout', common.mustCall(function(socket) {
+  server.on('timeout', common.mustCall((socket) => {
     if (secReceived) {
       socket.destroy();
       server.close();
       cb();
     }
   }));
-  server.listen(common.mustCall(function() {
+  server.listen(common.mustCall(() => {
     const options = {
       port: server.address().port,
       allowHalfOpen: true,
     };
-    const c = net.connect(options, function() {
+    const c = net.connect(options, () => {
       c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
       c.write('GET /2 HTTP/1.1\r\nHost: localhost\r\n\r\n');
       c.write('GET /3 HTTP/1.1\r\nHost: localhost\r\n\r\n');
@@ -151,23 +151,23 @@ test(function serverResponseTimeoutWithPipeline(cb) {
 });
 
 test(function idleTimeout(cb) {
-  const server = http.createServer(common.mustCall(function(req, res) {
+  const server = http.createServer(common.mustCall((req, res) => {
     req.on('timeout', common.mustNotCall());
     res.on('timeout', common.mustNotCall());
     res.end();
   }));
-  const s = server.setTimeout(50, common.mustCall(function(socket) {
+  const s = server.setTimeout(50, common.mustCall((socket) => {
     socket.destroy();
     server.close();
     cb();
   }));
   assert.ok(s instanceof http.Server);
-  server.listen(common.mustCall(function() {
+  server.listen(common.mustCall(() => {
     const options = {
       port: server.address().port,
       allowHalfOpen: true,
     };
-    const c = net.connect(options, function() {
+    const c = net.connect(options, () => {
       c.write('GET /1 HTTP/1.1\r\nHost: localhost\r\n\r\n');
       // Keep-Alive
     });
