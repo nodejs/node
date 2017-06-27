a5fd766709c86368a092e8db550bd082d1b38475
Matt Loring <mattloring@google.com>
2017-06-27 10:49:06 +0200

    deps: backport c4852ea from upstream V8
    Original commit message:
    
      Pull tracing related methods out of Platform
      This will allow for embedders to easily implement their own Platform
      without duplicating the tracing controller code.
    
      BUG=v8:6511
      R=fmeawad@chromium.org
    
      Cq-Include-Trybots:
      master.tryserver.chromium.linux:linux_chromium_rel_ng
      Change-Id: I7c64933d12b2cf53f0636fbc87f6ad5d22019f5c
      Reviewed-on: https://chromium-review.googlesource.com/543015
      Commit-Queue: Jochen Eisinger <jochen@chromium.org>
      Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
      Cr-Commit-Position: refs/heads/master@{#46118}
    
    PR-URL: https://github.com/nodejs/node/pull/14001
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- deps/v8/test/cctest/heap/test-incremental-marking.cc
+++ deps/v8/test/cctest/heap/test-incremental-marking.cc
@@ -71,29 +71,6 @@ class MockPlatform : public v8::Platform {
     delete task;
   }
 
-  using Platform::AddTraceEvent;
-  uint64_t AddTraceEvent(char phase, const uint8_t* categoryEnabledFlag,
-                         const char* name, const char* scope, uint64_t id,
-                         uint64_t bind_id, int numArgs, const char** argNames,
-                         const uint8_t* argTypes, const uint64_t* argValues,
-                         unsigned int flags) override {
-    return 0;
-  }
-
-  void UpdateTraceEventDuration(const uint8_t* categoryEnabledFlag,
-                                const char* name, uint64_t handle) override {}
-
-  const uint8_t* GetCategoryGroupEnabled(const char* name) override {
-    static uint8_t no = 0;
-    return &no;
-  }
-
-  const char* GetCategoryGroupName(
-      const uint8_t* categoryEnabledFlag) override {
-    static const char* dummy = "dummy";
-    return dummy;
-  }
-
  private:
   v8::Platform* platform_;
   Task* task_;
