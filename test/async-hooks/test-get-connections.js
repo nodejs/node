bbdc7c1c6d4bd7302324d3d984b727381d8d8aa2
Matt Sergeant <matt@sergeant.org>
2017-06-26 21:44:31 -0400

    [squash] Address PR requests

--- test/async-hooks/test-get-connections.js
+++ test/async-hooks/test-get-connections.js
@@ -1,14 +1,14 @@
 'use strict';
 
+const common = require('../common');
 const assert = require('assert');
 const net = require('net');
 const server = net.createServer();
 
-try {
-    server.getConnections(() => {
-        assert.ok(true, "No error thrown")
-    });
-}
-catch (e) {
-    assert.ok(false, "getConnections threw an error");
-}
+/* This test was based on an error raised by Haraka.
+   It caused server.getConnections to raise an exception.
+   
+   See: https://github.com/haraka/Haraka/pull/1951
+*/
+
+server.getConnections(common.mustCall());
