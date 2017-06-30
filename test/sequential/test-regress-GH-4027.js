34fce264361ae584e0486ea110547dc38d197fc5
Jaime Bernardo <jaime@janeasystems.com>
2017-06-30 19:35:52 +0100

    test,fs: delay unlink in test-regress-GH-4027.js
    The sequential/test-regress-GH-4027 test is flaky with an increased
    system load, failing when the watched file is unlinked before the
    first state of the watched file is retrieved.
    
    After increasing the delay before unlinking and calling setTimeout
    after watchFile, the flakiness stopped reproducing.
    
    PR-URL: https://github.com/nodejs/node/pull/14010
    Fixes: https://github.com/nodejs/node/issues/13800
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- test/sequential/test-regress-GH-4027.js
+++ test/sequential/test-regress-GH-4027.js
@@ -29,10 +29,11 @@ common.refreshTmpDir();
 
 const filename = path.join(common.tmpDir, 'watched');
 fs.writeFileSync(filename, 'quis custodiet ipsos custodes');
-setTimeout(fs.unlinkSync, 100, filename);
 
 fs.watchFile(filename, { interval: 50 }, common.mustCall(function(curr, prev) {
   assert.strictEqual(prev.nlink, 1);
   assert.strictEqual(curr.nlink, 0);
   fs.unwatchFile(filename);
 }));
+
+setTimeout(fs.unlinkSync, common.platformTimeout(300), filename);
