64d13620212fd46b1ab3e2651342d616559cd0da
Michael Dawson <michael_dawson@ca.ibm.com>
2017-06-30 16:02:01 -0400

    squash: address comments

--- test/addons-napi/test_general/test_general.c
+++ test/addons-napi/test_general/test_general.c
@@ -29,7 +29,7 @@ napi_value testGetVersion(napi_env env, napi_callback_info info) {
   uint32_t version;
   napi_value result;
   NAPI_CALL(env, napi_get_version(env, &version));
-  NAPI_CALL(env ,napi_create_number(env, version, &result));
+  NAPI_CALL(env,napi_create_number(env, version, &result));
   return result;
 }
 
@@ -95,32 +95,26 @@ napi_value testNapiTypeof(napi_env env, napi_callback_info info) {
   napi_value args[1];
   NAPI_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
 
-  napi_valuetype object_type;
-  NAPI_CALL(env, napi_typeof(env, args[0], &object_type));  
+  napi_valuetype argument_type;
+  NAPI_CALL(env, napi_typeof(env, args[0], &argument_type));  
 
   napi_value result;
-  if (object_type == napi_number) {
-    NAPI_CALL(env ,napi_create_number(env, 42, &result));
-  } else if (object_type == napi_string) {
-    NAPI_CALL(env, napi_create_string_utf8(env, "42", -1, &result));
-  } else if (object_type == napi_function) {
-    NAPI_CALL(env, napi_create_function(env,
-                                        NULL,
-                                        testNapiTypeof,
-                                        NULL,
-                                        &result));
-  } else if (object_type == napi_object) {
-    NAPI_CALL(env, napi_create_object(env, &result));
-  } else if (object_type == napi_boolean) {
-    NAPI_CALL(env, napi_get_boolean(env, true, &result));
-  } else if (object_type == napi_undefined) {
-    NAPI_CALL(env, napi_get_undefined(env, &result));
-  } else if (object_type == napi_symbol) {
-    napi_value symbol_description;
-    NAPI_CALL(env, napi_create_string_utf8(env, "NameKeySymbol", -1, &symbol_description));
-    NAPI_CALL(env, napi_create_symbol(env, symbol_description, &result));
-  } else if (object_type == napi_null) {
-    NAPI_CALL(env, napi_get_null(env, &result));
+  if (argument_type == napi_number) {
+    NAPI_CALL(env, napi_create_string_utf8(env, "number", -1, &result));
+  } else if (argument_type == napi_string) {
+    NAPI_CALL(env, napi_create_string_utf8(env, "string", -1, &result));
+  } else if (argument_type == napi_function) {
+    NAPI_CALL(env, napi_create_string_utf8(env, "function", -1, &result));
+  } else if (argument_type == napi_object) {
+    NAPI_CALL(env, napi_create_string_utf8(env, "object", -1, &result));
+  } else if (argument_type == napi_boolean) {
+    NAPI_CALL(env, napi_create_string_utf8(env, "boolean", -1, &result));
+  } else if (argument_type == napi_undefined) {
+    NAPI_CALL(env, napi_create_string_utf8(env, "undefined", -1, &result));
+  } else if (argument_type == napi_symbol) {
+    NAPI_CALL(env, napi_create_string_utf8(env, "symbol", -1, &result));
+  } else if (argument_type == napi_null) {
+    NAPI_CALL(env, napi_create_string_utf8(env, "null", -1, &result));
   }
   return result;
 }
