d288cf10ccfeb4139b9e14a4bc50a17de8d3d778
Rich Trott <rtrott@gmail.com>
2017-07-01 08:14:28 -0700

    test: skip test-fs-readdir-ucs2 if no support
    If the filesystem does not support UCS2, do not run the test.
    
    PR-URL: https://github.com/nodejs/node/pull/14029
    Fixes: https://github.com/nodejs/node/issues/14028
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>

--- test/parallel/test-fs-readdir-ucs2.js
+++ test/parallel/test-fs-readdir-ucs2.js
@@ -14,16 +14,18 @@ const root = Buffer.from(`${common.tmpDir}${path.sep}`);
 const filebuff = Buffer.from(filename, 'ucs2');
 const fullpath = Buffer.concat([root, filebuff]);
 
-fs.closeSync(fs.openSync(fullpath, 'w+'));
+try {
+  fs.closeSync(fs.openSync(fullpath, 'w+'));
+} catch (e) {
+  if (e.code === 'EINVAL')
+    common.skip('test requires filesystem that supports UCS2');
+  throw e;
+}
 
-fs.readdir(common.tmpDir, 'ucs2', (err, list) => {
+fs.readdir(common.tmpDir, 'ucs2', common.mustCall((err, list) => {
   assert.ifError(err);
   assert.strictEqual(1, list.length);
   const fn = list[0];
   assert.deepStrictEqual(filebuff, Buffer.from(fn, 'ucs2'));
   assert.strictEqual(fn, filename);
-});
-
-process.on('exit', () => {
-  fs.unlinkSync(fullpath);
-});
+}));
