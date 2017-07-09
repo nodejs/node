3e5d7ba468b71f77106c9ff902f9e710002da4d5
Andreas Madsen <amwebdk@gmail.com>
2017-07-09 13:50:59 +0200

    async_hooks: fix nested hooks mutation
    In some cases restoreTmpHooks is called too early, this causes
    active_hooks_array to change during execution of the init hooks.

---
+++ test/async-hooks/test-disable-in-init.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const async_hooks = require('async_hooks');
+const crypto = require('crypto');
+
+let nestedCall = false;
+
+const rootHook = async_hooks.createHook({
+  init: common.mustCall(function (id, type) {
+    nestedHook.disable();
+    if (!nestedCall) {
+      nestedCall = true;
+      crypto.randomBytes(1, common.mustCall());
+    }
+  }, 2)
+}).enable();
+
+const nestedHook = async_hooks.createHook({
+  init: common.mustCall(2)
+}).enable();
+
+crypto.randomBytes(1, common.mustCall());
---
+++ test/async-hooks/test-enable-in-init.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const async_hooks = require('async_hooks');
+const crypto = require('crypto');
+
+const nestedHook = async_hooks.createHook({
+  init: common.mustNotCall()
+});
+let nestedCall = false;
+
+const rootHook = async_hooks.createHook({
+  init: common.mustCall(function (id, type) {
+    nestedHook.enable();
+    if (!nestedCall) {
+      nestedCall = true;
+      crypto.randomBytes(1, common.mustCall());
+    }
+  }, 2)
+}).enable();
+
+crypto.randomBytes(1, common.mustCall());
