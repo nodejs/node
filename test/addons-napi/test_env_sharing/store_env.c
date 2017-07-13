8c10e94aec40cd9fcc1c799cda968e2a8a269a87
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-07-13 11:35:55 +0300

    n-api: re-use napi_env between modules
    Store the `napi_env` on the global object at a private key. This gives
    us one `napi_env` per context.
    
    Refs: https://github.com/nodejs/node/issues/14367
    PR-URL: https://github.com/nodejs/node/pull/14217
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>

---
+++ test/addons-napi/test_env_sharing/store_env.c
@@ -0,0 +1,12 @@
+#include <node_api.h>
+#include "../common.h"
+
+void Init(napi_env env, napi_value exports, napi_value module, void* context) {
+  napi_value external;
+  NAPI_CALL_RETURN_VOID(env,
+    napi_create_external(env, env, NULL, NULL, &external));
+  NAPI_CALL_RETURN_VOID(env,
+    napi_set_named_property(env, module, "exports", external));
+}
+
+NAPI_MODULE(store_env, Init)
