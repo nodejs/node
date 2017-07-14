b0a8a7c6baccea746da10e01bfb3dec18c0d723e
James M Snell <jasnell@gmail.com>
2017-07-14 15:05:24 -0700

    test: improvements to various http tests
    * Add common/countdown utility
    * Numerous improvements to http tests
    
    PR-URL: https://github.com/nodejs/node/pull/14315
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- test/parallel/test-http-agent-error-on-idle.js
+++ test/parallel/test-http-agent-error-on-idle.js
@@ -1,32 +1,31 @@
 'use strict';
-require('../common');
+
+const common = require('../common');
 const assert = require('assert');
 const http = require('http');
 const Agent = http.Agent;
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(common.mustCall((req, res) => {
   res.end('hello world');
-});
+}, 2));
 
-server.listen(0, function() {
-  const agent = new Agent({
-    keepAlive: true,
-  });
+server.listen(0, () => {
+  const agent = new Agent({ keepAlive: true });
 
   const requestParams = {
     host: 'localhost',
-    port: this.address().port,
+    port: server.address().port,
     agent: agent,
     path: '/'
   };
 
   const socketKey = agent.getName(requestParams);
 
-  get(function(res) {
+  http.get(requestParams, common.mustCall((res) => {
     assert.strictEqual(res.statusCode, 200);
     res.resume();
-    res.on('end', function() {
-      process.nextTick(function() {
+    res.on('end', common.mustCall(() => {
+      process.nextTick(common.mustCall(() => {
         const freeSockets = agent.freeSockets[socketKey];
         assert.strictEqual(freeSockets.length, 1,
                            `expect a free socket on ${socketKey}`);
@@ -35,14 +34,10 @@ server.listen(0, function() {
         const freeSocket = freeSockets[0];
         freeSocket.emit('error', new Error('ECONNRESET: test'));
 
-        get(done);
-      });
-    });
-  });
-
-  function get(callback) {
-    return http.get(requestParams, callback);
-  }
+        http.get(requestParams, done);
+      }));
+    }));
+  }));
 
   function done() {
     assert.strictEqual(Object.keys(agent.freeSockets).length, 0,
@@ -50,6 +45,5 @@ server.listen(0, function() {
 
     agent.destroy();
     server.close();
-    process.exit(0);
   }
 });
