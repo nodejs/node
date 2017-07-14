c56e2e7eba5b6a241d4aaa8d10bb258f0e4664e1
James M Snell <jasnell@gmail.com>
2017-07-14 15:05:24 -0700

    test: improvements to various http tests
    * Add common/countdown utility
    * Numerous improvements to http tests
    
    PR-URL: https://github.com/nodejs/node/pull/14315
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- test/parallel/test-http-agent-maxsockets.js
+++ test/parallel/test-http-agent-maxsockets.js
@@ -1,7 +1,8 @@
 'use strict';
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const http = require('http');
+const Countdown = require('../common/countdown');
 
 const agent = new http.Agent({
   keepAlive: true,
@@ -10,9 +11,9 @@ const agent = new http.Agent({
   maxFreeSockets: 2
 });
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(common.mustCall((req, res) => {
   res.end('hello world');
-});
+}, 2));
 
 function get(path, callback) {
   return http.get({
@@ -23,32 +24,25 @@ function get(path, callback) {
   }, callback);
 }
 
-let count = 0;
-function done() {
-  if (++count !== 2) {
-    return;
-  }
+const countdown = new Countdown(2, common.mustCall(() => {
   const freepool = agent.freeSockets[Object.keys(agent.freeSockets)[0]];
   assert.strictEqual(freepool.length, 2,
                      `expect keep 2 free sockets, but got ${freepool.length}`);
   agent.destroy();
   server.close();
+}));
+
+function dec() {
+  process.nextTick(() => countdown.dec());
 }
 
-server.listen(0, function() {
-  get('/1', function(res) {
-    assert.strictEqual(res.statusCode, 200);
-    res.resume();
-    res.on('end', function() {
-      process.nextTick(done);
-    });
-  });
+function onGet(res) {
+  assert.strictEqual(res.statusCode, 200);
+  res.resume();
+  res.on('end', common.mustCall(dec));
+}
 
-  get('/2', function(res) {
-    assert.strictEqual(res.statusCode, 200);
-    res.resume();
-    res.on('end', function() {
-      process.nextTick(done);
-    });
-  });
-});
+server.listen(0, common.mustCall(() => {
+  get('/1', common.mustCall(onGet));
+  get('/2', common.mustCall(onGet));
+}));
