6fd3dd20c00badd7e260a12ae7cd0d0fdb555898
rockcoder23 <rockcoder23@gmail.com>
2017-07-16 15:11:14 +0800

    test: replace concatenation with template literal
    PR-URL: https://github.com/nodejs/node/pull/14270
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: David Cai <davidcai1993@yahoo.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Evan Lucas <evanlucas@me.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- test/parallel/test-require-extensions-same-filename-as-dir-trailing-slash.js
+++ test/parallel/test-require-extensions-same-filename-as-dir-trailing-slash.js
@@ -2,9 +2,8 @@
 'use strict';
 const common = require('../common');
 const assert = require('assert');
-
-const content = require(common.fixturesDir +
-  '/json-with-directory-name-module/module-stub/one-trailing-slash/two/three.js');
+const filePath = '/json-with-directory-name-module/module-stub/one-trailing-slash/two/three.js';
+const content = require(`${common.fixturesDir}${filePath}`);
 
 assert.notStrictEqual(content.rocko, 'artischocko');
 assert.strictEqual(content, 'hello from module-stub!');
