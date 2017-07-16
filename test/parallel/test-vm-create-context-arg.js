e8170f2246717cd8171c450ba11ae04934cf7dd9
akira.xue <akira.xue@guanaitong.com>
2017-07-16 15:10:37 +0800

    test: use regluar expression in vm test
    update test/parallel/test-vm-create-context-arg.js
    in line 27 to change `TypeError` to the regular expression
    with the `/^TypeError: sandbox must be an object$/`.
    
    PR-URL: https://github.com/nodejs/node/pull/14266
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-vm-create-context-arg.js
+++ test/parallel/test-vm-create-context-arg.js
@@ -26,7 +26,7 @@ const vm = require('vm');
 
 assert.throws(function() {
   vm.createContext('string is not supported');
-}, TypeError);
+}, /^TypeError: sandbox must be an object$/);
 
 assert.doesNotThrow(function() {
   vm.createContext({ a: 1 });
