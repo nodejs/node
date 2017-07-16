438c877fc7e27b48e33f5d412a34d2526dea5842
ziyun <ziyun@meituan.com>
2017-07-16 15:49:46 +0800

    test: replace string concatenation with template
    PR-URL: https://github.com/nodejs/node/pull/14286
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-https-unix-socket-self-signed.js
+++ test/parallel/test-https-unix-socket-self-signed.js
@@ -9,8 +9,8 @@ common.refreshTmpDir();
 const fs = require('fs');
 const https = require('https');
 const options = {
-  cert: fs.readFileSync(common.fixturesDir + '/test_cert.pem'),
-  key: fs.readFileSync(common.fixturesDir + '/test_key.pem')
+  cert: fs.readFileSync(`${common.fixturesDir}/test_cert.pem`),
+  key: fs.readFileSync(`${common.fixturesDir}/test_key.pem`)
 };
 
 const server = https.createServer(options, common.mustCall((req, res) => {
