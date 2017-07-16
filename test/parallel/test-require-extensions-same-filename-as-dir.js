bc1d4c5db6c456a4dc53167f0b3fc97558ae9465
zzz <jankin.z@outlook.com>
2017-07-16 14:41:47 +0800

    test: use path.join for long path concatenation
    PR-URL: https://github.com/nodejs/node/pull/14280
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- test/parallel/test-require-extensions-same-filename-as-dir.js
+++ test/parallel/test-require-extensions-same-filename-as-dir.js
@@ -1,9 +1,17 @@
 'use strict';
 const common = require('../common');
 const assert = require('assert');
+const path = require('path');
 
-const content = require(common.fixturesDir +
-  '/json-with-directory-name-module/module-stub/one/two/three.js');
+const filePath = path.join(
+  common.fixturesDir,
+  'json-with-directory-name-module',
+  'module-stub',
+  'one',
+  'two',
+  'three.js'
+);
+const content = require(filePath);
 
 assert.notStrictEqual(content.rocko, 'artischocko');
 assert.strictEqual(content, 'hello from module-stub!');
