d217b2850efb9005819d55b697a37cbe5bd0003c
Andreas Madsen <amwebdk@gmail.com>
2017-07-17 16:47:12 -0700

    async_hooks: add trace events to async_hooks
    This will allow trace event to record timing information for all
    asynchronous operations that are observed by async_hooks.
    
    PR-URL: https://github.com/nodejs/node/pull/15538
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

---
+++ test/parallel/test-trace-events-v8.js
@@ -0,0 +1,58 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const cp = require('child_process');
+const fs = require('fs');
+
+const CODE =
+  'setTimeout(() => { for (var i = 0; i < 100000; i++) { "test" + i } }, 1)';
+const FILE_NAME = 'node_trace.1.log';
+
+common.refreshTmpDir();
+process.chdir(common.tmpDir);
+
+const proc = cp.spawn(process.execPath,
+                      [ '--trace-events-enabled',
+                        '--trace-event-categories', 'v8',
+                        '-e', CODE ]);
+
+proc.once('exit', common.mustCall(() => {
+  assert(common.fileExists(FILE_NAME));
+  fs.readFile(FILE_NAME, common.mustCall((err, data) => {
+    const traces = JSON.parse(data.toString()).traceEvents;
+    assert(traces.length > 0);
+    // V8 trace events should be generated.
+    assert(traces.some((trace) => {
+      if (trace.pid !== proc.pid)
+        return false;
+      if (trace.cat !== 'v8')
+        return false;
+      if (trace.name !== 'V8.ScriptCompiler')
+        return false;
+      return true;
+    }));
+
+    // C++ async_hooks trace events should be generated.
+    assert(!traces.some((trace) => {
+      if (trace.pid !== proc.pid)
+        return false;
+      if (trace.cat !== 'node.async_hooks')
+        return false;
+      if (trace.name !== 'TIMERWRAP')
+        return false;
+      return true;
+    }));
+
+
+    // JavaScript async_hooks trace events should be generated.
+    assert(!traces.some((trace) => {
+      if (trace.pid !== proc.pid)
+        return false;
+      if (trace.cat !== 'node.async_hooks')
+        return false;
+      if (trace.name !== 'Timeout')
+        return false;
+      return true;
+    }));
+  }));
+}));
