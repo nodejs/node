c49adbff6395654634d5c89661419696ce14352b
Rich Trott <rtrott@gmail.com>
2017-07-21 11:38:06 -0700

    tools: update to ESLint 4.3.0
    PR-URL: https://github.com/nodejs/node/pull/14417
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Teddy Katz <teddy.katz@gmail.com>
    Reviewed-By: Roman Reiss <me@silverwind.io>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- tools/eslint/node_modules/is-alphabetical/index.js
+++ tools/eslint/node_modules/is-alphabetical/index.js
@@ -1,25 +1,9 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module is-alphabetical
- * @fileoverview Check if a character is alphabetical.
- */
-
 'use strict';
 
-/* eslint-env commonjs */
-
-/* Expose. */
 module.exports = alphabetical;
 
-/**
- * Check whether the given character code, or the character
- * code at the first character, is alphabetical.
- *
- * @param {string|number} character
- * @return {boolean} - Whether `character` is alphabetical.
- */
+/* Check if the given character code, or the character
+ * code at the first character, is alphabetical. */
 function alphabetical(character) {
   var code = typeof character === 'string' ?
     character.charCodeAt(0) : character;
