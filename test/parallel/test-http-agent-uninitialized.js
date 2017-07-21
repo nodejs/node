aefbe11c851511438217f905857c49a11800820c
Trevor Norris <trev.norris@gmail.com>
2017-07-21 17:10:56 -0600

    http: check for handle before running asyncReset()
    If an uninitialized or user supplied Socket is in the freeSockets list
    of the Agent it would automatically attempt to run
    ._handle.asyncReset(), but would throw from those not existing. Guard
    against that by first checking that they exist.
    
    PR-URL: https://github.com/nodejs/node/pull/14419
    Fixes: https://github.com/nodejs/node/issues/13539
    Refs: https://github.com/nodejs/node/issues/13352
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

---
+++ test/parallel/test-http-agent-uninitialized.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const common = require('../common');
+const http = require('http');
+const net = require('net');
+
+const agent = new http.Agent({
+  keepAlive: true,
+});
+const socket = new net.Socket();
+const req = new http.ClientRequest(`http://localhost:${common.PORT}/`);
+
+const server = http.createServer(common.mustCall((req, res) => {
+  res.end();
+})).listen(common.PORT, common.mustCall(() => {
+  // Manually add the socket without a _handle.
+  agent.freeSockets[agent.getName(req)] = [socket];
+  // Now force the agent to use the socket and check that _handle exists before
+  // calling asyncReset().
+  agent.addRequest(req, {});
+  req.on('response', common.mustCall(() => {
+    server.close();
+  }));
+  req.end();
+}));
