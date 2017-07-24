4b23b429818d3fef1714cbdea4c6a1fd06410a5f
James M Snell <jasnell@gmail.com>
2017-07-24 12:08:28 -0700

    test: improve multiple vm tests
    PR-URL: https://github.com/nodejs/node/pull/14458
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>

--- test/parallel/test-vm-syntax-error-stderr.js
+++ test/parallel/test-vm-syntax-error-stderr.js
@@ -12,18 +12,14 @@ const p = child_process.spawn(process.execPath, [
   wrong_script
 ]);
 
-p.stdout.on('data', function(data) {
-  assert.fail(`Unexpected stdout data: ${data}`);
-});
+p.stdout.on('data', common.mustNotCall());
 
 let output = '';
 
-p.stderr.on('data', function(data) {
-  output += data;
-});
+p.stderr.on('data', (data) => output += data);
 
-process.on('exit', function() {
+p.stderr.on('end', common.mustCall(() => {
   assert(/BEGIN CERT/.test(output));
   assert(/^\s+\^/m.test(output));
   assert(/Invalid left-hand side expression in prefix operation/.test(output));
-});
+}));
