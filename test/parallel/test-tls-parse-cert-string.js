c75965a0adc56960fff91255cceec40d743ef768
Ben Noordhuis <info@bnoordhuis.nl>
2017-07-24 14:36:36 +0200

    tls: fix object prototype type confusion
    Use `Object.create(null)` for dictionary objects so that keys from
    certificate strings or the authorityInfoAccess field cannot conflict
    with Object.prototype properties.
    
    PR-URL: https://github.com/nodejs/node/pull/14447
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Fedor Indutny <fedor.indutny@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-tls-parse-cert-string.js
+++ test/parallel/test-tls-parse-cert-string.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-proto */
 'use strict';
 const common = require('../common');
 if (!common.hasCrypto)
@@ -11,6 +12,7 @@ const tls = require('tls');
                   'CN=ca1\nemailAddress=ry@clouds.org';
   const singlesOut = tls.parseCertString(singles);
   assert.deepStrictEqual(singlesOut, {
+    __proto__: null,
     C: 'US',
     ST: 'CA',
     L: 'SF',
@@ -26,6 +28,7 @@ const tls = require('tls');
                   'CN=*.nodejs.org';
   const doublesOut = tls.parseCertString(doubles);
   assert.deepStrictEqual(doublesOut, {
+    __proto__: null,
     OU: [ 'Domain Control Validated', 'PositiveSSL Wildcard' ],
     CN: '*.nodejs.org'
   });
@@ -34,5 +37,13 @@ const tls = require('tls');
 {
   const invalid = 'fhqwhgads';
   const invalidOut = tls.parseCertString(invalid);
-  assert.deepStrictEqual(invalidOut, {});
+  assert.deepStrictEqual(invalidOut, { __proto__: null });
+}
+
+{
+  const input = '__proto__=mostly harmless\nhasOwnProperty=not a function';
+  const expected = Object.create(null);
+  expected.__proto__ = 'mostly harmless';
+  expected.hasOwnProperty = 'not a function';
+  assert.deepStrictEqual(tls.parseCertString(input), expected);
 }
