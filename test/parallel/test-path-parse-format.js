a557622645f3a38584d891bb77c0f528b2eceb7a
Timothy Gu <timothygu99@gmail.com>
2017-07-24 08:39:02 +0800

    path: fix win32 volume-relative paths
    `path.resolve()` and `path.join()` are left alone in this commit due to
    the lack of clear semantics.
    
    PR-URL: https://github.com/nodejs/node/pull/14440
    Fixes: https://github.com/nodejs/node/issues/14405
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- test/parallel/test-path-parse-format.js
+++ test/parallel/test-path-parse-format.js
@@ -25,28 +25,33 @@ const assert = require('assert');
 const path = require('path');
 
 const winPaths = [
-  'C:\\path\\dir\\index.html',
-  'C:\\another_path\\DIR\\1\\2\\33\\\\index',
-  'another_path\\DIR with spaces\\1\\2\\33\\index',
-  '\\foo\\C:',
-  'file',
-  '.\\file',
-  'C:\\',
-  'C:',
-  '\\',
-  '',
+  // [path, root]
+  ['C:\\path\\dir\\index.html', 'C:\\'],
+  ['C:\\another_path\\DIR\\1\\2\\33\\\\index', 'C:\\'],
+  ['another_path\\DIR with spaces\\1\\2\\33\\index', ''],
+  ['\\', '\\'],
+  ['\\foo\\C:', '\\'],
+  ['file', ''],
+  ['file:stream', ''],
+  ['.\\file', ''],
+  ['C:', 'C:'],
+  ['C:.', 'C:'],
+  ['C:..', 'C:'],
+  ['C:abc', 'C:'],
+  ['C:\\', 'C:\\'],
+  ['C:\\abc', 'C:\\' ],
+  ['', ''],
 
   // unc
-  '\\\\server\\share\\file_path',
-  '\\\\server two\\shared folder\\file path.zip',
-  '\\\\teela\\admin$\\system32',
-  '\\\\?\\UNC\\server\\share'
+  ['\\\\server\\share\\file_path', '\\\\server\\share\\'],
+  ['\\\\server two\\shared folder\\file path.zip',
+   '\\\\server two\\shared folder\\'],
+  ['\\\\teela\\admin$\\system32', '\\\\teela\\admin$\\'],
+  ['\\\\?\\UNC\\server\\share', '\\\\?\\UNC\\']
 ];
 
 const winSpecialCaseParseTests = [
   ['/foo/bar', { root: '/' }],
-  ['C:', { root: 'C:', dir: 'C:', base: '' }],
-  ['C:\\', { root: 'C:\\', dir: 'C:\\', base: '' }]
 ];
 
 const winSpecialCaseFormatTests = [
@@ -60,26 +65,27 @@ const winSpecialCaseFormatTests = [
 ];
 
 const unixPaths = [
-  '/home/user/dir/file.txt',
-  '/home/user/a dir/another File.zip',
-  '/home/user/a dir//another&File.',
-  '/home/user/a$$$dir//another File.zip',
-  'user/dir/another File.zip',
-  'file',
-  '.\\file',
-  './file',
-  'C:\\foo',
-  '/',
-  '',
-  '.',
-  '..',
-  '/foo',
-  '/foo.',
-  '/foo.bar',
-  '/.',
-  '/.foo',
-  '/.foo.bar',
-  '/foo/bar.baz',
+  // [path, root]
+  ['/home/user/dir/file.txt', '/'],
+  ['/home/user/a dir/another File.zip', '/'],
+  ['/home/user/a dir//another&File.', '/'],
+  ['/home/user/a$$$dir//another File.zip', '/'],
+  ['user/dir/another File.zip', ''],
+  ['file', ''],
+  ['.\\file', ''],
+  ['./file', ''],
+  ['C:\\foo', ''],
+  ['/', '/'],
+  ['', ''],
+  ['.', ''],
+  ['..', ''],
+  ['/foo', '/'],
+  ['/foo.', '/'],
+  ['/foo.bar', '/'],
+  ['/.', '/'],
+  ['/.foo', '/'],
+  ['/.foo.bar', '/'],
+  ['/foo/bar.baz', '/']
 ];
 
 const unixSpecialCaseFormatTests = [
@@ -182,7 +188,7 @@ function checkErrors(path) {
 }
 
 function checkParseFormat(path, paths) {
-  paths.forEach(function(element) {
+  paths.forEach(function([element, root]) {
     const output = path.parse(element);
     assert.strictEqual(typeof output.root, 'string');
     assert.strictEqual(typeof output.dir, 'string');
@@ -190,6 +196,8 @@ function checkParseFormat(path, paths) {
     assert.strictEqual(typeof output.ext, 'string');
     assert.strictEqual(typeof output.name, 'string');
     assert.strictEqual(path.format(output), element);
+    assert.strictEqual(output.root, root);
+    assert(output.dir.startsWith(output.root));
     assert.strictEqual(output.dir, output.dir ? path.dirname(element) : '');
     assert.strictEqual(output.base, path.basename(element));
     assert.strictEqual(output.ext, path.extname(element));
