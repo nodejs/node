70664bf5a1914e74726cd3d17659cbba8f553f94
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-07-27 20:59:29 +0300

    n-api: add ability to remove a wrapping
    Calling napi_wrap() twice on the same object has the result of returning
    napi_invalid_arg on the second call. However, sometimes it is necessary
    to replace the native pointer associated with a given object. This new
    API allows one to remove an existing pointer, returning the object to
    its pristine, non-wrapped state.
    
    PR-URL: https://github.com/nodejs/node/pull/14658
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Fixes: https://github.com/nodejs/abi-stable-node/issues/266

--- test/addons-napi/test_general/test.js
+++ test/addons-napi/test_general/test.js
@@ -1,4 +1,5 @@
 'use strict';
+// Flags: --expose-gc
 
 const common = require('../../common');
 const test_general = require(`./build/${common.buildType}/test_general`);
@@ -56,10 +57,37 @@ assert.strictEqual(release, process.release.name);
 // for null
 assert.strictEqual(test_general.testNapiTypeof(null), 'null');
 
-const x = {};
+// Ensure that garbage collecting an object with a wrapped native item results
+// in the finalize callback being called.
+let w = {};
+test_general.wrap(w, []);
+w = null;
+global.gc();
+assert.strictEqual(test_general.derefItemWasCalled(), true,
+                   'deref_item() was called upon garbage collecting a ' +
+                   'wrapped object');
 
 // Assert that wrapping twice fails.
+const x = {};
 test_general.wrap(x, 25);
 assert.throws(function() {
   test_general.wrap(x, 'Blah');
 }, Error);
+
+// Ensure that wrapping, removing the wrap, and then wrapping again works.
+const y = {};
+test_general.wrap(y, -12);
+test_general.removeWrap(y);
+assert.doesNotThrow(function() {
+  test_general.wrap(y, 're-wrap!');
+}, Error, 'Wrapping twice succeeds if a remove_wrap() separates the instances');
+
+// Ensure that removing a wrap and garbage collecting does not fire the
+// finalize callback.
+let z = {};
+test_general.testFinalizeWrap(z);
+test_general.removeWrap(z);
+z = null;
+global.gc();
+assert.strictEqual(test_general.finalizeWasCalled(), false,
+                   'finalize callback was not called upon garbage collection');
