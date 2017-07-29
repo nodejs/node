435ab004e27475a15c728419258b6dd2367bd23c
Rich Trott <rtrott@gmail.com>
2017-07-29 09:04:00 -0700

    test: refactor test-vm-new-script-new-context
    * block scope test cases
    * clean up global leaks in individual test cases
    * enable global variable leak checking
    * remove console.error() statements
    
    PR-URL: https://github.com/nodejs/node/pull/14536
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-vm-new-script-new-context.js
+++ test/parallel/test-vm-new-script-new-context.js
@@ -1,69 +1,86 @@
 'use strict';
-const common = require('../common');
+require('../common');
+
 const assert = require('assert');
+
 const Script = require('vm').Script;
 
-common.globalCheck = false;
+{
+  const script = new Script('\'passed\';');
+  const result1 = script.runInNewContext();
+  const result2 = script.runInNewContext();
+  assert.strictEqual('passed', result1);
+  assert.strictEqual('passed', result2);
+}
 
-console.error('run a string');
-let script = new Script('\'passed\';');
-console.error('script created');
-const result1 = script.runInNewContext();
-const result2 = script.runInNewContext();
-assert.strictEqual('passed', result1);
-assert.strictEqual('passed', result2);
+{
+  const script = new Script('throw new Error(\'test\');');
+  assert.throws(function() {
+    script.runInNewContext();
+  }, /^Error: test$/);
+}
 
-console.error('thrown error');
-script = new Script('throw new Error(\'test\');');
-assert.throws(function() {
+{
+  const script = new Script('foo.bar = 5;');
+  assert.throws(function() {
+    script.runInNewContext();
+  }, /^ReferenceError: foo is not defined$/);
+}
+
+{
+  global.hello = 5;
+  const script = new Script('hello = 2');
   script.runInNewContext();
-}, /^Error: test$/);
+  assert.strictEqual(5, global.hello);
 
+  // cleanup
+  delete global.hello;
+}
 
-console.error('undefined reference');
-script = new Script('foo.bar = 5;');
-assert.throws(function() {
-  script.runInNewContext();
-}, /^ReferenceError: foo is not defined$/);
+{
+  global.code = 'foo = 1;' +
+                'bar = 2;' +
+                'if (baz !== 3) throw new Error(\'test fail\');';
+  global.foo = 2;
+  global.obj = { foo: 0, baz: 3 };
+  const script = new Script(global.code);
+  /* eslint-disable no-unused-vars */
+  const baz = script.runInNewContext(global.obj);
+  /* eslint-enable no-unused-vars */
+  assert.strictEqual(1, global.obj.foo);
+  assert.strictEqual(2, global.obj.bar);
+  assert.strictEqual(2, global.foo);
 
+  //cleanup
+  delete global.code;
+  delete global.foo;
+  delete global.obj;
+}
 
-global.hello = 5;
-script = new Script('hello = 2');
-script.runInNewContext();
-assert.strictEqual(5, global.hello);
+{
+  const script = new Script('f()');
+  function changeFoo() { global.foo = 100; }
+  script.runInNewContext({ f: changeFoo });
+  assert.strictEqual(global.foo, 100);
 
+  // cleanup
+  delete global.foo;
+}
 
-console.error('pass values in and out');
-global.code = 'foo = 1;' +
-              'bar = 2;' +
-              'if (baz !== 3) throw new Error(\'test fail\');';
-global.foo = 2;
-global.obj = { foo: 0, baz: 3 };
-script = new Script(global.code);
-/* eslint-disable no-unused-vars */
-const baz = script.runInNewContext(global.obj);
-/* eslint-enable no-unused-vars */
-assert.strictEqual(1, global.obj.foo);
-assert.strictEqual(2, global.obj.bar);
-assert.strictEqual(2, global.foo);
+{
+  const script = new Script('f.a = 2');
+  const f = { a: 1 };
+  script.runInNewContext({ f: f });
+  assert.strictEqual(f.a, 2);
 
-console.error('call a function by reference');
-script = new Script('f()');
-function changeFoo() { global.foo = 100; }
-script.runInNewContext({ f: changeFoo });
-assert.strictEqual(global.foo, 100);
+  assert.throws(function() {
+    script.runInNewContext();
+  }, /^ReferenceError: f is not defined$/);
+}
 
-console.error('modify an object by reference');
-script = new Script('f.a = 2');
-const f = { a: 1 };
-script.runInNewContext({ f: f });
-assert.strictEqual(f.a, 2);
-
-assert.throws(function() {
-  script.runInNewContext();
-}, /^ReferenceError: f is not defined$/);
-
-console.error('invalid this');
-assert.throws(function() {
-  script.runInNewContext.call('\'hello\';');
-}, /^TypeError: this\.runInContext is not a function$/);
+{
+  const script = new Script('');
+  assert.throws(function() {
+    script.runInNewContext.call('\'hello\';');
+  }, /^TypeError: this\.runInContext is not a function$/);
+}
