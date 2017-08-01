3b2daff1809dff449a8495d12f16a8b32e1f5363
Lance Ball <lball@redhat.com>
2017-08-01 16:10:06 -0400

    fix: address unnecessary indirection in repl test

--- test/parallel/test-repl-deprecations.js
+++ test/parallel/test-repl-deprecations.js
@@ -3,11 +3,7 @@ const common = require('../common');
 const assert = require('assert');
 const repl = require('repl');
 
-test();
-
-function test() {
-  testParseREPLKeyword();
-}
+testParseREPLKeyword();
 
 function testParseREPLKeyword() {
   const server = repl.start({ prompt: '> ' });
