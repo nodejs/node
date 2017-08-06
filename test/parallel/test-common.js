262bcb6ce1cdce76ac4d845f1eb82d4a05f54c8f
XadillaX <admin@xcoder.in>
2017-08-06 14:16:05 +0800

    test: fix hijackStdout behavior in console
    `console.log` and some other function will swallow the exception in
    `stdout.write`. So an asynchronous exception is needed, or
    `common.hijackStdout` won't detect some exception.
    
    Refs: https://github.com/nodejs/node/blob/v8.2.1/lib/console.js#L87
    
    PR-URL: https://github.com/nodejs/node/pull/14647
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Yuta Hiroto <hello@about-hiroppy.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

--- test/parallel/test-common.js
+++ test/parallel/test-common.js
@@ -108,3 +108,26 @@ const HIJACK_TEST_ARRAY = [ 'foo\n', 'bar\n', 'baz\n' ];
   common[`restoreStd${txt}`]();
   assert.strictEqual(originalWrite, stream.write);
 });
+
+// hijackStderr and hijackStdout again
+// for console
+[[ 'err', 'error' ], [ 'out', 'log' ]].forEach(([ type, method ]) => {
+  common[`hijackStd${type}`](common.mustCall(function(data) {
+    assert.strictEqual(data, 'test\n');
+
+    // throw an error
+    throw new Error(`console ${type} error`);
+  }));
+
+  console[method]('test');
+  common[`restoreStd${type}`]();
+});
+
+let uncaughtTimes = 0;
+process.on('uncaughtException', common.mustCallAtLeast(function(e) {
+  assert.strictEqual(uncaughtTimes < 2, true);
+  assert.strictEqual(e instanceof Error, true);
+  assert.strictEqual(
+    e.message,
+    `console ${([ 'err', 'out' ])[uncaughtTimes++]} error`);
+}, 2));
