4967e935660a41724a3dff4012c13f41ad92fdfa
XadillaX <admin@xcoder.in>
2017-08-10 14:47:14 +0800

    dns: fix `resolve` failed starts without network
    Fix the bug that you start process without network at first, but it
    connected lately, `dns.resolve` will stay failed with ECONNREFUSED
    because c-ares servers fallback to 127.0.0.1 at the very beginning.
    
    If c-ares servers "127.0.0.1" is detected and its not set by user self,
    and last query is not OK, recreating `ares_channel` operation will be
    triggered to reload servers.
    
    Fixes: https://github.com/nodejs/node/issues/1644
    Backport-PR-URL: https://github.com/nodejs/node/pull/14434
    PR-URL: https://github.com/nodejs/node/pull/13076
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/env.h
+++ src/env.h
@@ -437,6 +437,10 @@ class Environment {
   inline uv_timer_t* cares_timer_handle();
   inline ares_channel cares_channel();
   inline ares_channel* cares_channel_ptr();
+  inline bool cares_query_last_ok();
+  inline void set_cares_query_last_ok(bool ok);
+  inline bool cares_is_servers_default();
+  inline void set_cares_is_servers_default(bool is_default);
   inline node_ares_task_list* cares_task_list();
 
   inline bool using_domains() const;
@@ -555,6 +559,8 @@ class Environment {
   const uint64_t timer_base_;
   uv_timer_t cares_timer_handle_;
   ares_channel cares_channel_;
+  bool cares_query_last_ok_;
+  bool cares_is_servers_default_;
   node_ares_task_list cares_task_list_;
   bool using_domains_;
   bool printed_error_;
