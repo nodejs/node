21e2413b2b661ac63e97f029b24fdc5c9a2793e7
Yury Popov <github@phoenix.dj>
2017-08-12 19:06:35 +0300

    tls: multiple PFX in createSecureContext
    Add support for multiple PFX files in tls.createSecureContext.
    Also added support for object-style PFX pass.
    
    Fixes: https://github.com/nodejs/node/issues/14756

---
+++ test/parallel/test-tls-multi-pfx.js
@@ -0,0 +1,50 @@
+'use strict';
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const assert = require('assert');
+const tls = require('tls');
+const fs = require('fs');
+
+const options = {
+  pfx: [
+    {
+      buffer: fs.readFileSync(`${common.fixturesDir}/keys/agent1-pfx.pem`),
+      passphrase: 'sample'
+    },
+    fs.readFileSync(`${common.fixturesDir}/keys/ec-pfx.pem`)
+  ]
+};
+
+const ciphers = [];
+
+const server = tls.createServer(options, function(conn) {
+  conn.end('ok');
+}).listen(0, function() {
+  const ecdsa = tls.connect(this.address().port, {
+    ciphers: 'ECDHE-ECDSA-AES256-GCM-SHA384',
+    rejectUnauthorized: false
+  }, function() {
+    ciphers.push(ecdsa.getCipher());
+    const rsa = tls.connect(server.address().port, {
+      ciphers: 'ECDHE-RSA-AES256-GCM-SHA384',
+      rejectUnauthorized: false
+    }, function() {
+      ciphers.push(rsa.getCipher());
+      ecdsa.end();
+      rsa.end();
+      server.close();
+    });
+  });
+});
+
+process.on('exit', function() {
+  assert.deepStrictEqual(ciphers, [{
+    name: 'ECDHE-ECDSA-AES256-GCM-SHA384',
+    version: 'TLSv1/SSLv3'
+  }, {
+    name: 'ECDHE-RSA-AES256-GCM-SHA384',
+    version: 'TLSv1/SSLv3'
+  }]);
+});
