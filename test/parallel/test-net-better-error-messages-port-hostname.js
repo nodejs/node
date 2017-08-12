58b9495c428c9a7e4856b4662f2da090523800ac
icarter09 <icarter1391@gmail.com>
2017-08-11 23:04:41 -0400

    test: use reserved invalid hostname for tests
    PR-URL: https://github.com/nodejs/node/pull/14781
    Refs: https://tools.ietf.org/html/rfc2606#section-2
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- test/parallel/test-net-better-error-messages-port-hostname.js
+++ test/parallel/test-net-better-error-messages-port-hostname.js
@@ -4,12 +4,12 @@ const net = require('net');
 const assert = require('assert');
 
 // Using port 0 as hostname used is already invalid.
-const c = net.createConnection(0, '***');
+const c = net.createConnection(0, 'this.hostname.is.invalid');
 
 c.on('connect', common.mustNotCall());
 
 c.on('error', common.mustCall(function(e) {
   assert.strictEqual(e.code, 'ENOTFOUND');
   assert.strictEqual(e.port, 0);
-  assert.strictEqual(e.hostname, '***');
+  assert.strictEqual(e.hostname, 'this.hostname.is.invalid');
 }));
