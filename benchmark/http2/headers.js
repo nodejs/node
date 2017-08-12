23bd23b38eef4421f72e417ad6d3df55b7a9d940
Anna Henningsen <anna@addaleax.net>
2017-08-12 16:49:31 +0200

    http2: minor refactor of passing headers to JS
    - Make `ExternalString::New` return a `MaybeLocal`. Failing is
      left up to the caller.
    - Allow creating internalized strings for short header names
      to reduce memory consumption and increase performance.
    - Use persistent storage for statically allocated header names.
    
    PR-URL: https://github.com/nodejs/node/pull/14808
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>

--- benchmark/http2/headers.js
+++ benchmark/http2/headers.js
@@ -5,7 +5,7 @@ const PORT = common.PORT;
 
 var bench = common.createBenchmark(main, {
   n: [1e3],
-  nheaders: [100, 1000],
+  nheaders: [0, 10, 100, 1000],
 }, { flags: ['--expose-http2', '--no-warnings'] });
 
 function main(conf) {
@@ -14,7 +14,16 @@ function main(conf) {
   const http2 = require('http2');
   const server = http2.createServer();
 
-  const headersObject = { ':path': '/' };
+  const headersObject = {
+    ':path': '/',
+    ':scheme': 'http',
+    'accept-encoding': 'gzip, deflate',
+    'accept-language': 'en',
+    'content-type': 'text/plain',
+    'referer': 'https://example.org/',
+    'user-agent': 'SuperBenchmarker 3000'
+  };
+
   for (var i = 0; i < nheaders; i++) {
     headersObject[`foo${i}`] = `some header value ${i}`;
   }
