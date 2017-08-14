3f44989b24d6ef3cce42f6f20c46810990e1e1d8
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2017-08-14 12:12:49 +0100

    test: preserve env in test cases
    Allows env vars to be passed through to child processes. This is needed
    for things like NODE_TEST_DIR or LD_LIBRARY_PATH if testing the shared
    library.
    
    PR-URL: https://github.com/nodejs/node/pull/14822
    Refs: https://github.com/nodejs/node/pull/13390
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-tls-env-extra-ca.js
+++ test/parallel/test-tls-env-extra-ca.js
@@ -32,11 +32,11 @@ const server = tls.createServer(options, common.mustCall(function(s) {
   s.end('bye');
   server.close();
 })).listen(0, common.mustCall(function() {
-  const env = {
+  const env = Object.assign({}, process.env, {
     CHILD: 'yes',
     PORT: this.address().port,
     NODE_EXTRA_CA_CERTS: fixtures.path('keys', 'ca1-cert.pem')
-  };
+  });
 
   fork(__filename, {env: env}).on('exit', common.mustCall(function(status) {
     assert.strictEqual(status, 0, 'client did not succeed in connecting');
