df23963e821a1a944ff3b60b29096111efeb0fb8
Jesse Rosenberger <git@jro.cc>
2017-08-14 21:53:18 +0300

    Revert "deps: backport e093a04, 09db540 from upstream V8"
    This reverts commit 2d07fd71ee391d099e2f19e85455be6a718c27c9, with the
    exception of the `V8_PATCH_LEVEL` which will continue to increase.
    
    This commit was intended to be a backport of v8's e093a04 and 09db540,
    but it failed to consider the reversion of e093a04 in 5f5a328.
    
    PR-URL: https://github.com/nodejs/node/pull/14829
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- deps/v8/test/cctest/test-weakmaps.cc
+++ deps/v8/test/cctest/test-weakmaps.cc
@@ -123,7 +123,7 @@ TEST(Weakness) {
   heap->CollectAllGarbage(false);
   CHECK_EQ(1, NumberOfWeakCalls);
   CHECK_EQ(0, ObjectHashTable::cast(weakmap->table())->NumberOfElements());
-  CHECK_EQ(0,
+  CHECK_EQ(2,
            ObjectHashTable::cast(weakmap->table())->NumberOfDeletedElements());
 }
 
