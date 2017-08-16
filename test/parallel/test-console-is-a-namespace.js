490374fd75c01196b4c2654e904d8f0bf5535797
Rich Trott <rtrott@gmail.com>
2017-08-15 17:07:06 -0700

    squash: update test-console-is-a-namespace
    Update the test from the most recent WPT contents. Copyedit some
    existing comments.

--- test/parallel/test-console-is-a-namespace.js
+++ test/parallel/test-console-is-a-namespace.js
@@ -1,19 +1,16 @@
 'use strict';
 
-// https://heycam.github.io/webidl/#es-namespaces
-// https://console.spec.whatwg.org/#console-namespace
-// https://github.com/w3c/web-platform-tests/blob/40e451c/console/console-is-a-namespace.any.js
+require('../common');
 
-const common = require('../common');
 const assert = require('assert');
-const { test, assert_equals, assert_true, assert_false } = common.WPT;
+const { test, assert_equals, assert_true, assert_false } =
+  require('../common/wpt');
 
 assert.doesNotThrow(() => {
   global.console = global.console;
 });
 
-// Tests above are not from WPT.
-// Tests below are from WPT.
+const self = global;
 
 /* eslint-disable */
 /* The following tests are copied from */
@@ -21,36 +18,32 @@ assert.doesNotThrow(() => {
    https://github.com/w3c/web-platform-tests/blob/40e451c/console/console-is-a-namespace.any.js
    License: http://www.w3.org/Consortium/Legal/2008/04-testsuite-copyright.html
 */
-/* Do not modify. */
+
+// https://heycam.github.io/webidl/#es-namespaces
+// https://console.spec.whatwg.org/#console-namespace
 
 test(() => {
-  assert_true(global.hasOwnProperty('console'));
-}, 'console exists on the global object');
+  assert_true(self.hasOwnProperty("console"));
+}, "console exists on the global object");
 
 test(() => {
-  assert_true(global.hasOwnProperty('console'));
-}, 'console exists on the global object');
+  const propDesc = Object.getOwnPropertyDescriptor(self, "console");
+  assert_equals(propDesc.writable, true, "must be writable");
+  assert_equals(propDesc.enumerable, false, "must not be enumerable");
+  assert_equals(propDesc.configurable, true, "must be configurable");
+  assert_equals(propDesc.value, console, "must have the right value");
+}, "console has the right property descriptors");
 
 test(() => {
-  const propDesc = Object.getOwnPropertyDescriptor(global, 'console');
-  assert_equals(propDesc.writable, true, 'must be writable');
-  assert_equals(propDesc.enumerable, false, 'must not be enumerable');
-  assert_equals(propDesc.configurable, true, 'must be configurable');
-  assert_equals(propDesc.value, console, 'must have the right value');
-}, 'console has the right property descriptors');
+  assert_false("Console" in self);
+}, "Console (uppercase, as if it were an interface) must not exist");
 
-test(() => {
-  assert_false('Console' in global);
-}, 'Console (uppercase, as if it were an interface) must not exist');
 
-test(() => {
-  const prototype1 = Object.getPrototypeOf(console);
-  const prototype2 = Object.getPrototypeOf(prototype1);
-  // assert_equals(Object.getOwnPropertyNames(prototype1).length, 0,
-  //                'The [[Prototype]] must have no properties');
-  assert_equals(prototype2, Object.prototype,
-                'The [[Prototype]]\'s [[Prototype]] must be Object Prototype');
+// test(() => {
+//   const prototype1 = Object.getPrototypeOf(console);
+//   const prototype2 = Object.getPrototypeOf(prototype1);
 
-}, 'The prototype chain must be correct');
-
+//   assert_equals(Object.getOwnPropertyNames(prototype1).length, 0, "The [[Prototype]] must have no properties");
+//   assert_equals(prototype2, Object.prototype, "The [[Prototype]]'s [[Prototype]] must be %ObjectPrototype%");
+// }, "The prototype chain must be correct");
 /* eslint-enable */
