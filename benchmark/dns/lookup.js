841abf4c0721c838a12cb16275f353432984c98c
Ian Perkins <ian@ianperkins.io>
2017-08-18 12:09:57 +0100

    benchmark: fix issues in dns benchmark
    The benchmark script for dns contained functions with args declared
    but never used. This fix removes those arguments from the function
    signatures.
    
    No test existed for the dns benchmark so one was added to the
    parallel suite.
    To improve performance the tests are limited to 1 invocation to a
    single endpoint.
    
    PR-URL: https://github.com/nodejs/node/pull/14936
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- benchmark/dns/lookup.js
+++ benchmark/dns/lookup.js
@@ -5,20 +5,20 @@ const lookup = require('dns').lookup;
 
 const bench = common.createBenchmark(main, {
   name: ['', '127.0.0.1', '::1'],
-  all: [true, false],
+  all: ['true', 'false'],
   n: [5e6]
 });
 
 function main(conf) {
   const name = conf.name;
   const n = +conf.n;
-  const all = !!conf.all;
+  const all = conf.all === 'true' ? true : false;
   var i = 0;
 
   if (all) {
     const opts = { all: true };
     bench.start();
-    (function cb(err, results) {
+    (function cb() {
       if (i++ === n) {
         bench.end(n);
         return;
@@ -27,7 +27,7 @@ function main(conf) {
     })();
   } else {
     bench.start();
-    (function cb(err, result) {
+    (function cb() {
       if (i++ === n) {
         bench.end(n);
         return;
