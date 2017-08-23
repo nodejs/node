f2607b38522b3ba51ab24ac5b03af398b95cfd7d
Yury Popov <github@phoenix.dj>
2017-08-23 21:58:15 +0300

    Use mustCall for test callbacks

--- test/parallel/test-tls-multi-pfx.js
+++ test/parallel/test-tls-multi-pfx.js
@@ -25,18 +25,18 @@ const server = tls.createServer(options, function(conn) {
   const ecdsa = tls.connect(this.address().port, {
     ciphers: 'ECDHE-ECDSA-AES256-GCM-SHA384',
     rejectUnauthorized: false
-  }, function() {
+  }, common.mustCall(function() {
     ciphers.push(ecdsa.getCipher());
     const rsa = tls.connect(server.address().port, {
       ciphers: 'ECDHE-RSA-AES256-GCM-SHA384',
       rejectUnauthorized: false
-    }, function() {
+    }, common.mustCall(function() {
       ciphers.push(rsa.getCipher());
       ecdsa.end();
       rsa.end();
       server.close();
-    });
-  });
+    }));
+  }));
 });
 
 process.on('exit', function() {
