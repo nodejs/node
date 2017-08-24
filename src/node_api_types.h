6d094566921d56c731ca3720b60fa8ee2718fcfc
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-08-24 13:33:26 +0300

    n-api: implement promise
    Promise is implemented as a pair of objects. `napi_create_promise()`
    returns both a JavaScript promise and a newly allocated "deferred" in
    its out-params. The deferred is linked to the promise such that the
    deferred can be passed to `napi_resolve_deferred()` or
    `napi_reject_deferred()` to reject/resolve the promise.
    
    `napi_is_promise()` can be used to check if a `napi_value` is a native
    promise - that is, a promise created by the underlying engine, rather
    than a pure JS implementation of a promise.
    
    PR-URL: https://github.com/nodejs/node/pull/14365
    Fixes: https://github.com/nodejs/abi-stable-node/issues/242
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>

--- src/node_api_types.h
+++ src/node_api_types.h
@@ -17,6 +17,7 @@ typedef struct napi_handle_scope__ *napi_handle_scope;
 typedef struct napi_escapable_handle_scope__ *napi_escapable_handle_scope;
 typedef struct napi_callback_info__ *napi_callback_info;
 typedef struct napi_async_work__ *napi_async_work;
+typedef struct napi_deferred__ *napi_deferred;
 
 typedef enum {
   napi_default = 0,
