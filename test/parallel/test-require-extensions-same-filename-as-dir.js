9a43f2071d0b6b755623f63b6115e7e8b680e224
Bradley Farias <bradley.meck@gmail.com>
2017-08-24 09:59:52 -0500

    module: check file ext before dir as documented
    The documented resolution algorithm was regressed and started to
    search for package.json files prior to searching for file extensions
    when searching for a specifier. Oddly, it did not search for index
    files at same time it searched for package.json. This reverts that
    regression and restores the documented behavior of searching for
    file extensions prior to searching directories.
    
    Fixes: https://github.com/nodejs/node/issues/14990
    PR-URL: https://github.com/nodejs/node/pull/15015
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-require-extensions-same-filename-as-dir.js
+++ test/parallel/test-require-extensions-same-filename-as-dir.js
@@ -28,5 +28,5 @@ const content = require(fixtures.path('json-with-directory-name-module',
                                       'module-stub', 'one', 'two',
                                       'three.js'));
 
-assert.notStrictEqual(content.rocko, 'artischocko');
-assert.strictEqual(content, 'hello from module-stub!');
+assert.strictEqual(content.rocko, 'artischocko');
+assert.notStrictEqual(content, 'hello from module-stub!');
