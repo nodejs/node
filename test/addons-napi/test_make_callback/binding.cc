7c73bcc32dd6cd4fba1be26cb53bbf1ec6c1d49c
Jason Ginchereau <jasongin@microsoft.com>
2017-08-26 15:44:43 -0700

    n-api: Context for custom async operations
     - Add napi_async_context opaque pointer type.
       (If needed, we could later add APIs for getting the async IDs
       out of this context.)
     - Add napi_async_init() and napi_async_destroy() APIs.
     - Add async_context parameter to napi_make_callback().
     - Add code and checks to test_make_callback to validate async context
       APIs by checking async hooks are called with correct context.
     - Update API documentation.
    
    PR-URL: https://github.com/nodejs/node/pull/15189
    Fixes: https://github.com/nodejs/node/issues/13254
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/addons-napi/test_make_callback/binding.cc
+++ test/addons-napi/test_make_callback/binding.cc
@@ -24,13 +24,21 @@ napi_value MakeCallback(napi_env env, napi_callback_info info) {
 
   NAPI_CALL(env, napi_typeof(env, func, &func_type));
 
+  napi_value resource_name;
+  NAPI_CALL(env, napi_create_string_utf8(env, "test", -1, &resource_name));
+
+  napi_async_context context;
+  NAPI_CALL(env, napi_async_init(env, func, resource_name, &context));
+
   napi_value result;
   if (func_type == napi_function) {
-    NAPI_CALL(env,
-      napi_make_callback(env, recv, func, argv.size(), argv.data(), &result));
+    NAPI_CALL(env, napi_make_callback(
+        env, context, recv, func, argv.size(), argv.data(), &result));
   } else {
     NAPI_ASSERT(env, false, "Unexpected argument type");
   }
+
+  NAPI_CALL(env, napi_async_destroy(env, context));
 
   return result;
 }
