45fade14c289007120159934ef79ceda4f4ed4e8
Anatoli Papirovski <apapirovski@mac.com>
2017-08-30 21:49:41 -0400

    http2: adjust error types, test coverage
    Change error types emitted from request and validatePriorityOptions
    to be TypeError rather than the incorrect RangeError. Add tests
    to confirm that all errors are thrown as expected (weight, parent,
    exclusive, silent, endStream and getTrailers). Add test for method
    CONNECT throwing error on missing :authority or superfluous :scheme
    and :path.
    
    PR-URL: https://github.com/nodejs/node/pull/15109
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

---
+++ test/parallel/test-http2-client-request-options-errors.js
@@ -0,0 +1,63 @@
+// Flags: --expose-http2
+'use strict';
+
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+const assert = require('assert');
+const http2 = require('http2');
+
+// Check if correct errors are emitted when wrong type of data is passed
+// to certain options of ClientHttp2Session request method
+
+const optionsToTest = {
+  endStream: 'boolean',
+  getTrailers: 'function',
+  weight: 'number',
+  parent: 'number',
+  exclusive: 'boolean',
+  silent: 'boolean'
+};
+
+const types = {
+  boolean: true,
+  function: () => {},
+  number: 1,
+  object: {},
+  array: [],
+  null: null,
+  symbol: Symbol('test')
+};
+
+const server = http2.createServer(common.mustNotCall());
+
+server.listen(0, common.mustCall(() => {
+  const port = server.address().port;
+  const client = http2.connect(`http://localhost:${port}`);
+
+  Object.keys(optionsToTest).forEach((option) => {
+    Object.keys(types).forEach((type) => {
+      if (type === optionsToTest[option]) {
+        return;
+      }
+
+      assert.throws(
+        () => client.request({
+          ':method': 'CONNECT',
+          ':authority': `localhost:${port}`
+        }, {
+          [option]: types[type]
+        }),
+        common.expectsError({
+          type: TypeError,
+          code: 'ERR_INVALID_OPT_VALUE',
+          message: `The value "${String(types[type])}" is invalid ` +
+                   `for option "${option}"`
+        })
+      );
+    });
+  });
+
+  server.close();
+  client.destroy();
+}));
