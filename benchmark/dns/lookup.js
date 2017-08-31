45d5836cdf0bb417379a70d21684eed87ca249dc
Ian Perkins <ian@ianperkins.io>
2017-08-31 19:31:42 +0100

    benchmark: refactor to remove duplicate code
    The dns benchmark script contained an if-then-else branch which
    executed almost the same code in both cases, apart from the number
    of args passed to the core dns lookup function.

--- benchmark/dns/lookup.js
+++ benchmark/dns/lookup.js
@@ -6,33 +6,26 @@ const lookup = require('dns').lookup;
 const bench = common.createBenchmark(main, {
   name: ['', '127.0.0.1', '::1'],
   all: ['true', 'false'],
-  n: [5e6]
+  n: [5e4],
 });
 
 function main(conf) {
-  const name = conf.name;
-  const n = +conf.n;
-  const all = conf.all === 'true' ? true : false;
-  var i = 0;
-
-  if (all) {
-    const opts = { all: true };
-    bench.start();
-    (function cb() {
-      if (i++ === n) {
-        bench.end(n);
-        return;
-      }
-      lookup(name, opts, cb);
-    })();
-  } else {
-    bench.start();
-    (function cb() {
-      if (i++ === n) {
-        bench.end(n);
-        return;
-      }
-      lookup(name, cb);
-    })();
+  const argv = [conf.name];
+  if (conf.all === 'true') {
+    argv.push({ all: true });
   }
+
+  const n = +conf.n;
+  let i = 0;
+
+  bench.start();
+
+  (function cb() {
+    if (i++ === n) {
+      bench.end(n);
+      return;
+    }
+    lookup(...argv, cb);
+  })();
+
 }
