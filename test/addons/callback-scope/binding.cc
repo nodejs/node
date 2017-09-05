4a020253dcc5473c266915f824625206761d9e1e
Anna Henningsen <anna@addaleax.net>
2017-09-05 19:06:37 +0200

    test: add regression test for 5691
    With `CallbackScope`, this has become possible to do properly.
    
    Fixes: https://github.com/nodejs/node/issues/5691
    PR-URL: https://github.com/nodejs/node/pull/14697
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/addons/callback-scope/binding.cc
+++ test/addons/callback-scope/binding.cc
@@ -1,5 +1,6 @@
 #include "node.h"
 #include "v8.h"
+#include "uv.h"
 
 #include <assert.h>
 #include <vector>
@@ -30,8 +31,39 @@ void RunInCallbackScope(const v8::FunctionCallbackInfo<v8::Value>& args) {
     args.GetReturnValue().Set(ret.ToLocalChecked());
 }
 
+static v8::Persistent<v8::Promise::Resolver> persistent;
+
+static void Callback(uv_work_t* req, int ignored) {
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope scope(isolate);
+  node::CallbackScope callback_scope(isolate, v8::Object::New(isolate), {0, 0});
+
+  v8::Local<v8::Promise::Resolver> local =
+      v8::Local<v8::Promise::Resolver>::New(isolate, persistent);
+  local->Resolve(v8::Undefined(isolate));
+  delete req;
+}
+
+static void TestResolveAsync(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+
+  if (persistent.IsEmpty()) {
+    persistent.Reset(isolate, v8::Promise::Resolver::New(isolate));
+
+    uv_work_t* req = new uv_work_t;
+
+    uv_queue_work(uv_default_loop(), req, [](uv_work_t*) {}, Callback);
+  }
+
+  v8::Local<v8::Promise::Resolver> local =
+      v8::Local<v8::Promise::Resolver>::New(isolate, persistent);
+
+  args.GetReturnValue().Set(local->GetPromise());
+}
+
 void Initialize(v8::Local<v8::Object> exports) {
   NODE_SET_METHOD(exports, "runInCallbackScope", RunInCallbackScope);
+  NODE_SET_METHOD(exports, "testResolveAsync", TestResolveAsync);
 }
 
 }  // namespace
