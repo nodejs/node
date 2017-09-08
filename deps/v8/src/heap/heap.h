da1b01e2332bcc94a64ac9c9fcac63ac1f13b744
Anna Henningsen <anna@addaleax.net>
2017-09-08 15:34:45 +0200

    deps: cherry-pick b6158eb6befae from V8 upstream
    Original commit message:
    
        [heap] Move gc callbacks from List to std::vector
    
        Bug: v8:6333
        Change-Id: I4434c6cc59f886f1e37dfd315a3ad5fee28d3f63
        Reviewed-on: https://chromium-review.googlesource.com/634907
        Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
        Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
        Cr-Commit-Position: refs/heads/master@{#47601}
    
    PR-URL: https://github.com/nodejs/node/pull/15391
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- deps/v8/src/heap/heap.h
+++ deps/v8/src/heap/heap.h
@@ -1585,9 +1585,8 @@ class Heap {
                    bool pass_isolate)
         : callback(callback), gc_type(gc_type), pass_isolate(pass_isolate) {}
 
-    bool operator==(const GCCallbackPair& other) const {
-      return other.callback == callback;
-    }
+    bool operator==(const GCCallbackPair& other) const;
+    GCCallbackPair& operator=(const GCCallbackPair& other);
 
     v8::Isolate::GCCallback callback;
     GCType gc_type;
@@ -2255,8 +2254,8 @@ class Heap {
 
   Object* encountered_transition_arrays_;
 
-  List<GCCallbackPair> gc_epilogue_callbacks_;
-  List<GCCallbackPair> gc_prologue_callbacks_;
+  std::vector<GCCallbackPair> gc_epilogue_callbacks_;
+  std::vector<GCCallbackPair> gc_prologue_callbacks_;
 
   GetExternallyAllocatedMemoryInBytesCallback external_memory_callback_;
 
