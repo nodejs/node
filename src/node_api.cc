63e86053beef1ca5755f50ff1ff356924f853ac8
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-09-09 12:15:14 +0300

    n-api: stop creating references to primitives
    The binding testing napi_wrap() creates references to primitives passed
    into the binding in its second parameter. This is unnecessary and not
    at all the point of the test. Additionally, creating persistent
    references to primitive values may not be supported by all VMs, since
    primitives are best persisted in their native form.
    
    Instead, the point of the test is to make sure that the finalize
    callback gets called when it should get called, that it gets called
    with the correct pointer, and that it does not get called when it
    should not get called. Creating persistent references is not necessary
    for verifying this.
    
    PR-URL: https://github.com/nodejs/node/pull/15289
    Reviewed-By: Jason Ginchereau <jasongin@microsoft.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Re: https://github.com/nodejs/node-chakracore/issues/380

--- src/node_api.cc
+++ src/node_api.cc
@@ -2461,8 +2461,14 @@ napi_status napi_create_reference(napi_env env,
   CHECK_ARG(env, value);
   CHECK_ARG(env, result);
 
-  v8impl::Reference* reference = v8impl::Reference::New(
-      env, v8impl::V8LocalValueFromJsValue(value), initial_refcount, false);
+  v8::Local<v8::Value> v8_value = v8impl::V8LocalValueFromJsValue(value);
+
+  if (!(v8_value->IsObject() || v8_value->IsFunction())) {
+    return napi_set_last_error(env, napi_object_expected);
+  }
+
+  v8impl::Reference* reference =
+      v8impl::Reference::New(env, v8_value, initial_refcount, false);
 
   *result = reinterpret_cast<napi_ref>(reference);
   return napi_clear_last_error(env);
