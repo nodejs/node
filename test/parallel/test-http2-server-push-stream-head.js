ba9012d4bcd751c38a5e3f1ef07673640c7eb856
Anatoli Papirovski <apapirovski@mac.com>
2017-09-13 09:34:14 -0400

    http2: add tests for push stream error handling
    Add tests that cover errors for wrong arguments, as well as
    tests for error codes from nghttp2. Fix pushStream to emit
    NGHTTP2_ERR_STREAM_ID_NOT_AVAILABLE on session rather than
    stream.
    
    PR-URL: https://github.com/nodejs/node/pull/15281
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- test/parallel/test-http2-server-push-stream-head.js
+++ test/parallel/test-http2-server-push-stream-head.js
@@ -7,21 +7,19 @@ if (!common.hasCrypto)
 const assert = require('assert');
 const http2 = require('http2');
 
+// Check that pushStream handles method HEAD correctly
+// - stream should end immediately (no body)
+
 const server = http2.createServer();
 server.on('stream', common.mustCall((stream, headers) => {
   const port = server.address().port;
   if (headers[':path'] === '/') {
     stream.pushStream({
       ':scheme': 'http',
-      ':path': '/foobar',
+      ':method': 'HEAD',
       ':authority': `localhost:${port}`,
     }, common.mustCall((push, headers) => {
-      push.respond({
-        'content-type': 'text/html',
-        ':status': 200,
-        'x-push-data': 'pushed by server',
-      });
-      push.end('pushed by server data');
+      assert.strictEqual(push._writableState.ended, true);
       stream.end('test');
     }));
   }
@@ -36,16 +34,12 @@ server.listen(0, common.mustCall(() => {
   const headers = { ':path': '/' };
   const client = http2.connect(`http://localhost:${port}`);
   const req = client.request(headers);
+  req.setEncoding('utf8');
 
   client.on('stream', common.mustCall((stream, headers) => {
     assert.strictEqual(headers[':scheme'], 'http');
-    assert.strictEqual(headers[':path'], '/foobar');
+    assert.strictEqual(headers[':path'], '/');
     assert.strictEqual(headers[':authority'], `localhost:${port}`);
-    stream.on('push', common.mustCall((headers) => {
-      assert.strictEqual(headers[':status'], 200);
-      assert.strictEqual(headers['content-type'], 'text/html');
-      assert.strictEqual(headers['x-push-data'], 'pushed by server');
-    }));
   }));
 
   let data = '';
