62b8410c644f567969681be67df351e4d0d7715c
Refael Ackermann <refack@gmail.com>
2017-09-18 17:12:22 -0400

    use ‭c0000005 errorlevel

--- test/abort/test-abort-uncaught-exception.js
+++ test/abort/test-abort-uncaught-exception.js
@@ -21,7 +21,7 @@ function run(flags, signals) {
   child.on('exit', common.mustCall(function(code, sig) {
     if (common.isWindows) {
       if (signals)
-        assert.strictEqual(code, 3);
+        assert.strictEqual(code, 0xC0000005);
       else
         assert.strictEqual(code, 1);
     } else {
