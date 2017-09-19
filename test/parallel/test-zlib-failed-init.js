9e796d4575c38c6846f8285192a55a9d4c8b13f9
Drew Folta <drew@folta.net>
2017-09-19 13:37:13 -0700

    test: older shared zlib doesnt throw on create
    When using compiling using `./configure --shared-zlib` older versions
    of the shared zlib might not through as expected by the
    test-zlib-failed-init test.
    
    Refs: https://github.com/nodejs/node/pull/13697

--- test/parallel/test-zlib-failed-init.js
+++ test/parallel/test-zlib-failed-init.js
@@ -1,9 +1,14 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 
 const assert = require('assert');
 const zlib = require('zlib');
+
+if (process.config.variables.node_shared_zlib &&
+    /^1\.2\.[0-8]$/.test(process.versions.zlib)) {
+  common.skip("older versions of shared zlib don't throw on create");
+}
 
 // For raw deflate encoding, requests for 256-byte windows are rejected as
 // invalid by zlib.
