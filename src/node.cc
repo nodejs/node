bcb324c3ffd74147041cf892a0b2840aa340c248
Anna Henningsen <anna@addaleax.net>
2017-09-20 14:43:19 +0200

    src: add can_call_into_js flag
    This prevents calls back into JS from the shutdown phase.
    
    Many thanks for Stephen Belanger for reviewing the original version of
    this commit in the Ayo.js project.
    
    Refs: https://github.com/ayojs/ayo/pull/82
    PR-URL: https://github.com/nodejs/node/pull/19377
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node.cc
+++ src/node.cc
@@ -954,6 +954,11 @@ InternalCallbackScope::InternalCallbackScope(Environment* env,
     CHECK(!object.IsEmpty());
   }
 
+  if (!env->can_call_into_js()) {
+    failed_ = true;
+    return;
+  }
+
   HandleScope handle_scope(env->isolate());
   // If you hit this assertion, you forgot to enter the v8::Context first.
   CHECK_EQ(Environment::GetCurrent(env->isolate()), env);
@@ -997,6 +1002,7 @@ void InternalCallbackScope::Close() {
 
   Environment::TickInfo* tick_info = env_->tick_info();
 
+  if (!env_->can_call_into_js()) return;
   if (!tick_info->has_scheduled()) {
     env_->isolate()->RunMicrotasks();
   }
@@ -1013,6 +1019,8 @@ void InternalCallbackScope::Close() {
   }
 
   Local<Object> process = env_->process_object();
+
+  if (!env_->can_call_into_js()) return;
 
   if (env_->tick_callback_function()->Call(process, 0, nullptr).IsEmpty()) {
     env_->tick_info()->set_has_thrown(true);
@@ -4552,6 +4560,7 @@ inline int Start(Isolate* isolate, IsolateData* isolate_data,
 
   WaitForInspectorDisconnect(&env);
 
+  env.set_can_call_into_js(false);
   env.RunCleanup();
   RunAtExit(&env);
 
