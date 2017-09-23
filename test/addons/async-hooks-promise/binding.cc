bf7c61593c66e7dc0ac1104080e45b8b18d11312
cPhost <phostphp@gmail.com>
2017-09-23 13:13:14 -0400

    Added line "using namespace v8;"
    Make code consistent and fixed indentation

--- test/addons/async-hooks-promise/binding.cc
+++ test/addons/async-hooks-promise/binding.cc
@@ -1,43 +1,36 @@
 #include <node.h>
 #include <v8.h>
 
+using namsespace v8;
+
 namespace {
 
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::NewStringType;
-using v8::Object;
-using v8::Promise;
-using v8::String;
-using v8::Value;
+  static void ThrowError(Isolate* isolate, const char* err_msg) {
+    Local<String> str = String::NewFromOneByte(
+        isolate,
+        reinterpret_cast<const uint8_t*>(err_msg),
+        NewStringType::kNormal).ToLocalChecked();
+    isolate->ThrowException(str);
+  }
 
-static void ThrowError(Isolate* isolate, const char* err_msg) {
-  Local<String> str = String::NewFromOneByte(
-      isolate,
-      reinterpret_cast<const uint8_t*>(err_msg),
-      NewStringType::kNormal).ToLocalChecked();
-  isolate->ThrowException(str);
-}
+  static void GetPromiseField(const FunctionCallbackInfo<Value>& args) {
+    auto isolate = args.GetIsolate();
 
-static void GetPromiseField(const FunctionCallbackInfo<Value>& args) {
-  auto isolate = args.GetIsolate();
+    if (!args[0]->IsPromise())
+      return ThrowError(isolate, "arg is not an Promise");
 
-  if (!args[0]->IsPromise())
-    return ThrowError(isolate, "arg is not an Promise");
+    auto p = args[0].As<Promise>();
+    if (p->InternalFieldCount() < 1)
+      return ThrowError(isolate, "Promise has no internal field");
 
-  auto p = args[0].As<Promise>();
-  if (p->InternalFieldCount() < 1)
-    return ThrowError(isolate, "Promise has no internal field");
+    auto l = p->GetInternalField(0);
+    args.GetReturnValue().Set(l);
+  }
 
-  auto l = p->GetInternalField(0);
-  args.GetReturnValue().Set(l);
-}
+  inline void Initialize(v8::Local<v8::Object> binding) {
+    NODE_SET_METHOD(binding, "getPromiseField", GetPromiseField);
+  }
 
-inline void Initialize(v8::Local<v8::Object> binding) {
-  NODE_SET_METHOD(binding, "getPromiseField", GetPromiseField);
-}
-
-NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+  NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
 
 }  // anonymous namespace
