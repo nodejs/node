f9f6e1172766812fc68c893996fd4af0edca7f33
cPhost <phostphp@gmail.com>
2017-09-23 13:35:06 -0400

    Added line "using namespace v8;"

--- test/addons/make-callback/binding.cc
+++ test/addons/make-callback/binding.cc
@@ -4,35 +4,36 @@
 #include <assert.h>
 #include <vector>
 
+using namespace v8;
 namespace {
 
-void MakeCallback(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  assert(args[0]->IsObject());
-  assert(args[1]->IsFunction() || args[1]->IsString());
-  auto isolate = args.GetIsolate();
-  auto recv = args[0].As<v8::Object>();
-  std::vector<v8::Local<v8::Value>> argv;
-  for (size_t n = 2; n < static_cast<size_t>(args.Length()); n += 1) {
-    argv.push_back(args[n]);
+  void MakeCallback(const FunctionCallbackInfo<Value>& args) {
+    assert(args[0]->IsObject());
+    assert(args[1]->IsFunction() || args[1]->IsString());
+    auto isolate = args.GetIsolate();
+    auto recv = args[0].As<Object>();
+    std::vector<Local<Value>> argv;
+    for (size_t n = 2; n < static_cast<size_t>(args.Length()); n += 1) {
+      argv.push_back(args[n]);
+    }
+    Local<Value> result;
+    if (args[1]->IsFunction()) {
+      auto method = args[1].As<Function>();
+      result =
+          node::MakeCallback(isolate, recv, method, argv.size(), argv.data());
+    } else if (args[1]->IsString()) {
+      auto method = args[1].As<String>();
+      result =
+          node::MakeCallback(isolate, recv, method, argv.size(), argv.data());
+    } else {
+      assert(0 && "unreachable");
+    }
+    args.GetReturnValue().Set(result);
   }
-  v8::Local<v8::Value> result;
-  if (args[1]->IsFunction()) {
-    auto method = args[1].As<v8::Function>();
-    result =
-        node::MakeCallback(isolate, recv, method, argv.size(), argv.data());
-  } else if (args[1]->IsString()) {
-    auto method = args[1].As<v8::String>();
-    result =
-        node::MakeCallback(isolate, recv, method, argv.size(), argv.data());
-  } else {
-    assert(0 && "unreachable");
+  
+  void Initialize(Local<Object> exports) {
+    NODE_SET_METHOD(exports, "makeCallback", MakeCallback);
   }
-  args.GetReturnValue().Set(result);
-}
-
-void Initialize(v8::Local<v8::Object> exports) {
-  NODE_SET_METHOD(exports, "makeCallback", MakeCallback);
-}
 
 }  // namespace
 
