9d2cb6a9769af8fa2a8f81303e8f7c6f4e06a62f
Mattias Holmlund <mattias.holmlund@netinsight.net>
2017-09-25 20:34:26 +0200

    tls: use canonicalIP from cares_wrap

--- test/parallel/test-tls-canonical-ip.js
+++ test/parallel/test-tls-canonical-ip.js
@@ -1,4 +1,3 @@
-// Flags: --expose-internals
 'use strict';
 require('../common');
 
@@ -7,24 +6,33 @@ require('../common');
 // of a TLS certificate
 
 const assert = require('assert');
-const tls = require('internal/tls');
+const canonicalIP = process.binding('cares_wrap').canonicalIP;
 
-assert.strictEqual(tls.canonicalIp('127.0.0.1'), '127.0.0.1');
-assert.strictEqual(tls.canonicalIp('010.001.0.1'), '10.1.0.1');
-assert.strictEqual(tls.canonicalIp('::1'), '0:0:0:0:0:0:0:1');
-assert.strictEqual(tls.canonicalIp('fe80::1'), 'fe80:0:0:0:0:0:0:1');
-assert.strictEqual(tls.canonicalIp('fe80::'), 'fe80:0:0:0:0:0:0:0');
+assert.strictEqual(canonicalIP('127.0.0.1'), '127.0.0.1');
+assert.strictEqual(canonicalIP('10.1.0.1'), '10.1.0.1');
+assert.strictEqual(canonicalIP('::1'), '::1');
+assert.strictEqual(canonicalIP('fe80:0:0:0:0:0:0:1'), 'fe80::1');
+assert.strictEqual(canonicalIP('fe80:0:0:0:0:0:0:0'), 'fe80::');
 assert.strictEqual(
-  tls.canonicalIp('fe80::0000:0010:0001'),
-  'fe80:0:0:0:0:0:10:1');
+  canonicalIP('fe80::0000:0010:0001'),
+  'fe80::10:1');
 assert.strictEqual(
-  tls.canonicalIp('0001:2222:3333:4444:5555:6666:7777:0088'),
+  canonicalIP('0001:2222:3333:4444:5555:6666:7777:0088'),
   '1:2222:3333:4444:5555:6666:7777:88');
 
 assert.strictEqual(
-  tls.canonicalIp('0001:2222:3333:4444:5555:6666::'),
-  '1:2222:3333:4444:5555:6666:0:0');
+  canonicalIP('0001:2222:3333:4444:5555:6666::'),
+  '1:2222:3333:4444:5555:6666::');
 
 assert.strictEqual(
-  tls.canonicalIp('a002:B12:00Ba:4444:5555:6666::'),
-  'a002:b12:ba:4444:5555:6666:0:0');
+  canonicalIP('a002:B12:00Ba:4444:5555:6666:0:0'),
+  'a002:b12:ba:4444:5555:6666::');
+
+// IPv4 address represented in IPv6
+assert.strictEqual(
+  canonicalIP('0:0:0:0:0:ffff:c0a8:101'),
+  '::ffff:192.168.1.1');
+
+  assert.strictEqual(
+    canonicalIP('::ffff:192.168.1.1'),
+    '::ffff:192.168.1.1');
