f1f3377f2323441b8eb564d04401660902fbfe7b
Bryan English <bryan@bryanenglish.com>
2017-09-28 11:57:36 -0700

    test: fix flaky test-crypto-classes.js
    On non-FIPS, we can instantiate DiffieHellman with 256 instead of 1024.
    This should be quite a bit faster, and therefore prevent the timeouts.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/16245
    PR-URL: https://github.com/nodejs/node/pull/15662
    Fixes: https://github.com/nodejs/node/issues/15655
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-crypto-classes.js
+++ test/parallel/test-crypto-classes.js
@@ -25,6 +25,7 @@ const TEST_CASES = {
 if (!common.hasFipsCrypto) {
   TEST_CASES.Cipher = ['aes192', 'secret'];
   TEST_CASES.Decipher = ['aes192', 'secret'];
+  TEST_CASES.DiffieHellman = [256];
 }
 
 function entries(obj) {
