0bd2fd562777ceb56574f5b2fb13524edda2149b
Rich Trott <rtrott@gmail.com>
2017-09-29 17:06:28 -0700

    errors: make properties mutable
    Userland code can break if it depends on a mutable `code` property for
    errors. Allow users to change the `code` property but do not propagate
    changes to the error `name`.
    
    Additionally, make `message` and `name` consistent with `Error` object
    (non-enumerable). Test that `console.log()` and `.toString()` calls on
    internal `Error` objects with mutated properties have analogous results
    with the standard ECMAScript `Error` objects.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/16078
    PR-URL: https://github.com/nodejs/node/pull/15694
    Fixes: https://github.com/nodejs/node/issues/15658
    Reviewed-By: Evan Lucas <evanlucas@me.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/parallel/test-internal-errors.js
+++ test/parallel/test-internal-errors.js
@@ -1,9 +1,9 @@
 // Flags: --expose-internals
 'use strict';
-
 const common = require('../common');
-const errors = require('internal/errors');
+
 const assert = require('assert');
+const errors = require('internal/errors');
 
 function invalidKey(key) {
   return new RegExp(`^An invalid error message key was used: ${key}\\.$`);
@@ -235,3 +235,45 @@ assert.throws(
     code: 'ERR_ASSERTION',
     message: /^At least one arg needs to be specified$/
   }));
+
+
+// Test that `code` property is mutable and that changing it does not change the
+// name.
+{
+  const myError = new errors.Error('ERR_MISSING_ARGS', ['name']);
+  assert.strictEqual(myError.code, 'ERR_MISSING_ARGS');
+  const initialName = myError.name;
+  myError.code = 'FHQWHGADS';
+  assert.strictEqual(myError.code, 'FHQWHGADS');
+  assert.strictEqual(myError.name, initialName);
+  assert.ok(myError.name.includes('ERR_MISSING_ARGS'));
+  assert.ok(!myError.name.includes('FHQWHGADS'));
+}
+
+// Test that `name` and `message` are mutable and that changing them alters
+// `toString()` but not `console.log()` results, which is the behavior of
+// `Error` objects in the browser.
+{
+  function test(prop) {
+    let initialConsoleLog = '';
+    common.hijackStdout((data) => { initialConsoleLog += data; });
+    const myError = new errors.Error('ERR_MISSING_ARGS', ['name']);
+    const initialToString = myError.toString();
+    console.log(myError);
+    assert.notStrictEqual(initialConsoleLog, '');
+
+    common.restoreStdout();
+
+    let subsequentConsoleLog = '';
+    common.hijackStdout((data) => { subsequentConsoleLog += data; });
+    myError[prop] = 'Fhqwhgads';
+    assert.notStrictEqual(myError.toString(), initialToString);
+    console.log(myError);
+    assert.strictEqual(subsequentConsoleLog, initialConsoleLog);
+
+    common.restoreStdout();
+  }
+
+  test('name');
+  test('message');
+}
