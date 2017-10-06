bcdec587c23675ba1f5fbd5270f1d01e0ecd234a
Joel Dart <jdart@remember.com>
2017-10-06 12:05:11 -0700

    test: use correct method + remove unused fs

--- test/parallel/test-tls-disable-renegotiation.js
+++ test/parallel/test-tls-disable-renegotiation.js
@@ -2,7 +2,6 @@
 const common = require('../common');
 const assert = require('assert');
 const fixtures = require('../common/fixtures');
-const fs = require('fs');
 
 // Tests that calling disableRenegotiation on a TLSSocket stops renegotiation.
 
