1a884cd9e80f7f533fbe20ac62350f394c41c963
Peter <ppaugh@chariotsolutions.com>
2017-10-06 10:58:33 -0700

    test: common.fixturesDir ->  common.fixtures
    Update test-https-agent-session-reuse.js
    
    PR-URL: https://github.com/nodejs/node/pull/16028
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/parallel/test-https-agent-session-reuse.js
+++ test/parallel/test-https-agent-session-reuse.js
@@ -7,15 +7,14 @@ if (!common.hasCrypto)
 
 const https = require('https');
 const crypto = require('crypto');
-
-const fs = require('fs');
+const fixtures = require('../common/fixtures');
 
 const options = {
-  key: fs.readFileSync(`${common.fixturesDir}/keys/agent1-key.pem`),
-  cert: fs.readFileSync(`${common.fixturesDir}/keys/agent1-cert.pem`)
+  key: fixtures.readKey('agent1-key.pem'),
+  cert: fixtures.readKey('agent1-cert.pem')
 };
 
-const ca = fs.readFileSync(`${common.fixturesDir}/keys/ca1-cert.pem`);
+const ca = fixtures.readKey('ca1-cert.pem');
 
 const clientSessions = {};
 let serverRequests = 0;
