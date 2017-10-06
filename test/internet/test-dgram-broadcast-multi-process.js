edf5c17b288c62949c15b21f7f83139a10798771
Nikki St Onge <nstonge@gasbuddy.com>
2017-10-06 12:04:00 -0600

    test: improve assert message in internet test
    PR-URL: https://github.com/nodejs/node/pull/15998
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/internet/test-dgram-broadcast-multi-process.js
+++ test/internet/test-dgram-broadcast-multi-process.js
@@ -129,11 +129,7 @@ if (process.argv[2] !== 'child') {
                             worker.pid,
                             count);
 
-              assert.strictEqual(
-                count,
-                messages.length,
-                'A worker received an invalid multicast message'
-              );
+              assert.strictEqual(count, messages.length);
             });
 
             clearTimeout(timer);
