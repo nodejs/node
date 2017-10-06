2bf182dc46f9da7c9ed98d9f98898f815da0d2f6
Greg Matthews <greg.j.matthews@gmail.com>
2017-10-06 09:52:28 -0700

    test: replace fixturesDir with fixtures module
    PR-URL: https://github.com/nodejs/node/pull/15932
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-https-eof-for-eom.js
+++ test/parallel/test-https-eof-for-eom.js
@@ -35,11 +35,11 @@ if (!common.hasCrypto)
 const assert = require('assert');
 const https = require('https');
 const tls = require('tls');
-const fs = require('fs');
+const fixtures = require('../common/fixtures');
 
 const options = {
-  key: fs.readFileSync(`${common.fixturesDir}/keys/agent1-key.pem`),
-  cert: fs.readFileSync(`${common.fixturesDir}/keys/agent1-cert.pem`)
+  key: fixtures.readKey('agent1-key.pem'),
+  cert: fixtures.readKey('agent1-cert.pem')
 };
 
 
