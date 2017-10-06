d88ec3347d87d71f895cca001aa155a5f6dec6db
ekulnivek <ekulnivek@hotmail.com>
2017-10-06 09:41:16 -0700

    http2: replace common.fixturesDir with common.fixtures.fixturesDir

--- test/parallel/test-http2-respond-file-304.js
+++ test/parallel/test-http2-respond-file-304.js
@@ -3,6 +3,7 @@
 const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
+const fixtures = require('../common/fixtures');
 const http2 = require('http2');
 const assert = require('assert');
 const path = require('path');
@@ -12,7 +13,7 @@ const {
   HTTP2_HEADER_STATUS
 } = http2.constants;
 
-const fname = path.resolve(common.fixturesDir, 'elipses.txt');
+const fname = path.resolve(fixtures.fixturesDir, 'elipses.txt');
 
 const server = http2.createServer();
 server.on('stream', (stream) => {
