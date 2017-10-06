fecd1c8c0545fb45c19c6e3456fcc2f923affda6
tabulatedreams <sung.hwang@ubc.ca>
2017-10-06 12:25:05 -0700

    test: replace fixturesDir with fixtures module
    PR-URL: https://github.com/nodejs/node/pull/16036
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-repl-syntax-error-stack.js
+++ test/parallel/test-repl-syntax-error-stack.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const common = require('../common');
+const fixtures = require('../common/fixtures');
 const assert = require('assert');
-const path = require('path');
 const repl = require('repl');
 let found = false;
 
@@ -17,7 +17,7 @@ common.ArrayStream.prototype.write = function(output) {
 
 const putIn = new common.ArrayStream();
 repl.start('', putIn);
-let file = path.join(common.fixturesDir, 'syntax', 'bad_syntax');
+let file = fixtures.path('syntax', 'bad_syntax');
 
 if (common.isWindows)
   file = file.replace(/\\/g, '\\\\');
