a14b447bbbfcded8779d1681a405e824a18589e6
Pavel Pomerantsev <pomerantsevp@gmail.com>
2017-10-06 09:50:01 -0700

    test: improve an error message
    PR-URL: https://github.com/nodejs/node/pull/15921
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-cluster-master-kill.js
+++ test/parallel/test-cluster-master-kill.js
@@ -79,7 +79,8 @@ if (cluster.isWorker) {
   }));
 
   process.once('exit', () => {
-    assert.strictEqual(typeof pid, 'number', 'did not get worker pid info');
+    assert.strictEqual(typeof pid, 'number',
+                       `got ${pid} instead of a worker pid`);
     assert.strictEqual(alive, false, 'worker was alive after master died');
   });
 
