4a758af48ba9b6028bb534127d553c5d7fd20b1e
Shaun Sweet <shaun@shaunsweet.com>
2017-10-06 11:17:02 -0700

    test: use common.fixtures module
    PR-URL: https://github.com/nodejs/node/pull/15992
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-eval-require.js
+++ test/parallel/test-eval-require.js
@@ -21,11 +21,12 @@
 
 'use strict';
 const common = require('../common');
+const { fixturesDir } = require('../common/fixtures');
 const assert = require('assert');
 const spawn = require('child_process').spawn;
 
 const options = {
-  cwd: common.fixturesDir
+  cwd: fixturesDir
 };
 const child = spawn(process.execPath, ['-e', 'require("foo")'], options);
 child.on('exit', common.mustCall((code) => {
