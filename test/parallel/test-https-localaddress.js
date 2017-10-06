b9c8fd8338be88c074a91bd01a1101ba828d1fd4
Charles T Wall III <CharlesTWall3@gmail.com>
2017-10-06 12:38:04 -0400

    test: use fixtures in test-https-localaddress.js
    PR-URL: https://github.com/nodejs/node/pull/15811
    Reviewed-By: Ryan Graham <r.m.graham@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>

--- test/parallel/test-https-localaddress.js
+++ test/parallel/test-https-localaddress.js
@@ -27,13 +27,13 @@ if (!common.hasCrypto)
 if (!common.hasMultiLocalhost())
   common.skip('platform-specific test.');
 
-const fs = require('fs');
+const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const https = require('https');
 
 const options = {
-  key: fs.readFileSync(`${common.fixturesDir}/keys/agent1-key.pem`),
-  cert: fs.readFileSync(`${common.fixturesDir}/keys/agent1-cert.pem`)
+  key: fixtures.readKey('agent1-key.pem'),
+  cert: fixtures.readKey('agent1-cert.pem')
 };
 
 const server = https.createServer(options, function(req, res) {
