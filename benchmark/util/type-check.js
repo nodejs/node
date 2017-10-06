50fe1a840915a22517ffd2c480558409dbfd6677
Sarah Meyer <sarahsaltrick@gmail.com>
2017-10-06 12:58:55 -0700

    tools, benchmark: test util benchmark
    Create a minimal test for the util benchmark files.
    
    PR-URL: https://github.com/nodejs/node/pull/16050
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>

--- benchmark/util/type-check.js
+++ benchmark/util/type-check.js
@@ -29,16 +29,19 @@ const bench = common.createBenchmark(main, {
   type: Object.keys(args),
   version: ['native', 'js'],
   argument: ['true', 'false-primitive', 'false-object'],
-  millions: ['5']
+  n: [5e6]
 }, {
   flags: ['--expose-internals']
 });
 
 function main(conf) {
+  // For testing, if supplied with an empty type, default to ArrayBufferView.
+  conf.type = conf.type || 'ArrayBufferView';
+
   const util = process.binding('util');
   const types = require('internal/util/types');
 
-  const n = (+conf.millions * 1e6) | 0;
+  const n = (+conf.n) | 0;
   const func = { native: util, js: types }[conf.version][`is${conf.type}`];
   const arg = args[conf.type][conf.argument];
 
