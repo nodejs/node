b43fed41a0eaeefc2ced04273612f6d7e70a7ff9
Brant Barger <brant.barger@gmail.com>
2017-10-06 10:34:33 -0700

    test: replace literal with template string
    PR-URL: https://github.com/nodejs/node/pull/15957
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-cluster-server-restart-rr.js
+++ test/parallel/test-cluster-server-restart-rr.js
@@ -10,15 +10,31 @@ if (cluster.isMaster) {
   worker1.on('listening', common.mustCall(() => {
     const worker2 = cluster.fork();
     worker2.on('exit', (code, signal) => {
-      assert.strictEqual(code, 0, 'worker2 did not exit normally');
-      assert.strictEqual(signal, null, 'worker2 did not exit normally');
+      assert.strictEqual(
+        code,
+        0,
+        `worker${worker2.id} did not exit normally. Exit with code: ${code}`
+      );
+      assert.strictEqual(
+        signal,
+        null,
+        `worker${worker2.id} did not exit normally. Exit with signal: ${signal}`
+      );
       worker1.disconnect();
     });
   }));
 
   worker1.on('exit', common.mustCall((code, signal) => {
-    assert.strictEqual(code, 0, 'worker1 did not exit normally');
-    assert.strictEqual(signal, null, 'worker1 did not exit normally');
+    assert.strictEqual(
+      code,
+      0,
+      `worker${worker1.id} did not exit normally. Exit with code: ${code}`
+    );
+    assert.strictEqual(
+      signal,
+      null,
+      `worker${worker1.id} did not exit normally. Exit with code: ${signal}`
+    );
   }));
 } else {
   const net = require('net');
