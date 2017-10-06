c0bba73ac2263b7997484537bbe9f0d30fdeaf85
Donovan Buck <donovan@donovan.bz>
2017-10-06 11:03:41 -0700

    test: use common/fixtures in tls-connect-no-host
    PR-URL: https://github.com/nodejs/node/pull/15986
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-tls-connect-no-host.js
+++ test/parallel/test-tls-connect-no-host.js
@@ -1,5 +1,6 @@
 'use strict';
 const common = require('../common');
+const fixtures = require('../common/fixtures');
 
 if (!common.hasCrypto)
   common.skip('missing crypto');
@@ -7,11 +8,9 @@ if (!common.hasCrypto)
 const tls = require('tls');
 
 const assert = require('assert');
-const fs = require('fs');
-const path = require('path');
 
-const cert = fs.readFileSync(path.join(common.fixturesDir, 'test_cert.pem'));
-const key = fs.readFileSync(path.join(common.fixturesDir, 'test_key.pem'));
+const cert = fixtures.readSync('test_cert.pem');
+const key = fixtures.readSync('test_key.pem');
 
 // https://github.com/nodejs/node/issues/1489
 // tls.connect(options) with no options.host should accept a cert with
