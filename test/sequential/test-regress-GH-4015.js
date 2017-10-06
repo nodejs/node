1e0a55529faa6774f13408975d9b75fa0e6b7f8e
Christian Murphy <Christian.Murphy.42@gmail.com>
2017-10-06 09:50:09 -0700

    test: replaces fixturesDir with fixtures methods
    PR-URL: https://github.com/nodejs/node/pull/15817
    Reviewed-By: Ryan Graham <r.m.graham@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>

--- test/sequential/test-regress-GH-4015.js
+++ test/sequential/test-regress-GH-4015.js
@@ -20,12 +20,13 @@
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 'use strict';
-const common = require('../common');
+require('../common');
+const { fixturesDir } = require('../common/fixtures');
 const assert = require('assert');
-const exec = require('child_process').exec;
+const { exec } = require('child_process');
 
 const cmd =
-  `"${process.execPath}" "${common.fixturesDir}/test-regress-GH-4015.js"`;
+  `"${process.execPath}" "${fixturesDir}/test-regress-GH-4015.js"`;
 
 exec(cmd, function(err, stdout, stderr) {
   assert(/RangeError: Maximum call stack size exceeded/.test(stderr));
