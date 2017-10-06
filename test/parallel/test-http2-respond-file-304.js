6b5433dcd2f518643d67a16a5f89ad4e845e4902
ekulnivek <ekulnivek@hotmail.com>
2017-10-06 09:41:16 -0700

    http2: replace fixturesDir with common.fixtures
    PR-URL: https://github.com/nodejs/node/pull/15839
    Reviewed-By: Ryan Graham <r.m.graham@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-http2-respond-file-304.js
+++ test/parallel/test-http2-respond-file-304.js
@@ -3,16 +3,16 @@
 const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
+const fixtures = require('../common/fixtures');
 const http2 = require('http2');
 const assert = require('assert');
-const path = require('path');
 
 const {
   HTTP2_HEADER_CONTENT_TYPE,
   HTTP2_HEADER_STATUS
 } = http2.constants;
 
-const fname = path.resolve(common.fixturesDir, 'elipses.txt');
+const fname = fixtures.path('elipses.txt');
 
 const server = http2.createServer();
 server.on('stream', (stream) => {
