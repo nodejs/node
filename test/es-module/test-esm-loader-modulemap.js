8ec3e59700936e61cfae2ed8d433300175b01022
Christopher Sidebottom <chris@damouse.co.uk>
2017-10-06 12:11:55 -0700

    add test for ModuleMap set with ModuleJob but bad url

--- test/es-module/test-esm-loader-modulemap.js
+++ test/es-module/test-esm-loader-modulemap.js
@@ -4,10 +4,18 @@
 // This tests ensures that the type checking of ModuleMap throws errors appropriately
 
 const assert = require('assert');
+const { URL } = require('url');
 
+const Loader = require('internal/loader/Loader');
 const ModuleMap = require('internal/loader/ModuleMap');
+const ModuleJob = require('internal/loader/ModuleJob');
+const { createDynamicModule } = require('internal/loader/ModuleWrap');
 
+const stubModuleUrl = new URL('file://tmp/test');
+const stubModule = createDynamicModule(['default'], stubModuleUrl);
+const loader = new Loader();
 const moduleMap = new ModuleMap();
+const moduleJob = new ModuleJob(loader, stubModule.module);
 
 assert.throws(() => {
   moduleMap.get(1);
@@ -25,4 +33,7 @@ assert.doesNotThrow(() => {
   moduleMap.has('somestring');
 });
 
+assert.throws(() => {
+  moduleMap.set(1, moduleJob);
+}, 'TypeError [ERR_INVALID_ARG_TYPE]: The "url" argument must be of type string');
 
