35a1a9e21e8b53b0cbe5616e900596ca581534b0
Stefania Sharp <stefaniasharp@gmail.com>
2017-10-06 11:09:42 -0700

    test: expand error message
    PR-URL: https://github.com/nodejs/node/pull/15991
    Reviewed-By: Yosuke Furukawa <yosuke.furukawa@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-cluster-worker-isdead.js
+++ test/parallel/test-cluster-worker-isdead.js
@@ -5,13 +5,16 @@ const assert = require('assert');
 
 if (cluster.isMaster) {
   const worker = cluster.fork();
-  assert.ok(
-    !worker.isDead(),
-    'isDead() should return false right after the worker has been created.');
+  let workerDead = worker.isDead();
+  assert.ok(!workerDead,
+            `isDead() returned ${workerDead}. isDead() should return ` +
+            'false right after the worker has been created.');
 
   worker.on('exit', function() {
-    assert.ok(worker.isDead(),
-              'After an event has been emitted, isDead should return true');
+    workerDead = worker.isDead();
+    assert.ok(workerDead,
+              `isDead() returned ${workerDead}. After an event has been ` +
+              'emitted, isDead should return true');
   });
 
   worker.on('message', function(msg) {
@@ -21,7 +24,9 @@ if (cluster.isMaster) {
   });
 
 } else if (cluster.isWorker) {
-  assert.ok(!cluster.worker.isDead(),
-            'isDead() should return false when called from within a worker');
+  const workerDead = cluster.worker.isDead();
+  assert.ok(!workerDead,
+            `isDead() returned ${workerDead}. isDead() should return ` +
+            'false when called from within a worker');
   process.send('readyToDie');
 }
