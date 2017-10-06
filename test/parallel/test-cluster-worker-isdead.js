a91ed7ea4cc17c1c434bd21e1d2bab4c47074442
Stefania Sharp <stefaniasharp@gmail.com>
2017-10-06 11:09:42 -0700

    test: expand error message

--- test/parallel/test-cluster-worker-isdead.js
+++ test/parallel/test-cluster-worker-isdead.js
@@ -7,11 +7,11 @@ if (cluster.isMaster) {
   const worker = cluster.fork();
   assert.ok(
     !worker.isDead(),
-    'isDead() should return false right after the worker has been created.');
+    `isDead() returned ${worker.isDead()}. isDead() should return false right after the worker has been created.`);
 
   worker.on('exit', function() {
     assert.ok(worker.isDead(),
-              'After an event has been emitted, isDead should return true');
+              `isDead() returned ${worker.isDead()}. After an event has been emitted, isDead should return true`);
   });
 
   worker.on('message', function(msg) {
@@ -22,6 +22,6 @@ if (cluster.isMaster) {
 
 } else if (cluster.isWorker) {
   assert.ok(!cluster.worker.isDead(),
-            'isDead() should return false when called from within a worker');
+            `isDead() returned ${cluster.worker.isDead()}. isDead() should return false when called from within a worker`);
   process.send('readyToDie');
 }
