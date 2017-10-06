5f374a87736601ecef60f1e64423b17bf8fc8885
Alec Ferguson <alec.ferguson@creditkarma.com>
2017-10-06 10:40:29 -0700

    prettifying the code

--- test/parallel/test-zlib-const.js
+++ test/parallel/test-zlib-const.js
@@ -4,15 +4,20 @@ const assert = require('assert');
 
 const zlib = require('zlib');
 
-assert.strictEqual(zlib.constants.Z_OK, 0, `Expected Z_OK to be 0; got ${zlib.constants.Z_OK}`);
+assert.strictEqual(zlib.constants.Z_OK, 0,
+          `Expected Z_OK to be 0; got ${zlib.constants.Z_OK}`);
 zlib.constants.Z_OK = 1;
-assert.strictEqual(zlib.constants.Z_OK, 0, `Z_OK should be immutable. Expected to get 0, got ${zlib.constants.Z_OK}`);
+assert.strictEqual(zlib.constants.Z_OK, 0,
+          `Z_OK should be immutable. Expected to get 0, got ${zlib.constants.Z_OK}`);
 
-assert.strictEqual(zlib.codes.Z_OK, 0, `Expected Z_OK to be 0; got ${zlib.codes.Z_OK}`);
+assert.strictEqual(zlib.codes.Z_OK, 0,
+          `Expected Z_OK to be 0; got ${zlib.codes.Z_OK}`);
 zlib.codes.Z_OK = 1;
-assert.strictEqual(zlib.codes.Z_OK, 0, `Z_OK should be immutable. Expected to get 0, got ${zlib.codes.Z_OK}`);
+assert.strictEqual(zlib.codes.Z_OK, 0,
+          `Z_OK should be immutable. Expected to get 0, got ${zlib.codes.Z_OK}`);
 zlib.codes = { Z_OK: 1 };
-assert.strictEqual(zlib.codes.Z_OK, 0, `Z_OK should be immutable. Expected to get 0, got ${zlib.codes.Z_OK}`);
+assert.strictEqual(zlib.codes.Z_OK, 0,
+          `Z_OK should be immutable. Expected to get 0, got ${zlib.codes.Z_OK}`);
 
 assert.ok(Object.isFrozen(zlib.codes),
-  `Expected zlib.codes to be frozen, but Object.isFrozen returned ${Object.isFrozen(zlib.codes)}`);
+          `Expected zlib.codes to be frozen, but Object.isFrozen returned ${Object.isFrozen(zlib.codes)}`);
