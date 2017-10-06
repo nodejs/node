81bb56040a4fa4947bb408772a3782516ab4c863
WeiPlanet <xwjiang@gmail.com>
2017-10-06 12:05:13 -0700

    test: replace fixturesDir with the fixtures module
    PR-URL: https://github.com/nodejs/node/pull/16027
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Yuta Hiroto <hello@about-hiroppy.com>

--- test/parallel/test-http-default-port.js
+++ test/parallel/test-http-default-port.js
@@ -1,14 +1,16 @@
 'use strict';
 const common = require('../common');
+
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const fixtures = require('../common/fixtures');
 const http = require('http');
 const assert = require('assert');
 const hostExpect = 'localhost';
-const fs = require('fs');
-const path = require('path');
-const fixtures = path.join(common.fixturesDir, 'keys');
 const options = {
-  key: fs.readFileSync(`${fixtures}/agent1-key.pem`),
-  cert: fs.readFileSync(`${fixtures}/agent1-cert.pem`)
+  key: fixtures.readKey('agent1-key.pem'),
+  cert: fixtures.readKey('agent1-cert.pem')
 };
 let gotHttpsResp = false;
 let gotHttpResp = false;
