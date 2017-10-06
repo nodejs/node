3c176fd6f61f3777656b9546428e01615286466c
Jason Walton <dev@lucid.thedreaming.org>
2017-10-06 12:56:13 -0400

    test: replace common.fixturesDir w/common.fixtures
    PR-URL: https://github.com/nodejs/node/pull/15853
    Reviewed-By: Ryan Graham <r.m.graham@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-module-require-depth.js
+++ test/parallel/test-module-require-depth.js
@@ -1,13 +1,14 @@
 // Flags: --expose_internals
 'use strict';
-const common = require('../common');
+require('../common');
+const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const internalModule = require('internal/module');
 
 // Module one loads two too so the expected depth for two is, well, two.
 assert.strictEqual(internalModule.requireDepth, 0);
-const one = require(`${common.fixturesDir}/module-require-depth/one`);
-const two = require(`${common.fixturesDir}/module-require-depth/two`);
+const one = require(fixtures.path('module-require-depth', 'one'));
+const two = require(fixtures.path('module-require-depth', 'two'));
 assert.deepStrictEqual(one, { requireDepth: 1 });
 assert.deepStrictEqual(two, { requireDepth: 2 });
 assert.strictEqual(internalModule.requireDepth, 0);
