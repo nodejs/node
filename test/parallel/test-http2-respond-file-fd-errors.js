0e825a1b42ea518c05b9b5ad8bde701b75be5456
Cristian Peñarrieta <cristian.penarrieta@gmail.com>
2017-10-07 00:01:15 -0700

    using fixtures.path instead of path.resolve

--- test/parallel/test-http2-respond-file-fd-errors.js
+++ test/parallel/test-http2-respond-file-fd-errors.js
@@ -3,9 +3,8 @@
 const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
-const { fixturesDir } = require('../common/fixtures');
+const fixtures = require('../common/fixtures');
 const http2 = require('http2');
-const path = require('path');
 const fs = require('fs');
 
 const {
@@ -30,7 +29,7 @@ const types = {
   symbol: Symbol('test')
 };
 
-const fname = path.resolve(fixturesDir, 'elipses.txt');
+const fname = fixtures.path('elipses.txt');
 const fd = fs.openSync(fname, 'r');
 
 const server = http2.createServer();
