7de5d3b4185a1d417e4528227e7b05e76daab9bf
Vladimir Ilic <burger.boy.daddy@gmail.com>
2017-10-09 18:11:26 -0700

    test: replace fixtureDir with fixtures methods
    PR-URL: https://github.com/nodejs/node/pull/16114
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>

--- test/parallel/test-fs-read.js
+++ test/parallel/test-fs-read.js
@@ -21,10 +21,10 @@
 
 'use strict';
 const common = require('../common');
+const fixtures = require('../common/fixtures');
 const assert = require('assert');
-const path = require('path');
 const fs = require('fs');
-const filepath = path.join(common.fixturesDir, 'x.txt');
+const filepath = fixtures.path('x.txt');
 const fd = fs.openSync(filepath, 'r');
 
 const expected = Buffer.from('xyz\n');
