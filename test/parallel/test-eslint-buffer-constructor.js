cd5ee52d70889a39f8251eb7d49f8063f96f2d8c
Teddy Katz <teddy.katz@gmail.com>
2017-10-11 01:42:47 -0400

    test: add tests for eslint rules
    This adds tests for the custom eslint rules in this repository, using
    the `RuleTester` test utility bundled with eslint.
    
    PR-URL: https://github.com/nodejs/node/pull/16138
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Yuta Hiroto <hello@about-hiroppy.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/parallel/test-eslint-buffer-constructor.js
@@ -0,0 +1,26 @@
+'use strict';
+
+require('../common');
+
+const RuleTester = require('../../tools/eslint').RuleTester;
+const rule = require('../../tools/eslint-rules/buffer-constructor');
+
+const message = 'Use of the Buffer() constructor has been deprecated. ' +
+                'Please use either Buffer.alloc(), Buffer.allocUnsafe(), ' +
+                'or Buffer.from()';
+
+new RuleTester().run('buffer-constructor', rule, {
+  valid: [
+    'Buffer.from(foo)'
+  ],
+  invalid: [
+    {
+      code: 'Buffer(foo)',
+      errors: [{ message }]
+    },
+    {
+      code: 'new Buffer(foo)',
+      errors: [{ message }]
+    }
+  ]
+});
