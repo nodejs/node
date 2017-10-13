5ead8e0d90e3d4ac3b6b1306ad3b586e127f1347
Rob Paton <rtpaton@gmail.com>
2017-10-13 10:52:59 +0100

    Use common for asserting errors are thrown

--- test/es-module/test-esm-loader-modulemap.js
+++ test/es-module/test-esm-loader-modulemap.js
@@ -3,9 +3,9 @@
 
 // This test ensures that the type checking of ModuleMap throws errors appropriately
 
-const assert = require('assert');
 const { URL } = require('url');
 
+const common = require('../common');
 const Loader = require('internal/loader/Loader');
 const ModuleMap = require('internal/loader/ModuleMap');
 const ModuleJob = require('internal/loader/ModuleJob');
@@ -17,34 +17,38 @@ const loader = new Loader();
 const moduleMap = new ModuleMap();
 const moduleJob = new ModuleJob(loader, stubModule.module);
 
-assert.throws(() => {
-  moduleMap.get(1);
-}, /TypeError \[ERR_INVALID_ARG_TYPE\]: The "url" argument must be of type string/);
+common.expectsError(
+  () => moduleMap.get(1),
+  {
+    code: 'ERR_INVALID_ARG_TYPE',
+    type: TypeError,
+    message: 'The "url" argument must be of type string'
+  }
+);
 
-assert.doesNotThrow(() => {
-  moduleMap.get('somestring');
-});
+common.expectsError(
+  () => moduleMap.set(1, moduleJob),
+  {
+    code: 'ERR_INVALID_ARG_TYPE',
+    type: TypeError,
+    message: 'The "url" argument must be of type string'
+  }
+);
 
-assert.throws(() => {
-  moduleMap.set(1, moduleJob);
-}, /TypeError \[ERR_INVALID_ARG_TYPE\]: The "url" argument must be of type string/);
+common.expectsError(
+  () => moduleMap.set('somestring', 'notamodulejob'),
+  {
+    code: 'ERR_INVALID_ARG_TYPE',
+    type: TypeError,
+    message: 'The "job" argument must be of type ModuleJob'
+  }
+);
 
-assert.doesNotThrow(() => {
-  moduleMap.set('somestring', moduleJob);
-});
-
-assert.throws(() => {
-  moduleMap.set('somestring', 'notamodulejob');
-}, /TypeError \[ERR_INVALID_ARG_TYPE\]: The "job" argument must be of type ModuleJob/);
-
-assert.doesNotThrow(() => {
-  moduleMap.set('somestring', moduleJob);
-});
-
-assert.throws(() => {
-  moduleMap.has(1);
-}, /TypeError \[ERR_INVALID_ARG_TYPE\]: The "url" argument must be of type string/);
-
-assert.doesNotThrow(() => {
-  moduleMap.has('somestring');
-});
+common.expectsError(
+  () => moduleMap.has(1),
+  {
+    code: 'ERR_INVALID_ARG_TYPE',
+    type: TypeError,
+    message: 'The "url" argument must be of type string'
+  }
+);
