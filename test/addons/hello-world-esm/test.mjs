e32a9fe093ab7e86397ce0cb47ee89fa0e352570
Rich Trott <rtrott@gmail.com>
2017-10-12 22:02:59 -0700

    test: fix test-esm-addons
    Move test-esm-addons to test/addons/hello-world-esm.
    
    Test should now work properly on CI machines where `addons` are not
    always built at the expected relative path from the es-modules tests.
    
    Test should now work in Debug builds.
    
    Fixes: https://github.com/nodejs/node/issues/16155

--- test/addons/hello-world-esm/test.mjs
+++ test/addons/hello-world-esm/test.mjs
@@ -1,7 +1,6 @@
-// Flags: --experimental-modules
 /* eslint-disable required-modules */
 
 import assert from 'assert';
-import binding from '../addons/hello-world/build/Release/binding.node';
+import binding from './build/binding.node';
 assert.strictEqual(binding.hello(), 'world');
 console.log('binding.hello() =', binding.hello());
