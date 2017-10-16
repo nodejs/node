7a29f440714e443a24bfe312ef9c799e0b821b0a
Lance Ball <lball@redhat.com>
2017-10-16 17:24:17 -0400

    repl: deprecate REPLServer.prototype.memory
    This method is only useful for the internal mechanics of the REPLServer
    and does not need to be exposed in user space. It was previously not
    documented, so I believe a Runtime deprecation makes sense.
    
    PR-URL: https://github.com/nodejs/node/pull/16242
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Jeremiah Senkpiel <fishrock123@rocketmail.com>

---
+++ test/parallel/test-repl-memory-deprecation.js
@@ -0,0 +1,15 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const repl = require('repl');
+
+testMemory();
+
+function testMemory() {
+  const server = repl.start({ prompt: '> ' });
+  const warn = 'REPLServer.memory() is deprecated';
+
+  common.expectWarning('DeprecationWarning', warn);
+  assert.strictEqual(server.memory(), undefined);
+  server.close();
+}
