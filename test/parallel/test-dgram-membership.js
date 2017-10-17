d653f03c0e9fa097abb9959931a78d02a3eb8814
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-10-17 07:27:07 +0200

    test: allow tests to pass without internet
    Currently when running the test without an internet connection there are
    two JavaScript test failures and one cctest. The cctest only fails on
    Mac as far as I know. (I've only tested using Mac and Linux thus far).
    
    This commit moves the two JavaScript tests to test/internet.
    
    The details for test_inspector_socket_server.cc:
    
    [ RUN      ] InspectorSocketServerTest.FailsToBindToNodejsHost
    make[1]: *** [cctest] Segmentation fault: 11
    make: *** [test] Error 2
    
    lldb output:
    
    [ RUN      ] InspectorSocketServerTest.FailsToBindToNodejsHost
    Process 63058 stopped
    * thread #1: tid = 0x7b175, 0x00007fff96d04384
    * libsystem_info.dylib`_gai_simple + 87, queue =
    * 'com.apple.main-thread', stop reason = EXC_BAD_ACCESS (code=1,
    * address=0x0)
        frame #0: 0x00007fff96d04384 libsystem_info.dylib`_gai_simple + 87
    libsystem_info.dylib`_gai_simple:
    ->  0x7fff96d04384 <+87>: movw   (%rdx), %ax
        0x7fff96d04387 <+90>: movw   %ax, -0x2a(%rbp)
        0x7fff96d0438b <+94>: movq   %r13, -0x38(%rbp)
        0x7fff96d0438f <+98>: movq   0x18(%rbp), %rcx
    
    (lldb) bt
    * thread #1: tid = 0x7b175, 0x00007fff96d04384
    * libsystem_info.dylib`_gai_simple + 87, queue =
    * 'com.apple.main-thread', stop reason = EXC_BAD_ACCESS (code=1,
    * address=0x0)
      * frame #0: 0x00007fff96d04384 libsystem_info.dylib`_gai_simple + 87
        frame #1: 0x00007fff96cfe98b libsystem_info.dylib`search_addrinfo +
    179
        frame #2: 0x00007fff96cfafef libsystem_info.dylib`si_addrinfo + 2255
        frame #3: 0x00007fff96cfa67b libsystem_info.dylib`getaddrinfo + 179
        frame #4: 0x00000001017d8888
    cctest`uv__getaddrinfo_work(w=0x00007fff5fbfe210) + 72 at
    getaddrinfo.c:102
        frame #5: 0x00000001017d880e
    cctest`uv_getaddrinfo(loop=0x000000010287cb80, req=0x00007fff5fbfe1c8,
    cb=0x0000000000000000, hostname="nodejs.org", service="0",
    hints=0x00007fff5fbfe268) + 734 at getaddrinfo.c:192
        frame #6: 0x000000010171f781
    cctest`node::inspector::InspectorSocketServer::Start(this=0x00007fff5fbfe658)
    + 801 at inspector_socket_server.cc:398
        frame #7: 0x00000001016ed590
    cctest`InspectorSocketServerTest_FailsToBindToNodejsHost_Test::TestBody(this=0x0000000105001fd0)
    + 288 at test_inspector_socket_server.cc:593
    
    I'm not sure about the exact cause for this but when using a standalone
    c program to simulate this it seems like when the ai_flags
    `AI_NUMERICSERV` is set, which is done in inspector_socket_server.cc
    line 394, the servname (the port in the FailsToBindToNodejsHost test) is
    expected to be a numeric port string to avoid looking it up in
    /etc/services. When the port is 0 as is it was before this commit the
    segment fault occurs but not if it is non-zero.
    
    PR-URL: https://github.com/nodejs/node/pull/16255
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-dgram-membership.js
+++ test/parallel/test-dgram-membership.js
@@ -76,32 +76,3 @@ const setup = dgram.createSocket.bind(dgram, { type: 'udp4', reuseAddr: true });
                 /^Error: dropMembership EINVAL$/);
   socket.close();
 }
-
-// addMembership() with valid socket and multicast address should not throw
-{
-  const socket = setup();
-  assert.doesNotThrow(() => { socket.addMembership(multicastAddress); });
-  socket.close();
-}
-
-// dropMembership() without previous addMembership should throw
-{
-  const socket = setup();
-  assert.throws(
-    () => { socket.dropMembership(multicastAddress); },
-    /^Error: dropMembership EADDRNOTAVAIL$/
-  );
-  socket.close();
-}
-
-// dropMembership() after addMembership() should not throw
-{
-  const socket = setup();
-  assert.doesNotThrow(
-    () => {
-      socket.addMembership(multicastAddress);
-      socket.dropMembership(multicastAddress);
-    }
-  );
-  socket.close();
-}
