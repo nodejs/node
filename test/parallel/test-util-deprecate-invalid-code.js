ff2cf631480364f11ee065b1858f9d80eb0ff003
Ashish Kaila <ashishkaila@hotmail.com>
2017-10-18 13:59:48 -0700

    util: expand test coverage for util.deprecate
    Test for invalid argument types passed to code on util.deprecate.
    
    PR-URL: https://github.com/nodejs/node/pull/16305
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

---
+++ test/parallel/test-util-deprecate-invalid-code.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const common = require('../common');
+const util = require('util');
+
+[1, true, false, null, {}].forEach((notString) => {
+  common.expectsError(() => util.deprecate(() => {}, 'message', notString), {
+    code: 'ERR_INVALID_ARG_TYPE',
+    type: TypeError,
+    message: 'The "code" argument must be of type string'
+  });
+});
