41f1e25dbc28d3d7e59f2fbd2b658db5379c7a5c
zhangzifa <tonyzzf@163.com>
2017-10-19 17:06:34 +0800

    test: update test-timers-block-eventloop.js
    When CPU is busy, the above sequential case fails occasionally,
    expand the timeout value to fix it.
    
    Fixes: https://github.com/nodejs/node/issues/16310

--- test/sequential/test-timers-block-eventloop.js
+++ test/sequential/test-timers-block-eventloop.js
@@ -2,21 +2,23 @@
 
 const common = require('../common');
 const fs = require('fs');
+const commonTimeout = common.platformTimeout;
 
 const t1 = setInterval(() => {
-  common.busyLoop(12);
-}, 10);
+  common.busyLoop(commonTimeout(12));
+}, common.platformTimeout(10));
 
 const t2 = setInterval(() => {
-  common.busyLoop(15);
-}, 10);
+  common.busyLoop(commonTimeout(15));
+}, commonTimeout(10));
 
-const t3 = setTimeout(common.mustNotCall('eventloop blocked!'), 100);
+const t3 =
+  setTimeout(common.mustNotCall('eventloop blocked!'), commonTimeout(200));
 
 setTimeout(function() {
-  fs.stat('./nonexistent.txt', (err, stats) => {
+  fs.stat('/dev/nonexistent', (err, stats) => {
     clearInterval(t1);
     clearInterval(t2);
     clearTimeout(t3);
   });
-}, 50);
+}, commonTimeout(50));
