0b09032ddd6aa62271bc9589752436efc684fb6c
Rich Trott <rtrott@gmail.com>
2017-10-22 11:17:59 -0700

    test: apply eslint exceptions narrowly
    In test-util-inspect, apply ESLint exception for accessor-pairs rule
    narrowly. It had been applied to nearly the whole file, but is only
    needed for two lines.

--- test/parallel/test-util-inspect.js
+++ test/parallel/test-util-inspect.js
@@ -26,8 +26,6 @@ const JSStream = process.binding('js_stream').JSStream;
 const util = require('util');
 const vm = require('vm');
 
-/* eslint-disable accessor-pairs */
-
 assert.strictEqual(util.inspect(1), '1');
 assert.strictEqual(util.inspect(false), 'false');
 assert.strictEqual(util.inspect(''), "''");
@@ -280,6 +278,7 @@ assert.strictEqual(
     '{ readwrite: [Getter/Setter] }');
 
   assert.strictEqual(
+    // eslint-disable-next-line accessor-pairs
     util.inspect({ set writeonly(val) {} }),
     '{ writeonly: [Setter] }');
 
@@ -476,7 +475,7 @@ assert.strictEqual(util.inspect(-0), '-0');
     }
   });
   const setter = Object.create(null, {
-    b: {
+    b: { // eslint-disable-line accessor-pairs
       set: function() {}
     }
   });
@@ -1151,4 +1150,3 @@ if (typeof Symbol !== 'undefined') {
 }
 
 assert.doesNotThrow(() => util.inspect(process));
-/* eslint-enable accessor-pairs */
