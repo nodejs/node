c7d7a76782f8e0b9086adbb7c2047d4cb4b8390f
Bryan English <bryan@bryanenglish.com>
2017-10-21 23:25:59 -0700

    buffer: move setupBufferJS to internal
    Stashing it away in internal/buffer so that it can't be used in
    userland, but can still be used in internals.
    
    PR-URL: https://github.com/nodejs/node/pull/16391
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-buffer-bindingobj-no-zerofill.js
+++ test/parallel/test-buffer-bindingobj-no-zerofill.js
@@ -11,13 +11,11 @@ const assert = require('assert');
 const buffer = require('buffer');
 
 // Monkey-patch setupBufferJS() to have an undefined zeroFill.
-const process = require('process');
-const originalBinding = process.binding;
+const internalBuffer = require('internal/buffer');
 
-const binding = originalBinding('buffer');
-const originalSetup = binding.setupBufferJS;
+const originalSetup = internalBuffer.setupBufferJS;
 
-binding.setupBufferJS = (proto, obj) => {
+internalBuffer.setupBufferJS = (proto, obj) => {
   originalSetup(proto, obj);
   assert.strictEqual(obj.zeroFill[0], 1);
   delete obj.zeroFill;
@@ -25,19 +23,14 @@ binding.setupBufferJS = (proto, obj) => {
 
 const bindingObj = {};
 
-binding.setupBufferJS(Buffer.prototype, bindingObj);
+internalBuffer.setupBufferJS(Buffer.prototype, bindingObj);
 assert.strictEqual(bindingObj.zeroFill, undefined);
-
-process.binding = (bindee) => {
-  if (bindee === 'buffer')
-    return binding;
-  return originalBinding(bindee);
-};
 
 // Load from file system because internal buffer is already loaded and we're
 // testing code that runs on first load only.
 // Do not move this require() to top of file. It is important that
-// `process.binding('buffer').setupBufferJS` be monkey-patched before this runs.
+// `require('internal/buffer').setupBufferJS` be monkey-patched before this
+// runs.
 const monkeyPatchedBuffer = require('../../lib/buffer');
 
 // On unpatched buffer, allocUnsafe() should not zero fill memory. It's always
@@ -51,3 +44,6 @@ while (uninitialized.every((val) => val === 0))
 // zero-fill in that case.
 const zeroFilled = monkeyPatchedBuffer.Buffer.allocUnsafe(1024);
 assert(zeroFilled.every((val) => val === 0));
+
+// setupBufferJS shouldn't still be exposed on the binding
+assert(!('setupBufferJs' in process.binding('buffer')));
