5d7d9cad4fc97b040e8e55dd11d7de4b25fd9c14
WhoMeNope <toman.martin@live.com>
2017-10-22 13:38:27 +0200

    whitespace change reversed

--- test/es-module/test-esm-require-cache.mjs
+++ test/es-module/test-esm-require-cache.mjs
@@ -3,6 +3,5 @@ import '../common';
 import '../fixtures/es-module-require-cache/preload.js';
 import '../fixtures/es-module-require-cache/counter.js';
 import assert from 'assert';
-
 assert.strictEqual(global.counter, 1);
 delete global.counter;
