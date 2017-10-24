9ce7e6492dc4ccc7f0ce9398aa37bac3c931c2f7
James M Snell <jasnell@gmail.com>
2017-10-23 21:02:12 -0700

    [Squash]

--- test/parallel/test-whatwg-encoding-textdecoder-streaming.js
+++ test/parallel/test-whatwg-encoding-textdecoder-streaming.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// From: https://github.com/w3c/web-platform-tests/blob/fa9436d12c5edb21704c3382e299aaac31683b24/encoding/textdecoder-streaming.html
+// From: https://github.com/w3c/web-platform-tests/blob/fa9436d12c/encoding/textdecoder-streaming.html
 
 const common = require('../common');
 
