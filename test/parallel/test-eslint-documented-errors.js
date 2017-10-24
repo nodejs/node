d9fc1facf1c6dcfdc009d4f60e146ed51996139d
James M Snell <jasnell@gmail.com>
2017-10-24 10:57:21 -0700

    tools: add eslint rule for documented errors
    PR-URL: https://github.com/nodejs/node/pull/16450
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Teddy Katz <teddy.katz@gmail.com>

---
+++ test/parallel/test-eslint-documented-errors.js
@@ -0,0 +1,34 @@
+'use strict';
+
+require('../common');
+
+const RuleTester = require('../../tools/eslint').RuleTester;
+const rule = require('../../tools/eslint-rules/documented-errors');
+
+const invalidCode = 'UNDOCUMENTED ERROR CODE';
+
+new RuleTester().run('documented-errors', rule, {
+  valid: [
+    `
+      E('ERR_ASSERTION', 'foo');
+    `
+  ],
+  invalid: [
+    {
+      code: `
+        E('${invalidCode}', 'bar');
+      `,
+      errors: [
+        {
+          message: `"${invalidCode}" is not documented in doc/api/errors.md`,
+          line: 2
+        },
+        {
+          message:
+            `doc/api/errors.md does not have an anchor for "${invalidCode}"`,
+          line: 2
+        }
+      ]
+    }
+  ]
+});
