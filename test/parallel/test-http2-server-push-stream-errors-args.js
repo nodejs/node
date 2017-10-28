98b9705cb80f4d80a482d11eacbfeed41cbaae1d
Anatoli Papirovski <apapirovski@mac.com>
2017-10-28 14:04:16 -0400

    http2: simplify mapToHeaders, stricter validation
    No longer check whether key is a symbol as Object.keys does not
    return symbols. No longer convert key to string as it is always
    a string. Validate that only one value is passed for each
    pseudo-header.
    
    Extend illegal connection header message to include the name of
    the problematic header.
    
    Extend tests to cover this behaviour.
    
    PR-URL: https://github.com/nodejs/node/pull/16575
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-http2-server-push-stream-errors-args.js
+++ test/parallel/test-http2-server-push-stream-errors-args.js
@@ -32,7 +32,7 @@ server.on('stream', common.mustCall((stream, headers) => {
     () => stream.pushStream({ 'connection': 'test' }, {}, () => {}),
     {
       code: 'ERR_HTTP2_INVALID_CONNECTION_HEADERS',
-      message: 'HTTP/1 Connection specific headers are forbidden'
+      message: 'HTTP/1 Connection specific headers are forbidden: "connection"'
     }
   );
 
