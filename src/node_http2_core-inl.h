ecec88265f8d96a7030cc0c83b62f30f49d27e0b
Anatoli Papirovski <apapirovski@mac.com>
2017-10-29 20:48:25 -0400

    [fixup] mark tests as flaky, adjust minor issues

--- src/node_http2_core-inl.h
+++ src/node_http2_core-inl.h
@@ -510,7 +510,7 @@ inline void Nghttp2Session::SendPendingData() {
     // the proceed with the rest.
     while (srcRemaining > destRemaining) {
       DEBUG_HTTP2("Nghttp2Session %s: pushing %d bytes to the socket\n",
-                  TypeName(), destRemaining);
+                  TypeName(), destLength + destRemaining);
       memcpy(dest.base + destOffset, src + srcOffset, destRemaining);
       destLength += destRemaining;
       Send(&dest, destLength);
