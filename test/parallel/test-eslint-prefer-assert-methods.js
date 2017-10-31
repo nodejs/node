4f1939bc55f93651f5d7c1e6a08e4330fb955542
Shobhit Chittora <chittorashobhit@gmail.com>
2017-11-01 02:12:56 +0530

    tools: auto fix custom eslint rule
    1. Extends tests
    2. Refactors code
    3. Adds fixer
    
    Refs: #16636
    
    PR-URL: https://github.com/nodejs/node/pull/16652
    Refs: https://github.com/nodejs/node/issues/16636
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-eslint-prefer-assert-methods.js
+++ test/parallel/test-eslint-prefer-assert-methods.js
@@ -9,31 +9,46 @@ const rule = require('../../tools/eslint-rules/prefer-assert-methods');
 
 new RuleTester().run('prefer-assert-methods', rule, {
   valid: [
-    'assert.strictEqual(foo, bar)',
-    'assert(foo === bar && baz)'
+    'assert.strictEqual(foo, bar);',
+    'assert(foo === bar && baz);',
+    'assert.notStrictEqual(foo, bar);',
+    'assert(foo !== bar && baz);',
+    'assert.equal(foo, bar);',
+    'assert(foo == bar && baz);',
+    'assert.notEqual(foo, bar);',
+    'assert(foo != bar && baz);',
+    'assert.ok(foo);',
+    'assert.ok(foo != bar);',
+    'assert.ok(foo === bar && baz);'
   ],
   invalid: [
     {
-      code: 'assert(foo == bar)',
-      errors: [{ message: "'assert.equal' should be used instead of '=='" }]
+      code: 'assert(foo == bar);',
+      errors: [{
+        message: "'assert.equal' should be used instead of '=='"
+      }],
+      output: 'assert.equal(foo, bar);'
     },
     {
-      code: 'assert(foo === bar)',
+      code: 'assert(foo === bar);',
       errors: [{
         message: "'assert.strictEqual' should be used instead of '==='"
-      }]
+      }],
+      output: 'assert.strictEqual(foo, bar);'
     },
     {
-      code: 'assert(foo != bar)',
+      code: 'assert(foo != bar);',
       errors: [{
         message: "'assert.notEqual' should be used instead of '!='"
-      }]
+      }],
+      output: 'assert.notEqual(foo, bar);'
     },
     {
-      code: 'assert(foo !== bar)',
+      code: 'assert(foo !== bar);',
       errors: [{
         message: "'assert.notStrictEqual' should be used instead of '!=='"
-      }]
-    },
+      }],
+      output: 'assert.notStrictEqual(foo, bar);'
+    }
   ]
 });
