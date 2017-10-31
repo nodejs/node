16e64b5572d0a87142365d7c28a756a75420e548
Shobhit Chittora <chittorashobhit@gmail.com>
2017-11-01 02:12:56 +0530

    tools: auto fix custom eslint rule for prefer-assert-methods.js
    1. Extends tests
    2. Refactors code
    3. Adds fixer
    
    Refs: #16636

--- tools/eslint-rules/prefer-assert-methods.js
+++ tools/eslint-rules/prefer-assert-methods.js
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview Prohibit the use of assert operators ( ===, !==, ==, != )
+ */
+
 'use strict';
 
 const astSelector = 'ExpressionStatement[expression.type="CallExpression"]' +
@@ -8,20 +12,42 @@ function parseError(method, op) {
   return `'assert.${method}' should be used instead of '${op}'`;
 }
 
-const preferedAssertMethod = {
-  '===': 'strictEqual',
-  '!==': 'notStrictEqual',
-  '==': 'equal',
-  '!=': 'notEqual'
-};
+function checkExpression(arg) {
+  const type = arg.type;
+  return arg && (type === 'BinaryExpression');
+}
+
+function preferedAssertMethod(op) {
+  switch (op) {
+    case '===': return 'strictEqual';
+    case '!==': return 'notStrictEqual';
+    case '==': return 'equal';
+    case '!=': return 'notEqual';
+  }
+}
 
 module.exports = function(context) {
   return {
-    [astSelector]: function(node) {
-      const arg = node.expression.arguments[0];
-      const assertMethod = preferedAssertMethod[arg.operator];
-      if (assertMethod) {
-        context.report(node, parseError(assertMethod, arg.operator));
+    ExpressionStatement(node) {
+      if (isAssert(node)) {
+        const arg = getFirstArg(node.expression);
+        if (checkExpression(arg)) {
+          const assertMethod = preferedAssertMethod(arg.operator);
+          if (assertMethod) {
+            context.report({
+              node,
+              message: parseError(assertMethod, arg.operator),
+              fix: (fixer) => {
+                const sourceCode = context.getSourceCode();
+                const args = `${sourceCode.getText(arg)}`;
+                return fixer.replaceText(
+                  node,
+                  `assert.${assertMethod}(${args});`
+                );
+              }
+            });
+          }
+        }
       }
     }
   };
