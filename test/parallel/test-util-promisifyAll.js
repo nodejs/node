2464aaf0663996d67452c61459defda25bba3157
Harry <himion0@gmail.com>
2017-11-02 16:50:12 +0000

    Satifiying Linter

--- test/parallel/test-util-promisifyAll.js
+++ test/parallel/test-util-promisifyAll.js
@@ -2,37 +2,35 @@
 // Flags: --expose-internals
 const common = require('../common');
 const assert = require('assert');
-const fs = require('fs');
 const { promisifyAll } = require('util');
 
 common.crashOnUnhandledRejection();
 
 {
-  let obj = {
-    string: "",
+
+  const pobj = promisifyAll({
+    string: '',
     int: 2,
-    func: function (cb) {
-      cb(null)
+    func: function(cb) {
+      cb(null);
     },
     obj: {
-      func: function (cb) {
-        cb(null)
+      func: function(cb) {
+        cb(null);
       },
-      string: "",
+      string: '',
       int: 2
     }
-  };
-
-  let pobj = promisifyAll(obj);
+  });
 
   // Does not promisfy non functions
-  assert.strictEqual(typeof pobj.string, "string");
-  assert.strictEqual(typeof pobj.int, "number");
-  assert.strictEqual(typeof pobj.obj, "object");
-  assert.strictEqual(typeof pobj.func, "function");
+  assert.strictEqual(typeof pobj.string, 'string');
+  assert.strictEqual(typeof pobj.int, 'number');
+  assert.strictEqual(typeof pobj.obj, 'object');
+  assert.strictEqual(typeof pobj.func, 'function');
   assert(pobj.func() instanceof Promise);
-  assert.strictEqual(typeof pobj.obj.func, "function");
+  assert.strictEqual(typeof pobj.obj.func, 'function');
   assert(pobj.obj.func() instanceof Promise);
-  assert.strictEqual(typeof pobj.obj.string, "string");
-  assert.strictEqual(typeof pobj.obj.int, "number");
+  assert.strictEqual(typeof pobj.obj.string, 'string');
+  assert.strictEqual(typeof pobj.obj.int, 'number');
 }
