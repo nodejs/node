34f24cdd3c902c6892733351581e77c57cc3a8cc
Joyee Cheung <joyeec9h3@gmail.com>
2017-11-03 22:50:32 +0800

    http2: improve errors thrown in header validation

--- test/parallel/test-http2-compat-serverresponse-trailers.js
+++ test/parallel/test-http2-compat-serverresponse-trailers.js
@@ -28,7 +28,7 @@ server.listen(0, common.mustCall(() => {
       {
         code: 'ERR_HTTP2_INVALID_HEADER_VALUE',
         type: TypeError,
-        message: 'Value must not be undefined or null'
+        message: 'Invalid value "undefined" for header "test"'
       }
     );
     common.expectsError(
@@ -36,7 +36,7 @@ server.listen(0, common.mustCall(() => {
       {
         code: 'ERR_HTTP2_INVALID_HEADER_VALUE',
         type: TypeError,
-        message: 'Value must not be undefined or null'
+        message: 'Invalid value "null" for header "test"'
       }
     );
     common.expectsError(
