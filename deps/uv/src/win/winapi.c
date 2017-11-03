92f86632d534125aa7a86fe11a34d7b794b22991
Bartosz Sosnowski <bartosz@janeasystems.com>
2017-11-03 12:18:52 +0100

    deps: cherry-pick e7f4e9e from upstream libuv
    Original commit message:
        tty, win: get SetWinEventHook pointer at startup
    
        SetWinEventHook is not available on some Windows versions.
    
        Fixes: https://github.com/nodejs/node/issues/16603
        Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
        Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    
    PR-URL: https://github.com/nodejs/node/pull/16724
    Fixes: https://github.com/https://github.com/nodejs/node/issues/16603
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>

--- deps/uv/src/win/winapi.c
+++ deps/uv/src/win/winapi.c
@@ -52,11 +52,15 @@ sGetFinalPathNameByHandleW pGetFinalPathNameByHandleW;
 /* Powrprof.dll function pointer */
 sPowerRegisterSuspendResumeNotification pPowerRegisterSuspendResumeNotification;
 
+/* User32.dll function pointer */
+sSetWinEventHook pSetWinEventHook;
+
 
 void uv_winapi_init(void) {
   HMODULE ntdll_module;
   HMODULE kernel32_module;
   HMODULE powrprof_module;
+  HMODULE user32_module;
 
   ntdll_module = GetModuleHandleA("ntdll.dll");
   if (ntdll_module == NULL) {
@@ -154,6 +158,12 @@ void uv_winapi_init(void) {
   if (powrprof_module != NULL) {
     pPowerRegisterSuspendResumeNotification = (sPowerRegisterSuspendResumeNotification)
       GetProcAddress(powrprof_module, "PowerRegisterSuspendResumeNotification");
+  }
+
+  user32_module = LoadLibraryA("user32.dll");
+  if (user32_module != NULL) {
+    pSetWinEventHook = (sSetWinEventHook)
+      GetProcAddress(user32_module, "SetWinEventHook");
   }
 
 }
