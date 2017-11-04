ce0b70bebfad38c5dbe4432bfd123f55944f02c9
Steve Kinney <hello@stevekinney.net>
2017-11-04 17:10:41 -0600

    test: Refactor to fixtures.readSync
    Updates call to fs.readFileSync to the fixtures.readSync utility
    function.

--- test/parallel/test-sync-fileread.js
+++ test/parallel/test-sync-fileread.js
@@ -1,7 +1,6 @@
 'use strict';
 require('../common');
 const assert = require('assert');
-const fs = require('fs');
 const fixtures = require('../common/fixtures');
 
-assert.strictEqual(fs.readFileSync(fixtures.path('x.txt')).toString(), 'xyz\n');
+assert.strictEqual(fixtures.readSync('x.txt').toString(), 'xyz\n');
