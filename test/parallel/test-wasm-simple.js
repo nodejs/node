71036dd75160892dc6b82c59b6c9a18626b0fbca
Steve Kinney <hello@stevekinney.net>
2017-11-04 16:08:37 -0600

    test: Add basic WebAssembly test
    Tests a basic WebAssembly module that adds two numbers.
    wasm example from the WebAssembly/wabt repo licensed
    Apache 2.0.
    
    Refs: https://github.com/WebAssembly/wabt/blob/master/demo/wat2wasm/examples.js#L27-L32

---
+++ test/parallel/test-wasm-simple.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('../common');
+
+const assert = require('assert');
+const fixtures = require('../common/fixtures');
+
+const buffer = fixtures.readSync('test.wasm');
+
+assert.ok(WebAssembly.validate(buffer), 'Buffer should be valid WebAssembly');
+
+WebAssembly.instantiate(buffer, {}).then((results) => {
+  assert.strictEqual(
+    results.instance.exports.addTwo(10, 20),
+    30,
+    'Exported function should add two numbers.',
+  );
+});
