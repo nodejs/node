a9815e4d227d288ff5de88f3bd056c0a27a35e89
Adam Jeffery <adam.jeffery@intercity.technology>
2017-11-06 15:44:56 +0000

    test: add values to error message
    In test-require-extensions-main, include the values that caused the test
    to fail in the messages reporting the failure.
    
    PR-URL: https://github.com/nodejs/node/pull/16831
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>

--- test/parallel/test-require-extensions-main.js
+++ test/parallel/test-require-extensions-main.js
@@ -3,11 +3,11 @@ require('../common');
 const assert = require('assert');
 const fixtures = require('../common/fixtures');
 
-const fixturesRequire =
-  require(fixtures.path('require-bin', 'bin', 'req.js'));
+const fixturesRequire = require(fixtures.path('require-bin', 'bin', 'req.js'));
 
 assert.strictEqual(
   fixturesRequire,
   '',
-  'test-require-extensions-main failed to import fixture requirements'
+  'test-require-extensions-main failed to import fixture requirements: ' +
+    fixturesRequire
 );
