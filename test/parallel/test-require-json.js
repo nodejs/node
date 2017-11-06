af136788890f0e80e3c183df76717f613488a6fc
Matthias Reis <matthias.reis@xing.com>
2017-11-06 16:09:54 +0100

    test: clarified assert message for test-require-json.js
    PR-URL: https://github.com/nodejs/node/pull/16807
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-require-json.js
+++ test/parallel/test-require-json.js
@@ -29,5 +29,6 @@ try {
 } catch (err) {
   assert.ok(
     /test[/\\]fixtures[/\\]invalid\.json: Unexpected string/.test(err.message),
-    'require() json error should include path');
+    `require() json error should include path - currently: ${err.message}`
+  );
 }
