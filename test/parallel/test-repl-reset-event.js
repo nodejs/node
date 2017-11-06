7ef44e930d9895122512eee5d1e4dab072e5a0b8
Adri Van Houdt <adri.van.houdt@gmail.com>
2017-11-06 17:02:21 +0000

    test: make assertions clearer

--- test/parallel/test-repl-reset-event.js
+++ test/parallel/test-repl-reset-event.js
@@ -39,11 +39,13 @@ function testReset(cb) {
   r.context.foo = 42;
   r.on('reset', common.mustCall(function(context) {
     assert(!!context, 'REPL did not emit a context with reset event');
-    assert.strictEqual(context, r.context, `REPL emitted incorrect context. context is ${util.inspect(context)}, expected ${util.inspect(r.context)}`);
+    assert.strictEqual(context, r.context, 'REPL emitted incorrect context.' +
+    `context is ${util.inspect(context)}, expected ${util.inspect(r.context)}`);
     assert.strictEqual(
       context.foo,
       undefined,
-      `REPL emitted the previous context, and is not using global as context. context.foo is ${context.foo}, expected undefined.`
+      'REPL emitted the previous context, and is not using global as context.' +
+      `context.foo is ${context.foo}, expected undefined.`
     );
     context.foo = 42;
     cb();
@@ -62,7 +64,8 @@ function testResetGlobal() {
     assert.strictEqual(
       context.foo,
       42,
-      `"foo" property is missing or different from REPL using global as context. context.foo is ${context.foo}, expected 42.`
+      '"foo" property is different from REPL using global as context.' +
+      `context.foo is ${context.foo}, expected 42.`
     );
   }));
   r.resetContext();
