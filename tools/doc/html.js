f3749d7b2cabcc4a17c73b5dfa443304f4c07241
Vse Mozhet Byt <vsemozhetbyt@gmail.com>
2017-11-06 20:46:06 +0200

    tools: remove unneeded parentheses in doc/html.js
    PR-URL: https://github.com/nodejs/node/pull/16845
    Ref: https://github.com/nodejs/node/pull/16801#discussion_r149142120
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- tools/doc/html.js
+++ tools/doc/html.js
@@ -466,7 +466,7 @@ function getId(text) {
   text = text.replace(/^_+|_+$/, '');
   text = text.replace(/^([^a-z])/, '_$1');
   if (idCounters.hasOwnProperty(text)) {
-    text += `_${(++idCounters[text])}`;
+    text += `_${++idCounters[text]}`;
   } else {
     idCounters[text] = 0;
   }
