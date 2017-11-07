a9d7c3d5b49b094cba3c5b95221bca014bba03a9
Paul Ashfield <paul.ashfield@gmail.com>
2017-11-07 10:30:46 +0000

    Updated test case based on feedback from @apapirovski @vsemozhetbyt and Gireesh - thanks all

--- test/addons-napi/test_general/testNapiRun.js
+++ test/addons-napi/test_general/testNapiRun.js
@@ -7,10 +7,9 @@ const assert = require('assert');
 // eslint-disable-next-line no-unused-vars
 const addon = require(`./build/${common.buildType}/test_general`);
 
-const args = [41.92, 0.08];
-const testCase = '(' + args.join(' + ') + ');';
-const expected = 43;
+const testCase = '(41.92 + 0.08);';
+const expected = 42;
 const actual = addon.testNapiRun(testCase);
 
-assert.strictEqual(actual, expected, `testNapiRun${testCase} gets ${actual} not expected ${expected}`);
+assert.strictEqual(actual, expected);
 assert.throws(() => addon.testNapiRun({abc: 'def'}), /string was expected/);
