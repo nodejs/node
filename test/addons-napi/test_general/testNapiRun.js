2b1ad0aa44cb362455d7e14f3cb9a8d5dbfba64b
Rich Trott <rtrott@gmail.com>
2017-11-08 12:28:56 +0000

    squash: lint fix

--- test/addons-napi/test_general/testNapiRun.js
+++ test/addons-napi/test_general/testNapiRun.js
@@ -12,4 +12,4 @@ const expected = 42;
 const actual = addon.testNapiRun(testCase);
 
 assert.strictEqual(actual, expected);
-assert.throws(() => addon.testNapiRun({abc: 'def'}), /string was expected/);
+assert.throws(() => addon.testNapiRun({ abc: 'def' }), /string was expected/);
