0fdd88a374e23e1dd4a05d93afd5eb0c3b080fd5
Ben Noordhuis <info@bnoordhuis.nl>
2017-11-09 13:26:47 +0100

    module: speed up package.json parsing more
    Move the logic from the previous commit to C++ land in order to avoid
    creating a new string when we know we won't parse it anyway.
    
    PR-URL: https://github.com/nodejs/node/pull/15767
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-module-binding.js
+++ test/parallel/test-module-binding.js
@@ -2,8 +2,13 @@
 require('../common');
 const fixtures = require('../common/fixtures');
 const { internalModuleReadFile } = process.binding('fs');
+const { readFileSync } = require('fs');
 const { strictEqual } = require('assert');
 
 strictEqual(internalModuleReadFile('nosuchfile'), undefined);
 strictEqual(internalModuleReadFile(fixtures.path('empty.txt')), '');
 strictEqual(internalModuleReadFile(fixtures.path('empty-with-bom.txt')), '');
+{
+  const filename = fixtures.path('require-bin/package.json');
+  strictEqual(internalModuleReadFile(filename), readFileSync(filename, 'utf8'));
+}
