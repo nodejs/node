804eb3cd734176f123142f0db496279cb2f645fd
Ben Noordhuis <info@bnoordhuis.nl>
2017-11-15 22:02:08 +0100

    src: remove process._debugPause()
    This method is undocumented and depends on a V8 API that is slated for
    removal.  The inspector and node-inspect don't use it and I could find
    no third-party code that depends on it.  Remove it.
    
    PR-URL: https://github.com/nodejs/node/pull/17060
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Evan Lucas <evanlucas@me.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>

--- src/node.cc
+++ src/node.cc
@@ -67,7 +67,6 @@
 #if NODE_USE_V8_PLATFORM
 #include "libplatform/libplatform.h"
 #endif  // NODE_USE_V8_PLATFORM
-#include "v8-debug.h"
 #include "v8-profiler.h"
 #include "zlib.h"
 
@@ -2931,7 +2930,6 @@ static void DebugPortSetter(Local<Name> property,
 
 
 static void DebugProcess(const FunctionCallbackInfo<Value>& args);
-static void DebugPause(const FunctionCallbackInfo<Value>& args);
 static void DebugEnd(const FunctionCallbackInfo<Value>& args);
 
 namespace {
@@ -3363,7 +3361,6 @@ void SetupProcessObject(Environment* env,
   env->SetMethod(process, "_kill", Kill);
 
   env->SetMethod(process, "_debugProcess", DebugProcess);
-  env->SetMethod(process, "_debugPause", DebugPause);
   env->SetMethod(process, "_debugEnd", DebugEnd);
 
   env->SetMethod(process, "hrtime", Hrtime);
@@ -4089,11 +4086,6 @@ static void DebugProcess(const FunctionCallbackInfo<Value>& args) {
     CloseHandle(mapping);
 }
 #endif  // _WIN32
-
-
-static void DebugPause(const FunctionCallbackInfo<Value>& args) {
-  v8::Debug::DebugBreak(args.GetIsolate());
-}
 
 
 static void DebugEnd(const FunctionCallbackInfo<Value>& args) {
