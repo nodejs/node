3af0573eba1b7b1468cc195f8ecf643c293d32b0
XadillaX <i@2333.moe>
2017-11-15 15:12:42 +0800

    fix a typo

--- test/internet/test-dns-setserver-in-callback-of-resolve4.js
+++ test/internet/test-dns-setserver-in-callback-of-resolve4.js
@@ -9,14 +9,14 @@ const { addresses } = require('../common/internet');
 const dns = require('dns');
 
 dns.resolve4(
-  addresses.INET_HOST,
+  addresses.INET4_HOST,
   common.mustCall(function(/* err, nameServers */) {
-    dns.setServers([ '8.8.8.8' ]);
+    dns.setServers([ addresses.DNS4_SERVER ]);
   }));
 
 // Test https://github.com/nodejs/node/issues/14734
 dns.resolve4(
-  addresses.INET_HOST,
+  addresses.INET4_HOST,
   common.mustCall(function() {
     // DO NOTHING
   }));
