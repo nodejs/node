f12efd59902ef5650667b9839649f5b9baf56b3e
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2017-11-16 15:56:20 +0100

    src: use unique_ptr for http2_state
    PR-URL: https://github.com/nodejs/node/pull/17078
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- src/env-inl.h
+++ src/env-inl.h
@@ -361,7 +361,6 @@ inline Environment::~Environment() {
   delete[] heap_statistics_buffer_;
   delete[] heap_space_statistics_buffer_;
   delete[] http_parser_buffer_;
-  delete http2_state_;
   free(performance_state_);
 }
 
@@ -516,12 +515,13 @@ inline void Environment::set_http_parser_buffer(char* buffer) {
 }
 
 inline http2::http2_state* Environment::http2_state() const {
-  return http2_state_;
+  return http2_state_.get();
 }
 
-inline void Environment::set_http2_state(http2::http2_state* buffer) {
-  CHECK_EQ(http2_state_, nullptr);  // Should be set only once.
-  http2_state_ = buffer;
+inline void Environment::set_http2_state(
+    std::unique_ptr<http2::http2_state> buffer) {
+  CHECK(!http2_state_);  // Should be set only once.
+  http2_state_ = std::move(buffer);
 }
 
 inline double* Environment::fs_stats_field_array() const {
