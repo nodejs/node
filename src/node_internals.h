0db1f8782518bced6ef23110bac5d25a3adfe5be
Ben Noordhuis <info@bnoordhuis.nl>
2017-11-16 15:15:05 +0100

    src: use NODE_BUILTIN_MODULE_CONTEXT_AWARE() macro
    Commit d217b2850e ("async_hooks: add trace events to async_hooks")
    used `NODE_MODULE_CONTEXT_AWARE_BUILTIN()` instead.
    
    After commit 8680bb9f1a ("src: explicitly register built-in modules")
    it no longer works for static library builds so remove it.
    
    PR-URL: https://github.com/nodejs/node/pull/17071
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>

--- src/node_internals.h
+++ src/node_internals.h
@@ -55,6 +55,12 @@
 #define Z_MAX_LEVEL 9
 #define Z_DEFAULT_LEVEL Z_DEFAULT_COMPRESSION
 
+enum {
+  NM_F_BUILTIN  = 1 << 0,
+  NM_F_LINKED   = 1 << 1,
+  NM_F_INTERNAL = 1 << 2,
+};
+
 struct sockaddr;
 
 // Variation on NODE_DEFINE_CONSTANT that sets a String value.
@@ -115,6 +121,7 @@ struct sockaddr;
     V(stream_wrap)                                                            \
     V(tcp_wrap)                                                               \
     V(timer_wrap)                                                             \
+    V(trace_events)                                                           \
     V(tty_wrap)                                                               \
     V(udp_wrap)                                                               \
     V(url)                                                                    \
