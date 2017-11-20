ba4adc55f8c5a7ee2879ba9f544bb336e755276d
Madara Uchiha <me@madara.ninja>
2017-11-20 21:23:06 +0200

    Make tests pass
    For unhandled rejection error message change.

--- test/parallel/test-promises-unhandled-symbol-rejections.js
+++ test/parallel/test-promises-unhandled-symbol-rejections.js
@@ -6,8 +6,11 @@ const expectedDeprecationWarning = 'Unhandled promise rejections are ' +
                                    'rejections that are not handled will ' +
                                    'terminate the Node.js process with a ' +
                                    'non-zero exit code.';
-const expectedPromiseWarning = 'Unhandled promise rejection (rejection id: ' +
-                               '1): Symbol()';
+const expectedPromiseWarning = 'Unhandled promise rejection. ' +
+  'This error originated either by throwing ' +
+  'inside of an async function without a catch ' +
+  'block, or by rejecting a promise which was ' +
+  'not handled with .catch(). (rejection id: 1)';
 
 common.expectWarning({
   DeprecationWarning: expectedDeprecationWarning,
--- test/parallel/test-promises-warning-on-unhandled-rejection.js
+++ test/parallel/test-promises-warning-on-unhandled-rejection.js
@@ -12,18 +12,21 @@ let b = 0;
 process.on('warning', common.mustCall((warning) => {
   switch (b++) {
     case 0:
-      assert.strictEqual(warning.name, 'UnhandledPromiseRejectionWarning');
-      assert(/Unhandled promise rejection/.test(warning.message));
+      assert.strictEqual(warning.message, 'This was rejected');
       break;
     case 1:
-      assert.strictEqual(warning.name, 'DeprecationWarning');
+      assert.strictEqual(warning.name, 'UnhandledPromiseRejectionWarning');
+      assert(/Unhandled promise rejection/.test(warning.message), 'Expected warning message to contain "Unhandled promise rejection" but did not. Had "' + warning.message + '" instead.');
       break;
     case 2:
+      assert.strictEqual(warning.name, 'DeprecationWarning');
+      break;
+    case 3:
       assert.strictEqual(warning.name, 'PromiseRejectionHandledWarning');
       assert(/Promise rejection was handled asynchronously/
         .test(warning.message));
   }
-}, 3));
+}, 4));
 
 const p = Promise.reject('This was rejected');
 setImmediate(common.mustCall(() => p.catch(() => {})));
