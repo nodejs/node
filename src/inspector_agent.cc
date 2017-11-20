0b07dda7679ef7b17ceb4f25e7f23d3dcbd5cc64
Ben Noordhuis <info@bnoordhuis.nl>
2017-11-20 23:37:50 +0100

    src: inspector context name = program title + pid
    Report (for example) "node[1337]" as the human-readable name rather
    than the more generic and less helpful "Node.js Main Context."
    
    While not perfect yet, it should be an improvement to people that
    debug multiple processes from DevTools, VS Code, etc.
    
    PR-URL: https://github.com/nodejs/node/pull/17087
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Eugene Ostroukhov <eostroukhov@google.com>
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>

--- src/inspector_agent.cc
+++ src/inspector_agent.cc
@@ -302,7 +302,8 @@ class NodeInspectorClient : public V8InspectorClient {
       : env_(env), platform_(platform), terminated_(false),
         running_nested_loop_(false) {
     client_ = V8Inspector::create(env->isolate(), this);
-    contextCreated(env->context(), "Node.js Main Context");
+    // TODO(bnoordhuis) Make name configurable from src/node.cc.
+    contextCreated(env->context(), GetHumanReadableProcessName());
   }
 
   void runMessageLoopOnPause(int context_group_id) override {
