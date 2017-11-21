27ee7117dd489aeca78121a8dfc85d0421908637
suman-mitra <summitra@in.ibm.com>
2017-11-21 12:28:47 +0530

    test: wrap callback in common.mustCall
    PR-URL: https://github.com/nodejs/node/pull/17173
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-cluster-message.js
+++ test/parallel/test-cluster-message.js
@@ -20,7 +20,7 @@
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 'use strict';
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const cluster = require('cluster');
 const net = require('net');
@@ -132,9 +132,9 @@ if (cluster.isWorker) {
       worker.kill();
     });
 
-    worker.on('exit', function() {
+    worker.on('exit', common.mustCall(function() {
       process.exit(0);
-    });
+    }));
   });
 
   process.once('exit', function() {
