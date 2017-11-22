f1de0da7b9704f7e1c5692413638a8de1d2f4bf7
Rich Trott <rtrott@gmail.com>
2017-11-22 12:47:50 -0800

    doc,test: remove unnecessary await with return instances
    Remove unnecessary `await` in combination with `return` in preparation
    for enabling lint rule.
    
    PR-URL: https://github.com/nodejs/node/pull/17265
    Reviewed-By: Timothy Gu <timothygu99@gmail.com>
    Reviewed-By: Vse Mozhet Byt <vsemozhetbyt@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>

--- test/common/inspector-helper.js
+++ test/common/inspector-helper.js
@@ -390,7 +390,7 @@ class NodeInstance {
     console.log('[test]', 'Connecting to a child Node process');
     const response = await this.httpGet(null, '/json/list');
     const url = response[0]['webSocketDebuggerUrl'];
-    return await this.wsHandshake(url);
+    return this.wsHandshake(url);
   }
 
   expectShutdown() {
