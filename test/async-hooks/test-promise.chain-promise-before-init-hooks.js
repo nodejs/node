fc8cc82433e0b7917ea28d197229eb54f685c75c
aryung chen <aryung@gmail.com>
2017-11-22 16:35:13 +0800

    fix typo

--- test/async-hooks/test-promise.chain-promise-before-init-hooks.js
+++ test/async-hooks/test-promise.chain-promise-before-init-hooks.js
@@ -4,7 +4,7 @@ const common = require('../common');
 const assert = require('assert');
 const initHooks = require('./init-hooks');
 const { checkInvocations } = require('./hook-checks');
-common.crashOnUnhandleRejection();
+common.crashOnUnhandledRejection();
 
 const p = new Promise(common.mustCall(function executor(resolve, reject) {
   resolve(5);
