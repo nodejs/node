aa3b9f8820faed56f3cb44bbfe146dd8e5c86cfc
Jason Chung <jason.chung@positivegrid.com>
2017-11-23 09:21:22 +0800

    fix: move common.crashOnUnhandledRejection to below the require statements

--- test/parallel/test-repl-load-multiline.js
+++ test/parallel/test-repl-load-multiline.js
@@ -1,9 +1,10 @@
 'use strict';
 const common = require('../common');
-common.crashOnUnhandledRejection();
 const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const repl = require('repl');
+
+common.crashOnUnhandledRejection();
 
 const command = `.load ${fixtures.path('repl-load-multiline.js')}`;
 const terminalCode = '\u001b[1G\u001b[0J \u001b[1G';
