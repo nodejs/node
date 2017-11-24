7fd00ac3d5aaee68826b22d4f2bd4ea8a9164823
Andreas Madsen <amwebdk@gmail.com>
2017-11-24 16:08:47 +0100

    trace_events: add executionAsyncId to init events
    async_hooks emits trace_events. This adds the executionAsyncId to the
    init events. In theory this could be inferred from the before and after
    events but this is much simpler and doesn't require knowledge of all
    events.
    
    PR-URL: https://github.com/nodejs/node/pull/17196
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-trace-events-binding.js
+++ test/parallel/test-trace-events-binding.js
@@ -10,7 +10,10 @@ const CODE = `
     'type-value', 10, 'extra-value', 20);
   process.binding("trace_events").emit(
     'b'.charCodeAt(0), 'custom',
-    'type-value', 20);
+    'type-value', 20, 'first-value', 20, 'second-value', 30);
+  process.binding("trace_events").emit(
+    'b'.charCodeAt(0), 'custom',
+    'type-value', 30);
   process.binding("trace_events").emit(
     'b'.charCodeAt(0), 'missing',
     'type-value', 10, 'extra-value', 20);
@@ -29,7 +32,7 @@ proc.once('exit', common.mustCall(() => {
   assert(common.fileExists(FILE_NAME));
   fs.readFile(FILE_NAME, common.mustCall((err, data) => {
     const traces = JSON.parse(data.toString()).traceEvents;
-    assert.strictEqual(traces.length, 2);
+    assert.strictEqual(traces.length, 3);
 
     assert.strictEqual(traces[0].pid, proc.pid);
     assert.strictEqual(traces[0].ph, 'b');
@@ -43,6 +46,16 @@ proc.once('exit', common.mustCall(() => {
     assert.strictEqual(traces[1].cat, 'custom');
     assert.strictEqual(traces[1].name, 'type-value');
     assert.strictEqual(traces[1].id, '0x14');
-    assert.deepStrictEqual(traces[1].args, { });
+    assert.deepStrictEqual(traces[1].args, {
+      'first-value': 20,
+      'second-value': 30
+    });
+
+    assert.strictEqual(traces[2].pid, proc.pid);
+    assert.strictEqual(traces[2].ph, 'b');
+    assert.strictEqual(traces[2].cat, 'custom');
+    assert.strictEqual(traces[2].name, 'type-value');
+    assert.strictEqual(traces[2].id, '0x1e');
+    assert.deepStrictEqual(traces[2].args, { });
   }));
 }));
