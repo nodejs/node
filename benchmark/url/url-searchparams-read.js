3639d0c026a770b40129dbfc6738ec8502428719
James M Snell <jasnell@gmail.com>
2017-11-27 20:53:16 -0800

    url: adding WHATWG URL support
    Implements WHATWG URL support. Example:
    
    ```
    var u = new url.URL('http://example.org');
    ```
    
    Many, many other commits improving the implementation have been squashed
    into this backport PR. They are not listed separately here for brevity.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/17365
    PR-URL: https://github.com/nodejs/node/pull/7448
    Reviewed-By: Ilkka Myller <ilkka.myller@nodefield.com>

---
+++ benchmark/url/url-searchparams-read.js
@@ -0,0 +1,58 @@
+'use strict';
+const common = require('../common.js');
+const { URLSearchParams } = require('url');
+
+const bench = common.createBenchmark(main, {
+  method: ['get', 'getAll', 'has'],
+  param: ['one', 'two', 'three', 'nonexistent'],
+  n: [2e7]
+});
+
+const str = 'one=single&two=first&three=first&two=2nd&three=2nd&three=3rd';
+
+function get(n, param) {
+  const params = new URLSearchParams(str);
+
+  bench.start();
+  for (var i = 0; i < n; i += 1)
+    params.get(param);
+  bench.end(n);
+}
+
+function getAll(n, param) {
+  const params = new URLSearchParams(str);
+
+  bench.start();
+  for (var i = 0; i < n; i += 1)
+    params.getAll(param);
+  bench.end(n);
+}
+
+function has(n, param) {
+  const params = new URLSearchParams(str);
+
+  bench.start();
+  for (var i = 0; i < n; i += 1)
+    params.has(param);
+  bench.end(n);
+}
+
+function main(conf) {
+  const method = conf.method;
+  const param = conf.param;
+  const n = conf.n | 0;
+
+  switch (method) {
+    case 'get':
+      get(n, param);
+      break;
+    case 'getAll':
+      getAll(n, param);
+      break;
+    case 'has':
+      has(n, param);
+      break;
+    default:
+      throw new Error('Unknown method');
+  }
+}
