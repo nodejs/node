9e23ec226ba99464b3eccbb9562c676fbe4ab68a
Lance Ball <lball@redhat.com>
2017-11-28 12:29:57 -0500

    test: add check for win32 and modify message
    Because of the way this test splits up the message string on ':'
    we need to ensure that the initial drive letter does not affect
    the string manipulation. Shortening it by 2 characters solves this.

--- test/parallel/test-common-must-not-call.js
+++ test/parallel/test-common-must-not-call.js
@@ -10,6 +10,9 @@ const testFunction = common.mustNotCall(message);
 const validateError = common.mustCall((e) => {
   const prefix = `${message} at `;
   assert.ok(e.message.startsWith(prefix));
+  if (process.platform === 'win32') {
+    e.message = e.message.substring(2); // remove 'C:'
+  }
   const [ fileName, lineNumber ] = e.message
                                     .substring(prefix.length).split(':');
   assert.strictEqual(path.basename(fileName), 'test-common-must-not-call.js');
