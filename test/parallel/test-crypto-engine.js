254b5f04e7b309146cb8ade1d1fb0b804b964a51
Leko <leko.noor@gmail.com>
2017-12-02 03:10:26 +0900

    test: add test case for missing branch
    - The case of id is string, flags is number
    - The case of flags is not 0
    
    PR-URL: https://github.com/nodejs/node/pull/17418
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Jon Moss <me@jonathanmoss.me>

--- test/parallel/test-crypto-engine.js
+++ test/parallel/test-crypto-engine.js
@@ -5,6 +5,7 @@ if (!common.hasCrypto)
   common.skip('missing crypto');
 
 const crypto = require('crypto');
+const invalidEngineName = 'xxx';
 
 common.expectsError(
   () => crypto.setEngine(true),
@@ -20,4 +21,20 @@ common.expectsError(
     code: 'ERR_INVALID_ARG_TYPE',
     type: TypeError,
     message: 'The "flags" argument must be of type number'
+  });
+
+common.expectsError(
+  () => crypto.setEngine(invalidEngineName),
+  {
+    code: 'ERR_CRYPTO_ENGINE_UNKNOWN',
+    type: Error,
+    message: `Engine "${invalidEngineName}" was not found`
+  });
+
+common.expectsError(
+  () => crypto.setEngine(invalidEngineName, crypto.constants.ENGINE_METHOD_RSA),
+  {
+    code: 'ERR_CRYPTO_ENGINE_UNKNOWN',
+    type: Error,
+    message: `Engine "${invalidEngineName}" was not found`
   });
