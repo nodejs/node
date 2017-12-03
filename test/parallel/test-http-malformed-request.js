f9adf51744f3465e8ffe283a38d03d24c740553c
Mithun Sasidharan <mithunsasidharan89@gmail.com>
2017-12-03 20:43:01 +0530

    test: use common.mustCall in test-http-malformed-request
    PR-URL: https://github.com/nodejs/node/pull/17439
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Jon Moss <me@jonathanmoss.me>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-http-malformed-request.js
+++ test/parallel/test-http-malformed-request.js
@@ -1,25 +1,20 @@
 'use strict';
-require('../common');
-const assert = require('assert');
+const common = require('../common');
 const net = require('net');
 const http = require('http');
 const url = require('url');
 
 // Make sure no exceptions are thrown when receiving malformed HTTP
 // requests.
-
-let nrequests_completed = 0;
-const nrequests_expected = 1;
-
-const server = http.createServer(function(req, res) {
+const server = http.createServer(common.mustCall((req, res) => {
   console.log(`req: ${JSON.stringify(url.parse(req.url))}`);
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World');
   res.end();
 
-  if (++nrequests_completed === nrequests_expected) server.close();
-});
+  server.close();
+}));
 server.listen(0);
 
 server.on('listening', function() {
@@ -28,8 +23,4 @@ server.on('listening', function() {
     c.write('GET /hello?foo=%99bar HTTP/1.1\r\n\r\n');
     c.end();
   });
-});
-
-process.on('exit', function() {
-  assert.strictEqual(nrequests_expected, nrequests_completed);
 });
