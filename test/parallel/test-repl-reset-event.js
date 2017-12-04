15f8a1fe2575e20eaa312d3c3b63b80d3acf720a
Adri Van Houdt <adri.van.houdt@gmail.com>
2017-12-04 09:44:29 +0100

    test: add space at the end

--- test/parallel/test-repl-reset-event.js
+++ test/parallel/test-repl-reset-event.js
@@ -39,12 +39,12 @@ function testReset(cb) {
   r.context.foo = 42;
   r.on('reset', common.mustCall(function(context) {
     assert(!!context, 'REPL did not emit a context with reset event');
-    assert.strictEqual(context, r.context, 'REPL emitted incorrect context.' +
+    assert.strictEqual(context, r.context, 'REPL emitted incorrect context. ' +
     `context is ${util.inspect(context)}, expected ${util.inspect(r.context)}`);
     assert.strictEqual(
       context.foo,
       undefined,
-      'REPL emitted the previous context, and is not using global as context.' +
+      'REPL emitted the previous context, and is not using global as context. ' +
       `context.foo is ${context.foo}, expected undefined.`
     );
     context.foo = 42;
@@ -64,7 +64,7 @@ function testResetGlobal() {
     assert.strictEqual(
       context.foo,
       42,
-      '"foo" property is different from REPL using global as context.' +
+      '"foo" property is different from REPL using global as context. ' +
       `context.foo is ${context.foo}, expected 42.`
     );
   }));
