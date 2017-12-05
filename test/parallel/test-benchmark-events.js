394ace63161da3675b58bd9fd6a168efe843db51
Rich Trott <rtrott@gmail.com>
2017-12-05 09:19:31 -0800

    test: fix flaky test-benchmark-events
    Make sure each benchmark file only runs one combination of options.
    Allow for zero iterations for short benchmark.

--- test/parallel/test-benchmark-events.js
+++ test/parallel/test-benchmark-events.js
@@ -4,4 +4,6 @@ require('../common');
 
 const runBenchmark = require('../common/benchmark');
 
-runBenchmark('events', ['n=1']);
+runBenchmark('events',
+             ['argc=0', 'listeners=1', 'n=1'],
+             { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
