aada57b893933d771449de346049c6a026921f44
Jamie Davis <davisjam@vt.edu>
2017-12-07 12:44:42 -0500

    test: add test description to fs.readFile tests
    PR-URL: https://github.com/nodejs/node/pull/17610
    Refs: https://github.com/nodejs/node/pull/17054#discussion_r155406755
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Evan Lucas <evanlucas@me.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
    Reviewed-By: Jon Moss <me@jonathanmoss.me>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-fs-readfile-zero-byte-liar.js
+++ test/parallel/test-fs-readfile-zero-byte-liar.js
@@ -21,6 +21,9 @@
 
 'use strict';
 const common = require('../common');
+
+// Test that readFile works even when stat returns size 0.
+
 const assert = require('assert');
 const fs = require('fs');
 
