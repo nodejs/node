nodejs/node

commit 90a43906ab5ca8bdf154fb7644d70da9e3acdf00

repl: show proxies as Proxy objects
Before this commit they transparently invoked their magic methods but
that sometimes throws confusing exceptions with misbehaving proxies.

This change is not wholly uncontroversial but we can always change the
default if necessary.  Let's see how it goes.

Fixes: https://github.com/nodejs/node/issues/16483
PR-URL: https://github.com/nodejs/node/pull/16485
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
Reviewed-By: James M Snell <jasnell@gmail.com>

lib/repl.js
@@ -100,7 +100,9 @@ function hasOwnProperty(obj, prop) {
 
 // Can overridden with custom print functions, such as `probe` or `eyes.js`.
 // This is the default "writer" value if none is passed in the REPL options.
-exports.writer = util.inspect;
+const writer = exports.writer = (obj) => util.inspect(obj, writer.options);
+writer.options =
+    Object.assign(util.inspect.defaultOptions, { showProxy: true });
 
 exports._builtinLibs = internalModule.builtinLibs;
 
@@ -373,11 +375,10 @@ function REPLServer(prompt,
   }
   self.useColors = !!options.useColors;
 
-  if (self.useColors && self.writer === util.inspect) {
+  if (self.useColors && self.writer === writer) {
     // Turn on ANSI coloring.
-    self.writer = function(obj, showHidden, depth) {
-      return util.inspect(obj, showHidden, depth, true);
-    };
+    self.writer = (obj) => util.inspect(obj, self.writer.options);
+    self.writer.options = Object.assign(writer.options, { colors: true });
   }
 
   function filterInternalStackFrames(error, structuredStack) {

test/parallel/test-repl.js
@@ -413,6 +413,17 @@ function error_test() {
       expect: `${prompt_multiline}${prompt_multiline}undefined\n${prompt_unix}`
     },
 
+    // https://github.com/nodejs/node/issues/16483
+    {
+      client: client_unix, send: 'new Proxy({x:42}, {get(){throw null}});',
+      expect: `Proxy [ { x: 42 }, { get: [Function: get] } ]\n${prompt_unix}`
+    },
+    {
+      client: client_unix,
+      send: 'repl.writer.options.showProxy = false, new Proxy({x:42}, {});',
+      expect: `{ x: 42 }\n${prompt_unix}`
+    },
+
     // Newline within template string maintains whitespace.
     { client: client_unix, send: '`foo \n`',
       expect: `${prompt_multiline}'foo \\n'\n${prompt_unix}` },

commit e8eeacd72a3d38b69329804429d52cd77a811a3d

repl: fix util.inspect() coloring regression
The `Object.assign()` calls introduced in commit 90a4390 ("repl: show
proxies as Proxy objects") mutated their first argument, causing the
`{ colors: true }` option from the REPL to leak over into the global
`util.inspect.defaultOptions` object.

Refs: https://github.com/nodejs/node/pull/16485#issuecomment-350428638

lib/repl.js
@@ -103,7 +103,7 @@ function hasOwnProperty(obj, prop) {
 // This is the default "writer" value if none is passed in the REPL options.
 const writer = exports.writer = (obj) => util.inspect(obj, writer.options);
 writer.options =
-    Object.assign(util.inspect.defaultOptions, { showProxy: true });
+    Object.assign({}, util.inspect.defaultOptions, { showProxy: true });
 
 exports._builtinLibs = internalModule.builtinLibs;
 
@@ -470,7 +470,7 @@ function REPLServer(prompt,
   if (self.useColors && self.writer === writer) {
     // Turn on ANSI coloring.
     self.writer = (obj) => util.inspect(obj, self.writer.options);
-    self.writer.options = Object.assign(writer.options, { colors: true });
+    self.writer.options = Object.assign({}, writer.options, { colors: true });
   }
 
   function filterInternalStackFrames(error, structuredStack) {

test/parallel/test-repl-colors.js
@@ -1,0 +1,31 @@
+/* eslint-disable quotes */
+'use strict';
+require('../common');
+const { Duplex } = require('stream');
+const { inspect } = require('util');
+const { strictEqual } = require('assert');
+const { REPLServer } = require('repl');
+
+let output = '';
+
+const inout = new Duplex({ decodeStrings: false });
+inout._read = function() {
+  this.push('util.inspect("string")\n');
+  this.push(null);
+};
+inout._write = function(s, _, cb) {
+  output += s;
+  cb();
+};
+
+const repl = new REPLServer({ input: inout, output: inout, useColors: true });
+
+process.on('exit', function() {
+  // https://github.com/nodejs/node/pull/16485#issuecomment-350428638
+  // The color setting of the REPL should not have leaked over into
+  // the color setting of `util.inspect.defaultOptions`.
+  strictEqual(output.includes(`'\\'string\\''`), true);
+  strictEqual(output.includes(`'\u001b[32m\\'string\\'\u001b[39m'`), false);
+  strictEqual(inspect.defaultOptions.colors, false);
+  strictEqual(repl.writer.options.colors, true);
+});