813f6d0a9cc35d29e1daa6f1d011b96180d83728
Weijia Wang <381152119@qq.com>
2017-12-12 09:27:35 +0800

    util: improve error message of _errnoException
    The usage of ERR_INVALID_ARG_TYPE in _errnoException
    is a little inappropriate. This change is to improve it.

--- test/parallel/test-uv-errno.js
+++ test/parallel/test-uv-errno.js
@@ -20,12 +20,24 @@ keys.forEach((key) => {
   });
 });
 
-[0, 1, 'test', {}, [], Infinity, -Infinity, NaN].forEach((key) => {
+['test', {}, []].forEach((key) => {
   common.expectsError(
     () => util._errnoException(key),
     {
       code: 'ERR_INVALID_ARG_TYPE',
       type: TypeError,
-      message: 'The "err" argument must be of type negative number'
+      message: 'The "err" argument must be of type number. ' +
+               `Received type ${typeof key}`
+    });
+});
+
+[0, 1, Infinity, -Infinity, NaN].forEach((key) => {
+  common.expectsError(
+    () => util._errnoException(key),
+    {
+      code: 'ERR_VALUE_OUT_OF_RANGE',
+      type: RangeError,
+      message: 'The value of "err" must be a negative integer. ' +
+               `Received "${key}"`
     });
 });
