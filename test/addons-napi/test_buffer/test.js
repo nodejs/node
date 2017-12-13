412cc177485f8c4835b2ecf9263f063bf649b21a
Rich Trott <rtrott@gmail.com>
2017-12-12 21:20:47 -0800

    test: remove literals that obscure assert messages
    Remove string literals as messages to `assert.strictEqual()`. They can
    be misleading here (where perhaps the reason an assertino failed isn't
    that the deleter wasn't called but rather was called too many times.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/19447
    PR-URL: https://github.com/nodejs/node/pull/17642
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Jon Moss <me@jonathanmoss.me>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/addons-napi/test_buffer/test.js
+++ test/addons-napi/test_buffer/test.js
@@ -9,14 +9,13 @@ assert.strictEqual(binding.newBuffer().toString(), binding.theText);
 assert.strictEqual(binding.newExternalBuffer().toString(), binding.theText);
 console.log('gc1');
 global.gc();
-assert.strictEqual(binding.getDeleterCallCount(), 1, 'deleter was not called');
+assert.strictEqual(binding.getDeleterCallCount(), 1);
 assert.strictEqual(binding.copyBuffer().toString(), binding.theText);
 
 let buffer = binding.staticBuffer();
-assert.strictEqual(binding.bufferHasInstance(buffer), true,
-                   'buffer type checking fails');
+assert.strictEqual(binding.bufferHasInstance(buffer), true);
 assert.strictEqual(binding.bufferInfo(buffer), true);
 buffer = null;
 global.gc();
 console.log('gc2');
-assert.strictEqual(binding.getDeleterCallCount(), 2, 'deleter was not called');
+assert.strictEqual(binding.getDeleterCallCount(), 2);
