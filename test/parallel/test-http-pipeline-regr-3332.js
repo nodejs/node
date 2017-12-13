de1743e9dbac7ec7e26fe48f9e6206de31bf5223
sreepurnajasti <sreepurna.jasti@gmail.com>
2017-12-13 14:53:03 +0530

    test: Use countdown in test file
    Fixes: https://github.com/nodejs/node/issues/17169
    
    PR-URL: https://github.com/nodejs/node/pull/17646
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>

--- test/parallel/test-http-pipeline-regr-3332.js
+++ test/parallel/test-http-pipeline-regr-3332.js
@@ -1,40 +1,27 @@
 'use strict';
 require('../common');
-const assert = require('assert');
 const http = require('http');
 const net = require('net');
+const Countdown = require('../common/countdown');
 
 const big = Buffer.alloc(16 * 1024, 'A');
 
 const COUNT = 1e4;
 
-let received = 0;
+const countdown = new Countdown(COUNT, () => {
+  server.close();
+  client.end();
+});
 
 let client;
 const server = http.createServer(function(req, res) {
   res.end(big, function() {
-    if (++received === COUNT) {
-      server.close();
-      client.end();
-    }
+    countdown.dec();
   });
 }).listen(0, function() {
   const req = new Array(COUNT + 1).join('GET / HTTP/1.1\r\n\r\n');
   client = net.connect(this.address().port, function() {
     client.write(req);
   });
-
-  // Just let the test terminate instead of hanging
-  client.on('close', function() {
-    if (received !== COUNT)
-      server.close();
-  });
   client.resume();
-});
-
-process.on('exit', function() {
-  // The server should pause connection on pipeline flood, but it shoul still
-  // resume it and finish processing the requests, when its output queue will
-  // be empty again.
-  assert.strictEqual(received, COUNT);
 });
