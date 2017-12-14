010616d6eccbf5e80db40b11852607cbfd0ecd1d
XadillaX <i@2333.moe>
2017-12-14 12:40:32 +0800

    http: add rawPacket in err of `clientError` event
    The `rawPacket` is the current buffer that just parsed. Adding this
    buffer to the error object of `clientError` event is to make it possible
    that developers can log the broken packet.
    
    PR-URL: https://github.com/nodejs/node/pull/17672
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-http-server-client-error.js
+++ test/parallel/test-http-server-client-error.js
@@ -10,6 +10,12 @@ const server = http.createServer(common.mustCall(function(req, res) {
 }));
 
 server.on('clientError', common.mustCall(function(err, socket) {
+  assert.strictEqual(err instanceof Error, true);
+  assert.strictEqual(err.code, 'HPE_INVALID_METHOD');
+  assert.strictEqual(err.bytesParsed, 1);
+  assert.strictEqual(err.message, 'Parse Error');
+  assert.strictEqual(err.rawPacket.toString(), 'Oopsie-doopsie\r\n');
+
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 
   server.close();
