7f65b0b038edb4030f403dc1bf63bb83df0fa002
Ruben Bridgewater <ruben@bridgewater.de>
2017-12-18 00:26:28 -0300

    fixup - address comment

--- benchmark/misc/arguments.js
+++ benchmark/misc/arguments.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const { createBenchmark } = require('../common.js');
-const { Writable } = require('stream');
-const { format, inherits } = require('util');
+const { format } = require('util');
 
 const methods = [
   'restAndSpread',
@@ -16,22 +15,20 @@ const bench = createBenchmark(main, {
   n: [1e6]
 });
 
-const nullStream = createNullStream();
-
 function usingRestAndSpread(...args) {
-  nullStream.write(format(...args));
+  format(...args);
 }
 
 function usingRestAndApply(...args) {
-  nullStream.write(format.apply(null, args));
+  format.apply(null, args);
 }
 
 function usingArgumentsAndApply() {
-  nullStream.write(format.apply(null, arguments));
+  format.apply(null, arguments);
 }
 
 function usingPredefined() {
-  nullStream.write(format('part 1', 'part', 2, 'part 3', 'part', 4));
+  format('part 1', 'part', 2, 'part 3', 'part', 4);
 }
 
 function main({ n, method, args }) {
@@ -59,14 +56,4 @@ function main({ n, method, args }) {
   for (var i = 0; i < n; i++)
     fn('part 1', 'part', 2, 'part 3', 'part', 4);
   bench.end(n);
-}
-
-function createNullStream() {
-  // Used to approximate /dev/null
-  function NullStream() {
-    Writable.call(this, {});
-  }
-  inherits(NullStream, Writable);
-  NullStream.prototype._write = function() {};
-  return new NullStream();
 }
