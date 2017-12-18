c53428f75169aba3caa6ba511833ae544ee5421b
Ben Noordhuis <info@bnoordhuis.nl>
2017-12-18 14:13:06 +0100

    fixup! silence linter

--- test/parallel/test-timer-immediate.js
+++ test/parallel/test-timer-immediate.js
@@ -1,5 +1,6 @@
 'use strict';
 const common = require('../common');
 common.globalCheck = false;
+// eslint-disable-next-line no-global-assign
 process = {};  // Boom!
 setImmediate(common.mustCall());
