16bf5fed69905e64d860c6997a00e0248f5b7cf4
Tobias Nießen <tniessen@tnie.de>
2017-12-18 13:23:46 +0100

    crypto: reuse variable instead of reevaluation
    Backport-PR-URL: https://github.com/nodejs/node/pull/19114
    PR-URL: https://github.com/nodejs/node/pull/17735
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Jon Moss <me@jonathanmoss.me>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -3449,7 +3449,7 @@ void CipherBase::Init(const char* cipher_type,
                     nullptr,
                     reinterpret_cast<unsigned char*>(key),
                     reinterpret_cast<unsigned char*>(iv),
-                    kind_ == kCipher);
+                    encrypt);
 }
 
 
@@ -3518,7 +3518,7 @@ void CipherBase::InitIv(const char* cipher_type,
                     nullptr,
                     reinterpret_cast<const unsigned char*>(key),
                     reinterpret_cast<const unsigned char*>(iv),
-                    kind_ == kCipher);
+                    encrypt);
 }
 
 
