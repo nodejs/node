309ab33b49fc23eb629a46a205552686daab0e7b
Rich Trott <rtrott@gmail.com>
2017-12-21 10:19:54 -0800

    Update test-repl-definecommand.js

--- test/parallel/test-repl-definecommand.js
+++ test/parallel/test-repl-definecommand.js
@@ -35,8 +35,8 @@ r.defineCommand('say2', function() {
 });
 
 inputStream.write('.help\n');
-assert.ok(/\n\.say1     help for say1\n/.test(output),
-          'help for say1 not present');
+assert(/\n\.say1     help for say1\n/.test(output),
+       'help for say1 not present');
 assert(/\n\.say2\n/.test(output), 'help for say2 not present');
 inputStream.write('.say1 node developer\n');
 assert.ok(output.startsWith('hello node developer\n'),
