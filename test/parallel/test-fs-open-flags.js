68fbe08e4d887b693f9a786383065d565569e3b2
Rich Trott <rtrott@gmail.com>
2017-12-21 14:46:46 -0800

    test: do not open fixture files for writing
    Use temp directory for open with `O_DSYNC` (which indicates a write may
    occur) rather than `fixtures` directory. Additionally, test can be run
    on macOS so allow that in addition to Linux.
    
    PR-URL: https://github.com/nodejs/node/pull/17810
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jon Moss <me@jonathanmoss.me>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-fs-open-flags.js
+++ test/parallel/test-fs-open-flags.js
@@ -22,8 +22,12 @@
 // Flags: --expose_internals
 'use strict';
 const common = require('../common');
+
+const fixtures = require('../common/fixtures');
+
 const assert = require('assert');
 const fs = require('fs');
+const path = require('path');
 
 const O_APPEND = fs.constants.O_APPEND || 0;
 const O_CREAT = fs.constants.O_CREAT || 0;
@@ -79,9 +83,10 @@ assert.throws(
   /^Error: Unknown file open flag: null$/
 );
 
-if (common.isLinux) {
-  const file = `${__dirname}/../fixtures/a.js`;
-
+if (common.isLinux || common.isOSX) {
+  common.refreshTmpDir();
+  const file = path.join(common.tmpDir, 'a.js');
+  fs.copyFileSync(fixtures.path('a.js'), file);
   fs.open(file, O_DSYNC, common.mustCall(assert.ifError));
 }
 
