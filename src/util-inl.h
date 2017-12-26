2050c4cde717cf170fc806082e9b82c08bd91d26
Matheus Marchini <matheusdot@gmail.com>
2017-12-25 22:17:25 -0200

    src, test: node internals' postmortem metadata
    Before these changes, only V8 added postmortem metadata to Node's
    binary, limiting the possibilities for debugger's developers to add some
    features that rely on investigating Node's internal structures.
    
    These changes are first steps towards empowering debug tools to
    navigate Node's internal structures. One example of what can be
    achieved with this is shown at nodejs/llnode#122 (a command which prints
    information about handles and requests on the queue for a core dump
    file). Node postmortem metadata are prefixed with nodedbg_.
    
    This also adds tests to validate if all postmortem metadata are
    calculated correctly, plus some documentation on what is postmortem
    metadata and a few care to be taken to avoid breaking it.
    
    Ref: https://github.com/nodejs/llnode/pull/122
    Ref: https://github.com/nodejs/post-mortem/issues/46
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/19176
    PR-URL: https://github.com/nodejs/node/pull/14901
    Refs: https://github.com/nodejs/post-mortem/issues/46
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

--- src/util-inl.h
+++ src/util-inl.h
@@ -155,12 +155,16 @@ typename ListHead<T, M>::Iterator ListHead<T, M>::end() const {
 }
 
 template <typename Inner, typename Outer>
+constexpr uintptr_t OffsetOf(Inner Outer::*field) {
+  return reinterpret_cast<uintptr_t>(&(static_cast<Outer*>(0)->*field));
+}
+
+template <typename Inner, typename Outer>
 ContainerOfHelper<Inner, Outer>::ContainerOfHelper(Inner Outer::*field,
                                                    Inner* pointer)
-    : pointer_(reinterpret_cast<Outer*>(
-          reinterpret_cast<uintptr_t>(pointer) -
-          reinterpret_cast<uintptr_t>(&(static_cast<Outer*>(0)->*field)))) {
-}
+    : pointer_(
+        reinterpret_cast<Outer*>(
+            reinterpret_cast<uintptr_t>(pointer) - OffsetOf(field))) {}
 
 template <typename Inner, typename Outer>
 template <typename TypeName>
