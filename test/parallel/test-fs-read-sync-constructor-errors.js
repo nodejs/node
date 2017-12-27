bfae5acd62ba75b634cfb72c7fd17428773b3902
Omar Crisostomo <omarcb@micorreo.upp.edu.mx>
2017-12-27 14:22:12 -0600

    test: add test-fs-read-sync-constructor-errors test
    test added to extend the code coverage testing of the project

---
+++ test/parallel/test-fs-read-sync-constructor-errors.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const common = require('../common');
+const fixtures = require('../common/fixtures');
+const fs = require('fs');
+const filepath = fixtures.path('x.txt');
+const fd = fs.openSync(filepath, 'r');
+const expected = Buffer.from('xyz\n');
+
+common.expectsError(() => {
+  fs.readSync('notnumber',
+              Buffer.allocUnsafe(expected.length),
+              0,
+              expected.length,
+              0);
+}, { code: 'ERR_INVALID_ARG_TYPE', type: TypeError });
+
+common.expectsError(() => {
+  fs.readSync(fd,
+              Buffer.allocUnsafe(expected.length),
+              -1,
+              expected.length,
+              0,
+              common.mustNotCall());
+}, { code: 'ERR_OUT_OF_RANGE', type: RangeError });
+
+common.expectsError(() => {
+  fs.readSync(fd,
+              Buffer.allocUnsafe(expected.length),
+              0,
+              -1,
+              0,
+              common.mustNotCall());
+}, { code: 'ERR_OUT_OF_RANGE', type: RangeError });
