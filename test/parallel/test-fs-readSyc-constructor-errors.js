d7326b245437deb004bda8ac673bcaf78df5e2cc
Omar Crisostomo <omarcb@micorreo.upp.edu.mx>
2017-12-26 22:17:17 -0600

    test: add test-fs-readSync-constructor-errors test
    test added to extend the code coverage testing of the project

---
+++ test/parallel/test-fs-readSyc-constructor-errors.js
@@ -0,0 +1,103 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+const common = require('../common');
+const fixtures = require('../common/fixtures');
+const fs = require('fs');
+const filepath = fixtures.path('x.txt');
+const fd = fs.openSync(filepath, 'r');
+
+const expected = Buffer.from('xyz\n');
+
+//fs.readSync expects incorrect fd parameter
+function testFd(bufferAsync, offset, bufferSync, expected) {
+  common.expectsError(
+    fs.readSync.bind(fd,
+                     bufferAsync,
+                     offset,
+                     expected.length,
+                     0),
+    { code: 'ERR_INVALID_ARG_TYPE', type: TypeError }
+  );
+}
+
+//fs.readSync expects fd parameter out of range
+function testFdRangeOut(bufferAsync, offset, bufferSync, expected) {
+  common.expectsError(
+    fs.readSync.bind(undefined,
+                     -1,
+                     bufferAsync,
+                     offset,
+                     expected.length,
+                     0),
+    { code: 'ERR_OUT_OF_RANGE', type: RangeError }
+  );
+}
+
+//fs.readSync expects offset parameter out of range
+function testOffSet(bufferAsync, offset, length, expected) {
+  common.expectsError(
+    fs.readSync.bind(undefined,
+                     fd,
+                     bufferAsync,
+                     offset,
+                     expected.length,
+                     0),
+    { code: 'ERR_OUT_OF_RANGE', type: RangeError }
+  );
+}
+
+//fs.readSync expects length parameter out of range
+function testLenght(bufferAsync, offset, length) {
+  common.expectsError(
+    fs.readSync.bind(undefined,
+                     fd,
+                     bufferAsync,
+                     offset,
+                     length,
+                     0),
+    { code: 'ERR_OUT_OF_RANGE', type: RangeError }
+  );
+}
+
+//test fd parameter
+testFd(Buffer.allocUnsafe(expected.length),
+       0,
+       Buffer.allocUnsafe(expected.length),
+       expected);
+
+//test fd parameter out of range
+testFdRangeOut(Buffer.allocUnsafe(expected.length),
+               0,
+               Buffer.allocUnsafe(expected.length),
+               expected);
+
+//test offset parameter out of range
+testOffSet(Buffer.allocUnsafe(expected.length),
+           -1,
+           Buffer.allocUnsafe(expected.length),
+           expected);
+
+//test length parameter out of range
+testLenght(Buffer.allocUnsafe(expected.length),
+           0,
+           -1);
