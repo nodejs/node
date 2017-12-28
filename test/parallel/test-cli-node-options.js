d9e85737513b7c197eac5061fbe9432ae3628569
Leko <leko.noor@gmail.com>
2017-12-28 23:29:34 +0900

    test: Not run test in Windows or 32bit

--- test/parallel/test-cli-node-options.js
+++ test/parallel/test-cli-node-options.js
@@ -26,7 +26,8 @@ expect('--v8-pool-size=10', 'B\n');
 expect('--trace-event-categories node', 'B\n');
 expect('--perf-basic-prof', 'B\n');
 
-if (common.isLinux && ['arm', 'x64', 'ia32', 'mips'].includes(process.arch)) {
+const archMatched = ['arm', 'x64', 'mips'].includes(process.arch);
+if (common.isLinux && !common.isWindows && archMatched) {
   // PerfJitLogger is only implemented in Linux.
   expect('--perf-prof', 'B\n');
 }
