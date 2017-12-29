ee7e0c65cb6bd91fc1391a07d85b1afd8b4f96ea
Omar Crisostomo <omarcb@micorreo.upp.edu.mx>
2017-12-28 21:57:24 -0600

    test:parameters of fs read and readsync
    added test-fs-read-sync-constructor-errors and test-fs-read-constructor-errors
    
    tests added to extend the code coverage testing of the project

---
+++ test/parallel/test-fs-read-parameters-errors.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const common = require('../common');
+const fixtures = require('../common/fixtures');
+const fs = require('fs');
+const filepath = fixtures.path('x.txt');
+const fd = fs.openSync(filepath, 'r');
+const expected = Buffer.from('xyz\n');
+
+[true, null, undefined, () => {}, {}].forEach((value) => {
+  common.expectsError(() => {
+    fs.read(value,
+            Buffer.allocUnsafe(expected.length),
+            0,
+            expected.length,
+            0,
+            common.mustNotCall());
+  }, { code: 'ERR_INVALID_ARG_TYPE', type: TypeError });
+});
+
+common.expectsError(() => {
+  fs.read(fd,
+          Buffer.allocUnsafe(expected.length),
+          -1,
+          expected.length,
+          0,
+          common.mustNotCall());
+}, { code: 'ERR_OUT_OF_RANGE', type: RangeError });
+
+common.expectsError(() => {
+  fs.read(fd,
+          Buffer.allocUnsafe(expected.length),
+          0,
+          -1,
+          0,
+          common.mustNotCall());
+}, { code: 'ERR_OUT_OF_RANGE', type: RangeError });
