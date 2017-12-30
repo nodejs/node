88f4bf219de76b4d5745ef23dc2d486a5456662b
Ruben Bridgewater <ruben@bridgewater.de>
2017-12-30 03:56:30 +0100

    benchmark: (process) use destructuring
    PR-URL: https://github.com/nodejs/node/pull/18250
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- benchmark/process/memoryUsage.js
+++ benchmark/process/memoryUsage.js
@@ -5,9 +5,7 @@ const bench = common.createBenchmark(main, {
   n: [1e5]
 });
 
-function main(conf) {
-  const n = +conf.n;
-
+function main({ n }) {
   bench.start();
   for (var i = 0; i < n; i++) {
     process.memoryUsage();
