8b3c23392c24d1e1de5c0135717745afc8c0c05d
Tobias Nießen <tniessen@tnie.de>
2018-01-02 23:32:55 +0100

    test: fix crypto test case to use correct encoding
    The callback would have errored out anyway due to the incorrect
    encoding, and that error is not the point of this test case.
    
    PR-URL: https://github.com/nodejs/node/pull/17956
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-crypto-authenticated.js
+++ test/parallel/test-crypto-authenticated.js
@@ -391,7 +391,7 @@ for (const i in TEST_CASES) {
       assert.strictEqual(msg, test.plain);
     } else {
       // assert that final throws if input data could not be verified!
-      assert.throws(function() { decrypt.final('ascii'); }, errMessages.auth);
+      assert.throws(function() { decrypt.final('hex'); }, errMessages.auth);
     }
   }
 
