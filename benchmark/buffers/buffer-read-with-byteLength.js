bac64baa749b64dcd658b609b503f1ef5d64a0b5
Rich Trott <rtrott@gmail.com>
2018-01-01 20:00:59 -0800

    benchmark: improve buffer.readIntLE()/readIntBE() benchmarks
    Split them into their own benhmark file and use different byteLength
    values.

--- benchmark/buffers/buffer-read-with-byteLength.js
+++ benchmark/buffers/buffer-read-with-byteLength.js
@@ -2,32 +2,21 @@
 const common = require('../common.js');
 
 const types = [
-  'UInt8',
-  'UInt16LE',
-  'UInt16BE',
-  'UInt32LE',
-  'UInt32BE',
-  'Int8',
-  'Int16LE',
-  'Int16BE',
-  'Int32LE',
-  'Int32BE',
-  'FloatLE',
-  'FloatBE',
-  'DoubleLE',
-  'DoubleBE'
+  'IntLE',
+  'IntBE',
 ];
 
 const bench = common.createBenchmark(main, {
   noAssert: ['false', 'true'],
   buffer: ['fast', 'slow'],
   type: types,
-  millions: [1]
+  millions: [1],
+  byteLength: [1, 2, 4, 6]
 });
 
 function main(conf) {
   const noAssert = conf.noAssert === 'true';
-  const len = +conf.millions * 1e6;
+  const len = conf.millions * 1e6;
   const clazz = conf.buf === 'fast' ? Buffer : require('buffer').SlowBuffer;
   const buff = new clazz(8);
   const type = conf.type || 'UInt8';
@@ -36,7 +25,7 @@ function main(conf) {
   buff.writeDoubleLE(0, 0, noAssert);
   const testFunction = new Function('buff', `
     for (var i = 0; i !== ${len}; i++) {
-      buff.${fn}(0, ${JSON.stringify(noAssert)});
+      buff.${fn}(0, ${conf.byteLength}, ${JSON.stringify(noAssert)});
     }
   `);
   bench.start();
