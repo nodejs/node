9cef0603336e8fe39b11fee9e70dc1786f6cda64
Anatoli Papirovski <apapirovski@mac.com>
2018-01-03 11:57:23 -0500

    test: fix flaky http-writable-true-after-close
    PR-URL: https://github.com/nodejs/node/pull/17952
    Fixes: https://github.com/nodejs/node/issues/16321
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-http-writable-true-after-close.js
+++ test/parallel/test-http-writable-true-after-close.js
@@ -12,16 +12,16 @@ let external;
 
 // Proxy server
 const server = createServer(common.mustCall((req, res) => {
+  const listener = common.mustCall(() => {
+    assert.strictEqual(res.writable, true);
+  });
+
+  // on CentOS 5, 'finish' is emitted
+  res.on('finish', listener);
+  // everywhere else, 'close' is emitted
+  res.on('close', listener);
+
   get(`http://127.0.0.1:${internal.address().port}`, common.mustCall((inner) => {
-    const listener = common.mustCall(() => {
-      assert.strictEqual(res.writable, true);
-    });
-
-    // on CentOS 5, 'finish' is emitted
-    res.on('finish', listener);
-    // everywhere else, 'close' is emitted
-    res.on('close', listener);
-
     inner.pipe(res);
   }));
 })).listen(0, () => {
