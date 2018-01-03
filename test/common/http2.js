3dd59e1814c341f86108f657f4111841992de338
James M Snell <jasnell@gmail.com>
2018-01-03 13:34:45 -0800

    http2: verify flood error and unsolicited frames
    * verify protections against ping and settings flooding
    * Strictly handle and verify handling of unsolicited ping and
      settings frame acks.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/18050
    PR-URL: https://github.com/nodejs/node/pull/17969
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- test/common/http2.js
+++ test/common/http2.js
@@ -118,11 +118,21 @@ class HeadersFrame extends Frame {
   }
 }
 
+class PingFrame extends Frame {
+  constructor(ack = false) {
+    const buffers = [Buffer.alloc(8)];
+    super(8, 6, ack ? 1 : 0, 0);
+    buffers.unshift(this[kFrameData]);
+    this[kFrameData] = Buffer.concat(buffers);
+  }
+}
+
 module.exports = {
   Frame,
   DataFrame,
   HeadersFrame,
   SettingsFrame,
+  PingFrame,
   kFakeRequestHeaders,
   kFakeResponseHeaders,
   kClientMagic
