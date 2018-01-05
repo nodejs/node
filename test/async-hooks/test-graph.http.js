cb18aa4676dbedd330b080b513c98a131472ba81
Andreas Madsen <amwebdk@gmail.com>
2018-01-05 23:09:12 +0100

    [squash] prevent race condition

--- test/async-hooks/test-graph.http.js
+++ test/async-hooks/test-graph.http.js
@@ -12,9 +12,9 @@ const server = http.createServer(common.mustCall(function(req, res) {
   res.end();
   this.close(common.mustCall());
 }));
-server.listen(0, common.mustCall());
-
-http.get(`http://127.0.0.1:${server.address().port}`, common.mustCall());
+server.listen(0, common.mustCall(function() {
+  http.get(`http://127.0.0.1:${server.address().port}`, common.mustCall());
+}));
 
 process.on('exit', function() {
   hooks.disable();
@@ -24,12 +24,16 @@ process.on('exit', function() {
     [ { type: 'TCPSERVERWRAP',
         id: 'tcpserver:1',
         triggerAsyncId: null },
-      { type: 'TCPWRAP', id: 'tcp:1', triggerAsyncId: null },
+      { type: 'TCPWRAP', id: 'tcp:1', triggerAsyncId: 'tcpserver:1' },
       { type: 'TCPCONNECTWRAP',
         id: 'tcpconnect:1',
         triggerAsyncId: 'tcp:1' },
-      { type: 'HTTPPARSER', id: 'httpparser:1', triggerAsyncId: null },
-      { type: 'HTTPPARSER', id: 'httpparser:2', triggerAsyncId: null },
+      { type: 'HTTPPARSER',
+        id: 'httpparser:1',
+        triggerAsyncId: 'tcpserver:1' },
+      { type: 'HTTPPARSER',
+        id: 'httpparser:2',
+        triggerAsyncId: 'tcpserver:1' },
       { type: 'TCPWRAP', id: 'tcp:2', triggerAsyncId: 'tcpserver:1' },
       { type: 'Timeout', id: 'timeout:1', triggerAsyncId: 'tcp:2' },
       { type: 'TIMERWRAP', id: 'timer:1', triggerAsyncId: 'tcp:2' },
