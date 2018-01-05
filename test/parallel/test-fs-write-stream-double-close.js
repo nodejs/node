0a4854dbec67e5a40c7a2de3ce7b26fc8ec84b39
Matteo Collina <hello@matteocollina.com>
2018-01-05 10:41:14 +0100

    fs: guarantee order of callbacks in ws.close
    Refactor WriteStream.prototype.close and WriteStream.prototype._destroy
    to always call the callback passed to close in order. Protects from
    calling .close() without a callback.
    
    Fixes: https://github.com/nodejs/node/issues/17951
    See: https://github.com/nodejs/node/pull/15407

--- test/parallel/test-fs-write-stream-double-close.js
+++ test/parallel/test-fs-write-stream-double-close.js
@@ -1,12 +1,45 @@
 'use strict';
 
 const common = require('../common');
+const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
 common.refreshTmpDir();
 
-const s = fs.createWriteStream(path.join(common.tmpDir, 'rw'));
+{
+  const s = fs.createWriteStream(path.join(common.tmpDir, 'rw'));
 
-s.close(common.mustCall());
-s.close(common.mustCall());
+  s.close(common.mustCall());
+  s.close(common.mustCall());
+}
+
+{
+  const s = fs.createWriteStream(path.join(common.tmpDir, 'rw2'));
+
+  let emits = 0;
+  s.on('close', () => {
+    emits++;
+  });
+
+  s.close(common.mustCall(() => {
+    assert.strictEqual(emits, 1);
+    s.close(common.mustCall(() => {
+      assert.strictEqual(emits, 1);
+    }));
+    process.nextTick(() => {
+      s.close(common.mustCall(() => {
+        assert.strictEqual(emits, 1);
+      }));
+    });
+  }));
+}
+
+{
+  const s = fs.createWriteStream(path.join(common.tmpDir, 'rw'), {
+    autoClose: false
+  });
+
+  s.close(common.mustCall());
+  s.close(common.mustCall());
+}
