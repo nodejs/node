1122723f1cdd261648bfd1e04d6ed9be97136cff
Andreas Madsen <amwebdk@gmail.com>
2018-01-05 16:40:18 +0100

    trace_events: stop tracing agent in process.exit()

--- src/node.cc
+++ src/node.cc
@@ -2029,6 +2029,7 @@ static void WaitForInspectorDisconnect(Environment* env) {
 
 static void Exit(const FunctionCallbackInfo<Value>& args) {
   WaitForInspectorDisconnect(Environment::GetCurrent(args));
+  v8_platform.StopTracingAgent();
   exit(args[0]->Int32Value());
 }
 
