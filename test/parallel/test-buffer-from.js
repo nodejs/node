3299a1a19bdd3adf97c2ccc5e736564f6822f964
sreepurnajasti <sreepurna.jasti@gmail.com>
2018-01-08 14:31:26 +0530

    test: remove trivial buffer imports
    Refs: #13836
    
    PR-URL: https://github.com/nodejs/node/pull/18034
    Refs: https://github.com/nodejs/node/issues/13836
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Khaidi Chu <i@2333.moe>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-buffer-from.js
+++ test/parallel/test-buffer-from.js
@@ -2,7 +2,6 @@
 
 const common = require('../common');
 const { deepStrictEqual, throws } = require('assert');
-const { Buffer } = require('buffer');
 const { runInNewContext } = require('vm');
 
 const checkString = 'test';
