1b0d9795b6b738bdc27880a71764d3fe86e68e45
sreepurnajasti <sreepurna.jasti@gmail.com>
2018-01-08 14:31:26 +0530

    test: remove trivial buffer imports
    Refs: #13836
    
    PR-URL: https://github.com/nodejs/node/pull/18034
    Refs: https://github.com/nodejs/node/issues/13836
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Khaidi Chu <i@2333.moe>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-buffer-over-max-length.js
+++ test/parallel/test-buffer-over-max-length.js
@@ -3,7 +3,6 @@ const common = require('../common');
 const assert = require('assert');
 
 const buffer = require('buffer');
-const Buffer = buffer.Buffer;
 const SlowBuffer = buffer.SlowBuffer;
 
 const kMaxLength = buffer.kMaxLength;
