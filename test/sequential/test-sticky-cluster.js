0a3aaee67959cb49811dfe02f9c5289ddd1b2988
wallet77 <wallet77@gmail.com>
2018-01-10 18:25:07 +0100

    cluster : add sticky sessions algorithm
    By default, cluster uses round robin algorithm to route a request
    to a worker.
    However, sticky session feature enables the load balancer to bind
    a user's session to a specific worker.

---
+++ test/sequential/test-sticky-cluster.js
@@ -0,0 +1,56 @@
+'use strict';
+const common = require('../common');
+
+const net = require('net');
+const cluster = require('cluster');
+
+const assert = require('assert');
+
+cluster.schedulingPolicy = cluster.SCHED_STICKY;
+//cluster.schedulingPolicy = cluster.SCHED_RR;
+
+const nbConnectionsPerWorker = 10;
+
+if (cluster.isMaster) {
+
+  let clusterId;
+  let nbMessages = 0;
+  const nbWorkers = 3;
+
+  for (let i = 0; i < nbWorkers; i++) {
+    const worker = cluster.fork({ id: i + 1 });
+
+    worker.on('message', function(msg) {
+
+      // save first worker's id
+      // all connections should land to this worker (same IP)
+      if (!clusterId) {
+        clusterId = this.id;
+      } else {
+        // check if current worker is the same for the first any connection
+        assert.strictEqual(this.id, clusterId);
+      }
+      nbMessages++;
+
+      // when all connections are done
+      if (nbMessages === nbWorkers * nbConnectionsPerWorker) {
+        cluster.disconnect();
+        process.exit(0);
+      }
+    });
+  }
+} else {
+  const server = net.createServer();
+
+  server.on('connection', function(socket) {
+    process.send('connection');
+  });
+
+  server.on('listening', function() {
+    for (let i = 0; i < nbConnectionsPerWorker; i++) {
+      net.connect(common.PORT, common.localhostIPv4);
+    }
+  });
+
+  server.listen(common.PORT, common.localhostIPv4);
+}
