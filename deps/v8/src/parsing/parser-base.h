2785cc5c21f5217b812d60d88d475d3beb0ea05e
Michaël Zasso <targos@protonmail.com>
2018-01-11 09:58:50 +0100

    deps: cherry-pick c3458a8 from upstream V8
    Original commit message:
    
        [parser] Add new FunctionNameInferrer state before parsing param
    
        Create new state before parsing FormalParameter because we don't
        want to use any of the parameters as an inferred function name.
    
        Previously the stacktrace was:
          test.js:3: Error: boom
              throw new Error('boom');
              ^
          Error: boom
              at param (test.js:3:11)
              at test.js:4:5
              at test.js:6:3
    
        The stacktrace with this patch:
          test.js:3: Error: boom
              throw new Error('boom');
              ^
          Error: boom
              at test.js:3:11
              at test.js:4:5
              at test.js:6:3
    
        Bug: v8:6822, v8:6513
        Change-Id: Ifbadc660fc4e85248af405acd67c025f11662bd4
        Reviewed-on: https://chromium-review.googlesource.com/742657
        Reviewed-by: Adam Klein <adamk@chromium.org>
        Commit-Queue: Sathya Gunasekaran <gsathya@chromium.org>
        Cr-Commit-Position: refs/heads/master@{#49042}
    
    PR-URL: https://github.com/nodejs/node/pull/18060
    Backport-PR-URL: https://github.com/nodejs/node/pull/16413
    Fixes: https://github.com/nodejs/node/issues/15386
    Refs: https://github.com/v8/v8/commit/c3458a86722d735ef4c4e31f9bcaa966e5093e47
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- deps/v8/src/parsing/parser-base.h
+++ deps/v8/src/parsing/parser-base.h
@@ -3634,6 +3634,7 @@ void ParserBase<Impl>::ParseFormalParameter(FormalParametersT* parameters,
   //   BindingElement[?Yield, ?GeneratorParameter]
   bool is_rest = parameters->has_rest;
 
+  FuncNameInferrer::State fni_state(fni_);
   ExpressionT pattern = ParsePrimaryExpression(CHECK_OK_CUSTOM(Void));
   ValidateBindingPattern(CHECK_OK_CUSTOM(Void));
 
