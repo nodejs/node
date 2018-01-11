eaa30e49476123a63f826a66436e680bee7b6d1b
Tobias Nießen <tniessen@tnie.de>
2018-01-11 17:32:05 +0100

    test: simplify loadDHParam in TLS test
    PR-URL: https://github.com/nodejs/node/pull/18103
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-tls-client-getephemeralkeyinfo.js
+++ test/parallel/test-tls-client-getephemeralkeyinfo.js
@@ -6,7 +6,6 @@ const fixtures = require('../common/fixtures');
 
 const assert = require('assert');
 const tls = require('tls');
-const fs = require('fs');
 
 const key = fixtures.readKey('agent2-key.pem');
 const cert = fixtures.readKey('agent2-cert.pem');
@@ -15,9 +14,7 @@ let ntests = 0;
 let nsuccess = 0;
 
 function loadDHParam(n) {
-  let path = fixtures.fixturesDir;
-  if (n !== 'error') path += '/keys';
-  return fs.readFileSync(`${path}/dh${n}.pem`);
+  return fixtures.readKey(`dh${n}.pem`);
 }
 
 const cipherlist = {
