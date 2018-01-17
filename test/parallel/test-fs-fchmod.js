6cd50ac90d3dc676ba1fd4b82b175d35293ce9ad
Luca Maraschi <luca.maraschi@gmail.com>
2018-01-17 15:30:03 -0800

    test: added input validation test for fchmod
    Added a test to ensure input validation for FD and mode for fs.fchmod.
    Removed check for values lower than 0 for `mode` as it's already checked
    by `validateUint32`.

---
+++ test/parallel/test-fs-fchmod.js
@@ -0,0 +1,67 @@
+'use strict';
+const common = require('../common');
+const fs = require('fs');
+
+// This test ensures that input for fchmod is valid, testing for valid
+// inputs for fd and mode
+
+// Check input type
+['', false, null, undefined, {}, [], Infinity, -1].forEach((i) => {
+  common.expectsError(
+    () => fs.fchmod(i),
+    {
+      code: 'ERR_INVALID_ARG_TYPE',
+      type: TypeError,
+      message: 'The "fd" argument must be of type integer'
+    }
+  );
+  common.expectsError(
+    () => fs.fchmodSync(i),
+    {
+      code: 'ERR_INVALID_ARG_TYPE',
+      type: TypeError,
+      message: 'The "fd" argument must be of type integer'
+    }
+  );
+
+  common.expectsError(
+    () => fs.fchmod(1, i),
+    {
+      code: 'ERR_INVALID_ARG_TYPE',
+      type: TypeError,
+      message: 'The "mode" argument must be of type integer'
+    }
+  );
+  common.expectsError(
+    () => fs.fchmodSync(1, i),
+    {
+      code: 'ERR_INVALID_ARG_TYPE',
+      type: TypeError,
+      message: 'The "mode" argument must be of type integer'
+    }
+  );
+});
+
+// Check for mode values range
+const modeUpperBoundaryValue = 0o777;
+common.mustCall(() => fs.fchmod(1, modeUpperBoundaryValue);
+common.mustCall(() => fs.fchmodSync(1, modeUpperBoundaryValue);
+
+// umask of 0o777 is equal to 775
+const modeOutsideUpperBoundValue = 776;
+common.expectsError(
+  () => fs.fchmod(1, i),
+  {
+    code: 'ERR_OUT_OF_RANGE',
+    type: RangeError,
+    message: 'The value of "mode" is out of range.'
+  }
+);
+common.expectsError(
+  () => fs.fchmodSync(1, i),
+  {
+    code: 'ERR_OUT_OF_RANGE',
+    type: RangeError,
+    message: 'The value of "mode" is out of range.'
+  }
+);
