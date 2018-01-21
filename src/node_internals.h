bd6f50b4070f97843472da904af802b50acc21bd
Joyee Cheung <joyeec9h3@gmail.com>
2018-01-21 14:16:47 +0800

    fs: use AliasedBuffer for fs_stats_field_array

--- src/node_internals.h
+++ src/node_internals.h
@@ -292,7 +292,8 @@ v8::Maybe<bool> ProcessEmitDeprecationWarning(Environment* env,
                                               const char* warning,
                                               const char* deprecation_code);
 
-void FillStatsArray(double* fields, const uv_stat_t* s);
+void FillStatsArray(AliasedBuffer<double, v8::Float64Array>* fields_ptr,
+                    const uv_stat_t* s, int offset = 0);
 
 void SetupProcessObject(Environment* env,
                         int argc,
