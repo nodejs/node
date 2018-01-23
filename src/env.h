1385daf4a1c6f76d7e8e347fb2a2529e1acf6882
Gabriel Schulhof <gabriel.schulhof@intel.com>
2018-01-22 23:13:02 -0500

    n-api: implement wrapping using private properties
    PR-URL: https://github.com/nodejs/node/pull/18311
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Fixes: https://github.com/nodejs/node/issues/14367

--- src/env.h
+++ src/env.h
@@ -91,6 +91,8 @@ class ModuleWrap;
   V(decorated_private_symbol, "node:decorated")                               \
   V(npn_buffer_private_symbol, "node:npnBuffer")                              \
   V(selected_npn_buffer_private_symbol, "node:selectedNpnBuffer")             \
+  V(napi_env, "node:napi:env")                                                \
+  V(napi_wrapper, "node:napi:wrapper")                                        \
 
 // Strings are per-isolate primitives but Environment proxies them
 // for the sake of convenience.  Strings should be ASCII-only.
