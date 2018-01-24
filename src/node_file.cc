f5e287ba208b8d9eed3c850577ff9a46d9935d9a
Joyee Cheung <joyeec9h3@gmail.com>
2018-01-24 09:37:52 +0800

    fs: throw errors from fs.fdatasyncSync in JS
    PR-URL: https://github.com/nodejs/node/pull/18348
    Refs: https://github.com/nodejs/node/issues/18106
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_file.cc
+++ src/node_file.cc
@@ -735,16 +735,21 @@ static void FTruncate(const FunctionCallbackInfo<Value>& args) {
 static void Fdatasync(const FunctionCallbackInfo<Value>& args) {
   Environment* env = Environment::GetCurrent(args);
 
+  const int argc = args.Length();
+  CHECK_GE(argc, 2);
+
   CHECK(args[0]->IsInt32());
+  const int fd = args[0]->As<Int32>()->Value();
 
-  int fd = args[0]->Int32Value();
-
-  if (args[1]->IsObject()) {
-    CHECK_EQ(args.Length(), 2);
+  if (args[1]->IsObject()) {  // fdatasync(fd, req)
+    CHECK_EQ(argc, 2);
     AsyncCall(env, args, "fdatasync", UTF8, AfterNoArgs,
               uv_fs_fdatasync, fd);
-  } else {
-    SYNC_CALL(fdatasync, 0, fd)
+  } else {  // fdatasync(fd, undefined, ctx)
+    CHECK_EQ(argc, 3);
+    fs_req_wrap req_wrap;
+    SyncCall(env, args[2], &req_wrap, "fdatasync",
+             uv_fs_fdatasync, fd);
   }
 }
 
