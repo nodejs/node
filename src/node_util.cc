6786f419ed9a7e4d0ff7bb058479a1a8c251cbb7
Anna Henningsen <anna@addaleax.net>
2018-01-27 22:01:32 +0100

    util: introduce `util.types.is[…]` type checks
    Provide public APIs for native typechecking that is actually useful.
    The motivation for this is providing alternatives to userland
    modules that would currently rely on `process.binding('util')`.

--- src/node_util.cc
+++ src/node_util.cc
@@ -17,40 +17,6 @@ using v8::Proxy;
 using v8::String;
 using v8::Value;
 
-
-#define VALUE_METHOD_MAP(V)                                                   \
-  V(isArrayBuffer, IsArrayBuffer)                                             \
-  V(isArrayBufferView, IsArrayBufferView)                                     \
-  V(isAsyncFunction, IsAsyncFunction)                                         \
-  V(isDataView, IsDataView)                                                   \
-  V(isDate, IsDate)                                                           \
-  V(isExternal, IsExternal)                                                   \
-  V(isMap, IsMap)                                                             \
-  V(isMapIterator, IsMapIterator)                                             \
-  V(isNativeError, IsNativeError)                                             \
-  V(isPromise, IsPromise)                                                     \
-  V(isRegExp, IsRegExp)                                                       \
-  V(isSet, IsSet)                                                             \
-  V(isSetIterator, IsSetIterator)                                             \
-  V(isTypedArray, IsTypedArray)                                               \
-  V(isUint8Array, IsUint8Array)
-
-
-#define V(_, ucname) \
-  static void ucname(const FunctionCallbackInfo<Value>& args) {               \
-    CHECK_EQ(1, args.Length());                                               \
-    args.GetReturnValue().Set(args[0]->ucname());                             \
-  }
-
-  VALUE_METHOD_MAP(V)
-#undef V
-
-static void IsAnyArrayBuffer(const FunctionCallbackInfo<Value>& args) {
-  CHECK_EQ(1, args.Length());
-  args.GetReturnValue().Set(
-    args[0]->IsArrayBuffer() || args[0]->IsSharedArrayBuffer());
-}
-
 static void GetPromiseDetails(const FunctionCallbackInfo<Value>& args) {
   // Return undefined if it's not a Promise.
   if (!args[0]->IsPromise())
@@ -190,12 +156,6 @@ void Initialize(Local<Object> target,
                 Local<Value> unused,
                 Local<Context> context) {
   Environment* env = Environment::GetCurrent(context);
-
-#define V(lcname, ucname) env->SetMethod(target, #lcname, ucname);
-  VALUE_METHOD_MAP(V)
-#undef V
-
-  env->SetMethod(target, "isAnyArrayBuffer", IsAnyArrayBuffer);
 
 #define V(name, _)                                                            \
   target->Set(context,                                                        \
