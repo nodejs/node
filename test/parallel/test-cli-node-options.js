85212bb182f555f6d09e0b2f5f78d2d8e19bcef1
Andreas Madsen <amwebdk@gmail.com>
2018-01-31 17:12:09 +0100

    trace_events: add file pattern cli option
    Allow the user to specify the filepath for the trace_events log file
    using a template string.
    
    PR-URL: https://github.com/nodejs/node/pull/18480
    Reviewed-By: Ali Ijaz Sheikh <ofrobots@google.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-cli-node-options.js
+++ test/parallel/test-cli-node-options.js
@@ -25,6 +25,8 @@ expect('--throw-deprecation', 'B\n');
 expect('--zero-fill-buffers', 'B\n');
 expect('--v8-pool-size=10', 'B\n');
 expect('--trace-event-categories node', 'B\n');
+// eslint-disable-next-line no-template-curly-in-string
+expect('--trace-event-file-pattern {pid}-${rotation}.trace_events', 'B\n');
 
 if (!common.isWindows) {
   expect('--perf-basic-prof', 'B\n');
