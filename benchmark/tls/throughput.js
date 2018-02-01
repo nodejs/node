c57018096ced0b6c92328810d3ae36e7086eebf6
Anatoli Papirovski <apapirovski@mac.com>
2018-02-01 08:10:18 -0500

    test: add test for tls benchmarks
    PR-URL: https://github.com/nodejs/node/pull/18489
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- benchmark/tls/throughput.js
+++ benchmark/tls/throughput.js
@@ -45,11 +45,11 @@ function main(conf) {
   };
 
   server = tls.createServer(options, onConnection);
-  setTimeout(done, dur * 1000);
   var conn;
   server.listen(common.PORT, function() {
     const opt = { port: common.PORT, rejectUnauthorized: false };
     conn = tls.connect(opt, function() {
+      setTimeout(done, dur * 1000);
       bench.start();
       conn.on('drain', write);
       write();
