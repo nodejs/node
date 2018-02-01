0cf9d0483fd395d1e4433b0fa4ce6124881ac703
Anatoli Papirovski <apapirovski@mac.com>
2018-02-01 08:10:18 -0500

    test: add test for tls benchmarks
    PR-URL: https://github.com/nodejs/node/pull/18489
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/sequential/test-benchmark-tls.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const common = require('../common');
+
+if (!common.enoughTestMem)
+  common.skip('Insufficient memory for TLS benchmark test');
+
+// Because the TLS benchmarks use hardcoded ports, this should be in sequential
+// rather than parallel to make sure it does not conflict with tests that choose
+// random available ports.
+
+const runBenchmark = require('../common/benchmark');
+
+runBenchmark('tls',
+             [
+               'concurrency=1',
+               'dur=0.1',
+               'n=1',
+               'size=2',
+               'type=asc'
+             ],
+             {
+               NODEJS_BENCHMARK_ZERO_ALLOWED: 1,
+               duration: 0
+             });
