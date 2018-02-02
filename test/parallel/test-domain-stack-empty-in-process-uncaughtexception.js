6bc307ff321c33831d9ecea18752fb130b2d4905
Chin Huang <chhuang@us.ibm.com>
2018-02-02 15:03:07 -0800

    test: add useful info to error msg and refactor
    Add useful info about process.domain to error meesages in the
    uncaughtException event listener and the beforeExit event listener.
    
    Refactor code such as using template literals, and also make sure
    uncaughtException listner is detached after firing once to avoid
    endless loop in case of exception throw in the beforeExit event
    listner.
    
    PR-URL: https://github.com/nodejs/node/pull/18541
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-domain-stack-empty-in-process-uncaughtexception.js
+++ test/parallel/test-domain-stack-empty-in-process-uncaughtexception.js
@@ -6,15 +6,18 @@ const assert = require('assert');
 
 const d = domain.create();
 
-process.on('uncaughtException', common.mustCall(function onUncaught() {
+process.once('uncaughtException', common.mustCall(function onUncaught() {
   assert.strictEqual(
     process.domain, null,
-    'domains stack should be empty in uncaughtException handler');
+    'Domains stack should be empty in uncaughtException handler ' +
+    `but the value of process.domain is ${JSON.stringify(process.domain)}`);
 }));
 
 process.on('beforeExit', common.mustCall(function onBeforeExit() {
-  assert.strictEqual(process.domain, null,
-                     'domains stack should be empty in beforeExit handler');
+  assert.strictEqual(
+    process.domain, null,
+    'Domains stack should be empty in beforeExit handler ' +
+    `but the value of process.domain is ${JSON.stringify(process.domain)}`);
 }));
 
 d.run(function() {
