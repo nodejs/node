6bc4cd5e34fed941ea9373458b233a6d1c49edea
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-02-02 10:35:43 +0100

    test: remove destructor from node_test_fixture
    This commit removes the destructor from node_test_fixture.h which calls
    the TearDown function causing TearDown to be called twice. This also
    allows us to remove the check of the platform_ in TearDown.
    
    Also the Setup/TearDown functions in AliasBufferTest are removed as they
    are not necessary.
    
    PR-URL: https://github.com/nodejs/node/pull/18524
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/cctest/node_test_fixture.h
+++ test/cctest/node_test_fixture.h
@@ -63,10 +63,6 @@ class NodeTestFixture : public ::testing::Test {
  protected:
   v8::Isolate* isolate_;
 
-  ~NodeTestFixture() {
-    TearDown();
-  }
-
   virtual void SetUp() {
     CHECK_EQ(0, uv_loop_init(&current_loop));
     platform_ = new node::NodePlatform(8, nullptr);
@@ -78,7 +74,6 @@ class NodeTestFixture : public ::testing::Test {
   }
 
   virtual void TearDown() {
-    if (platform_ == nullptr) return;
     platform_->Shutdown();
     while (uv_loop_alive(&current_loop)) {
       uv_run(&current_loop, UV_RUN_ONCE);
