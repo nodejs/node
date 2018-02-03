9632966e9e389db8f48be18c3346ce2822780cfd
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-02-03 18:02:57 +0100

    test: introduce SetUpTestCase/TearDownTestCase
    This commit add SetUpTestCase and TearDownTestCase functions that will
    be called once per test case. Currently we only have SetUp/TearDown
    which are called for each test.
    
    This commit moves the initialization and configuration of Node and V8 to
    be done on a per test case basis, but gives each test a new Isolate.
    
    PR-URL: https://github.com/nodejs/node/pull/18558
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/cctest/node_test_fixture.cc
+++ test/cctest/node_test_fixture.cc
@@ -1,3 +1,6 @@
 #include "node_test_fixture.h"
 
-uv_loop_t current_loop;
+uv_loop_t NodeTestFixture::current_loop;
+std::unique_ptr<node::NodePlatform> NodeTestFixture::platform;
+std::unique_ptr<v8::ArrayBuffer::Allocator> NodeTestFixture::allocator;
+v8::Isolate::CreateParams NodeTestFixture::params;
