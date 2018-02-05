42c14c5c17c427aa82722fff41f3e723354239b9
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-02-05 11:20:07 +0100

    src: set thread local env in CreateEnvironment
    This commit set the Environment as a thread local when CreateEnvironment
    is called which is currently not being done. This would lead to a
    segment fault if later node::AtExit is called without specifying the
    environment parameter. This specific issue was reported by Electron.
    
    If I recall correctly, back when this was implemented the motivation was
    that if embedders have multiple environments per isolate they should be
    using the AtExit functions that take an environment. This is not the
    case with Electron which only create a single environment (as far as I
    know), and if a native module calls AtExit this would lead to the
    segment fault.
    
    I was able to reproduce Electron issue and the provided test simulates
    it. I was also able to use this patch and verify that it works for the
    Electron issue as well.
    
    PR-URL: https://github.com/nodejs/node/pull/18573
    Refs: https://github.com/nodejs/node/pull/9163
    Refs: https://github.com/electron/electron/issues/11299
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Matheus Marchini <matheus@sthima.com>

--- src/node.cc
+++ src/node.cc
@@ -4207,11 +4207,8 @@ uv_loop_t* GetCurrentEventLoop(v8::Isolate* isolate) {
 }
 
 
-static uv_key_t thread_local_env;
-
-
 void AtExit(void (*cb)(void* arg), void* arg) {
-  auto env = static_cast<Environment*>(uv_key_get(&thread_local_env));
+  auto env = Environment::GetThreadLocalEnv();
   AtExit(env, cb, arg);
 }
 
@@ -4342,8 +4339,6 @@ inline int Start(Isolate* isolate, IsolateData* isolate_data,
   Local<Context> context = NewContext(isolate);
   Context::Scope context_scope(context);
   Environment env(isolate_data, context);
-  CHECK_EQ(0, uv_key_create(&thread_local_env));
-  uv_key_set(&thread_local_env, &env);
   env.Start(argc, argv, exec_argc, exec_argv, v8_is_profiling);
 
   const char* path = argc > 1 ? argv[1] : nullptr;
@@ -4393,7 +4388,6 @@ inline int Start(Isolate* isolate, IsolateData* isolate_data,
 
   const int exit_code = EmitExit(&env);
   RunAtExit(&env);
-  uv_key_delete(&thread_local_env);
 
   v8_platform.DrainVMTasks(isolate);
   v8_platform.CancelVMTasks(isolate);
