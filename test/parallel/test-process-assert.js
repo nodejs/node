af70bb63dc1c66dbe50a7e56945a8dda4e4f0efa
Ruben Bridgewater <ruben@bridgewater.de>
2018-02-08 23:50:34 +0100

    process: deprecate process.assert()
    This was never documented and the `assert` module should be used
    instead.

--- test/parallel/test-process-assert.js
+++ test/parallel/test-process-assert.js
@@ -2,6 +2,11 @@
 const common = require('../common');
 const assert = require('assert');
 
+common.expectWarning(
+  'DeprecationWarning',
+  'process.assert() is deprecated. Use the `assert` module instead.'
+);
+
 assert.strictEqual(process.assert(1, 'error'), undefined);
 common.expectsError(() => {
   process.assert(undefined, 'errorMessage');
@@ -9,13 +14,11 @@ common.expectsError(() => {
   code: 'ERR_ASSERTION',
   type: Error,
   message: 'errorMessage'
-}
-);
+});
 common.expectsError(() => {
   process.assert(false);
 }, {
   code: 'ERR_ASSERTION',
   type: Error,
   message: 'assertion error'
-}
-);
+});
