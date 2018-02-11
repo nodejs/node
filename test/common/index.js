1bd2161ac18a8b32b76b13758fa8630a1416d405
killagu <killa123@126.com>
2018-02-11 11:50:31 +0800

    tools, test: fix prof polyfill readline
    `node --prof foo.js` may not print the full profile log file, leaving
    the last line broken (for example `tick,`. When that happens, `readline`
    will be stuck in an infinite loop. This patch fixes it.
    
    Also introduced `common.isCPPSymbolsNotMapped` to avoid duplicated code
    on tick-processor tests.
    
    PR-URL: https://github.com/nodejs/node/pull/18641
    Reviewed-By: Khaidi Chu <i@2333.moe>
    Reviewed-By: Matheus Marchini <matheus@sthima.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/common/index.js
+++ test/common/index.js
@@ -823,3 +823,9 @@ exports.firstInvalidFD = function firstInvalidFD() {
   } catch (e) {}
   return fd;
 };
+
+exports.isCPPSymbolsNotMapped = exports.isWindows ||
+                                exports.isSunOS ||
+                                exports.isAIX ||
+                                exports.isLinuxPPCBE ||
+                                exports.isFreeBSD;
