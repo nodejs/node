762231d751816de79f728a12e0fee426ee10e756
juggernaut451 <singhjug1994@gmail.com>
2018-02-11 22:49:22 +0530

    Adding common.mustCall over callbacks

--- test/parallel/test-tls-pause.js
+++ test/parallel/test-tls-pause.js
@@ -40,23 +40,23 @@ const bufSize = 1024 * 1024;
 let sent = 0;
 let received = 0;
 
-const server = tls.Server(options, (socket) => {
+const server = tls.Server(options, common.mustCall((socket) => {
   socket.pipe(socket);
   socket.on('data', (c) => {
     console.error('data', c.length);
   });
-});
+}));
 
 server.listen(0, () => {
   let resumed = false;
   const client = tls.connect({
     port: server.address().port,
     rejectUnauthorized: false
-  }, () => {
+  }, common.mustCall(() => {
     console.error('connected');
     client.pause();
     console.error('paused');
-    let send = (() => {
+    const send = (() => {
       console.error('sending');
       const ret = client.write(Buffer.allocUnsafe(bufSize));
       console.error(`write => ${ret}`);
@@ -72,7 +72,7 @@ server.listen(0, () => {
       client.resume();
       console.error('resumed', client);
     })();
-  });
+  }));
   client.on('data', (data) => {
     console.error('data');
     assert.ok(resumed);
