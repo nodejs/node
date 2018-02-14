nodejs/node

commit de848ac1e0483327a2ce8716c3f8567eaeacb660

repl: refactor tests to not rely on timing
Tests relying on synchronous timing have been migrated to use events.

PR-URL: https://github.com/nodejs/node/pull/17828
Reviewed-By: James M Snell <jasnell@gmail.com>
Reviewed-By: Michaël Zasso <targos@protonmail.com>

lib/internal/repl.js
@@ -8,7 +8,7 @@ const os = require('os');
 const util = require('util');
 const debug = util.debuglog('repl');
 module.exports = Object.create(REPL);
-module.exports.createInternalRepl = createRepl;
+module.exports.createInternalRepl = createInternalRepl;
 
 // XXX(chrisdickinson): The 15ms debounce value is somewhat arbitrary.
 // The debounce is to guard against code pasted into the REPL.
@@ -19,7 +19,7 @@ function _writeToOutput(repl, message) {
   repl._refreshLine();
 }
 
-function createRepl(env, opts, cb) {
+function createInternalRepl(env, opts, cb) {
   if (typeof opts === 'function') {
     cb = opts;
     opts = null;

lib/repl.js
@@ -109,6 +109,11 @@ writer.options = Object.assign({},
 
 exports._builtinLibs = internalModule.builtinLibs;
 
+const sep = '\u0000\u0000\u0000';
+const regExMatcher = new RegExp(`^${sep}(.*)${sep}(.*)${sep}(.*)${sep}(.*)` +
+                                `${sep}(.*)${sep}(.*)${sep}(.*)${sep}(.*)` +
+                                `${sep}(.*)$`);
+
 function REPLServer(prompt,
                     stream,
                     eval_,
@@ -149,6 +154,7 @@ function REPLServer(prompt,
   }
 
   var self = this;
+  replMap.set(self, self);
 
   self._domain = dom || domain.create();
   self.useGlobal = !!useGlobal;
@@ -165,41 +171,6 @@ function REPLServer(prompt,
   self.rli = this;
 
   const savedRegExMatches = ['', '', '', '', '', '', '', '', '', ''];
-  const sep = '\u0000\u0000\u0000';
-  const regExMatcher = new RegExp(`^${sep}(.*)${sep}(.*)${sep}(.*)${sep}(.*)` +
-                                  `${sep}(.*)${sep}(.*)${sep}(.*)${sep}(.*)` +
-                                  `${sep}(.*)$`);
-
-  eval_ = eval_ || defaultEval;
-
-  // Pause taking in new input, and store the keys in a buffer.
-  const pausedBuffer = [];
-  let paused = false;
-  function pause() {
-    paused = true;
-  }
-  function unpause() {
-    if (!paused) return;
-    paused = false;
-    let entry;
-    while (entry = pausedBuffer.shift()) {
-      const [type, payload] = entry;
-      switch (type) {
-        case 'key': {
-          const [d, key] = payload;
-          self._ttyWrite(d, key);
-          break;
-        }
-        case 'close':
-          self.emit('exit');
-          break;
-      }
-      if (paused) {
-        break;
-      }
-    }
-  }
-
   function defaultEval(code, context, file, cb) {
     var err, result, script, wrappedErr;
     var wrappedCmd = false;
@@ -331,7 +302,6 @@ function REPLServer(prompt,
 
       if (awaitPromise && !err) {
         let sigintListener;
-        pause();
         let promise = result;
         if (self.breakEvalOnSigint) {
           const interrupt = new Promise((resolve, reject) => {
@@ -350,7 +320,6 @@ function REPLServer(prompt,
           prioritizedSigintQueue.delete(sigintListener);
 
           finishExecution(undefined, result);
-          unpause();
         }, (err) => {
           // Remove prioritized SIGINT listener if it was not called.
           prioritizedSigintQueue.delete(sigintListener);
@@ -360,7 +329,6 @@ function REPLServer(prompt,
             Object.defineProperty(err, 'stack', { value: '' });
           }
 
-          unpause();
           if (err && process.domain) {
             debug('not recoverable, send to domain');
             process.domain.emit('error', err);
@@ -374,6 +342,36 @@ function REPLServer(prompt,
 
     if (!awaitPromise || err) {
       finishExecution(err, result);
+    }
+  }
+
+  eval_ = eval_ || defaultEval;
+
+  // Pause taking in new input, and store the keys in a buffer.
+  const pausedBuffer = [];
+  let paused = false;
+  function pause() {
+    paused = true;
+  }
+  function unpause() {
+    if (!paused) return;
+    paused = false;
+    let entry;
+    while (entry = pausedBuffer.shift()) {
+      const [type, payload] = entry;
+      switch (type) {
+        case 'key': {
+          const [d, key] = payload;
+          self._ttyWrite(d, key);
+          break;
+        }
+        case 'close':
+          self.emit('exit');
+          break;
+      }
+      if (paused) {
+        break;
+      }
     }
   }
 
@@ -405,6 +403,7 @@ function REPLServer(prompt,
     top.clearBufferedCommand();
     top.lines.level = [];
     top.displayPrompt();
+    unpause();
   });
 
   if (!input && !output) {
@@ -593,6 +592,7 @@ function REPLServer(prompt,
     const evalCmd = self[kBufferedCommandSymbol] + cmd + '\n';
 
     debug('eval %j', evalCmd);
+    pause();
     self.eval(evalCmd, self.context, 'repl', finish);
 
     function finish(e, ret) {
@@ -605,6 +605,7 @@ function REPLServer(prompt,
                                 '(Press Control-D to exit.)\n');
         self.clearBufferedCommand();
         self.displayPrompt();
+        unpause();
         return;
       }
 
@@ -642,6 +643,7 @@ function REPLServer(prompt,
 
       // Display prompt again
       self.displayPrompt();
+      unpause();
     }
   });
 
@@ -724,7 +726,6 @@ exports.start = function(prompt,
                             ignoreUndefined,
                             replMode);
   if (!exports.repl) exports.repl = repl;
-  replMap.set(repl, repl);
   return repl;
 };
 

test/parallel/test-repl-autolibs.js
@@ -29,7 +29,22 @@ const repl = require('repl');
 common.globalCheck = false;
 
 const putIn = new common.ArrayStream();
-repl.start('', putIn, null, true);
+
+
+const replserver = repl.start('', putIn, null, true);
+const callbacks = [];
+const $eval = replserver.eval;
+replserver.eval = function(code, context, file, cb) {
+  const expected = callbacks.shift();
+  return $eval.call(this, code, context, file, (...args) => {
+    try {
+      expected(cb, ...args);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
+  });
+};
 
 test1();
 
@@ -48,6 +63,11 @@ function test1() {
     }
   };
   assert(!gotWrite);
+  callbacks.push(common.mustCall((cb, err, result) => {
+    assert.ifError(err);
+    assert.strictEqual(result, require('fs'));
+    cb(err, result);
+  }));
   putIn.run(['fs']);
   assert(gotWrite);
 }
@@ -66,6 +86,11 @@ function test2() {
   const val = {};
   global.url = val;
   assert(!gotWrite);
+  callbacks.push(common.mustCall((cb, err, result) => {
+    assert.ifError(err);
+    assert.strictEqual(result, val);
+    cb(err, result);
+  }));
   putIn.run(['url']);
   assert(gotWrite);
 }

test/parallel/test-repl-context.js
@@ -27,40 +27,57 @@ function testContext(repl) {
   repl.close();
 }
 
-testContextSideEffects(repl.start({ input: stream, output: stream }));
+const replserver = repl.start({ input: stream, output: stream });
+const callbacks = [];
+const $eval = replserver.eval;
+replserver.eval = function(code, context, file, cb) {
+  const expected = callbacks.shift();
+  return $eval.call(this, code, context, file, (...args) => {
+    try {
+      expected(cb, ...args);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
+  });
+};
+testContextSideEffects(replserver);
 
 function testContextSideEffects(server) {
   assert.ok(!server.underscoreAssigned);
   assert.strictEqual(server.lines.length, 0);
 
   // an assignment to '_' in the repl server
+  callbacks.push(common.mustCall((cb, ...args) => {
+    assert.ok(server.underscoreAssigned);
+    assert.strictEqual(server.last, 500);
+    cb(...args);
+    assert.strictEqual(server.lines.length, 1);
+    assert.strictEqual(server.lines[0], '_ = 500;');
+
+    // use the server to create a new context
+    const context = server.createContext();
+
+    // ensure that creating a new context does not
+    // have side effects on the server
+    assert.ok(server.underscoreAssigned);
+    assert.strictEqual(server.lines.length, 1);
+    assert.strictEqual(server.lines[0], '_ = 500;');
+    assert.strictEqual(server.last, 500);
+
+    // reset the server context
+    server.resetContext();
+    assert.ok(!server.underscoreAssigned);
+    assert.strictEqual(server.lines.length, 0);
+
+    // ensure that assigning to '_' in the new context
+    // does not change the value in our server.
+    assert.ok(!server.underscoreAssigned);
+    vm.runInContext('_ = 1000;\n', context);
+
+    assert.ok(!server.underscoreAssigned);
+    assert.strictEqual(server.lines.length, 0);
+    server.close();
+  }));
   server.write('_ = 500;\n');
-  assert.ok(server.underscoreAssigned);
-  assert.strictEqual(server.lines.length, 1);
-  assert.strictEqual(server.lines[0], '_ = 500;');
-  assert.strictEqual(server.last, 500);
-
-  // use the server to create a new context
-  const context = server.createContext();
-
-  // ensure that creating a new context does not
-  // have side effects on the server
-  assert.ok(server.underscoreAssigned);
-  assert.strictEqual(server.lines.length, 1);
-  assert.strictEqual(server.lines[0], '_ = 500;');
-  assert.strictEqual(server.last, 500);
-
-  // reset the server context
-  server.resetContext();
-  assert.ok(!server.underscoreAssigned);
-  assert.strictEqual(server.lines.length, 0);
-
-  // ensure that assigning to '_' in the new context
-  // does not change the value in our server.
-  assert.ok(!server.underscoreAssigned);
-  vm.runInContext('_ = 1000;\n', context);
-
-  assert.ok(!server.underscoreAssigned);
-  assert.strictEqual(server.lines.length, 0);
-  server.close();
 }

test/parallel/test-repl-end-emits-exit.js
@@ -21,10 +21,7 @@
 
 'use strict';
 const common = require('../common');
-const assert = require('assert');
 const repl = require('repl');
-let terminalExit = 0;
-let regularExit = 0;
 
 // Create a dummy stream that does nothing
 const stream = new common.ArrayStream();
@@ -41,11 +38,10 @@ function testTerminalMode() {
     stream.emit('data', '\u0004');
   });
 
-  r1.on('exit', function() {
+  r1.on('exit', common.mustCall(function() {
     // should be fired from the simulated ^D keypress
-    terminalExit++;
     testRegularMode();
-  });
+  }));
 }
 
 function testRegularMode() {
@@ -59,16 +55,10 @@ function testRegularMode() {
     stream.emit('end');
   });
 
-  r2.on('exit', function() {
+  r2.on('exit', common.mustCall(function() {
     // should be fired from the simulated 'end' event
-    regularExit++;
-  });
+  }));
 }
-
-process.on('exit', function() {
-  assert.strictEqual(terminalExit, 1);
-  assert.strictEqual(regularExit, 1);
-});
 
 
 // start

test/parallel/test-repl-eval-scope.js
@@ -3,21 +3,27 @@ const common = require('../common');
 const assert = require('assert');
 const repl = require('repl');
 
-{
-  const stream = new common.ArrayStream();
-  const options = {
-    eval: common.mustCall((cmd, context) => {
-      assert.strictEqual(cmd, '.scope\n');
-      assert.deepStrictEqual(context, { animal: 'Sterrance' });
-    }),
-    input: stream,
-    output: stream,
-    terminal: true
-  };
+const exitTests = [];
+process.on('exit', () => {
+  for (const test of exitTests) test();
+});
+const CONTEXT = { animal: 'Sterrance' };
+const stream = new common.ArrayStream();
+const options = {
+  eval: common.mustCall((cmd, context) => {
+    // need to escape the domain
+    exitTests.push(common.mustCall(() => {
+      assert.strictEqual(cmd, '.scope');
+      assert.ok(context === CONTEXT);
+    }));
+  }),
+  input: stream,
+  output: stream,
+  terminal: true
+};
 
-  const r = repl.start(options);
-  r.context = { animal: 'Sterrance' };
+const r = repl.start(options);
+r.context = CONTEXT;
 
-  stream.emit('data', '\t');
-  stream.emit('.exit\n');
-}
+stream.emit('data', '\t');
+stream.emit('.exit\n');

test/parallel/test-repl-eval.js
@@ -3,31 +3,31 @@ const common = require('../common');
 const assert = require('assert');
 const repl = require('repl');
 
-{
-  let evalCalledWithExpectedArgs = false;
+const exitTests = [];
+process.on('exit', () => {
+  for (const test of exitTests) test();
+});
+const options = {
+  eval: common.mustCall((cmd, context) => {
+    // Assertions here will not cause the test to exit with an error code
+    // so set a boolean that is checked later instead.
+    exitTests.push(common.mustCall(() => {
+      assert.strictEqual(cmd, 'function f() {}\n');
+      assert.strictEqual(context.foo, 'bar');
+    }));
+  })
+};
 
-  const options = {
-    eval: common.mustCall((cmd, context) => {
-      // Assertions here will not cause the test to exit with an error code
-      // so set a boolean that is checked later instead.
-      evalCalledWithExpectedArgs = (cmd === 'function f() {}\n' &&
-                                    context.foo === 'bar');
-    })
-  };
+const r = repl.start(options);
+r.context = { foo: 'bar' };
 
-  const r = repl.start(options);
-  r.context = { foo: 'bar' };
-
-  try {
-    // Default preprocessor transforms
-    //  function f() {}  to
-    //  var f = function f() {}
-    // Test to ensure that original input is preserved.
-    // Reference: https://github.com/nodejs/node/issues/9743
-    r.write('function f() {}\n');
-  } finally {
-    r.write('.exit\n');
-  }
-
-  assert(evalCalledWithExpectedArgs);
+try {
+  // Default preprocessor transforms
+  //  function f() {}  to
+  //  var f = function f() {}
+  // Test to ensure that original input is preserved.
+  // Reference: https://github.com/nodejs/node/issues/9743
+  r.write('function f() {}\n');
+} finally {
+  r.write('.exit\n');
 }

test/parallel/test-repl-function-definition-edge-case.js
@@ -7,32 +7,47 @@ const stream = require('stream');
 
 common.globalCheck = false;
 
-const r = initRepl();
+const input = new stream();
+input.write = input.pause = input.resume = () => {};
+input.readable = true;
 
-r.input.emit('data', 'function a() { return 42; } (1)\n');
-r.input.emit('data', 'a\n');
-r.input.emit('data', '.exit');
+const output = new stream();
+output.writable = true;
+output.accumulator = [];
 
-const expected = '1\n[Function: a]\n';
-const got = r.output.accumulator.join('');
-assert.strictEqual(got, expected);
+output.write = (data) => output.accumulator.push(data);
 
-function initRepl() {
-  const input = new stream();
-  input.write = input.pause = input.resume = () => {};
-  input.readable = true;
-
-  const output = new stream();
-  output.writable = true;
-  output.accumulator = [];
-
-  output.write = (data) => output.accumulator.push(data);
-
-  return repl.start({
-    input,
-    output,
-    useColors: false,
-    terminal: false,
-    prompt: ''
+const replserver = repl.start({
+  input,
+  output,
+  useColors: false,
+  terminal: false,
+  prompt: ''
+});
+const callbacks = [];
+const $eval = replserver.eval;
+replserver.eval = function(code, context, file, cb) {
+  const expected = callbacks.shift();
+  return $eval.call(this, code, context, file, (...args) => {
+    try {
+      expected(...args);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
+    cb(...args);
   });
-}
+};
+
+callbacks.push(common.mustCall((err, result) => {
+  assert.ifError(err);
+  assert.strictEqual(result, 1);
+}));
+replserver.input.emit('data', 'function a() { return 42; } (1)\n');
+callbacks.push(common.mustCall((err, result) => {
+  assert.ifError(err);
+  assert.strictEqual(typeof result, 'function');
+  assert.strictEqual(result.toString(), 'function a() { return 42; }');
+}));
+replserver.input.emit('data', 'a\n');
+replserver.input.emit('data', '.exit');

commit 92c86fd84d5f9be1a22388dd5ebb91ee8039e839

test: add multiline repl input regression test
This commit adds a regression test for
de848ac1e0483327a2ce8716c3f8567eaeacb660, which broke
multiline input in the REPL.

PR-URL: https://github.com/nodejs/node/pull/18718
Refs: https://github.com/nodejs/node/pull/17828
Refs: https://github.com/nodejs/node/pull/18715
Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

test/parallel/test-repl-multiline.js
@@ -1,0 +1,35 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const repl = require('repl');
+const inputStream = new common.ArrayStream();
+const outputStream = new common.ArrayStream();
+const input = ['var foo = {', '};', 'foo;'];
+let output = '';
+
+outputStream.write = (data) => { output += data.replace('\r', ''); };
+
+const r = repl.start({
+  prompt: '',
+  input: inputStream,
+  output: outputStream,
+  terminal: true,
+  useColors: false
+});
+
+r.on('exit', common.mustCall(() => {
+  const actual = output.split('\n');
+
+  // Validate the output, which contains terminal escape codes.
+  assert.strictEqual(actual.length, 6);
+  assert.ok(actual[0].endsWith(input[0]));
+  assert.ok(actual[1].includes('... '));
+  assert.ok(actual[1].endsWith(input[1]));
+  assert.strictEqual(actual[2], 'undefined');
+  assert.ok(actual[3].endsWith(input[2]));
+  assert.strictEqual(actual[4], '{}');
+  // Ignore the last line, which is nothing but escape codes.
+}));
+
+inputStream.run(input);
+r.close();