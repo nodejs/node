70190f9fab3ed127e14247d46f53fa46bf801d33
Myles Borins <mylesborins@google.com>
2018-02-15 18:47:10 -0500

    test: add common.skipIfEslintMissing
    We've added a number of tests that hook into ESLint which can error
    when running the test suite with the distributed tarball. This PR
    adds a new test helper `common.skipIfEslintMissing` and will skip
    remaining tests in a file when `ESLint` is not available at
    `tools/node_modules/eslint`

--- test/common/index.js
+++ test/common/index.js
@@ -493,6 +493,14 @@ exports.fileExists = function(pathname) {
   }
 };
 
+exports.skipIfEslintMissing = function() {
+  if (!exports.fileExists(
+    path.join('..', '..', 'tools', 'node_modules', 'eslint')
+  )) {
+    exports.skip('missing ESLint');
+  }
+};
+
 exports.canCreateSymLink = function() {
   // On Windows, creating symlinks requires admin privileges.
   // We'll only try to run symlink test if we have enough privileges.
