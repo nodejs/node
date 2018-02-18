652a037c883d3df1bfa590838f0c4536f059ff62
Trivikram <16024985+trivikr@users.noreply.github.com>
2018-02-17 20:35:01 -0800

    test: http2 client setNextStreamID errors
    Backport-PR-URL: https://github.com/nodejs/node/pull/18848
    PR-URL: https://github.com/nodejs/node/pull/18848
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Minwoo Jung <minwoo@nodesource.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/parallel/test-http2-client-setNextStreamID-errors.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const http2 = require('http2');
+
+const server = http2.createServer();
+server.on('stream', (stream) => {
+  stream.respond();
+  stream.end('ok');
+});
+
+const types = {
+  boolean: true,
+  function: () => {},
+  number: 1,
+  object: {},
+  array: [],
+  null: null,
+  symbol: Symbol('test')
+};
+
+server.listen(0, common.mustCall(() => {
+  const client = http2.connect(`http://localhost:${server.address().port}`);
+
+  client.on('connect', () => {
+    const outOfRangeNum = 2 ** 31;
+    common.expectsError(
+      () => client.setNextStreamID(outOfRangeNum),
+      {
+        type: RangeError,
+        code: 'ERR_OUT_OF_RANGE',
+        message: 'The "id" argument is out of range'
+      }
+    );
+
+    // should throw if something other than number is passed to setNextStreamID
+    Object.entries(types).forEach(([type, value]) => {
+      if (type === 'number') {
+        return;
+      }
+
+      common.expectsError(
+        () => client.setNextStreamID(value),
+        {
+          type: TypeError,
+          code: 'ERR_INVALID_ARG_TYPE',
+          message: 'The "id" argument must be of type number'
+        }
+      );
+    });
+
+    server.close();
+    client.close();
+  });
+}));
