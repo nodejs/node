cfc03aa515ea59529c97f6b42e1c15b94dd26e25
Andrew Johnston <apjohnsto@gmail.com>
2018-02-18 19:28:16 +0100

    working

--- test/parallel/test-http-client-spurious-aborted.js
+++ test/parallel/test-http-client-spurious-aborted.js
@@ -5,7 +5,6 @@ const http = require('http');
 const assert = require('assert');
 const fs = require('fs');
 const Countdown = require('../common/countdown');
-const { URL } = require('url');
 
 function cleanup(fname) {
   try {
@@ -13,35 +12,57 @@ function cleanup(fname) {
   } catch (err) {}
 }
 
-const N = 1;
+const N = 2;
 const fname = '/dev/null';
-const keepAlive = false;
-const useRemote = process.env.SPURIOUS_ABORTED_REMOTE === 'on';
-let url = new URL('http://www.pdf995.com/samples/pdf.pdf');
-let server;
+let abortRequest = true;
 
-const keepAliveAgent = new http.Agent({ keepAlive: true });
+const server = http.Server(common.mustCall((req, res) => {
+  const headers = { 'Content-Type': 'text/plain' };
+  headers['Content-Length'] = 50;
+  const socket = res.socket;
+  res.writeHead(200, headers);
+  setTimeout(() => res.write('aaaaaaaaaa'), 100);
+  setTimeout(() => res.write('bbbbbbbbbb'), 200);
+  setTimeout(() => res.write('cccccccccc'), 300);
+  setTimeout(() => res.write('dddddddddd'), 400);
+  if (abortRequest) {
+    setTimeout(() => socket.destroy(), 600);
+  } else {
+    setTimeout(() => res.end('eeeeeeeeee'), 1000);
+  }
+}, N));
+
+server.listen(0, common.mustCall(() => {
+  cleanup(fname);
+  download();
+}));
+
+const finishCountdown = new Countdown(N, common.mustCall(() => {
+  server.close();
+}));
 const reqCountdown = new Countdown(N, common.mustCall());
 
 function download() {
   const opts = {
-    host: url.hostname,
-    path: url.pathname,
-    port: url.port
+    port: server.address().port,
+    path: '/',
   };
-  if (keepAlive) opts.agent = keepAliveAgent;
   const req = http.get(opts);
   req.on('error', common.mustNotCall());
   req.on('response', (res) => {
     assert.strictEqual(res.statusCode, 200);
-    if (keepAlive) {
-      assert.strictEqual(res.headers.connection, 'keep-alive');
-    } else {
-      assert.strictEqual(res.headers.connection, 'close');
-    }
+    assert.strictEqual(res.headers.connection, 'close');
     let aborted = false;
     const fstream = fs.createWriteStream(fname);
     res.pipe(fstream);
+    const _handle = res.socket._handle;
+    _handle._close = res.socket._handle.close;
+    _handle.close = function(callback) {
+      _handle._close();
+      // set readable to true event though request is complete
+      if (res.complete) res.readable = true;
+      callback();
+    };
     res.on('end', common.mustCall(() => {
       reqCountdown.dec();
     }));
@@ -50,44 +71,13 @@ function download() {
     });
     res.on('error', common.mustNotCall());
     fstream.on('finish', () => {
-      assert.strictEqual(aborted, false);
+      assert.strictEqual(aborted, abortRequest);
       cleanup(fname);
       finishCountdown.dec();
       if (finishCountdown.remaining === 0) return;
+      abortRequest = false; // next one should be a good response
       download();
     });
   });
   req.end();
-}
-
-const finishCountdown = new Countdown(N, common.mustCall(() => {
-  if (!useRemote) server.close();
-}));
-
-if (useRemote) {
-  cleanup(fname);
-  download();
-} else {
-  server = http.Server(common.mustCall((req, res) => {
-    const headers = { 'Content-Type': 'text/plain' };
-    if (req.url === '/content-length') {
-      headers['Content-Length'] = 50;
-    }
-    const socket = res.socket;
-    res.writeHead(200, headers);
-    setTimeout(() => res.write('aaaaaaaaaa'), 100);
-    setTimeout(() => res.write('bbbbbbbbbb'), 200);
-    setTimeout(() => res.write('cccccccccc'), 300);
-    setTimeout(() => res.write('dddddddddd'), 400);
-    setTimeout(() => {
-      res.end('eeeeeeeeee');
-      const endAfter = Math.floor(Math.random() * 400);
-      setTimeout(() => socket.destroy(), endAfter);
-    }, 600);
-  }, N));
-  server.listen(0, common.mustCall(() => {
-    url = new URL(`http://127.0.0.1:${server.address().port}/content-length`);
-    cleanup(fname);
-    download();
-  }));
 }
