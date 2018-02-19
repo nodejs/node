112c9a3a19c54a2e9baec52f268acfb5f2a175a7
Luigi Pinca <luigipinca@gmail.com>
2018-02-19 15:09:48 +0100

    http: remove default 'drain' listener on upgrade
    Ensure that the default `'drain'` listener is removed before the
    `'connect'` or `'upgrade'` event is emitted.
    
    PR-URL: https://github.com/nodejs/node/pull/18866
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-http-connect.js
+++ test/parallel/test-http-connect.js
@@ -67,6 +67,7 @@ server.listen(0, common.mustCall(() => {
     assert.strictEqual(socket._httpMessage, null);
     assert.strictEqual(socket.listeners('connect').length, 0);
     assert.strictEqual(socket.listeners('data').length, 0);
+    assert.strictEqual(socket.listeners('drain').length, 0);
 
     // the stream.Duplex onend listener
     // allow 0 here, so that i can run the same test on streams1 impl
