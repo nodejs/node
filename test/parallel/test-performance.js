8267d269a4272290e625b0051893726e57e5174f
Timothy Gu <timothygu99@gmail.com>
2018-02-25 14:26:22 -0800

    perf_hooks: fix timing
    Fixes: https://github.com/nodejs/node/issues/17892
    Fixes: https://github.com/nodejs/node/issues/17893
    Fixes: https://github.com/nodejs/node/issues/18992
    
    PR-URL: https://github.com/nodejs/node/pull/18993
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>

--- test/parallel/test-performance.js
+++ test/parallel/test-performance.js
@@ -7,6 +7,12 @@ const { performance } = require('perf_hooks');
 assert(performance);
 assert(performance.nodeTiming);
 assert.strictEqual(typeof performance.timeOrigin, 'number');
+// Use a fairly large epsilon value, since we can only guarantee that the node
+// process started up in 20 seconds.
+assert(Math.abs(performance.timeOrigin - Date.now()) < 20000);
+
+const inited = performance.now();
+assert(inited < 20000);
 
 {
   const entries = performance.getEntries();
@@ -104,23 +110,81 @@ assert.strictEqual(typeof performance.timeOrigin, 'number');
 assert.strictEqual(performance.nodeTiming.name, 'node');
 assert.strictEqual(performance.nodeTiming.entryType, 'node');
 
-[
-  'startTime',
-  'duration',
-  'nodeStart',
-  'v8Start',
-  'bootstrapComplete',
-  'environment',
-  'loopStart',
-  'loopExit',
-  'thirdPartyMainStart',
-  'thirdPartyMainEnd',
-  'clusterSetupStart',
-  'clusterSetupEnd',
-  'moduleLoadStart',
-  'moduleLoadEnd',
-  'preloadModuleLoadStart',
-  'preloadModuleLoadEnd'
-].forEach((i) => {
-  assert.strictEqual(typeof performance.nodeTiming[i], 'number');
+function checkNodeTiming(props) {
+  for (const prop of Object.keys(props)) {
+    if (props[prop].around !== undefined) {
+      assert.strictEqual(typeof performance.nodeTiming[prop], 'number');
+      const delta = performance.nodeTiming[prop] - props[prop].around;
+      assert(Math.abs(delta) < 1000);
+    } else {
+      assert.strictEqual(performance.nodeTiming[prop], props[prop]);
+    }
+  }
+}
+
+checkNodeTiming({
+  name: 'node',
+  entryType: 'node',
+  startTime: 0,
+  duration: { around: performance.now() },
+  nodeStart: { around: 0 },
+  v8Start: { around: 0 },
+  bootstrapComplete: -1,
+  environment: { around: 0 },
+  loopStart: -1,
+  loopExit: -1,
+  thirdPartyMainStart: -1,
+  thirdPartyMainEnd: -1,
+  clusterSetupStart: -1,
+  clusterSetupEnd: -1,
+  moduleLoadStart: { around: inited },
+  moduleLoadEnd: { around: inited },
+  preloadModuleLoadStart: { around: inited },
+  preloadModuleLoadEnd: { around: inited },
+});
+
+setTimeout(() => {
+  checkNodeTiming({
+    name: 'node',
+    entryType: 'node',
+    startTime: 0,
+    duration: { around: performance.now() },
+    nodeStart: { around: 0 },
+    v8Start: { around: 0 },
+    bootstrapComplete: { around: inited },
+    environment: { around: 0 },
+    loopStart: { around: inited },
+    loopExit: -1,
+    thirdPartyMainStart: -1,
+    thirdPartyMainEnd: -1,
+    clusterSetupStart: -1,
+    clusterSetupEnd: -1,
+    moduleLoadStart: { around: inited },
+    moduleLoadEnd: { around: inited },
+    preloadModuleLoadStart: { around: inited },
+    preloadModuleLoadEnd: { around: inited },
+  });
+}, 2000);
+
+process.on('exit', () => {
+  checkNodeTiming({
+    name: 'node',
+    entryType: 'node',
+    startTime: 0,
+    duration: { around: performance.now() },
+    nodeStart: { around: 0 },
+    v8Start: { around: 0 },
+    bootstrapComplete: { around: inited },
+    environment: { around: 0 },
+    loopStart: { around: inited },
+    loopExit: { around: performance.now() },
+    thirdPartyMainStart: -1,
+    thirdPartyMainEnd: -1,
+    clusterSetupStart: -1,
+    clusterSetupEnd: -1,
+    moduleLoadStart: { around: inited },
+    moduleLoadEnd: { around: inited },
+    preloadModuleLoadStart: { around: inited },
+    preloadModuleLoadEnd: { around: inited },
+  });
 });
