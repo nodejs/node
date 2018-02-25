0c25cdf39a40a94fcb829ea91caa217d640054b1
Anna Henningsen <anna@addaleax.net>
2018-02-25 21:56:08 +0100

    tls,http2: handle writes after SSL destroy more gracefully
    This might otherwise result in a hard crash when trying
    to write to a socket after a sudden disconnect.
    
    Note that the test here uses an aborted `h2load` run to create
    the failing requests; That’s far from ideal, but it provides
    a reasonably reliably reproduction at this point.
    
    PR-URL: https://github.com/nodejs/node/pull/18987
    Fixes: https://github.com/nodejs/node/issues/18973
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- src/tls_wrap.cc
+++ src/tls_wrap.cc
@@ -553,7 +553,12 @@ int TLSWrap::DoWrite(WriteWrap* w,
                      size_t count,
                      uv_stream_t* send_handle) {
   CHECK_EQ(send_handle, nullptr);
-  CHECK_NE(ssl_, nullptr);
+
+  if (ssl_ == nullptr) {
+    ClearError();
+    error_ = "Write after DestroySSL";
+    return UV_EPROTO;
+  }
 
   bool empty = true;
 
@@ -591,12 +596,6 @@ int TLSWrap::DoWrite(WriteWrap* w,
   if (empty) {
     EncOut();
     return 0;
-  }
-
-  if (ssl_ == nullptr) {
-    ClearError();
-    error_ = "Write after DestroySSL";
-    return UV_EPROTO;
   }
 
   crypto::MarkPopErrorOnReturn mark_pop_error_on_return;
