aff6d018fd4aca6633fa5503cb50fa6ceb6bdcbb
Rich Trott <rtrott@gmail.com>
2018-02-25 15:53:37 -0800

    no reason to think this will go any better but why not?

--- test/sequential/test-inspector-stop-profile-after-done.js
+++ test/sequential/test-inspector-stop-profile-after-done.js
@@ -5,6 +5,8 @@ common.skipIfInspectorDisabled();
 const assert = require('assert');
 const { NodeInstance } = require('../common/inspector-helper.js');
 
+const stderrMessages = [];
+
 async function runTests() {
   const child = new NodeInstance(['--inspect-brk=0'],
                                  `let c = 0;
@@ -13,36 +15,24 @@ async function runTests() {
                                    if (c++ === 10)
                                      clearInterval(interval);
                                  }, 10);`);
-  const session = await child.connectInspectorSession();
 
-  let stderrString = await child.nextStderrString();
-  assert(stderrString.includes('Debugger listening on'), stderrString);
-  stderrString = await child.nextStderrString();
-  assert.strictEqual(stderrString,
-                     'For help see https://nodejs.org/en/docs/inspector');
-  stderrString = await child.nextStderrString();
-  assert.strictEqual(stderrString, '');
+  child._process.stderr.on('data', async (data) => {
+    const message = data.toString();
+    stderrMessages.push(message);
+    if (message.trim() === 'Waiting for the debugger to disconnect...') {
+      await session.send({ 'method': 'Profiler.stop' });
+      session.disconnect();
+      assert.strictEqual(0, (await child.expectShutdown()).exitCode);
+    }
+  })
+  const session = await child.connectInspectorSession();
 
   session.send([
     { 'method': 'Profiler.setSamplingInterval', 'params': { 'interval': 100 } },
     { 'method': 'Profiler.enable' },
     { 'method': 'Runtime.runIfWaitingForDebugger' },
-  ]);
-
-  stderrString = await child.nextStderrString();
-  assert.strictEqual(stderrString, 'Debugger attached.')
-  stderrString = await child.nextStderrString();
-  assert.strictEqual(stderrString, '');
-
-  await session.send({ 'method': 'Profiler.start' });
-
-  stderrString = await child.nextStderrString();
-  assert.strictEqual(stderrString, 'Waiting for the debugger to disconnect...');
-
-  // await session.send({ 'method': 'Profiler.stop' });
-  // session.disconnect();
-  // assert.strictEqual(0, (await child.expectShutdown()).exitCode);
-  child.kill();
+    { 'method': 'Profiler.start' }
+  ]);  
 }
 
 common.crashOnUnhandledRejection();
