8a43ed1f2d2845d645cb96d16bd14c25228af929
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-02-27 09:00:04 +0100

    squash: add prefix to option name and refact test

--- test/parallel/test-async-hooks-constructor.js
+++ test/parallel/test-async-hooks-constructor.js
@@ -6,30 +6,18 @@ const common = require('../common');
 const async_hooks = require('async_hooks');
 const non_function = 10;
 
-common.expectsError(() => {
-  async_hooks.createHook({ init: non_function });
-}, typeErrorForFunction('init'));
-
-common.expectsError(() => {
-  async_hooks.createHook({ before: non_function });
-}, typeErrorForFunction('before'));
-
-common.expectsError(() => {
-  async_hooks.createHook({ after: non_function });
-}, typeErrorForFunction('after'));
-
-common.expectsError(() => {
-  async_hooks.createHook({ destroy: non_function });
-}, typeErrorForFunction('destroy'));
-
-common.expectsError(() => {
-  async_hooks.createHook({ promiseResolve: non_function });
-}, typeErrorForFunction('promiseResolve'));
+typeErrorForFunction('init');
+typeErrorForFunction('before');
+typeErrorForFunction('after');
+typeErrorForFunction('destroy');
+typeErrorForFunction('promiseResolve');
 
 function typeErrorForFunction(functionName) {
-  return {
+  common.expectsError(() => {
+    async_hooks.createHook({ [functionName]: non_function });
+  }, {
     code: 'ERR_ASYNC_CALLBACK',
     type: TypeError,
-    message: `${functionName} must be a function`,
-  };
+    message: `hook.${functionName} must be a function`
+  });
 }
