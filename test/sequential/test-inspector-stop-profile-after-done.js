e259d26ee5c3ed53a748f8f25b4a8a96a79a4844
Rich Trott <rtrott@gmail.com>
2018-03-01 18:01:21 -0800

    increase timeouts

--- test/sequential/test-inspector-stop-profile-after-done.js
+++ test/sequential/test-inspector-stop-profile-after-done.js
@@ -5,45 +5,27 @@ common.skipIfInspectorDisabled();
 const assert = require('assert');
 const { NodeInstance } = require('../common/inspector-helper.js');
 
-const stderrMessages = [];
-
 async function runTests() {
   const child = new NodeInstance(['--inspect-brk=0'],
-                                 `const interval = setInterval(() => {
-                                    console.log(new Object());
-                                  }, 200);
-                                  process.stdin.on('data', (msg) => {
-                                    if (msg.toString() === 'fhqwhgads') {
-                                      clearInterval(interval);
-                                      process.exit();
-                                    }
-                                  });`);
-
-  child._process.stderr.on('data', async (data) => {
-    const message = data.toString();
-    stderrMessages.push(message);
-    if (message.trim() === 'Waiting for the debugger to disconnect...') {
-      await session.send({ 'method': 'Profiler.stop' });
-      session.disconnect();
-      assert.strictEqual(0, (await child.expectShutdown()).exitCode);
-    }
-  })
+                                 `let c = 0;
+                                  const interval = setInterval(() => {
+                                   console.log(new Object());
+                                   if (c++ === 10)
+                                     clearInterval(interval);
+                                 }, ${common.platformTimeout(30)});`);
   const session = await child.connectInspectorSession();
 
   session.send([
-    { 'method': 'Profiler.setSamplingInterval', 'params': { 'interval': 2000 } },
-    { 'method': 'Profiler.enable' },
-    { 'method': 'Runtime.runIfWaitingForDebugger' }
-  ]);
-
-  await child.nextStderrString();
-  await child.nextStderrString();
-  await child.nextStderrString();
-  const stderrString = await child.nextStderrString();
-  assert.strictEqual(stderrString, 'Debugger attached.');
-
-  session.send({ 'method': 'Profiler.start' })
-  setTimeout(() => { child._process.stdin.write('fhqwhgads'); }, 4000);
+    { method: 'Profiler.setSamplingInterval',
+      params: { interval: common.platformTimeout(300) } },
+    { method: 'Profiler.enable' },
+    { method: 'Runtime.runIfWaitingForDebugger' },
+    { method: 'Profiler.start' }]);
+  while (await child.nextStderrString() !==
+         'Waiting for the debugger to disconnect...');
+  await session.send({ method: 'Profiler.stop' });
+  session.disconnect();
+  assert.strictEqual(0, (await child.expectShutdown()).exitCode);
 }
 
 common.crashOnUnhandledRejection();
