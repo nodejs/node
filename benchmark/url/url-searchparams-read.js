3cd0d7f6d4bb84a7655824abd10b498244f5419a
Sergey Golovin <golovim@gmail.com>
2018-03-02 20:36:35 +0300

    benchmark: rename different parameters
    - rename different parameters with the same names to make it possible to run tests for url benchmarks
    - add options in the test for all url benchmarks

--- benchmark/url/url-searchparams-read.js
+++ benchmark/url/url-searchparams-read.js
@@ -3,20 +3,20 @@ const common = require('../common.js');
 const { URLSearchParams } = require('url');
 
 const bench = common.createBenchmark(main, {
-  method: ['get', 'getAll', 'has'],
+  accessMethod: ['get', 'getAll', 'has'],
   param: ['one', 'two', 'three', 'nonexistent'],
   n: [2e7]
 });
 
 const str = 'one=single&two=first&three=first&two=2nd&three=2nd&three=3rd';
 
-function main({ method, param, n }) {
+function main({ accessMethod, param, n }) {
   const params = new URLSearchParams(str);
-  if (!params[method])
-    throw new Error(`Unknown method ${method}`);
+  if (!params[accessMethod])
+    throw new Error(`Unknown method ${accessMethod}`);
 
   bench.start();
   for (var i = 0; i < n; i += 1)
-    params[method](param);
+    params[accessMethod](param);
   bench.end(n);
 }
--- test/parallel/test-benchmark-url.js
+++ test/parallel/test-benchmark-url.js
@@ -4,4 +4,19 @@ require('../common');
 
 const runBenchmark = require('../common/benchmark');
 
-runBenchmark('url', ['n=1'], { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
+runBenchmark('url',
+             [
+               'method=legacy',
+               'loopMethod=forEach',
+               'accessMethod=get',
+               'type=short',
+               'searchParam=noencode',
+               'href=short',
+               'input=short',
+               'domain=empty',
+               'path=up',
+               'to=ascii',
+               'prop=href',
+               'n=1',
+             ],
+             { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
