41406713acfa4ad4003171b3936a7d9975468c1b
Anna Henningsen <anna@addaleax.net>
2018-03-02 12:54:29 +0000

    [squash] add requested block

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -1701,9 +1701,10 @@ void Http2Session::OnStreamRead(ssize_t nread, const uv_buf_t& buf) {
 }
 
 bool Http2Session::HasWritesOnSocketForStream(Http2Stream* stream) {
-  for (const nghttp2_stream_write& wr : outgoing_buffers_)
+  for (const nghttp2_stream_write& wr : outgoing_buffers_) {
     if (wr.req_wrap != nullptr && wr.req_wrap->stream() == stream)
       return true;
+  }
   return false;
 }
 
