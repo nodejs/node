f3257dd3ead29e620b2bc096d54b07b12ec5c5a3
Sergey Golovin <golovim@gmail.com>
2018-03-02 16:23:16 +0300

    benchmark: fix benchmark for url
    Rename different parameters with the same names to make it possible
    to run tests for url benchmarks.
    
    PR-URL: https://github.com/nodejs/node/pull/19084
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Khaidi Chu <i@2333.moe>
    Reviewed-By: Shingo Inoue <leko.noor@gmail.com>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- benchmark/url/legacy-vs-whatwg-url-searchparams-parse.js
+++ benchmark/url/legacy-vs-whatwg-url-searchparams-parse.js
@@ -2,10 +2,10 @@
 const common = require('../common.js');
 const { URLSearchParams } = require('url');
 const querystring = require('querystring');
-const inputs = require('../fixtures/url-inputs.js').searchParams;
+const searchParams = require('../fixtures/url-inputs.js').searchParams;
 
 const bench = common.createBenchmark(main, {
-  type: Object.keys(inputs),
+  searchParam: Object.keys(searchParams),
   method: ['legacy', 'whatwg'],
   n: [1e6]
 });
@@ -19,27 +19,27 @@ function useLegacy(n, input) {
   bench.end(n);
 }
 
-function useWHATWG(n, input) {
-  new URLSearchParams(input);
+function useWHATWG(n, param) {
+  new URLSearchParams(param);
   bench.start();
   for (var i = 0; i < n; i += 1) {
-    new URLSearchParams(input);
+    new URLSearchParams(param);
   }
   bench.end(n);
 }
 
-function main({ type, n, method }) {
-  const input = inputs[type];
-  if (!input) {
-    throw new Error(`Unknown input type "${type}"`);
+function main({ searchParam, n, method }) {
+  const param = searchParams[searchParam];
+  if (!param) {
+    throw new Error(`Unknown search parameter type "${searchParam}"`);
   }
 
   switch (method) {
     case 'legacy':
-      useLegacy(n, input);
+      useLegacy(n, param);
       break;
     case 'whatwg':
-      useWHATWG(n, input);
+      useWHATWG(n, param);
       break;
     default:
       throw new Error(`Unknown method ${method}`);
