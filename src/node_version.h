bf17d50a676c0618c9ed4f9c57af1734d7291a2a
Michaël Zasso <targos@protonmail.com>
2018-03-07 08:54:54 +0100

    src: update NODE_MODULE_VERSION to 62
    Major V8 updates are usually API/ABI incompatible with previous
    versions. This commit adapts NODE_MODULE_VERSION for V8 6.5.
    
    Refs: https://github.com/nodejs/CTC/blob/master/meetings/2016-09-28.md

--- src/node_version.h
+++ src/node_version.h
@@ -105,10 +105,11 @@
  * V8 6.2: 59
  * V8 6.3: 60
  * V8 6.4: 61
+ * V8 6.5: 62
  *
  * More information can be found at https://nodejs.org/en/download/releases/
  */
-#define NODE_MODULE_VERSION 61
+#define NODE_MODULE_VERSION 62
 
 // the NAPI_VERSION provided by this version of the runtime
 #define NAPI_VERSION  2
