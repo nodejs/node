8e50585348fd8d2afb3761608d56394e0fdcc670
Ujjwal Sharma <usharma1998@gmail.com>
2018-03-09 01:55:18 +0530

    test: handle (almost) all nits in #19212
    Refs: https://github.com/nodejs/node/pull/19212
    Refs: Refs: https://github.com/nodejs/node/issues/19105

--- test/parallel/test-cluster-kill-disconnect.js
+++ test/parallel/test-cluster-kill-disconnect.js
@@ -4,6 +4,7 @@ const common = require('../common');
 // Check that cluster works perfectly for both `kill` and `disconnect` cases.
 // Also take into account that the `disconnect` event may be received after the
 // `exit` event.
+// https://github.com/nodejs/node/issues/3238
 
 const assert = require('assert');
 const cluster = require('cluster');
--- test/parallel/test-fs-readfilesync-enoent.js
+++ test/parallel/test-fs-readfilesync-enoent.js
@@ -1,18 +1,17 @@
 'use strict';
 const common = require('../common');
 
+// This test is only relevant on Windows.
+if (!common.isWindows)
+  common.skip('Windows specific test.');
+
 // This test ensures fs.realpathSync works on properly on Windows without
 // throwing ENOENT when the path involves a fileserver
 // https://github.com/nodejs/node-v0.x-archive/issues/3542
 
-// This test is only relevant on Windows.
-
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
-
-if (!common.isWindows)
-  common.skip('Windows specific test.');
 
 function test(p) {
   const result = fs.realpathSync(p);
