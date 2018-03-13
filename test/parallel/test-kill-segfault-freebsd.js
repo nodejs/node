8fa5bd37611b2952e6b641932700cd0850bd4b40
Ujjwal Sharma <usharma1998@gmail.com>
2018-03-14 03:13:29 +0530

    test: rename regression tests file names
    Rename the tests appropriately alongside mentioning the subsystem.
    Also, make a few basic changes to make sure the tests conform to the
    standard test structure.
    
    - Rename test-regress-GH-io-1068 to test-tty-stdin-end
    - Rename test-regress-GH-io-1811 to test-zlib-kmaxlength-rangeerror
    - Rename test-regress-GH-node-9326 to test-kill-segfault-freebsd
    - Rename test-timers-regress-GH-9765 to test-timers-setimmediate-infinite-loop
    - Rename test-tls-pfx-gh-5100-regr to test-tls-pfx-authorizationerror
    - Rename test-tls-regr-gh-5108 to test-tls-tlswrap-segfault
    
    PR-URL: https://github.com/nodejs/node/pull/19332
    Fixes: https://github.com/nodejs/node/issues/19105
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Weijia Wang <starkwang@126.com>
    Reviewed-By: Yuta Hiroto <hello@hiroppy.me>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Shingo Inoue <leko.noor@gmail.com>

--- test/parallel/test-kill-segfault-freebsd.js
+++ test/parallel/test-kill-segfault-freebsd.js
@@ -1,5 +1,10 @@
 'use strict';
 require('../common');
+
+// This test ensures Node.js doesn't crash on hitting Ctrl+C in order to
+// terminate the currently running process (especially on FreeBSD).
+// https://github.com/nodejs/node-v0.x-archive/issues/9326
+
 const assert = require('assert');
 const child_process = require('child_process');
 
--- test/parallel/test-tls-tlswrap-segfault.js
+++ test/parallel/test-tls-tlswrap-segfault.js
@@ -1,18 +1,20 @@
 'use strict';
 const common = require('../common');
-
 if (!common.hasCrypto)
   common.skip('missing crypto');
+const fixtures = require('../common/fixtures');
+
+// This test ensures that Node.js doesn't incur a segfault while accessing
+// TLSWrap fields after the parent handle was destroyed.
+// https://github.com/nodejs/node/issues/5108
 
 const assert = require('assert');
 const tls = require('tls');
-const fixtures = require('../common/fixtures');
 
 const options = {
   key: fixtures.readKey('agent1-key.pem'),
   cert: fixtures.readKey('agent1-cert.pem')
 };
-
 
 const server = tls.createServer(options, function(s) {
   s.end('hello');
@@ -25,7 +27,6 @@ const server = tls.createServer(options, function(s) {
     putImmediate(client);
   });
 });
-
 
 function putImmediate(client) {
   setImmediate(function() {
