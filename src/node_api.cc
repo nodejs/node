528798c3f465c4c6f2f162478f85bc111a363bb0
Michael Dawson <michael_dawson@ca.ibm.com>
2018-03-14 22:05:47 -0400

    n-api: add missing exception checking
    Add checks for a pending exception in napi_make_callback
    after the callback has been invoked.  If there is a pending
    exception then we need to avoid checking the result as that
    will not be able to complete properly.
    
    Add additional checks to the unit test for napi_make_callback
    to catch this case.
    
    PR-URL: https://github.com/nodejs/node/pull/19362
    Fixes: https://github.com/nodejs/node-addon-api/issues/235
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/node_api.cc
+++ src/node_api.cc
@@ -2811,11 +2811,15 @@ napi_status napi_make_callback(napi_env env,
       isolate, v8recv, v8func, argc,
       reinterpret_cast<v8::Local<v8::Value>*>(const_cast<napi_value*>(argv)),
       *node_async_context);
-  CHECK_MAYBE_EMPTY(env, callback_result, napi_generic_failure);
 
-  if (result != nullptr) {
-    *result = v8impl::JsValueFromV8LocalValue(
-        callback_result.ToLocalChecked());
+  if (try_catch.HasCaught()) {
+    return napi_set_last_error(env, napi_pending_exception);
+  } else {
+    CHECK_MAYBE_EMPTY(env, callback_result, napi_generic_failure);
+    if (result != nullptr) {
+      *result = v8impl::JsValueFromV8LocalValue(
+          callback_result.ToLocalChecked());
+    }
   }
 
   return GET_RETURN_STATUS(env);
