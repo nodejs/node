367113f5d7e0e085afb1d7f8dbf8f5ecb6cf4858
Michael Dawson <mdawson@devrus.com>
2018-03-20 17:14:24 -0400

    n-api: bump version of n-api supported
    Bump the version due to additions to the api.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/19265
    PR-URL: https://github.com/nodejs/node/pull/19497
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- src/node_version.h
+++ src/node_version.h
@@ -108,6 +108,6 @@
 #define NODE_MODULE_VERSION 57
 
 // the NAPI_VERSION provided by this version of the runtime
-#define NAPI_VERSION  2
+#define NAPI_VERSION  3
 
 #endif  // SRC_NODE_VERSION_H_
--- test/addons-napi/test_general/test.js
+++ test/addons-napi/test_general/test.js
@@ -33,8 +33,8 @@ assert.notStrictEqual(test_general.testGetPrototype(baseObject),
                       test_general.testGetPrototype(extendedObject));
 
 // test version management functions
-// expected version is currently 1
-assert.strictEqual(test_general.testGetVersion(), 2);
+// expected version is currently 3
+assert.strictEqual(test_general.testGetVersion(), 3);
 
 const [ major, minor, patch, release ] = test_general.testGetNodeVersion();
 assert.strictEqual(process.version.split('-')[0],
