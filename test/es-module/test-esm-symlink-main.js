053cf5d1fe5794f9849b6b06c09e8f651b3c941a
Guy Bedford <guybedford@gmail.com>
2018-03-20 16:38:44 +0200

    generate symlink in tmpdir

--- test/es-module/test-esm-symlink-main.js
+++ test/es-module/test-esm-symlink-main.js
@@ -2,10 +2,22 @@
 
 const assert = require('assert');
 const path = require('path');
-const entry = path.resolve(__dirname, '../fixtures/es-modules/symlink.js');
 const { spawn } = require('child_process');
+const tmpdir = require('../common/tmpdir');
+const fs = require('fs');
+tmpdir.refresh();
 
-spawn(process.execPath, ['--experimental-modules', '--preserve-symlinks', entry],
+const realPath = path.resolve(__dirname, '../fixtures/es-modules/symlink.mjs');
+const symlinkPath = path.resolve(tmpdir.path, 'symlink.js');
+
+try {
+  fs.symlinkSync(realPath, symlinkPath);
+} catch (err) {
+  if (err.code !== 'EPERM') throw err;
+  common.skip('insufficient privileges for symlinks');
+}
+
+spawn(process.execPath, ['--experimental-modules', '--preserve-symlinks', symlinkPath],
       { stdio: 'inherit' }).on('exit', (code) => {
   assert.strictEqual(code, 0);
 });
