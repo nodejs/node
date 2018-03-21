008474a13a8e00f864c8e7a337873861155de64f
Sameer Srivastava <sameer13@gmail.com>
2018-03-21 15:21:45 +0530

    fs,net: Fixed linter errors in test/parallel/test-fs-ready-event-stream.js
    Fixes: https://github.com/nodejs/node/issues/19304

--- test/parallel/test-fs-ready-event-stream.js
+++ test/parallel/test-fs-ready-event-stream.js
@@ -1,8 +1,6 @@
-
 'use strict';
 const common = require('../common');
 const fs = require('fs');
-const assert = require('assert');
 const path = require('path');
 const tmpdir = require('../common/tmpdir');
 
@@ -11,5 +9,5 @@ readStream.on('ready', common.mustCall(() => {}, 1));
 
 const writeFile = path.join(tmpdir.path, 'write-fsreadyevent.txt');
 tmpdir.refresh();
-let writeStream = fs.createWriteStream(writeFile, { autoClose: true });
+const writeStream = fs.createWriteStream(writeFile, { autoClose: true });
 writeStream.on('ready', common.mustCall(() => {}, 1));
