0778b5866510d04312da97881b701e73e6d6c750
Sameer Srivastava <sameer13@gmail.com>
2018-03-21 15:18:00 +0530

    fs,net: updated test case to check for ready event on writestream
    updated test case updated test case to check for ready event
    on writestream
    
    Fixes: https://github.com/nodejs/node/issues/19304

--- test/parallel/test-fs-ready-event-stream.js
+++ test/parallel/test-fs-ready-event-stream.js
@@ -1,7 +1,15 @@
+
 'use strict';
 const common = require('../common');
 const fs = require('fs');
+const assert = require('assert');
+const path = require('path');
+const tmpdir = require('../common/tmpdir');
 
-const file = fs.createReadStream(__filename);
+const readStream = fs.createReadStream(__filename);
+readStream.on('ready', common.mustCall(() => {}, 1));
 
-file.on('ready', common.mustCall(() => {}, 1));
+const writeFile = path.join(tmpdir.path, 'write-fsreadyevent.txt');
+tmpdir.refresh();
+let writeStream = fs.createWriteStream(writeFile, { autoClose: true });
+writeStream.on('ready', common.mustCall(() => {}, 1));
