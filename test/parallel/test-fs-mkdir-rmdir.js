8996d3cf45158c691568e5674e22c0dffd40bb33
davis.okoth@kemsa.co.ke <davis.okoth@kemsa.co.ke>
2018-03-22 22:04:38 +0300

    test: remove third param from assert.strictEqual
    Removing third argument in calls to assert.strictEqual() so that the
    values of the first two arguments are shown instead as this is more
    useful for debugging.
    
    Refs: https://nodejs.org/api/assert.html#assert_assert_strictequal_actual_expected_message
    
    PR-URL: https://github.com/nodejs/node/pull/19536
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- test/parallel/test-fs-mkdir-rmdir.js
+++ test/parallel/test-fs-mkdir-rmdir.js
@@ -31,8 +31,8 @@ fs.mkdir(d, 0o666, common.mustCall(function(err) {
   fs.mkdir(d, 0o666, common.mustCall(function(err) {
     assert.ok(err, 'got no error');
     assert.ok(/^EEXIST/.test(err.message), 'got no EEXIST message');
-    assert.strictEqual(err.code, 'EEXIST', 'got no EEXIST code');
-    assert.strictEqual(err.path, d, 'got no proper path for EEXIST');
+    assert.strictEqual(err.code, 'EEXIST');
+    assert.strictEqual(err.path, d);
 
     fs.rmdir(d, assert.ifError);
   }));
