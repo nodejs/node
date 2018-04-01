83f478e0dbc9c91ace685dc0225fad8c0ac4fdd2
Ujjwal Sharma <usharma1998@gmail.com>
2018-04-02 00:06:12 +0530

    fs, test: add tests for #19732
    Add regression tests for https://github.com/nodejs/node/pull/19732,
    making sure appropraite TypeErrors are thrown for NaN values.
    
    Refs: https://github.com/nodejs/node/pull/19732

--- test/parallel/test-fs-read-stream-throw-type-error.js
+++ test/parallel/test-fs-read-stream-throw-type-error.js
@@ -1,6 +1,10 @@
 'use strict';
 const common = require('../common');
 const fixtures = require('../common/fixtures');
+
+// This test ensures that fs.createReadStream throws a TypeError when invalid
+// arguments are passed to it.
+
 const fs = require('fs');
 
 const example = fixtures.path('x.txt');
@@ -18,10 +22,16 @@ const createReadStreamErr = (path, opt) => {
     {
       code: 'ERR_INVALID_ARG_TYPE',
       type: TypeError
-    });
+    }
+  );
 };
 
 createReadStreamErr(example, 123);
 createReadStreamErr(example, 0);
 createReadStreamErr(example, true);
 createReadStreamErr(example, false);
+
+// Should also throw on NaN (for https://github.com/nodejs/node/pull/19732)
+createReadStreamErr(example, { start: NaN });
+createReadStreamErr(example, { end: NaN });
+createReadStreamErr(example, { start: NaN, end: NaN });
