86814022289e8924d48b63ae590bdf3a41876366
James M Snell <jasnell@gmail.com>
2018-04-03 11:26:38 -0700

    http2: fixup http2stream cleanup and other nits
    This fixes CVE-2018-7161.
    
    PR-URL: https://github.com/nodejs-private/node-private/pull/115
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Evan Lucas <evanlucas@me.com>

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -499,6 +499,8 @@ Http2Session::Http2Session(Environment* env,
 Http2Session::~Http2Session() {
   CHECK_EQ(flags_ & SESSION_STATE_HAS_SCOPE, 0);
   Debug(this, "freeing nghttp2 session");
+  for (const auto& iter : streams_)
+    iter.second->session_ = nullptr;
   nghttp2_session_del(session_);
 }
 
@@ -651,6 +653,8 @@ inline void Http2Session::AddStream(Http2Stream* stream) {
 
 
 inline void Http2Session::RemoveStream(Http2Stream* stream) {
+  if (streams_.empty() || stream == nullptr)
+    return;  // Nothing to remove, item was never added?
   streams_.erase(stream->id());
   DecrementCurrentSessionMemory(stream->self_size());
 }
