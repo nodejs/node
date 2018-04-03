74903b525a0bdae4ded26a598918dae1cf115a53
Ujjwal Sharma <usharma1998@gmail.com>
2018-04-03 23:04:20 +0530

    fs: make ReadStream throw TypeError on NaN
    Make ReadStream (and thus createReadStream) throw a TypeError signalling
    towards an invalid argument type when either options.start or
    options.end (or obviously, both) are set to NaN.
    Also add regression tests for the same.
    
    Fixes: https://github.com/nodejs/node/issues/19715

--- test/parallel/test-fs-read-stream-throw-type-error.js
+++ test/parallel/test-fs-read-stream-throw-type-error.js
@@ -3,6 +3,9 @@ const common = require('../common');
 const fixtures = require('../common/fixtures');
 const fs = require('fs');
 
+// This test ensures that appropriate TypeError is thrown by createReadStream
+// when an argument with invalid type is passed
+
 const example = fixtures.path('x.txt');
 // Should not throw.
 fs.createReadStream(example, undefined);
@@ -25,3 +28,8 @@ createReadStreamErr(example, 123);
 createReadStreamErr(example, 0);
 createReadStreamErr(example, true);
 createReadStreamErr(example, false);
+
+// createReadSteam _should_ throw on NaN
+createReadStreamErr(example, { start: NaN });
+createReadStreamErr(example, { end: NaN });
+createReadStreamErr(example, { start: NaN, end: NaN });
