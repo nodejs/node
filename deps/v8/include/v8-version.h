09f5e252bfbeb2425a9692c25665dca97c1a61f4
Michaël Zasso <targos@protonmail.com>
2018-04-05 10:43:58 +0200

    deps: cherry-pick 7abdadc from upstream V8
    Original commit message:
    
        Sprinkle some DisallowHeapAllocation
    
        Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_rel_ng
        Change-Id: I7d34ccddeea08f5935e360e8c36791365f27f89e
        Reviewed-on: https://chromium-review.googlesource.com/647706
        Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
        Commit-Queue: Camillo Bruni <cbruni@chromium.org>
        Cr-Commit-Position: refs/heads/master@{#47804}
    
    PR-URL: https://github.com/nodejs/node/pull/19824
    Refs: https://github.com/v8/v8/commit/7abdadca0e3e5a780647b8ade0c586c2e7583f9c
    Fixes: https://github.com/nodejs/node/issues/19769
    Reviewed-By: Yang Guo <yangguo@chromium.org>
    Reviewed-By: Gibson Fahnestock <gibfahn@gmail.com>

--- deps/v8/include/v8-version.h
+++ deps/v8/include/v8-version.h
@@ -11,7 +11,7 @@
 #define V8_MAJOR_VERSION 6
 #define V8_MINOR_VERSION 2
 #define V8_BUILD_NUMBER 414
-#define V8_PATCH_LEVEL 51
+#define V8_PATCH_LEVEL 52
 
 // Use 1 for candidates and 0 otherwise.
 // (Boolean macro values are not supported by all preprocessors.)
