ae530d4fe9196fc91210249ad0a8d34572c36020
Mathias Buus <mathiasbuus@gmail.com>
2018-04-06 17:50:58 +0200

    http2: add failing writes with destroy test

---
+++ test/parallel/test-http2-many-writes-and-destroy.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+const http2 = require('http2');
+const assert = require('assert');
+
+{
+  const server = http2.createServer((req, res) => {
+    req.pipe(res);
+  });
+
+  setTimeout(() => {
+    assert(false, 'should exit before 10s');
+  }, 10000).unref();
+
+  server.listen(0, () => {
+    const url = `http://localhost:${server.address().port}`;
+    const client = http2.connect(url);
+    const req = client.request({ ':method': 'POST' });
+
+    for (let i = 0; i < 4000; i++) {
+      req.write(Buffer.alloc(6));
+    }
+
+    req.on('close', common.mustCall(() => {
+      console.log('(req onclose)');
+      server.close();
+      client.close();
+    }));
+
+    req.once('data', common.mustCall(() => req.destroy()));
+  });
+}
