30c285fc882d655373c6c6f4384ebf4744ea88a6
Ujjwal Sharma <usharma1998@gmail.com>
2018-04-10 16:05:37 +0530

    test,fs: update tests for improvements in ReadStream
    Add new tests and update existing ones reflecting the changes in how
    ReadStream handles the `start` and `end` arguments.

--- test/parallel/test-fs-read-stream-throw-type-error.js
+++ test/parallel/test-fs-read-stream-throw-type-error.js
@@ -13,23 +13,54 @@ fs.createReadStream(example, null);
 fs.createReadStream(example, 'utf8');
 fs.createReadStream(example, { encoding: 'utf8' });
 
-const createReadStreamErr = (path, opt) => {
-  common.expectsError(
-    () => {
-      fs.createReadStream(path, opt);
-    },
-    {
-      code: 'ERR_INVALID_ARG_TYPE',
-      type: TypeError
-    });
+const createReadStreamErr = (path, opt, error) => {
+  common.expectsError(() => {
+    fs.createReadStream(path, opt);
+  }, error);
 };
 
-createReadStreamErr(example, 123);
-createReadStreamErr(example, 0);
-createReadStreamErr(example, true);
-createReadStreamErr(example, false);
+const typeError = {
+  code: 'ERR_INVALID_ARG_TYPE',
+  type: TypeError
+};
 
-// createReadSteam _should_ throw on NaN
-createReadStreamErr(example, { start: NaN });
-createReadStreamErr(example, { end: NaN });
-createReadStreamErr(example, { start: NaN, end: NaN });
+const rangeError = {
+  code: 'ERR_OUT_OF_RANGE',
+  type: RangeError
+};
+
+createReadStreamErr(example, 123, typeError);
+createReadStreamErr(example, 0, typeError);
+createReadStreamErr(example, true, typeError);
+createReadStreamErr(example, false, typeError);
+
+// Case 0: Should not throw if either start or end is undefined
+fs.createReadStream(example, {});
+fs.createReadStream(example, { start: 0 });
+fs.createReadStream(example, { end: Infinity });
+
+// Case 1: Should throw TypeError if either start or end is not of type 'number'
+createReadStreamErr(example, { start: 'invalid' }, typeError);
+createReadStreamErr(example, { end: 'invalid' }, typeError);
+createReadStreamErr(example, { start: 'invalid', end: 'invalid' }, typeError);
+
+// Case 2: Should throw RangeError if either start or end is NaN
+createReadStreamErr(example, { start: NaN }, rangeError);
+createReadStreamErr(example, { end: NaN }, rangeError);
+createReadStreamErr(example, { start: NaN, end: NaN }, rangeError);
+
+// Case 3: Should throw RangeError if either start or end is negative
+createReadStreamErr(example, { start: -1 }, rangeError);
+createReadStreamErr(example, { end: -1 }, rangeError);
+createReadStreamErr(example, { start: -1, end: -1 }, rangeError);
+
+// Case 4: Should not throw if either start or end is fractional
+fs.createReadStream(example, { start: 0.1 });
+fs.createReadStream(example, { end: 0.1 });
+fs.createReadStream(example, { start: 0.1, end: 0.1 });
+
+// Case 5: Should not throw if both start and end are whole numbers
+fs.createReadStream(example, { start: 1, end: 5 });
+
+// Case 6: Should throw RangeError if start is greater than end
+createReadStreamErr(example, { start: 5, end: 1 }, rangeError);
