d28211ec3da0eb6047c846d5cc7cdaa3993e16b8
Ruben Bridgewater <ruben@bridgewater.de>
2018-04-11 03:10:22 +0200

    errors: validate input arguments
    This makes sure the input arguments get validated so implementation
    errors will be caught early. It also improves a couple of error
    messages by providing more detailed information and fixes errors
    detected by the new functionality. Besides that a error type got
    simplified and tests got refactored.
    
    PR-URL: https://github.com/nodejs/node/pull/19924
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-fs-read-type.js
+++ test/parallel/test-fs-read-type.js
@@ -1,6 +1,7 @@
 'use strict';
 const common = require('../common');
 const fs = require('fs');
+const assert = require('assert');
 const fixtures = require('../common/fixtures');
 
 const filepath = fixtures.path('x.txt');
@@ -9,18 +10,18 @@ const expected = 'xyz\n';
 
 
 // Error must be thrown with string
-common.expectsError(
+assert.throws(
   () => fs.read(fd, expected.length, 0, 'utf-8', common.mustNotCall()),
   {
     code: 'ERR_INVALID_ARG_TYPE',
-    type: TypeError,
+    name: 'TypeError [ERR_INVALID_ARG_TYPE]',
     message: 'The "buffer" argument must be one of type Buffer or Uint8Array.' +
              ' Received type number'
   }
 );
 
 [true, null, undefined, () => {}, {}].forEach((value) => {
-  common.expectsError(() => {
+  assert.throws(() => {
     fs.read(value,
             Buffer.allocUnsafe(expected.length),
             0,
@@ -29,45 +30,53 @@ common.expectsError(
             common.mustNotCall());
   }, {
     code: 'ERR_INVALID_ARG_TYPE',
-    type: TypeError,
+    name: 'TypeError [ERR_INVALID_ARG_TYPE]',
     message: 'The "fd" argument must be of type number. ' +
              `Received type ${typeof value}`
   });
 });
 
-common.expectsError(() => {
+assert.throws(() => {
   fs.read(fd,
           Buffer.allocUnsafe(expected.length),
           -1,
           expected.length,
           0,
           common.mustNotCall());
-}, { code: 'ERR_OUT_OF_RANGE', type: RangeError,
-     message: 'The value of "offset" is out of range.' });
+}, {
+  code: 'ERR_OUT_OF_RANGE',
+  name: 'RangeError [ERR_OUT_OF_RANGE]',
+  message: 'The value of "offset" is out of range. It must be >= 0 && <= 4. ' +
+           'Received -1'
+});
 
-common.expectsError(() => {
+assert.throws(() => {
   fs.read(fd,
           Buffer.allocUnsafe(expected.length),
           0,
           -1,
           0,
           common.mustNotCall());
-}, { code: 'ERR_OUT_OF_RANGE', type: RangeError,
-     message: 'The value of "length" is out of range.' });
+}, {
+  code: 'ERR_OUT_OF_RANGE',
+  name: 'RangeError [ERR_OUT_OF_RANGE]',
+  message: 'The value of "length" is out of range. ' +
+           'It must be >= 0 && <= 4. Received -1'
+});
 
 
-common.expectsError(
+assert.throws(
   () => fs.readSync(fd, expected.length, 0, 'utf-8'),
   {
     code: 'ERR_INVALID_ARG_TYPE',
-    type: TypeError,
+    name: 'TypeError [ERR_INVALID_ARG_TYPE]',
     message: 'The "buffer" argument must be one of type Buffer or Uint8Array.' +
              ' Received type number'
   }
 );
 
 [true, null, undefined, () => {}, {}].forEach((value) => {
-  common.expectsError(() => {
+  assert.throws(() => {
     fs.readSync(value,
                 Buffer.allocUnsafe(expected.length),
                 0,
@@ -75,26 +84,34 @@ common.expectsError(
                 0);
   }, {
     code: 'ERR_INVALID_ARG_TYPE',
-    type: TypeError,
+    name: 'TypeError [ERR_INVALID_ARG_TYPE]',
     message: 'The "fd" argument must be of type number. ' +
              `Received type ${typeof value}`
   });
 });
 
-common.expectsError(() => {
+assert.throws(() => {
   fs.readSync(fd,
               Buffer.allocUnsafe(expected.length),
               -1,
               expected.length,
               0);
-}, { code: 'ERR_OUT_OF_RANGE', type: RangeError,
-     message: 'The value of "offset" is out of range.' });
+}, {
+  code: 'ERR_OUT_OF_RANGE',
+  name: 'RangeError [ERR_OUT_OF_RANGE]',
+  message: 'The value of "offset" is out of range. ' +
+           'It must be >= 0 && <= 4. Received -1'
+});
 
-common.expectsError(() => {
+assert.throws(() => {
   fs.readSync(fd,
               Buffer.allocUnsafe(expected.length),
               0,
               -1,
               0);
-}, { code: 'ERR_OUT_OF_RANGE', type: RangeError,
-     message: 'The value of "length" is out of range.' });
+}, {
+  code: 'ERR_OUT_OF_RANGE',
+  name: 'RangeError [ERR_OUT_OF_RANGE]',
+  message: 'The value of "length" is out of range. ' +
+           'It must be >= 0 && <= 4. Received -1'
+});
