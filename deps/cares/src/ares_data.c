ec3f4d980b39113276d35cce5decb738b314f1b0
Rod Vagg <rod@vagg.org>
2018-04-11 21:34:43 +1000

    deps: upgrade to c-ares v1.14.0
     * Fix patch for CVE-2017-1000381 to not be overly aggressive.
     * win32: Preserve DNS server order returned by Windows when sorting and
       exclude DNS servers in legacy subnets.
     * win32: Support most recent Visual Studio 2017.
     * gethostbyaddr should fail with ECANCELLED not ENOTFOUND when
       ares_cancel is called.
     * win32: Exclude legacy ipv6 subnets.
     * android: Applications compiled for Oreo can no longer use
       __system_property_get and must use Java calls to retrieve DNS
       servers.
     * win32: Force use of ANSI functions.
     * CMake minimum version is now 3.1.
     * ares_gethostbyname.3: fix callback status values.
     * docs: Document WSAStartup requirement.
     * Fix a typo in init_by_resolv_conf.
     * Android JNI code leaks local references in some cases.
     * Force using the ANSI versions of WinAPI functions.
    
    The most important changes have already been included via
    
     * 50e580df21 deps: cherry-pick 0ef4a0c64b6 from c-ares upstream
     * 9a0631dffe deps: cherry-pick 18ea996 from c-ares upstream
    
    PR-URL: https://github.com/nodejs/node/pull/19939
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- deps/cares/src/ares_data.c
+++ deps/cares/src/ares_data.c
@@ -40,10 +40,9 @@
 
 void ares_free_data(void *dataptr)
 {
-  struct ares_data *ptr;
-
-  if (!dataptr)
-    return;
+  while (dataptr != NULL) {
+    struct ares_data *ptr;
+    void *next_data = NULL;
 
 #ifdef __INTEL_COMPILER
 #  pragma warning(push)
@@ -51,80 +50,82 @@ void ares_free_data(void *dataptr)
    /* 1684: conversion from pointer to same-sized integral type */
 #endif
 
-  ptr = (void *)((char *)dataptr - offsetof(struct ares_data, data));
+    ptr = (void *)((char *)dataptr - offsetof(struct ares_data, data));
 
 #ifdef __INTEL_COMPILER
 #  pragma warning(pop)
 #endif
 
-  if (ptr->mark != ARES_DATATYPE_MARK)
-    return;
+    if (ptr->mark != ARES_DATATYPE_MARK)
+      return;
 
-  switch (ptr->type)
-    {
-      case ARES_DATATYPE_MX_REPLY:
+    switch (ptr->type)
+      {
+        case ARES_DATATYPE_MX_REPLY:
 
-        if (ptr->data.mx_reply.next)
-          ares_free_data(ptr->data.mx_reply.next);
-        if (ptr->data.mx_reply.host)
-          ares_free(ptr->data.mx_reply.host);
-        break;
+          if (ptr->data.mx_reply.next)
+            next_data = ptr->data.mx_reply.next;
+          if (ptr->data.mx_reply.host)
+            ares_free(ptr->data.mx_reply.host);
+          break;
 
-      case ARES_DATATYPE_SRV_REPLY:
+        case ARES_DATATYPE_SRV_REPLY:
 
-        if (ptr->data.srv_reply.next)
-          ares_free_data(ptr->data.srv_reply.next);
-        if (ptr->data.srv_reply.host)
-          ares_free(ptr->data.srv_reply.host);
-        break;
+          if (ptr->data.srv_reply.next)
+            next_data = ptr->data.srv_reply.next;
+          if (ptr->data.srv_reply.host)
+            ares_free(ptr->data.srv_reply.host);
+          break;
 
-      case ARES_DATATYPE_TXT_REPLY:
-      case ARES_DATATYPE_TXT_EXT:
+        case ARES_DATATYPE_TXT_REPLY:
+        case ARES_DATATYPE_TXT_EXT:
 
-        if (ptr->data.txt_reply.next)
-          ares_free_data(ptr->data.txt_reply.next);
-        if (ptr->data.txt_reply.txt)
-          ares_free(ptr->data.txt_reply.txt);
-        break;
+          if (ptr->data.txt_reply.next)
+            next_data = ptr->data.txt_reply.next;
+          if (ptr->data.txt_reply.txt)
+            ares_free(ptr->data.txt_reply.txt);
+          break;
 
-      case ARES_DATATYPE_ADDR_NODE:
+        case ARES_DATATYPE_ADDR_NODE:
 
-        if (ptr->data.addr_node.next)
-          ares_free_data(ptr->data.addr_node.next);
-        break;
+          if (ptr->data.addr_node.next)
+            next_data = ptr->data.addr_node.next;
+          break;
 
-      case ARES_DATATYPE_ADDR_PORT_NODE:
+        case ARES_DATATYPE_ADDR_PORT_NODE:
 
-        if (ptr->data.addr_port_node.next)
-          ares_free_data(ptr->data.addr_port_node.next);
-        break;
+          if (ptr->data.addr_port_node.next)
+            next_data = ptr->data.addr_port_node.next;
+          break;
 
-      case ARES_DATATYPE_NAPTR_REPLY:
+        case ARES_DATATYPE_NAPTR_REPLY:
 
-        if (ptr->data.naptr_reply.next)
-          ares_free_data(ptr->data.naptr_reply.next);
-        if (ptr->data.naptr_reply.flags)
-          ares_free(ptr->data.naptr_reply.flags);
-        if (ptr->data.naptr_reply.service)
-          ares_free(ptr->data.naptr_reply.service);
-        if (ptr->data.naptr_reply.regexp)
-          ares_free(ptr->data.naptr_reply.regexp);
-        if (ptr->data.naptr_reply.replacement)
-          ares_free(ptr->data.naptr_reply.replacement);
-        break;
+          if (ptr->data.naptr_reply.next)
+            next_data = ptr->data.naptr_reply.next;
+          if (ptr->data.naptr_reply.flags)
+            ares_free(ptr->data.naptr_reply.flags);
+          if (ptr->data.naptr_reply.service)
+            ares_free(ptr->data.naptr_reply.service);
+          if (ptr->data.naptr_reply.regexp)
+            ares_free(ptr->data.naptr_reply.regexp);
+          if (ptr->data.naptr_reply.replacement)
+            ares_free(ptr->data.naptr_reply.replacement);
+          break;
 
-      case ARES_DATATYPE_SOA_REPLY:
-        if (ptr->data.soa_reply.nsname)
-          ares_free(ptr->data.soa_reply.nsname);
-        if (ptr->data.soa_reply.hostmaster)
-          ares_free(ptr->data.soa_reply.hostmaster);
-	break;
+        case ARES_DATATYPE_SOA_REPLY:
+          if (ptr->data.soa_reply.nsname)
+            ares_free(ptr->data.soa_reply.nsname);
+          if (ptr->data.soa_reply.hostmaster)
+            ares_free(ptr->data.soa_reply.hostmaster);
+          break;
 
-      default:
-        return;
-    }
+        default:
+          return;
+      }
 
-  ares_free(ptr);
+    ares_free(ptr);
+    dataptr = next_data;
+  }
 }
 
 
--- deps/cares/src/ares_platform.c
+++ deps/cares/src/ares_platform.c
@@ -36,6 +36,10 @@ win_platform ares__getplatform(void)
 
   memset(&OsvEx, 0, sizeof(OsvEx));
   OsvEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable:4996) /* warning C4996: 'GetVersionExW': was declared deprecated */
+#endif
   if (!GetVersionEx((void *)&OsvEx))
     {
       memset(&OsvEx, 0, sizeof(OsvEx));
@@ -43,6 +47,9 @@ win_platform ares__getplatform(void)
       if (!GetVersionEx((void *)&OsvEx))
         return WIN_UNKNOWN;
     }
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
   switch(OsvEx.dwPlatformId)
     {
