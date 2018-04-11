b0b7afcfbe0f38715af062ebcb9e1c6bbc68ba68
Rod Vagg <rod@vagg.org>
2018-04-11 21:34:43 +1000

    deps: upgrade to c-ares v1.14.0
     * Fix patch for CVE-2017-1000381 to not be overly aggressive.
     * win32: Preserve DNS server order returned by Windows when sorting and
       exclude DNS servers in legacy subnets.
     * win32: Support most recent Visual Studio 2017.
     * gethostbyaddr should fail with ECANCELLED not ENOTFOUND when
       ares_cancel is called.
     * win32: Exclude legacy ipv6 subnets.
     * android: Applications compiled for Oreo can no longer use
       __system_property_get and must use Java calls to retrieve DNS
       servers.
     * win32: Force use of ANSI functions.
     * CMake minimum version is now 3.1.
     * ares_gethostbyname.3: fix callback status values.
     * docs: Document WSAStartup requirement.
     * Fix a typo in init_by_resolv_conf.
     * Android JNI code leaks local references in some cases.
     * Force using the ANSI versions of WinAPI functions.
    
    The most important changes have already been included via
    
     * 50e580df21 deps: cherry-pick 0ef4a0c64b6 from c-ares upstream
     * 9a0631dffe deps: cherry-pick 18ea996 from c-ares upstream
    
    PR-URL: https://github.com/nodejs/node/pull/19939
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- deps/cares/include/ares_build.h
+++ deps/cares/include/ares_build.h
@@ -194,16 +194,14 @@
 /* Data type definition of ares_ssize_t. */
 #ifdef _WIN32
 #  ifdef _WIN64
-     typedef __int64 ares_ssize_t;
+#    define CARES_TYPEOF_ARES_SSIZE_T __int64
 #  else
-     typedef long ares_ssize_t;
+#    define CARES_TYPEOF_ARES_SSIZE_T long
 #  endif
 #else
-#  ifdef CARES_TYPEOF_ARES_SSIZE_T
-     typedef CARES_TYPEOF_ARES_SSIZE_T ares_ssize_t;
-#  else
-     typedef ssize_t ares_ssize_t;
-#  endif
+#  define CARES_TYPEOF_ARES_SSIZE_T ssize_t
 #endif
+
+typedef CARES_TYPEOF_ARES_SSIZE_T ares_ssize_t;
 
 #endif /* __CARES_BUILD_H */
