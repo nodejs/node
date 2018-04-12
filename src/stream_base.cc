0cb3325f124805c0f8911627a38cfb34be35b675
Anna Henningsen <anna@addaleax.net>
2018-04-12 21:53:59 +0200

    tls: fix SSL write error handling
    Fix an use-after-free bug in the TLS implementation.
    
    If we return from `DoWrite()` with an early error, we should
    not be storing the `WriteWrap` object and complete it
    again at a later point, when it has already been freed
    (because of the write error).
    
    This issue was reported by Jordan Zebor at F5 Networks,
    who also helped with investigating this bug and coming
    up with a reproduction.
    
    This fixes CVE-2018-7162.
    
    Fixes: https://github.com/nodejs-private/security/issues/189
    PR-URL: https://github.com/nodejs-private/node-private/pull/127
    Reviewed-By: Evan Lucas <evanlucas@me.com>

--- src/stream_base.cc
+++ src/stream_base.cc
@@ -387,6 +387,7 @@ void ReportWritesToJSStreamListener::OnStreamAfterReqFinished(
   AsyncWrap* async_wrap = req_wrap->GetAsyncWrap();
   HandleScope handle_scope(env->isolate());
   Context::Scope context_scope(env->context());
+  CHECK(!async_wrap->persistent().IsEmpty());
   Local<Object> req_wrap_obj = async_wrap->object();
 
   Local<Value> argv[] = {
