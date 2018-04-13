eeb88e1f83ac135f4447b334c4c57b6a4f05de07
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-04-13 15:11:38 +0200

    test: remove test case 0 from tls-cnnic-whitelist
    I looks like this test has not worked as expected since commit
    2bc7841d0fcdd066fe477873229125b6f003b693 ("test: use random ports
    where possible"). The test in that commit checked for `CERT_REVOKED`
    which was returned by CheckWhitelistedServerCert.
    
    CheckWhitelistedServerCert was later removed in commit
    dc875438a3953102febffa79b691317bb24ba2aa ("src: drop CNNIC+StartCom
    certificate whitelisting").
    
    I'm suggesting that this test case be removed as I don't think it is
    valid anymore.
    
    PR-URL: https://github.com/nodejs/node/pull/19767
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-tls-cnnic-whitelist.js
+++ test/parallel/test-tls-cnnic-whitelist.js
@@ -14,22 +14,6 @@ function loadPEM(n) {
 }
 
 const testCases = [
-  { // Test 0: for the check of a cert not in the whitelist.
-    // agent7-cert.pem is issued by the fake CNNIC root CA so that its
-    // hash is not listed in the whitelist.
-    // fake-cnnic-root-cert has the same subject name as the original
-    // rootCA.
-    serverOpts: {
-      key: loadPEM('agent7-key'),
-      cert: loadPEM('agent7-cert')
-    },
-    clientOpts: {
-      port: undefined,
-      rejectUnauthorized: true,
-      ca: [loadPEM('fake-cnnic-root-cert')]
-    },
-    errorCode: 'CERT_HAS_EXPIRED'
-  },
   // Test 1: for the fix of node#2061
   // agent6-cert.pem is signed by intermediate cert of ca3.
   // The server has a cert chain of agent6->ca3->ca1(root) but
