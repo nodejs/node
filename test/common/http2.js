0ab90acaf328ecbd5e3a3ea705ea59b5c674f20a
James M Snell <jasnell@gmail.com>
2018-04-13 08:11:26 -0700

    test: add regression test for nghttp2 CVE-2018-1000168
    PR-URL: https://github.com/nodejs-private/node-private/pull/124
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Evan Lucas <evanlucas@me.com>

--- test/common/http2.js
+++ test/common/http2.js
@@ -127,8 +127,18 @@ class PingFrame extends Frame {
   }
 }
 
+class AltSvcFrame extends Frame {
+  constructor(size) {
+    const buffers = [Buffer.alloc(size)];
+    super(size, 10, 0, 0);
+    buffers.unshift(this[kFrameData]);
+    this[kFrameData] = Buffer.concat(buffers);
+  }
+}
+
 module.exports = {
   Frame,
+  AltSvcFrame,
   DataFrame,
   HeadersFrame,
   SettingsFrame,
