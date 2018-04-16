d4877172d51c6788dfbb2239eaa18c30a2a8bc0c
Anatoli Papirovski <apapirovski@mac.com>
2018-04-16 08:50:33 +0200

    fixup: less flaky test

--- test/parallel/test-timers-enroll-second-time.js
+++ test/parallel/test-timers-enroll-second-time.js
@@ -5,12 +5,12 @@ const common = require('../common');
 const assert = require('assert');
 const timers = require('timers');
 
-let didCall = false;
 const enrollObj = {
-  _onTimeout: common.mustCall(() => assert(didCall)),
+  _onTimeout: common.mustCall(),
 };
 
 timers.enroll(enrollObj, 1);
+assert.strictEqual(enrollObj._idleTimeout, 1);
 timers.enroll(enrollObj, 10);
+assert.strictEqual(enrollObj._idleTimeout, 10);
 timers.active(enrollObj);
-setTimeout(common.mustCall(() => { didCall = true; }), 1);
