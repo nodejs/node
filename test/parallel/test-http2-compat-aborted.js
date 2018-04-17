1edd7f6393f3ca16650e0aa5f4b3c1abc270b566
Robert Nagy <ronagy@icloud.com>
2018-04-17 11:37:50 +0200

    http: added aborted property to request
    Backport-PR-URL: https://github.com/nodejs/node/pull/22850
    PR-URL: https://github.com/nodejs/node/pull/20094
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

---
+++ test/parallel/test-http2-compat-aborted.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+const h2 = require('http2');
+const assert = require('assert');
+
+
+const server = h2.createServer(common.mustCall(function(req, res) {
+  req.on('aborted', common.mustCall(function() {
+    assert.strictEqual(this.aborted, true);
+  }));
+  assert.strictEqual(req.aborted, false);
+  res.write('hello');
+  server.close();
+}));
+
+server.listen(0, common.mustCall(function() {
+  const url = `http://localhost:${server.address().port}`;
+  const client = h2.connect(url, common.mustCall(() => {
+    const request = client.request();
+    request.on('data', common.mustCall((chunk) => {
+      client.destroy();
+    }));
+  }));
+}));
