f074612b744fecfd1f79fd05d7f10fe9a1487e5a
Gus Caplan <me@gus.host>
2018-04-22 18:56:42 -0500

    esm: provide named exports for builtin libs
    Provide named exports for all builtin libraries so that the libraries
    may be imported in a nicer way for esm users. The default export is left
    as the entire namespace (module.exports) and wrapped in a proxy such
    that APMs and other behavior are still left intact.
    
    PR-URL: https://github.com/nodejs/node/pull/20403
    Reviewed-By: Bradley Farias <bradley.meck@gmail.com>
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: Jan Krems <jan.krems@gmail.com>

--- test/fixtures/es-module-loaders/js-loader.mjs
+++ test/fixtures/es-module-loaders/js-loader.mjs
@@ -1,10 +1,11 @@
-import _url from 'url';
+import { URL } from 'url';
+
 const builtins = new Set(
   Object.keys(process.binding('natives')).filter(str =>
     /^(?!(?:internal|node|v8)\/)/.test(str))
 )
 
-const baseURL = new _url.URL('file://');
+const baseURL = new URL('file://');
 baseURL.pathname = process.cwd() + '/';
 
 export function resolve (specifier, base = baseURL) {
@@ -15,7 +16,7 @@ export function resolve (specifier, base = baseURL) {
     };
   }
   // load all dependencies as esm, regardless of file extension
-  const url = new _url.URL(specifier, base).href;
+  const url = new URL(specifier, base).href;
   return {
     url,
     format: 'esm'
