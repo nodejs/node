f4a26e6e329ff203f3446e4319596991383b77b7
Gabriel Schulhof <gabriel.schulhof@intel.com>
2018-04-24 19:07:08 -0400

    n-api: heed linter

--- test/addons-napi/test_inheritance/test.js
+++ test/addons-napi/test_inheritance/test.js
@@ -1,5 +1,6 @@
 'use strict';
 const common = require('../../common');
-const test_inheritance = require(`./build/${common.buildType}/test_inheritance`);
+const test_inheritance =
+    require(`./build/${common.buildType}/test_inheritance`);
 const x = new test_inheritance.Subclass();
 x.superclassMethod();
