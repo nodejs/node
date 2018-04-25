e8e6e775fbe3f92724411391e57abe0dd5a44622
Gabriel Schulhof <gabriel.schulhof@intel.com>
2018-04-25 13:15:50 -0400

    n-api,test: remove superfluous persistent
    Remove a superfluos persistent from test_constructor_name.c.
    
    PR-URL: https://github.com/nodejs/node/pull/20299
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/addons-napi/test_constructor/test_constructor_name.c
+++ test/addons-napi/test_constructor/test_constructor_name.c
@@ -1,8 +1,6 @@
 #include <node_api.h>
 #include "../common.h"
 
-napi_ref constructor_;
-
 static napi_value New(napi_env env, napi_callback_info info) {
   napi_value _this;
   NAPI_CALL(env, napi_get_cb_info(env, info, NULL, NULL, &_this, NULL));
@@ -14,9 +12,6 @@ static napi_value Init(napi_env env, napi_value exports) {
   napi_value cons;
   NAPI_CALL(env, napi_define_class(
       env, "MyObject_Extra", 8, New, NULL, 0, NULL, &cons));
-
-  NAPI_CALL(env,
-      napi_create_reference(env, cons, 1, &constructor_));
   return cons;
 }
 
