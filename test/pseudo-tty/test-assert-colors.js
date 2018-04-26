8a0fb137d887db56337a7206804affcbc8e18304
Ruben Bridgewater <ruben@bridgewater.de>
2018-04-26 03:24:45 +0200

    assert: make skipping indicator blue
    If lines gets skipped, they are marked with three dots. This makes
    sure they are better visualized to distinguish them from everything
    else.
    
    PR-URL: https://github.com/nodejs/node/pull/20315
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>

--- test/pseudo-tty/test-assert-colors.js
+++ test/pseudo-tty/test-assert-colors.js
@@ -5,13 +5,16 @@ const assert = require('assert').strict;
 try {
   // Activate colors even if the tty does not support colors.
   process.env.COLORTERM = '1';
-  assert.deepStrictEqual([1, 2], [2, 2]);
+  assert.deepStrictEqual([1, 2, 2, 2], [2, 2, 2, 2]);
 } catch (err) {
   const expected = 'Input A expected to strictly deep-equal input B:\n' +
-    '\u001b[32m+ expected\u001b[39m \u001b[31m- actual\u001b[39m\n\n' +
+    '\u001b[32m+ expected\u001b[39m \u001b[31m- actual\u001b[39m' +
+      ' \u001b[34m...\u001b[39m Lines skipped\n\n' +
     '  [\n' +
     '\u001b[31m-\u001b[39m   1,\n' +
     '\u001b[32m+\u001b[39m   2,\n' +
+    '    2,\n' +
+    '\u001b[34m...\u001b[39m\n' +
     '    2\n' +
     '  ]';
   assert.strictEqual(err.message, expected);
