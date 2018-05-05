a9a236cd6cca2db1bb7e137b5452d9e1217ef0eb
rakshith_bellare@outlook.com <rakshithbellare@Rakshiths-MacBook-Pro.local>
2018-05-05 23:30:00 +0530

    test: removal of extra space in comment

--- test/parallel/test-module-relative-lookup.js
+++ test/parallel/test-module-relative-lookup.js
@@ -6,10 +6,10 @@ const _module = require('module'); // avoid collision with global.module
 const lookupResults = _module._resolveLookupPaths('./lodash');
 let paths = lookupResults[1];
 
-//  Current directory gets highest priority for local modules
+// Current directory gets highest priority for local modules
 assert.strictEqual(paths[0], '.');
 
 paths = _module._resolveLookupPaths('./lodash', null, true);
 
-//  Current directory gets highest priority for local modules
+// Current directory gets highest priority for local modules
 assert.strictEqual(paths && paths[0], '.');
