63f3ff6e533f35e32844548570db6a5fac683e3a
Rich Trott <rtrott@gmail.com>
2018-05-05 19:28:59 -0700

    test: apply fs.chown() test to fsPromises.chown()
    Make test-fs-chown-type-check test both fs.chown() and
    fsPromises.chown().

--- test/parallel/test-fs-chown-type-check.js
+++ test/parallel/test-fs-chown-type-check.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const common = require('../common');
+
+const assert = require('assert');
 const fs = require('fs');
 
 [false, 1, {}, [], null, undefined].forEach((i) => {
-  common.expectsError(
-    () => fs.chown(i, 1, 1, common.mustNotCall()),
-    {
-      code: 'ERR_INVALID_ARG_TYPE',
-      type: TypeError
-    }
-  );
+  const checkErr = (e) => {
+    assert.strictEqual(e.code, 'ERR_INVALID_ARG_TYPE');
+    assert.ok(e instanceof TypeError);
+    return true;
+  };
+  common.fsTest('chown', [i, 1, 1, checkErr], { throws: true });
   common.expectsError(
     () => fs.chownSync(i, 1, 1),
     {
@@ -21,20 +22,18 @@ const fs = require('fs');
 });
 
 [false, 'test', {}, [], null, undefined].forEach((i) => {
-  common.expectsError(
-    () => fs.chown('not_a_file_that_exists', i, 1, common.mustNotCall()),
-    {
-      code: 'ERR_INVALID_ARG_TYPE',
-      type: TypeError
-    }
+  const checkErr = (e) => {
+    assert.strictEqual(e.code, 'ERR_INVALID_ARG_TYPE');
+    assert.ok(e instanceof TypeError);
+    return true;
+  };
+  common.fsTest(
+    'chown', ['not_a_file_that_exists', i, 1, checkErr], { throws: true }
   );
-  common.expectsError(
-    () => fs.chown('not_a_file_that_exists', 1, i, common.mustNotCall()),
-    {
-      code: 'ERR_INVALID_ARG_TYPE',
-      type: TypeError
-    }
+  common.fsTest(
+    'chown', ['not_a_file_that_exists', 1, i, checkErr], { throws: true }
   );
+
   common.expectsError(
     () => fs.chownSync('not_a_file_that_exists', i, 1),
     {
