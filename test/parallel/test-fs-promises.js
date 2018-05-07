6558dcbf75c0b4b749d42ec8678af0a8a728201f
Masashi Hirano <cherrydog07@gmail.com>
2018-05-08 02:16:18 +0900

    test: add test for fs.promises.lchmod
    To increase test coverage for fs.promises by adding a test for
    lchmod.
    
    PR-URL: https://github.com/nodejs/node/pull/20584
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/parallel/test-fs-promises.js
+++ test/parallel/test-fs-promises.js
@@ -12,6 +12,7 @@ const {
   chmod,
   copyFile,
   link,
+  lchmod,
   lstat,
   mkdir,
   mkdtemp,
@@ -129,7 +130,6 @@ function verifyStatObject(stat) {
     if (common.canCreateSymLink()) {
       const newLink = path.resolve(tmpDir, 'baz3.js');
       await symlink(newPath, newLink);
-
       stats = await lstat(newLink);
       verifyStatObject(stats);
 
@@ -137,6 +137,14 @@ function verifyStatObject(stat) {
                          (await realpath(newLink)).toLowerCase());
       assert.strictEqual(newPath.toLowerCase(),
                          (await readlink(newLink)).toLowerCase());
+      if (common.isOSX) {
+        // lchmod is only available on macOS
+        const newMode = 0o666;
+        await lchmod(newLink, newMode);
+        stats = await lstat(newLink);
+        assert.strictEqual(stats.mode & 0o777, newMode);
+      }
+
 
       await unlink(newLink);
     }
