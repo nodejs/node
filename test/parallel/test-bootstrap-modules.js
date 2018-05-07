3ca0b58759baa4bb2a25e76111fe75781f122373
Ruben Bridgewater <ruben@bridgewater.de>
2018-05-07 06:04:07 +0200

    test: add loaded modules test
    This makes sure there is no regression by switching to loading
    eagerly.

---
+++ test/parallel/test-bootstrap-modules.js
@@ -0,0 +1,14 @@
+/* eslint-disable node-core/required-modules */
+
+'use strict';
+
+// Ordinarily test files must require('common') but that action causes
+// the global console to be compiled, defeating the purpose of this test.
+// This makes sure no additional files are added without carefully considering
+// lazy loading. Please adjust the value if necessary.
+
+const list = process.moduleLoadList.slice();
+
+const assert = require('assert');
+
+assert(list.length <= 73, list);
