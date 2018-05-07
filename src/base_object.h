36793b8c650e80ad6f35728d586eda7637703466
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-05-07 10:24:42 +0200

    src: rename handle parameter object
    This commit renames the handle parameter for the BaseObject constructor
    to object instead of handle.
    
    The motivation for doing this is that when stepping through an
    inheritance chain it can sometimes be a little confusing when
    HandleWrap is in involved. HandleWrap has a handle parameter
    but calls the object that is passed to AsyncWrap object, but
    then when you end up in BaseObject it is named handle.
    
    PR-URL: https://github.com/nodejs/node/pull/20570
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/base_object.h
+++ src/base_object.h
@@ -34,9 +34,9 @@ class Environment;
 
 class BaseObject {
  public:
-  // Associates this object with `handle`. It uses the 0th internal field for
+  // Associates this object with `object`. It uses the 0th internal field for
   // that, and in particular aborts if there is no such field.
-  inline BaseObject(Environment* env, v8::Local<v8::Object> handle);
+  inline BaseObject(Environment* env, v8::Local<v8::Object> object);
   virtual inline ~BaseObject();
 
   // Returns the wrapped object.  Returns an empty handle when
