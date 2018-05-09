da7eaa8ef0fac2444f6892753c9db5cae39f6e1a
Ruben Bridgewater <ruben@bridgewater.de>
2018-05-09 15:52:37 +0200

    fixup: test

--- test/parallel/test-async-hooks-async-await-regression.js
+++ test/parallel/test-async-hooks-async-await-regression.js
@@ -11,7 +11,7 @@ const asyncIds = [];
 
 async_hooks.createHook({
   init: (asyncId, type, triggerAsyncId, resource) => {
-    asyncIds.push(`${triggerAsyncId} => ${asyncId}`);
+    asyncIds.push([triggerAsyncId, asyncId]);
   }
 }).enable();
 
@@ -21,5 +21,14 @@ async function main() {
   console.log('world');
 }
 
-main().then(() => assert.deepStrictEqual(
-  asyncIds, [ '1 => 3', '1 => 4', '1 => 5', '3 => 6', '3 => 7' ]));
+main().then(() => {
+  // Verify correct relationship between ids, e.g.:
+  // '1 => 3', '1 => 4', '1 => 5', '3 => 6', '3 => 7'
+  assert.strictEqual(asyncIds[0][0], asyncIds[1][0]);
+  assert.strictEqual(asyncIds[0][0], asyncIds[2][0]);
+  assert.strictEqual(asyncIds[0][1], asyncIds[3][0]);
+  assert.strictEqual(asyncIds[0][1], asyncIds[4][0]);
+  assert.strictEqual(asyncIds[0][1] + 1, asyncIds[1][1]);
+  assert.strictEqual(asyncIds[0][1] + 2, asyncIds[2][1]);
+  assert.strictEqual(asyncIds[3][1] + 1, asyncIds[4][1]);
+});
