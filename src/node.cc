e93726ad10ee240c8b86a5522f69144eeb9a06f5
Anna Henningsen <anna@addaleax.net>
2018-05-09 19:52:53 +0200

    src: fix nullptr dereference for signal during startup
    This fixes a test failure when running
    `test/parallel/test-child-process-spawnsync-kill-signal.js`
    under load. What would happen is that `SignalExit()` tries
    to shutdown the tracing agent, which might not have been set
    up by the point that Node.js receives the signal.
    
    PR-URL: https://github.com/nodejs/node/pull/20637
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node.cc
+++ src/node.cc
@@ -326,7 +326,8 @@ static struct {
   }
 
   void StopTracingAgent() {
-    tracing_agent_->Stop();
+    if (tracing_agent_)
+      tracing_agent_->Stop();
   }
 
   tracing::Agent* GetTracingAgent() const {
