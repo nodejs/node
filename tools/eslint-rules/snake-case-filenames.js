a96c7ac0913b54c621f8526872ed86bf9dbd3191
Shobhit Chittora <chittorashobhit@gmail.com>
2018-05-13 02:43:09 +0530

    tools: Adds check for target dirs + fix regex test Refs:  #20446

--- tools/eslint-rules/snake-case-filenames.js
+++ tools/eslint-rules/snake-case-filenames.js
@@ -12,13 +12,19 @@ const path = require('path');
 
 module.exports = function(context) {
   const snakeCaseRegex = new RegExp('^[a-z_]+$');
+  const targetDirs = ['lib', 'deps', 'src'];
+
+  function checkIfInTargetDir(dirName) {
+    return targetDirs.filter((dir) => dirName.indexOf(dir) !== -1).length !== 0;
+  }
 
   return {
     'Program': function(node) {
       const filename = path.resolve(context.getFilename());
       const name = path.basename(filename, path.extname(filename));
+      const dirName = path.dirname(filename);
 
-      if (snakeCaseRegex.test(name)) {
+      if (!snakeCaseRegex.test(name) && checkIfInTargetDir(dirName)) {
         context.report(node, "Filename '{{name}}' is not in snake_case.", {
           name
         });
