352ae2397434ddde058c14cd357e3bdd1d029cb6
Ruben Bridgewater <ruben@bridgewater.de>
2018-05-12 12:02:38 +0200

    test: reduce runtime
    This refactors some tests to reduce the runtime of those.
    
    PR-URL: https://github.com/nodejs/node/pull/20688
    Refs: https://github.com/nodejs/node/issues/20128
    Reviewed-By: Tiancheng "Timothy" Gu <timothygu99@gmail.com>
    Reviewed-By: Yang Guo <yangguo@chromium.org>

--- test/parallel/test-child-process-exec-encoding.js
+++ test/parallel/test-child-process-exec-encoding.js
@@ -1,17 +1,17 @@
 'use strict';
 const common = require('../common');
-const assert = require('assert');
-const cp = require('child_process');
 const stdoutData = 'foo';
 const stderrData = 'bar';
-const expectedStdout = `${stdoutData}\n`;
-const expectedStderr = `${stderrData}\n`;
 
 if (process.argv[2] === 'child') {
   // The following console calls are part of the test.
   console.log(stdoutData);
   console.error(stderrData);
 } else {
+  const assert = require('assert');
+  const cp = require('child_process');
+  const expectedStdout = `${stdoutData}\n`;
+  const expectedStderr = `${stderrData}\n`;
   function run(options, callback) {
     const cmd = `"${process.execPath}" "${__filename}" child`;
 
