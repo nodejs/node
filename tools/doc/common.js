38fc741c36d2ef5feb7e2af8ff31bd95f565905a
Vse Mozhet Byt <vsemozhetbyt@gmail.com>
2018-05-13 17:47:55 +0300

    tools: eliminate intermediate module in doctools
    PR-URL: https://github.com/nodejs/node/pull/20701
    Fixes: https://github.com/nodejs/node/issues/20685
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- tools/doc/common.js
+++ tools/doc/common.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const yaml = require('js-yaml');
+const yaml =
+  require(`${__dirname}/../node_modules/eslint/node_modules/js-yaml`);
 
 function isYAMLBlock(text) {
   return /^<!-- YAML/.test(text);
