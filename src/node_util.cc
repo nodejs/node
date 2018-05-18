bd1f355fc5805c8307da85a16d1a385e5c48813a
Ruben Bridgewater <ruben@bridgewater.de>
2018-05-19 00:25:07 +0200

    lib,src: replace all C++ promises with JS promises
    C++ promises can not be properly optimized by V8. They also behave
    a tiny bit different than "regular" promises.
    
    PR-URL: https://github.com/nodejs/node/pull/20830
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: Tiancheng "Timothy" Gu <timothygu99@gmail.com>
    Reviewed-By: Benedikt Meurer <benedikt.meurer@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- src/node_util.cc
+++ src/node_util.cc
@@ -9,7 +9,6 @@ using v8::Context;
 using v8::FunctionCallbackInfo;
 using v8::Integer;
 using v8::Local;
-using v8::Maybe;
 using v8::Object;
 using v8::Private;
 using v8::Promise;
@@ -127,36 +126,6 @@ void WatchdogHasPendingSigint(const FunctionCallbackInfo<Value>& args) {
   args.GetReturnValue().Set(ret);
 }
 
-
-void CreatePromise(const FunctionCallbackInfo<Value>& args) {
-  Local<Context> context = args.GetIsolate()->GetCurrentContext();
-  auto maybe_resolver = Promise::Resolver::New(context);
-  if (!maybe_resolver.IsEmpty())
-    args.GetReturnValue().Set(maybe_resolver.ToLocalChecked());
-}
-
-
-void PromiseResolve(const FunctionCallbackInfo<Value>& args) {
-  Local<Context> context = args.GetIsolate()->GetCurrentContext();
-  Local<Value> promise = args[0];
-  CHECK(promise->IsPromise());
-  if (promise.As<Promise>()->State() != Promise::kPending) return;
-  Local<Promise::Resolver> resolver = promise.As<Promise::Resolver>();  // sic
-  Maybe<bool> ret = resolver->Resolve(context, args[1]);
-  args.GetReturnValue().Set(ret.FromMaybe(false));
-}
-
-
-void PromiseReject(const FunctionCallbackInfo<Value>& args) {
-  Local<Context> context = args.GetIsolate()->GetCurrentContext();
-  Local<Value> promise = args[0];
-  CHECK(promise->IsPromise());
-  if (promise.As<Promise>()->State() != Promise::kPending) return;
-  Local<Promise::Resolver> resolver = promise.As<Promise::Resolver>();  // sic
-  Maybe<bool> ret = resolver->Reject(context, args[1]);
-  args.GetReturnValue().Set(ret.FromMaybe(false));
-}
-
 void SafeGetenv(const FunctionCallbackInfo<Value>& args) {
   CHECK(args[0]->IsString());
   Utf8Value strenvtag(args.GetIsolate(), args[0]);
@@ -210,10 +179,6 @@ void Initialize(Local<Object> target,
   env->SetMethod(target, "stopSigintWatchdog", StopSigintWatchdog);
   env->SetMethodNoSideEffect(target, "watchdogHasPendingSigint",
                              WatchdogHasPendingSigint);
-
-  env->SetMethodNoSideEffect(target, "createPromise", CreatePromise);
-  env->SetMethod(target, "promiseResolve", PromiseResolve);
-  env->SetMethod(target, "promiseReject", PromiseReject);
 
   env->SetMethod(target, "safeGetenv", SafeGetenv);
 }
