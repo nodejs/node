f2f8375a728f51da0126e92fec3d52bacd2262aa
cjihrig <cjihrig@gmail.com>
2018-05-20 11:41:39 -0400

    fs: don't limit ftruncate() length to 32 bits
    The length used by ftruncate() is 64 bits in the binding layer.
    This commit removes the 32 bit restriction in the JS layer.
    
    PR-URL: https://github.com/nodejs/node/pull/20851
    Fixes: https://github.com/nodejs/node/issues/20844
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- test/parallel/test-fs-truncate.js
+++ test/parallel/test-fs-truncate.js
@@ -220,17 +220,14 @@ function testFtruncate(cb) {
                   `an integer. Received ${input}`
       }
     );
-  });
 
-  // 2 ** 31 = 2147483648
-  [2147483648, -2147483649].forEach((input) => {
     assert.throws(
       () => fs.ftruncate(fd, input),
       {
         code: 'ERR_OUT_OF_RANGE',
         name: 'RangeError [ERR_OUT_OF_RANGE]',
         message: 'The value of "len" is out of range. It must be ' +
-                  `> -2147483649 && < 2147483648. Received ${input}`
+                  `an integer. Received ${input}`
       }
     );
   });
