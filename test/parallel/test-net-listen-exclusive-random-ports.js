00b02444038ef01fce0428a0f0472a9e1d0e0b99
nam <nam.nguyen@de.ibm.com>
2018-05-22 14:01:50 +0200

    test: string to template literals, to include port
    test: string to template literals, to include port
    
    test: string to template literals, to include port

--- test/parallel/test-net-listen-exclusive-random-ports.js
+++ test/parallel/test-net-listen-exclusive-random-ports.js
@@ -9,11 +9,11 @@ if (cluster.isMaster) {
   const worker1 = cluster.fork();
 
   worker1.on('message', function(port1) {
-    assert.strictEqual(port1, port1 | 0, 'first worker could not listen');
+    assert.strictEqual(port1, port1 | 0, `first worker could not listen on port ${port1}`);
     const worker2 = cluster.fork();
 
     worker2.on('message', function(port2) {
-      assert.strictEqual(port2, port2 | 0, 'second worker could not listen');
+      assert.strictEqual(port2, port2 | 0, `second worker could not listen on port ${port2}`);
       assert.notStrictEqual(port1, port2, 'ports should not be equal');
       worker1.kill();
       worker2.kill();
@@ -28,7 +28,7 @@ if (cluster.isMaster) {
 
   server.listen({
     port: 0,
-    exclusive: true
+    exclusive: true,
   }, function() {
     process.send(server.address().port);
   });
