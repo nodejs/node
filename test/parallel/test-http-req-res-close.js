8ce20aff2dbee80ca2739f37c36999e3906f5f06
Robert Nagy <ronagy@icloud.com>
2018-05-24 16:30:35 +0200

    http: fix res emit close before user finish
    PR-URL: https://github.com/nodejs/node/pull/20941
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-http-req-res-close.js
+++ test/parallel/test-http-req-res-close.js
@@ -2,12 +2,21 @@
 
 const common = require('../common');
 const http = require('http');
+const assert = require('assert');
 
 const server = http.Server(common.mustCall((req, res) => {
+  let resClosed = false;
+
   res.end();
-  res.on('finish', common.mustCall());
-  res.on('close', common.mustCall());
-  req.on('close', common.mustCall());
+  res.on('finish', common.mustCall(() => {
+    assert.strictEqual(resClosed, false);
+  }));
+  res.on('close', common.mustCall(() => {
+    resClosed = true;
+  }));
+  req.on('close', common.mustCall(() => {
+    assert.strictEqual(req._readableState.ended, true);
+  }));
   res.socket.on('close', () => server.close());
 }));
 
