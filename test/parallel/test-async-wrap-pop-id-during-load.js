5baf75cc48cb1fb25d1ebe78825bdaba9a7bc94d
Rich Trott <rtrott@gmail.com>
2018-05-24 13:54:21 -0700

    test: improve error message in async-wrap test
    Improve AssertionError message in
    test/parallel/test-async-wrap-pop-id-during-load.js to include the
    contents of stderr when the spawned process fails.
    
    Refs: https://github.com/nodejs/node/pull/20940/files#r190707951

--- test/parallel/test-async-wrap-pop-id-during-load.js
+++ test/parallel/test-async-wrap-pop-id-during-load.js
@@ -17,7 +17,8 @@ const ret = spawnSync(
   process.execPath,
   ['--stack_size=50', __filename, 'async']
 );
-assert.strictEqual(ret.status, 0);
+assert.strictEqual(ret.status, 0,
+                   `EXIT CODE: ${ret.status}, STDERR:\n${ret.stderr}`);
 const stderr = ret.stderr.toString('utf8', 0, 2048);
 assert.ok(!/async.*hook/i.test(stderr));
 assert.ok(stderr.includes('UnhandledPromiseRejectionWarning: Error'), stderr);
