067757edc54aa24fd2a491399738486c7791bc2d
Rich Trott <rtrott@gmail.com>
2018-05-30 22:30:24 +0200

    test: check that benchmark tests are minimal
    Check that benchmark tests are not running longer than necessary by
    confirming that they only produce one set of configs to report on per
    benchmark file.

--- test/common/benchmark.js
+++ test/common/benchmark.js
@@ -20,10 +20,27 @@ function runBenchmark(name, args, env) {
 
   const mergedEnv = Object.assign({}, process.env, env);
 
-  const child = fork(runjs, argv, { env: mergedEnv });
+  const child = fork(runjs, argv, { env: mergedEnv, stdio: 'pipe' });
+  child.stdout.setEncoding('utf8');
+
+  let stdout = '';
+  child.stdout.on('data', (line) => {
+    stdout += line;
+  });
+
   child.on('exit', (code, signal) => {
     assert.strictEqual(code, 0);
     assert.strictEqual(signal, null);
+    // This bit makes sure that each benchmark file is being sent settings such
+    // that the benchmark file runs just one set of options. This helps keep the
+    // benchmark tests from taking a long time to run. Therefore, each benchmark
+    // file should result in three lines of output: a blank line, a line with
+    // the name of the benchmark file, and a line with the only results that we
+    // get from testing the benchmark file.
+    assert.ok(
+      /^(?:\n.+?\n.+?\n)+$/.test(stdout),
+      `benchmark file not running exactly one configuration in test: ${stdout}`
+    );
   });
 }
 
