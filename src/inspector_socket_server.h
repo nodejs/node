1c211ec90162088008228ed983ecb476a2cb2cbe
Eugene Ostroukhov <eostroukhov@google.com>
2018-05-31 14:24:21 -0700

    inspector: code cleanup
    Remove some unused code from the WS server implementation and switch to
    smart pointers where possible.
    
    PR-URL: https://github.com/nodejs/node/pull/21070
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/inspector_socket_server.h
+++ src/inspector_socket_server.h
@@ -16,19 +16,20 @@
 namespace node {
 namespace inspector {
 
-class Closer;
+class InspectorSocketServer;
 class SocketSession;
 class ServerSocket;
 
 class SocketServerDelegate {
  public:
+  virtual void AssignServer(InspectorSocketServer* server) = 0;
   virtual void StartSession(int session_id, const std::string& target_id) = 0;
   virtual void EndSession(int session_id) = 0;
   virtual void MessageReceived(int session_id, const std::string& message) = 0;
   virtual std::vector<std::string> GetTargetIds() = 0;
   virtual std::string GetTargetTitle(const std::string& id) = 0;
   virtual std::string GetTargetUrl(const std::string& id) = 0;
-  virtual void ServerDone() = 0;
+  virtual ~SocketServerDelegate() {}
 };
 
 // HTTP Server, writes messages requested as TransportActions, and responds
@@ -36,8 +37,7 @@ class SocketServerDelegate {
 
 class InspectorSocketServer {
  public:
-  using ServerCallback = void (*)(InspectorSocketServer*);
-  InspectorSocketServer(SocketServerDelegate* delegate,
+  InspectorSocketServer(std::unique_ptr<SocketServerDelegate> delegate,
                         uv_loop_t* loop,
                         const std::string& host,
                         int port,
@@ -49,7 +49,7 @@ class InspectorSocketServer {
 
   // Called by the TransportAction sent with InspectorIo::Write():
   //   kKill and kStop
-  void Stop(ServerCallback callback);
+  void Stop();
   //   kSendMessage
   void Send(int session_id, const std::string& message);
   //   kKill
@@ -58,12 +58,7 @@ class InspectorSocketServer {
   void AcceptSession(int session_id);
   //   kDeclineSession
   void DeclineSession(int session_id);
-
   int Port() const;
-
-  // Server socket lifecycle. There may be multiple sockets
-  void ServerSocketListening(ServerSocket* server_socket);
-  void ServerSocketClosed(ServerSocket* server_socket);
 
   // Session connection lifecycle
   void Accept(int server_port, uv_stream_t* server_socket);
@@ -76,27 +71,30 @@ class InspectorSocketServer {
     delegate_->MessageReceived(session_id, message);
   }
   SocketSession* Session(int session_id);
+  bool done() const {
+    return server_sockets_.empty() && connected_sessions_.empty();
+  }
 
  private:
+  static void CloseServerSocket(ServerSocket*);
+  using ServerSocketPtr = DeleteFnPtr<ServerSocket, CloseServerSocket>;
+
   void SendListResponse(InspectorSocket* socket, const std::string& host,
                         SocketSession* session);
   bool TargetExists(const std::string& id);
 
   enum class ServerState {kNew, kRunning, kStopping, kStopped};
   uv_loop_t* loop_;
-  SocketServerDelegate* const delegate_;
+  std::unique_ptr<SocketServerDelegate> delegate_;
   const std::string host_;
   int port_;
   std::string path_;
-  std::vector<ServerSocket*> server_sockets_;
-  Closer* closer_;
+  std::vector<ServerSocketPtr> server_sockets_;
   std::map<int, std::pair<std::string, std::unique_ptr<SocketSession>>>
       connected_sessions_;
   int next_session_id_;
   FILE* out_;
   ServerState state_;
-
-  friend class Closer;
 };
 
 }  // namespace inspector
