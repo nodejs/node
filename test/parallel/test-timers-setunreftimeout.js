64e15a73b8c3d83595f4176a7bbc634070a08bf1
Keita Akutsu <kakts.git@gmail.com>
2018-06-03 17:54:35 +0900

    test: refactor test-timers-setunreftimeout.js

--- test/parallel/test-timers-setunreftimeout.js
+++ test/parallel/test-timers-setunreftimeout.js
@@ -20,46 +20,42 @@ const { setUnrefTimeout } = require('internal/timers');
   );
 }
 
-{
-  const timer = setUnrefTimeout(common.mustCall(() => {
-    clearTimeout(testTimer);
-  }), 1);
-
-  const testTimer = setTimeout(common.mustNotCall(() => {
-  }), 1);
-}
-
 // test case for passing the arguments
 {
   for (let i = 0; i < 4; i++) {
-    let results = [];
-    let inputArgs = [];
+    const results = [];
+    const inputArgs = [];
     // set the input argument params
-    for (let j = 0; j <= i ; j++) {
+    for (let j = 0; j <= i; j++) {
       inputArgs.push(j);
     }
 
-    // For checking the arguments.length, callback function should not be arrow fucntion.
-    const timer = setUnrefTimeout(common.mustCall(function(args1, args2, args3, args4) {
-      // check the number of arguments passed to this callback.
-      strictEqual(arguments.length, i + 1,
-                  `arguments.length should be ${i + 1}. actual ${arguments.length}`
-      );
-      results.push(args1);
-      results.push(args2);
-      results.push(args3);
-      results.push(args4);
-    }), 1, ...inputArgs);
+    // For checking the arguments.length,
+    // callback function should not be arrow fucntion.
+    const timer = setUnrefTimeout(common.mustCall(
+      function(args1, args2, args3, args4) {
+        // check the number of arguments passed to this callback.
+        strictEqual(arguments.length, i + 1,
+                    `arguments.length should be ${i + 1}.` +
+                    `actual ${arguments.length}`
+        );
+        results.push(args1);
+        results.push(args2);
+        results.push(args3);
+        results.push(args4);
+      }
+    ), 1, ...inputArgs);
 
     const testTimer = setTimeout(common.mustCall(() => {
       const maxArgsNum = 4;
       for (let k = 0; k < maxArgsNum; k++) {
         // Checking the arguments passed to setUnrefTimeout
-        let expect = (k <= i) ? inputArgs[k] : undefined;
+        const expect = (k <= i) ? inputArgs[k] : undefined;
         strictEqual(expect, results[k],
                     `result ${k} should be ${inputArgs[k]}. actual ${expect}`);
       }
       clearTimeout(testTimer);
+      clearTimeout(timer);
     }), 100);
   }
 }
