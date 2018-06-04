3d8ec8f85c6c83da381d1b8b0358bae3ac9336b3
Rich Trott <rtrott@gmail.com>
2018-06-04 13:10:09 +0000

    test: make url-parse-invalid-input engine agnostic
    test-url-parse-invalid-input checks the message of an error that is
    generated by the JavaScript engine. Error messages that change in the
    underlying JavaScript engine should not be breaking changes in Node.js
    and therefore should not cause tests to fail. Remove the message check
    and replace it with a check of the type of the Error object along with
    the absence of a `code` property. (If a `code` property were present, it
    would indicate that the error was coming from Node.js rather than the
    JavaScript engine.)
    
    This also makes this test usable without modification in the ChakraCore
    fork of Node.js.
    
    PR-URL: https://github.com/nodejs/node/pull/21132
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- test/parallel/test-url-parse-invalid-input.js
+++ test/parallel/test-url-parse-invalid-input.js
@@ -26,4 +26,12 @@ const url = require('url');
 });
 
 assert.throws(() => { url.parse('http://%E0%A4%A@fail'); },
-              /^URIError: URI malformed$/);
+              (e) => {
+                // The error should be a URIError.
+                if (!(e instanceof URIError))
+                  return false;
+
+                // The error should be from the JS engine and not from Node.js.
+                // JS engine errors do not have the `code` property.
+                return e.code === undefined;
+              });
