d0943bcb693dc8a0cf0b903c04918c4ac6e694ea
Anatoli Papirovski <apapirovski@mac.com>
2018-06-04 15:08:16 +0200

    test: move benchmark-dgram to sequential
    This test uses hardcoded ports, it should not be located in parallel.
    
    PR-URL: https://github.com/nodejs/node/pull/21144
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/sequential/test-benchmark-dgram.js
+++ test/sequential/test-benchmark-dgram.js
@@ -4,6 +4,10 @@ require('../common');
 
 const runBenchmark = require('../common/benchmark');
 
+// Because the dgram benchmarks use hardcoded ports, this should be in
+// sequential rather than parallel to make sure it does not conflict with
+// tests that choose random available ports.
+
 runBenchmark('dgram', ['address=true',
                        'chunks=2',
                        'dur=0.1',
