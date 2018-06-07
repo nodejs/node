84414b2192154d012de784e739423006beadd983
Ujjwal Sharma <usharma1998@gmail.com>
2018-06-07 23:33:47 +0530

    fixup! v8: replace Buffer with FastBuffer in deserialize

---
+++ test/parallel/test-v8-deserialize-buffer.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const v8 = require('v8');
+
+process.on('warning', common.mustNotCall());
+v8.deserialize(v8.serialize(Buffer.alloc(0)));
