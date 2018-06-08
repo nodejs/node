165d8afa97618c1bbe6d1adced099bd09eb67c9a
Viktor <viktor.konsta@gmail.com>
2018-06-08 23:17:26 +0300

    test,module: Add Todo to http esm test

--- test/es-module/test-esm-http-lookup.mjs
+++ test/es-module/test-esm-http-lookup.mjs
@@ -1,6 +1,7 @@
 import '../common';
 import assert from 'assert';
 
+// TODO: Need to find another source to test remote modules
 import { a } from 'https://raw.githubusercontent.com/kuroljov/esm-test/master/a.js';
 import { b } from 'https://raw.githubusercontent.com/kuroljov/esm-test/master/b.js';
 
