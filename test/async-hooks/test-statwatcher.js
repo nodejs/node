adfb579ec3d8cb480fd6f0d3a275f474bed13d27
Anna Henningsen <anna@addaleax.net>
2018-06-10 16:46:56 +0200

    fixup! test: improve statwatcher async_hooks test

--- test/async-hooks/test-statwatcher.js
+++ test/async-hooks/test-statwatcher.js
@@ -1,21 +1,29 @@
 'use strict';
 
 const common = require('../common');
-const commonPath = require.resolve('../common');
+const tmpdir = require('../common/tmpdir');
 const assert = require('assert');
 const initHooks = require('./init-hooks');
 const { checkInvocations } = require('./hook-checks');
 const fs = require('fs');
+const path = require('path');
 
 if (!common.isMainThread)
   common.skip('Worker bootstrapping works differently -> different async IDs');
+
+tmpdir.refresh();
+
+const file1 = path.join(tmpdir.path, 'file1');
+const file2 = path.join(tmpdir.path, 'file2');
+fs.writeFileSync(file1, 'foo');
+fs.writeFileSync(file2, 'bar');
 
 const hooks = initHooks();
 hooks.enable();
 
 function onchange() {}
 // install first file watcher
-const w1 = fs.watchFile(__filename, { interval: 10 }, onchange);
+const w1 = fs.watchFile(file1, { interval: 10 }, onchange);
 
 let as = hooks.activitiesOfTypes('STATWATCHER');
 assert.strictEqual(as.length, 1);
@@ -28,7 +36,7 @@ checkInvocations(statwatcher1, { init: 1 },
                  'watcher1: when started to watch file');
 
 // install second file watcher
-const w2 = fs.watchFile(commonPath, { interval: 10 }, onchange);
+const w2 = fs.watchFile(file2, { interval: 10 }, onchange);
 as = hooks.activitiesOfTypes('STATWATCHER');
 assert.strictEqual(as.length, 2);
 
@@ -41,9 +49,7 @@ checkInvocations(statwatcher1, { init: 1 },
 checkInvocations(statwatcher2, { init: 1 },
                  'watcher2: when started to watch second file');
 
-// Touch the first file by modifying its access time.
-const origStat = fs.statSync(__filename);
-fs.utimesSync(__filename, Date.now() + 10, origStat.mtime);
+fs.writeFileSync(file1, 'foo++');
 w1.once('change', common.mustCall(() => {
   setImmediate(() => {
     checkInvocations(statwatcher1, { init: 1, before: 1, after: 1 },
@@ -52,16 +58,15 @@ w1.once('change', common.mustCall(() => {
                      'watcher2: when unwatched first file');
 
     // Touch the second file by modifying its access time.
-    const origStat = fs.statSync(commonPath);
-    fs.utimesSync(commonPath, Date.now() + 10, origStat.mtime);
+    fs.writeFileSync(file2, 'bar++');
     w2.once('change', common.mustCall(() => {
       setImmediate(() => {
         checkInvocations(statwatcher1, { init: 1, before: 1, after: 1 },
                          'watcher1: when unwatched second file');
         checkInvocations(statwatcher2, { init: 1, before: 1, after: 1 },
                          'watcher2: when unwatched second file');
-        w1.stop();
-        w2.stop();
+        fs.unwatchFile(file1);
+        fs.unwatchFile(file2);
       });
     }));
   });
