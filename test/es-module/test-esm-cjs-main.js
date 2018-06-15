f9d777f756ff1508aa6d7f12fb2176b102a05616
guybedford <guybedford@gmail.com>
2018-06-15 16:45:50 +0200

    add test for cjs execution stalling

--- test/es-module/test-esm-cjs-main.js
+++ test/es-module/test-esm-cjs-main.js
@@ -1,6 +1,27 @@
-// Flags: --experimental-modules
 'use strict';
+
 require('../common');
+const { spawn } = require('child_process');
 const assert = require('assert');
-exports.asdf = 'asdf';
-assert.strictEqual(require.main.exports.asdf, 'asdf');
+const path = require('path');
+
+const entry = path.resolve(__dirname, '../fixtures/es-modules/cjs.js');
+
+const child = spawn(process.execPath, ['--experimental-modules', entry]);
+let experimentalWarning = false;
+let validatedExecution = false;
+child.stderr.on('data', (data) => {
+  if (!experimentalWarning) {
+    experimentalWarning = true;
+    return;
+  }
+  throw new Error(data.toString());
+});
+child.stdout.on('data', (data) => {
+  assert.strictEqual(data.toString(), 'executed\n');
+  validatedExecution = true;
+});
+child.on('close', (code, stdout) => {
+  assert.strictEqual(validatedExecution, true);
+  assert.strictEqual(code, 0);
+});
