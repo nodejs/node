233cf2af93b14dfd442a4d336381420e7c766982
guybedford <guybedford@gmail.com>
2018-06-18 13:58:11 +0200

    error handling feedback

--- test/es-module/test-esm-loader-invalid-url.mjs
+++ test/es-module/test-esm-loader-invalid-url.mjs
@@ -1,12 +1,11 @@
 // Flags: --experimental-modules --loader ./test/fixtures/es-module-loaders/loader-invalid-url.mjs
-/* eslint-disable node-core/required-modules */
+import { expectsError, mustCall } from '../common';
 import assert from 'assert';
-import common from '../common';
 
 import('../fixtures/es-modules/test-esm-ok.mjs')
-.then(() => {
-  assert.fail();
-}, (err) => {
-  assert.strictEqual(err.code, 'ERR_INVALID_RETURN_PROPERTY_STRING');
-})
-.then(common.mustCall());
+.then(assert.fail, expectsError({
+  code: 'ERR_INVALID_RETURN_PROPERTY_STRING',
+  message: 'Expected a valid url to be returned for the url from the "loader ' +
+           'resolve" function but got ../fixtures/es-modules/test-esm-ok.mjs.'
+}))
+.then(mustCall());
--- test/fixtures/es-module-loaders/loader-invalid-url.mjs
+++ test/fixtures/es-module-loaders/loader-invalid-url.mjs
@@ -1,5 +1,5 @@
 export async function resolve(specifier, parentModuleURL, defaultResolve) {
-  if (parentModuleURL && specifier !== 'assert') {
+  if (parentModuleURL && specifier === '../fixtures/es-modules/test-esm-ok.mjs') {
     return {
       url: specifier,
       format: 'esm'
