a8c61fd8d89fa70f95d05b61e69c19d6e89cd49e
AdityaSrivast <yashsri421@gmail.com>
2018-06-20 02:59:19 +0530

    lib: add empty buffer case to read and readSync function in fs
    While using read() or readSync() function, it should be verified that
    the arguments of the function are in proper range and hold legitimate
    values, for a successful read process.
    For this validateOffsetLengthRead() function is called, which gives an
    error message for offset and length passed by the user, if not in
    order. But there should also be an error when an empty buffer is passed
    as argument, when it cannot be written over.
    The empty buffer case should be checked before calling
    validateOffsetLengthRead() and ERR_INVALID_ARG_VALUE should be thrown
    corresponding to the empty buffer argument in read and readSync
    function.
    
    Fixes: https://github.com/nodejs/node/issues/21193

--- test/parallel/test-fs-read-empty-buffer.js
+++ test/parallel/test-fs-read-empty-buffer.js
@@ -3,7 +3,7 @@ require('../common');
 const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const fs = require('fs');
-const filepath = fixtures.path('testcodes.txt');
+const filepath = fixtures.path('x.txt');
 const fd = fs.openSync(filepath, 'r');
 
 const buffer = new Uint8Array();
@@ -12,5 +12,7 @@ assert.throws(
   () => fs.readSync(fd, buffer, 0, 10, 0),
   {
     code: 'ERR_INVALID_ARG_VALUE',
+    message: 'The argument \'buffer\' is empty and cannot be written. ' +
+    'Received Uint8Array [  ]'
   }
 );
