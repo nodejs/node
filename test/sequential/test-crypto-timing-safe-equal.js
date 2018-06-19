90ff08f621a8c88d727a9ef4ad10d5e7cb5d91b3
ZaneHannanAU <ZaneHannanAU@users.noreply.github.com>
2018-06-19 21:17:45 +1000

    Update test-crypto-timing-safe-equal.js
    Add various modern ArrayBuffer based timingSafeEqual variants.

--- test/sequential/test-crypto-timing-safe-equal.js
+++ test/sequential/test-crypto-timing-safe-equal.js
@@ -13,6 +13,36 @@ assert.strictEqual(
 );
 
 assert.strictEqual(
+  crypto.timingSafeEqual(Uint8Array.of(1, 0), Uint16Array.of(1)),
+  true,
+  'should consider equal binary integers to be equal'
+);
+
+assert.strictEqual(
+  crypto.timingSafeEqual(Uint8Array.of(0, 1), Uint16Array.of(1)),
+  false,
+  'should consider unequal binary integers to be unequal'
+);
+
+assert.strictEqual(
+  crypto.timingSafeEqual(Uint8Array.of(1, 0, 0, 0, 0, 0, 0), BigUint64Array.of(1n)),
+  true,
+  'should consider equal binary integers to be equal'
+);
+
+assert.strictEqual(
+  crypto.timingSafeEqual(Buffer.allocUnsafe(8).fill(255), BigInt64Array.of(-1n)),
+  true,
+  'should consider equal binary integers to be equal'
+);
+
+assert.strictEqual(
+  crypto.timingSafeEqual(new DataView(new ArrayBuffer(8)), BigInt64Array.of(0n)),
+  true,
+  'should consider equal views to be equal'
+);
+
+assert.strictEqual(
   crypto.timingSafeEqual(Buffer.from('foo'), Buffer.from('bar')),
   false,
   'should consider unequal strings to be unequal'
@@ -20,6 +50,15 @@ assert.strictEqual(
 
 common.expectsError(
   () => crypto.timingSafeEqual(Buffer.from([1, 2, 3]), Buffer.from([1, 2])),
+  {
+    code: 'ERR_CRYPTO_TIMING_SAFE_EQUAL_LENGTH',
+    type: RangeError,
+    message: 'Input buffers must have the same length'
+  }
+);
+
+common.expectsError(
+  () => crypto.timingSafeEqual(Uint8Array.of(1, 2, 3), Uint16Array.of(1, 2, 3)),
   {
     code: 'ERR_CRYPTO_TIMING_SAFE_EQUAL_LENGTH',
     type: RangeError,
