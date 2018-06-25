5d92b633b361b718aa91ceacf595cc9bb581fb5a
itaysabato <itaysabato@gmail.com>
2018-06-25 10:13:01 +0300

    test: add a request-response test for MessageChannel
    This commit adds a request-response test for MessageChannel.
    
    PR-URL: https://github.com/nodejs/node/pull/21514
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- test/parallel/test-message-port.js
+++ test/parallel/test-message-port.js
@@ -21,6 +21,21 @@ const { MessageChannel, MessagePort } = require('worker_threads');
 {
   const { port1, port2 } = new MessageChannel();
 
+  port1.onmessage = common.mustCall((message) => {
+    assert.strictEqual(message, 4);
+    port2.close(common.mustCall());
+  });
+
+  port1.postMessage(2);
+
+  port2.onmessage = common.mustCall((message) => {
+    port2.postMessage(message * 2);
+  });
+}
+
+{
+  const { port1, port2 } = new MessageChannel();
+
   const input = { a: 1 };
   port1.postMessage(input);
   // Check that the message still gets delivered if `port2` has its
