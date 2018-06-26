e3942017503496f90c1700ab451b56a8381b6c62
Javier Gonzalez <xaviergonz@gmail.com>
2018-06-26 20:12:25 +0200

    fix: reinterpret cast and linting

--- src/env.cc
+++ src/env.cc
@@ -33,9 +33,10 @@ using worker::Worker;
 
 const int kNodeContextTag = 0x6e6f64;
 // We don't store a pointer to kNodeContextTag itself since there might be
-// the case where a promise might reside on a different process, yet is a node context
+// the case where a promise might reside on a different process, yet is a
+// node context
 void* kNodeContextTagPtr = const_cast<void*>(
-    static_cast<const void*>(kNodeContextTag));
+    reinterpret_cast<const void*>(kNodeContextTag));
 
 IsolateData::IsolateData(Isolate* isolate,
                          uv_loop_t* event_loop,
