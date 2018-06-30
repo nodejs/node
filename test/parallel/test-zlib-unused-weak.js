6e16ad7a63ba843aafa1284e51ce220a775e11d8
Anna Henningsen <anna@addaleax.net>
2018-06-30 14:58:19 +0200

    zlib: fix memory leak for unused zlib instances
    An oversight in an earlier commit led to a memory leak
    in the untypical situation that zlib instances are created
    but never used, because zlib handles no longer started
    out their life as weak handles.
    
    The bug was introduced in bd201102862a194f3f5ce669e0a3c8143eafc900.
    
    Refs: https://github.com/nodejs/node/pull/20455
    
    PR-URL: https://github.com/nodejs/node/pull/21607
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

---
+++ test/parallel/test-zlib-unused-weak.js
@@ -0,0 +1,18 @@
+'use strict';
+// Flags: --expose-gc
+require('../common');
+const assert = require('assert');
+const zlib = require('zlib');
+
+// Tests that native zlib handles start out their life as weak handles.
+
+const before = process.memoryUsage().external;
+for (let i = 0; i < 100; ++i)
+  zlib.createGzip();
+const afterCreation = process.memoryUsage().external;
+global.gc();
+const afterGC = process.memoryUsage().external;
+
+assert((afterGC - before) / (afterCreation - before) <= 0.05,
+       `Expected after-GC delta ${afterGC - before} to be less than 5 %` +
+       ` of before-GC delta ${afterCreation - before}`);
