a9ec8884ff45ca760c926da7fc5e709c1cd0c3f3
Gus Caplan <me@gus.host>
2018-07-03 17:42:16 -0500

    deps: cherry-pick 477df06 from upstream v8
    Original commit message:
    
        [API] Expand BigInt API
    
        Provide a more complete BigInt API.
    
        Bug: v8:7712
        Cq-Include-Trybots: luci.chromium.try:linux_chromium_rel_ng
        Change-Id: Ic8562d616f3125deabdf8b52c7019b191bef0e07
        Reviewed-on: chromium-review.googlesource.com/1101198
        Commit-Queue: Yang Guo <yangguo@chromium.org>
        Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
        Reviewed-by: Yang Guo <yangguo@chromium.org>
        Cr-Commit-Position: refs/heads/master@{#54122}
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/21668
    PR-URL: https://github.com/nodejs/node/pull/21644
    Reviewed-By: Tiancheng "Timothy" Gu <timothygu99@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- deps/v8/src/api.h
+++ deps/v8/src/api.h
@@ -127,6 +127,7 @@ class RegisteredExtension {
   V(Promise, JSPromise)                        \
   V(Primitive, Object)                         \
   V(PrimitiveArray, FixedArray)                \
+  V(BigInt, BigInt)                            \
   V(ScriptOrModule, Script)
 
 class Utils {
