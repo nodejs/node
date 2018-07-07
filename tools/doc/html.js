7252864b31c83dadf1cde598e4e90e178ef60051
Rich Trott <rtrott@gmail.com>
2018-07-07 15:33:06 -0700

    doc: add "Edit on GitHub" link
    Proof of concept for an "Edit on GitHub" link, inspired by the
    Serverless docs.
    
    One issue is that the link is to the version of the docs on the master
    branch even if the person was reading a different version of the doc. I
    don't consider that a big problem, although we can always remove the
    link if it turns out to be a big problem. I don't think there is a good
    solution. PRs need to be opened against the master branch generally.
    Having a bunch of PRs against staging branches is probably not what we
    want. If there's an update to one version of the doc, there will usually
    be an update to other versions.

--- tools/doc/html.js
+++ tools/doc/html.js
@@ -96,6 +96,8 @@ function toHTML({ input, filename, nodeVersion, analytics }, cb) {
     HTML = HTML.replace('__ALTDOCS__', '');
   }
 
+  HTML = HTML.replace('__EDIT_ON_GITHUB__', editOnGitHub(filename));
+
   // Content insertion has to be the last thing we do with the lexed tokens,
   // because it's destructive.
   HTML = HTML.replace('__CONTENT__', marked.parser(lexed));
@@ -376,4 +378,9 @@ function altDocs(filename, docCreated) {
       <ol class="version-picker">${list}</ol>
     </li>
   ` : '';
+}
+
+function editOnGitHub(filename) {
+  const svg = '<svg style="height: 17px; vertical-align: middle; margin: -2px 5px 0 0;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.083-.73.083-.73 1.205.085 1.84 1.238 1.84 1.238 1.07 1.834 2.806 1.304 3.49.997.108-.776.42-1.306.763-1.605-2.665-.305-5.467-1.334-5.467-5.93 0-1.312.47-2.382 1.236-3.222-.125-.303-.536-1.524.116-3.176 0 0 1.008-.322 3.3 1.23A11.51 11.51 0 0 1 12 5.803c1.02.005 2.047.138 3.006.404 2.29-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.236 1.91 1.236 3.22 0 4.61-2.807 5.625-5.48 5.922.43.372.824 1.102.824 2.222v3.293c0 .32.192.694.8.576C20.567 21.796 24 17.3 24 12c0-6.627-5.373-12-12-12z"></path></svg>';
+  return `<li><a href="https://github.com/nodejs/node/edit/master/doc/api/${filename}.md">${svg}Edit on GitHub</a></li>`;
 }
