ededb4b510d37fa73582f71d7bd6d3a5bb046be2
James M Snell <jasnell@gmail.com>
2018-07-12 12:22:54 -0700

    trace_events: add more process metadata
    Now that TracedValue has landed, add more detailed
    process `__metadata` including versions, arch, platform,
    release detail, and argv/execArgv to the trace event
    log.
    
    PR-URL: https://github.com/nodejs/node/pull/21785
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-trace-events-metadata.js
+++ test/parallel/test-trace-events-metadata.js
@@ -23,25 +23,53 @@ const proc = cp.spawn(process.execPath,
 proc.once('exit', common.mustCall(() => {
   assert(common.fileExists(FILE_NAME));
   fs.readFile(FILE_NAME, common.mustCall((err, data) => {
-    const traces = JSON.parse(data.toString()).traceEvents;
+    const traces = JSON.parse(data.toString()).traceEvents
+      .filter((trace) => trace.cat === '__metadata');
     assert(traces.length > 0);
     assert(traces.some((trace) =>
-      trace.cat === '__metadata' && trace.name === 'thread_name' &&
+      trace.name === 'thread_name' &&
         trace.args.name === 'JavaScriptMainThread'));
     assert(traces.some((trace) =>
-      trace.cat === '__metadata' && trace.name === 'thread_name' &&
+      trace.name === 'thread_name' &&
         trace.args.name === 'BackgroundTaskRunner'));
     assert(traces.some((trace) =>
-      trace.cat === '__metadata' && trace.name === 'version' &&
+      trace.name === 'version' &&
         trace.args.node === process.versions.node));
+
+    assert(traces.some((trace) =>
+      trace.name === 'node' &&
+        trace.args.process.versions.http_parser ===
+          process.versions.http_parser &&
+        trace.args.process.versions.node ===
+          process.versions.node &&
+        trace.args.process.versions.v8 ===
+          process.versions.v8 &&
+        trace.args.process.versions.uv ===
+          process.versions.uv &&
+        trace.args.process.versions.zlib ===
+          process.versions.zlib &&
+        trace.args.process.versions.ares ===
+          process.versions.ares &&
+        trace.args.process.versions.modules ===
+          process.versions.modules &&
+        trace.args.process.versions.nghttp2 ===
+          process.versions.nghttp2 &&
+        trace.args.process.versions.napi ===
+          process.versions.napi &&
+        trace.args.process.versions.openssl ===
+          process.versions.openssl &&
+        trace.args.process.arch === process.arch &&
+        trace.args.process.platform === process.platform &&
+        trace.args.process.release.name === process.release.name &&
+        (!process.release.lts ||
+          trace.args.process.release.lts === process.release.lts)));
+
     if (!common.isSunOS) {
       // Changing process.title is currently unsupported on SunOS/SmartOS
       assert(traces.some((trace) =>
-        trace.cat === '__metadata' && trace.name === 'process_name' &&
-          trace.args.name === 'foo'));
+        trace.name === 'process_name' && trace.args.name === 'foo'));
       assert(traces.some((trace) =>
-        trace.cat === '__metadata' && trace.name === 'process_name' &&
-          trace.args.name === 'bar'));
+        trace.name === 'process_name' && trace.args.name === 'bar'));
     }
   }));
 }));
