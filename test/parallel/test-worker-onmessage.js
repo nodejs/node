0a8a6d2e61ddbea7aaf4082ae7d997d8b5c2456e
Anna Henningsen <anna@addaleax.net>
2018-07-13 15:07:23 +0200

    test: fix timeouts when running worker tests with `--worker`
    These tests have been added after the original workers PR and
    time out when run inside a worker by themselves.
    This is needed for enabling `--worker` tests in our regular CI.
    
    Refs: https://github.com/nodejs/build/issues/1318

--- test/parallel/test-worker-onmessage.js
+++ test/parallel/test-worker-onmessage.js
@@ -2,9 +2,11 @@
 'use strict';
 const common = require('../common');
 const assert = require('assert');
-const { Worker, isMainThread, parentPort } = require('worker_threads');
+const { Worker, parentPort } = require('worker_threads');
 
-if (isMainThread) {
+// Do not use isMainThread so that this test itself can be run inside a Worker.
+if (!process.env.HAS_STARTED_WORKER) {
+  process.env.HAS_STARTED_WORKER = 1;
   const w = new Worker(__filename);
   w.on('message', common.mustCall((message) => {
     assert.strictEqual(message, 4);
