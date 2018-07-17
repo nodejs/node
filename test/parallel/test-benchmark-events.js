a3af552af91a7f0ac0222f783d81dbdf06f5fc5b
Denys Otrishko <shishugi@gmail.com>
2018-07-18 01:42:53 +0300

    fixup! implement proxy for each EE, add benchmarks

--- test/parallel/test-benchmark-events.js
+++ test/parallel/test-benchmark-events.js
@@ -5,5 +5,5 @@ require('../common');
 const runBenchmark = require('../common/benchmark');
 
 runBenchmark('events',
-             ['argc=0', 'listeners=1', 'n=1'],
+             ['argc=0', 'staleEventsCount=0', 'listenerCount=1', 'n=1'],
              { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
