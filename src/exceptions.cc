5b6232e680cc1c23752e6c93f04b3881aba09c86
Ujjwal Sharma <usharma1998@gmail.com>
2018-07-22 01:57:29 +0530

    fixup! src: remove calls to deprecated v8 functions (NewFromUtf8)

--- src/exceptions.cc
+++ src/exceptions.cc
@@ -66,18 +66,20 @@ Local<Value> ErrnoException(Isolate* isolate,
 }
 
 static Local<String> StringFromPath(Isolate* isolate, const char* path) {
-  v8::NewStringType type = v8::NewStringType::kNormal;
 #ifdef _WIN32
   if (strncmp(path, "\\\\?\\UNC\\", 8) == 0) {
     return String::Concat(FIXED_ONE_BYTE_STRING(isolate, "\\\\"),
-                          String::NewFromUtf8(isolate, path + 8, type)
+                          String::NewFromUtf8(isolate, path + 8,
+                                              v8::NewStringType::kNormal)
                               .ToLocalChecked());
   } else if (strncmp(path, "\\\\?\\", 4) == 0) {
-    return String::NewFromUtf8(isolate, path + 4, type).ToLocalChecked();
+    return String::NewFromUtf8(isolate, path + 4, v8::NewStringType::kNormal)
+        .ToLocalChecked();
   }
 #endif
 
-  return String::NewFromUtf8(isolate, path, type).ToLocalChecked();
+  return String::NewFromUtf8(isolate, path, v8::NewStringType::kNormal)
+      .ToLocalChecked();
 }
 
 
@@ -182,13 +184,13 @@ Local<Value> WinapiErrnoException(Isolate* isolate,
   }
   Local<String> message = OneByteString(env->isolate(), msg);
 
-  v8::NewStringType type = v8::NewStringType::kNormal;
   if (path) {
     Local<String> cons1 =
         String::Concat(message, FIXED_ONE_BYTE_STRING(isolate, " '"));
     Local<String> cons2 =
         String::Concat(cons1,
-            String::NewFromUtf8(isolate, path, type).ToLocalChecked());
+            String::NewFromUtf8(isolate, path, v8::NewStringType::kNormal)
+                .ToLocalChecked());
     Local<String> cons3 =
         String::Concat(cons2, FIXED_ONE_BYTE_STRING(isolate, "'"));
     e = Exception::Error(cons3);
@@ -201,7 +203,8 @@ Local<Value> WinapiErrnoException(Isolate* isolate,
 
   if (path != nullptr) {
     obj->Set(env->path_string(),
-        String::NewFromUtf8(isolate, path, type).ToLocalChecked());
+             String::NewFromUtf8(isolate, path, v8::NewStringType::kNormal)
+                 .ToLocalChecked());
   }
 
   if (syscall != nullptr) {
