4f6d4a0f219a501b1dcf75ce46f490b3315a63cc
Gautam Arora <gautamsarora@gmail.com>
2018-08-02 15:35:44 -0400

    test: error code on win vs posix for rmdir on file

--- test/parallel/test-fs-mkdir-rmdir.js
+++ test/parallel/test-fs-mkdir-rmdir.js
@@ -6,6 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const tmpdir = require('../common/tmpdir');
 const d = path.join(tmpdir.path, 'dir');
+const f = path.join(d, 'test.bin');
 
 tmpdir.refresh();
 
@@ -24,17 +25,44 @@ fs.rmdirSync(d);
 // Make sure the directory does not exist
 assert(!fs.existsSync(d));
 
-// Similarly test the Async version
-fs.mkdir(d, 0o666, common.mustCall(function(err) {
-  assert.ifError(err);
+{
+  tmpdir.refresh();
+  // Create the directory
+  assert(!common.fileExists(d));
+  fs.mkdirSync(d);
+  assert(common.fileExists(d));
+  // Create the file
+  fs.writeFileSync(f, 'foo'.repeat(2 ** 15));
+  // Make sure the file exists
+  assert(common.fileExists(f));
+  // Try removing a file that exists, ...
+  // in POSIX systems: it should fail with ENOTDIR
+  // in Window systems: it should fail with ENOENT
+  if (common.isWindows) {
+    assert.throws(function() {
+      fs.rmdirSync(f);
+    }, /ENOENT: not a directory, rmdir/);
+  } else {
+    assert.throws(function() {
+      fs.rmdirSync(f);
+    }, /ENOTDIR: not a directory, rmdir/);
+  }
+}
 
+{
+  tmpdir.refresh();
+  // Similarly test the Async version
   fs.mkdir(d, 0o666, common.mustCall(function(err) {
-    assert.strictEqual(this, undefined);
-    assert.ok(err, 'got no error');
-    assert.ok(/^EEXIST/.test(err.message), 'got no EEXIST message');
-    assert.strictEqual(err.code, 'EEXIST');
-    assert.strictEqual(err.path, d);
+    assert.ifError(err);
 
-    fs.rmdir(d, assert.ifError);
+    fs.mkdir(d, 0o666, common.mustCall(function(err) {
+      assert.strictEqual(this, undefined);
+      assert.ok(err, 'got no error');
+      assert.ok(/^EEXIST/.test(err.message), 'got no EEXIST message');
+      assert.strictEqual(err.code, 'EEXIST');
+      assert.strictEqual(err.path, d);
+
+      fs.rmdir(d, assert.ifError);
+    }));
   }));
-}));
+}
