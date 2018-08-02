a17f9073a43b6e6e3cae76557368431db6b023e8
Tobias Nießen <tniessen@tnie.de>
2018-08-02 20:21:29 +0200

    fixup! crypto: move createCipher to runtime deprecation

--- test/parallel/test-process-emit-warning-from-native.js
+++ test/parallel/test-process-emit-warning-from-native.js
@@ -11,12 +11,17 @@ const crypto = require('crypto');
 const key = '0123456789';
 
 {
-  common.expectWarning('DeprecationWarning',
-                       'crypto.createCipher is deprecated.',
-                       'DEP0106');
+  common.expectWarning({
+    DeprecationWarning: [
+      ['crypto.createCipher is deprecated.', 'DEP0106']
+    ],
+    Warning: [
+      ['Use Cipheriv for counter mode of aes-256-gcm', common.noWarnCode]
+    ]
+  });
 
   // Emits regular warning expected by expectWarning()
-  crypto.createCipher('aes-256-cbc', key);
+  crypto.createCipher('aes-256-gcm', key);
 }
 
 const realEmitWarning = process.emitWarning;
