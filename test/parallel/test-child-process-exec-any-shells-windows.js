27ba48f1b50dbe3ca9733130c072d72cfde2d582
Tessei Kameyama <kamenoko315@ruri.waseda.jp>
2018-08-02 17:59:01 +0900

    test: add testcases to ensure uppercase shell name and complex commands

--- test/parallel/test-child-process-exec-any-shells-windows.js
+++ test/parallel/test-child-process-exec-any-shells-windows.js
@@ -18,10 +18,19 @@ const test = (shell) => {
 
 test('cmd');
 test('cmd.exe');
+test('CMD');
 test('powershell');
+
 cp.exec('where bash', (error, stdout) => {
   if (error) {
     return;
   }
   test(stdout.trim());
 });
+
+cp.exec(`Get-ChildItem "${__dirname}" | Select-Object -Property Name`,
+        { shell: 'PowerShell' }, (error, stdout, stderror) => {
+          assert.ok(!error && !stderror);
+          assert.ok(stdout.includes(
+            'test-child-process-exec-any-shells-windows.js'));
+        });
