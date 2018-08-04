54fc80f90b67ceefc1a8044f0f6b2056436f7e3d
Yaniv Friedensohn <yaniv.friedensohn@gmail.com>
2018-08-04 18:01:34 +0300

    net: use ERR_INVALID_OPT_VALUE instead of ERR_PROPERTY_NOT_IN_OBJECT

--- test/parallel/test-net-server-listen-options.js
+++ test/parallel/test-net-server-listen-options.js
@@ -58,21 +58,12 @@ const listenOnPort = [
       net.createServer().listen(options, common.mustNotCall());
     };
 
-    if (typeof options === 'object' && !('port' in options)) {
-      common.expectsError(block,
-                          {
-                            code: 'ERR_PROPERTY_NOT_IN_OBJECT',
-                            type: TypeError,
-                            message: 'port does not exist in options',
-                          });
-    } else {
-      common.expectsError(block,
-                          {
-                            code: 'ERR_INVALID_OPT_VALUE',
-                            type: TypeError,
-                            message: /^The value "{.*}" is invalid for option "options"$/
-                          });
-    }
+    common.expectsError(block,
+                        {
+                          code: 'ERR_INVALID_OPT_VALUE',
+                          type: TypeError,
+                          message: /^The value "{.*}" is invalid for option "options"(?:\. .+)?$/,
+                        });
   }
 
   shouldFailToListen(false, { port: false });
