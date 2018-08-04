a28a51d40b8cf3f1feeb5a045842ba81181e1502
Ben Newman <ben@meteor.com>
2018-08-04 11:28:29 -0400

    deps: backport a8f6869 from upstream V8
    Original commit message:
    
      [debug] Fully implement Debug::ArchiveDebug and Debug::RestoreDebug.
    
      I have a project that embeds V8 and uses a single `Isolate` from multiple
      threads. The program runs just fine, but sometimes the inspector doesn't
      stop on the correct line after stepping over a statement that switches
      threads behind the scenes, even though the original thread is restored by
      the time the next statement is executed.
    
      After some digging, I discovered that the `Debug::ArchiveDebug` and
      `Debug::RestoreDebug` methods, which should be responsible for
      saving/restoring this `ThreadLocal` information when switching threads,
      currently don't do anything.
    
      This commit implements those methods using MemCopy, in the style of other
      Archive/Restore methods in the V8 codebase.
    
      Related: https://groups.google.com/forum/#!topic/v8-users/_Qf2rwljRk8
    
      R=yangguo@chromium.org,jgruber@chromium.org
      CC=info@bnoordhuis.nl
    
      Bug: v8:7230
      Change-Id: Id517c873eb81cd53f7216c7efd441b956cf7f943
      Reviewed-on: https://chromium-review.googlesource.com/833260
      Commit-Queue: Yang Guo <yangguo@chromium.org>
      Reviewed-by: Yang Guo <yangguo@chromium.org>
      Cr-Commit-Position: refs/heads/master@{#54902}

--- deps/v8/src/debug/debug.h
+++ deps/v8/src/debug/debug.h
@@ -327,6 +327,7 @@ class Debug {
   static int ArchiveSpacePerThread();
   void FreeThreadResources() { }
   void Iterate(RootVisitor* v);
+  void InitThread(const ExecutionAccess& lock) { ThreadInit(); }
 
   bool CheckExecutionState(int id) {
     return CheckExecutionState() && break_id() == id;
