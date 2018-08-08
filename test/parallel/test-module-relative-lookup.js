cf0e7c2316a229dbe30254d1c3bf3a23bda69492
João Reis <reis@janeasystems.com>
2018-08-08 03:58:06 +0100

    module: accept Windows relative path
    Before this change, require('.\\test.js') failed while
    require('./test.js') succeeded. This changes _resolveLookupPaths so
    that both ways are accepted on Windows.
    
    Fixes: https://github.com/nodejs/node/issues/21918

--- test/parallel/test-module-relative-lookup.js
+++ test/parallel/test-module-relative-lookup.js
@@ -1,15 +1,23 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const _module = require('module'); // avoid collision with global.module
-const lookupResults = _module._resolveLookupPaths('./lodash');
-let paths = lookupResults[1];
 
-// Current directory gets highest priority for local modules
-assert.strictEqual(paths[0], '.');
+function runTest(moduleName) {
+  const lookupResults = _module._resolveLookupPaths(moduleName);
+  let paths = lookupResults[1];
 
-paths = _module._resolveLookupPaths('./lodash', null, true);
+  // Current directory gets highest priority for local modules
+  assert.strictEqual(paths[0], '.');
 
-// Current directory gets highest priority for local modules
-assert.strictEqual(paths && paths[0], '.');
+  paths = _module._resolveLookupPaths(moduleName, null, true);
+
+  // Current directory gets highest priority for local modules
+  assert.strictEqual(paths && paths[0], '.');
+}
+
+runTest('./lodash');
+if (common.isWindows) {
+  runTest('.\\lodash');
+}
