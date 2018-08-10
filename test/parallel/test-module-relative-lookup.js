1154edc0201163e7be79bea5be704ba47a391a88
João Reis <reis@janeasystems.com>
2018-08-10 18:49:36 +0100

    fixup: test windows path everywhere

--- test/parallel/test-module-relative-lookup.js
+++ test/parallel/test-module-relative-lookup.js
@@ -4,20 +4,22 @@ const common = require('../common');
 const assert = require('assert');
 const _module = require('module'); // avoid collision with global.module
 
-function runTest(moduleName) {
-  const lookupResults = _module._resolveLookupPaths(moduleName);
-  let paths = lookupResults[1];
+// Current directory gets highest priority for local modules
+function testFirstInPath(moduleName, isLocalModule) {
+  const assertFunction = isLocalModule ?
+    assert.strictEqual :
+    assert.notStrictEqual;
 
-  // Current directory gets highest priority for local modules
-  assert.strictEqual(paths[0], '.');
+  const lookupResults = _module._resolveLookupPaths(moduleName);
+
+  let paths = lookupResults[1];
+  assertFunction(paths[0], '.');
 
   paths = _module._resolveLookupPaths(moduleName, null, true);
-
-  // Current directory gets highest priority for local modules
-  assert.strictEqual(paths && paths[0], '.');
+  assertFunction(paths && paths[0], '.');
 }
 
-runTest('./lodash');
-if (common.isWindows) {
-  runTest('.\\lodash');
-}
+testFirstInPath('./lodash', true);
+
+// Relative path on Windows, but a regular file name elsewhere
+testFirstInPath('.\\lodash', common.isWindows);
