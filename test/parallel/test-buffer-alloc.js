40a7beeddac9b9ec9ef5b49157daaf8470648b08
cjihrig <cjihrig@gmail.com>
2018-08-09 21:45:40 -0400

    buffer: stop alloc() uninitialized memory return
    CVE-2018-7166
    Discovered by ChALkeR - Сковорода Никита Андреевич
    
    Prevent Buffer.alloc(size, fill, number) from returning uninitialized memory.
    
    Fixes: https://github.com/nodejs-private/security/issues/202
    PR-URL: https://github.com/nodejs-private/node-private/pull/137
    Reviewed-By: Rod Vagg <rod@vagg.org>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Evan Lucas <evanlucas@me.com>
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/parallel/test-buffer-alloc.js
+++ test/parallel/test-buffer-alloc.js
@@ -1039,3 +1039,10 @@ common.expectsError(() => {
   code: 'ERR_INVALID_ARG_VALUE',
   type: TypeError
 });
+
+common.expectsError(() => {
+  Buffer.alloc(40, 'x', 20);
+}, {
+  code: 'ERR_INVALID_ARG_TYPE',
+  type: TypeError
+});
