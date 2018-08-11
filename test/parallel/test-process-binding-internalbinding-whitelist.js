0257fd7ce91660b1f85b1a90a7d7acf0b90384ce
James M Snell <jasnell@gmail.com>
2018-08-11 09:54:41 -0700

    process: wrap process.binding for selective fallthrough
    Selectively fallthrough `process.binding()` to `internalBinding()`
    
    Refs: https://github.com/nodejs/node/pull/22163
    
    PR-URL: https://github.com/nodejs/node/pull/22269
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jon Moss <me@jonathanmoss.me>
    Reviewed-By: John-David Dalton <john.david.dalton@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

---
+++ test/parallel/test-process-binding-internalbinding-whitelist.js
@@ -0,0 +1,9 @@
+// Flags: --no-warnings
+'use strict';
+
+require('../common');
+const assert = require('assert');
+
+// Assert that whitelisted internalBinding modules are accessible via
+// process.binding().
+assert(process.binding('uv'));
