de33a5aa6ea357fb7c6fde7b75a024e16be88a47
Ruben Bridgewater <ruben@bridgewater.de>
2018-08-13 17:51:58 +0200

    benchmark: refactor util benchmarks
    This significantly reduces the benchmark runtime. It removes to many
    variations that do not provide any benefit and reduces the iterations.
    
    PR-URL: https://github.com/nodejs/node/pull/22503
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: John-David Dalton <john.david.dalton@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- benchmark/util/inspect.js
+++ benchmark/util/inspect.js
@@ -9,7 +9,7 @@ const opts = {
   none: undefined
 };
 const bench = common.createBenchmark(main, {
-  n: [2e6],
+  n: [2e4],
   method: [
     'Object',
     'Object_empty',
@@ -81,7 +81,7 @@ function main({ method, n, option }) {
       benchmark(n, new Error('error'), options);
       break;
     case 'Array':
-      benchmark(n, Array(20).fill().map((_, i) => i), options);
+      benchmark(n, Array(50).fill().map((_, i) => i), options);
       break;
     case 'TypedArray':
       obj = new Uint8Array(Array(50).fill().map((_, i) => i));
