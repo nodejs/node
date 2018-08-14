88105c998ef9d3f54aa8f22b82ec8cc31cbfac95
Rod Vagg <rod@vagg.org>
2018-08-14 20:18:06 +1000

    buffer: avoid overrun on UCS-2 string write
    CVE-2018-12115
    Discovered by ChALkeR - Сковорода Никита Андреевич
    Fix by Anna Henningsen
    
    Writing to the second-to-last byte with UCS-2 encoding will cause a -1
    length to be send to String::Write(), writing all of the provided Buffer
    from that point and beyond.
    
    Fixes: https://github.com/nodejs-private/security/issues/203
    PR-URL: https://github.com/nodejs-private/node-private/pull/138

--- src/string_bytes.cc
+++ src/string_bytes.cc
@@ -265,7 +265,11 @@ size_t StringBytes::WriteUCS2(char* buf,
                               size_t* chars_written) {
   uint16_t* const dst = reinterpret_cast<uint16_t*>(buf);
 
-  size_t max_chars = (buflen / sizeof(*dst));
+  size_t max_chars = buflen / sizeof(*dst);
+  if (max_chars == 0) {
+    return 0;
+  }
+
   size_t nchars;
   size_t alignment = reinterpret_cast<uintptr_t>(dst) % sizeof(*dst);
   if (alignment == 0) {
