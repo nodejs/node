15a6f6278f942a1e69f439dd102d70fce70a91e6
Denys Otrishko <shishugi@gmail.com>
2018-08-16 20:19:06 +0300

    module: fix inconsistency between load and _findPath
    Files with multiple extensions are not handled by require-module system
    therefore if you have file 'file.foo.bar' and require('./file') it won't
    be found even while using require.extensions['foo.bar'] but before this
    commit if you have require.extensions['foo.bar'] and
    require.extensions['bar'] set then the latter will be called if you do
    require('./file') but if you remove the former the former ('foo.bar')
    property it will fail.
    This commit makes it always fail in such cases.
    
    Fixes: https://github.com/nodejs/node/issues/4778

---
+++ test/parallel/test-module-deleted-extensions.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// Refs: https://github.com/nodejs/node/issues/4778
+
+const common = require('../common');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const tmpdir = require('../common/tmpdir');
+const file = path.join(tmpdir.path, 'test-extensions.foo.bar');
+
+tmpdir.refresh();
+fs.writeFileSync(file, '', 'utf8');
+
+{
+  require.extensions['.bar'] = common.mustNotCall();
+  require.extensions['.foo.bar'] = common.mustNotCall();
+  const modulePath = path.join(tmpdir.path, 'test-extensions');
+  assert.throws(
+    () => require(modulePath),
+    new Error(`Cannot find module '${modulePath}'`)
+  );
+}
+
+{
+  require.extensions['.foo.bar'] = common.mustNotCall();
+  delete require.extensions['.foo.bar'];
+  const modulePath = path.join(tmpdir.path, 'test-extensions');
+  assert.throws(
+    () => require(modulePath),
+    new Error(`Cannot find module '${modulePath}'`)
+  );
+}
+
+{
+  require.extensions['.bar'] = common.mustCall((module, path) => {
+    assert.strictEqual(module.id, file);
+    assert.strictEqual(path, file);
+  });
+
+  const modulePath = path.join(tmpdir.path, 'test-extensions.foo');
+  require(modulePath);
+}
