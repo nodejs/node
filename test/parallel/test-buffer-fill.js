5d95542212d20db007ef518d8e5874985dc687f6
Weijia Wang <381152119@qq.com>
2018-08-17 16:33:45 +0800

    buffer: move process.binding('buffer') to internalBinding
    PR-URL: https://github.com/nodejs/node/pull/22370
    Refs: https://github.com/nodejs/node/issues/22160
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-buffer-fill.js
+++ test/parallel/test-buffer-fill.js
@@ -3,6 +3,7 @@
 const common = require('../common');
 const assert = require('assert');
 const { codes: { ERR_OUT_OF_RANGE } } = require('internal/errors');
+const { internalBinding } = require('internal/test/binding');
 const SIZE = 28;
 
 const buf1 = Buffer.allocUnsafe(SIZE);
@@ -327,7 +328,7 @@ Buffer.alloc(8, '');
 // Testing process.binding. Make sure "start" is properly checked for -1 wrap
 // around.
 assert.strictEqual(
-  process.binding('buffer').fill(Buffer.alloc(1), 1, -1, 0, 1), -2);
+  internalBinding('buffer').fill(Buffer.alloc(1), 1, -1, 0, 1), -2);
 
 // Make sure "end" is properly checked, even if it's magically mangled using
 // Symbol.toPrimitive.
@@ -365,7 +366,7 @@ assert.strictEqual(
 // Testing process.binding. Make sure "end" is properly checked for -1 wrap
 // around.
 assert.strictEqual(
-  process.binding('buffer').fill(Buffer.alloc(1), 1, 1, -2, 1), -2);
+  internalBinding('buffer').fill(Buffer.alloc(1), 1, 1, -2, 1), -2);
 
 // Test that bypassing 'length' won't cause an abort.
 common.expectsError(() => {
