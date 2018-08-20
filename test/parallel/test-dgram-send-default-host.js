be6aee9f539038ff4df8c000b37dd8a6dcd82232
Sarat Addepalli <sarat.addepalli@paytm.com>
2018-08-20 16:21:02 +0530

    dgram: allow typed arrays in .send()
    PR-URL: https://github.com/nodejs/node/pull/22413
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>

--- test/parallel/test-dgram-send-default-host.js
+++ test/parallel/test-dgram-send-default-host.js
@@ -12,6 +12,11 @@ const toSend = [Buffer.alloc(256, 'x'),
                 'hello'];
 
 const received = [];
+let totalBytesSent = 0;
+let totalBytesReceived = 0;
+const arrayBufferViewsCount = common.getArrayBufferViews(
+  Buffer.from('')
+).length;
 
 client.on('listening', common.mustCall(() => {
   const port = client.address().port;
@@ -21,24 +26,47 @@ client.on('listening', common.mustCall(() => {
   client.send([toSend[2]], port);
   client.send(toSend[3], 0, toSend[3].length, port);
 
-  client.send(new Uint8Array(toSend[0]), 0, toSend[0].length, port);
-  client.send(new Uint8Array(toSend[1]), port);
-  client.send([new Uint8Array(toSend[2])], port);
-  client.send(new Uint8Array(Buffer.from(toSend[3])),
-              0, toSend[3].length, port);
+  totalBytesSent += toSend.map((buf) => buf.length)
+                      .reduce((a, b) => a + b, 0);
+
+  for (const msgBuf of common.getArrayBufferViews(toSend[0])) {
+    client.send(msgBuf, 0, msgBuf.byteLength, port);
+    totalBytesSent += msgBuf.byteLength;
+  }
+  for (const msgBuf of common.getArrayBufferViews(toSend[1])) {
+    client.send(msgBuf, port);
+    totalBytesSent += msgBuf.byteLength;
+  }
+  for (const msgBuf of common.getArrayBufferViews(toSend[2])) {
+    client.send([msgBuf], port);
+    totalBytesSent += msgBuf.byteLength;
+  }
 }));
 
 client.on('message', common.mustCall((buf, info) => {
   received.push(buf.toString());
+  totalBytesReceived += info.size;
 
-  if (received.length === toSend.length * 2) {
-    // The replies may arrive out of order -> sort them before checking.
-    received.sort();
-
-    const expected = toSend.concat(toSend).map(String).sort();
-    assert.deepStrictEqual(received, expected);
+  if (totalBytesReceived === totalBytesSent) {
     client.close();
   }
-}, toSend.length * 2));
+  // For every buffer in `toSend`, we send the raw Buffer,
+  // as well as every TypedArray in getArrayBufferViews()
+}, toSend.length + (toSend.length - 1) * arrayBufferViewsCount));
+
+client.on('close', common.mustCall((buf, info) => {
+  // The replies may arrive out of order -> sort them before checking.
+  received.sort();
+
+  const repeated = [...toSend];
+  for (let i = 0; i < arrayBufferViewsCount; i++) {
+    repeated.push(...toSend.slice(0, 3));
+  }
+
+  assert.strictEqual(totalBytesSent, totalBytesReceived);
+
+  const expected = repeated.map(String).sort();
+  assert.deepStrictEqual(received, expected);
+}));
 
 client.bind(0);
