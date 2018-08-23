ee618a7ab239c98d945c723a4e225bc409151736
Matteo Collina <hello@matteocollina.com>
2018-08-23 16:46:07 +0200

    http,https: protect against slow headers attack
    CVE-2018-12122
    
    An attacker can send a char/s within headers and exahust the resources
    (file descriptors) of a system even with a tight max header length
    protection. This PR destroys a socket if it has not received the headers
    in 40s.
    
    PR-URL: https://github.com/nodejs-private/node-private/pull/144
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/async-hooks/test-graph.http.js
+++ test/async-hooks/test-graph.http.js
@@ -45,7 +45,7 @@ process.on('exit', function() {
         triggerAsyncId: 'tcp:2' },
       { type: 'Timeout',
         id: 'timeout:2',
-        triggerAsyncId: 'httpparser:2' },
+        triggerAsyncId: 'tcp:2' },
       { type: 'SHUTDOWNWRAP',
         id: 'shutdown:1',
         triggerAsyncId: 'tcp:2' } ]
