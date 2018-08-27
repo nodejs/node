df079ee649b7fc8021311e1e6e50f1078ca0066e
Sam Ruby <rubys@intertwingly.net>
2018-08-27 14:52:17 -0400

    [squashable] prepare for multiple apilinks tests
    1) Run tests against every file in test/fixtures/apilinks/
    2) Embed expected results in the fixture itself

--- test/doctool/test-apilinks.js
+++ test/doctool/test-apilinks.js
@@ -2,23 +2,36 @@
 
 require('../common');
 const fixtures = require('../common/fixtures');
+const fs = require('fs');
 const assert = require('assert');
 const path = require('path');
 const { execFileSync } = require('child_process');
 
 const script = path.join(__dirname, '..', '..', 'tools', 'doc', 'apilinks.js');
-const fixture = fixtures.path('apilinks', 'known1.js');
-const expected = {
-  'Known.classField': 'known1.js#L7',
-  'known1.createKnown': 'known1.js#L9'
-};
-const ret = execFileSync(
-  process.execPath, [script, fixture], { encoding: 'utf-8' }
-);
-const links = JSON.parse(ret);
-for (const [k, v] of Object.entries(expected)) {
-  assert.ok(k in links);
-  assert.ok(links[k].includes(v));
-  delete links[k];
-}
-assert.strictEqual(Object.keys(links).length, 0);
+
+const apilinks = fixtures.path('apilinks');
+fs.readdirSync(apilinks).forEach((fixture) => {
+  if (!fixture.endsWith('.js')) return;
+  const file = path.join(apilinks, fixture);
+
+  let expected = {};
+  eval(/(expected\s*=.*?);/s.exec(fs.readFileSync(file, 'utf8'))[1]);
+
+  const links = JSON.parse(execFileSync(
+    process.execPath,
+    [script, file],
+    { encoding: 'utf-8' }
+  ));
+
+  for (const [k, v] of Object.entries(expected)) {
+    assert.ok(k in links, `link not found: ${k}`);
+    assert.ok(links[k].endsWith('/' + v),
+              `link ${links[k]} expected to end with ${v}`);
+    delete links[k];
+  }
+
+  assert.strictEqual(
+    Object.keys(links).length, 0,
+    `unexpected links returned ${JSON.stringify(Object.keys(links))}`
+  );
+});
