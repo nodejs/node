262c9c1e020f1de7291e8cddd46298106e66a309
Shobhit Chittora <chittorashobhit@gmail.com>
2018-08-29 12:06:06 +0530

    child_process: refactored test as per the comments + alignment fix

--- test/parallel/test-child-process-fork-options.js
+++ test/parallel/test-child-process-fork-options.js
@@ -1,17 +1,17 @@
-/**
- * fork should parse options correctly if args is undefined or null
- */
-
 'use strict';
 const common = require('../common');
+const fixtures = require('../common/fixtures');
+
+// This test ensures that fork should parse options
+// correctly if args is undefined or null
+
 const assert = require('assert');
 const { fork } = require('child_process');
-const fixtures = require('../common/fixtures');
 
 const expectedEnv = { foo: 'bar' };
 const cp = fork(fixtures.path('child-process-echo-options.js'), undefined,
                 { env: Object.assign({}, expectedEnv) });
 
-cp.on('message', common.mustCall(function({ env }) {
+cp.on('message', common.mustCall(({ env }) => {
   assert.strictEqual(env.foo, expectedEnv.foo);
 }));
