87d7a73d2ccc013cd5a28980f3eb91bc9981f3ac
guybedford <guybedford@gmail.com>
2018-08-30 18:26:31 +0200

    encode "\" in paths in posix

--- test/parallel/test-url-pathtofileurl.js
+++ test/parallel/test-url-pathtofileurl.js
@@ -1,5 +1,5 @@
 'use strict';
-require('../common');
+const { isWindows } = require('../common');
 const assert = require('assert');
 const url = require('url');
 
@@ -12,7 +12,10 @@ const url = require('url');
 {
   const fileURL = url.pathToFileURL('test\\').href;
   assert.ok(fileURL.startsWith('file:///'));
-  assert.ok(fileURL.endsWith('/'));
+  if (isWindows)
+    assert.ok(fileURL.endsWith('/'));
+  else
+    assert.ok(fileURL.endsWith('%5C'));
 }
 
 {
