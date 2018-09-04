63aa1f1bdd6297ca8b710d652f91c89b94ad1e3a
Peter Marshall <p.s.marshall0@gmail.com>
2018-09-04 15:48:15 +0200

    deps: backport detailed line info for CPU profiler
    [cpu-profiler] Add flag to always generate accurate line info.
    https://chromium.googlesource.com/v8/v8/+/
    56baf56790de439b3f69e887e94beb3b301ed77c
    
    [cpu-profiler] Turn on detailed line info for optimized code
    https://chromium.googlesource.com/v8/v8/+/
    84894ce6d2af7feb9e1f5574409355120887326c
    
    [cpu-profiler] Separate the flags for generating extra line information
    https://chromium.googlesource.com/v8/v8/+/
    30ff6719db441cc7ef220d449970cc169067e256
    
    PR-URL: https://github.com/nodejs/node/pull/22688
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- deps/v8/src/isolate.h
+++ deps/v8/src/isolate.h
@@ -1044,6 +1044,8 @@ class Isolate : private HiddenFactory {
 
   bool NeedsSourcePositionsForProfiling() const;
 
+  bool NeedsDetailedOptimizedCodeLineInfo() const;
+
   bool is_best_effort_code_coverage() const {
     return code_coverage_mode() == debug::Coverage::kBestEffort;
   }
