2e28090855f4c47b5602ea71c4c15770f503c85d
Michaël Zasso <targos@protonmail.com>
2018-09-07 17:07:17 +0200

    src: update NODE_MODULE_VERSION to 66
    Major V8 updates are usually API/ABI incompatible with previous
    versions. This commit adapts NODE_MODULE_VERSION for V8 6.9.
    
    Refs: https://github.com/nodejs/CTC/blob/master/meetings/2016-09-28.md
    
    PR-URL: https://github.com/nodejs/node/pull/21983
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: Ujjwal Sharma <usharma1998@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- src/node_version.h
+++ src/node_version.h
@@ -109,10 +109,11 @@
  * V8 6.6: 63
  * V8 6.7: 64
  * V8 6.8: 65
+ * V8 6.9: 66
  *
  * More information can be found at https://nodejs.org/en/download/releases/
  */
-#define NODE_MODULE_VERSION 65
+#define NODE_MODULE_VERSION 66
 
 // the NAPI_VERSION provided by this version of the runtime
 #define NAPI_VERSION  3
