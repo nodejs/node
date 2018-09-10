9c268d049219462de0792284c504f137751cf198
Matteo Collina <hello@matteocollina.com>
2018-09-10 12:57:07 +0200

    url: avoid hostname spoofing w/ javascript protocol
    CVE-2018-12123
    
    Fixes: https://github.com/nodejs-private/security/issues/205
    PR-URL: https://github.com/nodejs-private/node-private/pull/145
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-url.js
+++ test/parallel/test-url.js
@@ -903,6 +903,39 @@ const parseTests = {
     hostname: 'www.example.com',
     pathname: '/',
     path: '/'
+  },
+
+  // The following two URLs are the same, but they differ for
+  // a capital A: it is important that we verify that the protocol
+  // is checked in a case-insensitive manner.
+  'javascript:alert(1);a=\x27@white-listed.com\x27': {
+    protocol: 'javascript:',
+    slashes: null,
+    auth: null,
+    host: null,
+    port: null,
+    hostname: null,
+    hash: null,
+    search: null,
+    query: null,
+    pathname: "alert(1);a='@white-listed.com'",
+    path: "alert(1);a='@white-listed.com'",
+    href: "javascript:alert(1);a='@white-listed.com'"
+  },
+
+  'javAscript:alert(1);a=\x27@white-listed.com\x27': {
+    protocol: 'javascript:',
+    slashes: null,
+    auth: null,
+    host: null,
+    port: null,
+    hostname: null,
+    hash: null,
+    search: null,
+    query: null,
+    pathname: "alert(1);a='@white-listed.com'",
+    path: "alert(1);a='@white-listed.com'",
+    href: "javascript:alert(1);a='@white-listed.com'"
   }
 };
 
