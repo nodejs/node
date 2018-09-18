c55ebd8502352c6405bfd8c3b356285ccdd242fd
Refael Ackermann <refack@gmail.com>
2018-09-18 14:57:05 -0400

    build: make config verbose on CI
    PR-URL: https://github.com/nodejs/node/pull/22935
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- vcbuild.bat
+++ vcbuild.bat
@@ -175,6 +175,7 @@ if defined i18n_arg         set configure_flags=%configure_flags% --with-intl=%i
 if defined config_flags     set configure_flags=%configure_flags% %config_flags%
 if defined target_arch      set configure_flags=%configure_flags% --dest-cpu=%target_arch%
 if defined openssl_no_asm   set configure_flags=%configure_flags% --openssl-no-asm
+if defined DEBUG_HELPER     set configure_flags=%configure_flags% --verbose
 
 if not exist "%~dp0deps\icu" goto no-depsicu
 if "%target%"=="Clean" echo deleting %~dp0deps\icu
