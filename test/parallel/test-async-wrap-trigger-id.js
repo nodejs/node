bc0ff7c7c732d9ddad3f5d265a1a578d3152e7e6
MaleDong <maledong_github@outlook.com>
2018-09-20 14:28:58 +0800

    Fix to keep the valuable msg

--- test/parallel/test-async-wrap-trigger-id.js
+++ test/parallel/test-async-wrap-trigger-id.js
@@ -11,12 +11,16 @@ let triggerAsyncId1;
 process.nextTick(() => {
   process.nextTick(() => {
     triggerAsyncId1 = triggerAsyncId();
+    // Async resources having different causal ancestry
+    // should have different triggerAsyncIds
     assert.notStrictEqual(
       triggerAsyncId0,
       triggerAsyncId1);
   });
   process.nextTick(() => {
     const triggerAsyncId2 = triggerAsyncId();
+    // Async resources having the same causal ancestry
+    // should have the same triggerAsyncId
     assert.strictEqual(
       triggerAsyncId1,
       triggerAsyncId2);
