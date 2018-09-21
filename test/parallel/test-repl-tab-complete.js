5903929628e5aec1f99fa55099c92c7dfeba7399
Rich Trott <rtrott@gmail.com>
2018-09-21 11:55:55 -0700

    repl: do not swallow errors in nested REPLs
    For tab completion, a REPLServer instance will sometimes create another
    REPLServer instance. If a callback is sent to the `.complete()` function
    and that callback throws an error, it will be swallowed by the nested
    REPLs domain. Re-throw the error so that processes don't silently exit
    without any indication of an error (including a status code).
    
    Fixes: https://github.com/nodejs/node/issues/21586

--- test/parallel/test-repl-tab-complete.js
+++ test/parallel/test-repl-tab-complete.js
@@ -154,10 +154,10 @@ putIn.run([
   ' one:1',
   '};'
 ]);
-// See: https://github.com/nodejs/node/issues/21586
-// testMe.complete('inner.o', getNoResultsFunction());
 testMe.complete('inner.o', common.mustCall(function(error, data) {
+  assert.deepStrictEqual(data, works);
 }));
+
 
 putIn.run(['.clear']);
 
