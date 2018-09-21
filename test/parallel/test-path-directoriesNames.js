8bb05b82b0fddae261f57896452ac8f471defdd0
jy95 <jacques.yakoub@gmail.com>
2018-09-21 23:29:51 +0200

    test: fix directoriesNames test in path.js

--- test/parallel/test-path-directoriesNames.js
+++ test/parallel/test-path-directoriesNames.js
@@ -5,31 +5,27 @@ const path = require('path');
 
 // A windows path
 assert.strictEqual(
-  path.directoriesNames(
-    'D:\\DDL\\ANIME\\les chevaliers du zodiaques\\whateverFile.avi'
+  JSON.stringify(
+    path.directoriesNames(
+      'D:\\DDL\\ANIME\\les chevaliers du zodiaques\\whateverFile.avi'
+    )
   ),
-  ['D:', 'DDL', 'ANIME', 'les chevaliers du zodiaques']
+  JSON.stringify(['D:', 'DDL', 'ANIME', 'les chevaliers du zodiaques'])
 );
 
 // A Linux path
 assert.strictEqual(
-  path.directoriesNames(
-    'D:/DDL/EBOOK/whatEverFile.pub'
-  ),
-  [ 'D:', 'DDL', 'EBOOK' ]
+  JSON.stringify(path.directoriesNames('D:/DDL/EBOOK/whatEverFile.pub')),
+  JSON.stringify(['D:', 'DDL', 'EBOOK'])
 );
 
 // Path near root (Windows / Linux)
 assert.strictEqual(
-  path.directoriesNames(
-    '/foo'
-  ),
-  ['/']
+  JSON.stringify(path.directoriesNames('/foo')),
+  JSON.stringify(['/'])
 );
 
 assert.strictEqual(
-  path.directoriesNames(
-    'D:\\test.avi'
-  ),
-  ['D:']
+  JSON.stringify(path.directoriesNames('D:\\test.avi')),
+  JSON.stringify(['D:'])
 );
