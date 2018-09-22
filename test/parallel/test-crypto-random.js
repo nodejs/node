709b3b1e1c75afe6b5dd9d7859d0142dd4d20e5c
Anna Henningsen <anna@addaleax.net>
2018-09-22 11:45:42 +0200

    crypto: downgrade DEP0115 to `--pending-deprecation` only
    Aliases are very cheap to maintain, so an unconditional runtime
    deprecation that affects existing ecosystem code is not
    a good idea. This commit turns the runtime deprecation into
    a `--pending-deprecation` one.
    
    Fixes: https://github.com/nodejs/node/issues/23013
    
    PR-URL: https://github.com/nodejs/node/pull/23017
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- test/parallel/test-crypto-random.js
+++ test/parallel/test-crypto-random.js
@@ -19,6 +19,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// Flags: --pending-deprecation
 'use strict';
 const common = require('../common');
 
