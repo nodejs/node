e03dce2150c2f6d0b261fcb972c05ce69eb9d1d3
Josh Broomfield <josh.m.broomfield@gmail.com>
2018-09-27 02:48:51 -0400

    test: add more descriptive err message to assert
    PR-URL: https://github.com/nodejs/node/pull/23118
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-child-process-disconnect.js
+++ test/parallel/test-child-process-disconnect.js
@@ -93,7 +93,11 @@ if (process.argv[2] === 'child') {
         // ready to be disconnected
         if (data === 'ready') {
           child.disconnect();
-          assert.throws(child.disconnect.bind(child), Error);
+          assert.throws(
+            child.disconnect.bind(child),
+            {
+              code: 'ERR_IPC_DISCONNECTED'
+            });
           return;
         }
 
