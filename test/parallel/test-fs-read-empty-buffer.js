a29631b23717370f7786b563049b0be387fc0a41
shisama <cherrydog07@gmail.com>
2018-09-28 14:50:04 +0900

    test: error when empty buffer is passed to fs.read()
    Added tests to occur error when empty buffer is passed to fs.read()
    to increase coverage.
    
    PR-URL: https://github.com/nodejs/node/pull/23141
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-fs-read-empty-buffer.js
+++ test/parallel/test-fs-read-empty-buffer.js
@@ -1,5 +1,6 @@
 'use strict';
 require('../common');
+const common = require('../common');
 const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const fs = require('fs');
@@ -10,6 +11,15 @@ const buffer = new Uint8Array();
 
 assert.throws(
   () => fs.readSync(fd, buffer, 0, 10, 0),
+  {
+    code: 'ERR_INVALID_ARG_VALUE',
+    message: 'The argument \'buffer\' is empty and cannot be written. ' +
+    'Received Uint8Array []'
+  }
+);
+
+assert.throws(
+  () => fs.read(fd, buffer, 0, 1, 0, common.mustNotCall()),
   {
     code: 'ERR_INVALID_ARG_VALUE',
     message: 'The argument \'buffer\' is empty and cannot be written. ' +
