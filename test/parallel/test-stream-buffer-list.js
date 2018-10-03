ceebd5783225e3ea14376871532e50a673cfaaee
Refael Ackermann <refack@gmail.com>
2018-10-02 21:13:04 -0400

    Revert "util: change util.inspect depth default"
    This reverts commits:
    ac7450a09a4c167cd43c14d7c88721d22f077529
    1a1fe53e3dbd0042807b75caac94dcae0abe4dc1

--- test/parallel/test-stream-buffer-list.js
+++ test/parallel/test-stream-buffer-list.js
@@ -3,7 +3,6 @@
 require('../common');
 const assert = require('assert');
 const BufferList = require('internal/streams/buffer_list');
-const util = require('util');
 
 // Test empty buffer list.
 const emptyList = new BufferList();
@@ -31,10 +30,3 @@ assert.strictEqual(list.join(','), 'foo');
 const shifted = list.shift();
 assert.strictEqual(shifted, buf);
 assert.deepStrictEqual(list, new BufferList());
-
-const tmp = util.inspect.defaultOptions.colors;
-util.inspect.defaultOptions = { colors: true };
-assert.strictEqual(
-  util.inspect(list),
-  'BufferList { length: \u001b[33m0\u001b[39m }');
-util.inspect.defaultOptions = { colors: tmp };
