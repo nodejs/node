9487c4242485cd7b959660ed72481670e2ea943b
Ouyang Yadong <oyydoibh@gmail.com>
2018-10-07 21:18:15 +0800

    tls: make StreamWrap work correctly in "drain" callback
    When an instance of StreamWrap is shutting down and a "drain" event
    is emitted, the instance will abort as its
    `this[kCurrentShutdownRequest]` is already set. The following test
    will fail before this commit.
    
    PR-URL: https://github.com/nodejs/node/pull/23294
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>

---
+++ test/parallel/test-stream-wrap-drain.js
@@ -0,0 +1,48 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const { StreamWrap } = require('_stream_wrap');
+const { Duplex } = require('stream');
+const { ShutdownWrap } = process.binding('stream_wrap');
+
+// This test makes sure that when an instance of JSStreamWrap is waiting for
+// a "drain" event to `doShutdown`, the instance will work correctly when a
+// "drain" event emitted.
+{
+  let resolve = null;
+
+  class TestDuplex extends Duplex {
+    _write(chunk, encoding, callback) {
+      // We will resolve the write later.
+      resolve = () => {
+        callback();
+      };
+    }
+
+    _read() {}
+  }
+
+  const testDuplex = new TestDuplex();
+  const socket = new StreamWrap(testDuplex);
+
+  socket.write(
+    // Make the buffer long enough so that the `Writable` will emit "drain".
+    Buffer.allocUnsafe(socket.writableHighWaterMark * 2),
+    common.mustCall()
+  );
+
+  // Make sure that the 'drain' events will be emitted.
+  testDuplex.on('drain', common.mustCall(() => {
+    console.log('testDuplex drain');
+  }));
+
+  assert.strictEqual(typeof resolve, 'function');
+
+  const req = new ShutdownWrap();
+  req.oncomplete = common.mustCall();
+  req.handle = socket._handle;
+  // Should not throw.
+  socket._handle.shutdown(req);
+
+  resolve();
+}
