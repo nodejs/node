9749d487290ac222915e896f4f6ceb2ad19d1eef
Rich Trott <rtrott@gmail.com>
2018-10-06 17:08:02 -0700

    benchmark: increase lint compliance
    Remove two eslint-disable comments by replacing string concatenation
    with template literals. These changes are in catch blocks that are not
    part of the actual code being benchmarked.
    
    PR-URL: https://github.com/nodejs/node/pull/23305
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Vse Mozhet Byt <vsemozhetbyt@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- benchmark/napi/function_args/index.js
+++ benchmark/napi/function_args/index.js
@@ -12,16 +12,14 @@ let napi;
 try {
   v8 = require('./build/Release/binding');
 } catch (err) {
-  // eslint-disable-next-line no-path-concat
-  console.error(__filename + ': V8 Binding failed to load');
+  console.error(`${__filename}: V8 Binding failed to load`);
   process.exit(0);
 }
 
 try {
   napi = require('./build/Release/napi_binding');
 } catch (err) {
-  // eslint-disable-next-line no-path-concat
-  console.error(__filename + ': NAPI-Binding failed to load');
+  console.error(`${__filename}: NAPI-Binding failed to load`);
   process.exit(0);
 }
 
