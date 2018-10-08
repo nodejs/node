c65a523597ccdf2b3544244679dae581040cf52f
cjihrig <cjihrig@gmail.com>
2018-10-08 13:14:58 -0400

    deps: upgrade to libuv 1.23.2
    PR-URL: https://github.com/nodejs/node/pull/23336
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Fixes: https://github.com/nodejs/node/issues/23043
    Fixes: https://github.com/nodejs/node/issues/21773
    Fixes: https://github.com/nodejs/node/issues/16601
    Fixes: https://github.com/nodejs/node/issues/22999
    Fixes: https://github.com/nodejs/node/issues/23219
    Fixes: https://github.com/nodejs/node/issues/23066
    Fixes: https://github.com/nodejs/node/issues/23067
    Fixes: https://github.com/nodejs/node/issues/23089

--- deps/uv/src/unix/bsd-ifaddrs.c
+++ deps/uv/src/unix/bsd-ifaddrs.c
@@ -119,16 +119,13 @@ int uv_interface_addresses(uv_interface_address_t** addresses, int* count) {
       continue;
 
     address = *addresses;
+    memset(address->phys_addr, 0, sizeof(address->phys_addr));
 
     for (i = 0; i < *count; i++) {
       if (strcmp(address->name, ent->ifa_name) == 0) {
-#if defined(__CYGWIN__) || defined(__MSYS__)
-        memset(address->phys_addr, 0, sizeof(address->phys_addr));
-#else
         struct sockaddr_dl* sa_addr;
         sa_addr = (struct sockaddr_dl*)(ent->ifa_addr);
         memcpy(address->phys_addr, LLADDR(sa_addr), sizeof(address->phys_addr));
-#endif
       }
       address++;
     }
