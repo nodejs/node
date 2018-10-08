63b06551f4c51d8868f36b184a6539ebc51cf0b4
Masashi Hirano <cherrydog07@gmail.com>
2018-10-09 08:10:07 +0900

    src,lib: make process.binding('config') internal
    PR-URL: https://github.com/nodejs/node/pull/23400
    Refs: https://github.com/nodejs/node/issues/22160
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- src/node_config.cc
+++ src/node_config.cc
@@ -112,4 +112,4 @@ static void Initialize(Local<Object> target,
 
 }  // namespace node
 
-NODE_BUILTIN_MODULE_CONTEXT_AWARE(config, node::Initialize)
+NODE_MODULE_CONTEXT_AWARE_INTERNAL(config, node::Initialize)
