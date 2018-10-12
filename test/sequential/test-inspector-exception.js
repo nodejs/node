5d7676c0475cb85d60bb595ce4780c5bab30d80c
keeysnc <keeysnc@gmail.com>
2018-10-12 11:03:37 -0700

    test: updated assert test values to doc standards
    PR-URL: https://github.com/nodejs/node/pull/23593
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

--- test/sequential/test-inspector-exception.js
+++ test/sequential/test-inspector-exception.js
@@ -39,7 +39,7 @@ async function runTest() {
   const session = await child.connectInspectorSession();
   await testBreakpointOnStart(session);
   await session.runToCompletion();
-  assert.strictEqual(1, (await child.expectShutdown()).exitCode);
+  assert.strictEqual((await child.expectShutdown()).exitCode, 1);
 }
 
 runTest();
