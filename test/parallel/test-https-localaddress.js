697c3da30820c37cdd571e64c467494452b38d4b
Ian Sutherland <ian@iansutherland.ca>
2018-10-12 11:33:54 -0700

    test: fix assert parameter order in test-https-localaddress.js
    PR-URL: https://github.com/nodejs/node/pull/23599
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

--- test/parallel/test-https-localaddress.js
+++ test/parallel/test-https-localaddress.js
@@ -39,7 +39,7 @@ const options = {
 
 const server = https.createServer(options, function(req, res) {
   console.log(`Connect from: ${req.connection.remoteAddress}`);
-  assert.strictEqual('127.0.0.2', req.connection.remoteAddress);
+  assert.strictEqual(req.connection.remoteAddress, '127.0.0.2');
 
   req.on('end', function() {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
