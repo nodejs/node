b61d31a8458f5e90f94096ad822c0b457bcc9114
Dolapo Toki <dolapotoki@gmail.com>
2018-10-12 11:11:44 -0700

    src: add deprecation warning to errname()
    PR-URL: https://github.com/nodejs/node/pull/23597
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

---
+++ test/parallel/test-err-name-deprecation.js
@@ -0,0 +1,13 @@
+'use strict';
+const common = require('../common');
+
+// Flags: --pending-deprecation
+
+common.expectWarning(
+  'DeprecationWarning',
+  'Directly calling process.binding(\'uv\').errname(<val>) is being ' +
+  'deprecated. Please make sure to use util.getSystemErrorName() instead.',
+  'DEP0119'
+);
+
+process.binding('uv').errname(-1);
