bf20cf8ba25f4697deb32bd86832fb71328d145d
Anna Zhao <annatangzhao@gmail.com>
2018-10-12 10:06:42 -0700

    test: change to arrow functions in send-bad-arguments
    PR-URL: https://github.com/nodejs/node/pull/23483
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Shelley Vohr <codebytere@gmail.com>

--- test/parallel/test-dgram-send-bad-arguments.js
+++ test/parallel/test-dgram-send-bad-arguments.js
@@ -28,17 +28,17 @@ const buf = Buffer.from('test');
 const host = '127.0.0.1';
 const sock = dgram.createSocket('udp4');
 
-assert.throws(function() {
+assert.throws(() => {
   sock.send();
 }, TypeError);  // First argument should be a buffer.
 
 // send(buf, offset, length, port, host)
-assert.throws(function() { sock.send(buf, 1, 1, -1, host); }, RangeError);
-assert.throws(function() { sock.send(buf, 1, 1, 0, host); }, RangeError);
-assert.throws(function() { sock.send(buf, 1, 1, 65536, host); }, RangeError);
+assert.throws(() => { sock.send(buf, 1, 1, -1, host); }, RangeError);
+assert.throws(() => { sock.send(buf, 1, 1, 0, host); }, RangeError);
+assert.throws(() => { sock.send(buf, 1, 1, 65536, host); }, RangeError);
 
 // send(buf, port, host)
-assert.throws(function() { sock.send(23, 12345, host); }, TypeError);
+assert.throws(() => { sock.send(23, 12345, host); }, TypeError);
 
 // send([buf1, ..], port, host)
-assert.throws(function() { sock.send([buf, 23], 12345, host); }, TypeError);
+assert.throws(() => { sock.send([buf, 23], 12345, host); }, TypeError);
