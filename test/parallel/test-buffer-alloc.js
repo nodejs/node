232fe58a4cf67edad4f5c3a83104c2cfa5335451
Nancy Truong <truongh.nancy@gmail.com>
2018-10-12 10:23:36 -0700

    test: check codes of thrown errors
    PR-URL: https://github.com/nodejs/node/pull/23519
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: George Adams <george.adams@uk.ibm.com>

--- test/parallel/test-buffer-alloc.js
+++ test/parallel/test-buffer-alloc.js
@@ -967,12 +967,16 @@ common.expectsError(
     message: 'argument must be a buffer'
   });
 
-const regErrorMsg =
-  new RegExp('The first argument must be one of type string, Buffer, ' +
-             'ArrayBuffer, Array, or Array-like Object\\.');
-
-assert.throws(() => Buffer.from(), regErrorMsg);
-assert.throws(() => Buffer.from(null), regErrorMsg);
+assert.throws(() => Buffer.from(), {
+  name: 'TypeError [ERR_INVALID_ARG_TYPE]',
+  message: 'The first argument must be one of type string, Buffer, ' +
+  'ArrayBuffer, Array, or Array-like Object. Received type undefined'
+});
+assert.throws(() => Buffer.from(null), {
+  name: 'TypeError [ERR_INVALID_ARG_TYPE]',
+  message: 'The first argument must be one of type string, Buffer, ' +
+  'ArrayBuffer, Array, or Array-like Object. Received type object'
+});
 
 // Test prototype getters don't throw
 assert.strictEqual(Buffer.prototype.parent, undefined);
