e7ca5efc4bcb637bbcf713db2568692400ba8829
Rich Trott <rtrott@gmail.com>
2018-10-12 20:20:35 -0700

    Update test-benchmark-napi.js

--- test/sequential/test-benchmark-napi.js
+++ test/sequential/test-benchmark-napi.js
@@ -3,7 +3,7 @@
 const common = require('../common');
 
 if (common.isWindows) {
-  common.skip('vcbuild.bat doesn\'t build the n-api benchmarks yet')
+  common.skip('vcbuild.bat doesn\'t build the n-api benchmarks yet');
 }
 
 const runBenchmark = require('../common/benchmark');
