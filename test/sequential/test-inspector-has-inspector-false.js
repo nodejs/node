32a811545333757b93fb45546ffc4b30aa8e6d2c
Andres Monge <jacjam@gmail.com>
2018-10-13 10:57:42 -0700

    test: remove the process variable back up
    Since the process exists after the test, there is no need to backup and
    restore the process.config.variables.v8_enable_inspector variable.

--- test/sequential/test-inspector-has-inspector-false.js
+++ test/sequential/test-inspector-has-inspector-false.js
@@ -6,15 +6,10 @@ const common = require('../common');
 // This is to ensure that the sendInspectorCommand function calls the error
 // function if its called with the v8_enable_inspector is disabled
 
-const v8EnableInspectorBk = process.config.variables.v8_enable_inspector;
 process.config.variables.v8_enable_inspector = 0;
 const inspector = require('internal/util/inspector');
 
-function restoreProcessVar() {
-  process.config.variables.v8_enable_inspector = v8EnableInspectorBk;
-}
-
 inspector.sendInspectorCommand(
   common.mustNotCall('Inspector callback should not be called'),
-  common.mustCall(restoreProcessVar, 1),
+  common.mustCall(1),
 );
