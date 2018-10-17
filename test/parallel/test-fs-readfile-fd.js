8f4b924f4a7b37bd16ddff65329c8e96fc5f0f2d
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2018-10-17 14:40:08 +0530

    fs: make writeFile consistent with readFile wrt fd
    As it is, `readFile` always reads from the current position of the file,
    if a file descriptor is used. But `writeFile` always writes from the
    beginning of the file.
    
    This patch fixes this inconsistency by making `writeFile` also to write
    from the current position of the file when used with a file descriptor.
    
    PR-URL: https://github.com/nodejs/node/pull/23709
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Vse Mozhet Byt <vsemozhetbyt@gmail.com>
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Rod Vagg <rod@vagg.org>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-fs-readfile-fd.js
+++ test/parallel/test-fs-readfile-fd.js
@@ -1,5 +1,5 @@
 'use strict';
-require('../common');
+const common = require('../common');
 
 // Test fs.readFile using a file descriptor.
 
@@ -7,6 +7,9 @@ const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const fs = require('fs');
 const fn = fixtures.path('empty.txt');
+const join = require('path').join;
+const tmpdir = require('../common/tmpdir');
+tmpdir.refresh();
 
 tempFd(function(fd, close) {
   fs.readFile(fd, function(err, data) {
@@ -45,4 +48,49 @@ function tempFdSync(callback) {
   const fd = fs.openSync(fn, 'r');
   callback(fd);
   fs.closeSync(fd);
+}
+
+{
+  /*
+   * This test makes sure that `readFile()` always reads from the current
+   * position of the file, instead of reading from the beginning of the file,
+   * when used with file descriptors.
+   */
+
+  const filename = join(tmpdir.path, 'test.txt');
+  fs.writeFileSync(filename, 'Hello World');
+
+  {
+    /* Tests the fs.readFileSync(). */
+    const fd = fs.openSync(filename, 'r');
+
+    /* Read only five bytes, so that the position moves to five. */
+    const buf = Buffer.alloc(5);
+    assert.deepStrictEqual(fs.readSync(fd, buf, 0, 5), 5);
+    assert.deepStrictEqual(buf.toString(), 'Hello');
+
+    /* readFileSync() should read from position five, instead of zero. */
+    assert.deepStrictEqual(fs.readFileSync(fd).toString(), ' World');
+  }
+
+  {
+    /* Tests the fs.readFile(). */
+    fs.open(filename, 'r', common.mustCall((err, fd) => {
+      assert.ifError(err);
+      const buf = Buffer.alloc(5);
+
+      /* Read only five bytes, so that the position moves to five. */
+      fs.read(fd, buf, 0, 5, null, common.mustCall((err, bytes) => {
+        assert.ifError(err);
+        assert.strictEqual(bytes, 5);
+        assert.deepStrictEqual(buf.toString(), 'Hello');
+
+        fs.readFile(fd, common.mustCall((err, data) => {
+          assert.ifError(err);
+          /* readFile() should read from position five, instead of zero. */
+          assert.deepStrictEqual(data.toString(), ' World');
+        }));
+      }));
+    }));
+  }
 }
