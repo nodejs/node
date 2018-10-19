a1ee86c427d53819d856cf47c1d3261754021ed9
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-10-19 23:18:30 +0200

    test: fix strictEqual() arguments order
    PR-URL: https://github.com/nodejs/node/pull/23771
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-http-parser-bad-ref.js
+++ test/parallel/test-http-parser-bad-ref.js
@@ -82,7 +82,7 @@ demoBug('POST /1/22 HTTP/1.1\r\n' +
         'pong');
 
 process.on('exit', function() {
-  assert.strictEqual(2, headersComplete);
-  assert.strictEqual(2, messagesComplete);
+  assert.strictEqual(headersComplete, 2);
+  assert.strictEqual(messagesComplete, 2);
   console.log('done!');
 });
