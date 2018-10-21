d39da714cf1d3ebdb224d5366dd4bda231059b86
Anna Henningsen <anna@addaleax.net>
2018-10-21 00:44:48 -0400

    buffer: test fix crash for invalid index types

--- test/parallel/test-buffer-copy.js
+++ test/parallel/test-buffer-copy.js
@@ -19,6 +19,17 @@ let cntr = 0;
 }
 
 {
+  // Current behavior is to coerce values to integers.
+  b.fill(++cntr);
+  c.fill(++cntr);
+  const copied = b.copy(c, '0', '0', '512');
+  assert.strictEqual(copied, 512);
+  for (let i = 0; i < c.length; i++) {
+    assert.strictEqual(c[i], b[i]);
+  }
+}
+
+{
   // copy c into b, without specifying sourceEnd
   b.fill(++cntr);
   c.fill(++cntr);
@@ -144,3 +155,13 @@ assert.strictEqual(b.copy(c, 512, 0, 10), 0);
     assert.strictEqual(c[i], e[i]);
   }
 }
+
+// https://github.com/nodejs/node/issues/23668: Do not crash for invalid input.
+c.fill('c');
+b.copy(c, 'not a valid offset');
+// Make sure this acted like a regular copy with `0` offset.
+assert.deepStrictEqual(c, b.slice(0, c.length));
+
+assert.throws(() => {
+  b.copy(c, { [Symbol.toPrimitive]() { throw new Error('foo'); } });
+}, /foo/);
