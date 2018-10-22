2cf060dfbaa8f1ff74660e0c936e138d1cff684a
Shobhit Chittora <chittorashobhit@gmail.com>
2018-10-22 14:55:58 +0530

    child_process: adds test for null check

--- test/parallel/test-child-process-fork-options.js
+++ test/parallel/test-child-process-fork-options.js
@@ -9,13 +9,29 @@ const assert = require('assert');
 const { fork } = require('child_process');
 
 const expectedEnv = { foo: 'bar' };
-const cp = fork(fixtures.path('child-process-echo-options.js'), undefined,
-                { env: Object.assign({}, process.env, expectedEnv) });
 
-cp.on('message', common.mustCall(({ env }) => {
-  assert.strictEqual(env.foo, expectedEnv.foo);
-}));
+{
+  const cp = fork(fixtures.path('child-process-echo-options.js'), undefined,
+                  { env: Object.assign({}, process.env, expectedEnv) });
 
-cp.on('exit', common.mustCall((code) => {
-  assert.strictEqual(code, 0);
-}));
+  cp.on('message', common.mustCall(({ env }) => {
+    assert.strictEqual(env.foo, expectedEnv.foo);
+  }));
+
+  cp.on('exit', common.mustCall((code) => {
+    assert.strictEqual(code, 0);
+  }));
+}
+
+{
+  const cp = fork(fixtures.path('child-process-echo-options.js'), null,
+                  { env: Object.assign({}, process.env, expectedEnv) });
+
+  cp.on('message', common.mustCall(({ env }) => {
+    assert.strictEqual(env.foo, expectedEnv.foo);
+  }));
+
+  cp.on('exit', common.mustCall((code) => {
+    assert.strictEqual(code, 0);
+  }));
+}
