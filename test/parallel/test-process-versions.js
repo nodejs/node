7b8b7234f09963fe8d08ae5cdcf1ccd955a44178
Adam Majer <amajer@suse.de>
2018-10-25 13:19:27 +0200

    test: fix regression when compiled with FIPS
    In commit bff53c5a9d6, a check was added for very specific OpenSSL
    format. Unfortunately, when OpenSSL is compiled in FIPS mode, this
    check fails. Added additional regex to satisfy OpenSSL version
    strings in both regular and FIPS modes.

--- test/parallel/test-process-versions.js
+++ test/parallel/test-process-versions.js
@@ -34,7 +34,7 @@ assert(/^\d+\.\d+\.\d+(?:\.\d+)?-node\.\d+(?: \(candidate\))?$/
 assert(/^\d+$/.test(process.versions.modules));
 
 if (common.hasCrypto) {
-  assert(/^\d+\.\d+\.\d+[a-z]?$/.test(process.versions.openssl));
+  assert(/^\d+\.\d+\.\d+[a-z]?(-fips)?$/.test(process.versions.openssl));
 }
 
 for (let i = 0; i < expected_keys.length; i++) {
