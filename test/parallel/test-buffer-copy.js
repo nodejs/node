a15de4876a0a3c014c491086940bac579d88d805
mritunjaygoutam12 <mritunjaygoutam2204@gmail.com>
2018-10-30 12:59:02 +0530

    test: add property for RangeError in test-buffer-copy
    PR-URL: https://github.com/nodejs/node/pull/23968
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-buffer-copy.js
+++ test/parallel/test-buffer-copy.js
@@ -1,10 +1,17 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 
 const b = Buffer.allocUnsafe(1024);
 const c = Buffer.allocUnsafe(512);
+
+const errorProperty = {
+  code: 'ERR_INDEX_OUT_OF_RANGE',
+  type: RangeError,
+  message: 'Index out of range'
+};
+
 let cntr = 0;
 
 {
@@ -107,9 +114,9 @@ bb.fill('hello crazy world');
 b.copy(c, 0, 100, 10);
 
 // copy throws at negative sourceStart
-assert.throws(function() {
-  Buffer.allocUnsafe(5).copy(Buffer.allocUnsafe(5), 0, -1);
-}, RangeError);
+common.expectsError(
+  () => Buffer.allocUnsafe(5).copy(Buffer.allocUnsafe(5), 0, -1),
+  errorProperty);
 
 {
   // check sourceEnd resets to targetEnd if former is greater than the latter
@@ -122,7 +129,8 @@ assert.throws(function() {
 }
 
 // throw with negative sourceEnd
-assert.throws(() => b.copy(c, 0, 0, -1), RangeError);
+common.expectsError(
+  () => b.copy(c, 0, -1), errorProperty);
 
 // when sourceStart is greater than sourceEnd, zero copied
 assert.strictEqual(b.copy(c, 0, 100, 10), 0);
