b04d5944cfada5c430c6371a7aad2d6f601875d6
Benjamin <ben@npmjs.com>
2018-11-03 15:42:13 -0700

    coverage: make coverage work for Node.js

--- test/parallel/test-heapdump-inspector.js
+++ test/parallel/test-heapdump-inspector.js
@@ -7,17 +7,38 @@ common.skipIfInspectorDisabled();
 const { validateSnapshotNodes } = require('../common/heap');
 const inspector = require('inspector');
 
-const session = new inspector.Session();
-validateSnapshotNodes('Node / JSBindingsConnection', []);
-session.connect();
-validateSnapshotNodes('Node / JSBindingsConnection', [
-  {
-    children: [
-      { node_name: 'Node / InspectorSession', edge_name: 'session' },
-      { node_name: 'Connection', edge_name: 'wrapped' },
-      (edge) => edge.name === 'callback' &&
-        (edge.to.type === undefined || // embedded graph
-         edge.to.type === 'closure') // snapshot
-    ]
+const snapshotNode = {
+  children: [
+    { node_name: 'Node / InspectorSession', edge_name: 'session' }
+  ]
+};
+
+// starts with no JSBindingsConnection (or 1 if coverage enabled).
+{
+  const expected = [];
+  if (process.env.NODE_V8_COVERAGE) {
+    expected.push(snapshotNode);
   }
-]);
+  validateSnapshotNodes('Node / JSBindingsConnection', expected);
+}
+
+// JSBindingsConnection should be added.
+{
+  const session = new inspector.Session();
+  session.connect();
+  const expected = [
+    {
+      children: [
+        { node_name: 'Node / InspectorSession', edge_name: 'session' },
+        { node_name: 'Connection', edge_name: 'wrapped' },
+        (edge) => edge.name === 'callback' &&
+          (edge.to.type === undefined || // embedded graph
+           edge.to.type === 'closure') // snapshot
+      ]
+    }
+  ];
+  if (process.env.NODE_V8_COVERAGE) {
+    expected.push(snapshotNode);
+  }
+  validateSnapshotNodes('Node / JSBindingsConnection', expected);
+}
