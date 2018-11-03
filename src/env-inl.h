1cda9b39883d825aadf0c011be869e34d8fe2300
cjihrig <cjihrig@gmail.com>
2018-11-03 11:52:37 -0400

    src: fix Set() usage in env-inl.h
    PR-URL: https://github.com/nodejs/node/pull/24060
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

--- src/env-inl.h
+++ src/env-inl.h
@@ -740,7 +740,7 @@ inline void Environment::SetMethod(v8::Local<v8::Object> that,
   const v8::NewStringType type = v8::NewStringType::kInternalized;
   v8::Local<v8::String> name_string =
       v8::String::NewFromUtf8(isolate(), name, type).ToLocalChecked();
-  that->Set(name_string, function);
+  that->Set(context, name_string, function).FromJust();
   function->SetName(name_string);  // NODE_SET_METHOD() compatibility.
 }
 
@@ -760,7 +760,7 @@ inline void Environment::SetMethodNoSideEffect(v8::Local<v8::Object> that,
   const v8::NewStringType type = v8::NewStringType::kInternalized;
   v8::Local<v8::String> name_string =
       v8::String::NewFromUtf8(isolate(), name, type).ToLocalChecked();
-  that->Set(name_string, function);
+  that->Set(context, name_string, function).FromJust();
   function->SetName(name_string);  // NODE_SET_METHOD() compatibility.
 }
 
