ee2f86824d1bcbb75a43fdd78bd47d1669b86371
dexterleng <thequicklab@gmail.com>
2018-11-04 18:57:22 +0800

    Add test

---
+++ test/parallel/test-fs-stream-no-events-after-destroy.js
@@ -0,0 +1,16 @@
+'use strict';
+const common = require('../common');
+const fs = require('fs');
+
+const tmpdir = require('../common/tmpdir');
+tmpdir.refresh();
+
+test(fs.createReadStream(__filename));
+test(fs.createWriteStream(`${tmpdir.path}/dummy`));
+
+function test(stream) {
+  const err = new Error('DESTROYED');
+  stream.destroy(err);
+  stream.on('open', common.mustNotCall());
+  stream.on('ready', common.mustNotCall());
+}
