a4cae978fc9a60c31ec443ee72bfc5770a742b92
James Herrington <james@getimpala.com>
2018-11-06 15:19:21 +0000

    test: add tests for process.initgroups
    - test argument validation
    - test function throws if provided group is invalid
    
    PR-URL: https://github.com/nodejs/node/pull/24154
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/parallel/test-process-initgroups.js
@@ -0,0 +1,54 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+
+if (common.isWindows || !common.isMainThread) {
+  assert.strictEqual(process.initgroups, undefined);
+  return;
+}
+
+[undefined, null, true, {}, [], () => {}].forEach((val) => {
+  assert.throws(
+    () => {
+      process.initgroups(val);
+    },
+    {
+      code: 'ERR_INVALID_ARG_TYPE',
+      name: 'TypeError [ERR_INVALID_ARG_TYPE]',
+      message:
+        'The "user" argument must be ' +
+        'one of type number or string. ' +
+        `Received type ${typeof val}`
+    }
+  );
+});
+
+[undefined, null, true, {}, [], () => {}].forEach((val) => {
+  assert.throws(
+    () => {
+      process.initgroups('foo', val);
+    },
+    {
+      code: 'ERR_INVALID_ARG_TYPE',
+      name: 'TypeError [ERR_INVALID_ARG_TYPE]',
+      message:
+        'The "extraGroup" argument must be ' +
+        'one of type number or string. ' +
+        `Received type ${typeof val}`
+    }
+  );
+});
+
+assert.throws(
+  () => {
+    process.initgroups(
+      'fhqwhgadshgnsdhjsdbkhsdabkfabkveyb',
+      'fhqwhgadshgnsdhjsdbkhsdabkfabkveyb'
+    );
+  },
+  {
+    code: 'ERR_UNKNOWN_CREDENTIAL',
+    message:
+      'Group identifier does not exist: fhqwhgadshgnsdhjsdbkhsdabkfabkveyb'
+  }
+);
