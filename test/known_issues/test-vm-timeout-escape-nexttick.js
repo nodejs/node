989c2aaf830c974dc87a0f080834ff08bfd831ab
Rich Trott <rtrott@gmail.com>
2018-11-08 10:03:30 -0800

    test: fix flaky test-vm-timeout-escape-nexttick
    Increase the VM timeout. If it is too small, the VM does not exit before
    the code has a chance to create the problematic condition that causes
    the timeout to be ignored.
    
    Fixes: https://github.com/nodejs/node/issues/24120
    
    PR-URL: https://github.com/nodejs/node/pull/24251
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/known_issues/test-vm-timeout-escape-nexttick.js
+++ test/known_issues/test-vm-timeout-escape-nexttick.js
@@ -35,7 +35,7 @@ assert.throws(() => {
       nextTick,
       loop
     },
-    { timeout: common.platformTimeout(5) }
+    { timeout: common.platformTimeout(10) }
   );
 }, {
   code: 'ERR_SCRIPT_EXECUTION_TIMEOUT'
