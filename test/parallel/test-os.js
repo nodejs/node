51cea618e21ba88bc20feb537a72c1b831a94b46
Joyee Cheung <joyeec9h3@gmail.com>
2018-11-09 12:22:30 +0800

    os: do not call into JS to push values to an array in GetCPUInfo
    Instead of calling into JS from C++ to push values into an array,
    use the new Array::New API that takes a pointer and a length
    directly.
    
    PR-URL: https://github.com/nodejs/node/pull/24264
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- test/parallel/test-os.js
+++ test/parallel/test-os.js
@@ -92,6 +92,15 @@ assert.ok(uptime > 0);
 const cpus = os.cpus();
 is.array(cpus);
 assert.ok(cpus.length > 0);
+for (const cpu of cpus) {
+  assert.strictEqual(typeof cpu.model, 'string');
+  assert.strictEqual(typeof cpu.speed, 'number');
+  assert.strictEqual(typeof cpu.times.user, 'number');
+  assert.strictEqual(typeof cpu.times.nice, 'number');
+  assert.strictEqual(typeof cpu.times.sys, 'number');
+  assert.strictEqual(typeof cpu.times.idle, 'number');
+  assert.strictEqual(typeof cpu.times.irq, 'number');
+}
 
 const type = os.type();
 is.string(type);
