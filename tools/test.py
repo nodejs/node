de378c0c2d5a95726a78c779922b7b02b963e82d
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-11-15 05:37:55 +0100

    test: skip test that use --tls-v1.x flags
    Currently, configuring --without-ssl will cause the following test to
    fail:
    
    === release test-https-agent-additional-options ===
    Path: parallel/test-https-agent-additional-options
    out/Release/node: bad option: --tls-v1.1
    Command: out/Release/node --tls-v1.1
      /node/test/parallel/test-https-agent-additional-options.js
    
    === release test-https-agent-session-eviction ===
    Path: parallel/test-https-agent-session-eviction
    out/Release/node: bad option: --tls-v1.0
    
    Command: out/Release/node --tls-v1.0
      /node/test/parallel/test-https-agent-session-eviction.js
    
    This commit adds a check for the --tls-v.x flags and skips them if node
    was built without crypto support.
    
    PR-URL: https://github.com/nodejs/node/pull/24376
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
    Reviewed-By: Rod Vagg <rod@vagg.org>

--- tools/test.py
+++ tools/test.py
@@ -907,6 +907,7 @@ class Context(object):
     self.repeat = repeat
     self.abort_on_timeout = abort_on_timeout
     self.v8_enable_inspector = True
+    self.node_has_crypto = True
 
   def GetVm(self, arch, mode):
     if arch == 'none':
@@ -1632,9 +1633,14 @@ def Main():
 
   # We want to skip the inspector tests if node was built without the inspector.
   has_inspector = Execute([vm,
-      "-p", "process.config.variables.v8_enable_inspector"], context)
-  if has_inspector.stdout.rstrip() == "0":
-      context.v8_enable_inspector = False
+      '-p', 'process.config.variables.v8_enable_inspector'], context)
+  if has_inspector.stdout.rstrip() == '0':
+    context.v8_enable_inspector = False
+
+  has_crypto = Execute([vm,
+      '-p', 'process.versions.openssl'], context)
+  if has_crypto.stdout.rstrip() == 'undefined':
+    context.node_has_crypto = False
 
   if options.cat:
     visited = set()
