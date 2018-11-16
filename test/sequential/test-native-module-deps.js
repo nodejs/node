987fe8772a15c4dc75d0973b8bebc14af072c66b
Joyee Cheung <joyeec9h3@gmail.com>
2018-11-17 04:35:03 +0800

    test: add a test to make sure the modules can be required independently
    This patch adds a test that makes sure all the modules (internal
    or not) can be independently required - that is, when there is
    circular dependency, one module does not rely on another module
    being loaded first to initialize, instead it should lazily load
    that dependency after initialization.

---
+++ test/sequential/test-native-module-deps.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// This tests that all the native modules can be loaded independently
+// Flags: --expose-internals
+
+if (process.argv[3]) {
+  require(process.argv[3]);
+  return;
+}
+
+require('../common');
+const {
+  cachableBuiltins
+} = require('internal/bootstrap/cache');
+const { fork } = require('child_process');
+const assert = require('assert');
+
+for (const key of cachableBuiltins) {
+  run(key);
+}
+
+function run(key) {
+  const child = fork(__filename,
+    [ '--expose-internals', key ],
+    { silent: true }
+  );
+
+  let stdout = '';
+  let stderr = '';
+  child.stdout.on('data', (data) => (stdout += data.toString()));
+  child.stderr.on('data', (data) => (stderr += data.toString()));
+  child.on('close', (code) => {
+    if (code === 0) {
+      return;
+    }
+    console.log(`Failed to require ${key}`)
+    console.log('----- stderr ----')
+    console.log(stderr);
+    console.log('----- stdout ----')
+    console.log(stdout);
+    assert.strictEqual(code, 0);
+  });
+}
