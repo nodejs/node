5c3736a7720f37004e343ff80d3cd13d357c753f
cclauss <cclauss@bluewin.ch>
2018-11-19 11:43:52 +0100

    build: use print() function in configure.py
    PR-URL: https://github.com/nodejs/node/pull/24484
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- configure.py
+++ configure.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import json
 import sys
 import errno
@@ -608,7 +610,7 @@ def print_verbose(x):
   if not options.verbose:
     return
   if type(x) is str:
-    print x
+    print(x)
   else:
     pprint.pprint(x, indent=2)
 
