1066b688bf56b490cb46d26d87e7b619037db151
susantruong <susantruong2412@gmail.com>
2018-11-23 18:45:55 -0500

    n-api,test: remove last argument in assert.strictEqual()
    PR-URL: https://github.com/nodejs/node/pull/24584
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- test/addons-napi/test_env_sharing/test.js
+++ test/addons-napi/test_env_sharing/test.js
@@ -5,6 +5,5 @@ const storeEnv = require(`./build/${common.buildType}/store_env`);
 const compareEnv = require(`./build/${common.buildType}/compare_env`);
 const assert = require('assert');
 
-assert.strictEqual(compareEnv(storeEnv), true,
-                   'N-API environment pointers in two different modules have ' +
-                   'the same value');
+// N-API environment pointers in two different modules have the same value
+assert.strictEqual(compareEnv(storeEnv), true);
