8c0aa84f854d29ef44485d52c8bddba7a3ce77d0
Rich Trott <rtrott@gmail.com>
2018-11-24 11:27:15 -0800

    test: split out http2 from test-stream-pipeline
    Splitting out the http2 portion of the test has a few benfits:
    
    * We don't skip the rest of the tests if `node` is compiled without
      crypto.
    * We can find out if the http2 portion of the test is responsible for
      the timeouts reported in issue 24456.
    
    Refs: https://github.com/nodejs/node/issues/24456
    
    PR-URL: https://github.com/nodejs/node/pull/24631
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-stream-pipeline.js
+++ test/parallel/test-stream-pipeline.js
@@ -1,12 +1,9 @@
 'use strict';
 
 const common = require('../common');
-if (!common.hasCrypto)
-  common.skip('missing crypto');
 const { Stream, Writable, Readable, Transform, pipeline } = require('stream');
 const assert = require('assert');
 const http = require('http');
-const http2 = require('http2');
 const { promisify } = require('util');
 
 {
@@ -271,35 +268,6 @@ const { promisify } = require('util');
         cnt--;
         if (cnt === 0) rs.destroy();
       });
-    });
-  });
-}
-
-{
-  const server = http2.createServer((req, res) => {
-    pipeline(req, res, common.mustCall());
-  });
-
-  server.listen(0, () => {
-    const url = `http://localhost:${server.address().port}`;
-    const client = http2.connect(url);
-    const req = client.request({ ':method': 'POST' });
-
-    const rs = new Readable({
-      read() {
-        rs.push('hello');
-      }
-    });
-
-    pipeline(rs, req, common.mustCall((err) => {
-      server.close();
-      client.close();
-    }));
-
-    let cnt = 10;
-    req.on('data', (data) => {
-      cnt--;
-      if (cnt === 0) rs.destroy();
     });
   });
 }
