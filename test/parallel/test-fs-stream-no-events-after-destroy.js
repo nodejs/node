650079e14fbd1413699f1dd6d79b92a58428d228
dexterleng <thequicklab@gmail.com>
2018-11-26 12:10:27 +0800

    fs: address comments

--- test/parallel/test-fs-stream-no-events-after-destroy.js
+++ test/parallel/test-fs-stream-no-events-after-destroy.js
@@ -1,17 +1,21 @@
 'use strict';
 const common = require('../common');
 const fs = require('fs');
+const assert = require('assert');
 
 const tmpdir = require('../common/tmpdir');
 tmpdir.refresh();
 
-test(fs.createReadStream(__filename));
-test(fs.createWriteStream(`${tmpdir.path}/dummy`));
-
-function test(stream) {
+const test = (stream) => {
   const err = new Error('DESTROYED');
   stream.destroy(err);
   stream.on('open', common.mustNotCall());
   stream.on('ready', common.mustNotCall());
   stream.on('close', common.mustCall());
+  stream.on('error', common.mustCall((er) => {
+    assert.strictEqual(err, er);
+  }));
 }
+
+test(fs.createReadStream(__filename));
+test(fs.createWriteStream(`${tmpdir.path}/dummy`));
