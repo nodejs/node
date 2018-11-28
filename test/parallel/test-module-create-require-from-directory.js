2b1f48e01f26b67877748aa7481299e868cf7460
Gilles De Mey <gilles.de.mey@gmail.com>
2018-11-28 16:30:58 +0100

    test: add a negative test to test-module-create-require-from-directory

--- test/parallel/test-module-create-require-from-directory.js
+++ test/parallel/test-module-create-require-from-directory.js
@@ -6,7 +6,13 @@ const path = require('path');
 
 const { createRequireFromPath } = require('module');
 
-const p = path.join(path.resolve(__dirname, '..', 'fixtures'), path.sep);
+const fixPath = path.resolve(__dirname, '..', 'fixtures');
+const p = path.join(fixPath, path.sep);
 
 const req = createRequireFromPath(p);
+const reqFromNotDir = createRequireFromPath(fixPath);
+
 assert.strictEqual(req('./baz'), 'perhaps I work');
+assert.throws(() => {
+  reqFromNotDir('./baz');
+}, { code: 'MODULE_NOT_FOUND' });
