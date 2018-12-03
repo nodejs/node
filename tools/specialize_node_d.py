6a72d52a67f7bec36e0cd7524c87a1c7b221fc8d
cclauss <cclauss@bluewin.ch>
2018-12-03 11:52:35 +0100

    tools: prepare tools/specialize_node_d.py for Python 3
    PR-URL: https://github.com/nodejs/node/pull/24797
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- tools/specialize_node_d.py
+++ tools/specialize_node_d.py
@@ -12,20 +12,20 @@ import sys
 
 if len(sys.argv) != 5:
   print("usage: specialize_node_d.py outfile src/node.d flavor arch")
-  sys.exit(2);
+  sys.exit(2)
 
-outfile = file(sys.argv[1], 'w');
-infile = file(sys.argv[2], 'r');
-flavor = sys.argv[3];
-arch = sys.argv[4];
+outfile = open(sys.argv[1], 'w')
+infile = open(sys.argv[2], 'r')
+flavor = sys.argv[3]
+arch = sys.argv[4]
 
 model = r'curpsinfo->pr_dmodel == PR_MODEL_ILP32'
 
 for line in infile:
   if flavor == 'freebsd':
-    line = re.sub('procfs.d', 'psinfo.d', line);
+    line = re.sub('procfs.d', 'psinfo.d', line)
     if arch == 'x64':
-      line = re.sub(model, '0', line);
+      line = re.sub(model, '0', line)
     else:
-      line = re.sub(model, '1', line);
-  outfile.write(line);
+      line = re.sub(model, '1', line)
+  outfile.write(line)
