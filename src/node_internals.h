5fe774104fc2ffa638d9ddfd0df1bdbc7cc84e47
Anna Henningsen <anna@addaleax.net>
2018-12-04 12:39:30 +0100

    src: dispose of V8 platform in `process.exit()`
    Calling `process.exit()` calls the C `exit()` function, which in turn
    calls the destructors of static C++ objects. This can lead to race
    conditions with other concurrently executing threads; disposing of all
    Worker threads and then the V8 platform instance helps with this
    (although it might not be a full solution for all problems of
    this kind).
    
    Refs: https://github.com/nodejs/node/issues/24403
    Refs: https://github.com/nodejs/node/issues/25007
    
    PR-URL: https://github.com/nodejs/node/pull/25061
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node_internals.h
+++ src/node_internals.h
@@ -358,6 +358,7 @@ int ThreadPoolWork::CancelWork() {
 }
 
 tracing::AgentWriterHandle* GetTracingAgentWriter();
+void DisposePlatform();
 
 static inline const char* errno_string(int errorno) {
 #define ERRNO_CASE(e)  case e: return #e;
