14d01f21d310a1b1a073d2e4c2ffd3804743a7af
cclauss <cclauss@bluewin.ch>
2018-12-07 07:38:20 +0100

    test: do not lint macros files (again)
    PR-URL: https://github.com/nodejs/node/pull/24886
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- src/notrace_macros.py
+++ src/notrace_macros.py
@@ -1,6 +1,9 @@
 # This file is used by tools/js2c.py to preprocess out the DTRACE symbols in
 # builds that don't support DTrace. This is not used in builds that support
 # DTrace.
+
+# flake8: noqa
+
 macro DTRACE_HTTP_CLIENT_REQUEST(x) = ;
 macro DTRACE_HTTP_CLIENT_RESPONSE(x) = ;
 macro DTRACE_HTTP_SERVER_REQUEST(x) = ;
