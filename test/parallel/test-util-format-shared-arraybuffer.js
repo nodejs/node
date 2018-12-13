901d3d095908bb6c136c06c5b30172d240c0f6be
Ruben Bridgewater <ruben@bridgewater.de>
2018-12-13 05:23:16 +0100

    util: inspect ArrayBuffers contents as well
    Inspecting an ArrayBuffer now also shows their binary contents.
    
    PR-URL: https://github.com/nodejs/node/pull/25006
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-util-format-shared-arraybuffer.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-require('../common');
-const assert = require('assert');
-const util = require('util');
-const sab = new SharedArrayBuffer(4);
-assert.strictEqual(util.format(sab), 'SharedArrayBuffer { byteLength: 4 }');
