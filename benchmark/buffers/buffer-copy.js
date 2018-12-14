15dfef30b077338473cc6b3e33871de532473d1e
reklatsmasters <reklatsmasters@users.noreply.github.com>
2018-12-14 06:00:43 +0500

    update

--- benchmark/buffers/buffer-copy.js
+++ benchmark/buffers/buffer-copy.js
@@ -1,5 +1,6 @@
 'use strict';
 const common = require('../common.js');
+const { randomFillSync } = require('crypto');
 
 const bench = common.createBenchmark(main, {
   size: [10, 1024, 2048, 8192, 64 * 1024, 256 * 1024, 1024 * 1024, 8 * 1024 * 1024, 32 * 1024 * 1024],
@@ -7,12 +8,12 @@ const bench = common.createBenchmark(main, {
 });
 
 function main({ n, size }) {
-  const source = Buffer.allocUnsafe(size);
-  const target = Buffer.allocUnsafe(size);
+  const buf = Buffer.allocUnsafe(size * 2);
+  randomFillSync(buf);
 
   bench.start();
   for (var i = 0; i < n * 1024; i++) {
-    source.copy(target);
+    buf.copy(buf, size, 0, size);
   }
   bench.end(n);
 }
