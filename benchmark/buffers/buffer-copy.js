158e5842363c483e632b501a91b63810e405dab7
reklatsmasters <reklatsmasters@users.noreply.github.com>
2018-12-14 09:49:03 +0500

    fix edge case

--- benchmark/buffers/buffer-copy.js
+++ benchmark/buffers/buffer-copy.js
@@ -3,19 +3,21 @@ const common = require('../common.js');
 const { randomBytes } = require('crypto');
 
 const KB = 1024;
-const MB = KB * KB;
 
 const bench = common.createBenchmark(main, {
-  size: [10, KB, 2 * KB, 8 * KB, 64 * KB, 256 * KB, MB, 8 * MB, 32 * MB],
+  size: [10, KB, 2 * KB, 8 * KB, 64 * KB /* , 256 * KB */],
+  // This option checks edge case when target.length !== source.length.
+  targetStart: [0, 1],
   n: [1024]
 });
 
-const buf = randomBytes(32 * MB * 2);
+function main({ n, size, targetStart }) {
+  const source = randomBytes(size);
+  const target = randomBytes(size);
 
-function main({ n, size }) {
   bench.start();
   for (var i = 0; i < n * 1024; i++) {
-    buf.copy(buf, size, 0, size);
+    source.copy(target, targetStart);
   }
   bench.end(n);
 }
