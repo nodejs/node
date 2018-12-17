15c5aeab84e9fc4b02ac1cf0679b6d65b560bd41
Beni von Cheni <benjaminlchen@gmail.com>
2018-12-16 22:34:12 -0500

    test: update require fs constants in test-process-constants-noatime.js

--- test/parallel/test-process-constants-noatime.js
+++ test/parallel/test-process-constants-noatime.js
@@ -1,14 +1,12 @@
-// Flags: --expose-internals
 'use strict';
 
 const common = require('../common');
 const assert = require('assert');
-const { internalBinding } = require('internal/test/binding');
-const constants = internalBinding('constants');
+const constants = require('fs').constants;
 
 if (common.isLinux) {
-  assert('O_NOATIME' in constants.fs);
-  assert.strictEqual(constants.fs.O_NOATIME, 0x40000);
+  assert('O_NOATIME' in constants);
+  assert.strictEqual(constants.O_NOATIME, 0x40000);
 } else {
-  assert(!('O_NOATIME' in constants.fs));
+  assert(!('O_NOATIME' in constants));
 }
