62689e49aa137965900c493a23b703d4da0ba403
Joyee Cheung <joyeec9h3@gmail.com>
2018-12-19 20:33:20 +0800

    src: move symbols binding into node_symbols.cc

---
+++ src/node_symbols.cc
@@ -0,0 +1,29 @@
+#include "env-inl.h"
+#include "node_binding.h"
+
+namespace node {
+
+using v8::Context;
+using v8::Local;
+using v8::Object;
+using v8::Value;
+
+namespace symbols {
+
+static void Initialize(Local<Object> target,
+                       Local<Value> unused,
+                       Local<Context> context,
+                       void* priv) {
+  Environment* env = Environment::GetCurrent(context);
+#define V(PropertyName, StringValue)                                           \
+  target                                                                       \
+      ->Set(env->context(), env->PropertyName()->Name(), env->PropertyName())  \
+      .FromJust();
+  PER_ISOLATE_SYMBOL_PROPERTIES(V)
+#undef V
+}
+
+}  // namespace symbols
+}  // namespace node
+
+NODE_MODULE_CONTEXT_AWARE_INTERNAL(symbols, node::symbols::Initialize)
