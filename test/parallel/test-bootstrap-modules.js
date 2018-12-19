7108077d52ffcd4194396ffd139995070cdc5a4f
Joyee Cheung <joyeec9h3@gmail.com>
2018-12-19 09:53:12 +0800

    src: move more process methods initialization in bootstrap/node.js
    Instead of:
    
    - Writing methods onto the process directly in C++ during
      `SetupProcessObject()` and overwrite with argument checks later
    - Or, wrapping and writing them in `internal/process/*.js`
    
    Do:
    
    - Move the C++ implementations in node_process.cc and mark them static
      wherever possible
    - Expose the C++ methods through a new
      `internalBinding('process_methods')`
    - Wrap the methods in `internal/process/*.js` in a
      side-effect-free manner and return them back to
      `internal/bootstrap/node.js`
    - Centralize the write to the process object based on conditions
      in `bootstrap/node.js`
    
    So it's easier to see what methods are attached to the process object
    during bootstrap under what condition and in what order.
    
    The eventual goal is to figure out the dependency of process methods
    and the write/read access to the process object during bootstrap, group
    these access properly and remove the process properties that should not
    be exposed to users this way.
    
    Also correct the NODE_PERFORMANCE_MILESTONE_BOOTSTRAP_COMPLETE milestone
    which should be marked before code execution.
    
    Refs: https://github.com/nodejs/node/issues/24961

--- test/parallel/test-bootstrap-modules.js
+++ test/parallel/test-bootstrap-modules.js
@@ -9,7 +9,7 @@ const common = require('../common');
 const assert = require('assert');
 
 const isMainThread = common.isMainThread;
-const kMaxModuleCount = isMainThread ? 60 : 82;
+const kMaxModuleCount = isMainThread ? 61 : 83;
 
 assert(list.length <= kMaxModuleCount,
        `Total length: ${list.length}\n` + list.join('\n')
