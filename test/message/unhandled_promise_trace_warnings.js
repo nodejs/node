43295201c33f6dded887e69d238862426401cc68
Rich Trott <rtrott@gmail.com>
2018-12-20 18:21:56 -0800

    test: use common.relaunchWithFlags() for --trace-warnings

--- test/message/unhandled_promise_trace_warnings.js
+++ test/message/unhandled_promise_trace_warnings.js
@@ -1,6 +1,7 @@
-// Flags: --trace-warnings
 'use strict';
 const common = require('../common');
+if (!process.execArgv.includes('--trace-warnings'))
+  common.relaunchWithFlags(['--trace-warnings']);
 common.disableCrashOnUnhandledRejection();
 const p = Promise.reject(new Error('This was rejected'));
 setImmediate(() => p.catch(() => {}));
