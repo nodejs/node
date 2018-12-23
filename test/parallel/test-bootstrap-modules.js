456b1b55b12dc583d2b4a5b3fc62a476399c9069
Joyee Cheung <joyeec9h3@gmail.com>
2018-12-23 11:42:28 +0800

    process: move eval and exception bootstrap ito process/execution.js
    This patch:
    
    - Moves `tryGetCwd`, `evalScript` and `fatalException` from
      `bootstrap/node.js` into `process/execution.js` so that
      they do have to be passed into the worker thread
      setup function, instead the worker code can require them
      when necessary.
    - Moves `setUncaughtExceptionCaptureCallback` and
      `hasUncaughtExceptionCaptureCallback` along with the two
      global state `exceptionHandlerState` and
      `shouldAbortOnUncaughtToggle` info `process.execution.js`
      as those are only used by the fatalException and these
      two accessors as one self-contained unit.
    
    PR-URL: https://github.com/nodejs/node/pull/25199
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-bootstrap-modules.js
+++ test/parallel/test-bootstrap-modules.js
@@ -9,7 +9,7 @@ const common = require('../common');
 const assert = require('assert');
 
 const isMainThread = common.isMainThread;
-const kMaxModuleCount = isMainThread ? 63 : 83;
+const kMaxModuleCount = isMainThread ? 64 : 84;
 
 assert(list.length <= kMaxModuleCount,
        `Total length: ${list.length}\n` + list.join('\n')
