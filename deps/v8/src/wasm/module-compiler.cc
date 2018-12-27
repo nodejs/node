5a6353fb380179c5f1013dc5caee6a94b14f30bf
Ali Ijaz Sheikh <ofrobots@google.com>
2018-12-27 15:41:08 -0800

    deps: V8: backport 442977e
    Original commit message:
    
      Merged: [wasm] Fix dispatch table instance update
    
      This CL fixes a bug where the receiving instance was updated improperly
      in the dispatch table(s) of an imported table.
    
      BUG=chromium:875322
      R=​mstarzinger@chromium.org
      CC=titzer@chromium.org
    
      Change-Id: Iff24953a1fb6a8ab794e12a7a976d544b56fc3c2
      Originally-reviewed-on: https://chromium-review.googlesource.com/1196886
      No-Try: true
      No-Presubmit: true
      No-Treechecks: true
      Reviewed-on: https://chromium-review.googlesource.com/1212922
      Reviewed-by: Michael Starzinger <mstarzinger@chromium.org>
      Commit-Queue: Clemens Hammacher <clemensh@chromium.org>
      Cr-Commit-Position: refs/branch-heads/6.9@{#45}
      Cr-Branched-From: d7b61abe7b48928aed739f02bf7695732d359e7e-refs/heads/6.9.427@{#1}
      Cr-Branched-From: b7e108d6016bf6b7de3a34e6d61cb522f5193460-refs/heads/master@{#54504}
    
    Refs: https://github.com/v8/v8/commit/442977e1ae21e3df4e7cc5cc880f728dcc0c2711

--- deps/v8/src/wasm/module-compiler.cc
+++ deps/v8/src/wasm/module-compiler.cc
@@ -2653,20 +2653,20 @@ void InstanceBuilder::LoadTableSegments(Handle<WasmInstanceObject> instance) {
 
         // Update the local dispatch table first.
         uint32_t sig_id = module_->signature_ids[function->sig_index];
-        WasmInstanceObject* target_instance = *instance;
+        Handle<WasmInstanceObject> target_instance = instance;
         Address call_target;
         const bool is_import = func_index < module_->num_imported_functions;
         if (is_import) {
           // For imported calls, take target instance and address from the
           // import table.
           ImportedFunctionEntry entry(instance, func_index);
-          target_instance = entry.instance();
+          target_instance = handle(entry.instance(), isolate_);
           call_target = entry.target();
         } else {
           call_target = native_module->GetCallTargetForFunction(func_index);
         }
         IndirectFunctionTableEntry(instance, table_index)
-            .set(sig_id, target_instance, call_target);
+            .set(sig_id, *target_instance, call_target);
 
         if (!table_instance.table_object.is_null()) {
           // Update the table object's other dispatch tables.
@@ -2700,17 +2700,16 @@ void InstanceBuilder::LoadTableSegments(Handle<WasmInstanceObject> instance) {
           }
           table_instance.js_wrappers->set(table_index,
                                           *js_wrappers_[func_index]);
-          // UpdateDispatchTables() should update this instance as well.
+          // UpdateDispatchTables() updates all other dispatch tables, since
+          // we have not yet added the dispatch table we are currently building.
           WasmTableObject::UpdateDispatchTables(
               isolate_, table_instance.table_object, table_index, function->sig,
-              instance, call_target);
+              target_instance, call_target);
         }
       }
     }
 
-    // TODO(titzer): we add the new dispatch table at the end to avoid
-    // redundant work and also because the new instance is not yet fully
-    // initialized.
+    // Add the new dispatch table at the end to avoid redundant lookups.
     if (!table_instance.table_object.is_null()) {
       // Add the new dispatch table to the WebAssembly.Table object.
       WasmTableObject::AddDispatchTable(isolate_, table_instance.table_object,
