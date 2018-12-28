43fa5560ed8242c0d91ab05c947bc897eed2cacc
Ruben Bridgewater <ruben@bridgewater.de>
2018-12-28 14:25:39 +0100

    lib: remove internal util.inherits() usage
    It is recommended not to use this function anymore. Therefore all
    internal usage is removed and switched to `Object.setPrototypeOf()`.

--- test/parallel/test-net-access-byteswritten.js
+++ test/parallel/test-net-access-byteswritten.js
@@ -12,8 +12,10 @@ const tty = require('tty');
 // Check that the bytesWritten getter doesn't crash if object isn't
 // constructed.
 assert.strictEqual(net.Socket.prototype.bytesWritten, undefined);
-assert.strictEqual(tls.TLSSocket.super_.prototype.bytesWritten, undefined);
+assert.strictEqual(Object.getPrototypeOf(tls.TLSSocket).prototype.bytesWritten,
+                   undefined);
 assert.strictEqual(tls.TLSSocket.prototype.bytesWritten, undefined);
-assert.strictEqual(tty.ReadStream.super_.prototype.bytesWritten, undefined);
+assert.strictEqual(Object.getPrototypeOf(tty.ReadStream).prototype.bytesWritten,
+                   undefined);
 assert.strictEqual(tty.ReadStream.prototype.bytesWritten, undefined);
 assert.strictEqual(tty.WriteStream.prototype.bytesWritten, undefined);
