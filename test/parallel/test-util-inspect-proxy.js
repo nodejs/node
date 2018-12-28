944ec835a7d26b9f9dd08d85f38c13ae39fd69e1
Weijia Wang <starkwang@126.com>
2018-12-28 10:03:00 +0800

    util: fix util.inspect with proxied function

--- test/parallel/test-util-inspect-proxy.js
+++ test/parallel/test-util-inspect-proxy.js
@@ -87,3 +87,17 @@ assert.strictEqual(util.inspect(proxy8, opts), expected8);
 assert.strictEqual(util.inspect(proxy9, opts), expected9);
 assert.strictEqual(util.inspect(proxy8), '[Function: Date]');
 assert.strictEqual(util.inspect(proxy9), '[Function: Date]');
+
+const proxy10 = new Proxy(() => {}, {});
+const proxy11 = new Proxy(() => {}, {
+  get() {
+    return proxy11;
+  },
+  apply() {
+    return proxy11;
+  }
+});
+const expected10 = '[Function]';
+const expected11 = '[Function]';
+assert.strictEqual(util.inspect(proxy10), expected10);
+assert.strictEqual(util.inspect(proxy11), expected11);
