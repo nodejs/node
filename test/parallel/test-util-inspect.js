ee8a4a291dbf54184b41c1644caede013c077efe
cjihrig <cjihrig@gmail.com>
2018-12-29 18:14:20 -0500

    test: remove unnecessary test flags
    This commit removes unnecessary flags used when starting
    tests via the "// Flags:" directive.
    
    PR-URL: https://github.com/nodejs/node/pull/25277
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-util-inspect.js
+++ test/parallel/test-util-inspect.js
@@ -19,8 +19,6 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-// Flags: --expose-internals
 'use strict';
 const common = require('../common');
 const assert = require('assert');
