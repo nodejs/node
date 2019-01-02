c6dbe1a709deaf0fea5f22c904613fd88706e989
Anna Henningsen <anna@addaleax.net>
2019-01-02 16:04:48 +0100

    fixup! worker: use engine-provided deleter for `SharedArrayBuffer`s

--- test/parallel/test-worker-message-port-wasm-threads.js
+++ test/parallel/test-worker-message-port-wasm-threads.js
@@ -2,7 +2,7 @@
 'use strict';
 const common = require('../common');
 const assert = require('assert');
-const { MessageChannel } = require('worker_threads');
+const { MessageChannel, Worker } = require('worker_threads');
 
 // Test that SharedArrayBuffer instances created from WASM are transferrable
 // through MessageChannels (without crashing).
@@ -15,12 +15,33 @@ const instance = new WebAssembly.Instance(wasmModule);
 const { buffer } = instance.exports.memory;
 assert(buffer instanceof SharedArrayBuffer);
 
-const { port1, port2 } = new MessageChannel();
-port1.postMessage(buffer);
-port2.once('message', common.mustCall((buffer2) => {
-  // Make sure serialized + deserialized buffer refer to the same memory.
-  const expected = 'Hello, world!';
-  const bytes = Buffer.from(buffer).write(expected);
-  const deserialized = Buffer.from(buffer2).toString('utf8', 0, bytes);
-  assert.deepStrictEqual(deserialized, expected);
-}));
+{
+  const { port1, port2 } = new MessageChannel();
+  port1.postMessage(buffer);
+  port2.once('message', common.mustCall((buffer2) => {
+    // Make sure serialized + deserialized buffer refer to the same memory.
+    const expected = 'Hello, world!';
+    const bytes = Buffer.from(buffer).write(expected);
+    const deserialized = Buffer.from(buffer2).toString('utf8', 0, bytes);
+    assert.deepStrictEqual(deserialized, expected);
+  }));
+}
+
+{
+  // Make sure we can free WASM memory originating from a thread that already
+  // stopped when we exit.
+  const worker = new Worker(`
+  const { parentPort } = require('worker_threads');
+  const fixtures = require('${__dirname}/../common/fixtures');
+  const wasmSource = fixtures.readSync('wasm-threads-shared-memory.wasm');
+  const wasmModule = new WebAssembly.Module(wasmSource);
+  const instance = new WebAssembly.Instance(wasmModule);
+  parentPort.postMessage(instance.exports.memory);
+  `, { eval: true });
+  worker.once('message', common.mustCall(({ buffer }) => {
+    assert(buffer instanceof SharedArrayBuffer);
+    worker.buf = buffer; // Basically just keep the reference to buffer alive.
+  }));
+  worker.once('exit', common.mustCall());
+  worker.postMessage({ wasmModule });
+}
