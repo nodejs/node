f6f7dbce0c10a2a7aefe816dd1ac83a97680decc
Rich Trott <rtrott@gmail.com>
2019-01-05 16:21:00 -0800

    test: tune test-uv-threadpool-schedule
    test-uv-threadpool-schedule has been failing consistently in
    node-daily-master CI. It also fails consistently on my personal laptop.
    These changes make it pass consistently with current master but fail
    consistently with Node.js 10.11 (which was the last release in the 10.x
    line before the fix that the test is for). It succeeds/fails as expected
    whether or not I am connected to the network.
    
    Fixes: https://github.com/nodejs/node/issues/25305
    
    PR-URL: https://github.com/nodejs/node/pull/25358
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>

--- test/internet/test-uv-threadpool-schedule.js
+++ test/internet/test-uv-threadpool-schedule.js
@@ -31,13 +31,13 @@ function onResolve() {
               'fast I/O was throttled due to threadpool congestion.');
 
     // More realistic expectation: finish disc I/O at least within
-    // a time duration that is 1/100th of net I/O.
+    // a time duration that is half of net I/O.
     // Ideally the slow I/O should not affect the fast I/O as those
     // have two different thread-pool buckets. However, this could be
     // highly load / platform dependent, so don't be very greedy.
     const fastIOtime = fastIOend - start;
     const slowIOtime = slowIOend - start;
-    const expectedMax = slowIOtime / 100;
+    const expectedMax = slowIOtime / 2;
     assert.ok(fastIOtime < expectedMax,
               'fast I/O took longer to complete, ' +
               `actual: ${fastIOtime}, expected: ${expectedMax}`);
