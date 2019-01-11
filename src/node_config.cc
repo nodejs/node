7aeec973072330a059c81c236f2cf59c33c15898
Joyee Cheung <joyeec9h3@gmail.com>
2019-01-11 22:27:13 +0800

    Revert "lib: remove internalBinding('config').pendingDeprecation"
    This reverts commit 617bfe48de5e0b3ba7852a45d075c075e8ba6064.

--- src/node_config.cc
+++ src/node_config.cc
@@ -77,6 +77,9 @@ static void Initialize(Local<Object> target,
   if (env->options()->experimental_repl_await)
     READONLY_TRUE_PROPERTY(target, "experimentalREPLAwait");
 
+  if (env->options()->pending_deprecation)
+    READONLY_TRUE_PROPERTY(target, "pendingDeprecation");
+
   if (env->options()->expose_internals)
     READONLY_TRUE_PROPERTY(target, "exposeInternals");
 
