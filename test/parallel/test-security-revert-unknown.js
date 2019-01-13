7ee7565d62ae1109b1f5e427431d159b1390179a
Anna Henningsen <anna@addaleax.net>
2019-01-13 15:39:51 +0100

    fixup! src: pass along errors from `--security-reverts`

--- test/parallel/test-security-revert-unknown.js
+++ test/parallel/test-security-revert-unknown.js
@@ -2,6 +2,7 @@
 require('../common');
 const assert = require('assert');
 const { spawnSync } = require('child_process');
+const os = require('os');
 
 const { signal, status, output } =
   spawnSync(process.execPath, ['--security-reverts=not-a-cve']);
@@ -10,4 +11,4 @@ assert.strictEqual(status, 12);
 assert.strictEqual(
   output[2].toString(),
   `${process.execPath}: Error: ` +
-  'Attempt to revert an unknown CVE [not-a-cve]\n');
+  `Attempt to revert an unknown CVE [not-a-cve]${os.EOL}`);
