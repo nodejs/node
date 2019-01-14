c1004ad6e4e29120a112cebd390a58733573d95a
Julien Gilli <jgilli@netflix.com>
2019-01-14 13:15:19 -0800

    changes according to latest code review

--- test/parallel/test-http-parser-reuse-domain.js
+++ test/parallel/test-http-parser-reuse-domain.js
@@ -10,8 +10,20 @@ const agent = new http.Agent({
   keepAlive: true
 });
 
+const reqSockets = new Set();
+
 function performHttpRequest(opts, cb) {
   const req = http.get({ port: server.address().port, agent }, (res) => {
+    if (!req.socket) {
+      console.log('missing socket property on req object, failing test');
+      markTestAsFailed();
+      req.abort();
+      cb(new Error('req.socket missing'));
+      return;
+    }
+
+    reqSockets.add(req.socket);
+
     // Consume the data from the response to make sure the 'end' event is
     // emitted.
     res.on('data', function noop() {});
@@ -24,11 +36,13 @@ function performHttpRequest(opts, cb) {
     });
 
     res.on('error', (resErr) => {
-      process.exit(1);
+      console.log('got error on response, failing test:', resErr);
+      markTestAsFailed();
     });
 
   }).on('error', (reqErr) => {
-    process.exit(1);
+    console.log('got error on request, failing test:', reqErr);
+    markTestAsFailed();
   });
 
   req.end();
@@ -52,6 +66,10 @@ server.listen(0, common.mustCall(function() {
   const d1 = performHttpRequestInNewDomain({
     shouldThrow: false
   }, common.mustCall((firstReqErr) => {
+    if (firstReqErr) {
+      markTestAsFailed();
+      return;
+    }
     // We want to schedule the second request on the next turn of the event
     // loop so that the parser from the first request is actually reused.
     setImmediate(common.mustCall(() => {
@@ -61,13 +79,19 @@ server.listen(0, common.mustCall(function() {
         // Since this request throws before its callback has the chance
         // to be called, we mark the test as failed if this callback is
         // called.
-        process.exit(1);
+        console.log('second request\'s cb called unexpectedly, failing test');
+        markTestAsFailed();
       });
 
       // The second request throws when its response's end event is
       // emitted. So we expect its domain to emit an error event.
       d2.on('error', common.mustCall((d2err) => {
         server.close();
+        if (reqSockets.size !== 1) {
+          console.log('more than 1 socket used for both requests, failing ' +
+            'test');
+          markTestAsFailed();
+        }
       }, 1));
     }));
   }));
@@ -75,6 +99,15 @@ server.listen(0, common.mustCall(function() {
   d1.on('error', (d1err) => {
     // d1 is the domain attached to the first request, which doesn't throw,
     // so we don't expect its error handler to be called.
-    process.exit(1);
+    console.log('first request\'s domain\'s error handler called, ' +
+      'failing test');
+    markTestAsFailed();
   });
 }));
+
+function markTestAsFailed() {
+  if (server) {
+    server.close();
+  }
+  process.exitCode = 1;
+}
