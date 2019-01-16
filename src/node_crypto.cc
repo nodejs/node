b800d297e17253f21bb0b2dce95e72e56f68a4b8
Sam Roberts <vieuxtech@gmail.com>
2019-01-16 11:12:30 -0800

    src: in-source comments and minor TLS cleanups
    Renamed some internal C++ methods and properties for consistency, and
    commented SSL I/O.
    
    - Rename waiting_new_session_ after is_waiting_new_session(), instead of
      using reverse naming (new_session_wait_), and change "waiting" to
      "awaiting".
    - Make TLSWrap::ClearIn() return void, the value is never used.
    - Fix a getTicketKeys() cut-n-paste error. Since it doesn't use the
      arguments, remove them from the js wrapper.
    - Remove call of setTicketKeys(getTicketKeys()), its a no-op.
    
    PR-URL: https://github.com/nodejs/node/pull/25713
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -1475,7 +1475,7 @@ int SSLWrap<Base>::NewSessionCallback(SSL* s, SSL_SESSION* sess) {
       reinterpret_cast<const char*>(session_id),
       session_id_length).ToLocalChecked();
   Local<Value> argv[] = { session, buff };
-  w->new_session_wait_ = true;
+  w->awaiting_new_session_ = true;
   w->MakeCallback(env->onnewsession_string(), arraysize(argv), argv);
 
   return 0;
@@ -2003,6 +2003,7 @@ void SSLWrap<Base>::Renegotiate(const FunctionCallbackInfo<Value>& args) {
 
   ClearErrorOnReturn clear_error_on_return;
 
+  // XXX(sam) Return/throw an error, don't discard the SSL error reason.
   bool yes = SSL_renegotiate(w->ssl_.get()) == 1;
   args.GetReturnValue().Set(yes);
 }
@@ -2036,7 +2037,7 @@ template <class Base>
 void SSLWrap<Base>::NewSessionDone(const FunctionCallbackInfo<Value>& args) {
   Base* w;
   ASSIGN_OR_RETURN_UNWRAP(&w, args.Holder());
-  w->new_session_wait_ = false;
+  w->awaiting_new_session_ = false;
   w->NewSessionDoneCb();
 }
 
