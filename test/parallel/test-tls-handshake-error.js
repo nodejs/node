51ed9ab93ba0e03fa5898c0de0c829b28f67b472
Daniel Bevenius <daniel.bevenius@gmail.com>
2019-01-18 12:14:18 +0100

    squash!: change cipher option to 'no-such-cipher'

--- test/parallel/test-tls-handshake-error.js
+++ test/parallel/test-tls-handshake-error.js
@@ -19,7 +19,7 @@ const server = tls.createServer({
   assert.throws(() => {
     tls.connect({
       port: this.address().port,
-      ciphers: 'missing'
+      ciphers: 'no-such-cipher'
     }, common.mustNotCall());
   }, /no cipher match/i);
 
