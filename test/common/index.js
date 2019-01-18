d1dee495db1451e74f5364c8687148a53ff0b50d
Benjamin Coe <bencoe@gmail.com>
2019-01-18 13:46:49 -0800

    test: switch to native v8 coverage
    PR-URL: https://github.com/nodejs/node/pull/25157
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- test/common/index.js
+++ test/common/index.js
@@ -238,9 +238,6 @@ function platformTimeout(ms) {
   if (process.features.debug)
     ms = multipliers.two * ms;
 
-  if (global.__coverage__)
-    ms = multipliers.four * ms;
-
   if (isAIX)
     return multipliers.two * ms; // default localhost speed is slower on AIX
 
@@ -299,11 +296,7 @@ function leakedGlobals() {
     }
   }
 
-  if (global.__coverage__) {
-    return leaked.filter((varname) => !/^(?:cov_|__cov)/.test(varname));
-  } else {
-    return leaked;
-  }
+  return leaked;
 }
 
 process.on('exit', function() {
