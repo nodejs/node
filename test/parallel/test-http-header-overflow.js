bcf2886a84407028572fd1084242a1c789c056f8
Albert Still <albertjamesstill@gmail.com>
2019-01-21 17:47:32 +1100

    http: return HTTP 431 on HPE_HEADER_OVERFLOW error
    Instead of returning a generic 400 response when the
    max header size is reached, return a 431 Request Header
    Fields Too Large.
    
    This is a semver-major because it changes the HTTP
    status code for requests that trigger the header
    overflow error.
    
    PR-URL: https://github.com/nodejs/node/pull/25605
    Fixes: https://github.com/nodejs/node/issues/25528
    Refs: https://tools.ietf.org/html/rfc6585#section-5
    
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Sakthipriyan Vairamani <thechargingvolcano@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/parallel/test-http-header-overflow.js
@@ -0,0 +1,47 @@
+'use strict';
+const assert = require('assert');
+const { createServer, maxHeaderSize } = require('http');
+const { createConnection } = require('net');
+const { expectsError, mustCall } = require('../common');
+
+const CRLF = '\r\n';
+const DUMMY_HEADER_NAME = 'Cookie: ';
+const DUMMY_HEADER_VALUE = 'a'.repeat(
+  // plus one is to make it 1 byte too big
+  maxHeaderSize - DUMMY_HEADER_NAME.length - (2 * CRLF.length) + 1
+);
+const PAYLOAD_GET = 'GET /blah HTTP/1.1';
+const PAYLOAD = PAYLOAD_GET + CRLF +
+  DUMMY_HEADER_NAME + DUMMY_HEADER_VALUE + CRLF.repeat(2);
+
+const server = createServer();
+
+server.on('connection', mustCall((socket) => {
+  socket.on('error', expectsError({
+    type: Error,
+    message: 'Parse Error',
+    code: 'HPE_HEADER_OVERFLOW',
+    bytesParsed: maxHeaderSize + PAYLOAD_GET.length,
+    rawPacket: Buffer.from(PAYLOAD)
+  }));
+}));
+
+server.listen(0, mustCall(() => {
+  const c = createConnection(server.address().port);
+  let received = '';
+
+  c.on('connect', mustCall(() => {
+    c.write(PAYLOAD);
+  }));
+  c.on('data', mustCall((data) => {
+    received += data.toString();
+  }));
+  c.on('end', mustCall(() => {
+    assert.strictEqual(
+      received,
+      'HTTP/1.1 431 Request Header Fields Too Large\r\n\r\n'
+    );
+    c.end();
+  }));
+  c.on('close', mustCall(() => server.close()));
+}));
