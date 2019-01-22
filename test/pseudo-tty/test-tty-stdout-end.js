573ec5b0229da2fb6263987f1456595b7bac8b09
Anna Henningsen <anna@addaleax.net>
2019-01-22 16:48:45 +0100

    test: remove unused uncaughtException handler
    This has been unused since cbc3ef64ceaf95ec1.
    
    Refs: https://github.com/nodejs/node/pull/23053
    
    PR-URL: https://github.com/nodejs/node/pull/25641
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/pseudo-tty/test-tty-stdout-end.js
+++ test/pseudo-tty/test-tty-stdout-end.js
@@ -1,10 +1,4 @@
 'use strict';
-const common = require('../common');
-
-process.on('uncaughtException', common.expectsError({
-  code: 'ERR_STDOUT_CLOSE',
-  type: Error,
-  message: 'process.stdout cannot be closed'
-}));
+require('../common');
 
 process.stdout.end();
