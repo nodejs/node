399ef4f5369ea3e7a7edf5f8bb09e889e3715ba0
Rich Trott <rtrott@gmail.com>
2019-01-22 19:27:14 -0800

    test: remove common.isOSXMojave
    common.isOSXMojave was added because it was believed that there was a
    bug in macOS Mojave that allowed unprivileged users to bind to
    privileged ports. As it turns out, that was a feature not a bug. It is
    likely to be in all future versions of macOS. Remove isOSXMojave and
    skip appropriate tests based on isOSX.
    
    Refs: https://news.ycombinator.com/item?id=18302380
    PR-URL: https://github.com/nodejs/node/pull/25658
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Beth Griggs <Bethany.Griggs@uk.ibm.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: Minwoo Jung <minwoo@nodesource.com>

--- test/common/index.js
+++ test/common/index.js
@@ -99,8 +99,6 @@ const isOpenBSD = process.platform === 'openbsd';
 const isLinux = process.platform === 'linux';
 const isOSX = process.platform === 'darwin';
 
-const isOSXMojave = isOSX && (os.release().startsWith('18'));
-
 const enoughTestMem = os.totalmem() > 0x70000000; /* 1.75 Gb */
 const cpus = os.cpus();
 const enoughTestCpu = Array.isArray(cpus) &&
@@ -739,7 +737,6 @@ module.exports = {
   isMainThread,
   isOpenBSD,
   isOSX,
-  isOSXMojave,
   isSunOS,
   isWindows,
   localIPv6Hosts,
