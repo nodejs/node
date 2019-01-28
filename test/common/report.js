c64b1aef07dd1f036f1e940a2b104db63406cafc
cjihrig <cjihrig@gmail.com>
2019-01-27 20:09:09 -0500

    test: refactor test/common/report.js
    - Don't unnecessarily require('../common').
    - Eliminate state maintained in tmppath.
    - validate() was being used synchronously, but was actually
      asynchronous. Make it synchronous.
    - Other misc. drive by cleanup.
    
    PR-URL: https://github.com/nodejs/node/pull/25754
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Anto Aravinth <anto.aravinth.cse@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>

--- test/common/report.js
+++ test/common/report.js
@@ -1,43 +1,39 @@
+/* eslint-disable node-core/required-modules */
 'use strict';
-require('../common');
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
-const REPORT_SECTIONS = [ 'header',
-                          'javascriptStack',
-                          'nativeStack',
-                          'javascriptHeap',
-                          'libuv',
-                          'environmentVariables',
-                          'sharedObjects' ];
-
-let tmppath = '';
-
-exports.findReports = (pid, path) => {
+function findReports(pid, dir) {
   // Default filenames are of the form
   // report.<date>.<time>.<pid>.<seq>.json
-  tmppath = path;
   const format = '^report\\.\\d+\\.\\d+\\.' + pid + '\\.\\d+\\.json$';
   const filePattern = new RegExp(format);
-  const files = fs.readdirSync(path);
-  return files.filter((file) => filePattern.test(file));
-};
+  const files = fs.readdirSync(dir);
+  const results = [];
 
-exports.validate = (report, options) => {
-  const jtmp = path.join(tmppath, report);
-  fs.readFile(jtmp, (err, data) => {
-    this.validateContent(data, options);
+  files.forEach((file) => {
+    if (filePattern.test(file))
+      results.push(path.join(dir, file));
   });
-};
 
+  return results;
+}
 
-exports.validateContent = function validateContent(data, options) {
+function validate(report) {
+  const data = fs.readFileSync(report, 'utf8');
+
+  validateContent(data);
+}
+
+function validateContent(data) {
   const report = JSON.parse(data);
-  const comp = Object.keys(report);
 
-  // Check all sections are present
-  REPORT_SECTIONS.forEach((section) => {
-    assert.ok(comp.includes(section));
+  // Verify that all sections are present.
+  ['header', 'javascriptStack', 'nativeStack', 'javascriptHeap',
+   'libuv', 'environmentVariables', 'sharedObjects'].forEach((section) => {
+    assert(report.hasOwnProperty(section));
   });
-};
+}
+
+module.exports = { findReports, validate, validateContent };
