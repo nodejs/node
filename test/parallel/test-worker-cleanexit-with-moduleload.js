722bf77eb0259d074cacbab5d04693cc6dd538fe
Daniel Bevenius <daniel.bevenius@gmail.com>
2019-01-31 08:18:51 +0100

    squash!: fix linter issues

--- test/parallel/test-worker-cleanexit-with-moduleload.js
+++ test/parallel/test-worker-cleanexit-with-moduleload.js
@@ -10,14 +10,14 @@ const common = require('../common');
 
 const { Worker } = require('worker_threads');
 const modules = [ 'fs', 'assert', 'async_hooks', 'buffer', 'child_process',
-  'net', 'http', 'os', 'path', 'v8', 'vm'
+                  'net', 'http', 'os', 'path', 'v8', 'vm'
 ];
 if (common.hasCrypto) {
   modules.push('https');
 }
 
 for (let i = 0; i < 10; i++) {
-  new Worker(`const modules = [${modules.map(m => `'${m}'`)}];` +
+  new Worker(`const modules = [${modules.map((m) => `'${m}'`)}];` +
     'modules.forEach((module) => {' +
     'const m = require(module);' +
     '});', { eval: true });
