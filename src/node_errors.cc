7564e4fda9d51fb5580418db742e6b7f56bf0557
Anna Henningsen <anna@addaleax.net>
2019-02-01 15:38:28 +0100

    src: use struct as arguments to node::Assert
    This just makes the code a bit more obvious (subjectively).

--- src/node_errors.cc
+++ src/node_errors.cc
@@ -166,23 +166,18 @@ void AppendExceptionLine(Environment* env,
   ABORT_NO_BACKTRACE();
 }
 
-[[noreturn]] void Assert(const char* const (*args)[4]) {
-  auto filename = (*args)[0];
-  auto linenum = (*args)[1];
-  auto message = (*args)[2];
-  auto function = (*args)[3];
-
+[[noreturn]] void Assert(const AssertionInfo& info) {
   char name[1024];
   GetHumanReadableProcessName(&name);
 
   fprintf(stderr,
-          "%s: %s:%s:%s%s Assertion `%s' failed.\n",
+          "%s: %s:%u:%s%s Assertion `%s' failed.\n",
           name,
-          filename,
-          linenum,
-          function,
-          *function ? ":" : "",
-          message);
+          info.filename,
+          info.linenum,
+          info.function,
+          *info.function ? ":" : "",
+          info.message);
   fflush(stderr);
 
   Abort();
