7586e6b259f3df1e25fed3171d9bcf9dd03acb98
Brian White <mscdex@mscdex.net>
2019-01-31 20:44:33 -0500

    http: fix error check in `Execute()`
    Refs: https://github.com/nodejs/node/pull/24738
    Fixes: https://github.com/nodejs/node/issues/25858

--- test/parallel/test-http-max-header-size.js
+++ test/parallel/test-http-max-header-size.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const { spawnSync } = require('child_process');
 const http = require('http');
@@ -9,3 +9,16 @@ assert.strictEqual(http.maxHeaderSize, 8 * 1024);
 const child = spawnSync(process.execPath, ['--max-http-header-size=10', '-p',
                                            'http.maxHeaderSize']);
 assert.strictEqual(+child.stdout.toString().trim(), 10);
+
+{
+  const server = http.createServer(common.mustNotCall());
+  server.listen(0, common.mustCall(() => {
+    http.get({
+      port: server.address().port,
+      headers: { foo: 'x'.repeat(http.maxHeaderSize + 1) }
+    }, common.mustCall((res) => {
+      assert.strictEqual(res.statusCode, 400);
+      server.close();
+    }));
+  }));
+}
