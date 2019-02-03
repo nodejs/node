403c84a1cf964a24608a79c2952230270b6d1ca8
Joyee Cheung <joyeec9h3@gmail.com>
2019-02-03 11:47:26 +0800

    src: port --bash-completion to C++
    So that it gets handle earlier and faster during the bootstrap
    process.
    
    Drive-by fixes:
    
    - Remove `[has_eval_string]` and `[ssl_openssl_cert_store]` from
      the completion output
    - Set `kProfProcess` execution mode for `--prof-process` instead
      of `kPrintBashProcess` which is removed in this patch.
    - Append new line to the end of the output of --bash-completion
    
    PR-URL: https://github.com/nodejs/node/pull/25901
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_options.h
+++ src/node_options.h
@@ -254,6 +254,7 @@ namespace options_parser {
 HostPort SplitHostPort(const std::string& arg,
     std::vector<std::string>* errors);
 void GetOptions(const v8::FunctionCallbackInfo<v8::Value>& args);
+std::string GetBashCompletion();
 
 enum OptionType {
   kNoOp,
@@ -437,6 +438,7 @@ class OptionsParser {
   friend class OptionsParser;
 
   friend void GetOptions(const v8::FunctionCallbackInfo<v8::Value>& args);
+  friend std::string GetBashCompletion();
 };
 
 using StringVector = std::vector<std::string>;
