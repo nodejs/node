d481424c83358cfb7991f1d9a995f5da1f1c8b34
Sam Roberts <vieuxtech@gmail.com>
2019-02-13 14:54:07 -0800

    tls: allow enabling the TLS debug trace
    Enable the same trace output that the OpenSSL s_client and s_server
    support with their `-trace` option. This is invaluable when debugging
    reports of TLS bugs as well as when debugging the internal TLS
    implementation.
    
    See:
    - https://github.com/nodejs/node/issues/25383
    - https://github.com/nodejs/node/issues/17936
    - https://github.com/postmanlabs/postman-app-support/issues/5918#issuecomment-465311423

--- src/tls_wrap.h
+++ src/tls_wrap.h
@@ -160,6 +160,7 @@ class TLSWrap : public AsyncWrap,
   static void SetVerifyMode(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void EnableSessionCallbacks(
       const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void EnableTrace(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void EnableCertCb(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void DestroySSL(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void GetServername(const v8::FunctionCallbackInfo<v8::Value>& args);
