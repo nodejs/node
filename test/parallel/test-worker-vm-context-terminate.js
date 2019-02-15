5d74e49b8bc712828442036fc72d31fd34b7cac7
Anna Henningsen <anna@addaleax.net>
2019-02-15 11:21:56 +0100

    fixup! vm: do not overwrite error when creating context

--- test/parallel/test-worker-vm-context-terminate.js
+++ test/parallel/test-worker-vm-context-terminate.js
@@ -8,7 +8,9 @@ const { Worker } = require('worker_threads');
 if (!process.env.HAS_STARTED_WORKER) {
   process.env.HAS_STARTED_WORKER = 1;
   const w = new Worker(__filename);
-  setTimeout(() => w.terminate(), 50);
+  w.on('online', common.mustCall(() => {
+    setTimeout(() => w.terminate(), 50);
+  }));
   w.on('error', common.mustNotCall());
   w.on('exit', common.mustCall((code) => assert.strictEqual(code, 1)));
 } else {
