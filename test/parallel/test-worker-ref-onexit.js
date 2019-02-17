441b5453a0fd18db08d8391d7ae14e606a4ac57f
Anna Henningsen <anna@addaleax.net>
2019-02-17 19:06:24 +0100

    test: fix flaky test-worker-ref-onexit
    Fixes: https://github.com/nodejs/node/issues/26167
    
    PR-URL: https://github.com/nodejs/node/pull/26170
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Beth Griggs <Bethany.Griggs@uk.ibm.com>

--- test/parallel/test-worker-ref-onexit.js
+++ test/parallel/test-worker-ref-onexit.js
@@ -5,6 +5,8 @@ const { Worker } = require('worker_threads');
 // Check that worker.unref() makes the 'exit' event not be emitted, if it is
 // the only thing we would otherwise be waiting for.
 
-const w = new Worker('', { eval: true });
+// Use `setInterval()` to make sure the worker is alive until the end of the
+// event loop turn.
+const w = new Worker('setInterval(() => {}, 100);', { eval: true });
 w.unref();
 w.on('exit', common.mustNotCall());
