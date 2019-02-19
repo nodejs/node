c3c35d9d28554ade4d8d638be31732c11de0ab7f
Julien Gilli <jgilli@netflix.com>
2019-02-19 14:29:31 -0800

    domain: error handler runs outside of its domain
    Before this change, domains' error handlers would run with the
    corresponding domain as the active domain. This creates the
    possibility for domains' error handlers to call themselves recursively
    if an event emitter created in the error handler emits an error, or if
    the error handler throws an error.
    
    This change sets the active domain to be the domain's parent (or null
    if the domain for which the error handler is called has no parent) to
    prevent that from happening.
    
    Fixes: https://github.com/nodejs/node/issues/26086

--- test/parallel/test-domain-top-level-error-handler-clears-stack.js
+++ test/parallel/test-domain-top-level-error-handler-clears-stack.js
@@ -10,19 +10,18 @@ const domain = require('domain');
 const d = domain.create();
 
 d.on('error', common.mustCall(() => {
+  // Scheduling a callback with process.nextTick _could_ enter a _new_ domain,
+  // but domain's error handlers are called outside of their domain's context.
+  // So there should _no_ domain on the domains stack if the domains stack was
+  // cleared properly when the domain error handler was called.
   process.nextTick(() => {
-    // Scheduling a callback with process.nextTick will enter a _new_ domain,
-    // and the callback will be called after the domain that handled the error
-    // was exited. So there should be only one domain on the domains stack if
-    // the domains stack was cleared properly when the domain error handler
-    // returned.
-    if (domain._stack.length !== 1) {
+    if (domain._stack.length !== 0) {
       // Do not use assert to perform this test: this callback runs in a
       // different callstack as the original process._fatalException that
       // handled the original error, thus throwing here would trigger another
       // call to process._fatalException, and so on recursively and
       // indefinitely.
-      console.error('domains stack length should be 1, but instead is:',
+      console.error('domains stack length should be 0, but instead is:',
                     domain._stack.length);
       process.exit(1);
     }
