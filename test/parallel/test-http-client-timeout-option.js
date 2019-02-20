907941d48e48a30b40a2ee73da4ccd974019fb6e
cjihrig <cjihrig@gmail.com>
2019-02-20 10:02:46 -0500

    http: validate timeout in ClientRequest()
    Validate the timeout option in the ClientRequest() constructor
    to prevent asynchronously thrown validation errors.
    
    PR-URL: https://github.com/nodejs/node/pull/26214
    Fixes: https://github.com/nodejs/node/issues/26143
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Wyatt Preul <wpreul@gmail.com>
    Reviewed-By: Ali Ijaz Sheikh <ofrobots@google.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/parallel/test-http-client-timeout-option.js
+++ test/parallel/test-http-client-timeout-option.js
@@ -3,6 +3,10 @@ const common = require('../common');
 const assert = require('assert');
 const http = require('http');
 
+assert.throws(() => {
+  http.request({ timeout: null });
+}, /The "timeout" argument must be of type number/);
+
 const options = {
   method: 'GET',
   port: undefined,
