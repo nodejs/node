cae7232e7aa82ad57c81b55b0f8852ea3e70c5b3
cjihrig <cjihrig@gmail.com>
2019-02-22 15:04:18 -0500

    test: consolidate triggerReport() tests
    PR-URL: https://github.com/nodejs/node/pull/26268
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/node-report/test-api-nohooks.js
+++ test/node-report/test-api-nohooks.js
@@ -5,6 +5,8 @@
 const common = require('../common');
 common.skipIfReportDisabled();
 const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
 const helper = require('../common/report');
 const tmpdir = require('../common/tmpdir');
 
@@ -13,7 +15,55 @@ common.expectWarning('ExperimentalWarning',
                      'change at any time');
 tmpdir.refresh();
 process.report.setOptions({ path: tmpdir.path });
-process.report.triggerReport();
-const reports = helper.findReports(process.pid, tmpdir.path);
-assert.strictEqual(reports.length, 1);
-helper.validate(reports[0]);
+
+function validate() {
+  const reports = helper.findReports(process.pid, tmpdir.path);
+  assert.strictEqual(reports.length, 1);
+  helper.validate(reports[0]);
+  fs.unlinkSync(reports[0]);
+  return reports[0];
+}
+
+{
+  // Test with no arguments.
+  process.report.triggerReport();
+  validate();
+}
+
+{
+  // Test with an error argument.
+  process.report.triggerReport(new Error('test error'));
+  validate();
+}
+
+{
+  // Test with a file argument.
+  const file = process.report.triggerReport('custom-name-1.json');
+  const absolutePath = path.join(tmpdir.path, file);
+  assert.strictEqual(helper.findReports(process.pid, tmpdir.path).length, 0);
+  assert.strictEqual(file, 'custom-name-1.json');
+  helper.validate(absolutePath);
+  fs.unlinkSync(absolutePath);
+}
+
+{
+  // Test with file and error arguments.
+  const file = process.report.triggerReport('custom-name-2.json',
+                                            new Error('test error'));
+  const absolutePath = path.join(tmpdir.path, file);
+  assert.strictEqual(helper.findReports(process.pid, tmpdir.path).length, 0);
+  assert.strictEqual(file, 'custom-name-2.json');
+  helper.validate(absolutePath);
+  fs.unlinkSync(absolutePath);
+}
+
+{
+  // Test with a filename option.
+  const filename = path.join(tmpdir.path, 'custom-name-3.json');
+  process.report.setOptions({ filename });
+  const file = process.report.triggerReport();
+  assert.strictEqual(helper.findReports(process.pid, tmpdir.path).length, 0);
+  assert.strictEqual(file, filename);
+  helper.validate(filename);
+  fs.unlinkSync(filename);
+}
