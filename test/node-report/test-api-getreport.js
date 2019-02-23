9d44950539e51461ede6fc04b63fb97d033f5468
cjihrig <cjihrig@gmail.com>
2019-02-23 09:52:03 -0500

    test: increase getReport() coverage
    PR-URL: https://github.com/nodejs/node/pull/26276
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/node-report/test-api-getreport.js
+++ test/node-report/test-api-getreport.js
@@ -8,5 +8,22 @@ const helper = require('../common/report');
 common.expectWarning('ExperimentalWarning',
                      'report is an experimental feature. This feature could ' +
                      'change at any time');
-helper.validateContent(process.report.getReport());
-assert.deepStrictEqual(helper.findReports(process.pid, process.cwd()), []);
+
+{
+  // Test with no arguments.
+  helper.validateContent(process.report.getReport());
+  assert.deepStrictEqual(helper.findReports(process.pid, process.cwd()), []);
+}
+
+{
+  // Test with an error argument.
+  helper.validateContent(process.report.getReport(new Error('test error')));
+  assert.deepStrictEqual(helper.findReports(process.pid, process.cwd()), []);
+}
+
+// Test with an invalid error argument.
+[null, 1, Symbol(), function() {}, 'foo'].forEach((error) => {
+  common.expectsError(() => {
+    process.report.getReport(error);
+  }, { code: 'ERR_INVALID_ARG_TYPE' });
+});
