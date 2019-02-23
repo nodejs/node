5109c4f432ab20e3a03d671564a610f752446efd
Shigeki Ohtsu <ohtsu@ohtsu.org>
2019-02-24 02:47:25 +0900

    deps: add ARM64 Windows support in openssl
    This adds ARM64 Windows support in the OpenSSL build system.
    
    Since OpenSSL's ARM64 Windows support does not have support for ASM--
    that is, VC-WIN64-ARM inherits from VC-noCE-common which has no ASM
    files--`openssl_no_asm.gypi` is always used for building. This
    essentially forces the 'no-asm' Configure flag.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/27419
    PR-URL: https://github.com/nodejs/node/pull/26001
    Fixes: https://github.com/nodejs/node/issues/25998
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>
    Signed-off-by: Beth Griggs <Bethany.Griggs@uk.ibm.com>

--- deps/openssl/config/dso_conf_no-asm.h
+++ deps/openssl/config/dso_conf_no-asm.h
@@ -21,6 +21,8 @@
 # include "./archs/VC-WIN32/no-asm/crypto/include/internal/dso_conf.h"
 #elif defined(_WIN32) && defined(_M_X64)
 # include "./archs/VC-WIN64A/no-asm/crypto/include/internal/dso_conf.h"
+#elif defined(_WIN32) && defined(_M_ARM64)
+# include "./archs/VC-WIN64-ARM/no-asm/crypto/include/internal/dso_conf.h"
 #elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__i386__)
 # include "./archs/BSD-x86/no-asm/crypto/include/internal/dso_conf.h"
 #elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__x86_64__)
