481929653e9b43498ebca6006444795e4bdece6b
Pavel Feldman <pfeldman@chromium.org>
2019-02-25 12:43:13 -0800

    tools: roll inspector_protocol to f67ec5
    Fixes: https://github.com/nodejs/node/issues/25808
    
    PR-URL: https://github.com/nodejs/node/pull/26303
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

---
+++ tools/inspector_protocol/convert_protocol_to_json.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import collections
+import json
+import os.path
+import re
+import sys
+
+import pdl
+
+def main(argv):
+    parser = argparse.ArgumentParser(description=(
+        "Converts from .pdl to .json by invoking the pdl Python module."))
+    parser.add_argument('--map_binary_to_string', type=bool,
+                        help=('If set, binary in the .pdl is mapped to a '
+                              'string in .json. Client code will have to '
+                              'base64 decode the string to get the payload.'))
+    parser.add_argument("pdl_file", help="The .pdl input file to parse.")
+    parser.add_argument("json_file", help="The .json output file write.")
+    args = parser.parse_args(argv)
+    file_name = os.path.normpath(args.pdl_file)
+    input_file = open(file_name, "r")
+    pdl_string = input_file.read()
+    protocol = pdl.loads(pdl_string, file_name, args.map_binary_to_string)
+    input_file.close()
+
+    output_file = open(os.path.normpath(args.json_file), 'wb')
+    json.dump(protocol, output_file, indent=4, separators=(',', ': '))
+    output_file.close()
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
