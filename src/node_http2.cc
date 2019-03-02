e418b4f6504ba008b6c3fa05bcb3246c0d1e447f
gengjiawen <technicalcute@gmail.com>
2019-03-02 21:39:52 +0800

    src: fix if indent in node_http2.cc
    PR-URL: https://github.com/nodejs/node/pull/26396
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Masashi Hirano <shisama07@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -1881,8 +1881,9 @@ Http2Stream::Http2Stream(
 
   // Limit the number of header pairs
   max_header_pairs_ = session->GetMaxHeaderPairs();
-  if (max_header_pairs_ == 0)
-  max_header_pairs_ = DEFAULT_MAX_HEADER_LIST_PAIRS;
+  if (max_header_pairs_ == 0) {
+    max_header_pairs_ = DEFAULT_MAX_HEADER_LIST_PAIRS;
+  }
   current_headers_.reserve(max_header_pairs_);
 
   // Limit the number of header octets
