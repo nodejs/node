eefd0c211e1e86bf7eb419dff02dba5e1d402fac
Anna Henningsen <anna@addaleax.net>
2019-03-07 20:29:25 +0100

    fixup! process: add --pending-deprecation to `process.binding()`

--- test/parallel/test-err-name-deprecation.js
+++ test/parallel/test-err-name-deprecation.js
@@ -5,7 +5,7 @@ const common = require('../common');
 
 common.expectWarning({
   DeprecationWarning: [
-    ['`process.binding()` is deprecated. Please use public APIs instead.',
+    ['process.binding() is deprecated. Please use public APIs instead.',
      'DEP0111'],
     ['Directly calling process.binding(\'uv\').errname(<val>) is being ' +
      'deprecated. Please make sure to use util.getSystemErrorName() instead.',
