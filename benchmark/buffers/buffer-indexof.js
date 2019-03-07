a628ad8cb982da22ffc0c8661f3776da3447a6eb
ZYSzys <zyszys98@gmail.com>
2019-03-07 22:57:39 +0800

    buffer: improve performance of buffer indexof method

--- benchmark/buffers/buffer-indexof.js
+++ benchmark/buffers/buffer-indexof.js
@@ -23,9 +23,15 @@ const searchStrings = [
 
 const bench = common.createBenchmark(main, {
   search: searchStrings,
-  encoding: ['undefined', 'utf8', 'ucs2', 'binary'],
+  encoding: [
+    'utf8', 'utf-8', 'UTF8', 'UTF-8',
+    'ucs2', 'ucs-2', 'UCS2', 'UCS-2',
+    'utf16le', 'utf-16le', 'UTF16LE', 'UTF-16LE',
+    'latin1', 'LATIN1', 'binary', 'BINARY', 'base64', 'BASE64',
+    'ascii', 'ASCII', 'hex', 'HEX', 'undefined',
+  ],
   type: ['buffer', 'string'],
-  n: [100000]
+  n: [1000]
 });
 
 function main({ n, search, encoding, type }) {
@@ -37,7 +43,7 @@ function main({ n, search, encoding, type }) {
     encoding = undefined;
   }
 
-  if (encoding === 'ucs2') {
+  if (/^(ucs-?2|utf-?16le)$/i.test(encoding)) {
     aliceBuffer = Buffer.from(aliceBuffer.toString(), encoding);
   }
 
