c6536b4adbde2c6a131d81e6ebba4de9d4e7dd9b
Ruben Bridgewater <ruben@bridgewater.de>
2019-03-07 11:02:47 +0100

    process: make Symbol.toStringTag writable
    The ecosystem broke by making it non-writable, so this is a good
    intermediate fix.
    
    PR-URL: https://github.com/nodejs/node/pull/26488
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Yuta Hiroto <hello@hiroppy.me>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/es-module/test-esm-process.mjs
+++ test/es-module/test-esm-process.mjs
@@ -4,3 +4,4 @@ import assert from 'assert';
 import process from 'process';
 
 assert.strictEqual(Object.prototype.toString.call(process), '[object process]');
+assert(Object.getOwnPropertyDescriptor(process, Symbol.toStringTag).writable);
