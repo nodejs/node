d8f3ffe151a52fc6a32c39da7f5ef1a113e65a7a
Joyee Cheung <joyeec9h3@gmail.com>
2019-03-13 00:48:00 +0800

    fixup! inspector: patch C++ debug options instead of process._breakFirstLine

--- src/node_options.cc
+++ src/node_options.cc
@@ -569,8 +569,6 @@ void GetOptions(const FunctionCallbackInfo<Value>& args) {
     return env->ThrowError(
         "Should not query options before bootstrapping is done");
   }
-  // Only allows serialization once.
-  CHECK(!env->has_serialized_options());
   env->set_has_serialized_options(true);
 
   Isolate* isolate = env->isolate();
