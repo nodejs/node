173f7cfedd8585e97d9d3f0a11aab540631a9ca5
Nikolai Vavilov <vvnicholas@gmail.com>
2019-03-12 15:49:54 +0200

    merge fixes

--- test/parallel/test-buffer-bigint64.js
+++ test/parallel/test-buffer-bigint64.js
@@ -5,46 +5,46 @@ const assert = require('assert');
 const buf = Buffer.allocUnsafe(8);
 
 ['LE', 'BE'].forEach(function(endianness) {
-  // should allow simple BigInts to be written and read
+  // Should allow simple BigInts to be written and read
   let val = 123456789n;
   buf['writeBigInt64' + endianness](val, 0);
   let rtn = buf['readBigInt64' + endianness](0);
   assert.strictEqual(val, rtn);
 
-  // should allow INT64_MAX to be written and read
+  // Should allow INT64_MAX to be written and read
   val = 0x7fffffffffffffffn;
   buf['writeBigInt64' + endianness](val, 0);
   rtn = buf['readBigInt64' + endianness](0);
   assert.strictEqual(val, rtn);
 
-  // should read and write a negative signed 64-bit integer
+  // Should read and write a negative signed 64-bit integer
   val = -123456789n;
   buf['writeBigInt64' + endianness](val, 0);
   assert.strictEqual(val, buf['readBigInt64' + endianness](0));
 
-  // should read and write an unsigned 64-bit integer
+  // Should read and write an unsigned 64-bit integer
   val = 123456789n;
   buf['writeBigUInt64' + endianness](val, 0);
   assert.strictEqual(val, buf['readBigUInt64' + endianness](0));
 
-  // should throw a RangeError upon INT64_MAX+1 being written
+  // Should throw a RangeError upon INT64_MAX+1 being written
   assert.throws(function() {
     const val = 0x8000000000000000n;
     buf['writeBigInt64' + endianness](val, 0);
   }, RangeError);
 
-  // should throw a RangeError upon UINT64_MAX+1 being written
+  // Should throw a RangeError upon UINT64_MAX+1 being written
   assert.throws(function() {
     const val = 0x10000000000000000n;
     buf['writeBigUInt64' + endianness](val, 0);
   }, RangeError);
 
-  // should throw a TypeError upon invalid input
+  // Should throw a TypeError upon invalid input
   assert.throws(function() {
     buf['writeBigInt64' + endianness]('bad', 0);
   }, TypeError);
 
-  // should throw a TypeError upon invalid input
+  // Should throw a TypeError upon invalid input
   assert.throws(function() {
     buf['writeBigUInt64' + endianness]('bad', 0);
   }, TypeError);
