90e9df31923dcd5b5142cec9f4c6a39f3080c20c
Tobias Nießen <tniessen@tnie.de>
2019-03-13 17:31:03 +0100

    Revert "crypto: add KeyObject.asymmetricKeySize"
    This reverts commit 4895927a0a4372e0699f84657e0a299393a3d281.
    
    Fixes: https://github.com/nodejs/node/issues/26631

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -3329,8 +3329,6 @@ Local<Function> KeyObject::Initialize(Environment* env, Local<Object> target) {
   t->InstanceTemplate()->SetInternalFieldCount(1);
 
   env->SetProtoMethod(t, "init", Init);
-  env->SetProtoMethodNoSideEffect(t, "getAsymmetricKeySize",
-                                  GetAsymmetricKeySize);
   env->SetProtoMethodNoSideEffect(t, "getSymmetricKeySize",
                                   GetSymmetricKeySize);
   env->SetProtoMethodNoSideEffect(t, "getAsymmetricKeyType",
@@ -3374,11 +3372,6 @@ ManagedEVPPKey KeyObject::GetAsymmetricKey() const {
 const char* KeyObject::GetSymmetricKey() const {
   CHECK_EQ(key_type_, kKeyTypeSecret);
   return this->symmetric_key_.get();
-}
-
-size_t KeyObject::GetAsymmetricKeySize() const {
-  CHECK_NE(key_type_, kKeyTypeSecret);
-  return EVP_PKEY_size(this->asymmetric_key_.get());
 }
 
 size_t KeyObject::GetSymmetricKeySize() const {
@@ -3482,12 +3475,6 @@ void KeyObject::GetAsymmetricKeyType(const FunctionCallbackInfo<Value>& args) {
   ASSIGN_OR_RETURN_UNWRAP(&key, args.Holder());
 
   args.GetReturnValue().Set(key->GetAsymmetricKeyType());
-}
-
-void KeyObject::GetAsymmetricKeySize(const FunctionCallbackInfo<Value>& args) {
-  KeyObject* key;
-  ASSIGN_OR_RETURN_UNWRAP(&key, args.Holder());
-  args.GetReturnValue().Set(static_cast<uint32_t>(key->GetAsymmetricKeySize()));
 }
 
 void KeyObject::GetSymmetricKeySize(const FunctionCallbackInfo<Value>& args) {
