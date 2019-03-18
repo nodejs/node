nodejs/ecmascript-modules

esm: scoped --type, cpp refactoring

lib/internal/modules/cjs/loader.js
@@ -860,17 +860,15 @@ Module.runMain = function() {
   // Load the main module--the command line argument.
   if (experimentalModules) {
     if (asyncESM === undefined) lazyLoadESM();
-    if (asyncESM.typeFlag !== 'commonjs') {
-      asyncESM.loaderPromise.then((loader) => {
-        return loader.import(pathToFileURL(process.argv[1]).pathname);
-      })
-      .catch((e) => {
-        internalBinding('util').triggerFatalException(e);
-      });
-      // Handle any nextTicks added in the first tick of the program
-      process._tickCallback();
-      return;
-    }
+    asyncESM.loaderPromise.then((loader) => {
+      return loader.import(pathToFileURL(process.argv[1]).pathname);
+    })
+    .catch((e) => {
+      internalBinding('util').triggerFatalException(e);
+    });
+    // Handle any nextTicks added in the first tick of the program
+    process._tickCallback();
+    return;
   }
   Module._load(process.argv[1], null, true);
   // Handle any nextTicks added in the first tick of the program

src/module_wrap.h
@@ -65,6 +65,7 @@ class ModuleWrap : public BaseObject {
       const v8::FunctionCallbackInfo<v8::Value>& args);
 
   static void Resolve(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void SetScopeType(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetImportModuleDynamicallyCallback(
       const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetInitializeImportMetaObjectCallback(

nodejs/node

Co-authored-by: Myles Borins <mylesborins@google.com>

lib/internal/modules/cjs/loader.js
@@ -866,17 +866,15 @@ Module.runMain = function() {
   // Load the main module--the command line argument.
   if (experimentalModules) {
     if (asyncESM === undefined) lazyLoadESM();
-    if (asyncESM.typeFlag !== 'commonjs') {
-      asyncESM.loaderPromise.then((loader) => {
-        return loader.import(pathToFileURL(process.argv[1]).pathname);
-      })
-      .catch((e) => {
-        internalBinding('task_queue').triggerFatalException(e);
-      });
-      // Handle any nextTicks added in the first tick of the program
-      process._tickCallback();
-      return;
-    }
+    asyncESM.loaderPromise.then((loader) => {
+      return loader.import(pathToFileURL(process.argv[1]).pathname);
+    })
+    .catch((e) => {
+      internalBinding('task_queue').triggerFatalException(e);
+    });
+    // Handle any nextTicks added in the first tick of the program
+    process._tickCallback();
+    return;
   }
   Module._load(process.argv[1], null, true);
   // Handle any nextTicks added in the first tick of the program

src/module_wrap.h
@@ -65,6 +65,7 @@ class ModuleWrap : public BaseObject {
       const v8::FunctionCallbackInfo<v8::Value>& args);
 
   static void Resolve(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void GetPackageType(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetImportModuleDynamicallyCallback(
       const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetInitializeImportMetaObjectCallback(