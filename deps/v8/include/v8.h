71cd94c7a4372125bb37178bf6491c8e3e49ba99
Michaël Zasso <targos@protonmail.com>
2019-03-27 17:57:28 +0100

    deps: V8: silence irrelevant warning
    PR-URL: https://github.com/nodejs/node/pull/26685
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>

--- deps/v8/include/v8.h
+++ deps/v8/include/v8.h
@@ -6712,8 +6712,7 @@ class PromiseRejectMessage {
 typedef void (*PromiseRejectCallback)(PromiseRejectMessage message);
 
 // --- Microtasks Callbacks ---
-V8_DEPRECATE_SOON("Use *WithData version.",
-                  typedef void (*MicrotasksCompletedCallback)(Isolate*));
+typedef void (*MicrotasksCompletedCallback)(Isolate*);
 typedef void (*MicrotasksCompletedCallbackWithData)(Isolate*, void*);
 typedef void (*MicrotaskCallback)(void* data);
 
