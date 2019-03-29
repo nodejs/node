d3fb7e1b3658a6f00e1c631aa551f2ea0ab81f5e
Ruben Bridgewater <ruben@bridgewater.de>
2019-03-29 03:11:41 +0100

    module: mark DEP0019 as End-of-Life
    In certain cases, `require('.')` could resolve outside the package
    directory. This behavior has been removed.

--- test/parallel/test-require-dot.js
+++ test/parallel/test-require-dot.js
@@ -14,9 +14,10 @@ assert.strictEqual(a, b);
 process.env.NODE_PATH = fixtures.path('module-require', 'relative');
 m._initPaths();
 
-const c = require('.');
-assert.strictEqual(
-  c.value,
-  42,
-  `require(".") should honor NODE_PATH; expected 42, found ${c.value}`
+assert.throws(
+  () => require('.'),
+  {
+    message: /Cannot find module '\.'/,
+    code: 'MODULE_NOT_FOUND'
+  }
 );
