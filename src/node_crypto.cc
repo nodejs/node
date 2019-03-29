05bd6071a63077187c86fe163c2fc50825514a80
cjihrig <cjihrig@gmail.com>
2019-03-29 11:55:24 -0400

    crypto: use EVP_PKEY_X448 in GetEphemeralKeyInfo
    PR-URL: https://github.com/nodejs/node/pull/26988
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -2452,8 +2452,8 @@ void SSLWrap<Base>::GetEphemeralKeyInfo(
             .FromJust();
         break;
       case EVP_PKEY_EC:
-      // TODO(shigeki) Add EVP_PKEY_X448.
       case EVP_PKEY_X25519:
+      case EVP_PKEY_X448:
         {
           const char* curve_name;
           if (kid == EVP_PKEY_EC) {
