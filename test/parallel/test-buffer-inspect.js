d834275a48bc1f85e2289bf7e52a5035a4d97f7e
Ruben Bridgewater <ruben@bridgewater.de>
2019-04-03 19:54:37 +0200

    buffer: fix custom inspection with extra properties
    This broke due to a recent change that prevents exposing inspect
    internals. It now relies on the public API instead and should be a
    bit more robust due to that.
    
    PR-URL: https://github.com/nodejs/node/pull/27074
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Yongsheng Zhang <zyszys98@gmail.com>

--- test/parallel/test-buffer-inspect.js
+++ test/parallel/test-buffer-inspect.js
@@ -55,4 +55,16 @@ assert.strictEqual(util.inspect(b), expected);
 assert.strictEqual(util.inspect(s), expected);
 
 b.inspect = undefined;
-assert.strictEqual(util.inspect(b), '<Buffer 31 32, inspect: undefined>');
+b.prop = new Uint8Array(0);
+assert.strictEqual(
+  util.inspect(b),
+  '<Buffer 31 32, inspect: undefined, prop: Uint8Array []>'
+);
+
+b = Buffer.alloc(0);
+b.prop = 123;
+
+assert.strictEqual(
+  util.inspect(b),
+  '<Buffer prop: 123>'
+);
