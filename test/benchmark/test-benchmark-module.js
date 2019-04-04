c86883cfacc927c2433af9554ee103b1e6b00589
Rich Trott <rtrott@gmail.com>
2019-04-04 16:05:49 -0700

    test: fix test-benchmark-module
    A recent commit broke test-benchmark-module. This fixes it.
    
    PR-URL: https://github.com/nodejs/node/pull/27094
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Bryan English <bryan@bryanenglish.com>

--- test/benchmark/test-benchmark-module.js
+++ test/benchmark/test-benchmark-module.js
@@ -5,7 +5,11 @@ require('../common');
 const runBenchmark = require('../common/benchmark');
 
 runBenchmark('module', [
+  'cache=true',
+  'dir=rel',
+  'ext=',
+  'fullPath=true',
   'n=1',
+  'name=/',
   'useCache=true',
-  'fullPath=true'
 ]);
