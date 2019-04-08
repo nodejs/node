44a3acb6272accf6296be521ff2f626514874acc
cjihrig <cjihrig@gmail.com>
2019-04-08 13:23:36 -0400

    report: improve signal name validation
    PR-URL: https://github.com/nodejs/node/pull/27137
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>

--- test/report/test-report-config.js
+++ test/report/test-report-config.js
@@ -68,7 +68,16 @@ if (!common.isWindows) {
   }, { code: 'ERR_INVALID_ARG_TYPE' });
   common.expectsError(() => {
     process.report.signal = 'foo';
-  }, { code: 'ERR_UNKNOWN_SIGNAL' });
+  }, {
+    code: 'ERR_UNKNOWN_SIGNAL',
+    message: 'Unknown signal: foo'
+  });
+  common.expectsError(() => {
+    process.report.signal = 'sigusr1';
+  }, {
+    code: 'ERR_UNKNOWN_SIGNAL',
+    message: 'Unknown signal: sigusr1 (signals must use all capital letters)'
+  });
   assert.strictEqual(process.report.signal, 'SIGUSR2');
   process.report.signal = 'SIGUSR1';
   assert.strictEqual(process.report.signal, 'SIGUSR1');
