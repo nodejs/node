f99c8f3a6d2d3fab5489c78a85e15150502ce81a
Ruben Bridgewater <ruben@bridgewater.de>
2019-04-08 20:50:45 +0200

    fixup! test: add known issue

--- test/known_issues/test-repl-uncaught-exception-error.js
+++ test/known_issues/test-repl-uncaught-exception-error.js
@@ -1,39 +1,61 @@
 'use strict';
 
 require('../common');
-const ArrayStream = require('../common/arraystream');
-const repl = require('repl');
 
 // Adding an `uncaughtException` listener in an REPL instance suppresses errors
 // in the whole application.
 // Closing the instance won't remove those listeners either.
 
-let accum = '';
+if (process.argv[2] === 'child') {
 
-const output = new ArrayStream();
-output.write = (data) => accum += data.replace('\r', '');
+  const ArrayStream = require('../common/arraystream');
+  const repl = require('repl');
 
-const r = repl.start({
-  prompt: '',
-  input: new ArrayStream(),
-  output,
-  terminal: false,
-  useColors: false,
-  global: false
-});
+  let accum = '';
 
-r.write(
-  'setTimeout(() => {\n' +
-  '  process.on("uncaughtException", () => console.log("Foo"));\n' +
-  '}, 1);\n'
+  const output = new ArrayStream();
+  output.write = (data) => accum += data.replace('\r', '');
+
+  const r = repl.start({
+    prompt: '',
+    input: new ArrayStream(),
+    output,
+    terminal: false,
+    useColors: false,
+    global: false
+  });
+
+  r.write(
+    'setTimeout(() => {\n' +
+    '  process.on("uncaughtException", () => console.log("Foo"));\n' +
+    '}, 1);\n'
+  );
+
+  // Event listeners added to the global `process` won't be removed after
+  // closing the REPL instance! Those should be removed again, especially since
+  // the REPL's `global` option is set to `false`.
+  r.close();
+
+  setTimeout(() => {
+    // This should definitely not be silenced!
+    throw new Error('HU');
+  }, 2);
+
+  setTimeout(() => {
+    console.error('FAILED');
+    process.exit(1);
+  }, 10);
+
+  return;
+}
+
+const cp = require('child_process');
+const assert = require('assert');
+
+const res = cp.spawnSync(
+  process.execPath,
+  [__filename, 'child'],
+  { encoding: 'utf8' }
 );
 
-// Event listeners added to the global `process` won't be removed after
-// closing the REPL instance! Those should be removed again, especially since
-// the REPL's `global` option is set to `false`.
-r.close();
-
-setTimeout(() => {
-  // This should definitely not be silenced!
-  throw new Error('HU');
-}, 2);
+assert.notStrictEqual(res.stderr, 'FAILED\n');
