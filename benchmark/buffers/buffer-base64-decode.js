3f8ac4a81252e6aa03f4b15ce3d69b04b36e369f
Rich Trott <rtrott@gmail.com>
2019-04-16 06:42:13 -0700

    benchmark: fix buffer-base64-decode.js
    693401d0ddd752e5fa47b882e56e252c42c94c0e added stricter range checking
    for buffer operations and that apparently seems to have uncovered the
    fact that one of our benchmarks was overflowing a buffer. Increase the
    buffer size so the benchmark doesn't throw an error anymore.

--- benchmark/buffers/buffer-base64-decode.js
+++ benchmark/buffers/buffer-base64-decode.js
@@ -12,7 +12,7 @@ function main({ n, size }) {
   // eslint-disable-next-line node-core/no-unescaped-regexp-dot
   s.match(/./);  // Flatten string.
   assert.strictEqual(s.length % 4, 0);
-  const b = Buffer.allocUnsafe(s.length / 4 * 3);
+  const b = Buffer.allocUnsafe(s.length);
   b.write(s, 0, s.length, 'base64');
   bench.start();
   for (var i = 0; i < n; i += 1) b.base64Write(s, 0, s.length);
