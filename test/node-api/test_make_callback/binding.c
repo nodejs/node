8b5d73867fe3c4be967af3d47fa801a79afca4f8
Anna Henningsen <anna@addaleax.net>
2019-04-16 13:19:40 +0200

    n-api: do not require JS Context for `napi_async_destroy()`
    Allow the function to be called during GC, which is a common use case.
    
    Fixes: https://github.com/nodejs/node/issues/27218
    
    PR-URL: https://github.com/nodejs/node/pull/27255
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/node-api/test_make_callback/binding.c
+++ test/node-api/test_make_callback/binding.c
@@ -1,4 +1,6 @@
+#define NAPI_EXPERIMENTAL  // napi_add_finalizer
 #include <node_api.h>
+#include <assert.h>
 #include "../../js-native-api/common.h"
 
 #define MAX_ARGUMENTS 10
@@ -44,6 +46,30 @@ static napi_value MakeCallback(napi_env env, napi_callback_info info) {
   return result;
 }
 
+static void AsyncDestroyCb(napi_env env, void* data, void* hint) {
+  napi_status status = napi_async_destroy(env, (napi_async_context)data);
+  // We cannot use NAPI_ASSERT_RETURN_VOID because we need to have a JS stack
+  // below in order to use exceptions.
+  assert(status == napi_ok);
+}
+
+static napi_value CreateAsyncResource(napi_env env, napi_callback_info info) {
+  napi_value object;
+  NAPI_CALL(env, napi_create_object(env, &object));
+
+  napi_value resource_name;
+  NAPI_CALL(env, napi_create_string_utf8(
+      env, "test_gcable", NAPI_AUTO_LENGTH, &resource_name));
+
+  napi_async_context context;
+  NAPI_CALL(env, napi_async_init(env, object, resource_name, &context));
+
+  NAPI_CALL(env, napi_add_finalizer(
+      env, object, (void*)context, AsyncDestroyCb, NULL, NULL));
+
+  return object;
+}
+
 static
 napi_value Init(napi_env env, napi_value exports) {
   napi_value fn;
@@ -51,6 +77,11 @@ napi_value Init(napi_env env, napi_value exports) {
       // NOLINTNEXTLINE (readability/null_usage)
       env, NULL, NAPI_AUTO_LENGTH, MakeCallback, NULL, &fn));
   NAPI_CALL(env, napi_set_named_property(env, exports, "makeCallback", fn));
+  NAPI_CALL(env, napi_create_function(
+      // NOLINTNEXTLINE (readability/null_usage)
+      env, NULL, NAPI_AUTO_LENGTH, CreateAsyncResource, NULL, &fn));
+  NAPI_CALL(env, napi_set_named_property(
+      env, exports, "createAsyncResource", fn));
   return exports;
 }
 
