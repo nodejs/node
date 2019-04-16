b72b012f7cb1203b4504f385cefd9c808daaffd1
Rich Trott <rtrott@gmail.com>
2019-04-16 11:30:56 -0700

    fixup! benchmark: fix buffer-base64-decode.js

--- benchmark/buffers/buffer-base64-decode.js
+++ benchmark/buffers/buffer-base64-decode.js
@@ -12,8 +12,8 @@ function main({ n, size }) {
   // eslint-disable-next-line node-core/no-unescaped-regexp-dot
   s.match(/./);  // Flatten string.
   assert.strictEqual(s.length % 4, 0);
-  const b = Buffer.allocUnsafe(s.length);
-  b.write(s, 0, s.length, 'base64');
+  const b = Buffer.allocUnsafe(size * 3);
+  b.write(s, 0, size * 3, 'base64');
   bench.start();
   for (var i = 0; i < n; i += 1) b.base64Write(s, 0, s.length);
   bench.end(n);
