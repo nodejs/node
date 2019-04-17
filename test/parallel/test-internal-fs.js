94b21fc1215531488f90fe39936c37c835741a61
Ruwan Geeganage <rpgeeg@gmail.com>
2019-04-17 02:22:08 +0200

    fs: changed isWindows to a function in utils
    Added tests for preprocessSymlinkDestination

--- test/parallel/test-internal-fs.js
+++ test/parallel/test-internal-fs.js
@@ -1,8 +1,10 @@
 // Flags: --expose-internals
 'use strict';
 
+const assert = require('assert');
 const common = require('../common');
 const fs = require('internal/fs/utils');
+const pathModule = require('path');
 
 // Valid encodings and no args should not throw.
 fs.assertEncoding();
@@ -12,3 +14,42 @@ common.expectsError(
   () => fs.assertEncoding('foo'),
   { code: 'ERR_INVALID_OPT_VALUE_ENCODING', type: TypeError }
 );
+
+{
+  // Enable monkey patching for
+  // 1. path.resolve
+  const originalResolve = pathModule.resolve;
+
+  // 2. path.toNamespacedPath
+  const originalToNamespacedPath = pathModule.toNamespacedPath;
+
+  // 3. process.platform
+  const originalPlatform = process.platform;
+  let platform = null;
+  Object.defineProperty(process, 'platform', { get: () => platform });
+
+  function test(newPlatform, pathString, linkPath) {
+    platform = newPlatform;
+    pathModule.resolve = pathModule[newPlatform].resolve;
+    pathModule.toNamespacedPath = pathModule[newPlatform].toNamespacedPath;
+
+    const preprocessSymlinkDestination = fs.preprocessSymlinkDestination(
+      pathString,
+      'junction',
+      linkPath
+    );
+
+    const constructedPath = pathModule[newPlatform].toNamespacedPath(
+      pathModule[newPlatform].resolve(linkPath, '..', pathString)
+    );
+
+    assert.strictEqual(preprocessSymlinkDestination, constructedPath);
+  }
+
+  test('win32', '\\test2', '\\test1');
+  test('posix', '\\test2', '\\test1');
+
+  platform = originalPlatform;
+  pathModule.resolve = originalResolve;
+  pathModule.toNamespacedPath = originalToNamespacedPath;
+}
