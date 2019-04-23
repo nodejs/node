21b86d7f19486c19f2e821e36b4b0999bf2542c8
Refael Ackermann <refack@gmail.com>
2019-04-23 10:13:17 -0400

    test,v8: skip less and stabilize test-linux-perf.js
    Co-authored-by: Matheus Marchini <mat@mmarchini.me>
    
    PR-URL: https://github.com/nodejs/node/pull/27364
    Refs: https://github.com/nodejs/build/issues/1774
    Reviewed-By: Matheus Marchini <mat@mmarchini.me>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/fixtures/linux-perf.js
+++ test/fixtures/linux-perf.js
@@ -1,26 +1,17 @@
 'use strict';
 
-const crypto = require('crypto');
+const { spawnSync } = require("child_process");
+const sleepTime = new Number(process.argv[2] || "0.1");
+const repeat = new Number(process.argv[3]) || 5;
 
-// Functions should be complex enough for V8 to run them a few times before
-// compiling, but not complex enough to always stay in interpreted mode. They
-// should also take some time to run, otherwise Linux perf might miss them
-// entirely even when sampling at a high frequency.
-function functionOne(i) {
-  for (let j=i; j > 0; j--) {
-    crypto.createHash('md5').update(functionTwo(i, j)).digest("hex");
-  }
+function functionOne() {
+  functionTwo();
 }
 
-function functionTwo(x, y) {
-  let data = ((((x * y) + (x / y)) * y) ** (x + 1)).toString();
-  if (x % 2 == 0) {
-    return crypto.createHash('md5').update(data.repeat((x % 100) + 1)).digest("hex");
-  } else {
-    return crypto.createHash('md5').update(data.repeat((y % 100) + 1)).digest("hex");
-  }
+function functionTwo() {
+  spawnSync('sleep', [`${sleepTime}`]);
 }
 
-for (let i = 0; i < 1000; i++) {
-  functionOne(i);
+for (let i = 0; i < repeat; i++) {
+  functionOne();
 }
