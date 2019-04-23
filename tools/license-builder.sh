fcb98bb25bd201dad9689ba43397544e6964b629
Ujjwal Sharma <usharma1998@gmail.com>
2019-04-23 18:51:42 +0530

    tools: update tools/license-builder.sh
    Update tools/license-builder.sh in order to work normally after jinja2
    and markupsafe were moved from tools/ to tools/inspector_protocol/ in
    an earlier commit.
    
    Refs: https://github.com/nodejs/node/pull/25614
    
    PR-URL: https://github.com/nodejs/node/pull/27362
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>

--- tools/license-builder.sh
+++ tools/license-builder.sh
@@ -75,8 +75,8 @@ addlicense "npm" "deps/npm" "$(cat ${rootdir}/deps/npm/LICENSE)"
 # Build tools
 addlicense "GYP" "tools/gyp" "$(cat ${rootdir}/tools/gyp/LICENSE)"
 addlicense "inspector_protocol" "tools/inspector_protocol" "$(cat ${rootdir}/tools/inspector_protocol/LICENSE)"
-addlicense "jinja2" "tools/jinja2" "$(cat ${rootdir}/tools/jinja2/LICENSE)"
-addlicense "markupsafe" "tools/markupsafe" "$(cat ${rootdir}/tools/markupsafe/LICENSE)"
+addlicense "jinja2" "tools/inspector_protocol/jinja2" "$(cat ${rootdir}/tools/inspector_protocol/jinja2/LICENSE)"
+addlicense "markupsafe" "tools/inspector_protocol/markupsafe" "$(cat ${rootdir}/tools/inspector_protocol/markupsafe/LICENSE)"
 
 # Testing tools
 addlicense "cpplint.py" "tools/cpplint.py" \
