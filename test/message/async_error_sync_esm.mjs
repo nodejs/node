3def28d083125a007d4201779d5f2983ada5fdc6
Joyee Cheung <joyeec9h3@gmail.com>
2019-04-25 03:25:05 +0800

    fixup! process: reduce the number of internal frames in async stack trace

--- test/message/async_error_sync_esm.mjs
+++ test/message/async_error_sync_esm.mjs
@@ -1,6 +1,6 @@
 // Flags: --experimental-modules
 /* eslint-disable node-core/required-modules */
-// import '../common/index.mjs';
+import '../common/index.mjs';
 import four from '../fixtures/async-error.js';
 
 async function main() {
