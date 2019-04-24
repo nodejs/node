7647666eab5456ba6933d98d77fc573e0ecb6726
Ruben Bridgewater <ruben@bridgewater.de>
2019-04-25 01:12:11 +0200

    module: resolve the filename in `createRequireFromPath()`
    This makes sure the filename passed to `createRequireFromPath()` is
    actually resolved. That makes sure the current working directory is
    taken into account. Otherwise a directory change would not be noticed
    by relative paths passed to `createRequireFromPath()`.

--- test/parallel/test-module-create-require.js
+++ test/parallel/test-module-create-require.js
@@ -4,9 +4,23 @@ require('../common');
 const assert = require('assert');
 const path = require('path');
 
+const util = require('util');
+util.inspect.defaultOptions.depth = 4;
+
 const { createRequireFromPath } = require('module');
 
-const p = path.resolve(__dirname, '..', 'fixtures', 'fake.js');
+const p = path.join(process.cwd(), 'test/fixtures/fake.js');
+const p2 = './test/fixtures/fake.js';
+const p3 = './fixtures/fake.js';
 
 const req = createRequireFromPath(p);
+const req2 = createRequireFromPath(p2);
+const req3 = createRequireFromPath(p3);
+process.chdir('./test/');
+const req4 = createRequireFromPath(p3);
+
 assert.strictEqual(req('./baz'), 'perhaps I work');
+assert.strictEqual(req2('./baz'), 'perhaps I work');
+// TODO: req3('./baz') should throw an error. There's some caching involved.
+assert.strictEqual(req3('./baz'), 'perhaps I work');
+assert.strictEqual(req4('./baz'), 'perhaps I work');
