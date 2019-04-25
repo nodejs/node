149412ca02874cdca34aba86495ac8bb383127c7
Anto Aravinth <anto.aravinth.cse@gmail.com>
2019-04-25 20:14:47 +0530

    repl: add autocomplete for filesystem modules
    PR-URL: https://github.com/nodejs/node/pull/26648
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-repl-tab-complete.js
+++ test/parallel/test-repl-tab-complete.js
@@ -397,6 +397,45 @@ testMe.complete('obj.', common.mustCall((error, data) => {
   assert(data[0].includes('obj.key'));
 }));
 
+// Tab completion for files/directories
+{
+  putIn.run(['.clear']);
+  process.chdir(__dirname);
+
+  const readFileSync = 'fs.readFileSync("';
+  const fixturePath = `${readFileSync}../fixtures/test-repl-tab-completion`;
+  if (!common.isWindows) {
+    testMe.complete(fixturePath, common.mustCall((err, data) => {
+      assert.strictEqual(err, null);
+      assert.ok(data[0][0].includes('.hiddenfiles'));
+      assert.ok(data[0][1].includes('hellorandom.txt'));
+      assert.ok(data[0][2].includes('helloworld.js'));
+    }));
+
+    testMe.complete(`${fixturePath}/hello`,
+                    common.mustCall((err, data) => {
+                      assert.strictEqual(err, null);
+                      assert.ok(data[0][0].includes('hellorandom.txt'));
+                      assert.ok(data[0][1].includes('helloworld.js'));
+                    })
+    );
+
+    testMe.complete(`${fixturePath}/.h`,
+                    common.mustCall((err, data) => {
+                      assert.strictEqual(err, null);
+                      assert.ok(data[0][0].includes('.hiddenfiles'));
+                    })
+    );
+
+    testMe.complete(`${readFileSync}./xxxRandom/random`,
+                    common.mustCall((err, data) => {
+                      assert.strictEqual(err, null);
+                      assert.strictEqual(data[0].length, 0);
+                    })
+    );
+  }
+}
+
 [
   Array,
   Buffer,
