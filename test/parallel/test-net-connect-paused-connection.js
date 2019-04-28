3711684ccf62a6eef4cbf82f943981edae8a0377
sujunfei <sujunfei@bytedance.com>
2019-04-28 17:05:52 +0800

    test: add mustCall for parallel/test-net-connect-paused-connection
    Add common.mustCall test on net.createServer callback and listen
    callback in the parallel/test-net-connect-paused-connection
    
    remove the mustCall of net.createServer callback
    
    PR-URL: https://github.com/nodejs/node/pull/27463
    Reviewed-By: Yongsheng Zhang <zyszys98@gmail.com>
    Reviewed-By: Weijia Wang <starkwang@126.com>
    Reviewed-By: Yorkie Liu <yorkiefixer@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Ouyang Yadong <oyydoibh@gmail.com>

--- test/parallel/test-net-connect-paused-connection.js
+++ test/parallel/test-net-connect-paused-connection.js
@@ -26,8 +26,8 @@ const net = require('net');
 
 net.createServer(function(conn) {
   conn.unref();
-}).listen(0, function() {
+}).listen(0, common.mustCall(function() {
   net.connect(this.address().port, 'localhost').pause();
 
   setTimeout(common.mustNotCall('expected to exit'), 1000).unref();
-}).unref();
+})).unref();
