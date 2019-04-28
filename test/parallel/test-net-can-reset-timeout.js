7d80999454ba269078d74bac7086825e348dfc1b
xinyulee <lixinyu1994123@gmail.com>
2019-04-28 16:56:12 +0800

    test: add mustCall to net-can-reset-timeout
    PR-URL: https://github.com/nodejs/node/pull/27462
    Reviewed-By: Weijia Wang <starkwang@126.com>
    Reviewed-By: Yorkie Liu <yorkiefixer@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Yongsheng Zhang <zyszys98@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-net-can-reset-timeout.js
+++ test/parallel/test-net-can-reset-timeout.js
@@ -37,13 +37,13 @@ const server = net.createServer(common.mustCall(function(stream) {
     stream.write('WHAT.');
   }));
 
-  stream.on('end', function() {
+  stream.on('end', common.mustCall(function() {
     console.log('server side end');
     stream.end();
-  });
+  }));
 }));
 
-server.listen(0, function() {
+server.listen(0, common.mustCall(function() {
   const c = net.createConnection(this.address().port);
 
   c.on('data', function() {
@@ -54,4 +54,4 @@ server.listen(0, function() {
     console.log('client side end');
     server.close();
   });
-});
+}));
