77621d0e31e8e2a586361b9f262cd68638dd7e90
Ruwan Geeganage <rpgeeg@gmail.com>
2019-04-28 23:51:08 +0200

    fs: added tests for util file preprocessSymlinkDestination

--- test/parallel/test-internal-fs.js
+++ test/parallel/test-internal-fs.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const common = require('../common');
+const assert = require('assert');
 const fs = require('internal/fs/utils');
 
 // Valid encodings and no args should not throw.
@@ -12,3 +13,41 @@ common.expectsError(
   () => fs.assertEncoding('foo'),
   { code: 'ERR_INVALID_OPT_VALUE_ENCODING', type: TypeError }
 );
+
+// Test junction symlinks
+{
+  const pathString = 'c:\\test1';
+  const linkPathString = '\\test2';
+
+  const preprocessSymlinkDestination = fs.preprocessSymlinkDestination(
+    pathString,
+    'junction',
+    linkPathString
+  );
+
+  if (process.platform === 'win32') {
+    assert.strictEqual(/^\\\\\?\\/.test(preprocessSymlinkDestination), true);
+  } else {
+    assert.strictEqual(preprocessSymlinkDestination, pathString);
+  }
+}
+
+// Test none junction symlinks
+{
+  const pathString = 'c:\\test1';
+  const linkPathString = '\\test2';
+
+  const preprocessSymlinkDestination = fs.preprocessSymlinkDestination(
+    pathString,
+    undefined,
+    linkPathString
+  );
+
+  if (process.platform === 'win32') {
+    // There should not be any forward slashes
+    assert.strictEqual(
+      /\//.test(preprocessSymlinkDestination), false);
+  } else {
+    assert.strictEqual(preprocessSymlinkDestination, pathString);
+  }
+}
