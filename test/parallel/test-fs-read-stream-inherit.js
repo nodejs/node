f5e2ad784ff8d51db275a7268660fc72cf58289d
nilianzhu <nilianzhu@bytedance.com>
2019-04-28 16:53:17 +0800

    test: add mustCallAtLeast to test-fs-read-stream-inherit
    PR-URL: https://github.com/nodejs/node/pull/27457
    Reviewed-By: Weijia Wang <starkwang@126.com>
    Reviewed-By: Yorkie Liu <yorkiefixer@gmail.com>
    Reviewed-By: Yongsheng Zhang <zyszys98@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-fs-read-stream-inherit.js
+++ test/parallel/test-fs-read-stream-inherit.js
@@ -26,7 +26,7 @@ const rangeFile = fixtures.path('x.txt');
     file.resume();
   }));
 
-  file.on('data', function(data) {
+  file.on('data', common.mustCallAtLeast(function(data) {
     assert.ok(data instanceof Buffer);
     assert.ok(!paused);
     file.length += data.length;
@@ -38,7 +38,7 @@ const rangeFile = fixtures.path('x.txt');
       paused = false;
       file.resume();
     }, 10);
-  });
+  }));
 
 
   file.on('end', common.mustCall());
