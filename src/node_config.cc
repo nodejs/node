d3c2287e59fce447bc4624821bd0ebb394014b90
Sam Roberts <vieuxtech@gmail.com>
2019-05-09 14:31:42 -0700

    src: declare unused priv argument
    PR-URL: https://github.com/nodejs/node/pull/27631
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- src/node_config.cc
+++ src/node_config.cc
@@ -21,7 +21,8 @@ using v8::Value;
 
 static void Initialize(Local<Object> target,
                        Local<Value> unused,
-                       Local<Context> context) {
+                       Local<Context> context,
+                       void* priv) {
   Environment* env = Environment::GetCurrent(context);
   Isolate* isolate = env->isolate();
 
