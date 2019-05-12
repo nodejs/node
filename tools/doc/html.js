5e82b7938700ae4407774cc89f9954df4100aa9a
Richard Lau <riclau@uk.ibm.com>
2019-05-12 09:29:32 -0400

    tools: add `12.x` to alternative docs versions
    PR-URL: https://github.com/nodejs/node/pull/27658
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>

--- tools/doc/html.js
+++ tools/doc/html.js
@@ -384,6 +384,7 @@ function altDocs(filename, docCreated) {
   const [, docCreatedMajor, docCreatedMinor] = docCreated.map(Number);
   const host = 'https://nodejs.org';
   const versions = [
+    { num: '12.x' },
     { num: '11.x' },
     { num: '10.x', lts: true },
     { num: '9.x' },
