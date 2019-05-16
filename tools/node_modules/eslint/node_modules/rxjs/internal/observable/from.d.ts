c3d16756f27b3914e4231d30a39fff7c1dac3f7b
Rich Trott <rtrott@gmail.com>
2019-05-16 02:41:40 -0700

    tools: remove unneeded dependency files
    The new version of dmn more effectively cleans unneeded dependency
    files. (Thanks, BridgeAR!) It's now in the udpate-*.sh files but run it
    manually to get the benefits now (and to have a smaller diff when we
    update).
    
    PR-URL: https://github.com/nodejs/node/pull/27730
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Yongsheng Zhang <zyszys98@gmail.com>
    Reviewed-By: Roman Reiss <me@silverwind.io>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- tools/node_modules/eslint/node_modules/rxjs/internal/observable/from.d.ts
+++
@@ -1,3 +0,0 @@
-import { Observable } from '../Observable';
-import { ObservableInput, SchedulerLike, ObservedValueOf } from '../types';
-export declare function from<O extends ObservableInput<any>>(input: O, scheduler?: SchedulerLike): Observable<ObservedValueOf<O>>;
