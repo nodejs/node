1dd31fe1aacb7b5574ded1a89d39dbe72b0d827f
Michaël Zasso <targos@protonmail.com>
2019-05-18 14:42:15 +0200

    tools: sync gypfiles with V8 7.6
    Co-authored-by: Refael Ackermann (רפאל פלחי) <refack@gmail.com>
    Co-authored-by: Ujjwal Sharma <usharma1998@gmail.com>
    
    PR-URL: https://github.com/nodejs/node/pull/28016
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Refael Ackermann (רפאל פלחי) <refack@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>

--- tools/msvs/pch/v8_pch.h
+++ tools/msvs/pch/v8_pch.h
@@ -1,14 +1,13 @@
-#include "src/api-inl.h"
+#include "src/api/api-inl.h"
 #include "src/ast/ast.h"
 #include "src/builtins/builtins-definitions.h"
-#include "src/code-stub-assembler.h"
-#include "src/globals.h"
-#include "src/objects-inl.h"
+#include "src/common/globals.h"
+#include "src/objects/objects-inl.h"
 #include "src/objects/dictionary-inl.h"
 #include "src/objects/js-objects-inl.h"
 #include "src/objects/fixed-array-inl.h"
-#include "src/utils.h"
-#include "src/vector.h"
+#include "src/utils/utils.h"
+#include "src/utils/vector.h"
 
 
 #include <algorithm>
