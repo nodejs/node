3853b0ce876ba20e613631c4de5f49b0936f4e1b
Anna Henningsen <anna@addaleax.net>
2019-05-21 21:46:19 +0200

    src: remove util-inl.h include in node.h
    `node.h` may only include public APIs, which `util-inl.h` is not.
    There does not seem to be any reason for including it, so remove it,
    because otherwise native addon compilation is broken due to us not
    shipping the `util-inl.h` header.
    
    Refs: https://github.com/nodejs/node/pull/27631
    Fixes: https://github.com/nodejs/node/issues/27803

--- src/node.h
+++ src/node.h
@@ -103,14 +103,6 @@
 # endif
 #endif
 
-#if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
-// Internally, do not include util-inl.h into files unless they need it's
-// inline definitions.
-#else
-// Externally, it must be included for backwards API compatibility.
-#  include <util-inl.h>
-#endif
-
 // Forward-declare libuv loop
 struct uv_loop_s;
 
