86dba64e6d917c1fd983bc5cf48b8d658267c4c2
Refael Ackermann <refack@gmail.com>
2019-05-22 11:33:02 -0400

    deps: V8: patch register-arm64.h
    Fixes a compilation issue on some platforms
    
    PR-URL: https://github.com/nodejs/node/pull/27375
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Ujjwal Sharma <usharma1998@gmail.com>
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- deps/v8/src/codegen/arm64/register-arm64.h
+++ deps/v8/src/codegen/arm64/register-arm64.h
@@ -559,6 +559,8 @@ using Simd128Register = VRegister;
 // Lists of registers.
 class V8_EXPORT_PRIVATE CPURegList {
  public:
+  CPURegList() = default;
+
   template <typename... CPURegisters>
   explicit CPURegList(CPURegister reg0, CPURegisters... regs)
       : list_(CPURegister::ListOf(reg0, regs...)),
