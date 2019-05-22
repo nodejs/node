144db48b6d2e2372bb14fee843f515fc97e49c31
Joyee Cheung <joyeec9h3@gmail.com>
2019-05-22 16:23:01 +0200

    test: update wpt/url to 418f7fabeb
    Using `git node wpt url`
    
    PR-URL: https://github.com/nodejs/node/pull/27822
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/fixtures/wpt/url/urlsearchparams-constructor.any.js
+++ test/fixtures/wpt/url/urlsearchparams-constructor.any.js
@@ -14,6 +14,11 @@ test(function() {
     assert_equals(params.toString(), "")
 }, "URLSearchParams constructor, no arguments")
 
+test(function () {
+    var params = new URLSearchParams("?a=b")
+    assert_equals(params.toString(), "a=b")
+}, 'URLSearchParams constructor, remove leading "?"')
+
 test(() => {
     var params = new URLSearchParams(DOMException);
     assert_equals(params.toString(), "INDEX_SIZE_ERR=1&DOMSTRING_SIZE_ERR=2&HIERARCHY_REQUEST_ERR=3&WRONG_DOCUMENT_ERR=4&INVALID_CHARACTER_ERR=5&NO_DATA_ALLOWED_ERR=6&NO_MODIFICATION_ALLOWED_ERR=7&NOT_FOUND_ERR=8&NOT_SUPPORTED_ERR=9&INUSE_ATTRIBUTE_ERR=10&INVALID_STATE_ERR=11&SYNTAX_ERR=12&INVALID_MODIFICATION_ERR=13&NAMESPACE_ERR=14&INVALID_ACCESS_ERR=15&VALIDATION_ERR=16&TYPE_MISMATCH_ERR=17&SECURITY_ERR=18&NETWORK_ERR=19&ABORT_ERR=20&URL_MISMATCH_ERR=21&QUOTA_EXCEEDED_ERR=22&TIMEOUT_ERR=23&INVALID_NODE_TYPE_ERR=24&DATA_CLONE_ERR=25")
