c30b94c3f9269707e576096027310d091eeae1df
Artem Kobzar <dendishiz0@gmail.com>
2019-05-26 15:57:28 +0300

    test: add test cases for cluster profiling
    Add test cases for `cluster` fork method with `--prof` and `--logfile`
    arguments.
    
    Refs: https://coverage.nodejs.org/coverage-a388f4d13aa29ddb/lib/internal/cluster/master.js.html#L64

---
+++ test/parallel/test-cluster-prof-and-logfile.js
@@ -0,0 +1,85 @@
+'use strict';
+const common = require('../common');
+const tmpdir = require('../common/tmpdir');
+
+const fs = require('fs');
+const assert = require('assert');
+const cluster = require('cluster');
+
+const TEST_TIMEOUT = 1000;
+
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
+const waitForEvent = (eventEmiter, event) => new Promise(resolve => eventEmiter.on(event, resolve));
+
+function findLogFileWithName(name, dir) {
+  const data = fs.readdirSync(dir, 'utf8');
+  return data.filter(file => file.includes(name));
+}
+
+async function shouldFillLogfileArg() {
+  tmpdir.refresh();
+
+  cluster.setupMaster({
+    exec: __dirname,
+    execArgv: ['--prof', '--logfile'],
+    cwd: tmpdir.path
+  });
+
+  cluster.fork();
+  
+  const handler = common.mustCall(worker => {
+    const EXPECTED_FILENAME = 'v8-' + worker.process.pid;
+    const reports = findLogFileWithName(EXPECTED_FILENAME, tmpdir.path);
+    assert.strictEqual(reports.length, 1);
+  });
+
+  cluster.on('exit', handler);
+
+  try {
+    await Promise.race([
+      timeout(TEST_TIMEOUT),
+      waitForEvent(cluster, 'exit')
+    ]);
+  } finally {
+    cluster.removeListener('exit', handler);
+    tmpdir.refresh();
+  }
+}
+
+async function shouldCreateLogWithArgName() {
+  tmpdir.refresh();
+
+  const EXPECTED_FILENAME = 'test';
+
+  cluster.setupMaster({
+    exec: __dirname,
+    execArgv: ['--prof', '--logfile=' + EXPECTED_FILENAME],
+    cwd: tmpdir.path
+  });
+
+  cluster.fork();
+
+  const handler = common.mustCall(worker => {
+    const reports = findLogFileWithName(EXPECTED_FILENAME, tmpdir.path);
+    assert.strictEqual(reports.length, 1);
+  });
+
+  cluster.on('exit', handler);
+
+  try {
+    await Promise.race([
+      timeout(TEST_TIMEOUT),
+      waitForEvent(cluster, 'exit')
+    ]);
+  } finally {
+    cluster.removeListener('exit', handler);
+    tmpdir.refresh();
+  }
+}
+
+if (cluster.isMaster) {
+  (async () => {
+    await shouldFillLogfileArg();
+    await shouldCreateLogWithArgName();
+  })()
+}
