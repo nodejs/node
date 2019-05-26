1770bc870e6ea05e95a32be43705107147a22442
Andrey Melikhov <amel.truelife@gmail.com>
2019-05-26 15:51:36 +0300

    module: increase code coverage of cjs loader
    Add test cases to cover uncovered wrap and wrapper getters.
    
    Refs: https://coverage.nodejs.org/coverage-99268b1e996d13a0/lib/internal/modules/cjs/loader.js.html#L153
    
    PR-URL: https://github.com/nodejs/node/pull/27898
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Jan Krems <jan.krems@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-module-wrapper.js
+++ test/parallel/test-module-wrapper.js
@@ -3,7 +3,7 @@ require('../common');
 const fixtures = require('../common/fixtures');
 const { execFileSync } = require('child_process');
 
-const cjsModuleWrapTest = fixtures.path('cjs-module-wrap.js');
+const cjsModuleWrapTest = fixtures.path('cjs-module-wrapper.js');
 const node = process.execPath;
 
 execFileSync(node, [cjsModuleWrapTest], { stdio: 'pipe' });
