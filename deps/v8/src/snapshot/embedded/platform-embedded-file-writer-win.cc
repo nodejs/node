35ce2f63c1289443fff9ae8c9169ac19c83a3596
Refael Ackermann <refack@gmail.com>
2019-05-30 10:49:26 -0400

    deps: V8: fix linking issue for MSVS
    Backport-PR-URL: https://github.com/nodejs/node/pull/30109
    PR-URL: https://github.com/nodejs/node/pull/28016
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Refael Ackermann (רפאל פלחי) <refack@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>

--- deps/v8/src/snapshot/embedded/platform-embedded-file-writer-win.cc
+++ deps/v8/src/snapshot/embedded/platform-embedded-file-writer-win.cc
@@ -11,6 +11,7 @@
 #if defined(V8_OS_WIN64)
 #include "src/builtins/builtins.h"
 #include "src/diagnostics/unwinding-info-win64.h"
+#include "src/objects/objects-inl.h"
 #include "src/snapshot/embedded/embedded-data.h"
 #include "src/snapshot/embedded/embedded-file-writer.h"
 #endif  // V8_OS_WIN64
