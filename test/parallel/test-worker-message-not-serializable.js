6a5ce36fb8af710abd9143448755475510d494fb
Rich Trott <rtrott@gmail.com>
2019-05-31 17:46:14 +0200

    test: handle unknown message type in worker threads
    Check that main thread handles an unknown message type from a worker
    thread as expected.
    
    PR-URL: https://github.com/nodejs/node/pull/27995
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-worker-message-not-serializable.js
+++ test/parallel/test-worker-message-not-serializable.js
@@ -6,20 +6,19 @@
 // expected.
 
 const common = require('../common');
-common.skipIfWorker();
 
 const assert = require('assert');
 
-const { Worker, isMainThread } = require('worker_threads');
-if (isMainThread) {
-  const worker = new Worker(__filename);
-  worker.on('error', common.mustCall((e) => {
-    assert.strictEqual(e.code, 'ERR_WORKER_UNSERIALIZABLE_ERROR');
-  }));
-} else {
+const { Worker } = require('worker_threads');
+
+const worker = new Worker(`
   const { internalBinding } = require('internal/test/binding');
   const { getEnvMessagePort } = internalBinding('worker');
   const { messageTypes } = require('internal/worker/io');
   const messagePort = getEnvMessagePort();
   messagePort.postMessage({ type: messageTypes.COULD_NOT_SERIALIZE_ERROR });
-}
+`, { eval: true });
+
+worker.on('error', common.mustCall((e) => {
+  assert.strictEqual(e.code, 'ERR_WORKER_UNSERIALIZABLE_ERROR');
+}));
