9032ab4763fb05d0718c0075d05a023aa0bcbb70
Refael Ackermann <refack@gmail.com>
2019-06-02 16:34:19 -0400

    test: always suffix `tmpdir`
    This makes temp dir names consistent whether we run in stand-alone mode,
    via `test.py` in single process, or in multi-process.
    
    PR-URL: https://github.com/nodejs/node/pull/28035
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>

--- test/common/tmpdir.js
+++ test/common/tmpdir.js
@@ -89,11 +89,7 @@ const testRoot = process.env.NODE_TEST_DIR ?
 
 // Using a `.` prefixed name, which is the convention for "hidden" on POSIX,
 // gets tools to ignore it by default or by simple rules, especially eslint.
-let tmpdirName = '.tmp';
-if (process.env.TEST_THREAD_ID) {
-  tmpdirName += `.${process.env.TEST_THREAD_ID}`;
-}
-
+const tmpdirName = '.tmp.' + (process.env.TEST_THREAD_ID || '0');
 const tmpPath = path.join(testRoot, tmpdirName);
 
 function refresh(opts = {}) {
