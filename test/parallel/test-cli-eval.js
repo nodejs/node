bed5887b72a2d11c446ef94b1693737987e110ea
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-06-04 12:50:43 +0300

    test: validate import.meta and require

--- test/parallel/test-cli-eval.js
+++ test/parallel/test-cli-eval.js
@@ -237,7 +237,7 @@ child.exec(`${nodejs} --use-strict -p process.execArgv`,
 const execOptions = '--experimental-modules --input-type module';
 child.exec(
   `${nodejs} ${execOptions} --eval "console.log(42)"`,
-  common.mustCall((err, stdout, stderr) => {
+  common.mustCall((err, stdout) => {
     assert.ifError(err);
     assert.strictEqual(stdout, '42\n');
   }));
@@ -245,7 +245,7 @@ child.exec(
 // Assert that "42\n" is written to stdout with print option.
 child.exec(
   `${nodejs} ${execOptions} --print --eval "42"`,
-  common.mustCall((err, stdout, stderr) => {
+  common.mustCall((err, stdout) => {
     assert.ifError(err);
     assert.strictEqual(stdout, '42\n');
   }));
@@ -257,4 +257,20 @@ child.exec(
     assert.ok(err);
     assert.strictEqual(stdout, '');
     assert.ok(stderr.indexOf('SyntaxError: Unexpected end of input') > 0);
+  }));
+
+// Assert that require is undefined in ESM support
+child.exec(
+  `${nodejs} ${execOptions} --eval "console.log(typeof require);"`,
+  common.mustCall((err, stdout) => {
+    assert.ifError(err);
+    assert.strictEqual(stdout, 'undefined\n');
+  }));
+
+// Assert that import.meta is defined in ESM
+child.exec(
+  `${nodejs} ${execOptions} --eval "console.log(typeof import.meta);"`,
+  common.mustCall((err, stdout) => {
+    assert.ifError(err);
+    assert.strictEqual(stdout, 'object\n');
   }));
