c5c3c6fd9c62e9bedea9d4507aaf1158a20bee3f
Anatoli Papirovski <anatoli.papirovski@postmates.com>
2019-06-05 12:34:56 +0200

    fixup: add comment explaining the test

--- test/async-hooks/test-late-hook-enable.js
+++ test/async-hooks/test-late-hook-enable.js
@@ -3,6 +3,9 @@ const common = require('../common');
 const assert = require('assert');
 const async_hooks = require('async_hooks');
 
+// Checks that enabling async hooks in a callback actually
+// triggers after & destroy as expected.
+
 const fnsToTest = [setTimeout, setImmediate, (cb) => {
   process.nextTick(() => {
     cb();
