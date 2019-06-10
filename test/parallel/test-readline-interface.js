f832e8665e07828a5393d4bf8103ad8c7b4a1d67
Sam Roberts <vieuxtech@gmail.com>
2019-06-10 13:09:12 -0700

    readline: revert semver-major of 28109
    PR-URL: https://github.com/nodejs/node/pull/28109

--- test/parallel/test-readline-interface.js
+++ test/parallel/test-readline-interface.js
@@ -367,25 +367,19 @@ function isWarned(emitter) {
       code: 'ERR_INVALID_OPT_VALUE'
     });
 
-    common.expectsError(function() {
+    common.mustCall(function() {
       readline.createInterface({
         input: fi,
         completer: ''
       });
-    }, {
-      type: TypeError,
-      code: 'ERR_INVALID_OPT_VALUE'
-    });
+    })();
 
-    common.expectsError(function() {
+    common.mustCall(function() {
       readline.createInterface({
         input: fi,
         completer: false
       });
-    }, {
-      type: TypeError,
-      code: 'ERR_INVALID_OPT_VALUE'
-    });
+    })();
   }
 
   // Constructor throws if historySize is not a positive number
