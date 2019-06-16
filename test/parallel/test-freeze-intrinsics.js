b4365d0514a96f8ecea97c8335f666b68879c7fd
Guy Bedford <guybedford@gmail.com>
2019-06-16 23:00:17 +0200

    bootstrap: --frozen-intrinsics override problem workaround

--- test/parallel/test-freeze-intrinsics.js
+++ test/parallel/test-freeze-intrinsics.js
@@ -7,3 +7,28 @@ assert.throws(
   () => Object.defineProperty = 'asdf',
   TypeError
 );
+
+// Ensure we can extend Console
+{
+  const logs = [];
+  class ExtendedConsole extends console.Console {
+    constructor() {
+      super();
+
+      this.log = (msg) => logs.push(msg);
+    }
+  }
+
+  const s = new ExtendedConsole();
+  s.log('custom');
+
+  assert.strictEqual(logs.length, 1);
+  assert.strictEqual(logs[0], 'custom');
+}
+
+// Ensure we can write override Object prototype properties on instances
+{
+  const o = {};
+  o.toString = () => 'Custom toString';
+  assert.strictEqual(o + 'asdf', 'Custom toStringasdf');
+}
