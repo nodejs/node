449bdef6044b74041ec1ab8569fc87037a1d8bc8
Juan Bedoya <juansb827@gmail.com>
2019-06-21 16:16:49 -0500

    test: refactor test using assert instead of try/catch

--- test/pseudo-tty/test-assert-no-color.js
+++ test/pseudo-tty/test-assert-no-color.js
@@ -4,16 +4,16 @@ const assert = require('assert').strict;
 
 process.env.NODE_DISABLE_COLORS = true;
 
-try {
-  assert.deepStrictEqual({}, { foo: 'bar' });
-} catch (error) {
-  const expected =
-    'Expected values to be strictly deep-equal:\n' +
-    '+ actual - expected\n' +
-    '\n' +
-    '+ {}\n' +
-    '- {\n' +
-    '-   foo: \'bar\'\n' +
-    '- }';
-  assert.strictEqual(error.message, expected);
-}
+assert.throws(
+  () => {
+    assert.deepStrictEqual({}, { foo: 'bar' });
+  },
+  {
+    message: 'Expected values to be strictly deep-equal:\n' +
+      '+ actual - expected\n' +
+      '\n' +
+      '+ {}\n' +
+      '- {\n' +
+      '-   foo: \'bar\'\n' +
+      '- }'
+  });
