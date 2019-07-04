7cf6f9e964aa00772965391c23acda6d71972a9a
cjihrig <cjihrig@gmail.com>
2019-07-04 16:20:20 -0400

    process: refactor unhandledRejection logic
    This commit prevents a deprecation warning from being emitted
    if the unhandledRejection event was actually handled.
    
    PR-URL: https://github.com/nodejs/node/pull/28540
    Fixes: https://github.com/nodejs/node/issues/28539
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Masashi Hirano <shisama07@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

---
+++ test/parallel/test-promise-handled-rejection-no-warning.js
@@ -0,0 +1,8 @@
+'use strict';
+const common = require('../common');
+
+// This test verifies that DEP0018 does not occur when rejections are handled.
+common.disableCrashOnUnhandledRejection();
+process.on('warning', common.mustNotCall());
+process.on('unhandledRejection', common.mustCall());
+Promise.reject(new Error());
