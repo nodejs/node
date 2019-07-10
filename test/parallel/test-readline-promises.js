edcdf05009347f34d6ce962eecb8e52766ff5e63
cjihrig <cjihrig@gmail.com>
2019-07-10 10:17:24 -0400

    fixup! readline: add readline.promises
    Remove the readline.promises experimental warning per
    PR review comment.

--- test/parallel/test-readline-promises.js
+++ test/parallel/test-readline-promises.js
@@ -3,10 +3,6 @@ const common = require('../common');
 const assert = require('assert');
 const readline = require('readline');
 const { PassThrough } = require('stream');
-
-common.expectWarning('ExperimentalWarning',
-                     'The readline.promises API is experimental');
-
 const promises = readline.promises;
 
 {
