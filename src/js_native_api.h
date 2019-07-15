860c0d89b660b8e464578ef349b6aaa45722d4fc
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-07-14 17:21:13 -0700

    n-api: add APIs for per-instance state management
    Adds `napi_set_instance_data()` and `napi_get_instance_data()`, which
    allow native addons to store their data on and retrieve their data from
    `napi_env`. `napi_set_instance_data()` accepts a finalizer which is
    called when the `node::Environment()` is destroyed.
    
    This entails rendering the `napi_env` local to each add-on.
    
    Fixes: https://github.com/nodejs/abi-stable-node/issues/378
    PR-URL: https://github.com/nodejs/node/pull/28682
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- src/js_native_api.h
+++ src/js_native_api.h
@@ -499,6 +499,15 @@ NAPI_EXTERN napi_status napi_add_finalizer(napi_env env,
                                            napi_finalize finalize_cb,
                                            void* finalize_hint,
                                            napi_ref* result);
+
+// Instance data
+NAPI_EXTERN napi_status napi_set_instance_data(napi_env env,
+                                               void* data,
+                                               napi_finalize finalize_cb,
+                                               void* finalize_hint);
+
+NAPI_EXTERN napi_status napi_get_instance_data(napi_env env,
+                                               void** data);
 #endif  // NAPI_EXPERIMENTAL
 
 EXTERN_C_END
