5030e81ce305cc6bc5a1e17dd0ef8a9c761f035b
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-07-14 17:21:13 -0700

    n-api: add APIs for per-instance state management
    Adds `napi_set_instance_data()` and `napi_get_instance_data()`, which
    allow native addons to store their data on and retrieve their data from
    `napi_env`. `napi_set_instance_data()` accepts a finalizer which is
    called when the `node::Environment()` is destroyed.
    
    This entails rendering the `napi_env` local to each add-on.
    
    Fixes: https://github.com/nodejs/abi-stable-node/issues/378
    PR-URL: https://github.com/nodejs/node/pull/28682
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

---
+++ test/js-native-api/test_instance_data/test.js
@@ -0,0 +1,40 @@
+'use strict';
+// Test API calls for instance data.
+
+const common = require('../../common');
+const assert = require('assert');
+
+if (module.parent) {
+  // When required as a module, run the tests.
+  const test_instance_data =
+    require(`./build/${common.buildType}/test_instance_data`);
+
+  // Print to stdout when the environment deletes the instance data. This output
+  // is checked by the parent process.
+  test_instance_data.setPrintOnDelete();
+
+  // Test that instance data can be accessed from a binding.
+  assert.strictEqual(test_instance_data.increment(), 42);
+
+  // Test that the instance data can be accessed from a finalizer.
+  test_instance_data.objectWithFinalizer(common.mustCall());
+  global.gc();
+} else {
+  // When launched as a script, run tests in either a child process or in a
+  // worker thread.
+  const requireAs = require('../../common/require-as');
+  const runOptions = { stdio: ['inherit', 'pipe', 'inherit'] };
+
+  function checkOutput(child) {
+    assert.strictEqual(child.status, 0);
+    assert.strictEqual(
+      (child.stdout.toString().split(/\r\n?|\n/) || [])[0],
+      'deleting addon data');
+  }
+
+  // Run tests in a child process.
+  checkOutput(requireAs(__filename, ['--expose-gc'], runOptions, 'child'));
+
+  // Run tests in a worker thread in a child process.
+  checkOutput(requireAs(__filename, ['--expose-gc'], runOptions, 'worker'));
+}
