b7aee2ea44c7dc07988372273b21ec2b77fa3e06
Tobias Nießen <tniessen@tnie.de>
2019-07-19 02:44:31 +0200

    crypto: add hashSize option to crypto.createHash
    This change adds a hashSize option to crypto.createHash which allows
    users to produce variable-length hash values using XOF hash functons.
    
    Fixes: https://github.com/nodejs/node/issues/28757

--- test/parallel/test-crypto-hash.js
+++ test/parallel/test-crypto-hash.js
@@ -185,3 +185,68 @@ common.expectsError(
   assert(instance instanceof Hash, 'Hash is expected to return a new instance' +
                                    ' when called without `new`');
 }
+
+// Test XOF hash functions and the hashSize option.
+{
+  // Default hashSizes.
+  assert.strictEqual(crypto.createHash('shake128').digest('hex'),
+                     '7f9c2ba4e88f827d616045507605853e');
+  assert.strictEqual(crypto.createHash('shake256').digest('hex'),
+                     '46b9dd2b0ba88d13233b3feb743eeb24' +
+                     '3fcd52ea62b81b82b50c27646ed5762f');
+
+  // Short hashSizes.
+  assert.strictEqual(crypto.createHash('shake128', { hashSize: 0 })
+                           .digest('hex'),
+                     '');
+  assert.strictEqual(crypto.createHash('shake128', { hashSize: 5 })
+                           .digest('hex'),
+                     '7f9c2ba4e8');
+  assert.strictEqual(crypto.createHash('shake128', { hashSize: 15 })
+                           .digest('hex'),
+                     '7f9c2ba4e88f827d61604550760585');
+  assert.strictEqual(crypto.createHash('shake256', { hashSize: 16 })
+                           .digest('hex'),
+                     '46b9dd2b0ba88d13233b3feb743eeb24');
+
+  // Large hashSizes.
+  assert.strictEqual(crypto.createHash('shake128', { hashSize: 128 })
+                           .digest('hex'),
+                     '7f9c2ba4e88f827d616045507605853e' +
+                     'd73b8093f6efbc88eb1a6eacfa66ef26' +
+                     '3cb1eea988004b93103cfb0aeefd2a68' +
+                     '6e01fa4a58e8a3639ca8a1e3f9ae57e2' +
+                     '35b8cc873c23dc62b8d260169afa2f75' +
+                     'ab916a58d974918835d25e6a435085b2' +
+                     'badfd6dfaac359a5efbb7bcc4b59d538' +
+                     'df9a04302e10c8bc1cbf1a0b3a5120ea');
+  const superLongHash = crypto.createHash('shake256', { hashSize: 1024 * 1024 })
+                              .update('The message is shorter than the hash!')
+                              .digest('hex');
+  assert.strictEqual(superLongHash.length, 2 * 1024 * 1024);
+  assert.ok(superLongHash.endsWith('193414035ddba77bf7bba97981e656ec'));
+  assert.ok(superLongHash.startsWith('a2a28dbc49cfd6e5d6ceea3d03e77748'));
+
+  // Non-XOF hash functions should accept valid hashSize options as well.
+  assert.strictEqual(crypto.createHash('sha224', { hashSize: 28 })
+                           .digest('hex'),
+                     'd14a028c2a3a2bc9476102bb288234c4' +
+                     '15a2b01f828ea62ac5b3e42f');
+
+  // Passing invalid sizes should throw during creation.
+  common.expectsError(() => {
+    crypto.createHash('sha256', { hashSize: 28 });
+  }, {
+    code: 'ERR_OSSL_EVP_NOT_XOF_OR_INVALID_LENGTH'
+  });
+
+  for (const hashSize of [null, {}, 'foo', false]) {
+    common.expectsError(() => crypto.createHash('sha256', { hashSize }),
+                        { code: 'ERR_INVALID_ARG_TYPE' });
+  }
+
+  for (const hashSize of [-1, .5, Infinity, 2 ** 90]) {
+    common.expectsError(() => crypto.createHash('sha256', { hashSize }),
+                        { code: 'ERR_OUT_OF_RANGE' });
+  }
+}
