c93df0cfc397c9430e22d21059f16424880bb140
Octavian Soldea <octavian.soldea@intel.com>
2019-07-27 17:42:19 -0700

    n-api: refactoring napi_create_function testing
    This is a refactoring of https://github.com/nodejs/node/pull/26998
    following https://github.com/nodejs/node/pull/28505.
    
    The functions `add_last_status()` and `add_returned_status()` are now
    reused, see also https://github.com/nodejs/node/pull/28848.
    
    PR-URL: https://github.com/nodejs/node/pull/28894
    Reviewed-By: Gabriel Schulhof <gabriel.schulhof@intel.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/js-native-api/test_function/test.js
+++ test/js-native-api/test_function/test.js
@@ -37,8 +37,8 @@ tracked_function = null;
 global.gc();
 
 assert.deepStrictEqual(test_function.TestCreateFunctionParameters(), {
-  envIsNull: 'pass',
-  nameIsNull: 'pass',
-  cbIsNull: 'pass',
-  resultIsNull: 'pass'
+  envIsNull: 'Invalid argument',
+  nameIsNull: 'napi_ok',
+  cbIsNull: 'Invalid argument',
+  resultIsNull: 'Invalid argument'
 });
