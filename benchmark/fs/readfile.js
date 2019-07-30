0376b5b7baf627b112915183edf9e1ea2d3856b7
João Reis <reis@janeasystems.com>
2019-07-30 16:55:15 +0100

    benchmark: use test/common/tmpdir consistently
    Many benchmarks use test/common/tmpdir. This changes 3 benchmarks that
    use NODE_TMPDIR to also use test/common/tmpdir.
    
    This is necessary in preparation for the next commit that changes tmpdir
    to delete tmpdir.path when the Node.js process exits. Thus, if multiple
    benchmarks are run sequentially, the ones that use tmpdir will remove
    the directory and the ones changed here would fail because it does not
    exist. This happens when running test/benchmark.
    
    Note: to explicitly select a directory for tmpdir, use NODE_TEST_DIR.
    
    PR-URL: https://github.com/nodejs/node/pull/28858
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- benchmark/fs/readfile.js
+++ benchmark/fs/readfile.js
@@ -5,10 +5,13 @@
 
 const path = require('path');
 const common = require('../common.js');
-const filename = path.resolve(process.env.NODE_TMPDIR || __dirname,
-                              `.removeme-benchmark-garbage-${process.pid}`);
 const fs = require('fs');
 const assert = require('assert');
+
+const tmpdir = require('../../test/common/tmpdir');
+tmpdir.refresh();
+const filename = path.resolve(tmpdir.path,
+                              `.removeme-benchmark-garbage-${process.pid}`);
 
 const bench = common.createBenchmark(main, {
   dur: [5],
