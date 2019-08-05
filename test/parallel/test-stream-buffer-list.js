c15b4969e9f79cbe6d436a223390dbacb5329dfc
Robert Nagy <ronagy@icloud.com>
2019-08-05 13:26:29 +0200

    stream: encapsulate buffer-list
    PR-URL: https://github.com/nodejs/node/pull/28974
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-stream-buffer-list.js
+++ test/parallel/test-stream-buffer-list.js
@@ -16,11 +16,28 @@ assert.deepStrictEqual(emptyList.concat(0), Buffer.alloc(0));
 
 const buf = Buffer.from('foo');
 
+function testIterator(list, count) {
+  // test iterator
+  let len = 0;
+  // eslint-disable-next-line no-unused-vars
+  for (const x of list) {
+    len++;
+  }
+  assert.strictEqual(len, count);
+}
+
 // Test buffer list with one element.
 const list = new BufferList();
+testIterator(list, 0);
+
 list.push(buf);
+testIterator(list, 1);
+for (const x of list) {
+  assert.strictEqual(x, buf);
+}
 
 const copy = list.concat(3);
+testIterator(copy, 3);
 
 assert.notStrictEqual(copy, buf);
 assert.deepStrictEqual(copy, buf);
@@ -28,5 +45,6 @@ assert.deepStrictEqual(copy, buf);
 assert.strictEqual(list.join(','), 'foo');
 
 const shifted = list.shift();
+testIterator(list, 0);
 assert.strictEqual(shifted, buf);
 assert.deepStrictEqual(list, new BufferList());
