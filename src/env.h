530004ef32333520812c5cecf8f768cecf9a4392
Anna Henningsen <anna@addaleax.net>
2019-08-10 00:51:51 +0200

    http2: only call into JS when necessary for session events
    For some JS events, it only makes sense to call into JS when there
    are listeners for the event in question.
    
    The overhead is noticeable if a lot of these events are emitted during
    the lifetime of a session. To reduce this overhead, keep track of
    whether any/how many JS listeners are present, and if there are none,
    skip calls into JS altogether.
    
    This is part of performance improvements to mitigate CVE-2019-9513.
    
    PR-URL: https://github.com/nodejs/node/pull/29122
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/env.h
+++ src/env.h
@@ -208,6 +208,7 @@ constexpr size_t kFsStatsBufferLength = kFsStatsFieldsNumber * 2;
   V(family_string, "family")                                                   \
   V(fatal_exception_string, "_fatalException")                                 \
   V(fd_string, "fd")                                                           \
+  V(fields_string, "fields")                                                   \
   V(file_string, "file")                                                       \
   V(fingerprint256_string, "fingerprint256")                                   \
   V(fingerprint_string, "fingerprint")                                         \
