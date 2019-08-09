88726f2384f3b3b50e103984f31bbbc6881d7ab1
Anna Henningsen <anna@addaleax.net>
2019-08-10 01:30:35 +0200

    http2: do not create ArrayBuffers when no DATA received
    Lazily allocate `ArrayBuffer`s for the contents of DATA frames.
    Creating `ArrayBuffer`s is, sadly, not a cheap operation with V8.
    
    This is part of performance improvements to mitigate CVE-2019-9513.
    
    Together with the previous commit, these changes improve throughput
    in the adversarial case by about 100 %, and there is little more
    that we can do besides artificially limiting the rate of incoming
    metadata frames (i.e. after this patch, CPU usage is virtually
    exclusively in libnghttp2).
    
    PR-URL: https://github.com/nodejs/node/pull/29122
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_http2.h
+++ src/node_http2.h
@@ -993,7 +993,8 @@ class Http2Session : public AsyncWrap, public StreamListener {
   uint32_t chunks_sent_since_last_write_ = 0;
 
   uv_buf_t stream_buf_ = uv_buf_init(nullptr, 0);
-  v8::Local<v8::ArrayBuffer> stream_buf_ab_;
+  v8::Global<v8::ArrayBuffer> stream_buf_ab_;
+  AllocatedBuffer stream_buf_allocation_;
 
   size_t max_outstanding_pings_ = DEFAULT_MAX_PINGS;
   std::queue<std::unique_ptr<Http2Ping>> outstanding_pings_;
