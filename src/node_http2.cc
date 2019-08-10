765240669b32acb79b406aca8f36793444cd47b6
Anna Henningsen <anna@addaleax.net>
2019-08-10 22:27:48 +0200

    http2: limit number of rejected stream openings
    Limit the number of streams that are rejected upon creation. Since
    each such rejection is associated with an `NGHTTP2_ENHANCE_YOUR_CALM`
    error that should tell the peer to not open any more streams,
    continuing to open streams should be read as a sign of a misbehaving
    peer. The limit is currently set to 100 but could be changed or made
    configurable.
    
    This is intended to mitigate CVE-2019-9514.

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -4,6 +4,8 @@
 #include "node_http2.h"
 #include "node_http2_state.h"
 #include "node_perf.h"
+#include "node_revert.h"
+#include "util-inl.h"
 
 #include <algorithm>
 
@@ -970,15 +972,18 @@ inline int Http2Session::OnBeginHeadersCallback(nghttp2_session* handle,
     if (LIKELY(session->CanAddStream())) {
       new Http2Stream(session, id, frame->headers.cat);
     } else {
+      if (session->rejected_stream_count_++ > 100 &&
+          !IsReverted(SECURITY_REVERT_CVE_2019_9514)) {
+        return NGHTTP2_ERR_CALLBACK_FAILURE;
+      }
       // Too many concurrent streams being opened
       nghttp2_submit_rst_stream(**session, NGHTTP2_FLAG_NONE, id,
                                 NGHTTP2_ENHANCE_YOUR_CALM);
       return NGHTTP2_ERR_TEMPORAL_CALLBACK_FAILURE;
     }
-  } else {
-    // If the stream has already been destroyed, ignore.
-    if (stream->IsDestroyed())
-      return 0;
+
+    session->rejected_stream_count_ = 0;
+  } else if (!stream->IsDestroyed()) {
     stream->StartHeaders(frame->headers.cat);
   }
   return 0;
