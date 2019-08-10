05dada46eea59c0bfdabe4f54d64cda2f315cec9
Anna Henningsen <anna@addaleax.net>
2019-08-10 22:27:48 +0200

    http2: limit number of rejected stream openings
    Limit the number of streams that are rejected upon creation. Since
    each such rejection is associated with an `NGHTTP2_ENHANCE_YOUR_CALM`
    error that should tell the peer to not open any more streams,
    continuing to open streams should be read as a sign of a misbehaving
    peer. The limit is currently set to 100 but could be changed or made
    configurable.
    
    This is intended to mitigate CVE-2019-9514.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/29123
    PR-URL: https://github.com/nodejs/node/pull/29122
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_http2.h
+++ src/node_http2.h
@@ -1017,6 +1017,11 @@ class Http2Session : public AsyncWrap, public StreamListener {
   std::vector<nghttp2_stream_write> outgoing_buffers_;
   std::vector<uint8_t> outgoing_storage_;
   std::vector<int32_t> pending_rst_streams_;
+  // Count streams that have been rejected while being opened. Exceeding a fixed
+  // limit will result in the session being destroyed, as an indication of a
+  // misbehaving peer. This counter is reset once new streams are being
+  // accepted again.
+  int32_t rejected_stream_count_ = 0;
 
   void CopyDataIntoOutgoing(const uint8_t* src, size_t src_length);
   void ClearOutgoing(int status);
