0c905656fa960082f5c08c915f2e87849c4e9ba9
Anna Henningsen <anna@addaleax.net>
2019-08-12 22:55:16 +0200

    http2: limit number of invalid incoming frames
    Limit the number of invalid input frames, as they may be pointing towards a
    misbehaving peer. The limit is currently set to 1000 but could be changed or
    made configurable.
    
    This is intended to mitigate CVE-2019-9514.
    
    [This commit differs from the v12.x one due to the lack of
    https://github.com/libuv/libuv/commit/ee24ce900e5714c950b248da2b.
    See the comment in the test for more details.]

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -1018,6 +1018,10 @@ int Http2Session::OnInvalidFrame(nghttp2_session* handle,
   Http2Session* session = static_cast<Http2Session*>(user_data);
 
   Debug(session, "invalid frame received, code: %d", lib_error_code);
+  if (session->invalid_frame_count_++ > 1000 &&
+      !IsReverted(SECURITY_REVERT_CVE_2019_9514)) {
+    return 1;
+  }
 
   // If the error is fatal or if error code is ERR_STREAM_CLOSED... emit error
   if (nghttp2_is_fatal(lib_error_code) ||
