ec60b625b66288cb63d63a51b115661a8503e19e
Anna Henningsen <anna@addaleax.net>
2019-08-12 23:36:00 +0200

    http2: allow security revert for Ping/Settings Flood
    nghttp2 has updated its limit for outstanding Ping/Settings ACKs
    to 1000. This commit allows reverting to the old default of 10000.
    
    The associated CVEs are CVE-2019-9512/CVE-2019-9515.
    
    PR-URL: https://github.com/nodejs/node/pull/29122
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_revert.h
+++ src/node_revert.h
@@ -16,6 +16,7 @@
 namespace node {
 
 #define SECURITY_REVERSIONS(XX)                                            \
+  XX(CVE_2019_9512, "CVE-2019-9512", "HTTP/2 Ping/Settings Flood")         \
   XX(CVE_2019_9514, "CVE-2019-9514", "HTTP/2 Reset Flood")                 \
   XX(CVE_2019_9516, "CVE-2019-9516", "HTTP/2 0-Length Headers Leak")       \
   XX(CVE_2019_9518, "CVE-2019-9518", "HTTP/2 Empty DATA Frame Flooding")   \
