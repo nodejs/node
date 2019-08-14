6d427378c09e23639a5b292f6cc89b3fc6bb9ee8
Anna Henningsen <anna@addaleax.net>
2019-08-14 11:32:06 +0200

    deps: update nghttp2 to 1.39.2
    This includes mitigations for CVE-2019-9512/CVE-2019-9515.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/29124
    PR-URL: https://github.com/nodejs/node/pull/29122
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- deps/nghttp2/lib/nghttp2_option.c
+++ deps/nghttp2/lib/nghttp2_option.c
@@ -116,3 +116,8 @@ void nghttp2_option_set_no_closed_streams(nghttp2_option *option, int val) {
   option->opt_set_mask |= NGHTTP2_OPT_NO_CLOSED_STREAMS;
   option->no_closed_streams = val;
 }
+
+void nghttp2_option_set_max_outbound_ack(nghttp2_option *option, size_t val) {
+  option->opt_set_mask |= NGHTTP2_OPT_MAX_OUTBOUND_ACK;
+  option->max_outbound_ack = val;
+}
