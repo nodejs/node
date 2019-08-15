3e89ac19dfd38948690341e33c751021dbf5c0e6
Luigi Pinca <luigipinca@gmail.com>
2019-08-15 14:52:31 +0200

    fixup! tls: allow client-side sockets to be half-opened

--- test/parallel/test-tls-connect-allow-half-open-option.js
+++ test/parallel/test-tls-connect-allow-half-open-option.js
@@ -21,22 +21,27 @@ const tls = require('tls');
   assert.strictEqual(socket.allowHalfOpen, false);
 }
 
-
 const server = tls.createServer({
   key: fixtures.readKey('agent1-key.pem'),
   cert: fixtures.readKey('agent1-cert.pem'),
 }, common.mustCall((socket) => {
   server.close();
 
+  let message = '';
+
   socket.setEncoding('utf8');
-  socket.on('data', common.mustCall((chunk) => {
-    if (chunk === 'Hello') {
-      socket.end(chunk);
-    } else {
-      assert.strictEqual(chunk, 'Bye');
-      assert(!socket.writable);
+  socket.on('data', (chunk) => {
+    message += chunk;
+
+    if (message === 'Hello') {
+      socket.end(message);
+      message = '';
     }
-  }, 2));
+  });
+
+  socket.on('end', common.mustCall(() => {
+    assert.strictEqual(message, 'Bye');
+  }));
 }));
 
 server.listen(0, common.mustCall(() => {
@@ -45,10 +50,15 @@ server.listen(0, common.mustCall(() => {
     rejectUnauthorized: false,
     allowHalfOpen: true,
   }, common.mustCall(() => {
-    socket.on('data', common.mustCall((chunk) => {
-      assert.strictEqual(chunk, 'Hello');
-    }));
+    let message = '';
+
+    socket.on('data', (chunk) => {
+      message += chunk;
+    });
+
     socket.on('end', common.mustCall(() => {
+      assert.strictEqual(message, 'Hello');
+
       setTimeout(() => {
         assert(socket.writable);
         assert(socket.write('Bye'));
