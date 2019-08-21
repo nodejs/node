dc9730540c282860af0bbe0bcb08509f37393bdf
Xavier Stouder <xavier@stouder.io>
2019-08-21 21:03:43 +0200

    test: adapt tests
    Change regexp of test-console to accept other possible units and create test for formatTime

---
+++ test/parallel/test-console-formatTime.js
@@ -0,0 +1,13 @@
+// Flags: --expose-internals
+const { formatTime } = require('internal/console/constructor');
+const assert = require('assert');
+
+const test1 = formatTime(100);
+const test2 = formatTime(1500);
+const test3 = formatTime(60300);
+const test4 = formatTime(4000000);
+
+assert.strictEqual(test1, '100ms');
+assert.strictEqual(test2, '1.5s');
+assert.strictEqual(test3, '1.005min')
+assert.strictEqual(test4, '1.111h')
