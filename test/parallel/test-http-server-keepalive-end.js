ae0a0e97ba7cf46c3362a238979838cbc3e5deec
Anna Henningsen <anna@addaleax.net>
2019-08-24 20:42:11 +0200

    http: reset parser.incoming when server request is finished
    This resolves a memory leak for keep-alive connections and does not
    regress in the way that 779a05d5d1bfe2eeb05386f did by waiting for
    the incoming request to be finished before releasing the
    `parser.incoming` object.
    
    Refs: https://github.com/nodejs/node/pull/28646
    Refs: https://github.com/nodejs/node/pull/29263
    Fixes: https://github.com/nodejs/node/issues/9668
    
    PR-URL: https://github.com/nodejs/node/pull/29297
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- test/parallel/test-http-server-keepalive-end.js
+++ test/parallel/test-http-server-keepalive-end.js
@@ -1,18 +1,27 @@
 'use strict';
-
 const common = require('../common');
+const assert = require('assert');
 const { createServer } = require('http');
 const { connect } = require('net');
 
+// Make sure that for HTTP keepalive requests, the .on('end') event is emitted
+// on the incoming request object, and that the parser instance does not hold
+// on to that request object afterwards.
+
 const server = createServer(common.mustCall((req, res) => {
-  req.on('end', common.mustCall());
+  req.on('end', common.mustCall(() => {
+    const parser = req.socket.parser;
+    assert.strictEqual(parser.incoming, req);
+    process.nextTick(() => {
+      assert.strictEqual(parser.incoming, null);
+    });
+  }));
   res.end('hello world');
 }));
 
 server.unref();
 
 server.listen(0, common.mustCall(() => {
-
   const client = connect(server.address().port);
 
   const req = [
