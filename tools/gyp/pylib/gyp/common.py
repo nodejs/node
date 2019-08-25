8675152f0f15002915225216f48e8bdd39612a81
David Carlier <devnexen@gmail.com>
2019-08-25 16:49:46 +0100

    build: integrate DragonFlyBSD into gyp build
    PR-URL: https://github.com/nodejs/node/pull/29313
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- tools/gyp/pylib/gyp/common.py
+++ tools/gyp/pylib/gyp/common.py
@@ -421,6 +421,8 @@ def GetFlavor(params):
     return 'solaris'
   if sys.platform.startswith('freebsd'):
     return 'freebsd'
+  if sys.platform.startswith('dragonfly'):
+    return 'freebsd'
   if sys.platform.startswith('openbsd'):
     return 'openbsd'
   if sys.platform.startswith('netbsd'):
