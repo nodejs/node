b7981826a909575d9735aeed99b689f0362a3a4d
Bradley Farias <bfarias@godaddy.com>
2019-09-04 11:19:14 -0500

    esm: make dynamic import work in the REPL

---
+++ test/es-module/test-esm-repl.js
@@ -0,0 +1,19 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+const { spawn } = require('child_process');
+
+const child = spawn(process.execPath, [
+  '--experimental-modules',
+  '--interactive'
+]);
+child.stdin.end(`
+import('fs').then(
+  ns => ns.default === require('fs') ? 0 : 1,
+  _ => 2
+).then(process.exit)
+`);
+
+child.on('exit', (code) => {
+  assert.strictEqual(code, 0);
+});
