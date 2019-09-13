f90740d73450514b0d7c8e28781ebe7da56cc5e7
Kamil Rytarowski <n54@gmx.com>
2019-09-13 03:42:24 +0200

    build: allow clang 10+ in configure.py
    Detected on NetBSD/amd64.
    
    Fixes: https://github.com/nodejs/node/issues/29536
    
    PR-URL: https://github.com/nodejs/node/pull/29541
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>

--- configure.py
+++ configure.py
@@ -724,7 +724,7 @@ def get_nasm_version(asm):
 
 def get_llvm_version(cc):
   return get_version_helper(
-    cc, r"(^(?:FreeBSD )?clang version|based on LLVM) ([3-9]\.[0-9]+)")
+    cc, r"(^(?:FreeBSD )?clang version|based on LLVM) ([0-9]+\.[0-9]+)")
 
 def get_xcode_version(cc):
   return get_version_helper(
