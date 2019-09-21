9e45a591b82a490ef3b3803eab44acc51f883688
Anna Henningsen <anna@addaleax.net>
2019-09-21 12:48:09 +0200

    fixup! src: fix closing weak `HandleWrap`s on GC

--- test/sequential/test-performance-eventloopdelay.js
+++ test/sequential/test-performance-eventloopdelay.js
@@ -99,4 +99,6 @@ const {
   spinAWhile();
 }
 
+// Make sure that the histogram instances can be garbage-collected without
+// and not just implictly destroyed when the Environment is torn down.
 process.on('exit', global.gc);
