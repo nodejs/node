e1ada5bf7c596f3a71e872d8e52cfce5eb7a66a4
Tobias Nießen <tniessen@tnie.de>
2019-09-23 02:43:30 +0200

    test: simplify timingSafeEqual test case

--- test/sequential/test-crypto-timing-safe-equal.js
+++ test/sequential/test-crypto-timing-safe-equal.js
@@ -19,105 +19,17 @@ assert.strictEqual(
 );
 
 {
-  const ab32 = new ArrayBuffer(32);
-  const dv32 = new DataView(ab32);
-  dv32.setUint32(0, 1);
-  dv32.setUint32(4, 1, true);
-  dv32.setBigUint64(8, 1n);
-  dv32.setBigUint64(16, 1n, true);
-  dv32.setUint16(24, 1);
-  dv32.setUint16(26, 1, true);
-  dv32.setUint8(28, 1);
-  dv32.setUint8(29, 1);
+  // Test TypedArrays with different lengths but equal byteLengths.
+  const buf = crypto.randomBytes(16).buffer;
+  const a1 = new Uint8Array(buf);
+  const a2 = new Uint16Array(buf);
+  const a3 = new Uint32Array(buf);
 
-  // 'should consider equal buffers to be equal'
-  assert.strictEqual(
-    crypto.timingSafeEqual(Buffer.from(ab32), dv32),
-    true
-  );
-  assert.strictEqual(
-    crypto.timingSafeEqual(new Uint32Array(ab32), dv32),
-    true
-  );
-  assert.strictEqual(
-    crypto.timingSafeEqual(
-      new Uint8Array(ab32, 0, 16),
-      Buffer.from(ab32, 0, 16)
-    ),
-    true
-  );
-  assert.strictEqual(
-    crypto.timingSafeEqual(
-      new Uint32Array(ab32, 0, 8),
-      Buffer.of(0, 0, 0, 1, // 4
-                1, 0, 0, 0, // 8
-                0, 0, 0, 0, 0, 0, 0, 1, // 16
-                1, 0, 0, 0, 0, 0, 0, 0, // 24
-                0, 1, // 26
-                1, 0, // 28
-                1, 1, // 30
-                0, 0) // 32
-    ),
-    true
-  );
-
-  // 'should consider unequal buffer views to be unequal'
-  assert.strictEqual(
-    crypto.timingSafeEqual(
-      new Uint32Array(ab32, 16, 4),
-      Buffer.from(ab32, 0, 16)
-    ),
-    false
-  );
-  assert.strictEqual(
-    crypto.timingSafeEqual(
-      new Uint8Array(ab32, 0, 16),
-      Buffer.from(ab32, 16, 16)
-    ),
-    false
-  );
-  assert.strictEqual(
-    crypto.timingSafeEqual(
-      new Uint32Array(ab32, 0, 8),
-      Buffer.of(0, 0, 0, 1, // 4
-                1, 0, 0, 0, // 8
-                0, 0, 0, 0, 0, 0, 0, 1, // 16
-                1, 0, 0, 0, 0, 0, 0, 0, // 24
-                0, 1, // 26
-                1, 0, // 28
-                1, 1, // 30
-                0, 1) // 32
-    ),
-    false
-  );
-  // 'buffers with differing byteLength must throw an equal length error'
-  common.expectsError(
-    () => crypto.timingSafeEqual(Buffer.from(ab32, 0, 8),
-                                 Buffer.from(ab32, 0, 9)),
-    {
-      code: 'ERR_CRYPTO_TIMING_SAFE_EQUAL_LENGTH',
-      type: RangeError,
-      message: 'Input buffers must have the same number of bytes'
+  for (const left of [a1, a2, a3]) {
+    for (const right of [a1, a2, a3]) {
+      assert.strictEqual(crypto.timingSafeEqual(left, right), true);
     }
-  );
-  common.expectsError(
-    () => crypto.timingSafeEqual(
-      new Uint32Array(ab32, 0, 8), // 32
-      Buffer.of(0, 0, 0, 1, // 4
-                1, 0, 0, 0, // 8
-                0, 0, 0, 0, 0, 0, 0, 1, // 16
-                1, 0, 0, 0, 0, 0, 0, 0, // 24
-                0, 1, // 26
-                1, 0, // 28
-                1, 1, // 30
-                0) // 31
-    ),
-    {
-      code: 'ERR_CRYPTO_TIMING_SAFE_EQUAL_LENGTH',
-      type: RangeError,
-      message: 'Input buffers must have the same number of bytes'
-    }
-  );
+  }
 }
 
 common.expectsError(
