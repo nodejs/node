1c5e1b14ad1307767c52bcedb11eda84518c324d
Ruben Bridgewater <ruben@bridgewater.de>
2019-09-23 21:59:39 +0200

    fixup: reformat for easier comparison

--- test/parallel/test-console-formatTime.js
+++ test/parallel/test-console-formatTime.js
@@ -6,8 +6,9 @@ const assert = require('assert');
 
 assert.strictEqual(formatTime(100.0096), '100.01ms');
 assert.strictEqual(formatTime(100.0115), '100.011ms');
-assert.strictEqual(formatTime(1500.04), '1s and 500ms');
-assert.strictEqual(formatTime(1500.056), '1s and 500.1ms');
-assert.strictEqual(formatTime(60300.3), '1min and 300ms');
-assert.strictEqual(formatTime(4000457.4), '1h, 6min and 40.5s');
-assert.strictEqual(formatTime(3601017.4), '1h and 1s');
+assert.strictEqual(formatTime(1500.04), '1.500s');
+assert.strictEqual(formatTime(1000.056), '1.000s');
+assert.strictEqual(formatTime(60300.3), '1:00.300 (m:ss.mmm)');
+assert.strictEqual(formatTime(4000457.4), '1:06:40.457 (h:mm:ss.mmm)');
+assert.strictEqual(formatTime(3601310.4), '1:00:01.310 (h:mm:ss.mmm)');
+assert.strictEqual(formatTime(3213601017.6), '892:40:01.018 (h:mm:ss.mmm)');
