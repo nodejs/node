690a863aaae16cba328d36b523558ded2a2c38e7
cjihrig <cjihrig@gmail.com>
2019-09-25 11:10:36 -0400

    test: simplify force-context-aware test
    PR-URL: https://github.com/nodejs/node/pull/29705
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Shelley Vohr <codebytere@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/addons/force-context-aware/test.js
+++ test/addons/force-context-aware/test.js
@@ -1,13 +1,8 @@
+// Flags: --force-context-aware
 'use strict';
 const common = require('../../common');
-const childProcess = require('child_process');
 const assert = require('assert');
-const path = require('path');
 
-const mod = path.join('test', 'addons', 'force-context-aware', 'index.js');
-
-const execString = `"${process.execPath}" --force-context-aware ./${mod}`;
-childProcess.exec(execString, common.mustCall((err) => {
-  const errMsg = 'Loading non context-aware native modules has been disabled';
-  assert.strictEqual(err.message.includes(errMsg), true);
-}));
+assert.throws(() => {
+  require(`./build/${common.buildType}/binding`);
+}, /^Error: Loading non context-aware native modules has been disabled$/);
