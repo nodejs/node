6bd7ec9f36dc1ce0fe22a0d40ec7d9e12dbbadf4
Robert Nagy <ronagy@icloud.com>
2019-10-01 09:23:36 +0200

    stream: unpipe on destroy
    Avoid writing to destination if it has been destroyed.
    'close' is emitted asynchronously which can cause
    another write to be performed after destroy() causing
    a write after destroy error.
    
    Fixes: https://github.com/nodejs/node/issues/29790

---
+++ test/parallel/test-stream-pipe-destroy-write.js
@@ -0,0 +1,38 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const stream = require('stream');
+
+{
+  const ws = new stream.Writable();
+  const rs = new stream.Readable({ highWaterMark: 16 });
+
+  ws._write = common.mustNotCall();
+
+  rs._read = function() {
+    rs.push('hello world');
+  };
+
+  rs.pipe(ws);
+  ws.destroy();
+  ws.on('error', common.mustCall(err => {
+    assert.strictEqual(err.code, 'ERR_STREAM_DESTROYED');
+  }));
+}
+
+{
+  const ws = new stream.Writable();
+  const rs = new stream.Readable({ highWaterMark: 16 });
+
+  ws.write = common.mustNotCall();
+
+  rs._read = function() {
+    rs.push('hello world');
+  };
+
+  rs.pipe(ws);
+  ws.destroy(new Error('asd'));
+  ws.on('error', common.mustCall(err => {
+    assert.strictEqual(err.message, 'asd');
+  }));
+}
