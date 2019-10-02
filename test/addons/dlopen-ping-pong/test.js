adee99883a63d475d0239724b4d580eab7736bd6
Sam Roberts <vieuxtech@gmail.com>
2019-10-02 14:47:08 -0700

    test: debug output for dlopen-ping-pong test
    Make test failures easier to diagnose through progress logging, and
    reporting of dlerror().
    
    PR-URL: https://github.com/nodejs/node/pull/29818
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Christian Clauss <cclauss@me.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- test/addons/dlopen-ping-pong/test.js
+++ test/addons/dlopen-ping-pong/test.js
@@ -9,12 +9,15 @@ const path = require('path');
 const os = require('os');
 
 const bindingPath = require.resolve(`./build/${common.buildType}/binding`);
+console.log('process.dlopen:', bindingPath);
 process.dlopen(module, bindingPath,
                os.constants.dlopen.RTLD_NOW | os.constants.dlopen.RTLD_GLOBAL);
+console.log('module.exports.load:', `${path.dirname(bindingPath)}/ping.so`);
 module.exports.load(`${path.dirname(bindingPath)}/ping.so`);
 assert.strictEqual(module.exports.ping(), 'pong');
 
 // Check that after the addon is loaded with
 // process.dlopen() a require() call fails.
+console.log('require:', `./build/${common.buildType}/binding`);
 const re = /^Error: Module did not self-register\.$/;
 assert.throws(() => require(`./build/${common.buildType}/binding`), re);
