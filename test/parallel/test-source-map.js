95441e5f7274ef6f37941174dde8e0103990f2b4
bcoe <bencoe@google.com>
2019-10-06 16:22:23 -0700

    test: Windows has additional newline marker

--- test/parallel/test-source-map.js
+++ test/parallel/test-source-map.js
@@ -216,7 +216,11 @@ function nextdir() {
     'istanbul-throw.js',
     coverageDirectory
   );
-  assert.deepStrictEqual(sourceMap.lineLengths, [1085, 30, 184, 648, 0]);
+  if (common.isWindows) {
+    assert.deepStrictEqual(sourceMap.lineLengths, [1087, 301, 185, 649, 0]);
+  } else {
+    assert.deepStrictEqual(sourceMap.lineLengths, [1085, 30, 184, 648, 0]);
+  }
 }
 
 function getSourceMapFromCache(fixtureFile, coverageDirectory) {
