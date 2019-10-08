fb621e04e71ef05961e7ad2f0759c1f68bfc3447
Richard Lau <riclau@uk.ibm.com>
2019-10-08 12:57:09 -0400

    build: make full-icu the default for releases
    Release builds specifically set the `--with-intl=` configure option
    instead of relying on the default. Explicitly set to `full-icu`.
    
    Refs: https://github.com/nodejs/node/pull/29522

--- vcbuild.bat
+++ vcbuild.bat
@@ -120,7 +120,7 @@ if /i "%1"=="lint-md-build" set lint_md_build=1&goto arg-ok
 if /i "%1"=="lint"          set lint_cpp=1&set lint_js=1&set lint_md=1&goto arg-ok
 if /i "%1"=="lint-ci"       set lint_cpp=1&set lint_js_ci=1&goto arg-ok
 if /i "%1"=="package"       set package=1&goto arg-ok
-if /i "%1"=="msi"           set msi=1&set licensertf=1&set download_arg="--download=all"&set i18n_arg=small-icu&goto arg-ok
+if /i "%1"=="msi"           set msi=1&set licensertf=1&set download_arg="--download=all"&set i18n_arg=full-icu&goto arg-ok
 if /i "%1"=="build-release" set build_release=1&set sign=1&goto arg-ok
 if /i "%1"=="upload"        set upload=1&goto arg-ok
 if /i "%1"=="small-icu"     set i18n_arg=%1&goto arg-ok
@@ -158,7 +158,7 @@ if defined build_release (
   set msi=1
   set licensertf=1
   set download_arg="--download=all"
-  set i18n_arg=small-icu
+  set i18n_arg=full-icu
   set projgen=1
   set cctest=1
   set ltcg=1
