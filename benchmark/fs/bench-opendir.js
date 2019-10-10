6d209fce082ce2ae73dc9555badcdeedb0079b41
Anna Henningsen <anna@addaleax.net>
2019-10-10 21:46:10 +0200

    fixup! fs: buffer dir entries in opendir()

--- benchmark/fs/bench-opendir.js
+++ benchmark/fs/bench-opendir.js
@@ -10,26 +10,24 @@ const bench = common.createBenchmark(main, {
   mode: [ 'async', 'sync' ]
 });
 
-function main({ n, dir, mode }) {
+async function main({ n, dir, mode }) {
   const fullPath = path.resolve(__dirname, '../../', dir);
 
-  (async () => {
-    bench.start();
+  bench.start();
 
-    let counter = 0;
-    for (let i = 0; i < n; i++) {
-      if (mode === 'async') {
-        // eslint-disable-next-line no-unused-vars
-        for await (const entry of await fs.promises.opendir(fullPath))
-          counter++;
-      } else {
-        const dir = fs.opendirSync(fullPath);
-        while (dir.readSync() !== null)
-          counter++;
-        dir.closeSync();
-      }
+  let counter = 0;
+  for (let i = 0; i < n; i++) {
+    if (mode === 'async') {
+      // eslint-disable-next-line no-unused-vars
+      for await (const entry of await fs.promises.opendir(fullPath))
+        counter++;
+    } else {
+      const dir = fs.opendirSync(fullPath);
+      while (dir.readSync() !== null)
+        counter++;
+      dir.closeSync();
     }
+  }
 
-    bench.end(counter);
-  })();
+  bench.end(counter);
 }
