eaa5cc5c95192d9c1dba3fa98404bed905e3d2e6
Anna Henningsen <anna@addaleax.net>
2019-10-11 18:38:50 +0200

    module: warn on using unfinished circular dependency
    Warn when a non-existent property of an unfinished module.exports
    object is being accessed, as that very often indicates the presence
    of a hard-to-detect and hard-to-debug problem.
    
    This mechanism is only used if `module.exports` is still a
    regular object at the point at which the second, circular `require()`
    happens.
    
    The downside is that, temporarily, `module.exports` will have a
    prototype other than `Object.prototype`, and that there may
    be valid uses of accessing non-existent properties of unfinished
    `module.exports` objects.
    
    Performance of circular require calls in general is not
    noticeably impacted.
    
                                                   confidence improvement accuracy (*)   (**)  (***)
         module/module-loader-circular.js n=10000                 3.96 %       ±5.12% ±6.82% ±8.89%
    
    Example:
    
        $ cat a.js
        'use strict';
        const b = require('./b.js');
    
        exports.fn = () => {};
        $ cat b.js
        'use strict';
        const a = require('./a.js');
    
        a.fn();
        $ node a.js
        (node:1617) Warning: Accessing non-existent property 'fn' of module exports inside circular dependency
        /tmp/b.js:4
        a.fn();
          ^
    
        TypeError: a.fn is not a function
            at Object.<anonymous> (/tmp/b.js:4:3)
            [...]

---
+++ test/fixtures/cycles/warning-b.js
@@ -0,0 +1,1 @@
+require('./warning-a.js').missingPropB;
---
+++ test/parallel/test-module-circular-dependency-warning.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const fixtures = require('../common/fixtures');
+
+common.expectWarning({
+  Warning: [
+    ["Accessing non-existent property 'missingPropB' " +
+     'of module exports inside circular dependency'],
+    ["Accessing non-existent property 'missingPropModuleExportsB' " +
+     'of module exports inside circular dependency']
+  ]
+});
+const required = require(fixtures.path('cycles', 'warning-a.js'));
+assert.strictEqual(Object.getPrototypeOf(required), Object.prototype);
+
+const requiredWithModuleExportsOverridden =
+  require(fixtures.path('cycles', 'warning-moduleexports-a.js'));
+assert.strictEqual(Object.getPrototypeOf(requiredWithModuleExportsOverridden),
+                   Object.prototype);
+
+// If module.exports is not a regular object, no warning should be emitted.
+const classExport =
+  require(fixtures.path('cycles', 'warning-moduleexports-class-a.js'));
+assert.strictEqual(Object.getPrototypeOf(classExport).name, 'Parent');
