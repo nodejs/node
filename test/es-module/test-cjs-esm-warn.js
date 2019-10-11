9153981ab54341d1eac75edd59757951b3f9ff1e
Guy Bedford <guybedford@gmail.com>
2019-10-11 15:19:18 -0400

    further warning text adjustments

--- test/es-module/test-cjs-esm-warn.js
+++ test/es-module/test-cjs-esm-warn.js
@@ -14,6 +14,8 @@ const pjson = path.resolve(
   fixtures.path('/es-modules/package-type-module/package.json')
 );
 
+const basename = 'cjs.js';
+
 const child = spawn(process.execPath, [requiring]);
 let stderr = '';
 child.stderr.setEncoding('utf8');
@@ -23,11 +25,14 @@ child.stderr.on('data', (data) => {
 child.on('close', common.mustCall((code, signal) => {
   assert.strictEqual(code, 0);
   assert.strictEqual(signal, null);
-  assert.strictEqual(stderr, `(node:${child.pid}) Warning: ${requiring} ` +
-    `contains a require() of ${required}, which is a .js file whose nearest ` +
-    'parent package.json contains "type": "module" which therefore defines ' +
-    'all .js files in that package scope as ES modules. require() of ES ' +
-    `modules is not allowed. Instead, rename ${required} to end in .cjs, or ` +
-    'change the requiring code to use import(), or remove "type": "module" ' +
-    `from ${pjson}.\n`);
+
+  assert.strictEqual(stderr, `(node:${child.pid}) Warning: ` +
+    'require() of ES modules is not supported.\nrequire() of ' +
+    `${required} from ${requiring} ` +
+    'is an ES module file as it is a .js file whose nearest parent ' +
+    'package.json contains "type": "module" which defines all .js ' +
+    'files in that package scope as ES modules.\nInstead rename ' +
+    `${basename} to end in .cjs, change the requiring code to use ` +
+    'import(), or remove "type": "module" from ' +
+    `${pjson}.\n`);
 }));
