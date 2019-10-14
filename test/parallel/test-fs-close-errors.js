e22efba812b2a6c2ee6d35f4e11af5b08afd881d
Matteo Rossi <teorossi82@yahoo.it>
2019-10-14 17:39:39 +0200

    test: add cb error test for fs.close()
    Provides some missing test coverage.
    
    PR-URL: https://github.com/nodejs/node/pull/29970
    Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-fs-close-errors.js
+++ test/parallel/test-fs-close-errors.js
@@ -17,3 +17,19 @@ const fs = require('fs');
   assert.throws(() => fs.close(input), errObj);
   assert.throws(() => fs.closeSync(input), errObj);
 });
+
+{
+  // Test error when cb is not a function
+  const fd = fs.openSync(__filename, 'r');
+
+  const errObj = {
+    code: 'ERR_INVALID_CALLBACK',
+    name: 'TypeError'
+  };
+
+  ['', false, null, {}, []].forEach((input) => {
+    assert.throws(() => fs.close(fd, input), errObj);
+  });
+
+  fs.closeSync(fd);
+}
