dc990cf02077006c5d8716d750dd6b21a226cb77
Anna Henningsen <anna@addaleax.net>
2019-10-27 01:49:32 +0200

    fixup! fs: add `bufferSize` option to `fs.opendir()`

--- test/parallel/test-fs-opendir.js
+++ test/parallel/test-fs-opendir.js
@@ -184,12 +184,19 @@ async function doAsyncIterThrowTest() {
 doAsyncIterThrowTest().then(common.mustCall());
 
 // Check error thrown on invalid values of bufferSize
-for (const bufferSize of [-1, 0, 0.5, 1.5, Infinity, NaN, '', '1', null]) {
-  assert.throws(() => fs.opendirSync(testDir, { bufferSize }),
-                {
-                  message: /The value ".*" is invalid for option "bufferSize"/,
-                  code: 'ERR_INVALID_OPT_VALUE'
-                });
+for (const bufferSize of [-1, 0, 0.5, 1.5, Infinity, NaN]) {
+  assert.throws(
+    () => fs.opendirSync(testDir, { bufferSize }),
+    {
+      code: 'ERR_OUT_OF_RANGE'
+    });
+}
+for (const bufferSize of ['', '1', null]) {
+  assert.throws(
+    () => fs.opendirSync(testDir, { bufferSize }),
+    {
+      code: 'ERR_INVALID_ARG_TYPE'
+    });
 }
 
 // Check that passing a positive integer as bufferSize works
