2e729f2dc9daf668c71e6c9ccc5b55f165f4ce23
Anna Henningsen <anna@addaleax.net>
2019-11-02 20:28:41 +0100

    src: make AtExit() callbacks run in reverse order
    This makes the actual behaviour match the documented (and arguably
    the correct) behaviour.
    
    PR-URL: https://github.com/nodejs/node/pull/30230
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>

--- test/cctest/test_environment.cc
+++ test/cctest/test_environment.cc
@@ -10,8 +10,12 @@ using node::RunAtExit;
 
 static bool called_cb_1 = false;
 static bool called_cb_2 = false;
+static bool called_cb_ordered_1 = false;
+static bool called_cb_ordered_2 = false;
 static void at_exit_callback1(void* arg);
 static void at_exit_callback2(void* arg);
+static void at_exit_callback_ordered1(void* arg);
+static void at_exit_callback_ordered2(void* arg);
 static std::string cb_1_arg;  // NOLINT(runtime/string)
 
 class EnvironmentTest : public EnvironmentTestFixture {
@@ -20,6 +24,8 @@ class EnvironmentTest : public EnvironmentTestFixture {
     NodeTestFixture::TearDown();
     called_cb_1 = false;
     called_cb_2 = false;
+    called_cb_ordered_1 = false;
+    called_cb_ordered_2 = false;
   }
 };
 
@@ -59,6 +65,19 @@ TEST_F(EnvironmentTest, AtExitWithoutEnvironment) {
   AtExit(at_exit_callback1);  // No Environment is passed to AtExit.
   RunAtExit(*env);
   EXPECT_TRUE(called_cb_1);
+}
+
+TEST_F(EnvironmentTest, AtExitOrder) {
+  const v8::HandleScope handle_scope(isolate_);
+  const Argv argv;
+  Env env {handle_scope, argv};
+
+  // Test that callbacks are run in reverse order.
+  AtExit(*env, at_exit_callback_ordered1);
+  AtExit(*env, at_exit_callback_ordered2);
+  RunAtExit(*env);
+  EXPECT_TRUE(called_cb_ordered_1);
+  EXPECT_TRUE(called_cb_ordered_2);
 }
 
 TEST_F(EnvironmentTest, AtExitWithArgument) {
@@ -133,4 +152,14 @@ static void at_exit_callback1(void* arg) {
 
 static void at_exit_callback2(void* arg) {
   called_cb_2 = true;
+}
+
+static void at_exit_callback_ordered1(void* arg) {
+  EXPECT_TRUE(called_cb_ordered_2);
+  called_cb_ordered_1 = true;
+}
+
+static void at_exit_callback_ordered2(void* arg) {
+  EXPECT_FALSE(called_cb_ordered_1);
+  called_cb_ordered_2 = true;
 }
