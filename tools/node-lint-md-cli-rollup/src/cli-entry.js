b20ddde2f67b27052bed231d66ac0affddcdd352
Derek Lewis <DerekNonGeneric@inf.is>
2019-11-02 04:35:32 -0400

    tools: enable Markdown linter's usage information
    Prior to this commit, running `node tools/lint-md --help` and
    `node tools/lint-md --version` resulted in an error.
    
    * Use `unified-args` to start processor
    * Use `unified-args`'s error handler
    
    Fixes: https://github.com/nodejs/node/issues/30156
    PR-URL: https://github.com/nodejs/node/pull/30216
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- tools/node-lint-md-cli-rollup/src/cli-entry.js
+++ tools/node-lint-md-cli-rollup/src/cli-entry.js
@@ -1,29 +1,29 @@
+#!/usr/bin/env node
 'use strict';
 
-const engine = require('unified-engine');
-const options = require('unified-args/lib/options');
+// To aid in future maintenance, this layout closely matches remark-cli/cli.js.
+// https://github.com/remarkjs/remark/blob/master/packages/remark-cli/cli.js
+
+const start = require('unified-args');
 const extensions = require('markdown-extensions');
 const processor = require('remark');
 const proc = require('remark/package.json');
 const cli = require('../package.json');
-const { plugins } = require('remark-preset-lint-node');
+const lintNode = require('remark-preset-lint-node');
 
-const args = {
-  processor: processor,
+start({
+  processor: processor().use(lintNode),
   name: proc.name,
   description: cli.description,
   version: [
     proc.name + ': ' + proc.version,
     cli.name + ': ' + cli.version,
   ].join(', '),
+  pluginPrefix: proc.name,
+  presetPrefix: proc.name + '-preset',
+  packageField: proc.name + 'Config',
+  rcName: '.' + proc.name + 'rc',
   ignoreName: '.' + proc.name + 'ignore',
-  extensions: extensions
-};
-const config = options(process.argv.slice(2), args);
-config.detectConfig = false;
-config.plugins = plugins;
-
-engine(config, (err, code) => {
-  if (err) console.error(err);
-  process.exit(code);
+  extensions: extensions,
+  detectConfig: false,
 });
