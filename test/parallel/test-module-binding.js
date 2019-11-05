7ec31437297a6ab17ea8bfde66e7698bb93ad36e
Guy Bedford <guybedford@gmail.com>
2019-11-04 22:59:57 -0500

    moar fixups

--- test/parallel/test-module-binding.js
+++ test/parallel/test-module-binding.js
@@ -5,12 +5,11 @@ const fixtures = require('../common/fixtures');
 const { internalBinding } = require('internal/test/binding');
 const { internalModuleReadJSON } = internalBinding('fs');
 const { readFileSync } = require('fs');
-const { deepEqual, strictEqual } = require('assert');
+const { strictEqual } = require('assert');
 
 strictEqual(internalModuleReadJSON('nosuchfile'), undefined);
 strictEqual(internalModuleReadJSON(fixtures.path('empty.txt')), '{}');
-strictEqual(internalModuleReadJSON(fixtures.path('empty-with-bom.txt')),
-            undefined);
+strictEqual(internalModuleReadJSON(fixtures.path('empty-with-bom.txt')), '{}');
 {
   const filename = fixtures.path('require-bin/package.json');
   strictEqual(internalModuleReadJSON(filename), readFileSync(filename, 'utf8'));
