d1ce04c592bcb00d034effd8d528bc67aaa2923c
dnlup <dwon.dnl@gmail.com>
2019-11-12 19:56:03 +0100

    benchmark: use let instead of var in assert
    PR-URL: https://github.com/nodejs/node/pull/30450
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- benchmark/assert/deepequal-set.js
+++ benchmark/assert/deepequal-set.js
@@ -24,7 +24,7 @@ function benchmark(method, n, values, values2) {
   const deepCopy = JSON.parse(JSON.stringify(values2 ? values2 : values));
   const expected = new Set(deepCopy);
   bench.start();
-  for (var i = 0; i < n; ++i) {
+  for (let i = 0; i < n; ++i) {
     method(actual, expected);
   }
   bench.end(n);
@@ -33,45 +33,49 @@ function benchmark(method, n, values, values2) {
 function main({ n, len, method, strict }) {
   const array = Array(len).fill(1);
 
-  var values, values2;
-
   switch (method) {
     case '':
       // Empty string falls through to next line as default, mostly for tests.
-    case 'deepEqual_primitiveOnly':
-      values = array.map((_, i) => `str_${i}`);
+    case 'deepEqual_primitiveOnly': {
+      const values = array.map((_, i) => `str_${i}`);
       benchmark(strict ? deepStrictEqual : deepEqual, n, values);
       break;
-    case 'deepEqual_objectOnly':
-      values = array.map((_, i) => [`str_${i}`, null]);
+    }
+    case 'deepEqual_objectOnly': {
+      const values = array.map((_, i) => [`str_${i}`, null]);
       benchmark(strict ? deepStrictEqual : deepEqual, n, values);
       break;
-    case 'deepEqual_mixed':
-      values = array.map((_, i) => {
+    }
+    case 'deepEqual_mixed': {
+      const values = array.map((_, i) => {
         return i % 2 ? [`str_${i}`, null] : `str_${i}`;
       });
       benchmark(strict ? deepStrictEqual : deepEqual, n, values);
       break;
-    case 'notDeepEqual_primitiveOnly':
-      values = array.map((_, i) => `str_${i}`);
-      values2 = values.slice(0);
+    }
+    case 'notDeepEqual_primitiveOnly': {
+      const values = array.map((_, i) => `str_${i}`);
+      const values2 = values.slice(0);
       values2[Math.floor(len / 2)] = 'w00t';
       benchmark(strict ? notDeepStrictEqual : notDeepEqual, n, values, values2);
       break;
-    case 'notDeepEqual_objectOnly':
-      values = array.map((_, i) => [`str_${i}`, null]);
-      values2 = values.slice(0);
+    }
+    case 'notDeepEqual_objectOnly': {
+      const values = array.map((_, i) => [`str_${i}`, null]);
+      const values2 = values.slice(0);
       values2[Math.floor(len / 2)] = ['w00t'];
       benchmark(strict ? notDeepStrictEqual : notDeepEqual, n, values, values2);
       break;
-    case 'notDeepEqual_mixed':
-      values = array.map((_, i) => {
+    }
+    case 'notDeepEqual_mixed': {
+      const values = array.map((_, i) => {
         return i % 2 ? [`str_${i}`, null] : `str_${i}`;
       });
-      values2 = values.slice();
+      const values2 = values.slice();
       values2[0] = 'w00t';
       benchmark(strict ? notDeepStrictEqual : notDeepEqual, n, values, values2);
       break;
+    }
     default:
       throw new Error(`Unsupported method "${method}"`);
   }
