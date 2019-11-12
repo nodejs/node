0130d2b6e0eac24a330a25cbef08cd6bcde346e1
dnlup <dwon.dnl@gmail.com>
2019-11-12 19:56:03 +0100

    benchmark: use let instead of var in assert
    PR-URL: https://github.com/nodejs/node/pull/30450
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- benchmark/assert/deepequal-prims-and-objs-big-array-set.js
+++ benchmark/assert/deepequal-prims-and-objs-big-array-set.js
@@ -26,7 +26,7 @@ const bench = common.createBenchmark(main, {
 
 function run(fn, n, actual, expected) {
   bench.start();
-  for (var i = 0; i < n; ++i) {
+  for (let i = 0; i < n; ++i) {
     fn(actual, expected);
   }
   bench.end(n);
@@ -38,7 +38,7 @@ function main({ n, len, primitive, method, strict }) {
   const expected = [];
   const expectedWrong = [];
 
-  for (var x = 0; x < len; x++) {
+  for (let x = 0; x < len; x++) {
     actual.push(prim);
     expected.push(prim);
     expectedWrong.push(prim);
