eead42470b3eb1be1066aa4027ae36fa60b7323f
dnlup <dwon.dnl@gmail.com>
2019-11-12 16:43:37 +0100

    test: use spread instead of Object.assign
    PR-URL: https://github.com/nodejs/node/pull/30419
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- test/benchmark/test-benchmark-dns.js
+++ test/benchmark/test-benchmark-dns.js
@@ -4,7 +4,6 @@ require('../common');
 
 const runBenchmark = require('../common/benchmark');
 
-const env = Object.assign({}, process.env,
-                          { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
+const env = { ...process.env, NODEJS_BENCHMARK_ZERO_ALLOWED: 1 };
 
 runBenchmark('dns', ['n=1', 'all=false', 'name=127.0.0.1'], env);
