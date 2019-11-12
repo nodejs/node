b05c0e694eab23a8b1e7af94aac667c26347a3bd
Duncan Healy <duncan.healy@gmail.com>
2019-11-12 19:20:22 +0000

    test: add function declaration tests for fat arrow

--- test/parallel/test-vm-function-declaration.js
+++ test/parallel/test-vm-function-declaration.js
@@ -31,6 +31,8 @@ const o = vm.createContext({ console });
 let code = 'let a = function() {};\n';
 code += 'function b(){}\n';
 code += 'var c = function() {};\n';
+code += 'var d = () => {};\n';
+code += 'let e = () => {};\n';
 
 // Grab the global b function as the completion value, to ensure that
 // we are getting the global function, and not some other thing
@@ -39,8 +41,9 @@ code += '(function(){return this})().b;\n';
 const res = vm.runInContext(code, o, 'test');
 assert.strictEqual(typeof res, 'function');
 assert.strictEqual(res.name, 'b');
-assert.strictEqual(typeof o.a, 'function');
+assert.strictEqual(typeof o.a, 'undefined');
 assert.strictEqual(typeof o.b, 'function');
 assert.strictEqual(typeof o.c, 'function');
-
+assert.strictEqual(typeof o.d, 'function');
+assert.strictEqual(typeof o.e, 'undefined');
 assert.strictEqual(res, o.b);
