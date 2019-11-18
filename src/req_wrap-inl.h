52a3e3560484ec3ad4479ef80d829f7c99d39e80
Anna Henningsen <anna@addaleax.net>
2019-11-18 14:49:56 +0100

    src: clean up node_file.h
    - Move inline functions into an `-inl.h` file
    - Move override function definitions into `.cc` files
    - Remove `using` statements from header files
    - Make data fields of classes private
    - Mark classes at the end of hierarchies as `final`
    
    This is also partially being done in an attempt to avoid
    a particular internal compiler error, see
    https://github.com/nodejs/node/pull/30475#issuecomment-554740850
    for details.
    
    PR-URL: https://github.com/nodejs/node/pull/30530
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/req_wrap-inl.h
+++ src/req_wrap-inl.h
@@ -120,7 +120,7 @@ struct MakeLibuvRequestCallback<ReqT, void(*)(ReqT*, Args...)> {
   using F = void(*)(ReqT* req, Args... args);
 
   static void Wrapper(ReqT* req, Args... args) {
-    ReqWrap<ReqT>* req_wrap = ContainerOf(&ReqWrap<ReqT>::req_, req);
+    ReqWrap<ReqT>* req_wrap = ReqWrap<ReqT>::from_req(req);
     req_wrap->env()->DecreaseWaitingRequestCounter();
     F original_callback = reinterpret_cast<F>(req_wrap->original_callback_);
     original_callback(req, args...);
