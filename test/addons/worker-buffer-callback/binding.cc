8252c06165959164921f37a589eeddf65975f089
Anna Henningsen <anna@addaleax.net>
2019-11-19 21:34:44 +0100

    buffer: release buffers with free callbacks on env exit
    Invoke the free callback for a given `Buffer` if it was created
    with one, and mark the underlying `ArrayBuffer` as detached.
    
    This makes sure that the memory is released e.g. when addons inside
    Workers create such `Buffer`s.
    
    PR-URL: https://github.com/nodejs/node/pull/30551
    Backport-PR-URL: https://github.com/nodejs/node/pull/31355
    Reviewed-By: Gabriel Schulhof <gabriel.schulhof@intel.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/addons/worker-buffer-callback/binding.cc
+++ test/addons/worker-buffer-callback/binding.cc
@@ -3,17 +3,24 @@
 #include <v8.h>
 
 using v8::Context;
+using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
 using v8::Object;
 using v8::Value;
 
+uint32_t free_call_count = 0;
 char data[] = "hello";
+
+void GetFreeCallCount(const FunctionCallbackInfo<Value>& args) {
+  args.GetReturnValue().Set(free_call_count);
+}
 
 void Initialize(Local<Object> exports,
                 Local<Value> module,
                 Local<Context> context) {
   Isolate* isolate = context->GetIsolate();
+  NODE_SET_METHOD(exports, "getFreeCallCount", GetFreeCallCount);
   exports->Set(context,
                v8::String::NewFromUtf8(
                    isolate, "buffer", v8::NewStringType::kNormal)
@@ -22,7 +29,9 @@ void Initialize(Local<Object> exports,
                    isolate,
                    data,
                    sizeof(data),
-                   [](char* data, void* hint) {},
+                   [](char* data, void* hint) {
+                     free_call_count++;
+                   },
                    nullptr).ToLocalChecked()).Check();
 }
 
