eb4b47f4694f5f28ad5c53be2748b9d99cf394be
Anna Henningsen <anna@addaleax.net>
2019-11-19 21:34:44 +0100

    buffer: release buffers with free callbacks on env exit
    Invoke the free callback for a given `Buffer` if it was created
    with one, and mark the underlying `ArrayBuffer` as detached.
    
    This makes sure that the memory is released e.g. when addons inside
    Workers create such `Buffer`s.
    
    PR-URL: https://github.com/nodejs/node/pull/30551
    Backport-PR-URL: https://github.com/nodejs/node/pull/31355
    Reviewed-By: Gabriel Schulhof <gabriel.schulhof@intel.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/cctest/test_environment.cc
+++ test/cctest/test_environment.cc
@@ -1,3 +1,4 @@
+#include "node_buffer.h"
 #include "node_internals.h"
 #include "libplatform/libplatform.h"
 
@@ -184,4 +185,35 @@ static void at_exit_js(void* arg) {
   assert(!obj.IsEmpty());  // Assert VM is still alive.
   assert(obj->IsObject());
   called_at_exit_js = true;
+}
+
+static char hello[] = "hello";
+
+TEST_F(EnvironmentTest, BufferWithFreeCallbackIsDetached) {
+  // Test that a Buffer allocated with a free callback is detached after
+  // its callback has been called.
+  const v8::HandleScope handle_scope(isolate_);
+  const Argv argv;
+
+  int callback_calls = 0;
+
+  v8::Local<v8::ArrayBuffer> ab;
+  {
+    Env env {handle_scope, argv};
+    v8::Local<v8::Object> buf_obj = node::Buffer::New(
+        isolate_,
+        hello,
+        sizeof(hello),
+        [](char* data, void* hint) {
+          CHECK_EQ(data, hello);
+          ++*static_cast<int*>(hint);
+        },
+        &callback_calls).ToLocalChecked();
+    CHECK(buf_obj->IsUint8Array());
+    ab = buf_obj.As<v8::Uint8Array>()->Buffer();
+    CHECK_EQ(ab->ByteLength(), sizeof(hello));
+  }
+
+  CHECK_EQ(callback_calls, 1);
+  CHECK_EQ(ab->ByteLength(), 0);
 }
