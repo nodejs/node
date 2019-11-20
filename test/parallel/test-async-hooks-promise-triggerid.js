50a71ad0812155ff76ff7fe60e71c8d01e500b28
Jeny <yenymzo@gmail.com>
2019-11-20 23:33:12 +0100

    fixes linter issue

--- test/parallel/test-async-hooks-promise-triggerid.js
+++ test/parallel/test-async-hooks-promise-triggerid.js
@@ -13,7 +13,8 @@ async_hooks.createHook({
     if (type === 'PROMISE') {
       // Check that the last known Promise is triggering the creation of
       // this one.
-      assert.strictEqual(triggerId, promiseAsyncIds[promiseAsyncIds.length - 1] || 1);
+      assert.strictEqual(triggerId,
+                         promiseAsyncIds[promiseAsyncIds.length - 1] || 1);
       promiseAsyncIds.push(id);
     }
   }, 3),
