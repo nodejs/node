9696e9edad69b484a860f9a9dd93f122f1443808
Rongjian Zhang <pd4d10@gmail.com>
2019-11-22 14:29:30 +0800

    module: fix specifier resolution algorithm
    Fixes: https://github.com/nodejs/node/issues/30520
    
    PR-URL: https://github.com/nodejs/node/pull/30574
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>

--- test/es-module/test-esm-specifiers.mjs
+++ test/es-module/test-esm-specifiers.mjs
@@ -1,6 +1,9 @@
 // Flags: --experimental-modules --experimental-specifier-resolution=node
 import { mustNotCall } from '../common/index.mjs';
 import assert from 'assert';
+import path from 'path';
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
 
 // commonJS index.js
 import commonjs from '../fixtures/es-module-specifiers/package-type-commonjs';
@@ -33,3 +36,24 @@ async function main() {
 }
 
 main().catch(mustNotCall);
+
+// Test path from command line arguments
+[
+  'package-type-commonjs',
+  'package-type-module',
+  '/',
+  '/index',
+].forEach((item) => {
+  const modulePath = path.join(
+    fileURLToPath(import.meta.url),
+    '../../fixtures/es-module-specifiers',
+    item,
+  );
+  spawn(process.execPath, [
+    '--experimental-modules',
+    '--es-module-specifier-resolution=node',
+    modulePath
+  ], { stdio: 'inherit' }).on('exit', (code) => {
+    assert.strictEqual(code, 0);
+  });
+});
