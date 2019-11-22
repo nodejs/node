924a9a871328d6a7c96766b29d1c099a90eacac7
Bradley Farias <bfarias@godaddy.com>
2019-11-22 10:34:31 -0600

    esm: data URLs should ignore unknown parameters
    PR-URL: https://github.com/nodejs/node/pull/30593
    Reviewed-By: Jan Krems <jan.krems@gmail.com>
    Reviewed-By: Guy Bedford <guybedford@gmail.com>

--- test/es-module/test-esm-data-urls.js
+++ test/es-module/test-esm-data-urls.js
@@ -38,6 +38,47 @@ function createBase64URL(mime, body) {
     assert.deepStrictEqual(ns.default, plainESMURL);
   }
   {
+    const body = 'export default import.meta.url;';
+    const plainESMURL = createURL('text/javascript;charset=UTF-8', body);
+    const ns = await import(plainESMURL);
+    assert.deepStrictEqual(Object.keys(ns), ['default']);
+    assert.deepStrictEqual(ns.default, plainESMURL);
+  }
+  {
+    const body = 'export default import.meta.url;';
+    const plainESMURL = createURL('text/javascript;charset="UTF-8"', body);
+    const ns = await import(plainESMURL);
+    assert.deepStrictEqual(Object.keys(ns), ['default']);
+    assert.deepStrictEqual(ns.default, plainESMURL);
+  }
+  {
+    const body = 'export default import.meta.url;';
+    const plainESMURL = createURL('text/javascript;;a=a;b=b;;', body);
+    const ns = await import(plainESMURL);
+    assert.deepStrictEqual(Object.keys(ns), ['default']);
+    assert.deepStrictEqual(ns.default, plainESMURL);
+  }
+  {
+    const ns = await import('data:application/json;foo="test,"this"');
+    assert.deepStrictEqual(Object.keys(ns), ['default']);
+    assert.deepStrictEqual(ns.default, 'this');
+  }
+  {
+    const ns = await import(`data:application/json;foo=${
+      encodeURIComponent('test,')
+    },0`);
+    assert.deepStrictEqual(Object.keys(ns), ['default']);
+    assert.deepStrictEqual(ns.default, 0);
+  }
+  {
+    await assert.rejects(async () => {
+      return import('data:application/json;foo="test,",0');
+    }, {
+      name: 'SyntaxError',
+      message: /Unexpected end of JSON input/
+    });
+  }
+  {
     const body = '{"x": 1}';
     const plainESMURL = createURL('application/json', body);
     const ns = await import(plainESMURL);
