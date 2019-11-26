2286fc0b0f4214fcdb8b40e5f4e5aaa194102b47
Anna Henningsen <anna@addaleax.net>
2019-11-26 17:08:11 +0100

    src: no SetImmediate from destructor in stream_pipe code
    Guard against running `SetImmediate()` from the destructor.
    The object will not be alive or usable in the callback,
    so it does not make sense to attempt to schedule the
    `SetImmediate()`.

--- src/stream_pipe.cc
+++ src/stream_pipe.cc
@@ -42,7 +42,7 @@ StreamPipe::StreamPipe(StreamBase* source,
 }
 
 StreamPipe::~StreamPipe() {
-  Unpipe();
+  Unpipe(true);
 }
 
 StreamBase* StreamPipe::source() {
@@ -53,7 +53,7 @@ StreamBase* StreamPipe::sink() {
   return static_cast<StreamBase*>(writable_listener_.stream());
 }
 
-void StreamPipe::Unpipe() {
+void StreamPipe::Unpipe(bool is_in_deletion) {
   if (is_closed_)
     return;
 
@@ -67,6 +67,8 @@ void StreamPipe::Unpipe() {
   is_reading_ = false;
   source()->RemoveStreamListener(&readable_listener_);
   sink()->RemoveStreamListener(&writable_listener_);
+
+  if (is_in_deletion) return;
 
   // Delay the JS-facing part with SetImmediate, because this might be from
   // inside the garbage collector, so we can’t run JS here.
