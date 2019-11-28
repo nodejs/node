81ac3023b3ab082804deda96d65530321d8af448
Juan José Arboleda <soyjuanarbol@gmail.com>
2019-11-27 20:26:36 -0500

    lib,test: improves ERR_REQUIRE_ESM message
    PR-URL: https://github.com/nodejs/node/pull/30694
    Fixes: https://github.com/nodejs/node/issues/30599
    Reviewed-By: Guy Bedford <guybedford@gmail.com>

--- test/es-module/test-cjs-esm-warn.js
+++ test/es-module/test-cjs-esm-warn.js
@@ -26,15 +26,19 @@ child.on('close', common.mustCall((code, signal) => {
   assert.strictEqual(code, 1);
   assert.strictEqual(signal, null);
 
-  assert.ok(stderr.startsWith(`(node:${child.pid}) Warning: ` +
-    'require() of ES modules is not supported.\nrequire() of ' +
+  assert.ok(stderr.indexOf(
+    `Error [ERR_REQUIRE_ESM]: Must use import to load ES Module: ${required}` +
+    '\nrequire() of ES modules is not supported.\nrequire() of ' +
     `${required} from ${requiring} ` +
     'is an ES module file as it is a .js file whose nearest parent ' +
     'package.json contains "type": "module" which defines all .js ' +
     'files in that package scope as ES modules.\nInstead rename ' +
     `${basename} to end in .cjs, change the requiring code to use ` +
     'import(), or remove "type": "module" from ' +
-    `${pjson}.\n`));
+    `${pjson}.\n`) !== -1);
   assert.ok(stderr.indexOf(
     'Error [ERR_REQUIRE_ESM]: Must use import to load ES Module') !== -1);
+
+  assert.strictEqual(
+    stderr.match(/Must use import to load ES Module/g).length, 1);
 }));
