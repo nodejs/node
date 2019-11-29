e968e26dbd1a0d5d9e777d6470b2a58806da4d86
Anna Henningsen <anna@addaleax.net>
2019-11-29 02:04:46 +0100

    stream: improve performance for sync write finishes
    Improve performance and reduce memory usage when a writable stream
    is written to with the same callback (which is the most common case)
    and when the write operation finishes synchronously (which is also
    often the case).
    
                                                             confidence improvement accuracy (*)    (**)   (***)
        streams/writable-manywrites.js sync='no' n=2000000                  0.99 %       ±3.20%  ±4.28%  ±5.61%
        streams/writable-manywrites.js sync='yes' n=2000000        ***    710.69 %      ±19.65% ±26.47% ±35.09%
    
    Refs: https://github.com/nodejs/node/issues/18013
    Refs: https://github.com/nodejs/node/issues/18367
    
    PR-URL: https://github.com/nodejs/node/pull/30710
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- benchmark/streams/writable-manywrites.js
+++ benchmark/streams/writable-manywrites.js
@@ -4,14 +4,19 @@ const common = require('../common');
 const Writable = require('stream').Writable;
 
 const bench = common.createBenchmark(main, {
-  n: [2e6]
+  n: [2e6],
+  sync: ['yes', 'no']
 });
 
-function main({ n }) {
+function main({ n, sync }) {
   const b = Buffer.allocUnsafe(1024);
   const s = new Writable();
+  sync = sync === 'yes';
   s._write = function(chunk, encoding, cb) {
-    cb();
+    if (sync)
+      cb();
+    else
+      process.nextTick(cb);
   };
 
   bench.start();
