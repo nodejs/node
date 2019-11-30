1698a53ed15f17ee376aee9686c8dd3281ccda24
qualitymanifest <qualitymanifest@gmail.com>
2019-11-29 23:11:21 -0500

    lib: add parent to ERR_UNKNOWN_FILE_EXTENSION
    - default_resolve updated to pass parentURL into error
    - ERR_UNKNOWN_FILE_EXTENSION updated to include parentURL
    - test added to check for import message in error
    
    PR-URL: https://github.com/nodejs/node/pull/30728
    Fixes: https://github.com/nodejs/node/issues/30721
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/es-module/test-esm-invalid-extension.js
+++ test/es-module/test-esm-invalid-extension.js
@@ -6,8 +6,10 @@ const { spawnSync } = require('child_process');
 const fixture = fixtures.path('/es-modules/import-invalid-ext.mjs');
 const child = spawnSync(process.execPath, ['--experimental-modules', fixture]);
 const errMsg = 'TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension';
+const importMsg = `imported from ${fixture}`;
 
 assert.strictEqual(child.status, 1);
 assert.strictEqual(child.signal, null);
 assert.strictEqual(child.stdout.toString().trim(), '');
 assert(child.stderr.toString().includes(errMsg));
+assert(child.stderr.toString().includes(importMsg));
