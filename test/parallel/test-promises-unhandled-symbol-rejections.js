8dfd99b6f78ec99c339c2bf8ee8a4e7dc89370d0
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-12-02 10:37:39 +0100

    change wording

--- test/parallel/test-promises-unhandled-symbol-rejections.js
+++ test/parallel/test-promises-unhandled-symbol-rejections.js
@@ -13,9 +13,9 @@ const expectedPromiseWarning = ['Unhandled promise rejection. ' +
   'This error originated either by throwing ' +
   'inside of an async function without a catch ' +
   'block, or by rejecting a promise which was ' +
-  'not handled with .catch(). If you want node ' +
-  'process to terminate on unhandled promise ' +
-  'rejection, use the CLI flag `--unhandled-rejections=strict` (see ' +
+  'not handled with .catch(). To terminate the ' +
+  'node process on unhandled promise rejection, ' +
+  'use the CLI flag `--unhandled-rejections=strict` (see ' +
   'https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)'];
 
 common.expectWarning({
