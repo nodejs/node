45b74fbb5baa29cd993be8085c561ea22f043abc
Rich Trott <rtrott@gmail.com>
2019-12-02 12:25:39 -0800

    test: improve wasi test coverage
    Add test coverage for options validation in WASI constructor.
    
    PR-URL: https://github.com/nodejs/node/pull/30770
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Yongsheng Zhang <zyszys98@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

---
+++ test/wasi/test-wasi-options-validation.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Flags: --experimental-wasi-unstable-preview0
+
+require('../common');
+const assert = require('assert');
+const { WASI } = require('wasi');
+
+// If args is undefined, it should default to [] and should not throw.
+new WASI({});
+
+// If args is not an Array and not undefined, it should throw.
+assert.throws(() => { new WASI({ args: 'fhqwhgads' }); },
+              { code: 'ERR_INVALID_ARG_TYPE' });
+
+// If env is not an Object and not undefined, it should throw.
+assert.throws(() => { new WASI({ env: 'fhqwhgads' }); },
+              { code: 'ERR_INVALID_ARG_TYPE' });
+
+// If preopens is not an Object and not undefined, it should throw.
+assert.throws(() => { new WASI({ preopens: 'fhqwhgads' }); },
+              { code: 'ERR_INVALID_ARG_TYPE' });
