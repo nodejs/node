ca47f7286851be913c9501c0d2d604dd35f88f9f
cjihrig <cjihrig@gmail.com>
2019-12-03 22:19:16 -0500

    deps: upgrade to libuv 1.34.0
    Notable changes:
    
    - Fix handling of large files in uv_fs_copyfile().
      Fixes: https://github.com/nodejs/node/issues/30085
    - Fix Android build errors.
    - uv_sleep() has been added.
    - uv_interface_addresses() IPv6 netmask support has been fixed.
      Fixes: https://github.com/nodejs/node/issues/30504
    - uv_fs_mkstemp() has been added.
    
    PR-URL: https://github.com/nodejs/node/pull/30783
    Fixes: https://github.com/nodejs/node/issues/30085
    Fixes: https://github.com/nodejs/node/issues/30504
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- deps/uv/src/random.c
+++ deps/uv/src/random.c
@@ -40,6 +40,8 @@ static int uv__random(void* buf, size_t buflen) {
   rc = uv__random_getentropy(buf, buflen);
   if (rc == UV_ENOSYS)
     rc = uv__random_devurandom(buf, buflen);
+#elif defined(__NetBSD__)
+  rc = uv__random_sysctl(buf, buflen);
 #elif defined(__FreeBSD__) || defined(__linux__)
   rc = uv__random_getrandom(buf, buflen);
   if (rc == UV_ENOSYS)
