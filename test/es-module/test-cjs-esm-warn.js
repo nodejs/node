17f02359508ff95afdf57cd31f2d5043342a148f
Guy Bedford <guybedford@gmail.com>
2019-12-05 01:19:26 -0500

    fixup windows bug attempt

--- test/es-module/test-cjs-esm-warn.js
+++ test/es-module/test-cjs-esm-warn.js
@@ -26,9 +26,9 @@ child.on('close', common.mustCall((code, signal) => {
   assert.strictEqual(code, 1);
   assert.strictEqual(signal, null);
 
-  assert.ok(stderr.indexOf(
+  assert.ok(stderr.replace(/\r/, '').indexOf(
     `Error [ERR_REQUIRE_ESM]: Must use import to load ES Module: ${required}` +
-    '\nrequire() of ES modules is not supported.\nrequire() of ' +
+    `\nrequire() of ES modules is not supported.\nrequire() of ` +
     `${required} from ${requiring} ` +
     'is an ES module file as it is a .js file whose nearest parent ' +
     'package.json contains "type": "module" which defines all .js ' +
