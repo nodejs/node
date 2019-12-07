7815ee6e79f640323b21b2f4e9671285dc97f5fd
ZYSzys <zyszys98@gmail.com>
2019-12-07 21:19:57 +0800

    module: fix require in node repl
    Fixes: https://github.com/nodejs/node/issues/30808

--- test/parallel/test-repl-require.js
+++ test/parallel/test-repl-require.js
@@ -11,28 +11,61 @@ if (!common.isMainThread)
 process.chdir(fixtures.fixturesDir);
 const repl = require('repl');
 
-const server = net.createServer((conn) => {
-  repl.start('', conn).on('exit', () => {
-    conn.destroy();
-    server.close();
+{
+  const server = net.createServer((conn) => {
+    repl.start('', conn).on('exit', () => {
+      conn.destroy();
+      server.close();
+    });
   });
-});
 
-const host = common.localhostIPv4;
-const port = 0;
-const options = { host, port };
+  const host = common.localhostIPv4;
+  const port = 0;
+  const options = { host, port };
 
-let answer = '';
-server.listen(options, function() {
-  options.port = this.address().port;
-  const conn = net.connect(options);
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => answer += data);
-  conn.write('require("baz")\nrequire("./baz")\n.exit\n');
-});
+  let answer = '';
+  server.listen(options, function() {
+    options.port = this.address().port;
+    const conn = net.connect(options);
+    conn.setEncoding('utf8');
+    conn.on('data', (data) => answer += data);
+    conn.write('require("baz")\nrequire("./baz")\n.exit\n');
+  });
 
-process.on('exit', function() {
-  assert.strictEqual(/Cannot find module/.test(answer), false);
-  assert.strictEqual(/Error/.test(answer), false);
-  assert.strictEqual(answer, '\'eye catcher\'\n\'perhaps I work\'\n');
-});
+  process.on('exit', function() {
+    assert.strictEqual(/Cannot find module/.test(answer), false);
+    assert.strictEqual(/Error/.test(answer), false);
+    assert.strictEqual(answer, '\'eye catcher\'\n\'perhaps I work\'\n');
+  });
+}
+
+// Test for https://github.com/nodejs/node/issues/30808
+// In REPL, we shouldn't look up relative modules from 'node_modules'.
+{
+  const server = net.createServer((conn) => {
+    repl.start('', conn).on('exit', () => {
+      conn.destroy();
+      server.close();
+    });
+  });
+
+  const host = common.localhostIPv4;
+  const port = 0;
+  const options = { host, port };
+
+  let answer = '';
+  server.listen(options, function() {
+    options.port = this.address().port;
+    const conn = net.connect(options);
+    conn.setEncoding('utf8');
+    conn.on('data', (data) => answer += data);
+    conn.write('require("./bar")\n.exit\n');
+  });
+
+  process.on('exit', function() {
+    assert.strictEqual(/Uncaught Error: Cannot find module '\.\/bar'/.test(answer), true);
+
+    assert.strictEqual(/code: 'MODULE_NOT_FOUND'/.test(answer), true);
+    assert.strictEqual(/requireStack: \[ '<repl>' \]/.test(answer), true);
+  });
+}
