d86bcd70e6b1ec68477176f8c7924b0fdd73e121
Matheus Marchini <mmarchini@netflix.com>
2019-12-09 11:27:35 -0800

    deps: V8: cherry-pick b38dfaf3a633
    Original commit message:
    
        [postmortem] update Symbol and *String metadata
    
        Symbol and *String classes are now declared on Torque with
        generateCppClass, which means they don't use macro accessors anymore. As
        such, the gen-postmortem-metadata script is not able to automatically
        detect fields for those classes. Define metadata for those fields
        manually for now. In the future we might want to generate it from Torque
        for consistency.
    
        Also renamed a few *String fields metadata to match the expected format
        (className__fieldName__fieldType). For more context:
        https://github.com/nodejs/llnode/issues/287#issuecomment-539707117.
    
        R=bmeurer@chromium.org, hpayer@chromium.org, verwaest@chromium.org, yangguo@chromium.org
    
        Change-Id: I82fe8315cdbfd1b8c64c6a8d5dc011b1edaec39e
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1847783
        Reviewed-by: Toon Verwaest <verwaest@chromium.org>
        Commit-Queue: Toon Verwaest <verwaest@chromium.org>
        Cr-Commit-Position: refs/heads/master@{#64313}
    
    Refs: https://github.com/v8/v8/commit/b38dfaf3a6338bf5e42703d0a93d21c2622765ce

--- deps/v8/tools/gen-postmortem-metadata.py
+++ deps/v8/tools/gen-postmortem-metadata.py
@@ -230,15 +230,6 @@ consts_misc = [
 
     { 'name': 'class_SharedFunctionInfo__function_data__Object',
         'value': 'SharedFunctionInfo::kFunctionDataOffset' },
-
-    { 'name': 'class_ConsString__first_offset__int',
-        'value': 'ConsString::kFirstOffset' },
-    { 'name': 'class_ConsString__second_offset__int',
-        'value': 'ConsString::kSecondOffset' },
-    { 'name': 'class_SlicedString__offset_offset__int',
-        'value': 'SlicedString::kOffsetOffset' },
-    { 'name': 'class_ThinString__actual_offset__int',
-        'value': 'ThinString::kActualOffset' },
 ];
 
 #
@@ -287,6 +278,11 @@ extras_accessors = [
     'Code, instruction_size, int, kInstructionSizeOffset',
     'String, length, int32_t, kLengthOffset',
     'DescriptorArray, header_size, uintptr_t, kHeaderSize',
+    'ConsString, first, String, kFirstOffset',
+    'ConsString, second, String, kSecondOffset',
+    'SlicedString, offset, SMI, kOffsetOffset',
+    'ThinString, actual, String, kActualOffset',
+    'Symbol, name, Object, kNameOffset',
 ];
 
 #
