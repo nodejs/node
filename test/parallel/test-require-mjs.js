0a1faa47ba6f0877ab7f9112fb272ea3118706e6
Wesley Wigham <wwigham@gmail.com>
2019-12-10 15:42:54 -0800

    Fix lints, again

--- test/parallel/test-require-mjs.js
+++ test/parallel/test-require-mjs.js
@@ -2,4 +2,4 @@
 require('../common');
 const assert = require('assert');
 
-assert.equal(require('../fixtures/es-modules/test-esm-ok.mjs').default, true);
+assert.strictEqual(require('../fixtures/es-modules/test-esm-ok.mjs').default, true);
