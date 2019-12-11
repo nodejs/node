910a555f2c6fb1442f6f0caa11d55aeed07d5e7f
Juan José Arboleda <soyjuanarbol@gmail.com>
2019-12-10 21:52:30 -0500

    fixup

--- src/uv.cc
+++ src/uv.cc
@@ -60,15 +60,15 @@ using v8::Value;
 
 void ErrName(const FunctionCallbackInfo<Value>& args) {
   Environment* env = Environment::GetCurrent(args);
-  if (env->options()->pending_deprecation && env->EmitErrNameWarning()) {
-    if (ProcessEmitDeprecationWarning(
-        env,
-        "Directly calling process.binding('uv').errname(<val>) is being"
-        " deprecated. "
-        "Please make sure to use util.getSystemErrorName() instead.",
-        "DEP0119").IsNothing())
-    return;
-  }
+//  if (env->options()->pending_deprecation && env->EmitErrNameWarning()) {
+//    if (ProcessEmitDeprecationWarning(
+//        env,
+//        "Directly calling process.binding('uv').errname(<val>) is being"
+//        " deprecated. "
+//        "Please make sure to use util.getSystemErrorName() instead.",
+//        "DEP0119").IsNothing())
+//    return;
+//  }
   int err;
   if (!args[0]->Int32Value(env->context()).To(&err)) return;
   CHECK_LT(err, 0);
