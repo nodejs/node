6c542d1c26decd53fa1154a5aa7a53b4199cad1f
Ruben Bridgewater <ruben@bridgewater.de>
2019-12-11 19:26:47 +0100

    repl: improve completion
    This improves the completion output by removing the nested special
    handling. It never fully worked as expected and required a lot of
    hacks to even keep it working halfway reliable. Our tests did not
    cover syntax errors though and those can not be handled by this
    implementation. Those break the layout and confuse the REPL.
    
    Besides that the completion now also works in case the current line
    has leading whitespace.
    
    Also improve the error output in case the completion fails.
    
    PR-URL: https://github.com/nodejs/node/pull/30907
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/fixtures/repl-tab-completion-nested-repls.js
+++ test/fixtures/repl-tab-completion-nested-repls.js
@@ -31,7 +31,7 @@ const repl = require('repl');
 const putIn = new ArrayStream();
 const testMe = repl.start('', putIn);
 
-// Some errors are passed to the domain, but do not callback
+// Some errors are passed to the domain, but do not callback.
 testMe._domain.on('error', function(err) {
   throw err;
 });
