8f5ffffb616c0308a8d5f3961517cd6936130cf8
Rich Trott <rtrott@gmail.com>
2019-12-12 14:20:46 -0500

    test: add test for validation for wasi.start() argument
    PR-URL: https://github.com/nodejs/node/pull/30919
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>

---
+++ test/wasi/test-wasi-start-validation.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('../common');
+const assert = require('assert');
+const { WASI } = require('wasi');
+
+const fixtures = require('../common/fixtures');
+
+{
+  const wasi = new WASI();
+  assert.throws(
+    () => {
+      wasi.start();
+    },
+    { code: 'ERR_INVALID_ARG_TYPE', message: /\bWebAssembly\.Instance\b/ }
+  );
+}
+
+{
+  const wasi = new WASI({});
+  (async () => {
+    const bufferSource = fixtures.readSync('simple.wasm');
+    const wasm = await WebAssembly.compile(bufferSource);
+    const instance = await WebAssembly.instantiate(wasm);
+
+    assert.throws(
+      () => { wasi.start(instance); },
+      { code: 'ERR_INVALID_ARG_TYPE', message: /\bWebAssembly\.Memory\b/ }
+    );
+  })();
+}
