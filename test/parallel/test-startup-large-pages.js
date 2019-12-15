6e0fd1c7a9a25edca9bd3f03a2d6b3f480010e29
Gabriel Schulhof <gabriel.schulhof@intel.com>
2019-12-14 22:42:40 -0800

    update test

--- test/parallel/test-startup-large-pages.js
+++ test/parallel/test-startup-large-pages.js
@@ -7,13 +7,25 @@ require('../common');
 const assert = require('assert');
 const { spawnSync } = require('child_process');
 
-const child = spawnSync(process.execPath, [ '--use-largepages', '-p', '42' ]);
-const stdout = child.stdout.toString().match(/\S+/g);
+{
+  const child = spawnSync(process.execPath,
+                          [ '--use-largepages=1', '-p', '42' ]);
+  const stdout = child.stdout.toString().match(/\S+/g);
+  assert.strictEqual(child.status, 0);
+  assert.strictEqual(child.signal, null);
+  assert.strictEqual(stdout.length, 1);
+  assert.strictEqual(stdout[0], '42');
+}
 
-assert.strictEqual(child.status, 0);
-assert.strictEqual(child.signal, null);
-assert.strictEqual(stdout.length, 1);
-assert.strictEqual(stdout[0], '42');
+{
+  const child = spawnSync(process.execPath,
+                          [ '--use-largepages=3', '-p', '42' ]);
+  assert.strictEqual(child.status, 9);
+  assert.strictEqual(child.signal, null);
+  assert.strictEqual(child.stderr.toString().match(/\S+/g).slice(1).join(' '),
+                     '--use-largepages must be one of 0, 1, or 2');
+}
 
-// TODO(gabrielschulhof): Make assertions about the stderr, which may or may not
-// contain a message indicating that mapping to large pages has failed.
+// TODO(gabrielschulhof): Run with --use-largepages=2 and make assertions about
+// the stderr, which may or may not contain a message indicating that mapping to
+// large pages has failed.
