73701b2564133f9bccbf032aeee24335e7c798b7
cjihrig <cjihrig@gmail.com>
2019-12-17 20:52:25 -0500

    test: fix long lines
    This commit addresses several lines that are unnecessarily
    longer than the 80 character limit. The only reason they pass
    linting, I believe, is because they contain URLs.
    
    PR-URL: https://github.com/nodejs/node/pull/31014
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Anto Aravinth <anto.aravinth.cse@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-promises-unhandled-symbol-rejections.js
+++ test/parallel/test-promises-unhandled-symbol-rejections.js
@@ -16,7 +16,8 @@ const expectedPromiseWarning = ['Unhandled promise rejection. ' +
   'not handled with .catch(). To terminate the ' +
   'node process on unhandled promise rejection, ' +
   'use the CLI flag `--unhandled-rejections=strict` (see ' +
-  'https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)'];
+  'https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). ' +
+  '(rejection id: 1)'];
 
 common.expectWarning({
   DeprecationWarning: expectedDeprecationWarning,
