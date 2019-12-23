1d3405b05a0d61c07234d511b04edbaa73ee0f9b
Denys Otrishko <shishugi@gmail.com>
2019-12-23 16:38:55 +0200

    test: log errors in test-http2-propagate-session-destroy-code
    PR-URL: https://github.com/nodejs/node/pull/31072
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Beth Griggs <Bethany.Griggs@uk.ibm.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-http2-propagate-session-destroy-code.js
+++ test/parallel/test-http2-propagate-session-destroy-code.js
@@ -15,16 +15,16 @@ server.on('error', common.mustNotCall());
 server.on('session', (session) => {
   session.on('close', common.mustCall());
   session.on('error', common.mustCall((err) => {
-    assert(errRegEx.test(err));
+    assert.ok(errRegEx.test(err), `server session err: ${err}`);
     assert.strictEqual(session.closed, false);
     assert.strictEqual(session.destroyed, true);
   }));
 
   session.on('stream', common.mustCall((stream) => {
     stream.on('error', common.mustCall((err) => {
+      assert.ok(errRegEx.test(err), `server stream err: ${err}`);
       assert.strictEqual(session.closed, false);
       assert.strictEqual(session.destroyed, true);
-      assert(errRegEx.test(err));
       assert.strictEqual(stream.rstCode, destroyCode);
     }));
 
@@ -36,7 +36,7 @@ server.listen(0, common.mustCall(() => {
   const session = http2.connect(`http://localhost:${server.address().port}`);
 
   session.on('error', common.mustCall((err) => {
-    assert(errRegEx.test(err));
+    assert.ok(errRegEx.test(err), `client session err: ${err}`);
     assert.strictEqual(session.closed, false);
     assert.strictEqual(session.destroyed, true);
   }));
@@ -44,7 +44,7 @@ server.listen(0, common.mustCall(() => {
   const stream = session.request({ [http2.constants.HTTP2_HEADER_PATH]: '/' });
 
   stream.on('error', common.mustCall((err) => {
-    assert(errRegEx.test(err));
+    assert.ok(errRegEx.test(err), `client stream err: ${err}`);
     assert.strictEqual(stream.rstCode, destroyCode);
   }));
 
