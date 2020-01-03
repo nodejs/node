63beed703af2b965bdab81a58724a768591212fe
Juan José Arboleda <soyjuanarbol@gmail.com>
2020-01-03 01:34:25 -0500

    fixup

--- test/parallel/test-process-isX-deprecation.js
+++ test/parallel/test-process-isX-deprecation.js
@@ -4,32 +4,30 @@ require('../common');
 const assert = require('assert');
 const { spawnSync } = require('child_process');
 
-{
-  [
-    'isArrayBuffer',
-    'isArrayBufferView',
-    'isAsyncFunction',
-    'isDataView',
-    'isDate',
-    'isExternal',
-    'isMap',
-    'isMapIterator',
-    'isNativeError',
-    'isPromise',
-    'isRegExp',
-    'isSet',
-    'isSetIterator',
-    'isTypedArray',
-    'isUint8Array',
-    'isAnyArrayBuffer'
-  ].forEach((method) => {
-    const { stderr } = spawnSync(process.execPath, ['-p', `
-      process.binding('util')['${method}']('meow');
-    `]);
-    const expectedWarning = 'Accessing native typechecking bindings ' +
-                            'of Node directly is deprecated. ' +
-                            `Please use \`util.types.${method}\` instead.`;
-    assert(stderr.includes('[DEP0103] DeprecationWarning'), stderr);
-    assert(stderr.includes(expectedWarning), stderr);
-  });
-}
+[
+  'isArrayBuffer',
+  'isArrayBufferView',
+  'isAsyncFunction',
+  'isDataView',
+  'isDate',
+  'isExternal',
+  'isMap',
+  'isMapIterator',
+  'isNativeError',
+  'isPromise',
+  'isRegExp',
+  'isSet',
+  'isSetIterator',
+  'isTypedArray',
+  'isUint8Array',
+  'isAnyArrayBuffer'
+].forEach((method) => {
+  const { stderr } = spawnSync(process.execPath, ['-p', `
+    process.binding('util')['${method}']('meow');
+  `]);
+  const expectedWarning = '[DEP0103] DeprecationWarning: ' +
+                          'Accessing native typechecking bindings ' +
+                          'of Node directly is deprecated. ' +
+                          `Please use \`util.types.${method}\` instead.`;
+  assert(stderr.includes(expectedWarning), stderr);
+});
