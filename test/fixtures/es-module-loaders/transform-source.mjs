21e28cbfabff4febf61cbe90bdd1ccd98ce0873d
Geoffrey Booth <webmaster@geoffreybooth.com>
2020-01-03 00:01:57 -0800

    module: all esm hooks should return objects

--- test/fixtures/es-module-loaders/transform-source.mjs
+++ test/fixtures/es-module-loaders/transform-source.mjs
@@ -1,7 +1,9 @@
 export async function transformSource(
   { url, format, source, defaultTransformSource }) {
   if (source && source.replace) {
-    return source.replace(`'A message';`, `'A message'.toUpperCase();`);
+    return {
+      source: source.replace(`'A message';`, `'A message'.toUpperCase();`)
+    };
   } else { // source could be a buffer, e.g. for WASM
     return defaultTransformSource(
       {url, format, source, defaultTransformSource});
