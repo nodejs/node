4c7193e04396963b0f943b197444a39342901085
Robert Nagy <ronagy@icloud.com>
2020-01-04 11:24:50 +0100

    fixup: simplify test

--- test/parallel/test-stream-readable-error-end.js
+++ test/parallel/test-stream-readable-error-end.js
@@ -5,21 +5,11 @@ const { Readable } = require('stream');
 const assert = require('assert');
 
 {
-  // Ensure 'end' is not emitted after 'error'.
-  // This test is slightly more complicated than
-  // needed in order to better illustrate the invariant.
-
   const r = new Readable({ read() {} });
 
-  let errorEmitted = false;
-
   r.on('data', common.mustCall());
-  r.on('end', () => {
-    assert.strictEqual(!errorEmitted);
-  });
-  r.on('error', () => {
-    errorEmitted = true;
-  });
+  r.on('end', common.mustNotCall());
+  r.on('error', common.mustCall());
   r.push('asd');
   r.push(null);
   r.destroy(new Error('kaboom'));
