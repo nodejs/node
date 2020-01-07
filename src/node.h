4259afe583a06f180a843d9559f8327c3c095ff1
Ben Noordhuis <info@bnoordhuis.nl>
2020-01-07 14:18:56 +0100

    src: remove node::InitializeV8Platform()
    This API method was introduced in commit 90ae4bd0c9 ("src: add
    InitializeV8Platform function") from July 2018 but wasn't properly
    exported and therefore not usable on Windows or with shared library
    builds.
    
    The motivation from the commit log is mainly about making it easier
    to wire up the cctests and there are better ways to do that.
    
    Refs: https://github.com/nodejs/node/pull/31217
    
    PR-URL: https://github.com/nodejs/node/pull/31245
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/node.h
+++ src/node.h
@@ -389,7 +389,6 @@ NODE_EXTERN MultiIsolatePlatform* GetMainThreadMultiIsolatePlatform();
 NODE_EXTERN MultiIsolatePlatform* CreatePlatform(
     int thread_pool_size,
     node::tracing::TracingController* tracing_controller);
-MultiIsolatePlatform* InitializeV8Platform(int thread_pool_size);
 NODE_EXTERN void FreePlatform(MultiIsolatePlatform* platform);
 
 NODE_EXTERN void EmitBeforeExit(Environment* env);
