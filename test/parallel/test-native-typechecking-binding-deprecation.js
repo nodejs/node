f047602a5d36a39a6a1864e8803044f52c110529
Juan José Arboleda <soyjuanarbol@gmail.com>
2020-01-08 18:35:54 -0500

    fixup

---
+++ test/parallel/test-native-typechecking-binding-deprecation.js
@@ -0,0 +1,36 @@
+'use strict';
+
+require('../common');
+const assert = require('assert');
+const { spawnSync } = require('child_process');
+const util = process.binding('util');
+
+[
+  'isArrayBuffer',
+  'isArrayBufferView',
+  'isAsyncFunction',
+  'isDataView',
+  'isDate',
+  'isExternal',
+  'isMap',
+  'isMapIterator',
+  'isNativeError',
+  'isPromise',
+  'isRegExp',
+  'isSet',
+  'isSetIterator',
+  'isTypedArray',
+  'isUint8Array',
+  'isAnyArrayBuffer'
+].forEach((method) => {
+//  const { stderr } = spawnSync(process.execPath, ['-p', `
+//    process.binding('util')['${method}']('meow');
+//  `]);
+//  const expectedWarning = '[DEP0103] DeprecationWarning: ' +
+//                          'Accessing native typechecking bindings ' +
+//                          'of Node directly is deprecated. ' +
+//                          `Please use \`util.types.${method}\` instead.`;
+//  assert(stderr.includes(expectedWarning), stderr);
+  util[method]('meow')
+  console.log(method)
+});
