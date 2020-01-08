acee3de9411d75c3f24354e687458292c07c05f1
Gerhard Stoebich <18708370+Flarna@users.noreply.github.com>
2020-01-08 14:08:54 +0100

    fixup! improve fixture

--- test/fixtures/uncaught-exceptions/uncaught-monitor.js
+++ test/fixtures/uncaught-exceptions/uncaught-monitor.js
@@ -1,7 +1,10 @@
 'use strict';
 
+// Keep the event loop alive.
+setTimeout(() => {}, 1e6);
+
 process.on(process.uncaughtExceptionMonitor, (err) => {
-	console.log(`Monitored: ${err.message}`);
+  console.log(`Monitored: ${err.message}`);
 });
 
 throw new Error('Shall exit');
