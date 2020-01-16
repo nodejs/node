8b62bce4c6d7b0a8f75603445b9c468c418ce7ed
Ruben Bridgewater <ruben@bridgewater.de>
2020-01-16 15:56:49 +0100

    test: refactor all benchmark tests to use the new test option

--- test/benchmark/test-benchmark-querystring.js
+++ test/benchmark/test-benchmark-querystring.js
@@ -4,9 +4,4 @@ require('../common');
 
 const runBenchmark = require('../common/benchmark');
 
-runBenchmark('querystring',
-             [ 'n=1',
-               'input="there is nothing to unescape here"',
-               'type=noencode'
-             ],
-             { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
+runBenchmark('querystring', { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
--- test/benchmark/test-benchmark-vm.js
+++ test/benchmark/test-benchmark-vm.js
@@ -4,10 +4,4 @@ require('../common');
 
 const runBenchmark = require('../common/benchmark');
 
-runBenchmark('vm',
-             [
-               'breakOnSigint=0',
-               'withSigintListener=0',
-               'n=1'
-             ],
-             { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
+runBenchmark('vm', { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
