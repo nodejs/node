5fac02f97a79e2dec6bcd8e74276a5e84bd7975b
Robert Nagy <ronagy@icloud.com>
2020-01-17 08:56:26 +0100

    fixup: test

--- test/parallel/test-file-write-stream4.js
+++ test/parallel/test-file-write-stream4.js
@@ -4,9 +4,9 @@ const path = require('path');
 const fs = require('fs');
 
 const tmpdir = require('../common/tmpdir');
+tmpdir.refresh();
 
 const filepath = path.join(tmpdir.path, 'write_pos.txt');
-fs.mkdirSync(tmpdir.path, { recursive: true });
 
 const fileReadStream = fs.createReadStream(process.execPath);
 const fileWriteStream = fs.createWriteStream(filepath, {
