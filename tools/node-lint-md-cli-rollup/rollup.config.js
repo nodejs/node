d9b72900d79332acaa185e917a71e8ea9037deeb
Derek Lewis <DerekNonGeneric@inf.is>
2020-01-19 19:42:27 -0500

    tools: update Markdown linter to be cross-platform
    Prior to this commit, the dependencies were not
    matching the build procedure. This has been
    corrected and it has the added benefit of being
    able to be built on Windows as well.
    
    * continue using `rollup` rather than `ncc`
    * do not require `fs-event`s for non-macOS
    * use `npx` and `shx` for cross-platform building
    * ensure `lint-md-rollup` runs before `lint-md`
    
    PR-URL: https://github.com/nodejs/node/pull/31239
    Reviewed-By: Rich Trott <rtrott@gmail.com>

---
+++ tools/node-lint-md-cli-rollup/rollup.config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
+
+module.exports = {
+  input: 'src/cli-entry.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: false,
+  },
+  external: [
+    'stream',
+    'path',
+    'module',
+    'util',
+    'tty',
+    'os',
+    'fs',
+    'fsevents',
+    'events',
+    'assert',
+  ],
+  plugins: [
+    {
+      name: 'brute-replace',
+      transform(code, id) {
+        const normID = id.replace(__dirname, '').replace(/\\+/g, '/');
+        if (normID === '/node_modules/concat-stream/index.js') {
+          return code.replace('\'readable-stream\'', '\'stream\'');
+        }
+        if (normID === '/node_modules/unified-args/lib/options.js') {
+          return code.replace('\'./schema\'', '\'./schema.json\'');
+        }
+        if (normID === '/node_modules/chokidar/lib/fsevents-handler.js' &&
+            process.platform !== 'darwin') {
+          return code.replace(
+            'fsevents = require(\'fsevents\');', 'fsevents = undefined;'
+          );
+        }
+      }
+    },
+    json({
+      preferConst: true
+    }),
+    resolve(), // tells Rollup how to find date-fns in node_modules
+    commonjs(), // Converts date-fns to ES modules
+  ]
+};
