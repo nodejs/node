978f47783a1704acf8a64e31ce67c40e628bcd45
cjihrig <cjihrig@gmail.com>
2020-01-20 18:25:40 -0500

    deps: update to uvwasi 0.0.5
    This version improves file descriptor renumbering, and as a result
    fixes uvwasi_fd_renumber().

--- deps/uvwasi/src/uv_mapping.h
+++ deps/uvwasi/src/uv_mapping.h
@@ -11,5 +11,6 @@ int uvwasi__translate_to_uv_signal(uvwasi_signal_t sig);
 uvwasi_timestamp_t uvwasi__timespec_to_timestamp(const uv_timespec_t* ts);
 uvwasi_filetype_t uvwasi__stat_to_filetype(const uv_stat_t* stat);
 void uvwasi__stat_to_filestat(const uv_stat_t* stat, uvwasi_filestat_t* fs);
+uvwasi_errno_t uvwasi__get_filetype_by_fd(uv_file fd, uvwasi_filetype_t* type);
 
 #endif /* __UVWASI_UV_MAPPING_H__ */
