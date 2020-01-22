efce2bd036a604b9f3f480357d89e55a39985a43
James M Snell <jasnell@gmail.com>
2020-01-22 08:07:25 -0800

    quic: add test-quic-openstream-errors
    PR-URL: https://github.com/nodejs/quic/pull/292
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

---
+++ test/parallel/test-quic-openstream-errors.js
@@ -0,0 +1,56 @@
+// Flags: --no-warnings
+'use strict';
+const common = require('../common');
+if (!common.hasQuic)
+  common.skip('missing quic');
+
+// Test errors thrown when openStream is called incorrectly
+// or is not permitted
+
+const assert = require('assert');
+const quic = require('quic');
+
+const Countdown = require('../common/countdown');
+const fixtures = require('../common/fixtures');
+const key = fixtures.readKey('agent1-key.pem', 'binary');
+const cert = fixtures.readKey('agent1-cert.pem', 'binary');
+const ca = fixtures.readKey('ca1-cert.pem', 'binary');
+
+const options = { key, cert, ca, alpn: 'zzz', maxStreamsUni: 0 };
+const server = quic.createSocket({ server: options });
+const client = quic.createSocket({ client: options });
+
+const countdown = new Countdown(1, () => {
+  server.close();
+  client.close();
+});
+
+server.listen();
+server.on('session', common.mustCall((session) => {
+  session.on('stream', common.mustNotCall());
+}));
+
+server.on('ready', common.mustCall(() => {
+  const req = client.connect({
+    address: 'localhost',
+    port: server.endpoints[0].address.port
+  });
+
+  ['z', 1, {}, [], null, Infinity, 1n, new Date()].forEach((i) => {
+    assert.throws(
+      () => req.openStream({ halfOpen: i }),
+      { code: 'ERR_INVALID_ARG_TYPE' }
+    );
+  });
+
+  // Unidirectional streams are not allowed. openStream will succeeed
+  // but the stream will be destroyed immediately.
+  req.openStream({ halfOpen: true })
+    .on('error', common.expectsError({
+      code: 'ERR_QUICSTREAM_OPEN_FAILED'
+    }))
+    .on('error', common.mustCall(() => countdown.dec()));
+
+}));
+
+server.on('close', common.mustCall());
