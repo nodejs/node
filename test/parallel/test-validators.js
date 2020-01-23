993c243008f07e07d32815bd7d8bc1c6c4daf5e9
Denys Otrishko <shishugi@gmail.com>
2020-01-23 20:42:10 +0200

    lib: improve value validation utils
    Add common validators: `validateArray`, `validateBoolean`,
    `validateObject` and appropriate tests.

--- test/parallel/test-validators.js
+++ test/parallel/test-validators.js
@@ -1,26 +1,87 @@
 // Flags: --expose-internals
 'use strict';
+
 require('../common');
 const assert = require('assert');
 const {
-  validateInteger
+  validateArray,
+  validateBoolean,
+  validateInteger,
+  validateObject,
 } = require('internal/validators');
 const { MAX_SAFE_INTEGER, MIN_SAFE_INTEGER } = Number;
 const outOfRangeError = {
   code: 'ERR_OUT_OF_RANGE',
-  name: 'RangeError'
+  name: 'RangeError',
+};
+const invalidArgTypeError = {
+  code: 'ERR_INVALID_ARG_TYPE',
+  name: 'TypeError',
+};
+const invalidArgValueError = {
+  code: 'ERR_INVALID_ARG_VALUE',
+  name: 'TypeError',
 };
 
-// validateInteger() defaults to validating safe integers.
-validateInteger(MAX_SAFE_INTEGER, 'foo');
-validateInteger(MIN_SAFE_INTEGER, 'foo');
-assert.throws(() => {
-  validateInteger(MAX_SAFE_INTEGER + 1, 'foo');
-}, outOfRangeError);
-assert.throws(() => {
-  validateInteger(MIN_SAFE_INTEGER - 1, 'foo');
-}, outOfRangeError);
+{
+  // validateInteger tests.
 
-// validateInteger() works with unsafe integers.
-validateInteger(MAX_SAFE_INTEGER + 1, 'foo', 0, MAX_SAFE_INTEGER + 1);
-validateInteger(MIN_SAFE_INTEGER - 1, 'foo', MIN_SAFE_INTEGER - 1);
+  // validateInteger() defaults to validating safe integers.
+  validateInteger(MAX_SAFE_INTEGER, 'foo');
+  validateInteger(MIN_SAFE_INTEGER, 'foo');
+  assert.throws(() => {
+    validateInteger(MAX_SAFE_INTEGER + 1, 'foo');
+  }, outOfRangeError);
+  assert.throws(() => {
+    validateInteger(MIN_SAFE_INTEGER - 1, 'foo');
+  }, outOfRangeError);
+
+  // validateInteger() works with unsafe integers.
+  validateInteger(MAX_SAFE_INTEGER + 1, 'foo', 0, MAX_SAFE_INTEGER + 1);
+  validateInteger(MIN_SAFE_INTEGER - 1, 'foo', MIN_SAFE_INTEGER - 1);
+}
+
+{
+  // validateArray tests.
+  validateArray([], 'foo');
+  validateArray([1, 2, 3], 'foo');
+
+  [undefined, null, true, false, 0, 0.0, 42, '', 'string', {}]
+    .forEach((val) => {
+      assert.throws(() => {
+        validateArray(val, 'foo');
+      }, invalidArgTypeError);
+    });
+
+  validateArray([1], 'foo', { minLength: 1 });
+  assert.throws(() => {
+    validateArray([], 'foo', { minLength: 1 });
+  }, invalidArgValueError);
+}
+
+{
+  // validateBoolean tests.
+  validateBoolean(true, 'foo');
+  validateBoolean(false, 'foo');
+
+  [undefined, null, 0, 0.0, 42, '', 'string', {}, []].forEach((val) => {
+    assert.throws(() => {
+      validateBoolean(val, 'foo');
+    }, invalidArgTypeError);
+  });
+}
+
+{
+  // validateObject tests.
+  validateObject({}, 'foo');
+  validateObject({ a: 42, b: 'foo' }, 'foo');
+
+  [undefined, null, true, false, 0, 0.0, 42, '', 'string', []]
+    .forEach((val) => {
+      assert.throws(() => {
+        validateObject(val, 'foo');
+      }, invalidArgTypeError);
+    });
+
+  validateObject(null, 'foo', { nullable: true });
+}
