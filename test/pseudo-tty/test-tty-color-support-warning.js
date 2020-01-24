5b44ea9080a449d7eb0842b97a2b6c20c1718785
Ruben Bridgewater <ruben@bridgewater.de>
2020-01-24 08:19:10 +0100

    fixup - include all ignored envs

--- test/pseudo-tty/test-tty-color-support-warning.js
+++ test/pseudo-tty/test-tty-color-support-warning.js
@@ -2,7 +2,8 @@
 
 require('../common');
 
-process.env.NODE_DISABLE_COLORS = 1;
+process.env.NO_COLOR = '1';
+process.env.NODE_DISABLE_COLORS = '1';
 process.env.FORCE_COLOR = '3';
 
 console.log();
