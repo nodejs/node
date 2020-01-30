a0021fda98f352461d121dc9d106fb5a4858a181
ConorDavenport <cnrdavenport@gmail.com>
2020-01-30 10:56:02 +0000

    fixup! Update src/node_internals.h

--- src/node_internals.h
+++ src/node_internals.h
@@ -264,7 +264,7 @@ class ThreadPoolWork {
   virtual void DoThreadPoolWork() = 0;
   virtual void AfterThreadPoolWork(int status) = 0;
 
-  Environment* env() const {
+  Environment* get_env() const {
     return env_;
   }
 
