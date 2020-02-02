7a88f7e4628da77524ef637fc24a05ecee4f83d0
bcoe <bencoe@google.com>
2020-02-02 13:48:41 -0800

    fs: set path when mkdir recursive called on file
    PR-URL: https://github.com/nodejs/node/pull/31607
    Fixes: https://github.com/nodejs/node/issues/28015
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>

--- test/parallel/test-fs-mkdir.js
+++ test/parallel/test-fs-mkdir.js
@@ -148,6 +148,7 @@ function nextdir() {
       message: /ENOTDIR: .*mkdir/,
       name: 'Error',
       syscall: 'mkdir',
+      path: pathname // See: https://github.com/nodejs/node/issues/28015
     }
   );
 }
@@ -187,6 +188,11 @@ function nextdir() {
     assert.strictEqual(err.code, 'ENOTDIR');
     assert.strictEqual(err.syscall, 'mkdir');
     assert.strictEqual(fs.existsSync(pathname), false);
+    // See: https://github.com/nodejs/node/issues/28015
+    // The path field varies slightly in Windows errors, vs., other platforms
+    // see: https://github.com/libuv/libuv/issues/2661, for this reason we
+    // use startsWith() rather than comparing to the full "pathname".
+    assert(err.path.startsWith(filename));
   }));
 }
 
