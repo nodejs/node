8964077935530bbf349544b1f29af68cec35ac39
Ben Noordhuis <info@bnoordhuis.nl>
2020-02-04 15:52:39 +0100

    src: fix OOB reads in process.title getter
    The getter passed a stack-allocated, fixed-size buffer to
    uv_get_process_title() but neglected to check the return value.
    
    When the total length of the command line arguments exceeds the size of
    the buffer, libuv returns UV_ENOBUFS and doesn't modify the contents of
    the buffer. The getter then proceeded to return whatever garbage was on
    the stack at the time of the call, quite possibly reading beyond the
    end of the buffer.
    
    Add a GetProcessTitle() helper that reads the process title into a
    dynamically allocated buffer that is resized when necessary.
    
    Fixes: https://github.com/nodejs/node/issues/31631
    
    PR-URL: https://github.com/nodejs/node/pull/31633
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/node_process_object.cc
+++ src/node_process_object.cc
@@ -32,11 +32,11 @@ using v8::Value;
 
 static void ProcessTitleGetter(Local<Name> property,
                                const PropertyCallbackInfo<Value>& info) {
-  char buffer[512];
-  uv_get_process_title(buffer, sizeof(buffer));
+  std::string title = GetProcessTitle("node");
   info.GetReturnValue().Set(
-      String::NewFromUtf8(info.GetIsolate(), buffer, NewStringType::kNormal)
-          .ToLocalChecked());
+      String::NewFromUtf8(info.GetIsolate(), title.data(),
+                          NewStringType::kNormal, title.size())
+      .ToLocalChecked());
 }
 
 static void ProcessTitleSetter(Local<Name> property,
