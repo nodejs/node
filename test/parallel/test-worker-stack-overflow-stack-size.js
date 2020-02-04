0d30c302b61b7d9ae0ebffdc3799fac59ccf46f8
Anna Henningsen <anna@addaleax.net>
2020-02-04 17:37:16 +0100

    fixup! worker: reset `Isolate` stack limit after entering `Locker`

--- test/parallel/test-worker-stack-overflow-stack-size.js
+++ test/parallel/test-worker-stack-overflow-stack-size.js
@@ -36,5 +36,8 @@ async function runWorker() {
   const w1stack = await runWorker();
   v8.setFlagsFromString('--stack-size=1000');
   const w2stack = await runWorker();
-  assert.strictEqual(w1stack, w2stack);
+  // Make sure the two stack sizes are within 10 % of each other, i.e. not
+  // affected by the different `--stack-size` settings.
+  assert(Math.max(w1stack, w2stack) / Math.min(w1stack, w2stack) < 1.1,
+         `w1stack = ${w1stack}, w2stack ${w2stack} are too far apart`);
 })().then(common.mustCall());
