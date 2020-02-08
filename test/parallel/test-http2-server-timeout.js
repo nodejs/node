77e5b509a90b3e72865fc0787a6c7673683d0313
Andrey Pechkurov <apechkurov@gmail.com>
2020-02-08 14:11:42 +0300

    doc,test: add server.timeout property to http2 public API
    Both http and https modules have server.timeout property
    in public API. This commit adds documentation section and test
    for server.timeout in http2 module, so it becomes consistent
    with http and https.
    
    Also improves description of callback argument in documentation
    for server.setTimeout().
    
    PR-URL: https://github.com/nodejs/node/pull/31693
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-http2-server-timeout.js
+++ test/parallel/test-http2-server-timeout.js
@@ -5,21 +5,27 @@ if (!common.hasCrypto)
   common.skip('missing crypto');
 const http2 = require('http2');
 
-const server = http2.createServer();
-server.setTimeout(common.platformTimeout(50));
+function testServerTimeout(setTimeoutFn) {
+  const server = http2.createServer();
+  setTimeoutFn(server);
 
-const onServerTimeout = common.mustCall((session) => {
-  session.close();
-});
+  const onServerTimeout = common.mustCall((session) => {
+    session.close();
+  });
 
-server.on('stream', common.mustNotCall());
-server.once('timeout', onServerTimeout);
+  server.on('stream', common.mustNotCall());
+  server.once('timeout', onServerTimeout);
 
-server.listen(0, common.mustCall(() => {
-  const url = `http://localhost:${server.address().port}`;
-  const client = http2.connect(url);
-  client.on('close', common.mustCall(() => {
-    const client2 = http2.connect(url);
-    client2.on('close', common.mustCall(() => server.close()));
+  server.listen(0, common.mustCall(() => {
+    const url = `http://localhost:${server.address().port}`;
+    const client = http2.connect(url);
+    client.on('close', common.mustCall(() => {
+      const client2 = http2.connect(url);
+      client2.on('close', common.mustCall(() => server.close()));
+    }));
   }));
-}));
+}
+
+const timeout = common.platformTimeout(50);
+testServerTimeout((server) => server.setTimeout(timeout));
+testServerTimeout((server) => server.timeout = timeout);
