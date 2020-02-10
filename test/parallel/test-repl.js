7a14ddf1045f7992dbf9da71f5570b3378afec90
Ruben Bridgewater <ruben@bridgewater.de>
2020-02-10 13:07:30 +0100

    test: increase test timeout to prevent flakiness
    This increases the waiting time for each event from 500 to 750 ms.
    The former timeout could be hit on very slow machines with high load.
    
    PR-URL: https://github.com/nodejs/node/pull/31716
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-repl.js
+++ test/parallel/test-repl.js
@@ -861,7 +861,7 @@ function event(ee, expected) {
       const data = inspect(expected, { compact: false });
       const msg = `The REPL did not reply as expected for:\n\n${data}`;
       reject(new Error(msg));
-    }, common.platformTimeout(500));
+    }, common.platformTimeout(1000));
     ee.once('data', common.mustCall((...args) => {
       clearTimeout(timeout);
       resolve(...args);
