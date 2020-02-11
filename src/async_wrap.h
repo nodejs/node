6f6bf010a7bfde32f4015758ae2f2af8b45cb612
Anna Henningsen <anna@addaleax.net>
2020-02-12 00:51:53 +0100

    src: use symbol to store `AsyncWrap` resource
    Use a symbol on the bindings object to store the public resource object,
    rather than a `v8::Global` Persistent. This has several advantages:
    
    - It’s harder to inadvertently create memory leaks this way.
      The garbage collector sees the `AsyncWrap` →  resource link like
      a regular JS property, and can collect the objects as a group,
      even if the resource object should happen to point back to the
      `AsyncWrap` object.
    - This will make it easier in the future to use `owner_symbol` for
      this purpose, which is generally the direction we should be moving
      the `async_hooks` API into (i.e. using more public objects instead
      of letting internal wires stick out).
    
    PR-URL: https://github.com/nodejs/node/pull/31745
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>

--- src/async_wrap.h
+++ src/async_wrap.h
@@ -152,7 +152,7 @@ class AsyncWrap : public BaseObject {
   static void EmitAfter(Environment* env, double async_id);
   static void EmitPromiseResolve(Environment* env, double async_id);
 
-  void EmitDestroy();
+  void EmitDestroy(bool from_gc = false);
 
   void EmitTraceEventBefore();
   static void EmitTraceEventAfter(ProviderType type, double async_id);
@@ -199,7 +199,6 @@ class AsyncWrap : public BaseObject {
                                         v8::Local<v8::Object> obj);
 
   bool IsDoneInitializing() const override;
-  v8::Local<v8::Object> GetResource();
 
  private:
   friend class PromiseWrap;
@@ -219,7 +218,6 @@ class AsyncWrap : public BaseObject {
   // Because the values may be Reset(), cannot be made const.
   double async_id_ = kInvalidAsyncId;
   double trigger_async_id_;
-  v8::Global<v8::Object> resource_;
 };
 
 }  // namespace node
