fac0992c285686dd51ab37478d031674003972c1
Christian Niederer <aqpoba@gmail.com>
2020-02-11 16:27:19 +0100

    src: Check for overflow when extending AliasedBufferBase
    When resizing an aliased_buffer check if the new size will overflow.

--- src/aliased_buffer.h
+++ src/aliased_buffer.h
@@ -221,7 +221,8 @@ class AliasedBufferBase {
     const v8::HandleScope handle_scope(isolate_);
 
     const size_t old_size_in_bytes = sizeof(NativeT) * count_;
-    const size_t new_size_in_bytes = sizeof(NativeT) * new_capacity;
+    const size_t new_size_in_bytes = MultiplyWithOverflowCheck(sizeof(NativeT),
+                                                              new_capacity);
 
     // allocate v8 new ArrayBuffer
     v8::Local<v8::ArrayBuffer> ab = v8::ArrayBuffer::New(
