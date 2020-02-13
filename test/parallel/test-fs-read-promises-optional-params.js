39239751e2b0348685fd6df37bb6879eecbb862a
Lucas Holmquist <lholmqui@redhat.com>
2020-02-13 15:45:44 -0500

    squash: add test for promise based

--- test/parallel/test-fs-read-promises-optional-params.js
+++ test/parallel/test-fs-read-promises-optional-params.js
@@ -3,6 +3,7 @@
 const common = require('../common');
 const fixtures = require('../common/fixtures');
 const fs = require('fs');
+const read = require('util').promisify(fs.read);
 const assert = require('assert');
 const filepath = fixtures.path('x.txt');
 const fd = fs.openSync(filepath, 'r');
@@ -10,7 +11,9 @@ const fd = fs.openSync(filepath, 'r');
 const expected = Buffer.from('xyz\n');
 const defaultBufferAsync = Buffer.alloc(16384);
 
-fs.read(fd, {}, common.mustCall((err, bytesRead, buffer) => {
-  assert.strictEqual(bytesRead, expected.length);
-  assert.deepStrictEqual(defaultBufferAsync.length, buffer.length);
-}));
+read(fd, {})
+  .then(function({ bytesRead, buffer }) {
+    assert.strictEqual(bytesRead, expected.length);
+    assert.deepStrictEqual(defaultBufferAsync.length, buffer.length);
+  })
+  .then(common.mustCall());
