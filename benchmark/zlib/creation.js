b556670d559a3da5425eb3c04d2071ac7e8b185e
Daniele Belardi <dwon.dnl@gmail.com>
2020-02-14 14:59:09 +0100

    benchmark: use let instead of var in zlib
    PR-URL: https://github.com/nodejs/node/pull/31794
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- benchmark/zlib/creation.js
+++ benchmark/zlib/creation.js
@@ -15,17 +15,16 @@ function main({ n, type, options }) {
   const fn = zlib[`create${type}`];
   if (typeof fn !== 'function')
     throw new Error('Invalid zlib type');
-  var i = 0;
 
   if (options === 'true') {
     const opts = {};
     bench.start();
-    for (; i < n; ++i)
+    for (let i = 0; i < n; ++i)
       fn(opts);
     bench.end(n);
   } else {
     bench.start();
-    for (; i < n; ++i)
+    for (let i = 0; i < n; ++i)
       fn();
     bench.end(n);
   }
