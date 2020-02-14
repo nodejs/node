425373722c56c0e80aebea6e6e9680dab3b2e0a5
Daniele Belardi <dwon.dnl@gmail.com>
2020-02-14 12:47:44 +0100

    benchmark: use const instead of var in async_hooks

--- benchmark/async_hooks/async-resource-vs-destroy.js
+++ benchmark/async_hooks/async-resource-vs-destroy.js
@@ -55,7 +55,7 @@ function buildCurrentResource(getServe) {
   }
 
   function init(asyncId, type, triggerAsyncId, resource) {
-    var cr = executionAsyncResource();
+    const cr = executionAsyncResource();
     if (cr !== null) {
       resource[cls] = cr[cls];
     }
