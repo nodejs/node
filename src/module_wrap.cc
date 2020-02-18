ecddf6519f8cc10168f1728c2812ee82e0f1a764
Guy Bedford <guybedford@gmail.com>
2020-02-18 12:43:10 +0200

    module: disable conditional exports, self resolve warnings
    PR-URL: https://github.com/nodejs/node/pull/31845
    Reviewed-By: Jan Krems <jan.krems@gmail.com>
    Reviewed-By: Geoffrey Booth <webmaster@geoffreybooth.com>
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/32959
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- src/module_wrap.cc
+++ src/module_wrap.cc
@@ -1047,7 +1047,6 @@ Maybe<URL> ResolveExportsTarget(Environment* env,
             return Nothing<URL>();
           }
           CHECK(!try_catch.HasCaught());
-          ProcessEmitExperimentalWarning(env, "Conditional exports");
           return resolved;
         }
       } else if (key_str == "default") {
@@ -1068,7 +1067,6 @@ Maybe<URL> ResolveExportsTarget(Environment* env,
             return Nothing<URL>();
           }
           CHECK(!try_catch.HasCaught());
-          ProcessEmitExperimentalWarning(env, "Conditional exports");
           return resolved;
         }
       }
@@ -1284,7 +1282,6 @@ Maybe<URL> PackageResolve(Environment* env,
       }
     }
     if (found_pjson && pcfg->name == pkg_name && !pcfg->exports.IsEmpty()) {
-      ProcessEmitExperimentalWarning(env, "Package name self resolution");
       if (pkg_subpath == "./") {
         return Just(URL("./", pjson_url));
       } else if (!pkg_subpath.length()) {
