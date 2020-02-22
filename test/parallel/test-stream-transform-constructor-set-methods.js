6f0ec79e42c07357fe33f353e65d76856d922582
Luigi Pinca <luigipinca@gmail.com>
2020-02-22 12:28:26 +0100

    http,stream: make virtual methods throw an error
    Make virtual methods throw an ERR_METHOD_NOT_IMPLEMENTED error instead
    of emitting it.
    
    The error is not recoverable and the only way to handle it is to
    override the method.
    
    PR-URL: https://github.com/nodejs/node/pull/31912
    Refs: https://github.com/nodejs/node/pull/31818#pullrequestreview-359403469
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-stream-transform-constructor-set-methods.js
+++ test/parallel/test-stream-transform-constructor-set-methods.js
@@ -1,18 +1,21 @@
 'use strict';
 const common = require('../common');
 
-const { strictEqual } = require('assert');
+const assert = require('assert');
 const { Transform } = require('stream');
 
 const t = new Transform();
 
-t.on('error', common.expectsError({
-  name: 'Error',
-  code: 'ERR_METHOD_NOT_IMPLEMENTED',
-  message: 'The _transform() method is not implemented'
-}));
-
-t.end(Buffer.from('blerg'));
+assert.throws(
+  () => {
+    t.end(Buffer.from('blerg'));
+  },
+  {
+    name: 'Error',
+    code: 'ERR_METHOD_NOT_IMPLEMENTED',
+    message: 'The _transform() method is not implemented'
+  }
+);
 
 const _transform = common.mustCall((chunk, _, next) => {
   next();
@@ -32,9 +35,9 @@ const t2 = new Transform({
   final: _final
 });
 
-strictEqual(t2._transform, _transform);
-strictEqual(t2._flush, _flush);
-strictEqual(t2._final, _final);
+assert.strictEqual(t2._transform, _transform);
+assert.strictEqual(t2._flush, _flush);
+assert.strictEqual(t2._final, _final);
 
 t2.end(Buffer.from('blerg'));
 t2.resume();
