6510a741c4ed04288d56486a1384b1fbecd08eaf
Andrey Pechkurov <apechkurov@gmail.com>
2020-02-24 13:00:59 +0300

    async_hooks: add store arg in AsyncLocalStorage
    This commit introduces store as the first argument in
    AsyncLocalStorage's run methods. The change is motivated by the
    following expectation: most users are going to use a custom object
    as the store and an extra Map created by the previous implementation
    is an overhead for their use case.
    
    Important note. This is a backwards incompatible change.
    It was discussed and agreed an incompatible change is ok
    since the API is still experimental and the modified
    methods were only added within the last week so usage
    will be minimal to none.
    
    PR-URL: https://github.com/nodejs/node/pull/31930
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: Vladimir de Turckheim <vlad2t@hotmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/async-hooks/test-async-local-storage-args.js
+++ test/async-hooks/test-async-local-storage-args.js
@@ -5,14 +5,14 @@ const { AsyncLocalStorage } = require('async_hooks');
 
 const asyncLocalStorage = new AsyncLocalStorage();
 
-asyncLocalStorage.run((runArg) => {
+asyncLocalStorage.run({}, (runArg) => {
   assert.strictEqual(runArg, 1);
   asyncLocalStorage.exit((exitArg) => {
     assert.strictEqual(exitArg, 2);
   }, 2);
 }, 1);
 
-asyncLocalStorage.runSyncAndReturn((runArg) => {
+asyncLocalStorage.runSyncAndReturn({}, (runArg) => {
   assert.strictEqual(runArg, 'foo');
   asyncLocalStorage.exitSyncAndReturn((exitArg) => {
     assert.strictEqual(exitArg, 'bar');
