3497370d66117f9f734c3307867ff21b7e5956de
Shelley Vohr <shelley.vohr@gmail.com>
2020-02-24 19:14:15 -0800

    src: move InternalCallbackScope to StartExecution
    PR-URL: https://github.com/nodejs/node/pull/31944
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

--- src/node_worker.cc
+++ src/node_worker.cc
@@ -341,11 +341,6 @@ void Worker::Run() {
         env_->InitializeInspector(std::move(inspector_parent_handle_));
 #endif
         HandleScope handle_scope(isolate_);
-        InternalCallbackScope callback_scope(
-            env_.get(),
-            Object::New(isolate_),
-            { 1, 0 },
-            InternalCallbackScope::kSkipAsyncHooks);
 
         if (!env_->RunBootstrapping().IsEmpty()) {
           CreateEnvMessagePort(env_.get());
