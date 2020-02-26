f86bdc1e7cf476aba6280ba3615e03d4a6f1fd53
Lucas Holmquist <lholmqui@redhat.com>
2020-02-26 12:26:14 -0500

    squash: add test for optional options option

---
+++ test/parallel/test-fs-read-optional-options-params.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const common = require('../common');
+const fixtures = require('../common/fixtures');
+const fs = require('fs');
+const assert = require('assert');
+const filepath = fixtures.path('x.txt');
+const fd = fs.openSync(filepath, 'r');
+
+const expected = Buffer.from('xyz\n');
+const defaultBufferAsync = Buffer.alloc(16384);
+
+fs.read(fd, common.mustCall((err, bytesRead, buffer) => {
+  assert.strictEqual(bytesRead, expected.length);
+  assert.deepStrictEqual(defaultBufferAsync.length, buffer.length);
+}));
