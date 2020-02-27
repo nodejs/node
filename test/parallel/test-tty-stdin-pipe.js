da67338fad163d9488148eea9cd790be77130620
Pranshu Srivastava <rexagod@gmail.com>
2020-02-28 02:21:38 +0530

    readline: rename input field and remove excess params
    Removed excess parameters from the createInterface method,
    renamed input field to options, in accordance with the docs,
    and made corresponding changes in tests.
    
    Refs: https://github.com/nodejs/node/pull/31603#issuecomment-581053704

--- test/parallel/test-tty-stdin-pipe.js
+++ test/parallel/test-tty-stdin-pipe.js
@@ -28,7 +28,9 @@ require('../common');
 const assert = require('assert');
 const readline = require('readline');
 
-const rl = readline.createInterface(process.stdin, process.stdout);
+const rl = readline.createInterface(
+  { input: process.stdin, output: process.stdout }
+);
 rl.resume();
 
 let hasPaused = false;
