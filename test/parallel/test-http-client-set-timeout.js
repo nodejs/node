1ffa9f388fbd4d732c9d91a0696225e65ba68fe9
Robert Nagy <ronagy@icloud.com>
2020-02-28 22:27:39 +0100

    http: fix socket re-use races
    Whether and when a socket is destroyed or not after a timeout is up to
    the user. This leaves an edge case where a socket that has emitted
    'timeout' might be re-used from the free pool. Even if destroy is called
    on the socket, it won't be removed from the freelist until 'close' which
    can happen several ticks later.
    
    Sockets are removed from the free list on the 'close' event.
    However, there is a delay between calling destroy() and 'close'
    being emitted. This means that it possible for a socket that has
    been destroyed to be re-used from the free list, causing unexpected
    failures.
    
    PR-URL: https://github.com/nodejs/node/pull/32000
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-http-client-set-timeout.js
+++ test/parallel/test-http-client-set-timeout.js
@@ -42,7 +42,7 @@ server.listen(0, mustCall(() => {
   }));
 
   req.on('timeout', mustCall(() => {
-    strictEqual(req.socket.listenerCount('timeout'), 0);
+    strictEqual(req.socket.listenerCount('timeout'), 1);
     req.destroy();
   }));
 }));
