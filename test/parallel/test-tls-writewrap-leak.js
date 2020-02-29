b40a103eab79f98a240ff615dc3066cae9fbe849
Robert Nagy <ronagy@icloud.com>
2020-02-29 11:33:27 +0100

    fixup: reduce changes

--- test/parallel/test-tls-writewrap-leak.js
+++ test/parallel/test-tls-writewrap-leak.js
@@ -12,15 +12,13 @@ const server = net.createServer(common.mustCall((c) => {
   c.destroy();
 })).listen(0, common.mustCall(() => {
   const c = tls.connect({ port: server.address().port });
-
-  c.on('error', common.mustCall((err) => {
-    assert.strictEqual(err.code, 'ECONNRESET');
-    server.close();
-  }));
+  c.on('error', () => {
+    // Otherwise `.write()` callback won't be invoked.
+    c._undestroy();
+  });
 
   c.write('hello', common.mustCall((err) => {
-    // TODO
-    // assert.strictEqual(err.code, 'ECANCELED');
+    assert.strictEqual(err.code, 'ECANCELED');
     server.close();
   }));
 }));
