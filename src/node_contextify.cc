d19a2c33b35b3256a738966231848a6679d7bb7d
gengjiawen <technicalcute@gmail.com>
2020-03-04 19:43:37 +0800

    src: migrate measureMemory to new v8 api
    PR-URL: https://github.com/nodejs/node/pull/32116
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- src/node_contextify.cc
+++ src/node_contextify.cc
@@ -1224,15 +1224,18 @@ static void MeasureMemory(const FunctionCallbackInfo<Value>& args) {
                                                           args[1].As<Object>());
     CHECK_NOT_NULL(sandbox);
     context = sandbox->context();
-    if (context.IsEmpty()) {  // Not yet fully initilaized
+    if (context.IsEmpty()) {  // Not yet fully initialized
       return;
     }
   }
-  v8::Local<v8::Promise> promise;
-  if (!isolate->MeasureMemory(context, static_cast<v8::MeasureMemoryMode>(mode))
-           .ToLocal(&promise)) {
-    return;
-  }
+  Local<Promise::Resolver> resolver;
+  if (!Promise::Resolver::New(context).ToLocal(&resolver)) return;
+  std::unique_ptr<v8::MeasureMemoryDelegate> i =
+      v8::MeasureMemoryDelegate::Default(
+          isolate, context, resolver, static_cast<v8::MeasureMemoryMode>(mode));
+  CHECK_NOT_NULL(i);
+  v8::Local<v8::Promise> promise = resolver->GetPromise();
+
   args.GetReturnValue().Set(promise);
 }
 
