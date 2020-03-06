cb8c0ef9b80432156a92d58791554c10bf3d147b
Lucas Holmquist <lholmqui@redhat.com>
2020-03-06 09:49:41 -0500

    squash: combine tests and add another test that passes options

--- test/parallel/test-fs-read-optional-params.js
+++ test/parallel/test-fs-read-optional-params.js
@@ -9,8 +9,28 @@ const fd = fs.openSync(filepath, 'r');
 
 const expected = Buffer.from('xyz\n');
 const defaultBufferAsync = Buffer.alloc(16384);
+const bufferAsOption = Buffer.allocUnsafe(expected.length);
 
-fs.read(fd, {}, common.mustCall((err, bytesRead, buffer) => {
+// Test passing in an empty options object
+fs.read(fd, { position: 0 }, common.mustCall((err, bytesRead, buffer) => {
   assert.strictEqual(bytesRead, expected.length);
   assert.deepStrictEqual(defaultBufferAsync.length, buffer.length);
 }));
+
+// Test not passing in any options object
+fs.read(fd, common.mustCall((err, bytesRead, buffer) => {
+  assert.strictEqual(bytesRead, expected.length);
+  assert.deepStrictEqual(defaultBufferAsync.length, buffer.length);
+}));
+
+// Test passing in options
+fs.read(fd, {
+  buffer: bufferAsOption,
+  offset: 0,
+  lenght: bufferAsOption.length,
+  position: 0
+},
+        common.mustCall((err, bytesRead, buffer) => {
+          assert.strictEqual(bytesRead, expected.length);
+          assert.deepStrictEqual(bufferAsOption.length, buffer.length);
+        }));
