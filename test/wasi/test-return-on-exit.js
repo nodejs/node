3649a7e9ff4decf38c05f24e234ef9261cd3fcea
cjihrig <cjihrig@gmail.com>
2020-03-09 11:20:45 -0400

    test: verify that WASI errors are rethrown
    This commit adds a test to verify that exceptions thrown from a
    WASI application are properly caught and rethrown. This also
    gets the code coverage in lib/wasi.js back to 100%.
    
    PR-URL: https://github.com/nodejs/node/pull/32157
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/wasi/test-return-on-exit.js
+++ test/wasi/test-return-on-exit.js
@@ -5,14 +5,27 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const { WASI } = require('wasi');
-const wasi = new WASI({ returnOnExit: true });
-const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
 const wasmDir = path.join(__dirname, 'wasm');
 const modulePath = path.join(wasmDir, 'exitcode.wasm');
 const buffer = fs.readFileSync(modulePath);
 
 (async () => {
+  const wasi = new WASI({ returnOnExit: true });
+  const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
   const { instance } = await WebAssembly.instantiate(buffer, importObject);
 
   assert.strictEqual(wasi.start(instance), 120);
+})().then(common.mustCall());
+
+(async () => {
+  // Verify that if a WASI application throws an exception, Node rethrows it
+  // properly.
+  const wasi = new WASI({ returnOnExit: true });
+  wasi.wasiImport.proc_exit = () => { throw new Error('test error'); };
+  const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
+  const { instance } = await WebAssembly.instantiate(buffer, importObject);
+
+  assert.throws(() => {
+    wasi.start(instance);
+  }, /^Error: test error$/);
 })().then(common.mustCall());
