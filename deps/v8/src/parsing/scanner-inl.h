75da64c0587f2925319cd9c1717a19b647b93511
Matheus Marchini <mmarchini@netflix.com>
2020-03-10 10:45:10 -0700

    deps: V8: cherry-pick f9257802c1c0
    Original commit message:
    
        Fix scanner-level error reporting for hashbang
    
        When the file begins with a hashbang, the scanner is in a failed state
        when SkipHashbang() is called. This is usually not an issue but when
        the parser encounters an ILLEGAL token, it will reset the SyntaxError
        location because of it.
    
        Bug: v8:10110
        Change-Id: I1c7344bf5ad20079cff80130c991f3bff4d7e9a8
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/1995312
        Reviewed-by: Toon Verwaest <verwaest@chromium.org>
        Commit-Queue: Toon Verwaest <verwaest@chromium.org>
        Cr-Commit-Position: refs/heads/master@{#66038}
    
    Refs: https://github.com/v8/v8/commit/f9257802c1c0a0663a1d05bacf662283f6bd9050
    Fixes: https://github.com/nodejs/node/issues/31284
    Signed-off-by: Matheus Marchini <mmarchini@netflix.com>
    
    PR-URL: https://github.com/nodejs/node/pull/32180
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- deps/v8/src/parsing/scanner-inl.h
+++ deps/v8/src/parsing/scanner-inl.h
@@ -505,6 +505,10 @@ V8_INLINE Token::Value Scanner::ScanSingleToken() {
           return ScanTemplateSpan();
 
         case Token::PRIVATE_NAME:
+          if (source_pos() == 0 && Peek() == '!') {
+            token = SkipSingleLineComment();
+            continue;
+          }
           return ScanPrivateName();
 
         case Token::WHITESPACE:
