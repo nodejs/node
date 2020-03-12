4a08b85fc8d71ff4e0a669b280dd250f93e5030c
Matheus Marchini <mmarchini@netflix.com>
2020-03-12 15:40:21 -0700

    test: make test-memory-usage predictable
    The current test relies on undeterministic behavior from V8 GC, and on
    newer versions of V8 this test ocasionally fails because that behavior
    changed.  Prevent that from happening using --predictable-gc-schedule.
    If this test fails in the future, it should fail consistently instead of
    ocasionally, which should help debug.
    
    Ref: https://github.com/nodejs/node-v8/issues/144#issuecomment-597945219
    Signed-off-by: Matheus Marchini <mmarchini@netflix.com>
    
    PR-URL: https://github.com/nodejs/node/pull/32239
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>

--- test/parallel/test-memory-usage.js
+++ test/parallel/test-memory-usage.js
@@ -19,7 +19,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Flags: --no-concurrent-array-buffer-freeing
+// Flags: --predictable-gc-schedule
 'use strict';
 const common = require('../common');
 const assert = require('assert');
