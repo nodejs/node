f9dfae6480b71d8695ce4a4d872a6b6f62557e3a
Robert Nagy <ronagy@icloud.com>
2020-03-12 08:18:39 +0100

    zlib: align with streams
    - Ensure automatic destruction only happens after both
    'end' and 'finish' has been emitted through autoDestroy.
    - Ensure close() callback is always invoked.
    - Ensure 'error' is only emitted once.

--- test/parallel/test-zlib-destroy.js
+++ test/parallel/test-zlib-destroy.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 
 const assert = require('assert');
 const zlib = require('zlib');
@@ -8,6 +8,24 @@ const zlib = require('zlib');
 // Verify that the zlib transform does clean up
 // the handle when calling destroy.
 
-const ts = zlib.createGzip();
-ts.destroy();
-assert.strictEqual(ts._handle, null);
+{
+  const ts = zlib.createGzip();
+  ts.destroy();
+  assert.strictEqual(ts._handle, null);
+
+  ts.on('close', common.mustCall(() => {
+    ts.close(common.mustCall());
+  }));
+}
+
+{
+  // Ensure 'error' is only emitted once.
+  const decompress = zlib.createGunzip(15);
+
+  decompress.on('error', common.mustCall((err) => {
+    decompress.close();
+  }));
+
+  decompress.write('something invalid');
+  decompress.destroy(new Error('asd'));
+}
