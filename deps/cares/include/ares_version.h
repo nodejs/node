3825afed74ba3f295811914f62b298d9f266e703
Anna Henningsen <anna@addaleax.net>
2020-03-13 09:08:40 +0100

    deps: upgrade to c-ares v1.16.0
    Refs: https://github.com/c-ares/c-ares/releases/tag/cares-1_16_0
    
    PR-URL: https://github.com/nodejs/node/pull/32246
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: David Carlier <devnexen@gmail.com>

--- deps/cares/include/ares_version.h
+++ deps/cares/include/ares_version.h
@@ -3,15 +3,15 @@
 #define ARES__VERSION_H
 
 /* This is the global package copyright */
-#define ARES_COPYRIGHT "2004 - 2018 Daniel Stenberg, <daniel@haxx.se>."
+#define ARES_COPYRIGHT "2004 - 2020 Daniel Stenberg, <daniel@haxx.se>."
 
 #define ARES_VERSION_MAJOR 1
-#define ARES_VERSION_MINOR 15
+#define ARES_VERSION_MINOR 16
 #define ARES_VERSION_PATCH 0
 #define ARES_VERSION ((ARES_VERSION_MAJOR<<16)|\
                        (ARES_VERSION_MINOR<<8)|\
                        (ARES_VERSION_PATCH))
-#define ARES_VERSION_STR "1.15.0"
+#define ARES_VERSION_STR "1.16.0"
 
 #if (ARES_VERSION >= 0x010700)
 #  define CARES_HAVE_ARES_LIBRARY_INIT 1
