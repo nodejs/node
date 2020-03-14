eeccd52b4ef2db3219fc76737bb00c1da669aea2
Robert Nagy <ronagy@icloud.com>
2020-03-14 23:14:52 +0100

    net: make readable/writable start as true
    `net.Socket` is slightly breaking stream invariants by
    having readable/writable going from `false` to `true`.
    Streams assume that readable/writable starts out `true`
    and then goes to `false` through `push(null)`/`end()`
    after which it never goes back to `true`, e.g. once a
    stream is `writable == false` it is assumed it will
    never become `true`.
    
    This PR changes 2 things:
    
    Unless explicitly set to `false` through options:
    
    - starts as `readable`/`writable` `true` by default.
    - uses `push(null)`/`end()` to set `readable`/`writable`
      to `false`. Note that this would cause the socket to
      emit the `'end'`/`'finish'` events, which it did not
      do previously.
    
    In the case it is explicitly set to `false` through
    options` it is assumed to never become `true`.
    
    PR-URL: https://github.com/nodejs/node/pull/32272
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-net-socket-constructor.js
+++ test/parallel/test-net-socket-constructor.js
@@ -27,12 +27,12 @@ function test(sock, readable, writable) {
 }
 
 if (cluster.isMaster) {
-  test(undefined, false, false);
+  test(undefined, true, true);
 
   const server = net.createServer(common.mustCall((socket) => {
     socket.unref();
     test(socket, true, true);
-    test({ handle: socket._handle }, false, false);
+    test({ handle: socket._handle }, true, true);
     test({ handle: socket._handle, readable: true, writable: true },
          true, true);
     server.close();
@@ -45,7 +45,7 @@ if (cluster.isMaster) {
       socket.end();
     }));
 
-    test(socket, false, true);
+    test(socket, true, true);
   }));
 
   cluster.setupMaster({
@@ -58,8 +58,8 @@ if (cluster.isMaster) {
     assert.strictEqual(signal, null);
   }));
 } else {
-  test(4, false, false);
-  test({ fd: 5 }, false, false);
+  test(4, true, true);
+  test({ fd: 5 }, true, true);
   test({ fd: 6, readable: true, writable: true }, true, true);
   process.disconnect();
 }
