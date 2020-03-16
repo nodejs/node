7dbc5dea7cfd8a87f3019b94002923c4d78495ee
Harshitha KP <harshi46@in.ibm.com>
2020-03-16 09:21:16 -0400

    fixup: fix linter error

--- test/report/test-report-fatal-error.js
+++ test/report/test-report-fatal-error.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const common = require('../common');
+require('../common');
 const assert = require('assert');
 // Testcase to produce report on fatal error (javascript heap OOM)
 if (process.argv[2] === 'child') {
