8083d452e657e16a12f9a0d98bb22ffce0e5aed8
Harshitha KP <harshi46@in.ibm.com>
2020-03-16 12:28:54 -0400

    test: add a missing common.mustCall
    PR-URL: https://github.com/nodejs/node/pull/32305
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- test/parallel/test-process-getactiverequests.js
+++ test/parallel/test-process-getactiverequests.js
@@ -1,10 +1,10 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const fs = require('fs');
 
 for (let i = 0; i < 12; i++)
-  fs.open(__filename, 'r', () => {});
+  fs.open(__filename, 'r', common.mustCall());
 
 assert.strictEqual(process._getActiveRequests().length, 12);
