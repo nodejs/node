97e352d1a6e54cab81b8fad005b5ed9fe0658a61
Rich Trott <rtrott@gmail.com>
2020-03-19 16:12:41 -0700

    test: replace countdown with Promise.all() in cluster-net-listen tests
    PR-URL: https://github.com/nodejs/node/pull/32381
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/sequential/test-cluster-net-listen-ipv6only-none.js
+++ test/sequential/test-cluster-net-listen-ipv6only-none.js
@@ -7,7 +7,6 @@ if (!common.hasIPv6)
 const assert = require('assert');
 const cluster = require('cluster');
 const net = require('net');
-const Countdown = require('../common/countdown');
 
 // This test ensures that the `ipv6Only` option in `net.Server.listen()`
 // works as expected when we use cluster with `SCHED_NONE` schedulingPolicy.
@@ -18,7 +17,22 @@ const WORKER_ACCOUNT = 3;
 if (cluster.isMaster) {
   const workers = [];
 
-  const countdown = new Countdown(WORKER_ACCOUNT, () => {
+  for (let i = 0; i < WORKER_ACCOUNT; i += 1) {
+    const myWorker = new Promise((resolve) => {
+      const worker = cluster.fork().on('exit', common.mustCall((statusCode) => {
+        assert.strictEqual(statusCode, 0);
+      })).on('listening', common.mustCall((workerAddress) => {
+        assert.strictEqual(workerAddress.addressType, 6);
+        assert.strictEqual(workerAddress.address, host);
+        assert.strictEqual(workerAddress.port, common.PORT);
+        resolve(worker);
+      }));
+    });
+
+    workers.push(myWorker);
+  }
+
+  Promise.all(workers).then(common.mustCall((resolvedWorkers) => {
     // Make sure the `ipv6Only` option works. This is the part of the test that
     // requires the whole test to use `common.PORT` rather than port `0`. If it
     // used port `0` instead, then the operating system can supply a port that
@@ -30,24 +44,11 @@ if (cluster.isMaster) {
     }, common.mustCall(() => {
       // Exit.
       server.close();
-      workers.forEach((worker) => {
-        worker.disconnect();
+      resolvedWorkers.forEach((resolvedWorker) => {
+        resolvedWorker.disconnect();
       });
     }));
-  });
-
-  for (let i = 0; i < WORKER_ACCOUNT; i += 1) {
-    const worker = cluster.fork().on('exit', common.mustCall((statusCode) => {
-      assert.strictEqual(statusCode, 0);
-    })).on('listening', common.mustCall((workerAddress) => {
-      assert.strictEqual(workerAddress.addressType, 6);
-      assert.strictEqual(workerAddress.address, host);
-      assert.strictEqual(workerAddress.port, common.PORT);
-      countdown.dec();
-    }));
-
-    workers[i] = worker;
-  }
+  }));
 } else {
   net.createServer().listen({
     host,
