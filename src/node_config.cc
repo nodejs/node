a0b18403d2f1a3b64b8f60ea50c2d623214144f6
Bradley Farias <bradley.meck@gmail.com>
2020-03-20 15:51:03 -0500

    Update src/node_config.cc

--- src/node_config.cc
+++ src/node_config.cc
@@ -13,7 +13,6 @@ using v8::Isolate;
 using v8::Local;
 using v8::Number;
 using v8::Object;
-using v8::String;
 using v8::Value;
 
 // The config binding is used to provide an internal view of compile time
