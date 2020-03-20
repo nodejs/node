94952b4ac88264ed86378e20cf0bbe627c180cae
himself65 <himself65@outlook.com>
2020-03-20 16:23:02 +0800

    src: enhance C++ sprintf utility
    PR-URL: https://github.com/nodejs/node/pull/32385
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gabriel Schulhof <gabriel.schulhof@intel.com>

--- src/debug_utils-inl.h
+++ src/debug_utils-inl.h
@@ -27,11 +27,37 @@ struct ToStringHelper {
   }
   static std::string Convert(const std::string& value) { return value; }
   static std::string Convert(bool value) { return value ? "true" : "false"; }
+  template <unsigned BASE_BITS,
+            typename T,
+            typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
+  static std::string BaseConvert(T value) {
+    char ret[3 * sizeof(T)];
+    char* ptr = ret + 3 * sizeof(T) - 1;
+    *ptr = '\0';
+    const char* digits = "0123456789abcdef";
+    do {
+      unsigned digit = value & ((1 << BASE_BITS) - 1);
+      *--ptr =
+          (BASE_BITS < 4 ? static_cast<char>('0' + digit) : digits[digit]);
+    } while ((value >>= BASE_BITS) != 0);
+    return ptr;
+  }
+  template <unsigned BASE_BITS,
+            typename T,
+            typename std::enable_if<!std::is_integral<T>::value, int>::type = 0>
+  static std::string BaseConvert(T value) {
+    return Convert(std::forward<T>(value));
+  }
 };
 
 template <typename T>
 std::string ToString(const T& value) {
   return ToStringHelper::Convert(value);
+}
+
+template <unsigned BASE_BITS, typename T>
+std::string ToBaseString(T&& value) {
+  return ToStringHelper::BaseConvert<BASE_BITS>(std::forward<T>(value));
 }
 
 inline std::string SPrintFImpl(const char* format) {
@@ -64,7 +90,18 @@ std::string COLD_NOINLINE SPrintFImpl(  // NOLINT(runtime/string)
     case 'd':
     case 'i':
     case 'u':
-    case 's': ret += ToString(arg); break;
+    case 's':
+      ret += ToString(arg);
+      break;
+    case 'o':
+      ret += ToBaseString<3>(arg);
+      break;
+    case 'x':
+      ret += ToBaseString<4>(arg);
+      break;
+    case 'X':
+      ret += node::ToUpper(ToBaseString<4>(arg));
+      break;
     case 'p': {
       CHECK(std::is_pointer<typename std::remove_reference<Arg>::type>::value);
       char out[20];
