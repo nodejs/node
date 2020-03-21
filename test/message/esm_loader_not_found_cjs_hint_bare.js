8f87d252fa9ff071a8edf941d431b6efd6cb9727
Daniele Belardi <dwon.dnl@gmail.com>
2020-03-21 16:38:45 +0100

    esm: improve commonjs hint on module not found
    Run CommonJS resolver only if `error.code` is ERR_MODULE_NOT_FOUND.
    Avoid using absolute paths in hint by:
    * using a parent-relative path if the specifier is a relative path
    * using a `pkg/x.js` format if the specifier is bare (e.g. `pkg/x`)
    
    PR-URL: https://github.com/nodejs/node/pull/31906
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>

---
+++ test/message/esm_loader_not_found_cjs_hint_bare.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('../common');
+const { spawn } = require('child_process');
+const { join } = require('path');
+const { fixturesDir } = require('../common/fixtures');
+
+spawn(
+  process.execPath,
+  [
+    join(fixturesDir, 'esm_loader_not_found_cjs_hint_bare.mjs')
+  ],
+  {
+    cwd: fixturesDir,
+    stdio: 'inherit'
+  }
+);
