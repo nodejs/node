c2bb51bd281941d947d29ccfdb743dbd9651e0b5
Lucas Holmquist <lholmqui@redhat.com>
2020-03-25 12:54:12 -0400

    squash: fix up tests

--- test/parallel/test-fs-readSync-optional-params.js
+++ test/parallel/test-fs-readSync-optional-params.js
@@ -8,27 +8,20 @@ const filepath = fixtures.path('x.txt');
 const fd = fs.openSync(filepath, 'r');
 
 const expected = Buffer.from('xyz\n');
-const defaultBuffer1 = Buffer.allocUnsafe(expected.length);
-const defaultBuffer2 = Buffer.allocUnsafe(expected.length);
-const defaultBuffer3 = Buffer.allocUnsafe(expected.length);
+
+function runTest(defaultBuffer, options) {
+  const result = fs.readSync(fd, defaultBuffer, options);
+  assert.strictEqual(result, expected.length);
+  assert.deepStrictEqual(defaultBuffer, expected);
+}
 
 // Test passing in an empty options object
-const result1 = fs.readSync(fd, defaultBuffer1, { position: 0 });
-assert.strictEqual(result1, expected.length);
-assert.deepStrictEqual(defaultBuffer1, expected);
+runTest(Buffer.allocUnsafe(expected.length), { position: 0 });
 
 // Test not passing in any options object
-const result2 = fs.readSync(fd, defaultBuffer2);
-assert.strictEqual(result2, expected.length);
-assert.deepStrictEqual(defaultBuffer2, expected);
-
+runTest(Buffer.allocUnsafe(expected.length));
 
 // Test passing in options
-const result3 = fs.readSync(fd, defaultBuffer3, {
-  offset: 0,
-  length: defaultBuffer3.length,
-  position: 0
-});
-
-assert.strictEqual(result3, expected.length);
-assert.deepStrictEqual(defaultBuffer3, expected);
+runTest(Buffer.allocUnsafe(expected.length), { offset: 0,
+                                               length: expected.length,
+                                               position: 0 });
