867ff41d87e292997ff2ceb8d77e715145aed7b7
Sam Roberts <vieuxtech@gmail.com>
2020-03-25 12:37:39 -0700

    src: handle report options on fatalerror
    Follow on to https://github.com/nodejs/node/pull/32207, 3 other options
    are also not respected under situations that the isolate is not
    available.
    
    PR-URL: https://github.com/nodejs/node/pull/32497
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/report/test-report-fatal-error.js
+++ test/report/test-report-fatal-error.js
@@ -1,8 +1,14 @@
 'use strict';
 
+// Testcases for situations involving fatal errors, like Javascript heap OOM
+
 require('../common');
 const assert = require('assert');
-// Testcase to produce report on fatal error (javascript heap OOM)
+const fs = require('fs');
+const helper = require('../common/report.js');
+const spawnSync = require('child_process').spawnSync;
+const tmpdir = require('../common/tmpdir');
+
 if (process.argv[2] === 'child') {
 
   const list = [];
@@ -16,30 +22,100 @@ if (process.argv[2] === 'child') {
     this.id = 128;
     this.account = 98454324;
   }
-} else {
-  const helper = require('../common/report.js');
-  const tmpdir = require('../common/tmpdir');
+}
+
+// Common args that will cause an out-of-memory error for child process.
+const ARGS = [
+  '--max-old-space-size=20',
+  __filename,
+  'child'
+];
+
+{
+  // Verify that --report-on-fatalerror is respected when set.
   tmpdir.refresh();
-  const spawnSync = require('child_process').spawnSync;
-  let args = ['--report-on-fatalerror',
-              '--max-old-space-size=20',
-              __filename,
-              'child'];
-
-  let child = spawnSync(process.execPath, args, { cwd: tmpdir.path });
-
+  const args = ['--report-on-fatalerror', ...ARGS];
+  const child = spawnSync(process.execPath, args, { cwd: tmpdir.path });
   assert.notStrictEqual(child.status, 0, 'Process exited unexpectedly');
-  let reports = helper.findReports(child.pid, tmpdir.path);
+
+  const reports = helper.findReports(child.pid, tmpdir.path);
   assert.strictEqual(reports.length, 1);
+
   const report = reports[0];
   helper.validate(report);
-  // Verify that reports are not created on fatal error by default.
-  args = ['--max-old-space-size=20',
-          __filename,
-          'child'];
 
-  child = spawnSync(process.execPath, args, { cwd: tmpdir.path });
+  // Errors occur in a context where env is not available, so thread ID is
+  // unknown. Assert this, to verify that the underlying env-less situation is
+  // actually reached.
+  assert.strictEqual(require(report).header.threadId, null);
+}
+
+{
+  // Verify that --report-on-fatalerror is respected when not set.
+  const args = ARGS;
+  const child = spawnSync(process.execPath, args, { cwd: tmpdir.path });
   assert.notStrictEqual(child.status, 0, 'Process exited unexpectedly');
-  reports = helper.findReports(child.pid, tmpdir.path);
+  const reports = helper.findReports(child.pid, tmpdir.path);
   assert.strictEqual(reports.length, 0);
+}
+
+{
+  // Verify that --report-directory is respected when set.
+  // Verify that --report-compact is respected when not set.
+  tmpdir.refresh();
+  const dir = '--report-directory=' + tmpdir.path;
+  const args = ['--report-on-fatalerror', dir, ...ARGS];
+  const child = spawnSync(process.execPath, args, { });
+  assert.notStrictEqual(child.status, 0, 'Process exited unexpectedly');
+
+  const reports = helper.findReports(child.pid, tmpdir.path);
+  assert.strictEqual(reports.length, 1);
+
+  const report = reports[0];
+  helper.validate(report);
+  assert.strictEqual(require(report).header.threadId, null);
+  const lines = fs.readFileSync(report, 'utf8').split('\n').length - 1;
+  assert(lines > 10);
+}
+
+{
+  // Verify that --report-compact is respected when set.
+  tmpdir.refresh();
+  const args = ['--report-on-fatalerror', '--report-compact', ...ARGS];
+  const child = spawnSync(process.execPath, args, { cwd: tmpdir.path });
+  assert.notStrictEqual(child.status, 0, 'Process exited unexpectedly');
+
+  const reports = helper.findReports(child.pid, tmpdir.path);
+  assert.strictEqual(reports.length, 1);
+
+  const report = reports[0];
+  helper.validate(report);
+  assert.strictEqual(require(report).header.threadId, null);
+  // Subtract 1 because "xx\n".split("\n") => [ 'xx', '' ].
+  const lines = fs.readFileSync(report, 'utf8').split('\n').length - 1;
+  assert.strictEqual(lines, 1);
+}
+
+{
+  // Verify that --report-compact is respected when set.
+  // Verify that --report-filename is respected when set.
+  tmpdir.refresh();
+  const args = [
+    '--report-on-fatalerror',
+    '--report-compact',
+    '--report-filename=stderr',
+    ...ARGS
+  ];
+  const child = spawnSync(process.execPath, args, { encoding: 'utf8' });
+  assert.notStrictEqual(child.status, 0, 'Process exited unexpectedly');
+
+  const reports = helper.findReports(child.pid, tmpdir.path);
+  assert.strictEqual(reports.length, 0);
+
+  const lines = child.stderr.split('\n');
+  // Skip over unavoidable free-form output from V8.
+  const report = lines[1];
+  const json = JSON.parse(report);
+
+  assert.strictEqual(json.header.threadId, null);
 }
