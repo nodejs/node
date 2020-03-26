4ffa138c8131577a204d42d50609ac18b561937a
himself65 <himself65@outlook.com>
2020-03-27 00:46:10 +0800

    test: refactor test-worker
    PR-URL: https://github.com/nodejs/node/pull/32509
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>

--- test/parallel/test-worker.js
+++ test/parallel/test-worker.js
@@ -3,15 +3,17 @@ const common = require('../common');
 const assert = require('assert');
 const { Worker, isMainThread, parentPort } = require('worker_threads');
 
+const kTestString = 'Hello, world!';
+
 if (isMainThread) {
   const w = new Worker(__filename);
   w.on('message', common.mustCall((message) => {
-    assert.strictEqual(message, 'Hello, world!');
+    assert.strictEqual(message, kTestString);
   }));
 } else {
   setImmediate(() => {
     process.nextTick(() => {
-      parentPort.postMessage('Hello, world!');
+      parentPort.postMessage(kTestString);
     });
   });
 }
