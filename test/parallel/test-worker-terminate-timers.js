9375788169ef61cc21dca4749281ed908530e409
himself65 <himself65@outlook.com>
2020-03-29 13:16:38 +0800

    test: enhance test-worker-terminate-timers

--- test/parallel/test-worker-terminate-timers.js
+++ test/parallel/test-worker-terminate-timers.js
@@ -1,5 +1,6 @@
 'use strict';
 const common = require('../common');
+const assert = require('assert');
 const { Worker } = require('worker_threads');
 
 // Test that calling .terminate() during a timer callback works fine.
@@ -8,11 +9,14 @@ for (const fn of ['setTimeout', 'setImmediate', 'setInterval']) {
   const worker = new Worker(`
   const { parentPort } = require('worker_threads');
   ${fn}(() => {
-    require('worker_threads').parentPort.postMessage({});
+    require('worker_threads').parentPort.postMessage('${fn}');
     while (true);
   });`, { eval: true });
 
-  worker.on('message', common.mustCallAtLeast(() => {
+  worker.on('message', common.mustCallAtLeast((value) => {
+    assert.strictEqual(fn, value);
     worker.terminate();
   }));
+
+  worker.on('exit', common.mustCall(1));
 }
