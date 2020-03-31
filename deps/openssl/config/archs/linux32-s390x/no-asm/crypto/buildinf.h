9acf4352fa02055c9d6de637e77cc47c307f7b26
Hassaan Pasha <hassaan.pasha@teamo.io>
2020-03-31 18:10:56 +0500

    deps: update archs files for OpenSSL-1.1.1f
     After an OpenSSL source update, all the config files need to be
     regenerated and comitted by:
        $ cd deps/openssl/config
        $ make
        $ git add deps/openssl/config/archs
        $ git add deps/openssl/openssl/include/crypto/bn_conf.h
        $ git add deps/openssl/openssl/include/crypto/dso_conf.h
        $ git add deps/openssl/openssl/include/openssl/opensslconf.h
        $ git commit
    
    PR-URL: https://github.com/nodejs/node/pull/32583
    Reviewed-By: Sam Roberts <vieuxtech@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- deps/openssl/config/archs/linux32-s390x/no-asm/crypto/buildinf.h
+++ deps/openssl/config/archs/linux32-s390x/no-asm/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux32-s390x"
-#define DATE "built on: Wed Mar 18 21:09:09 2020 UTC"
+#define DATE "built on: Tue Mar 31 13:08:13 2020 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
