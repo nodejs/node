dd74601f962a2a9979d7c6df6496b51ce62d5d2a
Santiago Gimeno <santiago.gimeno@gmail.com>
2020-03-31 20:56:01 +0200

    net: fix crash if POLLHUP is received
    If the `onread` socket option is used and a `POLLHUP` event is received,
    libuv returns `UV_EOF` along with a `NULL` buffer in the read callback,
    causing the crash. Deal with this case.
    
    Fixes: https://github.com/nodejs/node/issues/31823
    
    PR-URL: https://github.com/nodejs/node/pull/32590
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/stream_base.cc
+++ src/stream_base.cc
@@ -517,12 +517,20 @@ uv_buf_t CustomBufferJSListener::OnStreamAlloc(size_t suggested_size) {
 
 void CustomBufferJSListener::OnStreamRead(ssize_t nread, const uv_buf_t& buf) {
   CHECK_NOT_NULL(stream_);
-  CHECK_EQ(buf.base, buffer_.base);
 
   StreamBase* stream = static_cast<StreamBase*>(stream_);
   Environment* env = stream->stream_env();
   HandleScope handle_scope(env->isolate());
   Context::Scope context_scope(env->context());
+
+  // To deal with the case where POLLHUP is received and UV_EOF is returned, as
+  // libuv returns an empty buffer (on unices only).
+  if (nread == UV_EOF && buf.base == nullptr) {
+    stream->CallJSOnreadMethod(nread, Local<ArrayBuffer>());
+    return;
+  }
+
+  CHECK_EQ(buf.base, buffer_.base);
 
   MaybeLocal<Value> ret = stream->CallJSOnreadMethod(nread,
                              Local<ArrayBuffer>(),
