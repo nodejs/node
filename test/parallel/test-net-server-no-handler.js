41475dc0968d257452d6b2b48c916a0ce2a463f7
Ben Noordhuis <info@bnoordhuis.nl>
2020-04-01 15:42:30 +0200

    fixup! use common.mustCall()

--- test/parallel/test-net-server-no-handler.js
+++ test/parallel/test-net-server-no-handler.js
@@ -2,10 +2,10 @@
 // incoming connections rather than accepting, then forgetting about them.
 
 'use strict';
-require('../common');
+const common = require('../common');
 const net = require('net');
 
-net.createServer(/* no handler */).listen(function() {
+net.createServer(common.mustCall()).listen(function() {
   const server = this;
   const { address: host, port } = server.address();
   net.connect(port, host).once('end', () => server.close());
