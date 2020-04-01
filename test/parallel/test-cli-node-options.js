edcbef00d2da4ea974aca6335f218b90561fbbb0
Myles Borins <mylesborins@google.com>
2020-04-01 12:17:17 -0400

    Update test/parallel/test-cli-node-options.js
    Co-Authored-By: Anna Henningsen <github@addaleax.net>

--- test/parallel/test-cli-node-options.js
+++ test/parallel/test-cli-node-options.js
@@ -67,7 +67,8 @@ if (common.hasCrypto) {
 // V8 options
 expect('--abort_on-uncaught_exception', 'B\n');
 expect('--disallow-code-generation-from-strings', 'B\n');
-expect('--jitless', 'B\n');
+if (!common.isAIX)
+  expect('--jitless', 'B\n');
 expect('--max-old-space-size=0', 'B\n');
 expect('--stack-trace-limit=100',
        /(\s*at f \(\[(eval|worker eval)\]:1:\d*\)\r?\n)/,
