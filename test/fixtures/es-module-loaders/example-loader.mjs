d849141d0916598143e4eeceefe6c4e3d96e907a
Derek Lewis <dereklewis@inf.is>
2020-04-03 02:34:42 -0400

    update `example-loader` test fixture

--- test/fixtures/es-module-loaders/example-loader.mjs
+++ test/fixtures/es-module-loaders/example-loader.mjs
@@ -1,48 +1,66 @@
-import { default as Module } from 'module';
 import { extname } from 'path';
-import { pathToFileURL } from 'url';
+import { default as Module } from 'module';
+
+const JS_EXTENSIONS = new Set(['.js', '.mjs']);
+const baseURL = new URL('file://');
+baseURL.pathname = process.cwd() + '/';
 
 /**
- * @param {string} specifier
- * @param {!(URL|string|undefined)} parentModuleURL
-//  * param {Function} defaultResolver
- * @returns {Promise<{url: string, format: string}>}
+ * @param {!(string)} specifier
+ * @param {{
+ *   parentURL:!(URL|string|undefined),
+ * }} context
+ * @param {!(Function)} defaultResolve
+ * @returns {{
+ *   url:!(string),
+ * }}
  */
-export async function resolve(
-  specifier,
-  parentModuleURL = undefined
-  // defaultResolver
-) {
-  const JS_EXTENSIONS = new Set(['.js', '.mjs']);
-
-  if (parentModuleURL === undefined) {
-    parentModuleURL = pathToFileURL(process.cwd()).href;
-  }
-
+export function resolve(specifier, { parentURL = baseURL }, defaultResolve) {
   if (Module.builtinModules.includes(specifier)) {
     return {
-      url: 'nodejs:' + specifier
+      url: 'nodejs:' + specifier,
     };
   }
-
-  if (!(specifier.startsWith('file:') || /^\.{1,2}[/]/.test(specifier))) {
+  if (
+    /^\.{1,2}[/]/.test(specifier) !== true &&
+    !specifier.startsWith('file:')
+  ) {
     // For node_modules support:
-    // return defaultResolver(specifier, parentModuleURL);
+    // return defaultResolve(specifier, {parentURL}, defaultResolve);
     throw new Error(
       "Imports must either be URLs or begin with './' or '../'; " +
-      `'${specifier}' satisfied neither of these requirements.`
+        `'${specifier}' satisfied neither of these requirements.`
     );
   }
+  const resolved = new URL(specifier, parentURL);
+  return {
+    url: resolved.href,
+  };
+}
 
-  const resolved = new URL(specifier, parentModuleURL);
-  const ext = extname(resolved.pathname);
-
+/**
+ * @param {!(string)} url
+// * param {!(Object)} context
+// * param {!(Function)} defaultGetFormat
+ * @returns {{
+ *   format:!(string),
+ * }}
+ */
+export function getFormat(url /* , context, defaultGetFormat */) {
+  if (url.startsWith('nodejs:') &&
+    Module.builtinModules.includes(url.slice(7))) {
+    return {
+      format: 'builtin',
+    };
+  }
+  const { pathname } = new URL(url);
+  const ext = extname(pathname);
   if (!JS_EXTENSIONS.has(ext)) {
     throw new Error(
-      `Cannot load file with non-JavaScript file extension ${ext}.`);
+      `Cannot load file with non-JavaScript file extension ${ext}.`
+    );
   }
-
   return {
-    format: 'module'
+    format: 'module',
   };
 }
