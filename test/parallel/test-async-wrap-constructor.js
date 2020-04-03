0dbe2cfcc5a94888776c8265a27bba8df538db24
Daniel Estiven Rico Posada <danielrico.posada@gmail.com>
2020-04-03 15:05:36 -0500

    fix message in test/parallel/test-async-wrap-constructor.js
    Co-Authored-By: Juan José <soyjuanarbol@gmail.com>

--- test/parallel/test-async-wrap-constructor.js
+++ test/parallel/test-async-wrap-constructor.js
@@ -14,7 +14,7 @@ const async_hooks = require('async_hooks');
     }, {
       code: 'ERR_ASYNC_CALLBACK',
       name: 'TypeError',
-      message: `something.${field} must be a function`
+      message: `hook.${field} must be a function`
     });
   });
 });
