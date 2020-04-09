b80b08fe35c9032bc2939f2e07c71fd1e5daa6f1
Anna Henningsen <anna@addaleax.net>
2020-04-09 18:44:28 +0200

    worker: fix type check in receiveMessageOnPort
    Use the same type check we use in `MoveToContext()` in
    `ReceiveMessage()`.
    
    Fixes: https://github.com/nodejs/node/issues/32742
    
    PR-URL: https://github.com/nodejs/node/pull/32745
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>
    Reviewed-By: Yongsheng Zhang <zyszys98@gmail.com>
    Reviewed-By: Zeyu Yang <himself65@outlook.com>

--- test/parallel/test-worker-message-port-receive-message.js
+++ test/parallel/test-worker-message-port-receive-message.js
@@ -23,3 +23,11 @@ port2.on('message', common.mustNotCall());
 port1.postMessage(message1);
 assert.deepStrictEqual(receiveMessageOnPort(port2), { message: message1 });
 port1.close();
+
+for (const value of [null, 0, -1, {}, []]) {
+  assert.throws(() => receiveMessageOnPort(value), {
+    name: 'TypeError',
+    code: 'ERR_INVALID_ARG_TYPE',
+    message: 'First argument needs to be a MessagePort instance'
+  });
+}
