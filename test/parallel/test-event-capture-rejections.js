579f68c5fd5c1f9023d3cf20842765e04652d37b
Adrian Estrada <edsadr@gmail.com>
2020-04-10 15:04:27 -0500

    test: better error validations for event-capture
    PR-URL: https://github.com/nodejs/node/pull/32771
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>

--- test/parallel/test-event-capture-rejections.js
+++ test/parallel/test-event-capture-rejections.js
@@ -278,14 +278,22 @@ function resetCaptureOnThrowInError() {
 function argValidation() {
 
   function testType(obj) {
+    const received = obj.constructor.name !== 'Number' ?
+      `an instance of ${obj.constructor.name}` :
+      `type number (${obj})`;
+
     assert.throws(() => new EventEmitter({ captureRejections: obj }), {
       code: 'ERR_INVALID_ARG_TYPE',
-      name: 'TypeError'
+      name: 'TypeError',
+      message: 'The "options.captureRejections" property must be of type ' +
+               `boolean. Received ${received}`
     });
 
     assert.throws(() => EventEmitter.captureRejections = obj, {
       code: 'ERR_INVALID_ARG_TYPE',
-      name: 'TypeError'
+      name: 'TypeError',
+      message: 'The "EventEmitter.captureRejections" property must be of ' +
+               `type boolean. Received ${received}`
     });
   }
 
