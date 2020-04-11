ea87809bb6696e2c1ec5d470031f137e18641183
Robert Nagy <ronagy@icloud.com>
2020-04-11 13:16:46 +0200

    stream: fix _final and 'prefinish' timing
    This PR fixes a few different things:
    
    The timing of 'prefinish' depends on whether or not
    _final is defined. In on case the event is emitted
    synchronously with end() and otherwise asynchronously.
    
    _final is currently unecessarily called asynchronously
    which forces implementors to use 'prefinish' as a hack
    to emulate synchronous behaviour. Furthermore, this hack
    is subtly broken due to the above issue.
    
    Refs: https://github.com/nodejs/node/issues/31401
    Refs: https://github.com/nodejs/node/pull/32763#discussion_r407041983
    
    PR-URL: https://github.com/nodejs/node/pull/32780
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-stream-writable-finished.js
+++ test/parallel/test-stream-writable-finished.js
@@ -30,7 +30,7 @@ const assert = require('assert');
 }
 
 {
-  // Emit finish asynchronously
+  // Emit finish asynchronously.
 
   const w = new Writable({
     write(chunk, encoding, cb) {
@@ -40,4 +40,60 @@ const assert = require('assert');
 
   w.end();
   w.on('finish', common.mustCall());
+}
+
+{
+  // Emit prefinish synchronously.
+
+  const w = new Writable({
+    write(chunk, encoding, cb) {
+      cb();
+    }
+  });
+
+  let sync = true;
+  w.on('prefinish', common.mustCall(() => {
+    assert.strictEqual(sync, true);
+  }));
+  w.end();
+  sync = false;
+}
+
+{
+  // Emit prefinish synchronously w/ final.
+
+  const w = new Writable({
+    write(chunk, encoding, cb) {
+      cb();
+    },
+    final(cb) {
+      cb();
+    }
+  });
+
+  let sync = true;
+  w.on('prefinish', common.mustCall(() => {
+    assert.strictEqual(sync, true);
+  }));
+  w.end();
+  sync = false;
+}
+
+
+{
+  // Call _final synchronously.
+
+  let sync = true;
+  const w = new Writable({
+    write(chunk, encoding, cb) {
+      cb();
+    },
+    final: common.mustCall((cb) => {
+      assert.strictEqual(sync, true);
+      cb();
+    })
+  });
+
+  w.end();
+  sync = false;
 }
