fdaa365b0bcb796e2578cb1558954ff9873d2874
Anna Henningsen <anna@addaleax.net>
2020-04-16 18:53:59 +0200

    deps: V8: stub backport 65238018ca4b and 8d08318e1a85
    Refs: https://github.com/v8/v8/commit/65238018ca4be983c804bf74115f415e0184b354
    Refs: https://github.com/v8/v8/commit/8d08318e1a852efc17f3c6a0ee6935f596734aaa
    
    PR-URL: https://github.com/nodejs/node/pull/32885
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Ujjwal Sharma <ryzokuken@disroot.org>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>
    Reviewed-By: Beth Griggs <Bethany.Griggs@uk.ibm.com>

--- deps/v8/include/v8.h
+++ deps/v8/include/v8.h
@@ -7545,6 +7545,8 @@ class V8_EXPORT HeapStatistics {
   size_t total_heap_size_executable() { return total_heap_size_executable_; }
   size_t total_physical_size() { return total_physical_size_; }
   size_t total_available_size() { return total_available_size_; }
+  size_t total_global_handles_size() { return total_global_handles_size_; }
+  size_t used_global_handles_size() { return used_global_handles_size_; }
   size_t used_heap_size() { return used_heap_size_; }
   size_t heap_size_limit() { return heap_size_limit_; }
   size_t malloced_memory() { return malloced_memory_; }
@@ -7572,6 +7574,8 @@ class V8_EXPORT HeapStatistics {
   bool does_zap_garbage_;
   size_t number_of_native_contexts_;
   size_t number_of_detached_contexts_;
+  size_t total_global_handles_size_;
+  size_t used_global_handles_size_;
 
   friend class V8;
   friend class Isolate;
