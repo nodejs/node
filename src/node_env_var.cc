20e0cc0592270bac03852120ec05f4b483b05ccb
Christopher Beeson <christopher@bl-ue.com>
2020-04-19 12:48:12 -0400

    src: do not compare against wide characters
    PR-URL: https://github.com/nodejs/node/pull/32921
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
    Reviewed-By: Zeyu Yang <himself65@outlook.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>

--- src/node_env_var.cc
+++ src/node_env_var.cc
@@ -121,7 +121,7 @@ void RealEnvStore::Set(Isolate* isolate,
   node::Utf8Value val(isolate, value);
 
 #ifdef _WIN32
-  if (key.length() > 0 && key[0] == L'=') return;
+  if (key.length() > 0 && key[0] == '=') return;
 #endif
   uv_os_setenv(*key, *val);
   DateTimeConfigurationChangeNotification(isolate, key);
@@ -139,7 +139,7 @@ int32_t RealEnvStore::Query(const char* key) const {
   }
 
 #ifdef _WIN32
-  if (key[0] == L'=') {
+  if (key[0] == '=') {
     return static_cast<int32_t>(v8::ReadOnly) |
            static_cast<int32_t>(v8::DontDelete) |
            static_cast<int32_t>(v8::DontEnum);
