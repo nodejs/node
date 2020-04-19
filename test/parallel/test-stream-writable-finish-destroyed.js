2b83ff1dfb6169eb344f4a229aa469f836013db2
Robert Nagy <ronagy@icloud.com>
2020-04-19 23:28:58 +0200

    stream: don't emit finish after close
    Writable stream could emit 'finish' after 'close'.

---
+++ test/parallel/test-stream-writable-finish-destroyed.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const common = require('../common');
+const { Writable } = require('stream');
+
+{
+  const w = new Writable({
+    write(chunk, encoding, cb) {
+      w.on('close', () => {
+        cb();
+      });
+    }
+  });
+
+  w.end('asd');
+  w.destroy();
+  w.on('finish', common.mustNotCall());
+}
+
+{
+  const w = new Writable({
+    write(chunk, encoding, cb) {
+      w.on('close', () => {
+        cb();
+        w.end();
+      });
+    }
+  });
+
+  w.write('asd');
+  w.destroy();
+  w.on('finish', common.mustNotCall());
+}
