c49e3ea20c2ce73bce64ea714e9cbfab001f97e0
rickyes <mail@zhoumq.cn>
2020-04-21 17:51:25 +0800

    console: support console constructor groupIndentation option
    PR-URL: https://github.com/nodejs/node/pull/32964
    Fixes: https://github.com/nodejs/node/issues/32947
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-console-group.js
+++ test/parallel/test-console-group.js
@@ -12,7 +12,7 @@ const Console = require('console').Console;
 
 let c, stdout, stderr;
 
-function setup() {
+function setup(groupIndentation) {
   stdout = '';
   hijackStdout(function(data) {
     stdout += data;
@@ -25,7 +25,8 @@ function setup() {
 
   c = new Console({ stdout: process.stdout,
                     stderr: process.stderr,
-                    colorMode: false });
+                    colorMode: false,
+                    groupIndentation: groupIndentation });
 }
 
 function teardown() {
@@ -154,4 +155,87 @@ function teardown() {
                       .map((val) => val.toString());
   assert(!keys.includes('Symbol(groupIndent)'),
          'groupIndent should not be enumerable');
+}
+
+// Check custom groupIndentation.
+{
+  setup(3);
+  const expectedOut = 'Set the groupIndentation parameter to 3\n' +
+                      'This is the outer level\n' +
+                      '   Level 2\n' +
+                      '      Level 3\n' +
+                      '   Back to level 2\n' +
+                      'Back to the outer level\n' +
+                      'Still at the outer level\n';
+
+
+  const expectedErr = '      More of level 3\n';
+
+  c.log('Set the groupIndentation parameter to 3');
+  c.log('This is the outer level');
+  c.group();
+  c.log('Level 2');
+  c.group();
+  c.log('Level 3');
+  c.warn('More of level 3');
+  c.groupEnd();
+  c.log('Back to level 2');
+  c.groupEnd();
+  c.log('Back to the outer level');
+  c.groupEnd();
+  c.log('Still at the outer level');
+
+  assert.strictEqual(stdout, expectedOut);
+  assert.strictEqual(stderr, expectedErr);
+  teardown();
+}
+
+// Check the correctness of the groupIndentation parameter.
+{
+  // TypeError
+  [null, 'str', [], false, true, {}].forEach((e) => {
+    assert.throws(
+      () => {
+        new Console({ stdout: process.stdout,
+                      stderr: process.stderr,
+                      groupIndentation: e });
+      },
+      {
+        code: 'ERR_INVALID_ARG_TYPE',
+        name: 'TypeError'
+      }
+    );
+  });
+
+  // RangeError for integer
+  [NaN, 1.01].forEach((e) => {
+    assert.throws(
+      () => {
+        new Console({ stdout: process.stdout,
+                      stderr: process.stderr,
+                      groupIndentation: e });
+      },
+      {
+        code: 'ERR_OUT_OF_RANGE',
+        name: 'RangeError',
+        message: /an integer/,
+      }
+    );
+  });
+
+  // RangeError
+  [-1, 1001].forEach((e) => {
+    assert.throws(
+      () => {
+        new Console({ stdout: process.stdout,
+                      stderr: process.stderr,
+                      groupIndentation: e });
+      },
+      {
+        code: 'ERR_OUT_OF_RANGE',
+        name: 'RangeError',
+        message: />= 0 && <= 1000/,
+      }
+    );
+  });
 }
