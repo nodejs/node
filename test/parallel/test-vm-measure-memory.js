95d381893097863fe6cd640cda398eba8d84b8a6
Joyee Cheung <joyeec9h3@gmail.com>
2020-04-22 10:28:20 +0800

    vm: fix vm.measureMemory() and introduce execution option
    https://github.com/nodejs/node-v8/pull/147 broke the
    `vm.measureMemory()` API. It only created a `MeasureMemoryDelegate` and
    without actually calling `v8::Isolate::MeasureMemory()` so the returned
    promise will never resolve. This was not caught by the tests because
    the promise resolvers were not wrapped with `common.mustCall()`.
    
    This patch migrates the API properly and also introduce the newly
    added execution option to the API. It also removes support for
    specifying contexts to measure - instead we'll just return the
    measurements for all contexts in the detailed mode, which is
    what the `performance.measureMemory()` prototype in V8 currently does.
    We can consider implementing our own `v8::MeasureMemoryDelegate`
    to select the target context in `ShouldMeasure()` in the future,
    but then we'll also need to implement `MeasurementComplete()`
    to assemble the result. For now it's probably too early to do that.
    
    Since this API is still experimental (and guarded with a warning),
    such breakage should be acceptable.
    
    Refs: https://github.com/nodejs/node-v8/pull/147

--- test/parallel/test-vm-measure-memory.js
+++ test/parallel/test-vm-measure-memory.js
@@ -1,42 +1,25 @@
 'use strict';
 const common = require('../common');
+const {
+  assertSummaryShape,
+  assertSingleDetailedShape,
+  expectExperimentalWarning
+} = require('../common/measure-memory');
 const assert = require('assert');
 const vm = require('vm');
 
-common.expectWarning('ExperimentalWarning',
-                     'vm.measureMemory is an experimental feature. ' +
-                     'This feature could change at any time');
+expectExperimentalWarning();
 
-// The formats could change when V8 is updated, then the tests should be
-// updated accordingly.
-function assertSummaryShape(result) {
-  assert.strictEqual(typeof result, 'object');
-  assert.strictEqual(typeof result.total, 'object');
-  assert.strictEqual(typeof result.total.jsMemoryEstimate, 'number');
-  assert(Array.isArray(result.total.jsMemoryRange));
-  assert.strictEqual(typeof result.total.jsMemoryRange[0], 'number');
-  assert.strictEqual(typeof result.total.jsMemoryRange[1], 'number');
-}
-
-function assertDetailedShape(result) {
-  // For now, the detailed shape is the same as the summary shape. This
-  // should change in future versions of V8.
-  return assertSummaryShape(result);
-}
-
-// Test measuring memory of the current context
+// Test eager memory measurement
 {
-  vm.measureMemory()
-    .then(assertSummaryShape);
+  vm.measureMemory({ execution: 'eager' })
+    .then(common.mustCall(assertSummaryShape));
 
-  vm.measureMemory({})
-    .then(assertSummaryShape);
+  vm.measureMemory({ mode: 'detailed', execution: 'eager' })
+    .then(common.mustCall(assertSingleDetailedShape));
 
-  vm.measureMemory({ mode: 'summary' })
-    .then(assertSummaryShape);
-
-  vm.measureMemory({ mode: 'detailed' })
-    .then(assertDetailedShape);
+  vm.measureMemory({ mode: 'summary', execution: 'eager' })
+    .then(common.mustCall(assertSummaryShape));
 
   assert.throws(() => vm.measureMemory(null), {
     code: 'ERR_INVALID_ARG_TYPE'
@@ -47,24 +30,7 @@ function assertDetailedShape(result) {
   assert.throws(() => vm.measureMemory({ mode: 'random' }), {
     code: 'ERR_INVALID_ARG_VALUE'
   });
-}
-
-// Test measuring memory of the sandbox
-{
-  const context = vm.createContext();
-  vm.measureMemory({ context })
-    .then(assertSummaryShape);
-
-  vm.measureMemory({ mode: 'summary', context },)
-    .then(assertSummaryShape);
-
-  vm.measureMemory({ mode: 'detailed', context })
-    .then(assertDetailedShape);
-
-  assert.throws(() => vm.measureMemory({ mode: 'summary', context: null }), {
-    code: 'ERR_INVALID_ARG_TYPE'
-  });
-  assert.throws(() => vm.measureMemory({ mode: 'summary', context: {} }), {
-    code: 'ERR_INVALID_ARG_TYPE'
+  assert.throws(() => vm.measureMemory({ execution: 'random' }), {
+    code: 'ERR_INVALID_ARG_VALUE'
   });
 }
