d3beb50da3b93cc26cb2ba2fadf66d74138a6afa
James M Snell <jasnell@gmail.com>
2020-04-27 09:27:49 -0700

    deps: update nghttp2 to 1.41.0
    Fixes: https://hackerone.com/reports/446662
    CVE-ID: CVE-2020-11080
    PR-URL: https://github.com/nodejs-private/node-private/pull/204
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- deps/nghttp2/lib/nghttp2_helper.c
+++ deps/nghttp2/lib/nghttp2_helper.c
@@ -334,6 +334,8 @@ const char *nghttp2_strerror(int error_code) {
   case NGHTTP2_ERR_FLOODED:
     return "Flooding was detected in this HTTP/2 session, and it must be "
            "closed";
+  case NGHTTP2_ERR_TOO_MANY_SETTINGS:
+    return "SETTINGS frame contained more than the maximum allowed entries";
   default:
     return "Unknown error code";
   }
