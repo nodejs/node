eb783385d3b204b225e32db968417e3bd7035974
Anna Henningsen <anna@addaleax.net>
2020-04-28 18:12:43 +0200

    test: skip memory usage tests when ASAN is enabled
    Running tests with an ASAN build leads to increased memory usage,
    rendering the memory usage assumptions in the test invalid.
    
    Refs: https://github.com/nodejs/node/pull/32776#issuecomment-620407014

--- test/parallel/test-crypto-dh-leak.js
+++ test/parallel/test-crypto-dh-leak.js
@@ -4,6 +4,8 @@
 const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
+if (process.config.variables.asan)
+  common.skip('ASAN messes with memory measurements');
 
 const assert = require('assert');
 const crypto = require('crypto');
