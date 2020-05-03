4732f15e30be9e0479619b02c2529386f6d791bd
Pranshu Srivastava <rexagod@gmail.com>
2020-05-03 11:02:13 +0530

    http: don't throw on `Uint8Array`s for `http.ServerResponse#write`
    Don't throw errors on Uint8Arrays and added test for all
    valid types.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/33490
    PR-URL: https://github.com/nodejs/node/pull/33155
    Fixes: https://github.com/nodejs/node/issues/33379
    Refs: https://github.com/nodejs/node/issues/29829
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Zeyu Yang <himself65@outlook.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-http-outgoing-proto.js
+++ test/parallel/test-http-outgoing-proto.js
@@ -74,7 +74,15 @@ assert.throws(() => {
   );
 }
 
-assert(OutgoingMessage.prototype.write.call({ _header: 'test' }));
+assert.throws(() => {
+  const outgoingMessage = new OutgoingMessage();
+  outgoingMessage.write.call({ _header: 'test' });
+}, {
+  code: 'ERR_INVALID_ARG_TYPE',
+  name: 'TypeError',
+  message: 'The "chunk" argument must be of type string or an instance of ' +
+           'Buffer or Uint8Array. Received undefined'
+});
 
 assert.throws(() => {
   const outgoingMessage = new OutgoingMessage();
@@ -82,8 +90,8 @@ assert.throws(() => {
 }, {
   code: 'ERR_INVALID_ARG_TYPE',
   name: 'TypeError',
-  message: 'The first argument must be of type string or an instance of ' +
-           'Buffer. Received undefined'
+  message: 'The "chunk" argument must be of type string or an instance of ' +
+           'Buffer or Uint8Array. Received undefined'
 });
 
 assert.throws(() => {
@@ -92,8 +100,8 @@ assert.throws(() => {
 }, {
   code: 'ERR_INVALID_ARG_TYPE',
   name: 'TypeError',
-  message: 'The first argument must be of type string or an instance of ' +
-           'Buffer. Received type number (1)'
+  message: 'The "chunk" argument must be of type string or an instance of ' +
+           'Buffer or Uint8Array. Received type number (1)'
 });
 
 // addTrailers()
