35594711536b09d93b485b94e8a2b65aa300805d
Kirill Shatskiy <shaackiy@gmail.com>
2020-05-04 12:50:42 +0300

    esm: share package.json cache between ESM and CJS loaders
    Refs: https://github.com/nodejs/node/issues/30674
    
    PR-URL: https://github.com/nodejs/node/pull/33229
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: Geoffrey Booth <webmaster@geoffreybooth.com>

--- test/parallel/test-module-binding.js
+++ test/parallel/test-module-binding.js
@@ -6,11 +6,32 @@ const { internalBinding } = require('internal/test/binding');
 const { internalModuleReadJSON } = internalBinding('fs');
 const { readFileSync } = require('fs');
 const { strictEqual } = require('assert');
-
-strictEqual(internalModuleReadJSON('nosuchfile'), undefined);
-strictEqual(internalModuleReadJSON(fixtures.path('empty.txt')), '{}');
-strictEqual(internalModuleReadJSON(fixtures.path('empty-with-bom.txt')), '{}');
+{
+  const [string, containsKeys] = internalModuleReadJSON('nosuchfile');
+  strictEqual(string, undefined);
+  strictEqual(containsKeys, undefined);
+}
+{
+  const [string, containsKeys] =
+    internalModuleReadJSON(fixtures.path('empty.txt'));
+  strictEqual(string, '');
+  strictEqual(containsKeys, false);
+}
+{
+  const [string, containsKeys] =
+    internalModuleReadJSON(fixtures.path('empty.txt'));
+  strictEqual(string, '');
+  strictEqual(containsKeys, false);
+}
+{
+  const [string, containsKeys] =
+    internalModuleReadJSON(fixtures.path('empty-with-bom.txt'));
+  strictEqual(string, '');
+  strictEqual(containsKeys, false);
+}
 {
   const filename = fixtures.path('require-bin/package.json');
-  strictEqual(internalModuleReadJSON(filename), readFileSync(filename, 'utf8'));
+  const [string, containsKeys] = internalModuleReadJSON(filename);
+  strictEqual(string, readFileSync(filename, 'utf8'));
+  strictEqual(containsKeys, true);
 }
