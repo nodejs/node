ad8680773e7cfecf6c90b97e055eb92dd886c667
Myles Borins <mylesborins@google.com>
2020-05-05 16:16:13 -0400

    module: better error for named exports from cjs
    We do not support importing named exports from a CJS module.
    This change decorates the error message for missing named exports in
    the case where the module being imported is expected to be CJS by the
    ESM loader.
    
    Signed-off-by: Myles Borins <myles.borins@gmail.com>
    
    PR-URL: https://github.com/nodejs/node/pull/33256
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

---
+++ test/es-module/test-esm-cjs-named-error.mjs
@@ -0,0 +1,64 @@
+import '../common/index.mjs';
+import { rejects } from 'assert';
+
+const fixtureBase = '../fixtures/es-modules/package-cjs-named-error';
+
+const expectedRelative = 'The requested module \'./fail.cjs\' is expected to ' +
+  'be of type CommonJS, which does not support named exports. CommonJS ' +
+  'modules can be imported by importing the default export.\n' +
+  'For example:\n' +
+  'import pkg from \'./fail.cjs\';\n' +
+  'const { comeOn } = pkg;';
+
+const expectedPackageHack = 'The requested module \'./json-hack/fail.js\' is ' +
+  'expected to be of type CommonJS, which does not support named exports. ' +
+  'CommonJS modules can be imported by importing the default export.\n' +
+  'For example:\n' +
+  'import pkg from \'./json-hack/fail.js\';\n' +
+  'const { comeOn } = pkg;';
+
+const expectedBare = 'The requested module \'deep-fail\' is expected to ' +
+  'be of type CommonJS, which does not support named exports. CommonJS ' +
+  'modules can be imported by importing the default export.\n' +
+  'For example:\n' +
+  'import pkg from \'deep-fail\';\n' +
+  'const { comeOn } = pkg;';
+
+rejects(async () => {
+  await import(`${fixtureBase}/single-quote.mjs`);
+}, {
+  name: 'SyntaxError',
+  message: expectedRelative
+}, 'should support relative specifiers with single quotes');
+
+rejects(async () => {
+  await import(`${fixtureBase}/double-quote.mjs`);
+}, {
+  name: 'SyntaxError',
+  message: expectedRelative
+}, 'should support relative specifiers with double quotes');
+
+rejects(async () => {
+  await import(`${fixtureBase}/json-hack.mjs`);
+}, {
+  name: 'SyntaxError',
+  message: expectedPackageHack
+}, 'should respect recursive package.json for module type');
+
+rejects(async () => {
+  await import(`${fixtureBase}/bare-import-single.mjs`);
+}, {
+  name: 'SyntaxError',
+  message: expectedBare
+}, 'should support bare specifiers with single quotes');
+
+rejects(async () => {
+  await import(`${fixtureBase}/bare-import-double.mjs`);
+}, {
+  name: 'SyntaxError',
+  message: expectedBare
+}, 'should support bare specifiers with double quotes');
+
+rejects(async () => {
+  await import(`${fixtureBase}/escaped-single-quote.mjs`);
+}, /import pkg from '\.\/oh'no\.cjs'/, 'should support relative specifiers with escaped single quote');
