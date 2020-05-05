621eb2358a54521bbd5f96ef4b9db338e367acab
Nikolai Vavilov <vvnicholas@gmail.com>
2020-05-05 13:35:34 +0300

    remove benchmark

--- benchmark/buffers/buffer-copy.js
+++ benchmark/buffers/buffer-copy.js
@@ -4,18 +4,16 @@ const common = require('../common.js');
 const bench = common.createBenchmark(main, {
   bytes: [0, 8, 128, 32 * 1024],
   partial: ['true', 'false'],
-  oob: ['true', 'false'],
   n: [6e6]
 });
 
-function main({ n, bytes, partial, oob }) {
+function main({ n, bytes, partial }) {
   const source = Buffer.allocUnsafe(bytes);
-  const target = Buffer.allocUnsafe(bytes * 2);
+  const target = Buffer.allocUnsafe(bytes);
   const sourceStart = (partial === 'true' ? Math.floor(bytes / 2) : 0);
-  const sourceEnd = (oob === 'true' ? source.length + 1 : source.length);
   bench.start();
   for (let i = 0; i < n; i++) {
-    source.copy(target, 0, sourceStart, sourceEnd);
+    source.copy(target, 0, sourceStart);
   }
   bench.end(n);
 }
