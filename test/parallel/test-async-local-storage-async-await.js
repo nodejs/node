cb867d7d6a258c13923642e904c3c1e39e2c9f0d
Stephen Belanger <admin@stephenbelanger.com>
2020-05-04 17:52:36 -0700

    async_hooks: add trackAsyncAwait option to AsyncLocalStorage

--- test/parallel/test-async-local-storage-async-await.js
+++ test/parallel/test-async-local-storage-async-await.js
@@ -3,7 +3,7 @@ const common = require('../common');
 const assert = require('assert');
 const { AsyncLocalStorage } = require('async_hooks');
 
-const store = new AsyncLocalStorage();
+const store = new AsyncLocalStorage({ trackAsyncAwait: true });
 let checked = 0;
 
 function thenable(expected, count) {
