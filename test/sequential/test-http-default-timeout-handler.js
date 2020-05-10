380bed10a7f5c6150b7dd06f24c9970d8a2e14cb
Robert Nagy <ronagy@icloud.com>
2020-05-10 22:17:01 +0200

    fixup

--- test/sequential/test-http-default-timeout-handler.js
+++ test/sequential/test-http-default-timeout-handler.js
@@ -10,20 +10,13 @@ const server = http.createServer(common.mustCall((req, res) => {
 }));
 
 server.listen(0, common.mustCall(() => {
-  let req;
-  const timer = setTimeout(() => {
-    req.abort();
-    assert.fail('should not timeout here');
-  }, 1000);
-
   const agent = new http.Agent({ timeout: 50 });
-  req = http.get({
+  http.get({
     path: '/',
     port: server.address().port,
     agent
   }, common.mustNotCall())
   .on('error', common.mustCall((err) => {
-    clearTimeout(timer);
     assert.strictEqual(err.message, 'Socket timeout');
     assert.strictEqual(err.code, 'ERR_HTTP_SOCKET_TIMEOUT');
   }));
