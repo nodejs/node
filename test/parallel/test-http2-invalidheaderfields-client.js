a0575e048d0b03e16e4696df23a185d33f2a06ed
Pranshu Srivastava <rexagod@gmail.com>
2020-05-11 01:34:38 +0530

    test: add checks for writeHead

--- test/parallel/test-http2-invalidheaderfields-client.js
+++ test/parallel/test-http2-invalidheaderfields-client.js
@@ -8,6 +8,7 @@ const server1 = http2.createServer();
 
 server1.listen(0, common.mustCall(() => {
   const session = http2.connect(`http://localhost:${server1.address().port}`);
+  // check for req headers
   session.request({ 'no underscore': 123 });
   session.on('error', common.mustCall((e) => {
     assert.strictEqual(e.code, 'ERR_INVALID_HTTP_TOKEN');
@@ -16,6 +17,7 @@ server1.listen(0, common.mustCall(() => {
 }));
 
 const server2 = http2.createServer(common.mustCall((req, res) => {
+  // check for setHeader
   res.setHeader('x y z', 123);
   res.end();
 }));
@@ -27,5 +29,27 @@ server2.listen(0, common.mustCall(() => {
     assert.strictEqual(e.code, 'ERR_HTTP2_STREAM_ERROR');
     session.close();
     server2.close();
+  }));
+}));
+
+const server3 = http2.createServer(common.mustCall((req, res) => {
+  // check for writeHead
+  assert.throws(common.mustCall(()=>{
+    res.writeHead(200, {
+      'an invalid header': 123
+    });
+  }), {
+    code: 'ERR_HTTP2_INVALID_STREAM'
+  });
+  res.end();
+}));
+
+server3.listen(0, common.mustCall(() => {
+  const session = http2.connect(`http://localhost:${server3.address().port}`);
+  const req = session.request();
+  req.on('error', common.mustCall((e) => {
+    assert.strictEqual(e.code, 'ERR_HTTP2_STREAM_ERROR');
+    server3.close();
+    session.close();
   }));
 }));
