112c5525693f482ed819d4dd7b1e2ae4a662d8a3
Antoine du Hamel <duhamelantoine1995@gmail.com>
2020-05-14 20:44:23 +0200

    fixup: Windows tests

--- test/parallel/test-directory-import.js
+++ test/parallel/test-directory-import.js
@@ -3,8 +3,12 @@
 require('../common');
 const fixtures = require('../common/fixtures');
 const assert = require('assert');
+const { pathToFileURL } = require('url');
 
 {
   assert.rejects(import('./'), /ERR_UNSUPPORTED_DIR_IMPORT/);
-  assert.rejects(import(fixtures.path('packages', 'main')), /Did you mean/);
+  assert.rejects(
+    import(pathToFileURL(fixtures.path('packages', 'main'))),
+    /Did you mean/,
+  );
 }
