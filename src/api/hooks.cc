d85a9332508bbc39ddc011e9e5a1fd5f71a985bb
Ben Noordhuis <info@bnoordhuis.nl>
2020-05-16 11:39:53 +0200

    wasi,worker: handle termination exception
    Be careful when emitting the 'beforeExit' event. It's not allowed to
    call into the runtime when a termination exception is pending.
    
    Fixes: https://github.com/nodejs/node/issues/33377
    
    PR-URL: https://github.com/nodejs/node/pull/33386
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/api/hooks.cc
+++ src/api/hooks.cc
@@ -37,11 +37,14 @@ void EmitBeforeExit(Environment* env) {
 
   HandleScope handle_scope(env->isolate());
   Context::Scope context_scope(env->context());
-  Local<Value> exit_code = env->process_object()
-                               ->Get(env->context(), env->exit_code_string())
-                               .ToLocalChecked()
-                               ->ToInteger(env->context())
-                               .ToLocalChecked();
+
+  Local<Value> exit_code_v;
+  if (!env->process_object()->Get(env->context(), env->exit_code_string())
+      .ToLocal(&exit_code_v)) return;
+
+  Local<Integer> exit_code;
+  if (!exit_code_v->ToInteger(env->context()).ToLocal(&exit_code)) return;
+
   ProcessEmit(env, "beforeExit", exit_code).ToLocalChecked();
 }
 
