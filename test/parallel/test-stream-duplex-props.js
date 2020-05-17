de35e4fabaca74ec54a8116b79f4d39642db4811
Robert Nagy <ronagy@icloud.com>
2020-05-17 10:39:19 +0200

    fixup! stream: Duplex forward writableObjectMode

--- test/parallel/test-stream-duplex-props.js
+++ test/parallel/test-stream-duplex-props.js
@@ -5,13 +5,15 @@ const assert = require('assert');
 const { Duplex } = require('stream');
 
 {
-  const d = new Duplex({ 
+  const d = new Duplex({
     objectMode: true,
     highWaterMark: 100
   });
 
   assert.strictEqual(d.writableObjectMode, true);
   assert.strictEqual(d.writableHighWaterMark, 100);
+  assert.strictEqual(d.readableObjectMode, true);
+  assert.strictEqual(d.readableHighWaterMark, 100);
 }
 
 {
