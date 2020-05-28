d70214d36ae704a62e68840a4553e75aa6acd057
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-05-28 05:34:29 +0200

    src: use MaybeLocal.ToLocal instead of IsEmpty worker
    This commit suggest using MaybeLocal.ToLocal and passing in the
    Local<String> node_opts. It also introduces a variable, arg_v8, of type
    Local<String> to enable the use of ToLocal.
    
    The motivation for doing this is that the following
    MaybeLocal::ToLocalChecked and MaybeLocal::FromMaybe calls can then be
    avoided.

--- src/node_worker.cc
+++ src/node_worker.cc
@@ -487,9 +487,9 @@ void Worker::New(const FunctionCallbackInfo<Value>& args) {
 #ifndef NODE_WITHOUT_NODE_OPTIONS
     MaybeLocal<String> maybe_node_opts =
         env_vars->Get(isolate, OneByteString(isolate, "NODE_OPTIONS"));
-    if (!maybe_node_opts.IsEmpty()) {
-      std::string node_options(
-          *String::Utf8Value(isolate, maybe_node_opts.ToLocalChecked()));
+    Local<String> node_opts;
+    if (maybe_node_opts.ToLocal(&node_opts)) {
+      std::string node_options(*String::Utf8Value(isolate, node_opts));
       std::vector<std::string> errors{};
       std::vector<std::string> env_argv =
           ParseNodeOptionsEnvVar(node_options, &errors);
@@ -529,14 +529,11 @@ void Worker::New(const FunctionCallbackInfo<Value>& args) {
       if (!array->Get(env->context(), i).ToLocal(&arg)) {
         return;
       }
-      MaybeLocal<String> arg_v8_string =
-          arg->ToString(env->context());
-      if (arg_v8_string.IsEmpty()) {
+      Local<String> arg_v8;
+      if (!arg->ToString(env->context()).ToLocal(&arg_v8)) {
         return;
       }
-      Utf8Value arg_utf8_value(
-          args.GetIsolate(),
-          arg_v8_string.FromMaybe(Local<String>()));
+      Utf8Value arg_utf8_value(args.GetIsolate(), arg_v8);
       std::string arg_string(arg_utf8_value.out(), arg_utf8_value.length());
       exec_argv.push_back(arg_string);
     }
