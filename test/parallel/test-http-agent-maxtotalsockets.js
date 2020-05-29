5dffb12f222cfa1fdb0d5c7ec2060630dd70f894
rickyes <ives199511@gmail.com>
2020-05-29 12:25:25 +0800

    fixup

--- test/parallel/test-http-agent-maxtotalsockets.js
+++ test/parallel/test-http-agent-maxtotalsockets.js
@@ -8,6 +8,24 @@ const Countdown = require('../common/countdown');
 const maxTotalSockets = 3;
 const maxSockets = 2;
 
+assert.throws(() => new http.Agent({
+  maxTotalSockets: 'test',
+}), {
+  code: 'ERR_INVALID_ARG_TYPE',
+  name: 'TypeError',
+  message: 'The "maxTotalSockets" argument must be of type number. ' +
+    "Received type string ('test')",
+});
+
+assert.throws(() => new http.Agent({
+  maxTotalSockets: -1,
+}), {
+  code: 'ERR_OUT_OF_RANGE',
+  name: 'RangeError',
+  message: 'The value of "maxTotalSockets" is out of range. ' +
+    'It must be > 0. Received -1',
+});
+
 const agent = new http.Agent({
   keepAlive: true,
   keepAliveMsecs: 1000,
@@ -41,17 +59,13 @@ function handler() {
       assert.strictEqual(res.statusCode, 200);
       res.resume();
       res.on('end', common.mustCall(() => {
-        testMaxSockets();
+        for (const key of Object.keys(agent.sockets)) {
+          assert(agent.sockets[key].length <= maxSockets);
+        }
         assert(getTotalSocketsCount() <= maxTotalSockets);
         countdown.dec();
       }));
     }));
-  }
-}
-
-function testMaxSockets() {
-  for(const key of Object.keys(agent.sockets)) {
-    assert(agent.sockets[key].length <= maxSockets)
   }
 }
 
