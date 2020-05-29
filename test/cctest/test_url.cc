ffc213ec1c3f664d933a79c299cd4677f183d096
Anna Henningsen <anna@addaleax.net>
2020-05-29 20:05:43 +0200

    fixup! src: avoid OOB read in URL parser

--- test/cctest/test_url.cc
+++ test/cctest/test_url.cc
@@ -95,10 +95,10 @@ TEST_F(URLTest, TruncatedAfterProtocol2) {
   char input[6] = { 'h', 't', 't', 'p', ':', '/' };
   URL simple(input, sizeof(input));
 
-  EXPECT_FALSE(simple.flags() & URL_FLAGS_FAILED);
+  EXPECT_TRUE(simple.flags() & URL_FLAGS_FAILED);
   EXPECT_EQ(simple.protocol(), "http:");
   EXPECT_EQ(simple.host(), "");
-  EXPECT_EQ(simple.path(), "/");
+  EXPECT_EQ(simple.path(), "");
 }
 
 TEST_F(URLTest, ToFilePath) {
