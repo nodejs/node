2b0301fbeb5eaa382d1ceb4eb1d884ed9ccad864
KuthorX <kuthorx@gmail.com>
2020-05-29 13:40:17 +0800

    fix code style by lint-js

--- test/parallel/test-http-client-input-function.js
+++ test/parallel/test-http-client-input-function.js
@@ -8,7 +8,7 @@ const ClientRequest = require('http').ClientRequest;
   const req = new ClientRequest(() => {});
   req.on('error', common.mustCall((err) => {
     assert.strictEqual(err.code, 'ECONNREFUSED');
-  }))
+  }));
   assert.strictEqual(req.path, '/');
   assert.strictEqual(req.method, 'GET');
 }
