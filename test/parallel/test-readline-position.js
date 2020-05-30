9d9c15a5c360ddab742293353976036f40a498b5
Ruben Bridgewater <ruben@bridgewater.de>
2020-05-30 03:30:30 +0200

    util: support Combining Diacritical Marks for Symbols
    This adds support for the "Combining Diacritical Marks for Symbols"
    unicode group to calculate a zero length width even if Node.js is
    built without ICU.
    
    Signed-off-by: Ruben Bridgewater <ruben@bridgewater.de>
    
    PR-URL: https://github.com/nodejs/node/pull/33650
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-readline-position.js
+++ test/parallel/test-readline-position.js
@@ -1,7 +1,6 @@
 // Flags: --expose-internals
 'use strict';
 const common = require('../common');
-const { internalBinding } = require('internal/test/binding');
 const { PassThrough } = require('stream');
 const readline = require('readline');
 const assert = require('assert');
@@ -21,21 +20,13 @@ common.skipIfDumbTerminal();
   const tests = [
     [1, 'a'],
     [2, 'ab'],
-    [2, '丁']
+    [2, '丁'],
+    [0, '\u0301'],   // COMBINING ACUTE ACCENT
+    [1, 'a\u0301'],  // á
+    [0, '\u20DD'],   // COMBINING ENCLOSING CIRCLE
+    [2, 'a\u20DDb'], // a⃝b
+    [0, '\u200E'],   // LEFT-TO-RIGHT MARK
   ];
-
-  // The non-ICU JS implementation of character width calculation is only aware
-  // of the wide/narrow distinction. Only test these more advanced cases when
-  // ICU is available.
-  if (internalBinding('config').hasIntl) {
-    tests.push(
-      [0, '\u0301'],   // COMBINING ACUTE ACCENT
-      [1, 'a\u0301'],  // á
-      [0, '\u20DD'],   // COMBINING ENCLOSING CIRCLE
-      [2, 'a\u20DDb'], // a⃝b
-      [0, '\u200E']    // LEFT-TO-RIGHT MARK
-    );
-  }
 
   for (const [cursor, string] of tests) {
     rl.write(string);
