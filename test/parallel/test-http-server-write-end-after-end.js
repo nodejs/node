30cc54275d570a804ced31843d1ff237dd701f85
Robert Nagy <ronagy@icloud.com>
2020-05-30 10:40:30 +0200

    http: don't emit error after close
    Refs: https://github.com/nodejs/node/issues/33591
    
    PR-URL: https://github.com/nodejs/node/pull/33654
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- test/parallel/test-http-server-write-end-after-end.js
+++ test/parallel/test-http-server-write-end-after-end.js
@@ -6,19 +6,23 @@ const http = require('http');
 const server = http.createServer(handle);
 
 function handle(req, res) {
-  res.on('error', common.mustCall((err) => {
-    common.expectsError({
-      code: 'ERR_STREAM_WRITE_AFTER_END',
-      name: 'Error'
-    })(err);
-    server.close();
-  }));
+  res.on('error', common.mustNotCall());
 
   res.write('hello');
   res.end();
 
   setImmediate(common.mustCall(() => {
     res.end('world');
+    process.nextTick(() => {
+      server.close();
+    });
+    res.write('world', common.mustCall((err) => {
+      common.expectsError({
+        code: 'ERR_STREAM_WRITE_AFTER_END',
+        name: 'Error'
+      })(err);
+      server.close();
+    }));
   }));
 }
 
