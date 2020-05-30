56ae0ffc7c2c37be77dc5d0df109a2e3bf84c7ac
James M Snell <jasnell@gmail.com>
2020-05-30 16:02:22 -0700

    events: remove NodeEventTarget
    The extending of EventTarget with EventEmitter emulation is contentious
    and not something that is strictly necessary for minimal support.
    
    Signed-off-by: James M Snell <jasnell@gmail.com>

--- test/parallel/test-eventtarget.js
+++ test/parallel/test-eventtarget.js
@@ -5,7 +5,6 @@ const common = require('../common');
 const {
   Event,
   EventTarget,
-  NodeEventTarget,
 } = require('internal/event_target');
 
 const {
@@ -50,10 +49,12 @@ ok(EventTarget);
   ev.preventDefault();
   strictEqual(ev.defaultPrevented, true);
 }
+
 {
   const ev = new Event('foo');
   deepStrictEqual(Object.keys(ev), ['isTrusted']);
 }
+
 {
   const eventTarget = new EventTarget();
 
@@ -78,6 +79,7 @@ ok(EventTarget);
   eventTarget.removeEventListener('foo', ev1);
   eventTarget.dispatchEvent(new Event('foo'));
 }
+
 {
   // event subclassing
   const SubEvent = class extends Event {};
@@ -87,124 +89,12 @@ ok(EventTarget);
   eventTarget.addEventListener('foo', fn, { once: true });
   eventTarget.dispatchEvent(ev);
 }
-{
-  const eventTarget = new NodeEventTarget();
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  deepStrictEqual(eventTarget.eventNames(), []);
-
-  const ev1 = common.mustCall(function(event) {
-    strictEqual(event.type, 'foo');
-    strictEqual(this, eventTarget);
-  }, 2);
-
-  const ev2 = {
-    handleEvent: common.mustCall(function(event) {
-      strictEqual(event.type, 'foo');
-      strictEqual(this, ev2);
-    })
-  };
-
-  eventTarget.addEventListener('foo', ev1);
-  eventTarget.addEventListener('foo', ev2, { once: true });
-  strictEqual(eventTarget.listenerCount('foo'), 2);
-  ok(eventTarget.dispatchEvent(new Event('foo')));
-  strictEqual(eventTarget.listenerCount('foo'), 1);
-  eventTarget.dispatchEvent(new Event('foo'));
-
-  eventTarget.removeEventListener('foo', ev1);
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  eventTarget.dispatchEvent(new Event('foo'));
-}
-
 
 {
   const eventTarget = new EventTarget();
   const event = new Event('foo', { cancelable: true });
   eventTarget.addEventListener('foo', (event) => event.preventDefault());
   ok(!eventTarget.dispatchEvent(event));
-}
-
-{
-  const eventTarget = new NodeEventTarget();
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  deepStrictEqual(eventTarget.eventNames(), []);
-
-  const ev1 = common.mustCall((event) => {
-    strictEqual(event.type, 'foo');
-  }, 2);
-
-  const ev2 = {
-    handleEvent: common.mustCall((event) => {
-      strictEqual(event.type, 'foo');
-    })
-  };
-
-  strictEqual(eventTarget.on('foo', ev1), eventTarget);
-  strictEqual(eventTarget.once('foo', ev2, { once: true }), eventTarget);
-  strictEqual(eventTarget.listenerCount('foo'), 2);
-  eventTarget.dispatchEvent(new Event('foo'));
-  strictEqual(eventTarget.listenerCount('foo'), 1);
-  eventTarget.dispatchEvent(new Event('foo'));
-
-  strictEqual(eventTarget.off('foo', ev1), eventTarget);
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  eventTarget.dispatchEvent(new Event('foo'));
-}
-
-{
-  const eventTarget = new NodeEventTarget();
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  deepStrictEqual(eventTarget.eventNames(), []);
-
-  const ev1 = common.mustCall((event) => {
-    strictEqual(event.type, 'foo');
-  }, 2);
-
-  const ev2 = {
-    handleEvent: common.mustCall((event) => {
-      strictEqual(event.type, 'foo');
-    })
-  };
-
-  eventTarget.addListener('foo', ev1);
-  eventTarget.once('foo', ev2, { once: true });
-  strictEqual(eventTarget.listenerCount('foo'), 2);
-  eventTarget.dispatchEvent(new Event('foo'));
-  strictEqual(eventTarget.listenerCount('foo'), 1);
-  eventTarget.dispatchEvent(new Event('foo'));
-
-  eventTarget.removeListener('foo', ev1);
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  eventTarget.dispatchEvent(new Event('foo'));
-}
-
-{
-  const eventTarget = new NodeEventTarget();
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  deepStrictEqual(eventTarget.eventNames(), []);
-
-  // Won't actually be called.
-  const ev1 = () => {};
-
-  // Won't actually be called.
-  const ev2 = { handleEvent() {} };
-
-  eventTarget.addListener('foo', ev1);
-  eventTarget.addEventListener('foo', ev1);
-  eventTarget.once('foo', ev2, { once: true });
-  eventTarget.once('foo', ev2, { once: false });
-  eventTarget.on('bar', ev1);
-  strictEqual(eventTarget.listenerCount('foo'), 2);
-  strictEqual(eventTarget.listenerCount('bar'), 1);
-  deepStrictEqual(eventTarget.eventNames(), ['foo', 'bar']);
-  eventTarget.removeAllListeners('foo');
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  strictEqual(eventTarget.listenerCount('bar'), 1);
-  deepStrictEqual(eventTarget.eventNames(), ['bar']);
-  eventTarget.removeAllListeners();
-  strictEqual(eventTarget.listenerCount('foo'), 0);
-  strictEqual(eventTarget.listenerCount('bar'), 0);
-  deepStrictEqual(eventTarget.eventNames(), []);
 }
 
 {
@@ -298,25 +188,6 @@ ok(EventTarget);
   const target = new EventTarget();
   once(target, 'foo').then(common.mustCall());
   target.dispatchEvent(new Event('foo'));
-}
-
-{
-  const target = new NodeEventTarget();
-
-  process.on('warning', common.mustCall((warning) => {
-    ok(warning instanceof Error);
-    strictEqual(warning.name, 'MaxListenersExceededWarning');
-    strictEqual(warning.target, target);
-    strictEqual(warning.count, 2);
-    strictEqual(warning.type, 'foo');
-    ok(warning.message.includes(
-      '2 foo listeners added to NodeEventTarget'));
-  }));
-
-  strictEqual(target.getMaxListeners(), NodeEventTarget.defaultMaxListeners);
-  target.setMaxListeners(1);
-  target.on('foo', () => {});
-  target.on('foo', () => {});
 }
 
 {
