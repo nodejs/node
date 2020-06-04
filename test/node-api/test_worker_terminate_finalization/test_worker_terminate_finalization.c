2c10e0913c41863379daec1ebb6143515324d7dc
Michael Dawson <michael_dawson@ca.ibm.com>
2020-06-04 18:47:56 -0400

    squash: address platform and asan issues

--- test/node-api/test_worker_terminate_finalization/test_worker_terminate_finalization.c
+++ test/node-api/test_worker_terminate_finalization/test_worker_terminate_finalization.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <node_api.h>
 #include <assert.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include "../../js-native-api/common.h"
+
+#define BUFFER_SIZE 4
 
 int wrappedNativeData;
 napi_ref ref;
 void WrapFinalizer(napi_env env, void* data, void* hint) {
   uint32_t count;
   NAPI_CALL_RETURN_VOID(env, napi_reference_unref(env, ref, &count));
+  NAPI_CALL_RETURN_VOID(env, napi_delete_reference(env, ref));
+}
+
+void BufferFinalizer(napi_env env, void* data, void* hint) {
+  free(hint);
 }
 
 napi_value Test(napi_env env, napi_callback_info info) {
@@ -16,12 +23,12 @@ napi_value Test(napi_env env, napi_callback_info info) {
   napi_value argv[1];
   napi_value result;
   napi_ref ref;
-  napi_ref wrapResult;
+  void* bufferData = malloc(BUFFER_SIZE);
 
   NAPI_CALL(env, napi_get_cb_info(env, info, &argc, argv, NULL, NULL));
-  NAPI_CALL(env, napi_create_external_buffer(env, 4, (void*) malloc(4), NULL, NULL, &result));
+  NAPI_CALL(env, napi_create_external_buffer(env, BUFFER_SIZE, bufferData, BufferFinalizer, bufferData, &result));
   NAPI_CALL(env, napi_create_reference(env, result, 1, &ref));
-  NAPI_CALL(env, napi_wrap(env, argv[0], (void*) &wrappedNativeData, WrapFinalizer, NULL, &wrapResult));
+  NAPI_CALL(env, napi_wrap(env, argv[0], (void*) &wrappedNativeData, WrapFinalizer, NULL, NULL));
   return NULL;
 }
 
