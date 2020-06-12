409fdba6d5c052f14af3ea73f0abb36db197f3d3
Nathan Blair <engineer.nblair@gmail.com>
2020-06-12 23:35:38 +0000

    configure: account for CLANG_VENDOR when checking for llvm version
    Fixes: https://github.com/nodejs/node/issues/29536
    
    PR-URL: https://github.com/nodejs/node/pull/33860
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- configure.py
+++ configure.py
@@ -852,7 +852,7 @@ def get_nasm_version(asm):
 
 def get_llvm_version(cc):
   return get_version_helper(
-    cc, r"(^(?:FreeBSD )?clang version|based on LLVM) ([0-9]+\.[0-9]+)")
+    cc, r"(^(?:.+ )?clang version|based on LLVM) ([0-9]+\.[0-9]+)")
 
 def get_xcode_version(cc):
   return get_version_helper(
