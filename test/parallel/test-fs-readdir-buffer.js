e6d39b357bb6e96dea1d9fc4a6753ccdf06e5ac7
Kirill Shatskiy <shaackiy@gmail.com>
2020-06-13 12:08:32 +0300

    fs: add join function which takes Buffers and strings as args
    Refs: https://github.com/nodejs/node/issues/33348

--- test/parallel/test-fs-readdir-buffer.js
+++ test/parallel/test-fs-readdir-buffer.js
@@ -1,17 +1,17 @@
 'use strict';
+const common = require('../common');
 const fs = require('fs');
 
-const common = require('../common');
 if (!common.isOSX) {
-    common.skip('this tests works only on MacOS');
+  common.skip('this tests works only on MacOS');
 }
 
 const assert = require('assert');
 
 fs.readdir(
-    Buffer.from("/dev"),
-    {withFileTypes: true, encoding: "buffer"},
-    common.mustCall((e,d) => {
-        assert.strictEqual(e, null);
-    })
+  Buffer.from('/dev'),
+  { withFileTypes: true, encoding: 'buffer' },
+  common.mustCall((e, d) => {
+    assert.strictEqual(e, null);
+  })
 );
