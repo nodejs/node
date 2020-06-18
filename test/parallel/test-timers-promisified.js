64d22c320c67255b42c8185a333a92a499c95820
Denys Otrishko <shishugi@gmail.com>
2020-06-18 22:58:42 +0300

    timers: fix multipleResolves in promisified timeouts/immediates
    After successful timer finish the abort event callback would still
    reject (already resolved promise) upon calling abortController.abort().
    
    Signed-off-by: Denys Otrishko <shishugi@gmail.com>
    
    PR-URL: https://github.com/nodejs/node/pull/33949
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-timers-promisified.js
+++ test/parallel/test-timers-promisified.js
@@ -10,6 +10,8 @@ const { promisify } = require('util');
 const setTimeout = promisify(timers.setTimeout);
 const setImmediate = promisify(timers.setImmediate);
 
+process.on('multipleResolves', common.mustNotCall());
+
 {
   const promise = setTimeout(1);
   promise.then(common.mustCall((value) => {
@@ -64,6 +66,23 @@ const setImmediate = promisify(timers.setImmediate);
   const signal = ac.signal;
   ac.abort(); // Abort in advance
   assert.rejects(setImmediate(10, { signal }), /AbortError/);
+}
+
+{
+  // Check that aborting after resolve will not reject.
+  const ac = new AbortController();
+  const signal = ac.signal;
+  setTimeout(10, undefined, { signal }).then(() => {
+    ac.abort();
+  });
+}
+{
+  // Check that aborting after resolve will not reject.
+  const ac = new AbortController();
+  const signal = ac.signal;
+  setImmediate(10, { signal }).then(() => {
+    ac.abort();
+  });
 }
 
 {
