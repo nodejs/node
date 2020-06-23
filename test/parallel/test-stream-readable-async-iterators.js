4bb40078da9ff51372459ff187b74866d73c3fb2
Robert Nagy <ronagy@icloud.com>
2020-06-23 23:08:14 +0200

    stream: simpler and faster Readable async iterator
    Reimplement as an async generator instead of a custom
    iterator class.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/34887
    PR-URL: https://github.com/nodejs/node/pull/34035
    Refs: https://github.com/nodejs/node/issues/34680
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-stream-readable-async-iterators.js
+++ test/parallel/test-stream-readable-async-iterators.js
@@ -12,17 +12,6 @@ const assert = require('assert');
 
 async function tests() {
   {
-    const AsyncIteratorPrototype = Object.getPrototypeOf(
-      Object.getPrototypeOf(async function* () {}).prototype);
-    const rs = new Readable({
-      read() {}
-    });
-    assert.strictEqual(
-      Object.getPrototypeOf(Object.getPrototypeOf(rs[Symbol.asyncIterator]())),
-      AsyncIteratorPrototype);
-  }
-
-  {
     // v1 stream
 
     const stream = new Stream();
@@ -53,7 +42,9 @@ async function tests() {
     });
 
     const iter = Readable.prototype[Symbol.asyncIterator].call(stream);
-    iter.next().catch(common.mustCall((err) => {
+    await iter.next();
+    await iter.next();
+    await iter.next().catch(common.mustCall((err) => {
       assert.strictEqual(err.message, 'asd');
     }));
   }
@@ -189,9 +180,16 @@ async function tests() {
     resolved.forEach(common.mustCall(
       (item, i) => assert.strictEqual(item.value, 'hello-' + i), max));
 
-    errors.forEach((promise) => {
+    errors.slice(0, 1).forEach((promise) => {
       promise.catch(common.mustCall((err) => {
         assert.strictEqual(err.message, 'kaboom');
+      }));
+    });
+
+    errors.slice(1).forEach((promise) => {
+      promise.then(common.mustCall(({ done, value }) => {
+        assert.strictEqual(done, true);
+        assert.strictEqual(value, undefined);
       }));
     });
 
@@ -282,28 +280,6 @@ async function tests() {
 
     assert.strictEqual(err.message, 'kaboom');
     assert.strictEqual(received, 1);
-  }
-
-  {
-    // Iterator throw.
-
-    const readable = new Readable({
-      objectMode: true,
-      read() {
-        this.push('hello');
-      }
-    });
-
-    readable.on('error', common.mustCall((err) => {
-      assert.strictEqual(err.message, 'kaboom');
-    }));
-
-    const it = readable[Symbol.asyncIterator]();
-    it.throw(new Error('kaboom')).catch(common.mustCall((err) => {
-      assert.strictEqual(err.message, 'kaboom');
-    }));
-
-    assert.strictEqual(readable.destroyed, true);
   }
 
   {
@@ -577,12 +553,15 @@ async function tests() {
       assert.strictEqual(e, err);
     })(), (async () => {
       let e;
+      let x;
       try {
-        await d;
+        x = await d;
       } catch (_e) {
         e = _e;
       }
-      assert.strictEqual(e, err);
+      assert.strictEqual(e, undefined);
+      assert.strictEqual(x.done, true);
+      assert.strictEqual(x.value, undefined);
     })()]);
   }
 
