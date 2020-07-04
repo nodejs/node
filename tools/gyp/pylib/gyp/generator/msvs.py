8474c6b16c0d2a8a61cd633a6dab320d5201fafa
Michaël Zasso <targos@protonmail.com>
2020-07-04 16:50:02 +0200

    tools: hack gyp to generate clang-cl solutions

--- tools/gyp/pylib/gyp/generator/msvs.py
+++ tools/gyp/pylib/gyp/generator/msvs.py
@@ -3022,7 +3022,7 @@ def _GetMSBuildLocalProperties(msbuild_toolset):
             [
                 "PropertyGroup",
                 {"Label": "Locals"},
-                ["PlatformToolset", msbuild_toolset],
+                ["PlatformToolset", "ClangCL"],
             ]
         ]
     return properties
