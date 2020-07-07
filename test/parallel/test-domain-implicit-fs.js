46a9295fdf9d3674ae890f5fe056c657048b18f0
Anna Henningsen <anna@addaleax.net>
2020-07-07 18:05:33 +0200

    domain: fix unintentional deprecation warning
    646e5a471766e27e8317bb changed the way that the domain hook callback
    is called. Previously, the callback was only used in the case that
    async_hooks were *not* being used (since domains already integrate
    with async hooks the way they should), and the corresponding
    deprecation warning also only emitted in that case.
    
    However, that commit didn’t move that condition along when the code
    was ported from C++ to JS. As a consequence, the domain hook callback
    was used when it wasn’t necessary to use it, and the deprecation
    warning emitted accidentally along with it.
    
    Refs: https://github.com/nodejs/node/commit/646e5a471766e27e8317bb54d1fd1d2c72cffb69#diff-9f21ce1b9d6d46fdd07b969e8a04e140L192
    Refs: https://github.com/nodejs/node/commit/646e5a471766e27e8317bb54d1fd1d2c72cffb69#diff-e6db408e12db906ead6ddfac3de15a6fR119
    Refs: https://github.com/nodejs/node/pull/33801#issuecomment-654744913
    
    PR-URL: https://github.com/nodejs/node/pull/34245
    Fixes: https://github.com/nodejs/node/issues/34069
    Reviewed-By: Vladimir de Turckheim <vlad2t@hotmail.com>
    Reviewed-By: Shelley Vohr <codebytere@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: Andrey Pechkurov <apechkurov@gmail.com>
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>

--- test/parallel/test-domain-implicit-fs.js
+++ test/parallel/test-domain-implicit-fs.js
@@ -26,6 +26,8 @@ const common = require('../common');
 const assert = require('assert');
 const domain = require('domain');
 
+process.on('warning', common.mustNotCall());
+
 const d = new domain.Domain();
 
 d.on('error', common.mustCall(function(er) {
