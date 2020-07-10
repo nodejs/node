b58c06d92c3865b41a50171c2692e36c0b487420
Anna Henningsen <anna@addaleax.net>
2020-07-11 01:57:04 +0200

    src: add option to track unmanaged file descriptors
    Add the ability to track “raw” file descriptors, i.e. integers returned
    by `fs.open()`, and close them on `Environment` shutdown, to match the
    behavior for all other resource types (which are also closed on
    shutdown).
    
    PR-URL: https://github.com/nodejs/node/pull/34303
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- src/env.h
+++ src/env.h
@@ -1058,6 +1058,7 @@ class Environment : public MemoryRetainer {
   inline bool should_not_register_esm_loader() const;
   inline bool owns_process_state() const;
   inline bool owns_inspector() const;
+  inline bool tracks_unmanaged_fds() const;
   inline uint64_t thread_id() const;
   inline worker::Worker* worker_context() const;
   Environment* worker_parent_env() const;
@@ -1274,6 +1275,9 @@ class Environment : public MemoryRetainer {
   inline std::unordered_map<char*, std::unique_ptr<v8::BackingStore>>*
       released_allocated_buffers();
 
+  void AddUnmanagedFd(int fd);
+  void RemoveUnmanagedFd(int fd);
+
  private:
   inline void ThrowError(v8::Local<v8::Value> (*fun)(v8::Local<v8::String>),
                          const char* errmsg);
@@ -1413,6 +1417,8 @@ class Environment : public MemoryRetainer {
   int64_t base_object_count_ = 0;
   int64_t initial_base_object_count_ = 0;
   std::atomic_bool is_stopping_ { false };
+
+  std::unordered_set<int> unmanaged_fds_;
 
   std::function<void(Environment*, int)> process_exit_handler_ {
       DefaultProcessExitHandler };
