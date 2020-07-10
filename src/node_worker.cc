9e04070d3c97ec4dee2e970bf5ca33aeaca45ea5
Anna Henningsen <anna@addaleax.net>
2020-07-11 01:59:21 +0200

    worker: add option to track unmanaged file descriptors
    Add a public option for Workers which adds tracking for raw
    file descriptors, as currently, those resources are not cleaned
    up, unlike e.g. `FileHandle`s.
    
    PR-URL: https://github.com/nodejs/node/pull/34303
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- src/node_worker.cc
+++ src/node_worker.cc
@@ -309,7 +309,7 @@ void Worker::Run() {
             context,
             std::move(argv_),
             std::move(exec_argv_),
-            EnvironmentFlags::kNoFlags,
+            static_cast<EnvironmentFlags::Flags>(environment_flags_),
             thread_id_,
             std::move(inspector_parent_handle_)));
         if (is_stopped()) return;
@@ -456,7 +456,6 @@ void Worker::New(const FunctionCallbackInfo<Value>& args) {
 
   std::vector<std::string> exec_argv_out;
 
-  CHECK_EQ(args.Length(), 4);
   // Argument might be a string or URL
   if (!args[0]->IsNullOrUndefined()) {
     Utf8Value value(
@@ -582,6 +581,10 @@ void Worker::New(const FunctionCallbackInfo<Value>& args) {
   CHECK_EQ(limit_info->Length(), kTotalResourceLimitCount);
   limit_info->CopyContents(worker->resource_limits_,
                            sizeof(worker->resource_limits_));
+
+  CHECK(args[4]->IsBoolean());
+  if (args[4]->IsTrue() || env->tracks_unmanaged_fds())
+    worker->environment_flags_ |= EnvironmentFlags::kTrackUnmanagedFds;
 }
 
 void Worker::StartThread(const FunctionCallbackInfo<Value>& args) {
