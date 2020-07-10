a894eccbb966aafb93135453a58bb3d44c3ee6fd
James M Snell <jasnell@gmail.com>
2020-07-10 12:38:04 -0700

    quic: implement QuicSocket Promise API, part 2
    PR-URL: https://github.com/nodejs/node/pull/34283
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-quic-quicsocket-close.js
+++ test/parallel/test-quic-quicsocket-close.js
@@ -8,11 +8,12 @@ if (!common.hasQuic)
 const assert = require('assert');
 const { createQuicSocket } = require('net');
 
-{
-  const socket = createQuicSocket();
-  socket.close(common.mustCall());
-  socket.on('close', common.mustCall());
-  assert.throws(() => socket.close(), {
+const socket = createQuicSocket();
+socket.on('close', common.mustCall());
+
+(async function() {
+  await socket.close();
+  assert.rejects(() => socket.close(), {
     code: 'ERR_INVALID_STATE'
   });
-}
+})().then(common.mustCall());
