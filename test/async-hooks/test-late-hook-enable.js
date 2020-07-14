00823f29677c2fc6d2c543970cf18769d09b6a31
Anna Henningsen <anna@addaleax.net>
2020-07-14 19:06:37 +0200

    src: add callback scope for native immediates
    This ensures that microtasks scheduled by native immediates are run
    after the tasks are done. In particular, this affects the inspector
    integration since 6f9f546406820dc.
    
    Fixes: https://github.com/nodejs/node/issues/33002
    Refs: https://github.com/nodejs/node/pull/32523
    
    PR-URL: https://github.com/nodejs/node/pull/34366
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/async-hooks/test-late-hook-enable.js
+++ test/async-hooks/test-late-hook-enable.js
@@ -17,14 +17,16 @@ const fnsToTest = [setTimeout, (cb) => {
     });
   });
 }, (cb) => {
-  process.nextTick(() => {
-    cb();
+  setImmediate(() => {
+    process.nextTick(() => {
+      cb();
 
-    // We need to keep the event loop open for this to actually work
-    // since destroy hooks are triggered in unrefed Immediates
-    setImmediate(() => {
-      hook.disable();
-      assert.strictEqual(fnsToTest.length, 0);
+      // We need to keep the event loop open for this to actually work
+      // since destroy hooks are triggered in unrefed Immediates
+      setImmediate(() => {
+        hook.disable();
+        assert.strictEqual(fnsToTest.length, 0);
+      });
     });
   });
 }];
