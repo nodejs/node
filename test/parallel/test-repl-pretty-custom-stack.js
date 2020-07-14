8a081f4609a82c68ecacc107a8c57babda621721
Bradley Farias <bradley.meck@gmail.com>
2020-07-14 16:31:29 -0500

    repl: give repl entries unique names
    This is a workaround for the REPL for a problem when multiple of the
    entries have the same source text
    
    Fixes: https://github.com/nodejs/node/issues/1337
    Refs: https://bugs.chromium.org/p/v8/issues/detail?id=10284
    
    PR-URL: https://github.com/nodejs/node/pull/34372
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Anto Aravinth <anto.aravinth.cse@gmail.com>

--- test/parallel/test-repl-pretty-custom-stack.js
+++ test/parallel/test-repl-pretty-custom-stack.js
@@ -5,7 +5,7 @@ const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const repl = require('repl');
 
-const stackRegExp = /repl:[0-9]+:[0-9]+/g;
+const stackRegExp = /(REPL\d+):[0-9]+:[0-9]+/g;
 
 function run({ command, expected }) {
   let accum = '';
@@ -25,8 +25,8 @@ function run({ command, expected }) {
 
   r.write(`${command}\n`);
   assert.strictEqual(
-    accum.replace(stackRegExp, 'repl:*:*'),
-    expected.replace(stackRegExp, 'repl:*:*')
+    accum.replace(stackRegExp, '$1:*:*'),
+    expected.replace(stackRegExp, '$1:*:*')
   );
   r.close();
 }
@@ -48,8 +48,8 @@ const tests = [
   {
     // test .load for a file that throws
     command: `.load ${fixtures.path('repl-pretty-stack.js')}`,
-    expected: 'Uncaught Error: Whoops!--->\nrepl:*:*--->\nd (repl:*:*)' +
-              '--->\nc (repl:*:*)--->\nb (repl:*:*)--->\na (repl:*:*)\n'
+    expected: 'Uncaught Error: Whoops!--->\nREPL1:*:*--->\nd (REPL1:*:*)' +
+              '--->\nc (REPL1:*:*)--->\nb (REPL1:*:*)--->\na (REPL1:*:*)\n'
   },
   {
     command: 'let x y;',
@@ -67,7 +67,7 @@ const tests = [
   // test anonymous IIFE
   {
     command: '(function() { throw new Error(\'Whoops!\'); })()',
-    expected: 'Uncaught Error: Whoops!--->\nrepl:*:*\n'
+    expected: 'Uncaught Error: Whoops!--->\nREPL5:*:*\n'
   }
 ];
 
