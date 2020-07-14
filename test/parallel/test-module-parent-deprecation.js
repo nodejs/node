48678528ec510e6672b2df42ae1ac2069388fbd6
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-07-14 20:20:08 +0100

    module: update expected module.parent DEP number
    PR-URL: https://github.com/nodejs/node/pull/34368
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>

--- test/parallel/test-module-parent-deprecation.js
+++ test/parallel/test-module-parent-deprecation.js
@@ -8,7 +8,7 @@ common.expectWarning(
   'DeprecationWarning',
   'module.parent is deprecated due to accuracy issues. Please use ' +
     'require.main to find program entry point instead.',
-  'DEP0143'
+  'DEP0144'
 );
 
 assert.strictEqual(module.parent, null);
