2922e340f3c2ececa8ae698ebaa13a2d2ca8a935
Rich Trott <rtrott@gmail.com>
2020-07-16 21:35:49 -0700

    test: refactor test-watch-file
    The way the test is written, the callback for `watchFile()` should run
    exactly once, but the test permits multiple invocations. Use
    common.mustCall() to confirm a single invocation.

--- test/pummel/test-watch-file.js
+++ test/pummel/test-watch-file.js
@@ -21,31 +21,26 @@
 
 'use strict';
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const fs = require('fs');
 const fixtures = require('../common/fixtures');
 
 const f = fixtures.path('x.txt');
 
-let changes = 0;
+const callback = common.mustCall((curr, prev) => {
+  assert.notDeepStrictEqual(curr.mtime, prev.mtime);
+  fs.unwatchFile(f);
+  watchFile();
+  fs.unwatchFile(f);
+});
+
 function watchFile() {
-  fs.watchFile(f, (curr, prev) => {
-    changes++;
-    assert.notDeepStrictEqual(curr.mtime, prev.mtime);
-    fs.unwatchFile(f);
-    watchFile();
-    fs.unwatchFile(f);
-  });
+  fs.watchFile(f, callback);
 }
 
 watchFile();
 
-
 const fd = fs.openSync(f, 'w+');
 fs.writeSync(fd, 'xyz\n');
 fs.closeSync(fd);
-
-process.on('exit', function() {
-  assert.ok(changes > 0);
-});
