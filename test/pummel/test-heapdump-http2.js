ac5dfbcbd3a4542ef7711fe309db933f8c6c4981
Rich Trott <rtrott@gmail.com>
2020-07-17 14:53:41 -0700

    test: fix flaky test-heapdump-http2
    Replace stream.respondWithFile(__filename) with
    stream.respondWithFile(process.execPath). The test file is probably
    small enough on some operating systems to be sent immediately, without
    waiting for the receiving side to do anything.
    
    The fix was figured out by addaleax.
    
    Co-authored-by: Anna Henningsen <anna@addaleax.net>
    Ref: https://github.com/nodejs/node/issues/34389#issuecomment-659297173
    Fixes: https://github.com/nodejs/node/issues/34389
    
    PR-URL: https://github.com/nodejs/node/pull/34415
    Refs: https://github.com/nodejs/node/issues/34389
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/pummel/test-heapdump-http2.js
+++ test/pummel/test-heapdump-http2.js
@@ -14,7 +14,7 @@ const http2 = require('http2');
 
 const server = http2.createServer();
 server.on('stream', (stream) => {
-  stream.respondWithFile(__filename);
+  stream.respondWithFile(process.execPath);
 });
 server.listen(0, () => {
   const client = http2.connect(`http://localhost:${server.address().port}`);
