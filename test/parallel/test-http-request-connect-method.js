247401a98db089aba9521f76dfc1b7081b12bdf5
Priyank Singh <priyanksingh8@gmail.com>
2020-07-17 21:21:57 +0530

    Fixed linting

--- test/parallel/test-http-request-connect-method.js
+++ test/parallel/test-http-request-connect-method.js
@@ -7,19 +7,18 @@ const http = require('http');
 const server = http.createServer();
 
 server.on('connect', common.mustCall((req, stream) => {
-    assert.notStrictEqual(req.url.charAt(0), '/');
-    stream.end();
+  assert.notStrictEqual(req.url.charAt(0), '/');
+  stream.end();
 }));
 
 server.listen(0);
 
 server.on('listening', common.mustCall(() => {
-    const url = new URL(`http://localhost:${server.address().port}/example.com`);
+  const url = new URL(`http://localhost:${server.address().port}/example.com`);
 
-    const req = http.request(url, {method: 'CONNECT'}).end();
-    req.once('connect', common.mustCall(res => {
-        res.destroy();
-        server.close();
-    }))
+  const req = http.request(url, { method: 'CONNECT' }).end();
+  req.once('connect', common.mustCall((res) => {
+    res.destroy();
+    server.close();
+  }));
 }));
-
