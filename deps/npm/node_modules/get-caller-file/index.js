7ef1f6a71dacd6474afc39590127117beeb8b757
claudiahdz <cghr1990@gmail.com>
2020-07-21 15:51:21 -0500

    deps: upgrade npm to 6.14.7
    PR-URL: https://github.com/nodejs/node/pull/34468
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ruy Adorno <ruyadorno@github.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Shelley Vohr <codebytere@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- deps/npm/node_modules/get-caller-file/index.js
+++ deps/npm/node_modules/get-caller-file/index.js
@@ -1,20 +1,22 @@
-'use strict';
-
+"use strict";
 // Call this function in a another function to find out the file from
 // which that function was called from. (Inspects the v8 stack trace)
 //
 // Inspired by http://stackoverflow.com/questions/13227489
-
-module.exports = function getCallerFile(_position) {
-  var oldPrepareStackTrace = Error.prepareStackTrace;
-  Error.prepareStackTrace = function(err, stack) { return stack; };
-  var stack = new Error().stack;
-  Error.prepareStackTrace = oldPrepareStackTrace;
-
-  var position = _position ? _position : 2;
-
-  // stack[0] holds this file
-  // stack[1] holds where this function was called
-  // stack[2] holds the file we're interested in
-  return stack[position] ? stack[position].getFileName() : undefined;
+module.exports = function getCallerFile(position) {
+    if (position === void 0) { position = 2; }
+    if (position >= Error.stackTraceLimit) {
+        throw new TypeError('getCallerFile(position) requires position be less then Error.stackTraceLimit but position was: `' + position + '` and Error.stackTraceLimit was: `' + Error.stackTraceLimit + '`');
+    }
+    var oldPrepareStackTrace = Error.prepareStackTrace;
+    Error.prepareStackTrace = function (_, stack) { return stack; };
+    var stack = new Error().stack;
+    Error.prepareStackTrace = oldPrepareStackTrace;
+    if (stack !== null && typeof stack === 'object') {
+        // stack[0] holds this file
+        // stack[1] holds where this function was called
+        // stack[2] holds the file we're interested in
+        return stack[position] ? stack[position].getFileName() : undefined;
+    }
 };
+//# sourceMappingURL=index.js.map
