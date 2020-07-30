dc00a07426f0b2dee9bfe39c766a703eb9b5b880
Gus Caplan <me@gus.host>
2020-07-30 10:28:38 -0500

    Revert "module: disable cjs snapshotting into esm loader"
    This reverts commit 1fe39f0b4bad8da38e5f02542c176d5999ad3ecb.
    
    PR-URL: https://github.com/nodejs/node/pull/34562
    Reviewed-By: Bradley Farias <bradley.meck@gmail.com>
    Reviewed-By: Guy Bedford <guybedford@gmail.com>

--- test/es-module/test-esm-snapshot.mjs
+++ test/es-module/test-esm-snapshot.mjs
@@ -3,4 +3,4 @@ import '../fixtures/es-modules/esm-snapshot-mutator.js';
 import one from '../fixtures/es-modules/esm-snapshot.js';
 import assert from 'assert';
 
-assert.strictEqual(one, 2);
+assert.strictEqual(one, 1);
