71ee48863adebc37c8733c2b32911fb47d706be6
Rich Trott <rtrott@gmail.com>
2020-07-30 10:46:22 -0700

    test: change Fixes: to Refs:
    Tests don't fix things generally, so use "Refs:" to refer people to
    GitHub issues.
    
    PR-URL: https://github.com/nodejs/node/pull/34568
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-http-server-response-standalone.js
+++ test/parallel/test-http-server-response-standalone.js
@@ -6,8 +6,8 @@ const { Writable } = require('stream');
 const assert = require('assert');
 
 // Check that ServerResponse can be used without a proper Socket
-// Fixes: https://github.com/nodejs/node/issues/14386
-// Fixes: https://github.com/nodejs/node/issues/14381
+// Refs: https://github.com/nodejs/node/issues/14386
+// Refs: https://github.com/nodejs/node/issues/14381
 
 const res = new ServerResponse({
   method: 'GET',
