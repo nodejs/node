0a4401713ad5ebf1a70b77c99ef8bf3257ad84b4
Gerhard Stoebich <18708370+Flarna@users.noreply.github.com>
2020-08-03 23:19:54 +0200

    async_hooks: avoid unneeded AsyncResource creation
    Inspired by the callstack at https://github.com/nodejs/node/issues/34556#issuecomment-666743528
    
    If the wanted store is equal to the active store it's not needed to
    create an AsyncResource.
    
    Refs: https://github.com/nodejs/node/issues/34556#issuecomment-666743528
    
    PR-URL: https://github.com/nodejs/node/pull/34616
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: Vladimir de Turckheim <vlad2t@hotmail.com>
    Reviewed-By: Andrey Pechkurov <apechkurov@gmail.com>

--- test/async-hooks/test-async-local-storage-run-resource.js
+++ test/async-hooks/test-async-local-storage-run-resource.js
@@ -10,8 +10,21 @@ const asyncLocalStorage = new AsyncLocalStorage();
 
 const outerResource = executionAsyncResource();
 
-asyncLocalStorage.run(new Map(), () => {
-  assert.notStrictEqual(executionAsyncResource(), outerResource);
+const store = new Map();
+asyncLocalStorage.run(store, () => {
+  assert.strictEqual(asyncLocalStorage.getStore(), store);
+  const innerResource = executionAsyncResource();
+  assert.notStrictEqual(innerResource, outerResource);
+  asyncLocalStorage.run(store, () => {
+    assert.strictEqual(asyncLocalStorage.getStore(), store);
+    assert.strictEqual(executionAsyncResource(), innerResource);
+    const otherStore = new Map();
+    asyncLocalStorage.run(otherStore, () => {
+      assert.strictEqual(asyncLocalStorage.getStore(), otherStore);
+      assert.notStrictEqual(executionAsyncResource(), innerResource);
+      assert.notStrictEqual(executionAsyncResource(), outerResource);
+    });
+  });
 });
 
 assert.strictEqual(executionAsyncResource(), outerResource);
