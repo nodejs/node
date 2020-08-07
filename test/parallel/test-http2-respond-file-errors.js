dbcb36d55340d33d2bfa87a3581e49ed769f484a
Denys Otrishko <shishugi@gmail.com>
2020-08-07 20:31:20 +0300

    errors: improve ERR_INVALID_OPT_VALUE error
    * use util.inspect for value presentation
    * allow to optionally specify error reason
    
    PR-URL: https://github.com/nodejs/node/pull/34671
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Mary Marchini <oss@mmarchini.me>

--- test/parallel/test-http2-respond-file-errors.js
+++ test/parallel/test-http2-respond-file-errors.js
@@ -6,6 +6,7 @@ if (!common.hasCrypto)
 const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const http2 = require('http2');
+const { inspect } = require('util');
 
 const optionsWithTypeError = {
   offset: 'number',
@@ -45,7 +46,7 @@ server.on('stream', common.mustCall((stream) => {
         {
           name: 'TypeError',
           code: 'ERR_INVALID_OPT_VALUE',
-          message: `The value "${String(types[type])}" is invalid ` +
+          message: `The value "${inspect(types[type])}" is invalid ` +
                    `for option "${option}"`
         }
       );
