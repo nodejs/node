aa20c6fb2636fb5fa5e99a5f2a101d702ae87037
Bartosz Sosnowski <bartosz@janeasystems.com>
2020-08-13 10:31:15 +0200

    fixup: add logging

--- test/parallel/test-unicode-node-options.js
+++ test/parallel/test-unicode-node-options.js
@@ -11,9 +11,13 @@ if (process.argv.length === 2) {
   const NODE_OPTIONS = `--redirect-warnings=${expected_redirect_value}`;
   const result = cp.spawnSync(process.argv0,
                               ['--expose-internals', __filename, 'test'],
-                              { env: { NODE_OPTIONS } });
+                              {
+                                env: { NODE_OPTIONS },
+                                stdio: 'inherit'
+                              });
   assert.strictEqual(result.status, 0);
 } else {
   const redirect_value = getOptionValue('--redirect-warnings');
+  console.log(`--redirect-warings=${redirect_value}`);
   assert.strictEqual(redirect_value, expected_redirect_value);
 }
