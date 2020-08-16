8f38c19c0860daa13563f4c9ab253cd9b5be49ab
Denys Otrishko <shishugi@gmail.com>
2020-08-16 12:28:54 +0300

    esm: improve error message of ERR_UNSUPPORTED_ESM_URL_SCHEME
    Refs: https://github.com/nodejs/node/issues/34765
    
    PR-URL: https://github.com/nodejs/node/pull/34795
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Jan Krems <jan.krems@gmail.com>
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: Bradley Farias <bradley.meck@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/es-module/test-esm-dynamic-import.js
+++ test/es-module/test-esm-dynamic-import.js
@@ -8,15 +8,11 @@ const absolutePath = require.resolve('../fixtures/es-modules/test-esm-ok.mjs');
 const targetURL = new URL('file:///');
 targetURL.pathname = absolutePath;
 
-function expectErrorProperty(result, propertyKey, value) {
-  Promise.resolve(result)
-    .catch(common.mustCall((error) => {
-      assert.strictEqual(error[propertyKey], value);
-    }));
-}
-
-function expectModuleError(result, err) {
-  expectErrorProperty(result, 'code', err);
+function expectModuleError(result, code, message) {
+  Promise.resolve(result).catch(common.mustCall((error) => {
+    assert.strictEqual(error.code, code);
+    if (message) assert.strictEqual(error.message, message);
+  }));
 }
 
 function expectOkNamespace(result) {
@@ -60,4 +56,13 @@ function expectFsNamespace(result) {
                     'ERR_MODULE_NOT_FOUND');
   expectModuleError(import('http://example.com/foo.js'),
                     'ERR_UNSUPPORTED_ESM_URL_SCHEME');
+  if (common.isWindows) {
+    const msg =
+      'Only file and data URLs are supported by the default ESM loader. ' +
+      'Absolute Windows paths without prefix are not valid URLs, ' +
+      "consider using 'file://' prefix. Received protocol 'c:'";
+    expectModuleError(import('C:\\example\\foo.mjs'),
+                      'ERR_UNSUPPORTED_ESM_URL_SCHEME',
+                      msg);
+  }
 })();
