820b9a8f7ac136008504eb5a4b8b08329511ce50
Rich Trott <rtrott@gmail.com>
2020-08-27 21:18:49 -0700

    test: make test-tls-reuse-host-from-socket pass without internet
    Start up a TLS server on localhost rather than using example.org.
    
    PR-URL: https://github.com/nodejs/node/pull/34953
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-tls-reuse-host-from-socket.js
+++ test/parallel/test-tls-reuse-host-from-socket.js
@@ -25,13 +25,19 @@ const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
 
-const tls = require('tls');
-
 const net = require('net');
+const tls = require('tls');
+const fixtures = require('../common/fixtures');
 
-const socket = net.connect(443, 'www.example.org', common.mustCall(() => {
-  const secureSocket = tls.connect({ socket }, common.mustCall(() => {
-    secureSocket.destroy();
-    console.log('ok');
+const server = tls.createServer({
+  key: fixtures.readKey('agent1-key.pem'),
+  cert: fixtures.readKey('agent1-cert.pem')
+}).listen(0, common.mustCall(() => {
+  const socket = net.connect(server.address().port, common.mustCall(() => {
+    const opts = { socket, rejectUnauthorized: false };
+    const secureSocket = tls.connect(opts, common.mustCall(() => {
+      secureSocket.destroy();
+      server.close();
+    }));
   }));
 }));
