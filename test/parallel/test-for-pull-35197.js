40e47a723f5c5c4450ccfa636b66858e70bba789
masx200 <34191203+masx200@users.noreply.github.com>
2020-09-15 07:50:09 +0800

    Update test-for-pull-35197.js

--- test/parallel/test-for-pull-35197.js
+++ test/parallel/test-for-pull-35197.js
@@ -4,6 +4,11 @@
 const common = require('../common');
 const fixtures = require('../common/fixtures');
 const assert = require('assert');
-function loadPEM(n) {
-  return fixtures.readKey(`${n}.pem`);
-}
+
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const serverOptions = {
+  key: fixtures.readKey('agent1-key.pem'),
+  cert: fixtures.readKey('agent1-cert.pem')
+};
