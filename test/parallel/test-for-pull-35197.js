f8db9cd97bba7cf699d2c1e1527d27e49b24698e
masx200 <34191203+masx200@users.noreply.github.com>
2020-09-15 07:14:00 +0800

    Update test-for-pull-35197.js

--- test/parallel/test-for-pull-35197.js
+++ test/parallel/test-for-pull-35197.js
@@ -3,7 +3,7 @@
 'use strict';
 const common = require('../common');
 const fixtures = require('../common/fixtures');
-
+const assert = require('assert');
 function loadPEM(n) {
   return fixtures.readKey(`${n}.pem`);
 }
