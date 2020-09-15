832d616fc57329c29ee86c0ecf595143a570d123
masx200 <34191203+masx200@users.noreply.github.com>
2020-09-15 10:03:02 +0800

    Update test-for-pull-35197.js

--- test/parallel/test-for-pull-35197.js
+++ test/parallel/test-for-pull-35197.js
@@ -18,5 +18,25 @@ const server = http2.createSecureServer(
 const request=req
       assert(true===request.socket.encrypted)
       assert("encrypted" in request.socket)
-
+res.end()
     }));
+server.listen(common.mustCall(() => {
+const port=server.address().port
+
+const client = http2.connect('https://localhost:'+port, {
+  ca:  fixtures.readKey('agent1-cert.pem'),
+rejectUnauthorized: false
+});
+    const req = client.request({
+     
+      
+      
+    });
+    req.on('response',common.mustCall( (headers, flags) => {
+ console.log(headers)
+
+server.close(common.mustCall(()=>{}))
+}));
+    req.end();
+    
+  }));
