4f0b74a10b6e423c1dab26fbbd53cf1cd35fe983
masx200 <34191203+masx200@users.noreply.github.com>
2020-09-15 09:51:14 +0800

    Update test-for-pull-35197.js

--- test/parallel/test-for-pull-35197.js
+++ test/parallel/test-for-pull-35197.js
@@ -1,10 +1,10 @@
-//todo
+// todo
 
 'use strict';
 const common = require('../common');
 const fixtures = require('../common/fixtures');
 const assert = require('assert');
-
+const http2 = require('http2');
 if (!common.hasCrypto)
   common.skip('missing crypto');
 
@@ -12,3 +12,11 @@ const serverOptions = {
   key: fixtures.readKey('agent1-key.pem'),
   cert: fixtures.readKey('agent1-cert.pem')
 };
+const server = http2.createSecureServer(
+    serverOptions,
+    common.mustCall((req, res) => {
+const request=req
+      assert(true===request.socket.encrypted)
+      assert("encrypted" in request.socket)
+
+    }));
