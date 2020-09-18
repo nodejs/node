734fa6392553bb529bf3c8889a9f9ded03e9d300
Rich Trott <rtrott@gmail.com>
2020-09-18 09:36:10 -0700

    fixup! fixup! Update test-for-pull-35197.js

--- test/parallel/test-for-pull-35197.js
+++ test/parallel/test-for-pull-35197.js
@@ -1,22 +1,25 @@
 'use strict';
 const common = require('../common');
+if (!common.hasCrypto) {
+  common.skip('missing crypto');
+}
+
 const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const http2 = require('http2');
-if (!common.hasCrypto) {
-  common.skip('missing crypto');
 
-}
 const serverOptions = {
   key: fixtures.readKey('agent1-key.pem'),
   cert: fixtures.readKey('agent1-cert.pem')
 };
-const server = http2.createSecureServer(serverOptions, common.mustCall((req, res) => {
-  const request = req;
-  assert.strictEqual(true, request.socket.encrypted);
-  assert('encrypted' in request.socket);
-  res.end();
-}));
+const server = http2.createSecureServer(serverOptions, common.mustCall(
+  (req, res) => {
+    const request = req;
+    assert.strictEqual(request.socket.encrypted, true);
+    assert.ok('encrypted' in request.socket);
+    res.end();
+  }
+));
 server.listen(common.mustCall(() => {
   const port = server.address().port;
   const client = http2.connect('https://localhost:' + port, {
