40eff08893e49f4467621280f4bf68d3f33c40f7
Guy Bedford <guybedford@gmail.com>
2020-09-30 04:24:34 -0700

    module: refine module type mismatch error cases
    PR-URL: https://github.com/nodejs/node/pull/35426
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Bradley Farias <bradley.meck@gmail.com>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>

--- test/es-module/test-esm-cjs-exports.js
+++ test/es-module/test-esm-cjs-exports.js
@@ -7,7 +7,7 @@ const assert = require('assert');
 
 const entry = fixtures.path('/es-modules/cjs-exports.mjs');
 
-const child = spawn(process.execPath, [entry]);
+let child = spawn(process.execPath, [entry]);
 child.stderr.setEncoding('utf8');
 let stdout = '';
 child.stdout.setEncoding('utf8');
@@ -18,4 +18,18 @@ child.on('close', common.mustCall((code, signal) => {
   assert.strictEqual(code, 0);
   assert.strictEqual(signal, null);
   assert.strictEqual(stdout, 'ok\n');
+}));
+
+const entryInvalid = fixtures.path('/es-modules/cjs-exports-invalid.mjs');
+child = spawn(process.execPath, [entryInvalid]);
+let stderr = '';
+child.stderr.setEncoding('utf8');
+child.stderr.on('data', (data) => {
+  stderr += data;
+});
+child.on('close', common.mustCall((code, signal) => {
+  assert.strictEqual(code, 1);
+  assert.strictEqual(signal, null);
+  assert.ok(stderr.includes('Warning: To load an ES module'));
+  assert.ok(stderr.includes('Unexpected token \'export\''));
 }));
