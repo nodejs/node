e7fca82a4e9ba4bc7a04c274da606f80796dcf43
James M Snell <jasnell@gmail.com>
2020-10-03 18:09:00 -0700

    src: use env->ThrowUVException in pipe_wrap
    Signed-off-by: James M Snell <jasnell@gmail.com>
    
    PR-URL: https://github.com/nodejs/node/pull/35493
    Reviewed-By: Daijiro Wachi <daijiro.wachi@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: David Carlier <devnexen@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/pipe_wrap.cc
+++ src/pipe_wrap.cc
@@ -40,7 +40,6 @@ using v8::Function;
 using v8::FunctionCallbackInfo;
 using v8::FunctionTemplate;
 using v8::Int32;
-using v8::Isolate;
 using v8::Local;
 using v8::MaybeLocal;
 using v8::Object;
@@ -216,9 +215,8 @@ void PipeWrap::Open(const FunctionCallbackInfo<Value>& args) {
   int err = uv_pipe_open(&wrap->handle_, fd);
   wrap->set_fd(fd);
 
-  Isolate* isolate = env->isolate();
   if (err != 0)
-    isolate->ThrowException(UVException(isolate, err, "uv_pipe_open"));
+    env->ThrowUVException(err, "uv_pipe_open");
 }
 
 
