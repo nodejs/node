b46f7be071701baec241c1999f731c50d376df81
bcoe <bencoe@google.com>
2020-10-08 17:17:01 -0700

    fs: rimraf should not recurse on failure
    PR-URL: https://github.com/nodejs/node/pull/35566
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-fs-open-no-close.js
+++ test/parallel/test-fs-open-no-close.js
@@ -4,14 +4,7 @@
 // Failing to close a file should not keep the event loop open.
 
 const common = require('../common');
-
-// This issue only shows up on Raspberry Pi devices in our CI. When this test is
-// moved out of known_issues, this check can be removed, as the test should pass
-// on all platforms at that point.
 const assert = require('assert');
-if (process.arch !== 'arm' || process.config.variables.arm_version > 7) {
-  assert.fail('This test is for Raspberry Pi devices in CI');
-}
 
 const fs = require('fs');
 
