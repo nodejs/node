f3ce2811ddae10808615f0314b789e2efe5eb3ce
Guy Bedford <guybedford@gmail.com>
2020-10-10 04:08:58 -0700

    module: use Wasm CJS lexer when available
    PR-URL: https://github.com/nodejs/node/pull/35583
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Geoffrey Booth <webmaster@geoffreybooth.com>

--- test/parallel/test-bootstrap-modules.js
+++ test/parallel/test-bootstrap-modules.js
@@ -56,7 +56,6 @@ const expectedModules = new Set([
   'NativeModule internal/modules/cjs/helpers',
   'NativeModule internal/modules/cjs/loader',
   'NativeModule internal/modules/esm/create_dynamic_module',
-  'NativeModule internal/deps/cjs-module-lexer/lexer',
   'NativeModule internal/modules/esm/get_format',
   'NativeModule internal/modules/esm/get_source',
   'NativeModule internal/modules/esm/loader',
