c5b630ad64b69d2c2b6902b604debb5f45c7c1cf
Daijiro Wachi <daijiro.wachi@gmail.com>
2020-10-12 23:15:35 +0900

    test: update url test for win

--- test/cctest/test_url.cc
+++ test/cctest/test_url.cc
@@ -136,15 +136,15 @@ TEST_F(URLTest, FromFilePath) {
 #ifdef _WIN32
   file_url = URL::FromFilePath("C:\\Program Files\\");
   EXPECT_EQ("file:", file_url.protocol());
-  EXPECT_EQ("/C:/Program%20Files/", file_url.path());
+  EXPECT_EQ("//C:/Program%20Files/", file_url.path());
 
   file_url = URL::FromFilePath("C:\\a\\b\\c");
   EXPECT_EQ("file:", file_url.protocol());
-  EXPECT_EQ("/C:/a/b/c", file_url.path());
+  EXPECT_EQ("//C:/a/b/c", file_url.path());
 
   file_url = URL::FromFilePath("b:\\a\\%%.js");
   EXPECT_EQ("file:", file_url.protocol());
-  EXPECT_EQ("/b:/a/%25%25.js", file_url.path());
+  EXPECT_EQ("//b:/a/%25%25.js", file_url.path());
 #else
   file_url = URL::FromFilePath("/");
   EXPECT_EQ("file:", file_url.protocol());
