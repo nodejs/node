98f0700798d777f449df52f044a901fadfe78774
Jiawen Geng <technicalcute@gmail.com>
2020-10-14 12:22:20 +0000

    deps: fix V8 build issue with inline methods
    PR-URL: https://github.com/nodejs/node/pull/35415
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>
    
    deps: patch for v8 on windows
    
    PR-URL: https://github.com/nodejs/node/pull/40010
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    
    deps: patch v8 for vs2019 in std17
    
    PR-URL: https://github.com/nodejs/node/pull/40060
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    
    deps: workaround debug link error on Windows
    
    PR-URL: https://github.com/nodejs/node/pull/38807
    Refs: https://github.com/nodejs/node/pull/38788
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Ash Cripps <acripps@redhat.com>
    PR-URL: https://github.com/nodejs/node/pull/45579

--- deps/v8/src/objects/fixed-array-inl.h
+++ deps/v8/src/objects/fixed-array-inl.h
@@ -83,6 +83,7 @@ bool FixedArray::is_the_hole(Isolate* isolate, int index) {
   return get(isolate, index).IsTheHole(isolate);
 }
 
+#if !defined(_WIN32) || (defined(_WIN64) && _MSC_VER < 1930 && __cplusplus < 201703L)
 void FixedArray::set(int index, Smi value) {
   DCHECK_NE(map(), GetReadOnlyRoots().fixed_cow_array_map());
   DCHECK_LT(static_cast<unsigned>(index), static_cast<unsigned>(length()));
@@ -90,6 +91,7 @@ void FixedArray::set(int index, Smi value) {
   int offset = OffsetOfElementAt(index);
   RELAXED_WRITE_FIELD(*this, offset, value);
 }
+#endif
 
 void FixedArray::set(int index, Object value) {
   DCHECK_NE(GetReadOnlyRoots().fixed_cow_array_map(), map());
