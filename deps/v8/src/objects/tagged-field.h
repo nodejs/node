70f2bb8e51698799a54016138ae0625bf29afbb1
Jiawen Geng <technicalcute@gmail.com>
2020-10-14 12:22:20 +0000

    deps: fix V8 build issue with inline methods
    PR-URL: https://github.com/nodejs/node/pull/35415
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>

--- deps/v8/src/objects/tagged-field.h
+++ deps/v8/src/objects/tagged-field.h
@@ -48,7 +48,7 @@ class TaggedField : public AllStatic {
                                int offset = 0);
 
   static inline void Relaxed_Store(HeapObject host, T value);
-  static inline void Relaxed_Store(HeapObject host, int offset, T value);
+  static void Relaxed_Store(HeapObject host, int offset, T value);
 
   static inline T Acquire_Load(HeapObject host, int offset = 0);
   static inline T Acquire_Load(IsolateRoot isolate, HeapObject host,
