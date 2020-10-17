4a60d84dc43d7e1fc5de82d48989814c216a0fd9
Michaël Zasso <targos@protonmail.com>
2020-10-17 12:34:16 +0200

    module: only try to enrich CJS syntax errors
    It is guaranteed that V8 throws a syntax error when `import` or `export`
    is used outside of ESM.
    
    Fixes: https://github.com/nodejs/node/issues/35687
    
    PR-URL: https://github.com/nodejs/node/pull/35691
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/es-module/test-esm-cjs-load-error-note.mjs
+++ test/es-module/test-esm-cjs-load-error-note.mjs
@@ -10,6 +10,10 @@ const Import2 = fixtures.path('/es-modules/es-note-promiserej-import-2.cjs');
 const Import3 = fixtures.path('/es-modules/es-note-unexpected-import-3.cjs');
 const Import4 = fixtures.path('/es-modules/es-note-unexpected-import-4.cjs');
 const Import5 = fixtures.path('/es-modules/es-note-unexpected-import-5.cjs');
+const Error1 = fixtures.path('/es-modules/es-note-error-1.mjs');
+const Error2 = fixtures.path('/es-modules/es-note-error-2.mjs');
+const Error3 = fixtures.path('/es-modules/es-note-error-3.mjs');
+const Error4 = fixtures.path('/es-modules/es-note-error-4.mjs');
 
 // Expect note to be included in the error output
 const expectedNote = 'To load an ES module, ' +
@@ -105,4 +109,56 @@ pImport5.on('close', mustCall((code) => {
   assert.strictEqual(code, 0);
   assert.ok(!pImport5Stderr.includes(expectedNote),
             `${expectedNote} must not be included in ${pImport5Stderr}`);
+}));
+
+const pError1 = spawn(process.execPath, [Error1]);
+let pError1Stderr = '';
+pError1.stderr.setEncoding('utf8');
+pError1.stderr.on('data', (data) => {
+  pError1Stderr += data;
+});
+pError1.on('close', mustCall((code) => {
+  assert.strictEqual(code, expectedCode);
+  assert.ok(pError1Stderr.includes('Error: some error'));
+  assert.ok(!pError1Stderr.includes(expectedNote),
+            `${expectedNote} must not be included in ${pError1Stderr}`);
+}));
+
+const pError2 = spawn(process.execPath, [Error2]);
+let pError2Stderr = '';
+pError2.stderr.setEncoding('utf8');
+pError2.stderr.on('data', (data) => {
+  pError2Stderr += data;
+});
+pError2.on('close', mustCall((code) => {
+  assert.strictEqual(code, expectedCode);
+  assert.ok(pError2Stderr.includes('string'));
+  assert.ok(!pError2Stderr.includes(expectedNote),
+            `${expectedNote} must not be included in ${pError2Stderr}`);
+}));
+
+const pError3 = spawn(process.execPath, [Error3]);
+let pError3Stderr = '';
+pError3.stderr.setEncoding('utf8');
+pError3.stderr.on('data', (data) => {
+  pError3Stderr += data;
+});
+pError3.on('close', mustCall((code) => {
+  assert.strictEqual(code, expectedCode);
+  assert.ok(pError3Stderr.includes('null'));
+  assert.ok(!pError3Stderr.includes(expectedNote),
+            `${expectedNote} must not be included in ${pError3Stderr}`);
+}));
+
+const pError4 = spawn(process.execPath, [Error4]);
+let pError4Stderr = '';
+pError4.stderr.setEncoding('utf8');
+pError4.stderr.on('data', (data) => {
+  pError4Stderr += data;
+});
+pError4.on('close', mustCall((code) => {
+  assert.strictEqual(code, expectedCode);
+  assert.ok(pError4Stderr.includes('undefined'));
+  assert.ok(!pError4Stderr.includes(expectedNote),
+            `${expectedNote} must not be included in ${pError4Stderr}`);
 }));
