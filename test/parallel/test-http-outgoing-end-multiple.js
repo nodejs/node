387d92fd0e468232579f0f863ca532242f9943bc
rickyes <0x19951125@gmail.com>
2020-10-28 15:14:48 +0800

    http: onFinish will not be triggered again when finished
    PR-URL: https://github.com/nodejs/node/pull/35845
    Fixes: https://github.com/nodejs/node/issues/35833
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-http-outgoing-end-multiple.js
+++ test/parallel/test-http-outgoing-end-multiple.js
@@ -3,9 +3,17 @@ const common = require('../common');
 const assert = require('assert');
 const http = require('http');
 
+const onWriteAfterEndError = common.mustCall((err) => {
+  assert.strictEqual(err.code, 'ERR_STREAM_WRITE_AFTER_END');
+}, 2);
+
 const server = http.createServer(common.mustCall(function(req, res) {
   res.end('testing ended state', common.mustCall());
-  res.end(common.mustCall());
+  res.end(common.mustCall((err) => {
+    assert.strictEqual(err.code, 'ERR_STREAM_ALREADY_FINISHED');
+  }));
+  res.end('end', onWriteAfterEndError);
+  res.on('error', onWriteAfterEndError);
   res.on('finish', common.mustCall(() => {
     res.end(common.mustCall((err) => {
       assert.strictEqual(err.code, 'ERR_STREAM_ALREADY_FINISHED');
