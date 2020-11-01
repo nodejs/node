22acc3be557c9af3886b3cd2f13314dcd95684a3
Plamen Dragiyski <plamen@dragiyski.org>
2020-11-01 04:38:17 -0800

    test: use URLs when examine the stack trace
    Windows path (using backslash) won't be found in stack trace anymore.
    Use appropriate url.fileURLToPath and url.pathToFileURL when examine
    the stack trace.

--- test/parallel/test-http-timeout-client-warning.js
+++ test/parallel/test-http-timeout-client-warning.js
@@ -2,12 +2,13 @@
 const common = require('../common');
 const http = require('http');
 const assert = require('assert');
+const { pathToFileURL } = require('url');
 
 // Checks that the setTimeout duration overflow warning is emitted
 // synchronously and therefore contains a meaningful stacktrace.
 
 process.on('warning', common.mustCall((warning) => {
-  assert(warning.stack.includes(__filename));
+  assert(warning.stack.includes(pathToFileURL(__filename)));
 }));
 
 const server = http.createServer((req, resp) => resp.end());
