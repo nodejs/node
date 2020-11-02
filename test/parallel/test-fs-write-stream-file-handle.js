72873cf3dc63d44819a8b08e597f95244b8406e9
Momtchil Momtchev <momtchil@momtchev.com>
2020-11-02 13:10:36 +0100

    stream: add FileHandle support to Read/WriteStream
    Support creating a Read/WriteStream from a
    FileHandle instead of a raw file descriptor
    Add an EventEmitter to FileHandle with a single
    'close' event
    
    Refs: https://github.com/nodejs/node/issues/35240

---
+++ test/parallel/test-fs-write-stream-file-handle.js
@@ -0,0 +1,17 @@
+'use strict';
+const common = require('../common');
+const fs = require('fs');
+const path = require('path');
+const tmpdir = require('../common/tmpdir');
+const file = path.join(tmpdir.path, '/write_stream_filehandle_test.txt');
+const input = 'hello world';
+
+tmpdir.refresh();
+
+fs.promises.open(file, 'w+').then((handle) => {
+  handle.on('close', common.mustCall());
+  const stream = fs.createWriteStream(null, { fd: handle });
+
+  stream.end(input);
+  stream.on('close', common.mustCall());
+});
