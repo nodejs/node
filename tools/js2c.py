23dd2b00dd333592089e4b1a73e94511203e9cf3
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-11-05 21:47:47 +0100

    tools: fix config serialization w/ long strings
    So that “config.gypi” gets serialized correctly in cases such as:
    
        ./configure --v8-options='--write-protect-code-memory \
        --wasm-write-protect-code-memory'
    
    Where “v8_options” gets prettyprinted into a multiline string.
    
    PR-URL: https://github.com/nodejs/node/pull/35982
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- tools/js2c.py
+++ tools/js2c.py
@@ -163,9 +163,11 @@ def handle_config_gypi(config_filename):
 def jsonify(config):
   # 1. string comments
   config = re.sub(r'#.*?\n', '', config)
+  # 2. join multiline strings
+  config = re.sub(r"'$\s+'", '', config, flags=re.M)
   # 3. normalize string literals from ' into "
   config = re.sub('\'', '"', config)
-  # 2. turn pseudo-booleans strings into Booleans
+  # 4. turn pseudo-booleans strings into Booleans
   config = re.sub('"true"', 'true', config)
   config = re.sub('"false"', 'false', config)
   return config
