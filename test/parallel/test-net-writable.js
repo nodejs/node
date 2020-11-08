819b441042aef8b775333068beb0862a0409f892
Robert Nagy <ronagy@icloud.com>
2020-11-08 23:27:05 +0100

    fixup

--- test/parallel/test-net-writable.js
+++ test/parallel/test-net-writable.js
@@ -4,13 +4,13 @@ const assert = require('assert');
 
 const net = require('net');
 
-net.createServer(function(s) {
+net.createServer(common.mustCall(function(s) {
   this.close();
   s.end();
-}).listen(0, 'localhost', function() {
+})).listen(0, 'localhost', common.mustCall(function() {
   const socket = net.connect(this.address().port, 'localhost');
   socket.on('end', common.mustCall(() => {
     assert.strictEqual(socket.writable, true);
     socket.write('hello world');
   }));
-});
+}));
