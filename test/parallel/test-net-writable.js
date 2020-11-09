d9d5b77094e56eff2b4e90c57ffb4cd9d80cef1d
Robert Nagy <ronagy@icloud.com>
2020-11-09 20:52:45 +0100

    fixup

--- test/parallel/test-net-writable.js
+++ test/parallel/test-net-writable.js
@@ -1,11 +1,10 @@
 'use strict';
 const common = require('../common');
 const assert = require('assert');
-
 const net = require('net');
 
-net.createServer(common.mustCall(function(s) {
-  this.close();
+const server = net.createServer(common.mustCall(function(s) {
+  server.close();
   s.end();
 })).listen(0, 'localhost', common.mustCall(function() {
   const socket = net.connect(this.address().port, 'localhost');
