9834ef85a0a549a45a98f04dc51af1782a7126ee
James M Snell <jasnell@gmail.com>
2020-11-12 12:34:33 -0800

    src: retain pointers to WriteWrap/ShutdownWrap
    Avoids potential use-after-free when wrap req's are synchronously
    destroyed.
    
    CVE-ID: CVE-2020-8265
    Fixes: https://github.com/nodejs-private/node-private/issues/227
    Refs: https://hackerone.com/bugs?subject=nodejs&report_id=988103
    PR-URL: https://github.com/nodejs-private/node-private/pull/23
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/stream_base.h
+++ src/stream_base.h
@@ -25,6 +25,7 @@ struct StreamWriteResult {
   int err;
   WriteWrap* wrap;
   size_t bytes;
+  BaseObjectPtr<AsyncWrap> wrap_obj;
 };
 
 using JSMethodFunction = void(const v8::FunctionCallbackInfo<v8::Value>& args);
