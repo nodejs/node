d7bfa589420d089d48ae4e685bdbde446962a6d8
Benjamin Gruenbaum <benjamingr@gmail.com>
2020-11-14 13:46:29 +0200

    lib: set abort-controller toStringTag
    PR-URL: https://github.com/nodejs/node/pull/36115
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-eventtarget-memoryleakwarning.js
+++ test/parallel/test-eventtarget-memoryleakwarning.js
@@ -21,7 +21,7 @@ common.expectWarning({
      'Use events.setMaxListeners() to increase ' +
      'limit'],
     ['Possible EventTarget memory leak detected. 3 foo listeners added to ' +
-     '[AbortSignal [EventTarget]]. ' +
+     '[AbortSignal]. ' +
      'Use events.setMaxListeners() to increase ' +
      'limit'],
   ],
