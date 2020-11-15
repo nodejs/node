77b47dfd0834bbde13f45eed4ee9b25a59091070
Luigi Pinca <luigipinca@gmail.com>
2020-11-15 08:37:14 +0100

    test: fix typo in inspector-helper.js
    PR-URL: https://github.com/nodejs/node/pull/36127
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Daijiro Wachi <daijiro.wachi@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/common/inspector-helper.js
+++ test/common/inspector-helper.js
@@ -228,7 +228,7 @@ class InspectorSession {
 
   waitForNotification(methodOrPredicate, description) {
     const desc = description || methodOrPredicate;
-    const message = `Timed out waiting for matching notification (${desc}))`;
+    const message = `Timed out waiting for matching notification (${desc})`;
     return fires(
       this._asyncWaitForNotification(methodOrPredicate), message, TIMEOUT);
   }
