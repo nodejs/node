fa4c7537e2bee278d34d06496298daec8e3bdc1c
Danny Sonnenschein <my.card.god@web.de>
2020-11-21 09:56:22 +0100

    deps: update to c-ares 1.17.1
    PR-URL: https://github.com/nodejs/node/pull/36207
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Shelley Vohr <codebytere@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- deps/cares/src/ares_query.c
+++ deps/cares/src/ares_query.c
@@ -45,7 +45,7 @@ static void rc4(rc4_key* key, unsigned char *buffer_ptr, int buffer_len)
   unsigned char y;
   unsigned char* state;
   unsigned char xorIndex;
-  short counter;
+  int counter;
 
   x = key->x;
   y = key->y;
