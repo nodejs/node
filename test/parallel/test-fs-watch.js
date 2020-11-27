8976588084bbf226592b9dd05e31e3323ceb7653
Rich Trott <rtrott@gmail.com>
2020-11-27 05:58:31 -0800

    test: simplify test-fs-watch.js
    AIX supports returning file names on watched directories (judging from
    the similar test in sequential). Simplify logic of test-fs-watch in
    parallel.

--- test/parallel/test-fs-watch.js
+++ test/parallel/test-fs-watch.js
@@ -10,12 +10,14 @@ const fs = require('fs');
 const assert = require('assert');
 const { join } = require('path');
 
+const expectFilename =
+  common.isLinux || common.isOSX || common.isWindows || common.isAIX;
+
 class WatchTestCase {
-  constructor(shouldInclude, dirName, fileName, field) {
+  constructor(dirName, fileName, field) {
     this.dirName = dirName;
     this.fileName = fileName;
     this.field = field;
-    this.shouldSkip = !shouldInclude;
   }
   get dirPath() { return join(tmpdir.path, this.dirName); }
   get filePath() { return join(this.dirPath, this.fileName); }
@@ -24,14 +26,12 @@ class WatchTestCase {
 const cases = [
   // Watch on a directory should callback with a filename on supported systems
   new WatchTestCase(
-    common.isLinux || common.isOSX || common.isWindows || common.isAIX,
     'watch1',
     'foo',
     'filePath'
   ),
   // Watch on a file should callback with a filename on supported systems
   new WatchTestCase(
-    common.isLinux || common.isOSX || common.isWindows,
     'watch2',
     'bar',
     'dirPath'
@@ -41,50 +41,51 @@ const cases = [
 const tmpdir = require('../common/tmpdir');
 tmpdir.refresh();
 
-for (const testCase of cases) {
-  if (testCase.shouldSkip) continue;
-  fs.mkdirSync(testCase.dirPath);
-  // Long content so it's actually flushed.
-  const content1 = Date.now() + testCase.fileName.toLowerCase().repeat(1e4);
-  fs.writeFileSync(testCase.filePath, content1);
+if (expectFilename) {
+  for (const testCase of cases) {
+    fs.mkdirSync(testCase.dirPath);
+    // Long content so it's actually flushed.
+    const content1 = Date.now() + testCase.fileName.toLowerCase().repeat(1e4);
+    fs.writeFileSync(testCase.filePath, content1);
 
-  let interval;
-  const pathToWatch = testCase[testCase.field];
-  const watcher = fs.watch(pathToWatch);
-  watcher.on('error', (err) => {
-    if (interval) {
-      clearInterval(interval);
-      interval = null;
-    }
-    assert.fail(err);
-  });
-  watcher.on('close', common.mustCall(() => {
-    watcher.close(); // Closing a closed watcher should be a noop
-  }));
-  watcher.on('change', common.mustCall(function(eventType, argFilename) {
-    if (interval) {
-      clearInterval(interval);
-      interval = null;
-    }
-    if (common.isOSX)
-      assert.strictEqual(['rename', 'change'].includes(eventType), true);
-    else
-      assert.strictEqual(eventType, 'change');
-    assert.strictEqual(argFilename, testCase.fileName);
+    let interval;
+    const pathToWatch = testCase[testCase.field];
+    const watcher = fs.watch(pathToWatch);
+    watcher.on('error', (err) => {
+      if (interval) {
+        clearInterval(interval);
+        interval = null;
+      }
+      assert.fail(err);
+    });
+    watcher.on('close', common.mustCall(() => {
+      watcher.close(); // Closing a closed watcher should be a noop
+    }));
+    watcher.on('change', common.mustCall(function(eventType, argFilename) {
+      if (interval) {
+        clearInterval(interval);
+        interval = null;
+      }
+      if (common.isOSX)
+        assert.strictEqual(['rename', 'change'].includes(eventType), true);
+      else
+        assert.strictEqual(eventType, 'change');
+      assert.strictEqual(argFilename, testCase.fileName);
 
-    watcher.close();
+      watcher.close();
 
-    // We document that watchers cannot be used anymore when it's closed,
-    // here we turn the methods into noops instead of throwing
-    watcher.close(); // Closing a closed watcher should be a noop
-  }));
+      // We document that watchers cannot be used anymore when it's closed,
+      // here we turn the methods into noops instead of throwing
+      watcher.close(); // Closing a closed watcher should be a noop
+    }));
 
-  // Long content so it's actually flushed. toUpperCase so there's real change.
-  const content2 = Date.now() + testCase.fileName.toUpperCase().repeat(1e4);
-  interval = setInterval(() => {
-    fs.writeFileSync(testCase.filePath, '');
-    fs.writeFileSync(testCase.filePath, content2);
-  }, 100);
+    // Long content so it's flushed. toUpperCase so there's real change.
+    const content2 = Date.now() + testCase.fileName.toUpperCase().repeat(1e4);
+    interval = setInterval(() => {
+      fs.writeFileSync(testCase.filePath, '');
+      fs.writeFileSync(testCase.filePath, content2);
+    }, 100);
+  }
 }
 
 [false, 1, {}, [], null, undefined].forEach((input) => {
