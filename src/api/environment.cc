4d647c187d95811292f9cf668c55f3551cccfc24
Anna Henningsen <anna@addaleax.net>
2020-12-08 11:59:03 +0100

    src: add way to get IsolateData and allocator from Environment
    Add a way to get the current `IsolateData*` and, from it, the current
    Node.js `ArrayBufferAllocator` if there is one. This can be useful
    for re-using either one of these structures as an embedder.

--- src/api/environment.cc
+++ src/api/environment.cc
@@ -436,6 +436,14 @@ Environment* GetCurrentEnvironment(Local<Context> context) {
   return Environment::GetCurrent(context);
 }
 
+IsolateData* GetEnvironmentIsolateData(Environment* env) {
+  return env->isolate_data();
+}
+
+ArrayBufferAllocator* GetArrayBufferAllocator(IsolateData* isolate_data) {
+  return isolate_data->node_allocator();
+}
+
 MultiIsolatePlatform* GetMultiIsolatePlatform(Environment* env) {
   return GetMultiIsolatePlatform(env->isolate_data());
 }
