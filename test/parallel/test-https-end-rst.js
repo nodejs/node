a1b606c68dfcc9f791f96421ac07655aa26bc50c
Momtchil Momtchev <momtchil@momtchev.com>
2020-12-09 12:46:50 +0100

    tls: support active closing for TLS sockets
    After discussions with @ronag and @lpinca, the
    best solution to the TLS RST problem is the one
    that limits the new behaviour to TLSSockets
    
    This is complementary to @vtjnash solution which
    allows the client to ignore this (somewhat normal) RST
    Both are needed, since both ends of Node.js TLS code
    must interact with other TLS implementations
    
    Refs: https://github.com/nodejs/node/pull/36205

--- test/parallel/test-https-end-rst.js
+++ test/parallel/test-https-end-rst.js
@@ -1,11 +1,20 @@
 'use strict';
 const common = require('../common');
 
-const http = require('http');
-const net = require('net');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const https = require('https');
+const tls = require('tls');
+const fixtures = require('../common/fixtures');
+
+const opt = {
+  key: fixtures.readKey('agent1-key.pem'),
+  cert: fixtures.readKey('agent1-cert.pem')
+};
 
 const data = 'hello';
-const server = http.createServer({}, common.mustCallAtLeast((req, res) => {
+const server = https.createServer(opt, common.mustCallAtLeast((req, res) => {
   res.setHeader('content-length', data.length);
   res.end(data);
 }, 2));
@@ -14,18 +23,18 @@ server.listen(0, function() {
   const options = {
     host: '127.0.0.1',
     port: server.address().port,
+    rejectUnauthorized: false,
+    ALPNProtocols: ['http/1.1'],
     allowHalfOpen: true
   };
-  const socket = net.connect(options, common.mustCall(() => {
-    socket.on('ready', common.mustCall(() => {
+  const socket = tls.connect(options, common.mustCall(() => {
+    socket.write('GET /\n\n');
+    socket.once('data', common.mustCall(() => {
       socket.write('GET /\n\n');
-      socket.once('data', common.mustCall(() => {
-        socket.write('GET /\n\n');
-        setTimeout(common.mustCall(() => {
-          socket.destroy();
-          server.close();
-        }), common.platformTimeout(10));
-      }));
+      setTimeout(common.mustCall(() => {
+        socket.destroy();
+        server.close();
+      }), common.platformTimeout(10));
     }));
   }));
 });
