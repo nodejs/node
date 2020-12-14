b57785d89bc548c4048118606919d66dff1c7eb7
Michaël Zasso <targos@protonmail.com>
2020-12-14 08:21:17 +0100

    doc: update and run license-builder for Babel
    PR-URL: https://github.com/nodejs/node/pull/36504
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Beth Griggs <bgriggs@redhat.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- tools/license-builder.sh
+++ tools/license-builder.sh
@@ -83,7 +83,7 @@ addlicense "markupsafe" "tools/inspector_protocol/markupsafe" "$(cat "${rootdir}
 addlicense "cpplint.py" "tools/cpplint.py" \
            "$(sed -e '/^$/,$d' -e 's/^#$//' -e 's/^# //' "${rootdir}"/tools/cpplint.py | tail -n +3)"
 addlicense "ESLint" "tools/node_modules/eslint" "$(cat "${rootdir}"/tools/node_modules/eslint/LICENSE)"
-addlicense "babel-eslint" "tools/node_modules/babel-eslint" "$(cat "${rootdir}"/tools/node_modules/babel-eslint/LICENSE)"
+addlicense "Babel" "tools/node_modules/@babel" "$(cat "${rootdir}"/tools/node_modules/@babel/core/LICENSE)"
 addlicense "gtest" "test/cctest/gtest" "$(cat "${rootdir}"/test/cctest/gtest/LICENSE)"
 
 # nghttp2
