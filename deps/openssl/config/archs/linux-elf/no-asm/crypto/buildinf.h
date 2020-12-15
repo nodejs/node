d62c650f7544a9b81d94dbbf3681671345925362
Myles Borins <mylesborins@github.com>
2020-12-14 23:18:10 -0500

    deps: update archs files for OpenSSL-1.1.1i
    After an OpenSSL source update, all the config files need to be
    regenerated and committed by:
      $ make gen-openssl
      $ git add deps/openssl/config/archs
      $ git add deps/openssl/openssl/include/crypto/bn_conf.h
      $ git add deps/openssl/openssl/include/crypto/dso_conf.h
      $ git add deps/openssl/openssl/include/openssl/opensslconf.h
      $ git commit
    
    PR-URL: https://github.com/nodejs/node/pull/36521
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Shelley Vohr <codebytere@gmail.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- deps/openssl/config/archs/linux-elf/no-asm/crypto/buildinf.h
+++ deps/openssl/config/archs/linux-elf/no-asm/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-elf"
-#define DATE "built on: Tue Apr 21 13:29:17 2020 UTC"
+#define DATE "built on: Tue Dec 15 04:04:08 2020 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
--- deps/openssl/config/archs/solaris-x86-gcc/asm/crypto/buildinf.h
+++ deps/openssl/config/archs/solaris-x86-gcc/asm/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: solaris-x86-gcc"
-#define DATE "built on: Tue Apr 21 13:30:08 2020 UTC"
+#define DATE "built on: Tue Dec 15 04:10:19 2020 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
