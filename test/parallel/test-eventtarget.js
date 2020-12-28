5bc130bd9ec5be1695e64e60ba3f6fe5ac035208
ZiJian Liu <Lxxyxzj@gmail.com>
2020-12-29 01:06:34 +0800

    test: increase coverage for events
    1. test EventEmitter.setMaxListeners with invalid listener count
    https://coverage.nodejs.org/coverage-0b6d3070a176d437/lib/events.js.html#L171
    
    2. test EventEmitter.setMaxListeners with invalid emitter
    https://coverage.nodejs.org/coverage-0b6d3070a176d437/lib/events.js.html#L186
    
    3. test getEventListeners with invalid emiiter
    Refs: https://coverage.nodejs.org/coverage-0b6d3070a176d437/lib/events.js.html#L706
    
    4. test events.once with options: null
    Refs: https://coverage.nodejs.org/coverage-0b6d3070a176d437/lib/events.js.html#L712
    
    5. add test case for inspect new Event()
    Refs: https://coverage.nodejs.org/coverage-0b6d3070a176d437/lib/internal/event_target.js.html#L111
    
    6. add test case for insepct new EventTarget()
    Refs: https://coverage.nodejs.org/coverage-0b6d3070a176d437/lib/internal/event_target.js.html#L446
    
    7. add test case for Event and EventTarget constructor name
    
    PR-URL: https://github.com/nodejs/node/pull/36668
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-eventtarget.js
+++ test/parallel/test-eventtarget.js
@@ -13,7 +13,7 @@ const {
 
 const { once } = require('events');
 
-const { promisify } = require('util');
+const { promisify, inspect } = require('util');
 const delay = promisify(setTimeout);
 
 // The globals are defined.
@@ -540,4 +540,33 @@ let asyncTest = Promise.resolve();
   }));
   et.addEventListener('foo', listener);
   et.dispatchEvent(new Event('foo'));
+}
+
+{
+  const ev = new Event('test');
+  const evConstructorName = inspect(ev, {
+    depth: -1
+  });
+  strictEqual(evConstructorName, 'Event');
+
+  const inspectResult = inspect(ev, {
+    depth: 1
+  });
+  ok(inspectResult.includes('Event'));
+}
+
+{
+  const et = new EventTarget();
+  const inspectResult = inspect(et, {
+    depth: 1
+  });
+  ok(inspectResult.includes('EventTarget'));
+}
+
+{
+  const ev = new Event('test');
+  strictEqual(ev.constructor.name, 'Event');
+
+  const et = new EventTarget();
+  strictEqual(et.constructor.name, 'EventTarget');
 }
