451e040da67597ea011494b68168c07993177063
Pooja D P <Pooja.D.P@ibm.com>
2021-01-04 03:09:19 -0800

    test: use faster variant for rss in test-vm-memleak.js
    PR-URL: https://github.com/nodejs/node/pull/36769
    Refs: https://github.com/nodejs/node/pull/34291
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/pummel/test-vm-memleak.js
+++ test/pummel/test-vm-memleak.js
@@ -34,7 +34,7 @@ const interval = setInterval(function() {
   } catch {
   }
 
-  const rss = process.memoryUsage().rss;
+  const rss = process.memoryUsage.rss();
   assert.ok(rss < 64 * 1024 * 1024,
             `memory usage: ${Math.round(rss / (1024 * 1024))}Mb`);
 
