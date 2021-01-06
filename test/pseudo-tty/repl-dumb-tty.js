546862596330a94cebfdf0598c17a580b54cef80
Robert Nagy <ronagy@icloud.com>
2021-01-06 23:36:09 +0100

    fixup

--- test/pseudo-tty/repl-dumb-tty.js
+++ test/pseudo-tty/repl-dumb-tty.js
@@ -26,14 +26,12 @@ repl.start('> ');
   replServer.write(null, { ctrl: true, name: 'd' });
 }
 
-setImmediate(() => {
-  process.stdin.push('conso'); // No completion preview.
-  process.stdin.push('le.log("foo")\n');
-  process.stdin.push('1 + 2'); // No input preview.
-  process.stdin.push('\n');
-  process.stdin.push('"str"\n');
-  process.stdin.push('console.dir({ a: 1 })\n');
-  process.stdin.push('{ a: 1 }\n');
-  process.stdin.push('\n');
-  process.stdin.push('.exit\n');
-});
+process.stdin.push('conso'); // No completion preview.
+process.stdin.push('le.log("foo")\n');
+process.stdin.push('1 + 2'); // No input preview.
+process.stdin.push('\n');
+process.stdin.push('"str"\n');
+process.stdin.push('console.dir({ a: 1 })\n');
+process.stdin.push('{ a: 1 }\n');
+process.stdin.push('\n');
+process.stdin.push('.exit\n');
