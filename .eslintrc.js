8dc8adc78235ffa3301d176887770d1ebe9b29d4
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-01-08 18:40:19 +0100

    tools: add support for top-level await syntax in linter
    PR-URL: https://github.com/nodejs/node/pull/36911
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ const hacks = [
   'eslint-plugin-markdown',
   '@babel/eslint-parser',
   '@babel/plugin-syntax-class-properties',
+  '@babel/plugin-syntax-top-level-await',
 ];
 Module._findPath = (request, paths, isMain) => {
   const r = ModuleFindPath(request, paths, isMain);
@@ -41,7 +42,10 @@ module.exports = {
   parser: '@babel/eslint-parser',
   parserOptions: {
     babelOptions: {
-      plugins: [Module._findPath('@babel/plugin-syntax-class-properties')],
+      plugins: [
+        Module._findPath('@babel/plugin-syntax-class-properties'),
+        Module._findPath('@babel/plugin-syntax-top-level-await'),
+      ],
     },
     requireConfigFile: false,
     sourceType: 'script',
