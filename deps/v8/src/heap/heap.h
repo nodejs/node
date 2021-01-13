64aee1ba4d7737b55fffba42b974d66e95f5d813
Michaël Zasso <targos@protonmail.com>
2021-01-13 13:11:42 +0100

    deps: V8: cherry-pick deb0813166f3
    Original commit message:
    
        [heap] Add proper rebind support to StrongRootBlockAllocator
    
        MSVC's STL in debug mode rebinds the allocator passed to vectors to
        allocate helper structures, so we need StrongRootBlockAllocator to have
        proper rebind support rather than assuming it always rebinds to Address.
    
        Bug: v8:11241
        Change-Id: I15688e43fe2c71ec4ff0c287a03e36ca57427417
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/2622915
        Auto-Submit: Leszek Swirski <leszeks@chromium.org>
        Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
        Commit-Queue: Leszek Swirski <leszeks@chromium.org>
        Cr-Commit-Position: refs/heads/master@{#72060}
    
    Refs: https://github.com/v8/v8/commit/deb0813166f3fcb72c742f76e3c7228f23568bf1
    
    PR-URL: https://github.com/nodejs/node/pull/36139
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Myles Borins <myles.borins@gmail.com>
    Reviewed-By: Shelley Vohr <codebytere@gmail.com>

--- deps/v8/src/heap/heap.h
+++ deps/v8/src/heap/heap.h
@@ -2610,10 +2610,7 @@ class StrongRootBlockAllocator {
   using size_type = size_t;
   using difference_type = ptrdiff_t;
   template <class U>
-  struct rebind {
-    STATIC_ASSERT((std::is_same<Address, U>::value));
-    using other = StrongRootBlockAllocator;
-  };
+  struct rebind;
 
   explicit StrongRootBlockAllocator(Heap* heap) : heap_(heap) {}
 
@@ -2622,6 +2619,23 @@ class StrongRootBlockAllocator {
 
  private:
   Heap* heap_;
+};
+
+// Rebinding to Address gives another StrongRootBlockAllocator.
+template <>
+struct StrongRootBlockAllocator::rebind<Address> {
+  using other = StrongRootBlockAllocator;
+};
+
+// Rebinding to something other than Address gives a std::allocator that
+// is copy-constructable from StrongRootBlockAllocator.
+template <class U>
+struct StrongRootBlockAllocator::rebind {
+  class other : public std::allocator<U> {
+   public:
+    // NOLINTNEXTLINE
+    other(const StrongRootBlockAllocator&) {}
+  };
 };
 
 }  // namespace internal
