43ae9c46c35a935723a9109828c87380d781e147
Matteo Collina <hello@matteocollina.com>
2021-01-14 16:04:44 +0100

    src: drop localhost6 as allowed host for inspector
    CVE-ID: CVE-2021-22884
    Refs: https://hackerone.com/bugs?report_id=1069487
    PR-URL: https://github.com/nodejs-private/node-private/pull/244
    Reviewed-By: Beth Griggs <bgriggs@redhat.com>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Mary Marchini <oss@mmarchini.me>
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/inspector_socket.cc
+++ src/inspector_socket.cc
@@ -580,8 +580,7 @@ class HttpHandler : public ProtocolHandler {
   bool IsAllowedHost(const std::string& host_with_port) const {
     std::string host = TrimPort(host_with_port);
     return host.empty() || IsIPAddress(host)
-           || node::StringEqualNoCase(host.data(), "localhost")
-           || node::StringEqualNoCase(host.data(), "localhost6");
+           || node::StringEqualNoCase(host.data(), "localhost");
   }
 
   bool parsing_value_;
