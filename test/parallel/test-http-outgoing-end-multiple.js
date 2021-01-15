5f944b98d7c076ec8407bbf1bb4a0788b158f3c8
Dimitris Halatsis <dimitris_ha@hotmail.com>
2021-01-15 12:05:44 +0200

    test: assert http writable corked state after end

--- test/parallel/test-http-outgoing-end-multiple.js
+++ test/parallel/test-http-outgoing-end-multiple.js
@@ -5,12 +5,15 @@ const http = require('http');
 
 const server = http.createServer(common.mustCall(function(req, res) {
   res.end('testing ended state', common.mustCall());
+  assert.strictEqual(res.writableCorked, 0);
   res.end(common.mustCall());
+  assert.strictEqual(res.writableCorked, 0);
   res.on('finish', common.mustCall(() => {
     res.end(common.mustCall((err) => {
       assert.strictEqual(err.code, 'ERR_STREAM_ALREADY_FINISHED');
       server.close();
     }));
+    assert.strictEqual(res.writableCorked, 0);
   }));
 }));
 
