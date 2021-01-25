c19b457e0ef54d884aceaad0166ee6fb6cb14e1e
Rabbit <ttzztztz@outlook.com>
2021-01-25 15:04:49 +0800

    chore: comments for the extra unit test

--- test/parallel/test-process-beforeexit.js
+++ test/parallel/test-process-beforeexit.js
@@ -49,6 +49,8 @@ function tryListen() {
 // Test that a function invoked from the beforeExit handler can use a timer
 // to keep the event loop open, which can use another timer to keep the event
 // loop open, etc.
+// 
+// After N times, call function tryNextTick to test behaviors of process.nextTick
 function tryRepeatedTimer() {
   const N = 5;
   let n = 0;
@@ -61,6 +63,7 @@ function tryRepeatedTimer() {
   setTimeout(repeatedTimer, 1);
 }
 
+// Test if the callback of process.nextTick can be invoked
 function tryNextTick() {
   process.nextTick(common.mustCall(function() {
     process.once('beforeExit', common.mustNotCall());
