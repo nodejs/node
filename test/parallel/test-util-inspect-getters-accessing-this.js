486f8b8155325137a3791428fcd785cffe8729a4
Ruben Bridgewater <ruben@bridgewater.de>
2021-01-26 16:52:31 +0100

    util: fix infinite recursion during inspection
    A specially crafted object with circular structures behind getters
    could cause a infinite recursion. This is now fixed by detecting the
    objects as already visited.
    
    Signed-off-by: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-util-inspect-getters-accessing-this.js
+++ test/parallel/test-util-inspect-getters-accessing-this.js
@@ -7,24 +7,61 @@ require('../common');
 
 const assert = require('assert');
 
-const util = require('util');
+const { inspect } = require('util');
 
-class X {
-  constructor() {
-    this._y = 123;
+{
+  class X {
+    constructor() {
+      this._y = 123;
+    }
+
+    get y() {
+      return this._y;
+    }
   }
 
-  get y() {
-    return this._y;
-  }
+  const result = inspect(new X(), {
+    getters: true,
+    showHidden: true
+  });
+
+  assert.strictEqual(
+    result,
+    'X { _y: 123, [y]: [Getter: 123] }'
+  );
 }
 
-const result = util.inspect(new X(), {
-  getters: true,
-  showHidden: true
-});
+// Regression test for https://github.com/nodejs/node/issues/37054
+{
+  class A {
+    constructor(B) {
+      this.B = B;
+    }
+    get b() {
+      return this.B;
+    }
+  }
 
-assert.strictEqual(
-  result,
-  'X { _y: 123, [y]: [Getter: 123] }'
-);
+  class B {
+    constructor() {
+      this.A = new A(this);
+    }
+    get a() {
+      return this.A;
+    }
+  }
+
+  const result = inspect(new B(), {
+    depth: 1,
+    getters: true,
+    showHidden: true
+  });
+
+  assert.strictEqual(
+    result,
+    '<ref *1> B {\n' +
+    '  A: A { B: [Circular *1], [b]: [Getter] [Circular *1] },\n' +
+    '  [a]: [Getter] A { B: [Circular *1], [b]: [Getter] [Circular *1] }\n' +
+    '}',
+  );
+}
