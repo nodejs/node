44b35f3ab45a0a88bb0cba21615037851f768e86
Robert Nagy <ronagy@icloud.com>
2021-02-03 11:52:57 +0100

    fixup

--- test/parallel/test-http-client-abort3.js
+++ test/parallel/test-http-client-abort3.js
@@ -1,12 +1,42 @@
 'use strict';
 
 const common = require('../common');
-const assert = require('assert');
 const http = require('http');
+const net = require('net');
+const assert = require('assert');
 
-const req = http.get('http://[2604:1380:45f1:3f00::1]:4002');
+{
+  const req = http.get('http://[2604:1380:45f1:3f00::1]:4002');
 
-req.on('error', common.mustCall((err) => {
-  assert.strictEqual(err.code, 'EHOSTUNREACH');
-}));
-req.abort();
+  req.on('error', common.mustCall((err) => {
+    assert.strictEqual(err.code, 'EHOSTUNREACH');
+  }));
+  req.abort();
+}
+
+function createConnection() {
+  const socket = new net.Socket();
+
+  process.nextTick(function() {
+    socket.destroy(new Error('Oops'));
+  });
+
+  return socket;
+}
+
+{
+  const req = http.get({ createConnection });
+
+  req.on('error', common.expectsError({ name: 'Error', message: 'Oops' }));
+  req.abort();
+}
+
+{
+  class CustomAgent extends http.Agent {}
+  CustomAgent.prototype.createConnection = createConnection;
+
+  const req = http.get({ agent: new CustomAgent() });
+
+  req.on('error', common.expectsError({ name: 'Error', message: 'Oops' }));
+  req.abort();
+}
