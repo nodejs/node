48a634e514718e4d94785e5873d1d365d9dccade
Gabriel Schulhof <gabriel.schulhof@intel.com>
2021-02-03 12:01:28 -0800

    test: rename n-api to node-api
    This renames the macros used in the tests from `NAPI_*` to
    `NODE_API_*`.
    
    PR-URL: https://github.com/nodejs/node/pull/37217
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Beth Griggs <bgriggs@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/js-native-api/test_symbol/test_symbol.c
+++ test/js-native-api/test_symbol/test_symbol.c
@@ -4,21 +4,21 @@
 static napi_value New(napi_env env, napi_callback_info info) {
   size_t argc = 1;
   napi_value args[1];
-  NAPI_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
+  NODE_API_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
 
   napi_value description = NULL;
   if (argc >= 1) {
     napi_valuetype valuetype;
-    NAPI_CALL(env, napi_typeof(env, args[0], &valuetype));
+    NODE_API_CALL(env, napi_typeof(env, args[0], &valuetype));
 
-    NAPI_ASSERT(env, valuetype == napi_string,
+    NODE_API_ASSERT(env, valuetype == napi_string,
         "Wrong type of arguments. Expects a string.");
 
     description = args[0];
   }
 
   napi_value symbol;
-  NAPI_CALL(env, napi_create_symbol(env, description, &symbol));
+  NODE_API_CALL(env, napi_create_symbol(env, description, &symbol));
 
   return symbol;
 }
@@ -26,10 +26,10 @@ static napi_value New(napi_env env, napi_callback_info info) {
 EXTERN_C_START
 napi_value Init(napi_env env, napi_value exports) {
   napi_property_descriptor properties[] = {
-    DECLARE_NAPI_PROPERTY("New", New),
+    DECLARE_NODE_API_PROPERTY("New", New),
   };
 
-  NAPI_CALL(env, napi_define_properties(
+  NODE_API_CALL(env, napi_define_properties(
       env, exports, sizeof(properties) / sizeof(*properties), properties));
 
   return exports;
