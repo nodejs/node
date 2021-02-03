9948036ee05093e60e82a6cf90d33a25e4b03226
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-02-03 20:18:50 +0100

    fs: remove permissive rmdir recursive
    PR-URL: https://github.com/nodejs/node/pull/37216
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ben Coe <bencoe@gmail.com>
    Reviewed-By: Ian Sutherland <ian@iansutherland.ca>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- test/parallel/test-fs-rmdir-recursive.js
+++ test/parallel/test-fs-rmdir-recursive.js
@@ -80,8 +80,9 @@ function removeAsync(dir) {
 
       // Recursive removal should succeed.
       fs.rmdir(dir, { recursive: true }, common.mustSucceed(() => {
-        // No error should occur if recursive and the directory does not exist.
-        fs.rmdir(dir, { recursive: true }, common.mustSucceed(() => {
+        // An error should occur if recursive and the directory does not exist.
+        fs.rmdir(dir, { recursive: true }, common.mustCall((err) => {
+          assert.strictEqual(err.code, 'ENOENT');
           // Attempted removal should fail now because the directory is gone.
           fs.rmdir(dir, common.mustCall((err) => {
             assert.strictEqual(err.syscall, 'rmdir');
@@ -126,8 +127,9 @@ function removeAsync(dir) {
   // Recursive removal should succeed.
   fs.rmdirSync(dir, { recursive: true });
 
-  // No error should occur if recursive and the directory does not exist.
-  fs.rmdirSync(dir, { recursive: true });
+  // An error should occur if recursive and the directory does not exist.
+  assert.throws(() => fs.rmdirSync(dir, { recursive: true }),
+                { code: 'ENOENT' });
 
   // Attempted removal should fail now because the directory is gone.
   assert.throws(() => fs.rmdirSync(dir), { syscall: 'rmdir' });
@@ -147,8 +149,9 @@ function removeAsync(dir) {
   // Recursive removal should succeed.
   await fs.promises.rmdir(dir, { recursive: true });
 
-  // No error should occur if recursive and the directory does not exist.
-  await fs.promises.rmdir(dir, { recursive: true });
+  // An error should occur if recursive and the directory does not exist.
+  await assert.rejects(fs.promises.rmdir(dir, { recursive: true }),
+                       { code: 'ENOENT' });
 
   // Attempted removal should fail now because the directory is gone.
   assert.rejects(fs.promises.rmdir(dir), { syscall: 'rmdir' });
