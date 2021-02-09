463b2811601c78f71425411fddf64b3fbf055baa
Bradley Farias <bradley.meck@gmail.com>
2021-02-09 10:56:53 -0600

    policy: fix cascade getting scope
    PR-URL: https://github.com/nodejs/node/pull/37298
    Reviewed-By: Vladimir de Turckheim <vlad2t@hotmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Co-authored-by: James M Snell <jasnell@gmail.com>

--- test/parallel/test-policy-scopes.js
+++ test/parallel/test-policy-scopes.js
@@ -10,12 +10,26 @@ const fixtures = require('../common/fixtures');
 const assert = require('assert');
 const { spawnSync } = require('child_process');
 
-const dep = fixtures.path('policy', 'main.mjs');
 {
+  const dep = fixtures.path('policy', 'main.mjs');
   const depPolicy = fixtures.path(
     'policy',
     'dependencies',
     'dependencies-scopes-policy.json');
+  const { status } = spawnSync(
+    process.execPath,
+    [
+      '--experimental-policy', depPolicy, dep,
+    ]
+  );
+  assert.strictEqual(status, 0);
+}
+{
+  const dep = fixtures.path('policy', 'multi-deps.js');
+  const depPolicy = fixtures.path(
+    'policy',
+    'dependencies',
+    'dependencies-scopes-and-resources-policy.json');
   const { status } = spawnSync(
     process.execPath,
     [
