78d6e62fd07ad500f84f1dad8a2509be597dcfc1
cjihrig <cjihrig@gmail.com>
2021-02-13 13:03:10 -0500

    deps: upgrade to libuv 1.41.0
    Notable changes:
    
    - The IBM i platform has been promoted to a Tier 2 platform.
    - libuv is now built with `-fno-strict-aliasing`, and recommends
      that projects using libuv do the same.
    - `uv_fs_mkdir()` now returns `UV_EINVAL` for invalid directory
      names on Windows.
    - `uv_uptime()` now returns the correct value on OpenVZ containers.
    - Windows 8 is the new minimum supported version of Windows.
    - Bind errors are now reported from `uv_tcp_connect()`.
    - The `uv_pipe()` function has been added.
    - The `uv_socketpair()` function has been added.
    - `uv_read_start()` error handling has been unified across
      Windows and Unix.
    
    PR-URL: https://github.com/nodejs/node/pull/37360
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>

--- deps/uv/test/test-platform-output.c
+++ deps/uv/test/test-platform-output.c
@@ -155,6 +155,7 @@ TEST_IMPL(platform_output) {
   printf("  username: %s\n", pwd.username);
   printf("  shell: %s\n", pwd.shell);
   printf("  home directory: %s\n", pwd.homedir);
+  uv_os_free_passwd(&pwd);
 
   pid = uv_os_getpid();
   ASSERT(pid > 0);
