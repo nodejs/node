a4658c36f9e8f78a1963103e6b0c82e2d0bb7ac3
Jan Staněk <jstanek@redhat.com>
2021-02-15 15:27:04 +0100

    fixup! Gate tests for FIPS flags behind common.hasCrypto

--- test/parallel/test-cli-node-print-help.js
+++ test/parallel/test-cli-node-print-help.js
@@ -8,7 +8,6 @@ const common = require('../common');
 
 const assert = require('assert');
 const { exec } = require('child_process');
-const { internalBinding } = require('internal/test/binding');
 let stdOut;
 
 
