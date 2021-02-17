47b959666eee8875c7a14363ede209febdb6a45e
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-02-17 13:58:42 +0100

    deps: upgrade openssl sources to 1.1.1j
    This updates all sources in deps/openssl/openssl by:
        $ cd deps/openssl/
        $ rm -rf openssl
        $ tar zxf ~/tmp/openssl-1.1.1j.tar.gz
        $ mv openssl-1.1.1j openssl
        $ git add --all openssl
        $ git commit openssl

--- deps/openssl/openssl/ssl/ssl_lib.c
+++ deps/openssl/openssl/ssl/ssl_lib.c
@@ -2632,7 +2632,7 @@ char *SSL_get_shared_ciphers(const SSL *s, char *buf, int size)
  * - if we are before or during/after the handshake,
  * - if a resumption or normal handshake is being attempted/has occurred
  * - whether we have negotiated TLSv1.2 (or below) or TLSv1.3
- *
+ * 
  * Note that only the host_name type is defined (RFC 3546).
  */
 const char *SSL_get_servername(const SSL *s, const int type)
