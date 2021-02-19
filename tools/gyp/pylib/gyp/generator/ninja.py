ddacdd9054f5b6e3bdc6db066b2ea2c0796cbe70
Isuru Fernando <isuruf@gmail.com>
2021-02-19 09:48:02 -0600

    tools: use LDFLAGS_host for host toolset
    This makes the behaviour similar to that of CFLAGS_host

--- tools/gyp/pylib/gyp/generator/ninja.py
+++ tools/gyp/pylib/gyp/generator/ninja.py
@@ -1427,7 +1427,11 @@ class NinjaWriter(object):
         is_executable = spec["type"] == "executable"
         # The ldflags config key is not used on mac or win. On those platforms
         # linker flags are set via xcode_settings and msvs_settings, respectively.
-        env_ldflags = os.environ.get("LDFLAGS", "").split()
+        if self.toolset == "target":
+            env_ldflags = os.environ.get("LDFLAGS", "").split()
+        elif self.toolset == "host":
+            env_ldflags = os.environ.get("LDFLAGS_host", "").split()
+
         if self.flavor == "mac":
             ldflags = self.xcode_settings.GetLdflags(
                 config_name,
