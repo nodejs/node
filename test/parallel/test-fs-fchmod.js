da217d0773a7f037bf0a270c2c1c2802e126fa0a
James M Snell <jasnell@gmail.com>
2021-02-22 12:03:32 -0800

    fs: fix flag and mode validation
    The `flag` and `mode` options were not being validated correctly.
    
    Signed-off-by: James M Snell <jasnell@gmail.com>
    Fixes: https://github.com/nodejs/node/issues/37430
    
    PR-URL: https://github.com/nodejs/node/pull/37480
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-fs-fchmod.js
+++ test/parallel/test-fs-fchmod.js
@@ -1,7 +1,6 @@
 'use strict';
 const common = require('../common');
 const assert = require('assert');
-const util = require('util');
 const fs = require('fs');
 
 // This test ensures that input for fchmod is valid, testing for valid
@@ -20,15 +19,16 @@ const fs = require('fs');
 });
 
 
-[false, null, undefined, {}, [], '', '123x'].forEach((input) => {
+[false, null, {}, []].forEach((input) => {
   const errObj = {
-    code: 'ERR_INVALID_ARG_VALUE',
-    name: 'TypeError',
-    message: 'The argument \'mode\' must be a 32-bit unsigned integer or an ' +
-             `octal string. Received ${util.inspect(input)}`
+    code: 'ERR_INVALID_ARG_TYPE',
   };
   assert.throws(() => fs.fchmod(1, input), errObj);
   assert.throws(() => fs.fchmodSync(1, input), errObj);
+});
+
+assert.throws(() => fs.fchmod(1, '123x'), {
+  code: 'ERR_INVALID_ARG_VALUE'
 });
 
 [-1, 2 ** 32].forEach((input) => {
