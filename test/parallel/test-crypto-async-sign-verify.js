66b1802672cd5180ad08a6cedeb4964d7e17923f
Filip Skokan <panva.ip@gmail.com>
2021-02-26 15:47:16 +0100

    fixup! crypto: add optional callback to crypto.sign and crypto.verify

--- test/parallel/test-crypto-async-sign-verify.js
+++ test/parallel/test-crypto-async-sign-verify.js
@@ -7,7 +7,7 @@ const assert = require('assert');
 const crypto = require('crypto');
 const fixtures = require('../common/fixtures');
 
-async function test(
+function test(
   publicFixture,
   privateFixture,
   algorithm,
@@ -61,40 +61,38 @@ async function test(
   }
 }
 
-Promise.all([
-  // RSA w/ default padding
-  test('rsa_public.pem', 'rsa_private.pem', 'sha256', true),
-  test('rsa_public.pem', 'rsa_private.pem', 'sha256', true,
-       { padding: crypto.constants.RSA_PKCS1_PADDING }),
+// RSA w/ default padding
+test('rsa_public.pem', 'rsa_private.pem', 'sha256', true),
+test('rsa_public.pem', 'rsa_private.pem', 'sha256', true,
+     { padding: crypto.constants.RSA_PKCS1_PADDING });
 
-  // RSA w/ PSS_PADDING and default saltLength
-  test('rsa_public.pem', 'rsa_private.pem', 'sha256', false,
-       { padding: crypto.constants.RSA_PKCS1_PSS_PADDING }),
-  test('rsa_public.pem', 'rsa_private.pem', 'sha256', false,
-       {
-         padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
-         saltLength: crypto.constants.RSA_PSS_SALTLEN_MAX_SIGN
-       }),
+// RSA w/ PSS_PADDING and default saltLength
+test('rsa_public.pem', 'rsa_private.pem', 'sha256', false,
+     { padding: crypto.constants.RSA_PKCS1_PSS_PADDING });
+test('rsa_public.pem', 'rsa_private.pem', 'sha256', false,
+     {
+       padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+       saltLength: crypto.constants.RSA_PSS_SALTLEN_MAX_SIGN
+     });
 
-  // RSA w/ PSS_PADDING and PSS_SALTLEN_DIGEST
-  test('rsa_public.pem', 'rsa_private.pem', 'sha256', false,
-       {
-         padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
-         saltLength: crypto.constants.RSA_PSS_SALTLEN_DIGEST
-       }),
+// RSA w/ PSS_PADDING and PSS_SALTLEN_DIGEST
+test('rsa_public.pem', 'rsa_private.pem', 'sha256', false,
+     {
+       padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+       saltLength: crypto.constants.RSA_PSS_SALTLEN_DIGEST
+     });
 
-  // ED25519
-  test('ed25519_public.pem', 'ed25519_private.pem', undefined, true),
-  // ED448
-  test('ed448_public.pem', 'ed448_private.pem', undefined, true),
+// ED25519
+test('ed25519_public.pem', 'ed25519_private.pem', undefined, true),
+// ED448
+test('ed448_public.pem', 'ed448_private.pem', undefined, true),
 
-  // ECDSA w/ der signature encoding
-  test('ec_secp256k1_public.pem', 'ec_secp256k1_private.pem', 'sha384',
-       false),
-  test('ec_secp256k1_public.pem', 'ec_secp256k1_private.pem', 'sha384',
-       false, { dsaEncoding: 'der' }),
+// ECDSA w/ der signature encoding
+test('ec_secp256k1_public.pem', 'ec_secp256k1_private.pem', 'sha384',
+     false),
+test('ec_secp256k1_public.pem', 'ec_secp256k1_private.pem', 'sha384',
+     false, { dsaEncoding: 'der' });
 
-  // ECDSA w/ ieee-p1363 signature encoding
-  test('ec_secp256k1_public.pem', 'ec_secp256k1_private.pem', 'sha384', false,
-       { dsaEncoding: 'ieee-p1363' }),
-]).then(common.mustCall());
+// ECDSA w/ ieee-p1363 signature encoding
+test('ec_secp256k1_public.pem', 'ec_secp256k1_private.pem', 'sha384', false,
+     { dsaEncoding: 'ieee-p1363' });
