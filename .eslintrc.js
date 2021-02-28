2463bd0689051ac6941ec75320a644f5880f10d5
cjihrig <cjihrig@gmail.com>
2021-02-28 13:05:42 -0500

    tools: update eslint-plugin-markdown configuration
    This commit updates the linting setup to work with
    eslint-plugin-markdown@2.0.0. This also allows the update-eslint
    script to continue to function properly without changes.
    
    PR-URL: https://github.com/nodejs/node/pull/37549
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,11 +53,11 @@ module.exports = {
   overrides: [
     {
       files: [
-        'doc/api/esm.md',
-        'doc/api/module.md',
-        'doc/api/modules.md',
-        'doc/api/packages.md',
-        'doc/api/wasi.md',
+        'doc/api/esm.md/*.js',
+        'doc/api/module.md/*.js',
+        'doc/api/modules.md/*.js',
+        'doc/api/packages.md/*.js',
+        'doc/api/wasi.md/*.js',
         'test/es-module/test-esm-type-flag.js',
         'test/es-module/test-esm-type-flag-alias.js',
         '*.mjs',
@@ -67,6 +67,10 @@ module.exports = {
     },
     {
       files: ['**/*.md'],
+      processor: 'markdown/markdown',
+    },
+    {
+      files: ['**/*.md/*.js'],
       parserOptions: { ecmaFeatures: { impliedStrict: true } },
       rules: { strict: 'off' },
     },
