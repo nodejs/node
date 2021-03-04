bd62771a2265b55959acb85ec6944b62cdef68b3
Ruy Adorno <ruyadorno@hotmail.com>
2021-03-04 18:19:28 -0500

    tools: use bundled npm in update scripts
    The scripts `./tools/update-babel-eslint.sh` and
    `./tools/update-eslint.sh` are relying on the version of `npm` found in
    the local-defined `$PATH` env.
    
    This changeset proposes to modify these scripts to run the version of
    npm bundled in the current branch (found at `./deps/npm`) - in order to:
    
    a) Standardize the version of npm that should be use to install these
    deps, avoids the pitfall of having an inadverted user run these
    scripts with an unsupported/incompatible npm version.
    b) Given that npm7 has a different install algorithm than npm6 that
    takes into account and install peer dependencies, it might be a safer
    choice to ensure what version of npm should be use during this
    transitional period in which users might still have npm6 by default in
    their local system.
    c) Avoids the possible extra churn of having different resulting files
    being shuffled around between installs due to usage of a disparate
    version of the npm cli.
    
    PR-URL: https://github.com/nodejs/node/pull/37613
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- tools/update-eslint.sh
+++ tools/update-eslint.sh
@@ -2,8 +2,6 @@
 
 # Shell script to update ESLint in the source tree to the latest release.
 
-# Depends on npm, npx, and node being in $PATH.
-
 # This script must be in the tools directory when it runs because it uses the
 # script source file path to determine directories to work in.
 
@@ -12,16 +10,22 @@ rm -rf node_modules/eslint node_modules/eslint-plugin-markdown
 (
     mkdir eslint-tmp
     cd eslint-tmp || exit
-    npm init --yes
 
-    npm install --global-style --no-bin-links --production --no-package-lock eslint@latest
-    npm install --global-style --no-bin-links --production --no-package-lock eslint-plugin-markdown@latest
+    ROOT="$PWD/../.."
+    [ -z "$NODE" ] && NODE="$ROOT/out/Release/node"
+    [ -x "$NODE" ] || NODE=`command -v node`
+    NPM="$ROOT/deps/npm"
+
+    "$NODE" "$NPM" init --yes
+
+    "$NODE" "$NPM" install --global-style --no-bin-links --production --no-package-lock eslint@latest
+    "$NODE" "$NPM" install --global-style --no-bin-links --production --no-package-lock eslint-plugin-markdown@latest
 
     # Use dmn to remove some unneeded files.
-    npx dmn@2.2.2 -f clean
+    "$NODE" "$NPM" exec -- dmn@2.2.2 -f clean
     # Use removeNPMAbsolutePaths to remove unused data in package.json.
     # This avoids churn as absolute paths can change from one dev to another.
-    npx removeNPMAbsolutePaths@1.0.4 .
+    "$NODE" "$NPM" exec -- removeNPMAbsolutePaths@1.0.4 .
 )
 
 mv eslint-tmp/node_modules/eslint node_modules/eslint
