53b9805f7a90373a2b254b82abeecc1d82b6b304
James M Snell <jasnell@gmail.com>
2021-03-04 06:50:36 -0800

    deps: update archs files for OpenSSL-1.1.1+quic
    Signed-off-by: James M Snell <jasnell@gmail.com>
    
    PR-URL: https://github.com/nodejs/node/pull/37601
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- deps/openssl/config/archs/linux-elf/asm_avx2/crypto/buildinf.h
+++ deps/openssl/config/archs/linux-elf/asm_avx2/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-elf"
-#define DATE "built on: Wed Feb 17 12:03:21 2021 UTC"
+#define DATE "built on: Thu Mar  4 14:47:04 2021 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
