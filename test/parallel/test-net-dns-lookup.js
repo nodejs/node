604366cb67a575f048eae471c497d54c29f370fa
treysis <treysis@gmx.net>
2021-03-13 19:05:18 +0100

    Fix test-net-dns-lookup.js

--- test/parallel/test-net-dns-lookup.js
+++ test/parallel/test-net-dns-lookup.js
@@ -29,12 +29,16 @@ const server = net.createServer(function(client) {
   server.close();
 });
 
-server.listen(0, '127.0.0.1', common.mustCall(function() {
-  net.connect(this.address().port, 'localhost')
+server.listen(0, common.localhostIP, common.mustCall(function() {
+  net.connect({
+    port: this.address().port,
+    host: 'localhost',
+    family: common.hasIPv6 ? 6 : 4,
+  })
     .on('lookup', common.mustCall(function(err, ip, type, host) {
       assert.strictEqual(err, null);
-      assert.match(ip, /^(127\.0\.0\.1|::1)$/);
-      assert.match(type.toString(), /^(4|6)$/);
+      assert.strictEqual(ip, common.hasIPv6 ? '::1' : '127.0.0.1');
+      assert.strictEqual(type, common.hasIPv6 ? 6 : 4);
       assert.strictEqual(host, 'localhost');
     }));
 }));
