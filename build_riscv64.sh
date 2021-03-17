7db7019766157a91b9065af0b7dcdd4f5c081245
luyahan <yahan@iscas.ac.cn>
2021-03-17 15:31:42 +0900

    add buils shell

---
+++ build_riscv64.sh
@@ -0,0 +1,7 @@
+export CC=riscv64-unknown-linux-gnu-gcc
+export CXX=riscv64-unknown-linux-gnu-g++
+export CC_host=gcc
+export CXX_host=g++
+
+
+./configure --cross-compiling --dest-cpu=riscv64  --verbose  --without-ssl
