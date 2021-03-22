04da72fa791beaafe40c46435f6745232f9487b7
Guy Bedford <guybedford@gmail.com>
2021-03-22 18:36:48 +0200

    deps: backport v8 f19142e6
    [top-level-await] Implement the new post-order requirement for
    async subgraphs
    
    Refs: https://github.com/v8/v8/commit/f19142e6139979da3a177cb0b9f382e459f5ccec
    PR-URL: https://github.com/nodejs/node/pull/37864
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- deps/v8/src/objects/module-inl.h
+++ deps/v8/src/objects/module-inl.h
@@ -37,8 +37,8 @@ SMI_ACCESSORS(Module, status, kStatusOffset)
 SMI_ACCESSORS(Module, hash, kHashOffset)
 
 BOOL_ACCESSORS(SourceTextModule, flags, async, AsyncBit::kShift)
-BOOL_ACCESSORS(SourceTextModule, flags, async_evaluating,
-               AsyncEvaluatingBit::kShift)
+BIT_FIELD_ACCESSORS(SourceTextModule, flags, async_evaluating_ordinal,
+                    SourceTextModule::AsyncEvaluatingOrdinalBits)
 ACCESSORS(SourceTextModule, async_parent_modules, ArrayList,
           kAsyncParentModulesOffset)
 ACCESSORS(SourceTextModule, top_level_capability, HeapObject,
@@ -139,6 +139,10 @@ Handle<SourceTextModule> SourceTextModule::GetAsyncParentModule(
 
 int SourceTextModule::AsyncParentModuleCount() {
   return async_parent_modules().Length();
+}
+
+bool SourceTextModule::IsAsyncEvaluating() const {
+  return async_evaluating_ordinal() >= kFirstAsyncEvaluatingOrdinal;
 }
 
 bool SourceTextModule::HasPendingAsyncDependencies() {
