98b3ffbaf1ec10d99c1f44189ba0a9b4aa0b3531
treysis <treysis@gmx.net>
2021-03-24 11:11:55 +0100

    Update test-net-writable.js

--- test/parallel/test-net-writable.js
+++ test/parallel/test-net-writable.js
@@ -6,7 +6,7 @@ const net = require('net');
 const server = net.createServer(common.mustCall(function(s) {
   server.close();
   s.end();
-})).listen(0, '127.0.0.1', common.mustCall(function() {
+})).listen(0, 'localhost', common.mustCall(function() {
   const socket = net.connect(this.address().port, '127.0.0.1');
   socket.on('end', common.mustCall(() => {
     assert.strictEqual(socket.writable, true);
