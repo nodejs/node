e3bd5f5a2b1c3d69a853506520b6e8624b4daa99
Nils Dralle <nils.91@googlemail.com>
2021-03-30 16:58:32 +0200

    crypto: exit potentially infinite loop in GetLastIssuedCert if stuck
    Fixes: https://github.com/nodejs/node/issues/37757
    Refs: https://github.com/nodejs/node/issues/37889

--- src/crypto/crypto_common.cc
+++ src/crypto/crypto_common.cc
@@ -480,8 +480,17 @@ MaybeLocal<Object> GetLastIssuedCert(
       return MaybeLocal<Object>();
     issuer_chain = ca_info;
 
+    // Take the value of cert->get() before and after the call to cert->reset()
+    // in order to compare them and provide a way to exit this loop
+    // in case it gets stuck
+    X509* value_before_reset = cert->get();
+
     // Delete previous cert and continue aggregating issuers.
     cert->reset(ca);
+
+    X509* value_after_reset = cert->get();
+    if (value_before_reset == value_after_reset)
+      break;
   }
   return MaybeLocal<Object>(issuer_chain);
 }
