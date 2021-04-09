581c0be7fed145b00d6bb5b71b80970fe748ea13
Nitzan Uziely <linkgoron@gmail.com>
2021-04-10 02:07:56 +0300

    tls: fix session and keylog add listener segfault
    Fix an issue where adding a session or keylog listener on a tlsSocket
    after it was destroyed caused a segfault.
    
    fixes: https://github.com/nodejs/node/issues/38133
    fixes: https://github.com/nodejs/node/issues/38135

---
+++ test/parallel/test-tls-tlswrap-segfault-2.js
@@ -0,0 +1,24 @@
+'use strict';
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+// This test ensures that Node.js doesn't incur a segfault while
+// adding session or keylog listeners after destroy.
+// https://github.com/nodejs/node/issues/38133
+// https://github.com/nodejs/node/issues/38135
+
+const tls = require('tls');
+const tlsSocketKeyLog = tls.connect('cause-error');
+tlsSocketKeyLog.on('error', () => {
+  setTimeout(() => {
+    tlsSocketKeyLog.on('keylog', () => { });
+  }, 10);
+});
+
+const tlsSocketSession = tls.connect('cause-error-2');
+tlsSocketSession.on('error', (e) => {
+  setTimeout(() => {
+    tlsSocketSession.on('keylog', () => { });
+  }, 10);
+});
