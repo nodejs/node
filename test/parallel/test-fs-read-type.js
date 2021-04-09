742e381be0b6c2baaccf907579609bcc5efd138c
Xu Meng <mengxumx@cn.ibm.com>
2021-04-08 20:39:31 -0500

    test: check the different error code on IBM i
    Fixes: https://github.com/nodejs/node/issues/36925
    
    PR-URL: https://github.com/nodejs/node/pull/38159
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>

--- test/parallel/test-fs-read-type.js
+++ test/parallel/test-fs-read-type.js
@@ -118,7 +118,10 @@ fs.read(fd,
         2n ** 53n - 1n,
         common.mustCall((err) => {
           if (err) {
-            assert.strictEqual(err.code, 'EFBIG');
+            if (common.isIBMi)
+              assert.strictEqual(err.errno, -127);
+            else
+              assert.strictEqual(err.code, 'EFBIG');
           }
         }));
 
@@ -239,5 +242,6 @@ try {
   // On systems where max file size is below 2^53-1, we'd expect a EFBIG error.
   // This is not using `assert.throws` because the above call should not raise
   // any error on systems that allows file of that size.
-  if (err.code !== 'EFBIG') throw err;
+  if (err.code !== 'EFBIG' && !(common.isIBMi && err.errno === -127))
+    throw err;
 }
