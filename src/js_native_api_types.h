d9070f35be97b75d85b7863ed93b2c98b3fa29fb
Darshan Sen <raisinten@gmail.com>
2021-04-13 20:08:51 +0530

    src: replace typedef with using in js_native_api_types.h

--- src/js_native_api_types.h
+++ src/js_native_api_types.h
@@ -8,20 +8,20 @@
 #include <stdint.h>  // NOLINT(modernize-deprecated-headers)
 
 #if !defined __cplusplus || (defined(_MSC_VER) && _MSC_VER < 1900)
-    typedef uint16_t char16_t;
+    using char16_t = uint16_t;
 #endif
 
 // JSVM API types are all opaque pointers for ABI stability
-// typedef undefined structs instead of void* for compile time type safety
-typedef struct napi_env__* napi_env;
-typedef struct napi_value__* napi_value;
-typedef struct napi_ref__* napi_ref;
-typedef struct napi_handle_scope__* napi_handle_scope;
-typedef struct napi_escapable_handle_scope__* napi_escapable_handle_scope;
-typedef struct napi_callback_info__* napi_callback_info;
-typedef struct napi_deferred__* napi_deferred;
+// using undefined structs instead of void* for compile time type safety
+using napi_env = struct napi_env__*;
+using napi_value = struct napi_value__*;
+using napi_ref = struct napi_ref__*;
+using napi_handle_scope = struct napi_handle_scope__*;
+using napi_escapable_handle_scope = struct napi_escapable_handle_scope__*;
+using napi_callback_info = struct napi_callback_info__*;
+using napi_deferred = struct napi_deferred__*;
 
-typedef enum {
+using napi_property_attributes = enum {
   napi_default = 0,
   napi_writable = 1 << 0,
   napi_enumerable = 1 << 1,
@@ -40,9 +40,9 @@ typedef enum {
                             napi_enumerable |
                             napi_configurable,
 #endif  // NAPI_VERSION >= 8
-} napi_property_attributes;
+};
 
-typedef enum {
+using napi_valuetype = enum {
   // ES6 types (corresponds to typeof)
   napi_undefined,
   napi_null,
@@ -54,9 +54,9 @@ typedef enum {
   napi_function,
   napi_external,
   napi_bigint,
-} napi_valuetype;
+};
 
-typedef enum {
+using napi_typedarray_type = enum {
   napi_int8_array,
   napi_uint8_array,
   napi_uint8_clamped_array,
@@ -68,9 +68,9 @@ typedef enum {
   napi_float64_array,
   napi_bigint64_array,
   napi_biguint64_array,
-} napi_typedarray_type;
+};
 
-typedef enum {
+using napi_status = enum {
   napi_ok,
   napi_invalid_arg,
   napi_object_expected,
@@ -93,7 +93,7 @@ typedef enum {
   napi_arraybuffer_expected,
   napi_detachable_arraybuffer_expected,
   napi_would_deadlock  // unused
-} napi_status;
+};
 // Note: when adding a new enum value to `napi_status`, please also update
 //   * `const int last_status` in the definition of `napi_get_last_error_info()'
 //     in file js_native_api_v8.cc.
@@ -102,13 +102,13 @@ typedef enum {
 //   * the definition of `napi_status` in doc/api/n-api.md to reflect the newly
 //     added value(s).
 
-typedef napi_value (*napi_callback)(napi_env env,
+using napi_callback = napi_value (*)(napi_env env,
                                     napi_callback_info info);
-typedef void (*napi_finalize)(napi_env env,
+using napi_finalize = void (*)(napi_env env,
                               void* finalize_data,
                               void* finalize_hint);
 
-typedef struct {
+using napi_property_descriptor = struct {
   // One of utf8name or name should be NULL.
   const char* utf8name;
   napi_value name;
@@ -120,41 +120,41 @@ typedef struct {
 
   napi_property_attributes attributes;
   void* data;
-} napi_property_descriptor;
+};
 
-typedef struct {
+using napi_extended_error_info = struct {
   const char* error_message;
   void* engine_reserved;
   uint32_t engine_error_code;
   napi_status error_code;
-} napi_extended_error_info;
+};
 
 #if NAPI_VERSION >= 6
-typedef enum {
+using napi_key_collection_mode = enum {
   napi_key_include_prototypes,
   napi_key_own_only
-} napi_key_collection_mode;
+};
 
-typedef enum {
+using napi_key_filter = enum {
   napi_key_all_properties = 0,
   napi_key_writable = 1,
   napi_key_enumerable = 1 << 1,
   napi_key_configurable = 1 << 2,
   napi_key_skip_strings = 1 << 3,
   napi_key_skip_symbols = 1 << 4
-} napi_key_filter;
+};
 
-typedef enum {
+using napi_key_conversion = enum {
   napi_key_keep_numbers,
   napi_key_numbers_to_strings
-} napi_key_conversion;
+};
 #endif  // NAPI_VERSION >= 6
 
 #if NAPI_VERSION >= 8
-typedef struct {
+using napi_type_tag = struct {
   uint64_t lower;
   uint64_t upper;
-} napi_type_tag;
+};
 #endif  // NAPI_VERSION >= 8
 
 #endif  // SRC_JS_NATIVE_API_TYPES_H_
