d053f12f83b400ba778522ddc9756bcfc7194cf0
Fedor Indutny <fedor@indutny.com>
2021-04-17 11:07:31 -0700

    deps: update llhttp to 6.0.0
    See: https://github.com/nodejs/node/pull/37678#issuecomment-821156758

--- deps/llhttp/src/api.c
+++ deps/llhttp/src/api.c
@@ -199,7 +199,7 @@ const char* llhttp_errno_name(llhttp_errno_t err) {
 const char* llhttp_method_name(llhttp_method_t method) {
 #define HTTP_METHOD_GEN(NUM, NAME, STRING) case HTTP_##NAME: return #STRING;
   switch (method) {
-    HTTP_METHOD_MAP(HTTP_METHOD_GEN)
+    HTTP_ALL_METHOD_MAP(HTTP_METHOD_GEN)
     default: abort();
   }
 #undef HTTP_METHOD_GEN
