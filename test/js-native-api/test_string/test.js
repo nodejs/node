78f163374f4900793f3e4d7fca2a0ef93dc046da
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-04-17 07:32:36 +0430

    Pass function directly (test_string)
    Passed functions directly instead of nesting it inside another.

--- test/js-native-api/test_string/test.js
+++ test/js-native-api/test_string/test.js
@@ -70,16 +70,10 @@ assert.strictEqual(test_string.TestUtf16Insufficient(str6), str6.slice(0, 3));
 assert.strictEqual(test_string.Utf16Length(str6), 5);
 assert.strictEqual(test_string.Utf8Length(str6), 14);
 
-assert.throws(() => {
-  test_string.TestLargeUtf8();
-}, /^Error: Invalid argument$/);
+assert.throws(test_string.TestLargeUtf8, /^Error: Invalid argument$/);
 
-assert.throws(() => {
-  test_string.TestLargeLatin1();
-}, /^Error: Invalid argument$/);
+assert.throws(test_string.TestLargeLatin1, /^Error: Invalid argument$/);
 
-assert.throws(() => {
-  test_string.TestLargeUtf16();
-}, /^Error: Invalid argument$/);
+assert.throws(test_string.TestLargeUtf16, /^Error: Invalid argument$/);
 
 test_string.TestMemoryCorruption(' '.repeat(64 * 1024));
