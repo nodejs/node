92d438831da6138c3866225851d4a4347bc074ac
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-04-19 13:48:37 +0200

    fixup! tools: install eslint-plugin-jsdoc

--- tools/update-eslint.sh
+++ tools/update-eslint.sh
@@ -6,7 +6,7 @@
 # script source file path to determine directories to work in.
 
 cd "$( dirname "$0" )" || exit
-rm -rf node_modules/eslint node_modules/eslint-plugin-markdown
+rm -rf node_modules/eslint
 (
     mkdir eslint-tmp
     cd eslint-tmp || exit
@@ -18,14 +18,20 @@ rm -rf node_modules/eslint node_modules/eslint-plugin-markdown
 
     "$NODE" "$NPM" init --yes
 
+    mkdir eslint-plugin-markdown eslint-plugin-jsdoc
     "$NODE" "$NPM" install --production --no-save --no-package-lock \
         eslint@latest \
         eslint-plugin-markdown@latest \
         eslint-plugin-jsdoc@latest \
-        rollup @rollup/plugin-node-resolve @rollup/plugin-commonjs @rollup/plugin-json
+        rollup @rollup/plugin-node-resolve @rollup/plugin-commonjs @rollup/plugin-json \
+        terser
     node -p '"export default"+JSON.stringify({external:Object.keys(require("eslint/package.json").dependencies),output:{format:"cjs",exports:"default"}})' > rollup.config.mjs
-    "$NODE" "$NPM" exec -- rollup -c rollup.config.mjs -p '@rollup/plugin-node-resolve' -p '@rollup/plugin-commonjs' -p '@rollup/plugin-json' -f cjs --file=./eslint-plugin-markdown/index.js -- ./node_modules/eslint-plugin-markdown/index.js
-    "$NODE" "$NPM" exec -- rollup -c rollup.config.mjs -p '@rollup/plugin-node-resolve' -p '@rollup/plugin-commonjs' -p '@rollup/plugin-json' -f cjs --file=./eslint-plugin-jsdoc/index.js -- ./node_modules/eslint-plugin-jsdoc/dist/index.js
+
+    "$NODE" "$NPM" exec -- rollup -c rollup.config.mjs -p '@rollup/plugin-node-resolve' -p '@rollup/plugin-commonjs' -p '@rollup/plugin-json' -- ./node_modules/eslint-plugin-markdown/index.js | \
+    "$NODE" "$NPM" exec -- terser --toplevel --compress --mangle --timings -o ./eslint-plugin-markdown/index.js
+
+    "$NODE" "$NPM" exec -- rollup -c rollup.config.mjs -p '@rollup/plugin-node-resolve' -p '@rollup/plugin-commonjs' -p '@rollup/plugin-json' -- ./node_modules/eslint-plugin-jsdoc/dist/index.js | \
+    "$NODE" "$NPM" exec -- terser --toplevel --compress --mangle --timings -o ./eslint-plugin-jsdoc/index.js
     rm -r node_modules
 
     "$NODE" "$NPM" install --global-style --no-bin-links --production --no-package-lock eslint@latest
