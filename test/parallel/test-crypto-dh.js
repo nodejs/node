88bc8645e733ee3f9b86272b6c1ae9c1da7790c9
eladkeyshawn <elad.keyshawn@gmail.com>
2021-04-20 14:28:20 +0300

    crypto: fix DiffieHellman `generator` validation
    PR-URL: https://github.com/nodejs/node/pull/38311
    Fixes: https://github.com/nodejs/node/issues/38302
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>

--- test/parallel/test-crypto-dh.js
+++ test/parallel/test-crypto-dh.js
@@ -495,3 +495,7 @@ assert.throws(
     code: 'ERR_INVALID_ARG_TYPE'
   }
 );
+[true, Symbol(), {}, () => {}, []].forEach((generator) => assert.throws(
+  () => crypto.createDiffieHellman('', 'base64', generator),
+  { code: 'ERR_INVALID_ARG_TYPE' }
+));
