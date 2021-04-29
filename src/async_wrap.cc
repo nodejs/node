df97c7e5b1dc7bba033758629dfbf19857729d72
Darshan Sen <raisinten@gmail.com>
2021-04-29 20:47:09 +0530

    async_hooks: merge resource_symbol with owner_symbol

--- src/async_wrap.cc
+++ src/async_wrap.cc
@@ -582,7 +582,7 @@ void AsyncWrap::EmitDestroy(bool from_gc) {
 
   if (!persistent().IsEmpty() && !from_gc) {
     HandleScope handle_scope(env()->isolate());
-    USE(object()->Set(env()->context(), env()->resource_symbol(), object()));
+    USE(object()->Set(env()->context(), env()->owner_symbol(), object()));
   }
 }
 
@@ -862,7 +862,7 @@ void AsyncWrap::AsyncReset(Local<Object> resource, double execution_async_id,
     Local<Object> obj = object();
     CHECK(!obj.IsEmpty());
     if (resource != obj) {
-      USE(obj->Set(env()->context(), env()->resource_symbol(), resource));
+      USE(obj->Set(env()->context(), env()->owner_symbol(), resource));
     }
   }
 
