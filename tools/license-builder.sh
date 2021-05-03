7c4748b0dc5f0a49c5cd90fa442a1f1462b5926e
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-05-03 11:15:35 +0200

    doc: use `HEAD` instead of `master` for links
    PR-URL: https://github.com/nodejs/node/pull/38518
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>

--- tools/license-builder.sh
+++ tools/license-builder.sh
@@ -63,7 +63,7 @@ addlicense "llhttp" "deps/llhttp" "$(cat deps/llhttp/LICENSE-MIT)"
 addlicense "OpenSSL" "deps/openssl" \
            "$(sed -e '/^ \*\/$/,$d' -e '/^ [^*].*$/d' -e '/\/\*.*$/d' -e '/^$/d' -e 's/^[/ ]\* *//' "${rootdir}"/deps/openssl/openssl/LICENSE)"
 addlicense "Punycode.js" "lib/punycode.js" \
-           "$(curl -sL https://raw.githubusercontent.com/bestiejs/punycode.js/master/LICENSE-MIT.txt)"
+           "$(curl -sL https://raw.githubusercontent.com/bestiejs/punycode.js/HEAD/LICENSE-MIT.txt)"
 addlicense "V8" "deps/v8" "$(cat "${rootdir}"/deps/v8/LICENSE)"
 addlicense "SipHash" "deps/v8/src/third_party/siphash" \
            "$(sed -e '/You should have received a copy of the CC0/,$d' -e 's/^\/\* *//' -e 's/^ \* *//' deps/v8/src/third_party/siphash/halfsiphash.cc)"
