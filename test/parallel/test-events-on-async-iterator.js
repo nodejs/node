b3b59953fe2e4f330f6f6102341b15b5885c9ada
Rongjian Zhang <pd4d10@gmail.com>
2021-05-07 21:49:07 +0800

    test: improve coverage of lib/events.js
    PR-URL: https://github.com/nodejs/node/pull/38582
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Zijian Liu <lxxyxzj@gmail.com>

--- test/parallel/test-events-on-async-iterator.js
+++ test/parallel/test-events-on-async-iterator.js
@@ -37,6 +37,13 @@ async function basic() {
   assert.strictEqual(ee.listenerCount('error'), 0);
 }
 
+async function invalidArgType() {
+  assert.throws(() => on({}, 'foo'), common.expectsError({
+    code: 'ERR_INVALID_ARG_TYPE',
+    name: 'TypeError',
+  }));
+}
+
 async function error() {
   const ee = new EventEmitter();
   const _err = new Error('kaboom');
@@ -363,6 +370,7 @@ async function abortableOnAfterDone() {
 async function run() {
   const funcs = [
     basic,
+    invalidArgType,
     error,
     errorDelayed,
     throwInLoop,
