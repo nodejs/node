fbe3d6a74bfc5551d23b8b8f8a33a875b379bb0a
legendecas <legendecas@gmail.com>
2021-05-11 00:45:24 +0800

    src: fix fatal errors when a current isolate not exist
    napi_fatal_error and node watchdog trigger fatal error but rather
    running on a thread that hold no current isolate.
    
    PR-URL: https://github.com/nodejs/node/pull/38624
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- test/node-api/test_fatal/test_threads.js
+++ test/node-api/test_fatal/test_threads.js
@@ -7,13 +7,14 @@ const test_fatal = require(`./build/${common.buildType}/test_fatal`);
 // Test in a child process because the test code will trigger a fatal error
 // that crashes the process.
 if (process.argv[2] === 'child') {
-  test_fatal.TestStringLength();
-  return;
+  test_fatal.TestThread();
+  // Busy loop to allow the work thread to abort.
+  while (true) {}
 }
 
 const p = child_process.spawnSync(
-  process.execPath, [ '--napi-modules', __filename, 'child' ]);
+  process.execPath, [ __filename, 'child' ]);
 assert.ifError(p.error);
 assert.ok(p.stderr.toString().includes(
-  'FATAL ERROR: test_fatal::Test fatal message'));
+  'FATAL ERROR: work_thread foobar'));
 assert.ok(p.status === 134 || p.signal === 'SIGABRT');
