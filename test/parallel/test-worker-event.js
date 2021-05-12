f2ce981ca01ec34192cb03077a719d5a34c08aae
James M Snell <jasnell@gmail.com>
2021-05-12 15:34:29 -0700

    tools: add `Worker` to type-parser
    Signed-off-by: James M Snell <jasnell@gmail.com>
    
    PR-URL: https://github.com/nodejs/node/pull/38659
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>

--- test/parallel/test-worker-event.js
+++ test/parallel/test-worker-event.js
@@ -4,10 +4,11 @@ const common = require('../common');
 const assert = require('assert');
 const {
   Worker,
+  threadId: parentThreadId,
 } = require('worker_threads');
 
 process.on('worker', common.mustCall(({ threadId }) => {
-  assert.strictEqual(threadId, 1);
+  assert.strictEqual(threadId, parentThreadId + 1);
 }));
 
 new Worker('', { eval: true });
