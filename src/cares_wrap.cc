8809ef98f92cfe5bd349e425ebe0e69c51ea9153
Anna Henningsen <anna@addaleax.net>
2021-05-13 15:53:52 +0200

    src: update cares_wrap OpenBSD defines
    Move the `#define`s back into `cares_wrap.cc`, as they are part of
    the implementation, not the declarations used in `cares_wrap.h`,
    and apply the suggestion from
    https://github.com/nodejs/node/pull/38572#discussion_r627809407
    to make the defines a bit more generic and not check for OpenBSD
    specifically.
    
    Refs: https://github.com/nodejs/node/pull/38572
    
    PR-URL: https://github.com/nodejs/node/pull/38670
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/cares_wrap.cc
+++ src/cares_wrap.cc
@@ -38,6 +38,19 @@
 #include <vector>
 #include <unordered_set>
 
+#ifndef T_CAA
+# define T_CAA    257 /* Certification Authority Authorization */
+#endif
+
+// OpenBSD does not define these
+#ifndef AI_ALL
+# define AI_ALL 0
+#endif
+#ifndef AI_V4MAPPED
+# define AI_V4MAPPED 0
+#endif
+
+
 namespace node {
 namespace cares_wrap {
 
