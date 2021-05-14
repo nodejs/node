e265d8ee1b10ddfe0fdddf2287c295d71c5c2530
Gabriel Schulhof <gabrielschulhof@gmail.com>
2021-05-14 09:17:02 -0700

    test: give js-native-api tests consistent names
    The convention for js-native-api/<test_name>:
      * <test_name>.c or <test_name>.cc has the entry point
      * The name of the target is <test_name>
    
    PR-URL: https://github.com/nodejs/node/pull/38692
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/js-native-api/test_new_target/test.js
+++ test/js-native-api/test_new_target/test.js
@@ -2,7 +2,7 @@
 
 const common = require('../../common');
 const assert = require('assert');
-const binding = require(`./build/${common.buildType}/binding`);
+const binding = require(`./build/${common.buildType}/test_new_target`);
 
 class Class extends binding.BaseClass {
   constructor() {
