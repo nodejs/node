d6009ee869c8b6ccdc0cfd4d2801943f70016b10
Ben Noordhuis <info@bnoordhuis.nl>
2021-05-21 11:23:36 +0200

    deps: uv: cherry-pick 99c29c9c2c9b
    Original commit message:
    
        idna: fix OOB read in punycode decoder
    
        Reported by Eric Sesterhenn in collaboration with
        Cure53 and ExpressVPN.
    
        Reported-By: Eric Sesterhenn <eric.sesterhenn@x41-dsec.de>
        PR-URL: https://github.com/libuv/libuv-private/pull/1
        Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
        Reviewed-By: Richard Lau <rlau@redhat.com>
    
    CVE-ID: CVE-2021-22918
    Refs: https://hackerone.com/reports/1209681
    PR-URL: https://github.com/nodejs-private/node-private/pull/267
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Beth Griggs <bgriggs@redhat.com>

--- deps/uv/test/test-idna.c
+++ deps/uv/test/test-idna.c
@@ -96,6 +96,25 @@ TEST_IMPL(utf8_decode1) {
   return 0;
 }
 
+TEST_IMPL(utf8_decode1_overrun) {
+  const char* p;
+  char b[1];
+
+  /* Single byte. */
+  p = b;
+  b[0] = 0x7F;
+  ASSERT_EQ(0x7F, uv__utf8_decode1(&p, b + 1));
+  ASSERT_EQ(p, b + 1);
+
+  /* Multi-byte. */
+  p = b;
+  b[0] = 0xC0;
+  ASSERT_EQ((unsigned) -1, uv__utf8_decode1(&p, b + 1));
+  ASSERT_EQ(p, b + 1);
+
+  return 0;
+}
+
 /* Doesn't work on z/OS because that platform uses EBCDIC, not ASCII. */
 #ifndef __MVS__
 
--- deps/uv/test/test-list.h
+++ deps/uv/test/test-list.h
@@ -524,6 +524,7 @@ TEST_DECLARE  (fork_threadpool_queue_work_simple)
 
 TEST_DECLARE  (idna_toascii)
 TEST_DECLARE  (utf8_decode1)
+TEST_DECLARE  (utf8_decode1_overrun)
 TEST_DECLARE  (uname)
 
 TEST_DECLARE  (metrics_idle_time)
@@ -1120,6 +1121,7 @@ TASK_LIST_START
 #endif
 
   TEST_ENTRY  (utf8_decode1)
+  TEST_ENTRY  (utf8_decode1_overrun)
   TEST_ENTRY  (uname)
 
 /* Doesn't work on z/OS because that platform uses EBCDIC, not ASCII. */
