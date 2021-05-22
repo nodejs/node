b6471c9e7623bf211873ae4bc024ab0f5558e08f
Luigi Pinca <luigipinca@gmail.com>
2021-05-22 07:15:17 +0200

    tools: update ESLint to 7.27.0
    PR-URL: https://github.com/nodejs/node/pull/38764
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

---
+++ tools/node_modules/eslint/messages/extend-config-missing.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = function(it) {
+    const { configName, importerName } = it;
+
+    return `
+ESLint couldn't find the config "${configName}" to extend from. Please check that the name of the config is correct.
+
+The config "${configName}" was referenced from the config file in "${importerName}".
+
+If you still have problems, please stop by https://eslint.org/chat/help to chat with the team.
+`.trimLeft();
+};
