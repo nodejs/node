d5a21a5488aeb323de4ed34a785da9b41b7c244b
legendecas <legendecas@gmail.com>
2021-05-24 23:39:36 +0800

    node-api: correctly return pending exception status on napi_new_instance
    When there are any JavaScript exceptions pending,
    `napi_pending_exception` should be returned.

--- test/js-native-api/test_exception/test_exception.c
+++ test/js-native-api/test_exception/test_exception.c
@@ -22,6 +22,22 @@ static napi_value returnException(napi_env env, napi_callback_info info) {
   return NULL;
 }
 
+static napi_value constructReturnException(napi_env env, napi_callback_info info) {
+  size_t argc = 1;
+  napi_value args[1];
+  NODE_API_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
+
+  napi_value result;
+  napi_status status = napi_new_instance(env, args[0], 0, 0, &result);
+  if (status == napi_pending_exception) {
+    napi_value ex;
+    NODE_API_CALL(env, napi_get_and_clear_last_exception(env, &ex));
+    return ex;
+  }
+
+  return NULL;
+}
+
 static napi_value allowException(napi_env env, napi_callback_info info) {
   size_t argc = 1;
   napi_value args[1];
@@ -32,6 +48,19 @@ static napi_value allowException(napi_env env, napi_callback_info info) {
 
   napi_value result;
   napi_call_function(env, global, args[0], 0, 0, &result);
+  // Ignore status and check napi_is_exception_pending() instead.
+
+  NODE_API_CALL(env, napi_is_exception_pending(env, &exceptionWasPending));
+  return NULL;
+}
+
+static napi_value constructAllowException(napi_env env, napi_callback_info info) {
+  size_t argc = 1;
+  napi_value args[1];
+  NODE_API_CALL(env, napi_get_cb_info(env, info, &argc, args, NULL, NULL));
+
+  napi_value result;
+  napi_new_instance(env, args[0], 0, 0, &result);
   // Ignore status and check napi_is_exception_pending() instead.
 
   NODE_API_CALL(env, napi_is_exception_pending(env, &exceptionWasPending));
@@ -64,6 +93,8 @@ napi_value Init(napi_env env, napi_value exports) {
   napi_property_descriptor descriptors[] = {
     DECLARE_NODE_API_PROPERTY("returnException", returnException),
     DECLARE_NODE_API_PROPERTY("allowException", allowException),
+    DECLARE_NODE_API_PROPERTY("constructReturnException", constructReturnException),
+    DECLARE_NODE_API_PROPERTY("constructAllowException", constructAllowException),
     DECLARE_NODE_API_PROPERTY("wasPending", wasPending),
     DECLARE_NODE_API_PROPERTY("createExternal", createExternal),
   };
