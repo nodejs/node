172467191b8060dd02ee146a53ce7e230047392a
Joyee Cheung <joyeec9h3@gmail.com>
2021-05-27 20:51:46 +0800

    bootstrap: implement --snapshot-blob and --build-snapshot
    This patch introduces `--build-snapshot` and `--snapshot-blob` options
    for creating and using user land snapshots.
    
    For the initial iteration, user land CJS modules and ESM are not yet
    supported in the snapshot, so only one single file can be snapshotted
    (users can bundle their applications into a single script with their
    bundler of choice to build a snapshot though).
    
    A subset of builtins should already work, and support for more builtins
    are being added. This PR includes tests checking that the TypeScript
    compiler and the marked markdown renderer (and the builtins they use)
    can be snapshotted and deserialized.
    
    To generate a snapshot using `snapshot.js` as entry point and write the
    snapshot blob to `snapshot.blob`:
    
    ```
    $ echo "globalThis.foo = 'I am from the snapshot'" > snapshot.js
    $ node --snapshot-blob snapshot.blob --build-snapshot snapshot.js
    ```
    
    To restore application state from `snapshot.blob`, with `index.js` as
    the entry point script for the deserialized application:
    
    ```
    $ echo "console.log(globalThis.foo)" > index.js
    $ node --snapshot-blob snapshot.blob index.js
    I am from the snapshot
    ```
    
    Users can also use the `v8.startupSnapshot` API to specify an entry
    point at snapshot building time, thus avoiding the need of an additional
    entry script at deserialization time:
    
    ```
    $ echo "require('v8').startupSnapshot.setDeserializeMainFunction(() => console.log('I am from the snapshot'))" > snapshot.js
    $ node --snapshot-blob snapshot.blob --build-snapshot snapshot.js
    $ node --snapshot-blob snapshot.blob
    I am from the snapshot
    ```
    
    Note that this patch only adds functionality to the `node` executable
    for building run-time user-land snapshots, the generated snapshot is
    stored into a separate file on disk. Building a single binary with both
    Node.js and an embedded snapshot has already been possible with the
    `--node-snapshot-main` option to the `configure` script if the user
    compiles Node.js from source. It would be a different task to enable the
    `node` executable to produce a single binary that contains both Node.js
    and an embedded snapshot without building Node.js from source, which
    should be layered on top of the SEA (Single Executable Apps) initiative.
    
    Known limitations/bugs that are being fixed in the upstream:
    
    - V8 hits a DCHECK when deserializing certain mutated globals, e.g.
      `Error.stackTraceLimit` (it should work fine in the release build,
      however): https://chromium-review.googlesource.com/c/v8/v8/+/3319481
    - Layout of V8's read-only heap can be inconsistent after
      deserialization, resulting in memory corruption:
      https://bugs.chromium.org/p/v8/issues/detail?id=12921
    
    PR-URL: https://github.com/nodejs/node/pull/38905
    Refs: https://github.com/nodejs/node/issues/35711
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

---
+++ test/parallel/test-snapshot-api.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// This tests snapshot JS API
+
+require('../common');
+const assert = require('assert');
+const { spawnSync } = require('child_process');
+const tmpdir = require('../common/tmpdir');
+const fixtures = require('../common/fixtures');
+const path = require('path');
+const fs = require('fs');
+
+tmpdir.refresh();
+const blobPath = path.join(tmpdir.path, 'snapshot.blob');
+const entry = fixtures.path('snapshot', 'v8-startup-snapshot-api.js');
+{
+  const child = spawnSync(process.execPath, [
+    '--snapshot-blob',
+    blobPath,
+    '--build-snapshot',
+    entry,
+  ], {
+    cwd: tmpdir.path
+  });
+  if (child.status !== 0) {
+    console.log(child.stderr.toString());
+    console.log(child.stdout.toString());
+    assert.strictEqual(child.status, 0);
+  }
+  const stats = fs.statSync(path.join(tmpdir.path, 'snapshot.blob'));
+  assert(stats.isFile());
+}
+
+{
+  const child = spawnSync(process.execPath, [
+    '--snapshot-blob',
+    blobPath,
+  ], {
+    cwd: tmpdir.path,
+    env: {
+      ...process.env,
+    }
+  });
+
+  const stdout = child.stdout.toString().trim();
+  const file = fs.readFileSync(fixtures.path('x1024.txt'), 'utf8');
+  assert.strictEqual(stdout, file);
+  assert.strictEqual(child.status, 0);
+}
