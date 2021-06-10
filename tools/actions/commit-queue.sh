0266c48b3944fc1cf8dd6d947bec2ebf9d20e564
bl-ue <bl-ue@users.noreply.github.com>
2021-06-10 18:24:30 -0400

    tools: fix typo in commit-queue.sh
    PR-URL: https://github.com/nodejs/node/pull/39000
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Zijian Liu <lxxyxzj@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- tools/actions/commit-queue.sh
+++ tools/actions/commit-queue.sh
@@ -77,7 +77,7 @@ for pr in "$@"; do
     rm output output.json
     # If `git node land --abort` fails, we're in unknown state. Better to stop
     # the script here, current PR was removed from the queue so it shouldn't
-    # interfer again in the future
+    # interfere again in the future.
     git node land --abort --yes
   else
     rm output
