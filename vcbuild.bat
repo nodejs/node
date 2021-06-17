0f3328a6881f8e9150b564edf891b88cb9d37188
Joyee Cheung <joyee@igalia.com>
2021-06-18 03:47:06 +0800

    build: reconfigure when gyp files change on Windows
    Previously only changes to .gypi files trigger a reconfigure
    on Windows.
    
    PR-URL: https://github.com/nodejs/node/pull/39066
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- vcbuild.bat
+++ vcbuild.bat
@@ -295,7 +295,7 @@ if defined projgen goto run-configure
 if not exist node.sln goto run-configure
 if not exist .gyp_configure_stamp goto run-configure
 echo %configure_flags% > .tmp_gyp_configure_stamp
-where /R . /T *.gyp? >> .tmp_gyp_configure_stamp
+where /R . /T *.gyp* >> .tmp_gyp_configure_stamp
 fc .gyp_configure_stamp .tmp_gyp_configure_stamp >NUL 2>&1
 if errorlevel 1 goto run-configure
 
@@ -316,7 +316,7 @@ if not exist node.sln goto create-msvs-files-failed
 set project_generated=1
 echo Project files generated.
 echo %configure_flags% > .gyp_configure_stamp
-where /R . /T *.gyp? >> .gyp_configure_stamp
+where /R . /T *.gyp* >> .gyp_configure_stamp
 
 :msbuild
 @rem Skip build if requested.
