4d7b8c6adf7140e574ca5b729d6f89f8cb222d83
Michaël Zasso <targos@protonmail.com>
2021-06-20 12:37:17 +0200

    build: adapt v8_pch.h to V8 9.3
    A header was moved.
    
    Refs: https://github.com/v8/v8/commit/7f5383e8ad4d1dd6d0fa732d7dacf4ed08371ebe
    
    PR-URL: https://github.com/nodejs/node/pull/39469
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- tools/msvs/pch/v8_pch.h
+++ tools/msvs/pch/v8_pch.h
@@ -1,5 +1,6 @@
 #include "src/api/api-inl.h"
 #include "src/ast/ast.h"
+#include "src/base/vector.h"
 #include "src/builtins/builtins-definitions.h"
 #include "src/common/globals.h"
 #include "src/objects/objects-inl.h"
@@ -7,7 +8,6 @@
 #include "src/objects/js-objects-inl.h"
 #include "src/objects/fixed-array-inl.h"
 #include "src/utils/utils.h"
-#include "src/utils/vector.h"
 
 
 #include <algorithm>
