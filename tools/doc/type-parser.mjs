11dd3b01d886ee4d5ac8126d6ae1988441d30fc4
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-07-01 17:03:15 +0200

    tools,doc: fix error message for unrecognized type

--- tools/doc/type-parser.mjs
+++ tools/doc/type-parser.mjs
@@ -288,7 +288,7 @@ export function toLink(typeInput) {
       } else {
         throw new Error(
           `Unrecognized type: '${typeTextFull}'.\n` +
-          "Please, edit the type or update the 'tools/doc/type-parser.js'."
+          `Please, edit the type or update '${import.meta.url}'.`
         );
       }
     } else {
