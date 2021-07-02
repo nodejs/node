12622c5f86fde7c878c7e4e7f441206cf0980457
Rich Trott <rtrott@gmail.com>
2021-07-02 06:44:15 -0700

    test: remove workaround code in debugger test
    Remove code that made a check more lenient to account for a known issue
    that is no longer reproducible.
    
    Refs: https://github.com/nodejs/node-inspect/issues/35
    
    PR-URL: https://github.com/nodejs/node/pull/39238
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jan Krems <jan.krems@gmail.com>

--- test/sequential/test-debugger-preserve-breaks.js
+++ test/sequential/test-debugger-preserve-breaks.js
@@ -53,19 +53,9 @@ const path = require('path');
     })
     .then(() => cli.command('breakpoints'))
     .then(() => {
-      // TODO: There is a known issue on AIX and some other operating systems
-      // where the breakpoints aren't properly resolved yet when we reach this
-      // point. Eventually that should be figured out but for now we don't
-      // want to fail builds because of it.
-      // What it should be:
-      //
-      // const msg = `SCRIPT: ${script}, OUTPUT: ${cli.output}`;
-      // assert.ok(cli.output.includes(`#0 ${script}:2`), msg);
-      // assert.ok(cli.output.includes(`#1 ${script}:3`), msg);
-      //
-      // What we're doing for now instead:
-      assert.match(cli.output, /#0 [^\n]+three-lines\.js\$?:2/);
-      assert.match(cli.output, /#1 [^\n]+three-lines\.js\$?:3/);
+      const msg = `SCRIPT: ${script}, OUTPUT: ${cli.output}`;
+      assert.ok(cli.output.includes(`#0 ${script}:2`), msg);
+      assert.ok(cli.output.includes(`#1 ${script}:3`), msg);
     })
     .then(() => cli.quit())
     .then(null, onFatal);
