3a4f3873be8b9a21d362dbc983c2efabcffe76e5
Joyee Cheung <joyeec9h3@gmail.com>
2021-07-10 01:54:09 +0800

    process: support hrtime in the snapshot
    Put the hrtime backing store in the process methods binding data
    so that it can be integrated into the snapshot builder. For
    now we simply discard the contents of the hrtime buffer during
    serialization and create new buffers upon deserialization because
    the contents are only useful in a synchronous call.
    
    This also moves the helper function for creating V8 FastAPI methods
    into `Environment::SetFastMethod()` for code reuse. The v8::CFunction
    need to be created before the Environment is created so that we
    have the CTypeInfo address available for external reference
    registration.
    
    PR-URL: https://github.com/nodejs/node/pull/40649
    Refs: https://github.com/nodejs/node/issues/35711
    Refs: https://github.com/nodejs/node/issues/37476
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_external_reference.h
+++ src/node_external_reference.h
@@ -5,9 +5,12 @@
 
 #include <cinttypes>
 #include <vector>
+#include "v8-fast-api-calls.h"
 #include "v8.h"
 
 namespace node {
+
+using CFunctionCallback = void (*)(v8::Local<v8::Value> receiver);
 
 // This class manages the external references from the V8 heap
 // to the C++ addresses in Node.js.
@@ -16,6 +19,8 @@ class ExternalReferenceRegistry {
   ExternalReferenceRegistry();
 
 #define ALLOWED_EXTERNAL_REFERENCE_TYPES(V)                                    \
+  V(CFunctionCallback)                                                         \
+  V(const v8::CFunctionInfo*)                                                  \
   V(v8::FunctionCallback)                                                      \
   V(v8::AccessorGetterCallback)                                                \
   V(v8::AccessorSetterCallback)                                                \
