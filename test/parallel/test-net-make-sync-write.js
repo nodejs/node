4f8f590e704b7fae2b151cf9583077b22b31e79c
tatsuyafukui <nakedtatsuya@gmail.com>
2021-07-10 14:57:15 +0900

    test: add internal/net makeSyncWrite test

---
+++ test/parallel/test-net-make-sync-write.js
@@ -0,0 +1,19 @@
+'use strict';
+// Flags: --expose-internals
+const common = require('../common');
+const assert = require('assert');
+const { makeSyncWrite } = require('internal/net');
+{
+  const _this = {
+    _handle: {
+      bytesWritten: 0,
+    },
+    write: makeSyncWrite(1),
+  };
+  _this.write(
+    'chunk\n',
+    null,
+    common.mustCall(() => {})
+  );
+  assert.strictEqual(_this._handle.bytesWritten, 6);
+}
