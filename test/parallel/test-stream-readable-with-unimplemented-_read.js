09d8c0c8d2c17fc07e9e313c0f35a7b696d18bc8
Robert Nagy <ronagy@icloud.com>
2021-07-10 15:03:02 +0200

    stream: destroy readable on read error
    PR-URL: https://github.com/nodejs/node/pull/39342
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- test/parallel/test-stream-readable-with-unimplemented-_read.js
+++ test/parallel/test-stream-readable-with-unimplemented-_read.js
@@ -1,18 +1,13 @@
 'use strict';
-require('../common');
-
-const assert = require('assert');
+const common = require('../common');
 const { Readable } = require('stream');
 
 const readable = new Readable();
 
-assert.throws(
-  () => {
-    readable.read();
-  },
-  {
-    code: 'ERR_METHOD_NOT_IMPLEMENTED',
-    name: 'Error',
-    message: 'The _read() method is not implemented'
-  }
-);
+readable.read();
+readable.on('error', common.expectsError({
+  code: 'ERR_METHOD_NOT_IMPLEMENTED',
+  name: 'Error',
+  message: 'The _read() method is not implemented'
+}));
+readable.on('close', common.mustCall());
