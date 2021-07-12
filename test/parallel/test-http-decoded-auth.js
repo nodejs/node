151a63ebd0b9b7df2f40e047dc9c578985c9d92d
Lew Gordon <lew.gordon@genesys.com>
2021-07-12 09:11:53 -0400

    fixup! linting fixes

--- test/parallel/test-http-decoded-auth.js
+++ test/parallel/test-http-decoded-auth.js
@@ -4,12 +4,32 @@ const assert = require('assert');
 const http = require('http');
 
 const testCases = [
-  {username: 'test@test"', password: '123456^', expected: 'dGVzdEB0ZXN0IjoxMjM0NTZe'},
-  {username: 'test%40test', password: '123456', expected: 'dGVzdEB0ZXN0OjEyMzQ1Ng=='},
-  {username: 'not%3Agood', password: 'god', expected: 'bm90Omdvb2Q6Z29k'},
-  {username: 'not%22good', password: 'g%5Eod', expected: 'bm90Imdvb2Q6Z15vZA=='},
-  {username: 'test1234::::', password: 'mypass', expected: 'dGVzdDEyMzQ6Ojo6Om15cGFzcw=='}
-]
+  {
+    username: 'test@test"',
+    password: '123456^',
+    expected: 'dGVzdEB0ZXN0IjoxMjM0NTZe'
+  },
+  {
+    username: 'test%40test',
+    password: '123456',
+    expected: 'dGVzdEB0ZXN0OjEyMzQ1Ng=='
+  },
+  {
+    username: 'not%3Agood',
+    password: 'god',
+    expected: 'bm90Omdvb2Q6Z29k'
+  },
+  {
+    username: 'not%22good',
+    password: 'g%5Eod',
+    expected: 'bm90Imdvb2Q6Z15vZA=='
+  },
+  {
+    username: 'test1234::::',
+    password: 'mypass',
+    expected: 'dGVzdDEyMzQ6Ojo6Om15cGFzcw=='
+  },
+];
 
 for (const testCase of testCases) {
   const server = http.createServer(function(request, response) {
@@ -19,7 +39,7 @@ for (const testCase of testCases) {
     response.end('ok');
     server.close();
   });
-  
+
   server.listen(0, function() {
     // make the request
     const url = new URL(`http://${testCase.username}:${testCase.password}@localhost:${this.address().port}`);
