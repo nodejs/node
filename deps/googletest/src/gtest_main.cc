9761005d266e57f418ed33534e4ddf3c25af0138
legendecas <legendecas@gmail.com>
2021-07-14 23:20:27 +0800

    deps: extract gtest source files to deps/googletest
    PR-URL: https://github.com/nodejs/node/pull/39386
    Refs: https://github.com/nodejs/node/pull/39361
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- deps/googletest/src/gtest_main.cc
+++ deps/googletest/src/gtest_main.cc
@@ -27,35 +27,28 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-//
-// Google C++ Testing and Mocking Framework definitions useful in production
-// code. GOOGLETEST_CM0003 DO NOT DELETE
+#include <cstdio>
+#include "gtest/gtest.h"
 
-#ifndef GOOGLETEST_INCLUDE_GTEST_GTEST_PROD_H_ // NOLINT
-#define GOOGLETEST_INCLUDE_GTEST_GTEST_PROD_H_
+#if GTEST_OS_ESP8266 || GTEST_OS_ESP32
+#if GTEST_OS_ESP8266
+extern "C" {
+#endif
+void setup() {
+  testing::InitGoogleTest();
+}
 
-// When you need to test the private or protected members of a class,
-// use the FRIEND_TEST macro to declare your tests as friends of the
-// class.  For example:
-//
-// class MyClass {
-//  private:
-//   void PrivateMethod();
-//   FRIEND_TEST(MyClassTest, PrivateMethodWorks);
-// };
-//
-// class MyClassTest : public testing::Test {
-//   // ...
-// };
-//
-// TEST_F(MyClassTest, PrivateMethodWorks) {
-//   // Can call MyClass::PrivateMethod() here.
-// }
-//
-// Note: The test class must be in the same namespace as the class being tested.
-// For example, putting MyClassTest in an anonymous namespace will not work.
+void loop() { RUN_ALL_TESTS(); }
 
-#define FRIEND_TEST(test_case_name, test_name)                                 \
-  friend class test_case_name##_##test_name##_Test
+#if GTEST_OS_ESP8266
+}
+#endif
 
-#endif  // GOOGLETEST_INCLUDE_GTEST_GTEST_PROD_H_ // NOLINT
+#else
+
+GTEST_API_ int main(int argc, char **argv) {
+  printf("Running main() from %s\n", __FILE__);
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+#endif
