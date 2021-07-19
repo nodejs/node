7789ff46c6c6b379b19bc122ec0a1692dc9f2b9f
himself65 <himself65@mask.io>
2021-07-19 15:51:29 +0800

    fixup!: cpp lint fix

--- src/crypto/crypto_keys.cc
+++ src/crypto/crypto_keys.cc
@@ -62,8 +62,10 @@ void GetKeyFormatAndTypeFromJs(
           args[*offset + 1].As<Int32>()->Value()));
     } else {
       CHECK(
-          (context == kKeyContextInput && config->format_ == kKeyFormatPEM) ||
-            (context == kKeyContextGenerate && config->format_ == kKeyFormatJWK));
+          (context == kKeyContextInput &&
+           config->format_ == kKeyFormatPEM) ||
+          (context == kKeyContextGenerate &&
+           config->format_ == kKeyFormatJWK));
       CHECK(args[*offset + 1]->IsNullOrUndefined());
       config->type_ = Nothing<PKEncodingType>();
     }
