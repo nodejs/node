cdaef9639e4dc2738d111587a70547f3675294b6
Robert Nagy <ronagy@icloud.com>
2021-07-27 12:48:32 +0200

    fixup

--- test/parallel/test-stream-duplex-from.js
+++ test/parallel/test-stream-duplex-from.js
@@ -2,7 +2,7 @@
 
 const common = require('../common');
 const assert = require('assert');
-const { Duplex, Readable, Writable, finished } = require('stream');
+const { Duplex, Readable, Writable } = require('stream');
 
 {
   const d = Duplex.from({
