53dd60b83af797bff39813a7e2050af705c0c047
Robert Nagy <ronagy@icloud.com>
2021-08-03 09:01:31 +0200

    stream: don't emit 'data' after 'error' or 'close'
    As per doc we should not emit further events after 'close' and
    only 'close' after 'error'.
    
    Fixes: https://github.com/nodejs/node/issues/39630

--- test/parallel/test-stream-readable-destroy.js
+++ test/parallel/test-stream-readable-destroy.js
@@ -319,3 +319,86 @@ const assert = require('assert');
   })(), /AbortError/);
   setTimeout(() => controller.abort(), 0);
 }
+
+{
+  const read = new Readable({
+    read() {
+    },
+  });
+  read.push('asd');
+
+  read.on('data', common.mustNotCall());
+  read.on('error', common.mustCall((e) => {
+    read.read();
+  }));
+  read.on('close', common.mustCall((e) => {
+    read.read();
+  }));
+  read.destroy(new Error('asd'));
+}
+
+{
+  const read = new Readable({
+    read() {
+    },
+  });
+  read.push('asd');
+
+  read.on('data', common.mustNotCall());
+  read.on('close', common.mustCall((e) => {
+    read.read();
+  }));
+  read.destroy();
+}
+
+{
+  const read = new Readable({
+    read() {
+    },
+  });
+  read.push('asd');
+
+  read.on('data', common.mustNotCall());
+  read.on('close', common.mustCall((e) => {
+    read.unshift('asd');
+  }));
+  read.destroy();
+}
+
+{
+  const read = new Readable({
+    read() {
+    },
+  });
+  read.push('asd');
+
+  read.on('data', common.mustNotCall());
+  read.destroy();
+  read.unshift('asd');
+}
+
+{
+  const read = new Readable({
+    read() {
+    },
+  });
+  read.push('asd');
+
+  read.on('data', common.mustNotCall());
+  read.on('close', common.mustCall((e) => {
+    read.push('asd');
+  }));
+  read.destroy();
+}
+
+{
+  const read = new Readable({
+    read() {
+    },
+  });
+  read.push('asd');
+
+  read.on('data', common.mustNotCall());
+  read.destroy();
+  read.push('asd');
+}
