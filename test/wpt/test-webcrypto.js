ca6f52fa5871d64ed327ac28a9ae8be5bb5e347b
Michaël Zasso <targos@protonmail.com>
2021-08-04 09:06:40 +0200

    fixup! test: pull Web Platform Tests for WebCryptoAPI

--- test/wpt/test-webcrypto.js
+++ test/wpt/test-webcrypto.js
@@ -1,6 +1,9 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
 const { WPTRunner } = require('../common/wpt');
 
 const runner = new WPTRunner('WebCryptoAPI');
