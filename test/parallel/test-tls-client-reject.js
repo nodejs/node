35b86110e45083a75d7dc8e6be5a930b262494f6
Matteo Collina <hello@matteocollina.com>
2021-08-04 18:40:00 +0200

    tls: validate "rejectUnauthorized: undefined"
    Incomplete validation of rejectUnauthorized parameter (Low)
    
    If the Node.js https API was used incorrectly and "undefined" was passed
    in for the "rejectUnauthorized" parameter, no error was returned and
    connections to servers with an expired certificate would have been
    accepted.
    
    CVE-ID: CVE-2021-22939
    Refs: https://nvd.nist.gov/vuln/detail/CVE-2021-22939
    Refs: https://hackerone.com/reports/1278254
    PR-URL: https://github.com/nodejs-private/node-private/pull/276
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Akshay K <iit.akshay@gmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>

--- test/parallel/test-tls-client-reject.js
+++ test/parallel/test-tls-client-reject.js
@@ -72,6 +72,19 @@ function rejectUnauthorized() {
   }, common.mustNotCall());
   socket.on('data', common.mustNotCall());
   socket.on('error', common.mustCall(function(err) {
+    rejectUnauthorizedUndefined();
+  }));
+  socket.end('ng');
+}
+
+function rejectUnauthorizedUndefined() {
+  console.log('reject unauthorized undefined');
+  const socket = tls.connect(server.address().port, {
+    servername: 'localhost',
+    rejectUnauthorized: undefined
+  }, common.mustNotCall());
+  socket.on('data', common.mustNotCall());
+  socket.on('error', common.mustCall(function(err) {
     authorized();
   }));
   socket.end('ng');
