f46935844311b002fd6328d4093c428b6f0a3257
Michaël Zasso <targos@protonmail.com>
2021-08-07 20:38:00 +0200

    test: update WPT abort tests
    PR-URL: https://github.com/nodejs/node/pull/39697
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

---
+++ test/fixtures/wpt/dom/abort/AbortSignal.any.js
@@ -0,0 +1,12 @@
+test(t => {
+  const signal = AbortSignal.abort();
+  assert_true(signal instanceof AbortSignal, "returned object is an AbortSignal");
+  assert_true(signal.aborted, "returned signal is already aborted");
+}, "the AbortSignal.abort() static returns an already aborted signal");
+
+async_test(t => {
+  const s = AbortSignal.abort();
+  s.addEventListener("abort", t.unreached_func("abort event listener called"));
+  s.onabort = t.unreached_func("abort event handler called");
+  t.step_timeout(() => { t.done(); }, 2000);
+}, "signal returned by AbortSignal.abort() should not fire abort event");
