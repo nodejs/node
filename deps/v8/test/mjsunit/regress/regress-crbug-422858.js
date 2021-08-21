a01e3ab41d89dd7c59dd8f01cfeb34349a1ec7a3
Darshan Sen <darshan.sen@postman.com>
2021-08-21 16:37:48 +0530

    deps: V8: cherry-pick 00bb1a77c03e
    Original commit message:
    
        [date] Fix Date#getMinutes() test failures
    
        After building V8 using Clang (./out/x64.release/v8_build_config.json
        says that "is_clang" is true), I could reproduce the referenced bug
        report locally. Replacing the getMinutes() calls with getUTCMinutes()
        calls fixed the test failure.
    
        Signed-off-by: Darshan Sen <raisinten@gmail.com>
        Bug: v8:11200
        Change-Id: Ia36be481f2c8728380d550ead856ef8e51b1069c
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3093362
        Reviewed-by: Toon Verwaest <verwaest@chromium.org>
        Commit-Queue: Toon Verwaest <verwaest@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#76367}
    
    Refs: https://github.com/v8/v8/commit/00bb1a77c03ec951a5def21c64bee38cd855be7b
    Signed-off-by: Darshan Sen <darshan.sen@postman.com>
    
    PR-URL: https://github.com/nodejs/node/pull/39829
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- deps/v8/test/mjsunit/regress/regress-crbug-422858.js
+++ deps/v8/test/mjsunit/regress/regress-crbug-422858.js
@@ -3,21 +3,21 @@
 // found in the LICENSE file.
 
 var date = new Date("2016/01/02 10:00 GMT-8")
-assertEquals(0, date.getMinutes());
+assertEquals(0, date.getUTCMinutes());
 assertEquals(18, date.getUTCHours());
 
 date = new Date("2016/01/02 10:00 GMT-12")
-assertEquals(0, date.getMinutes());
+assertEquals(0, date.getUTCMinutes());
 assertEquals(22, date.getUTCHours());
 
 date = new Date("2016/01/02 10:00 GMT-123")
-assertEquals(23, date.getMinutes());
+assertEquals(23, date.getUTCMinutes());
 assertEquals(11, date.getUTCHours());
 
 date = new Date("2016/01/02 10:00 GMT-0856")
-assertEquals(56, date.getMinutes());
+assertEquals(56, date.getUTCMinutes());
 assertEquals(18, date.getUTCHours());
 
 date = new Date("2016/01/02 10:00 GMT-08000")
-assertEquals(NaN, date.getMinutes());
+assertEquals(NaN, date.getUTCMinutes());
 assertEquals(NaN, date.getUTCHours());
