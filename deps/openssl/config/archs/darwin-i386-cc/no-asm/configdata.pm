229b7d219d33d41012ccfb8a0a0b22f60f8e63f1
Richard Lau <rlau@redhat.com>
2021-08-24 11:30:13 -0400

    deps: update archs files for OpenSSL-1.1.1l+quic
    After an OpenSSL source update, all the config files need to be
    regenerated and committed by:
        $ make -C deps/openssl/config
        $ git add deps/openssl/config/archs
        $ git add deps/openssl/openssl/include/crypto/bn_conf.h
        $ git add deps/openssl/openssl/include/crypto/dso_conf.h
        $ git add deps/openssl/openssl/include/openssl/opensslconf.h
        $ git commit
    
    PR-URL: https://github.com/nodejs/node/pull/39867
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Beth Griggs <bgriggs@redhat.com>

--- deps/openssl/config/archs/darwin-i386-cc/no-asm/configdata.pm
+++ deps/openssl/config/archs/darwin-i386-cc/no-asm/configdata.pm
@@ -110,8 +110,8 @@ our %config = (
   sourcedir => ".",
   target => "darwin-i386-cc",
   tdirs => [ "ossl_shim" ],
-  version => "1.1.1k+quic",
-  version_num => "0x101010bfL",
+  version => "1.1.1l+quic",
+  version_num => "0x101010cfL",
 );
 
 our %target = (
