65986c3b407d7b190a5027159edaf76782ed9963
Michaël Zasso <targos@protonmail.com>
2021-08-29 08:47:12 +0200

    test: adapt test-repl to V8 9.5
    Syntax errors for RegExp literals now show hint to error location.
    
    PR-URL: https://github.com/nodejs/node/pull/40178
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-repl.js
+++ test/parallel/test-repl.js
@@ -86,7 +86,7 @@ async function runReplTests(socket, prompt, tests) {
 
       console.error('in:', JSON.stringify(actualLine));
 
-      // Match a string directly, or a RegExp through .test().
+      // Match a string directly, or a RegExp.
       if (typeof expectedLine === 'string') {
         assert.strictEqual(actualLine, expectedLine);
       } else {
@@ -226,7 +226,12 @@ const errorTests = [
   // should throw
   {
     send: '/(/;',
-    expect: [/^Uncaught SyntaxError: /]
+    expect: [
+      kSource,
+      kArrow,
+      '',
+      /^Uncaught SyntaxError: /,
+    ]
   },
   // invalid RegExp modifiers are a special case of syntax error,
   // should throw (GH-4012)
