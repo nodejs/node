a2c1c3ef64d20a992fc4ce9cefb02474d4bd4ea0
Joyee Cheung <joyeec9h3@gmail.com>
2021-08-31 23:17:06 +0800

    src: register external references of BaseObject for snapshot
    PR-URL: https://github.com/nodejs/node/pull/39961
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- src/node_external_reference.cc
+++ src/node_external_reference.cc
@@ -1,6 +1,7 @@
 #include "node_external_reference.h"
 #include <cinttypes>
 #include <vector>
+#include "base_object-inl.h"
 #include "util.h"
 
 namespace node {
@@ -13,6 +14,8 @@ const std::vector<intptr_t>& ExternalReferenceRegistry::external_references() {
 }
 
 ExternalReferenceRegistry::ExternalReferenceRegistry() {
+  this->Register(BaseObject::LazilyInitializedJSTemplateConstructor);
+
 #define V(modname) _register_external_reference_##modname(this);
   EXTERNAL_REFERENCE_BINDING_LIST(V)
 #undef V
