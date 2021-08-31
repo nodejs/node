d225f89330d1167c8616c57458f5b3c7d07d2ece
Anna Henningsen <anna@addaleax.net>
2021-08-31 12:43:45 +0200

    Revert "src: skip test_fatal/test_threads for Debug builds"
    This reverts commit 1fc4d43a3255273709f5fcb51bb0c5b0407de4ac.
    
    PR-URL: https://github.com/nodejs/node/pull/39954
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/node-api/test_fatal/test_threads.js
+++ test/node-api/test_fatal/test_threads.js
@@ -4,10 +4,6 @@ const assert = require('assert');
 const child_process = require('child_process');
 const test_fatal = require(`./build/${common.buildType}/test_fatal`);
 
-if (common.buildType === 'Debug')
-  common.skip('as this will currently fail with a Debug check ' +
-              'in v8::Isolate::GetCurrent()');
-
 // Test in a child process because the test code will trigger a fatal error
 // that crashes the process.
 if (process.argv[2] === 'child') {
