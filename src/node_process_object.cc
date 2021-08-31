f01ae6f349d9b8074665316d7fd2feddc6d8bed5
Joyee Cheung <joyeec9h3@gmail.com>
2021-08-31 23:18:59 +0800

    src: register missing process methods external references
    PR-URL: https://github.com/nodejs/node/pull/39961
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- src/node_process_object.cc
+++ src/node_process_object.cc
@@ -211,6 +211,11 @@ void PatchProcessObject(const FunctionCallbackInfo<Value>& args) {
 
 void RegisterProcessExternalReferences(ExternalReferenceRegistry* registry) {
   registry->Register(RawDebug);
+  registry->Register(GetParentProcessId);
+  registry->Register(DebugPortSetter);
+  registry->Register(DebugPortGetter);
+  registry->Register(ProcessTitleSetter);
+  registry->Register(ProcessTitleGetter);
 }
 
 }  // namespace node
