130a233df960984f8696902e61267068ebba2be9
Michaël Zasso <targos@protonmail.com>
2021-09-08 21:49:55 +0200

    tools: update doc generator dependencies

---
+++ tools/doc/deps.mjs
@@ -0,0 +1,7 @@
+// Re-exporting dependencies for tests in test/doctool.
+
+export { default as rehypeRaw } from 'rehype-raw';
+export { default as rehypeStringify } from 'rehype-stringify';
+export { default as remarkParse } from 'remark-parse';
+export { default as remarkRehype } from 'remark-rehype';
+export { unified } from 'unified';
