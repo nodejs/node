3f6ef4ecab5d235647159e79c8cc9585437058e8
Jiawen Geng <technicalcute@gmail.com>
2021-09-10 04:21:11 +0000

    deps: patch v8 for vs2019 in std17
    PR-URL: https://github.com/nodejs/node/pull/40060
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- deps/v8/src/objects/fixed-array-inl.h
+++ deps/v8/src/objects/fixed-array-inl.h
@@ -84,7 +84,7 @@ bool FixedArray::is_the_hole(Isolate* isolate, int index) {
   return get(isolate, index).IsTheHole(isolate);
 }
 
-#if !defined(_WIN32) || (defined(_WIN64) && _MSC_VER < 1930)
+#if !defined(_WIN32) || (defined(_WIN64) && _MSC_VER < 1930 && __cplusplus < 201703L)
 void FixedArray::set(int index, Smi value) {
   DCHECK_NE(map(), GetReadOnlyRoots().fixed_cow_array_map());
   DCHECK_LT(static_cast<unsigned>(index), static_cast<unsigned>(length()));
