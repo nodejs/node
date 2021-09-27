795db39025ef6e60b4bef34d4259a23ac234c056
Michaël Zasso <targos@protonmail.com>
2021-09-27 16:50:49 +0200

    fixup! typings: define types for os binding

--- typings/internalBinding/os.d.ts
+++ typings/internalBinding/os.d.ts
@@ -1,21 +1,21 @@
 declare function InternalBinding(binding: 'os'): {
-  getHostname(ctx: object): string | undefined;
+  getHostname(ctx: {}): string | undefined;
   getLoadAvg(array: Float64Array): void;
   getUptime(): number;
   getTotalMem(): number;
   getFreeMem(): number;
   getCPUs(): Array<string | number>;
-  getInterfaceAddresses(ctx: object): Array<string | number | boolean> | undefined;
-  getHomeDirectory(ctx: object): string | undefined;
-  getUserInfo(options: {encoding?: string} | undefined, ctx: object): {
+  getInterfaceAddresses(ctx: {}): Array<string | number | boolean> | undefined;
+  getHomeDirectory(ctx: {}): string | undefined;
+  getUserInfo(options: { encoding?: string } | undefined, ctx: {}): {
     uid: number;
     gid: number;
     username: string;
     homedir: string;
     shell: string | null;
   } | undefined;
-  setPriority(pid: number, priority: number, ctx: object): number;
-  getPriority(pid: number, ctx: object): number | undefined;
-  getOSInformation(ctx: object): [sysname: string, version: string, release: string];
+  setPriority(pid: number, priority: number, ctx: {}): number;
+  getPriority(pid: number, ctx: {}): number | undefined;
+  getOSInformation(ctx: {}): [sysname: string, version: string, release: string];
   isBigEndian: boolean;
 };
