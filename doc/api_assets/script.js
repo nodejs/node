13863e63f88b615485e11902a8aebae0a54512fe
Sanchit verma <sanchitverma78@gmail.com>
2021-09-28 18:05:38 +0000

    linting

--- doc/api_assets/script.js
+++ doc/api_assets/script.js
@@ -1,57 +1,59 @@
-let eqIsh = (a, b, fuzz = 2) => {
-    return (Math.abs(a - b) <= fuzz);
-  };
+'use strict';
 
-  let rectNotEQ = (a, b) => {
-    return (!eqIsh(a.width, b.width) ||
+const eqIsh = (a, b, fuzz = 2) => {
+  return (Math.abs(a - b) <= fuzz);
+};
+
+const rectNotEQ = (a, b) => {
+  return (!eqIsh(a.width, b.width) ||
       !eqIsh(a.height, b.height));
-  };
+};
 
-  let spaced = new WeakMap();
+const spaced = new WeakMap();
 
-  let reserveSpace = (el, rect = el.getClientBoundingRect()) => {
-    let old = spaced.get(el);
-    if (!old || rectNotEQ(old, rect)) {
-      spaced.set(el, rect);
-      el.attributeStyleMap.set(
-        "contain-intrinsic-size",
-        `${rect.width}px ${rect.height}px`
-      );
-    }
-  };
+const reserveSpace = (el, rect = el.getClientBoundingRect()) => {
+  const old = spaced.get(el);
+  if (!old || rectNotEQ(old, rect)) {
+    spaced.set(el, rect);
+    el.attributeStyleMap.set(
+      'contain-intrinsic-size',
+      `${rect.width}px ${rect.height}px`
+    );
+  }
+};
 
-  let iObs = new IntersectionObserver(
-    (entries, o) => {
-      entries.forEach((entry) => {
+const iObs = new IntersectionObserver(
+  (entries, o) => {
+    entries.forEach((entry) => {
 
-        reserveSpace(entry.target,
-          entry.boundingClientRect);
-      });
-    },
-    { rootMargin: "500px 0px 500px 0px" }
-  );
-
-  let rObs = new ResizeObserver(
-    (entries, o) => {
-      entries.forEach((entry) => {
-        reserveSpace(entry.target, entry.contentRect);
-      });
-    }
-  );
-
-  let articles =
-    document.querySelectorAll("#apicontent>section");
-
-  articles.forEach((el) => {
-    iObs.observe(el);
-    rObs.observe(el);
-  });
-
-  requestAnimationFrame(() => {
-    requestAnimationFrame(() => {
-      articles[0].attributeStyleMap.set(
-        "content-visibility",
-        "auto"
-      );
+      reserveSpace(entry.target,
+                   entry.boundingClientRect);
     });
+  },
+  { rootMargin: '500px 0px 500px 0px' }
+);
+
+const rObs = new ResizeObserver(
+  (entries, o) => {
+    entries.forEach((entry) => {
+      reserveSpace(entry.target, entry.contentRect);
+    });
+  }
+);
+
+const articles =
+    document.querySelectorAll('#apicontent>section');
+
+articles.forEach((el) => {
+  iObs.observe(el);
+  rObs.observe(el);
+});
+
+requestAnimationFrame(() => {
+  requestAnimationFrame(() => {
+    articles[0].attributeStyleMap.set(
+      'content-visibility',
+      'auto'
+    );
   });
+});
