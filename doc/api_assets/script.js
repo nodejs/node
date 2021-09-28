1678e16268ca2661fa655764d60094db7755fe2c
Sanchit verma <sanchitverma78@gmail.com>
2021-09-28 19:09:10 +0000

    lint

--- doc/api_assets/script.js
+++ doc/api_assets/script.js
@@ -2,60 +2,60 @@
 
 {
   const eqIsh = (a, b, fuzz = 2) => {
-  return (Math.abs(a - b) <= fuzz);
-};
+    return (Math.abs(a - b) <= fuzz);
+  };
 
-const rectNotEQ = (a, b) => {
-  return (!eqIsh(a.width, b.width) ||
+  const rectNotEQ = (a, b) => {
+    return (!eqIsh(a.width, b.width) ||
       !eqIsh(a.height, b.height));
-};
+  };
 
-const spaced = new WeakMap();
+  const spaced = new WeakMap();
 
-const reserveSpace = (el, rect = el.getClientBoundingRect()) => {
-  const old = spaced.get(el);
-  if (!old || rectNotEQ(old, rect)) {
-    spaced.set(el, rect);
-    el.attributeStyleMap.set(
-      'contain-intrinsic-size',
-      `${rect.width}px ${rect.height}px`
-    );
-  }
-};
+  const reserveSpace = (el, rect = el.getClientBoundingRect()) => {
+    const old = spaced.get(el);
+    if (!old || rectNotEQ(old, rect)) {
+      spaced.set(el, rect);
+      el.attributeStyleMap.set(
+        'contain-intrinsic-size',
+        `${rect.width}px ${rect.height}px`
+      );
+    }
+  };
 
-const iObs = new IntersectionObserver(
-  (entries, o) => {
-    entries.forEach((entry) => {
+  const iObs = new IntersectionObserver(
+    (entries, o) => {
+      entries.forEach((entry) => {
 
-      reserveSpace(entry.target,
-                   entry.boundingClientRect);
-    });
-  },
-  { rootMargin: '500px 0px 500px 0px' }
-);
+        reserveSpace(entry.target,
+                     entry.boundingClientRect);
+      });
+    },
+    { rootMargin: '500px 0px 500px 0px' }
+  );
 
-const rObs = new ResizeObserver(
-  (entries, o) => {
-    entries.forEach((entry) => {
-      reserveSpace(entry.target, entry.contentRect);
-    });
-  }
-);
+  const rObs = new ResizeObserver(
+    (entries, o) => {
+      entries.forEach((entry) => {
+        reserveSpace(entry.target, entry.contentRect);
+      });
+    }
+  );
 
-const articles =
+  const articles =
     document.querySelectorAll('#apicontent>section');
 
-articles.forEach((el) => {
-  iObs.observe(el);
-  rObs.observe(el);
-});
-
-requestAnimationFrame(() => {
-  requestAnimationFrame(() => {
-    articles[0].attributeStyleMap.set(
-      'content-visibility',
-      'auto'
-    );
+  articles.forEach((el) => {
+    iObs.observe(el);
+    rObs.observe(el);
   });
-});
+
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      articles[0].attributeStyleMap.set(
+        'content-visibility',
+        'auto'
+      );
+    });
+  });
 }
