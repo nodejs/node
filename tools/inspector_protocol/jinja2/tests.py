1007a6e87f0f6fe03aaa7483de5c54d4185b4bd8
Michaël Zasso <targos@protonmail.com>
2021-10-05 13:34:39 +0200

    tools: patch jinja2 for Python 3.10 compat
    PR-URL: https://github.com/nodejs/node/pull/40296
    Fixes: https://github.com/nodejs/node/issues/40294
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Christian Clauss <cclauss@me.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- tools/inspector_protocol/jinja2/tests.py
+++ tools/inspector_protocol/jinja2/tests.py
@@ -10,7 +10,10 @@
 """
 import operator
 import re
-from collections import Mapping
+try:
+  from collections.abc import Mapping
+except ImportError:
+  from collections import Mapping
 from jinja2.runtime import Undefined
 from jinja2._compat import text_type, string_types, integer_types
 import decimal
