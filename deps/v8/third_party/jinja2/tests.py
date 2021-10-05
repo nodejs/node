947f3dc9af98bb1d654fb08da6363b914331cca0
Michaël Zasso <targos@protonmail.com>
2021-10-05 13:33:50 +0200

    deps: V8: patch jinja2 for Python 3.10 compat
    PR-URL: https://github.com/nodejs/node/pull/40296
    Fixes: https://github.com/nodejs/node/issues/40294
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Christian Clauss <cclauss@me.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- deps/v8/third_party/jinja2/tests.py
+++ deps/v8/third_party/jinja2/tests.py
@@ -10,7 +10,7 @@
 """
 import operator
 import re
-from collections import Mapping
+from collections.abc import Mapping
 from jinja2.runtime import Undefined
 from jinja2._compat import text_type, string_types, integer_types
 import decimal
