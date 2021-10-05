881dd7ba2afb3c4dd70a484cb4969101c00a26df
Francesco Trotta <github@fasttime.org>
2021-10-05 07:42:08 +0200

    lib: fix regular expression to detect `/` and `\`
    Fixes #40305
    
    PR-URL: https://github.com/nodejs/node/pull/40325
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Zeyu Yang <himself65@outlook.com>

--- test/es-module/test-esm-pkgname.mjs
+++ test/es-module/test-esm-pkgname.mjs
@@ -7,6 +7,10 @@ importFixture('as%2Ff').catch(mustCall((err) => {
   strictEqual(err.code, 'ERR_INVALID_MODULE_SPECIFIER');
 }));
 
+importFixture('as%5Cf').catch(mustCall((err) => {
+  strictEqual(err.code, 'ERR_INVALID_MODULE_SPECIFIER');
+}));
+
 importFixture('as\\df').catch(mustCall((err) => {
   strictEqual(err.code, 'ERR_INVALID_MODULE_SPECIFIER');
 }));
