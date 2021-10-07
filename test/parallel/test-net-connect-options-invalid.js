b9c41d378aea0df2470e1c425dfa06aee27525cb
Daijiro Wachi <daijiro.wachi@gmail.com>
2021-10-07 18:49:07 +0900

    test: replace common port with specific number

--- test/parallel/test-net-connect-options-invalid.js
+++ test/parallel/test-net-connect-options-invalid.js
@@ -1,5 +1,5 @@
 'use strict';
-const common = require('../common');
+require('../common');
 const assert = require('assert');
 const net = require('net');
 
@@ -11,7 +11,7 @@ const net = require('net');
   ];
   invalidKeys.forEach((invalidKey) => {
     const option = {
-      ...common.localhostIPv4,
+      port: 8080,
       [invalidKey]: true
     };
     const message = `The property 'options.${invalidKey}' is not supported. Received true`;
