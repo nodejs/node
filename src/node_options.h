857af2ba99031511ee3970ad585260bf08bbfee6
Michael Dawson <mdawson@devrus.com>
2021-10-07 21:02:49 -0400

    src: add missing initialization
    This missing initialization was reported by the coverity scans
    we are in the process of re-enabling.
    
    PR-URL: https://github.com/nodejs/node/pull/40370
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>

--- src/node_options.h
+++ src/node_options.h
@@ -111,7 +111,7 @@ class EnvironmentOptions : public Options {
   std::string module_type;
   std::string experimental_policy;
   std::string experimental_policy_integrity;
-  bool has_policy_integrity_string;
+  bool has_policy_integrity_string = false;
   bool experimental_repl_await = true;
   bool experimental_vm_modules = false;
   bool expose_internals = false;
