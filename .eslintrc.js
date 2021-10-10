11294779e8dbd912920e233639c60581eabbf1fc
Rich Trott <rtrott@gmail.com>
2021-10-09 19:08:38 -0700

    tools: remove @babel/plugin-syntax-top-level-await
    ESLint 8.0.0 introduced support for top level await, so
    @babel/plugin-syntax-top-level-await is no longer necessary.
    
    PR-URL: https://github.com/nodejs/node/pull/40394
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ const hacks = [
   '@babel/eslint-parser',
   '@babel/plugin-syntax-class-properties',
   '@babel/plugin-syntax-import-assertions',
-  '@babel/plugin-syntax-top-level-await',
 ];
 Module._findPath = (request, paths, isMain) => {
   const r = ModuleFindPath(request, paths, isMain);
@@ -43,7 +42,6 @@ module.exports = {
       plugins: [
         Module._findPath('@babel/plugin-syntax-class-properties'),
         Module._findPath('@babel/plugin-syntax-import-assertions'),
-        Module._findPath('@babel/plugin-syntax-top-level-await'),
       ],
     },
     requireConfigFile: false,
