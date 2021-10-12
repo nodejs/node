d4b45cc24920ff036f9f299e5df7b9719fb77d7a
Darshan Sen <darshan.sen@postman.com>
2021-10-12 15:32:03 +0530

    src,stream: remove `*Check*()` calls from non-`Initialize()` functions
    There is no need to crash the process if any of these checks fail.
    
    Signed-off-by: Darshan Sen <darshan.sen@postman.com>
    
    PR-URL: https://github.com/nodejs/node/pull/40425
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/stream_pipe.h
+++ src/stream_pipe.h
@@ -9,11 +9,14 @@ namespace node {
 
 class StreamPipe : public AsyncWrap {
  public:
-  StreamPipe(StreamBase* source, StreamBase* sink, v8::Local<v8::Object> obj);
   ~StreamPipe() override;
 
   void Unpipe(bool is_in_deletion = false);
 
+  // TODO(RaisinTen): Just like MessagePort, add the following overload:
+  // static StreamPipe* New(StreamBase* source, StreamBase* sink,
+  //                        v8::Local<v8::Object> obj);
+  // so that we can indicate if there is a pending exception/termination.
   static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Start(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Unpipe(const v8::FunctionCallbackInfo<v8::Value>& args);
@@ -25,6 +28,8 @@ class StreamPipe : public AsyncWrap {
   SET_SELF_SIZE(StreamPipe)
 
  private:
+  StreamPipe(StreamBase* source, StreamBase* sink, v8::Local<v8::Object> obj);
+
   inline StreamBase* source();
   inline StreamBase* sink();
 
