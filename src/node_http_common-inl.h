dc146f909dab6967614cca4d31ef250b1da5ee68
Michael Dawson <mdawson@devrus.com>
2021-10-25 10:53:19 -0400

    fixup: remove unecessary move
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- src/node_http_common-inl.h
+++ src/node_http_common-inl.h
@@ -131,7 +131,7 @@ NgHeader<T>::NgHeader(
 
 template <typename T>
 NgHeader<T>::NgHeader(NgHeader<T>&& other) noexcept
-    : env_(std::move(other.env_)),
+    : env_(other.env_),
       name_(std::move(other.name_)),
       value_(std::move(other.value_)),
       token_(other.token_),
