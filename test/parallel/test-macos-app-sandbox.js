35ccafe627e9c8423f71d789edce67ddae2bc737
Michaël Zasso <targos@protonmail.com>
2021-10-26 13:03:16 +0200

    test: skip macos sandbox test with builtin modules path
    Refs: https://github.com/nodejs/node/issues/40605
    
    PR-URL: https://github.com/nodejs/node/pull/40607
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/parallel/test-macos-app-sandbox.js
+++ test/parallel/test-macos-app-sandbox.js
@@ -2,6 +2,8 @@
 const common = require('../common');
 if (process.platform !== 'darwin')
   common.skip('App Sandbox is only available on Darwin');
+if (process.config.variables.node_builtin_modules_path)
+  common.skip('App Sandbox cannot load modules from outside the sandbox');
 
 const fixtures = require('../common/fixtures');
 const tmpdir = require('../common/tmpdir');
