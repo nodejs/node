a901b6c53c6bcba609726a2358888fdf11dbe283
Richard Lau <rlau@redhat.com>
2021-10-29 12:04:24 -0400

    deps: update c-ares to 1.18.1
    Updated as described in doc/guides/maintaining-c-ares.md.
    
    PR-URL: https://github.com/nodejs/node/pull/40660
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- deps/cares/src/lib/ares_parse_aaaa_reply.c
+++ deps/cares/src/lib/ares_parse_aaaa_reply.c
@@ -48,165 +48,45 @@ int ares_parse_aaaa_reply(const unsigned char *abuf, int alen,
                           int *naddrttls)
 {
   struct ares_addrinfo ai;
-  struct ares_addrinfo_node *next;
-  struct ares_addrinfo_cname *next_cname;
-  char **aliases = NULL;
   char *question_hostname = NULL;
-  struct hostent *hostent = NULL;
-  struct ares_in6_addr *addrs = NULL;
-  int naliases = 0, naddrs = 0, alias = 0, i;
-  int cname_ttl = INT_MAX;
   int status;
-
-  memset(&ai, 0, sizeof(ai));
-
-  status = ares__parse_into_addrinfo2(abuf, alen, &question_hostname, &ai);
-  if (status != ARES_SUCCESS)
-    {
-      ares_free(question_hostname);
-
-      if (naddrttls)
-        {
-          *naddrttls = 0;
-        }
-
-      return status;
-    }
-
-  hostent = ares_malloc(sizeof(struct hostent));
-  if (!hostent)
-    {
-      goto enomem;
-    }
-
-  next = ai.nodes;
-  while (next)
-    {
-      if(next->ai_family == AF_INET6)
-        {
-          ++naddrs;
-        }
-      next = next->ai_next;
-    }
-
-  next_cname = ai.cnames;
-  while (next_cname)
-    {
-      if(next_cname->alias)
-        ++naliases;
-      next_cname = next_cname->next;
-    }
-
-  aliases = ares_malloc((naliases + 1) * sizeof(char *));
-  if (!aliases)
-    {
-      goto enomem;
-    }
-
-  if (naliases)
-    {
-      next_cname = ai.cnames;
-      while (next_cname)
-        {
-          if(next_cname->alias)
-            aliases[alias++] = ares_strdup(next_cname->alias);
-          if(next_cname->ttl < cname_ttl)
-            cname_ttl = next_cname->ttl;
-          next_cname = next_cname->next;
-        }
-    }
-
-  aliases[alias] = NULL;
-
-  hostent->h_addr_list = ares_malloc((naddrs + 1) * sizeof(char *));
-  if (!hostent->h_addr_list)
-    {
-      goto enomem;
-    }
-
-  for (i = 0; i < naddrs + 1; ++i)
-    {
-      hostent->h_addr_list[i] = NULL;
-    }
-
-  if (ai.cnames)
-    {
-      hostent->h_name = ares_strdup(ai.cnames->name);
-      ares_free(question_hostname);
-    }
-  else
-    {
-      hostent->h_name = question_hostname;
-    }
-
-  hostent->h_aliases = aliases;
-  hostent->h_addrtype = AF_INET6;
-  hostent->h_length = sizeof(struct ares_in6_addr);
-
-  if (naddrs)
-    {
-      addrs = ares_malloc(naddrs * sizeof(struct ares_in6_addr));
-      if (!addrs)
-        {
-          goto enomem;
-        }
-
-      i = 0;
-      next = ai.nodes;
-      while (next)
-        {
-          if(next->ai_family == AF_INET6)
-            {
-              hostent->h_addr_list[i] = (char*)&addrs[i];
-              memcpy(hostent->h_addr_list[i],
-                     &(CARES_INADDR_CAST(struct sockaddr_in6 *, next->ai_addr)->sin6_addr),
-                     sizeof(struct ares_in6_addr));
-              if (naddrttls && i < *naddrttls)
-                {
-                    if(next->ai_ttl > cname_ttl)
-                      addrttls[i].ttl = cname_ttl;
-                    else
-                      addrttls[i].ttl = next->ai_ttl;
-
-                    memcpy(&addrttls[i].ip6addr,
-                           &(CARES_INADDR_CAST(struct sockaddr_in6 *, next->ai_addr)->sin6_addr),
-                           sizeof(struct ares_in6_addr));
-                }
-              ++i;
-            }
-          next = next->ai_next;
-        }
-
-      if (i == 0)
-        {
-          ares_free(addrs);
-        }
-    }
-
-  if (host)
-    {
-      *host = hostent;
-    }
-  else
-    {
-      ares_free_hostent(hostent);
-    }
+  int req_naddrttls = 0;
 
   if (naddrttls)
     {
-      /* Truncated to at most *naddrttls entries */
-      *naddrttls = (naddrs > *naddrttls)?*naddrttls:naddrs;
+      req_naddrttls = *naddrttls;
+      *naddrttls = 0;
     }
 
-  ares__freeaddrinfo_cnames(ai.cnames);
-  ares__freeaddrinfo_nodes(ai.nodes);
-  return ARES_SUCCESS;
+  memset(&ai, 0, sizeof(ai));
 
-enomem:
-  ares_free(aliases);
-  ares_free(hostent);
+  status = ares__parse_into_addrinfo(abuf, alen, 0, 0, &ai);
+  if (status != ARES_SUCCESS && status != ARES_ENODATA)
+    {
+      goto fail;
+    }
+
+  if (host != NULL)
+    {
+      status = ares__addrinfo2hostent(&ai, AF_INET6, host);
+      if (status != ARES_SUCCESS && status != ARES_ENODATA)
+        {
+          goto fail;
+        }
+    }
+
+  if (addrttls != NULL && req_naddrttls)
+   {
+     ares__addrinfo2addrttl(&ai, AF_INET6, req_naddrttls, NULL,
+                            addrttls, naddrttls);
+   }
+
+fail:
   ares__freeaddrinfo_cnames(ai.cnames);
   ares__freeaddrinfo_nodes(ai.nodes);
   ares_free(question_hostname);
-  return ARES_ENOMEM;
+  ares_free(ai.name);
+
+  return status;
 }
+
