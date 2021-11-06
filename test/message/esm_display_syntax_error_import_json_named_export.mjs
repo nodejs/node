e58cce49fd0a26646d648c283afe67adabc9b60d
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-11-06 10:06:29 +0100

    test: disable warnings to fix flaky test
    Fixes: https://github.com/nodejs/node/issues/40738
    
    PR-URL: https://github.com/nodejs/node/pull/40739
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/message/esm_display_syntax_error_import_json_named_export.mjs
+++ test/message/esm_display_syntax_error_import_json_named_export.mjs
@@ -1,4 +1,4 @@
-// Flags: --experimental-json-modules
+// Flags: --experimental-json-modules --no-warnings
 /* eslint-disable no-unused-vars */
 import '../common/index.mjs';
 import { ofLife } from '../fixtures/experimental.json' assert { type: 'json' };
