7c4f9a34ae9b1cc670a37c830bdea870707891d3
Robert Nagy <ronagy@icloud.com>
2021-11-10 10:14:48 +0200

    stream: allow calling callback before promise
    Refs: https://github.com/nodejs/node/issues/39535
    
    PR-URL: https://github.com/nodejs/node/pull/40772
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-stream-construct-async-error.js
+++ test/parallel/test-stream-construct-async-error.js
@@ -11,26 +11,6 @@ const assert = require('assert');
 
 {
   class Foo extends Duplex {
-    async _construct(cb) {
-      // eslint-disable-next-line no-restricted-syntax
-      await setTimeout(common.platformTimeout(1));
-      cb();
-      throw new Error('boom');
-    }
-  }
-
-  const foo = new Foo();
-  foo.on('error', common.expectsError({
-    message: 'boom'
-  }));
-  foo.on('close', common.mustCall(() => {
-    assert(foo._writableState.constructed);
-    assert(foo._readableState.constructed);
-  }));
-}
-
-{
-  class Foo extends Duplex {
     async _destroy(err, cb) {
       // eslint-disable-next-line no-restricted-syntax
       await setTimeout(common.platformTimeout(1));
@@ -98,9 +78,7 @@ const assert = require('assert');
 
   const foo = new Foo();
   foo.write('test', common.mustCall());
-  foo.on('error', common.expectsError({
-    code: 'ERR_MULTIPLE_CALLBACK'
-  }));
+  foo.on('error', common.mustNotCall());
 }
 
 {
