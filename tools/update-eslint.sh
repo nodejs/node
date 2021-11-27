1de6a0e63d8811abe8b958f7dfac9acda03875ce
Rich Trott <rtrott@gmail.com>
2021-11-27 08:20:13 -0800

    tools: update ESLint update script to consolidate dependencies
    PR-URL: https://github.com/nodejs/node/pull/40995
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- tools/update-eslint.sh
+++ tools/update-eslint.sh
@@ -5,23 +5,24 @@
 # This script must be in the tools directory when it runs because it uses the
 # script source file path to determine directories to work in.
 
-set -e
+set -ex
 
 cd "$( dirname "$0" )" || exit
-rm -rf node_modules/eslint node_modules/eslint-plugin-markdown
+rm -rf node_modules/eslint
 (
+    rm -rf eslint-tmp
     mkdir eslint-tmp
     cd eslint-tmp || exit
 
     ROOT="$PWD/../.."
     [ -z "$NODE" ] && NODE="$ROOT/out/Release/node"
-    [ -x "$NODE" ] || NODE=`command -v node`
+    [ -x "$NODE" ] || NODE=$(command -v node)
     NPM="$ROOT/deps/npm/bin/npm-cli.js"
 
     "$NODE" "$NPM" init --yes
 
-    "$NODE" "$NPM" install --global-style --no-bin-links --ignore-scripts --no-package-lock eslint eslint-plugin-markdown
-
+    "$NODE" "$NPM" install --global-style --no-bin-links --ignore-scripts eslint
+    (cd node_modules/eslint && "$NODE" "$NPM" install --no-bin-links --ignore-scripts --production --omit=peer eslint-plugin-markdown @babel/core @babel/eslint-parser @babel/plugin-syntax-import-assertions)
     # Use dmn to remove some unneeded files.
     "$NODE" "$NPM" exec -- dmn@2.2.2 -f clean
     # Use removeNPMAbsolutePaths to remove unused data in package.json.
@@ -30,5 +31,4 @@ rm -rf node_modules/eslint node_modules/eslint-plugin-markdown
 )
 
 mv eslint-tmp/node_modules/eslint node_modules/eslint
-mv eslint-tmp/node_modules/eslint-plugin-markdown node_modules/eslint-plugin-markdown
 rm -rf eslint-tmp/
