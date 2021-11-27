5810c23b980b7848449b9c16088790f5ba833bb0
Rich Trott <rtrott@gmail.com>
2021-11-27 09:36:35 -0800

    tools: consolidate ESLint dependencies
    PR-URL: https://github.com/nodejs/node/pull/40995
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- tools/node_modules/@babel/core/src/transformation/util/clone-deep.ts
+++
@@ -1,9 +0,0 @@
-import v8 from "v8";
-import cloneDeep from "./clone-deep-browser";
-
-export default function (value) {
-  if (v8.deserialize && v8.serialize) {
-    return v8.deserialize(v8.serialize(value));
-  }
-  return cloneDeep(value);
-}
