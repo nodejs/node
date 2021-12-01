ab28941a24f14ff0f173c1b5da6a47eac7440b9b
James M Snell <jasnell@gmail.com>
2021-12-01 07:00:16 -0800

    errors: make AbortError structured cloneable
    Refs: https://github.com/nodejs/node/issues/41038
    Signed-off-by: James M Snell <jasnell@gmail.com>

--- test/parallel/test-errors-cloneabledomexception.js
+++ test/parallel/test-errors-cloneabledomexception.js
@@ -1,8 +1,14 @@
+// Flags: --expose-internals
 'use strict';
 
 const common = require('../common');
 
-const { strictEqual, ok } = require('assert');
+const {
+  strictEqual,
+  notStrictEqual,
+  ok,
+} = require('assert');
+const { AbortError } = require('internal/errors');
 
 const exception = new DOMException('foo', 'AbortError');
 strictEqual(exception.name, 'AbortError');
@@ -19,3 +25,17 @@ mc.port1.onmessage = common.mustCall(({ data }) => {
   mc.port1.close();
 });
 mc.port2.postMessage(exception);
+
+// Let's make sure AbortError is cloneable also
+const abort = new AbortError();
+const mc2 = new MessageChannel();
+mc2.port1.onmessage = common.mustCall(({ data }) => {
+  ok(data instanceof AbortError);
+  ok(data instanceof Error);
+  notStrictEqual(data, abort);
+  strictEqual(data.name, abort.name);
+  strictEqual(data.message, abort.message);
+  strictEqual(data.code, abort.code);
+  mc2.port1.close();
+});
+mc2.port2.postMessage(abort);
