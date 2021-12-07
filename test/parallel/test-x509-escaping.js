b11b4cc69dcaa7da4a99cde5a1b1f1abe909f731
Tobias Nießen <tniessen@tnie.de>
2021-12-07 02:14:49 +0000

    tls: drop support for URI alternative names
    Previously, Node.js incorrectly accepted uniformResourceIdentifier (URI)
    subject alternative names in checkServerIdentity regardless of the
    application protocol. This was incorrect even in the most common cases.
    For example, RFC 2818 specifies (and RFC 6125 confirms) that HTTP over
    TLS only uses dNSName and iPAddress subject alternative names, but not
    uniformResourceIdentifier subject alternative names.
    
    Additionally, name constrained certificate authorities might not be
    constrained to specific URIs, allowing them to issue certificates for
    URIs that specify hosts that they would not be allowed to issue dNSName
    certificates for.
    
    Even for application protocols that make use of URI subject alternative
    names (such as SIP, see RFC 5922), Node.js did not implement the
    required checks correctly, for example, because checkServerIdentity
    ignores the URI scheme.
    
    As a side effect, this also fixes an edge case. When a hostname is not
    an IP address and no dNSName subject alternative name exists, the
    subject's Common Name should be considered even when an iPAddress
    subject alternative name exists.
    
    It remains possible for users to pass a custom checkServerIdentity
    function to the TLS implementation in order to implement custom identity
    verification logic.
    
    This addresses CVE-2021-44531.
    
    Co-authored-by: Akshay K <iit.akshay@gmail.com>
    CVE-ID: CVE-2021-44531
    Backport-PR-URL: https://github.com/nodejs-private/node-private/pull/303
    PR-URL: https://github.com/nodejs-private/node-private/pull/300
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-x509-escaping.js
+++ test/parallel/test-x509-escaping.js
@@ -20,10 +20,6 @@ const { hasOpenSSL3 } = common;
   const numLeaves = 5;
 
   for (let i = 0; i < numLeaves; i++) {
-    // TODO(tniessen): this test case requires proper handling of URI SANs,
-    // which node currently does not implement.
-    if (i === 3) continue;
-
     const name = `x509-escaping/google/leaf${i}.pem`;
     const leafPEM = fixtures.readSync(name, 'utf8');
 
@@ -335,4 +331,31 @@ const { hasOpenSSL3 } = common;
                                       ' altnames: DNS:evil.example.com');
     }));
   })).unref();
+}
+
+// The subject MUST NOT be ignored if no dNSName subject alternative name
+// exists, even if other subject alternative names exist.
+{
+  const key = fixtures.readKey('irrelevant_san_correct_subject-key.pem');
+  const cert = fixtures.readKey('irrelevant_san_correct_subject-cert.pem');
+
+  // The hostname is the CN, but there is no dNSName SAN entry.
+  const servername = 'good.example.com';
+  const certX509 = new X509Certificate(cert);
+  assert.strictEqual(certX509.subject, `CN=${servername}`);
+  assert.strictEqual(certX509.subjectAltName, 'IP Address:1.2.3.4');
+
+  // Connect to a server that uses the self-signed certificate.
+  const server = tls.createServer({ key, cert }, common.mustCall((socket) => {
+    socket.destroy();
+    server.close();
+  })).listen(common.mustCall(() => {
+    const { port } = server.address();
+    tls.connect(port, {
+      ca: cert,
+      servername,
+    }, common.mustCall(() => {
+      // Do nothing, the server will close the connection.
+    }));
+  }));
 }
