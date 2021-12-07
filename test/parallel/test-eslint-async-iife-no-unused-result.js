28c52f60f6c93de4b1622e2d79f92cfc5bfa2e1c
Rich Trott <rtrott@gmail.com>
2021-12-07 02:01:36 -0800

    test: skip ESLint tests if no Intl
    ESLint dependency now requires Intl because it uses regexp unicode
    character properties.
    
    Fixes: https://github.com/nodejs/node/issues/41102
    
    PR-URL: https://github.com/nodejs/node/pull/41105
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Derek Lewis <DerekNonGeneric@inf.is>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- test/parallel/test-eslint-async-iife-no-unused-result.js
+++ test/parallel/test-eslint-async-iife-no-unused-result.js
@@ -1,7 +1,8 @@
 'use strict';
 const common = require('../common');
-if (!common.hasCrypto)
-  common.skip('missing crypto');
+if ((!common.hasCrypto) || (!common.hasIntl)) {
+  common.skip('ESLint tests require crypto and Intl');
+}
 common.skipIfEslintMissing();
 
 const RuleTester = require('../../tools/node_modules/eslint').RuleTester;
