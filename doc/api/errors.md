e52882da4ca99bd9c4f63890224327632f8bda42
Tobias Nießen <tniessen@tnie.de>
2021-12-07 00:21:28 +0000

    crypto,tls: implement safe x509 GeneralName format
    This change introduces JSON-compatible escaping rules for strings that
    include X.509 GeneralName components (see RFC 5280). This non-standard
    format avoids ambiguities and prevents injection attacks that could
    previously lead to X.509 certificates being accepted even though they
    were not valid for the target hostname.
    
    These changes affect the format of subject alternative names and the
    format of authority information access. The checkServerIdentity function
    has been modified to safely handle the new format, eliminating the
    possibility of injecting subject alternative names into the verification
    logic.
    
    Because each subject alternative name is only encoded as a JSON string
    literal if necessary for security purposes, this change will only be
    visible in rare cases.
    
    This addresses CVE-2021-44532.
    
    Co-authored-by: Akshay K <iit.akshay@gmail.com>
    CVE-ID: CVE-2021-44532
    Backport-PR-URL: https://github.com/nodejs-private/node-private/pull/304
    PR-URL: https://github.com/nodejs-private/node-private/pull/300
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- doc/api/errors.md
+++ doc/api/errors.md
@@ -2505,6 +2505,15 @@ An unspecified or non-specific system error has occurred within the Node.js
 process. The error object will have an `err.info` object property with
 additional details.
 
+<a id="ERR_TLS_CERT_ALTNAME_FORMAT"></a>
+
+### `ERR_TLS_CERT_ALTNAME_FORMAT`
+
+This error is thrown by `checkServerIdentity` if a user-supplied
+`subjectaltname` property violates encoding rules. Certificate objects produced
+by Node.js itself always comply with encoding rules and will never cause
+this error.
+
 <a id="ERR_TLS_CERT_ALTNAME_INVALID"></a>
 
 ### `ERR_TLS_CERT_ALTNAME_INVALID`
--- src/crypto/crypto_common.h
+++ src/crypto/crypto_common.h
@@ -140,7 +140,7 @@ v8::MaybeLocal<v8::Value> GetSerialNumber(Environment* env, X509* cert);
 v8::MaybeLocal<v8::Object> GetRawDERCertificate(Environment* env, X509* cert);
 
 v8::Local<v8::Value> ToV8Value(Environment* env, const BIOPointer& bio);
-bool SafeX509ExtPrint(const BIOPointer& out, X509_EXTENSION* ext);
+bool SafeX509SubjectAltNamePrint(const BIOPointer& out, X509_EXTENSION* ext);
 
 v8::MaybeLocal<v8::Value> GetSubject(
     Environment* env,
@@ -152,26 +152,15 @@ v8::MaybeLocal<v8::Value> GetIssuerString(
     const BIOPointer& bio,
     X509* cert);
 
-template <int nid>
-v8::MaybeLocal<v8::Value> GetInfoString(
+v8::MaybeLocal<v8::Value> GetSubjectAltNameString(
     Environment* env,
     const BIOPointer& bio,
-    X509* cert) {
-  int index = X509_get_ext_by_NID(cert, nid, -1);
-  if (index < 0)
-    return Undefined(env->isolate());
+    X509* cert);
 
-  X509_EXTENSION* ext = X509_get_ext(cert, index);
-  CHECK_NOT_NULL(ext);
-
-  if (!SafeX509ExtPrint(bio, ext) &&
-      X509V3_EXT_print(bio.get(), ext, 0, 0) != 1) {
-    USE(BIO_reset(bio.get()));
-    return v8::Null(env->isolate());
-  }
-
-  return ToV8Value(env, bio);
-}
+v8::MaybeLocal<v8::Value> GetInfoAccessString(
+    Environment* env,
+    const BIOPointer& bio,
+    X509* cert);
 
 }  // namespace crypto
 }  // namespace node
