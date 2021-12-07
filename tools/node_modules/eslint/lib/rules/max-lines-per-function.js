6beda5b22614e407f03e9370aa3fe55de7d3d8ce
Rich Trott <rtrott@gmail.com>
2021-12-07 15:53:42 -0800

    tools: update ESLint to 8.4.1

--- tools/node_modules/eslint/lib/rules/max-lines-per-function.js
+++ tools/node_modules/eslint/lib/rules/max-lines-per-function.js
@@ -80,7 +80,7 @@ module.exports = {
             OPTIONS_OR_INTEGER_SCHEMA
         ],
         messages: {
-            exceed: "{{name}} has exceeded the limit of lines allowed by {{linesExceed}}. Maximum allowed number of lines per function is {{maxLines}}."
+            exceed: "{{name}} has too many lines ({{lineCount}}). Maximum allowed is {{maxLines}}."
         }
     },
 
@@ -170,26 +170,18 @@ module.exports = {
                 return;
             }
             let lineCount = 0;
-            let comments = 0;
-            let blankLines = 0;
 
             for (let i = node.loc.start.line - 1; i < node.loc.end.line; ++i) {
                 const line = lines[i];
 
                 if (skipComments) {
                     if (commentLineNumbers.has(i + 1) && isFullLineComment(line, i + 1, commentLineNumbers.get(i + 1))) {
-                        if (lineCount <= maxLines) {
-                            comments++;
-                        }
                         continue;
                     }
                 }
 
                 if (skipBlankLines) {
                     if (line.match(/^\s*$/u)) {
-                        if (lineCount <= maxLines) {
-                            blankLines++;
-                        }
                         continue;
                     }
                 }
@@ -199,21 +191,11 @@ module.exports = {
 
             if (lineCount > maxLines) {
                 const name = upperCaseFirst(astUtils.getFunctionNameWithKind(funcNode));
-                const linesExceed = lineCount - maxLines;
-
-                const loc = {
-                    start: {
-                        line: node.loc.start.line + maxLines + (comments + blankLines),
-                        column: 0
-                    },
-                    end: node.loc.end
-                };
 
                 context.report({
                     node,
-                    loc,
                     messageId: "exceed",
-                    data: { name, linesExceed, maxLines }
+                    data: { name, lineCount, maxLines }
                 });
             }
         }
