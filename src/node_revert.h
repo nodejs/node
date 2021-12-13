9a0a189b0bbb7f3678df63f81508afd6033468e2
Michael Dawson <mdawson@devrus.com>
2021-12-13 17:04:37 -0500

    src: add cve reverts and associated tests
    Signed-off-by: Michael Dawson <mdawson@devrus.com>
    Co-authored-by: Akshay K <iit.akshay@gmail.com>
    Backport-PR-URL: https://github.com/nodejs-private/node-private/pull/304
    PR-URL: https://github.com/nodejs-private/node-private/pull/300
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/node_revert.h
+++ src/node_revert.h
@@ -16,6 +16,8 @@
 namespace node {
 
 #define SECURITY_REVERSIONS(XX)                                            \
+  XX(CVE_2021_44531, "CVE-2021-44531", "Cert Verif Bypass via URI SAN")    \
+  XX(CVE_2021_44532, "CVE-2021-44532", "Cert Verif Bypass via Str Inject") \
 //  XX(CVE_2016_PEND, "CVE-2016-PEND", "Vulnerability Title")
 
 enum reversion {
