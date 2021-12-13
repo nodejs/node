3e60925dbcf837d4cea0e5cc4c26bac8c976bd7d
Gabriel Bota <94833492+dygabo@users.noreply.github.com>
2021-12-13 21:44:00 +0100

    loader: return package format from defaultResolve if known
    This is a proposed modification of defaultResolve to return the package
    format in case it has been found during package resolution.
    The format will be returned as described in the documentation:
    https://nodejs.org/api/esm.html#resolvespecifier-context-defaultresolve
    There is one new unit test as well:
    test/es-module/test-esm-resolve-type.js
    
    PR-URL: https://github.com/nodejs/node/pull/40980
    Reviewed-By: Bradley Farias <bradley.meck@gmail.com>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    
    Backport-PR-URL: #41752

---
+++ test/es-module/test-esm-loader-resolve-type.mjs
@@ -0,0 +1,41 @@
+// Flags: --loader ./test/fixtures/es-module-loaders/hook-resolve-type.mjs
+import { allowGlobals } from '../common/index.mjs';
+import * as fixtures from '../common/fixtures.mjs';
+import { strict as assert } from 'assert';
+import * as fs from 'fs';
+
+allowGlobals(global.getModuleTypeStats);
+
+const basePath =
+  new URL('./node_modules/', import.meta.url);
+
+const rel = (file) => new URL(file, basePath);
+const createDir = (path) => {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+};
+
+const moduleName = 'module-counter-by-type';
+
+const moduleDir = rel(`${moduleName}`);
+createDir(basePath);
+createDir(moduleDir);
+fs.cpSync(
+  fixtures.path('es-modules', moduleName),
+  moduleDir,
+  { recursive: true }
+);
+
+const { importedESM: importedESMBefore,
+        importedCJS: importedCJSBefore } = global.getModuleTypeStats();
+
+import(`${moduleName}`).finally(() => {
+  fs.rmSync(basePath, { recursive: true, force: true });
+});
+
+const { importedESM: importedESMAfter,
+        importedCJS: importedCJSAfter } = global.getModuleTypeStats();
+
+assert.strictEqual(importedESMBefore + 1, importedESMAfter);
+assert.strictEqual(importedCJSBefore, importedCJSAfter);
