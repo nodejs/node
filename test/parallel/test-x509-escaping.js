1f7fdff64a9d39ccdd3a5ff3be780cacf2628941
Tobias Nießen <tniessen@tnie.de>
2021-12-19 18:23:01 +0000

    tls: fix handling of x509 subject and issuer
    When subject and verifier are represented as strings, escape special
    characters (such as '+') to guarantee unambiguity. Previously, different
    distinguished names could result in the same string when encoded. In
    particular, inserting a '+' in a single-value Relative Distinguished
    Name (e.g., L or OU) would produce a string that is indistinguishable
    from a multi-value Relative Distinguished Name. Third-party code that
    correctly interprets the generated string representation as a
    multi-value Relative Distinguished Name could then be vulnerable to an
    injection attack, e.g., when an attacker includes a single-value RDN
    with type OU and value 'HR + CN=example.com', the string representation
    produced by unpatched versions of Node.js would be
    'OU=HR + CN=example.com', which represents a multi-value RDN.
    
    Node.js itself is not vulnerable to this attack because the current
    implementation that parses such strings into objects does not handle '+'
    at all. This oversight leads to incorrect results, but at the same time
    appears to prevent injection attacks (as described above).
    
    With this change, the JavaScript objects representing the subject and
    issuer Relative Distinguished Names are constructed in C++ directly,
    instead of (incorrectly) encoding them as strings and then (incorrectly)
    decoding the strings in JavaScript.
    
    This addresses CVE-2021-44533.
    
    Co-authored-by: Akshay K <iit.akshay@gmail.com>
    CVE-ID: CVE-2021-44533
    Backport-PR-URL: https://github.com/nodejs-private/node-private/pull/303
    PR-URL: https://github.com/nodejs-private/node-private/pull/300
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-x509-escaping.js
+++ test/parallel/test-x509-escaping.js
@@ -247,6 +247,115 @@ const { hasOpenSSL3 } = common;
   }
 }
 
+// Test escaping rules for the subject field.
+{
+  const expectedSubjects = [
+    {
+      text: 'L=Somewhere\nCN=evil.example.com',
+      legacy: {
+        L: 'Somewhere',
+        CN: 'evil.example.com',
+      },
+    },
+    {
+      text: 'L=Somewhere\\00evil.example.com',
+      legacy: {
+        L: 'Somewhere\0evil.example.com',
+      },
+    },
+    {
+      text: 'L=Somewhere\\0ACN=evil.example.com',
+      legacy: {
+        L: 'Somewhere\nCN=evil.example.com'
+      },
+    },
+    {
+      text: 'L=Somewhere\\, CN = evil.example.com',
+      legacy: {
+        L: 'Somewhere, CN = evil.example.com'
+      },
+    },
+    {
+      text: 'L=Somewhere/CN=evil.example.com',
+      legacy: {
+        L: 'Somewhere/CN=evil.example.com'
+      },
+    },
+    {
+      text: 'L=München\\\\\\0ACN=evil.example.com',
+      legacy: {
+        L: 'München\\\nCN=evil.example.com'
+      }
+    },
+    {
+      text: 'L=Somewhere + CN=evil.example.com',
+      legacy: {
+        L: 'Somewhere',
+        CN: 'evil.example.com',
+      }
+    },
+    {
+      text: 'L=Somewhere \\+ CN=evil.example.com',
+      legacy: {
+        L: 'Somewhere + CN=evil.example.com'
+      }
+    },
+    // Observe that the legacy representation cannot properly distinguish
+    // between multi-value RDNs and multiple single-value RDNs.
+    {
+      text: 'L=L1 + L=L2\nL=L3',
+      legacy: {
+        L: ['L1', 'L2', 'L3']
+      },
+    },
+    {
+      text: 'L=L1\nL=L2\nL=L3',
+      legacy: {
+        L: ['L1', 'L2', 'L3']
+      },
+    },
+  ];
+
+  const serverKey = fixtures.readSync('x509-escaping/server-key.pem', 'utf8');
+
+  for (let i = 0; i < expectedSubjects.length; i++) {
+    const pem = fixtures.readSync(`x509-escaping/subj-${i}-cert.pem`, 'utf8');
+    const expected = expectedSubjects[i];
+
+    // Test the subject property of the X509Certificate API.
+    const cert = new X509Certificate(pem);
+    assert.strictEqual(cert.subject, expected.text);
+    // The issuer MUST be the same as the subject since the cert is self-signed.
+    assert.strictEqual(cert.issuer, expected.text);
+
+    // Test that the certificate obtained by checkServerIdentity has the correct
+    // subject property.
+    const server = tls.createServer({
+      key: serverKey,
+      cert: pem,
+    }, common.mustCall((conn) => {
+      conn.destroy();
+      server.close();
+    })).listen(common.mustCall(() => {
+      const { port } = server.address();
+      tls.connect(port, {
+        ca: pem,
+        servername: 'example.com',
+        checkServerIdentity: (hostname, peerCert) => {
+          assert.strictEqual(hostname, 'example.com');
+          const expectedObject = Object.assign(Object.create(null),
+                                               expected.legacy);
+          assert.deepStrictEqual(peerCert.subject, expectedObject);
+          // The issuer MUST be the same as the subject since the cert is
+          // self-signed. Otherwise, OpenSSL would have already rejected the
+          // certificate while connecting to the TLS server.
+          assert.deepStrictEqual(peerCert.issuer, expectedObject);
+        },
+      }, common.mustCall());
+    }));
+  }
+}
+
 // The internal parsing logic must match the JSON specification exactly.
 {
   // This list is partially based on V8's own JSON tests.
