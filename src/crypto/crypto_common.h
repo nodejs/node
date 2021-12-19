40536c8fb8351b30d90c428fe83a7a098e48e7ce
Tobias Nießen <tniessen@tnie.de>
2021-12-19 18:23:01 +0000

    tls: fix handling of x509 subject and issuer
    When subject and verifier are represented as strings, escape special
    characters (such as '+') to guarantee unambiguity. Previously, different
    distinguished names could result in the same string when encoded. In
    particular, inserting a '+' in a single-value Relative Distinguished
    Name (e.g., L or OU) would produce a string that is indistinguishable
    from a multi-value Relative Distinguished Name. Third-party code that
    correctly interprets the generated string representation as a
    multi-value Relative Distinguished Name could then be vulnerable to an
    injection attack, e.g., when an attacker includes a single-value RDN
    with type OU and value 'HR + CN=example.com', the string representation
    produced by unpatched versions of Node.js would be
    'OU=HR + CN=example.com', which represents a multi-value RDN.
    
    Node.js itself is not vulnerable to this attack because the current
    implementation that parses such strings into objects does not handle '+'
    at all. This oversight leads to incorrect results, but at the same time
    appears to prevent injection attacks (as described above).
    
    With this change, the JavaScript objects representing the subject and
    issuer Relative Distinguished Names are constructed in C++ directly,
    instead of (incorrectly) encoding them as strings and then (incorrectly)
    decoding the strings in JavaScript.
    
    This addresses CVE-2021-44533.
    
    CVE-ID: CVE-2021-44533
    PR-URL: https://github.com/nodejs-private/node-private/pull/300
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/crypto/crypto_common.h
+++ src/crypto/crypto_common.h
@@ -116,7 +116,8 @@ v8::MaybeLocal<v8::Object> ECPointToBuffer(
 
 v8::MaybeLocal<v8::Object> X509ToObject(
     Environment* env,
-    X509* cert);
+    X509* cert,
+    bool names_as_string = false);
 
 v8::MaybeLocal<v8::Value> GetValidTo(
     Environment* env,
--- src/crypto/crypto_x509.cc
+++ src/crypto/crypto_x509.cc
@@ -470,7 +470,7 @@ void X509Certificate::ToLegacy(const FunctionCallbackInfo<Value>& args) {
   X509Certificate* cert;
   ASSIGN_OR_RETURN_UNWRAP(&cert, args.Holder());
   Local<Value> ret;
-  if (X509ToObject(env, cert->get()).ToLocal(&ret))
+  if (X509ToObject(env, cert->get(), true).ToLocal(&ret))
     args.GetReturnValue().Set(ret);
 }
 
