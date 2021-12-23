aa970ef27dd8d3d88a2a1c419f0a54757ecb1568
Darshan Sen <darshan.sen@postman.com>
2021-12-23 12:12:34 +0530

    Revert "process: add `getActiveResourcesInfo()`"
    This reverts commit 0d9f3bd9e87d648c5f7d64fa402d01792bc795f5.

--- test/sequential/test-net-connect-econnrefused.js
+++ test/sequential/test-net-connect-econnrefused.js
@@ -54,8 +54,9 @@ function pummel() {
 
 function check() {
   setTimeout(common.mustCall(function() {
-    assert.strictEqual(process.getActiveResourcesInfo().filter(
-      (type) => type === 'TCPWRAP').length, 0);
+    assert.strictEqual(process._getActiveRequests().length, 0);
+    const activeHandles = process._getActiveHandles();
+    assert.ok(activeHandles.every((val) => val.constructor.name !== 'Socket'));
   }), 0);
 }
 
