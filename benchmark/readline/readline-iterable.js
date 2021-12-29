83919d2e88583723941ff021058f8fbd19ea9adc
Thiago Santos <tos.oliveira@gmail.com>
2021-12-28 22:44:39 -0300

    lib: adding benchmarks for readline iterable

---
+++ benchmark/readline/readline-iterable.js
@@ -0,0 +1,46 @@
+'use strict';
+const common = require('../common.js');
+const readline = require('readline');
+const { Readable } = require('stream');
+
+const bench = common.createBenchmark(main, {
+  n: [1e6],
+});
+
+const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna.
+Eget dolor morbi non arcu risus quis varius quam quisque.
+Lacus viverra vitae congue eu consequat ac felis donec.
+Amet porttitor eget dolor morbi non arcu.
+Velit ut tortor pretium viverra suspendisse.
+Mauris nunc congue nisi vitae suscipit tellus.
+Amet nisl suscipit adipiscing bibendum est ultricies integer.
+Sit amet dictum sit amet justo donec enim diam.
+Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin.
+Diam in arcu cursus euismod quis viverra nibh.
+Rest of line`;
+
+function getLoremIpsumStream(repetitions) {
+  const readable = Readable({
+    objectMode: true,
+  });
+  let i = 0;
+  readable._read = () => readable.push(
+    i++ >= repetitions ? null : loremIpsum
+  );
+  return readable;
+}
+
+async function main({ n }) {
+  bench.start();
+  let lineCount = 0;
+
+  const iterable = readline.createInterface({
+    input: getLoremIpsumStream(n),
+  });
+
+  for await (const line of iterable) {
+    lineCount++;
+  }
+  bench.end(n);
+}
