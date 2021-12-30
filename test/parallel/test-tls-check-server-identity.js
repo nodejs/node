461a0c674bf66d300152e327f9bd85889fabfd5f
Tobias Nießen <tniessen@tnie.de>
2021-12-29 20:23:11 -0500

    tls: drop support for URI alternative names
    Previously, Node.js incorrectly accepted uniformResourceIdentifier (URI)
    subject alternative names in checkServerIdentity regardless of the
    application protocol. This was incorrect even in the most common cases.
    For example, RFC 2818 specifies (and RFC 6125 confirms) that HTTP over
    TLS only uses dNSName and iPAddress subject alternative names, but not
    uniformResourceIdentifier subject alternative names.
    
    Additionally, name constrained certificate authorities might not be
    constrained to specific URIs, allowing them to issue certificates for
    URIs that specify hosts that they would not be allowed to issue dNSName
    certificates for.
    
    Even for application protocols that make use of URI subject alternative
    names (such as SIP, see RFC 5922), Node.js did not implement the
    required checks correctly, for example, because checkServerIdentity
    ignores the URI scheme.
    
    As a side effect, this also fixes an edge case. When a hostname is not
    an IP address and no dNSName subject alternative name exists, the
    subject's Common Name should be considered even when an iPAddress
    subject alternative name exists.
    
    It remains possible for users to pass a custom checkServerIdentity
    function to the TLS implementation in order to implement custom identity
    verification logic.
    
    This addresses CVE-2021-44531.
    
    Co-authored-by: Akshay K <iit.akshay@gmail.com>
    CVE-ID: CVE-2021-44531
    Backport-PR-URL: https://github.com/nodejs-private/node-private/pull/305
    PR-URL: https://github.com/nodejs-private/node-private/pull/300
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-tls-check-server-identity.js
+++ test/parallel/test-tls-check-server-identity.js
@@ -30,13 +30,6 @@ const util = require('util');
 
 const tls = require('tls');
 
-common.expectWarning('DeprecationWarning', [
-  ['The URI http://[a.b.a.com]/ found in cert.subjectaltname ' +
-  'is not a valid URI, and is supported in the tls module ' +
-  'solely for compatibility.',
-   'DEP0109'],
-]);
-
 const tests = [
   // False-y values.
   {
@@ -141,7 +134,7 @@ const tests = [
   {
     host: 'a.com',
     cert: { },
-    error: 'Cert is empty'
+    error: 'Cert does not contain a DNS name'
   },
 
   // Empty Subject w/DNS name
@@ -155,7 +148,8 @@ const tests = [
   {
     host: 'a.b.a.com', cert: {
       subjectaltname: 'URI:http://a.b.a.com/',
-    }
+    },
+    error: 'Cert does not contain a DNS name'
   },
 
   // Multiple CN fields
@@ -272,22 +266,15 @@ const tests = [
     host: 'a.b.a.com', cert: {
       subjectaltname: 'URI:http://a.b.a.com/',
       subject: {}
-    }
+    },
+    error: 'Cert does not contain a DNS name'
   },
   {
     host: 'a.b.a.com', cert: {
       subjectaltname: 'URI:http://*.b.a.com/',
       subject: {}
     },
-    error: 'Host: a.b.a.com. is not in the cert\'s altnames: ' +
-           'URI:http://*.b.a.com/'
-  },
-  // Invalid URI
-  {
-    host: 'a.b.a.com', cert: {
-      subjectaltname: 'URI:http://[a.b.a.com]/',
-      subject: {}
-    }
+    error: 'Cert does not contain a DNS name'
   },
   // IP addresses
   {
@@ -295,8 +282,7 @@ const tests = [
       subjectaltname: 'IP Address:127.0.0.1',
       subject: {}
     },
-    error: 'Host: a.b.a.com. is not in the cert\'s altnames: ' +
-           'IP Address:127.0.0.1'
+    error: 'Cert does not contain a DNS name'
   },
   {
     host: '127.0.0.1', cert: {
