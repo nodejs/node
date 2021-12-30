54821b74fc9bd14a4ef5c1037541f4f4a0bcb200
Geoffrey Booth <webadmin@geoffreybooth.com>
2021-12-29 20:29:21 -0800

    fixup! esm: refactor esm tests out of test/message

---
+++ test/es-module/test-esm-loader-obsolete-hooks.mjs
@@ -0,0 +1,26 @@
+import '../common/index.mjs';
+import { path } from '../common/fixtures.mjs';
+import { ok } from 'assert';
+import { spawn } from 'child_process';
+import { execPath } from 'process';
+
+const child = spawn(execPath, [
+  '--no-warnings',
+  '--throw-deprecation',
+  '--experimental-loader',
+  path('/es-module-loaders/hooks-obsolete.mjs'),
+  path('/print-error-message.js')
+]);
+
+let stderr = '';
+child.stderr.setEncoding('utf8');
+child.stderr.on('data', (data) => {
+  stderr += data;
+});
+child.on('close', () => {
+  stderr = stderr.toString()
+  ok(stderr.includes(
+    'DeprecationWarning: Obsolete loader hook(s) supplied and will be ' +
+    'ignored: dynamicInstantiate, getFormat, getSource, transformSource'
+  ));
+});
