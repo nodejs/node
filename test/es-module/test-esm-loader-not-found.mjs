6b8320b6cb38e502b06af899c930688beb42487f
Geoffrey Booth <webadmin@geoffreybooth.com>
2021-12-29 18:48:39 -0800

    fixup! esm: refactor esm tests out of test/message

---
+++ test/es-module/test-esm-loader-not-found.mjs
@@ -0,0 +1,25 @@
+import '../common/index.mjs';
+import { path } from '../common/fixtures.mjs';
+import { ok } from 'assert';
+import { spawn } from 'child_process';
+import { execPath } from 'process';
+
+const child = spawn(execPath, [
+  '--experimental-loader',
+  'i-dont-exist',
+  path('/print-error-message.js')
+]);
+
+let stderr = '';
+child.stderr.setEncoding('utf8');
+child.stderr.on('data', (data) => {
+  stderr += data;
+});
+child.on('close', () => {
+  stderr = stderr.toString()
+  ok(stderr.includes(
+    'Error [ERR_MODULE_NOT_FOUND]: Cannot find package \'i-dont-exist\' ' +
+    'imported from'
+  ));
+  ok(!stderr.includes('Bad command or file name'));
+});
