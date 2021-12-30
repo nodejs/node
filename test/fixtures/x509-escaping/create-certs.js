8c2db2c86baff110a1d905ed1e0dd4e1c4fd2dd1
Tobias Nießen <tniessen@tnie.de>
2021-12-29 21:07:49 -0500

    tls: fix handling of x509 subject and issuer
    When subject and verifier are represented as strings, escape special
    characters (such as '+') to guarantee unambiguity. Previously, different
    distinguished names could result in the same string when encoded. In
    particular, inserting a '+' in a single-value Relative Distinguished
    Name (e.g., L or OU) would produce a string that is indistinguishable
    from a multi-value Relative Distinguished Name. Third-party code that
    correctly interprets the generated string representation as a
    multi-value Relative Distinguished Name could then be vulnerable to an
    injection attack, e.g., when an attacker includes a single-value RDN
    with type OU and value 'HR + CN=example.com', the string representation
    produced by unpatched versions of Node.js would be
    'OU=HR + CN=example.com', which represents a multi-value RDN.
    
    Node.js itself is not vulnerable to this attack because the current
    implementation that parses such strings into objects does not handle '+'
    at all. This oversight leads to incorrect results, but at the same time
    appears to prevent injection attacks (as described above).
    
    With this change, the JavaScript objects representing the subject and
    issuer Relative Distinguished Names are constructed in C++ directly,
    instead of (incorrectly) encoding them as strings and then (incorrectly)
    decoding the strings in JavaScript.
    
    This addresses CVE-2021-44533.
    
    Co-authored-by: Akshay K <iit.akshay@gmail.com>
    CVE-ID: CVE-2021-44533
    Backport-PR-URL: https://github.com/nodejs-private/node-private/pull/306
    PR-URL: https://github.com/nodejs-private/node-private/pull/300
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/fixtures/x509-escaping/create-certs.js
+++ test/fixtures/x509-escaping/create-certs.js
@@ -500,3 +500,144 @@ for (let i = 0; i < infoAccessExtensions.length; i++) {
   });
   writeFileSync(`./info-${i}-cert.pem`, `${pem}\n`);
 }
+
+const subjects = [
+  [
+    [
+      { type: oid.localityName, value: UTF8String.encode('Somewhere') }
+    ],
+    [
+      { type: oid.commonName, value: UTF8String.encode('evil.example.com') }
+    ]
+  ],
+  [
+    [
+      {
+        type: oid.localityName,
+        value: UTF8String.encode('Somewhere\0evil.example.com'),
+      }
+    ]
+  ],
+  [
+    [
+      {
+        type: oid.localityName,
+        value: UTF8String.encode('Somewhere\nCN=evil.example.com')
+      }
+    ]
+  ],
+  [
+    [
+      {
+        type: oid.localityName,
+        value: UTF8String.encode('Somewhere, CN = evil.example.com')
+      }
+    ]
+  ],
+  [
+    [
+      {
+        type: oid.localityName,
+        value: UTF8String.encode('Somewhere/CN=evil.example.com')
+      }
+    ]
+  ],
+  [
+    [
+      {
+        type: oid.localityName,
+        value: UTF8String.encode('M\u00fcnchen\\\nCN=evil.example.com')
+      }
+    ]
+  ],
+  [
+    [
+      { type: oid.localityName, value: UTF8String.encode('Somewhere') },
+      { type: oid.commonName, value: UTF8String.encode('evil.example.com') },
+    ]
+  ],
+  [
+    [
+      {
+        type: oid.localityName,
+        value: UTF8String.encode('Somewhere + CN=evil.example.com'),
+      }
+    ]
+  ],
+  [
+    [
+      { type: oid.localityName, value: UTF8String.encode('L1') },
+      { type: oid.localityName, value: UTF8String.encode('L2') },
+    ],
+    [
+      { type: oid.localityName, value: UTF8String.encode('L3') },
+    ]
+  ],
+  [
+    [
+      { type: oid.localityName, value: UTF8String.encode('L1') },
+    ],
+    [
+      { type: oid.localityName, value: UTF8String.encode('L2') },
+    ],
+    [
+      { type: oid.localityName, value: UTF8String.encode('L3') },
+    ],
+  ],
+];
+
+for (let i = 0; i < subjects.length; i++) {
+  const tbs = {
+    version: 'v3',
+    serialNumber: new BN('01', 16),
+    signature: {
+      algorithm: oid.sha256WithRSAEncryption,
+      parameters: null_
+    },
+    issuer: {
+      type: 'rdnSequence',
+      value: subjects[i]
+    },
+    validity: {
+      notBefore: { type: 'utcTime', value: now },
+      notAfter: { type: 'utcTime', value: now + days * 86400000 }
+    },
+    subject: {
+      type: 'rdnSequence',
+      value: subjects[i]
+    },
+    subjectPublicKeyInfo: {
+      algorithm: {
+        algorithm: oid.rsaEncryption,
+        parameters: null_
+      },
+      subjectPublicKey: {
+        unused: 0,
+        data: publicKey
+      }
+    }
+  };
+
+  // Self-sign the certificate.
+  const tbsDer = rfc5280.TBSCertificate.encode(tbs, 'der');
+  const signature = crypto.createSign(digest).update(tbsDer).sign(privateKey);
+
+  // Construct the signed certificate.
+  const cert = {
+    tbsCertificate: tbs,
+    signatureAlgorithm: {
+      algorithm: oid.sha256WithRSAEncryption,
+      parameters: null_
+    },
+    signature: {
+      unused: 0,
+      data: signature
+    }
+  };
+
+  // Store the signed certificate.
+  const pem = rfc5280.Certificate.encode(cert, 'pem', {
+    label: 'CERTIFICATE'
+  });
+  writeFileSync(`./subj-${i}-cert.pem`, `${pem}\n`);
+}
