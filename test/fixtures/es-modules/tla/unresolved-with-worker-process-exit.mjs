5ebf9b0d73be6000c30302955b597a0bad8a4a1e
Gang Chen <13298548+MoonBall@users.noreply.github.com>
2022-01-04 07:27:57 +0800

    Update test/fixtures/es-modules/tla/unresolved-with-worker-process-exit.mjs
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/fixtures/es-modules/tla/unresolved-with-worker-process-exit.mjs
+++ test/fixtures/es-modules/tla/unresolved-with-worker-process-exit.mjs
@@ -3,7 +3,7 @@ import { Worker } from 'worker_threads';
 // Do not use isMainThread so that this test itself can be run inside a Worker.
 if (!process.env.HAS_STARTED_WORKER) {
   process.env.HAS_STARTED_WORKER = 1;
-  new Worker(import.meta.url.slice('file://'.length));
+  new Worker(new URL(import.meta.url));
   await new Promise(() => {});
 } else {
   process.exit()
