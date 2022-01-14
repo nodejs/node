492fcae16d3e3413be677fea34a86315942ccdc9
Rich Trott <rtrott@gmail.com>
2022-01-13 20:53:22 -0800

    Update test/es-module/test-esm-import-assertion-2.mjs

--- test/es-module/test-esm-import-assertion-2.mjs
+++ test/es-module/test-esm-import-assertion-2.mjs
@@ -2,7 +2,6 @@
 import '../common/index.mjs';
 import { strictEqual } from 'assert';
 
-
 import secret from '../fixtures/experimental.json' assert { type: 'json', unsupportedAssertion: 'should ignore' };
 
 strictEqual(secret.ofLife, 42);
