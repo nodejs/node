4fd3ddc578a7b100efe999e41cd2b83ca48dba28
Rich Trott <rtrott@gmail.com>
2022-01-14 19:49:19 -0800

    tools: disable max-len ESLint rule for strings

--- test/parallel/test-policy-integrity-flag.js
+++ test/parallel/test-policy-integrity-flag.js
@@ -21,12 +21,10 @@ const emptySRI = `sha512-${emptyHash.digest('base64')}`;
 const policyHash = crypto.createHash('sha512');
 policyHash.update(fs.readFileSync(depPolicy));
 
-/* eslint-disable max-len */
 // When using \n only
 const nixPolicySRI = 'sha512-u/nXI6UacK5fKDC2bopcgnuQY4JXJKlK3dESO3GIKKxwogVHjJqpF9rgk7Zw+TJXIc96xBUWKHuUgOzic8/4tQ==';
 // When \n is turned into \r\n
 const windowsPolicySRI = 'sha512-OeyCPRo4OZMosHyquZXDHpuU1F4KzG9UHFnn12FMaHsvqFUt3TFZ+7wmZE7ThZ5rsQWkUjc9ZH0knGZ2e8BYPQ==';
-/* eslint-enable max-len */
 
 const depPolicySRI = `${nixPolicySRI} ${windowsPolicySRI}`;
 {
