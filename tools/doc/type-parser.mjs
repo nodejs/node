2e679a248a1e3b01f000c2f7788645c2abe79571
LiviaMedeiros <livia@cirno.name>
2022-01-18 00:04:10 +0800

    tools: revert TypedArray types
    This reverts commit 5f1c397784674e23451b24414328bf4fce60f932.

--- tools/doc/type-parser.mjs
+++ tools/doc/type-parser.mjs
@@ -16,10 +16,7 @@ const jsGlobalTypes = [
   'AggregateError', 'Array', 'ArrayBuffer', 'DataView', 'Date', 'Error',
   'EvalError', 'Function', 'Map', 'Object', 'Promise', 'RangeError',
   'ReferenceError', 'RegExp', 'Set', 'SharedArrayBuffer', 'SyntaxError',
-  'TypeError', 'TypedArray', 'URIError', 'Int8Array', 'Uint8Array',
-  'Uint8ClampedArray', 'Int16Array', 'Uint16Array', 'Int32Array',
-  'Uint32Array', 'Float32Array', 'Float64Array', 'BigInt64Array',
-  'BigUint64Array',
+  'TypeError', 'TypedArray', 'URIError', 'Uint8Array',
 ];
 
 const customTypesMap = {
