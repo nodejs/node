0b2748689578951f59cc218aa9f9c5b526746a9b
Lu Yahan <yahan@iscas.ac.cn>
2022-01-17 15:54:35 +0800

    deps: V8: cherry-pick e74d6918fb9f
    Original commit message:
    
        [sparkplug] Simplify arch-guards to ENABLE_SPARKPLUG
    
        There is still a place to simplify.
    
        Bug: v8:11420, v8:11421
        Change-Id: I774139c52d911323f162350532a493e70f518643
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3096885
        Auto-Submit: Yahan Lu <yahan@iscas.ac.cn>
        Commit-Queue: Leszek Swirski <leszeks@chromium.org>
        Reviewed-by: Leszek Swirski <leszeks@chromium.org>
        Cr-Commit-Position: refs/heads/master@{#76296}
    
    Refs: v8/v8@e74d6918fb9
    
    PR-URL: https://github.com/nodejs/node/pull/41566
    Refs: https://github.com/v8/v8/commit/d8dc66f
    Refs: https://github.com/v8/v8/commit/3cab84c
    Refs: https://github.com/v8/v8/commit/471f862
    Refs: https://github.com/v8/v8/commit/77599ff
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- deps/v8/src/builtins/builtins-internal-gen.cc
+++ deps/v8/src/builtins/builtins-internal-gen.cc
@@ -1053,9 +1053,7 @@ void Builtins::Generate_MemMove(MacroAssembler* masm) {
 
 // TODO(v8:11421): Remove #if once baseline compiler is ported to other
 // architectures.
-#if V8_TARGET_ARCH_IA32 || V8_TARGET_ARCH_X64 || V8_TARGET_ARCH_ARM64 ||     \
-    V8_TARGET_ARCH_ARM || V8_TARGET_ARCH_RISCV64 || V8_TARGET_ARCH_MIPS64 || \
-    V8_TARGET_ARCH_MIPS
+#if ENABLE_SPARKPLUG
 void Builtins::Generate_BaselineLeaveFrame(MacroAssembler* masm) {
   EmitReturnBaseline(masm);
 }
