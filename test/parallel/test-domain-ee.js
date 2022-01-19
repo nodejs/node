40ff0b5af5b793a77330432377e0edffa213d6f9
Gang Chen <13298548+MoonBall@users.noreply.github.com>
2022-01-19 19:26:38 +0800

    domain: pass opts to `EventEmitter.init`
    PR-URL: https://github.com/nodejs/node/pull/41414
    Fixes: https://github.com/nodejs/node/issues/41391
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Yongsheng Zhang <zyszys98@gmail.com>
    Reviewed-By: Anatoli Papirovski <apapirovski@mac.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-domain-ee.js
+++ test/parallel/test-domain-ee.js
@@ -18,3 +18,11 @@ d.on('error', common.mustCall((err) => {
 
 d.add(e);
 e.emit('error', new Error('foobar'));
+
+{
+  // Ensure initial params pass to origin `EventEmitter.init` function
+  const e = new EventEmitter({ captureRejections: true });
+  const kCapture = Object.getOwnPropertySymbols(e)
+    .find((it) => it.description === 'kCapture');
+  assert.strictEqual(e[kCapture], true);
+}
