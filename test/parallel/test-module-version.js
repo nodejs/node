797c71b0334e5f865e6a46dff85a76de429c492b
Rich Trott <rtrott@gmail.com>
2022-01-22 21:33:39 -0800

    test: use Object.hasOwn() where applicable
    Replace Object.prototpye.hasOwnProperty() with Object.hasOwn() where
    applicable.

--- test/parallel/test-module-version.js
+++ test/parallel/test-module-version.js
@@ -3,7 +3,7 @@ require('../common');
 const assert = require('assert');
 
 // check for existence
-assert(process.config.variables.hasOwnProperty('node_module_version'));
+assert(Object.hasOwn(process.config.variables, 'node_module_version'));
 
 // Ensure that `node_module_version` is an Integer > 0
 assert(Number.isInteger(process.config.variables.node_module_version));
