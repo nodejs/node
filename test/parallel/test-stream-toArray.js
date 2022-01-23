31cee4f914d26d669adb57a70689ca5440760d3f
Benjamin Gruenbaum <benjamingr@gmail.com>
2022-01-23 10:19:53 +0200

    stream: never flatten on toArray
    PR-URL: https://github.com/nodejs/node/pull/41615
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-stream-toArray.js
+++ test/parallel/test-stream-toArray.js
@@ -24,14 +24,16 @@ const assert = require('assert');
 }
 
 {
-  // Works on a non-object-mode stream and flattens it
+  // Works on a non-object-mode stream
   (async () => {
+    const firstBuffer = Buffer.from([1, 2, 3]);
+    const secondBuffer = Buffer.from([4, 5, 6]);
     const stream = Readable.from(
-      [Buffer.from([1, 2, 3]), Buffer.from([4, 5, 6])]
-      , { objectMode: false });
+      [firstBuffer, secondBuffer],
+      { objectMode: false });
     const result = await stream.toArray();
-    assert.strictEqual(Buffer.isBuffer(result), true);
-    assert.deepStrictEqual(Array.from(result), [1, 2, 3, 4, 5, 6]);
+    assert.strictEqual(Array.isArray(result), true);
+    assert.deepStrictEqual(result, [firstBuffer, secondBuffer]);
   })().then(common.mustCall());
 }
 
