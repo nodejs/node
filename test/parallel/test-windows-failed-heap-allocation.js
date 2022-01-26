7752eedcc7db586fb0a7c586908d4405c6408325
Benjamin Gruenbaum <benjamingr@gmail.com>
2022-01-26 19:56:47 +0200

    test: improve stability of oom test
    The OOM test uses a value that caused an OOM crash from V8 on certain
    machines when V8 did not notify the host of OOM soon enough.
    
    PR-URL: https://github.com/nodejs/node/pull/41681
    Refs: https://github.com/tc39/proposal-iterator-helpers#asindexedpairs
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- test/parallel/test-windows-failed-heap-allocation.js
+++ test/parallel/test-windows-failed-heap-allocation.js
@@ -19,8 +19,8 @@ const tmpdir = require('../common/tmpdir');
 tmpdir.refresh();
 
 // --max-old-space-size=3 is the min 'old space' in V8, explodes fast
-const cmd = `"${process.execPath}" --max-old-space-size=3 "${__filename}"`;
-exec(`${cmd} heapBomb`, { cwd: tmpdir.path }, common.mustCall((err) => {
+const cmd = `"${process.execPath}" --max-old-space-size=30 "${__filename}"`;
+exec(`${cmd} heapBomb`, { cwd: tmpdir.path }, common.mustCall((err, stdout, stderr) => {
   const msg = `Wrong exit code of ${err.code}! Expected 134 for abort`;
   // Note: common.nodeProcessAborted() is not asserted here because it
   // returns true on 134 as well as 0x80000003 (V8's base::OS::Abort)
