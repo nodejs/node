9ad8f574b7e9a9e2a739dc813553dc1ef33c29eb
Geoffrey Booth <webadmin@geoffreybooth.com>
2022-01-28 15:08:05 -0800

    module: unflag esm json modules

--- test/es-module/test-esm-invalid-data-urls.js
+++ test/es-module/test-esm-invalid-data-urls.js
@@ -5,20 +5,14 @@ const assert = require('assert');
 (async () => {
   await assert.rejects(import('data:text/plain,export default0'), {
     code: 'ERR_INVALID_MODULE_SPECIFIER',
-    message:
-      'Invalid module "data:text/plain,export default0" has an unsupported ' +
-      'MIME type "text/plain"',
+    message: 'Invalid module "data:text/plain,export default0" has an unsupported MIME type "text/plain"',
   });
   await assert.rejects(import('data:text/plain;base64,'), {
     code: 'ERR_INVALID_MODULE_SPECIFIER',
-    message:
-      'Invalid module "data:text/plain;base64," has an unsupported ' +
-      'MIME type "text/plain"',
+    message: 'Invalid module "data:text/plain;base64," has an unsupported MIME type "text/plain"',
   });
-  await assert.rejects(import('data:application/json,[]'), {
+  await assert.rejects(import('data:text/css,.error { color: red; }'), {
     code: 'ERR_INVALID_MODULE_SPECIFIER',
-    message:
-      'Invalid module "data:application/json,[]" has an unsupported ' +
-      'MIME type "application/json"',
+    message: 'Invalid module "data:text/css,.error { color: red; }" has an unsupported MIME type "text/css"',
   });
 })().then(common.mustCall());
