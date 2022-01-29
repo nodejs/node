192a782221c84fbd63c451bd7029e7c9c9d5a95b
Anna Henningsen <anna@addaleax.net>
2022-01-29 22:05:36 +0100

    src: return proper URLs from node_api_get_module_file_name
    Using `file://${path}` does not properly escape special URL characters.

--- test/node-api/test_general/test.js
+++ test/node-api/test_general/test.js
@@ -1,15 +1,38 @@
 'use strict';
 
 const common = require('../../common');
+const tmpdir = require('../../common/tmpdir');
+const fs = require('fs');
+const path = require('path');
 const filename = require.resolve(`./build/${common.buildType}/test_general`);
 const test_general = require(filename);
 const assert = require('assert');
 
-// TODO(gabrielschulhof): This test may need updating if/when the filename
-// becomes a full-fledged URL.
-assert.strictEqual(test_general.filename, `file://${filename}`);
+tmpdir.refresh();
 
-const [ major, minor, patch, release ] = test_general.testGetNodeVersion();
-assert.strictEqual(process.version.split('-')[0],
-                   `v${major}.${minor}.${patch}`);
-assert.strictEqual(release, process.release.name);
+{
+  // TODO(gabrielschulhof): This test may need updating if/when the filename
+  // becomes a full-fledged URL.
+  assert.strictEqual(
+    decodeURIComponent(test_general.filename),
+    `file://${filename}`);
+}
+
+{
+  const urlTestDir = path.join(tmpdir.path, 'foo%?#bar');
+  const urlTestFile = path.join(urlTestDir, path.basename(filename));
+  fs.mkdirSync(urlTestDir, { recursive: true });
+  fs.copyFileSync(filename, urlTestFile);
+  const reportedFilename = require(urlTestFile).filename;
+  assert.doesNotMatch(reportedFilename, /foo%\?#bar/);
+  assert.strictEqual(
+    decodeURIComponent(reportedFilename),
+    `file://${urlTestFile}`);
+}
+
+{
+  const [ major, minor, patch, release ] = test_general.testGetNodeVersion();
+  assert.strictEqual(process.version.split('-')[0],
+                     `v${major}.${minor}.${patch}`);
+  assert.strictEqual(release, process.release.name);
+}
