0a3e3c705f42ae4e1c012b5f983a368045c3a221
Erick Wendel <erick.workspace@gmail.com>
2022-02-01 19:10:22 -0300

    worker: improve code coverage for getHeapSnapshot

---
+++ test/parallel/test-worker-heap-snapshot.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Flags: --expose-internals
+const common = require('../common');
+const assert = require('assert');
+const { Worker } = require('worker_threads');
+const { HeapSnapshotStream } = require('internal/heap_utils');
+const { kHandle } = require('internal/worker');
+
+const worker = new Worker(__filename);
+const snapShotResult = { ondone: () => {} };
+worker[kHandle].takeHeapSnapshot = common.mustCall(() => snapShotResult);
+
+worker.on('online', () => {
+  const snapShotResponse = worker.getHeapSnapshot();
+  snapShotResult.ondone({});
+
+  snapShotResponse.then(common.mustCall((heapSnapshotStream) => {
+    assert.ok(heapSnapshotStream instanceof HeapSnapshotStream);
+    worker.terminate();
+  }));
+});
