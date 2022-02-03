1b0fa8392aefc7fb3d67d0c27e0197a6b4d0386b
Benjamin Gruenbaum <benjamingr@gmail.com>
2022-02-03 14:30:27 +0200

    buffer: fix atob/btoa no-arg case
    PR-URL: https://github.com/nodejs/node/pull/41478
    Fixes: https://github.com/nodejs/node/issues/41450
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

---
+++ test/parallel/test-btoa-atob.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('../common');
+
+const { strictEqual, throws } = require('assert');
+const buffer = require('buffer');
+
+// Exported on the global object
+strictEqual(globalThis.atob, buffer.atob);
+strictEqual(globalThis.btoa, buffer.btoa);
+
+// Throws type error on no argument passed
+throws(() => buffer.atob(), /TypeError/);
+throws(() => buffer.btoa(), /TypeError/);
