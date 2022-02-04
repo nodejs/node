ba84defb3c1e6056d2c54c9c6e2a9ae4b7c8c317
Nitzan Uziely <linkgoron@gmail.com>
2022-02-04 14:24:41 +0200

    fixup! stream: add iterator helper find

--- test/parallel/test-stream-some-find-every.mjs
+++ test/parallel/test-stream-some-find-every.mjs
@@ -1,11 +1,8 @@
-'use strict';
+import * as common from '../common/index.mjs';
+import { setTimeout } from 'timers/promises';
+import { Readable } from 'stream';
+import assert from 'assert';
 
-const common = require('../common');
-const { setTimeout } = require('timers/promises');
-const {
-  Readable,
-} = require('stream');
-const assert = require('assert');
 
 function oneTo5() {
   return Readable.from([1, 2, 3, 4, 5]);
@@ -19,53 +16,45 @@ function oneTo5Async() {
 }
 {
   // Some, find, and every work with a synchronous stream and predicate
-  (async () => {
-    assert.strictEqual(await oneTo5().some((x) => x > 3), true);
-    assert.strictEqual(await oneTo5().every((x) => x > 3), false);
-    assert.strictEqual(await oneTo5().find((x) => x > 3), 4);
-    assert.strictEqual(await oneTo5().some((x) => x > 6), false);
-    assert.strictEqual(await oneTo5().every((x) => x < 6), true);
-    assert.strictEqual(await oneTo5().find((x) => x > 6), undefined);
-    assert.strictEqual(await Readable.from([]).some(() => true), false);
-    assert.strictEqual(await Readable.from([]).every(() => true), true);
-    assert.strictEqual(await Readable.from([]).find(() => true), undefined);
-  })().then(common.mustCall());
+  assert.strictEqual(await oneTo5().some((x) => x > 3), true);
+  assert.strictEqual(await oneTo5().every((x) => x > 3), false);
+  assert.strictEqual(await oneTo5().find((x) => x > 3), 4);
+  assert.strictEqual(await oneTo5().some((x) => x > 6), false);
+  assert.strictEqual(await oneTo5().every((x) => x < 6), true);
+  assert.strictEqual(await oneTo5().find((x) => x > 6), undefined);
+  assert.strictEqual(await Readable.from([]).some(() => true), false);
+  assert.strictEqual(await Readable.from([]).every(() => true), true);
+  assert.strictEqual(await Readable.from([]).find(() => true), undefined);
 }
 
 {
   // Some, find, and every work with an asynchronous stream and synchronous predicate
-  (async () => {
-    assert.strictEqual(await oneTo5Async().some((x) => x > 3), true);
-    assert.strictEqual(await oneTo5Async().every((x) => x > 3), false);
-    assert.strictEqual(await oneTo5Async().find((x) => x > 3), 4);
-    assert.strictEqual(await oneTo5Async().some((x) => x > 6), false);
-    assert.strictEqual(await oneTo5Async().every((x) => x < 6), true);
-    assert.strictEqual(await oneTo5Async().find((x) => x > 6), undefined);
-  })().then(common.mustCall());
+  assert.strictEqual(await oneTo5Async().some((x) => x > 3), true);
+  assert.strictEqual(await oneTo5Async().every((x) => x > 3), false);
+  assert.strictEqual(await oneTo5Async().find((x) => x > 3), 4);
+  assert.strictEqual(await oneTo5Async().some((x) => x > 6), false);
+  assert.strictEqual(await oneTo5Async().every((x) => x < 6), true);
+  assert.strictEqual(await oneTo5Async().find((x) => x > 6), undefined);
 }
 
 {
   // Some, find, and every work on synchronous streams with an asynchronous predicate
-  (async () => {
-    assert.strictEqual(await oneTo5().some(async (x) => x > 3), true);
-    assert.strictEqual(await oneTo5().every(async (x) => x > 3), false);
-    assert.strictEqual(await oneTo5().find(async (x) => x > 3), 4);
-    assert.strictEqual(await oneTo5().some(async (x) => x > 6), false);
-    assert.strictEqual(await oneTo5().every(async (x) => x < 6), true);
-    assert.strictEqual(await oneTo5().find(async (x) => x > 6), undefined);
-  })().then(common.mustCall());
+  assert.strictEqual(await oneTo5().some(async (x) => x > 3), true);
+  assert.strictEqual(await oneTo5().every(async (x) => x > 3), false);
+  assert.strictEqual(await oneTo5().find(async (x) => x > 3), 4);
+  assert.strictEqual(await oneTo5().some(async (x) => x > 6), false);
+  assert.strictEqual(await oneTo5().every(async (x) => x < 6), true);
+  assert.strictEqual(await oneTo5().find(async (x) => x > 6), undefined);
 }
 
 {
   // Some, find, and every work on asynchronous streams with an asynchronous predicate
-  (async () => {
-    assert.strictEqual(await oneTo5Async().some(async (x) => x > 3), true);
-    assert.strictEqual(await oneTo5Async().every(async (x) => x > 3), false);
-    assert.strictEqual(await oneTo5Async().find(async (x) => x > 3), 4);
-    assert.strictEqual(await oneTo5Async().some(async (x) => x > 6), false);
-    assert.strictEqual(await oneTo5Async().every(async (x) => x < 6), true);
-    assert.strictEqual(await oneTo5Async().find(async (x) => x > 6), undefined);
-  })().then(common.mustCall());
+  assert.strictEqual(await oneTo5Async().some(async (x) => x > 3), true);
+  assert.strictEqual(await oneTo5Async().every(async (x) => x > 3), false);
+  assert.strictEqual(await oneTo5Async().find(async (x) => x > 3), 4);
+  assert.strictEqual(await oneTo5Async().some(async (x) => x > 6), false);
+  assert.strictEqual(await oneTo5Async().every(async (x) => x < 6), true);
+  assert.strictEqual(await oneTo5Async().find(async (x) => x > 6), undefined);
 }
 
 {
@@ -74,8 +63,9 @@ function oneTo5Async() {
     await setTimeout();
     assert.strictEqual(stream.destroyed, true);
   }
-  // Some, find, and every short circuit
-  (async () => {
+
+  {
+    // Some, find, and every short circuit
     const someStream = oneTo5();
     await someStream.some(common.mustCall((x) => x > 2, 3));
     await checkDestroyed(someStream);
@@ -92,10 +82,10 @@ function oneTo5Async() {
     await oneTo5().some(common.mustCall(() => false, 5));
     await oneTo5().every(common.mustCall(() => true, 5));
     await oneTo5().find(common.mustCall(() => false, 5));
-  })().then(common.mustCall());
+  }
 
-  // Some, find, and every short circuit async stream/predicate
-  (async () => {
+  {
+    // Some, find, and every short circuit async stream/predicate
     const someStream = oneTo5Async();
     await someStream.some(common.mustCall(async (x) => x > 2, 3));
     await checkDestroyed(someStream);
@@ -112,21 +102,19 @@ function oneTo5Async() {
     await oneTo5Async().some(common.mustCall(async () => false, 5));
     await oneTo5Async().every(common.mustCall(async () => true, 5));
     await oneTo5Async().find(common.mustCall(async () => false, 5));
-  })().then(common.mustCall());
+  }
 }
 
 {
   // Concurrency doesn't affect which value is found.
-  (async () => {
-    const found = await Readable.from([1, 2]).find(async (val) => {
-      if (val === 1) {
-        // eslint-disable-next-line no-restricted-syntax
-        await setTimeout(100);
-      }
-      return true;
-    }, { concurrency: 2 });
-    assert.strictEqual(found, 1);
-  })().then(common.mustCall());
+  const found = await Readable.from([1, 2]).find(async (val) => {
+    if (val === 1) {
+      // eslint-disable-next-line no-restricted-syntax
+      await setTimeout(100);
+    }
+    return true;
+  }, { concurrency: 2 });
+  assert.strictEqual(found, 1);
 }
 
 {
@@ -172,5 +160,15 @@ function oneTo5Async() {
         signal: true
       });
     }, /ERR_INVALID_ARG_TYPE/, `${op} should throw for invalid signal`).then(common.mustCall());
+  }
+}
+{
+  for (const op of ['some', 'every', 'find']) {
+    const stream = oneTo5();
+    Object.defineProperty(stream, 'map', {
+      value: common.mustNotCall(() => {}),
+    });
+    // Check that map isn't getting called.
+    stream[op](() => {});
   }
 }
