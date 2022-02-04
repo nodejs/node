bb0fff5f6636c59df506c45ff604f637c23e29a6
Nitzan Uziely <linkgoron@gmail.com>
2022-02-04 14:24:01 +0200

    stream: add iterator helper find
    Continue iterator-helpers work by adding `find` to readable streams.

--- doc/api/stream.md
+++ doc/api/stream.md
@@ -1976,6 +1976,56 @@ console.log(anyBigFile); // `true` if any file in the list is bigger than 1MB
 console.log('done'); // Stream has finished
 ```
 
+### `readable.find(fn[, options])`
+
+<!-- YAML
+added: REPLACEME
+-->
+
+> Stability: 1 - Experimental
+
+* `fn` {Function|AsyncFunction} a function to call on each item of the stream.
+  * `data` {any} a chunk of data from the stream.
+  * `options` {Object}
+    * `signal` {AbortSignal} aborted if the stream is destroyed allowing to
+      abort the `fn` call early.
+* `options` {Object}
+  * `concurrency` {number} the maximum concurrent invocation of `fn` to call
+    on the stream at once. **Default:** `1`.
+  * `signal` {AbortSignal} allows destroying the stream if the signal is
+    aborted.
+* Returns: {Promise} a promise evaluating to the first chunk for which `fn`
+  evaluated with a truthy value, or `undefined` if no element was found.
+
+This method is similar to `Array.prototype.find` and calls `fn` on each chunk
+in the stream to find a chunk with a truthy value for `fn`. Once an `fn` call's
+awaited return value is truthy, the stream is destroyed and the promise is
+fulfilled with value for which the value of `fn` was truthy for. If all of the
+`fn` calls on the chunks return a falsy value, the promise is fulfilled with
+`undefined`.
+
+```mjs
+import { Readable } from 'stream';
+import { stat } from 'fs/promises';
+
+// With a synchronous predicate.
+await Readable.from([1, 2, 3, 4]).find((x) => x > 2); // 3
+await Readable.from([1, 2, 3, 4]).find((x) => x > 0); // 1
+await Readable.from([1, 2, 3, 4]).find((x) => x > 10); // undefined
+
+// With an asynchronous predicate, making at most 2 file checks at a time.
+const foundBigFile = await Readable.from([
+  'file1',
+  'file2',
+  'file3',
+]).find(async (fileName) => {
+  const stats = await stat(fileName);
+  return stat.size > 1024 * 1024;
+}, { concurrency: 2 });
+console.log(foundBigFile); // File name of large file, if any file in the list is bigger than 1MB
+console.log('done'); // Stream has finished
+```
+
 ### `readable.every(fn[, options])`
 
 <!-- YAML
--- test/parallel/test-stream-some-every.js
+++ test/parallel/test-stream-some-every.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const common = require('../common');
+const { setTimeout } = require('timers/promises');
 const {
   Readable,
 } = require('stream');
@@ -17,90 +18,159 @@ function oneTo5Async() {
   });
 }
 {
-  // Some and every work with a synchronous stream and predicate
+  // Some, find, and every work with a synchronous stream and predicate
   (async () => {
     assert.strictEqual(await oneTo5().some((x) => x > 3), true);
     assert.strictEqual(await oneTo5().every((x) => x > 3), false);
+    assert.strictEqual(await oneTo5().find((x) => x > 3), 4);
     assert.strictEqual(await oneTo5().some((x) => x > 6), false);
     assert.strictEqual(await oneTo5().every((x) => x < 6), true);
-    assert.strictEqual(await Readable.from([]).some((x) => true), false);
-    assert.strictEqual(await Readable.from([]).every((x) => true), true);
+    assert.strictEqual(await oneTo5().find((x) => x > 6), undefined);
+    assert.strictEqual(await Readable.from([]).some(() => true), false);
+    assert.strictEqual(await Readable.from([]).every(() => true), true);
+    assert.strictEqual(await Readable.from([]).find(() => true), undefined);
   })().then(common.mustCall());
 }
 
 {
-  // Some and every work with an asynchronous stream and synchronous predicate
+  // Some, find, and every work with an asynchronous stream and synchronous predicate
   (async () => {
     assert.strictEqual(await oneTo5Async().some((x) => x > 3), true);
     assert.strictEqual(await oneTo5Async().every((x) => x > 3), false);
+    assert.strictEqual(await oneTo5Async().find((x) => x > 3), 4);
     assert.strictEqual(await oneTo5Async().some((x) => x > 6), false);
     assert.strictEqual(await oneTo5Async().every((x) => x < 6), true);
+    assert.strictEqual(await oneTo5Async().find((x) => x > 6), undefined);
   })().then(common.mustCall());
 }
 
 {
-  // Some and every work on asynchronous streams with an asynchronous predicate
+  // Some, find, and every work on synchronous streams with an asynchronous predicate
   (async () => {
     assert.strictEqual(await oneTo5().some(async (x) => x > 3), true);
     assert.strictEqual(await oneTo5().every(async (x) => x > 3), false);
+    assert.strictEqual(await oneTo5().find(async (x) => x > 3), 4);
     assert.strictEqual(await oneTo5().some(async (x) => x > 6), false);
     assert.strictEqual(await oneTo5().every(async (x) => x < 6), true);
+    assert.strictEqual(await oneTo5().find(async (x) => x > 6), undefined);
   })().then(common.mustCall());
 }
 
 {
-  // Some and every short circuit
+  // Some, find, and every work on asynchronous streams with an asynchronous predicate
   (async () => {
-    await oneTo5().some(common.mustCall((x) => x > 2, 3));
-    await oneTo5().every(common.mustCall((x) => x < 3, 3));
-    // When short circuit isn't possible the whole stream is iterated
-    await oneTo5().some(common.mustCall((x) => x > 6, 5));
-    // The stream is destroyed afterwards
-    const stream = oneTo5();
-    await stream.some(common.mustCall((x) => x > 2, 3));
+    assert.strictEqual(await oneTo5Async().some(async (x) => x > 3), true);
+    assert.strictEqual(await oneTo5Async().every(async (x) => x > 3), false);
+    assert.strictEqual(await oneTo5Async().find(async (x) => x > 3), 4);
+    assert.strictEqual(await oneTo5Async().some(async (x) => x > 6), false);
+    assert.strictEqual(await oneTo5Async().every(async (x) => x < 6), true);
+    assert.strictEqual(await oneTo5Async().find(async (x) => x > 6), undefined);
+  })().then(common.mustCall());
+}
+
+{
+  async function checkDestroyed(stream) {
+    // eslint-disable-next-line no-restricted-syntax
+    await setTimeout();
     assert.strictEqual(stream.destroyed, true);
+  }
+  // Some, find, and every short circuit
+  (async () => {
+    const someStream = oneTo5();
+    await someStream.some(common.mustCall((x) => x > 2, 3));
+    await checkDestroyed(someStream);
+
+    const everyStream = oneTo5();
+    await everyStream.every(common.mustCall((x) => x < 3, 3));
+    await checkDestroyed(everyStream);
+
+    const findStream = oneTo5();
+    await findStream.find(common.mustCall((x) => x > 1, 2));
+    await checkDestroyed(findStream);
+
+    // When short circuit isn't possible the whole stream is iterated
+    await oneTo5().some(common.mustCall(() => false, 5));
+    await oneTo5().every(common.mustCall(() => true, 5));
+    await oneTo5().find(common.mustCall(() => false, 5));
+  })().then(common.mustCall());
+
+  // Some, find, and every short circuit async stream/predicate
+  (async () => {
+    const someStream = oneTo5Async();
+    await someStream.some(common.mustCall(async (x) => x > 2, 3));
+    await checkDestroyed(someStream);
+
+    const everyStream = oneTo5Async();
+    await everyStream.every(common.mustCall(async (x) => x < 3, 3));
+    await checkDestroyed(everyStream);
+
+    const findStream = oneTo5Async();
+    await findStream.find(common.mustCall(async (x) => x > 1, 2));
+    await checkDestroyed(findStream);
+
+    // When short circuit isn't possible the whole stream is iterated
+    await oneTo5Async().some(common.mustCall(async () => false, 5));
+    await oneTo5Async().every(common.mustCall(async () => true, 5));
+    await oneTo5Async().find(common.mustCall(async () => false, 5));
+  })().then(common.mustCall());
+}
+
+{
+  // Concurrency doesn't affect which value is found.
+  (async () => {
+    const found = await Readable.from([1, 2]).find(async (val) => {
+      if (val === 1) {
+        // eslint-disable-next-line no-restricted-syntax
+        await setTimeout(100);
+      }
+      return true;
+    }, { concurrency: 2 });
+    assert.strictEqual(found, 1);
   })().then(common.mustCall());
 }
 
 {
   // Support for AbortSignal
-  const ac = new AbortController();
-  assert.rejects(Readable.from([1, 2, 3]).some(
-    () => new Promise(() => {}),
-    { signal: ac.signal }
-  ), {
-    name: 'AbortError',
-  }).then(common.mustCall());
-  ac.abort();
-}
-{
-  // Support for pre-aborted AbortSignal
-  assert.rejects(Readable.from([1, 2, 3]).some(
-    () => new Promise(() => {}),
-    { signal: AbortSignal.abort() }
-  ), {
-    name: 'AbortError',
-  }).then(common.mustCall());
+  for (const op of ['some', 'every', 'find']) {
+    {
+      const ac = new AbortController();
+      assert.rejects(Readable.from([1, 2, 3])[op](
+        () => new Promise(() => { }),
+        { signal: ac.signal }
+      ), {
+        name: 'AbortError',
+      }, `${op} should abort correctly with sync abort`).then(common.mustCall());
+      ac.abort();
+    }
+    {
+      // Support for pre-aborted AbortSignal
+      assert.rejects(Readable.from([1, 2, 3])[op](
+        () => new Promise(() => { }),
+        { signal: AbortSignal.abort() }
+      ), {
+        name: 'AbortError',
+      }, `${op} should abort with pre-aborted abort controller`).then(common.mustCall());
+    }
+  }
 }
 {
   // Error cases
-  assert.rejects(async () => {
-    await Readable.from([1]).every(1);
-  }, /ERR_INVALID_ARG_TYPE/).then(common.mustCall());
-
-  assert.rejects(async () => {
-    await Readable.from([1]).every((x) => x, 1);
-  }, /ERR_INVALID_ARG_TYPE/).then(common.mustCall());
-
-  assert.rejects(async () => {
-    await Readable.from([1]).every((x) => x, {
-      signal: true
-    });
-  }, /ERR_INVALID_ARG_TYPE/).then(common.mustCall());
-
-  assert.rejects(async () => {
-    await Readable.from([1]).every((x) => x, {
-      concurrency: 'Foo'
-    });
-  }, /ERR_OUT_OF_RANGE/).then(common.mustCall());
+  for (const op of ['some', 'every', 'find']) {
+    assert.rejects(async () => {
+      await Readable.from([1])[op](1);
+    }, /ERR_INVALID_ARG_TYPE/, `${op} should throw for invalid function`).then(common.mustCall());
+    assert.rejects(async () => {
+      await Readable.from([1])[op]((x) => x, {
+        concurrency: 'Foo'
+      });
+    }, /ERR_OUT_OF_RANGE/, `${op} should throw for invalid concurrency`).then(common.mustCall());
+    assert.rejects(async () => {
+      await Readable.from([1])[op]((x) => x, 1);
+    }, /ERR_INVALID_ARG_TYPE/, `${op} should throw for invalid concurrency`).then(common.mustCall());
+    assert.rejects(async () => {
+      await Readable.from([1])[op]((x) => x, {
+        signal: true
+      });
+    }, /ERR_INVALID_ARG_TYPE/, `${op} should throw for invalid signal`).then(common.mustCall());
+  }
 }
