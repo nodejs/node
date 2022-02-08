f30b5e3a6dc0bfa51af9f187c22ecc821bf575cc
Ikko Ashimine <eltociear@gmail.com>
2022-02-09 01:41:40 +0900

    tools: fix typo in prefer-primordials.js
    replacable -> replaceable
    
    PR-URL: https://github.com/nodejs/node/pull/41891
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Qingyu Deng <i@ayase-lab.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- tools/eslint-rules/prefer-primordials.js
+++ tools/eslint-rules/prefer-primordials.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview We shouldn't use global built-in object for security and
- *               performance reason. This linter rule reports replacable codes
+ *               performance reason. This linter rule reports replaceable codes
  *               that can be replaced with primordials.
  * @author Leko <leko.noor@gmail.com>
  */
