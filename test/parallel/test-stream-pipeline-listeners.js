97fb400489ed4c1eda8e37e50ed166e5004d7c10
meixg <meixg@foxmail.com>
2022-02-14 00:36:12 +0800

    stream: need to cleanup event listeners if last stream is readable
    Apply suggestions from code review.
    Add test case for async generators.
    Update doc.

--- test/parallel/test-stream-pipeline-listeners.js
+++ test/parallel/test-stream-pipeline-listeners.js
@@ -4,26 +4,24 @@ const common = require('../common');
 const { pipeline, Duplex, PassThrough, Writable } = require('stream');
 const assert = require('assert');
 
+process.on('uncaughtException', common.mustCall((err) => {
+  assert.strictEqual(err.message, 'no way');
+}, 2));
+
+// Ensure that listeners is removed if last stream is readble
+// And other stream's listeners unchanged
 const a = new PassThrough();
 a.end('foobar');
-
 const b = new Duplex({
   write(chunk, encoding, callback) {
     callback();
   }
 });
-
-process.on('uncaughtException', common.mustCall((err) => {
-  assert.strictEqual(err.message, 'no way');
-}, 1));
-
 pipeline(a, b, common.mustCall((error) => {
   if (error) {
     assert.ifError(error);
   }
 
-  // Ensure that listeners is removed if last stream is readble
-  // And other stream's listeners unchanged
   assert(a.listenerCount('error') > 0);
   assert.strictEqual(b.listenerCount('error'), 0);
   setTimeout(() => {
@@ -32,23 +30,47 @@ pipeline(a, b, common.mustCall((error) => {
   }, 100);
 }));
 
-// If last stream is not readable, will not throw and remove listeners
+// Async generators
 const c = new PassThrough();
 c.end('foobar');
-const d = new Writable({
+const d = pipeline(
+  c,
+  async function* (source) {
+    for await (const chunk of source) {
+      yield String(chunk).toUpperCase();
+    }
+  },
+  common.mustCall((error) => {
+    if (error) {
+      assert.ifError(error);
+    }
+
+    assert(c.listenerCount('error') > 0);
+    assert.strictEqual(d.listenerCount('error'), 0);
+    setTimeout(() => {
+      assert.strictEqual(b.listenerCount('error'), 0);
+      d.destroy(new Error('no way'));
+    }, 100);
+  })
+);
+
+// If last stream is not readable, will not throw and remove listeners
+const e = new PassThrough();
+e.end('foobar');
+const f = new Writable({
   write(chunk, encoding, callback) {
     callback();
   }
 });
-pipeline(c, d, common.mustCall((error) => {
+pipeline(e, f, common.mustCall((error) => {
   if (error) {
     assert.ifError(error);
   }
 
-  assert(c.listenerCount('error') > 0);
-  assert(d.listenerCount('error') > 0);
+  assert(e.listenerCount('error') > 0);
+  assert(f.listenerCount('error') > 0);
   setTimeout(() => {
-    assert(d.listenerCount('error') > 0);
-    d.destroy(new Error('no way'));
+    assert(f.listenerCount('error') > 0);
+    f.destroy(new Error('no way'));
   }, 100);
 }));
