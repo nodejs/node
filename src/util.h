f80cae8be5b4ff694e61e0b00a4a8329994714fe
Anna Henningsen <anna@addaleax.net>
2022-02-14 00:44:30 +0100

    src: merge ToJsSet into ToV8Value
    This addresses a `TODO` comment, and makes use of the opportunity
    to also clean up our `MaybeLocal` handling in this area and
    start accepting `std::string_view` where we accept `std::string`.
    
    PR-URL: https://github.com/nodejs/node/pull/41757
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- src/util.h
+++ src/util.h
@@ -36,7 +36,9 @@
 #include <limits>
 #include <memory>
 #include <string>
+#include <string_view>
 #include <type_traits>
+#include <set>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -644,7 +646,7 @@ using DeleteFnPtr = typename FunctionDeleter<T, function>::Pointer;
 std::vector<std::string> SplitString(const std::string& in, char delim);
 
 inline v8::MaybeLocal<v8::Value> ToV8Value(v8::Local<v8::Context> context,
-                                           const std::string& str,
+                                           std::string_view str,
                                            v8::Isolate* isolate = nullptr);
 template <typename T, typename test_for_number =
     typename std::enable_if<std::numeric_limits<T>::is_specialized, bool>::type>
@@ -654,6 +656,10 @@ inline v8::MaybeLocal<v8::Value> ToV8Value(v8::Local<v8::Context> context,
 template <typename T>
 inline v8::MaybeLocal<v8::Value> ToV8Value(v8::Local<v8::Context> context,
                                            const std::vector<T>& vec,
+                                           v8::Isolate* isolate = nullptr);
+template <typename T>
+inline v8::MaybeLocal<v8::Value> ToV8Value(v8::Local<v8::Context> context,
+                                           const std::set<T>& set,
                                            v8::Isolate* isolate = nullptr);
 template <typename T, typename U>
 inline v8::MaybeLocal<v8::Value> ToV8Value(v8::Local<v8::Context> context,
