84b1e4fba735f128e10232705e7f85e17c578027
Tobias Nießen <tniessen@tnie.de>
2022-02-18 00:11:54 +0100

    crypto: check result of BIO_new in X509ToObject
    Match other call sites of BIO_new(BIO_s_mem()) and CHECK the result of
    the call.
    
    Refs: https://github.com/nodejs/node/commit/fb3a9cd0d8e8cf5f3120012a28643230b31c1f83
    
    PR-URL: https://github.com/nodejs/node/pull/41979
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- src/crypto/crypto_common.cc
+++ src/crypto/crypto_common.cc
@@ -1351,6 +1351,7 @@ MaybeLocal<Object> X509ToObject(
   Local<Object> info = Object::New(env->isolate());
 
   BIOPointer bio(BIO_new(BIO_s_mem()));
+  CHECK(bio);
 
   if (names_as_string) {
     // TODO(tniessen): this branch should not have to exist. It is only here
