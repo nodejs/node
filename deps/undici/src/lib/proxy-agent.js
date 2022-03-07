04cfdf24006c66af1576373dd405f7a23a455307
Michaël Zasso <targos@protonmail.com>
2022-03-07 16:36:29 +0100

    deps: update undici to 4.15.1
    This also adds a script to automate the update and includes the sources
    included in the npm tarball.
    
    PR-URL: https://github.com/nodejs/node/pull/42246
    Fixes: https://github.com/nodejs/node/issues/42199
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Mestery <mestery@protonmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Tierney Cyren <hello@bnb.im>

---
+++ deps/undici/src/lib/proxy-agent.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const { kProxy } = require('./core/symbols')
+const url = require('url')
+const Agent = require('./agent')
+const Dispatcher = require('./dispatcher')
+const { InvalidArgumentError } = require('./core/errors')
+
+const kAgent = Symbol('proxy agent')
+
+class ProxyAgent extends Dispatcher {
+  constructor (opts) {
+    super(opts)
+    this[kProxy] = buildProxyOptions(opts)
+    this[kAgent] = new Agent(opts)
+  }
+
+  dispatch (opts, handler) {
+    const { host } = url.parse(opts.origin)
+    return this[kAgent].dispatch(
+      {
+        ...opts,
+        origin: this[kProxy].uri,
+        path: opts.origin + opts.path,
+        headers: {
+          ...opts.headers,
+          host
+        }
+      },
+      handler
+    )
+  }
+
+  async close () {
+    await this[kAgent].close()
+  }
+}
+
+function buildProxyOptions (opts) {
+  if (typeof opts === 'string') {
+    opts = { uri: opts }
+  }
+
+  if (!opts || !opts.uri) {
+    throw new InvalidArgumentError('Proxy opts.uri is mandatory')
+  }
+
+  return {
+    uri: opts.uri,
+    protocol: opts.protocol || 'https'
+  }
+}
+
+module.exports = ProxyAgent
---
+++ deps/undici/src/types/errors.d.ts
@@ -0,0 +1,79 @@
+export = Errors
+import { SocketInfo } from './client'
+
+declare namespace Errors {
+  export class UndiciError extends Error { }
+
+  /** A header exceeds the `headersTimeout` option. */
+  export class HeadersTimeoutError extends UndiciError {
+    name: 'HeadersTimeoutError';
+    code: 'UND_ERR_HEADERS_TIMEOUT';
+  }
+
+  /** A body exceeds the `bodyTimeout` option. */
+  export class BodyTimeoutError extends UndiciError {
+    name: 'BodyTimeoutError';
+    code: 'UND_ERR_BODY_TIMEOUT';
+  }
+
+  /** A socket exceeds the `socketTimeout` option. */
+  export class SocketTimeoutError extends UndiciError {
+    name: 'SocketTimeoutError';
+    code: 'UND_ERR_SOCKET_TIMEOUT';
+  }
+
+  /** Passed an invalid argument. */
+  export class InvalidArgumentError extends UndiciError {
+    name: 'InvalidArgumentError';
+    code: 'UND_ERR_INVALID_ARG';
+  }
+
+  /** Returned an invalid value. */
+  export class InvalidReturnError extends UndiciError {
+    name: 'InvalidReturnError';
+    code: 'UND_ERR_INVALID_RETURN_VALUE';
+  }
+
+  /** The request has been aborted by the user. */
+  export class RequestAbortedError extends UndiciError {
+    name: 'RequestAbortedError';
+    code: 'UND_ERR_ABORTED';
+  }
+
+  /** Expected error with reason. */
+  export class InformationalError extends UndiciError {
+    name: 'InformationalError';
+    code: 'UND_ERR_INFO';
+  }
+
+  /** Body does not match content-length header. */
+  export class RequestContentLengthMismatchError extends UndiciError {
+    name: 'RequestContentLengthMismatchError';
+    code: 'UND_ERR_REQ_CONTENT_LENGTH_MISMATCH';
+  }
+
+  /** Trying to use a destroyed client. */
+  export class ClientDestroyedError extends UndiciError {
+    name: 'ClientDestroyedError';
+    code: 'UND_ERR_DESTROYED';
+  }
+
+  /** Trying to use a closed client. */
+  export class ClientClosedError extends UndiciError {
+    name: 'ClientClosedError';
+    code: 'UND_ERR_CLOSED';
+  }
+
+  /** There is an error with the socket. */
+  export class SocketError extends UndiciError {
+    name: 'SocketError';
+    code: 'UND_ERR_SOCKET';
+    socket: SocketInfo | null
+  }
+
+  /** Encountered unsupported functionality. */
+  export class NotSupportedError extends UndiciError {
+    name: 'NotSupportedError';
+    code: 'UND_ERR_NOT_SUPPORTED';
+  }
+}
