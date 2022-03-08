3cc06de597a7395aebaaa478957d633fb9fafead
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-03-08 23:40:18 +0100

    test: use global webcrypto for WPT tests
    PR-URL: https://github.com/nodejs/node/pull/42236
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Mestery <mestery@protonmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Anto Aravinth <anto.aravinth.cse@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/wpt/test-webcrypto.js
+++ test/wpt/test-webcrypto.js
@@ -9,45 +9,6 @@ const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('WebCryptoAPI');
 
 // Set Node.js flags required for the tests.
-runner.setFlags(['--expose-internals']);
-
-// Set a script that will be executed in the worker before running the tests.
-runner.setInitScript(`
-  const {
-    Crypto,
-    SubtleCrypto,
-    crypto,
-  } = require('internal/crypto/webcrypto');
-  const { internalBinding } = require('internal/test/binding');
-  const { DOMException } = internalBinding('messaging');
-  global.DOMException = DOMException;
-
-  Object.defineProperties(global, {
-    Crypto: {
-      value: Crypto,
-      configurable: true,
-      writable: true,
-      enumerable: false,
-    },
-    SubtleCrypto: {
-      value: SubtleCrypto,
-      configurable: true,
-      writable: true,
-      enumerable: false,
-    },
-    CryptoKey: {
-      value: crypto.CryptoKey,
-      configurable: true,
-      writable: true,
-      enumerable: false,
-    },
-    crypto: {
-      value: crypto,
-      configurable: true,
-      writable: true,
-      enumerable: false,
-    },
-  });
-`);
+runner.setFlags(['--experimental-global-webcrypto']);
 
 runner.runJsTests();
