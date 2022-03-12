091e5a3b08d1b3148e9d53160d7780f96d9f6915
Bradley Farias <bradley.meck@gmail.com>
2022-03-12 08:58:52 -0600

    fixup: reviews and make suggestion copy paste friendly

--- test/es-module/test-esm-basic-imports.mjs
+++ test/es-module/test-esm-basic-imports.mjs
@@ -1,10 +1,7 @@
 import '../common/index.mjs';
 import assert from 'assert';
 import ok from '../fixtures/es-modules/test-esm-ok.mjs';
-import * as okShebangNs from './test-esm-shebang.mjs';
-// encode the '.'
-import * as okShebangPercentNs from './test-esm-shebang%2emjs';
+import okShebang from './test-esm-shebang.mjs';
 
 assert(ok);
-assert(okShebangNs.default);
-assert.strict.equal(okShebangNs, okShebangPercentNs);
+assert(okShebang);
