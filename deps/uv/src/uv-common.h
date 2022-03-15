28bf0317c2e2e745f38275d930e3ead9e301ee1a
Luigi Pinca <luigipinca@gmail.com>
2022-03-15 09:34:01 +0100

    deps: upgrade to libuv 1.44.2
    Notable changes:
    
    - Build regression fixes for various platform updates
      (https://github.com/libuv/libuv/pull/3428,
      https://github.com/libuv/libuv/pull/3419,
      https://github.com/libuv/libuv/pull/3423,
      https://github.com/libuv/libuv/pull/3413,
      https://github.com/libuv/libuv/pull/3431)
    - Support for GNU/Hurd (https://github.com/libuv/libuv/pull/3450)
    - Release tool improvements
      (https://github.com/libuv/libuv-release-tool/pull/13)
    - Better performing rw locks on Win32 (https://github.com/libuv/libuv/pull/3383)
    - Support for posix_spawn API (https://github.com/libuv/libuv/pull/3257)
    - Fix regression on OpenBSD (https://github.com/libuv/libuv/pull/3506)
    - Add uv_available_parallelism() (https://github.com/libuv/libuv/pull/3499)
    - Don't use thread-unsafe strtok() (https://github.com/libuv/libuv/pull/3524)
    - Fix hang after NOTE_EXIT (https://github.com/libuv/libuv/pull/3521)
    - Better align order-of-events behavior between platforms
      (https://github.com/libuv/libuv/pull/3598)
    - Fix fs event not fired if the watched file is moved/removed/recreated
      (https://github.com/libuv/libuv/pull/3540)
    - Fix pipe resource leak if closed during connect (and other bugs)
      (https://github.com/libuv/libuv/pull/3611)
    - Don't error when killing a zombie process
      (https://github.com/libuv/libuv/pull/3625)
    - Avoid posix_spawnp() cwd bug (https://github.com/libuv/libuv/pull/3597)
    - Skip EVFILT_PROC events when invalidating events for an fd
      (https://github.com/libuv/libuv/pull/3629)
    
    Fixes: https://github.com/nodejs/node/issues/42290
    PR-URL: https://github.com/nodejs/node/pull/42340
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- deps/uv/src/uv-common.h
+++ deps/uv/src/uv-common.h
@@ -130,7 +130,10 @@ enum {
   UV_SIGNAL_ONE_SHOT                    = 0x02000000,
 
   /* Only used by uv_poll_t handles. */
-  UV_HANDLE_POLL_SLOW                   = 0x01000000
+  UV_HANDLE_POLL_SLOW                   = 0x01000000,
+
+  /* Only used by uv_process_t handles. */
+  UV_HANDLE_REAP                        = 0x10000000
 };
 
 int uv__loop_configure(uv_loop_t* loop, uv_loop_option option, va_list ap);
