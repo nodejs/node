1a2391d3485365783161499a2c6a2ca940496216
Tobias Nießen <tniessen@tnie.de>
2022-03-15 11:19:14 +0000

    test: improve https_renew_cert.sh script
    - To avoid unnecessarily large diffs, only generate a new private key
      if necessary. Otherwise, reuse the existing private key and only
      issue a new certificate.
    - Remove an unnecessary conversion step using openssl rsa and the
      intermediate rsa.pem and csr.pem files.
    - Extend the certificate validity from 1 year to 10 years.
    - Show a text representation of the issued certificate upon completion
      such that the user can verify the validity.
    - Make the script executable.
    - Use "#!/usr/bin/env bash" instead of "#!/bin/bash".
    - Allow the script to be called from any directory.
    
    Refs: https://github.com/nodejs/node/pull/42342
    Refs: https://github.com/nodejs/node/pull/37990

--- test/fixtures/keys/selfsigned-no-keycertsign/https_renew_cert.sh
+++ test/fixtures/keys/selfsigned-no-keycertsign/https_renew_cert.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
-openssl genrsa -out rsa.pem 2048
-openssl rsa -in rsa.pem -out key.pem
-openssl req -sha256 -new -key key.pem -out csr.pem -subj "/CN=localhost"
-openssl x509 -req -extfile cert.conf -extensions v3_req -days 365 -in csr.pem -signkey key.pem -out cert.pem
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s inherit_errexit
 
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+if [ ! -f key.pem ]; then
+  openssl genrsa -out key.pem 2048
+fi
+
+openssl req -sha256 -new -key key.pem -subj "/CN=localhost" | \
+  openssl x509 -req -extfile cert.conf -extensions v3_req -days 3650 -signkey key.pem -out cert.pem
+openssl x509 -in cert.pem -noout -text
