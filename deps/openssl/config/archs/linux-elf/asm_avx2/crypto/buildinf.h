55e293e05fac764eae7f0a533125c3e3a067a092
Hassaan Pasha <pasha.hassaan@gmail.com>
2022-03-16 05:05:03 +0000

    deps: update archs files for quictls/openssl-3.0.2+quic
    After an OpenSSL source update, all the config files need to be
    regenerated and committed by:
        $ make -C deps/openssl/config
        $ git add deps/openssl/config/archs
        $ git add deps/openssl/openssl
        $ git commit
    
    PR-URL: https://github.com/nodejs/node/pull/42356
    Refs: https://mta.openssl.org/pipermail/openssl-announce/2022-March/000217.html
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- deps/openssl/config/archs/linux-elf/asm_avx2/crypto/buildinf.h
+++ deps/openssl/config/archs/linux-elf/asm_avx2/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-elf"
-#define DATE "built on: Tue Dec 14 22:56:49 2021 UTC"
+#define DATE "built on: Wed Mar 16 04:56:30 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
--- deps/openssl/config/archs/solaris-x86-gcc/no-asm/crypto/buildinf.h
+++ deps/openssl/config/archs/solaris-x86-gcc/no-asm/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: solaris-x86-gcc"
-#define DATE "built on: Tue Dec 14 23:06:16 2021 UTC"
+#define DATE "built on: Wed Mar 16 05:01:51 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
