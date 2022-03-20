59eff1ff8763a3279e9f0ef8ea0d944e4af603d0
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2022-03-20 19:13:47 +0100

    WIP: switch specifier resolution warning to custom message
    IRL, both internal and external instances of ESMLoader happen. in the test, only 1 external happens and then the test fails.

--- test/es-module/test-esm-experimental-warnings.mjs
+++ test/es-module/test-esm-experimental-warnings.mjs
@@ -9,23 +9,30 @@ for (
   const [experiment, arg] of [
     [/Custom ESM Loaders/, `--experimental-loader=${fileURL('es-module-loaders', 'hooks-custom.mjs')}`],
     [/Network Imports/, '--experimental-network-imports'],
-    [/Specifier Resolution/, '--experimental-specifier-resolution=node'],
+    [/specifier resolution/, '--experimental-specifier-resolution=node'],
   ]
 ) {
+  const input = `import ${JSON.stringify(fileURL('es-module-loaders','module-named-exports.mjs'))}`;
+console.log({ input })
   const child = spawn(execPath, [
     arg,
     '--input-type=module',
     '--eval',
-    'const foo = "a"',
+    input,
   ]);
 
   let stderr = '';
   child.stderr.setEncoding('utf8');
   child.stderr.on('data', (data) => { stderr += data; });
+
+  let stdout = '';
+  child.stdout.setEncoding('utf8');
+  child.stdout.on('data', (data) => { stdout += data; });
   child.on('close', mustCall((code, signal) => {
+console.log({ experiment, code, signal, stderr, stdout })
     strictEqual(code, 0);
     strictEqual(signal, null);
-    match(stderr, /ExperimentalWarning:/);
+    match(stderr, /ExperimentalWarning/);
     match(stderr, experiment);
   }));
 }
