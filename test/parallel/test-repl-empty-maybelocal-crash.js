135f144cf07e2769ba4f96f3ad8c00e4f9112144
Darshan Sen <raisinten@gmail.com>
2022-03-20 15:03:25 +0530

    src,inspector: fix empty MaybeLocal crash
    Return early when the Inspector StringView to V8 String conversion fails
    and returns an empty MaybeLocal instead of running the invalid
    ToLocalChecked() assertion.
    
    Fixes: https://github.com/nodejs/node/issues/42407
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

---
+++ test/parallel/test-repl-empty-maybelocal-crash.js
@@ -0,0 +1,16 @@
+'use strict';
+require('../common');
+
+// The process should not crash when the REPL receives the string, 'ss'.
+// Test for https://github.com/nodejs/node/issues/42407.
+
+const repl = require('repl');
+
+const r = repl.start();
+
+r.write('var buf = Buffer.from({length:200e6},(_,i) => i%256);\n');
+r.write('var ss = buf.toString("binary");\n');
+r.write('ss');
+r.write('.');
+
+r.close();
