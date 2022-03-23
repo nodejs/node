aa7445a186c54138bd6b530b865ca4c5b3d4e18a
Colin Ihrig <cjihrig@gmail.com>
2022-03-23 18:44:21 -0400

    Update test/parallel/test-runner-import-no-scheme.js

--- test/parallel/test-runner-import-no-scheme.js
+++ test/parallel/test-runner-import-no-scheme.js
@@ -40,7 +40,7 @@ function test(argv) {
 }
 
 test(['-e', 'console.log(require("test"))']);
-test(['-e', 'import("test").then(m=>console.log(m.default))'])
+test(['-e', 'import("test").then(m=>console.log(m.default))']);
 test(['--input-type=module', '-e', 'import test from "test";console.log(test)']);
 test(['--input-type=module', '-e', 'console.log((await import("test")).default)']);
 
