364c9ceccecd43f509cf9d7ab48bf71c1e0c3722
Darshan Sen <raisinten@gmail.com>
2022-03-27 16:44:23 +0530

    Revert "test: set a timeout value"
    This reverts commit a363b6e02dd8f30334234000f11b95f4b9c42ced.

--- test/parallel/test-repl-empty-maybelocal-crash.js
+++ test/parallel/test-repl-empty-maybelocal-crash.js
@@ -1,23 +1,16 @@
 'use strict';
-const common = require('../common');
+require('../common');
 
 // The process should not crash when the REPL receives the string, 'ss'.
 // Test for https://github.com/nodejs/node/issues/42407.
 
-if (process.argv[2] === 'child') {
-  const repl = require('repl');
+const repl = require('repl');
 
-  const r = repl.start();
+const r = repl.start();
 
-  r.write('var buf = Buffer.from({length:200e6},(_,i) => i%256);\n');
-  r.write('var ss = buf.toString("binary");\n');
-  r.write('ss');
-  r.write('.');
+r.write('var buf = Buffer.from({length:200e6},(_,i) => i%256);\n');
+r.write('var ss = buf.toString("binary");\n');
+r.write('ss');
+r.write('.');
 
-  r.close();
-  return;
-}
-
-require('child_process')
-  .fork(__filename, ['child'], { timeout: common.platformTimeout(15000) })
-  .on('exit', (exitCode) => process.exit(exitCode === null ? 1 : exitCode));
+r.close();
