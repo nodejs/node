a363b6e02dd8f30334234000f11b95f4b9c42ced
Darshan Sen <raisinten@gmail.com>
2022-03-27 12:28:34 +0530

    test: set a timeout value
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test/parallel/test-repl-empty-maybelocal-crash.js
+++ test/parallel/test-repl-empty-maybelocal-crash.js
@@ -1,16 +1,23 @@
 'use strict';
-require('../common');
+const common = require('../common');
 
 // The process should not crash when the REPL receives the string, 'ss'.
 // Test for https://github.com/nodejs/node/issues/42407.
 
-const repl = require('repl');
+if (process.argv[2] === 'child') {
+  const repl = require('repl');
 
-const r = repl.start();
+  const r = repl.start();
 
-r.write('var buf = Buffer.from({length:200e6},(_,i) => i%256);\n');
-r.write('var ss = buf.toString("binary");\n');
-r.write('ss');
-r.write('.');
+  r.write('var buf = Buffer.from({length:200e6},(_,i) => i%256);\n');
+  r.write('var ss = buf.toString("binary");\n');
+  r.write('ss');
+  r.write('.');
 
-r.close();
+  r.close();
+  return;
+}
+
+require('child_process')
+  .fork(__filename, ['child'], { timeout: common.platformTimeout(15000) })
+  .on('exit', (exitCode) => process.exit(exitCode === null ? 1 : exitCode));
