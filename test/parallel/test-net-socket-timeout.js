d60a6f050de7a6e49e7f3e36a4826e1750408f63
Daeyeon Jeong <daeyeon.dev@gmail.com>
2022-03-29 05:37:31 +0900

    test: fix typos in test/parallel
    This pr fixes typos in some parallel tests.
    
    PR-URL: https://github.com/nodejs/node/pull/42502
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Qingyu Deng <i@ayase-lab.com>
    Reviewed-By: Mestery <mestery@protonmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-net-socket-timeout.js
+++ test/parallel/test-net-socket-timeout.js
@@ -53,9 +53,9 @@ for (let i = 0; i < validDelays.length; i++) {
 }
 
 for (let i = 0; i < invalidCallbacks.length; i++) {
-  [0, 1].forEach((mesc) =>
+  [0, 1].forEach((msec) =>
     assert.throws(
-      () => s.setTimeout(mesc, invalidCallbacks[i]),
+      () => s.setTimeout(msec, invalidCallbacks[i]),
       {
         code: 'ERR_INVALID_ARG_TYPE',
         name: 'TypeError',
