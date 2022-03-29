a840b01e321f5e971b8e7a1879b0d84958935fa2
Michael Dawson <mdawson@devrus.com>
2022-03-29 17:22:07 -0400

    Update tools/install.py

--- tools/install.py
+++ tools/install.py
@@ -144,7 +144,6 @@ def files(action):
     else:
       output_lib = 'libnode.' + variables.get('shlib_suffix')
       action([output_prefix + output_lib], 'lib/' + output_lib)
-  
   if 'true' == variables.get('node_use_dtrace'):
     action(['out/Release/node.d'], 'lib/dtrace/node.d')
 
