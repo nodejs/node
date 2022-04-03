0ad7f4b4b10ce03f96107b6e8a02beda2560c047
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-04-03 11:40:21 +0200

    test: remove hack for `atob` and `btoa` WPT tests
    PR-URL: https://github.com/nodejs/node/pull/42540
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Khaidi Chu <i@2333.moe>
    Reviewed-By: Mestery <mestery@protonmail.com>

--- test/wpt/test-atob.js
+++ test/wpt/test-atob.js
@@ -5,15 +5,4 @@ const { WPTRunner } = require('../common/wpt');
 
 const runner = new WPTRunner('html/webappapis/atob');
 
-// Needed to access to DOMException.
-runner.setFlags(['--expose-internals']);
-
-// Set a script that will be executed in the worker before running the tests.
-runner.setInitScript(`
-  const { internalBinding } = require('internal/test/binding');
-  const { atob, btoa } = require('buffer');
-  const { DOMException } = internalBinding('messaging');
-  global.DOMException = DOMException;
-`);
-
 runner.runJsTests();
