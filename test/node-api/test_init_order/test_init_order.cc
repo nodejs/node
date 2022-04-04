ec61c248e2665e28a3fa0a45a13a3851b6ecc418
Vladimir Morozov <vmorozov@microsoft.com>
2022-04-04 08:51:04 -0700

    Remove C++17 specific code from tests

--- test/node-api/test_init_order/test_init_order.cc
+++ test/node-api/test_init_order/test_init_order.cc
@@ -3,34 +3,39 @@
 #include <string>
 #include "../../js-native-api/common.h"
 
+// This test verifies that use of the NAPI_MODULE in C++ code does not
+// interfere with the C++ dynamic static initializers.
+
 namespace {
 
-inline std::string testString = "123";
-
-struct ValueHolder {
-  int value{42};
+// This class uses dynamic static initializers for the test.
+// In production code developers must avoid dynamic static initializers because
+// they affect the start up time. They must prefer static initialization such as
+// use of constexpr functions or classes with constexpr constructors. E.g.
+// instead of using std::string, it is preferrable to use const char[], or
+// constexpr std::string_view starting with C++17, or even constexpr
+// std::string starting with C++20.
+struct MyClass {
+  static const std::unique_ptr<int> valueHolder;
+  static const std::string testString;
 };
 
-class MyClass {
- public:
-  // Ensure that the static variable is initialized by a dynamic static
-  // initializer.
-  static std::unique_ptr<ValueHolder> valueHolder;
-};
-
-std::unique_ptr<ValueHolder> MyClass::valueHolder{new ValueHolder()};
+const std::unique_ptr<int> MyClass::valueHolder =
+    std::unique_ptr<int>(new int(42));
+// NOLINTNEXTLINE(runtime/string)
+const std::string MyClass::testString = std::string("123");
 
 }  // namespace
 
 EXTERN_C_START
 napi_value Init(napi_env env, napi_value exports) {
   napi_value cppIntValue, cppStringValue;
-  NODE_API_CALL(
-      env, napi_create_int32(env, MyClass::valueHolder->value, &cppIntValue));
+  NODE_API_CALL(env,
+                napi_create_int32(env, *MyClass::valueHolder, &cppIntValue));
   NODE_API_CALL(
       env,
       napi_create_string_utf8(
-          env, testString.c_str(), NAPI_AUTO_LENGTH, &cppStringValue));
+          env, MyClass::testString.c_str(), NAPI_AUTO_LENGTH, &cppStringValue));
 
   napi_property_descriptor descriptors[] = {
       DECLARE_NODE_API_PROPERTY_VALUE("cppIntValue", cppIntValue),
