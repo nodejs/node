4d4292d5987658a3b9c29b0add92ab55b46a827b
Daeyeon Jeong <daeyeon.dev@gmail.com>
2022-04-04 16:13:08 +0900

    fix: use Array.prototype.includes instead of some

--- test/parallel/test-dns-lookupService-promises.js
+++ test/parallel/test-dns-lookupService-promises.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 const dnsPromises = require('dns').promises;
 
 dnsPromises.lookupService('127.0.0.1', 22).then(common.mustCall((result) => {
-  assert(['ssh', '22'].some((x) => x === result.service));
+  assert(['ssh', '22'].includes(result.service));
   assert.strictEqual(typeof result.hostname, 'string');
   assert.notStrictEqual(result.hostname.length, 0);
 }));
