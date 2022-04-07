9be705c0baeb054ead297faea9d86c210fe1048e
Richard Lau <rlau@redhat.com>
2022-04-07 12:40:46 -0400

    test: only skip slow tests on Raspberry Pi devices
    Detect the Raspberry Pi devices in the Node.js CI and only skip the
    slow tests on those instead of all armv7l devices.

--- test/pummel/test-dh-regr.js
+++ test/pummel/test-dh-regr.js
@@ -26,8 +26,8 @@ if (!common.hasCrypto) {
   common.skip('missing crypto');
 }
 
-if (process.config.variables.arm_version === '7') {
-  common.skip('Too slow for armv7 bots');
+if (common.isPi) {
+  common.skip('Too slow for Raspberry Pi devices');
 }
 
 const assert = require('assert');
