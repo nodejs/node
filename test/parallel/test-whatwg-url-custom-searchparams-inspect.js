57e04b8b99abf3b61a3720adbed383988631b4e0
Yoshiki Kurihara <yosyos0306@gmail.com>
2022-04-13 17:35:09 +0900

    test: improve `internal/url.js` coverage
    PR-URL: https://github.com/nodejs/node/pull/42650
    Refs: https://coverage.nodejs.org/coverage-3c752648d4ef5510/lib/internal/url.js.html#L256
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Akhil Marsonya <akhil.marsonya27@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Zijian Liu <lxxyxzj@gmail.com>

--- test/parallel/test-whatwg-url-custom-searchparams-inspect.js
+++ test/parallel/test-whatwg-url-custom-searchparams-inspect.js
@@ -9,12 +9,20 @@ const util = require('util');
 const sp = new URLSearchParams('?a=a&b=b&b=c');
 assert.strictEqual(util.inspect(sp),
                    "URLSearchParams { 'a' => 'a', 'b' => 'b', 'b' => 'c' }");
+assert.strictEqual(util.inspect(sp, { depth: -1 }), '[Object]');
+assert.strictEqual(
+  util.inspect(sp, { breakLength: 1 }),
+  "URLSearchParams {\n  'a' => 'a',\n  'b' => 'b',\n  'b' => 'c' }"
+);
 assert.strictEqual(util.inspect(sp.keys()),
                    "URLSearchParams Iterator { 'a', 'b', 'b' }");
 assert.strictEqual(util.inspect(sp.values()),
                    "URLSearchParams Iterator { 'a', 'b', 'c' }");
 assert.strictEqual(util.inspect(sp.keys(), { breakLength: 1 }),
                    "URLSearchParams Iterator {\n  'a',\n  'b',\n  'b' }");
+assert.throws(() => sp[util.inspect.custom].call(), {
+  code: 'ERR_INVALID_THIS',
+});
 
 const iterator = sp.entries();
 assert.strictEqual(util.inspect(iterator),
@@ -27,3 +35,5 @@ iterator.next();
 iterator.next();
 assert.strictEqual(util.inspect(iterator),
                    'URLSearchParams Iterator {  }');
+const emptySp = new URLSearchParams();
+assert.strictEqual(util.inspect(emptySp), 'URLSearchParams {}');
