1f500a33ab9640ab6f4f64b24d02c83e24571982
Daeyeon Jeong <daeyeon.dev@gmail.com>
2022-04-14 17:46:46 +0900

    fix: lint-js error

--- test/parallel/test-child-process-hasref.js
+++ test/parallel/test-child-process-hasref.js
@@ -2,7 +2,7 @@
 
 const common = require('../common');
 const assert = require('assert');
-const { spawn, ChildProcess } = require('child_process');
+const { spawn } = require('child_process');
 
 const command = common.isWindows ? 'dir' : 'ls';
 const child = spawn(command);
