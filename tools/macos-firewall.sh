4d8ad64c0b166aa60a445d58fd6d1386062a15b9
Livia Medeiros <74449973+LiviaMedeiros@users.noreply.github.com>
2022-04-15 06:08:55 +0800

    tools: lint osx shell scripts
    PR-URL: https://github.com/nodejs/node/pull/42712
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Mestery <mestery@protonmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- tools/macos-firewall.sh
+++ tools/macos-firewall.sh
@@ -3,16 +3,16 @@
 # popups asking to accept incoming network connections when
 # running tests.
 SFW="/usr/libexec/ApplicationFirewall/socketfilterfw"
-TOOLSDIR="`dirname \"$0\"`"
-TOOLSDIR="`( cd \"$TOOLSDIR\" && pwd) `"
-ROOTDIR="`( cd \"$TOOLSDIR/..\" && pwd) `"
+TOOLSDIR="$(dirname "$0")"
+TOOLSDIR="$(cd "$TOOLSDIR" && pwd)"
+ROOTDIR="$(cd "$TOOLSDIR/.." && pwd)"
 OUTDIR="$TOOLSDIR/../out"
 # Using cd and pwd here so that the path used for socketfilterfw does not
 # contain a '..', which seems to cause the rules to be incorrectly added
 # and they are not removed when this script is re-run. Instead the new
 # rules are simply appended. By using pwd we can get the full path
 # without '..' and things work as expected.
-OUTDIR="`( cd \"$OUTDIR\" && pwd) `"
+OUTDIR="$(cd "$OUTDIR" && pwd)"
 NODE_RELEASE="$OUTDIR/Release/node"
 NODE_DEBUG="$OUTDIR/Debug/node"
 NODE_LINK="$ROOTDIR/node"
