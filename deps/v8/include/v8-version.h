f030a66feffc39c54c3fe12e17a5653ed0097d75
Michaël Zasso <targos@protonmail.com>
2022-04-14 08:16:31 +0200

    deps: patch V8 to 10.1.124.8
    Refs: https://github.com/v8/v8/compare/10.1.124.6...10.1.124.8

--- deps/v8/include/v8-version.h
+++ deps/v8/include/v8-version.h
@@ -11,7 +11,7 @@
 #define V8_MAJOR_VERSION 10
 #define V8_MINOR_VERSION 1
 #define V8_BUILD_NUMBER 124
-#define V8_PATCH_LEVEL 6
+#define V8_PATCH_LEVEL 8
 
 // Use 1 for candidates and 0 otherwise.
 // (Boolean macro values are not supported by all preprocessors.)
