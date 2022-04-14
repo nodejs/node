9e9098c552940e4132969df697fd1f56bddcf075
Kohei Ueno <kohei.ueno119@gmail.com>
2022-04-15 07:20:51 +0900

    test: remove the legacy url parser function
    PR-URL: https://github.com/nodejs/node/pull/42656
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Akhil Marsonya <akhil.marsonya27@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/common/inspector-helper.js
+++ test/common/inspector-helper.js
@@ -5,8 +5,7 @@ const fs = require('fs');
 const http = require('http');
 const fixtures = require('../common/fixtures');
 const { spawn } = require('child_process');
-const { parse: parseURL } = require('url');
-const { pathToFileURL } = require('url');
+const { URL, pathToFileURL } = require('url');
 const { EventEmitter } = require('events');
 
 const _MAINSCRIPT = fixtures.path('loop.js');
@@ -418,7 +417,7 @@ class NodeInstance extends EventEmitter {
     return http.get({
       port,
       family: 4,
-      path: parseURL(devtoolsUrl).path,
+      path: new URL(devtoolsUrl).pathname,
       headers: {
         'Connection': 'Upgrade',
         'Upgrade': 'websocket',
