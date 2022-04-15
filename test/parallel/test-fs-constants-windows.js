71fbff0bf8849b26a838c7a6650b17d649424044
Liviu Ionescu <ilg@livius.net>
2022-04-15 14:48:41 +0300

    src: define S_IWUSR & S_IRUSR for Windows
    On Windows, most of the POSIX file mode definitions are not available.
    However, functionally equivalent read/write definitions exists, and
    `chmod()` can use them. This patch defines two aliases, so that these
    definintions are issued in `fs.constants`.
    
    https://github.com/nodejs/node/issues/41591

---
+++ test/parallel/test-fs-constants-windows.js
@@ -0,0 +1,10 @@
+'use strict';
+const common = require('../common');
+const fs = require('fs');
+const { assert } = require('console');
+
+// Check if the two constants accepted by chmod() on Windows are defined.
+if (common.isWindows) {
+  assert(fs.constants.S_IRUSR !== undefined)
+  assert(fs.constants.S_IWUSR !== undefined)
+}
