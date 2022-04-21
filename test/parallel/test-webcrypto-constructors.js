5fb69a8a46cc519e7797522689822c43af044334
Filip Skokan <panva.ip@gmail.com>
2022-04-21 18:23:22 +0200

    crypto: validate `this` in all webcrypto methods and getters

---
+++ test/parallel/test-webcrypto-constructors.js
@@ -0,0 +1,138 @@
+// Flags: --experimental-global-webcrypto
+'use strict';
+
+const common = require('../common');
+
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const assert = require('assert');
+
+// Test CryptoKey constructor
+{
+  assert.throws(() => new CryptoKey(), {
+    name: 'TypeError', message: 'Illegal constructor', code: 'ERR_ILLEGAL_CONSTRUCTOR'
+  });
+}
+
+// Test SubtleCrypto constructor
+{
+  assert.throws(() => new SubtleCrypto(), {
+    name: 'TypeError', message: 'Illegal constructor', code: 'ERR_ILLEGAL_CONSTRUCTOR'
+  });
+}
+
+// Test Crypto constructor
+{
+  assert.throws(() => new Crypto(), {
+    name: 'TypeError', message: 'Illegal constructor', code: 'ERR_ILLEGAL_CONSTRUCTOR'
+  });
+}
+
+const notCrypto = Reflect.construct(function() {}, [], Crypto);
+const notSubtle = Reflect.construct(function() {}, [], SubtleCrypto);
+
+// Test Crypto.prototype.subtle
+{
+  assert.throws(() => notCrypto.subtle, {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  });
+}
+
+// Test Crypto.prototype.randomUUID
+{
+  assert.throws(() => notCrypto.randomUUID(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  });
+}
+
+// Test Crypto.prototype.getRandomValues
+{
+  assert.throws(() => notCrypto.getRandomValues(new Uint8Array(12)), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  });
+}
+
+// Test SubtleCrypto.prototype.encrypt
+{
+  assert.rejects(() => notSubtle.encrypt(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.decrypt
+{
+  assert.rejects(() => notSubtle.decrypt(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.sign
+{
+  assert.rejects(() => notSubtle.sign(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.verify
+{
+  assert.rejects(() => notSubtle.verify(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.digest
+{
+  assert.rejects(() => notSubtle.digest(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.generateKey
+{
+  assert.rejects(() => notSubtle.generateKey(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.deriveKey
+{
+  assert.rejects(() => notSubtle.deriveKey(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.deriveBits
+{
+  assert.rejects(() => notSubtle.deriveBits(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.importKey
+{
+  assert.rejects(() => notSubtle.importKey(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.exportKey
+{
+  assert.rejects(() => notSubtle.exportKey(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.wrapKey
+{
+  assert.rejects(() => notSubtle.wrapKey(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
+
+// Test SubtleCrypto.prototype.unwrapKey
+{
+  assert.rejects(() => notSubtle.unwrapKey(), {
+    name: 'TypeError', code: 'ERR_INVALID_THIS',
+  }).then(common.mustCall());
+}
