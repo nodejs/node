42b640c6ff185c7fb565eb613c31460c4e9661aa
LiviaMedeiros <livia@cirno.name>
2022-04-21 16:42:59 +0800

    squash: allow offset === null

--- test/parallel/test-fs-read-offset-null.js
+++ test/parallel/test-fs-read-offset-null.js
@@ -14,17 +14,25 @@ const filepath = fixtures.path('x.txt');
 const buf = Buffer.alloc(1);
 // Reading only one character, hence buffer of one byte is enough.
 
+// Test is done by making sure the first letter in buffer is
+// same as first letter in file.
+// 120 is the ascii code of letter x.
+
 // Test for callback API.
 fs.open(filepath, 'r', common.mustSucceed((fd) => {
-  assert.throws(
-    () => fs.read(fd, { offset: null, buffer: buf }, () => {}),
-    { code: 'ERR_INVALID_ARG_TYPE' }
-  );
-  assert.throws(
-    () => fs.read(fd, buf, { offset: null }, () => {}),
-    { code: 'ERR_INVALID_ARG_TYPE' }
-  );
-  fs.close(fd, common.mustSucceed(() => {}));
+  fs.read(fd, { offset: null, buffer: buf },
+          common.mustSucceed((bytesRead, buffer) => {
+            assert.strictEqual(buffer[0], 120);
+            fs.close(fd, common.mustSucceed(() => {}));
+          }));
+}));
+
+fs.open(filepath, 'r', common.mustSucceed((fd) => {
+  fs.read(fd, buf, { offset: null },
+          common.mustSucceed((bytesRead, buffer) => {
+            assert.strictEqual(buffer[0], 120);
+            fs.close(fd, common.mustSucceed(() => {}));
+          }));
 }));
 
 let filehandle = null;
@@ -32,19 +40,14 @@ let filehandle = null;
 // Test for promise api
 (async () => {
   filehandle = await fsPromises.open(filepath, 'r');
-  assert.rejects(
-    async () => filehandle.read(buf, { offset: null }),
-    { code: 'ERR_INVALID_ARG_TYPE' }
-  );
+  const readObject = await filehandle.read(buf, { offset: null }, buf.length);
+  assert.strictEqual(readObject.buffer[0], 120);
 })()
 .then(common.mustCall())
 .finally(() => filehandle?.close());
 
 (async () => {
   filehandle = await fsPromises.open(filepath, 'r');
-
-  // In this test, null is interpreted as default options, not null offset.
-  // 120 is the ascii code of letter x.
   const readObject = await filehandle.read(buf, null);
   assert.strictEqual(readObject.buffer[0], 120);
 })()
