b5432ae6a9c1d7d7bd577bbf84f24a315e3e4264
OneNail <OneNail@yeah.net>
2022-04-29 05:06:42 +0800

    http2: compat support for array headers

--- test/parallel/test-http2-compat-serverresponse-writehead-array.js
+++ test/parallel/test-http2-compat-serverresponse-writehead-array.js
@@ -4,40 +4,92 @@ const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
 const assert = require('assert');
-const h2 = require('http2');
+const http2 = require('http2');
 
-// Http2ServerResponse.writeHead should support nested arrays
+// Http2ServerResponse.writeHead should support arrays and nested arrays
 
-const server = h2.createServer();
-server.listen(0, common.mustCall(() => {
-  const port = server.address().port;
-  server.once('request', common.mustCall((request, response) => {
-    const returnVal = response.writeHead(200, [
-      ['foo', 'bar'],
-      ['ABC', 123],
-    ]);
-    assert.strictEqual(returnVal, response);
-    response.end(common.mustCall(() => { server.close(); }));
-  }));
+{
+  const server = http2.createServer();
+  server.listen(0, common.mustCall(() => {
+    const port = server.address().port;
 
-  const url = `http://localhost:${port}`;
-  const client = h2.connect(url, common.mustCall(() => {
-    const headers = {
-      ':path': '/',
-      ':method': 'GET',
-      ':scheme': 'http',
-      ':authority': `localhost:${port}`
-    };
-    const request = client.request(headers);
-    request.on('response', common.mustCall((headers) => {
-      assert.strictEqual(headers.foo, 'bar');
-      assert.strictEqual(headers.abc, '123');
-      assert.strictEqual(headers[':status'], 200);
-    }, 1));
-    request.on('end', common.mustCall(() => {
-      client.close();
+    server.once('request', common.mustCall((request, response) => {
+      const returnVal = response.writeHead(200, [
+        ['foo', 'bar'],
+        ['ABC', 123],
+      ]);
+      assert.strictEqual(returnVal, response);
+      response.end(common.mustCall(() => { server.close(); }));
     }));
-    request.end();
-    request.resume();
+
+    const client = http2.connect(`http://localhost:${port}`, common.mustCall(() => {
+      const request = client.request();
+
+      request.on('response', common.mustCall((headers) => {
+        assert.strictEqual(headers.foo, 'bar');
+        assert.strictEqual(headers.abc, '123');
+        assert.strictEqual(headers[':status'], 200);
+      }, 1));
+      request.on('end', common.mustCall(() => {
+        client.close();
+      }));
+      request.end();
+      request.resume();
+    }));
   }));
-}));
+}
+
+{
+  const server = http2.createServer();
+  server.listen(0, common.mustCall(() => {
+    const port = server.address().port;
+
+    server.once('request', common.mustCall((request, response) => {
+      const returnVal = response.writeHead(200, ['foo', 'bar', 'ABC', 123]);
+      assert.strictEqual(returnVal, response);
+      response.end(common.mustCall(() => { server.close(); }));
+    }));
+
+    const client = http2.connect(`http://localhost:${port}`, common.mustCall(() => {
+      const request = client.request();
+
+      request.on('response', common.mustCall((headers) => {
+        assert.strictEqual(headers.foo, 'bar');
+        assert.strictEqual(headers.abc, '123');
+        assert.strictEqual(headers[':status'], 200);
+      }, 1));
+      request.on('end', common.mustCall(() => {
+        client.close();
+      }));
+      request.end();
+      request.resume();
+    }));
+  }));
+}
+
+{
+  const server = http2.createServer();
+  server.listen(0, common.mustCall(() => {
+    const port = server.address().port;
+
+    server.once('request', common.mustCall((request, response) => {
+      try {
+        response.writeHead(200, ['foo', 'bar', 'ABC', 123, 'extra']);
+      } catch (err) {
+        assert.strictEqual(err.code, 'ERR_INVALID_ARG_VALUE');
+      }
+
+      response.end(common.mustCall(() => { server.close(); }));
+    }));
+
+    const client = http2.connect(`http://localhost:${port}`, common.mustCall(() => {
+      const request = client.request();
+
+      request.on('end', common.mustCall(() => {
+        client.close();
+      }));
+      request.end();
+      request.resume();
+    }));
+  }));
+}
