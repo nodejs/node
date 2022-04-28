dab15f69e3631355e53923dae44c7b25c3e32645
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-04-28 19:54:29 +0200

    esm: graduate top-level-await to stable
    Removes the experimental label in the docs and makes the
    `--experimental-top-level-await` flag a no-op.
    
    V8 has removed the harmony flag in V8 9.1 and consider the feature
    stable, there's no reason to keep it experimental in Node.js.
    
    PR-URL: https://github.com/nodejs/node/pull/42875
    Reviewed-By: Jacob Smith <jacob@frende.me>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Mohammed Keyvanzadeh <mohammadkeyvanzade94@gmail.com>
    Reviewed-By: Mestery <mestery@protonmail.com>
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- src/node_options.h
+++ src/node_options.h
@@ -205,7 +205,6 @@ class PerIsolateOptions : public Options {
   bool track_heap_objects = false;
   bool report_uncaught_exception = false;
   bool report_on_signal = false;
-  bool experimental_top_level_await = true;
   std::string report_signal = "SIGUSR2";
   inline EnvironmentOptions* get_per_env_options();
   void CheckOptions(std::vector<std::string>* errors) override;
