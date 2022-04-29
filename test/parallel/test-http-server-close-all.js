d1be124848a200875036af9e90dd3505ffdb33cb
Shogun <paolo@cowtech.it>
2022-04-29 07:52:40 +0200

    http: fix set push/pop

--- test/parallel/test-http-server-close-all.js
+++ test/parallel/test-http-server-close-all.js
@@ -8,40 +8,43 @@ const { connect } = require('net');
 const server = createServer(common.mustCall(function(req, res) {
   res.writeHead(200, { 'Connection': 'keep-alive' });
   res.end();
-}), { requestTimeout: common.platformTimeout(60000), headersTimeout: 0 });
+}), {
+  headersTimeout: 0,
+  keepAliveTimeout: 0,
+  requestTimeout: common.platformTimeout(60000),
+});
 
 server.listen(0, function() {
   const port = server.address().port;
 
-  // Create a new request, let it finish but leave the connection opened using HTTP keep-alive
+  // Create a first request but never finish it
   const client1 = connect(port);
-  let response1 = '';
 
-  client1.on('data', common.mustCall((chunk) => {
-    response1 += chunk.toString('utf-8');
-  }));
-
-  client1.on('end', common.mustCall(function() {
-    assert(response1.startsWith('HTTP/1.1 200 OK\r\nConnection: keep-alive'));
-  }));
   client1.on('close', common.mustCall());
+  
+  client1.on('error', () => {});
 
-  client1.resume();
-  client1.write('GET / HTTP/1.1\r\nConnection: keep-alive\r\n\r\n');
+  client1.write('GET / HTTP/1.1');
 
-  // Create a second request but never finish it
+  // Create a second request, let it finish but leave the connection opened using HTTP keep-alive
   const client2 = connect(port);
+  let response = '';
+
+  client2.on('data', common.mustCall((chunk) => {
+    response += chunk.toString('utf-8');
+
+    if (response.endsWith('0\r\n\r\n')) {
+      assert(response.startsWith('HTTP/1.1 200 OK\r\nConnection: keep-alive'));
+
+      server.closeAllConnections();
+      server.close(common.mustCall());
+
+      // This timer should never go off as the server.close should shut everything down
+      setTimeout(common.mustNotCall(), common.platformTimeout(1500)).unref();
+    }
+  }));
+
   client2.on('close', common.mustCall());
 
-  client2.resume();
-  client2.write('GET / HTTP/1.1\r\n');
-
-  // Now, after a while, close the server
-  setTimeout(common.mustCall(() => {
-    server.closeAllConnections();
-    server.close(common.mustCall());
-  }), common.platformTimeout(1000));
-
-  // This timer should never go off as the server.close should shut everything down
-  setTimeout(common.mustNotCall(), common.platformTimeout(1500)).unref();
+  client2.write('GET / HTTP/1.1\r\n\r\n');
 });
--- test/parallel/test-http-server-close-idle.js
+++ test/parallel/test-http-server-close-idle.js
@@ -8,52 +8,55 @@ const { connect } = require('net');
 const server = createServer(common.mustCall(function(req, res) {
   res.writeHead(200, { 'Connection': 'keep-alive' });
   res.end();
-}), { requestTimeout: common.platformTimeout(60000), headersTimeout: 0 });
+}), {
+  headersTimeout: 0,
+  keepAliveTimeout: 0,
+  requestTimeout: common.platformTimeout(60000),
+});
 
 server.listen(0, function() {
   const port = server.address().port;
-
-  // Create a new request, let it finish but leave the connection opened using HTTP keep-alive
-  const client1 = connect(port);
-  let response1 = '';
   let client1Closed = false;
+  let client2Closed = false;
 
-  client1.on('data', common.mustCall((chunk) => {
-    response1 += chunk.toString('utf-8');
-  }));
+  // Create a first request but never finish it
+  const client1 = connect(port);
 
-  client1.on('end', common.mustCall(function() {
-    assert(response1.startsWith('HTTP/1.1 200 OK\r\nConnection: keep-alive'));
-  }));
   client1.on('close', common.mustCall(() => {
     client1Closed = true;
   }));
+  
+  client1.on('error', () => {});
 
-  client1.resume();
-  client1.write('GET / HTTP/1.1\r\nConnection: keep-alive\r\n\r\n');
+  client1.write('GET / HTTP/1.1');
 
-  // Create a second request but never finish it
+  // Create a second request, let it finish but leave the connection opened using HTTP keep-alive
   const client2 = connect(port);
-  let client2Closed = false;
+  let response = '';
+
+  client2.on('data', common.mustCall((chunk) => {
+    response += chunk.toString('utf-8');
+
+    if (response.endsWith('0\r\n\r\n')) {
+      assert(response.startsWith('HTTP/1.1 200 OK\r\nConnection: keep-alive'));
+
+      server.closeIdleConnections();
+      server.close(common.mustCall());
+
+      // Check that only the idle connection got closed
+      setTimeout(common.mustCall(() => {
+        assert(!client1Closed);
+        assert(client2Closed);
+
+        server.closeAllConnections();
+        server.close(common.mustCall());
+      }), common.platformTimeout(500)).unref();
+    }
+  }));
+
   client2.on('close', common.mustCall(() => {
     client2Closed = true;
   }));
 
-  client2.resume();
-  client2.write('GET / HTTP/1.1\r\n');
-
-  // Now, after a while, close the server
-  setTimeout(common.mustCall(() => {
-    server.closeIdleConnections();
-    server.close(common.mustCall());
-  }), common.platformTimeout(1000));
-
-  // Check that only the idle connection got closed
-  setTimeout(common.mustCall(() => {
-    assert(client1Closed);
-    assert(!client2Closed);
-
-    server.closeAllConnections();
-    server.close();
-  }), common.platformTimeout(1500)).unref();
+  client2.write('GET / HTTP/1.1\r\n\r\n');
 });
