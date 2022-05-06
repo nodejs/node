172cffb341f5e6a9160aa2e9329598bec7dced13
Keyhan Vakil <60900335+airtable-keyhanvakil@users.noreply.github.com>
2022-05-06 15:01:04 -0700

    worker: fix stream racing with terminate
    `OnStreamAfterReqFinished` uses `v8::Object::Has` to check if it needs
    to call `oncomplete`. `v8::Object::Has` needs to execute Javascript.
    However when worker threads are involved, `OnStreamAfterReqFinished` may
    be called after the worker thread termination has begun via
    `worker.terminate()`. This makes `v8::Object::Has` return `Nothing`,
    which triggers an assert.
    
    This diff fixes the issue by simply defaulting us to `false` in the case
    where `Nothing` is returned. This is sound because we can't execute
    `oncomplete` anyway as the isolate is terminating.
    
    Fixes: https://github.com/nodejs/node/issues/38418
    
    PR-URL: https://github.com/nodejs/node/pull/42874
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- src/stream_base.cc
+++ src/stream_base.cc
@@ -601,6 +601,7 @@ void ReportWritesToJSStreamListener::OnStreamAfterReqFinished(
     StreamReq* req_wrap, int status) {
   StreamBase* stream = static_cast<StreamBase*>(stream_);
   Environment* env = stream->stream_env();
+  if (env->is_stopping()) return;
   AsyncWrap* async_wrap = req_wrap->GetAsyncWrap();
   HandleScope handle_scope(env->isolate());
   Context::Scope context_scope(env->context());
