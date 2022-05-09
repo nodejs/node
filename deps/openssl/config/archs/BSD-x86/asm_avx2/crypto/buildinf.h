379074b62f2fe4e44776bb619de3efbc39df044b
RafaelGSS <rafael.nunu@hotmail.com>
2022-05-09 16:55:20 -0300

    deps: update archs files for quictls/openssl-3.0.3
    After an OpenSSL source update, all the config files need to be
    regenerated and committed by:
        $ make -C deps/openssl/config
        $ git add deps/openssl/config/archs
        $ git add deps/openssl/openssl
        $ git commit

--- deps/openssl/config/archs/BSD-x86/asm_avx2/crypto/buildinf.h
+++ deps/openssl/config/archs/BSD-x86/asm_avx2/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: BSD-x86"
-#define DATE "built on: Wed Mar 16 04:51:50 2022 UTC"
+#define DATE "built on: Mon May  9 19:44:08 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
--- deps/openssl/config/archs/linux-ppc/no-asm/crypto/buildinf.h
+++ deps/openssl/config/archs/linux-ppc/no-asm/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-ppc"
-#define DATE "built on: Wed Mar 16 04:58:07 2022 UTC"
+#define DATE "built on: Mon May  9 19:48:28 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
