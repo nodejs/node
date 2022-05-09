ea6ada1974ab8d855f132457c34e3396ac4d7270
RafaelGSS <rafael.nunu@hotmail.com>
2022-05-09 14:46:56 -0300

    deps: update archs files for quictls/openssl-3.0.3+quic
    After an OpenSSL source update, all the config files need to be
    regenerated and committed by:
        $ make -C deps/openssl/config
        $ git add deps/openssl/config/archs
        $ git add deps/openssl/openssl
        $ git commit
    
    PR-URL: https://github.com/nodejs/node/pull/43022
    Refs: https://mta.openssl.org/pipermail/openssl-announce/2022-May/000223.html
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Beth Griggs <bgriggs@redhat.com>

--- deps/openssl/config/archs/BSD-x86_64/asm/configdata.pm
+++ deps/openssl/config/archs/BSD-x86_64/asm/configdata.pm
@@ -156,7 +156,7 @@ our %config = (
     ],
     "dynamic_engines" => "0",
     "ex_libs" => [],
-    "full_version" => "3.0.2+quic",
+    "full_version" => "3.0.3+quic",
     "includes" => [],
     "lflags" => [],
     "lib_defines" => [
@@ -203,10 +203,10 @@ our %config = (
     "openssl_sys_defines" => [],
     "openssldir" => "",
     "options" => "enable-ssl-trace enable-fips no-afalgeng no-asan no-buildtest-c++ no-comp no-crypto-mdebug no-crypto-mdebug-backtrace no-dynamic-engine no-ec_nistp_64_gcc_128 no-egd no-external-tests no-fuzz-afl no-fuzz-libfuzzer no-ktls no-loadereng no-md2 no-msan no-rc5 no-sctp no-shared no-ssl3 no-ssl3-method no-trace no-ubsan no-unit-test no-uplink no-weak-ssl-ciphers no-zlib no-zlib-dynamic",
-    "patch" => "2",
-    "perl_archname" => "x86_64-linux-thread-multi",
+    "patch" => "3",
+    "perl_archname" => "x86_64-linux-gnu-thread-multi",
     "perl_cmd" => "/usr/bin/perl",
-    "perl_version" => "5.34.1",
+    "perl_version" => "5.30.0",
     "perlargv" => [
         "no-comp",
         "no-shared",
@@ -255,11 +255,11 @@ our %config = (
     "prerelease" => "",
     "processor" => "",
     "rc4_int" => "unsigned int",
-    "release_date" => "15 Mar 2022",
+    "release_date" => "3 May 2022",
     "shlib_version" => "81.3",
     "sourcedir" => ".",
     "target" => "BSD-x86_64",
-    "version" => "3.0.2"
+    "version" => "3.0.3"
 );
 our %target = (
     "AR" => "ar",
@@ -1244,6 +1244,9 @@ our %unified_info = (
             "test/evp_libctx_test" => {
                 "noinst" => "1"
             },
+            "test/evp_pkey_ctx_new_from_name" => {
+                "noinst" => "1"
+            },
             "test/evp_pkey_dparams_test" => {
                 "noinst" => "1"
             },
@@ -1287,6 +1290,9 @@ our %unified_info = (
                 "noinst" => "1"
             },
             "test/lhash_test" => {
+                "noinst" => "1"
+            },
+            "test/localetest" => {
                 "noinst" => "1"
             },
             "test/mdc2_internal_test" => {
@@ -2892,6 +2898,9 @@ our %unified_info = (
         ],
         "doc/html/man3/OPENSSL_secure_malloc.html" => [
             "doc/man3/OPENSSL_secure_malloc.pod"
+        ],
+        "doc/html/man3/OPENSSL_strcasecmp.html" => [
+            "doc/man3/OPENSSL_strcasecmp.pod"
         ],
         "doc/html/man3/OSSL_CMP_CTX_new.html" => [
             "doc/man3/OSSL_CMP_CTX_new.pod"
@@ -5235,6 +5244,9 @@ our %unified_info = (
         ],
         "doc/man/man3/OPENSSL_secure_malloc.3" => [
             "doc/man3/OPENSSL_secure_malloc.pod"
+        ],
+        "doc/man/man3/OPENSSL_strcasecmp.3" => [
+            "doc/man3/OPENSSL_strcasecmp.pod"
         ],
         "doc/man/man3/OSSL_CMP_CTX_new.3" => [
             "doc/man3/OSSL_CMP_CTX_new.pod"
@@ -7616,6 +7628,9 @@ our %unified_info = (
             "libcrypto.a",
             "test/libtestutil.a"
         ],
+        "test/evp_pkey_ctx_new_from_name" => [
+            "libcrypto"
+        ],
         "test/evp_pkey_dparams_test" => [
             "libcrypto",
             "test/libtestutil.a"
@@ -7679,6 +7694,10 @@ our %unified_info = (
         ],
         "test/libtestutil.a" => [
             "libcrypto"
+        ],
+        "test/localetest" => [
+            "libcrypto",
+            "test/libtestutil.a"
         ],
         "test/mdc2_internal_test" => [
             "libcrypto.a",
@@ -9786,10 +9805,10 @@ our %unified_info = (
         },
         "providers/fips" => {
             "deps" => [
+                "providers/fips/fips-dso-fips_entry.o",
                 "providers/fips/libfips-lib-fipsprov.o",
                 "providers/fips/libfips-lib-self_test.o",
-                "providers/fips/libfips-lib-self_test_kats.o",
-                "providers/fips/fips-dso-fips_entry.o"
+                "providers/fips/libfips-lib-self_test_kats.o"
             ],
             "products" => {
                 "dso" => [
@@ -10529,7 +10548,7 @@ our %unified_info = (
         "crypto/chacha/chacha-c64xplus.S" => [
             "crypto/chacha/asm/chacha-c64xplus.pl"
         ],
-        "crypto/chacha/chacha-ia64.S" => [
+        "crypto/chacha/chacha-ia64.s" => [
             "crypto/chacha/asm/chacha-ia64.pl"
         ],
         "crypto/chacha/chacha-ppc.s" => [
@@ -11848,6 +11867,9 @@ our %unified_info = (
         ],
         "doc/html/man3/OPENSSL_secure_malloc.html" => [
             "doc/man3/OPENSSL_secure_malloc.pod"
+        ],
+        "doc/html/man3/OPENSSL_strcasecmp.html" => [
+            "doc/man3/OPENSSL_strcasecmp.pod"
         ],
         "doc/html/man3/OSSL_CMP_CTX_new.html" => [
             "doc/man3/OSSL_CMP_CTX_new.pod"
@@ -14192,6 +14214,9 @@ our %unified_info = (
         "doc/man/man3/OPENSSL_secure_malloc.3" => [
             "doc/man3/OPENSSL_secure_malloc.pod"
         ],
+        "doc/man/man3/OPENSSL_strcasecmp.3" => [
+            "doc/man3/OPENSSL_strcasecmp.pod"
+        ],
         "doc/man/man3/OSSL_CMP_CTX_new.3" => [
             "doc/man3/OSSL_CMP_CTX_new.pod"
         ],
@@ -16480,6 +16505,7 @@ our %unified_info = (
             "doc/html/man3/OPENSSL_malloc.html",
             "doc/html/man3/OPENSSL_s390xcap.html",
             "doc/html/man3/OPENSSL_secure_malloc.html",
+            "doc/html/man3/OPENSSL_strcasecmp.html",
             "doc/html/man3/OSSL_CMP_CTX_new.html",
             "doc/html/man3/OSSL_CMP_HDR_get0_transactionID.html",
             "doc/html/man3/OSSL_CMP_ITAV_set0.html",
@@ -18545,6 +18571,10 @@ our %unified_info = (
             "include",
             "apps/include"
         ],
+        "test/evp_pkey_ctx_new_from_name" => [
+            "include",
+            "apps/include"
+        ],
         "test/evp_pkey_dparams_test" => [
             "include",
             "apps/include"
@@ -18731,6 +18761,10 @@ our %unified_info = (
             "include",
             "apps/include",
             "."
+        ],
+        "test/localetest" => [
+            "include",
+            "apps/include"
         ],
         "test/mdc2_internal_test" => [
             ".",
@@ -19415,6 +19449,7 @@ our %unified_info = (
             "doc/man/man3/OPENSSL_malloc.3",
             "doc/man/man3/OPENSSL_s390xcap.3",
             "doc/man/man3/OPENSSL_secure_malloc.3",
+            "doc/man/man3/OPENSSL_strcasecmp.3",
             "doc/man/man3/OSSL_CMP_CTX_new.3",
             "doc/man/man3/OSSL_CMP_HDR_get0_transactionID.3",
             "doc/man/man3/OSSL_CMP_ITAV_set0.3",
@@ -20037,6 +20072,7 @@ our %unified_info = (
         "test/evp_fetch_prov_test",
         "test/evp_kdf_test",
         "test/evp_libctx_test",
+        "test/evp_pkey_ctx_new_from_name",
         "test/evp_pkey_dparams_test",
         "test/evp_pkey_provided_test",
         "test/evp_test",
@@ -20052,6 +20088,7 @@ our %unified_info = (
         "test/igetest",
         "test/keymgmt_internal_test",
         "test/lhash_test",
+        "test/localetest",
         "test/mdc2_internal_test",
         "test/mdc2test",
         "test/memleaktest",
@@ -26558,6 +26595,12 @@ our %unified_info = (
         "test/evp_libctx_test-bin-evp_libctx_test.o" => [
             "test/evp_libctx_test.c"
         ],
+        "test/evp_pkey_ctx_new_from_name" => [
+            "test/evp_pkey_ctx_new_from_name-bin-evp_pkey_ctx_new_from_name.o"
+        ],
+        "test/evp_pkey_ctx_new_from_name-bin-evp_pkey_ctx_new_from_name.o" => [
+            "test/evp_pkey_ctx_new_from_name.c"
+        ],
         "test/evp_pkey_dparams_test" => [
             "test/evp_pkey_dparams_test-bin-evp_pkey_dparams_test.o"
         ],
@@ -26746,6 +26789,12 @@ our %unified_info = (
             "test/testutil/libtestutil-lib-test_options.o",
             "test/testutil/libtestutil-lib-tests.o",
             "test/testutil/libtestutil-lib-testutil_init.o"
+        ],
+        "test/localetest" => [
+            "test/localetest-bin-localetest.o"
+        ],
+        "test/localetest-bin-localetest.o" => [
+            "test/localetest.c"
         ],
         "test/mdc2_internal_test" => [
             "test/mdc2_internal_test-bin-mdc2_internal_test.o"
@@ -27459,8 +27508,8 @@ unless (caller) {
     use File::Copy;
     use Pod::Usage;
 
-    use lib '/home/danielbevenius/work/nodejs/node/deps/openssl/openssl/util/perl';
-    use OpenSSL::fallback '/home/danielbevenius/work/nodejs/node/deps/openssl/openssl/external/perl/MODULES.txt';
+    use lib '/home/rafaelgss/repos/os/node/deps/openssl/openssl/util/perl';
+    use OpenSSL::fallback '/home/rafaelgss/repos/os/node/deps/openssl/openssl/external/perl/MODULES.txt';
 
     my $here = dirname($0);
 
@@ -27487,7 +27536,7 @@ unless (caller) {
             );
 
         use lib '.';
-        use lib '/home/danielbevenius/work/nodejs/node/deps/openssl/openssl/Configurations';
+        use lib '/home/rafaelgss/repos/os/node/deps/openssl/openssl/Configurations';
         use gentemplate;
 
         print 'Creating ',$buildfile_template,"\n";
@@ -27504,8 +27553,8 @@ unless (caller) {
 
         my $prepend = <<'_____';
 use File::Spec::Functions;
-use lib '/home/danielbevenius/work/nodejs/node/deps/openssl/openssl/util/perl';
-use lib '/home/danielbevenius/work/nodejs/node/deps/openssl/openssl/Configurations';
+use lib '/home/rafaelgss/repos/os/node/deps/openssl/openssl/util/perl';
+use lib '/home/rafaelgss/repos/os/node/deps/openssl/openssl/Configurations';
 use lib '.';
 use platform;
 _____
--- deps/openssl/config/archs/linux64-s390x/asm/crypto/buildinf.h
+++ deps/openssl/config/archs/linux64-s390x/asm/crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux64-s390x"
-#define DATE "built on: Thu May  5 17:09:32 2022 UTC"
+#define DATE "built on: Mon May  9 17:41:01 2022 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
