ad1e34baa6a0746b485aa7a029f3e6e4d3edc6c4
Krushnal Patel <krushnalpatel11@gmail.com>
2022-05-12 14:56:33 +0530

    test: updated content-mismatch test
    Linter does not support using assert.doesNotThrow() so added a comment
    
    Signed-off-by: Krushnal Patel <krushnalpatel11@gmail.com>

--- test/parallel/test-http-content-length-mismatch.js
+++ test/parallel/test-http-content-length-mismatch.js
@@ -1,13 +1,13 @@
 'use strict';
 const common = require('../common');
-const assert = require('node:assert');
 const http = require('node:http');
 
 // Simple test to check if body size is less  than the content length set is
 
 const server = http.createServer(common.mustCall((req, res) => {
   res.setHeader('content-length', 110);
-  res.write('hello')
+  // This line should not throw any error
+  res.write('hello');
   res.end();
 }));
 
