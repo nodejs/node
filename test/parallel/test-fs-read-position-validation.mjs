80965ad8bb2bea122ca7ce7ce9c298c061c01a9c
Livia Medeiros <74449973+LiviaMedeiros@users.noreply.github.com>
2022-05-15 22:52:06 +0800

    test: reduce flakiness of `test-fs-read-position-validation.mjs`
    PR-URL: https://github.com/nodejs/node/pull/42999
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-fs-read-position-validation.mjs
+++ test/parallel/test-fs-read-position-validation.mjs
@@ -14,73 +14,27 @@ const length = buffer.byteLength;
 // allowedErrors is an array of acceptable internal errors
 // For example, on some platforms read syscall might return -EFBIG
 async function testValid(position, allowedErrors = []) {
-  let fdSync;
-  try {
-    fdSync = fs.openSync(filepath, 'r');
-    fs.readSync(fdSync, buffer, offset, length, position);
-    fs.readSync(fdSync, buffer, { offset, length, position });
-  } catch (err) {
-    if (!allowedErrors.includes(err.code)) {
-      assert.fail(err);
-    }
-  } finally {
-    if (fdSync) fs.closeSync(fdSync);
-  }
-
-  fs.open(filepath, 'r', common.mustSucceed((fd) => {
-    try {
-      if (allowedErrors.length) {
-        fs.read(fd, buffer, offset, length, position, common.mustCall((err, ...results) => {
-          if (err && !allowedErrors.includes(err.code)) {
-            assert.fail(err);
-          }
-        }));
-        fs.read(fd, { buffer, offset, length, position }, common.mustCall((err, ...results) => {
-          if (err && !allowedErrors.includes(err.code)) {
-            assert.fail(err);
-          }
-        }));
-      } else {
-        fs.read(fd, buffer, offset, length, position, common.mustSucceed());
-        fs.read(fd, { buffer, offset, length, position }, common.mustSucceed());
-      }
-    } finally {
-      fs.close(fd, common.mustSucceed());
-    }
-  }));
+  return new Promise((resolve, reject) => {
+    fs.open(filepath, 'r', common.mustSucceed((fd) => {
+      let callCount = 3;
+      const handler = common.mustCall((err) => {
+        callCount--;
+        if (err && !allowedErrors.includes(err.code)) {
+          fs.close(fd, common.mustSucceed());
+          reject(err);
+        } else if (callCount === 0) {
+          fs.close(fd, common.mustSucceed(resolve));
+        }
+      }, callCount);
+      fs.read(fd, buffer, offset, length, position, handler);
+      fs.read(fd, { buffer, offset, length, position }, handler);
+      fs.read(fd, buffer, { offset, length, position }, handler);
+    }));
+  });
 }
 
-async function testInvalid(code, position, internalCatch = false) {
-  let fdSync;
-  try {
-    fdSync = fs.openSync(filepath, 'r');
-    assert.throws(
-      () => fs.readSync(fdSync, buffer, offset, length, position),
-      { code }
-    );
-    assert.throws(
-      () => fs.readSync(fdSync, buffer, { offset, length, position }),
-      { code }
-    );
-  } finally {
-    if (fdSync) fs.closeSync(fdSync);
-  }
-
-  // Set this flag for catching errors via first argument of callback function
-  if (internalCatch) {
-    fs.open(filepath, 'r', common.mustSucceed((fd) => {
-      try {
-        fs.read(fd, buffer, offset, length, position, (err, ...results) => {
-          assert.strictEqual(err.code, code);
-        });
-        fs.read(fd, { buffer, offset, length, position }, (err, ...results) => {
-          assert.strictEqual(err.code, code);
-        });
-      } finally {
-        fs.close(fd, common.mustSucceed());
-      }
-    }));
-  } else {
+async function testInvalid(code, position) {
+  return new Promise((resolve, reject) => {
     fs.open(filepath, 'r', common.mustSucceed((fd) => {
       try {
         assert.throws(
@@ -91,11 +45,18 @@ async function testInvalid(code, position, internalCatch = false) {
           () => fs.read(fd, { buffer, offset, length, position }, common.mustNotCall()),
           { code }
         );
+        assert.throws(
+          () => fs.read(fd, buffer, { offset, length, position }, common.mustNotCall()),
+          { code }
+        );
+        resolve();
+      } catch (err) {
+        reject(err);
       } finally {
         fs.close(fd, common.mustSucceed());
       }
     }));
-  }
+  });
 }
 
 {
