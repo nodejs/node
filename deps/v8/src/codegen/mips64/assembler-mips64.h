f7c4ce2255177977265b2b43a6a80875daef0648
Liu Yu <liuyu@loongson.cn>
2022-05-18 11:07:07 +0800

    deps: V8: cherry-pick 3ebf2052a1b2
      Original commit message:
    
        [mips64] Remove a useless declaration of Operand.
    
        Change-Id: I5144d8315b5c1aca751d138e4d87240b703dc9e3
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3650359
        Commit-Queue: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
        Auto-Submit: Yu Liu <liuyu@loongson.cn>
        Reviewed-by: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
        Cr-Commit-Position: refs/heads/main@{#80585}
    
    Refs: https://github.com/v8/v8/commit/3ebf2052a1b270393eafb093a98ceca844ad237a
    
    PR-URL: https://github.com/nodejs/node/pull/43147
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: Beth Griggs <bgriggs@redhat.com>

--- deps/v8/src/codegen/mips64/assembler-mips64.h
+++ deps/v8/src/codegen/mips64/assembler-mips64.h
@@ -71,7 +71,6 @@ class Operand {
       : rm_(no_reg), rmode_(RelocInfo::EXTERNAL_REFERENCE) {
     value_.immediate = static_cast<int64_t>(f.address());
   }
-  V8_INLINE explicit Operand(const char* s);
   explicit Operand(Handle<HeapObject> handle);
   V8_INLINE explicit Operand(Smi value) : rm_(no_reg), rmode_(RelocInfo::NONE) {
     value_.immediate = static_cast<intptr_t>(value.ptr());
