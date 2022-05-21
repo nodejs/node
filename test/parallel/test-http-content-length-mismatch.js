4be152ff2faa90f129c68f2eec342c2624d45139
Krushnal Patel <krushnalpatel11@gmail.com>
2022-05-22 01:40:00 +0530

    lib: corrected message length calculation test: fixed existing failing test cases

--- test/parallel/test-http-content-length-mismatch.js
+++ test/parallel/test-http-content-length-mismatch.js
@@ -1,22 +1,27 @@
 'use strict';
-const common = require('../common');
+require('../common');
 const http = require('node:http');
+const assert = require('node:assert');
 
 // Simple test to check if body size is less  than the content length set is
 
-const server = http.createServer(common.mustCall((req, res) => {
-  res.setHeader('content-length', 110);
-  // This line should not throw any error
-  res.write('hello');
-  res.end();
-}));
+function test(server) {
+  server.listen(0, () => {
+    http.get({ port: server.address().port });
+  });
+}
 
-server.listen(0, common.mustCall(() => {
-  http.get({
-    port: server.address().port
-  }, common.mustCall((res) => {
-    server.close();
-
-    res.resume();
-  }));
-}));
+// Test with message size longer than content-length header
+{
+  const server = http.createServer((req, res) => {
+    assert.throws(() => {
+      res.setHeader('content-length', 5);
+      res.write('hello world!!');
+    }, {
+      name: 'Error',
+      code: 'ERR_HTTP_CONTENT_LENGTH_EXCEEDED'
+    });
+    process.exit();
+  });
+  test(server);
+}
