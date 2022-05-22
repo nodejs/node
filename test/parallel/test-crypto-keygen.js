419669cb7946958047c24174d188b8cf06ff7836
mawaregetsuka <33221990+mawaregetsuka@users.noreply.github.com>
2022-05-22 23:44:12 +0800

    lib: refactor `validateInt32` and `validateUint32`
    PR-URL: https://github.com/nodejs/node/pull/43071
    Reviewed-By: Paolo Insogna <paolo@cowtech.it>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-crypto-keygen.js
+++ test/parallel/test-crypto-keygen.js
@@ -1122,18 +1122,6 @@ const sec1EncExp = (cipher) => getRegExpForPEM('EC PRIVATE KEY', cipher);
   }
 }
 
-function addNumericalSeparator(val) {
-  val = String(val);
-  let res = '';
-  let i = val.length;
-  const start = val[0] === '-' ? 1 : 0;
-  for (; i >= start + 4; i -= 3) {
-    res = `_${val.slice(i - 3, i)}${res}`;
-  }
-  return `${val.slice(0, i)}${res}`;
-}
-
-
 // Test RSA parameters.
 {
   // Test invalid modulus lengths. (non-number)
@@ -1170,10 +1158,6 @@ function addNumericalSeparator(val) {
     }, common.mustNotCall()), {
       name: 'RangeError',
       code: 'ERR_OUT_OF_RANGE',
-      message:
-        'The value of "options.modulusLength" is out of range. ' +
-        'It must be >= 0 && < 4294967296. ' +
-        `Received ${addNumericalSeparator(modulusLength)}`
     });
   }
 
@@ -1214,10 +1198,6 @@ function addNumericalSeparator(val) {
     }, common.mustNotCall()), {
       name: 'RangeError',
       code: 'ERR_OUT_OF_RANGE',
-      message:
-        'The value of "options.publicExponent" is out of range. ' +
-        'It must be >= 0 && < 4294967296. ' +
-        `Received ${addNumericalSeparator(publicExponent)}`
     });
   }
 }
@@ -1244,10 +1224,6 @@ function addNumericalSeparator(val) {
     }, common.mustNotCall()), {
       name: 'RangeError',
       code: 'ERR_OUT_OF_RANGE',
-      message:
-        'The value of "options.modulusLength" is out of range. ' +
-        'It must be an integer. ' +
-        `Received ${inspect(modulusLength)}`
     });
   }
 
@@ -1258,10 +1234,6 @@ function addNumericalSeparator(val) {
     }, common.mustNotCall()), {
       name: 'RangeError',
       code: 'ERR_OUT_OF_RANGE',
-      message:
-        'The value of "options.modulusLength" is out of range. ' +
-        'It must be >= 0 && < 4294967296. ' +
-        `Received ${addNumericalSeparator(modulusLength)}`
     });
   }
 
