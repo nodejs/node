d03354486c9b514cb3403ec6a454d112dc13c778
Tobias Nießen <tniessen@tnie.de>
2022-05-27 21:18:49 +0000

    src: fix IPv4 validation in inspector_socket
    Co-authored-by: RafaelGSS <rafael.nunu@hotmail.com>
    
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: RafaelGSS <rafael.nunu@hotmail.com>
    PR-URL: https://github.com/nodejs-private/node-private/pull/320
    CVE-ID: CVE-2022-32212

--- src/inspector_socket.cc
+++ src/inspector_socket.cc
@@ -164,14 +164,22 @@ static std::string TrimPort(const std::string& host) {
 static bool IsIPAddress(const std::string& host) {
   if (host.length() >= 4 && host.front() == '[' && host.back() == ']')
     return true;
-  int quads = 0;
+  uint_fast16_t accum = 0;
+  uint_fast8_t quads = 0;
+  bool empty = true;
+  auto endOctet = [&accum, &quads, &empty](bool final = false) {
+    return !empty && accum <= 0xff && ++quads <= 4 && final == (quads == 4) &&
+           (empty = true) && !(accum = 0);
+  };
   for (char c : host) {
-    if (c == '.')
-      quads++;
-    else if (!isdigit(c))
+    if (isdigit(c)) {
+      if ((accum = (accum * 10) + (c - '0')) > 0xff) return false;
+      empty = false;
+    } else if (c != '.' || !endOctet()) {
       return false;
+    }
   }
-  return quads == 3;
+  return endOctet(true);
 }
 
 // Constants for hybi-10 frame format.
