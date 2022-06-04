18fffe6108ffcc805fb34b2c4360e9869d7b7877
Meek Simbule <55823259+meekdenzo@users.noreply.github.com>
2022-06-04 12:55:06 -0400

    test: convert then to async/await
    PR-URL: https://github.com/nodejs/node/pull/43292
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-debugger-extract-function-name.js
+++ test/parallel/test-debugger-extract-function-name.js
@@ -10,28 +10,25 @@ const assert = require('assert');
 
 const cli = startCLI([fixtures.path('debugger', 'three-lines.js')]);
 
-function onFatal(error) {
-  cli.quit();
-  throw error;
-}
-
-cli.waitForInitialBreak()
-  .then(() => cli.waitForPrompt())
-  .then(() => cli.command('exec a = function func() {}; a;'))
-  .then(() => assert.match(cli.output, /\[Function: func\]/))
-  .then(() => cli.command('exec a = function func () {}; a;'))
-  .then(() => assert.match(cli.output, /\[Function\]/))
-  .then(() => cli.command('exec a = function() {}; a;'))
-  .then(() => assert.match(cli.output, /\[Function: function\]/))
-  .then(() => cli.command('exec a = () => {}; a;'))
-  .then(() => assert.match(cli.output, /\[Function\]/))
-  .then(() => cli.command('exec a = function* func() {}; a;'))
-  .then(() => assert.match(cli.output, /\[GeneratorFunction: func\]/))
-  .then(() => cli.command('exec a = function *func() {}; a;'))
-  .then(() => assert.match(cli.output, /\[GeneratorFunction: \*func\]/))
-  .then(() => cli.command('exec a = function*func() {}; a;'))
-  .then(() => assert.match(cli.output, /\[GeneratorFunction: function\*func\]/))
-  .then(() => cli.command('exec a = function * func() {}; a;'))
-  .then(() => assert.match(cli.output, /\[GeneratorFunction\]/))
-  .then(() => cli.quit())
-  .then(null, onFatal);
+(async () => {
+  await cli.waitForInitialBreak();
+  await cli.waitForPrompt();
+  await cli.command('exec a = function func() {}; a;');
+  assert.match(cli.output, /\[Function: func\]/);
+  await cli.command('exec a = function func () {}; a;');
+  assert.match(cli.output, /\[Function\]/);
+  await cli.command('exec a = function() {}; a;');
+  assert.match(cli.output, /\[Function: function\]/);
+  await cli.command('exec a = () => {}; a;');
+  assert.match(cli.output, /\[Function\]/);
+  await cli.command('exec a = function* func() {}; a;');
+  assert.match(cli.output, /\[GeneratorFunction: func\]/);
+  await cli.command('exec a = function *func() {}; a;');
+  assert.match(cli.output, /\[GeneratorFunction: \*func\]/);
+  await cli.command('exec a = function*func() {}; a;');
+  assert.match(cli.output, /\[GeneratorFunction: function\*func\]/);
+  await cli.command('exec a = function * func() {}; a;');
+  assert.match(cli.output, /\[GeneratorFunction\]/);
+})()
+.finally(() => cli.quit())
+.then(common.mustCall());
