765666a8d43f164daa75bc135e69aa5bed6c6fa6
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2022-06-05 19:33:46 +0200

    esm: fix chain advances when loader calls next<HookName> multiple times
    PR-URL: https://github.com/nodejs/node/pull/43303
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/es-module/test-esm-loader-chaining.mjs
+++ test/es-module/test-esm-loader-chaining.mjs
@@ -25,8 +25,8 @@ const commonArgs = [
   );
 
   assert.strictEqual(stderr, '');
-  assert.strictEqual(status, 0);
   assert.match(stdout, /number/); // node:fs is an object
+  assert.strictEqual(status, 0);
 }
 
 { // Verify loaded source is properly different when only load changes something
@@ -47,8 +47,8 @@ const commonArgs = [
   assert.strictEqual(stderr, '');
   assert.match(stdout, /load passthru/);
   assert.match(stdout, /resolve passthru/);
-  assert.strictEqual(status, 0);
   assert.match(stdout, /foo/);
+  assert.strictEqual(status, 0);
 }
 
 { // Verify multiple changes from hooks result in proper output
@@ -70,8 +70,8 @@ const commonArgs = [
 
   assert.strictEqual(stderr, '');
   assert.match(stdout, /resolve 42/); // It did go thru resolve-42
-  assert.strictEqual(status, 0);
   assert.match(stdout, /foo/); // LIFO, so resolve-foo won
+  assert.strictEqual(status, 0);
 }
 
 { // Verify modifying context within resolve chain is respected
@@ -117,8 +117,52 @@ const commonArgs = [
 
   assert.strictEqual(stderr, '');
   assert.match(stdout, /resolve foo/); // It did go thru resolve-foo
-  assert.strictEqual(status, 0);
   assert.match(stdout, /42/); // LIFO, so resolve-42 won
+  assert.strictEqual(status, 0);
+}
+
+{ // Verify multiple calls to next within same loader receive correct "next" fn
+  const { status, stderr, stdout } = spawnSync(
+    process.execPath,
+    [
+      '--loader',
+      fixtures.fileURL('es-module-loaders', 'loader-resolve-shortcircuit.mjs'),
+      '--loader',
+      fixtures.fileURL('es-module-loaders', 'loader-resolve-foo.mjs'),
+      '--loader',
+      fixtures.fileURL('es-module-loaders', 'loader-resolve-multiple-next-calls.mjs'),
+      '--loader',
+      fixtures.fileURL('es-module-loaders', 'loader-load-foo-or-42.mjs'),
+      ...commonArgs,
+    ],
+    { encoding: 'utf8' },
+  );
+
+  const countFoos = stdout.match(/resolve foo/g)?.length;
+
+  assert.strictEqual(stderr, '');
+  assert.strictEqual(countFoos, 2);
+  assert.strictEqual(status, 0);
+}
+
+{ // Verify next<HookName> function's `name` is correct
+  const { status, stderr, stdout } = spawnSync(
+    process.execPath,
+    [
+      '--loader',
+      fixtures.fileURL('es-module-loaders', 'loader-resolve-shortcircuit.mjs'),
+      '--loader',
+      fixtures.fileURL('es-module-loaders', 'loader-resolve-42.mjs'),
+      '--loader',
+      fixtures.fileURL('es-module-loaders', 'loader-load-foo-or-42.mjs'),
+      ...commonArgs,
+    ],
+    { encoding: 'utf8' },
+  );
+
+  assert.strictEqual(stderr, '');
+  assert.match(stdout, /next<HookName>: nextResolve/);
+  assert.strictEqual(status, 0);
 }
 
 { // Verify error thrown for incomplete resolve chain, citing errant loader & hook
@@ -137,10 +181,10 @@ const commonArgs = [
   );
 
   assert.match(stdout, /resolve passthru/);
-  assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_LOADER_CHAIN_INCOMPLETE/);
   assert.match(stderr, /loader-resolve-incomplete\.mjs/);
-  assert.match(stderr, /"resolve"/);
+  assert.match(stderr, /'resolve'/);
+  assert.strictEqual(status, 1);
 }
 
 { // Verify error NOT thrown when nested resolve hook signaled a short circuit
@@ -201,10 +245,10 @@ const commonArgs = [
   );
 
   assert.doesNotMatch(stdout, /resolve passthru/);
-  assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_LOADER_CHAIN_INCOMPLETE/);
   assert.match(stderr, /loader-resolve-incomplete\.mjs/);
-  assert.match(stderr, /"resolve"/);
+  assert.match(stderr, /'resolve'/);
+  assert.strictEqual(status, 1);
 }
 
 { // Verify error thrown for incomplete load chain, citing errant loader & hook
@@ -223,10 +267,10 @@ const commonArgs = [
   );
 
   assert.match(stdout, /load passthru/);
-  assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_LOADER_CHAIN_INCOMPLETE/);
   assert.match(stderr, /loader-load-incomplete\.mjs/);
-  assert.match(stderr, /"load"/);
+  assert.match(stderr, /'load'/);
+  assert.strictEqual(status, 1);
 }
 
 { // Verify chain does break and throws appropriately
@@ -245,13 +289,13 @@ const commonArgs = [
   );
 
   assert.doesNotMatch(stdout, /load passthru/);
-  assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_LOADER_CHAIN_INCOMPLETE/);
   assert.match(stderr, /loader-load-incomplete\.mjs/);
-  assert.match(stderr, /"load"/);
+  assert.match(stderr, /'load'/);
+  assert.strictEqual(status, 1);
 }
 
-{ // Verify error thrown when invalid `specifier` argument passed to `resolve…next`
+{ // Verify error thrown when invalid `specifier` argument passed to `nextResolve`
   const { status, stderr } = spawnSync(
     process.execPath,
     [
@@ -267,11 +311,10 @@ const commonArgs = [
   assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_INVALID_ARG_TYPE/);
   assert.match(stderr, /loader-resolve-bad-next-specifier\.mjs/);
-  assert.match(stderr, /"resolve"/);
-  assert.match(stderr, /nextResolve\(specifier\)/);
+  assert.match(stderr, /'resolve' hook's nextResolve\(\) specifier/);
 }
 
-{ // Verify error thrown when invalid `context` argument passed to `resolve…next`
+{ // Verify error thrown when invalid `context` argument passed to `nextResolve`
   const { status, stderr } = spawnSync(
     process.execPath,
     [
@@ -284,14 +327,13 @@ const commonArgs = [
     { encoding: 'utf8' },
   );
 
-  assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_INVALID_ARG_TYPE/);
   assert.match(stderr, /loader-resolve-bad-next-context\.mjs/);
-  assert.match(stderr, /"resolve"/);
-  assert.match(stderr, /nextResolve\(, context\)/);
+  assert.match(stderr, /'resolve' hook's nextResolve\(\) context/);
+  assert.strictEqual(status, 1);
 }
 
-{ // Verify error thrown when invalid `url` argument passed to `load…next`
+{ // Verify error thrown when invalid `url` argument passed to `nextLoad`
   const { status, stderr } = spawnSync(
     process.execPath,
     [
@@ -304,14 +346,13 @@ const commonArgs = [
     { encoding: 'utf8' },
   );
 
-  assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_INVALID_ARG_TYPE/);
   assert.match(stderr, /loader-load-bad-next-url\.mjs/);
-  assert.match(stderr, /"load"/);
-  assert.match(stderr, /nextLoad\(url\)/);
+  assert.match(stderr, /'load' hook's nextLoad\(\) url/);
+  assert.strictEqual(status, 1);
 }
 
-{ // Verify error thrown when invalid `url` argument passed to `load…next`
+{ // Verify error thrown when invalid `url` argument passed to `nextLoad`
   const { status, stderr } = spawnSync(
     process.execPath,
     [
@@ -324,14 +365,13 @@ const commonArgs = [
     { encoding: 'utf8' },
   );
 
-  assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_INVALID_ARG_VALUE/);
   assert.match(stderr, /loader-load-impersonating-next-url\.mjs/);
-  assert.match(stderr, /"load"/);
-  assert.match(stderr, /nextLoad\(url\)/);
+  assert.match(stderr, /'load' hook's nextLoad\(\) url/);
+  assert.strictEqual(status, 1);
 }
 
-{ // Verify error thrown when invalid `context` argument passed to `load…next`
+{ // Verify error thrown when invalid `context` argument passed to `nextLoad`
   const { status, stderr } = spawnSync(
     process.execPath,
     [
@@ -344,9 +384,8 @@ const commonArgs = [
     { encoding: 'utf8' },
   );
 
-  assert.strictEqual(status, 1);
   assert.match(stderr, /ERR_INVALID_ARG_TYPE/);
   assert.match(stderr, /loader-load-bad-next-context\.mjs/);
-  assert.match(stderr, /"load"/);
-  assert.match(stderr, /nextLoad\(, context\)/);
+  assert.match(stderr, /'load' hook's nextLoad\(\) context/);
+  assert.strictEqual(status, 1);
 }
