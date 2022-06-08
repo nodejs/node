046a2e1774ddfc4fafb373a5b6bcd152a6772126
木杉 <zhmushan@qq.com>
2022-06-08 14:26:05 +0800

    child_process: avoid repeated calls to `normalizeSpawnArguments`

--- test/parallel/test-child-process-execfile.js
+++ test/parallel/test-child-process-execfile.js
@@ -2,7 +2,7 @@
 
 const common = require('../common');
 const assert = require('assert');
-const execFile = require('child_process').execFile;
+const { execFile, execFileSync } = require('child_process');
 const { getEventListeners } = require('events');
 const { getSystemErrorName } = require('util');
 const fixtures = require('../common/fixtures');
@@ -98,4 +98,20 @@ const execOpts = { encoding: 'utf8', shell: true };
     assert.strictEqual(err, null);
   });
   execFile(process.execPath, [fixture, 0], { signal }, callback);
+}
+
+// Verify the execFile() stdout is the same as execFileSync().
+{
+  const file = 'echo';
+  const args = ['foo', 'bar'];
+
+  // Test with and without `{ shell: true }`
+  [null, { shell: true }].forEach((options) => {
+    const execFileSyncStdout = execFileSync(file, args, options);
+    assert.deepStrictEqual(execFileSyncStdout, Buffer.from('foo bar\n'));
+
+    execFile(file, args, options, common.mustCall((_, stdout) => {
+      assert.deepStrictEqual(Buffer.from(stdout), execFileSyncStdout);
+    }));
+  });
 }
