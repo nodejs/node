7a08b8f13ba4c5a93b92052ce0dc7a9febe032de
hemanth.hm <hemanth.hm@gmail.com>
2022-06-16 04:28:51 -0700

    prefix to tests
    Co-authored-by: mscdex <mscdex@users.noreply.github.com>

--- test/parallel/test-module-isBuiltIn.js
+++ test/parallel/test-module-isBuiltIn.js
@@ -6,6 +6,8 @@ const { isBuiltIn } = require('module');
 // Includes modules in lib/ (even deprecated ones)
 assert(isBuiltIn('http'));
 assert(isBuiltIn('sys'));
+assert(isBuiltIn('node:fs'));
+assert(isBuiltIn('node:test'));
 
 // Does not include internal modules
 assert(!isBuiltIn('internal'));
