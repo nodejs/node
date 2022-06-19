b47b8cac751f12f5ccb1c5eaf83822e989df4ed7
Jithil P Ponnan <jithil@outlook.com>
2022-06-19 11:25:44 +0530

    test: removed multiline string concat

--- test/fixtures/wpt/resources/idlharness.js
+++ test/fixtures/wpt/resources/idlharness.js
@@ -917,9 +917,8 @@ IdlArray.prototype.assert_type_is = function(value, type)
                 throw e;
             }
         }
-        // TODO: Is there a nice way to list the union's types in the message?
-        assert_true(false, "Attribute has value " + format_value(value)
-                    + " which doesn't match any of the types in the union");
+
+        assert_true(false, `Attribute has value ${format_value(value)} which doesn't match any of the types in the union`);
 
     }
 
