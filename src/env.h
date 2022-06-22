e9765f397bac7f5e8d1cda82662f79611d06bf9f
Joyee Cheung <joyeec9h3@gmail.com>
2022-06-22 15:24:34 +0800

    bootstrap: handle snapshot errors gracefully
    This patch refactors the SnapshotBuilder::Generate() routines
    so that when running into errors during the snapshot building
    process, they can exit gracefully by printing the error
    and return a non-zero exit code. If the error is likely to
    be caused by internal scripts, the return code would be 12,
    if the error is caused by user scripts the return code would
    be 1. In addition this refactors the generation of embedded
    snapshots and directly writes to the output file stream
    instead of producing an intermediate string with string
    streams.

--- src/env.h
+++ src/env.h
@@ -991,12 +991,16 @@ struct EnvSerializeInfo {
 };
 
 struct SnapshotData {
-  // The result of v8::SnapshotCreator::CreateBlob() during the snapshot
-  // building process.
-  v8::StartupData v8_snapshot_blob_data;
+  enum class DataOwnership { kOwned, kNotOwned };
 
   static const size_t kNodeBaseContextIndex = 0;
   static const size_t kNodeMainContextIndex = kNodeBaseContextIndex + 1;
+
+  DataOwnership data_ownership;
+
+  // The result of v8::SnapshotCreator::CreateBlob() during the snapshot
+  // building process.
+  v8::StartupData v8_snapshot_blob_data;
 
   std::vector<size_t> isolate_data_indices;
   // TODO(joyeecheung): there should be a vector of env_info once we snapshot
@@ -1007,6 +1011,17 @@ struct SnapshotData {
   // read only space. We use native_module::CodeCacheInfo because
   // v8::ScriptCompiler::CachedData is not copyable.
   std::vector<native_module::CodeCacheInfo> code_cache;
+
+  static std::unique_ptr<SnapshotData> New();
+  ~SnapshotData();
+
+  SnapshotData(const SnapshotData&) = delete;
+  SnapshotData& operator=(const SnapshotData&) = delete;
+  SnapshotData(SnapshotData&&) = delete;
+  SnapshotData& operator=(SnapshotData&&) = delete;
+
+  // Only invoked by New().
+  SnapshotData() = default;
 };
 
 class Environment : public MemoryRetainer {
