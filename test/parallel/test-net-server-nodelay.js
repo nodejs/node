474849d72e2bc8f175e63bb4198bee2109c82025
theanarkh <theratliter@gmail.com>
2022-06-25 02:47:43 +0800

    net: fix net keepalive and noDelay
    1. support setKeepAlive again
    2. set keepalive and nodelay to socket which is created by server

--- test/parallel/test-net-server-nodelay.js
+++ test/parallel/test-net-server-nodelay.js
@@ -6,6 +6,8 @@ const net = require('net');
 const server = net.createServer({
   noDelay: true
 }, common.mustCall((socket) => {
+  socket._handle.setNoDelay = common.mustNotCall();
+  socket.setNoDelay(true);
   socket.destroy();
   server.close();
 })).listen(0, common.mustCall(() => {
@@ -18,6 +20,7 @@ server._handle.onconnection = common.mustCall((err, clientHandle) => {
   clientHandle.setNoDelay = common.mustCall((enable) => {
     assert.strictEqual(enable, server.noDelay);
     setNoDelay.call(clientHandle, enable);
+    clientHandle.setNoDelay = setNoDelay;
   });
   onconnection.call(server._handle, err, clientHandle);
 });
