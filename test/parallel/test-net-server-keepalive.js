ad1d0541c5138a318f30c71ae74bd71d80ca10da
theanarkh <theratliter@gmail.com>
2022-06-29 17:58:35 +0800

    net: fix net keepalive and noDelay
    1. support setKeepAlive again
    2. set keepalive and nodelay to socket which is created by server
    
    PR-URL: https://github.com/nodejs/node/pull/43561
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-net-server-keepalive.js
+++ test/parallel/test-net-server-keepalive.js
@@ -7,6 +7,15 @@ const server = net.createServer({
   keepAlive: true,
   keepAliveInitialDelay: 1000
 }, common.mustCall((socket) => {
+  const setKeepAlive = socket._handle.setKeepAlive;
+  socket._handle.setKeepAlive = common.mustCall((enable, initialDelay) => {
+    assert.strictEqual(enable, true);
+    assert.match(String(initialDelay), /^2|3$/);
+    return setKeepAlive.call(socket._handle, enable, initialDelay);
+  }, 2);
+  socket.setKeepAlive(true, 1000);
+  socket.setKeepAlive(true, 2000);
+  socket.setKeepAlive(true, 3000);
   socket.destroy();
   server.close();
 })).listen(0, common.mustCall(() => {
@@ -20,6 +29,7 @@ server._handle.onconnection = common.mustCall((err, clientHandle) => {
     assert.strictEqual(enable, server.keepAlive);
     assert.strictEqual(initialDelayMsecs, server.keepAliveInitialDelay);
     setKeepAlive.call(clientHandle, enable, initialDelayMsecs);
+    clientHandle.setKeepAlive = setKeepAlive;
   });
   onconnection.call(server._handle, err, clientHandle);
 });
