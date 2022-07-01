bd24e6244b43fe34bdb035226da62dc7025e7acb
Moshe Atlow <moshe@atlow.co.il>
2022-07-01 10:32:10 +0300

    assert: callTracker throw a specific error message when possible

--- test/parallel/test-assert-calltracker-verify.js
+++ test/parallel/test-assert-calltracker-verify.js
@@ -6,27 +6,42 @@ const assert = require('assert');
 
 const tracker = new assert.CallTracker();
 
-const msg = 'Function(s) were not called the expected number of times';
+const generic_msg = 'Function(s) were not called the expected number of times';
 
 function foo() {}
 
-const callsfoo = tracker.calls(foo, 1);
+function bar() {}
 
-// Expects an error as callsfoo() was called less than one time.
+const callsfoo = tracker.calls(foo, 1);
+const callsbar = tracker.calls(bar, 1);
+
+// Expects an error as callsfoo() and callsbar() were called less than one time.
 assert.throws(
   () => tracker.verify(),
-  { message: msg }
+  { message: generic_msg }
 );
 
 callsfoo();
 
-// Will throw an error if callsfoo() isn't called exactly once.
-tracker.verify();
+// Expects an error as callsbar() was called less than one time.
+assert.throws(
+  () => tracker.verify(),
+  { message: 'Expected the bar function to be executed 1 time(s) but was executed 0 time(s).' }
+);
 
 callsfoo();
 
-// Expects an error as callsfoo() was called more than once.
+// Expects an error as callsfoo() was called more than once and callsbar() was called less than one time.
 assert.throws(
   () => tracker.verify(),
-  { message: msg }
+  { message: generic_msg }
+);
+
+callsbar();
+
+
+// Expects an error as callsfoo() was called more than once
+assert.throws(
+  () => tracker.verify(),
+  { message: 'Expected the foo function to be executed 1 time(s) but was executed 2 time(s).' }
 );
