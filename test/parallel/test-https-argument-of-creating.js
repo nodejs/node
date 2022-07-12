7acdb41a722140b0a57cf0cac249496af82232c2
LiviaMedeiros <livia@cirno.name>
2022-07-13 01:57:37 +0800

    test: use `common.mustNotMutateObjectDeep()` in immutability tests
    PR-URL: https://github.com/nodejs/node/pull/43196
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-https-argument-of-creating.js
+++ test/parallel/test-https-argument-of-creating.js
@@ -12,11 +12,13 @@ const dftProtocol = {};
 
 // Test for immutable `opts`
 {
-  const opts = { foo: 'bar', ALPNProtocols: [ 'http/1.1' ] };
+  const opts = common.mustNotMutateObjectDeep({
+    foo: 'bar',
+    ALPNProtocols: [ 'http/1.1' ],
+  });
   const server = https.createServer(opts);
 
   tls.convertALPNProtocols([ 'http/1.1' ], dftProtocol);
-  assert.deepStrictEqual(opts, { foo: 'bar', ALPNProtocols: [ 'http/1.1' ] });
   assert.strictEqual(server.ALPNProtocols.compare(dftProtocol.ALPNProtocols),
                      0);
 }
