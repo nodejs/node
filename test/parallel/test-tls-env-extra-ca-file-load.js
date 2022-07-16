1ede0a59b43b64fc494da4fdea56ef36bf1eefde
Kevin Lentin <kev@lentinfamily.com>
2022-07-16 13:20:05 +1000

    test: ensure NODE_EXTRA_CA_CERTS not set before tests
    The 'test-tls-env-extra-ca-file-load.js' test assumes that
    NODE_EXTRA_CA_CERTS is not set. If the build environment
    happens to have it set, this test will fail. This change
    deletes that env var before running the test.

--- test/parallel/test-tls-env-extra-ca-file-load.js
+++ test/parallel/test-tls-env-extra-ca-file-load.js
@@ -20,6 +20,8 @@ if (process.argv[2] !== 'child') {
   const NODE_EXTRA_CA_CERTS = fixtures.path('keys', 'ca1-cert.pem');
   const extendsEnv = (obj) => ({ ...process.env, ...obj });
 
+  // Clean the starting state
+  delete process.env.NODE_EXTRA_CA_CERTS;
   [
     extendsEnv({ CHILD_USE_EXTRA_CA_CERTS: 'yes', NODE_EXTRA_CA_CERTS }),
     extendsEnv({ CHILD_USE_EXTRA_CA_CERTS: 'no' }),
