f6a777f1c0a0f461a867219e71205c0a15a894a4
KrayzeeKev <github@lentinfamily.com>
2022-07-18 09:46:46 +1000

    Update test/parallel/test-tls-env-extra-ca-file-load.js
    Co-authored-by: Tobias Nießen <tniessen@tnie.de>

--- test/parallel/test-tls-env-extra-ca-file-load.js
+++ test/parallel/test-tls-env-extra-ca-file-load.js
@@ -20,7 +20,7 @@ if (process.argv[2] !== 'child') {
   const NODE_EXTRA_CA_CERTS = fixtures.path('keys', 'ca1-cert.pem');
   const extendsEnv = (obj) => ({ ...process.env, ...obj });
 
-  // Clean the starting state
+  // Remove any pre-existing extra CA certs.
   delete process.env.NODE_EXTRA_CA_CERTS;
   [
     extendsEnv({ CHILD_USE_EXTRA_CA_CERTS: 'yes', NODE_EXTRA_CA_CERTS }),
