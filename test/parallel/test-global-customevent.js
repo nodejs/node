323ca0f36c9b1457a00f6d3ad37a0feacc53801e
Daeyeon Jeong <daeyeon.dev@gmail.com>
2022-07-18 18:51:21 +0900

    fixup: clarify the test
    Signed-off-by: Daeyeon Jeong daeyeon.dev@gmail.com

--- test/parallel/test-global-customevent.js
+++ test/parallel/test-global-customevent.js
@@ -1,16 +1,11 @@
 // Flags: --experimental-global-customevent --expose-internals
 'use strict';
 
-const common = require('../common');
+require('../common');
 const { strictEqual, ok } = require('node:assert');
-const { Worker, isMainThread } = require('worker_threads');
-const { CustomEvent } = require('internal/event_target');
+const { CustomEvent: internalCustomEvent } = require('internal/event_target');
 
+// Global
 ok(CustomEvent);
-strictEqual(globalThis.CustomEvent, CustomEvent);
 
-if (isMainThread) {
-  const w = new Worker(__filename);
-  w.on('message', common.mustNotCall());
-  w.on('error', common.mustNotCall());
-}
+strictEqual(CustomEvent, internalCustomEvent);
