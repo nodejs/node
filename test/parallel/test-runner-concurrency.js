28b7d0ee239a57bb1e5254c8dcbd0897b3d424d4
Lenvin Gonsalves <lenvingonsalves@gmail.com>
2022-07-19 23:48:30 +0530

    test_runner: update test support less than 2 core

--- test/parallel/test-runner-concurrency.js
+++ test/parallel/test-runner-concurrency.js
@@ -2,6 +2,7 @@
 require('../common');
 const { describe, it } = require('node:test');
 const assert = require('assert');
+const { cpus } = require('os');
 
 describe('Concurrency option (boolean) = true ', { concurrency: true }, () => {
   let isFirstTestOver = false;
@@ -9,7 +10,12 @@ describe('Concurrency option (boolean) = true ', { concurrency: true }, () => {
     setTimeout(() => { resolve(); isFirstTestOver = true; }, 1000);
   }));
   it('should start before the previous test ends', () => {
-    assert.strictEqual(isFirstTestOver, false);
+    // On dual & single core machines, the concurrency is set to 1 if a truthy value is passed
+    if (cpus().length <= 2) {
+      assert.strictEqual(isFirstTestOver, true);
+    } else {
+      assert.strictEqual(isFirstTestOver, false);
+    }
   });
 });
 
@@ -26,3 +32,17 @@ describe(
     });
   }
 );
+
+describe('default concurrency (1)', () => {
+  let isFirstTestOver = false;
+  it('should end after 1000ms', () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+        isFirstTestOver = true;
+      }, 1000);
+    }));
+  it('should start after the previous test ends', () => {
+    assert.strictEqual(isFirstTestOver, true);
+  });
+});
