b43ecacc06a3d1971d6f03aac7f22bf03fe141fe
Keyhan Vakil <kvakil@sylph.kvakil.me>
2022-07-21 04:42:04 +0000

    src: remove usages of GetBackingStore in modules
    This removes all usages of GetBackingStore in modules. See the linked
    issue for an explanation.
    
    Refs: https://github.com/nodejs/node/issues/32226
    Refs: https://github.com/nodejs/node/pull/43921

--- src/module_wrap.cc
+++ src/module_wrap.cc
@@ -178,8 +178,8 @@ void ModuleWrap::New(const FunctionCallbackInfo<Value>& args) {
       if (!args[5]->IsUndefined()) {
         CHECK(args[5]->IsArrayBufferView());
         Local<ArrayBufferView> cached_data_buf = args[5].As<ArrayBufferView>();
-        uint8_t* data = static_cast<uint8_t*>(
-            cached_data_buf->Buffer()->GetBackingStore()->Data());
+        uint8_t* data =
+            static_cast<uint8_t*>(cached_data_buf->Buffer()->Data());
         cached_data =
             new ScriptCompiler::CachedData(data + cached_data_buf->ByteOffset(),
                                            cached_data_buf->ByteLength());
