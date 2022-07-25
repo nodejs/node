eaa919e863fec8af9d70b1f9033e0e28bf82fd56
Lenvin Gonsalves <lenvingonsalves@gmail.com>
2022-07-25 22:19:47 +0530

    tools: apply 2022a timezone patch on icu file
    Fixes: https://github.com/nodejs/node/issues/43134

--- tools/update-timezone.mjs
+++ tools/update-timezone.mjs
@@ -1,5 +1,33 @@
 #!/usr/bin/env node
 // Usage: tools/update-timezone.mjs [--dry]
 // Passing --dry will redirect output to stdout.
+import { execSync, spawnSync } from 'node:child_process';
+import { renameSync } from 'node:fs';
+const fileNames = [
+  'zoneinfo64.res',
+  'windowsZones.res',
+  'timezoneTypes.res',
+  'metaZones.res',
+];
 
-console.log('Initial Skeleton');
+execSync('rm -rf icu-data');
+execSync('git clone https://github.com/unicode-org/icu-data');
+execSync('bzip2 -d deps/icu-small/source/data/in/icudt70l.dat.bz2');
+fileNames.forEach((file)=>{
+  renameSync(`icu-data/tzdata/icunew/2022a/44/le/${file}`,`deps/icu-small/source/data/in/${file}`)
+})
+fileNames.forEach((file) => {
+  spawnSync(
+    `icupkg`,[
+      `-a`,
+      file,
+      `icudt70l.dat`
+    ],{cwd:`deps/icu-small/source/data/in/`}
+  );
+});
+execSync('bzip2 -z deps/icu-small/source/data/in/icudt70l.dat')
+fileNames.forEach((file)=>{
+  renameSync(`deps/icu-small/source/data/in/${file}`,`icu-data/tzdata/icunew/2022a/44/le/${file}`)
+})
+execSync('rm -rf icu-data');
+
