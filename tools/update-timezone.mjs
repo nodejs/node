50b226f99cb53ae552b0e534b18706b8166c4d6b
Lenvin Gonsalves <lenvingonsalves@gmail.com>
2022-07-25 22:50:36 +0530

    tools: get latest tz version
    Fixes: https://github.com/nodejs/node/issues/43134

--- tools/update-timezone.mjs
+++ tools/update-timezone.mjs
@@ -3,31 +3,57 @@
 // Passing --dry will redirect output to stdout.
 import { execSync, spawnSync } from 'node:child_process';
 import { renameSync } from 'node:fs';
+import { exit } from 'node:process';
+
 const fileNames = [
   'zoneinfo64.res',
   'windowsZones.res',
   'timezoneTypes.res',
   'metaZones.res',
 ];
-
 execSync('rm -rf icu-data');
 execSync('git clone https://github.com/unicode-org/icu-data');
-execSync('bzip2 -d deps/icu-small/source/data/in/icudt70l.dat.bz2');
-fileNames.forEach((file)=>{
-  renameSync(`icu-data/tzdata/icunew/2022a/44/le/${file}`,`deps/icu-small/source/data/in/${file}`)
-})
+const dirs = spawnSync(
+  'ls', {
+    cwd: 'icu-data/tzdata/icunew',
+    stdio: ['inherit', 'pipe', 'inherit']
+  }
+);
+
+const currentVersion = process.versions.tz;
+const availableVersions = dirs.stdout
+      .toString()
+      .split('\n')
+      .filter((_) => _);
+
+const latestVersion =
+  availableVersions
+  .reduce(
+    (acc, version) => (version > acc ? version : acc),
+    availableVersions[0]);
+
+if (latestVersion === currentVersion) {
+  exit();
+}
+
+execSync('bzip2 -d deps/icu-small/source/data/in/icudt*.dat.bz2');
+fileNames.forEach((file) => {
+  renameSync(`icu-data/tzdata/icunew/${latestVersion}/44/le/${file}`, `deps/icu-small/source/data/in/${file}`);
+});
 fileNames.forEach((file) => {
   spawnSync(
-    `icupkg`,[
-      `-a`,
+    'icupkg', [
+      '-a',
       file,
-      `icudt70l.dat`
-    ],{cwd:`deps/icu-small/source/data/in/`}
+      'icudt*.dat',
+    ], { cwd: 'deps/icu-small/source/data/in/' }
   );
 });
-execSync('bzip2 -z deps/icu-small/source/data/in/icudt70l.dat')
-fileNames.forEach((file)=>{
-  renameSync(`deps/icu-small/source/data/in/${file}`,`icu-data/tzdata/icunew/2022a/44/le/${file}`)
-})
+execSync('bzip2 -z deps/icu-small/source/data/in/icudt*.dat');
+fileNames.forEach((file) => {
+  renameSync(
+    `deps/icu-small/source/data/in/${file}`,
+    `icu-data/tzdata/icunew/${latestVersion}/44/le/${file}`
+  );
+});
 execSync('rm -rf icu-data');
-
