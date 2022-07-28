d52e20cbc816b27726d890cbbecb82448e6093f0
Brian White <mscdex@mscdex.net>
2022-07-28 18:38:25 -0400

    deps: upgrade base64 to dc6a41ce36e
    Fixes: https://github.com/nodejs/node/issues/43894
    PR-URL: https://github.com/nodejs/node/pull/44032
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Mestery <mestery@protonmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Feng Yu <F3n67u@outlook.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>

--- deps/base64/base64/lib/arch/neon64/enc_loop.c
+++ deps/base64/base64/lib/arch/neon64/enc_loop.c
@@ -1,72 +1,6 @@
-#ifdef BASE64_NEON64_USE_ASM
-static inline void
-enc_loop_neon64_inner_asm (const uint8_t **s, uint8_t **o, const uint8x16x4_t tbl_enc)
-{
-	// This function duplicates the functionality of enc_loop_neon64_inner,
-	// but entirely with inline assembly. This gives a significant speedup
-	// over using NEON intrinsics, which do not always generate very good
-	// code. The logic of the assembly is directly lifted from the
-	// intrinsics version, so it can be used as a guide to this code.
-
-	// Temporary registers, used as scratch space.
-	uint8x16_t tmp0, tmp1, tmp2, tmp3;
-
-	// Numeric constant.
-	const uint8x16_t n63 = vdupq_n_u8(63);
-
-	__asm__ (
-
-		// Load 48 bytes and deinterleave. The bytes are loaded to
-		// hard-coded registers v12, v13 and v14, to ensure that they
-		// are contiguous. Increment the source pointer.
-		"ld3 {v12.16b, v13.16b, v14.16b}, [%[src]], #48 \n\t"
-
-		// Reshuffle the bytes using temporaries.
-		"ushr %[t0].16b, v12.16b,   #2         \n\t"
-		"ushr %[t1].16b, v13.16b,   #4         \n\t"
-		"ushr %[t2].16b, v14.16b,   #6         \n\t"
-		"sli  %[t1].16b, v12.16b,   #4         \n\t"
-		"sli  %[t2].16b, v13.16b,   #2         \n\t"
-		"and  %[t1].16b, %[t1].16b, %[n63].16b \n\t"
-		"and  %[t2].16b, %[t2].16b, %[n63].16b \n\t"
-		"and  %[t3].16b, v14.16b,   %[n63].16b \n\t"
-
-		// Translate the values to the Base64 alphabet.
-		"tbl v12.16b, {%[l0].16b, %[l1].16b, %[l2].16b, %[l3].16b}, %[t0].16b \n\t"
-		"tbl v13.16b, {%[l0].16b, %[l1].16b, %[l2].16b, %[l3].16b}, %[t1].16b \n\t"
-		"tbl v14.16b, {%[l0].16b, %[l1].16b, %[l2].16b, %[l3].16b}, %[t2].16b \n\t"
-		"tbl v15.16b, {%[l0].16b, %[l1].16b, %[l2].16b, %[l3].16b}, %[t3].16b \n\t"
-
-		// Store 64 bytes and interleave. Increment the dest pointer.
-		"st4 {v12.16b, v13.16b, v14.16b, v15.16b}, [%[dst]], #64 \n\t"
-
-		// Outputs (modified).
-		: [src] "+r"  (*s),
-		  [dst] "+r"  (*o),
-		  [t0]  "=&w" (tmp0),
-		  [t1]  "=&w" (tmp1),
-		  [t2]  "=&w" (tmp2),
-		  [t3]  "=&w" (tmp3)
-
-		// Inputs (not modified).
-		: [n63] "w" (n63),
-		  [l0]  "w" (tbl_enc.val[0]),
-		  [l1]  "w" (tbl_enc.val[1]),
-		  [l2]  "w" (tbl_enc.val[2]),
-		  [l3]  "w" (tbl_enc.val[3])
-
-		// Clobbers.
-		: "v12", "v13", "v14", "v15"
-	);
-}
-#endif
-
 static inline void
 enc_loop_neon64_inner (const uint8_t **s, uint8_t **o, const uint8x16x4_t tbl_enc)
 {
-#ifdef BASE64_NEON64_USE_ASM
-	enc_loop_neon64_inner_asm(s, o, tbl_enc);
-#else
 	// Load 48 bytes and deinterleave:
 	uint8x16x3_t src = vld3q_u8(*s);
 
@@ -86,7 +20,6 @@ enc_loop_neon64_inner (const uint8_t **s, uint8_t **o, const uint8x16x4_t tbl_en
 
 	*s += 48;
 	*o += 64;
-#endif
 }
 
 static inline void
