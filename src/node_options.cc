e4b3e9871664b4bef881e5a34413549364b7304d
Moshe Atlow <moshe@atlow.co.il>
2022-07-31 00:17:10 +0300

    esm: add `--import` flag

--- src/node_options.cc
+++ src/node_options.cc
@@ -608,10 +608,14 @@ EnvironmentOptionsParser::EnvironmentOptionsParser() {
   AddAlias("-pe", { "--print", "--eval" });
   AddAlias("-p", "--print");
   AddOption("--require",
-            "module to preload (option can be repeated)",
-            &EnvironmentOptions::preload_modules,
+            "CommonJS module to preload (option can be repeated)",
+            &EnvironmentOptions::preload_cjs_modules,
             kAllowedInEnvironment);
   AddAlias("-r", "--require");
+  AddOption("--import",
+            "ES module to preload (option can be repeated)",
+            &EnvironmentOptions::preload_esm_modules,
+            kAllowedInEnvironment);
   AddOption("--interactive",
             "always enter the REPL even if stdin does not appear "
             "to be a terminal",
--- test/es-module/test-esm-loader-http-imports.mjs
+++ test/es-module/test-esm-loader-http-imports.mjs
@@ -5,13 +5,18 @@ import http from 'node:http';
 import path from 'node:path';
 import { execPath } from 'node:process';
 import { promisify } from 'node:util';
-import { describe, it } from 'node:test';
+import { describe, it, after } from 'node:test';
 
 
 const files = {
   'main.mjs': 'export * from "./lib.mjs";',
   'lib.mjs': 'export { sum } from "./sum.mjs";',
   'sum.mjs': 'export function sum(a, b) { return a + b }',
+  'console.mjs': `
+    import { sum } from './sum.mjs';
+    globalThis.sum = sum;
+    console.log("loaded over http");
+  `,
 };
 
 const requestListener = ({ url }, rsp) => {
@@ -41,13 +46,26 @@ const {
   port,
 } = server.address();
 
-/**
- * ! If more cases are added to this test, they cannot (yet) be concurrent because there is no
- * ! `afterAll` teardown in which to close the server.
- */
-
-describe('ESM: http import via loader', { concurrency: false }, () => {
-  it('should work', async () => {
+describe('ESM: http import via loader', { concurrency: true }, () => {
+  it('should load using --import flag', async () => {
+    // ! MUST NOT use spawnSync to avoid blocking the event loop
+    const { code, signal, stderr, stdout } = await spawnPromisified(
+      execPath,
+      [
+        '--no-warnings',
+        '--loader',
+        fixtures.fileURL('es-module-loaders', 'http-loader.mjs'),
+        '--import', `http://${host}:${port}/console.mjs`,
+        '--eval',
+        'console.log(sum(1, 2))',
+      ]
+    );
+    assert.strictEqual(stderr, '');
+    assert.match(stdout, /loaded over http\r?\n3/);
+    assert.strictEqual(code, 0);
+    assert.strictEqual(signal, null);
+  });
+  it('should load using import inside --eval code', async () => {
     // ! MUST NOT use spawnSync to avoid blocking the event loop
     const { code, signal, stderr, stdout } = await spawnPromisified(
       execPath,
@@ -65,7 +83,7 @@ describe('ESM: http import via loader', { concurrency: false }, () => {
     assert.strictEqual(stdout, '[Module: null prototype] { sum: [Function: sum] }\n');
     assert.strictEqual(code, 0);
     assert.strictEqual(signal, null);
-
-    server.close(); // ! This MUST come after the final test, but inside the async `it` function
   });
+
+  after(() => server.close());
 });
