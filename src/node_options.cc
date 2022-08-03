67ecb5e53de8d25d1eec7f245b847c1feeda8dde
Keyhan Vakil <kvakil@sylph.kvakil.me>
2022-08-03 00:37:35 +0000

    fixup! doc,worker: deprecate --trace-atomics-wait

--- src/node_options.cc
+++ src/node_options.cc
@@ -540,7 +540,7 @@ EnvironmentOptionsParser::EnvironmentOptionsParser() {
             &EnvironmentOptions::throw_deprecation,
             kAllowedInEnvironment);
   AddOption("--trace-atomics-wait",
-            "trace Atomics.wait() operations",
+            "(deprecated) trace Atomics.wait() operations",
             &EnvironmentOptions::trace_atomics_wait,
             kAllowedInEnvironment);
   AddOption("--trace-deprecation",
