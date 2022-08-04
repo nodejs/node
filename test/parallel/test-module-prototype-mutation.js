fa115ee8ac32883c96df4a93fafb600bd665a5aa
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-08-05 00:41:48 +0200

    module: protect against prototype mutation
    Ensures that mutating the `Object` prototype does not influence the
    parsing of `package.json` files.
    
    Backport-PR-URL: https://github.com/nodejs-private/node-private/pull/373
    PR-URL: https://github.com/nodejs/node/pull/44007
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>

---
+++ test/parallel/test-module-prototype-mutation.js
@@ -0,0 +1,12 @@
+'use strict';
+const common = require('../common');
+const fixtures = require('../common/fixtures');
+const assert = require('assert');
+
+assert.strictEqual(
+  require(fixtures.path('es-module-specifiers', 'node_modules', 'no-main-field')),
+  'no main field'
+);
+
+import(fixtures.fileURL('es-module-specifiers', 'index.mjs'))
+  .then(common.mustCall((module) => assert.strictEqual(module.noMain, 'no main field')));
