6f9eff880bc660a80240f23884dcd3253a56fd1e
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-08-08 10:51:35 +0200

    esm: do not bind loader hook functions
    PR-URL: https://github.com/nodejs/node/pull/44122
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

---
+++ test/parallel/test-loaders-this-value-inside-hook-functions.mjs
@@ -0,0 +1,4 @@
+// Flags: --experimental-loader ./test/fixtures/es-module-loaders/loader-this-value-inside-hook-functions.mjs
+import '../common/index.mjs';
+
+// Actual test is inside the loader module.
