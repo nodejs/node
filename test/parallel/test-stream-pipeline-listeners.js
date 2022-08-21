69dc082fc6ec7bf3ebc89476f636a7b2a5f451eb
Robert Nagy <ronagy@icloud.com>
2022-08-21 13:04:49 +0200

    fixup

--- test/parallel/test-stream-pipeline-listeners.js
+++ test/parallel/test-stream-pipeline-listeners.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 
 process.on('uncaughtException', common.mustCall((err) => {
   assert.strictEqual(err.message, 'no way');
-}, 2));
+}, 1));
 
 // Ensure that listeners is removed if last stream is readable
 // And other stream's listeners unchanged
@@ -29,30 +29,6 @@ pipeline(a, b, common.mustCall((error) => {
     b.destroy(new Error('no way'));
   }, 100);
 }));
-
-// Async generators
-const c = new PassThrough();
-c.end('foobar');
-const d = pipeline(
-  c,
-  async function* (source) {
-    for await (const chunk of source) {
-      yield String(chunk).toUpperCase();
-    }
-  },
-  common.mustCall((error) => {
-    if (error) {
-      assert.ifError(error);
-    }
-
-    assert(c.listenerCount('error') > 0);
-    assert.strictEqual(d.listenerCount('error'), 0);
-    setTimeout(() => {
-      assert.strictEqual(b.listenerCount('error'), 0);
-      d.destroy(new Error('no way'));
-    }, 100);
-  })
-);
 
 // If last stream is not readable, will not throw and remove listeners
 const e = new PassThrough();
