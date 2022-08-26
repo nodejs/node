a6573ba761baf23de4a4405b963da5a79993105e
Darshan Sen <raisinten@gmail.com>
2022-08-26 15:40:31 +0530

    src: resolve stdint.h FIXME
    Co-authored-by: Anna Henningsen <github@addaleax.net>

--- src/node_http2.h
+++ src/node_http2.h
@@ -3,8 +3,6 @@
 
 #if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
 
-// FIXME(joyeecheung): nghttp2.h needs stdint.h to compile on Windows
-#include <cstdint>
 // clang-format off
 #include "node.h"  // nghttp2.h needs ssize_t
 // clang-format on
