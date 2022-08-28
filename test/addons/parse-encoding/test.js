9a5701a40283900b80a20f0d09e0a622017b8300
Tony Gorez <gorez.tony@gmail.com>
2022-08-28 21:03:08 +0200

    test: add more cases for parse-encoding

--- test/addons/parse-encoding/test.js
+++ test/addons/parse-encoding/test.js
@@ -13,11 +13,27 @@ assert.strictEqual(parseEncoding('binary'), 'LATIN1');
 assert.strictEqual(parseEncoding('buffer'), 'BUFFER');
 assert.strictEqual(parseEncoding('hex'), 'HEX');
 assert.strictEqual(parseEncoding('latin1'), 'LATIN1');
+
+// ucs2 variations
 assert.strictEqual(parseEncoding('ucs2'), 'UCS2');
+assert.strictEqual(parseEncoding('ucs-2'), 'UCS2');
+assert.strictEqual(parseEncoding('UCS2'), 'UCS2');
+assert.strictEqual(parseEncoding('UCS-2'), 'UCS2');
+
+// utf8 variations
 assert.strictEqual(parseEncoding('utf8'), 'UTF8');
-assert.strictEqual(parseEncoding('utf-16LE'), 'UCS2');
+assert.strictEqual(parseEncoding('utf-8'), 'UTF8');
+assert.strictEqual(parseEncoding('UTF8'), 'UTF8');
+assert.strictEqual(parseEncoding('UTF-8'), 'UTF8');
+
+// utf16le variations
+assert.strictEqual(parseEncoding('utf16le'), 'UCS2');
+assert.strictEqual(parseEncoding('utf-16le'), 'UCS2');
+assert.strictEqual(parseEncoding('UTF16LE'), 'UCS2');
+assert.strictEqual(parseEncoding('UTF-16LE'), 'UCS2');
+
+// unknown cases
 assert.strictEqual(parseEncoding('utf-buffer'), 'UNKNOWN');
 assert.strictEqual(parseEncoding('utf-16leNOT'), 'UNKNOWN');
-
 assert.strictEqual(parseEncoding('linary'), 'UNKNOWN');
 assert.strictEqual(parseEncoding('luffer'), 'UNKNOWN');
