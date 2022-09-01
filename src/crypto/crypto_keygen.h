cae9c5d5c3502f986d71c4b383f1bd8b13c29e68
Tobias Nießen <tniessen@tnie.de>
2022-09-01 07:09:22 +0000

    src: fix incorrect comments in crypto
    PR-URL: https://github.com/nodejs/node/pull/44470
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/crypto/crypto_keygen.h
+++ src/crypto/crypto_keygen.h
@@ -197,8 +197,8 @@ struct KeyPairGenTraits final {
 };
 
 struct SecretKeyGenConfig final : public MemoryRetainer {
-  size_t length;  // Expressed a a number of bits
-  char* out = nullptr;  // Placeholder for the generated key bytes
+  size_t length;        // In bytes.
+  char* out = nullptr;  // Placeholder for the generated key bytes.
 
   void MemoryInfo(MemoryTracker* tracker) const override;
   SET_MEMORY_INFO_NAME(SecretKeyGenConfig)
