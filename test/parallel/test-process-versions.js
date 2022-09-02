6f805ded9a3c70a2f39c572f9b6f886f355cbb44
RafaelGSS <rafael.nunu@hotmail.com>
2022-09-02 10:42:54 -0300

    test: process versions to support develop branch

--- test/parallel/test-process-versions.js
+++ test/parallel/test-process-versions.js
@@ -36,7 +36,7 @@ const actual_keys = Object.keys(process.versions).sort();
 
 assert.deepStrictEqual(actual_keys, expected_keys);
 
-const commonTemplate = /^\d+\.\d+\.\d+(?:-.*)?$/;
+const commonTemplate = /^\d+\.\d+\.\d+(?:\.\d+)?(?:-.*)?$/;
 
 assert.match(process.versions.ares, commonTemplate);
 assert.match(process.versions.brotli, commonTemplate);
