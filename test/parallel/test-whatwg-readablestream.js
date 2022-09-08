00bba35e0cdd5ae26b438973da85be5228ff9856
Daeyeon Jeong <daeyeon.dev@gmail.com>
2022-09-09 07:06:06 +0900

    stream: add `ReadableByteStream.tee()`
    This supports teeing readable byte streams to meet
    the latest web streams standards.
    
    Signed-off-by: Daeyeon Jeong daeyeon.dev@gmail.com
    PR-URL: https://github.com/nodejs/node/pull/44505
    Refs: https://streams.spec.whatwg.org/#readable-stream-tee
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-whatwg-readablestream.js
+++ test/parallel/test-whatwg-readablestream.js
@@ -1571,7 +1571,7 @@ class Source {
   assert(!readableStreamDefaultControllerCanCloseOrEnqueue(controller));
   readableStreamDefaultControllerEnqueue(controller);
   readableByteStreamControllerClose(controller);
-  readableByteStreamControllerEnqueue(controller);
+  readableByteStreamControllerEnqueue(controller, new Uint8Array(1));
 }
 
 {
