587367d1079156731f4475d23dede91e43d224eb
legendecas <legendecas@gmail.com>
2022-09-15 02:06:23 +0800

    perf_hooks: expose webperf global scope interfaces
    Exposes `PerformanceEntry`, `PerformanceMark`, `PerformanceMeasure`,
    `PerformanceObserver`, `PerformanceObserverEntryList`,
    and `PerformanceResourceTiming` to the global scope.
    
    PR-URL: https://github.com/nodejs/node/pull/44483
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/wpt/test-user-timing.js
+++ test/wpt/test-user-timing.js
@@ -7,37 +7,5 @@ const runner = new WPTRunner('user-timing');
 
 runner.pretendGlobalThisAs('Window');
 runner.brandCheckGlobalScopeAttribute('performance');
-runner.setInitScript(`
-  const {
-    PerformanceEntry,
-    PerformanceMark,
-    PerformanceMeasure,
-    PerformanceObserver,
-  } = require('perf_hooks');
-  Object.defineProperty(global, 'PerformanceEntry', {
-    value: PerformanceEntry,
-    enumerable: false,
-    writable: true,
-    configurable: true,
-  });
-  Object.defineProperty(global, 'PerformanceMark', {
-    value: PerformanceMark,
-    enumerable: false,
-    writable: true,
-    configurable: true,
-  });
-  Object.defineProperty(global, 'PerformanceMeasure', {
-    value: PerformanceMeasure,
-    enumerable: false,
-    writable: true,
-    configurable: true,
-  });
-  Object.defineProperty(global, 'PerformanceObserver', {
-    value: PerformanceObserver,
-    enumerable: false,
-    writable: true,
-    configurable: true,
-  });
-`);
 
 runner.runJsTests();
