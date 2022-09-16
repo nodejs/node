23fd636eadc0ee8f802e92f4fc1791734f75f159
“Pooja <pakcutie14@gmail.com>
2022-09-16 12:44:48 -0700

    test: corrected failing lint test

--- test/parallel/test-debugger-unavailable-port.js
+++ test/parallel/test-debugger-unavailable-port.js
@@ -11,26 +11,26 @@ const { createServer } = require('net');
 
 // Launch w/ unavailable port.
 common.mustCall(async () => {
-    const blocker = createServer((socket) => socket.end());
-    const port = await new Promise((resolve, reject) => {
-      blocker.on('error', reject);
-      blocker.listen(0, '127.0.0.1', () => resolve(blocker.address().port));
-    });
-  
-    try {
-      const script = fixtures.path('debugger', 'three-lines.js');
-      const cli = startCLI([`--port=${port}`, script]);
-      const code = await cli.quit();
-  
-      assert.doesNotMatch(
-        cli.output,
-        /report this bug/,
-        'Omits message about reporting this as a bug');
-      assert.ok(
-        cli.output.includes(`waiting for 127.0.0.1:${port} to be free`),
-        'Tells the user that the port wasn\'t available');
-      assert.strictEqual(code, 1);
-    } finally {
-      blocker.close();
-    }
+  const blocker = createServer((socket) => socket.end());
+  const port = await new Promise((resolve, reject) => {
+    blocker.on('error', reject);
+    blocker.listen(0, '127.0.0.1', () => resolve(blocker.address().port));
+  });
+
+  try {
+    const script = fixtures.path('debugger', 'three-lines.js');
+    const cli = startCLI([`--port=${port}`, script]);
+    const code = await cli.quit();
+
+    assert.doesNotMatch(
+      cli.output,
+      /report this bug/,
+      'Omits message about reporting this as a bug');
+    assert.ok(
+      cli.output.includes(`waiting for 127.0.0.1:${port} to be free`),
+      'Tells the user that the port wasn\'t available');
+    assert.strictEqual(code, 1);
+  } finally {
+    blocker.close();
+  }
 })();
