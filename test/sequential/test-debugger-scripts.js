fa95c4b45864de9c4fd955da2737cc6f0890200c
mmeenapriya <42589859+mmeenapriya@users.noreply.github.com>
2022-09-16 16:38:47 -0500

    test: update test-debugger-scripts to use await/async

--- test/sequential/test-debugger-scripts.js
+++ test/sequential/test-debugger-scripts.js
@@ -9,39 +9,32 @@ const startCLI = require('../common/debugger');
 const assert = require('assert');
 
 // List scripts.
-{
-  const script = fixtures.path('debugger', 'three-lines.js');
-  const cli = startCLI([script]);
+const script = fixtures.path('debugger', 'three-lines.js');
+const cli = startCLI([script]);
 
-  function onFatal(error) {
-    cli.quit();
-    throw error;
-  }
-
-  return cli.waitForInitialBreak()
-    .then(() => cli.waitForPrompt())
-    .then(() => cli.command('scripts'))
-    .then(() => {
-      assert.match(
-        cli.output,
-        /^\* \d+: \S+debugger(?:\/|\\)three-lines\.js/m,
-        'lists the user script');
-      assert.doesNotMatch(
-        cli.output,
-        /\d+: node:internal\/buffer/,
-        'omits node-internal scripts');
-    })
-    .then(() => cli.command('scripts(true)'))
-    .then(() => {
-      assert.match(
-        cli.output,
-        /\* \d+: \S+debugger(?:\/|\\)three-lines\.js/,
-        'lists the user script');
-      assert.match(
-        cli.output,
-        /\d+: node:internal\/buffer/,
-        'includes node-internal scripts');
-    })
-    .then(() => cli.quit())
-    .then(null, onFatal);
-}
+(async () => {
+try{
+  await cli.waitForInitialBreak();
+  await cli.waitForPrompt();
+  await cli.command('scripts');
+  assert.match(
+    cli.output,
+    /^\* \d+: \S+debugger(?:\/|\\)three-lines\.js/m,
+    'lists the user script');
+  assert.doesNotMatch(
+    cli.output,
+    /\d+: node:internal\/buffer/,
+    'omits node-internal scripts');
+  await cli.command('scripts(true)');
+  assert.match(
+    cli.output,
+    /\* \d+: \S+debugger(?:\/|\\)three-lines\.js/,
+    'lists the user script');
+  assert.match(
+    cli.output,
+    /\d+: node:internal\/buffer/,
+    'includes node-internal scripts');
+ } finally {
+    cli.quit()
+ }
+})()
