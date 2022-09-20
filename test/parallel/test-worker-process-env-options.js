e3695726ac3d3bef18f07b5c0a04fc8b40373d07
Maël Nison <nison.mael@gmail.com>
2022-09-20 17:12:22 +0400

    Lint

--- test/parallel/test-worker-process-env-options.js
+++ test/parallel/test-worker-process-env-options.js
@@ -1,10 +1,11 @@
 'use strict';
+require('../common');
 const { Worker } = require('node:worker_threads');
 
 if (!require.main) {
   globalThis.setup = true;
 } else {
-  process.env.NODE_OPTIONS ??= ``;
+  process.env.NODE_OPTIONS ??= '';
   process.env.NODE_OPTIONS += ` --require ${JSON.stringify(__filename)}`;
 
   new Worker(`
