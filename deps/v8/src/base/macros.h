bd265f582a972aa3e957f1faf6d4951941fda85b
Michaël Zasso <targos@protonmail.com>
2022-09-21 15:47:37 +0200

    deps: always define V8_EXPORT_PRIVATE as no-op
    dllexport introduces issues when compiling with MSVC.
    
    PR-URL: https://github.com/nodejs/node/pull/47251
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- deps/v8/src/base/macros.h
+++ deps/v8/src/base/macros.h
@@ -430,9 +430,9 @@ bool is_inbounds(float_t v) {
 // Setup for Windows shared library export.
 #define V8_EXPORT_ENUM
 #ifdef BUILDING_V8_SHARED_PRIVATE
-#define V8_EXPORT_PRIVATE __declspec(dllexport)
+#define V8_EXPORT_PRIVATE
 #elif USING_V8_SHARED_PRIVATE
-#define V8_EXPORT_PRIVATE __declspec(dllimport)
+#define V8_EXPORT_PRIVATE
 #else
 #define V8_EXPORT_PRIVATE
 #endif  // BUILDING_V8_SHARED
@@ -442,8 +442,8 @@ bool is_inbounds(float_t v) {
 // Setup for Linux shared library export.
 #if V8_HAS_ATTRIBUTE_VISIBILITY
 #ifdef BUILDING_V8_SHARED_PRIVATE
-#define V8_EXPORT_PRIVATE __attribute__((visibility("default")))
-#define V8_EXPORT_ENUM V8_EXPORT_PRIVATE
+#define V8_EXPORT_PRIVATE
+#define V8_EXPORT_ENUM
 #else
 #define V8_EXPORT_PRIVATE
 #define V8_EXPORT_ENUM
