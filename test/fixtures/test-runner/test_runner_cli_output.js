32028b9edbcda927aaa29c2ee4627169e5d6ea3a
Wassim Chegham <github@wassim.dev>
2022-09-23 00:30:22 +0200

    test_runner: add initial stream parsing foundation
    Co-authored-by: @MoLow

---
+++ test/fixtures/test-runner/test_runner_cli_output.js
@@ -0,0 +1,4 @@
+// Flags: --no-warnings --test test/fixtures/test-runner/index.test.js test/fixtures/test-runner/nested.js test/fixtures/test-runner/invalid_tap.js
+// FlagsOnly
+'use strict';
+require('../common');
