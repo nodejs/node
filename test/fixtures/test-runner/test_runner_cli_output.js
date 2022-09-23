86d554b9df4b9140fba3e2b5d5637616017e756f
Wassim Chegham <github@wassim.dev>
2022-09-23 09:59:05 +0200

    test_runner: remove unused fixture

--- test/fixtures/test-runner/test_runner_cli_output.js
+++
@@ -1,4 +0,0 @@
-// Flags: --no-warnings --test test/fixtures/test-runner/index.test.js test/fixtures/test-runner/nested.js test/fixtures/test-runner/invalid_tap.js
-// FlagsOnly
-'use strict';
-require('../common');
