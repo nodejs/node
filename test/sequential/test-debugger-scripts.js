32e44f76454d7bbd26374c9c42b1e2e8283bb8a7
Rich Trott <rtrott@gmail.com>
2022-09-26 15:23:06 -0700

    fixup! test: update test-debugger-scripts to use await/async -fix lint errors

--- test/sequential/test-debugger-scripts.js
+++ test/sequential/test-debugger-scripts.js
@@ -13,39 +13,8 @@ const assert = require('assert');
   const script = fixtures.path('debugger', 'three-lines.js');
   const cli = startCLI([script]);
 
-  function onFatal(error) {
-    cli.quit();
-    throw error;
-  }
-
-  // return cli.waitForInitialBreak()
-  //   .then(() => cli.waitForPrompt())
-  //   .then(() => cli.command('scripts'))
-  //   .then(() => {
-  //     assert.match(
-  //       cli.output,
-  //       /^\* \d+: \S+debugger(?:\/|\\)three-lines\.js/m,
-  //       'lists the user script');
-  //     assert.doesNotMatch(
-  //       cli.output,
-  //       /\d+: node:internal\/buffer/,
-  //       'omits node-internal scripts');
-  //   })
-  //   .then(() => cli.command('scripts(true)'))
-  //   .then(() => {
-  //     assert.match(
-  //       cli.output,
-  //       /\* \d+: \S+debugger(?:\/|\\)three-lines\.js/,
-  //       'lists the user script');
-  //     assert.match(
-  //       cli.output,
-  //       /\d+: node:internal\/buffer/,
-  //       'includes node-internal scripts');
-  //   })
-  //   .then(() => cli.quit())
-  //   .then(null, onFatal);
-
   (async () => {
+    try {
       await cli.waitForInitialBreak();
       await cli.waitForPrompt();
       await cli.command('scripts');
@@ -66,8 +35,8 @@ const assert = require('assert');
         cli.output,
         /\d+: node:internal\/buffer/,
         'includes node-internal scripts');
-  })()
-  .then(() => cli.quit())
-  .then(null, onFatal);
-
+    } finally {
+      await cli.quit();
+    }
+  })().then(common.mustCall);
 }
