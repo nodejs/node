ec2b443f2dc77b35ff588645774af66701fe5000
andiemontoyeah <55297964+andiemontoyeah@users.noreply.github.com>
2022-09-26 19:39:39 -0700

    tools: update eslint-check.js to object style
    Updated rule from deprecated function-style
    format to object-style format.
    
    Refs: https://eslint.org/docs/latest/developer-guide/working-with-rules
    PR-URL: https://github.com/nodejs/node/pull/44706
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- tools/eslint-rules/eslint-check.js
+++ tools/eslint-rules/eslint-check.js
@@ -12,53 +12,54 @@ const utils = require('./rules-utils.js');
 const msg = 'Please add a skipIfEslintMissing() call to allow this test to ' +
             'be skipped when Node.js is built from a source tarball.';
 
-module.exports = function(context) {
-  const missingCheckNodes = [];
-  let commonModuleNode = null;
-  let hasEslintCheck = false;
+module.exports = {
+  meta: {
+    fixable: 'code',
+  },
+  create: function(context) {
+    const missingCheckNodes = [];
+    let commonModuleNode = null;
+    let hasEslintCheck = false;
 
-  function testEslintUsage(context, node) {
-    if (utils.isRequired(node, ['../../tools/node_modules/eslint'])) {
-      missingCheckNodes.push(node);
+    function testEslintUsage(context, node) {
+      if (utils.isRequired(node, ['../../tools/node_modules/eslint'])) {
+        missingCheckNodes.push(node);
+      }
+
+      if (utils.isCommonModule(node)) {
+        commonModuleNode = node;
+      }
     }
 
-    if (utils.isCommonModule(node)) {
-      commonModuleNode = node;
+    function checkMemberExpression(context, node) {
+      if (utils.usesCommonProperty(node, ['skipIfEslintMissing'])) {
+        hasEslintCheck = true;
+      }
     }
-  }
 
-  function checkMemberExpression(context, node) {
-    if (utils.usesCommonProperty(node, ['skipIfEslintMissing'])) {
-      hasEslintCheck = true;
-    }
-  }
-
-  function reportIfMissing(context) {
-    if (!hasEslintCheck) {
-      missingCheckNodes.forEach((node) => {
-        context.report({
-          node,
-          message: msg,
-          fix: (fixer) => {
-            if (commonModuleNode) {
-              return fixer.insertTextAfter(
-                commonModuleNode,
-                '\ncommon.skipIfEslintMissing();'
-              );
+    function reportIfMissing(context) {
+      if (!hasEslintCheck) {
+        missingCheckNodes.forEach((node) => {
+          context.report({
+            node,
+            message: msg,
+            fix: (fixer) => {
+              if (commonModuleNode) {
+                return fixer.insertTextAfter(
+                  commonModuleNode,
+                  '\ncommon.skipIfEslintMissing();'
+                );
+              }
             }
-          }
+          });
         });
-      });
+      }
     }
+
+    return {
+      'CallExpression': (node) => testEslintUsage(context, node),
+      'MemberExpression': (node) => checkMemberExpression(context, node),
+      'Program:exit': () => reportIfMissing(context)
+    };
   }
-
-  return {
-    'CallExpression': (node) => testEslintUsage(context, node),
-    'MemberExpression': (node) => checkMemberExpression(context, node),
-    'Program:exit': () => reportIfMissing(context)
-  };
-};
-
-module.exports.meta = {
-  fixable: 'code'
 };
