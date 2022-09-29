341133f57be7dc63b41bc92b3f193b5f3324b11e
Hope Olaidé <79100769+hopeolaide@users.noreply.github.com>
2022-09-29 05:15:10 -0400

    test: use async/await in test-debugger-sb-before-load
    PR-URL: https://github.com/nodejs/node/pull/44697
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- test/sequential/test-debugger-sb-before-load.js
+++ test/sequential/test-debugger-sb-before-load.js
@@ -10,35 +10,24 @@ const assert = require('assert');
 const path = require('path');
 
 // Using sb before loading file.
-{
-  const scriptFullPath = fixtures.path('debugger', 'cjs', 'index.js');
-  const script = path.relative(process.cwd(), scriptFullPath);
 
-  const otherScriptFullPath = fixtures.path('debugger', 'cjs', 'other.js');
-  const otherScript = path.relative(process.cwd(), otherScriptFullPath);
+const scriptFullPath = fixtures.path('debugger', 'cjs', 'index.js');
+const script = path.relative(process.cwd(), scriptFullPath);
 
-  const cli = startCLI([script]);
+const otherScriptFullPath = fixtures.path('debugger', 'cjs', 'other.js');
+const otherScript = path.relative(process.cwd(), otherScriptFullPath);
 
-  function onFatal(error) {
-    cli.quit();
-    throw error;
-  }
+const cli = startCLI([script]);
 
-  cli.waitForInitialBreak()
-    .then(() => cli.waitForPrompt())
-    .then(() => cli.command('sb("other.js", 2)'))
-    .then(() => {
-      assert.match(
-        cli.output,
-        /not loaded yet/,
-        'warns that the script was not loaded yet');
-    })
-    .then(() => cli.stepCommand('cont'))
-    .then(() => {
-      assert.ok(
-        cli.output.includes(`break in ${otherScript}:2`),
-        'found breakpoint in file that was not loaded yet');
-    })
-    .then(() => cli.quit())
-    .then(null, onFatal);
-}
+(async () => {
+  await cli.waitForInitialBreak();
+  await cli.waitForPrompt();
+  await cli.command('sb("other.js", 2)');
+  assert.match(cli.output, /not loaded yet/,
+               'warns that the script was not loaded yet');
+  await cli.stepCommand('cont');
+  assert.ok(cli.output.includes(`break in ${otherScript}:2`),
+            'found breakpoint in file that was not loaded yet');
+})()
+.then(common.mustCall())
+.finally(() => cli.quit());
