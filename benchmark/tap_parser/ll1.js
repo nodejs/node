2a5c87bdb983ce17c976a3fb16a0ebe83c95b14e
Wassim Chegham <github@wassim.dev>
2022-09-30 22:43:54 +0200

    test_runner: add regex parser
    Co-authored-by: @MoLow

---
+++ benchmark/tap_parser/ll1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const common = require('../common.js');
+const path = require('path');
+const { spawnSync } = require('child_process');
+
+const pwd = path.resolve(__dirname, '../../');
+const bench = common.createBenchmark(main, { n: [1024] });
+
+function main({ n }) {
+  bench.start();
+  for (let i = 0; i < n; ++i) {
+    spawnSync(process.execPath, ['--no-warnings', '--test',
+      path.resolve(pwd, 'test/fixtures/test-runner/index.test.js'),
+      path.resolve(pwd, 'test/fixtures/test-runner/nested.js'),
+      path.resolve(pwd, 'test/fixtures/test-runner/invalid_tap.js'),
+    ], { env: { REGEX_TAP_PARSER: 0 } });
+  }
+  bench.end(n);
+}
