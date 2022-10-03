4f214eae6cda5ab0e454b308611143e6f04561c1
Tony Gorez <gorez.tony@gmail.com>
2022-10-03 17:33:12 +0200

    fix: lint test file

--- test/parallel/test-repl-async-iife.js
+++ test/parallel/test-repl-async-iife.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 require('../common');
 
 // Note: This test ensures that async IIFE doesn't crash
@@ -8,4 +8,3 @@ const repl = require('repl').start({ terminal: true });
 
 repl.write('(async() => { })()\n');
 repl.write('.exit\n');
-
