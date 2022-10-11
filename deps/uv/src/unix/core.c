e7fb2ce189679db866a1dc76a68fc0b8e8b22dff
Momtchil Momtchev <momtchil@momtchev.com>
2022-10-11 13:52:35 +0200

    lib: process timers after the fds

--- deps/uv/src/unix/core.c
+++ deps/uv/src/unix/core.c
@@ -377,8 +377,8 @@ int uv_run(uv_loop_t* loop, uv_run_mode mode) {
 
   while (r != 0 && loop->stop_flag == 0) {
     uv__update_time(loop);
-    uv__run_timers(loop);
     ran_pending = uv__run_pending(loop);
+    uv__run_timers(loop);
     uv__run_idle(loop);
     uv__run_prepare(loop);
 
