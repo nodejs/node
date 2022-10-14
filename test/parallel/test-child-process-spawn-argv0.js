62a1f48f91199a217a0e487fc73b7752dc2dc45d
Juan José <soyjuanarbol@gmail.com>
2022-10-14 13:24:26 -0500

    test: improve test coverage in `test-child-process-spawn-argv0.js`
    PR-URL: https://github.com/nodejs/node/pull/44955
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-child-process-spawn-argv0.js
+++ test/parallel/test-child-process-spawn-argv0.js
@@ -1,5 +1,5 @@
 'use strict';
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const cp = require('child_process');
 
@@ -16,3 +16,12 @@ assert.strictEqual(noArgv0.stdout.toString().trim(), process.execPath);
 const withArgv0 = cp.spawnSync(process.execPath, [__filename, 'child'],
                                { argv0: 'withArgv0' });
 assert.strictEqual(withArgv0.stdout.toString().trim(), 'withArgv0');
+
+assert.throws(() => {
+  cp.spawnSync(process.execPath, [__filename, 'child'], { argv0: [] });
+}, {
+  code: 'ERR_INVALID_ARG_TYPE',
+  name: 'TypeError',
+  message: 'The "options.argv0" property must be of type string.' +
+           common.invalidArgTypeHelper([])
+});
