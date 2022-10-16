880eacdc0e41ff0ae5173d37ca16b65b3b071897
Wassim Chegham <github@wassim.dev>
2022-10-16 15:13:55 +0200

    test_runner: add output message tests

---
+++ test/message/test_runner_output_cli.js
@@ -0,0 +1,3 @@
+// Flags: --no-warnings --test test/fixtures/test-runner/index.test.js test/fixtures/test-runner/nested.js test/fixtures/test-runner/invalid_tap.js
+'use strict';
+require('../common');
