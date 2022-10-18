dbc696d36399b5b927d5dc78fa568ab2ce4a4147
Rich Trott <rtrott@gmail.com>
2022-10-18 05:13:41 -0700

    test: remove unnecessary noop function args to mustCall()
    PR-URL: https://github.com/nodejs/node/pull/45027
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-promise-hook-exceptions.js
+++ test/parallel/test-promise-hook-exceptions.js
@@ -26,6 +26,6 @@ testHook('onSettled');
 testHook('onBefore');
 testHook('onAfter');
 
-const stop = promiseHooks.onInit(common.mustCall(() => {}, 2));
+const stop = promiseHooks.onInit(common.mustCall(2));
 
 Promise.resolve().then(stop);
