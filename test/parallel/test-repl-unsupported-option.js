fc58b61e8237b7e28484ccc1b83c67e71a78fce7
Juan José <soyjuanarbol@gmail.com>
2022-10-18 07:14:02 -0500

    test: improve assertions in `test-repl-unsupported-option.js`
    PR-URL: https://github.com/nodejs/node/pull/44953
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: Kohei Ueno <kohei.ueno119@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-repl-unsupported-option.js
+++ test/parallel/test-repl-unsupported-option.js
@@ -7,4 +7,5 @@ const { spawnSync } = require('child_process');
 
 const result = spawnSync(process.execPath, ['-i', '--input-type=module']);
 
-assert.match(result.stderr.toString(), /Cannot specify --input-type for REPL/);
+assert.strictEqual(result.stderr.toString(), 'Cannot specify --input-type for REPL\n');
+assert.notStrictEqual(result.exitCode, 0);
