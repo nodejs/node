607d3481bea89981294f82e15a9e242846c6cb11
Rich Trott <rtrott@gmail.com>
2022-10-22 08:43:27 -0700

    fixup! url: remove \t \n \r in url.parse() similar to WHATWG

--- test/parallel/test-url-parse-format.js
+++ test/parallel/test-url-parse-format.js
@@ -843,11 +843,11 @@ const parseTests = {
     port: null,
     hostname: 'a.b',
     hash: null,
-    pathname: '/bcdref%20g%22hq%27j%3Ckl%3E',
-    path: '/bcdref%20g%22hq%27j%3Ckl%3E?mn%5Cop%5Eq=r%6099%7Bst%7Cuv%7Dwz',
+    pathname: '/%09bc%0Adr%0Def%20g%22hq%27j%3Ckl%3E',
+    path: '/%09bc%0Adr%0Def%20g%22hq%27j%3Ckl%3E?mn%5Cop%5Eq=r%6099%7Bst%7Cuv%7Dwz',
     search: '?mn%5Cop%5Eq=r%6099%7Bst%7Cuv%7Dwz',
     query: 'mn%5Cop%5Eq=r%6099%7Bst%7Cuv%7Dwz',
-    href: 'http://a.b/bcdref%20g%22hq%27j%3Ckl%3E?mn%5Cop%5Eq=r%6099%7Bst%7Cuv%7Dwz'
+    href: 'http://a.b/%09bc%0Adr%0Def%20g%22hq%27j%3Ckl%3E?mn%5Cop%5Eq=r%6099%7Bst%7Cuv%7Dwz'
   },
 
   'http://a\r" \t\n<\'b:b@c\r\nd/e?f': {
