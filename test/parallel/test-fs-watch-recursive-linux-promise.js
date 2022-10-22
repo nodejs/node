00ab12d6402e00b678b379abe75ddd50bd8d0d92
Yagiz Nizipli <yagiz@nizipli.com>
2022-10-22 15:58:03 -0400

    fs: add promise support to linux watcher

---
+++ test/parallel/test-fs-watch-recursive-linux-promise.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const common = require('../common');
+
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const { randomUUID } = require('crypto');
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs/promises');
+
+const tmpdir = require('../common/tmpdir');
+const testDir = tmpdir.path;
+tmpdir.refresh();
+
+(async () => {
+  {
+    // Add a file to already watching folder
+
+    const testsubdir = await fs.mkdtemp(testDir + path.sep);
+    const file = `${randomUUID()}.txt`;
+    const filePath = path.join(testsubdir, file);
+    const watcher = fs.watch(testsubdir, { recursive: true });
+
+    setTimeout(async () => {
+      await fs.writeFile(filePath, 'world');
+    }, 100);
+
+    for await (const payload of watcher) {
+      const { eventType, filename } = payload;
+
+      assert.ok(eventType === 'change' || eventType === 'rename');
+
+      if (filename === file) {
+        break;
+      }
+    }
+  }
+})().then(common.mustCall());
