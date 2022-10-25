af9140088621abd09016848f4526d66b7a81b9ba
RafaelGSS <rafael.nunu@hotmail.com>
2022-10-25 00:23:27 -0300

    policy: makeRequireFunction on mainModule.require
    Signed-off-by: RafaelGSS <rafael.nunu@hotmail.com>
    
    Co-authored-by: Bradley Farias <bradley.meck@gmail.com>
    Refs: https://hackerone.com/bugs?subject=nodejs&report_id=1747642
    CVE-ID: CVE-2023-23918
    PR-URL: https://github.com/nodejs-private/node-private/pull/358
    Reviewed-by: Bradley Farias <bradley.meck@gmail.com>
    Reviewed-by: Matteo Collina <matteo.collina@gmail.com>

--- src/env_properties.h
+++ src/env_properties.h
@@ -24,7 +24,8 @@
   V(napi_type_tag, "node:napi:type_tag")                                       \
   V(napi_wrapper, "node:napi:wrapper")                                         \
   V(untransferable_object_private_symbol, "node:untransferableObject")         \
-  V(exit_info_private_symbol, "node:exit_info_private_symbol")
+  V(exit_info_private_symbol, "node:exit_info_private_symbol")                 \
+  V(require_private_symbol, "node:require_private_symbol")
 
 // Symbols are per-isolate primitives but Environment proxies them
 // for the sake of convenience.
