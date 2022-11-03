f0041404d2119ef1e308af2083bdf638aa46b627
Darshan Sen <raisinten@gmail.com>
2022-11-03 21:18:42 +0530

    fixup! test: address review comments and skip if no intl
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test/parallel/test-tz-version.js
+++ test/parallel/test-tz-version.js
@@ -1,12 +1,17 @@
 'use strict';
 
-require('../common');
-const { path } = require('../common/fixtures');
+const common = require('../common');
+
+if (!common.hasIntl) {
+  common.skip('missing intl');
+}
+
+const fixtures = require('../common/fixtures');
 
 // This test ensures the correctness of the automated timezone upgrade PRs.
 
 const { strictEqual } = require('assert');
 const { readFileSync } = require('fs');
 
-const expectedVersion = readFileSync(path('tz-version.txt'), 'utf8').trim();
+const expectedVersion = readFileSync(fixtures.path('tz-version.txt'), 'utf8').trim();
 strictEqual(process.versions.tz, expectedVersion);
