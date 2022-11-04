693f9bc0b7d4f25711c66e65f326f9226b91d55f
Darshan Sen <raisinten@gmail.com>
2022-11-04 13:37:46 +0530

    fixup! test: add extra guards
    Refs: https://github.com/nodejs/node/pull/45299#discussion_r1013135497
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test/parallel/test-tz-version.js
+++ test/parallel/test-tz-version.js
@@ -6,6 +6,10 @@ if (!common.hasIntl) {
   common.skip('missing Intl');
 }
 
+if (process.config.variables.icu_path !== 'deps/icu-small') {
+  common.skip('not using the icu data file present in deps/icu-small/source/data/in/icudt##l.dat.bz2');
+}
+
 const fixtures = require('../common/fixtures');
 
 // This test ensures the correctness of the automated timezone upgrade PRs.
