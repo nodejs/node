26fc097da457833f79fb04e7a930e3521734c240
Yagiz Nizipli <yagiz@nizipli.com>
2022-11-06 15:27:41 -0500

    util: improve textdecoder decode performance
    PR-URL: https://github.com/nodejs/node/pull/45294
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

---
+++ benchmark/util/text-decoder.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const common = require('../common.js');
+
+const bench = common.createBenchmark(main, {
+  encoding: ['utf-8', 'latin1', 'iso-8859-3'],
+  ignoreBOM: [0, 1],
+  len: [256, 1024 * 16, 1024 * 512],
+  n: [1e6]
+});
+
+function main({ encoding, len, n, ignoreBOM }) {
+  const buf = Buffer.allocUnsafe(len);
+  const decoder = new TextDecoder(encoding, { ignoreBOM });
+
+  bench.start();
+  decoder.decode(buf);
+  bench.end(n);
+}
