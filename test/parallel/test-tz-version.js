c3ee43496199321faf2fe809c42970b9c9a55372
Darshan Sen <raisinten@gmail.com>
2022-11-06 16:34:51 +0530

    fixup! test: explain difference between small-icu and deps/icu-small
    Asked for in
    https://github.com/nodejs/node/pull/45299#discussion_r1014650144.
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test/parallel/test-tz-version.js
+++ test/parallel/test-tz-version.js
@@ -8,6 +8,12 @@ if (!common.hasIntl) {
 
 // Refs: https://github.com/nodejs/node/blob/1af63a90ca3a59ca05b3a12ad7dbea04008db7d9/configure.py#L1694-L1711
 if (process.config.variables.icu_path !== 'deps/icu-small') {
+  // If Node.js is configured to use its built-in ICU, it uses a strict subset
+  // of ICU formed using `tools/icu/shrink-icu-src.py`, which is present in
+  // `deps/icu-small`. It is not the same as configuring the build with
+  // `./configured --with-intl=small-icu`. The latter only uses a subset of the
+  // locales, i.e., it uses the English locale, `root,en`, by default and other
+  // locales can also be specified using the `--with-icu-locales` option.
   common.skip('not using the icu data file present in deps/icu-small/source/data/in/icudt##l.dat.bz2');
 }
 
