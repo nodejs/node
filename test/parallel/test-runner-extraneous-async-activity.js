5fdf374c740692e2590b3acdcf3284e271001ce0
MURAKAMI Masahiko <fossamagna2@gmail.com>
2022-11-07 21:02:56 +0900

    test_runner: avoid swallowing of asynchronously thrown errors
    Fixes: https://github.com/nodejs/node/issues/44612
    PR-URL: https://github.com/nodejs/node/pull/45264
    Backport-PR-URL: https://github.com/nodejs/node/pull/46839
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

---
+++ test/parallel/test-runner-extraneous-async-activity.js
@@ -0,0 +1,31 @@
+'use strict';
+require('../common');
+const fixtures = require('../common/fixtures');
+const assert = require('assert');
+const { spawnSync } = require('child_process');
+
+{
+  const child = spawnSync(process.execPath, [
+    '--test',
+    fixtures.path('test-runner', 'extraneous_set_immediate_async.mjs'),
+  ]);
+  const stdout = child.stdout.toString();
+  assert.match(stdout, /^# pass 0$/m);
+  assert.match(stdout, /^# fail 1$/m);
+  assert.match(stdout, /^# cancelled 0$/m);
+  assert.strictEqual(child.status, 1);
+  assert.strictEqual(child.signal, null);
+}
+
+{
+  const child = spawnSync(process.execPath, [
+    '--test',
+    fixtures.path('test-runner', 'extraneous_set_timeout_async.mjs'),
+  ]);
+  const stdout = child.stdout.toString();
+  assert.match(stdout, /^# pass 0$/m);
+  assert.match(stdout, /^# fail 1$/m);
+  assert.match(stdout, /^# cancelled 0$/m);
+  assert.strictEqual(child.status, 1);
+  assert.strictEqual(child.signal, null);
+}
