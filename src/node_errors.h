ca3ed363ffefa4819c1414aff7ffca34b53a7b95
Yagiz Nizipli <yagiz@nizipli.com>
2022-11-09 09:18:42 -0500

    util: improve text decoder performance
    PR-URL: https://github.com/nodejs/node/pull/45388
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/node_errors.h
+++ src/node_errors.h
@@ -60,6 +60,7 @@ void OOMErrorHandler(const char* location, const v8::OOMDetails& details);
   V(ERR_CRYPTO_JOB_INIT_FAILED, Error)                                         \
   V(ERR_DLOPEN_DISABLED, Error)                                                \
   V(ERR_DLOPEN_FAILED, Error)                                                  \
+  V(ERR_ENCODING_INVALID_ENCODED_DATA, TypeError)                              \
   V(ERR_EXECUTION_ENVIRONMENT_NOT_AVAILABLE, Error)                            \
   V(ERR_INVALID_ADDRESS, Error)                                                \
   V(ERR_INVALID_ARG_VALUE, TypeError)                                          \
