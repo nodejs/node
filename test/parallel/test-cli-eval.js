91d8336a575417dee59d09a5099ba982a0ba0478
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-11-15 14:44:57 +0100

    fix test on Windows

--- test/parallel/test-cli-eval.js
+++ test/parallel/test-cli-eval.js
@@ -356,10 +356,10 @@ child.exec(
   }));
 
 // Regression test for https://github.com/nodejs/node/issues/45336
-child.exec(`${nodejs} -p '` +
-            'Object.defineProperty(global, "fs", { configurable: false });' +
-            'fs === require("node:fs")' +
-          '\'',
-           common.mustSucceed((stdout) => {
-             assert.match(stdout, /^true/);
-           }));
+child.execFile(process.execPath,
+               ['-p',
+                'Object.defineProperty(global, "fs", { configurable: false });' +
+                'fs === require("node:fs")'],
+               common.mustSucceed((stdout) => {
+                 assert.match(stdout, /^true/);
+               }));
