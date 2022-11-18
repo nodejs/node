24d17b616e59ad2a6d9f0ce78af7c8c148d99cd8
Marco Ippolito <marcoippolito54@gmail.com>
2022-11-18 11:48:33 +0100

    fix: throw ERR_HTTP_HEADERS_SENT error after multiple writeHead

--- test/parallel/test-http-write-head.js
+++ test/parallel/test-http-write-head.js
@@ -58,7 +58,7 @@ const s = http.createServer(common.mustCall((req, res) => {
   }, {
     code: 'ERR_HTTP_HEADERS_SENT',
     name: 'Error',
-    message: 'Cannot render headers after they are sent to the client'
+    message: 'Cannot writeHead headers after they are sent to the client'
   });
 
   res.end();
@@ -75,4 +75,26 @@ function runTest() {
     }));
     response.resume();
   }));
+}
+
+{
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, [ 'test', '1' ]);
+    assert.throws(() => res.writeHead(200, [ 'test2', '2' ]), {
+      code: 'ERR_HTTP_HEADERS_SENT',
+      name: 'Error',
+      message: 'Cannot writeHead headers after they are sent to the client'
+    });
+    res.end();
+  });
+
+  server.listen(0, () => {
+    http.get({ port: server.address().port }, (res) => {
+      assert.strictEqual(res.headers.test, '1');
+      assert.strictEqual('test2' in res.headers, false);
+      res.resume().on('end', common.mustCall(() => {
+        server.close();
+      }));
+    });
+  });
 }
