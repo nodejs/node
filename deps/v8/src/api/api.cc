ac746e0e2462bce633b2291a7c9fc7c4887ab0dd
Yagiz Nizipli <yagiz@nizipli.com>
2022-11-18 04:46:49 -0500

    deps: V8: cherry-pick 9df5ef70ff18
    Original commit message:
    
        Add an `v8::ArrayBuffer::WasDetached` method to the C++ API
    
        V8's C++ API does not give a way to tell whether an ArrayBuffer has
        been detached from the `v8::ArrayBuffer` class. In fact, as far as can
        be told from the C++ API without running JS code, detached
        ArrayBuffers behave the same as zero-sized ArrayBuffers and there is
        no way to observe the difference. However, this difference can be
        observed in JS because constructing a TypedArray from a detached
        ArrayBuffer will throw.
    
        This change adds a `WasDetached` method to the `v8::ArrayBuffer` class
        to give embedders access to this information without having to run JS
        code.
    
        Bug: v8:13159
        Change-Id: I2bb1e380cee1cecd31f6d48ec3d9f28c03a8a673
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3810345
        Commit-Queue: Toon Verwaest <verwaest@chromium.org>
        Reviewed-by: Toon Verwaest <verwaest@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#83963}
    
    Refs: https://github.com/v8/v8/commit/9df5ef70ff18977b157028fc55ced5af4bcee535
    PR-URL: https://github.com/nodejs/node/pull/45474
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Daeyeon Jeong <daeyeon.dev@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- deps/v8/src/api/api.cc
+++ deps/v8/src/api/api.cc
@@ -8064,6 +8064,10 @@ bool v8::ArrayBuffer::IsDetachable() const {
   return Utils::OpenHandle(this)->is_detachable();
 }
 
+bool v8::ArrayBuffer::WasDetached() const {
+  return Utils::OpenHandle(this)->was_detached();
+}
+
 namespace {
 std::shared_ptr<i::BackingStore> ToInternal(
     std::shared_ptr<i::BackingStoreBase> backing_store) {
