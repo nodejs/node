a1fceaa31b105feed481085808c176829ec37dd4
Filip Skokan <panva.ip@gmail.com>
2022-11-19 15:55:27 +0100

    crypto: clear OpenSSL error queue after calling X509_check_private_key()
    Fixes: https://github.com/nodejs/node/issues/45485
    PR-URL: https://github.com/nodejs/node/pull/45495
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- test/parallel/test-crypto-x509.js
+++ test/parallel/test-crypto-x509.js
@@ -8,6 +8,7 @@ if (!common.hasCrypto)
 const {
   X509Certificate,
   createPrivateKey,
+  generateKeyPairSync,
 } = require('crypto');
 
 const {
@@ -186,10 +187,18 @@ const der = Buffer.from(
     code: 'ERR_INVALID_ARG_VALUE'
   });
 
-  // Confirm failure of X509Certificate:verify() doesn't affect other functions that use OpenSSL.
-  assert(!x509.verify(x509.publicKey));
-  // This call should not throw.
-  createPrivateKey(key);
+  {
+    // https://github.com/nodejs/node/issues/45377
+    // https://github.com/nodejs/node/issues/45485
+    // Confirm failures of X509Certificate:verify() and X509Certificate:CheckPrivateKey()
+    // do not affect other functions that use OpenSSL.
+    // Subsequent calls to e.g. createPrivateKey should not throw.
+    const keyPair = generateKeyPairSync('ed25519');
+    assert(!x509.verify(keyPair.publicKey));
+    createPrivateKey(key);
+    assert(!x509.checkPrivateKey(keyPair.privateKey));
+    createPrivateKey(key);
+  }
 
   // X509Certificate can be cloned via MessageChannel/MessagePort
   const mc = new MessageChannel();
