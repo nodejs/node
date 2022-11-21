eac26c0793b431eb24f5c67e255f4616370017ab
Rich Trott <rtrott@gmail.com>
2022-11-21 14:36:29 -0800

    Revert "http: headers(Distinct), trailers(Distinct) setters to be no-op"
    This reverts commit 4d723c7fd6f8b2ecd0dbc1d6f0595645f6471928.
    
    I'm not sure if we should re-apply this as a semver-major change or if
    we should accept it as valid and add tests/documentation, but either
    way, we have to revert it at least temporarily.
    
    Closes: https://github.com/nodejs/node/issues/45510
    PR-URL: https://github.com/nodejs/node/pull/45527
    Fixes: https://github.com/nodejs/node/issues/45510
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>

--- test/parallel/test-set-incoming-message-header.js
+++ test/parallel/test-set-incoming-message-header.js
@@ -4,23 +4,24 @@ require('../common');
 const { IncomingMessage } = require('http');
 const assert = require('assert');
 
-// Headers setter should be a No-Op
+// Headers setter function set a header correctly
 {
   const im = new IncomingMessage();
   im.headers = { key: 'value' };
-  assert.deepStrictEqual(im.headers, {});
+  assert.deepStrictEqual(im.headers, { key: 'value' });
 }
 
-// Trailers setter should be a No-Op
+// Trailers setter function set a header correctly
 {
   const im = new IncomingMessage();
   im.trailers = { key: 'value' };
-  assert.deepStrictEqual(im.trailers, {});
+  assert.deepStrictEqual(im.trailers, { key: 'value' });
 }
 
 // _addHeaderLines function set a header correctly
 {
   const im = new IncomingMessage();
+  im.headers = { key1: 'value1' };
   im._addHeaderLines(['key2', 'value2'], 2);
-  assert.deepStrictEqual(im.headers, { key2: 'value2' });
+  assert.deepStrictEqual(im.headers, { key1: 'value1', key2: 'value2' });
 }
