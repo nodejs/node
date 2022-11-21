c82c552661fd958808efd35292626dfcccdff95a
Daeyeon Jeong <daeyeon.dev@gmail.com>
2022-11-21 11:14:28 +0900

    src: set an appropriate thread pool size if given `--v8-pool-size=0`
    It doesn't terminate when any pending V8 tasks exist if no thread
    is in the pool.
    
    This allocates one thread at least for V8's background tasks if
    `--v8-pool-size=0` is given as a CLI option.
    
    Signed-off-by: Daeyeon Jeong <daeyeon.dev@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/45513
    Fixes: https://github.com/nodejs/node/issues/42523
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

--- doc/api/cli.md
+++ doc/api/cli.md
@@ -1570,11 +1570,12 @@ added: v5.10.0
 
 Set V8's thread pool size which will be used to allocate background jobs.
 
-If set to `0` then V8 will choose an appropriate size of the thread pool based
-on the number of online processors.
+If set to `0` then Node.js will choose an appropriate size of the thread pool
+based on an estimate of the amount of parallelism.
 
-If the value provided is larger than V8's maximum, then the largest value
-will be chosen.
+The amount of parallelism refers to the number of computations that can be
+carried out simultaneously in a given machine. In general, it's the same as the
+amount of CPUs, but it may diverge in environments such as VMs or containers.
 
 ### `--watch`
 
--- src/node_platform.cc
+++ src/node_platform.cc
@@ -45,6 +45,13 @@ static void PlatformWorkerThread(void* data) {
   }
 }
 
+static int GetActualThreadPoolSize(int thread_pool_size) {
+  if (thread_pool_size < 1) {
+    thread_pool_size = uv_available_parallelism() - 1;
+  }
+  return std::max(thread_pool_size, 1);
+}
+
 }  // namespace
 
 class WorkerThreadsTaskRunner::DelayedTaskScheduler {
@@ -340,6 +347,8 @@ NodePlatform::NodePlatform(int thread_pool_size,
   // current v8::Platform instance.
   SetTracingController(tracing_controller_);
   DCHECK_EQ(GetTracingController(), tracing_controller_);
+
+  thread_pool_size = GetActualThreadPoolSize(thread_pool_size);
   worker_thread_task_runner_ =
       std::make_shared<WorkerThreadsTaskRunner>(thread_pool_size);
 }
