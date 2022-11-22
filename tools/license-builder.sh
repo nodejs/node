fb008a2e9b7d20455e91025635aa26fcb3eda0e3
Shi Lei <lei.a.shi@intel.com>
2022-11-22 16:28:19 +0800

    build,deps,src: fix Intel VTune profiling support
    PR-URL: https://github.com/nodejs/node/pull/45248
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- tools/license-builder.sh
+++ tools/license-builder.sh
@@ -36,6 +36,8 @@ licenseText="$(tail -n +3 "${rootdir}/deps/cares/LICENSE.md")"
 addlicense "c-ares" "deps/cares" "$licenseText"
 licenseText="$(cat "${rootdir}/deps/cjs-module-lexer/LICENSE")"
 addlicense "cjs-module-lexer" "deps/cjs-module-lexer" "$licenseText"
+licenseText="$(cat "${rootdir}/deps/v8/third_party/ittapi/LICENSES/BSD-3-Clause.txt")"
+addlicense "ittapi" "deps/v8/third_party/ittapi" "$licenseText"
 if [ -f "${rootdir}/deps/icu/LICENSE" ]; then
   # ICU 57 and following. Drop the BOM
   licenseText="$(sed -e '1s/^[^a-zA-Z ]*ICU/ICU/' -e :a -e 's/<[^>]*>//g;s/	/ /g;s/ +$//;/</N;//ba' "${rootdir}/deps/icu/LICENSE")"
--- tools/v8_gypfiles/features.gypi
+++ tools/v8_gypfiles/features.gypi
@@ -106,6 +106,9 @@
     # Sets -dENABLE_HUGEPAGE
     'v8_enable_hugepage%': 0,
 
+    # Sets -dENABLE_VTUNE_JIT_INTERFACE.
+    'v8_enable_vtunejit%': 0,
+
     # Currently set for node by common.gypi, avoiding default because of gyp file bug.
     # Should be turned on only for debugging.
     #'v8_enable_handle_zapping%': 0,
@@ -306,6 +309,9 @@
       }],
       ['v8_enable_hugepage==1', {
         'defines': ['ENABLE_HUGEPAGE',],
+      }],
+      ['v8_enable_vtunejit==1', {
+        'defines': ['ENABLE_VTUNE_JIT_INTERFACE',],
       }],
       ['v8_enable_pointer_compression==1', {
         'defines': [
