a6e55d5d17606aa00f100c636dba4492103a29bb
Richard Lau <rlau@redhat.com>
2022-11-22 10:15:39 -0500

    deps: V8: cherry-pick 5fe919f78321
    Original commit message:
    
        PPC: clear VXCVI before doing a conversion
    
        This bit may not get cleared automatically and could show
        results from older executed instructions.
    
        Change-Id: I5976f9a6c5bf87b1a63ef0f35493b222729e20f6
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3812037
        Reviewed-by: Junliang Yan <junyan@redhat.com>
        Commit-Queue: Milad Farazmand <mfarazma@redhat.com>
        Cr-Commit-Position: refs/heads/main@{#82237}
    
    Refs: https://github.com/v8/v8/commit/5fe919f783214c978cb174425554ede8fc1eac5f

--- deps/v8/src/wasm/baseline/ppc/liftoff-assembler-ppc.h
+++ deps/v8/src/wasm/baseline/ppc/liftoff-assembler-ppc.h
@@ -1455,6 +1455,7 @@ bool LiftoffAssembler::emit_type_conversion(WasmOpcode opcode,
       fcmpu(src.fp(), kScratchDoubleReg);
       bunordered(trap);
 
+      mtfsb0(VXCVI);  // clear FPSCR:VXCVI bit
       fctiwz(kScratchDoubleReg, src.fp());
       MovDoubleLowToInt(dst.gp(), kScratchDoubleReg);
       mcrfs(cr7, VXCVI);
@@ -1463,6 +1464,7 @@ bool LiftoffAssembler::emit_type_conversion(WasmOpcode opcode,
     }
     case kExprI32UConvertF64:
     case kExprI32UConvertF32: {
+      mtfsb0(VXCVI);  // clear FPSCR:VXCVI bit
       ConvertDoubleToUnsignedInt64(src.fp(), r0, kScratchDoubleReg,
                                    kRoundToZero);
       mcrfs(cr7, VXCVI);  // extract FPSCR field containing VXCVI into cr7
@@ -1478,6 +1480,7 @@ bool LiftoffAssembler::emit_type_conversion(WasmOpcode opcode,
       fcmpu(src.fp(), kScratchDoubleReg);
       bunordered(trap);
 
+      mtfsb0(VXCVI);  // clear FPSCR:VXCVI bit
       fctidz(kScratchDoubleReg, src.fp());
       MovDoubleToInt64(dst.gp(), kScratchDoubleReg);
       mcrfs(cr7, VXCVI);
@@ -1490,6 +1493,7 @@ bool LiftoffAssembler::emit_type_conversion(WasmOpcode opcode,
       fcmpu(src.fp(), kScratchDoubleReg);
       bunordered(trap);
 
+      mtfsb0(VXCVI);  // clear FPSCR:VXCVI bit
       fctiduz(kScratchDoubleReg, src.fp());
       MovDoubleToInt64(dst.gp(), kScratchDoubleReg);
       mcrfs(cr7, VXCVI);
