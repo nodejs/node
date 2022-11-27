6a9724fd15232ff13a76c87745f96bd4d73284e3
Debadree Chatterjee <debadree333@gmail.com>
2022-11-27 23:56:27 +0530

    test: fixed linting and comments issues

--- test/parallel/test-stream-toWeb-allows-server-response.js
+++ test/parallel/test-stream-toWeb-allows-server-response.js
@@ -2,19 +2,18 @@
 const common = require('../common');
 const { Writable } = require('stream');
 
-// test the change i made
 const assert = require('assert');
 const http = require('http');
 
-// check if writeable.toWeb successfully works on the response object after creating a server
+// Check if Writable.toWeb works on the response object after creating a server.
 const server = http.createServer((req, res) => {
-    const webStreamResponse = Writable.toWeb(res);
-    // check instance of webStreamResponse
-    assert.strictEqual(webStreamResponse instanceof Writable, true);
+  const webStreamResponse = Writable.toWeb(res);
+  assert.strictEqual(webStreamResponse instanceof Writable, true);
 });
 
-server.listen(0, common.mustCall(() => {
+server.listen(
+  0,
+  common.mustCall(() => {
     server.close();
-}));
-
-
+  })
+);
