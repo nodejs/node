95f4921c5483feccdbd2b1574ceae24c835dc568
Debadree Chatterjee <debadree333@gmail.com>
2022-12-02 19:03:34 +0530

    tools: add comments when rewriting the file

--- tools/update-acorn.sh
+++ tools/update-acorn.sh
@@ -27,10 +27,13 @@ rm -rf deps/acorn/acorn
     # get acorn version
     ACORN_VERSION=$("$NODE" -p "require('./package.json').version")
     # update this version information in src/acorn_version.h
-    echo "#ifndef SRC_ACORN_VERSION_H_" > "$ROOT/src/acorn_version.h"
-    echo "#define SRC_ACORN_VERSION_H_" >> "$ROOT/src/acorn_version.h"
-    echo "#define ACORN_VERSION \"$ACORN_VERSION\"" >> "$ROOT/src/acorn_version.h"
-    echo "#endif  // SRC_ACORN_VERSION_H_" >> "$ROOT/src/acorn_version.h"
+    FILE_PATH="$ROOT/src/acorn_version.h"
+    echo "// This is a auto generated file, please do not edit." > "$FILE_PATH"
+    echo "// Refer to tools/update-acorn.sh" >> "$FILE_PATH"
+    echo "#ifndef SRC_ACORN_VERSION_H_" >> "$FILE_PATH"
+    echo "#define SRC_ACORN_VERSION_H_" >> "$FILE_PATH"
+    echo "#define ACORN_VERSION \"$ACORN_VERSION\"" >> "$FILE_PATH"
+    echo "#endif  // SRC_ACORN_VERSION_H_" >> "$FILE_PATH"
 )
 
 mv acorn-tmp/node_modules/acorn deps/acorn
