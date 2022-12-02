e275998efdfa442bc82c723779f8a67689665b7b
Debadree Chatterjee <debadree333@gmail.com>
2022-12-02 19:02:41 +0530

    src: added files are automated comments

--- src/acorn_version.h
+++ src/acorn_version.h
@@ -1,3 +1,5 @@
+// This is a auto generated file, please do not edit.
+// Refer to tools/update-acorn.sh
 #ifndef SRC_ACORN_VERSION_H_
 #define SRC_ACORN_VERSION_H_
 #define ACORN_VERSION "8.8.1"
--- src/undici_version.h
+++ src/undici_version.h
@@ -1,3 +1,5 @@
+// This is a auto generated file, please do not edit.
+// Refer to tools/update-undici.sh
 #ifndef SRC_UNDICI_VERSION_H_
 #define SRC_UNDICI_VERSION_H_
 #define UNDICI_VERSION "5.12.0"
