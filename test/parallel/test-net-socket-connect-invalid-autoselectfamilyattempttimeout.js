017002d8b2a47603259f109baef9b696c1d4741b
Deokjin Kim <deokjin81.kim@gmail.com>
2022-12-05 23:34:31 +0900

    use hard-coded negative integer and zero for test

--- test/parallel/test-net-socket-connect-invalid-autoselectfamilyattempttimeout.js
+++ test/parallel/test-net-socket-connect-invalid-autoselectfamilyattempttimeout.js
@@ -1,14 +1,14 @@
 'use strict';
-const common = require('../common');
+require('../common');
 const assert = require('assert');
 const net = require('net');
 
-const autoSelectFamilyAttemptTimeout = common.platformTimeout(-10);
-
-assert.throws(() => {
-  net.connect({
-    port: 8080,
-    autoSelectFamily: true,
-    autoSelectFamilyAttemptTimeout,
-  });
-}, { code: 'ERR_OUT_OF_RANGE' });
+for (const autoSelectFamilyAttemptTimeout of [-10, 0]) {
+  assert.throws(() => {
+    net.connect({
+      port: 8080,
+      autoSelectFamily: true,
+      autoSelectFamilyAttemptTimeout,
+    });
+  }, { code: 'ERR_OUT_OF_RANGE' });
+}
