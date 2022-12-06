9f6aff3c42c95bb655bfddca37fafd9fe282de92
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-12-07 00:16:59 +0100

    test: fix File API tests for v16.x

--- test/wpt/test-file.js
+++ test/wpt/test-file.js
@@ -6,7 +6,9 @@ const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('FileAPI/file');
 
 runner.setInitScript(`
-  const { File } = require('buffer');
+  const { File, Blob } = require('buffer');
+  // Global Blob is needed for some tests:
+  globalThis.Blob = Blob;
   globalThis.File = File;
 `);
 
