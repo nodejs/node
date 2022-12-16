992fba4b49543e6a4570586adbd1923b15c30c6d
Filip Skokan <panva.ip@gmail.com>
2022-12-16 20:55:30 +0100

    test: add CryptoKey transferring tests
    PR-URL: https://github.com/nodejs/node/pull/45811
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-crypto-worker-thread.js
+++ test/parallel/test-crypto-worker-thread.js
@@ -5,13 +5,30 @@ if (!common.hasCrypto)
 
 // Issue https://github.com/nodejs/node/issues/35263
 // Description: Test that passing keyobject to worker thread does not crash.
-const { createSecretKey } = require('crypto');
+const {
+  generateKeySync,
+  generateKeyPairSync,
+} = require('crypto');
+const { subtle } = globalThis.crypto;
+
+const assert = require('assert');
 
 const { Worker, isMainThread, workerData } = require('worker_threads');
 
 if (isMainThread) {
-  const key = createSecretKey(Buffer.from('hello'));
-  new Worker(__filename, { workerData: key });
+  (async () => {
+    const secretKey = generateKeySync('aes', { length: 128 });
+    const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+      modulusLength: 1024
+    });
+    const cryptoKey = await subtle.generateKey(
+      { name: 'AES-CBC', length: 128 }, false, ['encrypt']);
+
+    for (const key of [secretKey, publicKey, privateKey, cryptoKey]) {
+      new Worker(__filename, { workerData: key });
+    }
+  })().then(common.mustCall());
 } else {
   console.log(workerData);
+  assert.notDeepStrictEqual(workerData, {});
 }
