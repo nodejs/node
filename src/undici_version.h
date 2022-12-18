7b8b5fcd2bf72636086dbfbad207c7e5192b9966
Debadree Chatterjee <debadree333@gmail.com>
2022-12-18 21:19:08 +0530

    src: add undici and acorn to `process.versions`
    Fixes: https://github.com/nodejs/node/issues/45260
    Refs: https://github.com/nodejs/node/pull/45599
    Refs: https://github.com/nodejs/node/issues/45260
    PR-URL: https://github.com/nodejs/node/pull/45621
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>

---
+++ src/undici_version.h
@@ -0,0 +1,6 @@
+// This is an auto generated file, please do not edit.
+// Refer to tools/update-undici.sh
+#ifndef SRC_UNDICI_VERSION_H_
+#define SRC_UNDICI_VERSION_H_
+#define UNDICI_VERSION "5.14.0"
+#endif  // SRC_UNDICI_VERSION_H_
