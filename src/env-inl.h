03f6bb4fbeb8359fbb259e22a78920c846527cfa
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2022-12-19 21:16:22 +0100

    esm: move hook execution to separate thread
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Co-authored-by: Geoffrey Booth <webadmin@geoffreybooth.com>
    Co-authored-by: Michaël Zasso <targos@protonmail.com>

--- src/env-inl.h
+++ src/env-inl.h
@@ -636,8 +636,7 @@ inline bool Environment::owns_inspector() const {
 }
 
 inline bool Environment::should_create_inspector() const {
-  return (flags_ & EnvironmentFlags::kNoCreateInspector) == 0 &&
-         !options_->test_runner && !options_->watch_mode;
+  return (flags_ & EnvironmentFlags::kNoCreateInspector) == 0; // ! FIXME: do not merge
 }
 
 inline bool Environment::tracks_unmanaged_fds() const {
