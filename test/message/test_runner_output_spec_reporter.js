600e7c19f7500ad7a8193e0ebf404899b9a4160a
Moshe Atlow <moshe@atlow.co.il>
2022-12-19 10:18:16 +0200

    strip non ascii chars

--- test/message/test_runner_output_spec_reporter.js
+++ test/message/test_runner_output_spec_reporter.js
@@ -2,5 +2,9 @@
 'use strict';
 require('../common');
 const spawn = require('node:child_process').spawn;
-spawn(process.execPath,
-      ['--no-warnings', '--test-reporter', 'spec', 'test/message/test_runner_output.js'], { stdio: 'inherit' });
+const child = spawn(process.execPath,
+                    ['--no-warnings', '--test-reporter', 'spec', 'test/message/test_runner_output.js'],
+                    { stdio: 'pipe' });
+// eslint-disable-next-line no-control-regex
+child.stdout.on('data', (d) => process.stdout.write(d.toString().replace(/[^\x00-\x7F]/g, '').replace(/\u001b\[\d+m/g, '')));
+child.stderr.pipe(process.stderr);
