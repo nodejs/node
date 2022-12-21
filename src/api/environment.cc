01323d50c4b24cf730a651d06ba20633905ecbed
Anna Henningsen <anna.henningsen@mongodb.com>
2022-12-21 17:21:48 +0100

    src: use CreateEnvironment instead of inlining its code where possible
    We had a number of places in which we created an `Environment` instance
    by performing each step in `CreateEnvironment` manually. Instead,
    just call the function itself.
    
    PR-URL: https://github.com/nodejs/node/pull/45886
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

--- src/api/environment.cc
+++ src/api/environment.cc
@@ -375,12 +375,13 @@ Environment* CreateEnvironment(
   // options than the global parse call.
   Environment* env = new Environment(
       isolate_data, context, args, exec_args, nullptr, flags, thread_id);
+
 #if HAVE_INSPECTOR
   if (env->should_create_inspector()) {
     if (inspector_parent_handle) {
-      env->InitializeInspector(
-          std::move(static_cast<InspectorParentHandleImpl*>(
-              inspector_parent_handle.get())->impl));
+      env->InitializeInspector(std::move(
+          static_cast<InspectorParentHandleImpl*>(inspector_parent_handle.get())
+              ->impl));
     } else {
       env->InitializeInspector({});
     }
