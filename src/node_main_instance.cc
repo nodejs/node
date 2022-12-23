73d6fbfd088645e3c03c3942c169f2b5598944af
Anna Henningsen <anna.henningsen@mongodb.com>
2022-12-23 18:06:05 +0100

    src: fix creating `Isolate`s from addons
    daae938f32f2660 broke addons which create their own `Isolate`
    instances, because enabling the shared-readonly-heap feature
    of V8 requires all snapshots used for different `Isolate`s to
    be identical. Usage of addons that do this has probably
    decreased quite a bit since Worker threads were introduced
    in Node.js, but it’s still a valid use case, and in any case
    the breakage was probably not intentional (although the referenced
    commit did require test changes because of this issue).
    
    This commit addresses this issue partially by caching the
    V8 snapshot parameters and ignoring ones passed in from users
    in `NewIsolate()` when this feature is enabled, and makes
    the `NodeMainInstance` snapshot-based isolate creation
    also re-use this code.
    
    PR-URL: https://github.com/nodejs/node/pull/45885
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_main_instance.cc
+++ src/node_main_instance.cc
@@ -76,13 +76,9 @@ NodeMainInstance::NodeMainInstance(const SnapshotData* snapshot_data,
                                              isolate_params_.get());
   }
 
-  isolate_ = Isolate::Allocate();
+  isolate_ = NewIsolate(
+      isolate_params_.get(), event_loop, platform, snapshot_data != nullptr);
   CHECK_NOT_NULL(isolate_);
-  // Register the isolate on the platform before the isolate gets initialized,
-  // so that the isolate can access the platform during initialization.
-  platform->RegisterIsolate(isolate_, event_loop);
-  SetIsolateCreateParamsForNode(isolate_params_.get());
-  Isolate::Initialize(isolate_, *isolate_params_);
 
   // If the indexes are not nullptr, we are not deserializing
   isolate_data_ = std::make_unique<IsolateData>(
@@ -91,13 +87,7 @@ NodeMainInstance::NodeMainInstance(const SnapshotData* snapshot_data,
       platform,
       array_buffer_allocator_.get(),
       snapshot_data == nullptr ? nullptr : &(snapshot_data->isolate_data_info));
-  IsolateSettings s;
-  SetIsolateMiscHandlers(isolate_, s);
-  if (snapshot_data == nullptr) {
-    // If in deserialize mode, delay until after the deserialization is
-    // complete.
-    SetIsolateErrorHandlers(isolate_, s);
-  }
+
   isolate_data_->max_young_gen_size =
       isolate_params_->constraints.max_young_generation_size_in_bytes();
 }
