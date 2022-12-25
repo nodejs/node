907068c817246eb36ae6db7a3987d5f72f827ddf
Yagiz Nizipli <yagiz@nizipli.com>
2022-12-24 21:32:05 -0500

    buffer: add buffer.isUtf8 for utf8 validation
    PR-URL: https://github.com/nodejs/node/pull/45947
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/node_errors.h
+++ src/node_errors.h
@@ -68,6 +68,7 @@ void OOMErrorHandler(const char* location, bool is_heap_oom);
   V(ERR_INVALID_ARG_TYPE, TypeError)                                           \
   V(ERR_INVALID_OBJECT_DEFINE_PROPERTY, TypeError)                             \
   V(ERR_INVALID_MODULE, Error)                                                 \
+  V(ERR_INVALID_STATE, Error)                                                  \
   V(ERR_INVALID_THIS, TypeError)                                               \
   V(ERR_INVALID_TRANSFER_OBJECT, TypeError)                                    \
   V(ERR_MEMORY_ALLOCATION_FAILED, Error)                                       \
