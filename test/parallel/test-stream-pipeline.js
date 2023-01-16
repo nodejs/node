ef7b83f4a4953ef187c88f8a1c68321f70a36ab1
Debadree Chatterjee <debadree333@gmail.com>
2023-01-17 00:46:48 +0530

    stream: fix pipeline calling end on destination more than once
    Fixes: https://github.com/nodejs/node/issues/42866

--- test/parallel/test-stream-pipeline.js
+++ test/parallel/test-stream-pipeline.js
@@ -1556,3 +1556,38 @@ const tsp = require('timers/promises');
     })
   );
 }
+
+{
+  class CustomReadable extends Readable {
+    _read() {
+      this.push('asd');
+      this.push(null);
+    }
+  }
+
+  class CustomWritable extends Writable {
+    constructor() {
+      super();
+      this.endCount = 0;
+      this.str = '';
+    }
+
+    _write(chunk, enc, cb) {
+      this.str += chunk;
+      cb();
+    }
+
+    end() {
+      this.endCount += 1;
+      super.end();
+    }
+  }
+
+  const readable = new CustomReadable();
+  const writable = new CustomWritable();
+
+  pipeline(readable, writable, common.mustSucceed(() => {
+    assert.strictEqual(writable.str, 'asd');
+    assert.strictEqual(writable.endCount, 1);
+  }));
+}
