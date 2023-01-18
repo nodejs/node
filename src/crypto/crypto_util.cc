86fa1f109ddf5b2340ede540850da571e07e76e8
Richard Lau <rlau@redhat.com>
2023-01-18 07:57:17 -0500

    fixup! crypto: avoid hang when no algorithm available

--- src/crypto/crypto_util.cc
+++ src/crypto/crypto_util.cc
@@ -70,9 +70,13 @@ MUST_USE_RESULT CSPRNGResult CSPRNG(void* buffer, size_t length) {
     // A misconfigured OpenSSL 3 installation may report 1 from RAND_poll()
     // and RAND_status() but fail in RAND_bytes() if it cannot look up
     // a matching algorithm for the CSPRNG.
-    if (ERR_GET_LIB(code) == ERR_LIB_RAND &&
-        ERR_GET_REASON(code) == RAND_R_UNABLE_TO_FETCH_DRBG) {
-      return {false};
+    if (ERR_GET_LIB(code) == ERR_LIB_RAND) {
+      const auto reason = ERR_GET_REASON(code);
+      if (reason == RAND_R_ERROR_INSTANTIATING_DRBG ||
+          reason == RAND_R_UNABLE_TO_FETCH_DRBG ||
+          reason == RAND_R_UNABLE_TO_CREATE_DRBG) {
+        return {false};
+      }
     }
 #endif
   } while (1 == RAND_poll());
