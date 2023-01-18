1b1583e3afd7c69d4eeb136bd5fbb8cbaf263b9b
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-01-18 17:12:33 +0100

    rename `ModuleLoader` -> `createModuleLoader`

--- test/es-module/test-esm-loader-modulemap.js
+++ test/es-module/test-esm-loader-modulemap.js
@@ -4,7 +4,7 @@
 require('../common');
 
 const { strictEqual, throws } = require('assert');
-const { ModuleLoader } = require('internal/modules/esm/loader');
+const { createModuleLoader } = require('internal/modules/esm/loader');
 const ModuleMap = require('internal/modules/esm/module_map');
 const ModuleJob = require('internal/modules/esm/module_job');
 const createDynamicModule = require(
@@ -16,7 +16,7 @@ const jsonModuleDataUrl = 'data:application/json,""';
 const stubJsModule = createDynamicModule([], ['default'], jsModuleDataUrl);
 const stubJsonModule = createDynamicModule([], ['default'], jsonModuleDataUrl);
 
-const loader = new ModuleLoader(false);
+const loader = createModuleLoader(false);
 const jsModuleJob = new ModuleJob(loader, stubJsModule.module, undefined,
                                   () => new Promise(() => {}));
 const jsonModuleJob = new ModuleJob(loader, stubJsonModule.module,
