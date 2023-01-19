88e94cfb8fc399f56ebb6222a1014dc40b4e7ff0
Richard Lau <rlau@redhat.com>
2023-01-19 17:31:08 +0000

    crypto: avoid hang when no algorithm available
    Avoid an endless loop if no algorithm is available to seed the
    cryptographically secure pseudorandom number generator (CSPRNG).
    
    Co-authored-by: Anna Henningsen <anna@addaleax.net>
    PR-URL: https://github.com/nodejs/node/pull/46237
    Fixes: https://github.com/nodejs/node/issues/46200
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/crypto/crypto_util.cc
+++ src/crypto/crypto_util.cc
@@ -65,6 +65,20 @@ MUST_USE_RESULT CSPRNGResult CSPRNG(void* buffer, size_t length) {
     if (1 == RAND_status())
       if (1 == RAND_bytes(static_cast<unsigned char*>(buffer), length))
         return {true};
+#if OPENSSL_VERSION_MAJOR >= 3
+    const auto code = ERR_peek_last_error();
+    // A misconfigured OpenSSL 3 installation may report 1 from RAND_poll()
+    // and RAND_status() but fail in RAND_bytes() if it cannot look up
+    // a matching algorithm for the CSPRNG.
+    if (ERR_GET_LIB(code) == ERR_LIB_RAND) {
+      const auto reason = ERR_GET_REASON(code);
+      if (reason == RAND_R_ERROR_INSTANTIATING_DRBG ||
+          reason == RAND_R_UNABLE_TO_FETCH_DRBG ||
+          reason == RAND_R_UNABLE_TO_CREATE_DRBG) {
+        return {false};
+      }
+    }
+#endif
   } while (1 == RAND_poll());
 
   return {false};
