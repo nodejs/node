2e0f04787dbe64d853d9bd6fb5f4911ced525f26
Michaël Zasso <targos@protonmail.com>
2023-01-19 01:14:07 +0100

    test: refactor to avoid mutation of global by a loader
    This makes the test compatible with off-thread loaders.
    
    Co-Authored-By: Geoffrey Booth <webadmin@geoffreybooth.com>
    PR-URL: https://github.com/nodejs/node/pull/46220
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>

--- test/fixtures/es-module-loaders/hook-resolve-type.mjs
+++ test/fixtures/es-module-loaders/hook-resolve-type.mjs
@@ -1,6 +1,29 @@
 let importedESM = 0;
 let importedCJS = 0;
-global.getModuleTypeStats = () => { return {importedESM, importedCJS} };
+
+export function globalPreload({ port }) {
+  port.on('message', (int32) => {
+    port.postMessage({ importedESM, importedCJS });
+    Atomics.store(int32, 0, 1);
+    Atomics.notify(int32, 0);
+  });
+  port.unref();
+  return `
+    const { receiveMessageOnPort } = getBuiltin('worker_threads');
+    global.getModuleTypeStats = async function getModuleTypeStats() {
+      const sab = new SharedArrayBuffer(4);
+      const int32 = new Int32Array(sab);
+      port.postMessage(int32);
+      // Artificial timeout to keep the event loop alive.
+      // https://bugs.chromium.org/p/v8/issues/detail?id=13238
+      // TODO(targos) Remove when V8 issue is resolved.
+      const timeout = setTimeout(() => { throw new Error('timeout'); }, 1_000);
+      await Atomics.waitAsync(int32, 0, 0).value;
+      clearTimeout(timeout);
+      return receiveMessageOnPort(port).message;
+    };
+  `;
+}
 
 export async function load(url, context, next) {
   return next(url);
