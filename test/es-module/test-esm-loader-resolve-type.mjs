f368587f014fdd180e884f243dd5b272204dc4f7
Michaël Zasso <targos@protonmail.com>
2023-01-19 01:14:07 +0100

    test: refactor to avoid mutation of global by a loader
    This makes the test compatible with off-thread loaders.
    
    Co-Authored-By: Geoffrey Booth <webadmin@geoffreybooth.com>
    PR-URL: https://github.com/nodejs/node/pull/46220
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>

--- test/es-module/test-esm-loader-resolve-type.mjs
+++ test/es-module/test-esm-loader-resolve-type.mjs
@@ -6,6 +6,9 @@ import * as fs from 'fs';
 
 allowGlobals(global.getModuleTypeStats);
 
+const { importedESM: importedESMBefore,
+        importedCJS: importedCJSBefore } = await global.getModuleTypeStats();
+
 const basePath =
   new URL('./node_modules/', import.meta.url);
 
@@ -17,25 +20,24 @@ const createDir = (path) => {
 };
 
 const moduleName = 'module-counter-by-type';
-
 const moduleDir = rel(`${moduleName}`);
-createDir(basePath);
-createDir(moduleDir);
-fs.cpSync(
-  fixtures.path('es-modules', moduleName),
-  moduleDir,
-  { recursive: true }
-);
+try {
+  createDir(basePath);
+  createDir(moduleDir);
+  fs.cpSync(
+    fixtures.path('es-modules', moduleName),
+    moduleDir,
+    { recursive: true }
+  );
 
-const { importedESM: importedESMBefore,
-        importedCJS: importedCJSBefore } = global.getModuleTypeStats();
 
-await import(`${moduleName}`).finally(() => {
+  await import(`${moduleName}`);
+} finally {
   fs.rmSync(basePath, { recursive: true, force: true });
-});
+}
 
 const { importedESM: importedESMAfter,
-        importedCJS: importedCJSAfter } = global.getModuleTypeStats();
+        importedCJS: importedCJSAfter } = await global.getModuleTypeStats();
 
 // Dynamic import above should increment ESM counter but not CJS counter
 assert.strictEqual(importedESMBefore + 1, importedESMAfter);
