594a03887600240e6fc05418401ebf9399405510
Michael Dawson <mdawson@devrus.com>
2023-01-21 15:05:17 -0500

    tools: allow icutrim.py to run on python2
    Refs: https://github.com/nodejs/build/issues/2998
    
    Small icu seems broken from 14.x since it uses
    python2. Although main no longer supports python2
    landing and backporting this change to the 14.x line would
    allow us to simplify future backports as currently
    the files are the same across lines.
    
    Signed-off-by: Michael Dawson <mdawson@devrus.com>
    PR-URL: https://github.com/nodejs/node/pull/46263
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- tools/icu/icutrim.py
+++ tools/icu/icutrim.py
@@ -316,7 +316,7 @@ def removeList(count=0):
             erritems = fi.readlines()
             fi.close()
             #Item zone/zh_Hant_TW.res depends on missing item zone/zh_Hant.res
-            pat = re.compile(bytes(r"^Item ([^ ]+) depends on missing item ([^ ]+).*", 'utf-8'))
+            pat = re.compile(br"^Item ([^ ]+) depends on missing item ([^ ]+).*")
             for i in range(len(erritems)):
                 line = erritems[i].strip()
                 m = pat.match(line)
