cbcaf7725d0a36c4c6472ade05ccec6d8cd70ca5
Debadree Chatterjee <debadree333@gmail.com>
2023-01-22 00:58:21 +0530

    fixup! add more tests

--- test/parallel/test-webstreams-abort-controller.js
+++ test/parallel/test-webstreams-abort-controller.js
@@ -5,13 +5,27 @@ const { finished, addAbortSignal } = require('stream');
 const { ReadableStream, WritableStream } = require('stream/web');
 const assert = require('assert');
 
-{
-  const rs = new ReadableStream({
+function createTestReadableStream() {
+  return new ReadableStream({
     start(controller) {
-      controller.enqueue('hello');
+      controller.enqueue('a');
+      controller.enqueue('b');
+      controller.enqueue('c');
       controller.close();
     }
   });
+}
+
+function createTestWritableStream(values) {
+  return new WritableStream({
+    write(chunk) {
+      values.push(chunk);
+    }
+  });
+}
+
+{
+  const rs = createTestReadableStream();
 
   const reader = rs.getReader();
 
@@ -21,22 +35,18 @@ const assert = require('assert');
 
   finished(rs, common.mustCall((err) => {
     assert.strictEqual(err.name, 'AbortError');
+    assert.rejects(reader.read(), /AbortError/).then(common.mustCall());
+    assert.rejects(reader.closed, /AbortError/).then(common.mustCall());
   }));
 
-  ac.abort();
-
-  assert.rejects(reader.read(), 'AbortError: The operation was aborted.').then(common.mustCall());
+  reader.read().then(common.mustCall((result) => {
+    assert.strictEqual(result.value, 'a');
+    ac.abort();
+  }));
 }
 
 {
-  const rs = new ReadableStream({
-    start(controller) {
-      controller.enqueue('a');
-      controller.enqueue('b');
-      controller.enqueue('c');
-      controller.close();
-    }
-  });
+  const rs = createTestReadableStream();
 
   const ac = new AbortController();
 
@@ -52,17 +62,63 @@ const assert = require('assert');
 }
 
 {
-  const values = [];
-  const ws = new WritableStream({
-    write(chunk) {
-      values.push(chunk);
-    }
-  });
+  const rs1 = createTestReadableStream();
 
-  finished(ws, common.mustCall((err) => {
+  const rs2 = createTestReadableStream();
+
+  const ac = new AbortController();
+
+  addAbortSignal(ac.signal, rs1);
+  addAbortSignal(ac.signal, rs2);
+
+  const reader1 = rs1.getReader();
+  const reader2 = rs2.getReader();
+
+  finished(rs1, common.mustCall((err) => {
     assert.strictEqual(err.name, 'AbortError');
-    assert.deepStrictEqual(values, ['a']);
+    assert.rejects(reader1.read(), /AbortError/).then(common.mustCall());
+    assert.rejects(reader1.closed, /AbortError/).then(common.mustCall());
   }));
+
+  finished(rs2, common.mustCall((err) => {
+    assert.strictEqual(err.name, 'AbortError');
+    assert.rejects(reader2.read(), /AbortError/).then(common.mustCall());
+    assert.rejects(reader2.closed, /AbortError/).then(common.mustCall());
+  }));
+
+  ac.abort();
+}
+
+{
+  const rs = createTestReadableStream();
+
+  const { 0: rs1, 1: rs2 } = rs.tee();
+
+  const ac = new AbortController();
+
+  addAbortSignal(ac.signal, rs);
+
+  const reader1 = rs1.getReader();
+  const reader2 = rs2.getReader();
+
+  finished(rs1, common.mustCall((err) => {
+    assert.strictEqual(err.name, 'AbortError');
+    assert.rejects(reader1.read(), /AbortError/).then(common.mustCall());
+    assert.rejects(reader1.closed, /AbortError/).then(common.mustCall());
+  }));
+
+  finished(rs2, common.mustCall((err) => {
+    assert.strictEqual(err.name, 'AbortError');
+    assert.rejects(reader2.read(), /AbortError/).then(common.mustCall());
+    assert.rejects(reader2.closed, /AbortError/).then(common.mustCall());
+  }));
+
+  ac.abort();
+}
+
+{
+  const values = [];
+  const ws = createTestWritableStream(values);
 
   const ac = new AbortController();
 
@@ -70,7 +126,43 @@ const assert = require('assert');
 
   const writer = ws.getWriter();
 
+  finished(ws, common.mustCall((err) => {
+    assert.strictEqual(err.name, 'AbortError');
+    assert.deepStrictEqual(values, ['a']);
+    assert.rejects(writer.write('b'), /AbortError/).then(common.mustCall());
+    assert.rejects(writer.closed, /AbortError/).then(common.mustCall());
+  }));
+
   writer.write('a').then(() => {
     ac.abort();
   });
+}
+
+{
+  const values = [];
+
+  const ws1 = createTestWritableStream(values);
+  const ws2 = createTestWritableStream(values);
+
+  const ac = new AbortController();
+
+  addAbortSignal(ac.signal, ws1);
+  addAbortSignal(ac.signal, ws2);
+
+  const writer1 = ws1.getWriter();
+  const writer2 = ws2.getWriter();
+
+  finished(ws1, common.mustCall((err) => {
+    assert.strictEqual(err.name, 'AbortError');
+    assert.rejects(writer1.write('a'), /AbortError/).then(common.mustCall());
+    assert.rejects(writer1.closed, /AbortError/).then(common.mustCall());
+  }));
+
+  finished(ws2, common.mustCall((err) => {
+    assert.strictEqual(err.name, 'AbortError');
+    assert.rejects(writer2.write('a'), /AbortError/).then(common.mustCall());
+    assert.rejects(writer2.closed, /AbortError/).then(common.mustCall());
+  }));
+
+  ac.abort();
 }
