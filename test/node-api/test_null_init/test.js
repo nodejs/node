2a942d445826eef8a0c2b8f2e8666431190c55ee
Vladimir Morozov <vmoroz@users.noreply.github.com>
2023-01-23 07:15:34 -0800

    node-api: deprecate napi_module_register

--- test/node-api/test_null_init/test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const common = require('../../common');
-const assert = require('assert');
-
-assert.throws(
-  () => require(`./build/${common.buildType}/test_null_init`),
-  /Module has no declared entry point[.]/);
