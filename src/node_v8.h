a7aacd795eb693bad03e3a6683b0b61ba5c3083d
theanarkh <theratliter@gmail.com>
2023-01-25 20:02:09 +0800

    v8: support gc profile
    PR-URL: https://github.com/nodejs/node/pull/46255
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Franziska Hinkelmann <franziska.hinkelmann@gmail.com>

--- src/node_v8.h
+++ src/node_v8.h
@@ -3,8 +3,10 @@
 
 #if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
 
+#include <sstream>
 #include "aliased_buffer.h"
 #include "base_object.h"
+#include "json_utils.h"
 #include "node_snapshotable.h"
 #include "util.h"
 #include "v8.h"
@@ -32,6 +34,32 @@ class BindingData : public SnapshotableObject {
   void MemoryInfo(MemoryTracker* tracker) const override;
   SET_SELF_SIZE(BindingData)
   SET_MEMORY_INFO_NAME(BindingData)
+};
+
+class GCProfiler : public BaseObject {
+ public:
+  enum class GCProfilerState { kInitialized, kStarted, kStopped };
+  GCProfiler(Environment* env, v8::Local<v8::Object> object);
+  inline ~GCProfiler() override;
+  static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void Start(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void Stop(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+  JSONWriter* writer();
+
+  std::ostringstream* out_stream();
+
+  SET_NO_MEMORY_INFO()
+  SET_MEMORY_INFO_NAME(GCProfiler)
+  SET_SELF_SIZE(GCProfiler)
+
+  uint64_t start_time;
+  uint8_t current_gc_type;
+  GCProfilerState state;
+
+ private:
+  std::ostringstream out_stream_;
+  JSONWriter writer_;
 };
 
 }  // namespace v8_utils
