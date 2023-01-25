0cdb46831710cf0a9985fdcde8c2518d65221f99
Debadree Chatterjee <debadree333@gmail.com>
2023-01-25 16:21:01 +0530

    fixup! isFile benchmark

---
+++ benchmark/fs/bench-isFile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const common = require('../common');
+const fs = require('fs');
+
+const bench = common.createBenchmark(main, {
+  n: [1e6],
+  fileName: [__filename, './not-exist.txt'],
+});
+
+function main({ n, fileName }) {
+  bench.start();
+  for (let i = 0; i < n; i++) {
+    fs.isFile(fileName);
+  }
+  bench.end(n);
+}
