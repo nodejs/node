1b8cc04eb0fe0fcf08a8e822569cdd27131cd291
Khafra <42794878+KhafraDev@users.noreply.github.com>
2023-01-30 21:48:51 -0500

    stream: make readable byte stream cloneable

---
+++ test/parallel/test-webstream-byte-readable-clone.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('../common');
+const assert = require('assert');
+
+{
+  // https://github.com/nodejs/node/issues/46296
+  const stream = new ReadableStream({
+    start(controller) {
+      controller.close();
+    },
+    type: 'bytes',
+  });
+
+  const clone = structuredClone(stream);
+
+  assert(clone instanceof ReadableStream);
+}
