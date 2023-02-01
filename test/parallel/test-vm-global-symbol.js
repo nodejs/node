nodejs/node

commit 2dbf169c9c0f3f1cd6f7abf69a678bb7b1b8019a

src: always signal V8 for intercepted properties
Closes: https://github.com/nodejs/node/issues/42962

PR-URL: https://github.com/nodejs/node/pull/42963
Fixes: https://github.com/nodejs/node/issues/42962
Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

src/node_contextify.cc
@@ -449,16 +449,8 @@ void ContextifyContext::PropertySetterCallback(
       !is_function)
     return;
 
-  if (!is_declared_on_global_proxy && is_declared_on_sandbox  &&
-      args.ShouldThrowOnError() && is_contextual_store && !is_function) {
-    // The property exists on the sandbox but not on the global
-    // proxy. Setting it would throw because we are in strict mode.
-    // Don't attempt to set it by signaling that the call was
-    // intercepted. Only change the value on the sandbox.
-    args.GetReturnValue().Set(false);
-  }
-
   USE(ctx->sandbox()->Set(context, property, value));
+  args.GetReturnValue().Set(value);
 }
 
 // static

test/parallel/test-vm-global-setter.js
@@ -1,0 +1,32 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const vm = require('vm');
+
+const window = createWindow();
+
+const descriptor =
+  Object.getOwnPropertyDescriptor(window.globalProxy, 'onhashchange');
+
+assert.strictEqual(typeof descriptor.get, 'function');
+assert.strictEqual(typeof descriptor.set, 'function');
+assert.strictEqual(descriptor.configurable, true);
+
+// Regression test for GH-42962. This assignment should not throw.
+window.globalProxy.onhashchange = () => {};
+
+assert.strictEqual(window.globalProxy.onhashchange, 42);
+
+function createWindow() {
+  const obj = {};
+  vm.createContext(obj);
+  Object.defineProperty(obj, 'onhashchange', {
+    get: common.mustCall(() => 42),
+    set: common.mustCall(),
+    configurable: true
+  });
+
+  obj.globalProxy = vm.runInContext('this', obj);
+
+  return obj;
+}

commit 05992367a0d5be3f08d077753b9bb724c18d3008

vm: properly support symbols on globals
A regression has been introduced in node 18.2.0,
it makes the following snippet fails while it used to work in the past:

```
const assert = require('assert');
const vm = require('vm');
const global = vm.runInContext('this', vm.createContext());
const totoSymbol = Symbol.for('toto');
Object.defineProperty(global, totoSymbol, {
  enumerable: true,
  writable: true,
  value: 4,
  configurable: true,
});
assert(Object.getOwnPropertySymbols(global).includes(totoSymbol));
```

Regression introduced by: https://github.com/nodejs/node/pull/42963.
So I basically attempted to start understanding what it changed to make
it fix the initial issue while not breaking the symbol related one.

Fixes: https://github.com/nodejs/node/issues/45983

src/node_contextify.cc
@@ -530,7 +530,9 @@ void ContextifyContext::PropertySetterCallback(
     return;
 
   USE(ctx->sandbox()->Set(context, property, value));
-  args.GetReturnValue().Set(value);
+  if (is_function) {
+    args.GetReturnValue().Set(value);
+  }
 }
 
 // static

test/parallel/test-vm-global-symbol.js
@@ -1,0 +1,15 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+const vm = require('vm');
+
+const global = vm.runInContext('this', vm.createContext());
+const totoSymbol = Symbol.for('toto');
+Object.defineProperty(global, totoSymbol, {
+  enumerable: true,
+  writable: true,
+  value: 4,
+  configurable: true,
+});
+assert.ok(global[totoSymbol] === 4);
+assert.ok(Object.getOwnPropertySymbols(global).includes(totoSymbol));