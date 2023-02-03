88eaafd3581ffb2ae46b62a17a0def1a6de3c47a
Steve Herzog <srherzog@gmail.com>
2023-02-03 10:08:57 -0600

    minor test refactor

--- test/parallel/test-http-early-hints-invalid-argument.js
+++ test/parallel/test-http-early-hints-invalid-argument.js
@@ -6,28 +6,52 @@ const debug = require('node:util').debuglog('test');
 
 const testResBody = 'response content\n';
 
-const server = http.createServer(common.mustCall((req, res) => {
-  debug('Server sending early hints...');
-  res.writeEarlyHints('bad argument type');
+{
+  const server = http.createServer(common.mustCall((req, res) => {
+    debug('Server sending early hints...');
+    assert.throws(() => {
+      res.writeEarlyHints('bad argument type');
+    }, (err) => err.code === 'ERR_INVALID_ARG_TYPE');
 
-  debug('Server sending full response...');
-  res.end(testResBody);
-}));
+    debug('Server sending full response...');
+    res.end(testResBody);
+    server.close();
+  }));
 
-server.listen(0, common.mustCall(() => {
-  const req = http.request({
-    port: server.address().port, path: '/'
-  });
+  server.listen(0, common.mustCall(() => {
+    const req = http.request({
+      port: server.address().port, path: '/'
+    });
 
-  req.end();
-  debug('Client sending request...');
+    req.end();
+    debug('Client sending request...');
 
-  req.on('information', common.mustNotCall());
+    req.on('information', common.mustNotCall());
+  }));
+}
 
-  process.on('uncaughtException', (err) => {
-    debug(`Caught an exception: ${JSON.stringify(err)}`);
-    if (err.name === 'AssertionError') throw err;
-    assert.strictEqual(err.code, 'ERR_INVALID_ARG_TYPE');
-    process.exit(0);
-  });
-}));
+{
+  const server = http.createServer(common.mustCall((req, res) => {
+    debug('Server sending early hints...');
+    assert.throws(() => {
+      res.writeEarlyHints({
+        link: '</>; '
+      });
+    }, (err) => err.code === 'ERR_INVALID_ARG_VALUE');
+
+    debug('Server sending full response...');
+    res.end(testResBody);
+    server.close();
+  }));
+
+  server.listen(0, common.mustCall(() => {
+    const req = http.request({
+      port: server.address().port, path: '/'
+    });
+
+    req.end();
+    debug('Client sending request...');
+
+    req.on('information', common.mustNotCall());
+  }));
+}
