4ada7ce43d6c1de5d2565db86bfc972c15e51d56
Filip Skokan <panva.ip@gmail.com>
2023-02-03 15:19:55 +0100

    test: add WPTRunner support for variants and generating WPT reports

--- test/wpt/test-encoding.js
+++ test/wpt/test-encoding.js
@@ -4,7 +4,6 @@ const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('encoding');
 
 runner.setInitScript(`
-  globalThis.location ||= {};
   const { MessageChannel } = require('worker_threads');
   global.MessageChannel = MessageChannel;
 `);
