2e41a14fbf2fb63bd9c580374b2f539ca9aca0c0
Debadree Chatterjee <debadree333@gmail.com>
2023-02-05 00:06:52 +0530

    fixup! make resource compulsory

--- test/parallel/test-aborted-util.js
+++ test/parallel/test-aborted-util.js
@@ -7,15 +7,6 @@ const assert = require('assert');
 const { getEventListeners } = require('events');
 
 {
-  // Test aborted works
-  const ac = new AbortController();
-  aborted(ac.signal).then(common.mustCall());
-  ac.abort();
-  assert.strictEqual(ac.signal.aborted, true);
-  assert.strictEqual(getEventListeners(ac.signal, 'abort').length, 0);
-}
-
-{
   // Test aborted works when provided a resource
   const ac = new AbortController();
   aborted(ac.signal, {}).then(common.mustCall());
@@ -39,7 +30,15 @@ const { getEventListeners } = require('events');
 {
   // Fails with error if not provided abort signal
   const sig = new EventTarget();
-  assert.rejects(aborted(sig), {
+  assert.rejects(aborted(sig, {}), {
+    name: 'TypeError',
+  });
+}
+
+{
+  // Fails if not provided a resource
+  const ac = new AbortController();
+  assert.rejects(aborted(ac.signal, null), {
     name: 'TypeError',
   });
 }
