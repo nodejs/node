4fb2fc75362578d7e8ffbbfb1df1f21f63875148
Filip Skokan <panva.ip@gmail.com>
2023-02-07 11:39:36 +0100

    test: enable api-invalid-label.any.js in encoding WPTs
    PR-URL: https://github.com/nodejs/node/pull/46506
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/wpt/test-encoding.js
+++ test/wpt/test-encoding.js
@@ -4,6 +4,7 @@ const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('encoding');
 
 runner.setInitScript(`
+  globalThis.location ||= {};
   const { MessageChannel } = require('worker_threads');
   global.MessageChannel = MessageChannel;
 `);
