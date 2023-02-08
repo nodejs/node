5a33d4298982e256a703752cbacf0bb435723d9b
Congcong Cai <congcong.ca.cai@bmw.com>
2023-02-08 17:45:49 +0800

    src: handle wasm out of bound in osx will raise SIGBUS correctly
    fix: #46559
    OSX will raise both SIGBUS and SIGSEGV when out of bound memory visit,
    This commit set sigaction in OSX for two signals to handle this.

---
+++ test/parallel/test-wasm-memory-out-of-bound.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('../common');
+const assert = require('assert');
+const fixtures = require('../common/fixtures');
+
+const buffer = fixtures.readSync('out-of-bound.wasm');
+WebAssembly.instantiate(buffer, {}).then((results) => {
+  assert.throws(() => {
+    results.instance.exports._start();
+  });
+});
+
