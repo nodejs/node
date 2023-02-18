109545a8fa91f07c24e6f1ecf6d946bbd72accae
Livia Medeiros <livia@cirno.name>
2023-02-19 01:32:05 +0900

    test: fix `test-net-autoselectfamily` for kernel without IPv6 support
    PR-URL: https://github.com/nodejs/node/pull/45856
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Paolo Insogna <paolo@cowtech.it>

--- test/parallel/test-net-autoselectfamily.js
+++ test/parallel/test-net-autoselectfamily.js
@@ -215,6 +215,8 @@ if (common.hasIPv6) {
         if (common.hasIPv6) {
           assert.strictEqual(error.code, 'ECONNREFUSED');
           assert.strictEqual(error.message, `connect ECONNREFUSED ::1:${port}`);
+        } else if (error.code === 'EAFNOSUPPORT') {
+          assert.strictEqual(error.message, `connect EAFNOSUPPORT ::1:${port} - Local (undefined:undefined)`);
         } else {
           assert.strictEqual(error.code, 'EADDRNOTAVAIL');
           assert.strictEqual(error.message, `connect EADDRNOTAVAIL ::1:${port} - Local (:::0)`);
