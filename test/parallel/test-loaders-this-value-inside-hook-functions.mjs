320a8adb452a8961e83a4e201b8cad1d6c73ec18
Geoffrey Booth <webadmin@geoffreybooth.com>
2023-02-18 06:26:36 -0800

    esm: misc test refactors
    - add test specific to the event loop
    - move parallel tests into es-module folder
    - refactor fixture to add braces for if blocks
    - use 'os' instead of 'fs' as placeholder
    - spelling
    
    PR-URL: https://github.com/nodejs/node/pull/46631
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/parallel/test-loaders-this-value-inside-hook-functions.mjs
+++
@@ -1,4 +0,0 @@
-// Flags: --experimental-loader ./test/fixtures/es-module-loaders/loader-this-value-inside-hook-functions.mjs
-import '../common/index.mjs';
-
-// Actual test is inside the loader module.
