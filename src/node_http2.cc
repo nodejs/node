644633e38acc4e7d944b38033e2b4c4b5e1077b0
Rich Trott <rtrott@gmail.com>
2023-02-21 04:46:21 -0800

    Revert "src: let http2 streams end after session close"
    This reverts commit dee882e94f4caa4e1cd608013f90f6a14629403f.
    Moved the test that demonstrated what this commit was fixing to the
    `known_issues` folder.
    
    Fixes: https://github.com/nodejs/node/issues/46234
    PR-URL: https://github.com/nodejs/node/pull/46721
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Debadree Chatterjee <debadree333@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -1124,17 +1124,6 @@ int Http2Session::OnStreamClose(nghttp2_session* handle,
   if (!stream || stream->is_destroyed())
     return 0;
 
-  // Don't close synchronously in case there's pending data to be written. This
-  // may happen when writing trailing headers.
-  if (code == NGHTTP2_NO_ERROR && nghttp2_session_want_write(handle) &&
-      env->can_call_into_js()) {
-    env->SetImmediate([handle, id, code, user_data](Environment* env) {
-      OnStreamClose(handle, id, code, user_data);
-    });
-
-    return 0;
-  }
-
   stream->Close(code);
 
   // It is possible for the stream close to occur before the stream is
