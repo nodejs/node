dac08dafc9f26a6ffd25e786d57d8d147fe64316
Tobias Nießen <tniessen@tnie.de>
2023-02-23 15:13:16 +0000

    crypto: handle cert with invalid SPKI gracefully
    When attempting to convert the SPKI of a X509Certificate to a KeyObject,
    throw an error if the subjectPublicKey cannot be parsed instead of
    aborting the process.
    
    Fixes: https://hackerone.com/bugs?report_id=1884159
    PR-URL: https://github.com/nodejs-private/node-private/pull/393
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    CVE-ID: CVE-2023-30588

--- src/crypto/crypto_x509.cc
+++ src/crypto/crypto_x509.cc
@@ -301,7 +301,11 @@ void X509Certificate::PublicKey(const FunctionCallbackInfo<Value>& args) {
   X509Certificate* cert;
   ASSIGN_OR_RETURN_UNWRAP(&cert, args.Holder());
 
+  // TODO(tniessen): consider checking X509_get_pubkey() when the
+  // X509Certificate object is being created.
+  ClearErrorOnReturn clear_error_on_return;
   EVPKeyPointer pkey(X509_get_pubkey(cert->get()));
+  if (!pkey) return ThrowCryptoError(env, ERR_get_error());
   ManagedEVPPKey epkey(std::move(pkey));
   std::shared_ptr<KeyObjectData> key_data =
       KeyObjectData::CreateAsymmetric(kKeyTypePublic, epkey);
