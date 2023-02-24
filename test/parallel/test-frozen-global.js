7c6fc71d895dafaee921460cb4ea87dffc7fda06
Joyee Cheung <joyeec9h3@gmail.com>
2023-02-25 00:57:40 +0100

    lib: make sure globals can be loaded after globalThis is freezed
    Since we made some of the globals lazily loaded to reduce startup
    overhead, and tried to update then as data properties once loaded,
    the lazily-loaded globals can no longer be loaded once globalThis
    is freezed. Fix this by not attempting to overwrite the property
    on loading these globals (so the property descriptors won't be
    spec-compliant in this case, but at least it's a good compromise
    between breakages and startup overhead).
    
    Note that certain globals that come from undici are still broken
    because undici attempts to define a global dispatcher symbol
    on globalThis. This is reflected in the test.

---
+++ test/parallel/test-frozen-global.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// This tests that the globals can still be loaded after globalThis is freezed.
+
+require('../common');
+const assert = require('assert');
+
+Object.freeze(globalThis);
+const keys = Reflect.ownKeys(globalThis).filter((k) => typeof k === 'string');
+
+// These failures come from undici. We can remove them once they are fixed.
+const knownFailures = new Set(['FormData', 'Headers', 'Request', 'Response']);
+const failures = new Map();
+const success = new Map();
+for (const key of keys) {
+  try {
+    success.set(key, globalThis[key]);
+  } catch (e) {
+    failures.set(key, e);
+  }
+}
+
+console.log(failures);
+assert.deepStrictEqual(new Set(failures.keys()), knownFailures);
