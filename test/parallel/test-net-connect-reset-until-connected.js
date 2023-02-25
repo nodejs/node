a743113de824cafc0b15b936d5e20f4ab9ceafeb
Vita Batrla <34657903+batrla@users.noreply.github.com>
2023-02-25 19:53:11 +0100

    test: fix test-net-connect-reset-until-connected
    Fixes: https://github.com/nodejs/node/issues/43446
    PR-URL: https://github.com/nodejs/node/pull/46781
    Reviewed-By: theanarkh <theratliter@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-net-connect-reset-until-connected.js
+++ test/parallel/test-net-connect-reset-until-connected.js
@@ -3,12 +3,21 @@
 const common = require('../common');
 const net = require('net');
 
+function barrier(count, cb) {
+  return function() {
+    if (--count === 0)
+      cb();
+  };
+}
+
 const server = net.createServer();
 server.listen(0, common.mustCall(function() {
   const port = server.address().port;
   const conn = net.createConnection(port);
+  const connok = barrier(2, () => conn.resetAndDestroy());
   conn.on('close', common.mustCall());
   server.on('connection', (socket) => {
+    connok();
     socket.on('error', common.expectsError({
       code: 'ECONNRESET',
       message: 'read ECONNRESET',
@@ -16,5 +25,5 @@ server.listen(0, common.mustCall(function() {
     }));
     server.close();
   });
-  conn.resetAndDestroy();
+  conn.on('connect', connok);
 }));
