a0a2c23b51b7230e5a8cad72399d13c2386fe8b2
Filip Skokan <panva.ip@gmail.com>
2023-02-25 18:03:18 +0100

    test: remove useless WPT init scripts
    PR-URL: https://github.com/nodejs/node/pull/46801
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/wpt/test-blob.js
+++ test/wpt/test-blob.js
@@ -4,11 +4,4 @@ const { WPTRunner } = require('../common/wpt');
 
 const runner = new WPTRunner('FileAPI/blob');
 
-runner.setInitScript(`
-  const { Blob } = require('buffer');
-  const { ReadableStream } = require('stream/web');
-  global.Blob = Blob;
-  global.ReadableStream = ReadableStream;
-`);
-
 runner.runJsTests();
