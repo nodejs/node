abf90ba91ff26d74589e16c1ceebc363ece63e36
Tobias Nießen <tniessen@tnie.de>
2023-02-25 21:29:59 +0100

    lib,src: fix a few typos in comments
    PR-URL: https://github.com/nodejs/node/pull/46835
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Mohammed Keyvanzadeh <mohammadkeyvanzade94@gmail.com>

--- src/base_object.h
+++ src/base_object.h
@@ -250,7 +250,7 @@ inline T* Unwrap(v8::Local<v8::Value> obj) {
 // reset to nullptr once the BaseObject is destroyed.
 // The API matches std::shared_ptr closely. However, this class is not thread
 // safe, that is, we can't have different BaseObjectPtrImpl instances in
-// different threads refering to the same BaseObject instance.
+// different threads referring to the same BaseObject instance.
 template <typename T, bool kIsWeak>
 class BaseObjectPtrImpl final {
  public:
