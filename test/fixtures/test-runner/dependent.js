f2d72e98afcdfed842ec19b82997aea4092ceaeb
Moshe Atlow <moshe@atlow.co.il>
2023-02-26 00:22:06 +0200

    attempt fixing

--- test/fixtures/test-runner/dependent.js
+++ test/fixtures/test-runner/dependent.js
@@ -1,4 +1,5 @@
+const test = require('node:test');
 require('./dependency.js');
 import('./dependency.mjs');
 import('data:text/javascript,');
-console.log('ok 1 - test has ran');
+test('test has ran');
