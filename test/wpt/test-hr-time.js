3daf508993b8e82e04a6d2407a9dd699effed052
Filip Skokan <panva.ip@gmail.com>
2023-02-25 16:49:48 +0100

    test: stop faking performance idlharness
    PR-URL: https://github.com/nodejs/node/pull/46794
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/wpt/test-hr-time.js
+++ test/wpt/test-hr-time.js
@@ -6,6 +6,5 @@ const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('hr-time');
 
 runner.pretendGlobalThisAs('Window');
-runner.brandCheckGlobalScopeAttribute('performance');
 
 runner.runJsTests();
--- test/wpt/test-performance-timeline.js
+++ test/wpt/test-performance-timeline.js
@@ -5,7 +5,6 @@ const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('performance-timeline');
 
 runner.pretendGlobalThisAs('Window');
-runner.brandCheckGlobalScopeAttribute('performance');
 runner.setInitScript(`
   // Create a dummy resource timing entry to mimic how the browser would
   // record the initial page load.
