928998dd2d417137944aac57e5ec2d03c258ace3
Filip Skokan <panva.ip@gmail.com>
2023-02-25 18:03:18 +0100

    test: remove useless WPT init scripts
    PR-URL: https://github.com/nodejs/node/pull/46801
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/wpt/test-broadcastchannel.js
+++ test/wpt/test-broadcastchannel.js
@@ -5,9 +5,4 @@ const { WPTRunner } = require('../common/wpt');
 
 const runner = new WPTRunner('webmessaging/broadcastchannel');
 
-runner.setInitScript(`
-  const { BroadcastChannel } = require('worker_threads');
-  global.BroadcastChannel = BroadcastChannel;
-`);
-
 runner.runJsTests();
--- test/wpt/test-encoding.js
+++ test/wpt/test-encoding.js
@@ -3,10 +3,4 @@ require('../common');
 const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('encoding');
 
-runner.setInitScript(`
-  globalThis.location ||= {};
-  const { MessageChannel } = require('worker_threads');
-  global.MessageChannel = MessageChannel;
-`);
-
 runner.runJsTests();
