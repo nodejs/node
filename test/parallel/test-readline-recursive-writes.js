f5c31901d7510ba02966a6dd8c4cbfb5944a5c26
Theodor Steiner <theodor.steiner@linecorp.com>
2023-02-27 17:23:13 +0900

    test: add test that fails without the changes of #46742

---
+++ test/parallel/test-readline-recursive-writes.js
@@ -0,0 +1,31 @@
+'use strict';
+const common = require('../common');
+const ArrayStream = require('../common/arraystream');
+const assert = require('assert');
+
+common.skipIfDumbTerminal();
+
+const readline = require('readline');
+const rli = new readline.Interface({
+  terminal: true,
+  input: new ArrayStream(),
+});
+
+let recursionDepth = 0;
+
+rli.on('line', function onLine() {
+  // Abort in case of infinite loop
+  if (recursionDepth > 2) {
+    return
+  }
+  recursionDepth++
+  // Write something recursively to readline
+  rli.write('foo');
+  return;
+});
+
+// minimal reproduction for #46731
+const testInput = ' \n}\n';
+rli.write(testInput);
+
+assert.strictEqual(recursionDepth, testInput.match(/\n/g).length, "infinite loop");
