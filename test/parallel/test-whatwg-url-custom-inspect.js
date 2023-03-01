52a893be74188d3c445c9a1aec649a1ee6599131
Yagiz Nizipli <yagiz@nizipli.com>
2023-03-01 16:24:19 -0500

    url: use private properties for brand check

--- test/parallel/test-whatwg-url-custom-inspect.js
+++ test/parallel/test-whatwg-url-custom-inspect.js
@@ -61,7 +61,7 @@ assert.strictEqual(
 
 assert.strictEqual(
   util.inspect({ a: url }, { depth: 0 }),
-  '{ a: [URL] }');
+  '{ a: URL {} }');
 
 class MyURL extends URL {}
 assert(util.inspect(new MyURL(url.href)).startsWith('MyURL {'));
