2ba010f2adc0d79cf3a339677ff07fbc745fa45d
Tony Gorez <gorez.tony@gmail.com>
2023-03-02 13:10:59 +0100

    tools: refactor dep_updaters
    PR-URL: https://github.com/nodejs/node/pull/46488
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>

--- tools/dep_updaters/update-libuv.sh
+++ tools/dep_updaters/update-libuv.sh
@@ -4,12 +4,29 @@ set -e
 
 BASE_DIR=$(cd "$(dirname "$0")/../.." && pwd)
 DEPS_DIR="$BASE_DIR/deps"
-LIBUV_VERSION=$1
+[ -z "$NODE" ] && NODE="$BASE_DIR/out/Release/node"
+[ -x "$NODE" ] || NODE=$(command -v node)
 
-if [ "$#" -le 0 ]; then
-  echo "Error: please provide an libuv version to update to"
-  echo "	e.g. $0 1.44.2"
-  exit 1
+NEW_VERSION="$("$NODE" --input-type=module <<'EOF'
+const res = await fetch('https://api.github.com/repos/libuv/libuv/releases/latest');
+if (!res.ok) throw new Error(`FetchError: ${res.status} ${res.statusText}`, { cause: res });
+const { tag_name } = await res.json();
+console.log(tag_name.replace('v', ''));
+EOF
+)"
+
+VERSION_H="$DEPS_DIR/uv/include/uv/version.h"
+CURRENT_MAJOR_VERSION=$(grep "#define UV_VERSION_MAJOR" "$VERSION_H" | sed -n "s/^.*MAJOR \(.*\)/\1/p")
+CURRENT_MINOR_VERSION=$(grep "#define UV_VERSION_MINOR" "$VERSION_H" | sed -n "s/^.*MINOR \(.*\)/\1/p")
+CURRENT_PATCH_VERSION=$(grep "#define UV_VERSION_PATCH" "$VERSION_H" | sed -n "s/^.*PATCH \(.*\)/\1/p")
+CURRENT_IS_RELEASE=$(grep "#define UV_VERSION_IS_RELEASE" "$VERSION_H" | sed -n "s/^.*RELEASE \(.*\)/\1/p")
+CURRENT_SUFFIX_VERSION=$(grep "#define UV_VERSION_SUFFIX" "$VERSION_H" | sed -n "s/^.*SUFFIX \"\(.*\)\"/\1/p")
+SUFFIX_STRING=$([ "$CURRENT_IS_RELEASE" = 1 ] || [ -z "$CURRENT_SUFFIX_VERSION" ] && echo "" || echo "-$CURRENT_SUFFIX_VERSION")
+CURRENT_VERSION="$CURRENT_MAJOR_VERSION.$CURRENT_MINOR_VERSION.$CURRENT_PATCH_VERSION$SUFFIX_STRING"
+
+if [ "$NEW_VERSION" = "$CURRENT_VERSION" ]; then
+  echo "Skipped because libuv is on the latest version."
+  exit 0
 fi
 
 echo "Making temporary workspace..."
@@ -27,7 +44,7 @@ trap cleanup INT TERM EXIT
 cd "$WORKSPACE"
 
 echo "Fetching libuv source archive..."
-curl -sL "https://api.github.com/repos/libuv/libuv/tarball/v$LIBUV_VERSION" | tar xzf -
+curl -sL "https://api.github.com/repos/libuv/libuv/tarball/v$NEW_VERSION" | tar xzf -
 mv libuv-libuv-* uv
 
 echo "Replacing existing libuv (except GYP build files)"
@@ -40,5 +57,9 @@ echo ""
 echo "Please git add uv, commit the new version:"
 echo ""
 echo "$ git add -A deps/uv"
-echo "$ git commit -m \"deps: update libuv to $LIBUV_VERSION\""
+echo "$ git commit -m \"deps: update libuv to $NEW_VERSION\""
 echo ""
+
+# The last line of the script should always print the new version,
+# as we need to add it to $GITHUB_ENV variable.
+echo "NEW_VERSION=$NEW_VERSION"
