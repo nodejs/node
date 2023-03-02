48d97d65e9aefa41074bfb984714093d9dc753a1
Debadree Chatterjee <debadree333@gmail.com>
2023-03-02 23:55:26 +0530

    fixup! move worker code to a fixture

--- test/parallel/test-worker-name.js
+++ test/parallel/test-worker-name.js
@@ -1,28 +1,22 @@
 'use strict';
 
 const common = require('../common');
-common.skipIfInspectorDisabled();
-const assert = require('assert');
-const { Worker, isMainThread, parentPort } = require('worker_threads');
+const fixtures = require('../common/fixtures');
 
-const { Session } = require('inspector');
+common.skipIfInspectorDisabled();
+common.skipIfWorker(); // This test requires both main and worker threads.
+
+const assert = require('assert');
+const { Worker, isMainThread } = require('worker_threads');
 
 if (isMainThread) {
   const name = 'Hello Thread';
-  const worker = new Worker(__filename, {
+  const expectedTitle = `[worker 1] ${name}`;
+  const worker = new Worker(fixtures.path('worker-name.js'), {
     name,
   });
-  const expectedTitle = `[worker 1] ${name}`;
   worker.once('message', common.mustCall((message) => {
     assert.strictEqual(message, expectedTitle);
     worker.postMessage('done');
   }));
-} else {
-  const session = new Session();
-  parentPort.once('message', () => {});
-  session.connectToMainThread();
-  session.on('NodeWorker.attachedToWorker', common.mustCall(({ params: { workerInfo } }) => {
-    parentPort.postMessage(workerInfo.title);
-  }));
-  session.post('NodeWorker.enable', { waitForDebuggerOnStart: false });
 }
