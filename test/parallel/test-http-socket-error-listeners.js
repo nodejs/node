de99a80bc161f1279f989d4cf31fabb995db88cb
Paolo Insogna <paolo@cowtech.it>
2023-03-05 22:42:58 +0100

    http: remove useless test

--- test/parallel/test-http-socket-error-listeners.js
+++ test/parallel/test-http-socket-error-listeners.js
@@ -46,39 +46,3 @@ const net = require('net');
   });
 }
 
-{
-  const valid = 'GET / HTTP/1.1\r\nHost:a\r\nContent-Length:1\r\n\r\n1\r\n';
-
-  const server = http.createServer((req, res) => {
-    const handler = common.mustCall(function handler(error) {
-      req.socket.removeListener('error', handler);
-    }, 2);
-
-    req.on('end', handler);
-    req.socket.on('error', handler);
-
-    res.removeHeader('date');
-    res.writeHead(204);
-    res.end();
-  });
-
-  server.listen(0, () => {
-    const client = net.createConnection({ host: '127.0.0.1', port: server.address().port });
-    let data = Buffer.alloc(0);
-
-    client.on('data', (chunk) => {
-      data = Buffer.concat([data, chunk]);
-    });
-
-    client.on('end', () => {
-      assert.strictEqual(
-        data.toString('utf-8'),
-        'HTTP/1.1 204 No Content\r\nConnection: keep-alive\r\nKeep-Alive: timeout=5\r\n\r\n' +
-        'HTTP/1.1 400 Bad Request\r\nConnection: close\r\n\r\n',
-      );
-      server.close();
-    });
-
-    client.write(valid + 'INVALID');
-  });
-}
