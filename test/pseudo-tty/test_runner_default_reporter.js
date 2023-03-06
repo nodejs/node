960e68a8c237a5ecdb792d6435985e1532e4963d
Moshe Atlow <moshe@atlow.co.il>
2023-03-06 12:20:51 +0200

    test_runner: default to spec reporter when on TTY environment
    PR-URL: https://github.com/nodejs/node/pull/46969
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

---
+++ test/pseudo-tty/test_runner_default_reporter.js
@@ -0,0 +1,11 @@
+'use strict';
+process.env.FORCE_COLOR = '1';
+delete process.env.NODE_DISABLE_COLORS;
+delete process.env.NO_COLOR;
+
+require('../common');
+const test = require('node:test');
+
+test('should pass', () => {});
+test('should fail', () => { throw new Error('fail'); });
+test('should skip', { skip: true }, () => {});
