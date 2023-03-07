293688b1e6d1eb84268ed7d0926edf2636355595
Yagiz Nizipli <yagiz@nizipli.com>
2023-03-07 16:59:46 -0500

    test: remove unnecessary urlToHttpOptions test

--- test/parallel/test-url-urltooptions.js
+++ test/parallel/test-url-urltooptions.js
@@ -18,20 +18,3 @@ assert.strictEqual(opts.hash, '#test');
 
 const { hostname } = urlToHttpOptions(new URL('http://[::1]:21'));
 assert.strictEqual(hostname, '::1');
-
-// If a WHATWG URL object is copied, it is possible that the resulting copy
-// contains the Symbols that Node uses for brand checking, but not the data
-// properties, which are getters. Verify that urlToHttpOptions() can handle
-// such a case.
-const copiedUrlObj = { ...urlObj };
-const copiedOpts = urlToHttpOptions(copiedUrlObj);
-assert.strictEqual(copiedOpts instanceof URL, false);
-assert.strictEqual(copiedOpts.protocol, undefined);
-assert.strictEqual(copiedOpts.auth, undefined);
-assert.strictEqual(copiedOpts.hostname, undefined);
-assert.strictEqual(copiedOpts.port, NaN);
-assert.strictEqual(copiedOpts.path, '');
-assert.strictEqual(copiedOpts.pathname, undefined);
-assert.strictEqual(copiedOpts.search, undefined);
-assert.strictEqual(copiedOpts.hash, undefined);
-assert.strictEqual(copiedOpts.href, undefined);
