7148776c0a0e36381ba5feedc2076cf4bdec9e3d
btea <2356281422@qq.com>
2023-03-10 07:48:47 +0800

    test: update test

--- test/internet/test-dns-getDefaultResultOrder.js
+++ test/internet/test-dns-getDefaultResultOrder.js
@@ -11,3 +11,14 @@ assert.ok(dnsOrder === 'ipv4first');
 dns.setDefaultResultOrder('verbatim');
 dnsOrder = dns.getDefaultResultOrder();
 assert.ok(dnsOrder === 'verbatim');
+
+{
+  (async function() {
+    const result = await dns.promises.lookup('localhost');
+    const result1 = await dns.promises.lookup('localhost', { verbatim: true });
+    assert.ok(result !== undefined);
+    assert.ok(result1 !== undefined);
+    assert.ok(result.address === result1.address);
+    assert.ok(result.family === result1.family);
+  })().then(common.mustCall());
+}
