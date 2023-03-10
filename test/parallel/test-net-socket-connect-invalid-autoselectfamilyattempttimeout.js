9c85c34e15d840f849e1b56378ed2c5be2c265a2
Deokjin Kim <deokjin81.kim@gmail.com>
2023-03-11 00:07:55 +0900

    net: fix setting of value in 'setDefaultAutoSelectFamilyAttemptTimeout'
    Document describes that the value have to be 10 if passed value to
    `setDefaultAutoSelectFamilyAttemptTimeout` is less than 10.
    So need to use 10 for 'if' statement and fix typo in document.
    
    Refs: https://github.com/nodejs/node/blob/main/doc/api/net.md#netsetdefaultautoselectfamilyattempttimeoutvalue
    PR-URL: https://github.com/nodejs/node/pull/47012
    Reviewed-By: Paolo Insogna <paolo@cowtech.it>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>

--- test/parallel/test-net-socket-connect-invalid-autoselectfamilyattempttimeout.js
+++ test/parallel/test-net-socket-connect-invalid-autoselectfamilyattempttimeout.js
@@ -18,3 +18,10 @@ for (const autoSelectFamilyAttemptTimeout of [-10, 0]) {
     net.setDefaultAutoSelectFamilyAttemptTimeout(autoSelectFamilyAttemptTimeout);
   }, { code: 'ERR_OUT_OF_RANGE' });
 }
+
+// Check the default value of autoSelectFamilyAttemptTimeout is 10
+// if passed number is less than 10
+for (const autoSelectFamilyAttemptTimeout of [1, 9]) {
+  net.setDefaultAutoSelectFamilyAttemptTimeout(autoSelectFamilyAttemptTimeout);
+  assert.strictEqual(net.getDefaultAutoSelectFamilyAttemptTimeout(), 10);
+}
