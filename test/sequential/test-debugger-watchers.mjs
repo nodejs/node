28de12146fcc4b1d0120e400e3789799095c247d
eungyu.lee <eungyu.lee@linecorp.com>
2023-03-10 12:57:03 +0900

    debugger: improve validations and documents for watch and unwatch
    - debugger: add string validation for watch(expr)
    - debugger: add help document for watch(index)
    - test: add test for watch(index) command
    - doc: add information on unwatch(index) option

--- test/sequential/test-debugger-watchers.mjs
+++ test/sequential/test-debugger-watchers.mjs
@@ -28,18 +28,25 @@ try {
   await cli.command('watchers');
 
   assert.match(cli.output, /x is not defined/);
+  assert.match(cli.output, /1: "Hello" = 'Hello'/);
+  assert.match(cli.output, /2: 42 = 42/);
+  assert.match(cli.output, /3: NaN = NaN/);
+  assert.match(cli.output, /4: true = true/);
+  assert.match(cli.output, /5: \[1, 2\] = \[ 1, 2 \]/);
+  assert.match(cli.output, /6: process\.env =\n\s+\{[\s\S]+,\n\s+\.\.\. \}/,
+               'shows "..." for process.env');
 
+  await cli.command('unwatch(4)');
   await cli.command('unwatch("42")');
   await cli.stepCommand('n');
 
   assert.match(cli.output, /0: x = 10/);
   assert.match(cli.output, /1: "Hello" = 'Hello'/);
   assert.match(cli.output, /2: NaN = NaN/);
-  assert.match(cli.output, /3: true = true/);
-  assert.match(cli.output, /4: \[1, 2\] = \[ 1, 2 \]/);
+  assert.match(cli.output, /3: \[1, 2\] = \[ 1, 2 \]/);
   assert.match(
     cli.output,
-    /5: process\.env =\n\s+\{[\s\S]+,\n\s+\.\.\. \}/,
+    /4: process\.env =\n\s+\{[\s\S]+,\n\s+\.\.\. \}/,
     'shows "..." for process.env'
   );
 
