722fd0ed690844a38cf2ba8263ea53a88c8aebef
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2023-03-13 22:48:36 +0100

    fix: disable test's case concurrency to avoid deadlock ~1/10 times

--- test/es-module/test-esm-loader-chaining.mjs
+++ test/es-module/test-esm-loader-chaining.mjs
@@ -15,7 +15,11 @@ const commonArgs = [
   commonInput,
 ];
 
-describe('ESM: loader chaining', { concurrency: true }, () => {
+describe('ESM: loader chaining', {
+  // FIXME(JakobJingleheimer): Enabling test runner concurrency causes the CustomizedModuleLoader to
+  // hang ~1/10 times. Disabling it is a significant hit to test execution time: 0.650s -> 1.823s
+  concurrency: false
+}, () => {
   it('should load unadulterated source when there are no loaders', async () => {
     const { code, stderr, stdout } = await spawnPromisified(
       execPath,
