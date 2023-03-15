a8c1d2b4969c70cc5085c9e7d5ddec9d7bff4213
Tony Gorez <gorez.tony@gmail.com>
2023-03-15 20:03:58 +0100

    tools: align update-ada.sh with other scripts
    PR-URL: https://github.com/nodejs/node/pull/47044
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>

--- tools/dep_updaters/update-ada.sh
+++ tools/dep_updaters/update-ada.sh
@@ -4,12 +4,22 @@ set -e
 
 BASE_DIR=$(cd "$(dirname "$0")/../.." && pwd)
 DEPS_DIR="$BASE_DIR/deps"
-ADA_VERSION=$1
+[ -z "$NODE" ] && NODE="$BASE_DIR/out/Release/node"
+[ -x "$NODE" ] || NODE=$(command -v node)
 
-if [ "$#" -le 0 ]; then
-  echo "Error: please provide an ada version to update to"
-  echo "	e.g. $0 1.0.0"
-  exit 1
+NEW_VERSION="$("$NODE" --input-type=module <<'EOF'
+const res = await fetch('https://api.github.com/repos/ada-url/ada/releases/latest');
+if (!res.ok) throw new Error(`FetchError: ${res.status} ${res.statusText}`, { cause: res });
+const { tag_name } = await res.json();
+console.log(tag_name.replace('v', ''));
+EOF
+)"
+
+CURRENT_VERSION=$(grep "#define ADA_VERSION" "$DEPS_DIR/ada/ada.h" | sed -n "s/^.*VERSION \"\(.*\)\"/\1/p")
+
+if [ "$NEW_VERSION" = "$CURRENT_VERSION" ]; then
+  echo "Skipped because ada is on the latest version."
+  exit 0
 fi
 
 echo "Making temporary workspace..."
@@ -24,8 +34,8 @@ cleanup () {
 
 trap cleanup INT TERM EXIT
 
-ADA_REF="v$ADA_VERSION"
-ADA_ZIP="ada-$ADA_VERSION.zip"
+ADA_REF="v$NEW_VERSION"
+ADA_ZIP="ada-$NEW_VERSION.zip"
 ADA_LICENSE="LICENSE-MIT"
 
 cd "$WORKSPACE"
@@ -47,5 +57,9 @@ echo ""
 echo "Please git add ada, commit the new version:"
 echo ""
 echo "$ git add -A deps/ada"
-echo "$ git commit -m \"deps: update ada to $ADA_VERSION\""
+echo "$ git commit -m \"deps: update ada to $NEW_VERSION\""
 echo ""
+
+# The last line of the script should always print the new version,
+# as we need to add it to $GITHUB_ENV variable.
+echo "NEW_VERSION=$NEW_VERSION"
