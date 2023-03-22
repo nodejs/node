9dfd0394c54edd7ac505ce623edcd4fd9ce3d51e
Khafra <42794878+KhafraDev@users.noreply.github.com>
2023-03-22 16:12:45 -0400

    test: remove useless WPT init scripts
    PR-URL: https://github.com/nodejs/node/pull/47221
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- test/wpt/test-structured-clone.js
+++ test/wpt/test-structured-clone.js
@@ -4,9 +4,4 @@ const { WPTRunner } = require('../common/wpt');
 
 const runner = new WPTRunner('html/webappapis/structured-clone');
 
-runner.setInitScript(`
-  const { File } = require('buffer');
-  globalThis.File = File;
-`);
-
 runner.runJsTests();
