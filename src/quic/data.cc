e762f996e058d2a62a1dcfe86a80dd25812e7025
James M Snell <jasnell@gmail.com>
2023-03-26 16:53:26 -0700

    quic: add Store implementation
    Store is a utility class that abstracts working with BackingStore
    that will be used in the quic impl

--- src/quic/data.cc
+++ src/quic/data.cc
@@ -1,8 +1,15 @@
 #include "data.h"
+#include <env-inl.h>
+#include <memory_tracker-inl.h>
 #include <node_sockaddr-inl.h>
-#include "ngtcp2/ngtcp2.h"
+#include <ngtcp2/ngtcp2.h>
+#include <v8.h>
 
 namespace node {
+
+using v8::Local;
+using v8::Value;
+
 namespace quic {
 
 Path::Path(const SocketAddress& local, const SocketAddress& remote) {
@@ -12,6 +19,73 @@ Path::Path(const SocketAddress& local, const SocketAddress& remote) {
 
 PathStorage::PathStorage() { ngtcp2_path_storage_zero(this); }
 PathStorage::operator ngtcp2_path() { return path; }
+
+// ============================================================================
+
+Store::Store(std::shared_ptr<v8::BackingStore> store,
+    size_t length,
+    size_t offset)
+    : store_(std::move(store)),
+      length_(length),
+      offset_(offset) {
+  CHECK_LE(offset_, store->ByteLength());
+  CHECK_LE(length_, store->ByteLength() - offset_);
+}
+
+Store::Store(std::unique_ptr<v8::BackingStore> store,
+    size_t length,
+    size_t offset)
+    : store_(std::move(store)),
+      length_(length),
+      offset_(offset) {
+  CHECK_LE(offset_, store->ByteLength());
+  CHECK_LE(length_, store->ByteLength() - offset_);
+}
+
+Store::Store(v8::Local<v8::ArrayBuffer> buffer, Option option)
+    : Store(buffer->GetBackingStore(), buffer->ByteLength()) {
+  if (option == Option::DETACH) {
+    USE(buffer->Detach(Local<Value>()));
+  }
+}
+
+Store::Store(v8::Local<v8::ArrayBufferView> view, Option option)
+    : Store(view->Buffer()->GetBackingStore(),
+            view->ByteLength(),
+            view->ByteOffset()) {
+  if (option == Option::DETACH) {
+    USE(view->Buffer()->Detach(Local<Value>()));
+  }
+}
+
+Store::operator bool() const { return store_ != nullptr; }
+size_t Store::length() const { return length_; }
+
+template <typename T, typename t>
+T Store::convert() const {
+  T buf;
+  buf.base = store_ != nullptr ?
+      static_cast<t*>(store_->Data()) + offset_ :
+      nullptr;
+  buf.len = length_;
+  return buf;
+}
+
+Store::operator uv_buf_t() const {
+  return convert<uv_buf_t, char>();
+}
+
+Store::operator ngtcp2_vec() const {
+  return convert<ngtcp2_vec, uint8_t>();
+}
+
+Store::operator nghttp3_vec() const {
+  return convert<nghttp3_vec, uint8_t>();
+}
+
+void Store::MemoryInfo(MemoryTracker* tracker) const {
+  tracker->TrackField("store", store_);
+}
 
 }  // namespace quic
 }  // namespace node
--- src/quic/data.h
+++ src/quic/data.h
@@ -2,8 +2,12 @@
 
 #if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
 
+#include <memory_tracker.h>
+#include <node_internals.h>
 #include <node_sockaddr.h>
 #include <ngtcp2/ngtcp2.h>
+#include <nghttp3/nghttp3.h>
+#include <v8.h>
 
 namespace node {
 namespace quic {
@@ -15,6 +19,42 @@ struct Path final : public ngtcp2_path {
 struct PathStorage final: public ngtcp2_path_storage {
   PathStorage();
   operator ngtcp2_path();
+};
+
+class Store final : public MemoryRetainer {
+ public:
+  Store() = default;
+
+  Store(std::shared_ptr<v8::BackingStore> store,
+        size_t length,
+        size_t offset = 0);
+  Store(std::unique_ptr<v8::BackingStore> store,
+        size_t length,
+        size_t offset = 0);
+
+  enum class Option {
+    NONE,
+    DETACH,
+  };
+
+  Store(v8::Local<v8::ArrayBuffer> buffer, Option option = Option::NONE);
+  Store(v8::Local<v8::ArrayBufferView> view, Option option = Option::NONE);
+
+  operator uv_buf_t() const;
+  operator ngtcp2_vec() const;
+  operator nghttp3_vec() const;
+  operator bool() const;
+  size_t length() const;
+
+  void MemoryInfo(MemoryTracker* tracker) const override;
+  SET_MEMORY_INFO_NAME(Store);
+  SET_SELF_SIZE(Store);
+
+ private:
+   template <typename T, typename t> T convert() const;
+   std::shared_ptr<v8::BackingStore> store_;
+   size_t length_ = 0;
+   size_t offset_ = 0;
 };
 
 }  // namespace quic
