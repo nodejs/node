e1cf8bdd9b95ea9c19a0a45d691df40573cbb4e4
Darshan Sen <raisinten@gmail.com>
2023-03-30 14:06:13 +0530

    sea: fix memory leak detected by asan
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- src/node_sea.cc
+++ src/node_sea.cc
@@ -17,6 +17,7 @@
 #include <memory>
 #include <string_view>
 #include <tuple>
+#include <vector>
 
 #if !defined(DISABLE_SINGLE_EXECUTABLE_APPLICATION)
 
@@ -60,7 +61,7 @@ std::tuple<int, char**> FixupArgsForSEA(int argc, char** argv) {
   // Repeats argv[0] at position 1 on argv as a replacement for the missing
   // entry point file path.
   if (IsSingleExecutable()) {
-    char** new_argv = new char*[argc + 2];
+    static std::vector<char*> new_argv(argc + 2);
     int new_argc = 0;
     new_argv[new_argc++] = argv[0];
     new_argv[new_argc++] = argv[0];
@@ -72,7 +73,7 @@ std::tuple<int, char**> FixupArgsForSEA(int argc, char** argv) {
     new_argv[new_argc] = nullptr;
 
     argc = new_argc;
-    argv = new_argv;
+    argv = new_argv.data();
   }
 
   return {argc, argv};
