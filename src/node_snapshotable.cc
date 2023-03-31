6c0f3e1f1faef4c445db3e5b5f502e11e2c66982
Yagiz Nizipli <yagiz@nizipli.com>
2023-03-31 09:03:06 -0400

    url: use ada::url_aggregator for parsing urls
    PR-URL: https://github.com/nodejs/node/pull/47339
    Reviewed-By: Tiancheng "Timothy" Gu <timothygu99@gmail.com>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- src/node_snapshotable.cc
+++ src/node_snapshotable.cc
@@ -18,6 +18,7 @@
 #include "node_metadata.h"
 #include "node_process.h"
 #include "node_snapshot_builder.h"
+#include "node_url.h"
 #include "node_util.h"
 #include "node_v8.h"
 #include "node_v8_platform-inl.h"
