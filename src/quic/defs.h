e8ea83416a0101588d9e4aff032228250963aa38
James M Snell <jasnell@gmail.com>
2023-03-31 17:58:57 -0700

    quic: add more QUIC implementation
    * add TLSContext
    * quic: add stat collection utilities
    * add Packet
    * add NgTcp2CallbackScope/NgHttp3CallbackScope
    
    PR-URL: https://github.com/nodejs/node/pull/47494
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>

--- src/quic/defs.h
+++ src/quic/defs.h
@@ -1,11 +1,27 @@
 #pragma once
 
+#include <aliased_struct.h>
 #include <env.h>
 #include <node_errors.h>
+#include <uv.h>
 #include <v8.h>
 
 namespace node {
 namespace quic {
+
+template <typename Opt, std::string Opt::*member>
+bool SetOption(Environment* env,
+               Opt* options,
+               const v8::Local<v8::Object>& object,
+               const v8::Local<v8::String>& name) {
+  v8::Local<v8::Value> value;
+  if (!object->Get(env->context(), name).ToLocal(&value)) return false;
+  if (!value->IsUndefined()) {
+    Utf8Value utf8(env->isolate(), value);
+    options->*member = *utf8;
+  }
+  return true;
+}
 
 template <typename Opt, bool Opt::*member>
 bool SetOption(Environment* env,
@@ -49,6 +65,38 @@ bool SetOption(Environment* env,
   }
   return true;
 }
+
+// Utilities used to update the stats for Endpoint, Session, and Stream
+// objects. The stats themselves are maintained in an AliasedStruct within
+// each of the relevant classes.
+
+template <typename Stats, uint64_t Stats::*member>
+void IncrementStat(Stats* stats, uint64_t amt = 1) {
+  stats->*member += amt;
+}
+
+template <typename Stats, uint64_t Stats::*member>
+void RecordTimestampStat(Stats* stats) {
+  stats->*member = uv_hrtime();
+}
+
+template <typename Stats, uint64_t Stats::*member>
+void SetStat(Stats* stats, uint64_t val) {
+  stats->*member = val;
+}
+
+template <typename Stats, uint64_t Stats::*member>
+uint64_t GetStat(Stats* stats) {
+  return stats->*member;
+}
+
+#define STAT_INCREMENT(Type, name) IncrementStat<Type, &Type::name>(&stats_);
+#define STAT_INCREMENT_N(Type, name, amt)                                      \
+  IncrementStat<Type, &Type::name>(&stats_, amt);
+#define STAT_RECORD_TIMESTAMP(Type, name)                                      \
+  RecordTimestampStat<Type, &Type::name>(&stats_);
+#define STAT_SET(Type, name, val) SetStat<Type, &Type::name>(&stats_, val);
+#define STAT_GET(Type, name) GetStat<Type, &Type::name>(&stats_);
 
 }  // namespace quic
 }  // namespace node
