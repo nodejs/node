6d065863c268f3dab4a17fcda8be219eb2e45764
Pulkit Gupta <pulkit30.bsr@gmail.com>
2023-04-02 22:48:48 +0530

    test_runner: add code coverage support to spec reporter
    PR-URL: https://github.com/nodejs/node/pull/46674
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>

--- test/parallel/test-runner-coverage.js
+++ test/parallel/test-runner-coverage.js
@@ -18,7 +18,7 @@ function findCoverageFileForPid(pid) {
   });
 }
 
-function getCoverageFixtureReport() {
+function getTapCoverageFixtureReport() {
   const report = [
     '# start of coverage report',
     '# file | line % | branch % | funcs % | uncovered lines',
@@ -37,64 +37,120 @@ function getCoverageFixtureReport() {
   return report;
 }
 
-test('--experimental-test-coverage and --test cannot be combined', () => {
-  // TODO(cjihrig): This test can be removed once multi-process code coverage
-  // is supported.
-  const args = ['--test', '--experimental-test-coverage'];
-  const result = spawnSync(process.execPath, args);
+function getSpecCoverageFixtureReport() {
+  const report = [
+    '\u2139 start of coverage report',
+    '\u2139 file | line % | branch % | funcs % | uncovered lines',
+    '\u2139 test/fixtures/test-runner/coverage.js | 78.65 | 38.46 | 60.00 | 12, ' +
+    '13, 16, 17, 18, 19, 20, 21, 22, 27, 39, 43, 44, 61, 62, 66, 67, 71, 72',
+    '\u2139 test/fixtures/test-runner/invalid-tap.js | 100.00 | 100.00 | 100.00 | ',
+    '\u2139 test/fixtures/v8-coverage/throw.js | 71.43 | 50.00 | 100.00 | 5, 6',
+    '\u2139 all files | 78.35 | 43.75 | 60.00 |',
+    '\u2139 end of coverage report',
+  ].join('\n');
 
-  // 9 is the documented exit code for an invalid CLI argument.
-  assert.strictEqual(result.status, 9);
-  assert.match(
-    result.stderr.toString(),
-    /--experimental-test-coverage cannot be used with --test/
-  );
-});
-
-test('handles the inspector not being available', (t) => {
-  if (process.features.inspector) {
-    return;
+  if (common.isWindows) {
+    return report.replaceAll('/', '\\');
   }
 
-  const fixture = fixtures.path('test-runner', 'coverage.js');
-  const args = ['--experimental-test-coverage', fixture];
-  const result = spawnSync(process.execPath, args);
+  return report;
+}
 
-  assert(!result.stdout.toString().includes('# start of coverage report'));
-  assert(result.stderr.toString().includes('coverage could not be collected'));
-  assert.strictEqual(result.status, 0);
-  assert(!findCoverageFileForPid(result.pid));
+test('test coverage report', async (t) => {
+  await t.test('--experimental-test-coverage and --test cannot be combined', () => {
+    // TODO(cjihrig): This test can be removed once multi-process code coverage
+    // is supported.
+    const args = ['--test', '--experimental-test-coverage'];
+    const result = spawnSync(process.execPath, args);
+
+    // 9 is the documented exit code for an invalid CLI argument.
+    assert.strictEqual(result.status, 9);
+    assert.match(
+      result.stderr.toString(),
+      /--experimental-test-coverage cannot be used with --test/
+    );
+  });
+
+  await t.test('handles the inspector not being available', (t) => {
+    if (process.features.inspector) {
+      return;
+    }
+
+    const fixture = fixtures.path('test-runner', 'coverage.js');
+    const args = ['--experimental-test-coverage', fixture];
+    const result = spawnSync(process.execPath, args);
+
+    assert(!result.stdout.toString().includes('# start of coverage report'));
+    assert(result.stderr.toString().includes('coverage could not be collected'));
+    assert.strictEqual(result.status, 0);
+    assert(!findCoverageFileForPid(result.pid));
+  });
 });
 
-test('coverage is reported and dumped to NODE_V8_COVERAGE if present', (t) => {
-  if (!process.features.inspector) {
-    return;
-  }
+test('test tap coverage reporter', async (t) => {
+  await t.test('coverage is reported and dumped to NODE_V8_COVERAGE if present', (t) => {
+    if (!process.features.inspector) {
+      return;
+    }
 
-  const fixture = fixtures.path('test-runner', 'coverage.js');
-  const args = ['--experimental-test-coverage', fixture];
-  const options = { env: { ...process.env, NODE_V8_COVERAGE: tmpdir.path } };
-  const result = spawnSync(process.execPath, args, options);
-  const report = getCoverageFixtureReport();
+    const fixture = fixtures.path('test-runner', 'coverage.js');
+    const args = ['--experimental-test-coverage', '--test-reporter', 'tap', fixture];
+    const options = { env: { ...process.env, NODE_V8_COVERAGE: tmpdir.path } };
+    const result = spawnSync(process.execPath, args, options);
+    const report = getTapCoverageFixtureReport();
 
-  assert(result.stdout.toString().includes(report));
-  assert.strictEqual(result.stderr.toString(), '');
-  assert.strictEqual(result.status, 0);
-  assert(findCoverageFileForPid(result.pid));
+    assert(result.stdout.toString().includes(report));
+    assert.strictEqual(result.stderr.toString(), '');
+    assert.strictEqual(result.status, 0);
+    assert(findCoverageFileForPid(result.pid));
+  });
+
+  await t.test('coverage is reported without NODE_V8_COVERAGE present', (t) => {
+    if (!process.features.inspector) {
+      return;
+    }
+
+    const fixture = fixtures.path('test-runner', 'coverage.js');
+    const args = ['--experimental-test-coverage', '--test-reporter', 'tap', fixture];
+    const result = spawnSync(process.execPath, args);
+    const report = getTapCoverageFixtureReport();
+
+    assert(result.stdout.toString().includes(report));
+    assert.strictEqual(result.stderr.toString(), '');
+    assert.strictEqual(result.status, 0);
+    assert(!findCoverageFileForPid(result.pid));
+  });
 });
 
-test('coverage is reported without NODE_V8_COVERAGE present', (t) => {
-  if (!process.features.inspector) {
-    return;
-  }
+test('test spec coverage reporter', async (t) => {
+  await t.test('coverage is reported and dumped to NODE_V8_COVERAGE if present', (t) => {
+    if (!process.features.inspector) {
+      return;
+    }
+    const fixture = fixtures.path('test-runner', 'coverage.js');
+    const args = ['--experimental-test-coverage', '--test-reporter', 'spec', fixture];
+    const options = { env: { ...process.env, NODE_V8_COVERAGE: tmpdir.path } };
+    const result = spawnSync(process.execPath, args, options);
+    const report = getSpecCoverageFixtureReport();
 
-  const fixture = fixtures.path('test-runner', 'coverage.js');
-  const args = ['--experimental-test-coverage', fixture];
-  const result = spawnSync(process.execPath, args);
-  const report = getCoverageFixtureReport();
+    assert(result.stdout.toString().includes(report));
+    assert.strictEqual(result.stderr.toString(), '');
+    assert.strictEqual(result.status, 0);
+    assert(findCoverageFileForPid(result.pid));
+  });
 
-  assert(result.stdout.toString().includes(report));
-  assert.strictEqual(result.stderr.toString(), '');
-  assert.strictEqual(result.status, 0);
-  assert(!findCoverageFileForPid(result.pid));
+  await t.test('coverage is reported without NODE_V8_COVERAGE present', (t) => {
+    if (!process.features.inspector) {
+      return;
+    }
+    const fixture = fixtures.path('test-runner', 'coverage.js');
+    const args = ['--experimental-test-coverage', '--test-reporter', 'spec', fixture];
+    const result = spawnSync(process.execPath, args);
+    const report = getSpecCoverageFixtureReport();
+
+    assert(result.stdout.toString().includes(report));
+    assert.strictEqual(result.stderr.toString(), '');
+    assert.strictEqual(result.status, 0);
+    assert(!findCoverageFileForPid(result.pid));
+  });
 });
