4faf54748166822a43278707cac8cdd056242cb5
Sai Nagendra <31223871+sainagendra@users.noreply.github.com>
2023-04-11 01:36:05 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// function fib(n ){
+//   const fib = [0,1]
+//   for(let i=2; i<n; i++){
+//     fib[i]= fib[i-1]+ fib[i-2]
+// console.log(fib[i])
+//   }
+//   return fib
+// }
+// console.log(fib(5))
+
+// function factorial(n) {
+//   let result = 1;
+//   for(let i=2; i<n; i++){
+//     result = result*i;
+//   }
+//   return result;
+// }
+
+// console.log(factorial(4))
+
+
+
+
