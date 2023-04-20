4c0fbfda3baffda9ac4581bc0d20622a316f351c
Geoffrey Booth <webadmin@geoffreybooth.com>
2023-04-19 17:28:16 -0700

    Add tests, reimplement new test in current loaders tests style

---
+++ test/es-module/test-loaders-workers-spawned.mjs
@@ -0,0 +1,75 @@
+import { spawnPromisified } from '../common/index.mjs';
+import * as fixtures from '../common/fixtures.mjs';
+import assert from 'node:assert';
+import { execPath } from 'node:process';
+import { describe, it } from 'node:test';
+
+describe('Worker threads do not spawn infinitely', { concurrency: true }, () => {
+  it('should not trigger an infinite loop when using a loader exports no recognized hooks', async () => {
+    const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
+      '--no-warnings',
+      '--experimental-loader',
+      fixtures.fileURL('empty.js'),
+      '--eval',
+      'setTimeout(() => console.log("hello"),99)',
+    ]);
+
+    assert.strictEqual(stderr, '');
+    assert.match(stdout, /^hello\r?\n$/);
+    assert.strictEqual(code, 0);
+    assert.strictEqual(signal, null);
+  });
+
+  it('should support a CommonJS entry point and a loader that imports a CommonJS module', async () => {
+    const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
+      '--no-warnings',
+      '--experimental-loader',
+      fixtures.fileURL('es-module-loaders/loader-with-dep.mjs'),
+      fixtures.path('print-delayed.js'),
+    ]);
+
+    assert.strictEqual(stderr, '');
+    assert.match(stdout, /^delayed\r?\n$/);
+    assert.strictEqual(code, 0);
+    assert.strictEqual(signal, null);
+  });
+
+  it('should support --require and --import along with using a loader written in CommonJS', async () => {
+    const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
+      '--no-warnings',
+      '--require',
+      fixtures.path('printA.js'),
+      '--import',
+      fixtures.fileURL('printB.js'),
+      '--experimental-loader',
+      fixtures.fileURL('empty.js'),
+      '--eval',
+      'setTimeout(() => console.log("C"),99)',
+    ]);
+
+    assert.strictEqual(stderr, '');
+    assert.match(stdout, /^A\r?\nA\r?\nB\r?\nC\r?\n$/);
+    assert.strictEqual(code, 0);
+    assert.strictEqual(signal, null);
+  });
+
+  it('should support --require and --import along with using a loader written in ESM', async () => {
+    const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
+      '--no-warnings',
+      '--require',
+      fixtures.path('printA.js'),
+      '--import',
+      fixtures.fileURL('printB.js'),
+      '--experimental-loader',
+      fixtures.fileURL('empty.js'),
+      '--input-type=module',
+      '--eval',
+      'setTimeout(() => console.log("C"),99)',
+    ]);
+
+    assert.strictEqual(stderr, '');
+    assert.match(stdout, /^A\r?\nA\r?\nB\r?\nC\r?\n$/);
+    assert.strictEqual(code, 0);
+    assert.strictEqual(signal, null);
+  });
+});
---
+++ test/fixtures/print-delayed.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+  console.log('delayed');
+}, 1000);
