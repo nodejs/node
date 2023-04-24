6eb890753f5d5f0bef6407939c50b92ee34cdd01
btea <2356281422@qq.com>
2023-04-24 21:03:56 +0800

    docs: add scoll to top button

--- doc/api_assets/api.js
+++ doc/api_assets/api.js
@@ -166,6 +166,26 @@
     });
   }
 
+  const listenScrollToTopButton = () => {
+    const scrollToTop = document.querySelector('#scroll-to-top');
+  
+    const showScrollToTopIfOutOfBounds = () =>
+      window.requestAnimationFrame(
+        () =>
+          (scrollToTop.style.display =
+            window.pageYOffset * 2 > window.innerHeight ? 'block' : 'none')
+      );
+  
+    document.addEventListener('scroll', showScrollToTopIfOutOfBounds, {
+      passive: true,
+    });
+  
+    scrollToTop.addEventListener('click', e => {
+      e.preventDefault();
+      window.scrollTo(0, 0);
+    });
+  };
+
   function bootstrap() {
     // Check if we have JavaScript support.
     document.documentElement.classList.add('has-js');
@@ -183,6 +203,8 @@
     setupAltDocsLink();
 
     setupCopyButton();
+
+    listenScrollToTopButton();
   }
 
   if (document.readyState === 'loading') {
