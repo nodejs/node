3bbb1fc9902ed3e2b8cf424f8d55d828ba39c125
Moshe Atlow <moshe@atlow.co.il>
2023-04-24 15:04:59 +0300

    test_runner: fix test counting
    PR-URL: https://github.com/nodejs/node/pull/47675
    Fixes: https://github.com/nodejs/node/issues/47365
    Fixes: https://github.com/nodejs/node/issues/47696
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/fixtures/test-runner/output/output_cli.js
+++ test/fixtures/test-runner/output/output_cli.js
@@ -5,5 +5,5 @@ const fixtures = require('../../../common/fixtures');
 const spawn = require('node:child_process').spawn;
 
 spawn(process.execPath,
-      ['--no-warnings', '--test', '--test-reporter', 'tap', fixtures.path('test-runner/output/output.js')],
+      ['--no-warnings', '--test', '--test-reporter', 'tap', fixtures.path('test-runner/output/output.js'), fixtures.path('test-runner/output/single.js')],
       { stdio: 'inherit' });
