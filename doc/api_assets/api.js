38934928825764674c9e291ceb13ffa0fa4e5f01
btea <2356281422@qq.com>
2023-04-25 08:28:27 +0800

    chore: fix lint error

--- doc/api_assets/api.js
+++ doc/api_assets/api.js
@@ -168,19 +168,18 @@
 
   const listenScrollToTopButton = () => {
     const scrollToTop = document.querySelector('#scroll-to-top');
-  
+
     const showScrollToTopIfOutOfBounds = () =>
       window.requestAnimationFrame(
         () =>
-          (scrollToTop.style.display =
-            window.pageYOffset * 2 > window.innerHeight ? 'block' : 'none')
+          (scrollToTop.style.display = window.pageYOffset * 2 > window.innerHeight ? 'block' : 'none')
       );
-  
+
     document.addEventListener('scroll', showScrollToTopIfOutOfBounds, {
       passive: true,
     });
-  
-    scrollToTop.addEventListener('click', e => {
+
+    scrollToTop.addEventListener('click', (e) => {
       e.preventDefault();
       window.scrollTo(0, 0);
     });
