48b4255977562f8328f80bfe1efdb89ee8350700
Yagiz Nizipli <yagiz@nizipli.com>
2023-04-28 14:48:38 -0400

    src: replace idna functions with ada::idna
    Co-authored-by: Daniel Lemire <daniel@lemire.me>
    PR-URL: https://github.com/nodejs/node/pull/47735
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>

--- src/encoding_binding.h
+++ src/encoding_binding.h
@@ -32,6 +32,9 @@ class BindingData : public SnapshotableObject {
   static void EncodeUtf8String(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void DecodeUTF8(const v8::FunctionCallbackInfo<v8::Value>& args);
 
+  static void ToASCII(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void ToUnicode(const v8::FunctionCallbackInfo<v8::Value>& args);
+
   static void Initialize(v8::Local<v8::Object> target,
                          v8::Local<v8::Value> unused,
                          v8::Local<v8::Context> context,
