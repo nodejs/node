568ad96726504e5bd9bf7f5cf53fa342ca2637c3
Erick Wendel <erick.workspace@gmail.com>
2023-04-28 19:12:43 -0300

    test_runner: add initial draft for fakeTimers
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ test/parallel/test-runner-fake-timers.js
@@ -0,0 +1,46 @@
+'use strict';
+const common = require('../common');
+process.env.NODE_TEST_KNOWN_GLOBALS = 0;
+
+const assert = require('node:assert');
+const { fakeTimers, it, mock, afterEach, describe } = require('node:test');
+describe('Faketimers Test Suite', () => {
+
+  describe('setTimeout Suite', () => {
+    afterEach(() => fakeTimers.reset());
+
+    it('should advance in time and trigger timers when calling the .tick function', (t) => {
+      fakeTimers.enable();
+
+      const fn = mock.fn(() => {});
+
+      global.setTimeout(fn, 4000);
+
+      fakeTimers.tick(4000);
+      assert.ok(fn.mock.callCount());
+    });
+
+    it('should advance in time and trigger timers when calling the .tick function multiple times', (t) => {
+      fakeTimers.enable();
+      const fn = mock.fn();
+
+      global.setTimeout(fn, 2000);
+
+      fakeTimers.tick(1000);
+      fakeTimers.tick(1000);
+
+      assert.strictEqual(fn.mock.callCount(), 1);
+    });
+
+    it('should keep setTimeout working if fakeTimers are disabled', (t, done) => {
+      const now = Date.now();
+      const timeout = 2;
+      const expected = () => now - timeout;
+      global.setTimeout(common.mustCall(() => {
+        assert.strictEqual(now - timeout, expected());
+        done();
+      }), timeout);
+    });
+
+  });
+});
