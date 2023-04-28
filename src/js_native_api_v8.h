bb3cc12bb86d79ac0df9e04c5e812c1f8549f17f
Vladimir Morozov <vmorozov@microsoft.com>
2023-04-28 09:18:13 -0700

    add a comment for symbol_key_for_ field

--- src/js_native_api_v8.h
+++ src/js_native_api_v8.h
@@ -148,6 +148,7 @@ struct napi_env__ {
   int refs = 1;
   void* instance_data = nullptr;
   int32_t module_api_version = NODE_API_DEFAULT_MODULE_API_VERSION;
+  // Cached JavaScript Symbol.keyFor function.
   v8impl::Persistent<v8::Function> symbol_key_for_{};
 
  protected:
