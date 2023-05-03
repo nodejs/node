a22321fa451a55a52f6da46bdfcd0ae45560ed77
Erick Wendel <erick.workspace@gmail.com>
2023-05-03 18:31:52 -0300

    test_runner: rename from fakeTimers to mockTimers
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- test/parallel/test-runner-mock-timers.js
+++ test/parallel/test-runner-mock-timers.js
@@ -7,38 +7,38 @@ const { it, mock, afterEach, describe } = require('node:test');
 const nodeTimers = require('node:timers');
 const nodeTimersPromises = require('node:timers/promises');
 
-const { fakeTimers } = mock;
+const { timers } = mock;
 
-describe('Faketimers Test Suite', () => {
+describe('Timers Test Suite', () => {
   describe('globals/timers', () => {
     describe('setTimeout Suite', () => {
-      afterEach(() => fakeTimers.reset());
+      afterEach(() => timers.reset());
 
       it('should advance in time and trigger timers when calling the .tick function', (t) => {
-        fakeTimers.enable();
+        timers.enable();
 
         const fn = mock.fn(() => {});
 
         global.setTimeout(fn, 4000);
 
-        fakeTimers.tick(4000);
+        timers.tick(4000);
         assert.ok(fn.mock.callCount());
       });
 
       it('should advance in time and trigger timers when calling the .tick function multiple times', (t) => {
-        t.mock.fakeTimers.enable();
+        t.mock.timers.enable();
         const fn = t.mock.fn();
 
         global.setTimeout(fn, 2000);
 
-        t.mock.fakeTimers.tick(1000);
-        t.mock.fakeTimers.tick(500);
-        t.mock.fakeTimers.tick(500);
+        t.mock.timers.tick(1000);
+        t.mock.timers.tick(500);
+        t.mock.timers.tick(500);
 
         assert.strictEqual(fn.mock.callCount(), 1);
       });
 
-      it('should keep setTimeout working if fakeTimers are disabled', (t, done) => {
+      it('should keep setTimeout working if timers are disabled', (t, done) => {
         const now = Date.now();
         const timeout = 2;
         const expected = () => now - timeout;
@@ -54,14 +54,14 @@ describe('Faketimers Test Suite', () => {
   describe('timers Suite', () => {
     describe('setTimeout Suite', () => {
       it('should advance in time and trigger timers when calling the .tick function multiple times', (t) => {
-        t.mock.fakeTimers.enable();
+        t.mock.timers.enable();
         const fn = t.mock.fn();
         const { setTimeout } = nodeTimers;
         setTimeout(fn, 2000);
 
-        t.mock.fakeTimers.tick(1000);
-        t.mock.fakeTimers.tick(500);
-        t.mock.fakeTimers.tick(500);
+        t.mock.timers.tick(1000);
+        t.mock.timers.tick(500);
+        t.mock.timers.tick(500);
 
         assert.strictEqual(fn.mock.callCount(), 1);
       });
@@ -71,14 +71,14 @@ describe('Faketimers Test Suite', () => {
   describe('timers/promises', () => {
     describe('setTimeout Suite', () => {
       it('should advance in time and trigger timers when calling the .tick function multiple times', async (t) => {
-        t.mock.fakeTimers.enable();
+        t.mock.timers.enable();
 
         const p = nodeTimersPromises.setTimeout(2000);
 
-        t.mock.fakeTimers.tick(1000);
-        t.mock.fakeTimers.tick(500);
-        t.mock.fakeTimers.tick(500);
-        t.mock.fakeTimers.tick(500);
+        t.mock.timers.tick(1000);
+        t.mock.timers.tick(500);
+        t.mock.timers.tick(500);
+        t.mock.timers.tick(500);
 
         p.then(common.mustCall((result) => assert.ok(result)));
       });
