eeaf8430481a9e908e12e308d0dde84cbe260d36
Shu-yu Guo <syg@chromium.org>
2023-05-05 17:09:16 -0400

    Update test/parallel/test-experimental-shared-value-conveyor.js
    Co-authored-by: Joyee Cheung <joyeec9h3@gmail.com>

--- test/parallel/test-experimental-shared-value-conveyor.js
+++ test/parallel/test-experimental-shared-value-conveyor.js
@@ -6,7 +6,9 @@ const common = require('../common');
 const assert = require('assert');
 const { Worker, isMainThread, parentPort } = require('worker_threads');
 
-if (isMainThread) {
+// Do not use isMainThread so that this test itself can be run inside a Worker.
+if (!process.env.HAS_STARTED_WORKER) {
+  process.env.HAS_STARTED_WORKER = 1;
   const m = new globalThis.SharedArray(16);
 
   const worker = new Worker(__filename);
