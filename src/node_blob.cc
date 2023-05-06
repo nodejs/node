e15cc4595a81515ff7901a4009d21f40ed97ddca
RafaelGSS <rafael.nunu@hotmail.com>
2023-05-06 15:28:12 -0300

    permission: handle fs.openAsBlob
    Refs: https://hackerone.com/bugs?subject=nodejs&report_id=1966492
    PR-URL: https://github.com/nodejs-private/node-private/pull/405
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    CVE-ID: CVE-2023-30583

--- src/node_blob.cc
+++ src/node_blob.cc
@@ -8,6 +8,7 @@
 #include "node_errors.h"
 #include "node_external_reference.h"
 #include "node_file.h"
+#include "permission/permission.h"
 #include "util.h"
 #include "v8.h"
 
@@ -85,6 +86,10 @@ void Concat(const FunctionCallbackInfo<Value>& args) {
 
 void BlobFromFilePath(const FunctionCallbackInfo<Value>& args) {
   Environment* env = Environment::GetCurrent(args);
+  BufferValue path(env->isolate(), args[0]);
+  CHECK_NOT_NULL(*path);
+  THROW_IF_INSUFFICIENT_PERMISSIONS(
+      env, permission::PermissionScope::kFileSystemRead, path.ToStringView());
   auto entry = DataQueue::CreateFdEntry(env, args[0]);
   if (entry == nullptr) {
     return THROW_ERR_INVALID_ARG_VALUE(env, "Unabled to open file as blob");
--- test/fixtures/permission/fs-read.js
+++ test/fixtures/permission/fs-read.js
@@ -249,3 +249,14 @@ const regularFile = __filename;
     resource: path.toNamespacedPath(blockedFile),
   }));
 }
+
+// fs.openAsBlob
+{
+  assert.throws(() => {
+    fs.openAsBlob(blockedFile);
+  }, common.expectsError({
+    code: 'ERR_ACCESS_DENIED',
+    permission: 'FileSystemRead',
+    resource: path.toNamespacedPath(blockedFile),
+  }));
+}
