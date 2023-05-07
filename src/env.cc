34d92ed88c814e05b4b619705c67b3df3999c52d
RafaelGSS <rafael.nunu@hotmail.com>
2023-05-07 13:22:32 -0300

    src,permission: restrict inspector when pm enabled
    PR-URL: https://github.com/nodejs-private/node-private/pull/410
    Refs: https://hackerone.com/bugs?subject=nodejs&report_id=1962701
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    CVE-ID: CVE-2023-30587

--- src/env.cc
+++ src/env.cc
@@ -806,9 +806,12 @@ Environment::Environment(IsolateData* isolate_data,
   if (options_->experimental_permission) {
     permission()->EnablePermissions();
     // If any permission is set the process shouldn't be able to neither
-    // spawn/worker nor use addons unless explicitly allowed by the user
+    // spawn/worker nor use addons or enable inspector
+    // unless explicitly allowed by the user
     if (!options_->allow_fs_read.empty() || !options_->allow_fs_write.empty()) {
       options_->allow_native_addons = false;
+      flags_ = flags_ | EnvironmentFlags::kNoCreateInspector;
+      permission()->Apply("*", permission::PermissionScope::kInspector);
       if (!options_->allow_child_process) {
         permission()->Apply("*", permission::PermissionScope::kChildProcess);
       }
--- src/node_worker.cc
+++ src/node_worker.cc
@@ -85,8 +85,13 @@ Worker::Worker(Environment* env,
                 Number::New(env->isolate(), static_cast<double>(thread_id_.id)))
       .Check();
 
-  inspector_parent_handle_ =
-      GetInspectorParentHandle(env, thread_id_, url.c_str(), name.c_str());
+  // Without this check, to use the permission model with
+  // workers (--allow-worker) one would need to pass --allow-inspector as well
+  if (env->permission()->is_granted(
+          node::permission::PermissionScope::kInspector)) {
+    inspector_parent_handle_ =
+        GetInspectorParentHandle(env, thread_id_, url.c_str(), name.c_str());
+  }
 
   argv_ = std::vector<std::string>{env->argv()[0]};
   // Mark this Worker object as weak until we actually start the thread.
