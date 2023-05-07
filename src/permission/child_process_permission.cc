30b71330084498e9496e1da3788953f57bb9f3d2
Daeyeon Jeong <daeyeon.dev@gmail.com>
2023-05-07 18:58:25 +0900

    src: clarify the parameter name in `Permission::Apply`
    This fixes confusing parameter names. They are references
    to set allow-permission.
    
    Signed-off-by: Daeyeon Jeong <daeyeon.dev@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/47874
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Debadree Chatterjee <debadree333@gmail.com>

---
+++ src/permission/child_process_permission.cc
@@ -0,0 +1,23 @@
+#include "child_process_permission.h"
+
+#include <string>
+#include <vector>
+
+namespace node {
+
+namespace permission {
+
+// Currently, ChildProcess manage a single state
+// Once denied, it's always denied
+void ChildProcessPermission::Apply(const std::string& allow,
+                                   PermissionScope scope) {
+  deny_all_ = true;
+}
+
+bool ChildProcessPermission::is_granted(PermissionScope perm,
+                                        const std::string_view& param) {
+  return deny_all_ == false;
+}
+
+}  // namespace permission
+}  // namespace node
