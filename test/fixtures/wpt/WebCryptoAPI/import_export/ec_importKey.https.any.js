ea8fd2dfe0a234c6ea1ad245cc05dfe833f94adc
Filip Skokan <panva.ip@gmail.com>
2023-05-10 18:42:06 +0200

    test,crypto: update WebCryptoAPI WPT
    PR-URL: https://github.com/nodejs/node/pull/47921
    Refs: https://github.com/nodejs/node/issues/47864
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/fixtures/wpt/WebCryptoAPI/import_export/ec_importKey.https.any.js
+++ test/fixtures/wpt/WebCryptoAPI/import_export/ec_importKey.https.any.js
@@ -85,13 +85,13 @@
                 });
 
                 // Next, test private keys
-                allValidUsages(vector.privateUsages, []).forEach(function(usages) {
-                    ['pkcs8', 'jwk'].forEach(function(format) {
-                        var algorithm = {name: vector.name, namedCurve: curve};
-                        var data = keyData[curve];
-
+                ['pkcs8', 'jwk'].forEach(function(format) {
+                    var algorithm = {name: vector.name, namedCurve: curve};
+                    var data = keyData[curve];
+                    allValidUsages(vector.privateUsages, []).forEach(function(usages) {
                         testFormat(format, algorithm, data, curve, usages, extractable);
                     });
+                    testEmptyUsages(format, algorithm, data, curve, extractable);
                 });
             });
 
@@ -134,6 +134,21 @@
                 }
             });
         }, "Good parameters: " + keySize.toString() + " bits " + parameterString(format, compressed, keyData, algorithm, extractable, usages));
+    }
+
+    // Test importKey with a given key format and other parameters but with empty usages.
+    // Should fail with SyntaxError
+    function testEmptyUsages(format, algorithm, data, keySize, extractable) {
+        const keyData = data[format];
+        const usages = [];
+        promise_test(function(test) {
+            return subtle.importKey(format, keyData, algorithm, extractable, usages).
+            then(function(key) {
+                assert_unreached("importKey succeeded but should have failed with SyntaxError");
+            }, function(err) {
+                assert_equals(err.name, "SyntaxError", "Should throw correct error, not " + err.name + ": " + err.message);
+            });
+        }, "Empty Usages: " + keySize.toString() + " bits " + parameterString(format, false, keyData, algorithm, extractable, usages));
     }
 
 
