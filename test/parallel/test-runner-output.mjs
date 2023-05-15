636822c70fd9f2a4e0d7b03538b67ec35f642a82
Moshe Atlow <moshe@atlow.co.il>
2023-05-15 08:33:33 +0300

    handle non-drained buffer

--- test/parallel/test-runner-output.mjs
+++ test/parallel/test-runner-output.mjs
@@ -45,6 +45,7 @@ const tests = [
   { name: 'test-runner/output/name_pattern_with_only.js' },
   { name: 'test-runner/output/unresolved_promise.js' },
   { name: 'test-runner/output/default_output.js', transform: specTransform, tty: true },
+  { name: 'test-runner/output/arbitrary-output.js' },
 ].map(({ name, tty, transform }) => ({
   name,
   fn: common.mustCall(async () => {
