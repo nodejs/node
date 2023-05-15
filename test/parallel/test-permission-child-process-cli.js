e22968ec37c7370f2c439eb2104890a07f184e55
Daeyeon Jeong <daeyeon.dev@gmail.com>
2023-05-16 00:10:45 +0900

    test: allow other permission tests to access env vars
    Signed-off-by: Daeyeon Jeong <daeyeon.dev@gmail.com>

--- test/parallel/test-permission-child-process-cli.js
+++ test/parallel/test-permission-child-process-cli.js
@@ -1,4 +1,4 @@
-// Flags: --experimental-permission --allow-fs-read=*
+// Flags: --experimental-permission --allow-env=* --allow-fs-read=*
 'use strict';
 
 const common = require('../common');
--- test/parallel/test-permission-fs-symlink-target-write.js
+++ test/parallel/test-permission-fs-symlink-target-write.js
@@ -1,4 +1,4 @@
-// Flags: --experimental-permission --allow-fs-read=* --allow-fs-write=* --allow-child-process
+// Flags: --experimental-permission --allow-env=* --allow-fs-read=* --allow-fs-write=* --allow-child-process
 'use strict';
 
 const common = require('../common');
@@ -36,6 +36,7 @@ fs.writeFileSync(path.join(readWriteFolder, 'file'), 'NO evil file contents');
     process.execPath,
     [
       '--experimental-permission',
+      '--allow-env=*',
       `--allow-fs-read=${file},${commonPathWildcard},${readOnlyFolder},${readWriteFolder}`,
       `--allow-fs-write=${readWriteFolder},${writeOnlyFolder}`,
       file,
