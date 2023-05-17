ca4a0e3717645c875eeb921086920744402f5759
Daeyeon Jeong <daeyeon.dev@gmail.com>
2023-05-17 15:24:07 +0900

    test: fix parsing test flags
    This removes replacing `_` with `-` in the flags defined.
    
    Signed-off-by: Daeyeon Jeong <daeyeon.dev@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/48012
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/common/index.js
+++ test/common/index.js
@@ -81,7 +81,6 @@ function parseTestFlags(filename = process.argv[1]) {
   }
   return source
     .substring(flagStart, flagEnd)
-    .replace(/_/g, '-')
     .split(/\s+/)
     .filter(Boolean);
 }
@@ -97,9 +96,8 @@ if (process.argv.length === 2 &&
     require('cluster').isPrimary &&
     fs.existsSync(process.argv[1])) {
   const flags = parseTestFlags();
-  const args = process.execArgv.map((arg) => arg.replace(/_/g, '-'));
   for (const flag of flags) {
-    if (!args.includes(flag) &&
+    if (!process.execArgv.includes(flag) &&
         // If the binary is build without `intl` the inspect option is
         // invalid. The test itself should handle this case.
         (process.features.inspector || !flag.startsWith('--inspect'))) {
