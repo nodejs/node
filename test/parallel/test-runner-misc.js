c723b82c80f86b0c60b9998635714a3652064bd8
Benjamin Gruenbaum <benjamingr@gmail.com>
2023-05-17 11:57:21 +0300

    test: fix suite signal
    PR-URL: https://github.com/nodejs/node/pull/47800
    Fixes: https://github.com/nodejs/node/issues/47882
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-runner-misc.js
+++ test/parallel/test-runner-misc.js
@@ -21,11 +21,19 @@ if (process.argv[2] === 'child') {
     })).finally(common.mustCall(() => {
       test(() => assert.strictEqual(testSignal.aborted, true));
     }));
+
+    // TODO(benjamingr) add more tests to describe + AbortSignal
+    // this just tests the parameter is passed
+    test.describe('Abort Signal in describe', common.mustCall(({ signal }) => {
+      test.it('Supports AbortSignal', () => {
+        assert.strictEqual(signal.aborted, false);
+      });
+    }));
   } else assert.fail('unreachable');
 } else {
   const child = spawnSync(process.execPath, [__filename, 'child', 'abortSignal']);
   const stdout = child.stdout.toString();
-  assert.match(stdout, /^# pass 1$/m);
+  assert.match(stdout, /^# pass 2$/m);
   assert.match(stdout, /^# fail 0$/m);
   assert.match(stdout, /^# cancelled 1$/m);
   assert.strictEqual(child.status, 1);
