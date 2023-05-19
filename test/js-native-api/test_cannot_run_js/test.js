d6f3232626d9c041a1ae8aebe8801ef0d6fcb468
Gabriel Schulhof <gabrielschulhof@gmail.com>
2023-05-19 07:52:21 -0700

    test: address nit

--- test/js-native-api/test_cannot_run_js/test.js
+++ test/js-native-api/test_cannot_run_js/test.js
@@ -4,8 +4,9 @@
 // mode and `napi_pending_exception` otherwise. This test calls the add-on's
 // `createRef()` method, which creates a strong reference to a JS function. When
 // the process exits, it calls all reference finalizers. The finalizer for the
-// strong reference created herein will attempt to call `napi_call_function()`
-// and will abort the process if the API doesn't return the correct status.
+// strong reference created herein will attempt to call `napi_get_property()` on
+// a property of the global object and will abort the process if the API doesn't
+// return the correct status.
 
 const { buildType, mustNotCall } = require('../../common');
 const addon_v8 = require(`./build/${buildType}/test_pending_exception`);
