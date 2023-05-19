b47e380287e0179c57dfb4f5641b3b360ae7ba12
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-05-19 11:42:42 +0300

    test_runner: add test and fix error

--- test/fixtures/test-runner/output/dot_output_custom_columns.js
+++ test/fixtures/test-runner/output/dot_output_custom_columns.js
@@ -4,7 +4,16 @@ process.stdout.columns = 30;
 
 require('../../../common');
 const test = require('node:test');
+const {setTimeout} = require('timers/promises');
 
 for (let i = 0; i < 100; i++) {
-  test(i + ' example', () => {});
+  test(i + ' example', async () => {
+    if(i === 0) {
+      // So the reporter will run before all tests has started
+      await setTimeout(10);
+    }
+    // resize
+    if (i === 28)
+      process.stdout.columns = 41;
+  });
 }
