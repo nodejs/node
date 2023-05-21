237b429c75785b4d949cbd68b60d61d0c6cff709
Alberto Moretti <moretti919@gmail.com>
2023-05-21 18:03:29 +0200

    fs: fix mkdirp return UV_EROFS on read-only fs
    Fixes: https://github.com/nodejs/node/issues/47098

--- src/node_file.cc
+++ src/node_file.cc
@@ -1616,6 +1616,7 @@ int MKDirpSync(uv_loop_t* loop,
         case UV_EACCES:
         case UV_ENOSPC:
         case UV_ENOTDIR:
+        case UV_EROFS:
         case UV_EPERM: {
           return err;
         }
@@ -1695,6 +1696,7 @@ int MKDirpAsync(uv_loop_t* loop,
         }
         case UV_EACCES:
         case UV_ENOTDIR:
+        case UV_EROFS:
         case UV_EPERM: {
           req_wrap->continuation_data()->Done(err);
           break;
