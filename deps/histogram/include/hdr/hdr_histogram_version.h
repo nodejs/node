b948b7419a3302330acd151657efa0572331705c
Marco Ippolito <marcoippolito54@gmail.com>
2023-05-22 17:50:16 +0200

    deps: update histogram 0.11.8
    PR-URL: https://github.com/nodejs/node/pull/47742
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- deps/histogram/include/hdr/hdr_histogram_version.h
+++ deps/histogram/include/hdr/hdr_histogram_version.h
@@ -1,11 +1,12 @@
 /**
- * @file hdr_histogram_version.h
- * @brief Definitions for HdrHistogram's version number.
+ * hdr_histogram_version.h
+ * Written by Marco Ippolito, Michael Barker and released to the public domain,
+ * as explained at http://creativecommons.org/publicdomain/zero/1.0/
  */
 
 #ifndef HDR_HISTOGRAM_VERSION_H
 #define HDR_HISTOGRAM_VERSION_H
 
-#define HDR_HISTOGRAM_VERSION "0.11.7"
+#define HDR_HISTOGRAM_VERSION "0.11.8"
 
 #endif  // HDR_HISTOGRAM_VERSION_H
