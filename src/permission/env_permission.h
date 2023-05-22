2be253facae1620dd9edbd934cbc94a43a0b489b
Daeyeon Jeong <daeyeon.dev@gmail.com>
2023-05-22 18:52:22 +0900

    dev: clean up env permission
    Signed-off-by: Daeyeon Jeong <daeyeon.dev@gmail.com>

--- src/permission/env_permission.h
+++ src/permission/env_permission.h
@@ -3,9 +3,9 @@
 
 #if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
 
-#include <memory>
 #include <string>
-#include "permission/permission_util.h"
+#include <unordered_set>
+#include "permission/permission_base.h"
 
 namespace node {
 
@@ -13,28 +13,13 @@ namespace permission {
 
 class EnvPermission final : public PermissionBase {
  public:
-  class Patterns final : public SearchTree {
-   public:
-    bool Insert(const std::string& s) override;
-    bool Lookup(const std::string_view& s,
-                bool when_empty_return = false) override;
-  };
-
-  EnvPermission()
-      : denied_patterns_(std::make_shared<Patterns>()),
-        granted_patterns_(std::make_shared<Patterns>()) {}
-
   void Apply(const std::string& allow, PermissionScope scope) override;
   bool is_granted(PermissionScope scope,
                   const std::string_view& param = "") override;
 
  private:
-  bool IsGranted(const std::string_view& param);
-  bool Apply(const std::shared_ptr<Patterns>& patterns,
-             const std::string_view& pattern);
-
-  std::shared_ptr<Patterns> denied_patterns_;
-  std::shared_ptr<Patterns> granted_patterns_;
+  bool allow_all_ = false;
+  std::unordered_set<std::string> allowed_;
 };
 
 }  // namespace permission
