9d338f73123d27ebc0d140fa6da007ba11113a33
Andrea Fassina <fasenderos@gmail.com>
2023-05-29 00:24:57 +0200

    tools: authenticated request if GITHUB_TOKEN exist

--- tools/dep_updaters/update-cjs-module-lexer.sh
+++ tools/dep_updaters/update-cjs-module-lexer.sh
@@ -11,11 +11,12 @@ DEPS_DIR="$BASE_DIR/deps"
 NPM="$DEPS_DIR/npm/bin/npm-cli.js"
 
 NEW_VERSION="$("$NODE" --input-type=module <<'EOF'
-const res = await fetch('https://api.github.com/repos/nodejs/cjs-module-lexer/tags', {
-  headers: {
-    "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
-  },
-});
+const res = await fetch('https://api.github.com/repos/nodejs/cjs-module-lexer/tags',
+  process.env.GITHUB_TOKEN && {
+    headers: {
+      "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
+    },
+  });
 if (!res.ok) throw new Error(`FetchError: ${res.status} ${res.statusText}`, { cause: res });
 const tags = await res.json();
 const { name } = tags.at(0)
