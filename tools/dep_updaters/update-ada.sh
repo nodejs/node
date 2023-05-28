9bfeedf3e9c2e843438b23321b5baf2c4881e879
Andrea Fassina <fasenderos@gmail.com>
2023-05-29 00:22:12 +0200

    Update tools/dep_updaters/update-ada.sh
    Co-authored-by: Mestery <mestery@protonmail.com>

--- tools/dep_updaters/update-ada.sh
+++ tools/dep_updaters/update-ada.sh
@@ -11,7 +11,7 @@ DEPS_DIR="$BASE_DIR/deps"
 . "$BASE_DIR/tools/dep_updaters/utils.sh"
 
 NEW_VERSION="$("$NODE" --input-type=module <<'EOF'
-const res = await fetch('https://api.github.com/repos/ada-url/ada/releases/latest', {
+const res = await fetch('https://api.github.com/repos/ada-url/ada/releases/latest', process.env.GITHUB_TOKEN && {
   headers: {
     "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
   },
