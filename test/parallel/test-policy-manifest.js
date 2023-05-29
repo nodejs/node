9ec6ee971211479f88dfbb78d87f5341f98f2d98
RafaelGSS <rafael.nunu@hotmail.com>
2023-05-29 16:40:15 -0300

    policy: handle mainModule.__proto__ bypass
    PR-URL: https://github.com/nodejs-private/node-private/pull/416
    Fixes: https://hackerone.com/bugs?subject=nodejs&report_id=1877919
    Reviewed-By: Rich Trott <rtrott@gmail.com>
    CVE-ID: CVE-2023-30581

--- test/parallel/test-policy-manifest.js
+++ test/parallel/test-policy-manifest.js
@@ -66,3 +66,18 @@ const fixtures = require('../common/fixtures.js');
 
   assert.strictEqual(result.status, 0);
 }
+
+{
+  const policyFilepath = fixtures.path('policy-manifest', 'onerror-exit.json');
+  const mainModuleBypass = fixtures.path('policy-manifest', 'main-module-proto-bypass.js');
+  const result = spawnSync(process.execPath, [
+    '--experimental-policy',
+    policyFilepath,
+    mainModuleBypass,
+  ]);
+
+  assert.notStrictEqual(result.status, 0);
+  const stderr = result.stderr.toString();
+  assert.match(stderr, /ERR_MANIFEST_DEPENDENCY_MISSING/);
+  assert.match(stderr, /does not list os as a dependency specifier for conditions: require, node, node-addons/);
+}
