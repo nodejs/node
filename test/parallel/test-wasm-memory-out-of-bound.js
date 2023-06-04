456342fd8f3b8f282e32a7d84e1c97fb3f47d630
Congcong Cai <congcongcai0907@163.com>
2023-06-04 10:01:47 +0200

    Update test/parallel/test-wasm-memory-out-of-bound.js
    Co-authored-by: James M Snell <jasnell@gmail.com>

--- test/parallel/test-wasm-memory-out-of-bound.js
+++ test/parallel/test-wasm-memory-out-of-bound.js
@@ -1,12 +1,12 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const fixtures = require('../common/fixtures');
 
 const buffer = fixtures.readSync('out-of-bound.wasm');
-WebAssembly.instantiate(buffer, {}).then((results) => {
+WebAssembly.instantiate(buffer, {}).then(common.mustCall((results) => {
   assert.throws(() => {
     results.instance.exports._start();
   }, WebAssembly.RuntimeError('memory access out of bounds'));
-});
+}));
