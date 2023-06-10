5400a6e994751b6267486fa753eab8f2949daf97
João Lenon <lenonSec7@gmail.com>
2023-06-10 11:35:14 -0300

    test: use `URL` instances for file test case
    As mentioned by @aduh95, all tests in
    `test-esm-loader-programmatically.mjs` are using string literals as
    arguments for `register` fn. Start using URL instances for file test
    case.

--- test/fixtures/es-module-loaders/register-loader.mjs
+++ test/fixtures/es-module-loaders/register-loader.mjs
@@ -1,4 +1,4 @@
 import { register } from 'node:module';
 import fixtures from '../../common/fixtures.js';
 
-register(fixtures.fileURL('es-module-loaders', 'loader-resolve-passthru.mjs').href);
+register(fixtures.fileURL('es-module-loaders', 'loader-resolve-passthru.mjs'));
