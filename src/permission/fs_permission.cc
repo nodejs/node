ed198bfc247556d8a2b1f4bb089695e93c29d1ad
Yagiz Nizipli <yagiz@nizipli.com>
2023-06-22 18:23:28 -0400

    src: refactor `SplitString` in util
    PR-URL: https://github.com/nodejs/node/pull/48491
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- src/permission/fs_permission.cc
+++ src/permission/fs_permission.cc
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <filesystem>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace {
@@ -74,8 +75,9 @@ namespace permission {
 // allow = '*'
 // allow = '/tmp/,/home/example.js'
 void FSPermission::Apply(const std::string& allow, PermissionScope scope) {
-  for (const auto& res : SplitString(allow, ',')) {
-    if (res == "*") {
+  using std::string_view_literals::operator""sv;
+  for (const std::string_view res : SplitString(allow, ","sv)) {
+    if (res == "*"sv) {
       if (scope == PermissionScope::kFileSystemRead) {
         deny_all_in_ = false;
         allow_all_in_ = true;
@@ -85,7 +87,7 @@ void FSPermission::Apply(const std::string& allow, PermissionScope scope) {
       }
       return;
     }
-    GrantAccess(scope, res);
+    GrantAccess(scope, std::string(res.data(), res.size()));
   }
 }
 
