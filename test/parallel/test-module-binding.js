b133e1c6bb11f298aca92c24cdd594ae3b66cfed
Yagiz Nizipli <yagiz@nizipli.com>
2023-06-22 20:04:54 -0400

    test: add more tests

--- test/parallel/test-module-binding.js
+++ test/parallel/test-module-binding.js
@@ -3,8 +3,10 @@
 require('../common');
 const fixtures = require('../common/fixtures');
 const { internalBinding } = require('internal/test/binding');
+const { filterOwnProperties } = require('internal/util');
 const { internalModuleReadJSON } = internalBinding('fs');
-const { strictEqual } = require('assert');
+const { readFileSync } = require('fs');
+const { strictEqual, deepStrictEqual } = require('assert');
 
 {
   strictEqual(internalModuleReadJSON('nosuchfile'), undefined);
@@ -14,4 +16,14 @@ const { strictEqual } = require('assert');
 }
 {
   strictEqual(internalModuleReadJSON(fixtures.path('empty-with-bom.txt')), '');
+}
+{
+  const filename = fixtures.path('require-bin/package.json');
+  const returnValue = JSON.parse(internalModuleReadJSON(filename));
+  const file = JSON.parse(readFileSync(filename, 'utf-8'));
+  const expectedValue = filterOwnProperties(file, ['name', 'main', 'exports', 'imports', 'type']);
+  deepStrictEqual({
+    __proto__: null,
+    ...returnValue,
+  }, expectedValue);
 }
