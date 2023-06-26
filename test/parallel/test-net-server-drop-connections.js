5263b8b7e4abd0ea7aca858b80936c009e536fc9
ignoramous <ignoramous@users.noreply.github.com>
2023-06-26 11:47:51 +0530

    net: do not treat `server.maxConnections=0` as `Infinity`
    Setting the `maxConnections` to 0 should result in no connection.
    Instead, it was treated as if the option was not there.
    
    PR-URL: https://github.com/nodejs/node/pull/48276
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Paolo Insogna <paolo@cowtech.it>

--- test/parallel/test-net-server-drop-connections.js
+++ test/parallel/test-net-server-drop-connections.js
@@ -4,11 +4,22 @@ const assert = require('assert');
 const net = require('net');
 
 let firstSocket;
+const dormantServer = net.createServer(common.mustNotCall());
 const server = net.createServer(common.mustCall((socket) => {
   firstSocket = socket;
 }));
 
+dormantServer.maxConnections = 0;
 server.maxConnections = 1;
+
+dormantServer.on('drop', common.mustCall((data) => {
+  assert.strictEqual(!!data.localAddress, true);
+  assert.strictEqual(!!data.localPort, true);
+  assert.strictEqual(!!data.remoteAddress, true);
+  assert.strictEqual(!!data.remotePort, true);
+  assert.strictEqual(!!data.remoteFamily, true);
+  dormantServer.close();
+}));
 
 server.on('drop', common.mustCall((data) => {
   assert.strictEqual(!!data.localAddress, true);
@@ -19,6 +30,10 @@ server.on('drop', common.mustCall((data) => {
   firstSocket.destroy();
   server.close();
 }));
+
+dormantServer.listen(0, () => {
+  net.createConnection(dormantServer.address().port);
+});
 
 server.listen(0, () => {
   net.createConnection(server.address().port);
