a499ec1a3b59b4ae936b6d0e4f0a25f9106a63c8
Tobias Nießen <tobias.niessen@tuwien.ac.at>
2023-07-03 16:31:00 +0200

    src: deduplicate X509 getter implementations
    Reorder arguments of internal helper functions such that their order is
    consistent across X509 property getters. Add ReturnPropertyThroughBIO()
    and ReturnProperty(). Use these new helpers to deduplicate code across
    various X509 property getters.
    
    PR-URL: https://github.com/nodejs/node/pull/48563
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    Reviewed-By: Alba Mendez <me@alba.sh>

--- src/crypto/crypto_common.h
+++ src/crypto/crypto_common.h
@@ -118,30 +118,26 @@ v8::MaybeLocal<v8::Value> GetCurrentCipherVersion(Environment* env,
 
 v8::MaybeLocal<v8::Value> GetSerialNumber(Environment* env, X509* cert);
 
-v8::MaybeLocal<v8::Object> GetRawDERCertificate(Environment* env, X509* cert);
+v8::MaybeLocal<v8::Value> GetRawDERCertificate(Environment* env, X509* cert);
 
 v8::Local<v8::Value> ToV8Value(Environment* env, const BIOPointer& bio);
 bool SafeX509SubjectAltNamePrint(const BIOPointer& out, X509_EXTENSION* ext);
 
-v8::MaybeLocal<v8::Value> GetSubject(
-    Environment* env,
-    const BIOPointer& bio,
-    X509* cert);
+v8::MaybeLocal<v8::Value> GetSubject(Environment* env,
+                                     X509* cert,
+                                     const BIOPointer& bio);
 
-v8::MaybeLocal<v8::Value> GetIssuerString(
-    Environment* env,
-    const BIOPointer& bio,
-    X509* cert);
+v8::MaybeLocal<v8::Value> GetIssuerString(Environment* env,
+                                          X509* cert,
+                                          const BIOPointer& bio);
 
-v8::MaybeLocal<v8::Value> GetSubjectAltNameString(
-    Environment* env,
-    const BIOPointer& bio,
-    X509* cert);
+v8::MaybeLocal<v8::Value> GetSubjectAltNameString(Environment* env,
+                                                  X509* cert,
+                                                  const BIOPointer& bio);
 
-v8::MaybeLocal<v8::Value> GetInfoAccessString(
-    Environment* env,
-    const BIOPointer& bio,
-    X509* cert);
+v8::MaybeLocal<v8::Value> GetInfoAccessString(Environment* env,
+                                              X509* cert,
+                                              const BIOPointer& bio);
 
 }  // namespace crypto
 }  // namespace node
