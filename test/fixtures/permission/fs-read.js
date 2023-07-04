530515b71be8cba69c37b2330f0e3db59aa523ed
RafaelGSS <rafael.nunu@hotmail.com>
2023-07-04 18:56:18 -0300

    permission: handle fstatfs and add pm supported list
    PR-URL: https://github.com/nodejs-private/node-private/pull/441
    CVE-ID: CVE-2023-32005

--- test/fixtures/permission/fs-read.js
+++ test/fixtures/permission/fs-read.js
@@ -260,3 +260,22 @@ const regularFile = __filename;
     resource: path.toNamespacedPath(blockedFile),
   }));
 }
+
+// fs.exists
+{
+  // It will return false (without performing IO) when permissions is not met
+  fs.exists(blockedFile, (exists) => {
+    assert.equal(exists, false);
+  });
+}
+
+// fs.statfs
+{
+  assert.throws(() => {
+    fs.statfs(blockedFile, () => {});
+  }, common.expectsError({
+    code: 'ERR_ACCESS_DENIED',
+    permission: 'FileSystemRead',
+    resource: path.toNamespacedPath(blockedFile),
+  }));
+}
