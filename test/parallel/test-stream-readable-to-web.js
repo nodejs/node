7da26c5f79037c996b2e3c3cfc4e57f136b819e5
CGQAQ <m.jason.liu@outlook.com>
2023-07-20 08:18:53 +0000

    stream: change comment

--- test/parallel/test-stream-readable-to-web.js
+++ test/parallel/test-stream-readable-to-web.js
@@ -5,12 +5,13 @@ const process = require('process');
 const fs = require('fs');
 const assert = require('assert');
 
+// Based on: https://github.com/nodejs/node/issues/46347#issuecomment-1413886707
 {
   let currentMemoryUsage = process.memoryUsage().arrayBuffers;
 
   // We initialize a stream, but not start consuming it
   const randomNodeStream = fs.createReadStream('/dev/urandom');
-  // after 10 seconds, it'll get converted to web stream
+  // after 2 seconds, it'll get converted to web stream
   let randomWebStream;
 
   // We check memory usage every second
@@ -23,13 +24,13 @@ const assert = require('assert');
   };
   setInterval(reportMemoryUsage, 1000);
 
-  // after 10 seconds we use Readable.toWeb
+  // after 1 second we use Readable.toWeb
   // memory usage should stay pretty much the same since it's still a stream
   setTimeout(() => {
     randomWebStream = Readable.toWeb(randomNodeStream);
   }, 1000);
 
-  // after 15 seconds we start consuming the stream
+  // after 2 seconds we start consuming the stream
   // memory usage will grow, but the old chunks should be garbage-collected pretty quickly
   setTimeout(async () => {
     // eslint-disable-next-line no-unused-vars
@@ -39,6 +40,7 @@ const assert = require('assert');
   }, 2000);
 
   setTimeout(() => {
+    // Test considered passed if we don't crash
     process.exit(0);
   }, 5000);
 }
