de2dc623927a9001a35de90c940d33681000f7ee
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-07-25 21:02:54 +0300

    test_runner: fix timeout in *Each hook failing further tests

---
+++ test/fixtures/test-runner/output/timeout-in-before-each-should-not-affect-further-tests.js
@@ -0,0 +1,46 @@
+const {describe, test, beforeEach, afterEach} = require("node:test");
+const {setTimeout} = require("timers/promises");
+
+
+describe('before each timeout', () => {
+  let i = 0;
+
+  beforeEach(async () => {
+    if (i++ === 0) {
+      console.log('gonna timeout')
+      await setTimeout(50);
+      return;
+    }
+    console.log('not gonna timeout');
+  }, {timeout: 20});
+
+  test('first describe first test', () => {
+    console.log('before each test first');
+  });
+
+  test('first describe second test', () => {
+    console.log('before each test second');
+  });
+});
+
+
+describe('after each timeout', () => {
+  let i = 0;
+
+  afterEach(async () => {
+    if (i++ === 0) {
+      console.log('gonna timeout')
+      await setTimeout(50);
+      return;
+    }
+    console.log('not gonna timeout');
+  }, {timeout: 20});
+
+  test('second describe first test', () => {
+    console.log('after each test first');
+  });
+
+  test('second describe second test', () => {
+    console.log('after each test second');
+  });
+});
