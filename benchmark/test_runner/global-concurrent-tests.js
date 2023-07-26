82e20f7e9544eea475d7c41a6d7bb9afa330388d
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-07-26 23:13:05 +0300

    sync and async tests

--- benchmark/test_runner/global-concurrent-tests.js
+++ benchmark/test_runner/global-concurrent-tests.js
@@ -4,24 +4,37 @@ const { it } = require('node:test');
 
 const bench = common.createBenchmark(main, {
   n: [100, 1000, 1e4],
+  type: ['sync', 'async']
 }, {
   // We don't want to test the reporter here
   flags: ['--test-reporter=./benchmark/fixtures/empty-test-reporter.js'],
 });
 
-async function run(n) {
+async function run(n, type) {
   const promises = new Array(n);
-  for (let i = 0; i < n; i++) {
-    promises[i] = it(`${i}`, () => {
-    });
+  switch (type) {
+    case 'sync':{
+      for (let i = 0; i < n; i++) {
+        promises[i] = it(`${i}`, () => {
+        });
+      }
+      break;
+    }
+
+    case 'async':
+      for (let i = 0; i < n; i++) {
+        promises[i] = it(`${i}`, async () => {
+        });
+      }
+      break;
   }
 
   await Promise.all(promises);
 }
 
-function main({ n }) {
+function main({ n, type }) {
   bench.start();
-  run(n).then(() => {
+  run(n, type).then(() => {
     bench.end(n);
   });
 }
