69853c95be881199c632068ad9fe6a607e819a73
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-07-28 11:06:51 +0300

    try fixing the failed test

--- test/fixtures/test-runner/output/timeout_in_before_each_should_not_affect_further_tests.js
+++ test/fixtures/test-runner/output/timeout_in_before_each_should_not_affect_further_tests.js
@@ -15,11 +15,11 @@ describe('before each timeout', () => {
   }, {timeout: 20});
 
   test('first describe first test', () => {
-    console.log('before each test first');
+    console.log('before each test first ' + i);
   });
 
   test('first describe second test', () => {
-    console.log('before each test second');
+    console.log('before each test second ' + i);
   });
 });
 
@@ -37,10 +37,10 @@ describe('after each timeout', () => {
   }, {timeout: 20});
 
   test('second describe first test', () => {
-    console.log('after each test first');
+    console.log('after each test first ' + i);
   });
 
   test('second describe second test', () => {
-    console.log('after each test second');
+    console.log('after each test second ' + i);
   });
 });
