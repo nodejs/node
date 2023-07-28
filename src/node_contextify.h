079a7399cee4d887cf4cec185c75a1a2b73a303d
Joyee Cheung <joyeec9h3@gmail.com>
2023-07-28 13:54:36 +0200

    vm: store MicrotaskQueue in ContextifyContext directly
    Previously the ContextifyContext holds a MicrotaskQueueWrap which in
    turns holds a MicrotaskQueue in a shared pointer. The indirection is
    actually unnecessary, we can directly hold the MicrotaskQueue via
    a unique pointer in ContextifyContext, the lifetime would still
    remain the same but the graph would be simpler, and this removes
    the additional JS -> C++ to create the wrapper object.

--- src/node_contextify.h
+++ src/node_contextify.h
@@ -12,34 +12,12 @@ class ExternalReferenceRegistry;
 
 namespace contextify {
 
-class MicrotaskQueueWrap : public BaseObject {
- public:
-  MicrotaskQueueWrap(Environment* env, v8::Local<v8::Object> obj);
-
-  const std::shared_ptr<v8::MicrotaskQueue>& microtask_queue() const;
-
-  static void CreatePerIsolateProperties(IsolateData* isolate_data,
-                                         v8::Local<v8::ObjectTemplate> target);
-  static void RegisterExternalReferences(ExternalReferenceRegistry* registry);
-  static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
-
-  // This could have methods for running the microtask queue, if we ever decide
-  // to make that fully customizable from userland.
-
-  SET_NO_MEMORY_INFO()
-  SET_MEMORY_INFO_NAME(MicrotaskQueueWrap)
-  SET_SELF_SIZE(MicrotaskQueueWrap)
-
- private:
-  std::shared_ptr<v8::MicrotaskQueue> microtask_queue_;
-};
-
 struct ContextOptions {
   v8::Local<v8::String> name;
   v8::Local<v8::String> origin;
   v8::Local<v8::Boolean> allow_code_gen_strings;
   v8::Local<v8::Boolean> allow_code_gen_wasm;
-  BaseObjectPtr<MicrotaskQueueWrap> microtask_queue_wrap;
+  std::unique_ptr<v8::MicrotaskQueue> own_microtask_queue;
 };
 
 class ContextifyContext : public BaseObject {
@@ -47,7 +25,7 @@ class ContextifyContext : public BaseObject {
   ContextifyContext(Environment* env,
                     v8::Local<v8::Object> wrapper,
                     v8::Local<v8::Context> v8_context,
-                    const ContextOptions& options);
+                    ContextOptions* options);
   ~ContextifyContext();
 
   void MemoryInfo(MemoryTracker* tracker) const override;
@@ -80,9 +58,8 @@ class ContextifyContext : public BaseObject {
         .As<v8::Object>();
   }
 
-  inline std::shared_ptr<v8::MicrotaskQueue> microtask_queue() const {
-    if (!microtask_queue_wrap_) return {};
-    return microtask_queue_wrap_->microtask_queue();
+  inline v8::MicrotaskQueue* microtask_queue() const {
+    return microtask_queue_.get();
   }
 
   template <typename T>
@@ -94,12 +71,12 @@ class ContextifyContext : public BaseObject {
  private:
   static BaseObjectPtr<ContextifyContext> New(Environment* env,
                                               v8::Local<v8::Object> sandbox_obj,
-                                              const ContextOptions& options);
+                                              ContextOptions* options);
   // Initialize a context created from CreateV8Context()
   static BaseObjectPtr<ContextifyContext> New(v8::Local<v8::Context> ctx,
                                               Environment* env,
                                               v8::Local<v8::Object> sandbox_obj,
-                                              const ContextOptions& options);
+                                              ContextOptions* options);
 
   static bool IsStillInitializing(const ContextifyContext* ctx);
   static void MakeContext(const v8::FunctionCallbackInfo<v8::Value>& args);
@@ -146,7 +123,7 @@ class ContextifyContext : public BaseObject {
       const v8::PropertyCallbackInfo<v8::Boolean>& args);
 
   v8::Global<v8::Context> context_;
-  BaseObjectPtr<MicrotaskQueueWrap> microtask_queue_wrap_;
+  std::unique_ptr<v8::MicrotaskQueue> microtask_queue_;
 };
 
 class ContextifyScript : public BaseObject {
@@ -171,7 +148,7 @@ class ContextifyScript : public BaseObject {
                           const bool display_errors,
                           const bool break_on_sigint,
                           const bool break_on_first_line,
-                          std::shared_ptr<v8::MicrotaskQueue> microtask_queue,
+                          v8::MicrotaskQueue* microtask_queue,
                           const v8::FunctionCallbackInfo<v8::Value>& args);
 
   inline uint32_t id() { return id_; }
