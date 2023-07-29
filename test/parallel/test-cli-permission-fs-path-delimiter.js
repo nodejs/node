bb4b1fd21b8b5c8cd9fd8d660ca688a53734cbd5
Carlos Espa <carlos.espa@nearform.com>
2023-07-29 11:00:01 +0200

    change test from file

--- test/parallel/test-cli-permission-fs-path-delimiter.js
+++ test/parallel/test-cli-permission-fs-path-delimiter.js
@@ -6,6 +6,30 @@ const path = require('path');
 
 {
   const tmpPath = path.resolve('/tmp/');
+  const otherPath = path.resolve('/other-path/');
+  const { status, stdout } = spawnSync(
+    process.execPath,
+    [
+      '--experimental-permission',
+      '--allow-fs-write', `${tmpPath},${otherPath}`, '-e',
+      `console.log(process.permission.has("fs"));
+      console.log(process.permission.has("fs.read"));
+      console.log(process.permission.has("fs.write"));
+      console.log(process.permission.has("fs.write", "/tmp/"));
+      console.log(process.permission.has("fs.write", "/other-path/"));`,
+    ]
+  );
+  const [fs, fsIn, fsOut, fsOutAllowed1, fsOutAllowed2] = stdout.toString().split('\n');
+  assert.strictEqual(fs, 'false');
+  assert.strictEqual(fsIn, 'false');
+  assert.strictEqual(fsOut, 'false');
+  assert.strictEqual(fsOutAllowed1, 'true');
+  assert.strictEqual(fsOutAllowed2, 'true');
+  assert.strictEqual(status, 0);
+}
+
+{
+  const tmpPath = path.resolve('/tmp/');
   const pathWithComma = path.resolve('/other,path/');
   const { status, stdout } = spawnSync(
     process.execPath,
