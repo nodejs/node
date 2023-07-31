67d7faddb2ec22c26317da401de0a1d871a2f0ea
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-07-31 11:10:36 +0300

    test_runner: cleanup test timeout abort listener
    fix #48475
    
    PR-URL: https://github.com/nodejs/node/pull/48915
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Chemi Atlow <chemi@atlow.co.il>

--- test/parallel/test-runner-output.mjs
+++ test/parallel/test-runner-output.mjs
@@ -37,6 +37,8 @@ const tests = [
   { name: 'test-runner/output/describe_nested.js' },
   { name: 'test-runner/output/hooks.js' },
   { name: 'test-runner/output/hooks-with-no-global-test.js' },
+  { name: 'test-runner/output/before-and-after-each-too-many-listeners.js' },
+  { name: 'test-runner/output/before-and-after-each-with-timeout-too-many-listeners.js' },
   { name: 'test-runner/output/no_refs.js' },
   { name: 'test-runner/output/no_tests.js' },
   { name: 'test-runner/output/only_tests.js' },
