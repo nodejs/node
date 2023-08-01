7b9adff0be92f13d72c67152bc3385f2b9bbfc9e
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-08-01 22:05:51 +0200

    src: do not pass user input to format string
    PR-URL: https://github.com/nodejs/node/pull/48973
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- src/node_file.cc
+++ src/node_file.cc
@@ -3079,10 +3079,11 @@ void BindingData::LegacyMainResolve(const FunctionCallbackInfo<Value>& args) {
     return;
   }
 
-  std::string err_module_message =
-      "Cannot find package '" + module_path + "' imported from " + module_base;
   env->isolate()->ThrowException(
-      ERR_MODULE_NOT_FOUND(env->isolate(), err_module_message.c_str()));
+      ERR_MODULE_NOT_FOUND(env->isolate(),
+                           "Cannot find package '%s' imported from %s",
+                           module_path,
+                           module_base));
 }
 
 void BindingData::MemoryInfo(MemoryTracker* tracker) const {
