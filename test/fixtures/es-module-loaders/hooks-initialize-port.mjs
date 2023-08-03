6ad8318373664e5d9c4f4133c9393c59c720f6a9
Izaak Schroeder <izaak.schroeder@gmail.com>
2023-08-02 22:10:59 -0700

    esm: add `initialize` hook, integrate with `register`
    Follows @giltayar's proposed API:
    
    > `register` can pass any data it wants to the loader, which will be
    passed to the exported `initialize` function of the loader.
    Additionally, if the user of `register` wants to communicate with the
    loader, it can just create a `MessageChannel` and pass the port to the
    loader as data.
    
    The `register` API is now:
    
    ```ts
    interface Options {
      parentUrl?: string;
      data?: any;
      transferList?: any[];
    }
    
    function register(loader: string, parentUrl?: string): any;
    function register(loader: string, options?: Options): any;
    ```
    
    This API is backwards compatible with the old one (new arguments are
    optional and at the end) and allows for passing data into the new
    `initialize` hook. If this hook returns data it is passed back to
    `register`:
    
    ```ts
    function initialize(data: any): Promise<any>;
    ```
    
    **NOTE**: Currently there is no mechanism for a loader to exchange
    ownership of something back to the caller.
    
    Refs: https://github.com/nodejs/loaders/issues/147
    PR-URL: https://github.com/nodejs/node/pull/48842
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>

---
+++ test/fixtures/es-module-loaders/hooks-initialize-port.mjs
@@ -0,0 +1,17 @@
+let thePort = null;
+
+export async function initialize(port) {
+  port.postMessage('initialize');
+  thePort = port;
+  return 'ok';
+}
+
+export async function resolve(specifier, context, next) {
+  if (specifier === 'node:fs' || specifier.includes('loader')) {
+    return next(specifier);
+  }
+
+  thePort.postMessage(`resolve ${specifier}`);
+
+  return next(specifier);
+}
