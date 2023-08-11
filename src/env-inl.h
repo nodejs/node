099159ce042cae3022d9050121752336df20b740
Daryl Haresign <github@daryl.haresign.com>
2023-08-10 20:43:51 -0400

    src,tools: initialize cppgc
    This patch:
    
    - Initializes cppgc in InitializeOncePerProcess() when
      kNoInitializeCppgc is not set
    - Create a CppHeap and attach it to the Isolate when
      there isn't one already during IsolateData initialization.
      The CppHeap is detached and terminated when IsolateData
      is freed.
    - Publishes the cppgc headers in the tarball.
    
    This allows C++ addons to start using cppgc to manage objects.
    
    A helper node::SetCppgcReference() is also added to help addons
    enable cppgc tracing in a user-defined object.
    
    Co-authored-by: Joyee Cheung <joyeec9h3@gmail.com>
    Refs: https://github.com/nodejs/node/issues/40786
    PR-URL: https://github.com/nodejs/node/pull/45704
    Backport-PR-URL: https://github.com/nodejs/node/pull/49187
    Refs: https://docs.google.com/document/d/1ny2Qz_EsUnXGKJRGxoA-FXIE2xpLgaMAN6jD7eAkqFQ/edit
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    PR-URL: https://github.com/nodejs/node/pull/48660
    Refs: https://github.com/v8/v8/commit/93275031284c66be7852b13f1b18a8bbe7f3a0a9

--- src/env-inl.h
+++ src/env-inl.h
@@ -34,6 +34,7 @@
 #include "node_realm-inl.h"
 #include "util-inl.h"
 #include "uv.h"
+#include "v8-cppgc.h"
 #include "v8.h"
 
 #include <cstddef>
@@ -59,6 +60,31 @@ inline v8::Isolate* IsolateData::isolate() const {
 
 inline uv_loop_t* IsolateData::event_loop() const {
   return event_loop_;
+}
+
+inline void IsolateData::SetCppgcReference(v8::Isolate* isolate,
+                                           v8::Local<v8::Object> object,
+                                           void* wrappable) {
+  v8::CppHeap* heap = isolate->GetCppHeap();
+  CHECK_NOT_NULL(heap);
+  v8::WrapperDescriptor descriptor = heap->wrapper_descriptor();
+  uint16_t required_size = std::max(descriptor.wrappable_instance_index,
+                                    descriptor.wrappable_type_index);
+  CHECK_GT(object->InternalFieldCount(), required_size);
+
+  uint16_t* id_ptr = nullptr;
+  {
+    Mutex::ScopedLock lock(isolate_data_mutex_);
+    auto it =
+        wrapper_data_map_.find(descriptor.embedder_id_for_garbage_collected);
+    CHECK_NE(it, wrapper_data_map_.end());
+    id_ptr = &(it->second->cppgc_id);
+  }
+
+  object->SetAlignedPointerInInternalField(descriptor.wrappable_type_index,
+                                           id_ptr);
+  object->SetAlignedPointerInInternalField(descriptor.wrappable_instance_index,
+                                           wrappable);
 }
 
 inline uint16_t* IsolateData::embedder_id_for_cppgc() const {
