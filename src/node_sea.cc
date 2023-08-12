844339d5174c33602dba317670e049de42ac4d39
pluris <zighart8456@naver.com>
2023-08-13 02:19:23 +0900

    src: remove duplicated code in `GenerateSingleExecutableBlob()`

--- src/node_sea.cc
+++ src/node_sea.cc
@@ -496,12 +496,6 @@ ExitCode GenerateSingleExecutableBlob(
   std::optional<std::string_view> optional_sv_code_cache;
   std::string code_cache;
   if (static_cast<bool>(config.flags & SeaFlags::kUseCodeCache)) {
-    std::optional<std::string> optional_code_cache =
-        GenerateCodeCache(config.main_path, main_script);
-    if (!optional_code_cache.has_value()) {
-      FPrintF(stderr, "Cannot generate V8 code cache\n");
-      return ExitCode::kGenericUserError;
-    }
     code_cache = optional_code_cache.value();
     optional_sv_code_cache = code_cache;
   }
