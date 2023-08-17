4ec39e927a08abeaeab604eec289f0a9eb53b7a0
Joyee Cheung <joyeec9h3@gmail.com>
2023-08-17 19:24:46 +0200

    bootstrap: do not generate code cache in an unfinalized isolate
    V8 now no longer supports serializing code cache in an isolate
    with unfinalized read-only space. So guard the code cache regeneration
    with the `is_building_snapshot()` flag. When the isolate is created
    for snapshot generation, the code cache is going to be serialized
    separately anyway, so there is no need to do it in the builtin loader.
    
    PR-URL: https://github.com/nodejs/node/pull/49108
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- src/node_builtins.h
+++ src/node_builtins.h
@@ -147,7 +147,8 @@ class NODE_EXTERN_PRIVATE BuiltinLoader {
       v8::Local<v8::Context> context,
       const char* id,
       std::vector<v8::Local<v8::String>>* parameters,
-      Result* result);
+      Realm* optional_realm);
+  void SaveCodeCache(const char* id, v8::Local<v8::Function> fn);
 
   static void RecordResult(const char* id,
                            BuiltinLoader::Result result,
