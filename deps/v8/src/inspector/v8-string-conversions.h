2060738e193a30dac9a4d87feddc569920366d63
Andrey Kosyakov <caseq@chromium.org>
2023-08-17 13:50:11 -0700

    deps: V8: cherry-pick 182d9c05e78b
    Original commit message:
    
        Define UChar as char16_t
    
        We used to have UChar defined as uint16_t which does not go along
        with STL these days if you try to have an std::basic_string<> of it,
        as there are no standard std::char_traits<> specialization for uint16_t.
    
        This switches UChar to char16_t where practical, introducing a few
        compatibility shims to keep CL size small, as (1) this would likely
        have to be back-ported and (2) crdtp extensively uses uint16_t for
        wide chars.
    
        Bug: b:296390693
        Change-Id: I66a32d8f0050915225b187de56896c26dd76163d
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4789966
        Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
        Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
        Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#89559}
    
    Refs: https://github.com/v8/v8/commit/182d9c05e78b1ddb1cb8242cd3628a7855a0336f
    PR-URL: https://github.com/nodejs/node/pull/58342
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>

--- deps/v8/src/inspector/v8-string-conversions.h
+++ deps/v8/src/inspector/v8-string-conversions.h
@@ -5,14 +5,16 @@
 #ifndef V8_INSPECTOR_V8_STRING_CONVERSIONS_H_
 #define V8_INSPECTOR_V8_STRING_CONVERSIONS_H_
 
+#include <uchar.h>
+
 #include <cstdint>
 #include <string>
 
 // Conversion routines between UT8 and UTF16, used by string-16.{h,cc}. You may
 // want to use string-16.h directly rather than these.
 namespace v8_inspector {
-std::basic_string<uint16_t> UTF8ToUTF16(const char* stringStart, size_t length);
-std::string UTF16ToUTF8(const uint16_t* stringStart, size_t length);
+std::basic_string<char16_t> UTF8ToUTF16(const char* stringStart, size_t length);
+std::string UTF16ToUTF8(const char16_t* stringStart, size_t length);
 }  // namespace v8_inspector
 
 #endif  // V8_INSPECTOR_V8_STRING_CONVERSIONS_H_
