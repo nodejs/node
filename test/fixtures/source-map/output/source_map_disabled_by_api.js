11c85ffa980af120f09ae617e45bbd6146b892f4
翠 / green <green@sapphi.red>
2023-08-17 17:42:12 +0900

    lib: add api to detect whether source-maps are enabled
    Add `process.sourceMapsEnabled` to detect
    whether source-maps are enabled.
    
    Fixes: https://github.com/nodejs/node/issues/46304
    PR-URL: https://github.com/nodejs/node/pull/46391
    Reviewed-By: Chemi Atlow <chemi@atlow.co.il>

--- test/fixtures/source-map/output/source_map_disabled_by_api.js
+++ test/fixtures/source-map/output/source_map_disabled_by_api.js
@@ -2,9 +2,12 @@
 
 'use strict';
 require('../../../common');
+const assert = require('assert');
 Error.stackTraceLimit = 5;
 
+assert.strictEqual(process.sourceMapsEnabled, true);
 process.setSourceMapsEnabled(false);
+assert.strictEqual(process.sourceMapsEnabled, false);
 
 try {
   require('../enclosing-call-site-min.js');
@@ -17,6 +20,7 @@ delete require.cache[require
 
 // Re-enable.
 process.setSourceMapsEnabled(true);
+assert.strictEqual(process.sourceMapsEnabled, true);
 
 try {
   require('../enclosing-call-site-min.js');
