107015b45d6e620b56f78e1f37b1173e55d2812b
Yiyun Lei <yiyunlei.dev@gmail.com>
2023-08-29 06:36:23 -0400

    test: migrate message source map tests from Python to JS
    Migrate the remaining source map tests in the `test/message` folder
    from Python to JS.
    
    Fixes: https://github.com/nodejs/node/issues/47707
    
    test: migrate message source map tests from Python to JS
    PR-URL: https://github.com/nodejs/node/pull/49238
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>

--- test/parallel/test-node-output-sourcemaps.mjs
+++ test/parallel/test-node-output-sourcemaps.mjs
@@ -28,9 +28,15 @@ describe('sourcemaps output', { concurrency: true }, () => {
   const tests = [
     { name: 'source-map/output/source_map_disabled_by_api.js' },
     { name: 'source-map/output/source_map_enabled_by_api.js' },
+    { name: 'source-map/output/source_map_enclosing_function.js' },
     { name: 'source-map/output/source_map_eval.js' },
     { name: 'source-map/output/source_map_no_source_file.js' },
+    { name: 'source-map/output/source_map_reference_error_tabs.js' },
+    { name: 'source-map/output/source_map_sourcemapping_url_string.js' },
+    { name: 'source-map/output/source_map_throw_catch.js' },
     { name: 'source-map/output/source_map_throw_first_tick.js' },
+    { name: 'source-map/output/source_map_throw_icu.js' },
+    { name: 'source-map/output/source_map_throw_set_immediate.js' },
   ];
   for (const { name, transform } of tests) {
     it(name, async () => {
