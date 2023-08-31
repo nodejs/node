2a35782809b39d1705793248b8dbfb9d3b9c835d
Tobias Nießen <tniessen@tnie.de>
2023-08-31 21:33:22 +0200

    test: simplify test-crypto-dh-group-setters
    I can't tell why the test was written that way in the first place, but
    it seems sufficient to check that setPrivateKey and setPublicKey are
    both undefined.
    
    Refs: https://github.com/nodejs/node-v0.x-archive/pull/2638
    Refs: https://github.com/nodejs/node/pull/11253
    PR-URL: https://github.com/nodejs/node/pull/49404
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-crypto-dh-group-setters.js
+++ test/parallel/test-crypto-dh-group-setters.js
@@ -6,21 +6,8 @@ if (!common.hasCrypto)
 const assert = require('assert');
 const crypto = require('crypto');
 
-assert.throws(
-  function() {
-    crypto.getDiffieHellman('modp1').setPrivateKey('');
-  },
-  new RegExp('^TypeError: crypto\\.getDiffieHellman\\(\\.\\.\\.\\)\\.' +
-  'setPrivateKey is not a function$'),
-  'crypto.getDiffieHellman(\'modp1\').setPrivateKey(\'\') ' +
-  'failed to throw the expected error.'
-);
-assert.throws(
-  function() {
-    crypto.getDiffieHellman('modp1').setPublicKey('');
-  },
-  new RegExp('^TypeError: crypto\\.getDiffieHellman\\(\\.\\.\\.\\)\\.' +
-  'setPublicKey is not a function$'),
-  'crypto.getDiffieHellman(\'modp1\').setPublicKey(\'\') ' +
-  'failed to throw the expected error.'
-);
+// Unlike DiffieHellman, DiffieHellmanGroup does not have any setters.
+const dhg = crypto.getDiffieHellman('modp1');
+assert.strictEqual(dhg.constructor, crypto.DiffieHellmanGroup);
+assert.strictEqual(dhg.setPrivateKey, undefined);
+assert.strictEqual(dhg.setPublicKey, undefined);
