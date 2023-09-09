4cb61da684de0243ac4418b38c111cb27e3b68a7
Vidar Eldøy <vidar@eldoy.com>
2023-09-10 00:06:53 +0200

    doc: save user preference for JS flavor
    PR-URL: https://github.com/nodejs/node/pull/49526
    Fixes: https://github.com/nodejs/node/issues/49508
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Chemi Atlow <chemi@atlow.co.il>

--- doc/api_assets/api.js
+++ doc/api_assets/api.js
@@ -136,6 +136,29 @@
     updateHashes();
   }
 
+  function setupFlavorToggles() {
+    const kFlavorPreference = 'customFlavor';
+    const flavorSetting = localStorage.getItem(kFlavorPreference) === 'true';
+    const flavorToggles = document.querySelectorAll('.js-flavor-toggle');
+
+    flavorToggles.forEach((toggleElement) => {
+      toggleElement.checked = flavorSetting;
+      toggleElement.addEventListener('change', (e) => {
+        const checked = e.target.checked;
+
+        if (checked) {
+          localStorage.setItem(kFlavorPreference, true);
+        } else {
+          localStorage.removeItem(kFlavorPreference);
+        }
+
+        flavorToggles.forEach((el) => {
+          el.checked = checked;
+        });
+      });
+    });
+  }
+
   function setupCopyButton() {
     const buttons = document.querySelectorAll('.copy-button');
     buttons.forEach((button) => {
@@ -181,6 +204,8 @@
 
     // Make link to other versions of the doc open to the same hash target (if it exists).
     setupAltDocsLink();
+
+    setupFlavorToggles();
 
     setupCopyButton();
   }
