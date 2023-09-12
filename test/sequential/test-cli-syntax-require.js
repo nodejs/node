6cfd64f8c773e2d52bee25acc63d485523ea206b
Joyee Cheung <joyeec9h3@gmail.com>
2023-09-12 19:02:51 +0200

    test: show more info on failure in test-cli-syntax-require.js
    Use spawnSyncAndExit() to show more info when the tes fails.
    
    PR-URL: https://github.com/nodejs/node/pull/49561
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Ruy Adorno <ruyadorno@google.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Nitzan Uziely <linkgoron@gmail.com>

--- test/sequential/test-cli-syntax-require.js
+++ test/sequential/test-cli-syntax-require.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const common = require('../common');
+require('../common');
 const assert = require('assert');
-const { spawn } = require('child_process');
+const { spawnSyncAndExit } = require('../common/child_process');
 const fixtures = require('../common/fixtures');
 
 const node = process.execPath;
@@ -17,25 +17,18 @@ const syntaxErrorRE = /^SyntaxError: \b/m;
     const preloadFile = fixtures.path('no-wrapper.js');
     const file = fixtures.path('syntax', 'illegal_if_not_wrapped.js');
     const args = [requireFlag, preloadFile, checkFlag, file];
-    const cp = spawn(node, args);
+    spawnSyncAndExit(node, args, {
+      status: 1,
+      signal: null,
+      trim: true,
+      stdout: '',
+      stderr(output) {
+        // stderr should have a syntax error message
+        assert.match(output, syntaxErrorRE);
 
-    // No stdout should be produced
-    cp.stdout.on('data', common.mustNotCall('stdout data event'));
-
-    const stderrBuffer = [];
-    cp.stderr.on('data', (chunk) => stderrBuffer.push(chunk));
-
-    cp.on('exit', common.mustCall((code, signal) => {
-      assert.strictEqual(code, 1);
-      assert.strictEqual(signal, null);
-
-      const stderr = Buffer.concat(stderrBuffer).toString('utf-8');
-      // stderr should have a syntax error message
-      assert.match(stderr, syntaxErrorRE);
-
-      // stderr should include the filename
-      assert(stderr.startsWith(file), `${stderr} starts with ${file}`);
-    }));
-
+        // stderr should include the filename
+        assert(output.startsWith(file), `${output} starts with ${file}`);
+      }
+    });
   });
 });
