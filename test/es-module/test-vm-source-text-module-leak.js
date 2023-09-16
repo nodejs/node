0b156c6d28062b3e36d365223e4526e2f42ee94d
Joyee Cheung <joyeec9h3@gmail.com>
2023-09-16 15:08:18 +0200

    test: use checkIfCollectable in vm leak tests
    Previously we simply create a lot of the target objects and check
    if the process crash due to OOM. Due to how we use emphemeron GC
    to handle memory management, which is inefficient but necessary
    for correctness, the tests can produce false positives as
    the GC isn't efficient enough to catch up with a very fast
    heap growth.
    
    This patch uses a new checkIfCollectable() utility to terminate the
    test early once we detect that any of the target object can actually
    be garbage collected. This should lower the chance of false positives.
    As a drive-by this also allows us to use setImmediate() to grow the
    heap even faster to make the tests run faster.
    
    PR-URL: https://github.com/nodejs/node/pull/49671
    Backport-PR-URL: https://github.com/nodejs/node/pull/51004
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/es-module/test-vm-source-text-module-leak.js
+++ test/es-module/test-vm-source-text-module-leak.js
@@ -4,20 +4,18 @@
 // This tests that vm.SourceTextModule() does not leak.
 // See: https://github.com/nodejs/node/issues/33439
 require('../common');
-
+const { checkIfCollectable } = require('../common/gc');
 const vm = require('vm');
-let count = 0;
-async function createModule() {
+
+async function createSourceTextModule() {
   // Try to reach the maximum old space size.
   const m = new vm.SourceTextModule(`
-  const bar = new Array(512).fill("----");
-  export { bar };
-`);
+    const bar = new Array(512).fill("----");
+    export { bar };
+  `);
   await m.link(() => {});
   await m.evaluate();
-  if (count++ < 4096) {
-    setTimeout(createModule, 1);
-  }
   return m;
 }
-createModule();
+
+checkIfCollectable(createSourceTextModule, 4096, 1024);
