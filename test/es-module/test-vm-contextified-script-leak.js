355f10dab2110a24373677422317d41dc8c50294
Joyee Cheung <joyeec9h3@gmail.com>
2023-09-16 15:08:18 +0200

    test: use checkIfCollectable in vm leak tests
    Previously we simply create a lot of the target objects and check
    if the process crash due to OOM. Due to how we use emphemeron GC
    to handle memory management, which is inefficient but necessary
    for correctness, the tests can produce false positives as
    the GC isn't efficient enough to catch up with a very fast
    heap growth.
    
    This patch uses a new checkIfCollectable() utility to terminate the
    test early once we detect that any of the target object can actually
    be garbage collected. This should lower the chance of false positives.
    As a drive-by this also allows us to use setImmediate() to grow the
    heap even faster to make the tests run faster.
    
    Backport-PR-URL: https://github.com/nodejs/node/pull/49874
    PR-URL: https://github.com/nodejs/node/pull/49671
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/es-module/test-vm-contextified-script-leak.js
+++ test/es-module/test-vm-contextified-script-leak.js
@@ -4,16 +4,13 @@
 // This tests that vm.Script with dynamic import callback does not leak.
 // See: https://github.com/nodejs/node/issues/33439
 require('../common');
+const { checkIfCollectable } = require('../common/gc');
 const vm = require('vm');
-let count = 0;
 
-function main() {
+async function createContextifyScript() {
   // Try to reach the maximum old space size.
-  new vm.Script(`"${Math.random().toString().repeat(512)}";`, {
+  return new vm.Script(`"${Math.random().toString().repeat(512)}";`, {
     async importModuleDynamically() {},
   });
-  if (count++ < 2 * 1024) {
-    setTimeout(main, 1);
-  }
 }
-main();
+checkIfCollectable(createContextifyScript, 2048);
