1b8bf94095575b09e41affbf130557975230efb9
Joyee Cheung <joyeec9h3@gmail.com>
2023-09-16 15:08:18 +0200

    test: use checkIfCollectable in vm leak tests
    Previously we simply create a lot of the target objects and check
    if the process crash due to OOM. Due to how we use emphemeron GC
    to handle memory management, which is inefficient but necessary
    for correctness, the tests can produce false positives as
    the GC isn't efficient enough to catch up with a very fast
    heap growth.
    
    This patch uses a new checkIfCollectable() utility to terminate the
    test early once we detect that any of the target object can actually
    be garbage collected. This should lower the chance of false positives.
    As a drive-by this also allows us to use setImmediate() to grow the
    heap even faster to make the tests run faster.
    
    PR-URL: https://github.com/nodejs/node/pull/49671
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/es-module/test-vm-compile-function-leak.js
+++ test/es-module/test-vm-compile-function-leak.js
@@ -4,16 +4,13 @@
 // This tests that vm.compileFunction with dynamic import callback does not leak.
 // See https://github.com/nodejs/node/issues/44211
 require('../common');
+const { checkIfCollectable } = require('../common/gc');
 const vm = require('vm');
-let count = 0;
 
-function main() {
-  // Try to reach the maximum old space size.
-  vm.compileFunction(`"${Math.random().toString().repeat(512)}"`, [], {
+async function createCompiledFunction() {
+  return vm.compileFunction(`"${Math.random().toString().repeat(512)}"`, [], {
     async importModuleDynamically() {},
   });
-  if (count++ < 2048) {
-    setTimeout(main, 1);
-  }
 }
-main();
+
+checkIfCollectable(createCompiledFunction, 2048);
