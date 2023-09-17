7b2cfac4fc18a36e108c58256dfc188c86d94fd0
Maël Nison <nison.mael@gmail.com>
2023-09-17 13:26:39 +0200

    test: add a cwd cache bust test

---
+++ test/parallel/test-snapshot-cwd.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// This tests that user land snapshots works when the instance restored from
+// the snapshot is launched with -p and -e
+
+require('../common');
+const assert = require('assert');
+const { spawnSync } = require('child_process');
+const tmpdir = require('../common/tmpdir');
+const fixtures = require('../common/fixtures');
+const fs = require('fs');
+const path = require('path');
+
+tmpdir.refresh();
+const blobPath = tmpdir.resolve('snapshot.blob');
+const file = fixtures.path('snapshot', 'cwd.js');
+
+const subdir = path.join(tmpdir.path, 'foo');
+fs.mkdirSync(subdir);
+
+{
+  // Create the snapshot.
+  const child = spawnSync(process.execPath, [
+    '--snapshot-blob',
+    blobPath,
+    '--build-snapshot',
+    file,
+  ], {
+    cwd: tmpdir.path,
+    encoding: `utf8`
+  });
+
+  assert.strictEqual(child.status, 0);
+}
+
+{
+  // Check a custom works.
+  const child = spawnSync(process.execPath, [
+    '--snapshot-blob',
+    blobPath,
+    file,
+  ], {
+    cwd: subdir,
+    encoding: `utf8`
+  });
+
+  assert.strictEqual(child.status, 0);
+  assert.strictEqual(child.stdout, `${subdir}\n`);
+}
