ea22273e605b883b2fc68ee4c0fa240a88e23a85
Tobias Nießen <tniessen@tnie.de>
2023-09-19 16:01:49 +0000

    deps: disable io_uring support in libuv by default
    setuid() does not affect libuv's internal io_uring operations if
    initialized before the call to setuid(). This potentially allows the
    process to perform privileged operations despite presumably having
    dropped such privileges through a call to setuid(). Similar concerns
    apply to other functions that modify the process's user identity.
    
    This commit changes libuv's io_uring behavior from opt-out (through
    UV_USE_IO_URING=0) to opt-in (through UV_USE_IO_URING=1) until we figure
    out a better long-term solution.
    
    PR-URL: https://github.com/nodejs-private/node-private/pull/528
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    CVE-ID: CVE-2024-22017

--- deps/uv/src/unix/linux.c
+++ deps/uv/src/unix/linux.c
@@ -487,8 +487,8 @@ static int uv__use_io_uring(void) {
   use = atomic_load_explicit(&use_io_uring, memory_order_relaxed);
 
   if (use == 0) {
-    /* Older kernels have a bug where the sqpoll thread uses 100% CPU. */
-    use = uv__kernel_version() >= /* 5.10.186 */ 0x050ABA ? 1 : -1;
+    /* Disable io_uring by default due to CVE-2024-22017. */
+    use = -1;
 
     /* But users can still enable it if they so desire. */
     val = getenv("UV_USE_IO_URING");
