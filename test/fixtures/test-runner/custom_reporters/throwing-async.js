aebf089c17c7d1c564749346bfff62e4e6090d3a
Moshe Atlow <moshe@atlow.co.il>
2023-09-21 20:06:30 +0300

    Apply suggestions from code review
    Co-authored-by: Colin Ihrig <cjihrig@gmail.com>

--- test/fixtures/test-runner/custom_reporters/throwing-async.js
+++ test/fixtures/test-runner/custom_reporters/throwing-async.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = async function * customReporter() {
-  yield "Going to throw an error\n";
+  yield 'Going to throw an error\n';
   setImmediate(() => {
     throw new Error('Reporting error');
   });
