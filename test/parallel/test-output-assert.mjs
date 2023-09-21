fc0e7e06fa178a5a0a7437823864b071bfccbce9
Abd-allah khateeb <55080308+akhateeb22@users.noreply.github.com>
2023-09-21 07:41:16 +0300

    test: refactoring

--- test/parallel/test-output-assert.mjs
+++ test/parallel/test-output-assert.mjs
@@ -6,25 +6,25 @@ import { describe, it } from 'node:test';
 function replaceStackTrace(str) {
   return snapshot.replaceStackTrace(str, '$1at *$7\n');
 }
+function normalize(str) {
+  return str
+    .replaceAll(snapshot.replaceWindowsPaths(process.cwd()), '')
+    .replaceAll('/', '*')
+    .replaceAll(process.version, '*')
+    .replaceAll(/:\d+/g, '*');
+}
+const common = snapshot
+  .transform(snapshot.replaceWindowsLineEndings, snapshot.replaceWindowsPaths);
+
+const defaultTransform = snapshot.transform(common, replaceStackTrace, normalize);
+
+const tests = [
+  { name: 'assert/assert_throws_stack.js' },
+  { name: 'assert/internal_assert_fail.js' },
+  { name: 'assert/internal_assert.js' },
+];
 
 describe('assertion output', { concurrency: true }, () => {
-  function normalize(str) {
-    return str
-      .replaceAll(snapshot.replaceWindowsPaths(process.cwd()), '')
-      .replaceAll('/', '*')
-      .replaceAll(process.version, '*')
-      .replaceAll(/:\d+/g, '*');
-  }
-
-  const common = snapshot
-    .transform(snapshot.replaceWindowsLineEndings, snapshot.replaceWindowsPaths);
-  const defaultTransform = snapshot.transform(common, replaceStackTrace, normalize);
-
-  const tests = [
-    { name: 'assert/assert_throws_stack.js' },
-    { name: 'assert/internal_assert.js' },
-  ];
-
   for (const { name, transform } of tests) {
     it(name, async () => {
       await snapshot.spawnAndAssert(fixtures.path(name), transform ?? defaultTransform);
