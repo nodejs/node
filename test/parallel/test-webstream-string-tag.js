8780419f74b4abe0316b60fd14121ae0e9cadcad
chaks64 <shahchintan64@gmail.com>
2023-09-22 11:49:22 -0700

    Replaced forEach with for loop keeping all other things same.

--- test/parallel/test-webstream-string-tag.js
+++ test/parallel/test-webstream-string-tag.js
@@ -4,15 +4,40 @@ require('../common');
 
 const assert = require('assert');
 
-const classesToBeTested = [ WritableStream, WritableStreamDefaultWriter, WritableStreamDefaultController,
-                            ReadableStream, ReadableStreamBYOBRequest, ReadableStreamDefaultReader,
-                            ReadableStreamBYOBReader, ReadableStreamDefaultController, ReadableByteStreamController,
-                            ByteLengthQueuingStrategy, CountQueuingStrategy, TransformStream,
-                            TransformStreamDefaultController];
+const classesToBeTested = [
+  WritableStream,
+  WritableStreamDefaultWriter,
+  WritableStreamDefaultController,
+  ReadableStream,
+  ReadableStreamBYOBRequest,
+  ReadableStreamDefaultReader,
+  ReadableStreamBYOBReader,
+  ReadableStreamDefaultController,
+  ReadableByteStreamController,
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy,
+  TransformStream,
+  TransformStreamDefaultController,
+];
 
+/* Commenting forEach Loop as per the document */
+// classesToBeTested.forEach((cls) => {
+//   assert.strictEqual(cls.prototype[Symbol.toStringTag], cls.name);
+//   assert.deepStrictEqual(Object.getOwnPropertyDescriptor(cls.prototype, Symbol.toStringTag),
+//                          { configurable: true, enumerable: false, value: cls.name, writable: false });
+// });
 
-classesToBeTested.forEach((cls) => {
+/* Adding for loop with all the test included in the above code */
+for (let i = 0; i < classesToBeTested.length; i++) {
+  const cls = classesToBeTested[i];
   assert.strictEqual(cls.prototype[Symbol.toStringTag], cls.name);
-  assert.deepStrictEqual(Object.getOwnPropertyDescriptor(cls.prototype, Symbol.toStringTag),
-                         { configurable: true, enumerable: false, value: cls.name, writable: false });
-});
+  assert.deepStrictEqual(
+    Object.getOwnPropertyDescriptor(cls.prototype, Symbol.toStringTag),
+    {
+      configurable: true,
+      enumerable: false,
+      value: cls.name,
+      writable: false,
+    }
+  );
+}
