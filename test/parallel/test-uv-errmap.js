11bba3f362a8885ee38cde2c015bbbd19148781b
Dwiti Pandya <dwitipandya@microsoft.com>
2023-09-22 19:58:26 +0100

    test: Replace forEach() with for ... of in test-uv-errmap.js
    Replaced the forEach() loop with a for...of loop in test-uv-errmap.js file as part of the GHC Open Source Day activities

--- test/parallel/test-uv-errmap.js
+++ test/parallel/test-uv-errmap.js
@@ -3,10 +3,7 @@
 
 require('../common');
 const assert = require('assert');
-const {
-  getSystemErrorMap,
-  _errnoException
-} = require('util');
+const { getSystemErrorMap, _errnoException } = require('util');
 
 const { internalBinding } = require('internal/test/binding');
 const uv = internalBinding('uv');
@@ -14,11 +11,10 @@ const uvKeys = Object.keys(uv);
 
 const errMap = getSystemErrorMap();
 
-uvKeys.forEach((key) => {
-  if (!key.startsWith('UV_'))
-    return;
+for (key of uvKeys) {
+  if (!key.startsWith('UV_')) return;
 
   const err = _errnoException(uv[key]);
   const name = uv.errname(uv[key]);
   assert.strictEqual(errMap.get(err.errno)[0], name);
-});
+}
