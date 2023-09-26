d0939f01b42b94e6e02859fc3e9fcb281f019e46
Geoffrey Booth <webadmin@geoffreybooth.com>
2023-09-25 20:40:17 -0700

    Review notes

--- src/node_options.h
+++ src/node_options.h
@@ -117,8 +117,8 @@ class EnvironmentOptions : public Options {
   bool experimental_https_modules = false;
   bool experimental_wasm_modules = false;
   bool experimental_import_meta_resolve = false;
-  std::string input_type;
-  std::string type;
+  std::string input_type;  // Value of --input-type
+  std::string type;        // Value of --experimental-type
   std::string experimental_policy;
   std::string experimental_policy_integrity;
   bool has_policy_integrity_string = false;
