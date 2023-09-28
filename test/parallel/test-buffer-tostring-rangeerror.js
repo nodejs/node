c954b18ba1f9f766873cc5d026ea971b6e2e2b20
Michaël Zasso <targos@protonmail.com>
2023-09-28 14:50:20 +0200

    lib,test: do not hardcode Buffer.kMaxLength
    V8 will soon support typed arrays as large as the maximum array buffer
    length. This patch replaces hardcoded values related to
    Buffer.kMaxLength with the actual constant.
    It also fixes a test that was passing by accident.
    
    Refs: https://github.com/v8/v8/commit/44b299590083b888637c79fb5632806e607ab861
    PR-URL: https://github.com/nodejs/node/pull/49876
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- test/parallel/test-buffer-tostring-rangeerror.js
+++ test/parallel/test-buffer-tostring-rangeerror.js
@@ -1,17 +1,22 @@
 'use strict';
 require('../common');
 
-// This test ensures that Node.js throws a RangeError when trying to convert a
-// gigantic buffer into a string.
+// This test ensures that Node.js throws an Error when trying to convert a
+// large buffer into a string.
 // Regression test for https://github.com/nodejs/node/issues/649.
 
 const assert = require('assert');
-const SlowBuffer = require('buffer').SlowBuffer;
+const {
+  SlowBuffer,
+  constants: {
+    MAX_STRING_LENGTH,
+  },
+} = require('buffer');
 
-const len = 1422561062959;
+const len = MAX_STRING_LENGTH + 1;
 const message = {
-  code: 'ERR_OUT_OF_RANGE',
-  name: 'RangeError',
+  code: 'ERR_STRING_TOO_LONG',
+  name: 'Error',
 };
 assert.throws(() => Buffer(len).toString('utf8'), message);
 assert.throws(() => SlowBuffer(len).toString('utf8'), message);
