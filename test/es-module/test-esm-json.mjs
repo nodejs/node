a74d0436905c6311cece2174ae5ec5de870a6c6a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-09-29 09:46:32 +0100

    esm: fix cache collision on JSON files using file: URL
    PR-URL: https://github.com/nodejs/node/pull/49887
    Fixes: https://github.com/nodejs/node/issues/49724
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>
    Reviewed-By: LiviaMedeiros <livia@cirno.name>
    Reviewed-By: Jacob Smith <jacob@frende.me>
    Reviewed-By: Chemi Atlow <chemi@atlow.co.il>

--- test/es-module/test-esm-json.mjs
+++ test/es-module/test-esm-json.mjs
@@ -2,7 +2,10 @@ import { spawnPromisified } from '../common/index.mjs';
 import * as fixtures from '../common/fixtures.mjs';
 import assert from 'node:assert';
 import { execPath } from 'node:process';
-import { describe, it } from 'node:test';
+import { describe, it, test } from 'node:test';
+
+import { mkdir, rm, writeFile } from 'node:fs/promises';
+import * as tmpdir from '../common/tmpdir.js';
 
 import secret from '../fixtures/experimental.json' assert { type: 'json' };
 
@@ -20,5 +23,62 @@ describe('ESM: importing JSON', () => {
     assert.match(stderr, /ExperimentalWarning: Import assertions/);
     assert.strictEqual(code, 0);
     assert.strictEqual(signal, null);
+  });
+
+  test('should load different modules when the URL is different', async (t) => {
+    const root = tmpdir.fileURL(`./test-esm-json-${Math.random()}/`);
+    try {
+      await mkdir(root, { recursive: true });
+
+      await t.test('json', async () => {
+        let i = 0;
+        const url = new URL('./foo.json', root);
+        await writeFile(url, JSON.stringify({ id: i++ }));
+        const absoluteURL = await import(`${url}`, {
+          assert: { type: 'json' },
+          });
+        await writeFile(url, JSON.stringify({ id: i++ }));
+        const queryString = await import(`${url}?a=2`, {
+          assert: { type: 'json' },
+          });
+        await writeFile(url, JSON.stringify({ id: i++ }));
+        const hash = await import(`${url}#a=2`, {
+          assert: { type: 'json' },
+          });
+        await writeFile(url, JSON.stringify({ id: i++ }));
+        const queryStringAndHash = await import(`${url}?a=2#a=2`, {
+          assert: { type: 'json' },
+          });
+
+        assert.notDeepStrictEqual(absoluteURL, queryString);
+        assert.notDeepStrictEqual(absoluteURL, hash);
+        assert.notDeepStrictEqual(queryString, hash);
+        assert.notDeepStrictEqual(absoluteURL, queryStringAndHash);
+        assert.notDeepStrictEqual(queryString, queryStringAndHash);
+        assert.notDeepStrictEqual(hash, queryStringAndHash);
+      });
+
+      await t.test('js', async () => {
+        let i = 0;
+        const url = new URL('./foo.mjs', root);
+        await writeFile(url, `export default ${JSON.stringify({ id: i++ })}\n`);
+        const absoluteURL = await import(`${url}`);
+        await writeFile(url, `export default ${JSON.stringify({ id: i++ })}\n`);
+        const queryString = await import(`${url}?a=1`);
+        await writeFile(url, `export default ${JSON.stringify({ id: i++ })}\n`);
+        const hash = await import(`${url}#a=1`);
+        await writeFile(url, `export default ${JSON.stringify({ id: i++ })}\n`);
+        const queryStringAndHash = await import(`${url}?a=1#a=1`);
+
+        assert.notDeepStrictEqual(absoluteURL, queryString);
+        assert.notDeepStrictEqual(absoluteURL, hash);
+        assert.notDeepStrictEqual(queryString, hash);
+        assert.notDeepStrictEqual(absoluteURL, queryStringAndHash);
+        assert.notDeepStrictEqual(queryString, queryStringAndHash);
+        assert.notDeepStrictEqual(hash, queryStringAndHash);
+      });
+    } finally {
+      await rm(root, { force: true, recursive: true });
+    }
   });
 });
