4c4b008e5d6c70b65f2b18c9237b8d36eb5557af
Tony Rippy <trippy@twosigma.com>
2023-09-29 14:16:16 -0400

    tools: exempt FastApiCallbackOptions from runtime/references lint check
    The non-const reference is required by a V8 function signature.
    
    Fixes: nodejs/node#45761

--- src/node_wasi.cc
+++ src/node_wasi.cc
@@ -241,7 +241,6 @@ template <typename FT, FT F, typename R, typename... Args>
 R WASI::WasiFunction<FT, F, R, Args...>::FastCallback(
     Local<Object> receiver,
     Args... args,
-    // NOLINTNEXTLINE(runtime/references) This is V8 api.
     FastApiCallbackOptions& options) {
   WASI* wasi = reinterpret_cast<WASI*>(BaseObject::FromJSObject(receiver));
   if (UNLIKELY(wasi == nullptr)) return EinvalError<R>();
