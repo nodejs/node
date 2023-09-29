38879702966e6556a3954fcc5a64848b672404fd
Geoffrey Booth <webadmin@geoffreybooth.com>
2023-09-29 09:24:31 -0700

    Apply suggestions from code review
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-process-argv1.mjs
+++ test/parallel/test-process-argv1.mjs
@@ -6,14 +6,19 @@ import { dirname } from 'node:path';
 
 describe('process.argv1', { concurrency: true }, () => {
   it('should be the string for the entry point before the entry was resolved into an absolute path', async () => {
-    const fixtureAbsolutePath = fixtures.path('process-argv1.js');
     const fixtureRelativePath = './process-argv1.js';
+    const fixtureAbsolutePath = fixtures.path(fixtureRelativePath);
+
     const { code, signal, stdout, stderr } = await spawnPromisified(process.execPath, [fixtureRelativePath], {
       cwd: dirname(fixtureAbsolutePath),
     });
 
     strictEqual(stderr, '');
-    strictEqual(stdout, `${fixtureRelativePath}\n${fixtureAbsolutePath}\n`);
+    deepStrictEqual(stdout.split('\n'), [
+      fixtureRelativePath,
+      fixtureAbsolutePath,
+      '',
+    ]);
     strictEqual(code, 0);
     strictEqual(signal, null);
   });
