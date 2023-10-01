97b1966a2a2daa53f512e6d041ea6d31f147fb2b
Geoffrey Booth <webadmin@geoffreybooth.com>
2023-09-30 20:38:24 -0700

    esm: bypass CommonJS loader under --default-type

--- test/es-module/test-esm-type-flag-errors.mjs
+++ test/es-module/test-esm-type-flag-errors.mjs
@@ -3,6 +3,36 @@ import * as fixtures from '../common/fixtures.mjs';
 import { describe, it } from 'node:test';
 import { match, strictEqual } from 'node:assert';
 
+describe('--experimental-default-type=module should not support extension searching', { concurrency: true }, () => {
+  it('should support extension searching under --experimental-default-type=commonjs', async () => {
+    const { code, signal, stdout, stderr } = await spawnPromisified(process.execPath, [
+      '--experimental-default-type=commonjs',
+      './index',
+    ], {
+      cwd: fixtures.path('es-modules/package-without-type'),
+    });
+
+    strictEqual(stdout, 'package-without-type\n');
+    strictEqual(stderr, '');
+    strictEqual(code, 0);
+    strictEqual(signal, null);
+  });
+
+  it('should error with implicit extension under --experimental-default-type=module', async () => {
+    const { code, signal, stdout, stderr } = await spawnPromisified(process.execPath, [
+      '--experimental-default-type=module',
+      './index',
+    ], {
+      cwd: fixtures.path('es-modules/package-without-type'),
+    });
+
+    match(stderr, /ENOENT/);
+    strictEqual(stdout, '');
+    strictEqual(code, 1);
+    strictEqual(signal, null);
+  });
+});
+
 describe('--experimental-default-type=module should not affect the interpretation of files with unknown extensions',
          { concurrency: true }, () => {
            it('should error on an entry point with an unknown extension', async () => {
