0475027f0017fa56df2113a514ee214c1a9a00bb
Michael Dawson <midawson@redhat.com>
2023-10-04 12:35:39 -0400

    wasi: fix up wasi tests for ibmi
    ibmi now reports os400 instead of aix
    
    - update platform check in poll to allow for os400
    - update wasi-sdk level to 20
      - document the level of wasi-sdk used to compile the tests
      - remove platform check in readdir test as it does not seem to be
        needed with wasi-sdk version 20
      - comment out tests for some clocks which are no longer available when
        compiling with wasi-sdk level 20. These may be added back after
        https://github.com/WebAssembly/wasi-libc/issues/266 is resolved.
    
    Signed-off-by: Michael Dawson <midawson@redhat.com>
    PR-URL: https://github.com/nodejs/node/pull/49953
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/wasi/c/poll.c
+++ test/wasi/c/poll.c
@@ -10,11 +10,11 @@ int main(void) {
   time_t before, now;
   int ret;
   char* platform;
-  int is_aix;
+  int is_aix_or_os400;
   int is_win;
 
   platform = getenv("NODE_PLATFORM");
-  is_aix = platform != NULL && 0 == strcmp(platform, "aix");
+  is_aix_or_os400 = platform != NULL && (0 == strcmp(platform, "aix") || 0 == strcmp(platform, "os400"));
   is_win = platform != NULL && 0 == strcmp(platform, "win32");
 
   // Test sleep() behavior.
@@ -64,7 +64,7 @@ int main(void) {
   ret = poll(fds, 1, 2000);
   assert(ret == 1);
 
-  if (is_aix)
+  if (is_aix_or_os400)
     assert(fds[0].revents == POLLIN);
   else
     assert(fds[0].revents == (POLLHUP | POLLIN));
