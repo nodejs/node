3941379506ffe3f3a1198b848430ef40c84ea411
Toyo Li <lifenglin314@outlook.com>
2023-10-05 21:41:07 +0800

    node-api: update headers for better wasm support
    PR-URL: https://github.com/nodejs/node/pull/49037
    Reviewed-By: Gabriel Schulhof <gabrielschulhof@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>

--- src/js_native_api.h
+++ src/js_native_api.h
@@ -28,7 +28,7 @@
 #ifndef NAPI_EXTERN
 #ifdef _WIN32
 #define NAPI_EXTERN __declspec(dllexport)
-#elif defined(__wasm32__)
+#elif defined(__wasm__)
 #define NAPI_EXTERN                                                            \
   __attribute__((visibility("default")))                                       \
   __attribute__((__import_module__("napi")))
