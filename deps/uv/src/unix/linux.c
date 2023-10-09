3f6addd590360991503007d625d85034cb22c6d8
Tobias Nießen <tniessen@tnie.de>
2023-10-09 08:10:00 +0000

    src,deps: disable setuid() etc if io_uring enabled
    Within Node.js, attempt to determine if libuv is using io_uring. If it
    is, disable process.setuid() and other user identity setters.
    
    We cannot fully prevent users from changing the process's user identity,
    but this should still prevent some accidental, dangerous scenarios.
    
    PR-URL: https://github.com/nodejs-private/node-private/pull/529
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    CVE-ID: CVE-2024-22017

--- deps/uv/src/unix/linux.c
+++ deps/uv/src/unix/linux.c
@@ -442,6 +442,14 @@ static int uv__use_io_uring(void) {
 }
 
 
+UV_EXTERN int uv__node_patch_is_using_io_uring(void) {
+  // This function exists only in the modified copy of libuv in the Node.js
+  // repository. Node.js checks if this function exists and, if it does, uses it
+  // to determine whether libuv is using io_uring or not.
+  return uv__use_io_uring();
+}
+
+
 static void uv__iou_init(int epollfd,
                          struct uv__iou* iou,
                          uint32_t entries,
