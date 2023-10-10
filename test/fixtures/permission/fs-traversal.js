f447a4611a49d1843c17bf9ffbc86a835f1f1b9c
Tobias Nießen <tniessen@tnie.de>
2023-10-10 16:30:07 +0000

    permission: fix Uint8Array path traversal
    Previous security patches addressed path traversal vulnerabilities for
    string and Buffer inputs, but ignored Uint8Array inputs. This commit
    fixes the existing logic to account for the latter.
    
    The previous implementation would silently ignore unexpected inputs,
    whereas this commit introduces an explicit assertion to prevent that
    unsafe behavior.
    
    PR-URL: https://github.com/nodejs-private/node-private/pull/456
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    CVE-ID: CVE-2023-39332

--- test/fixtures/permission/fs-traversal.js
+++ test/fixtures/permission/fs-traversal.js
@@ -15,6 +15,7 @@ const allowedFolder = process.env.ALLOWEDFOLDER;
 const traversalPath = allowedFolder + '../file.md';
 const traversalFolderPath = allowedFolder + '../folder';
 const bufferTraversalPath = Buffer.from(allowedFolder + '../file.md');
+const uint8ArrayTraversalPath = new TextEncoder().encode(traversalPath);
 
 {
   assert.ok(process.permission.has('fs.read', allowedFolder));
@@ -74,6 +75,18 @@ const bufferTraversalPath = Buffer.from(allowedFolder + '../file.md');
 {
   assert.throws(() => {
     fs.readFile(bufferTraversalPath, (error) => {
+      assert.ifError(error);
+    });
+  }, common.expectsError({
+    code: 'ERR_ACCESS_DENIED',
+    permission: 'FileSystemRead',
+    resource: resolve(traversalPath),
+  }));
+}
+
+{
+  assert.throws(() => {
+    fs.readFile(uint8ArrayTraversalPath, (error) => {
       assert.ifError(error);
     });
   }, common.expectsError({
