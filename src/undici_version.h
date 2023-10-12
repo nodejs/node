b959d36b77ad6a4663f38ed33a1cf26322774c5f
Keksonoid <44970151+Keksonoid@users.noreply.github.com>
2023-10-13 04:08:05 +0600

    tools: fix comments referencing dep_updaters scripts
    PR-URL: https://github.com/nodejs/node/pull/50165
    Fixes: https://github.com/nodejs/node/issues/50159
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>

--- src/undici_version.h
+++ src/undici_version.h
@@ -1,5 +1,5 @@
 // This is an auto generated file, please do not edit.
-// Refer to tools/update-undici.sh
+// Refer to tools/dep_updaters/update-undici.sh
 #ifndef SRC_UNDICI_VERSION_H_
 #define SRC_UNDICI_VERSION_H_
 #define UNDICI_VERSION "5.26.4"
