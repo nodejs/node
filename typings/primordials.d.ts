1ec43abd939c6157b8fc936696388b939266b174
Niklas Mollenhauer <nikeee@users.noreply.github.com>
2023-10-14 08:41:59 +0200

    typings: use `Symbol.dispose` and `Symbol.asyncDispose` in types
    PR-URL: https://github.com/nodejs/node/pull/50123
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Debadree Chatterjee <debadree333@gmail.com>

--- typings/primordials.d.ts
+++ typings/primordials.d.ts
@@ -427,8 +427,8 @@ declare namespace primordials {
   export const SymbolFor: typeof Symbol.for
   export const SymbolKeyFor: typeof Symbol.keyFor
   export const SymbolAsyncIterator: typeof Symbol.asyncIterator
-  export const SymbolDispose: symbol // TODO(MoLow): use typeof Symbol.dispose when it's available
-  export const SymbolAsyncDispose: symbol // TODO(MoLow): use typeof Symbol.asyncDispose when it's available
+  export const SymbolDispose: typeof Symbol.dispose
+  export const SymbolAsyncDispose: typeof Symbol.asyncDispose
   export const SymbolHasInstance: typeof Symbol.hasInstance
   export const SymbolIsConcatSpreadable: typeof Symbol.isConcatSpreadable
   export const SymbolIterator: typeof Symbol.iterator
