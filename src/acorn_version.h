7b3cf7d3982bb50d26fe6d65279225a53ff47c4a
Jungku Lee <zighart8456@naver.com>
2023-10-16 05:28:23 +0900

    tools: update comment in `update-uncidi.sh` and `acorn_version.h`
    PR-URL: https://github.com/nodejs/node/pull/50175
    Fixes: https://github.com/nodejs/node/issues/50159
    Refs: https://github.com/nodejs/node/pull/50165
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- src/acorn_version.h
+++ src/acorn_version.h
@@ -1,5 +1,5 @@
 // This is an auto generated file, please do not edit.
-// Refer to tools/update-acorn.sh
+// Refer to tools/dep_updaters/update-acorn.sh
 #ifndef SRC_ACORN_VERSION_H_
 #define SRC_ACORN_VERSION_H_
 #define ACORN_VERSION "8.10.0"
