1ff6235e2050d85669fefc3c75f773caa25c734c
Aras Abbasi <aras.abbasi@googlemail.com>
2023-10-22 23:12:38 +0200

    lib: improve performance of validateStringArray and validateBooleanArray
    PR-URL: https://github.com/nodejs/node/pull/49756
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>

--- test/benchmark/test-benchmark-validators.js
+++ test/benchmark/test-benchmark-validators.js
@@ -4,7 +4,6 @@ require('../common');
 
 // Minimal test for assert benchmarks. This makes sure the benchmarks aren't
 // completely broken but nothing more than that.
-
 const runBenchmark = require('../common/benchmark');
 
 runBenchmark('validators');
