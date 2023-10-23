d542f8e91e57db5fb7bec9e3a2e28e70576619f1
jspades93 <jspades93@gmail.com>
2023-10-22 20:13:11 -0500

    test: migrate message util tests from Python to JS

---
+++ test/parallel/test-node-output-util.mjs
@@ -0,0 +1,32 @@
+import '../common/index.mjs';
+import * as fixtures from '../common/fixtures.mjs';
+import * as snapshot from '../common/assertSnapshot.js';
+import { describe, it } from 'node:test';
+
+function replaceStackTrace(str) {
+  return snapshot.replaceStackTrace(str, '$1at *$7\n');
+}
+function normalize(str) {
+  return str
+    .replaceAll(snapshot.replaceWindowsPaths(process.cwd()), '')
+    .replaceAll('/', '*')
+    .replaceAll(process.version, '*')
+    .replaceAll(/:\d+/g, '*');
+}
+const common = snapshot
+  .transform(snapshot.replaceWindowsLineEndings, snapshot.replaceWindowsPaths);
+
+const defaultTransform = snapshot.transform(common, replaceStackTrace, normalize);
+
+const tests = [
+  { name: 'util/util-inspect-error-cause.js' },
+  { name: 'util/util_inspect_error.js' },
+];
+
+describe('util output', { concurrency: true }, () => {
+  for (const { name, transform } of tests) {
+    it(name, async () => {
+      await snapshot.spawnAndAssert(fixtures.path(name), transform ?? defaultTransform);
+    });
+  }
+});
