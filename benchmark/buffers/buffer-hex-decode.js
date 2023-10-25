d8951303d1ee2a8768717dc77b4eb4fd9f8e4953
Aras Abbasi <aras.abbasi@googlemail.com>
2023-10-25 22:46:33 +0200

    benchmark: add benchmarks for encodings
    PR-URL: https://github.com/nodejs/node/pull/50348
    Reviewed-By: Vinícius Lourenço Claro Cardoso <contact@viniciusl.com.br>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- benchmark/buffers/buffer-hex-decode.js
+++ benchmark/buffers/buffer-hex-decode.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const common = require('../common.js');
+const assert = require('assert');
 
 const bench = common.createBenchmark(main, {
   len: [64, 1024],
@@ -13,12 +14,16 @@ function main({ len, n }) {
   for (let i = 0; i < buf.length; i++)
     buf[i] = i & 0xff;
 
-  const hex = buf.toString('hex');
+  const plain = buf;
 
   bench.start();
 
+  let tmp;
+
   for (let i = 0; i < n; i += 1)
-    Buffer.from(hex, 'hex');
+    tmp = plain.toString('hex');
 
   bench.end(n);
+
+  assert.strictEqual(typeof tmp, 'string');
 }
