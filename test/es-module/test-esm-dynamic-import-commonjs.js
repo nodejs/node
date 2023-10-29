7c420ad5ae2f1446541ff8cf60222d598739c892
Francesco Trotta <github@fasttime.org>
2023-10-29 11:33:34 +0100

    fix lint problems

--- test/es-module/test-esm-dynamic-import-commonjs.js
+++ test/es-module/test-esm-dynamic-import-commonjs.js
@@ -12,16 +12,16 @@ const commonjsURL = tmpdir.fileURL('commonjs-module-1.cjs');
 
 (async () => {
 
-    await Promise.all([fs.writeFile(preloadURL, ''), fs.writeFile(commonjsURL, '')]);
+  await Promise.all([fs.writeFile(preloadURL, ''), fs.writeFile(commonjsURL, '')]);
 
-    // Make sure that the CommonJS module lexer has been initialized.
-    // See https://github.com/nodejs/node/blob/v21.1.0/lib/internal/modules/esm/translators.js#L61-L81.
-    await import(preloadURL);
+  // Make sure that the CommonJS module lexer has been initialized.
+  // See https://github.com/nodejs/node/blob/v21.1.0/lib/internal/modules/esm/translators.js#L61-L81.
+  await import(preloadURL);
 
-    let tickDuringCJSImport = false;
-    process.nextTick(() => { tickDuringCJSImport = true; });
-    await import(commonjsURL);
-    
-    assert(!tickDuringCJSImport);
+  let tickDuringCJSImport = false;
+  process.nextTick(() => { tickDuringCJSImport = true; });
+  await import(commonjsURL);
+
+  assert(!tickDuringCJSImport);
 
 })().then(common.mustCall());
