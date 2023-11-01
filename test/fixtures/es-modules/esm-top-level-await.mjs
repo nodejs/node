3606a0a8487460e18061d207d5d7e180f0149d8b
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-11-01 13:38:01 +0100

    module: execute `--import` sequentially
    PR-URL: https://github.com/nodejs/node/pull/50474
    Fixes: https://github.com/nodejs/node/issues/50427
    Reviewed-By: Jacob Smith <jacob@frende.me>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>

--- test/fixtures/es-modules/esm-top-level-await.mjs
+++ test/fixtures/es-modules/esm-top-level-await.mjs
@@ -1,5 +1,7 @@
-import { setImmediate } from 'node:timers/promises';
+import { setTimeout } from 'node:timers/promises';
 
-await setImmediate();
+// Waiting some arbitrary amount of time to make sure other tasks won't start
+// executing in the mean time.
+await setTimeout(9);
 console.log(1);
 console.log(2);
