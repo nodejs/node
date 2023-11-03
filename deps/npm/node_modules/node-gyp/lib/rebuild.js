fd2368eafc2dc7df5b0b988e2057b9bf95991258
npm CLI robot <npm-cli+bot@github.com>
2023-11-03 07:03:57 -0700

    deps: upgrade npm to 10.2.3
    PR-URL: https://github.com/nodejs/node/pull/50531
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Debadree Chatterjee <debadree333@gmail.com>

--- deps/npm/node_modules/node-gyp/lib/rebuild.js
+++ deps/npm/node_modules/node-gyp/lib/rebuild.js
@@ -1,12 +1,11 @@
 'use strict'
 
-function rebuild (gyp, argv, callback) {
+async function rebuild (gyp, argv) {
   gyp.todo.push(
     { name: 'clean', args: [] }
     , { name: 'configure', args: argv }
     , { name: 'build', args: [] }
   )
-  process.nextTick(callback)
 }
 
 module.exports = rebuild
