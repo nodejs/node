6dcb6688f80a444d5bff71b2b4ba6adb6fce21a4
Francesco Trotta <github@fasttime.org>
2023-11-06 17:03:32 +0100

    use fixtures in tests

--- test/es-module/test-esm-dynamic-import-commonjs.mjs
+++ test/es-module/test-esm-dynamic-import-commonjs.mjs
@@ -1,16 +1,9 @@
 import '../common/index.mjs';
-import tmpdir from '../common/tmpdir.js';
+import fixtures from '../common/fixtures.js';
 import assert from 'node:assert';
-import fs from 'node:fs/promises';
-
-tmpdir.refresh();
-
-const commonjsURL = tmpdir.fileURL('commonjs-module-2.cjs');
-
-await fs.writeFile(commonjsURL, '');
 
 let tickDuringCJSImport = false;
 process.nextTick(() => { tickDuringCJSImport = true; });
-await import(commonjsURL);
+await import(fixtures.fileURL('empty.cjs'));
 
 assert(!tickDuringCJSImport);
