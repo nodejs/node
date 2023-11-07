bff0cbbe565ed97255814e2831cc0663f3bc071f
Marcel Laverdet <marcel@laverdet.com>
2023-11-07 14:58:35 -0500

    module: add `releaseLoadedModule`
    This is a new utility as discussed in #49442 which provides the ability
    to release a loaded module. Releasing a module means that it can be
    garbage collected (if it is abandoned) and reloaded (by importing it
    again with the same specifier).

---
+++ test/es-module/test-esm-evict.mjs
@@ -0,0 +1,16 @@
+import { strictEqual } from 'node:assert';
+import { releaseLoadedModule } from 'node:module';
+
+const specifier = "data:application/javascript,export default globalThis.value;";
+
+globalThis.value = 1;
+const instance1 = await import(specifier);
+strictEqual(instance1.default, 1);
+globalThis.value = 2;
+const instance2 = await import(specifier);
+strictEqual(instance2.default, 1);
+
+strictEqual(releaseLoadedModule(specifier, import.meta.url), true);
+strictEqual(releaseLoadedModule(specifier, import.meta.url), false);
+const instance3 = await import(specifier);
+strictEqual(instance3.default, 2);
