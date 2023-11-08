45c9a4af2b94b7fd6cf298623c6a3e8b5dd4fadd
Francesco Trotta <github@fasttime.org>
2023-11-08 21:04:53 +0100

    use base URL in test

--- test/es-module/test-esm-loader-with-source.mjs
+++ test/es-module/test-esm-loader-with-source.mjs
@@ -4,7 +4,7 @@ import * as fixtures from '../common/fixtures.mjs';
 import assert from 'assert';
 
 const { default: existingFileSource } = await import(fixtures.fileURL('es-modules', 'cjs-file.cjs'));
-const { default: noSuchFileSource } = await import('file:///no-such-file.cjs');
+const { default: noSuchFileSource } = await import(new URL('./no-such-file.cjs', import.meta.url));
 
 assert.strictEqual(existingFileSource, 'no .cjs file was read to get this source');
 assert.strictEqual(noSuchFileSource, 'no .cjs file was read to get this source');
