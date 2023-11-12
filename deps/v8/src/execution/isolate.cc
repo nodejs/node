ec023a7a793f2e2e457e811e4c6ab9e81d5feeb0
Keyhan Vakil <kvakil@sylph.kvakil.me>
2023-11-12 00:14:48 +0000

    deps: V8: cherry-pick 475c8cdf9a95
    Original commit message:
    
        [ptr-compr] Fix multi-cage mode
    
        This CL introduces PtrComprCageAccessScope which sets/restores current
        thread's pointer compression cage base values. It's supposed to be used
        by V8 jobs accessing V8 heap outside of v8::Isolate::Scope or
        i::LocalHeap or i::LocalIsolate scopes (they already ensure that the
        cage base values are properly initialized).
        For all other build modes PtrComprCageAccessScope is a no-op.
    
        For simplicity reasons the multi-cage mode is made incompatible with
        external code space.
    
        Bug: v8:13788, v8:14292
        Change-Id: I06c2d19a1eb7254fa7af07a17617e22d98abea9f
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4846592
        Reviewed-by: Jakob Linke <jgruber@chromium.org>
        Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
        Commit-Queue: Igor Sheludko <ishell@chromium.org>
        Reviewed-by: Dominik Inführ <dinfuehr@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#90075}
    
    Refs: https://github.com/v8/v8/commit/475c8cdf9a951bb06da3084794a0f659f8ef36c2
    PR-URL: https://github.com/nodejs/node/pull/50680
    Refs: https://bugs.chromium.org/p/v8/issues/detail?id=14292
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- deps/v8/src/execution/isolate.cc
+++ deps/v8/src/execution/isolate.cc
@@ -3933,6 +3933,14 @@ Isolate::~Isolate() {
 
 void Isolate::InitializeThreadLocal() {
   thread_local_top()->Initialize(this);
+#ifdef DEBUG
+  // This method might be called on a thread that's not bound to any Isolate
+  // and thus pointer compression schemes might have cage base value unset.
+  // Read-only roots accessors contain type DCHECKs which require access to
+  // V8 heap in order to check the object type. So, allow heap access here
+  // to let the checks work.
+  i::PtrComprCageAccessScope ptr_compr_cage_access_scope(this);
+#endif  // DEBUG
   clear_pending_exception();
   clear_pending_message();
   clear_scheduled_exception();
