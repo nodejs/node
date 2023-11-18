26eca268415f905455ddd890446cf97d110fc866
Mattias Buelens <mattias@buelens.com>
2023-11-18 12:55:58 +0100

    test: enable idlharness tests for encoding
    TextDecoderStream and TextEncoderStream are now exposed as globals,
    so we can run the entire Encoding idlharness test suite.

--- test/wpt/test-encoding.js
+++ test/wpt/test-encoding.js
@@ -3,4 +3,7 @@
 const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('encoding');
 
+// Set a script that will be executed in the worker before running the tests.
+runner.pretendGlobalThisAs('Window');
+
 runner.runJsTests();
