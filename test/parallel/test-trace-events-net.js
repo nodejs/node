6cd31cea996e45a7fcc02394aabd9fdcc890c910
Ianna83 <marco.iannantuono@democom.it>
2023-11-18 16:52:28 +0100

    test: replace forEach with for of in file test-trace-events-net.js

--- test/parallel/test-trace-events-net.js
+++ test/parallel/test-trace-events-net.js
@@ -32,12 +32,11 @@ proc.once('exit', common.mustCall(() => {
     const traces = JSON.parse(data.toString()).traceEvents;
     assert(traces.length > 0);
     let count = 0;
-    traces.forEach((trace) => {
-      if (trace.cat === 'node,node.net,node.net.native' &&
-          trace.name === 'connect') {
+    for (const trace of traces) {
+      if (trace.cat === 'node,node.net,node.net.native' && trace.name === 'connect') {
         count++;
       }
-    });
+    }
     // Two begin, two end
     assert.strictEqual(count, 4);
   }));
