258d7ff2ee93eb0a645cfbbda5601afa8865a01c
Mattias Buelens <649348+MattiasBuelens@users.noreply.github.com>
2023-11-18 13:18:32 +0100

    fixup
    Co-authored-by: Filip Skokan <panva.ip@gmail.com>

--- test/wpt/test-encoding.js
+++ test/wpt/test-encoding.js
@@ -3,7 +3,6 @@
 const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('encoding');
 
-// Set a script that will be executed in the worker before running the tests.
 runner.pretendGlobalThisAs('Window');
 
 runner.runJsTests();
