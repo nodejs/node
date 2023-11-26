8b780d1dba678370a0e291cb0f6fbedce5a57112
Moshe Atlow <moshe@atlow.co.il>
2023-11-26 09:31:58 +0200

    fs: fix glob returning duplicates
    PR-URL: https://github.com/nodejs/node/pull/50881
    Fixes: https://github.com/nodejs/node/issues/50875
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz.nizipli@sentry.io>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- test/parallel/test-fs-glob.mjs
+++ test/parallel/test-fs-glob.mjs
@@ -65,6 +65,7 @@ const patterns = {
   ],
   'a/{b,c,d,e,f}/**/g': [],
   'a/b/**': ['a/b', 'a/b/c', 'a/b/c/d'],
+  'a/{b/**,b/c}': ['a/b', 'a/b/c', 'a/b/c/d'],
   './**/g': ['a/abcdef/g', 'a/abcfed/g'],
   'a/abc{fed,def}/g/h': ['a/abcdef/g/h', 'a/abcfed/g/h'],
   'a/abc{fed/g,def}/**/': ['a/abcdef', 'a/abcdef/g', 'a/abcfed/g'],
