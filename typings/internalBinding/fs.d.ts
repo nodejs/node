e59268a0760a1afe2e8e1e309634704f0053782d
CanadaHonk <honk@goose.icu>
2023-11-27 00:01:39 +0000

    fs: add c++ fast path for writeFileSync utf8
    PR-URL: https://github.com/nodejs/node/pull/49884
    Reviewed-By: Yagiz Nizipli <yagiz.nizipli@sentry.io>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- typings/internalBinding/fs.d.ts
+++ typings/internalBinding/fs.d.ts
@@ -231,6 +231,9 @@ declare namespace InternalFSBinding {
   function writeString(fd: number, value: string, pos: unknown, encoding: unknown, usePromises: typeof kUsePromises): Promise<number>;
 
   function getFormatOfExtensionlessFile(url: string): ConstantsBinding['fs'];
+
+  function writeFileUtf8(path: string, data: string, flag: number, mode: number): void;
+  function writeFileUtf8(fd: number, data: string, flag: number, mode: number): void;
 }
 
 export interface FsBinding {
