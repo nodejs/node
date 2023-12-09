0caf7e977cf9243c97ba7278e070da9ba233ddb7
Lei Shi <lei.a.shi@intel.com>
2023-12-10 02:25:04 +0800

    benchmark: update iteration and size in benchmark/crypto/randomBytes.js
    Fixes: https://github.com/nodejs/node/issues/50571
    PR-URL: https://github.com/nodejs/node/pull/50868
    Reviewed-By: Vinícius Lourenço Claro Cardoso <contact@viniciusl.com.br>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- benchmark/crypto/randomBytes.js
+++ benchmark/crypto/randomBytes.js
@@ -3,14 +3,21 @@
 const common = require('../common.js');
 const { randomBytes } = require('crypto');
 
+// Add together with imports
+const assert = require('assert');
+
+let _cryptoResult;
+
 const bench = common.createBenchmark(main, {
-  size: [64, 1024, 8192, 512 * 1024],
-  n: [1e3],
+  size: [64, 1024, 8 * 1024, 16 * 1024],
+  n: [1e5],
 });
 
 function main({ n, size }) {
   bench.start();
   for (let i = 0; i < n; ++i)
-    randomBytes(size);
+    _cryptoResult = randomBytes(size);
   bench.end(n);
+  // Avoid V8 deadcode (elimination)
+  assert.ok(_cryptoResult);
 }
