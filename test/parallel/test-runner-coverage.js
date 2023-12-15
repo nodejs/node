452d29c62bb0817114d1bd573ce61093180e5bd0
Pulkit Gupta <pulkit30.bsr@gmail.com>
2023-12-15 12:23:08 +0530

    test_runner: format coverage report for tap reporter
    PR-URL: https://github.com/nodejs/node/pull/51119
    Reviewed-By: Raz Luvaton <rluvaton@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Debadree Chatterjee <debadree333@gmail.com>

--- test/parallel/test-runner-coverage.js
+++ test/parallel/test-runner-coverage.js
@@ -22,16 +22,21 @@ function findCoverageFileForPid(pid) {
 }
 
 function getTapCoverageFixtureReport() {
+  /* eslint-disable max-len */
   const report = [
     '# start of coverage report',
-    '# file | line % | branch % | funcs % | uncovered lines',
-    '# test/fixtures/test-runner/coverage.js | 78.65 | 38.46 | 60.00 | 12, ' +
-    '13, 16, 17, 18, 19, 20, 21, 22, 27, 39, 43, 44, 61, 62, 66, 67, 71, 72',
-    '# test/fixtures/test-runner/invalid-tap.js | 100.00 | 100.00 | 100.00 | ',
-    '# test/fixtures/v8-coverage/throw.js | 71.43 | 50.00 | 100.00 | 5, 6',
-    '# all files | 78.35 | 43.75 | 60.00 |',
+    '# -------------------------------------------------------------------------------------------------------------------',
+    '# file                                     | line % | branch % | funcs % | uncovered lines',
+    '# -------------------------------------------------------------------------------------------------------------------',
+    '# test/fixtures/test-runner/coverage.js    |  78.65 |    38.46 |   60.00 | 12-13 16-22 27 39 43-44 61-62 66-67 71-72',
+    '# test/fixtures/test-runner/invalid-tap.js | 100.00 |   100.00 |  100.00 | ',
+    '# test/fixtures/v8-coverage/throw.js       |  71.43 |    50.00 |  100.00 | 5-6',
+    '# -------------------------------------------------------------------------------------------------------------------',
+    '# all files                                |  78.35 |    43.75 |   60.00 |',
+    '# -------------------------------------------------------------------------------------------------------------------',
     '# end of coverage report',
   ].join('\n');
+  /* eslint-enable max-len */
 
   if (common.isWindows) {
     return report.replaceAll('/', '\\');
@@ -88,7 +93,6 @@ test('test tap coverage reporter', skipIfNoInspector, async (t) => {
     const options = { env: { ...process.env, NODE_V8_COVERAGE: tmpdir.path } };
     const result = spawnSync(process.execPath, args, options);
     const report = getTapCoverageFixtureReport();
-
     assert(result.stdout.toString().includes(report));
     assert.strictEqual(result.stderr.toString(), '');
     assert.strictEqual(result.status, 0);
@@ -152,16 +156,16 @@ test('single process coverage is the same with --test', skipIfNoInspector, () =>
 test('coverage is combined for multiple processes', skipIfNoInspector, () => {
   let report = [
     '# start of coverage report',
-    '# file | line % | branch % | funcs % | uncovered lines',
-    '# common.js | 89.86 | ' +
-    '62.50 | 100.00 | 8, 13, 14, 18, 34, 35, 53',
-    '# first.test.js | 83.33 | ' +
-    '100.00 | 50.00 | 5, 6',
-    '# second.test.js | 100.00 ' +
-    '| 100.00 | 100.00 | ',
-    '# third.test.js | 100.00 | ' +
-    '100.00 | 100.00 | ',
-    '# all files | 92.11 | 72.73 | 88.89 |',
+    '# -------------------------------------------------------------------',
+    '# file           | line % | branch % | funcs % | uncovered lines',
+    '# -------------------------------------------------------------------',
+    '# common.js      |  89.86 |    62.50 |  100.00 | 8 13-14 18 34-35 53',
+    '# first.test.js  |  83.33 |   100.00 |   50.00 | 5-6',
+    '# second.test.js | 100.00 |   100.00 |  100.00 | ',
+    '# third.test.js  | 100.00 |   100.00 |  100.00 | ',
+    '# -------------------------------------------------------------------',
+    '# all files      |  92.11 |    72.73 |   88.89 |',
+    '# -------------------------------------------------------------------',
     '# end of coverage report',
   ].join('\n');
 
