0a09229f342243811652cf3d6a2515bbf3e47d56
Yagiz Nizipli <yagiz@nizipli.com>
2023-12-19 16:16:56 -0500

    fs: remove `internalModuleReadJSON` binding

--- typings/internalBinding/fs.d.ts
+++ typings/internalBinding/fs.d.ts
@@ -260,7 +260,6 @@ export interface FsBinding {
   fsync: typeof InternalFSBinding.fsync;
   ftruncate: typeof InternalFSBinding.ftruncate;
   futimes: typeof InternalFSBinding.futimes;
-  internalModuleReadJSON: typeof InternalFSBinding.internalModuleReadJSON;
   internalModuleStat: typeof InternalFSBinding.internalModuleStat;
   lchown: typeof InternalFSBinding.lchown;
   link: typeof InternalFSBinding.link;
