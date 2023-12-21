3f2e234d98872b614e20e8426e3accccc96a0265
Rafael Gonzaga <rafael.nunu@hotmail.com>
2023-12-21 17:36:05 +0000

    permission: fix wildcard when children > 1
    PR-URL: https://github.com/nodejs/node/pull/51209
    Fixes: https://github.com/nodejs/node/issues/50659
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: Paolo Insogna <paolo@cowtech.it>
    Reviewed-By: Rich Trott <rtrott@gmail.com>

--- test/parallel/test-permission-fs-wildcard.js
+++ test/parallel/test-permission-fs-wildcard.js
@@ -98,3 +98,23 @@ if (common.isWindows) {
   );
   assert.strictEqual(status, 0, stderr.toString());
 }
+
+{
+  if (!common.isWindows) {
+    const { status, stderr } = spawnSync(
+      process.execPath,
+      [
+        '--experimental-permission',
+        '--allow-fs-read=/a/b/*',
+        '--allow-fs-read=/a/b/d',
+        '-e',
+        `
+        const assert = require('assert')
+        assert.ok(process.permission.has('fs.read', '/a/b/c'));
+        assert.ok(!process.permission.has('fs.read', '/a/c/c'));
+      `,
+      ]
+    );
+    assert.strictEqual(status, 0, stderr.toString());
+  }
+}
