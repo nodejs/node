99d7b737bff82c2e16dc13b9004c6ad7c782f083
Matteo Collina <hello@matteocollina.com>
2024-01-08 19:07:59 +0100

    fs: remove race condition for recursive watch on Linux
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ test/parallel/test-fs-recursive-sync-write.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const common = require('../common');
+const { mkdtempSync, watch, writeFileSync } = require('node:fs');
+const { tmpdir } = require('node:os');
+const { join } = require('node:path');
+const {platformTimeout} = require('../common');
+const tmpDir = mkdtempSync(join(tmpdir(), 'repro-test-'));
+const filename = join(tmpDir, 'test.file');
+const assert = require('assert');
+
+const keepalive = setTimeout(() => {
+  throw new Error('timed out');
+}, common.platformTimeout(30_0000));
+
+const watcher = watch(tmpDir, { recursive: true }, common.mustCall((eventType, _filename) => {
+  clearTimeout(keepalive);
+  watcher.close();
+  assert.strictEqual(eventType, 'rename');
+  assert.strictEqual(join(tmpDir, _filename), filename);
+  console.log(eventType, filename);
+}));
+
+writeFileSync(filename, 'foobar2');
