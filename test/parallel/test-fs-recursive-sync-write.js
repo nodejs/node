4586010723d2743705d84f18f3f2fe25cae393d8
Matteo Collina <hello@matteocollina.com>
2024-01-11 17:53:50 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/parallel/test-fs-recursive-sync-write.js
+++ test/parallel/test-fs-recursive-sync-write.js
@@ -2,22 +2,24 @@
 
 const common = require('../common');
 const { mkdtempSync, watch, writeFileSync } = require('node:fs');
-const { tmpdir } = require('node:os');
 const { join } = require('node:path');
-const tmpDir = mkdtempSync(join(tmpdir(), 'repro-test-'));
-const filename = join(tmpDir, 'test.file');
+const tmpdir = require('../common/tmpdir.js');
 const assert = require('assert');
+
+tmpdir.refresh();
+
+const tmpDir = tmpdir.path;
+const filename = join(tmpDir, 'test.file');
 
 const keepalive = setTimeout(() => {
   throw new Error('timed out');
-}, common.platformTimeout(30_0000));
+}, common.platformTimeout(30_000));
 
 const watcher = watch(tmpDir, { recursive: true }, common.mustCall((eventType, _filename) => {
   clearTimeout(keepalive);
   watcher.close();
   assert.strictEqual(eventType, 'rename');
   assert.strictEqual(join(tmpDir, _filename), filename);
-  console.log(eventType, filename);
 }));
 
 writeFileSync(filename, 'foobar2');
