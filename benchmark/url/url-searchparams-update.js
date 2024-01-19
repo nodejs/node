065c45cba196a95623eb264a186e318847c888b9
MattIPv4 <me@mattcowley.co.uk>
2024-01-19 21:11:04 +0000

    url: update variable naming, add more comments, include hash in benchmark

--- benchmark/url/url-searchparams-update.js
+++ benchmark/url/url-searchparams-update.js
@@ -1,19 +1,25 @@
 'use strict';
 const common = require('../common.js');
+const assert = require('assert');
 
 const bench = common.createBenchmark(main, {
   searchParams: ['true', 'false'],
-  property: ['pathname', 'search'],
+  property: ['pathname', 'search', 'hash'],
   n: [1e6],
 });
 
+function getMethod(url, property) {
+  if (property === 'pathname') return (x) => url.pathname = `/${x}`;
+  if (property === 'search') return (x) => url.search = `?${x}`;
+  if (property === 'hash') return (x) => url.hash = `#${x}`;
+  throw new Error(`Unsupported property "${property}"`);
+}
+
 function main({ searchParams, property, n }) {
   const url = new URL('https://nodejs.org');
-  if (searchParams === 'true') url.searchParams; // eslint-disable-line no-unused-expressions
+  if (searchParams === 'true') assert(url.searchParams);
 
-  const method = property === 'pathname' ?
-    (x) => url.pathname = `/${x}` :
-    (x) => url.search = `?${x}`;
+  const method = getMethod(url, property);
 
   bench.start();
   for (let i = 0; i < n; i++) {
