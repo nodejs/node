79fa7e8a58b18b00c9b107a0925459124df81171
Joyee Cheung <joyeec9h3@gmail.com>
2024-01-22 22:32:41 +0800

    deps: V8: cherry-pick efb1133eb894
    Original commit message:
    
        [api] Add v8::ScriptCompiler::CachedData::CompatibilityCheck()
    
        This patch adds a new API v8::ScriptCompiler::CachedData::CompatibilityCheck()
        in order to allow embedders to check if the code cache can be used in
        the current isolate without looking up for the source code. It also returns more detailed reasons about why the code cache cannot be used
        when it's bound to be rejected. This makes it possible to enforce
        portability checks in case code code becomes CPU-dependent in the
        future.
    
        Refs: https://github.com/nodejs/node/issues/42566#issuecomment-1735862123
    
        Change-Id: Ia1d677b949050add961af6fbf62c44342c061312
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/4905290
        Reviewed-by: Marja Hölttä <marja@chromium.org>
        Reviewed-by: Toon Verwaest <verwaest@chromium.org>
        Commit-Queue: Joyee Cheung <joyee@igalia.com>
        Cr-Commit-Position: refs/heads/main@{#90833}
    
    Refs: https://github.com/v8/v8/commit/efb1133eb894a283ff0341c4e44d43e913911b6c

--- deps/v8/include/v8-script.h
+++ deps/v8/include/v8-script.h
@@ -394,6 +394,27 @@ class V8_EXPORT ScriptCompiler {
     CachedData(const uint8_t* data, int length,
                BufferPolicy buffer_policy = BufferNotOwned);
     ~CachedData();
+
+    enum CompatibilityCheckResult {
+      // Don't change order/existing values of this enum since it keys into the
+      // `code_cache_reject_reason` histogram. Append-only!
+      kSuccess = 0,
+      kMagicNumberMismatch = 1,
+      kVersionMismatch = 2,
+      kSourceMismatch = 3,
+      kFlagsMismatch = 5,
+      kChecksumMismatch = 6,
+      kInvalidHeader = 7,
+      kLengthMismatch = 8,
+      kReadOnlySnapshotChecksumMismatch = 9,
+
+      // This should always point at the last real enum value.
+      kLast = kReadOnlySnapshotChecksumMismatch
+    };
+
+    // Check if the CachedData can be loaded in the given isolate.
+    CompatibilityCheckResult CompatibilityCheck(Isolate* isolate);
+
     // TODO(marja): Async compilation; add constructors which take a callback
     // which will be called when V8 no longer needs the data.
     const uint8_t* data;
