bf08b03d32db80d3f5518a89fc4e94c1d7634825
Gabriel Bota <gabriel.bota@dynatrace.com>
2024-01-23 11:16:50 +0100

    fixup! async_hooks,inspector: implement inspector api without async_wrap

--- test/parallel/test-inspector-async-context-brk.js
+++ test/parallel/test-inspector-async-context-brk.js
@@ -19,7 +19,6 @@ let valueInBreakpoint = 0;
 
 function debugged() {
   valueInFunction = getStore();
-  console.log('in code => ', getStore());
   return 42;
 }
 
@@ -27,14 +26,10 @@ async function test() {
   const session = new Session();
 
   session.connect();
-  console.log('Connected');
-
   session.post('Debugger.enable');
-  console.log('Debugger was enabled');
 
   session.on('Debugger.paused', () => {
     valueInBreakpoint = getStore();
-    console.log('on Debugger.paused callback => ', getStore());
   });
 
   await new Promise((resolve, reject) => {
@@ -47,22 +42,15 @@ async function test() {
       return error ? reject(error) : resolve(result);
     });
   });
-  console.log('Breakpoint was set');
 
   als.run(1, debugged);
   assert.strictEqual(valueInFunction, valueInBreakpoint);
   assert.strictEqual(valueInFunction, 1);
-  assert.notStrictEqual(valueInFunction, 0);
-  assert.notStrictEqual(valueInBreakpoint, 0);
-
-  console.log('Breakpoint was hit');
 
   session.disconnect();
-  console.log('Session disconnected');
 }
 
 const interval = setInterval(() => {}, 1000);
 test().then(common.mustCall(() => {
   clearInterval(interval);
-  console.log('Done!');
 }));
