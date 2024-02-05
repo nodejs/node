89bd5fc38f3e5c5f02e5e38850d8586231f7c81a
Matteo Collina <hello@matteocollina.com>
2024-02-05 17:21:04 +0100

    lib: update undici to v5.28.3
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    PR-URL: https://github.com/nodejs-private/node-private/pull/539
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    CVE-ID: CVE-2024-24758

--- deps/undici/undici.js
+++ deps/undici/undici.js
@@ -10618,6 +10618,7 @@ var require_fetch = __commonJS({
       }
       if (!sameOrigin(requestCurrentURL(request), locationURL)) {
         request.headersList.delete("authorization");
+        request.headersList.delete("proxy-authorization", true);
         request.headersList.delete("cookie");
         request.headersList.delete("host");
       }
