b202149dd1dfbdea054f84fa4661b89c0570c219
unbyte <i@shangyes.net>
2024-02-07 01:19:12 +0800

    feat: get source mapping url from v8

--- test/es-module/test-esm-source-map.mjs
+++ test/es-module/test-esm-source-map.mjs
@@ -7,59 +7,90 @@ import { describe, it } from 'node:test';
 describe('esm source-map', { concurrency: true }, () => {
   // Issue: https://github.com/nodejs/node/issues/51522
 
-  it('should extract source map url in middle from esm', async () => {
-    const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
-      '--no-warnings',
-      '--enable-source-maps',
-      fixtures.path('source-map/extract-url/esm-url-in-middle.mjs'),
-    ]);
+  [
+    [
+      'in middle from esm',
+      'source-map/extract-url/esm-url-in-middle.mjs',
+      true,
+    ],
+    [
+      'inside string from esm',
+      'source-map/extract-url/esm-url-in-string.mjs',
+      false,
+    ],
+  ].forEach(([name, path, shouldExtract]) => {
+    it((shouldExtract ? 'should extract source map url' : 'should not extract source map url') + name, async () => {
+      const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
+        '--no-warnings',
+        '--enable-source-maps',
+        fixtures.path(path),
+      ]);
 
-    assert.strictEqual(stdout, '');
-    assert.match(stderr, /index\.ts/);
-    assert.strictEqual(code, 1);
-    assert.strictEqual(signal, null);
+      assert.strictEqual(stdout, '');
+      if (shouldExtract) {
+        assert.match(stderr, /index\.ts/);
+      } else {
+        assert.doesNotMatch(stderr, /index\.ts/);
+      }
+      assert.strictEqual(code, 1);
+      assert.strictEqual(signal, null);
+    });
   });
 
-  it('should extract source map url in middle from cjs imported by esm', async () => {
-    const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
-      '--no-warnings',
-      '--enable-source-maps',
-      '--input-type=module',
-      '--eval',
-      `import ${JSON.stringify(fixtures.fileURL('source-map/extract-url/cjs-url-in-middle.js'))}`
-    ]);
+  [
+    [
+      'in middle from esm imported by esm',
+      `import ${JSON.stringify(fixtures.fileURL('source-map/extract-url/esm-url-in-middle.mjs'))}`,
+      true,
+    ],
+    [
+      'in middle from cjs imported by esm',
+      `import ${JSON.stringify(fixtures.fileURL('source-map/extract-url/cjs-url-in-middle.js'))}`,
+      true,
+    ],
+    [
+      'in middle from cjs required by esm',
+      "import { createRequire } from 'module';" +
+      'const require = createRequire(import.meta.url);' +
+      `require(${JSON.stringify(fixtures.path('source-map/extract-url/cjs-url-in-middle.js'))})`,
+      true,
+    ],
 
-    assert.strictEqual(stdout, '');
-    assert.match(stderr, /index\.ts/);
-    assert.strictEqual(code, 1);
-    assert.strictEqual(signal, null);
-  });
+    [
+      'inside string from esm imported by esm',
+      `import ${JSON.stringify(fixtures.fileURL('source-map/extract-url/esm-url-in-string.mjs'))}`,
+      false,
+    ],
+    [
+      'inside string from cjs imported by esm',
+      `import ${JSON.stringify(fixtures.fileURL('source-map/extract-url/cjs-url-in-string.js'))}`,
+      false,
+    ],
+    [
+      'inside string from cjs required by esm',
+      "import { createRequire } from 'module';" +
+      'const require = createRequire(import.meta.url);' +
+      `require(${JSON.stringify(fixtures.path('source-map/extract-url/cjs-url-in-string.js'))})`,
+      false,
+    ],
+  ].forEach(([name, evalCode, shouldExtract]) => {
+    it((shouldExtract ? 'should extract source map url' : 'should not extract source map url') + name, async () => {
+      const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
+        '--no-warnings',
+        '--enable-source-maps',
+        '--input-type=module',
+        '--eval',
+        evalCode,
+      ]);
 
-  it('should not extract source map url inside string from esm', async () => {
-    const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
-      '--no-warnings',
-      '--enable-source-maps',
-      fixtures.path('source-map/extract-url/esm-url-in-string.mjs'),
-    ]);
-
-    assert.strictEqual(stdout, '');
-    assert.doesNotMatch(stderr, /index\.ts/);
-    assert.strictEqual(code, 1);
-    assert.strictEqual(signal, null);
-  });
-
-  it('should not extract source map url inside string from cjs imported by esm', async () => {
-    const { code, signal, stdout, stderr } = await spawnPromisified(execPath, [
-      '--no-warnings',
-      '--enable-source-maps',
-      '--input-type=module',
-      '--eval',
-      `import ${JSON.stringify(fixtures.fileURL('source-map/extract-url/cjs-url-in-string.js'))}`
-    ]);
-
-    assert.strictEqual(stdout, '');
-    assert.doesNotMatch(stderr, /index\.ts/);
-    assert.strictEqual(code, 1);
-    assert.strictEqual(signal, null);
+      assert.strictEqual(stdout, '');
+      if (shouldExtract) {
+        assert.match(stderr, /index\.ts/);
+      } else {
+        assert.doesNotMatch(stderr, /index\.ts/);
+      }
+      assert.strictEqual(code, 1);
+      assert.strictEqual(signal, null);
+    });
   });
 });
