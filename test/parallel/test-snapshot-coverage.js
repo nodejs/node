ba47df3ebcca56f4586daa66fe1f2b385d6003a1
Joyee Cheung <joyeec9h3@gmail.com>
2024-02-23 22:59:33 +0100

    fixup! src: stop the profiler and the inspector before snapshot serialization

--- test/parallel/test-snapshot-coverage.js
+++ test/parallel/test-snapshot-coverage.js
@@ -2,7 +2,9 @@
 
 // This tests that the snapshot works with built-in coverage collection.
 
-require('../common');
+const common = require('../common');
+common.skipIfInspectorDisabled();
+
 const { spawnSyncAndExitWithoutError } = require('../common/child_process');
 const tmpdir = require('../common/tmpdir');
 const fixtures = require('../common/fixtures');
@@ -18,7 +20,7 @@ function filterCoverageFiles(name) {
 }
 {
   // Create the snapshot.
-  spawnSyncAndExitWithoutError(process.execPath, [
+  const { child } = spawnSyncAndExitWithoutError(process.execPath, [
     '--snapshot-blob',
     blobPath,
     '--build-snapshot',
@@ -28,16 +30,18 @@ function filterCoverageFiles(name) {
     env: {
       ...process.env,
       NODE_V8_COVERAGE: tmpdir.path,
+      NODE_DEBUG_NATIVE: 'inspector_profiler',
     }
   });
   const files = fs.readdirSync(tmpdir.path);
   console.log('Files in tmpdir.path', files);  // Log for debugging the test.
   const coverage = files.filter(filterCoverageFiles);
+  console.log(child.stderr.toString());
   assert.strictEqual(coverage.length, 1);
 }
 
 {
-  spawnSyncAndExitWithoutError(process.execPath, [
+  const { child } = spawnSyncAndExitWithoutError(process.execPath, [
     '--snapshot-blob',
     blobPath,
     file,
@@ -46,10 +50,12 @@ function filterCoverageFiles(name) {
     env: {
       ...process.env,
       NODE_V8_COVERAGE: tmpdir.path,
+      NODE_DEBUG_NATIVE: 'inspector_profiler',
     },
   });
   const files = fs.readdirSync(tmpdir.path);
   console.log('Files in tmpdir.path', files);  // Log for debugging the test.
   const coverage = files.filter(filterCoverageFiles);
+  console.log(child.stderr.toString());
   assert.strictEqual(coverage.length, 2);
 }
