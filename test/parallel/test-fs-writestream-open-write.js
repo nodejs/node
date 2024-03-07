f6e22849269b8924658c1cd9f53ef8c4d0f04a5c
Matteo Collina <hello@matteocollina.com>
2024-03-07 17:28:25 +0100

    fs: defer the writestream 'open' event by a nextTick
    Fixes: https://github.com/nodejs/node/issues/51993

---
+++ test/parallel/test-fs-writestream-open-write.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const common = require('../common');
+const { strictEqual } = require('assert');
+const { tmpdir } = require('os');
+const { join } = require('path');
+const fs = require('fs');
+
+// Regression test for https://github.com/nodejs/node/issues/51993
+
+const file = join(tmpdir(), `test-fs-writestream-open-write-${process.pid}.txt`);
+
+const w = fs.createWriteStream(file);
+
+w.on('open', common.mustCall(() => {
+  w.write('hello');
+
+  process.nextTick(() => {
+    w.write('world');
+    w.end();
+  });
+}));
+
+w.on('close', common.mustCall(() => {
+  strictEqual(fs.readFileSync(file, 'utf8'), 'helloworld');
+  fs.unlinkSync(file);
+}));
