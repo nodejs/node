3c9f963d8a44a9f92c51484551da3df1c6b37626
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2024-03-12 01:42:51 +0800

    tools: install manpage to share/man for FreeBSD
    FreeBSD now install manpages to share/man/ instead of man/.
    
    Refs: https://cgit.freebsd.org/ports/commit/?id=003a571d1d6585196545295efc181514f171c4c4
    Refs: https://cgit.freebsd.org/ports/commit/?id=99ea45a72a2800fc90e98ee225a42c3e88da8602
    Refs: https://cgit.freebsd.org/ports/commit/?id=94cb251581ce0b94a40dee0728884367c3a4a82d
    PR-URL: https://github.com/nodejs/node/pull/51791
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz.nizipli@sentry.io>
    Reviewed-By: Richard Lau <rlau@redhat.com>

--- tools/install.py
+++ tools/install.py
@@ -182,7 +182,7 @@ def files(options, action):
   action(options, [os.path.join(options.v8_dir, 'tools/gdbinit')], 'share/doc/node/')
   action(options, [os.path.join(options.v8_dir, 'tools/lldb_commands.py')], 'share/doc/node/')
 
-  if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
+  if 'openbsd' in sys.platform:
     action(options, ['doc/node.1'], 'man/man1/')
   else:
     action(options, ['doc/node.1'], 'share/man/man1/')
