c6b1be01377be73a2433f8ec2f3a633965f75d0b
Michael Dawson <midawson@redhat.com>
2024-03-18 20:04:29 +0000

    src: fix move after use reported by coverity
    - Fix move after use reported by coverity in
      node_contextify.cc
    
    Signed-off-by: Michael Dawson <midawson@redhat.com>
    PR-URL: https://github.com/nodejs/node/pull/52141
    Reviewed-By: Yagiz Nizipli <yagiz.nizipli@sentry.io>
    Reviewed-By: Geoffrey Booth <webadmin@geoffreybooth.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- src/node_contextify.cc
+++ src/node_contextify.cc
@@ -1472,7 +1472,7 @@ void ContextifyContext::ContainsModuleSyntax(
   ContextifyContext::CompileFunctionAndCacheResult(env,
                                                    context,
                                                    &source,
-                                                   std::move(params),
+                                                   params,
                                                    std::vector<Local<Object>>(),
                                                    options,
                                                    true,
