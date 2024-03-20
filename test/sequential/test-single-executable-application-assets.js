c714cda9a742432ac6ca8c05ebd19c7f09d09cb1
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-03-20 20:44:14 +0200

    test: add `spawnSyncAndAssert` util
    PR-URL: https://github.com/nodejs/node/pull/52132
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>

--- test/sequential/test-single-executable-application-assets.js
+++ test/sequential/test-single-executable-application-assets.js
@@ -14,6 +14,7 @@ const tmpdir = require('../common/tmpdir');
 
 const { copyFileSync, writeFileSync, existsSync } = require('fs');
 const {
+  spawnSyncAndAssert,
   spawnSyncAndExit,
   spawnSyncAndExitWithoutError,
 } = require('../common/child_process');
@@ -104,14 +105,13 @@ const outputFile = tmpdir.resolve(process.platform === 'win32' ? 'sea.exe' : 'se
         ...process.env,
       },
       cwd: tmpdir.path
-    },
-    {});
+    });
 
   assert(existsSync(seaPrepBlob));
 
   generateSEA(outputFile, process.execPath, seaPrepBlob);
 
-  spawnSyncAndExitWithoutError(
+  spawnSyncAndAssert(
     outputFile,
     {
       env: {
