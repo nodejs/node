f71e6d8d1bfb59177529fe2468d6c48ccc9ccc6a
cjihrig <cjihrig@gmail.com>
2024-03-22 17:17:04 -0400

    temp - add a failure

--- test/parallel/test-runner-cli-concurrency.js
+++ test/parallel/test-runner-cli-concurrency.js
@@ -23,6 +23,7 @@ test('concurrency of two', async () => {
   const args = ['--test', '--test-concurrency=2'];
   const cp = spawnSync(process.execPath, args, { cwd, env });
   assert.match(cp.stderr.toString(), /concurrency: 2,/);
+  throw new Error('bye');
 });
 
 test('isolation=none uses a concurrency of one', async () => {
