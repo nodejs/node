dbb31b101ced57068c3a75322fa3788aebdd3351
zhenweijin <zhenwei.jin@intel.com>
2024-03-26 13:50:33 +0800

    string_decoder: throw an error when writing a too long buffer

---
+++ test/pummel/test-string-decoder-large-buffer.js
@@ -0,0 +1,30 @@
+'use strict';
+const common = require('../common');
+
+// Buffer with size > INT32_MAX
+common.skipIf32Bits();
+
+const assert = require('assert');
+const { StringDecoder } = require('node:string_decoder');
+const kStringMaxLength = require('buffer').constants.MAX_STRING_LENGTH;
+
+const size = 2 ** 31;
+
+const stringTooLongError = {
+  message: `Cannot create a string longer than 0x${kStringMaxLength.toString(16)}` +
+    ' characters',
+  code: 'ERR_STRING_TOO_LONG',
+  name: 'Error',
+};
+
+try {
+  const buf = Buffer.alloc(size);
+  const decoder = new StringDecoder('utf8');
+  assert.throws(() => decoder.write(buf), stringTooLongError);
+  assert.throws(() => decoder.end(buf), stringTooLongError);
+} catch (e) {
+  if (e.code !== 'ERR_MEMORY_ALLOCATION_FAILED') {
+    throw e;
+  }
+  common.skip('insufficient space for Buffer.alloc');
+}
