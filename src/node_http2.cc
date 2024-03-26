ba1ae6d188df52b25251ccd805d9ad3bc918c327
RafaelGSS <rafael.nunu@hotmail.com>
2024-03-26 15:55:13 -0300

    src: ensure to close stream when destroying session
    Co-Authored-By: Anna Henningsen <anna@addaleax.net>
    PR-URL: https://github.com/nodejs-private/node-private/pull/561
    Fixes: https://hackerone.com/reports/2319584
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    CVE-ID: CVE-2024-27983

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -605,6 +605,12 @@ Http2Session::Http2Session(Http2State* http2_state,
 Http2Session::~Http2Session() {
   CHECK(!is_in_scope());
   Debug(this, "freeing nghttp2 session");
+  // Ensure that all `Http2Stream` instances and the memory they hold
+  // on to are destroyed before the nghttp2 session is.
+  for (const auto& [id, stream] : streams_) {
+    stream->Detach();
+  }
+  streams_.clear();
   // Explicitly reset session_ so the subsequent
   // current_nghttp2_memory_ check passes.
   session_.reset();
