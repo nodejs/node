42049a2ce5a452878508293eb0027ebcd7a7e15d
Moshe Atlow <moshe@atlow.co.il>
2024-04-01 22:01:41 +0300

    fixup! CR

--- test/fixtures/test-runner/output/arbitrary-output-colored.js
+++ test/fixtures/test-runner/output/arbitrary-output-colored.js
@@ -5,8 +5,7 @@ const { spawn } = require('node:child_process');
 const fixtures = require('../../../common/fixtures');
 
 (async function run() {
-  const test = fixtures.path('test-runner/output/arbitrary-output-colored-1.js');
-  const reset = fixtures.path('test-runner/output/reset-color-depth.js');
-  await once(spawn(process.execPath, ['-r', reset, '--test', test], { stdio: 'inherit' }), 'exit');
-  await once(spawn(process.execPath, ['-r', reset, '--test', '--test-reporter', 'tap', test], { stdio: 'inherit'  }), 'exit');
+  const test = fixtures.path('test-runner/output/arbitrary-output-colored-1.js');  
+  await once(spawn(process.execPath, ['--test', test], { stdio: 'inherit', env: { FORCE_COLOR: 1 } }), 'exit');
+  await once(spawn(process.execPath, ['--test', '--test-reporter', 'tap', test], { stdio: 'inherit', env: { FORCE_COLOR: 1 } }), 'exit');
 })().then(common.mustCall());
