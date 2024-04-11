c7ed046c2544cecc5b607f046a7c7a6114d7633a
Matteo Collina <hello@matteocollina.com>
2024-04-11 16:54:50 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/parallel/test-fs-watch-recursive-delete.js
+++ test/parallel/test-fs-watch-recursive-delete.js
@@ -10,12 +10,16 @@ fs.mkdirSync(tmpdir.resolve('./parent/child'), { recursive: true });
 
 fs.writeFileSync(tmpdir.resolve('./parent/child/test.tmp'), 'test');
 
-const watcher = fs.watch(tmpdir.resolve('./parent'), { recursive: true }, common.mustCallAtLeast((eventType, filename) => {
+const toWatch = tmpdir.resolve('./parent');
+
+const onFileUpdate = common.mustCallAtLeast((eventType, filename) => {
   // We are only checking for the filename to avoid having Windows, Linux and Mac specific assertions
   if (fs.readdirSync(tmpdir.resolve('./parent')).length === 0) {
     watcher.close();
   }
-}), 1);
+}, 1);
+
+const watcher = fs.watch(toWatch, { recursive: true }, onFileUpdate);
 
 // We must wait a bit `fs.rm()` to let the watcher be set up properly
 setTimeout(() => {
