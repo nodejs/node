fe22cd604d07568529ab3e706e082f9868b644b5
Tobias Nießen <tniessen@tnie.de>
2024-04-15 18:29:29 +0000

    src: remove erroneous CVE-2024-27980 revert option
    No security reverts should exist on the main branch.

--- src/node_revert.h
+++ src/node_revert.h
@@ -15,9 +15,8 @@
  **/
 namespace node {
 
-#define SECURITY_REVERSIONS(XX)                                            \
-  XX(CVE_2024_27980, "CVE-2024-27980", "Unsafe Windows batch file execution")
-//  XX(CVE_2016_PEND, "CVE-2016-PEND", "Vulnerability Title")
+#define SECURITY_REVERSIONS(XX)                                                \
+  //  XX(CVE_2016_PEND, "CVE-2016-PEND", "Vulnerability Title")
 
 enum reversion {
 #define V(code, ...) SECURITY_REVERT_##code,
