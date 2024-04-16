fcbd1b314a7b563d97a2d30d274a55a51c7f6614
Daeyeon Jeong <daeyeon.dev@gmail.com>
2024-04-16 10:42:57 +0900

    fixup! separate tc for clarify on patch expectations
    Signed-off-by: Daeyeon Jeong <daeyeon.dev@gmail.com>

--- test/parallel/test-inspector-exit-worker-in-wait-for-connection.mjs
+++ test/parallel/test-inspector-exit-worker-in-wait-for-connection.mjs
@@ -2,20 +2,31 @@
 import * as common from '../common/index.mjs';
 common.skipIfInspectorDisabled();
 
-import { isMainThread, Worker } from 'node:worker_threads';
+import { parentPort, isMainThread, Worker } from 'node:worker_threads';
 import { fileURLToPath } from 'node:url';
 import inspector from 'node:inspector';
+import assert from 'node:assert';
 
 // Ref: https://github.com/nodejs/node/issues/52467
-// - worker.terminate() should terminate the worker and the pending
-//   inspector.waitForDebugger().
-if (isMainThread) {
-  const worker = new Worker(fileURLToPath(import.meta.url));
-  await new Promise((r) => setTimeout(r, 2_000));
 
-  worker.on('exit', common.mustCall());
-  await worker.terminate();
+if (isMainThread) {
+  // worker.terminate() should terminate the worker and the pending
+  // inspector.waitForDebugger().
+  {
+    const worker = new Worker(fileURLToPath(import.meta.url));
+    await new Promise((r) => worker.on('message', r));
+    worker.on('exit', common.mustCall());
+    await worker.terminate();
+  }
+  // process.exit() should kill the process.
+  {
+    const worker = new Worker(fileURLToPath(import.meta.url));
+    await new Promise((r) => worker.on('message', r));
+    process.on('exit', (status) => assert.strictEqual(status, 0));
+    setImmediate(() => process.exit());
+  }
 } else {
   inspector.open();
+  parentPort.postMessage('open');
   inspector.waitForDebugger();
 }
