f3d09b1493cb233f564c60c48e0ddf2c3f92c1c9
Robert Waite (from Dev Box) <rowait@microsoft.com>
2024-04-17 15:38:06 -0700

    appease the linter

--- test/parallel/test-code-integrity.js
+++ test/parallel/test-code-integrity.js
@@ -1,48 +1,54 @@
-'use strict'
+'use strict';
 
 // Flags: --expose-internals
 
+require('../common');
 const assert = require('node:assert');
-const {describe, it, mock} = require('node:test');
+const { describe, it } = require('node:test');
 const ci = require('codeintegrity');
 const opt = require('internal/options');
-
 const pre_execution = require('internal/process/pre_execution');
 
 describe('when code integrity enforcement is enabled', () => {
-    it('should throw an error if no manifest is provided', (t) => {
-        t.mock.method(ci, ci.isCodeIntegrityForcedByOS.name, () => { return true });
-        
-        assert.throws(
-            pre_execution.readPolicyFromDisk,
-            {
-                code: "ERR_MANIFEST_SYSTEM_CI_VIOLATION"
-            }
-        );
+
+  it('should throw an error if no manifest is provided', (t) => {
+    t.mock.method(ci, ci.isCodeIntegrityForcedByOS.name, () => { return true; });
+
+    assert.throws(
+      pre_execution.readPolicyFromDisk,
+      {
+        code: 'ERR_MANIFEST_SYSTEM_CI_VIOLATION'
+      }
+    );
+  });
+
+  it('should throw an error if no policy signature is given', (t) => {
+    t.mock.method(ci, ci.isCodeIntegrityForcedByOS.name, () => { return true; });
+    t.mock.method(opt, opt.getOptionValue.name, (o) => {
+      if (o === '--policy-signature') {
+        return null;
+      }
+      return o;
     });
 
-    it('should throw an error if no policy signature is given', (t) => {
-        t.mock.method(opt, opt.getOptionValue.name, (o) => { if (o == "--policy-signature") { return null } else { return o } });
-        t.mock.method(ci, ci.isCodeIntegrityForcedByOS.name, () => { return true });
+    assert.throws(
+      pre_execution.readPolicyFromDisk,
+      {
+        code: 'ERR_MANIFEST_SYSTEM_CI_VIOLATION'
+      }
+    );
+  });
 
-        assert.throws(
-            pre_execution.readPolicyFromDisk,
-            {
-                code: "ERR_MANIFEST_SYSTEM_CI_VIOLATION"
-            }
-        );
-    })
+  it('should throw an error if the policy signature fails to validate', (t) => {
+    t.mock.method(opt, opt.getOptionValue.name, (o) => { return o; });
+    t.mock.method(ci, ci.isCodeIntegrityForcedByOS.name, () => { return true; });
+    t.mock.method(ci, ci.isFileTrustedBySystemCodeIntegrityPolicy.name, () => { return false; });
 
-    it('should throw an error if the policy signature fails to validate', (t) => {
-        t.mock.method(opt, opt.getOptionValue.name, (o) => { return o; });
-        t.mock.method(ci, ci.isCodeIntegrityForcedByOS.name, () => { return true });
-        t.mock.method(ci, ci.isFileTrustedBySystemCodeIntegrityPolicy.name, () => { return false });
-
-        assert.throws(
-            pre_execution.readPolicyFromDisk,
-            {
-                code: "ERR_MANIFEST_SYSTEM_CI_VIOLATION"
-            }
-        );
-    })
+    assert.throws(
+      pre_execution.readPolicyFromDisk,
+      {
+        code: 'ERR_MANIFEST_SYSTEM_CI_VIOLATION'
+      }
+    );
+  });
 });
