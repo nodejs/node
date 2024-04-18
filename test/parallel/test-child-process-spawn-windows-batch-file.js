4a6ca7a1d42d861b1679d842c51cf2571bd7f8df
Tobias Nießen <tniessen@tnie.de>
2024-04-18 15:09:09 +0200

    src: remove erroneous CVE-2024-27980 revert option
    No security reverts should exist on the main branch.
    
    PR-URL: https://github.com/nodejs/node/pull/52543
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Yagiz Nizipli <yagiz.nizipli@sentry.io>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-child-process-spawn-windows-batch-file.js
+++ test/parallel/test-child-process-spawn-windows-batch-file.js
@@ -19,23 +19,12 @@ const cp = require('child_process');
 const assert = require('assert');
 const { isWindows } = common;
 
-const arg = '--security-revert=CVE-2024-27980';
-const isRevert = process.execArgv.includes(arg);
-
-const expectedCode = isWindows && !isRevert ? 'EINVAL' : 'ENOENT';
+const expectedCode = isWindows ? 'EINVAL' : 'ENOENT';
 const expectedStatus = isWindows ? 1 : 127;
 
 const suffixes =
     'BAT bAT BaT baT BAt bAt Bat bat CMD cMD CmD cmD CMd cMd Cmd cmd'
     .split(' ');
-
-if (process.argv[2] === undefined) {
-  const a = cp.spawnSync(process.execPath, [__filename, 'child']);
-  const b = cp.spawnSync(process.execPath, [arg, __filename, 'child']);
-  assert.strictEqual(a.status, 0);
-  assert.strictEqual(b.status, 0);
-  return;
-}
 
 function testExec(filename) {
   return new Promise((resolve) => {
