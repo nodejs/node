3790d524c1781334ccc504a6eb510bc53ee2ad64
Tobias Nießen <tniessen@tnie.de>
2024-04-18 15:09:09 +0200

    src: remove erroneous CVE-2024-27980 revert option
    No security reverts should exist on the main branch.
    
    PR-URL: https://github.com/nodejs/node/pull/52543
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Yagiz Nizipli <yagiz.nizipli@sentry.io>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- src/util-inl.h
+++ src/util-inl.h
@@ -648,9 +648,8 @@ bool IsWindowsBatchFile(const char* filename) {
   static constexpr bool kIsWindows = false;
 #endif  // _WIN32
   if (kIsWindows)
-    if (!IsReverted(SECURITY_REVERT_CVE_2024_27980))
-      if (const char* p = strrchr(filename, '.'))
-        return StringEqualNoCase(p, ".bat") || StringEqualNoCase(p, ".cmd");
+    if (const char* p = strrchr(filename, '.'))
+      return StringEqualNoCase(p, ".bat") || StringEqualNoCase(p, ".cmd");
   return false;
 }
 
