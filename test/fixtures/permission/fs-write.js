01e9eac91234100bff0151c70caa84c110910081
RafaelGSS <rafael.nunu@hotmail.com>
2024-04-24 16:45:47 -0300

    lib,permission: disable fchmod/fchown when pm enabled
    PR-URL: https://github.com/nodejs-private/node-private/pull/584
    Refs: https://hackerone.com/reports/2472071
    CVE-ID: CVE-2024-36137

--- test/fixtures/permission/fs-write.js
+++ test/fixtures/permission/fs-write.js
@@ -463,3 +463,31 @@ const relativeProtectedFolder = process.env.RELATIVEBLOCKEDFOLDER;
     resource: path.toNamespacedPath(blockedFile),
   });
 }
+
+// fs.fchown with read-only fd
+{
+  assert.throws(() => {
+    // blocked file is allowed to read
+    const fd = fs.openSync(blockedFile, 'r');
+    fs.fchmod(fd, 777, common.expectsError({
+      code: 'ERR_ACCESS_DENIED',
+    }));
+    fs.fchmodSync(fd, 777);
+  }, {
+    code: 'ERR_ACCESS_DENIED',
+  });
+}
+
+// fs.fchmod with read-only fd
+{
+  assert.throws(() => {
+    // blocked file is allowed to read
+    const fd = fs.openSync(blockedFile, 'r');
+    fs.fchown(fd, 999, 999, common.expectsError({
+      code: 'ERR_ACCESS_DENIED',
+    }));
+    fs.fchownSync(fd, 999, 999);
+  }, {
+    code: 'ERR_ACCESS_DENIED',
+  });
+}
