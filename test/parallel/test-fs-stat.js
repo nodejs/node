c5107f2f7f573e71e1779cb438bcb0f1148ece09
Nicolò Ribaudo <nribaudo@igalia.com>
2024-04-26 16:45:56 +0200

    Add test for BigIntStats

--- test/parallel/test-fs-stat.js
+++ test/parallel/test-fs-stat.js
@@ -180,31 +180,35 @@ fs.lstat(__filename, undefined, common.mustCall());
   });
 }
 
-// Stat Date properties can be set before reading them
-fs.stat(__filename, common.mustSucceed((s) => {
-  s.atime = 2;
-  s.mtime = 3;
-  s.ctime = 4;
-  s.birthtime = 5;
+{
+  // These two tests have an equivalent in ./test-fs-stat-bigint.js
 
-  assert.strictEqual(s.atime, 2);
-  assert.strictEqual(s.mtime, 3);
-  assert.strictEqual(s.ctime, 4);
-  assert.strictEqual(s.birthtime, 5);
-}));
+  // Stats Date properties can be set before reading them
+  fs.stat(__filename, common.mustSucceed((s) => {
+    s.atime = 2;
+    s.mtime = 3;
+    s.ctime = 4;
+    s.birthtime = 5;
 
-// Stat Date properties can be set after reading them
-fs.stat(__filename, common.mustSucceed((s) => {
-  // eslint-disable-next-line no-unused-expressions
-  s.atime, s.mtime, s.ctime, s.birthtime;
+    assert.strictEqual(s.atime, 2);
+    assert.strictEqual(s.mtime, 3);
+    assert.strictEqual(s.ctime, 4);
+    assert.strictEqual(s.birthtime, 5);
+  }));
 
-  s.atime = 2;
-  s.mtime = 3;
-  s.ctime = 4;
-  s.birthtime = 5;
+  // Stats Date properties can be set after reading them
+  fs.stat(__filename, common.mustSucceed((s) => {
+    // eslint-disable-next-line no-unused-expressions
+    s.atime, s.mtime, s.ctime, s.birthtime;
 
-  assert.strictEqual(s.atime, 2);
-  assert.strictEqual(s.mtime, 3);
-  assert.strictEqual(s.ctime, 4);
-  assert.strictEqual(s.birthtime, 5);
-}));
+    s.atime = 2;
+    s.mtime = 3;
+    s.ctime = 4;
+    s.birthtime = 5;
+
+    assert.strictEqual(s.atime, 2);
+    assert.strictEqual(s.mtime, 3);
+    assert.strictEqual(s.ctime, 4);
+    assert.strictEqual(s.birthtime, 5);
+  }));
+}
