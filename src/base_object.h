bd151552ef35b0eed415eb1c50d30dafd341cee8
Igor Sheludko <isheludko@gmail.com>
2024-04-30 15:22:06 +0200

    src: use new V8 API to define stream accessor
    Define XxxStream.prototype.onread as an accessor in JavaScript sense.
    
    Previously it was defined via soon-to-be-deprecated
    `v8::ObjectTemplate::SetAccessor(..)` which used to call setter even
    for property stores via stream object.
    
    The replacement V8 API `v8::ObjectTemplate::SetNativeDataProperty(..)`
    defines a properly behaving data property and thus a store to a stream
    object will not trigger the "onread" setter callback.
    
    In order to preserve the desired behavior of storing the value in the
    receiver's internal field the "onread" property should be defined as
    a proper JavaScript accessor.
    
    PR-URL: https://github.com/nodejs/node/pull/53084
    Refs: https://github.com/v8/v8/commit/46c241eb99557fe8205acac5c526650c3847d180
    Refs: https://github.com/v8/v8/commit/6ec883986bd417e2a42ddb960bd9449deb7e4639
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

--- src/base_object.h
+++ src/base_object.h
@@ -111,12 +111,9 @@ class BaseObject : public MemoryRetainer {
 
   // Setter/Getter pair for internal fields that can be passed to SetAccessor.
   template <int Field>
-  static void InternalFieldGet(v8::Local<v8::Name> property,
-                               const v8::PropertyCallbackInfo<v8::Value>& info);
+  static void InternalFieldGet(const v8::FunctionCallbackInfo<v8::Value>& args);
   template <int Field, bool (v8::Value::*typecheck)() const>
-  static void InternalFieldSet(v8::Local<v8::Name> property,
-                               v8::Local<v8::Value> value,
-                               const v8::PropertyCallbackInfo<void>& info);
+  static void InternalFieldSet(const v8::FunctionCallbackInfo<v8::Value>& args);
 
   // This is a bit of a hack. See the override in async_wrap.cc for details.
   virtual bool IsDoneInitializing() const;
