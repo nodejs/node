3928044aff77b31a1dde8747496e9e90a6576bb1
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-05-02 15:08:51 +0300

    events: remove check for events should not be an object

--- test/parallel/test-event-emitter-set-max-listeners-side-effects.js
+++ test/parallel/test-event-emitter-set-max-listeners-side-effects.js
@@ -26,7 +26,6 @@ const events = require('events');
 
 const e = new events.EventEmitter();
 
-assert(!(e._events instanceof Object));
 assert.deepStrictEqual(Object.keys(e._events), []);
 e.setMaxListeners(5);
 assert.deepStrictEqual(Object.keys(e._events), []);
