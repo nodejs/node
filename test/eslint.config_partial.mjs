55cd7e857ebe04a39e34c8b865ba25d9184a0523
ZihongQu <zihongqu@gmail.com>
2024-05-16 20:44:44 +0930

    tools: move webcrypto into no-restricted-properties
    Since eslint fixed https://github.com/eslint/eslint/issues/16412 and we
    are on eslint v8.57.0 so that we can take advantage of
    no-restricted-properties rule for webcrypto.

--- test/eslint.config_partial.mjs
+++ test/eslint.config_partial.mjs
@@ -2,7 +2,6 @@
 
 import {
   noRestrictedSyntaxCommonAll,
-  noRestrictedSyntaxCommonTest,
   requireEslintTool,
 } from '../tools/eslint.config_utils.mjs';
 
@@ -26,7 +25,6 @@ export default [
       'no-restricted-syntax': [
         'error',
         ...noRestrictedSyntaxCommonAll,
-        ...noRestrictedSyntaxCommonTest,
         {
           selector: "CallExpression:matches([callee.name='deepStrictEqual'], [callee.property.name='deepStrictEqual']):matches([arguments.1.type='Literal']:not([arguments.1.regex]), [arguments.1.type='Identifier'][arguments.1.name='undefined'])",
           message: 'Use strictEqual instead of deepStrictEqual for literals or undefined.',
