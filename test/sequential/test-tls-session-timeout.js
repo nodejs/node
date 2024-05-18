f5263672b397e250caca1c1cd3a7292b3ed290d2
Tobias Nießen <tniessen@tnie.de>
2024-05-18 02:26:11 +0200

    tls: fix negative sessionTimeout handling
    For historical reasons, the second argument of SSL_CTX_set_timeout is a
    signed integer, and Node.js has so far passed arbitrary (signed) int32_t
    values. However, new versions of OpenSSL have changed the handling of
    negative values inside SSL_CTX_set_timeout, and we should shield users
    of Node.js from both the old and the new behavior. Hence, reject any
    negative values by throwing an error from within createSecureContext.
    
    Refs: https://github.com/openssl/openssl/pull/19082
    PR-URL: https://github.com/nodejs/node/pull/53002
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Tim Perry <pimterry@gmail.com>

--- test/sequential/test-tls-session-timeout.js
+++ test/sequential/test-tls-session-timeout.js
@@ -22,14 +22,42 @@
 'use strict';
 const common = require('../common');
 
-if (!common.opensslCli)
-  common.skip('node compiled without OpenSSL CLI.');
-
 if (!common.hasCrypto)
   common.skip('missing crypto');
 
 const tmpdir = require('../common/tmpdir');
 tmpdir.refresh();
+
+const assert = require('assert');
+const tls = require('tls');
+const fixtures = require('../common/fixtures');
+
+const key = fixtures.readKey('rsa_private.pem');
+const cert = fixtures.readKey('rsa_cert.crt');
+
+{
+  // Node.js should not allow setting negative timeouts since new versions of
+  // OpenSSL do not handle those as users might expect
+
+  for (const sessionTimeout of [-1, -100, -(2 ** 31)]) {
+    assert.throws(() => {
+      tls.createServer({
+        key: key,
+        cert: cert,
+        ca: [cert],
+        sessionTimeout,
+        maxVersion: 'TLSv1.2',
+      });
+    }, {
+      code: 'ERR_OUT_OF_RANGE',
+      message: 'The value of "options.sessionTimeout" is out of range. It ' +
+               `must be >= 0 && <= ${2 ** 31 - 1}. Received ${sessionTimeout}`,
+    });
+  }
+}
+
+if (!common.opensslCli)
+  common.skip('node compiled without OpenSSL CLI.');
 
 doTest();
 
@@ -42,16 +70,11 @@ doTest();
 //   that we used has expired by now.
 
 function doTest() {
-  const assert = require('assert');
-  const tls = require('tls');
   const fs = require('fs');
-  const fixtures = require('../common/fixtures');
   const spawn = require('child_process').spawn;
 
   const SESSION_TIMEOUT = 1;
 
-  const key = fixtures.readKey('rsa_private.pem');
-  const cert = fixtures.readKey('rsa_cert.crt');
   const options = {
     key: key,
     cert: cert,
