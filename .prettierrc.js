3321c3d49c634338886606cd58a492756baa9798
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2024-05-22 16:53:53 -0400

    meta: add prettier support

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// Written in JS so we can have comments
+
+module.exports = {
+  // While the ESLint config enforces a max length of 120,
+  // striving for 80 characters may be a good idea (for
+  // the formatter, not the linter)
+  printWidth: 80,
+
+  semi: true,
+  quoteProps: "consistent",
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+  proseWrap: "always",
+  embeddedLanguageFormatting: "auto",
+};
