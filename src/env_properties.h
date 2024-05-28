337357dd09fbcbb8bb264cbe870a14e16fedb1c2
Chengzhong Wu <chengzhong.wcz@alibaba-inc.com>
2024-05-28 23:56:56 +0100

    lib: allow CJS source map cache to be reclaimed
    Unifies the CJS and ESM source map cache map with SourceMapCacheMap
    and allows the CJS cache entries to be queried more efficiently with
    a source url without iteration on an IterableWeakMap.
    
    Add a test to verify that the CJS source map cache entry can be
    reclaimed.
    
    PR-URL: https://github.com/nodejs/node/pull/51711
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- src/env_properties.h
+++ src/env_properties.h
@@ -35,7 +35,8 @@
   V(napi_wrapper, "node:napi:wrapper")                                         \
   V(untransferable_object_private_symbol, "node:untransferableObject")         \
   V(exit_info_private_symbol, "node:exit_info_private_symbol")                 \
-  V(promise_trace_id, "node:promise_trace_id")
+  V(promise_trace_id, "node:promise_trace_id")                                 \
+  V(source_map_data_private_symbol, "node:source_map_data_private_symbol")
 
 // Symbols are per-isolate primitives but Environment proxies them
 // for the sake of convenience.
