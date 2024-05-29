de563b15bec7a1696a1e70800b9da8e86c44b474
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2024-05-29 16:53:25 +0200

    more verbose error checks

--- test/parallel/test-http2-respond-file-errors.js
+++ test/parallel/test-http2-respond-file-errors.js
@@ -93,7 +93,11 @@ server.listen(0, common.mustCall(() => {
   const client = http2.connect(`http://localhost:${server.address().port}`);
   const req = client.request();
 
-  req.once('error', common.mustCall());
+  req.once('error', common.expectsError({
+    name: 'Error',
+    code: 'ERR_HTTP2_STREAM_ERROR',
+    message: 'Stream closed with error code NGHTTP2_INTERNAL_ERROR'
+  }));
   req.on('close', common.mustCall(() => {
     client.close();
     server.close();
