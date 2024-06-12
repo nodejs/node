05bb310fdd56e5f62c431071c47edabda2105057
Hüseyin Açacak <110401522+huseyinacacak-janea@users.noreply.github.com>
2024-06-12 15:44:19 +0300

    src: fix IsIPAddress for IPv6
    Fix the bug when copying IPv6 host to a variable to remove brackets.
    
    Fixes: https://github.com/nodejs/node/issues/47427
    PR-URL: https://github.com/nodejs/node/pull/53400
    Reviewed-By: Tobias Nießen <tniessen@tnie.de>
    Reviewed-By: Richard Lau <rlau@redhat.com>

--- src/inspector_socket.cc
+++ src/inspector_socket.cc
@@ -192,7 +192,7 @@ static bool IsIPAddress(const std::string& host) {
     // Parse the IPv6 address to ensure it is syntactically valid.
     char ipv6_str[INET6_ADDRSTRLEN];
     std::copy(host.begin() + 1, host.end() - 1, ipv6_str);
-    ipv6_str[host.length()] = '\0';
+    ipv6_str[host.length() - 2] = '\0';
     unsigned char ipv6[sizeof(struct in6_addr)];
     if (uv_inet_pton(AF_INET6, ipv6_str, ipv6) != 0) return false;
 
