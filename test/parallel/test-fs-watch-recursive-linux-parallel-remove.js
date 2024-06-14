2b0f06927a15d16ad68c34603c54308b4aa6be71
Matteo Collina <hello@matteocollina.com>
2024-06-14 15:50:44 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/parallel/test-fs-watch-recursive-linux-parallel-remove.js
+++ test/parallel/test-fs-watch-recursive-linux-parallel-remove.js
@@ -5,7 +5,9 @@ const common = require('../common');
 if (!common.isLinux)
   common.skip('This test can run only on Linux');
 
-const assert = require('node:assert');
+// Test the watcher do not crash if the file "disappears" while
+// watch is being set up.
+
 const path = require('node:path');
 const fs = require('node:fs');
 const { spawn } = require('node:child_process');
@@ -18,15 +20,14 @@ const watcher = fs.watch(testDir, { recursive: true });
 watcher.on('change', function(event, filename) {
   // This console.log makes the error happen
   // do not remove
-  console.log(filename, event)
-  // assert.strictEqual(event, 'rename');
+  console.log(filename, event);
 });
 
-const testFile = path.join(testDir, 'a')
+const testFile = path.join(testDir, 'a');
 const child = spawn(process.argv[0], ['-e', `const fs = require('node:fs'); for (let i = 0; i < 10000; i++) { const fd = fs.openSync('${testFile}', 'w'); fs.writeSync(fd, Buffer.from('hello')); fs.rmSync('${testFile}') }`], {
   stdio: 'inherit'
-})
+});
 
-child.on('exit', function () {
-  watcher.close()
-})
+child.on('exit', function() {
+  watcher.close();
+});
