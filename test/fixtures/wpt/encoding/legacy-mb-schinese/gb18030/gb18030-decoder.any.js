b8f436c755bf35f3dc8c42130c16a6c3d7764bbb
Yagiz Nizipli <yagiz@nizipli.com>
2024-06-18 15:39:49 -0400

    test: update encoding web-platform tests
    PR-URL: https://github.com/nodejs/node/pull/53477
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/fixtures/wpt/encoding/legacy-mb-schinese/gb18030/gb18030-decoder.any.js
+++ test/fixtures/wpt/encoding/legacy-mb-schinese/gb18030/gb18030-decoder.any.js
@@ -47,6 +47,44 @@ decode([0x83, 0x36, 0xC8, 0x30], "\uE7C8", "legacy ICU special case 1");
 decode([0xA1, 0xAD], "\u2026", "legacy ICU special case 2");
 decode([0xA1, 0xAB], "\uFF5E", "legacy ICU special case 3");
 
+// GB18030-2022
+decode([0xA6, 0xD9], "\uFE10", "GB18030-2022 1");
+decode([0xA6, 0xDA], "\uFE12", "GB18030-2022 2");
+decode([0xA6, 0xDB], "\uFE11", "GB18030-2022 3");
+decode([0xA6, 0xDC], "\uFE13", "GB18030-2022 4");
+decode([0xA6, 0xDD], "\uFE14", "GB18030-2022 5");
+decode([0xA6, 0xDE], "\uFE15", "GB18030-2022 6");
+decode([0xA6, 0xDF], "\uFE16", "GB18030-2022 7");
+decode([0xA6, 0xEC], "\uFE17", "GB18030-2022 8");
+decode([0xA6, 0xED], "\uFE18", "GB18030-2022 9");
+decode([0xA6, 0xF3], "\uFE19", "GB18030-2022 10");
+decode([0xFE, 0x59], "\u9FB4", "GB18030-2022 11");
+decode([0xFE, 0x61], "\u9FB5", "GB18030-2022 12");
+decode([0xFE, 0x66], "\u9FB6", "GB18030-2022 13");
+decode([0xFE, 0x67], "\u9FB7", "GB18030-2022 14");
+decode([0xFE, 0x6D], "\u9FB8", "GB18030-2022 15");
+decode([0xFE, 0x7E], "\u9FB9", "GB18030-2022 16");
+decode([0xFE, 0x90], "\u9FBA", "GB18030-2022 17");
+decode([0xFE, 0xA0], "\u9FBB", "GB18030-2022 18");
+decode([0x82, 0x35, 0x90, 0x37], "\uE81E", "GB18030-2022 19");
+decode([0x82, 0x35, 0x90, 0x38], "\uE826", "GB18030-2022 20");
+decode([0x82, 0x35, 0x90, 0x39], "\uE82B", "GB18030-2022 21");
+decode([0x82, 0x35, 0x91, 0x30], "\uE82C", "GB18030-2022 22");
+decode([0x82, 0x35, 0x91, 0x31], "\uE832", "GB18030-2022 23");
+decode([0x82, 0x35, 0x91, 0x32], "\uE843", "GB18030-2022 24");
+decode([0x82, 0x35, 0x91, 0x33], "\uE854", "GB18030-2022 25");
+decode([0x82, 0x35, 0x91, 0x34], "\uE864", "GB18030-2022 26");
+decode([0x84, 0x31, 0x82, 0x36], "\uE78D", "GB18030-2022 27");
+decode([0x84, 0x31, 0x82, 0x37], "\uE78F", "GB18030-2022 28");
+decode([0x84, 0x31, 0x82, 0x38], "\uE78E", "GB18030-2022 29");
+decode([0x84, 0x31, 0x82, 0x39], "\uE790", "GB18030-2022 30");
+decode([0x84, 0x31, 0x83, 0x30], "\uE791", "GB18030-2022 31");
+decode([0x84, 0x31, 0x83, 0x31], "\uE792", "GB18030-2022 32");
+decode([0x84, 0x31, 0x83, 0x32], "\uE793", "GB18030-2022 33");
+decode([0x84, 0x31, 0x83, 0x33], "\uE794", "GB18030-2022 34");
+decode([0x84, 0x31, 0x83, 0x34], "\uE795", "GB18030-2022 35");
+decode([0x84, 0x31, 0x83, 0x35], "\uE796", "GB18030-2022 36");
+
 let i = 0;
 for (const range of ranges) {
   const pointer = range[0];
