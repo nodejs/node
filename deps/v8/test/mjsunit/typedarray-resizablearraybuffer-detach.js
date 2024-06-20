d3bc362b26b62e2dd8b48292af5af3f138763007
Chengzhong Wu <legendecas@gmail.com>
2024-06-20 23:48:03 +0100

    deps: V8: cherry-pick 9ebca66a5740
    Original commit message:
    
        [rab/gsab] Remove --harmony-rab-gsab (has been on by default for a while)
    
        Bug: v8:11111
        Change-Id: Ie74e7737f3e2e8730820cf00f1cbc7ae02b515af
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5516580
        Commit-Queue: Marja Hölttä <marja@chromium.org>
        Reviewed-by: Camillo Bruni <cbruni@chromium.org>
        Reviewed-by: Shu-yu Guo <syg@chromium.org>
        Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#93848}
    
    Refs: https://github.com/v8/v8/commit/9ebca66a57409dd9441868a9ce429c0c3f61409d

--- deps/v8/test/mjsunit/typedarray-resizablearraybuffer-detach.js
+++ deps/v8/test/mjsunit/typedarray-resizablearraybuffer-detach.js
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --harmony-rab-gsab --allow-natives-syntax --harmony-array-find-last
-// Flags: --js-float16array
+// Flags: --allow-natives-syntax --js-float16array
 
 "use strict";
 
