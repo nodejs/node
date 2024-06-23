91b2850303272a16e8e3779e9050f3da7e7529cc
Richard Lau <rlau@redhat.com>
2024-06-23 19:53:03 +0100

    test: extend env for `test-node-output-errors`
    Extend, rather than replace, `env` for `test-node-output-errors` so
    that `node` binaries that need `LD_LIBRARY_PATH`, `LIBPATH` or
    `DYLD_LIBRARY_PATH` can run.
    
    PR-URL: https://github.com/nodejs/node/pull/53535
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>

--- test/parallel/test-node-output-errors.mjs
+++ test/parallel/test-node-output-errors.mjs
@@ -6,8 +6,6 @@ import { describe, it } from 'node:test';
 import { pathToFileURL } from 'node:url';
 
 const skipForceColors =
-  process.config.variables.icu_gyp_path !== 'tools/icu/icu-generic.gyp' ||
-  process.config.variables.node_shared_openssl ||
   (common.isWindows && (Number(os.release().split('.')[0]) !== 10 || Number(os.release().split('.')[2]) < 14393)); // See https://github.com/nodejs/node/pull/33132
 
 
@@ -76,7 +74,7 @@ describe('errors output', { concurrency: !process.env.TEST_PARALLEL }, () => {
   ];
   for (const { name, transform = defaultTransform, env, skip = false } of tests) {
     it(name, { skip }, async () => {
-      await snapshot.spawnAndAssert(fixtures.path(name), transform, { env });
+      await snapshot.spawnAndAssert(fixtures.path(name), transform, { env: { ...env, ...process.env } });
     });
   }
 });
