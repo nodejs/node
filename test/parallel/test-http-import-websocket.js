576bdb0c076dffd76135666a5df05bf068e43086
Natalia Venditto <nataliafrontend@gmail.com>
2024-07-04 21:07:19 +0200

    test: update test/parallel/test-http-import-websocket.js
    Co-authored-by: Aviv Keller <38299977+RedYetiDev@users.noreply.github.com>

--- test/parallel/test-http-import-websocket.js
+++ test/parallel/test-http-import-websocket.js
@@ -4,9 +4,6 @@ require('../common');
 const assert = require('assert');
 const { WebSocket: NodeHttpWebSocket, CloseEvent: NodeHttpCloseEvent, MessageEvent: NodeHttpMessageEvent } = require('node:http');
 
-assert.strictEqual(typeof NodeHttpWebSocket, 'function');
-assert.strictEqual(typeof NodeHttpCloseEvent, 'function');
-assert.strictEqual(typeof NodeHttpMessageEvent, 'function');
 
 // compare with global objects
 assert.strictEqual(NodeHttpWebSocket, WebSocket);
