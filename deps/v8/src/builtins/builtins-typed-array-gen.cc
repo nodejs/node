e8c58779148796474453072cfbae02055920dd19
Joyee Cheung <joyeec9h3@gmail.com>
2024-07-07 20:22:17 +0200

    deps: V8: cherry-pick e061cf9970d9
    Original commit message:
    
        [arraybuffers] initialize max byte length of empty array buffers
    
        Without initializing the max byte length field, any empty array
        buffer captured in the snapshot can make the snapshot unreproducible.
    
        Refs: https://github.com/nodejs/node/issues/53579
    
        Change-Id: I2489ab2e57ecbb405ec431a87d0acc92822b777c
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5662576
        Reviewed-by: Marja Hölttä <marja@chromium.org>
        Commit-Queue: Marja Hölttä <marja@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#94754}
    
    Refs: https://github.com/v8/v8/commit/e061cf9970d95e98e2e8dad813b86a3068db01db
    PR-URL: https://github.com/nodejs/node/pull/53755
    Fixes: https://github.com/nodejs/node/issues/53579
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>

--- deps/v8/src/builtins/builtins-typed-array-gen.cc
+++ deps/v8/src/builtins/builtins-typed-array-gen.cc
@@ -69,6 +69,8 @@ TNode<JSArrayBuffer> TypedArrayBuiltinsAssembler::AllocateEmptyOnHeapBuffer(
                                  UndefinedConstant());
   StoreBoundedSizeToObject(buffer, JSArrayBuffer::kRawByteLengthOffset,
                            UintPtrConstant(0));
+  StoreBoundedSizeToObject(buffer, JSArrayBuffer::kRawMaxByteLengthOffset,
+                           UintPtrConstant(0));
   StoreSandboxedPointerToObject(buffer, JSArrayBuffer::kBackingStoreOffset,
                                 EmptyBackingStoreBufferConstant());
 #ifdef V8_COMPRESS_POINTERS
