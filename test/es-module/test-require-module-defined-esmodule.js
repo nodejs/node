ae54d52b1282ef633736c4b4b938fdffc3be8eaa
Joyee Cheung <joyeec9h3@gmail.com>
2024-07-10 16:15:27 +0200

    fixup! fixup! module: add __esModule to require()'d ESM

--- test/es-module/test-require-module-defined-esmodule.js
+++ test/es-module/test-require-module-defined-esmodule.js
@@ -1,12 +1,23 @@
 // Flags: --experimental-require-module
 'use strict';
 const common = require('../common');
-const mod = require('../fixtures/es-modules/export-es-module.mjs');
 
 // If an ESM already defines __esModule to be something else,
 // require(esm) should allow the user override.
-common.expectRequiredModule(
-  mod,
-  { default: { hello: 'world' }, __esModule: 'test' },
-  false,
-);
+{
+  const mod = require('../fixtures/es-modules/export-es-module.mjs');
+  common.expectRequiredModule(
+    mod,
+    { default: { hello: 'world' }, __esModule: 'test' },
+    false,
+  );
+}
+
+{
+  const mod = require('../fixtures/es-modules/export-es-module-2.mjs');
+  common.expectRequiredModule(
+    mod,
+    { default: { hello: 'world' }, __esModule: false },
+    false,
+  );
+}
