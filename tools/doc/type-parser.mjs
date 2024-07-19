3cdf94d403460f1f1474c15e808f78408bb0c272
jakecastelli <38635403+jakecastelli@users.noreply.github.com>
2024-07-19 17:53:24 +0700

    doc,tty: add documentation for ReadStream and WriteStream
    Co-authored-by: Qingyu Deng <i@ayase-lab.com>
    PR-URL: https://github.com/nodejs/node/pull/53567
    Fixes: https://github.com/nodejs/node/issues/37780
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Raz Luvaton <rluvaton@gmail.com>
    Reviewed-By: Claudio Wunder <cwunder@gnome.org>

--- tools/doc/type-parser.mjs
+++ tools/doc/type-parser.mjs
@@ -219,6 +219,9 @@ const customTypesMap = {
 
   'Tracing': 'tracing.html#tracing-object',
 
+  'tty.ReadStream': 'tty.html#class-ttyreadstream',
+  'tty.WriteStream': 'tty.html#class-ttywritestream',
+
   'URL': 'url.html#the-whatwg-url-api',
   'URLSearchParams': 'url.html#class-urlsearchparams',
 
