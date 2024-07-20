c667fbd9882bf75c5473c3e116c01798cef1a03d
Vinicius Lourenço <12551007+H4ad@users.noreply.github.com>
2024-07-19 21:22:24 -0300

    lib: improve error message when index not found on cjs
    PR-URL: https://github.com/nodejs/node/pull/53859
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>

--- src/node_file.cc
+++ src/node_file.cc
@@ -3143,6 +3143,8 @@ void BindingData::LegacyMainResolve(const FunctionCallbackInfo<Value>& args) {
     return;
   }
 
+  std::string package_initial_file = "";
+
   ada::result<ada::url_aggregator> file_path_url;
   std::optional<std::string> initial_file_path;
   std::string file_path;
@@ -3164,6 +3166,8 @@ void BindingData::LegacyMainResolve(const FunctionCallbackInfo<Value>& args) {
     }
 
     node::url::FromNamespacedPath(&initial_file_path.value());
+
+    package_initial_file = *initial_file_path;
 
     for (int i = 0; i < legacy_main_extensions_with_main_end; i++) {
       file_path = *initial_file_path + std::string(legacy_main_extensions[i]);
@@ -3220,13 +3224,10 @@ void BindingData::LegacyMainResolve(const FunctionCallbackInfo<Value>& args) {
     }
   }
 
-  std::optional<std::string> module_path =
-      node::url::FileURLToPath(env, *package_json_url);
-  std::optional<std::string> module_base;
+  if (package_initial_file == "")
+    package_initial_file = *initial_file_path + ".js";
 
-  if (!module_path.has_value()) {
-    return;
-  }
+  std::optional<std::string> module_base;
 
   if (args.Length() >= 3 && args[2]->IsString()) {
     Utf8Value utf8_base_path(isolate, args[2]);
@@ -3251,7 +3252,7 @@ void BindingData::LegacyMainResolve(const FunctionCallbackInfo<Value>& args) {
 
   THROW_ERR_MODULE_NOT_FOUND(isolate,
                              "Cannot find package '%s' imported from %s",
-                             *module_path,
+                             package_initial_file,
                              *module_base);
 }
 
