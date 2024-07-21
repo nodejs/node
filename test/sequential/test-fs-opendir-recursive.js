4d34bb63f81baaa3fdfc9f21307cbb11a1e08867
sonsurim <surim014@naver.com>
2024-07-21 20:28:06 +0900

    fs: revert changes in test-fs-opendir-recursive.js
    The getDirentPath function in test/sequential/test-fs-opendir-recursive.js has been reverted for stability.
    
    Refs: https://github.com/nodejs/node/pull/53969#discussion_r1685707107

--- test/sequential/test-fs-opendir-recursive.js
+++ test/sequential/test-fs-opendir-recursive.js
@@ -128,7 +128,7 @@ for (let i = 0; i < expected.length; i++) {
 }
 
 function getDirentPath(dirent) {
-  return pathModule.relative(testDir, pathModule.join(dirent.path, dirent.name));
+  return pathModule.relative(testDir, dirent.path);
 }
 
 function assertDirents(dirents) {
