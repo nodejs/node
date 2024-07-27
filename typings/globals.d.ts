9a8ea7fc9c5b0a33098e809400831f48bb535df4
1ilsang <1ilsang@naver.com>
2024-07-27 13:51:25 +0900

    typings: apply lint

--- typings/globals.d.ts
+++ typings/globals.d.ts
@@ -1,21 +1,21 @@
-import {AsyncWrapBinding} from "./internalBinding/async_wrap";
-import {BlobBinding} from "./internalBinding/blob";
-import {ConfigBinding} from "./internalBinding/config";
-import {ConstantsBinding} from "./internalBinding/constants";
-import {HttpParserBinding} from "./internalBinding/http_parser";
-import {FsBinding} from "./internalBinding/fs";
-import {FsDirBinding} from "./internalBinding/fs_dir";
-import {MessagingBinding} from "./internalBinding/messaging";
-import {OptionsBinding} from "./internalBinding/options";
-import {OSBinding} from "./internalBinding/os";
-import {SerdesBinding} from "./internalBinding/serdes";
-import {SymbolsBinding} from "./internalBinding/symbols";
-import {TimersBinding} from "./internalBinding/timers";
-import {TypesBinding} from "./internalBinding/types";
-import {URLBinding} from "./internalBinding/url";
-import {UtilBinding} from "./internalBinding/util";
-import {WorkerBinding} from "./internalBinding/worker";
-import {ModulesBinding} from "./internalBinding/modules";
+import { AsyncWrapBinding } from './internalBinding/async_wrap';
+import { BlobBinding } from './internalBinding/blob';
+import { ConfigBinding } from './internalBinding/config';
+import { ConstantsBinding } from './internalBinding/constants';
+import { HttpParserBinding } from './internalBinding/http_parser';
+import { FsBinding } from './internalBinding/fs';
+import { FsDirBinding } from './internalBinding/fs_dir';
+import { MessagingBinding } from './internalBinding/messaging';
+import { OptionsBinding } from './internalBinding/options';
+import { OSBinding } from './internalBinding/os';
+import { SerdesBinding } from './internalBinding/serdes';
+import { SymbolsBinding } from './internalBinding/symbols';
+import { TimersBinding } from './internalBinding/timers';
+import { TypesBinding } from './internalBinding/types';
+import { URLBinding } from './internalBinding/url';
+import { UtilBinding } from './internalBinding/util';
+import { WorkerBinding } from './internalBinding/worker';
+import { ModulesBinding } from './internalBinding/modules';
 
 declare type TypedArray =
   | Uint8Array
