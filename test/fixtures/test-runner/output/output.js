bbe850404425ac55c1fb3fdef6e01f1bd6a91404
Pietro Marchini <pietro.marchini94@gmail.com>
2024-07-31 08:50:07 +0200

    test_runner: move --test-only diagnostic warning tests to separate file

--- test/fixtures/test-runner/output/output.js
+++ test/fixtures/test-runner/output/output.js
@@ -3,7 +3,6 @@
 require('../../../common');
 const assert = require('node:assert');
 const test = require('node:test');
-const { describe, it } = require('node:test');
 const util = require('util');
 
 test('sync pass todo', (t) => {
@@ -404,41 +403,3 @@ test('assertion errors display actual and expected properly', async () => {
     throw err;
   }
 });
-
-describe('should NOT print --test-only diagnostic warning - describe-only-false', {only: false}, () => {
-  it('only false in describe');
-});
-
-describe('should NOT print --test-only diagnostic warning - it-only-false', () => {
-  it('only false in the subtest', {only: false});
-});
-
-describe('should NOT print --test-only diagnostic warning - no-only', () => {
-  it('no only');
-});
-
-test('should NOT print --test-only diagnostic warning - test-only-false', {only: false}, async (t) => {
-  await t.test('only false in parent test');
-});
-
-test('should NOT print --test-only diagnostic warning - t.test-only-false', async (t) => {
-  await t.test('only false in subtest', {only: false});
-});
-
-test('should NOT print --test-only diagnostic warning - no-only', async (t) => {
-  await t.test('no only');
-});
-
-test('should print --test-only diagnostic warning - test-only-true', {only: true}, async (t) => {
-  await t.test('only true in parent test');
-})
-
-test('should print --test-only diagnostic warning - t.test-only-true', async (t) => {
-  await t.test('only true in subtest', {only: true});
-});
-
-test('should print --test-only diagnostic warning - 2 levels of only', async (t) => {
-  await t.test('only true in parent test', {only: false}, async (t) => {
-    await t.test('only true in subtest', {only: true});
-  });
-})
