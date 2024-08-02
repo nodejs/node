e02aaa76caecbe1cf1278c43601dd8716a3558e9
cola119 <kohei.ueno119@gmail.com>
2024-08-02 16:18:54 +0900

    inspector: convert a header object to a simple string dictionary

--- test/parallel/test-inspector-network-domain.js
+++ test/parallel/test-inspector-network-domain.js
@@ -13,10 +13,25 @@ const inspector = require('node:inspector/promises');
 const session = new inspector.Session();
 session.connect();
 
+const requestHeaders = {
+  'accept-language': 'en-US',
+  'Cookie': ['k1=v1', 'k2=v2'],
+  'age': 1000,
+  'x-header1': ['value1', 'value2']
+};
+
+const setResponseHeaders = (res) => {
+  res.setHeader('server', 'node');
+  res.setHeader('etag', 12345);
+  res.setHeader('Set-Cookie', ['key1=value1', 'key2=value2']);
+  res.setHeader('x-header2', ['value1', 'value2']);
+};
+
 const httpServer = http.createServer((req, res) => {
   const path = req.url;
   switch (path) {
     case '/hello-world':
+      setResponseHeaders(res);
       res.writeHead(200);
       res.end('hello world\n');
       break;
@@ -32,6 +47,7 @@ const httpsServer = https.createServer({
   const path = req.url;
   switch (path) {
     case '/hello-world':
+      setResponseHeaders(res);
       res.writeHead(200);
       res.end('hello world\n');
       break;
@@ -53,6 +69,10 @@ const testHttpGet = () => new Promise((resolve, reject) => {
     assert.strictEqual(params.request.url, 'http://127.0.0.1/hello-world');
     assert.strictEqual(params.request.method, 'GET');
     assert.strictEqual(typeof params.request.headers, 'object');
+    assert.strictEqual(params.request.headers['accept-language'], 'en-US');
+    assert.strictEqual(params.request.headers.cookie, 'k1=v1; k2=v2');
+    assert.strictEqual(params.request.headers.age, '1000');
+    assert.strictEqual(params.request.headers['x-header1'], 'value1, value2');
     assert.strictEqual(typeof params.timestamp, 'number');
     assert.strictEqual(typeof params.wallTime, 'number');
   }));
@@ -64,6 +84,10 @@ const testHttpGet = () => new Promise((resolve, reject) => {
     assert.strictEqual(params.response.statusText, 'OK');
     assert.strictEqual(params.response.url, 'http://127.0.0.1/hello-world');
     assert.strictEqual(typeof params.response.headers, 'object');
+    assert.strictEqual(params.response.headers.server, 'node');
+    assert.strictEqual(params.response.headers.etag, '12345');
+    assert.strictEqual(params.response.headers['set-cookie'], 'key1=value1\nkey2=value2');
+    assert.strictEqual(params.response.headers['x-header2'], 'value1, value2');
   }));
   session.on('Network.loadingFinished', common.mustCall(({ params }) => {
     assert.ok(params.requestId.startsWith('node-network-event-'));
@@ -75,6 +99,7 @@ const testHttpGet = () => new Promise((resolve, reject) => {
     host: '127.0.0.1',
     port: httpServer.address().port,
     path: '/hello-world',
+    headers: requestHeaders
   }, common.mustCall());
 });
 
@@ -84,6 +109,10 @@ const testHttpsGet = () => new Promise((resolve, reject) => {
     assert.strictEqual(params.request.url, 'https://127.0.0.1/hello-world');
     assert.strictEqual(params.request.method, 'GET');
     assert.strictEqual(typeof params.request.headers, 'object');
+    assert.strictEqual(params.request.headers['accept-language'], 'en-US');
+    assert.strictEqual(params.request.headers.cookie, 'k1=v1; k2=v2');
+    assert.strictEqual(params.request.headers.age, '1000');
+    assert.strictEqual(params.request.headers['x-header1'], 'value1, value2');
     assert.strictEqual(typeof params.timestamp, 'number');
     assert.strictEqual(typeof params.wallTime, 'number');
   }));
@@ -95,6 +124,10 @@ const testHttpsGet = () => new Promise((resolve, reject) => {
     assert.strictEqual(params.response.statusText, 'OK');
     assert.strictEqual(params.response.url, 'https://127.0.0.1/hello-world');
     assert.strictEqual(typeof params.response.headers, 'object');
+    assert.strictEqual(params.response.headers.server, 'node');
+    assert.strictEqual(params.response.headers.etag, '12345');
+    assert.strictEqual(params.response.headers['set-cookie'], 'key1=value1\nkey2=value2');
+    assert.strictEqual(params.response.headers['x-header2'], 'value1, value2');
   }));
   session.on('Network.loadingFinished', common.mustCall(({ params }) => {
     assert.ok(params.requestId.startsWith('node-network-event-'));
@@ -107,6 +140,7 @@ const testHttpsGet = () => new Promise((resolve, reject) => {
     port: httpsServer.address().port,
     path: '/hello-world',
     rejectUnauthorized: false,
+    headers: requestHeaders,
   }, common.mustCall());
 });
 
