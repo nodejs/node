5dec57944583fe1459f2282d77f224845b878eb8
Giovanni <giovanni.bucci@fleetster.net>
2024-08-09 08:13:49 +0200

    dns: removed copyright from new test and fixed broken one

--- test/sequential/test-net-server-listen-ipv6-loopback.js
+++ test/sequential/test-net-server-listen-ipv6-loopback.js
@@ -1,24 +1,3 @@
-// Copyright Joyent, Inc. and other Node contributors.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to permit
-// persons to whom the Software is furnished to do so, subject to the
-// following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-// USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 'use strict';
 const common = require('../common');
 const assert = require('assert');
@@ -31,16 +10,22 @@ if (!common.hasIPv6) {
   return;
 }
 
-// Test on IPv6 Server, throws EADDRNOTAVAIL
+// Test on IPv6 Server, throws an error
 {
-  const host = 'fe80::1';
+  mock.method(dns, 'lookup', (hostname, options, callback) => {
+    if (hostname === 'ipv6_loopback') {
+      callback(null, [{ address: 'fe80::1', family: 6 }]);
+    } else {
+      dns.lookup.wrappedMethod(hostname, options, callback);
+    }
+  });
+  const host = 'ipv6_loopback';
 
   const server = net.createServer();
 
   server.on('error', common.mustCall((e) => {
-    assert.strictEqual(e.address, host);
+    assert.strictEqual(e.address, 'fe80::1');
     assert.strictEqual(e.syscall, 'listen');
-    assert.strictEqual(e.code, 'EADDRNOTAVAIL');
     assert.strictEqual(e.errno, -49);
   }));
 
@@ -66,10 +51,10 @@ if (!common.hasIPv6) {
 
   server.on('error', common.mustNotCall());
 
-  server.listen(common.PORT + 2, host, common.mustCall(() => {
+  server.listen(common.PORT + 3, host, common.mustCall(() => {
     const address = server.address();
     assert.strictEqual(address.address, '127.0.0.1');
-    assert.strictEqual(address.port, common.PORT + 2);
+    assert.strictEqual(address.port, common.PORT + 3);
     assert.strictEqual(address.family, family4);
     server.close();
   }));
