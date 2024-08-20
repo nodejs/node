bba8b9a6777d63fe90cc341115ac0c1b64d97454
CGQAQ <m.jason.liu@outlook.com>
2024-08-20 14:01:31 +0800

    run NODE=$(command -v node) make lint-js-fix

--- test/parallel/test-linux-dev-stdin.js
+++ test/parallel/test-linux-dev-stdin.js
@@ -7,11 +7,11 @@ const common = require('../common');
 //
 
 if (!common.isLinux) {
-    common.skip('Linux only test');
+  common.skip('Linux only test');
 }
 
-const child_process = require("child_process")
+const child_process = require('child_process');
 const assert = require('assert');
 
-const cp = child_process.execSync(`printf 'console.log(1)' | "${process.execPath}" /dev/stdin`, {stdio: 'pipe'})
+const cp = child_process.execSync(`printf 'console.log(1)' | "${process.execPath}" /dev/stdin`, { stdio: 'pipe' });
 assert.strictEqual(cp.toString(), '1\n');
