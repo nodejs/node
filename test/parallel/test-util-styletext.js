0d3a65d5583126a2dad6b569a6a03870d6e47b60
RafaelGSS <rafael.nunu@hotmail.com>
2024-08-20 10:41:16 -0300

    fixup! fixup! fixup! lib: respect terminal capabilities on styleText

--- test/parallel/test-util-styletext.js
+++ test/parallel/test-util-styletext.js
@@ -8,9 +8,6 @@ const { WriteStream } = require('node:tty');
 const styled = '\u001b[31mtest\u001b[39m';
 const noChange = 'test';
 
-const fd = common.getTTYfd();
-const writeStream = new WriteStream(fd);
-
 [
   undefined,
   null,
@@ -61,23 +58,30 @@ util.styleText('red', 'text', { stream: {}, validateStream: false });
 
 assert.strictEqual(util.styleText('red', 'test'), styled);
 
-const originalEnv = process.env;
-[
-  { isTTY: true, env: {}, expected: styled },
-  { isTTY: false, env: {}, expected: noChange },
-  { isTTY: true, env: { NODE_DISABLE_COLORS: '1' }, expected: noChange },
-  { isTTY: true, env: { NO_COLOR: '1' }, expected: noChange },
-  { isTTY: true, env: { FORCE_COLOR: '1' }, expected: styled },
-  { isTTY: true, env: { FORCE_COLOR: '1', NODE_DISABLE_COLORS: '1' }, expected: styled },
-  { isTTY: false, env: { FORCE_COLOR: '1', NO_COLOR: '1', NODE_DISABLE_COLORS: '1' }, expected: styled },
-  { isTTY: true, env: { FORCE_COLOR: '1', NO_COLOR: '1', NODE_DISABLE_COLORS: '1' }, expected: styled },
-].forEach((testCase) => {
-  writeStream.isTTY = testCase.isTTY;
-  process.env = {
-    ...process.env,
-    ...testCase.env
-  };
-  const output = util.styleText('red', 'test', { stream: writeStream });
-  assert.strictEqual(output, testCase.expected);
-  process.env = originalEnv;
-});
+const fd = common.getTTYfd();
+if (fd !== -1) {
+  const writeStream = new WriteStream(fd);
+
+  const originalEnv = process.env;
+  [
+    { isTTY: true, env: {}, expected: styled },
+    { isTTY: false, env: {}, expected: noChange },
+    { isTTY: true, env: { NODE_DISABLE_COLORS: '1' }, expected: noChange },
+    { isTTY: true, env: { NO_COLOR: '1' }, expected: noChange },
+    { isTTY: true, env: { FORCE_COLOR: '1' }, expected: styled },
+    { isTTY: true, env: { FORCE_COLOR: '1', NODE_DISABLE_COLORS: '1' }, expected: styled },
+    { isTTY: false, env: { FORCE_COLOR: '1', NO_COLOR: '1', NODE_DISABLE_COLORS: '1' }, expected: styled },
+    { isTTY: true, env: { FORCE_COLOR: '1', NO_COLOR: '1', NODE_DISABLE_COLORS: '1' }, expected: styled },
+  ].forEach((testCase) => {
+    writeStream.isTTY = testCase.isTTY;
+    process.env = {
+      ...process.env,
+      ...testCase.env
+    };
+    const output = util.styleText('red', 'test', { stream: writeStream });
+    assert.strictEqual(output, testCase.expected);
+    process.env = originalEnv;
+  });
+} else {
+  common.skip('Could not create TTY fd');
+}
