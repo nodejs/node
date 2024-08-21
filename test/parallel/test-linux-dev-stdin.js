e793947fe92987eeba843c8c748509ebaa25edbb
CGQAQ <m.jason.liu@outlook.com>
2024-08-21 12:37:44 +0800

    test symlink to /dev/stdin

--- test/parallel/test-linux-dev-stdin.js
+++ test/parallel/test-linux-dev-stdin.js
@@ -12,6 +12,15 @@ if (!common.isLinux) {
 
 const child_process = require('child_process');
 const assert = require('assert');
+const fs = require('fs');
 
 const cp = child_process.execSync(`printf 'console.log(1)' | "${process.execPath}" /dev/stdin`, { stdio: 'pipe' });
 assert.strictEqual(cp.toString(), '1\n');
+
+if (fs.existsSync('/tmp/stdin')) {
+  fs.unlinkSync('/tmp/stdin');
+}
+fs.symlinkSync('/dev/stdin', '/tmp/stdin');
+const cp2 = child_process.execSync(`printf 'console.log(2)' | "${process.execPath}" /tmp/stdin`, { stdio: 'pipe' });
+assert.strictEqual(cp2.toString(), '2\n');
+fs.unlinkSync('/tmp/stdin');
