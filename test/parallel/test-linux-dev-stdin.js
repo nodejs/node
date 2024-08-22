ba1a3b35be8ecc352eaaddeee6cb1547443139bf
CGQAQ <m.jason.liu@outlook.com>
2024-08-22 08:49:30 +0800

    apply reviews from RedYetiDev

--- test/parallel/test-linux-dev-stdin.js
+++ test/parallel/test-linux-dev-stdin.js
@@ -1,26 +1,25 @@
 'use strict';
 const common = require('../common');
+const tmpdir = require('../common/tmpdir');
+const fs = require('fs');
 
 //
-// This test ensures Node.js doesn't crash on linux when reading from /dev/stdin in the CLI
+// This test ensures Node.js doesn't crash when reading from /dev/stdin as an input.
 // ref: https://github.com/nodejs/node/issues/54200
 //
 
-if (!common.isLinux) {
-  common.skip('Linux only test');
+if (!fs.existsSync('/dev/stdin')) {
+  common.skip('Only test on platforms having /dev/stdin');
 }
 
 const child_process = require('child_process');
 const assert = require('assert');
-const fs = require('fs');
 
 const cp = child_process.execSync(`printf 'console.log(1)' | "${process.execPath}" /dev/stdin`, { stdio: 'pipe' });
 assert.strictEqual(cp.toString(), '1\n');
 
-if (fs.existsSync('/tmp/stdin')) {
-  fs.unlinkSync('/tmp/stdin');
-}
-fs.symlinkSync('/dev/stdin', '/tmp/stdin');
-const cp2 = child_process.execSync(`printf 'console.log(2)' | "${process.execPath}" /tmp/stdin`, { stdio: 'pipe' });
+tmpdir.refresh();
+const tmp = tmpdir.resolve('./stdin');
+fs.symlinkSync('/dev/stdin', tmp);
+const cp2 = child_process.execSync(`printf 'console.log(2)' | "${process.execPath}" ${tmp}`, { stdio: 'pipe' });
 assert.strictEqual(cp2.toString(), '2\n');
-fs.unlinkSync('/tmp/stdin');
