66b6cc1f28d987974bd962285d6ceb1bf4b447d6
Aviv Keller <38299977+RedYetiDev@users.noreply.github.com>
2024-08-22 16:47:57 -0400

    repl: catch `\v` and `\r` in new-line detection

---
+++ test/parallel/test-repl-preview-newlines.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('../common');
+const ArrayStream = require('../common/arraystream');
+const assert = require('assert');
+const repl = require('repl');
+
+const inputStream = new ArrayStream();
+const outputStream = new ArrayStream();
+repl.start({
+  input: inputStream,
+  output: outputStream,
+  useGlobal: false,
+  terminal: true,
+  useColors: true
+});
+
+let output = '';
+outputStream.write = (chunk) => output += chunk;
+
+const testChars = ['\\n', '\\v', '\\r'];
+
+for (const test of testChars) {
+  inputStream.emit('data', `"${test}"()`);
+  // Make sure the output is on a single line
+  assert.strictEqual(output, `"${test}"()\n\x1B[90mTypeError: "\x1B[39m\x1B[9G\x1B[1A`);
+  inputStream.run(['']);
+  output = '';
+}
