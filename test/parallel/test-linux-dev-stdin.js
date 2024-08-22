de425014923c7034e6d287f28fd6f15509099e5d
CGQAQ <m.jason.liu@outlook.com>
2024-08-22 08:59:38 +0800

    use node:test to split up tests

--- test/parallel/test-linux-dev-stdin.js
+++ test/parallel/test-linux-dev-stdin.js
@@ -14,18 +14,24 @@ if (!fs.existsSync('/dev/stdin')) {
 
 const child_process = require('child_process');
 const assert = require('assert');
+const { describe, it } = require('node:test');
 
-// Test reading sourcecode from /dev/stdin
-const cp = child_process.execSync(`printf 'console.log(1)' | "${process.execPath}" /dev/stdin`, { stdio: 'pipe' });
-assert.strictEqual(cp.toString(), '1\n');
+describe('Test reading SourceCode from stdin and it\'s symlinks', () => {
+  it('Test reading sourcecode from /dev/stdin', () => {
+    const cp = child_process.execSync(`printf 'console.log(1)' | "${process.execPath}" /dev/stdin`, { stdio: 'pipe' });
+    assert.strictEqual(cp.toString(), '1\n');
+  });
 
-// Test reading sourcecode from a symlink to /dev/stdin
-tmpdir.refresh();
-const tmp = tmpdir.resolve('./stdin');
-fs.symlinkSync('/dev/stdin', tmp);
-const cp2 = child_process.execSync(`printf 'console.log(2)' | "${process.execPath}" ${tmp}`, { stdio: 'pipe' });
-assert.strictEqual(cp2.toString(), '2\n');
+  it('Test reading sourcecode from a symlink to /dev/stdin', () => {
+    tmpdir.refresh();
+    const tmp = tmpdir.resolve('./stdin');
+    fs.symlinkSync('/dev/stdin', tmp);
+    const cp2 = child_process.execSync(`printf 'console.log(2)' | "${process.execPath}" ${tmp}`, { stdio: 'pipe' });
+    assert.strictEqual(cp2.toString(), '2\n');
+  });
 
-// Test reading sourcecode from a symlink to the `readlink /dev/stdin`
-const cp3 = child_process.execSync(`printf 'console.log(3)' | "${process.execPath}" $(readlink /dev/stdin)`, { stdio: 'pipe' });
-assert.strictEqual(cp3.toString(), '3\n');
+  it('Test reading sourcecode from a symlink to the `readlink /dev/stdin`', () => {
+    const cp3 = child_process.execSync(`printf 'console.log(3)' | "${process.execPath}" $(readlink /dev/stdin)`, { stdio: 'pipe' });
+    assert.strictEqual(cp3.toString(), '3\n');
+  });
+});
