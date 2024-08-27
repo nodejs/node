9fd217daa9b85345d33f90855f903c7e2d2a2831
Marco Ippolito <marcoippolito54@gmail.com>
2024-08-27 13:46:11 +0200

    src: add config file support
    PR-URL: https://github.com/nodejs/node/pull/57016
    Refs: https://github.com/nodejs/node/issues/53787
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Tierney Cyren <hello@bnb.im>
    Reviewed-By: Michael Dawson <midawson@redhat.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Paolo Insogna <paolo@cowtech.it>

--- src/node_options.h
+++ src/node_options.h
@@ -258,6 +258,7 @@ class EnvironmentOptions : public Options {
 
   bool report_exclude_env = false;
   bool report_exclude_network = false;
+  std::string experimental_config_file;
 
   inline DebugOptions* get_debug_options() { return &debug_options_; }
   inline const DebugOptions& debug_options() const { return debug_options_; }
@@ -390,6 +391,7 @@ enum OptionType {
   kHostPort,
   kStringList,
 };
+std::unordered_map<std::string, OptionType> MapEnvOptionsFlagInputType();
 
 template <typename Options>
 class OptionsParser {
@@ -570,6 +572,10 @@ class OptionsParser {
   friend void GetCLIOptionsInfo(
       const v8::FunctionCallbackInfo<v8::Value>& args);
   friend std::string GetBashCompletion();
+  friend std::unordered_map<std::string, OptionType>
+  MapEnvOptionsFlagInputType();
+  friend void GetEnvOptionsInputType(
+      const v8::FunctionCallbackInfo<v8::Value>& args);
 };
 
 using StringVector = std::vector<std::string>;
