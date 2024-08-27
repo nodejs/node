76441a74d3f57db9ba19c5c5591ac82cf4fadea8
Joyee Cheung <joyeec9h3@gmail.com>
2024-08-27 17:49:25 +0200

    module: remove bogus assertion in CJS entrypoint handling with --import
    The synchronous CJS translator can handle entrypoints now, this
    can be hit when --import is used, so lift the bogus assertions and
    added tests.

--- test/es-module/test-require-module-preload.js
+++ test/es-module/test-require-module-preload.js
@@ -6,65 +6,98 @@ const fixtures = require('../common/fixtures');
 
 const stderr = /ExperimentalWarning: Support for loading ES Module in require/;
 
-// Test named exports.
-{
-  spawnSyncAndExitWithoutError(
-    process.execPath,
-    [ '--experimental-require-module', '-r', fixtures.path('../fixtures/es-module-loaders/module-named-exports.mjs') ],
-    {
-      stderr,
-    }
-  );
+function testPreload(preloadFlag) {
+  // Test named exports.
+  {
+    spawnSyncAndExitWithoutError(
+      process.execPath,
+      [
+        '--experimental-require-module',
+        preloadFlag,
+        fixtures.path('es-module-loaders/module-named-exports.mjs'),
+        fixtures.path('printA.js'),
+      ],
+      {
+        stdout: 'A',
+        stderr,
+        trim: true,
+      }
+    );
+  }
+
+  // Test ESM that import ESM.
+  {
+    spawnSyncAndExitWithoutError(
+      process.execPath,
+      [
+        '--experimental-require-module',
+        preloadFlag,
+        fixtures.path('es-modules/import-esm.mjs'),
+        fixtures.path('printA.js'),
+      ],
+      {
+        stderr,
+        stdout: /^world\s+A$/,
+        trim: true,
+      }
+    );
+  }
+
+  // Test ESM that import CJS.
+  {
+    spawnSyncAndExitWithoutError(
+      process.execPath,
+      [
+        '--experimental-require-module',
+        preloadFlag,
+        fixtures.path('es-modules/cjs-exports.mjs'),
+        fixtures.path('printA.js'),
+      ],
+      {
+        stdout: /^ok\s+A$/,
+        stderr,
+        trim: true,
+      }
+    );
+  }
+
+  // Test ESM that require() CJS.
+  // Can't use the common/index.mjs here because that checks the globals, and
+  // -r injects a bunch of globals.
+  {
+    spawnSyncAndExitWithoutError(
+      process.execPath,
+      [
+        '--experimental-require-module',
+        preloadFlag,
+        fixtures.path('es-modules/require-cjs.mjs'),
+        fixtures.path('printA.js'),
+      ],
+      {
+        stdout: /^world\s+A$/,
+        stderr,
+        trim: true,
+      }
+    );
+  }
 }
 
-// Test ESM that import ESM.
-{
-  spawnSyncAndExitWithoutError(
-    process.execPath,
-    [ '--experimental-require-module', '-r', fixtures.path('../fixtures/es-modules/import-esm.mjs') ],
-    {
-      stderr,
-      stdout: 'world',
-      trim: true,
-    }
-  );
-}
+testPreload('--require');
+testPreload('--import');
 
-// Test ESM that import CJS.
+// Test "type": "module" and "main" field in package.json, this is only for --require because
+// --import does not support extension-less preloads.
 {
   spawnSyncAndExitWithoutError(
     process.execPath,
-    [ '--experimental-require-module', '-r', fixtures.path('../fixtures/es-modules/cjs-exports.mjs') ],
+    [
+      '--experimental-require-module',
+      '--require',
+      fixtures.path('es-modules/package-type-module'),
+      fixtures.path('printA.js'),
+    ],
     {
-      stdout: 'ok',
-      stderr,
-      trim: true,
-    }
-  );
-}
-
-// Test ESM that require() CJS.
-// Can't use the common/index.mjs here because that checks the globals, and
-// -r injects a bunch of globals.
-{
-  spawnSyncAndExitWithoutError(
-    process.execPath,
-    [ '--experimental-require-module', '-r', fixtures.path('../fixtures/es-modules/require-cjs.mjs') ],
-    {
-      stdout: 'world',
-      stderr,
-      trim: true,
-    }
-  );
-}
-
-// Test "type": "module" and "main" field in package.json.
-{
-  spawnSyncAndExitWithoutError(
-    process.execPath,
-    [ '--experimental-require-module', '-r', fixtures.path('../fixtures/es-modules/package-type-module') ],
-    {
-      stdout: 'package-type-module',
+      stdout: /^package-type-module\s+A$/,
       stderr,
       trim: true,
     }
