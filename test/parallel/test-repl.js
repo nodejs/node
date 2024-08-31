1b725b5a72e87717ff7689bba57b825d475ba717
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2024-08-31 19:16:27 -0400

    Revert "console: colorize console error and warn"
    This reverts commit a833c9e0bed85214884d6650fe344d5896c9e67a.

--- test/parallel/test-repl.js
+++ test/parallel/test-repl.js
@@ -772,7 +772,6 @@ const errorTests = [
       'Object [console] {',
       '  log: [Function: log],',
       '  warn: [Function: warn],',
-      '  error: [Function: error],',
       '  dir: [Function: dir],',
       '  time: [Function: time],',
       '  timeEnd: [Function: timeEnd],',
@@ -788,6 +787,7 @@ const errorTests = [
       / {2}debug: \[Function: (debug|log)],/,
       / {2}info: \[Function: (info|log)],/,
       / {2}dirxml: \[Function: (dirxml|log)],/,
+      / {2}error: \[Function: (error|warn)],/,
       / {2}groupCollapsed: \[Function: (groupCollapsed|group)],/,
       / {2}Console: \[Function: Console],?/,
       ...process.features.inspector ? [
