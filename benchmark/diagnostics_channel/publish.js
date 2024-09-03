6c14731588d6921e2e14192b01545844fefba92b
RafaelGSS <rafael.nunu@hotmail.com>
2024-09-03 17:13:42 -0300

    benchmark: enhance dc publish benchmark

--- benchmark/diagnostics_channel/publish.js
+++ benchmark/diagnostics_channel/publish.js
@@ -1,27 +1,44 @@
 'use strict';
 const common = require('../common.js');
-const dc = require('diagnostics_channel');
+const dc = require('node:diagnostics_channel');
 
 const bench = common.createBenchmark(main, {
   n: [1e8],
+  checkSubscribers: [1, 0],
+  objSize: [10, 1e2, 1e3],
   subscribers: [0, 1, 10],
 });
 
 function noop() {}
 
-function main({ n, subscribers }) {
+function createObj(size) {
+  return Array.from({ length: size }, (n) => ({
+    foo: 'yarp',
+    nope: {
+      bar: '123',
+      a: [1, 2, 3],
+      baz: n,
+      c: {},
+      b: [],
+    },
+  }));
+}
+
+function main({ n, subscribers, checkSubscribers, objSize }) {
   const channel = dc.channel('test');
   for (let i = 0; i < subscribers; i++) {
     channel.subscribe(noop);
   }
 
-  const data = {
-    foo: 'bar',
-  };
+  const data = createObj(objSize);
 
   bench.start();
   for (let i = 0; i < n; i++) {
-    if (channel.hasSubscribers) {
+    if (checkSubscribers) {
+      if(channel.hasSubscribers) {
+        channel.publish(data);
+      }
+    } else {
       channel.publish(data);
     }
   }
