c82ac3ce3ed9c194e5ac98e91c9896bfc7b22743
Joyee Cheung <joyeec9h3@gmail.com>
2024-09-04 20:09:56 +0200

    module: write compile cache to temporary file and then rename it
    This works better in terms of avoiding race conditions.

--- src/compile_cache.h
+++ src/compile_cache.h
@@ -30,6 +30,8 @@ struct CompileCacheEntry {
   std::string source_filename;
   CachedCodeType type;
   bool refreshed = false;
+  bool persisted = false;
+
   // Copy the cache into a new store for V8 to consume. Caller takes
   // ownership.
   v8::ScriptCompiler::CachedData* CopyCache() const;
