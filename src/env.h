9a73aa0d15b82c3e6730980fddcaccab1bbed34e
Joyee Cheung <joyeec9h3@gmail.com>
2024-09-04 20:09:56 +0200

    module: write compile cache to temporary file and then rename it
    This works better in terms of avoiding race conditions.
    
    PR-URL: https://github.com/nodejs/node/pull/54971
    Fixes: https://github.com/nodejs/node/issues/54770
    Fixes: https://github.com/nodejs/node/issues/54465
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

--- src/env.h
+++ src/env.h
@@ -1041,6 +1041,7 @@ class Environment final : public MemoryRetainer {
   // Enable built-in compile cache if it has not yet been enabled.
   // The cache will be persisted to disk on exit.
   CompileCacheEnableResult EnableCompileCache(const std::string& cache_dir);
+  void FlushCompileCache();
 
   void RunAndClearNativeImmediates(bool only_refed = false);
   void RunAndClearInterrupts();
