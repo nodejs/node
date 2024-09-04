a6a8888e611939bc789fe5415c891a7d91da3f52
Aviv Keller <redyetidev@gmail.com>
2024-09-03 21:16:22 -0400

    Update dotenv.js

--- benchmark/misc/dotenv.js
+++ benchmark/misc/dotenv.js
@@ -2,16 +2,18 @@
 
 const { createBenchmark } = require('../common.js');
 const path = require('path');
+const fs = require('fs');
+const util = reuire('util');
 
 const bench = createBenchmark(main, {
   n: 3e4,
 });
-const file = path.resolve(__dirname, '../fixtures/valid.env');
+const env = fs.readFileSync(path.resolve(__dirname, '../fixtures/valid.env'));
 
 function main({ n }) {
   bench.start();
   for (let i = 0; i < n; i++) {
-    process.loadEnvFile(file);
+    util.parseEnv(file);
   }
   bench.end(n);
 }
