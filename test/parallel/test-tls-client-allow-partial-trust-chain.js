5f501b457a587e65bbaf90d5c539449d5ea5c260
Anna Henningsen <anna@addaleax.net>
2024-09-05 17:56:02 +0200

    fixup! fixup! tls: add `allowPartialTrustChain` flag

--- test/parallel/test-tls-client-allow-partial-trust-chain.js
+++ test/parallel/test-tls-client-allow-partial-trust-chain.js
@@ -10,35 +10,46 @@ const tls = require('tls');
 const fixtures = require('../common/fixtures');
 
 // agent6-cert.pem is signed by intermediate cert of ca3.
-// The server has a cert chain of agent6->ca3->ca1(root) but
+// The server has a cert chain of agent6->ca3->ca1(root).
 
-async function test() {
-  const server = tls.createServer({
-    ca: fixtures.readKey('ca3-cert.pem'),
-    key: fixtures.readKey('agent6-key.pem'),
-    cert: fixtures.readKey('agent6-cert.pem'),
-  }, (socket) => socket.resume());
-  server.listen(0);
-  await once(server, 'listening');
+const { it, beforeEach, afterEach, describe } = require('node:test');
 
-  const opts = {
-    port: server.address().port,
-    ca: fixtures.readKey('ca3-cert.pem'),
-    checkServerIdentity() {}
-  };
+describe('allowPartialTrustChain', function() {
+  let server;
+  let client;
+  let opts;
 
-  // Connecting succeeds with allowPartialTrustChain: true
-  const client = tls.connect({ ...opts, allowPartialTrustChain: true });
-  await once(client, 'secureConnect');
-  client.destroy();
+  beforeEach(async function() {
+    server = tls.createServer({
+      ca: fixtures.readKey('ca3-cert.pem'),
+      key: fixtures.readKey('agent6-key.pem'),
+      cert: fixtures.readKey('agent6-cert.pem'),
+    }, (socket) => socket.resume());
+    server.listen(0);
+    await once(server, 'listening');
 
-  // Consistency check: Connecting fails without allowPartialTrustChain: true
-  await assert.rejects(async () => {
-    const client = tls.connect(opts);
-    await once(client, 'secureConnect');
-  }, { code: 'UNABLE_TO_GET_ISSUER_CERT' });
+    opts = {
+      port: server.address().port,
+      ca: fixtures.readKey('ca3-cert.pem'),
+      checkServerIdentity() {}
+    };
+  });
 
-  server.close();
-}
+  afterEach(async function() {
+    client?.destroy();
+    server?.close();
+  });
 
-test().catch((err) => process.nextTick(() => { throw err; }));
+  it('can connect successfully with allowPartialTrustChain: true', async function() {
+    client = tls.connect({ ...opts, allowPartialTrustChain: true });
+    await once(client, 'secureConnect'); // Should not throw
+  });
+
+  it('fails without with allowPartialTrustChain: true for an intermediate cert in the CA', async function() {
+    // Consistency check: Connecting fails without allowPartialTrustChain: true
+    await assert.rejects(async () => {
+      const client = tls.connect(opts);
+      await once(client, 'secureConnect');
+    }, { code: 'UNABLE_TO_GET_ISSUER_CERT' });
+  });
+});
