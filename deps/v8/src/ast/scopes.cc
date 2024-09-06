684f425afaa2388d93056401b75bdd8d2a76a841
Giancarlo Cordero Ortiz <giancarlo.cordero.ortiz@sap.com>
2024-09-06 11:30:30 -0400

    Revert "deps: V8: cherry-pick 6e5e1053fa61"
    This reverts commit 7d888ffa61944c162ec63fd8302c7cca20befbe0.

--- deps/v8/src/ast/scopes.cc
+++ deps/v8/src/ast/scopes.cc
@@ -2397,7 +2397,7 @@ bool Scope::MustAllocate(Variable* var) {
     var->set_is_used();
     if (inner_scope_calls_eval_ && !var->is_this()) var->SetMaybeAssigned();
   }
-  CHECK(!var->has_forced_context_allocation() || var->is_used());
+  DCHECK(!var->has_forced_context_allocation() || var->is_used());
   // Global variables do not need to be allocated.
   return !var->IsGlobalObjectProperty() && var->is_used();
 }
