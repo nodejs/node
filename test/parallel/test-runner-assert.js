0a06a62b486758fcc3d2f1653be85d5a41c418e7
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2024-09-12 15:07:50 -0400

    test_runner: reimplement `assert.ok` to allow stack parsing

--- test/parallel/test-runner-assert.js
+++ test/parallel/test-runner-assert.js
@@ -24,3 +24,7 @@ test('only methods from node:assert are on t.assert', (t) => {
     'throws',
   ]);
 });
+
+test('t.assert.ok correctly parses the stacktrace', (t) => {
+  t.assert.throws(() => t.assert.ok(1 === 2), /t\.assert\.ok\(1 === 2\)/);
+});
