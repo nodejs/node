43ef1e4d2d82730a8c389f1b5daf749db6d8259c
sonsurim <surim014@naver.com>
2024-09-12 23:30:08 +0900

    reflected review in glob benchmark

--- benchmark/fs/bench-glob.js
+++ benchmark/fs/bench-glob.js
@@ -7,25 +7,23 @@ const path = require('path');
 const benchmarkDirectory = path.resolve(__dirname, '..', '..');
 
 const configs = {
-  n: [1, 10, 100],
+  n: [1e3],
   dir: ['lib', 'test/parallel', 'benchmark'],
   pattern: ['**/*', '*.js', '**/**.js'],
-  mode: ['async', 'sync'],
-  recursive: ['true', 'false'],
+  useAsync: [true, false],
+  recursive: [true, false],
 };
 
 const bench = common.createBenchmark(main, configs);
 
 async function main(config) {
   const fullPath = path.resolve(benchmarkDirectory, config.dir);
-  const { pattern, recursive, mode } = config;
+  const { pattern, recursive, useAsync } = config;
 
   bench.start();
 
-  let counter = 0;
-
   for (let i = 0; i < config.n; i++) {
-    if (mode === 'async') {
+    if (useAsync) {
       const matches = await new Promise((resolve, reject) => {
         fs.glob(pattern, { cwd: fullPath, recursive }, (err, files) => {
           if (err) {
@@ -35,13 +33,10 @@ async function main(config) {
           };
         });
       });
-
-      counter += matches.length;
     } else {
       const matches = fs.globSync(pattern, { cwd: fullPath, recursive });
-      counter += matches.length;
     }
   }
 
-  bench.end(counter);
+  bench.end(config.n);
 }
