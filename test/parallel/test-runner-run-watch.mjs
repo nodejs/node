23fb03beed4a2c79712b21cb7f61c3e0231c5999
Wuli <zuowuli@gmail.com>
2024-09-15 17:02:56 +0930

    test: remove duplicate skip AIX
    PR-URL: https://github.com/nodejs/node/pull/54917
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Xuguang Mei <meixuguang@gmail.com>
    Reviewed-By: Jake Yuesong Li <jake.yuesong@gmail.com>

--- test/parallel/test-runner-run-watch.mjs
+++ test/parallel/test-runner-run-watch.mjs
@@ -199,7 +199,7 @@ describe('test runner watch mode', () => {
     await testWatch({ fileToUpdate: 'test.js', action: 'rename' });
   });
 
-  it('should not throw when deleting a watched test file', { skip: common.isAIX }, async () => {
+  it('should not throw when deleting a watched test file', async () => {
     await testWatch({ fileToUpdate: 'test.js', action: 'delete' });
   });
 
