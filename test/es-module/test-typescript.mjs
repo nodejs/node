bb405210c58919a61d8b2dd9577411784bcbed85
Marco Ippolito <marcoippolito54@gmail.com>
2024-09-19 15:36:52 +0200

    test_runner: support typescript module mocking
    PR-URL: https://github.com/nodejs/node/pull/54878
    Fixes: https://github.com/nodejs/node/issues/54428
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Erick Wendel <erick.workspace@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>
    Reviewed-By: Paolo Insogna <paolo@cowtech.it>

--- test/es-module/test-typescript.mjs
+++ test/es-module/test-typescript.mjs
@@ -1,4 +1,4 @@
-import { skip, spawnPromisified } from '../common/index.mjs';
+import { skip, spawnPromisified, isWindows } from '../common/index.mjs';
 import * as fixtures from '../common/fixtures.mjs';
 import { match, strictEqual } from 'node:assert';
 import { test } from 'node:test';
@@ -334,5 +334,22 @@ test('execute a JavaScript file importing a cjs TypeScript file', async () => {
   ]);
   strictEqual(result.stderr, '');
   match(result.stdout, /Hello, TypeScript!/);
+  strictEqual(result.code, 0);
+});
+
+// TODO(marco-ippolito) Due to a bug in SWC, the TypeScript loader
+// does not work on Windows arm64. This test should be re-enabled
+// when https://github.com/nodejs/node/issues/54645 is fixed
+test('execute a TypeScript test mocking module', { skip: isWindows && process.arch === 'arm64' }, async () => {
+  const result = await spawnPromisified(process.execPath, [
+    '--test',
+    '--experimental-test-module-mocks',
+    '--experimental-strip-types',
+    '--no-warnings',
+    fixtures.path('typescript/ts/test-mock-module.ts'),
+  ]);
+  strictEqual(result.stderr, '');
+  match(result.stdout, /Hello, TypeScript-Module!/);
+  match(result.stdout, /Hello, TypeScript-CommonJS!/);
   strictEqual(result.code, 0);
 });
