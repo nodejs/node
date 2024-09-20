fe45be207be1b51b3dde84265aad5fd79e0cfb2b
Yagiz Nizipli <yagiz@nizipli.com>
2024-09-20 13:17:30 -0400

    deps: V8: backport 0d5d6e71bbb0
    Original commit message:
    
        Remove `--js-promise-withresolvers` runtime flag
    
        Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
        Bug: 42204122
        Change-Id: I017a0d1ae0f8225513206ffb7806a4250be75d4c
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5843972
        Reviewed-by: Igor Sheludko <ishell@chromium.org>
        Commit-Queue: Erik Corry <erikcorry@chromium.org>
        Reviewed-by: Shu-yu Guo <syg@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#96215}
    
    Refs: https://github.com/v8/v8/commit/0d5d6e71bbb0d2b0a12567fffa7e3375c427bc92
    PR-URL: https://github.com/nodejs/node/pull/55115
    Reviewed-By: Jiawen Geng <technicalcute@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- deps/v8/src/init/bootstrapper.cc
+++ deps/v8/src/init/bootstrapper.cc
@@ -3228,6 +3228,15 @@ void Genesis::InitializeGlobal(Handle<JSGlobalObject> global_object,
     InstallFunctionWithBuiltinId(isolate_, promise_fun, "reject",
                                  Builtin::kPromiseReject, 1, true);
 
+    std::array<Handle<Name>, 3> fields{factory->promise_string(),
+                                       factory->resolve_string(),
+                                       factory->reject_string()};
+    DirectHandle<Map> result_map =
+        CreateLiteralObjectMapFromCache(isolate_, fields);
+    native_context()->set_promise_withresolvers_result_map(*result_map);
+    InstallFunctionWithBuiltinId(isolate_, promise_fun, "withResolvers",
+                                 Builtin::kPromiseWithResolvers, 0, true);
+
     SetConstructorInstanceType(isolate_, promise_fun,
                                JS_PROMISE_CONSTRUCTOR_TYPE);
 
@@ -5502,24 +5511,6 @@ void Genesis::InitializeGlobal_js_promise_try() {
       handle(native_context()->promise_function(), isolate());
   InstallFunctionWithBuiltinId(isolate(), promise_fun, "try",
                                Builtin::kPromiseTry, 1, false);
-}
-
-void Genesis::InitializeGlobal_js_promise_withresolvers() {
-  if (!v8_flags.js_promise_withresolvers) return;
-
-  Factory* factory = isolate()->factory();
-
-  std::array<Handle<Name>, 3> fields{factory->promise_string(),
-                                     factory->resolve_string(),
-                                     factory->reject_string()};
-  DirectHandle<Map> result_map =
-      CreateLiteralObjectMapFromCache(isolate(), fields);
-  native_context()->set_promise_withresolvers_result_map(*result_map);
-
-  Handle<JSFunction> promise_fun =
-      handle(native_context()->promise_function(), isolate());
-  InstallFunctionWithBuiltinId(isolate(), promise_fun, "withResolvers",
-                               Builtin::kPromiseWithResolvers, 0, true);
 }
 
 void Genesis::InitializeGlobal_harmony_set_methods() {
