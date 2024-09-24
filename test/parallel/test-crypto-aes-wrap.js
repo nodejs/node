79cef2874470d3943963c80e5a0337101130fc35
James M Snell <jasnell@gmail.com>
2024-09-24 16:17:11 -0700

    test: update test-crypto-aes-wrap to use node:test

--- test/parallel/test-crypto-aes-wrap.js
+++ test/parallel/test-crypto-aes-wrap.js
@@ -1,62 +1,69 @@
 'use strict';
 const common = require('../common');
-if (!common.hasCrypto)
-  common.skip('missing crypto');
+const { describe, it } = require('node:test');
 
-const assert = require('assert');
-const crypto = require('crypto');
+// Disabling the unnecessary lint rule here. We're testing the common.hasCrypto
+// in the skip option of the describe block.
+/* eslint-disable node-core/crypto-check */
 
-const test = [
-  {
-    algorithm: 'aes128-wrap',
-    key: 'b26f309fbe57e9b3bb6ae5ef31d54450',
-    iv: '3fd838af4093d749',
-    text: '12345678123456781234567812345678'
-  },
-  {
-    algorithm: 'id-aes128-wrap-pad',
-    key: 'b26f309fbe57e9b3bb6ae5ef31d54450',
-    iv: '3fd838af',
-    text: '12345678123456781234567812345678123'
-  },
-  {
-    algorithm: 'aes192-wrap',
-    key: '40978085d68091f7dfca0d7dfc7a5ee76d2cc7f2f345a304',
-    iv: '3fd838af4093d749',
-    text: '12345678123456781234567812345678'
-  },
-  {
-    algorithm: 'id-aes192-wrap-pad',
-    key: '40978085d68091f7dfca0d7dfc7a5ee76d2cc7f2f345a304',
-    iv: '3fd838af',
-    text: '12345678123456781234567812345678123'
-  },
-  {
-    algorithm: 'aes256-wrap',
-    key: '29c9eab5ed5ad44134a1437fe2e673b4d88a5b7c72e68454fea08721392b7323',
-    iv: '3fd838af4093d749',
-    text: '12345678123456781234567812345678'
-  },
-  {
-    algorithm: 'id-aes256-wrap-pad',
-    key: '29c9eab5ed5ad44134a1437fe2e673b4d88a5b7c72e68454fea08721392b7323',
-    iv: '3fd838af',
-    text: '12345678123456781234567812345678123'
-  },
-];
+describe('aes wrap', { skip: !common.hasCrypto }, () => {
+  const crypto = require('crypto');
+  const test = [
+    {
+      algorithm: 'aes128-wrap',
+      key: 'b26f309fbe57e9b3bb6ae5ef31d54450',
+      iv: '3fd838af4093d749',
+      text: '12345678123456781234567812345678'
+    },
+    {
+      algorithm: 'id-aes128-wrap-pad',
+      key: 'b26f309fbe57e9b3bb6ae5ef31d54450',
+      iv: '3fd838af',
+      text: '12345678123456781234567812345678123'
+    },
+    {
+      algorithm: 'aes192-wrap',
+      key: '40978085d68091f7dfca0d7dfc7a5ee76d2cc7f2f345a304',
+      iv: '3fd838af4093d749',
+      text: '12345678123456781234567812345678'
+    },
+    {
+      algorithm: 'id-aes192-wrap-pad',
+      key: '40978085d68091f7dfca0d7dfc7a5ee76d2cc7f2f345a304',
+      iv: '3fd838af',
+      text: '12345678123456781234567812345678123'
+    },
+    {
+      algorithm: 'aes256-wrap',
+      key: '29c9eab5ed5ad44134a1437fe2e673b4d88a5b7c72e68454fea08721392b7323',
+      iv: '3fd838af4093d749',
+      text: '12345678123456781234567812345678'
+    },
+    {
+      algorithm: 'id-aes256-wrap-pad',
+      key: '29c9eab5ed5ad44134a1437fe2e673b4d88a5b7c72e68454fea08721392b7323',
+      iv: '3fd838af',
+      text: '12345678123456781234567812345678123'
+    },
+  ];
 
-test.forEach((data) => {
-  const cipher = crypto.createCipheriv(
-    data.algorithm,
-    Buffer.from(data.key, 'hex'),
-    Buffer.from(data.iv, 'hex'));
-  const ciphertext = cipher.update(data.text, 'utf8');
+  test.forEach((data) => {
+    it(`${data.algorithm}`, async (t) => {
+      const cipher = crypto.createCipheriv(
+        data.algorithm,
+        Buffer.from(data.key, 'hex'),
+        Buffer.from(data.iv, 'hex'));
+      const ciphertext = cipher.update(data.text, 'utf8');
 
-  const decipher = crypto.createDecipheriv(
-    data.algorithm,
-    Buffer.from(data.key, 'hex'),
-    Buffer.from(data.iv, 'hex'));
-  const msg = decipher.update(ciphertext, 'buffer', 'utf8');
+      const decipher = crypto.createDecipheriv(
+        data.algorithm,
+        Buffer.from(data.key, 'hex'),
+        Buffer.from(data.iv, 'hex'));
+      const msg = decipher.update(ciphertext, 'buffer', 'utf8');
 
-  assert.strictEqual(msg, data.text, `${data.algorithm} test case failed`);
+      t.assert.strictEqual(msg, data.text);
+    });
+  });
 });
+
+/* eslint-enable node-core/crypto-check */
