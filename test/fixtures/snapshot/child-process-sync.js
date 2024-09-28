ad045ab4ba05e006262f1127d57fdbce3541370f
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-09-28 14:41:24 +0200

    fixup! test: add `escapePOSIXShell` util

--- test/fixtures/snapshot/child-process-sync.js
+++ test/fixtures/snapshot/child-process-sync.js
@@ -5,31 +5,10 @@ const {
   isBuildingSnapshot
 } = require('v8').startupSnapshot;
 
-/**
- * @see {import('../../common').escapePOSIXShell}
- */
-function escapePOSIXShell(cmdParts, ...args) {
-  if (process.platform === 'win32') {
-    // On Windows, paths cannot contain `"`, so we can return the string unchanged.
-    return [String.raw({ raw: cmdParts }, ...args)];
-  }
-  // On POSIX shells, we can pass values via the env, as there's a standard way for referencing a variable.
-  const env = { ...process.env };
-  let cmd = cmdParts[0];
-  for (let i = 0; i < args.length; i++) {
-    const envVarName = `ESCAPED_${i}`;
-    env[envVarName] = args[i];
-    cmd += '${' + envVarName + '}' + cmdParts[i + 1];
-  }
-
-  return [cmd, { env }];
-}
-
 function spawn() {
   const { spawnSync, execFileSync, execSync } = require('child_process');
   spawnSync(process.execPath, [ __filename, 'spawnSync' ], { stdio: 'inherit' });
-  const [cmd, opts] = escapePOSIXShell`"${process.execPath}" "${__filename}" "execSync"`;
-  execSync(cmd, { ...opts, stdio: 'inherit'});
+  execSync(`"${process.execPath}" "${__filename}" "execSync"`, { stdio: 'inherit' });
   execFileSync(process.execPath, [ __filename, 'execFileSync' ], { stdio: 'inherit' });
 }
 
