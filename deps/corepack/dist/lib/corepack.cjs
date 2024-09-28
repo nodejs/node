7ed42aaad581c35a2d9524e81c93cc092fcbee93
chlorine <nicksonlvqq@gmail.com>
2024-09-28 05:37:33 +0000

    doc: update directories to directory in corepack enable --help

--- deps/corepack/dist/lib/corepack.cjs
+++ deps/corepack/dist/lib/corepack.cjs
@@ -22553,7 +22553,7 @@ var EnableCommand = class extends Command {
     [`enable`]
   ];
   static usage = Command.Usage({
-    description: `Add the Corepack shims to the install directories`,
+    description: `Add the Corepack shims to the install directory`,
     details: `
       When run, this command will check whether the shims for the specified package managers can be found with the correct values inside the install directory. If not, or if they don't exist, they will be created.
 
