6c6562ce8b5276976bec4bb5bb1ca7c551f9c64c
Maël Nison <nison.mael@gmail.com>
2024-09-30 11:52:08 +0200

    http2: expose nghttp2_option_set_stream_reset_rate_limit as an option
    PR-URL: https://github.com/nodejs/node/pull/54875
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>

--- src/node_http2.cc
+++ src/node_http2.cc
@@ -208,6 +208,14 @@ Http2Options::Http2Options(Http2State* http2_state, SessionType type) {
         option,
         static_cast<size_t>(buffer[IDX_OPTIONS_MAX_SETTINGS]));
   }
+
+  if ((flags & (1 << IDX_OPTIONS_STREAM_RESET_BURST)) &&
+      (flags & (1 << IDX_OPTIONS_STREAM_RESET_RATE))) {
+    nghttp2_option_set_stream_reset_rate_limit(
+        option,
+        static_cast<uint64_t>(buffer[IDX_OPTIONS_STREAM_RESET_BURST]),
+        static_cast<uint64_t>(buffer[IDX_OPTIONS_STREAM_RESET_RATE]));
+  }
 }
 
 #define GRABSETTING(entries, count, name)                                      \
