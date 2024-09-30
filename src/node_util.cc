dfbf3b9eb228f379a11d37995dc695dd706ec7db
RafaelGSS <rafael.nunu@hotmail.com>
2024-09-30 08:46:24 -0300

    src: apply getCallSite optimization

--- src/node_util.cc
+++ src/node_util.cc
@@ -23,6 +23,7 @@ using v8::Isolate;
 using v8::KeyCollectionMode;
 using v8::Local;
 using v8::LocalVector;
+using v8::Name;
 using v8::Object;
 using v8::ObjectTemplate;
 using v8::ONLY_CONFIGURABLE;
@@ -262,28 +263,25 @@ static void GetCallSite(const FunctionCallbackInfo<Value>& args) {
 
   // Frame 0 is node:util. It should be skipped.
   for (int i = 1; i < frame_count; ++i) {
-    Local<Object> obj = Object::New(isolate);
     Local<StackFrame> stack_frame = stack->GetFrame(isolate, i);
 
     Utf8Value function_name(isolate, stack_frame->GetFunctionName());
     Utf8Value script_name(isolate, stack_frame->GetScriptName());
 
-    obj->Set(env->context(),
-             env->function_name_string(),
-             String::NewFromUtf8(isolate, *function_name).ToLocalChecked())
-        .Check();
-    obj->Set(env->context(),
-             env->script_name_string(),
-             String::NewFromUtf8(isolate, *script_name).ToLocalChecked())
-        .Check();
-    obj->Set(env->context(),
-             env->line_number_string(),
-             Integer::NewFromUnsigned(isolate, stack_frame->GetLineNumber()))
-        .Check();
-    obj->Set(env->context(),
-             env->column_string(),
-             Integer::NewFromUnsigned(isolate, stack_frame->GetColumn()))
-        .Check();
+    Local<Name> names[] = {
+        env->function_name_string(),
+        env->script_name_string(),
+        env->line_number_string(),
+        env->column_string(),
+    };
+    Local<Value> values[] = {
+        String::NewFromUtf8(isolate, *function_name).ToLocalChecked(),
+        String::NewFromUtf8(isolate, *script_name).ToLocalChecked(),
+        Integer::NewFromUnsigned(isolate, stack_frame->GetLineNumber()),
+        Integer::NewFromUnsigned(isolate, stack_frame->GetColumn()),
+    };
+    Local<Object> obj = Object::New(
+        isolate, v8::Null(isolate), names, values, arraysize(names));
 
     callsite_objects.push_back(obj);
   }
