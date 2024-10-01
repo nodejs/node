d4ede548a5714a7a0bca31a4dc6eb88d9bcbd0c6
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-10-01 22:54:01 +0200

    fixup: add some tests cases

--- test/parallel/test-get-package-json.js
+++ test/parallel/test-get-package-json.js
@@ -5,11 +5,16 @@ const assert = require('assert');
 const { getPackageJSON } = require('module');
 
 
-{ // Should throw when no arguments are provided
+{ // Throws when no arguments are provided
   assert.throws(
     () => getPackageJSON(),
-    {
-      code: 'ERR_INVALID_ARG_TYPE'
-    }
-  )
+    { code: 'ERR_INVALID_ARG_TYPE' }
+  );
+}
+
+{ // Throw when `everything` is not a boolean
+  for (const invalid of ['', {}, [], Symbol(), 1, 0, ]) assert.throws(
+    () => getPackageJSON('', invalid),
+    { code: 'ERR_INVALID_ARG_TYPE' }
+  );
 }
