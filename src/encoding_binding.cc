659f1b69e5debb21f1d9d92b76545ab71d1d77f9
Mert Can Altin <mert.altin@trendyol.com>
2024-10-05 19:36:56 +0300

    correct BOM check in decodeLatin1 for uint8_t comparison

--- src/encoding_binding.cc
+++ src/encoding_binding.cc
@@ -267,7 +267,7 @@ void BindingData::DecodeLatin1(const FunctionCallbackInfo<Value>& args) {
   const uint8_t* data = buffer.data();
   size_t length = buffer.length();
 
-  if (ignore_bom && length > 0 && data[0] == 0xFF) {
+  if (ignore_bom && length > 0 && data[0] == 0xFEFF) {
     data++;
     length--;
   }
