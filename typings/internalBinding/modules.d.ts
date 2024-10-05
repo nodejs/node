485b1e365ab5014a89ea4064a37e108d7fc80ee7
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-10-05 12:26:06 +0200

    fixup: remove temp fields & correct type decs

--- typings/internalBinding/modules.d.ts
+++ typings/internalBinding/modules.d.ts
@@ -1,30 +1,32 @@
 export type PackageType = 'commonjs' | 'module' | 'none'
-export type RecognisedPackageConfig = {
-  pjsonPath: URL['pathname']
-  exists: boolean
-  name?: string
+export type RecognizedPackageConfig = {
+  name: string
   main?: any
   type: PackageType
   exports?: string | string[] | Record<string, unknown>
   imports?: string | string[] | Record<string, unknown>
 }
-export type FullPackageConfig = RecognisedPackageConfig & {
+export type FullPackageConfig = RecognizedPackageConfig & {
   [key: string]: unknown,
 }
+export type DeserializedPackageConfig<everything = false> = {
+  data: everything extends true ? FullPackageConfig : RecognizedPackageConfig
+  path: URL['pathname'],
+}
 export type SerializedPackageConfig = [
-  RecognisedPackageConfig['name'],
-  RecognisedPackageConfig['main'],
-  RecognisedPackageConfig['type'],
+  RecognizedPackageConfig['name'],
+  RecognizedPackageConfig['main'],
+  RecognizedPackageConfig['type'],
   string | undefined, // exports
   string | undefined, // imports
-  RecognisedPackageConfig['pjsonPath'], // pjson file path
+  DeserializedPackageConfig['path'], // pjson file path
 ]
 
 export interface ModulesBinding {
   readPackageJSON(path: string): SerializedPackageConfig | undefined;
   getNearestParentPackageJSON(path: string): SerializedPackageConfig | undefined
-  getNearestRawParentPackageJSON(origin: URL['pathname']): [ReturnType<JSON['stringify']>, RecognisedPackageConfig['pjsonPath']] | undefined
-  getNearestParentPackageJSONType(path: string): RecognisedPackageConfig['type']
+  getNearestRawParentPackageJSON(origin: URL['pathname']): [ReturnType<JSON['stringify']>, DeserializedPackageConfig['path']] | undefined
+  getNearestParentPackageJSONType(path: string): RecognizedPackageConfig['type']
   getPackageScopeConfig(path: string): SerializedPackageConfig | undefined
   getPackageJSONScripts(): string | undefined
 }
