6987bb25e1aba7eb6a2891e5f53be4df94912679
Joyee Cheung <joyeec9h3@gmail.com>
2024-10-08 22:26:13 +0200

    module: check --experimental-require-module separately from detection
    Previously we assumed if `--experimental-detect-module` is true, then
    `--experimental-require-module` is true, which isn't the case, as
    the two can be enabled/disabled separately. This patch fixes the
    checks so `--no-experimental-require-module` is still effective when
    `--experimental-detect-module` is enabled.
    
    Drive-by: make the assertion messages more informative and remove
    obsolete TODO about allowing TLA in entrypoints handled by
    require(esm).
    
    PR-URL: https://github.com/nodejs/node/pull/55250
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Jacob Smith <jacob@frende.me>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- test/fixtures/es-modules/loose.js
+++ test/fixtures/es-modules/loose.js
@@ -1,3 +1,5 @@
-// This file can be run or imported only if `--experimental-default-type=module` is set.
+// This file can be run or imported only if `--experimental-default-type=module` is set
+// or `--experimental-detect-module` is not disabled. If it's loaded by
+// require(), then `--experimental-require-module` must not be disabled.
 export default 'module';
 console.log('executed');
