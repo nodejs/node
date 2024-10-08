96dda2e380573506f250daac5906711aac15b451
Santiago Gimeno <santiago.gimeno@gmail.com>
2024-10-08 09:18:15 +0200

    Fixup

--- deps/uv/src/unix/fs.c
+++ deps/uv/src/unix/fs.c
@@ -487,7 +487,10 @@ static ssize_t uv__preadv_or_pwritev(int fd,
     atomic_store_explicit(cache, (uintptr_t) p, memory_order_relaxed);
   }
 
-  f = p;
+  /* Use memcpy instead of `f = p` to work around a compiler bug,
+   * see https://github.com/libuv/libuv/issues/4532
+   */
+  memcpy(&f, &p, sizeof(p));
   return f(fd, bufs, nbufs, off);
 }
 
