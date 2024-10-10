4988bb549e79a6b92391b4ff529f675da32c0687
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-10-10 19:18:20 +0200

    tools: enforce ordering of error codes in `errors.md`
    PR-URL: https://github.com/nodejs/node/pull/55324
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- tools/eslint-rules/documented-errors.js
+++ tools/eslint-rules/documented-errors.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const { isDefiningError } = require('./rules-utils.js');
@@ -12,48 +13,55 @@ const doc = fs.readFileSync(docPath, 'utf8');
 function getErrorsInDoc() {
   const lines = doc.split('\n');
   let currentHeader;
-  const errors = new Map();
+  const errors = new Set();
+  const legacyErrors = new Set();
   const codePattern = /^### `([^`]+)`$/;
   const anchorPattern = /^<a id="([^"]+)"><\/a>$/;
 
-  function parse(line, legacy) {
-    const error = { legacy };
-    let code;
+  let previousAnchor;
 
-    const codeMatch = line.match(codePattern);
-    if (codeMatch) {
-      error.header = true;
-      code = codeMatch[1];
-    }
-
-    const anchorMatch = line.match(anchorPattern);
+  function parse(line, legacy, lineNumber) {
+    const anchorMatch = anchorPattern.exec(line);
     if (anchorMatch) {
-      error.anchor = true;
-      code ??= anchorMatch[1];
+      const code = anchorMatch[1];
+      if (previousAnchor != null && previousAnchor > code) {
+        throw new Error(`Unordered error anchor in ${docPath}:${lineNumber}`, { cause: `${previousAnchor} ≤ ${code}` });
+      }
+      previousAnchor = code;
+      return;
     }
 
-    if (!code) return;
+    const codeMatch = codePattern.exec(line);
+    if (codeMatch == null) return;
 
-    // If the code already exists in the Map, merge the new error data
-    errors.set(code, {
-      ...errors.get(code),
-      ...error,
-    });
+    const code = codeMatch[1];
+    if (previousAnchor == null) {
+      throw new Error(`Missing error anchor in ${docPath}:${lineNumber}`, { cause: code });
+    }
+    assert.strictEqual(code, previousAnchor, `Error anchor do not match with error code in ${docPath}:${lineNumber}`);
+
+    if (legacy && errors.has(code)) {
+      throw new Error(`Error is documented both as legacy and non-legacy in ${docPath}:${lineNumber}`, { cause: code });
+    }
+    (legacy ? legacyErrors : errors).add(code);
   }
 
+  let lineNumber = 0;
   for (const line of lines) {
+    lineNumber++;
     if (line.startsWith('## ')) currentHeader = line.substring(3);
-    if (currentHeader === 'Node.js error codes') parse(line, false);
-    if (currentHeader === 'Legacy Node.js error codes') parse(line, true);
+    if (currentHeader === 'Node.js error codes') parse(line, false, lineNumber);
+    if (line === '## Legacy Node.js error codes') previousAnchor = null;
+    if (currentHeader === 'Legacy Node.js error codes') parse(line, true, lineNumber);
   }
 
-  return errors;
+  return { errors, legacyErrors };
 }
 
 // Main rule export
 module.exports = {
   create(context) {
-    const errors = getErrorsInDoc();
+    const { errors, legacyErrors } = getErrorsInDoc();
     return {
       ExpressionStatement(node) {
         if (!isDefiningError(node)) return;
@@ -61,27 +69,15 @@ module.exports = {
         const code = node.expression.arguments?.[0]?.value;
         if (!code) return;
 
-        const err = errors.get(code); // Use Map's get method to retrieve the error
-
-        if (!err || !err.header) {
-          context.report({
-            node,
-            message: `"${code}" is not documented in doc/api/errors.md`,
-          });
-          if (!err) return;
-        }
-
-        if (!err.anchor) {
-          context.report({
-            node,
-            message: `doc/api/errors.md does not have an anchor for "${code}"`,
-          });
-        }
-
-        if (err.legacy) {
+        if (legacyErrors.has(code)) {
           context.report({
             node,
             message: `"${code}" is marked as legacy, yet it is used in lib/.`,
+          });
+        } else if (!errors.has(code)) {
+          context.report({
+            node,
+            message: `"${code}" is not documented in doc/api/errors.md`,
           });
         }
       },
