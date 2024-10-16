1742549cfc1887ece166fa2b1ec3ce531082ec61
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-10-16 22:26:21 +0200

    fixup!: enable Antoine's test-case

--- test/parallel/test-find-package-json.js
+++ test/parallel/test-find-package-json.js
@@ -4,6 +4,7 @@ const common = require('../common');
 const fixtures = require('../common/fixtures');
 const tmpdir = require('../common/tmpdir')
 const assert = require('node:assert');
+const fs = require('node:fs');
 const { findPackageJSON } = require('node:module');
 const { describe, it } = require('node:test');
 const { pathToFileURL } = require('node:url');
@@ -57,44 +58,51 @@ describe('findPackageJSON', () => { // Throws when no arguments are provided
 
     assert.strictEqual(answer, 43);
   });
+
+  it('should be able to do whatever the heck Antoine created', () => {
+    tmpdir.refresh();
+
+    fs.writeFileSync(tmpdir.resolve('entry.mjs'), `
+      import { findPackageJSON, createRequire } from 'node:module';
+      import fs from 'node:fs';
+      const require = createRequire(import.meta.url);
+
+      const readPJSON = (locus) => JSON.parse(fs.readFileSync(locus, 'utf8'));
+
+      const { secretNumber1 } = readPJSON(findPackageJSON('pkg', import.meta.url));
+      // This is impossible to resolve because pkg's pjson.exports blocks access
+      let secretNumber2Location = NaN;
+      try { secretNumber2Location = findPackageJSON('pkg/subfolder/', import.meta.url) } catch {}
+      const { secretNumber3 } = readPJSON(findPackageJSON('pkg2', import.meta.url));
+      const { secretNumber4 } = readPJSON(findPackageJSON('pkg2/package.json', import.meta.url));
+
+      console.log(secretNumber1, secretNumber2Location, secretNumber3, secretNumber4);
+    `);
+
+    const secretNumber1 = Math.ceil(Math.random() * 999);
+    const secretNumber2 = Math.ceil(Math.random() * 999);
+    const secretNumber3 = Math.ceil(Math.random() * 999);
+    const secretNumber4 = Math.ceil(Math.random() * 999);
+
+    fs.mkdirSync(tmpdir.resolve('node_modules/pkg/subfolder'), { recursive: true });
+    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder/index.js'), '');
+    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder/package.json'), JSON.stringify({ type: 'module', secretNumber1 }));
+    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/package.json'), JSON.stringify({ name: 'pkg', exports: './subfolder/index.js', secretNumber2 }));
+    fs.mkdirSync(tmpdir.resolve('node_modules/pkg/subfolder2'));
+    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder2/package.json'), JSON.stringify({ type: 'module', secretNumber3 }));
+    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder2/index.js'), '');
+    fs.mkdirSync(tmpdir.resolve('node_modules/pkg2'));
+    fs.writeFileSync(tmpdir.resolve('node_modules/pkg2/package.json'), JSON.stringify({ name: 'pkg', main: tmpdir.resolve('node_modules/pkg/subfolder2/index.js'), secretNumber4 }));
+
+    common.spawnPromisified(process.execPath, [tmpdir.resolve('entry.mjs')]).then(common.mustCall((result) => {
+      console.error(result.stderr)
+      console.log(result.stdout)
+      assert.deepStrictEqual(result, {
+        stdout: `${secretNumber1} NaN ${secretNumber3} ${secretNumber4}\n`,
+        stderr: '',
+        code: 0,
+        signal: null,
+      });
+    }));
+  });
 });
-
-// tmpdir.refresh();
-
-// {
-//   fs.writeFileSync(tmpdir.resolve('entry.mjs'), `
-//     import { findPackageJSON, createRequire } from 'node:module';
-//     const require = createRequire(import.meta.url);
-
-//     const { secretNumber1 } = findPackageJSON(import.meta.resolve('pkg'), true).data;
-//     const secretNumber2 = NaN; // TODO get the actual value
-//     const { secretNumber3 } = findPackageJSON(import.meta.resolve('pkg2'), true).data;
-//     const { secretNumber4 } = require('pkg2/package.json'); // TODO: is there a way to get that without relying on package.json being exported?
-//     console.log(secretNumber1, secretNumber2, secretNumber3, secretNumber4);
-//   `);
-
-//   const secretNumber1 = Math.ceil(Math.random() * 999);
-//   const secretNumber2 = Math.ceil(Math.random() * 999);
-//   const secretNumber3 = Math.ceil(Math.random() * 999);
-//   const secretNumber4 = Math.ceil(Math.random() * 999);
-
-//   fs.mkdirSync(tmpdir.resolve('node_modules/pkg/subfolder'), { recursive: true });
-//   fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder/index.js'), '');
-//   fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder/package.json'), JSON.stringify({ type: 'module', secretNumber1 }));
-//   fs.writeFileSync(tmpdir.resolve('node_modules/pkg/package.json'), JSON.stringify({ name: 'pkg', exports: './subfolder/index.js', secretNumber2 }));
-//   fs.mkdirSync(tmpdir.resolve('node_modules/pkg/subfolder2'));
-//   fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder2/package.json'), JSON.stringify({ type: 'module', secretNumber3 }));
-//   fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder2/index.js'), '');
-//   fs.mkdirSync(tmpdir.resolve('node_modules/pkg2'));
-//   fs.writeFileSync(tmpdir.resolve('node_modules/pkg2/package.json'), JSON.stringify({ name: 'pkg', main: tmpdir.resolve('node_modules/pkg/subfolder2/index.js'), secretNumber4 }));
-
-
-//   common.spawnPromisified(process.execPath, [tmpdir.resolve('entry.mjs')]).then(common.mustCall((result) => {
-//     assert.deepStrictEqual(result, {
-//       stdout: `${secretNumber1} ${secretNumber2} ${secretNumber3} ${secretNumber4}\n`,
-//       stderr: '',
-//       code: 0,
-//       signal: null,
-//     })
-//   }))
-// }
