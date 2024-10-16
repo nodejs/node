7568d49a9fb6139e7b5510bf61a4196b28527a0d
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-10-16 22:45:52 +0200

    fixup!: tidy

--- test/parallel/test-find-package-json.js
+++ test/parallel/test-find-package-json.js
@@ -2,7 +2,7 @@
 
 const common = require('../common');
 const fixtures = require('../common/fixtures');
-const tmpdir = require('../common/tmpdir')
+const tmpdir = require('../common/tmpdir');
 const assert = require('node:assert');
 const fs = require('node:fs');
 const { findPackageJSON } = require('node:module');
@@ -45,7 +45,7 @@ describe('findPackageJSON', () => { // Throws when no arguments are provided
 
   it('should be able to crawl up (ESM)', () => {
     const pathToMod = fixtures.path('packages/nested/sub-pkg-esm/index.js');
-    const parentPkg = require(pathToMod).default; // this test is a CJS file
+    const parentPkg = require(pathToMod).default; // This test is a CJS file
 
     const pathToParent = fixtures.path('packages/nested/package.json');
     assert.strictEqual(parentPkg, pathToParent);
@@ -53,7 +53,7 @@ describe('findPackageJSON', () => { // Throws when no arguments are provided
 
   it('can require via package.json', () => {
     const pathToMod = fixtures.path('packages/cjs-main-no-index/other.js');
-    // require() falls back to package.json values like "main" to resolve when there is no index
+    // `require()` falls back to package.json values like "main" to resolve when there is no index
     const answer = require(pathToMod);
 
     assert.strictEqual(answer, 43);
@@ -85,18 +85,52 @@ describe('findPackageJSON', () => { // Throws when no arguments are provided
     const secretNumber4 = Math.ceil(Math.random() * 999);
 
     fs.mkdirSync(tmpdir.resolve('node_modules/pkg/subfolder'), { recursive: true });
-    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder/index.js'), '');
-    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder/package.json'), JSON.stringify({ type: 'module', secretNumber1 }));
-    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/package.json'), JSON.stringify({ name: 'pkg', exports: './subfolder/index.js', secretNumber2 }));
+    fs.writeFileSync(
+      tmpdir.resolve('node_modules/pkg/subfolder/index.js'),
+      '',
+    );
+    fs.writeFileSync(
+      tmpdir.resolve('node_modules/pkg/subfolder/package.json'),
+      JSON.stringify({
+        type: 'module',
+        secretNumber1,
+      }),
+    );
+    fs.writeFileSync(
+      tmpdir.resolve('node_modules/pkg/package.json'),
+      JSON.stringify({
+        name: 'pkg',
+        exports: './subfolder/index.js',
+        secretNumber2,
+      }),
+    );
+
     fs.mkdirSync(tmpdir.resolve('node_modules/pkg/subfolder2'));
-    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder2/package.json'), JSON.stringify({ type: 'module', secretNumber3 }));
-    fs.writeFileSync(tmpdir.resolve('node_modules/pkg/subfolder2/index.js'), '');
+    fs.writeFileSync(
+      tmpdir.resolve('node_modules/pkg/subfolder2/package.json'),
+      JSON.stringify({
+        type: 'module',
+        secretNumber3,
+      }),
+    );
+    fs.writeFileSync(
+      tmpdir.resolve('node_modules/pkg/subfolder2/index.js'),
+      '',
+    );
+
     fs.mkdirSync(tmpdir.resolve('node_modules/pkg2'));
-    fs.writeFileSync(tmpdir.resolve('node_modules/pkg2/package.json'), JSON.stringify({ name: 'pkg', main: tmpdir.resolve('node_modules/pkg/subfolder2/index.js'), secretNumber4 }));
+    fs.writeFileSync(
+      tmpdir.resolve('node_modules/pkg2/package.json'),
+      JSON.stringify({
+        name: 'pkg',
+        main: tmpdir.resolve('node_modules/pkg/subfolder2/index.js'),
+        secretNumber4,
+      }),
+    );
 
     common.spawnPromisified(process.execPath, [tmpdir.resolve('entry.mjs')]).then(common.mustCall((result) => {
-      console.error(result.stderr)
-      console.log(result.stdout)
+      console.error(result.stderr);
+      console.log(result.stdout);
       assert.deepStrictEqual(result, {
         stdout: `${secretNumber1} NaN ${secretNumber3} ${secretNumber4}\n`,
         stderr: '',
