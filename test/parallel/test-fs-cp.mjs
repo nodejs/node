ramulugiragani/githutest03

skip tests that are hopeless

test/parallel/test-fs-cp.mjs
@@ -1,4 +1,4 @@
-import { mustCall, mustNotMutateObjectDeep } from '../common/index.mjs';
+import { mustCall, mustNotMutateObjectDeep, isInsideCWDWithUnusualChars } from '../common/index.mjs';
 
 import assert from 'assert';
 import fs from 'fs';
@@ -306,7 +306,7 @@ function nextdir() {
 }
 
 // It throws an error if attempt is made to copy socket.
-if (!isWindows) {
+if (!isWindows && !isInsideCWDWithUnusualChars) {
   const src = nextdir();
   mkdirSync(src);
   const dest = nextdir();

test/parallel/test-npm-install.js
@@ -2,6 +2,8 @@
 const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
+if (common.isInsideCWDWithUnusualChars)
+  common.skip('npm does not support this install path');
 
 const path = require('path');
 const exec = require('child_process').exec;

nodejs/node

test/parallel/test-fs-cp.mjs
@@ -1,4 +1,4 @@
-import { mustCall, mustNotMutateObjectDeep } from '../common/index.mjs';
+import { mustCall, mustNotMutateObjectDeep, isInsideDirWithUnusualChars } from '../common/index.mjs';
 
 import assert from 'assert';
 import fs from 'fs';
@@ -370,7 +370,7 @@ function nextdir(dirname) {
 }
 
 // It throws an error if attempt is made to copy socket.
-if (!isWindows) {
+if (!isWindows && !isInsideDirWithUnusualChars) {
   const src = nextdir();
   mkdirSync(src);
   const dest = nextdir();
@@ -738,7 +738,7 @@ if (!isWindows) {
 }
 
 // It returns an error if attempt is made to copy socket.
-if (!isWindows) {
+if (!isWindows && !isInsideDirWithUnusualChars) {
   const src = nextdir();
   mkdirSync(src);
   const dest = nextdir();

test/parallel/test-npm-install.js
@@ -2,6 +2,8 @@
 const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
+if (common.isInsideDirWithUnusualChars)
+  common.skip('npm does not support this install path');
 
 const path = require('path');
 const exec = require('child_process').exec;