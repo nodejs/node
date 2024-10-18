bde374ee6aa08b2bf1062f092eaadb1470d3c58b
Yagiz Nizipli <yagiz@nizipli.com>
2024-10-18 12:47:50 -0400

    src: remove icu based `ToASCII` and `ToUnicode`
    PR-URL: https://github.com/nodejs/node/pull/55156
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matthew Aitken <maitken033380023@gmail.com>
    Reviewed-By: Daniel Lemire <daniel@lemire.me>
    Reviewed-By: Richard Lau <rlau@redhat.com>

--- src/node_i18n.h
+++ src/node_i18n.h
@@ -53,19 +53,6 @@ enum class idna_mode {
   kStrict
 };
 
-// Implements the WHATWG URL Standard "domain to ASCII" algorithm.
-// https://url.spec.whatwg.org/#concept-domain-to-ascii
-int32_t ToASCII(MaybeStackBuffer<char>* buf,
-                const char* input,
-                size_t length,
-                idna_mode mode = idna_mode::kDefault);
-
-// Implements the WHATWG URL Standard "domain to Unicode" algorithm.
-// https://url.spec.whatwg.org/#concept-domain-to-unicode
-int32_t ToUnicode(MaybeStackBuffer<char>* buf,
-                  const char* input,
-                  size_t length);
-
 struct ConverterDeleter {
   void operator()(UConverter* pointer) const { ucnv_close(pointer); }
 };
