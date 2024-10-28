9bf85bc41c0686e5516420997cf384d5887aa53f
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2024-10-28 16:59:32 -0400

    fixup! fix flaky test

--- test/parallel/test-repl.js
+++ test/parallel/test-repl.js
@@ -1041,7 +1041,10 @@ function event(ee, expected) {
   common.expectWarning({
     DeprecationWarning: {
       DEP0185: 'Instantiating REPLServer without the \'new\' keyword has been deprecated.',
-      DEP0169: '`url.parse()` behavior is not standardized and prone to errors that have security implications. Use the WHATWG URL API instead. CVEs are not issued for `url.parse()` vulnerabilities.',
+      // For the 'url.format' test-case.
+      DEP0169:
+        '`url.parse()` behavior is not standardized and prone to errors that have security implications. ' +
+        'Use the WHATWG URL API instead. CVEs are not issued for `url.parse()` vulnerabilities.',
     }
   });
   server.emit('line', '.exit');
