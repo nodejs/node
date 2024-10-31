98bfc7dce5d9811ae35d47e5597ac931a33339ba
Marco Ippolito <marcoippolito54@gmail.com>
2024-10-31 08:04:25 -0500

    http: add diagnostic channel `http.client.request.created`
    PR-URL: https://github.com/nodejs/node/pull/55586
    Fixes: https://github.com/nodejs/node/issues/55352
    Reviewed-By: Paolo Insogna <paolo@cowtech.it>
    Reviewed-By: Jake Yuesong Li <jake.yuesong@gmail.com>
    Reviewed-By: theanarkh <theratliter@gmail.com>

--- test/parallel/test-diagnostics-channel-http.js
+++ test/parallel/test-diagnostics-channel-http.js
@@ -53,6 +53,11 @@ dc.subscribe('http.server.response.finish', common.mustCall(({
   assert.strictEqual(isHTTPServer(server), true);
 }));
 
+dc.subscribe('http.client.request.created', common.mustCall(({ request }) => {
+  assert.strictEqual(isOutgoingMessage(request), true);
+  assert.strictEqual(isHTTPServer(server), true);
+}, 2));
+
 const server = http.createServer(common.mustCall((req, res) => {
   res.end('done');
 }));
