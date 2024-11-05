37546ec993a11c96b9d6772f1efd179d4ae83756
LiviaMedeiros <livia@cirno.name>
2024-11-05 22:55:28 +0800

    squash: adjust test

--- test/parallel/test-fs-constants.js
+++ test/parallel/test-fs-constants.js
@@ -7,12 +7,21 @@ const assert = require('assert');
 assert.notStrictEqual(fs.constants.S_IRUSR, undefined);
 assert.notStrictEqual(fs.constants.S_IWUSR, undefined);
 
+// Check for runtime deprecation warning, there should be no setter
+const { F_OK, R_OK, W_OK, X_OK } = fs.constants;
+
+assert.throws(() => { fs.F_OK = 'overwritten' }, { name: 'TypeError' });
+assert.throws(() => { fs.R_OK = 'overwritten' }, { name: 'TypeError' });
+assert.throws(() => { fs.W_OK = 'overwritten' }, { name: 'TypeError' });
+assert.throws(() => { fs.X_OK = 'overwritten' }, { name: 'TypeError' });
+
 expectWarning(
   'DeprecationWarning',
   'fs.F_OK is deprecated, use fs.constants.F_OK instead',
   'DEP0176'
 );
-fs.F_OK;
-fs.R_OK;
-fs.W_OK;
-fs.X_OK;
+
+assert.strictEqual(fs.F_OK, F_OK);
+assert.strictEqual(fs.R_OK, R_OK);
+assert.strictEqual(fs.W_OK, W_OK);
+assert.strictEqual(fs.X_OK, X_OK);
