5d01b37e704da2a8cc3a72137bb6b83c7ba40a47
LiviaMedeiros <livia@cirno.name>
2024-11-05 22:44:31 +0800

    squash: add test

--- test/parallel/test-fs-constants.js
+++ test/parallel/test-fs-constants.js
@@ -1,8 +1,18 @@
 'use strict';
-require('../common');
+const { expectWarning } = require('../common');
 const fs = require('fs');
 const assert = require('assert');
 
 // Check if the two constants accepted by chmod() on Windows are defined.
 assert.notStrictEqual(fs.constants.S_IRUSR, undefined);
 assert.notStrictEqual(fs.constants.S_IWUSR, undefined);
+
+expectWarning(
+  'DeprecationWarning',
+  'fs.F_OK is deprecated, use fs.constants.F_OK instead',
+  'DEP0176'
+);
+fs.F_OK;
+fs.R_OK;
+fs.W_OK;
+fs.X_OK;
