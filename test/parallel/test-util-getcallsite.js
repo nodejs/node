6e9e54ac5ee2247f14c02d787f3bd63ab18d406f
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-11-06 13:47:19 +0100

    fixup! util: do not mark experimental feature as deprecated

--- test/parallel/test-util-getcallsite.js
+++ test/parallel/test-util-getcallsite.js
@@ -4,6 +4,6 @@ require('../common');
 const { getCallSite } = require('node:util');
 const { expectWarning } = require('../common');
 
-const warning = 'The `util.getCallSite` API is deprecated. Please use `util.getCallSites()` instead.';
+const warning = 'The `util.getCallSite` API has been renamed to `util.getCallSites()`.';
 expectWarning('ExperimentalWarning', warning);
 getCallSite();
