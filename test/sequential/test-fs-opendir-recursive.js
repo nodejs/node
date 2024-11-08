db0d648d8fe94ea81de6b7b036e71e52d9cb482c
Ethan Arrowood <ethan@arrowood.dev>
2024-11-08 16:03:55 +0000

    fs: fix bufferSize option for opendir recursive
    The bufferSize option was not respected in recursive mode. This PR
    implements a naive solution to fix this issue until a better
    implementation can be designed.
    
    Fixes: https://github.com/nodejs/node/issues/48820
    PR-URL: https://github.com/nodejs/node/pull/55744
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/sequential/test-fs-opendir-recursive.js
+++ test/sequential/test-fs-opendir-recursive.js
@@ -132,6 +132,7 @@ function getDirentPath(dirent) {
 }
 
 function assertDirents(dirents) {
+  assert.strictEqual(dirents.length, expected.length);
   dirents.sort((a, b) => (getDirentPath(a) < getDirentPath(b) ? -1 : 1));
   assert.deepStrictEqual(
     dirents.map((dirent) => {
@@ -220,4 +221,22 @@ function processDirCb(dir, cb) {
   }
 
   test().then(common.mustCall());
+}
+
+// Issue https://github.com/nodejs/node/issues/48820 highlights that
+// opendir recursive does not properly handle the buffer size option.
+// This test asserts that the buffer size option is respected.
+{
+  const dir = fs.opendirSync(testDir, { bufferSize: 1, recursive: true });
+  processDirSync(dir);
+  dir.closeSync();
+}
+
+{
+  fs.opendir(testDir, { recursive: true, bufferSize: 1 }, common.mustSucceed((dir) => {
+    processDirCb(dir, common.mustSucceed((dirents) => {
+      assertDirents(dirents);
+      dir.close(common.mustSucceed());
+    }));
+  }));
 }
