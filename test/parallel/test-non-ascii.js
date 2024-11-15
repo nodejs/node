fd7a3f57ec9df34cb3a0c3543518a5a88c32647b
islandryu <shimaryuhei@gmail.com>
2024-11-15 23:47:17 +0900

    move test file to fixtures

--- test/parallel/test-non-ascii.js
+++ test/parallel/test-non-ascii.js
@@ -1,18 +1,12 @@
 'use strict';
 
-const fs = require('node:fs');
 const common = require('../common');
-const tmpdir = require('../common/tmpdir');
+const path = require('path');
 const { test } = require('node:test');
 const assert = require('node:assert');
 
 test('Running from a directory with non-ASCII characters', async () => {
-    fs.mkdirSync(tmpdir.resolve('12月'), { recursive: true });
-    fs.writeFileSync(
-      tmpdir.resolve('12月/index.js'),
-      "console.log('12月');",
-    );
-    const { stdout } = await common.spawnPromisified(process.execPath, [tmpdir.resolve('12月/index.js')]);
-    assert.strictEqual(stdout, '12月\n');
-  });
+  const nonAsciiPath = path.resolve(__dirname, '../fixtures/全角文字/index.js');
+  const { stdout } = await common.spawnPromisified(process.execPath, [nonAsciiPath]);
+  assert.strictEqual(stdout, 'check non-ascii\n');
 });
