4906ffb7b51a9273cca3b650884f9d7e47756eb4
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2024-11-16 12:57:59 -0500

    fixup! doc: remove non-working example

--- tools/lint-md/man-page.mjs
+++ tools/lint-md/man-page.mjs
@@ -2,7 +2,7 @@ import { generators, createParser } from '@node-core/api-docs-tooling';
 import { read } from 'to-vfile';
 
 export default async function generateManPage(path) {
-    const { parseApiDocs } = createParser();
-    const parsedApiDocs = await parseApiDocs([read(path)]);
-    return generators['man-page'].generate(parsedApiDocs, {});
+  const { parseApiDocs } = createParser();
+  const parsedApiDocs = await parseApiDocs([read(path)]);
+  return generators['man-page'].generate(parsedApiDocs, {});
 }
