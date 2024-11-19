d02f7a9aa0f2e7d71b11a71a900b198a51c44dfe
James Sumners <git@zifbang.com>
2024-11-19 14:13:28 -0500

    satisfy codecov

--- test/parallel/test-bootstrap-rawdebug.js
+++ test/parallel/test-bootstrap-rawdebug.js
@@ -5,3 +5,7 @@ const assert = require('assert');
 
 common.allowGlobals('rawDebug');
 assert.strictEqual(typeof global.rawDebug === 'function', true);
+assert.strictEqual(global.rawDebug === process._rawDebug, true);
+
+global.rawDebug = 42;
+assert.strictEqual(global.rawDebug === 42, true);
