51f580326c482d216edb077fd0af9b470fdc69f8
RafaelGSS <rafael.nunu@hotmail.com>
2024-11-22 19:49:30 -0300

    fixup! fixup! build: add create release proposal action

--- tools/actions/create-release.sh
+++ tools/actions/create-release.sh
@@ -22,17 +22,9 @@ git push
 TITLE=$(awk "/^## ${RELEASE_DATE}/ { print substr(\$0, 4) }" "doc/changelogs/CHANGELOG_V${RELEASE_LINE}.md")
 
 # Use a temporary file for the PR body
-TEMP_BODY=$(mktemp)
-awk "/## ${RELEASE_DATE}/,/^<a id=/{ if (!/^<a id=/) print }" "doc/changelogs/CHANGELOG_V${RELEASE_LINE}.md" > "$TEMP_BODY"
+TEMP_BODY="$(awk "/## ${RELEASE_DATE}/,/^<a id=/{ if (!/^<a id=/) print }" "doc/changelogs/CHANGELOG_V${RELEASE_LINE}.md")"
 
-gh pr create --title "$TITLE" --body-file "$TEMP_BODY" --base "v$RELEASE_LINE.x"
-
-# Dynamically get the repository (owner/repo) and branch
-REPO=$(git remote get-url origin | sed -E 's|^.*github.com[/:]([^/]+/[^.]+)(\.git)?$|\1|')
-BRANCH=$(git branch --show-current)
-
-# Get the PR URL for the current branch in the repository
-PR_URL=$(gh pr list --repo "$REPO" --head "$BRANCH" --json url -q ".[0].url")
+PR_URL="$(gh pr create --title "$TITLE" --body "$TEMP_BODY" --base "v$RELEASE_LINE.x")"
 
 # Get the last commit message
 LAST_COMMIT_MSG=$(git log -1 --pretty=%B)
@@ -42,6 +34,3 @@ git commit --amend --no-edit -m "$(echo "$LAST_COMMIT_MSG" | sed "s|PR-URL: TODO
 
 # Force-push the amended commit
 git push --force
-
-rm "$TEMP_BODY"
-
