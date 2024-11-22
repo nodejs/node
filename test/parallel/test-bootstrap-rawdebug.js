12839f9f687f46fce60b6ff2b0a4cb5b92171891
James Sumners <git@zifbang.com>
2024-11-22 06:18:28 -0500

    remove impossible test

--- test/parallel/test-bootstrap-rawdebug.js
+++ test/parallel/test-bootstrap-rawdebug.js
@@ -5,7 +5,6 @@ const assert = require('assert');
 
 common.allowGlobals('rawDebug');
 assert.strictEqual(typeof global.rawDebug === 'function', true);
-assert.strictEqual(global.rawDebug === process._rawDebug, true);
 
 global.rawDebug = 42;
 assert.strictEqual(global.rawDebug === 42, true);
