551a7aea3ec31175e06b917768048878706d5875
Chengzhong Wu <cwu631@bloomberg.net>
2024-11-28 15:34:16 +0000

    fixup! node-api: allow napi_delete_reference in finalizers

--- test/js-native-api/6_object_wrap/test-basic-finalizer.js
+++ test/js-native-api/6_object_wrap/test-basic-finalizer.js
@@ -2,19 +2,23 @@
 
 'use strict';
 const common = require('../../common');
+const assert = require('assert');
 const addon = require(`./build/${common.buildType}/6_object_wrap_basic_finalizer`);
-const { onGC } = require('../../common/gc');
 
-/**
- * This test verifies that an ObjectWrap can be correctly finalized with an NAPI_EXPERIMENTAL
- * node_api_basic_finalizer.
- */
-
-{
+// This test verifies that ObjectWrap can be correctly finalized with a node_api_basic_finalizer
+// in the current JS loop tick
+(() => {
   let obj = new addon.MyObject(9);
-  onGC(obj, {
-    ongc: common.mustCall(),
-  });
   obj = null;
+  // Silent eslint about unused variables.
+  assert.strictEqual(obj, null);
+})();
+
+for (let i = 0; i < 10; ++i) {
   global.gc();
+  if (addon.getFinalizerCallCount() === 1) {
+    break;
+  }
 }
+
+assert.strictEqual(addon.getFinalizerCallCount(), 1);
