7a97799b370f5c5d6eea58bb486ac8dc1cc96854
Edy Silva <edigleyssonsilva@gmail.com>
2024-11-30 02:19:06 -0300

    test: improve matching

--- test/parallel/test-require-esm-with-no-experimental-require-module.mjs
+++ test/parallel/test-require-esm-with-no-experimental-require-module.mjs
@@ -3,27 +3,22 @@ import { test } from 'node:test';
 import * as fixtures from '../common/fixtures.mjs';
 import { spawnSync } from 'node:child_process';
 import assert from 'node:assert';
-import { EOL } from 'node:os';
 
 test('correctly reports error for a longer stack trace', () => {
   // The following regex matches the error message that is expected to be thrown
   //
-  // package-type-module/require-esm-error-annotation/longer-stack.cjs:6
+  // package-type-module/require-esm-error-annotation/index.cjs:6
   //   require('./app.js')
   //   ^
 
   const fixture = fixtures.path('es-modules/package-type-module/require-esm-error-annotation/index.cjs');
   const args = ['--no-experimental-require-module', fixture];
-
-  const lineNumber = 6;
-  const lineContent = "require('./app.js')";
-  const fullMessage = `${fixture}:${lineNumber}${EOL}  ${lineContent}${EOL}  ^${EOL}`;
+  const regex = /index\.cjs:6[\n\r]+\s{2}require\('\.\/app\.js'\)[\n\r]+\s{2}\^/;
 
   const result = spawnSync(process.execPath, args);
-
-  console.log(`STDERR: ${result.stderr.toString()}`);
+  const stderr = result.stderr.toString();
 
   assert.strictEqual(result.status, 1);
   assert.strictEqual(result.stdout.toString(), '');
-  assert(result.stderr.toString().indexOf(fullMessage) > -1);
+  assert.match(stderr, regex);
 });
