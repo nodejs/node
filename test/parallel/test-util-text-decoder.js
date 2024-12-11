d9e8b1af469130520eb2d6d6f31a3e14ed36c6f4
Mert Can Altin <mertgold60@gmail.com>
2024-12-11 23:27:55 +0300

    Update test/parallel/test-util-text-decoder.js
    Co-authored-by: Yagiz Nizipli <yagiz@nizipli.com>

--- test/parallel/test-util-text-decoder.js
+++ test/parallel/test-util-text-decoder.js
@@ -5,9 +5,7 @@ const common = require('../common');
 const test = require('node:test');
 const assert = require('node:assert');
 
-test('TextDecoder correctly decodes windows-1252 encoded data', (t) => {
-  if (!common.hasIntl) {
-    common.skip('missing Intl');
+test('TextDecoder correctly decodes windows-1252 encoded data', { skip: !common.hasIntl }, (t) => {
   }
 
   const latin1Bytes = new Uint8Array([0xc1, 0xe9, 0xf3]);
