ad7bc883836a0612160a063ff21df990c18b95ec
Mert Can Altin <mertgold60@gmail.com>
2024-12-14 21:09:49 +0300

    util: fix Latin1 decoding to return string output
    PR-URL: https://github.com/nodejs/node/pull/56222
    Fixes: https://github.com/nodejs/node/issues/56219
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Daniel Lemire <daniel@lemire.me>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/cctest/test_encoding_binding.cc
+++ test/cctest/test_encoding_binding.cc
@@ -26,7 +26,7 @@ bool RunDecodeLatin1(Environment* env,
     return false;
   }
 
-  *result = try_catch.Exception();
+  *result = args[0];
   return true;
 }
 
@@ -146,6 +146,27 @@ TEST_F(EncodingBindingTest, DecodeLatin1_BOMPresent) {
 
   Local<Value> result;
   EXPECT_TRUE(RunDecodeLatin1(env, args, true, false, &result));
+
+  String::Utf8Value utf8_result(isolate, result);
+  EXPECT_STREQ(*utf8_result, "Áéó");
+}
+
+TEST_F(EncodingBindingTest, DecodeLatin1_ReturnsString) {
+  Environment* env = CreateEnvironment();
+  Isolate* isolate = env->isolate();
+  HandleScope handle_scope(isolate);
+
+  const uint8_t latin1_data[] = {0xC1, 0xE9, 0xF3};
+  Local<ArrayBuffer> ab = ArrayBuffer::New(isolate, sizeof(latin1_data));
+  memcpy(ab->GetBackingStore()->Data(), latin1_data, sizeof(latin1_data));
+
+  Local<Uint8Array> array = Uint8Array::New(ab, 0, sizeof(latin1_data));
+  Local<Value> args[] = {array};
+
+  Local<Value> result;
+  ASSERT_TRUE(RunDecodeLatin1(env, args, false, false, &result));
+
+  ASSERT_TRUE(result->IsString());
 
   String::Utf8Value utf8_result(isolate, result);
   EXPECT_STREQ(*utf8_result, "Áéó");
