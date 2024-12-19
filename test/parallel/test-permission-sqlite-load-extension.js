ec6e6b534c2e44201e6437d448d891b6305f760a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-12-19 18:11:19 +0100

    test: make `test-permission-sqlite-load-extension` more robust
    PR-URL: https://github.com/nodejs/node/pull/56295
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>
    Reviewed-By: Xuguang Mei <meixuguang@gmail.com>

--- test/parallel/test-permission-sqlite-load-extension.js
+++ test/parallel/test-permission-sqlite-load-extension.js
@@ -1,18 +1,16 @@
 'use strict';
 const common = require('../common');
 const assert = require('node:assert');
-const childProcess = require('child_process');
 
 const code = `const sqlite = require('node:sqlite');
 const db = new sqlite.DatabaseSync(':memory:', { allowExtension: true });
 db.loadExtension('nonexistent');`.replace(/\n/g, ' ');
 
-childProcess.exec(
-  `${process.execPath} --permission -e "${code}"`,
-  {},
-  common.mustCall((err, _, stderr) => {
-    assert.strictEqual(err.code, 1);
-    assert.match(stderr, /Error: Cannot load SQLite extensions when the permission model is enabled/);
-    assert.match(stderr, /code: 'ERR_LOAD_SQLITE_EXTENSION'/);
-  })
-);
+common.spawnPromisified(
+  process.execPath,
+  ['--permission', '--eval', code],
+).then(common.mustCall(({ code, stderr }) => {
+  assert.match(stderr, /Error: Cannot load SQLite extensions when the permission model is enabled/);
+  assert.match(stderr, /code: 'ERR_LOAD_SQLITE_EXTENSION'/);
+  assert.strictEqual(code, 1);
+}));
