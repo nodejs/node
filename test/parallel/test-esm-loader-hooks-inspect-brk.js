990497c514c30a2f104ed8da3a0f91284bad26b0
Luigi Pinca <luigipinca@gmail.com>
2024-12-19 23:30:28 +0100

    test: deflake test-esm-loader-hooks-inspect-brk
    Refs: https://github.com/nodejs/node/pull/54827
    Refs: https://github.com/nodejs/node/pull/51560
    PR-URL: https://github.com/nodejs/node/pull/56050
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/parallel/test-esm-loader-hooks-inspect-brk.js
+++ test/parallel/test-esm-loader-hooks-inspect-brk.js
@@ -10,23 +10,20 @@ const assert = require('assert');
 const fixtures = require('../common/fixtures');
 const { NodeInstance } = require('../common/inspector-helper.js');
 
-async function runIfWaitingForDebugger(session) {
-  const commands = [
-    { 'method': 'Runtime.enable' },
-    { 'method': 'Debugger.enable' },
-    { 'method': 'Runtime.runIfWaitingForDebugger' },
-  ];
-
-  await session.send(commands);
-  await session.waitForNotification('Debugger.paused');
-}
-
 async function runTest() {
   const main = fixtures.path('es-module-loaders', 'register-loader.mjs');
   const child = new NodeInstance(['--inspect-brk=0'], '', main);
 
   const session = await child.connectInspectorSession();
-  await runIfWaitingForDebugger(session);
+  await session.send({ method: 'NodeRuntime.enable' });
+  await session.waitForNotification('NodeRuntime.waitingForDebugger');
+  await session.send([
+    { 'method': 'Runtime.enable' },
+    { 'method': 'Debugger.enable' },
+    { 'method': 'Runtime.runIfWaitingForDebugger' },
+  ]);
+  await session.send({ method: 'NodeRuntime.disable' });
+  await session.waitForNotification('Debugger.paused');
   await session.runToCompletion();
   assert.strictEqual((await child.expectShutdown()).exitCode, 0);
 }
