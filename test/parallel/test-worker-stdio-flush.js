cec4cedc22c7b9244ed38774a4b497fdfad41553
Matteo Collina <hello@matteocollina.com>
2024-12-26 13:42:47 +0100

    worker: flush stdout and stderr on exit
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ test/parallel/test-worker-stdio-flush.js
@@ -0,0 +1,28 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const fs = require('fs');
+const util = require('util');
+const { Writable } = require('stream');
+const { Worker, isMainThread } = require('worker_threads');
+
+if (isMainThread) {
+  const w = new Worker(__filename, { stdout: true });
+  const expected = 'hello world'
+
+  let data = ''
+  w.stdout.setEncoding('utf8')
+  w.stdout.on('data', (chunk) => {
+    data += chunk
+  })
+
+  w.on('exit', common.mustCall(() => {
+    assert.strictEqual(data, expected)
+  }));
+} else {
+  process.stdout.write('hello')
+  process.on('exit', () => {
+    process.stdout.write(' ')
+    process.stdout.write('world')
+  })
+}
