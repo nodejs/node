5451b5823bfd38ff548e93f80618184275d7fe61
Vitalii Akimov <vitaliy.akimov@gmail.com>
2024-12-30 21:12:16 +0000

    more readable test

--- test/module-hooks/test-module-hooks-load-url-change.js
+++ test/module-hooks/test-module-hooks-load-url-change.js
@@ -6,17 +6,18 @@ const { registerHooks } = require('module');
 const fixtures = require('../common/fixtures');
 
 // This tests shows the url parameter in `load` can be changed in the `nextLoad` call
+// It changes `foo` package name into `redirected-fs` and then loads `redirected-fs`
 
 const hook = registerHooks({
   resolve(specifier, context, nextLoad) {
-    assert.strictEqual(specifier, 'rfs');
+    assert.strictEqual(specifier, 'foo');
     return {
-      url: 'file:///rfs',
+      url: 'file:///foo',
       shortCircuit: true,
     };
   },
   load: common.mustCall(function load(url, context, nextLoad) {
-    assert.strictEqual(url, 'file:///rfs');
+    assert.strictEqual(url, 'file:///foo');
     return nextLoad(
       fixtures.fileURL('module-hooks', `redirected-fs.js`).href,
       context
@@ -24,6 +25,6 @@ const hook = registerHooks({
   }),
 });
 
-assert.strictEqual(require('rfs').exports_for_test, 'redirected fs');
+assert.strictEqual(require('foo').exports_for_test, 'redirected fs');
 
 hook.deregister();
