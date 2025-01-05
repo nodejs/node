839a06e9088e0018f2f14712e7ad031014d6f23c
Eddie Abbondanzio <me@eddieabbondanz.io>
2025-01-05 03:35:35 -0500

    test_runner: differentiate test types in enqueue dequeue events
    PR-URL: https://github.com/nodejs/node/pull/54049
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Pietro Marchini <pietro.marchini94@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Chemi Atlow <chemi@atlow.co.il>

--- test/parallel/test-runner-run.mjs
+++ test/parallel/test-runner-run.mjs
@@ -194,6 +194,33 @@ describe('require(\'node:test\').run', { concurrency: true }, () => {
     });
   });
 
+  it('should include test type in enqueue, dequeue events', async (t) => {
+    const stream = await run({
+      files: [join(testFixtures, 'default-behavior/test/suite_and_test.cjs')],
+    });
+    t.plan(4);
+
+    stream.on('test:enqueue', common.mustCall((data) => {
+      if (data.name === 'this is a suite') {
+        t.assert.strictEqual(data.type, 'suite');
+      }
+      if (data.name === 'this is a test') {
+        t.assert.strictEqual(data.type, 'test');
+      }
+    }, 2));
+    stream.on('test:dequeue', common.mustCall((data) => {
+      if (data.name === 'this is a suite') {
+        t.assert.strictEqual(data.type, 'suite');
+      }
+      if (data.name === 'this is a test') {
+        t.assert.strictEqual(data.type, 'test');
+      }
+    }, 2));
+
+    // eslint-disable-next-line no-unused-vars
+    for await (const _ of stream);
+  });
+
   describe('AbortSignal', () => {
     it('should accept a signal', async () => {
       const stream = run({ signal: AbortSignal.timeout(50), files: [
