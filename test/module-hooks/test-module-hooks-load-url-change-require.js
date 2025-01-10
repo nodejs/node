f60c6dfac264ccb138406b20855c1fdecfd2fdd4
Vitalii Akimov <vitaliy.akimov@gmail.com>
2025-01-10 18:29:24 +0000

    linter fix

--- test/module-hooks/test-module-hooks-load-url-change-require.js
+++ test/module-hooks/test-module-hooks-load-url-change-require.js
@@ -20,7 +20,7 @@ const hook = registerHooks({
     assert.strictEqual(url, 'foo://bar');
     return nextLoad(
       fixtures.fileURL('module-hooks', 'redirected-fs.js').href,
-      context
+      context,
     );
   }),
 });
