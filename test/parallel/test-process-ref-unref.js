499a5cb5638177c2605f3632de6076fe60112482
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-01-10 15:40:28 +0100

    process: fix symbol key and mark experimental new `node:process` methods
    PR-URL: https://github.com/nodejs/node/pull/56517
    Backport-PR-URL: https://github.com/nodejs/node/pull/56571
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-process-ref-unref.js
+++ test/parallel/test-process-ref-unref.js
@@ -25,6 +25,18 @@ class Foo {
 class Foo2 {
   refCalled = 0;
   unrefCalled = 0;
+  [Symbol.for('nodejs.ref')]() {
+    this.refCalled++;
+  }
+  [Symbol.for('nodejs.unref')]() {
+    this.unrefCalled++;
+  }
+}
+
+// TODO(aduh95): remove support for undocumented symbol
+class Foo3 {
+  refCalled = 0;
+  unrefCalled = 0;
   [Symbol.for('node:ref')]() {
     this.refCalled++;
   }
@@ -39,14 +51,19 @@ describe('process.ref/unref work as expected', () => {
     // just work.
     const foo1 = new Foo();
     const foo2 = new Foo2();
+    const foo3 = new Foo3();
     process.ref(foo1);
     process.unref(foo1);
     process.ref(foo2);
     process.unref(foo2);
+    process.ref(foo3);
+    process.unref(foo3);
     strictEqual(foo1.refCalled, 1);
     strictEqual(foo1.unrefCalled, 1);
     strictEqual(foo2.refCalled, 1);
     strictEqual(foo2.unrefCalled, 1);
+    strictEqual(foo3.refCalled, 1);
+    strictEqual(foo3.unrefCalled, 1);
 
     // Objects that implement the legacy API also just work.
     const i = setInterval(() => {}, 1000);
