58cb88e11ba6e8a7400bea20a96d0ed89be76ac5
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-01-10 15:53:48 +0100

    process: remove support for undocumented symbol

--- test/parallel/test-process-ref-unref.js
+++ test/parallel/test-process-ref-unref.js
@@ -33,37 +33,20 @@ class Foo2 {
   }
 }
 
-// TODO(aduh95): remove support for undocumented symbol
-class Foo3 {
-  refCalled = 0;
-  unrefCalled = 0;
-  [Symbol.for('node:ref')]() {
-    this.refCalled++;
-  }
-  [Symbol.for('node:unref')]() {
-    this.unrefCalled++;
-  }
-}
-
 describe('process.ref/unref work as expected', () => {
   it('refs...', () => {
     // Objects that implement the new Symbol-based API
     // just work.
     const foo1 = new Foo();
     const foo2 = new Foo2();
-    const foo3 = new Foo3();
     process.ref(foo1);
     process.unref(foo1);
     process.ref(foo2);
     process.unref(foo2);
-    process.ref(foo3);
-    process.unref(foo3);
     strictEqual(foo1.refCalled, 1);
     strictEqual(foo1.unrefCalled, 1);
     strictEqual(foo2.refCalled, 1);
     strictEqual(foo2.unrefCalled, 1);
-    strictEqual(foo3.refCalled, 1);
-    strictEqual(foo3.unrefCalled, 1);
 
     // Objects that implement the legacy API also just work.
     const i = setInterval(() => {}, 1000);
