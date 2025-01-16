fc5f4d66310c72f98169cc0e308b97a4361ee862
Rafael Gonzaga <rafael.nunu@hotmail.com>
2025-01-16 11:53:39 -0300

    src: handle duplicate paths granted
    This commit fixes a crash whenever someone tries to allow access to the
    same path twice.
    
    PR-URL: https://github.com/nodejs/node/pull/56591
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

--- src/permission/fs_permission.cc
+++ src/permission/fs_permission.cc
@@ -143,10 +143,12 @@ void FSPermission::Apply(Environment* env,
 
 void FSPermission::GrantAccess(PermissionScope perm, const std::string& res) {
   const std::string path = WildcardIfDir(res);
-  if (perm == PermissionScope::kFileSystemRead) {
+  if (perm == PermissionScope::kFileSystemRead &&
+      !granted_in_fs_.Lookup(path)) {
     granted_in_fs_.Insert(path);
     deny_all_in_ = false;
-  } else if (perm == PermissionScope::kFileSystemWrite) {
+  } else if (perm == PermissionScope::kFileSystemWrite &&
+             !granted_out_fs_.Lookup(path)) {
     granted_out_fs_.Insert(path);
     deny_all_out_ = false;
   }
