30387cf01b5f064deb7b5187b7b5f4043c9603b0
Daniel Venable <daniel.venable@proton.me>
2025-01-25 19:48:09 +0000

    readline: fix test so it tests aborting a question with both ctrl+C and ctrl+D

--- test/parallel/test-readline-promises-interface.js
+++ test/parallel/test-readline-promises-interface.js
@@ -951,12 +951,12 @@ for (let i = 0; i < 12; i++) {
     rli.close();
   }
 
-  // Aborting a question with ctrl+D
+  // Aborting a question with ctrl+C
   {
     const [rli, fi] = getInterface({ terminal: true });
     assert.rejects(rli.question('hello?'), { name: 'AbortError' })
         .then(common.mustCall());
-    fi.emit('keypress', '.', { ctrl: true, name: 'd' });
+    fi.emit('keypress', '.', { ctrl: true, name: 'c' });
   }
 
   // Aborting a question with ctrl+D
