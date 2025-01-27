90d092fb287684857238ec215bbb93d27976fb81
Luigi Pinca <luigipinca@gmail.com>
2025-01-27 11:35:11 +0100

    test: reduce number of written chunks
    Reduce chances of write errors while the request is sent.
    
    Refs: https://github.com/nodejs/node/pull/56756
    PR-URL: https://github.com/nodejs/node/pull/56757
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Stefan Stojanovic <stefan.stojanovic@janeasystems.com>

--- test/parallel/test-http-server-request-timeout-interrupted-headers.js
+++ test/parallel/test-http-server-request-timeout-interrupted-headers.js
@@ -47,9 +47,11 @@ server.listen(0, common.mustCall(() => {
   }));
 
   client.resume();
-  client.write('GET / HTTP/1.1\r\n');
-  client.write('Connection: close\r\n');
-  client.write('X-CRASH: ');
+  client.write(
+    'GET / HTTP/1.1\r\n' +
+    'Connection: close\r\n' +
+    'X-CRASH: '
+  );
 
   sendDelayedRequestHeaders = common.mustCall(() => {
     setTimeout(() => {
