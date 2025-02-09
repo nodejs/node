8977533cc53802d6438389e9600fe3e63405db2a
Jonas <jonas@badalic.com>
2025-02-09 10:32:59 -0500

    url: expose urlpattern as global
    PR-URL: https://github.com/nodejs/node/pull/56950
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Matthew Aitken <maitken033380023@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- test/wpt/test-urlpattern.js
+++ test/wpt/test-urlpattern.js
@@ -5,6 +5,4 @@ const { WPTRunner } = require('../common/wpt');
 const runner = new WPTRunner('urlpattern');
 
 runner.pretendGlobalThisAs('Window');
-// TODO(@anonrig): Remove this once URLPattern is global.
-runner.setInitScript(`global.URLPattern = require('node:url').URLPattern;`);
 runner.runJsTests();
