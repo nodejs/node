2023237b4e080ed10b29f3c07610cb7da4e31421
Shelley Vohr <shelley.vohr@gmail.com>
2025-02-15 10:08:02 +0100

    test,crypto: make tests work for BoringSSL
    Ref https://github.com/nodejs/node/pull/56559
    
    PR-URL: https://github.com/nodejs/node/pull/57021
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Richard Lau <rlau@redhat.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- test/parallel/test-crypto-prime.js
+++ test/parallel/test-crypto-prime.js
@@ -258,11 +258,11 @@ for (const checks of [-(2 ** 31), -1, 2 ** 31, 2 ** 32 - 1, 2 ** 32, 2 ** 50]) {
   bytes[0] = 0x1;
   assert.throws(() => checkPrime(bytes, common.mustNotCall()), {
     code: 'ERR_OSSL_BN_BIGNUM_TOO_LONG',
-    message: /bignum too long/
+    message: /bignum[_ ]too[_ ]long/i
   });
   assert.throws(() => checkPrimeSync(bytes), {
     code: 'ERR_OSSL_BN_BIGNUM_TOO_LONG',
-    message: /bignum too long/
+    message: /bignum[_ ]too[_ ]long/i
   });
 }
 
