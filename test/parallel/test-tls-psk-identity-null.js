4b0741226a92a2013e72b67efcf91031deb985be
Gürgün Dayıoğlu <hey@gurgun.day>
2025-02-15 17:19:56 +0100

    crypto: check for nullptr before dereferencing identity_buf

---
+++ test/parallel/test-tls-psk-identity-null.js
@@ -0,0 +1,54 @@
+'use strict';
+const common = require('../common');
+
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+const assert = require('assert');
+const tls = require('tls');
+
+{
+  const server = tls.createServer({
+    pskCallback: (socket, identity) => {
+      return undefined;
+    }
+  });
+
+  server.listen(0, common.mustCall(() => {
+    const client = tls.connect({
+      port: server.address().port,
+      pskIdentity: 'test',
+      pskCallback: () => {
+        return undefined;
+      }
+    });
+
+    client.on('error', common.mustCall((err) => {
+      assert.strictEqual(err.code, 'ERR_SSL_SSLV3_ALERT_HANDSHAKE_FAILURE');
+      server.close();
+    }));
+  }));
+}
+
+{
+  const server = tls.createServer({
+    pskCallback: (socket, identity) => {
+      return null;
+    }
+  });
+
+  server.listen(0, common.mustCall(() => {
+    const client = tls.connect({
+      port: server.address().port,
+      pskIdentity: 'test',
+      pskCallback: () => {
+        return null;
+      }
+    });
+
+    client.on('error', common.mustCall((err) => {
+      assert.strictEqual(err.code, 'ERR_SSL_SSLV3_ALERT_HANDSHAKE_FAILURE');
+      server.close();
+    }));
+  }));
+}
