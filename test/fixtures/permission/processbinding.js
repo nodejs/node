2bc0d4cbb68b986d535cc4d67323d4f0e9a6a138
Michaël Zasso <targos@protonmail.com>
2025-02-20 16:25:38 +0100

    lib: unexpose six process bindings
    Namely: async_wrap, crypto, http_parser, signal_wrap, url, and v8.
    
    They were runtime-deprecated 4 years ago.

--- test/fixtures/permission/processbinding.js
+++ test/fixtures/permission/processbinding.js
@@ -17,14 +17,6 @@ const assert = require('assert');
 
 {
   assert.throws(() => {
-    process.binding('async_wrap');
-  }, common.expectsError({
-    code: 'ERR_ACCESS_DENIED',
-  }));
-}
-
-{
-  assert.throws(() => {
     process.binding('fs');
   }, common.expectsError({
     code: 'ERR_ACCESS_DENIED',
