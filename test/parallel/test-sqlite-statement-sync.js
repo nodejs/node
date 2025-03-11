b5b51dc258cf969a3cfd68a04aab76f3448f8e3a
Colin Ihrig <cjihrig@gmail.com>
2025-03-10 20:33:27 -0400

    sqlite: reset statement immediately in run()
    This commit updates StatementSync.prototype.run() to reset the
    prepared statement immediately after calling sqlite3_step() to
    return the correct change metadata.
    
    Fixes: https://github.com/nodejs/node/issues/57344
    PR-URL: https://github.com/nodejs/node/pull/57350
    Reviewed-By: Edy Silva <edigleyssonsilva@gmail.com>
    Reviewed-By: Jake Yuesong Li <jake.yuesong@gmail.com>

--- test/parallel/test-sqlite-statement-sync.js
+++ test/parallel/test-sqlite-statement-sync.js
@@ -169,6 +169,25 @@ suite('StatementSync.prototype.run()', () => {
       errstr: 'constraint failed',
     });
   });
+
+  test('returns correct metadata when using RETURNING', (t) => {
+    const db = new DatabaseSync(':memory:');
+    const setup = db.exec(
+      'CREATE TABLE data(key INTEGER PRIMARY KEY, val INTEGER NOT NULL) STRICT;'
+    );
+    t.assert.strictEqual(setup, undefined);
+    const sql = 'INSERT INTO data (key, val) VALUES ($k, $v) RETURNING key';
+    const stmt = db.prepare(sql);
+    t.assert.deepStrictEqual(
+      stmt.run({ k: 1, v: 10 }), { changes: 1, lastInsertRowid: 1 }
+    );
+    t.assert.deepStrictEqual(
+      stmt.run({ k: 2, v: 20 }), { changes: 1, lastInsertRowid: 2 }
+    );
+    t.assert.deepStrictEqual(
+      stmt.run({ k: 3, v: 30 }), { changes: 1, lastInsertRowid: 3 }
+    );
+  });
 });
 
 suite('StatementSync.prototype.sourceSQL', () => {
