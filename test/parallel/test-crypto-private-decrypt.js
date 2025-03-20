756185df293d312fe77239d099ec683ee93181b8
Amit Prajapati <115357958+AmitPrajapati-1@users.noreply.github.com>
2025-03-20 15:47:15 +0000

    Fix lint errors and add hasCrypto check in test-crypto-private-decrypt.js

--- test/parallel/test-crypto-private-decrypt.js
+++ test/parallel/test-crypto-private-decrypt.js
@@ -1,27 +1,31 @@
-// file: test-crypto-private-decrypt.js
+'use strict';
+
+const common = require('../common');
 const crypto = require('crypto');
 
-const keys = crypto.generateKeyPairSync(
-    'rsa',
-    {
-        modulusLength: 2048,
-        publicKeyEncoding: { type: 'spki', format: 'pem' },
-        privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
-    }
-);
+if (!common.hasCrypto) {
+  common.skip('missing crypto');
+}
+
+const keys = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+});
 
 const empty = '';
 
 const ciphertext = crypto.publicEncrypt({
-    oaepHash:'sha1',
-    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    key: keys.publicKey,
+  oaepHash: 'sha1',
+  padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+  key: keys.publicKey
 }, Buffer.from(empty)).toString('base64');
 
 const plaintext = crypto.privateDecrypt({
-    oaepHash: 'sha1',
-    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    key: keys.privateKey
+  oaepHash: 'sha1',
+  padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+  key: keys.privateKey
 }, Buffer.from(ciphertext, 'base64')).toString('utf8').trim();
-console.log("Decrypt",plaintext);
+
+console.log('Decrypt', plaintext);
 console.assert(empty === plaintext, 'rsa-oaep `encrypt` empty string is success, but `decrypt` got unexpected string.');
