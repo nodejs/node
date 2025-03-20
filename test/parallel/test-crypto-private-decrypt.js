4eb99a4770fc38ece8359d9ac186a67417e96bff
Amit Prajapati <115357958+AmitPrajapati-1@users.noreply.github.com>
2025-03-20 15:26:16 +0000

    Fix RSA-OAEP encryption for empty strings

---
+++ test/parallel/test-crypto-private-decrypt.js
@@ -0,0 +1,27 @@
+// file: test-crypto-private-decrypt.js
+const crypto = require('crypto');
+
+const keys = crypto.generateKeyPairSync(
+    'rsa',
+    {
+        modulusLength: 2048,
+        publicKeyEncoding: { type: 'spki', format: 'pem' },
+        privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+    }
+);
+
+const empty = '';
+
+const ciphertext = crypto.publicEncrypt({
+    oaepHash:'sha1',
+    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    key: keys.publicKey,
+}, Buffer.from(empty)).toString('base64');
+
+const plaintext = crypto.privateDecrypt({
+    oaepHash: 'sha1',
+    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    key: keys.privateKey
+}, Buffer.from(ciphertext, 'base64')).toString('utf8').trim();
+console.log("Decrypt",plaintext);
+console.assert(empty === plaintext, 'rsa-oaep `encrypt` empty string is success, but `decrypt` got unexpected string.');
