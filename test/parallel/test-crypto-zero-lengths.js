547d3cf8c9346868766ec0d3373d25606634ca9a
Filip Skokan <panva.ip@gmail.com>
2025-03-21 16:16:08 +0100

    fixup! crypto: fix output of privateDecrypt with zero-length data

--- test/parallel/test-crypto-zero-lengths.js
+++ test/parallel/test-crypto-zero-lengths.js
@@ -1,11 +1,12 @@
-import * as common from '../common/index.mjs';
+'use strict';
+const common = require('../common');
 
 if (!common.hasCrypto)
   common.skip('missing crypto');
 
-import * as fixtures from '../common/fixtures.mjs';
-import assert from 'assert';
-import crypto from 'crypto';
+const fixtures = require('../common/fixtures');
+const assert = require('assert');
+const crypto = require('crypto');
 
 const { subtle } = globalThis.crypto;
 
@@ -37,15 +38,18 @@ const { subtle } = globalThis.crypto;
   }
 
   {
-    const pkcs8 = privateKey.export({ format: 'der', type: 'pkcs8' });
-    const spki = publicKey.export({ format: 'der', type: 'spki' });
-    const kp = {
-      privateKey: await subtle.importKey('pkcs8', pkcs8, { name: 'RSA-OAEP', hash: 'SHA-1' }, false, ['decrypt']),
-      publicKey: await subtle.importKey('spki', spki, { name: 'RSA-OAEP', hash: 'SHA-1' }, false, ['encrypt']),
-    };
+    (async () => {
+      const pkcs8 = privateKey.export({ format: 'der', type: 'pkcs8' });
+      const spki = publicKey.export({ format: 'der', type: 'spki' });
 
-    const ciphertext = await subtle.encrypt('RSA-OAEP', kp.publicKey, data);
-    const plaintext = await subtle.decrypt('RSA-OAEP', kp.privateKey, ciphertext);
-    assert.deepStrictEqual(plaintext, data.buffer);
+      const kp = {
+        privateKey: await subtle.importKey('pkcs8', pkcs8, { name: 'RSA-OAEP', hash: 'SHA-1' }, false, ['decrypt']),
+        publicKey: await subtle.importKey('spki', spki, { name: 'RSA-OAEP', hash: 'SHA-1' }, false, ['encrypt']),
+      };
+
+      const ciphertext = await subtle.encrypt('RSA-OAEP', kp.publicKey, data);
+      const plaintext = await subtle.decrypt('RSA-OAEP', kp.privateKey, ciphertext);
+      assert.deepStrictEqual(plaintext, data.buffer);
+    })().then(common.mustCall());
   }
 }
