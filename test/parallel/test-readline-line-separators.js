1d47e32290c79ed22a9262944cdf3f653bdf0e94
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2025-03-22 19:32:49 +0000

    fixup! readline: fix unicode line separators being ignored
    also include \r\n in test

--- test/parallel/test-readline-line-separators.js
+++ test/parallel/test-readline-line-separators.js
@@ -4,7 +4,7 @@ const assert = require('node:assert');
 const readline = require('node:readline');
 const { Readable } = require('node:stream');
 
-const str = '123\n456\r789\u{2028}ABC\u{2029}DEF';
+const str = '012\n345\r67\r\n89\u{2028}ABC\u{2029}DEF';
 
 const rli = new readline.Interface({
   input: Readable.from(str),
@@ -14,7 +14,7 @@ const linesRead = [];
 rli.on('line', (line) => linesRead.push(line));
 
 rli.on('close', common.mustCall(() => {
-  const regexpLines = str.split(/^/m).map((line) => line.trim());
+  const regexpLines = str.split(/^/m).map((line) => line.trim()).filter(Boolean);
   // Readline interprets different lines in the same way js regular expressions do
   assert.deepStrictEqual(linesRead, regexpLines);
 }));
