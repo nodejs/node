9213b5b13218d3fd3dcbe9bbfe40545cefb8c271
flakey5 <73616808+flakey5@users.noreply.github.com>
2025-03-25 08:43:23 -0700

    build, doc: use new api doc tooling
    Switches over to using the new doc generation tooling.
    For more background on this, please see #52343
    
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>
    
    Co-authored-by: Claudio W <cwunder@gnome.org>
    Co-authored-by: avivkeller <me@aviv.sh>
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>

---
+++ tools/doc/update.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if ! [ -x "$NODE" ]; then
+    NODE=$(command -v node)
+
+    if ! [ -x "$NODE" ]; then
+        echo node not found
+        exit 1
+    fi
+fi
+
+if ! [ -x "$NPM" ]; then
+    NPM=../../deps/npm/bin/npm-cli.js
+
+    if ! [ -x "$NPM" ]; then
+        echo npm not found
+        exit 1
+    fi
+fi
+
+latest_commit=$("$NODE" ./get-latest-commit.mjs)
+
+rm -rf node_modules/ package-lock.json
+
+"$NODE" "$NPM" install "git://github.com/nodejs/doc-kit#$latest_commit"
