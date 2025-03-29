c1c48ecb0c415b53ed32264c0496aa3378fc549e
Jake Yuesong Li <jake.yuesong@gmail.com>
2025-03-29 22:21:15 +1030

    test_runner: improve --test-timeout to be per test
    Previously `--test-timeout` is set on per test execution, this is
    obviously a bug as per test execution is hard to be expected, this patch
    addresses the issue by setting `timeout` from per execution to per test.
    
    This patch also fixes a minor issue that `--test-timeout` is not being
    respected when running without `--test`.

---
+++ test/fixtures/test-runner/output/test-timeout-flag.js
@@ -0,0 +1,16 @@
+// Flags: --test-timeout=500
+'use strict';
+const { describe, test } = require('node:test');
+const { setTimeout } = require('node:timers/promises');
+
+describe('--test-timeout is set to 500ms', () => {
+  test('test 1 must fail due to testTimeoutFailure with error test timed out after 500ms', async () => {
+    await setTimeout(1000);
+  })
+  test('test 2  must fail due to testTimeoutFailure with error test timed out after 600ms', { timeout: 600 }, async () => {
+    await setTimeout(1000);
+  })
+  test('test 3 must pass', async () => {
+    await setTimeout(10);
+  })
+});
--- test/parallel/test-runner-cli-timeout.js
+++ test/parallel/test-runner-cli-timeout.js
@@ -10,13 +10,13 @@ const env = { ...process.env, 'NODE_DEBUG': 'test_runner' };
 test('default timeout -- Infinity', async () => {
   const args = ['--test'];
   const cp = spawnSync(process.execPath, args, { cwd, env });
-  assert.match(cp.stderr.toString(), /timeout: Infinity,/);
+  assert.match(cp.stderr.toString(), /timeoutPerTest: Infinity/);
 });
 
 test('timeout of 10ms', async () => {
   const args = ['--test', '--test-timeout', 10];
   const cp = spawnSync(process.execPath, args, { cwd, env });
-  assert.match(cp.stderr.toString(), /timeout: 10,/);
+  assert.match(cp.stderr.toString(), /timeoutPerTest: 10/);
 });
 
 test('isolation=none uses the --test-timeout flag', async () => {
@@ -24,5 +24,5 @@ test('isolation=none uses the --test-timeout flag', async () => {
     '--test', '--test-isolation=none', '--test-timeout=10',
   ];
   const cp = spawnSync(process.execPath, args, { cwd, env });
-  assert.match(cp.stderr.toString(), /timeout: 10,/);
+  assert.match(cp.stderr.toString(), /timeoutPerTest: 10/);
 });
