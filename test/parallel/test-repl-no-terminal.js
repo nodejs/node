a55f5d5e634f9839b81c157d695d88db8c11ef78
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2025-03-29 22:20:33 +0000

    readline: add stricter validation for functions called after closed
    PR-URL: https://github.com/nodejs/node/pull/57680
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Edy Silva <edigleyssonsilva@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- test/parallel/test-repl-no-terminal.js
+++ test/parallel/test-repl-no-terminal.js
@@ -1,7 +1,12 @@
 'use strict';
 const common = require('../common');
-
+const ArrayStream = require('../common/arraystream');
 const repl = require('repl');
-const r = repl.start({ terminal: false });
-r.setupHistory('/nonexistent/file', common.mustSucceed());
-process.stdin.unref?.();
+
+const stream = new ArrayStream();
+
+const replServer = repl.start({ terminal: false, input: stream, output: stream });
+
+replServer.setupHistory('/nonexistent/file', common.mustSucceed(() => {
+  replServer.close();
+}));
