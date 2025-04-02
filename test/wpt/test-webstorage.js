a8de4a7a8b550162c89bd6208102c6c60ba3cf9f
Daniel M Brasil <danielmbrasil@protonmail.com>
2025-04-02 14:10:19 -0300

    test: remove --experimental-webstorage flag from tests

--- test/wpt/test-webstorage.js
+++ test/wpt/test-webstorage.js
@@ -9,7 +9,6 @@ const runner = new WPTRunner('webstorage', { concurrency: 1 });
 tmpdir.refresh();
 
 runner.setFlags([
-  '--experimental-webstorage',
   '--localstorage-file', join(tmpdir.path, 'wpt-tests.localstorage'),
 ]);
 runner.setInitScript(`
