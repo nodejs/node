70bb387f8274d5fc60d4801a74c500e6afeb5f70
Simon Zünd <szuend@chromium.org>
2025-04-03 21:38:27 +0200

    src: fix inefficient usage of v8_inspector::StringView
    v8_inspector::StringView can either be one-byte or two-byte strings.
    Node has currently two places where it's unconditionally assumed that
    it's a two-byte StringView.
    
    This requires the upstream V8 inspector to unnecessarily create
    a copy of the string:
    
    https://source.chromium.org/chromium/chromium/src/+/main:v8/src/inspector/v8-inspector-session-impl.cc;l=192-199;drc=05bacd38e7a31e92afe0fd66081dfa2cc03b934a
    
    This is particularly slow, especially for large CDP messages, as the
    serialized JSON is iterated 8-bit char by 8-bit char and each one
    widened to a 16-bit char.
    
    This PR introduces a small helper that correctly converts a StringView
    to a v8::String instance honoring the "is8Bit" flag. This allows
    upstream V8 to remove the unnecessary widening copy.
    
    PR-URL: https://github.com/nodejs/node/pull/52372
    Reviewed-By: Anna Henningsen <anna@addaleax.net>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Minwoo Jung <nodecorelab@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>

--- src/util.h
+++ src/util.h
@@ -25,6 +25,7 @@
 #if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
 
 #include "uv.h"
+#include "v8-inspector.h"
 #include "v8.h"
 
 #include "node.h"
@@ -720,6 +721,9 @@ std::vector<std::string_view> SplitString(const std::string_view in,
 inline v8::MaybeLocal<v8::Value> ToV8Value(v8::Local<v8::Context> context,
                                            std::string_view str,
                                            v8::Isolate* isolate = nullptr);
+inline v8::MaybeLocal<v8::Value> ToV8Value(v8::Local<v8::Context> context,
+                                           v8_inspector::StringView str,
+                                           v8::Isolate* isolate);
 template <typename T, typename test_for_number =
     typename std::enable_if<std::numeric_limits<T>::is_specialized, bool>::type>
 inline v8::MaybeLocal<v8::Value> ToV8Value(v8::Local<v8::Context> context,
