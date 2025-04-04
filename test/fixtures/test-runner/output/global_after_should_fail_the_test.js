e9c115733eb21a163898ba2f492373593caa7c2a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-04-04 10:44:21 +0200

    tools: enable linter in `test/fixtures/test-runner/output`
    PR-URL: https://github.com/nodejs/node/pull/57698
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Pietro Marchini <pietro.marchini94@gmail.com>

--- test/fixtures/test-runner/output/global_after_should_fail_the_test.js
+++ test/fixtures/test-runner/output/global_after_should_fail_the_test.js
@@ -2,9 +2,9 @@
 const { it, after } = require('node:test');
 
 after(() => {
-  throw new Error('this should fail the test')
+  throw new Error('this should fail the test');
 });
 
 it('this is a test', () => {
-  console.log('this is a test')
+  console.log('this is a test');
 });
--- test/fixtures/test-runner/output/lcov_reporter.js
+++ test/fixtures/test-runner/output/lcov_reporter.js
@@ -4,4 +4,10 @@ const fixtures = require('../../../common/fixtures');
 const spawn = require('node:child_process').spawn;
 
 spawn(process.execPath,
-      ['--no-warnings', '--experimental-test-coverage', '--test-reporter', 'lcov', fixtures.path('test-runner/output/output.js')], { stdio: 'inherit' });
+      [
+        '--no-warnings',
+        '--experimental-test-coverage',
+        '--test-reporter',
+        'lcov',
+        fixtures.path('test-runner/output/output.js'),
+      ], { stdio: 'inherit' });
