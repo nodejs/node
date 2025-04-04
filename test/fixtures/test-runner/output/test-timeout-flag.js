6559293f514baf9c0e1ae141e8a5b4c6b9a1f8d8
Jake Yuesong Li <jake.yuesong@gmail.com>
2025-04-05 02:54:49 +1030

    remove deuplicated if checks and move this.timeout into FileTest and Suite, unref timeout

--- test/fixtures/test-runner/output/test-timeout-flag.js
+++ test/fixtures/test-runner/output/test-timeout-flag.js
@@ -1,17 +1,25 @@
 // Flags: --test-timeout=20
 'use strict';
-const { describe, test } = require('node:test');
+const { describe, test, afterEach } = require('node:test');
 const { setTimeout } = require('node:timers/promises');
+
+let timeout;
 
 describe('--test-timeout is set to 20ms', () => {
   test('should timeout after 20ms', async () => {
-    await setTimeout(2000);
+    timeout = await setTimeout(2000);
   });
   test('should timeout after 5ms', { timeout: 5 }, async () => {
-    await setTimeout(2000);
+    timeout = await setTimeout(2000);
   });
   test('should not timeout', { timeout: 5000 }, async () => {
     await setTimeout(200);
   });
   test('should pass', async () => {});
+
+  afterEach(() => {
+    if (timeout) {
+      timeout.unref();
+    }
+  });
 });
