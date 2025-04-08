04f280b6b790b4b53c23e9db1beaf3986a181a54
Jake Yuesong Li <jake.yuesong@gmail.com>
2025-04-09 02:36:48 +0930

    fixup! address more comments

--- test/fixtures/test-runner/output/test-timeout-flag.js
+++ test/fixtures/test-runner/output/test-timeout-flag.js
@@ -1,25 +1,19 @@
 // Flags: --test-timeout=20
 'use strict';
-const { describe, test, afterEach } = require('node:test');
+const { describe, test } = require('node:test');
 const { setTimeout } = require('node:timers/promises');
-
-let timeout;
 
 describe('--test-timeout is set to 20ms', () => {
   test('should timeout after 20ms', async () => {
-    timeout = await setTimeout(2000);
+    await setTimeout(200000, undefined, { ref: false });
   });
   test('should timeout after 5ms', { timeout: 5 }, async () => {
-    timeout = await setTimeout(2000);
+    await setTimeout(200000, undefined, { ref: false });
   });
-  test('should not timeout', { timeout: 5000 }, async () => {
-    await setTimeout(200);
-  });
-  test('should pass', async () => {});
 
-  afterEach(() => {
-    if (timeout) {
-      timeout.unref();
-    }
+  test('should not timeout', { timeout: 50000 }, async () => {
+    await  setTimeout(1);
   });
+
+  test('should pass', async () => {});
 });
