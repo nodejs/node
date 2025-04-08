9fcffb49d0c7734d45e5f7d722e9412ca80986b6
Mert Can Altin <mertgold60@gmail.com>
2025-04-08 11:03:43 +0300

    fs: add signal option to fs.stat()

---
+++ test/parallel/test-fs-stat-abort-test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('../common');
+const test = require('node:test');
+const assert = require('node:assert');
+const fs = require('node:fs');
+
+const filePath = './temp.txt';
+fs.writeFileSync(filePath, 'Test');
+
+test('fs.stat should throw AbortError when abort signal is triggered', async () => {
+  const signal = AbortSignal.abort();
+
+  const { promise, resolve, reject } = Promise.withResolvers();
+  fs.stat(filePath, { signal }, (err, stats) => {
+    if (err) {
+      return reject(err);
+    }
+    resolve(stats);
+  });
+
+  await assert.rejects(promise, { name: 'AbortError' });
+
+  fs.unlinkSync(filePath);
+});
