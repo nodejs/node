0f36493e0af383bd19d33a46366826ab58a0a324
Kushagra Pandey <pandeykushagra51@gmail.com>
2025-04-10 02:04:51 +0530

    test: fixed request-response handling

--- test/parallel/test-http2-session-graceful-close.js
+++ test/parallel/test-http2-session-graceful-close.js
@@ -7,23 +7,17 @@ const assert = require('assert');
 const h2 = require('http2');
 
 const server = h2.createServer();
-let response;
+let session;
 
-server.on('session', common.mustCall(function(session) {
-  session.on('stream', common.mustCall(function (stream) {
-    response.end();
-    session.close();
-  }));
-  session.on('close', common.mustCall(function() {
-    server.close();
-  }));
+server.on('session', common.mustCall(function(s) {
+    session = s;
+    session.on('close', common.mustCall(function() {
+        server.close();
+    }));
 }));
 
 server.listen(0, common.mustCall(function() {
   const port = server.address().port;
-  server.once('request', common.mustCall(function (request, resp) {
-    response = resp;
-  }));
 
   const url = `http://localhost:${port}`;
   const client = h2.connect(url, common.mustCall(function() {
@@ -44,4 +38,11 @@ server.listen(0, common.mustCall(function() {
     request.resume();
   }));
   client.on('goaway', common.mustCallAtLeast(1));
+}));
+
+server.once('request', common.mustCall(function(request, response) {
+    response.on('finish', common.mustCall(function() {
+        session.close();
+    }));
+    response.end();
 }));
