46a23403f002e2eb52eee4bcfe0eb068728a9d9b
Pietro Marchini <pietro.marchini94@gmail.com>
2025-04-09 20:46:03 +0200

    test_runner: remove exit event listeners in harness

--- test/parallel/test-runner-global-setup-watch-mode.mjs
+++ test/parallel/test-runner-global-setup-watch-mode.mjs
@@ -16,8 +16,6 @@ let fixturePaths;
 
 // This test updates these files repeatedly,
 // Reading them from disk is unreliable due to race conditions.
-// We're using ipc to signal the child process to exit
-// as process.kill() does not work on Windows preventing the harness teardown from completing.
 const fixtureContent = {
   'test.js': `
 const test = require('node:test');
@@ -30,7 +28,7 @@ async function globalSetup({ context }) {
   console.log('Global setup executed');
   process.on('message', (message) => {
     if (message === 'exit') {
-      process.exit(0);
+      process.kill(process.pid, 'SIGINT');
     }
   });
 }
@@ -55,68 +53,73 @@ describe('test runner watch mode with global setup hooks', () => {
   beforeEach(refresh);
   for (const isolation of ['none', 'process']) {
     describe(`isolation: ${isolation}`, () => {
-      it(`should run global setup/teardown hooks with each test run in watch mode`, async () => {
-        const globalSetupFileFixture = fixturePaths['global-setup-teardown.js'];
-        const ran1 = Promise.withResolvers();
-        const ran2 = Promise.withResolvers();
+      it(`should run global setup/teardown hooks with each test run in watch mode`,
+        // TODO(pmarchini): Skip test on Windows as the VS2022 build
+        // has issues handling SIGTERM and SIGINT signals correctly.
+        // See: https://github.com/nodejs/node/issues/46097
+         { todo: common.isWindows() },
+         async () => {
+           const globalSetupFileFixture = fixturePaths['global-setup-teardown.js'];
+           const ran1 = Promise.withResolvers();
+           const ran2 = Promise.withResolvers();
 
-        const child = spawn(process.execPath,
-                            [
-                              '--watch',
-                              '--test',
-                              '--test-reporter=spec',
-                              `--test-isolation=${isolation}`,
-                              '--test-global-setup=' + globalSetupFileFixture,
-                              fixturePaths['test.js'],
-                            ],
-                            {
-                              encoding: 'utf8',
-                              stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
-                              cwd: tmpdir.path,
-                            });
+           const child = spawn(process.execPath,
+                               [
+                                 '--watch',
+                                 '--test',
+                                 '--test-reporter=spec',
+                                 `--test-isolation=${isolation}`,
+                                 '--test-global-setup=' + globalSetupFileFixture,
+                                 fixturePaths['test.js'],
+                               ],
+                               {
+                                 encoding: 'utf8',
+                                 stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+                                 cwd: tmpdir.path,
+                               });
 
-        let stdout = '';
-        let currentRun = '';
-        const runs = [];
+           let stdout = '';
+           let currentRun = '';
+           const runs = [];
 
-        child.stdout.on('data', (data) => {
-          stdout += data.toString();
-          currentRun += data.toString();
-          const testRuns = stdout.match(/duration_ms\s\d+/g);
-          if (testRuns?.length >= 1) ran1.resolve();
-          if (testRuns?.length >= 2) ran2.resolve();
-        });
+           child.stdout.on('data', (data) => {
+             stdout += data.toString();
+             currentRun += data.toString();
+             const testRuns = stdout.match(/duration_ms\s\d+/g);
+             if (testRuns?.length >= 1) ran1.resolve();
+             if (testRuns?.length >= 2) ran2.resolve();
+           });
 
-        await ran1.promise;
-        runs.push(currentRun);
-        currentRun = '';
+           await ran1.promise;
+           runs.push(currentRun);
+           currentRun = '';
 
-        const content = fixtureContent['test.js'];
-        const path = fixturePaths['test.js'];
-        writeFileSync(path, content);
+           const content = fixtureContent['test.js'];
+           const path = fixturePaths['test.js'];
+           writeFileSync(path, content);
 
-        await ran2.promise;
-        runs.push(currentRun);
+           await ran2.promise;
+           runs.push(currentRun);
 
-        currentRun = '';
-        child.send('exit');
-        await once(child, 'exit');
+           currentRun = '';
+           child.send('exit');
+           await once(child, 'exit');
 
-        assert.match(runs[0], /Global setup executed/);
-        assert.match(runs[0], /tests 1/);
-        assert.match(runs[0], /pass 1/);
-        assert.match(runs[0], /fail 0/);
+           assert.match(runs[0], /Global setup executed/);
+           assert.match(runs[0], /tests 1/);
+           assert.match(runs[0], /pass 1/);
+           assert.match(runs[0], /fail 0/);
 
-        assert.doesNotMatch(runs[1], /Global setup executed/);
-        assert.doesNotMatch(runs[1], /Global teardown executed/);
-        assert.match(runs[1], /tests 1/);
-        assert.match(runs[1], /pass 1/);
-        assert.match(runs[1], /fail 0/);
+           assert.doesNotMatch(runs[1], /Global setup executed/);
+           assert.doesNotMatch(runs[1], /Global teardown executed/);
+           assert.match(runs[1], /tests 1/);
+           assert.match(runs[1], /pass 1/);
+           assert.match(runs[1], /fail 0/);
 
-        // Verify stdout after killing the child
-        assert.doesNotMatch(currentRun, /Global setup executed/);
-        assert.match(currentRun, /Global teardown executed/);
-      });
+           // Verify stdout after killing the child
+           assert.doesNotMatch(currentRun, /Global setup executed/);
+           assert.match(currentRun, /Global teardown executed/);
+         });
     });
   }
 });
