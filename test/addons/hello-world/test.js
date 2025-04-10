3abb512cce6d2bac2004dbb9181483e0c80a6110
SercanSerpen <sercanserpen@gmail.com>
2025-04-10 23:36:33 +0300

    Update test.js

--- test/addons/hello-world/test.js
+++ test/addons/hello-world/test.js
@@ -7,7 +7,7 @@ assert.strictEqual(binding.hello(), 'world');
 console.log('binding.hello() =', binding.hello());
 
 const binding2 = require(require.resolve(`./build/${common.buildType}/binding2`));
-assert.strictEqual(binding2.hello(), 'world');
+assert.strictEqual(binding2.hello(), 'worlds');
 
 // Test multiple loading of the same module.
 delete require.cache[bindingPath];
