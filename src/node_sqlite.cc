34350019f871c047decfca64d762d1070a6fd9b1
Chengzhong Wu <cwu631@bloomberg.net>
2025-04-10 18:25:10 +0200

    src: add BaseObjectPtr nullptr operations
    Allow comparing a `BaseObjectPtr` or implicitly construct a
    `BaseObjectPtr` with `nullptr`.
    
    PR-URL: https://github.com/nodejs/node/pull/56585
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/node_sqlite.cc
+++ src/node_sqlite.cc
@@ -2331,7 +2331,7 @@ BaseObjectPtr<StatementSync> StatementSync::Create(
            ->InstanceTemplate()
            ->NewInstance(env->context())
            .ToLocal(&obj)) {
-    return BaseObjectPtr<StatementSync>();
+    return nullptr;
   }
 
   return MakeBaseObject<StatementSync>(env, obj, std::move(db), stmt);
@@ -2485,7 +2485,7 @@ BaseObjectPtr<Session> Session::Create(Environment* env,
            ->InstanceTemplate()
            ->NewInstance(env->context())
            .ToLocal(&obj)) {
-    return BaseObjectPtr<Session>();
+    return nullptr;
   }
 
   return MakeBaseObject<Session>(env, obj, std::move(database), session);
