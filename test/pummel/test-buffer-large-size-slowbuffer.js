795dd8eb7988ae38553e28d167cd91139f28f90d
jakecastelli <38635403+jakecastelli@users.noreply.github.com>
2025-04-11 13:01:26 +0930

    test: deflake test-buffer-large-size
    PR-URL: https://github.com/nodejs/node/pull/57789
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/pummel/test-buffer-large-size-slowbuffer.js
@@ -0,0 +1,22 @@
+'use strict';
+const common = require('../common');
+
+// Buffer with size > INT32_MAX
+common.skipIf32Bits();
+
+const assert = require('node:assert');
+const {
+  SlowBuffer,
+} = require('node:buffer');
+
+const size = 2 ** 31;
+
+// Test SlowBuffer with size larger than integer range
+try {
+  assert.throws(() => SlowBuffer(size).toString('utf8'), { code: 'ERR_STRING_TOO_LONG' });
+} catch (e) {
+  if (e.code !== 'ERR_MEMORY_ALLOCATION_FAILED') {
+    throw e;
+  }
+  common.skip('insufficient space for SlowBuffer');
+}
