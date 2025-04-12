f5a038b7472fd4ff281da6da283c7998302538fc
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2025-04-12 23:07:37 +0100

    Revert "move `process.stdin.unref` call inside `setupHistory` callback"
    This reverts commit e388d3bb0df68580068d6d38bded5e525a73aa7f.
    
    Note: it didn't really solve the CI issue unfortunately

--- test/parallel/test-repl-no-terminal.js
+++ test/parallel/test-repl-no-terminal.js
@@ -3,6 +3,5 @@ const common = require('../common');
 
 const repl = require('repl');
 const r = repl.start({ terminal: false });
-r.setupHistory('/nonexistent/file', common.mustSucceed(() => {
-  process.stdin.unref?.();
-}));
+r.setupHistory('/nonexistent/file', common.mustSucceed());
+process.stdin.unref?.();
