f1277a9c52a08333c62dd2ed899c1366f9dfc993
Gürgün Dayıoğlu <hey@gurgun.day>
2025-04-13 21:06:27 +0200

    lazy load asynclocalstorage and make the test more compact

--- test/async-hooks/test-async-local-storage-stream-finished.js
+++ test/async-hooks/test-async-local-storage-stream-finished.js
@@ -1,36 +1,20 @@
 'use strict';
 
 const common = require('../common');
-const assert = require('assert');
-const http = require('http');
+const { Readable, finished } = require('stream');
 const { AsyncLocalStorage } = require('async_hooks');
-const { finished } = require('stream');
+const { strictEqual } = require('assert');
 
 // This test verifies that AsyncLocalStorage context is maintained
 // when using stream.finished()
 
+const readable = new Readable();
 const als = new AsyncLocalStorage();
-const store = { foo: 'bar' };
 
-{
-  const server = http.createServer(common.mustCall((req, res) => {
-    als.run(store, () => {
-      finished(res, common.mustCall(() => {
-        assert.strictEqual(als.getStore()?.foo, 'bar');
-      }));
-    });
-
-    setTimeout(() => res.end(), 0);
+als.run(321, () => {
+  finished(readable, common.mustCall(() => {
+    strictEqual(als.getStore(), 321);
   }));
+});
 
-  server.listen(0, common.mustCall(() => {
-    const port = server.address().port;
-
-    http.get(`http://localhost:${port}`, common.mustCall((res) => {
-      res.resume();
-      res.on('end', common.mustCall(() => {
-        server.close();
-      }));
-    }));
-  }));
-}
+readable.destroy();
