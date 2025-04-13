b2bf620c7b2e60f2ba2b7e6b10c61d00f6325e71
Chengzhong Wu <legendecas@gmail.com>
2025-04-13 18:43:24 +0100

    src: internalize `v8::ConvertableToTraceFormat` in traces
    `v8::ConvertableToTraceFormat` is only available in legacy V8 tracing
    API and no longer supported in perfetto. This internalize
    `node::tracing::TracedValue` and `v8::ConvertableToTraceFormat` by
    defining specialized trace argument classes.
    
    The newly defined structured trace argument classes can be easily
    converted to `perfetto::TracedValue` by perfetto traced value protocol.
    For example, when adding perfetto support, `CastTracedValue` will be a
    no-op and these classes can add a new conversion method like:
    
    ```cpp
    class Foo {
      void WriteIntoTrace(TracedValue context) const {
        auto dict = std::move(context).WriteDictionary();
        dict->Add("key", 42);
        dict->Add("foo", "bar");
        dict->Add("member", member_);
      }
    };
    ```
    
    PR-URL: https://github.com/nodejs/node/pull/57866
    Refs: https://github.com/nodejs/diagnostics/issues/654
    Refs: https://github.com/google/perfetto/blob/9ddf987d48cdfd9129987a3af1e85052c377756f/include/perfetto/tracing/traced_value.h#L46
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

--- src/async_wrap.h
+++ src/async_wrap.h
@@ -177,6 +177,7 @@ class AsyncWrap : public BaseObject {
 
   void EmitDestroy(bool from_gc = false);
 
+  void EmitTraceAsyncStart() const;
   void EmitTraceEventBefore();
   static void EmitTraceEventAfter(ProviderType type, double async_id);
   void EmitTraceEventDestroy();
