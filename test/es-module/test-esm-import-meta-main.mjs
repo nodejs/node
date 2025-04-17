cc753b97ab342b7ca8b1a34fbfa27c96820dae87
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-17 16:26:16 +0100

    fix: failing test in folder with unusual characters

--- test/es-module/test-esm-import-meta-main.mjs
+++ test/es-module/test-esm-import-meta-main.mjs
@@ -1,10 +1,9 @@
 import { mustCall } from '../common/index.mjs';
-import fixtures from '../common/fixtures.js';
 import assert from 'assert';
 
 assert.strictEqual(import.meta.main, true);
 
-import(fixtures.path('/es-modules/import-meta-main.mjs')).then(
+import('../fixtures/es-modules/import-meta-main.mjs').then(
   mustCall(({ isMain }) => {
     assert.strictEqual(isMain, false);
   })
