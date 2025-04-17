a1c081f27467d3c91075138b00266612400dd653
LiviaMedeiros <livia@cirno.name>
2025-04-17 16:40:11 +0800

    squash: fix test
    Number.EPSILON < 2**-24

--- test/parallel/test-assert-typedarray-deepequal.js
+++ test/parallel/test-assert-typedarray-deepequal.js
@@ -78,7 +78,7 @@ suite('notEqualArrayPairs', () => {
     [new Int16Array([0]), new Uint16Array([256])],
     [new Int16Array([-256]), new Uint16Array([0xff00])], // same bits
     [new Int32Array([-256]), new Uint32Array([0xffffff00])], // ditto
-    [new Float16Array([Number.EPSILON]), new Float16Array([0.0])],
+    [new Float16Array([0.1]), new Float16Array([0.0])],
     [new Float16Array([0.1]), new Float16Array([0.1, 0.2])],
     [new Float32Array([0.1]), new Float32Array([0.0])],
     [new Float32Array([0.1]), new Float32Array([0.1, 0.2])],
