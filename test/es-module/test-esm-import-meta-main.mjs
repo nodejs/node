c1223c26f01ef9e990a1c79536bddb2a5f55d35f
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-20 15:26:33 +0100

    improve assertion messages

--- test/es-module/test-esm-import-meta-main.mjs
+++ test/es-module/test-esm-import-meta-main.mjs
@@ -1,12 +1,26 @@
 import { mustCall } from '../common/index.mjs';
 import assert from 'node:assert/strict';
+
 import { Worker } from 'node:worker_threads';
 
-assert.strictEqual(import.meta.main, true);
+function get_environment() {
+  if (process.env.HAS_STARTED_WORKER) return "in worker thread started by ES Module";
+  return "in ES Module";
+}
+
+assert.strictEqual(
+  import.meta.main,
+  true,
+  `\`import.meta.main\` at top-level module ${get_environment()} should evaluate \`true\``
+);
 
 import('../fixtures/es-modules/import-meta-main.mjs').then(
   mustCall(({ isMain }) => {
-    assert.strictEqual(isMain, false);
+    assert.strictEqual(
+      isMain,
+      false,
+      `\`import.meta.main\` at dynamically imported module ${get_environment()} should evaluate \`false\``
+    );
   })
 );
 
