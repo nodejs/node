e544c81424748be66d17a2032a7382dc8ee3d04b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-20 15:57:42 +0100

    style: fix linter complaints

--- test/es-module/test-esm-import-meta-main-eval.mjs
+++ test/es-module/test-esm-import-meta-main-eval.mjs
@@ -14,24 +14,24 @@ import('../fixtures/es-modules/import-meta-main.mjs').then(
     assert.strictEqual(isMain, false);
   })
 );
-`
+`;
 
 const scriptInWorker = `
 import { Worker } from 'node:worker_threads';
 new Worker(\`${script}\`, { eval: true });
-`
+`;
 
 function test_evaluated_script() {
-  let [cmd, opts] = escapePOSIXShell`"${process.execPath}" ${execOptions} --eval "${script}"`;
-  opts.cwd = import.meta.dirname
+  const [cmd, opts] = escapePOSIXShell`"${process.execPath}" ${execOptions} --eval "${script}"`;
+  opts.cwd = import.meta.dirname;
   child.exec(cmd, opts, mustSucceed((stdout) => {}));
 }
 
 function test_evaluated_worker_script() {
-  let [cmd, opts] = escapePOSIXShell`"${process.execPath}" ${execOptions} --eval "${scriptInWorker}"`;
-  opts.cwd = import.meta.dirname
+  const [cmd, opts] = escapePOSIXShell`"${process.execPath}" ${execOptions} --eval "${scriptInWorker}"`;
+  opts.cwd = import.meta.dirname;
   child.exec(cmd, opts, mustSucceed((stdout) => {}));
 }
 
-//test_evaluated_script()
-test_evaluated_worker_script()
+test_evaluated_script();
+test_evaluated_worker_script();
