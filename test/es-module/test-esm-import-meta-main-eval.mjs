90dec9d7a58fd06f9769d314ede92d7a3a9a473e
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-21 09:24:12 +0100

    commit @aduh95's suggestions r.e. test refactor-ability

--- test/es-module/test-esm-import-meta-main-eval.mjs
+++ test/es-module/test-esm-import-meta-main-eval.mjs
@@ -1,4 +1,5 @@
 import { spawnPromisified } from '../common/index.mjs';
+import * as fixtures from '../common/fixtures.js';
 import assert from 'node:assert/strict';
 
 const execOptions = ['--input-type', 'module'];
@@ -8,20 +9,21 @@ import assert from 'node:assert/strict';
 
 assert.strictEqual(import.meta.main, true);
 
-const { isMain: importedModuleIsMain } = await import('../fixtures/es-modules/import-meta-main.mjs');
+const { isMain: importedModuleIsMain } = await import(
+    ${JSON.stringify(fixtures.fileURL('es-modules/import-meta-main.mjs'))}
+);
 assert.strictEqual(importedModuleIsMain, false);
 `;
 
 const scriptInWorker = `
 import { Worker } from 'node:worker_threads';
-new Worker(\`${script}\`, { eval: true });
+new Worker(${JSON.stringify(script)}, { eval: true });
 `;
 
 async function test_evaluated_script() {
   const { stderr, code } = await spawnPromisified(
     process.execPath,
     [...execOptions, '--eval', script],
-    { cwd: import.meta.dirname }
   );
   assert.strictEqual(stderr, '');
   assert.strictEqual(code, 0);
@@ -31,7 +33,6 @@ async function test_evaluated_worker_script() {
   const { stderr, code } = await spawnPromisified(
     process.execPath,
     [...execOptions, '--eval', scriptInWorker],
-    { cwd: import.meta.dirname }
   );
   assert.strictEqual(stderr, '');
   assert.strictEqual(code, 0);
