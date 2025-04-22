c3c12a4dd71c32f16bbf6e94e52533a0ec2ba084
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2025-04-22 20:43:27 +0200

    fixup!: add test case for re-assigning `dirname` & `filename`

--- test/es-module/test-esm-import-meta.mjs
+++ test/es-module/test-esm-import-meta.mjs
@@ -9,11 +9,8 @@ assert.deepStrictEqual(Reflect.ownKeys(import.meta), keys);
 const descriptors = Object.getOwnPropertyDescriptors(import.meta);
 for (const descriptor of Object.values(descriptors)) {
   delete descriptor.value; // Values are verified below.
-  assert.deepStrictEqual(descriptor, {
-    enumerable: true,
-    writable: true,
-    configurable: true
-  });
+  assert.strictEqual(descriptor.configurable, true);
+  assert.strictEqual(descriptor.enumerable, true);
 }
 
 const urlReg = /^file:\/\/\/.*\/test\/es-module\/test-esm-import-meta\.mjs$/;
@@ -24,10 +21,18 @@ assert(import.meta.url.match(urlReg));
 const dirReg = /^(\/|\w:\\).*(\/|\\)test(\/|\\)es-module$/;
 assert.match(import.meta.dirname, dirReg);
 
+const newDirname = 'reassigned';
+import.meta.dirname = newDirname;
+assert.strictEqual(import.meta.dirname, newDirname);
+
 // Match *nix paths: `/some/path/test/es-module/test-esm-import-meta.mjs`
 // Match Windows paths: `d:\\some\\path\\test\\es-module\\test-esm-import-meta.js`
 const fileReg = /^(\/|\w:\\).*(\/|\\)test(\/|\\)es-module(\/|\\)test-esm-import-meta\.mjs$/;
 assert.match(import.meta.filename, fileReg);
+
+const newFilename = 'reassigned.js';
+import.meta.filename = newFilename;
+assert.strictEqual(import.meta.filename, newFilename);
 
 // Verify that `data:` imports do not behave like `file:` imports.
 import dataDirname from 'data:text/javascript,export default "dirname" in import.meta';
