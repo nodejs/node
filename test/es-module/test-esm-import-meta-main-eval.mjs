8dbf1872d511c426d0c4a40f3417af63c6fb06a6
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-22 18:09:44 +0100

    add `import.meta.main` test for worker with url input

--- test/es-module/test-esm-import-meta-main-eval.mjs
+++ test/es-module/test-esm-import-meta-main-eval.mjs
@@ -7,17 +7,24 @@ const execOptions = ['--input-type', 'module'];
 const script = `
 import assert from 'node:assert/strict';
 
-assert.strictEqual(import.meta.main, true);
+assert.strictEqual(import.meta.main, true, 'import.meta.main should evaluate true in main module');
 
 const { isMain: importedModuleIsMain } = await import(
     ${JSON.stringify(fixtures.fileURL('es-modules/import-meta-main.mjs'))}
 );
-assert.strictEqual(importedModuleIsMain, false);
+assert.strictEqual(importedModuleIsMain, false, 'import.meta.main should evaluate false in imported module');
 `;
 
-const scriptInWorker = `
+const scriptInEvalWorker = `
 import { Worker } from 'node:worker_threads';
 new Worker(${JSON.stringify(script)}, { eval: true });
+`;
+
+const scriptInUrlWorker = `
+import { Worker } from 'node:worker_threads';
+new Worker(new URL(${JSON.stringify(
+  `data:text/javascript,${encodeURIComponent(script)}`
+)}));
 `;
 
 async function test_evaluated_script() {
@@ -33,10 +40,23 @@ async function test_evaluated_script() {
   });
 }
 
-async function test_evaluated_worker_script() {
+async function test_evaluated_eval_worker_script() {
   const result = await spawnPromisified(
     process.execPath,
-    [...execOptions, '--eval', scriptInWorker],
+    [...execOptions, '--eval', scriptInEvalWorker],
+  );
+  assert.deepStrictEqual(result, {
+    stderr: '',
+    stdout: '',
+    code: 0,
+    signal: null,
+  });
+}
+
+async function test_evaluated_url_worker_script() {
+  const result = await spawnPromisified(
+    process.execPath,
+    [...execOptions, '--eval', scriptInUrlWorker],
   );
   assert.deepStrictEqual(result, {
     stderr: '',
@@ -47,4 +67,5 @@ async function test_evaluated_worker_script() {
 }
 
 await test_evaluated_script();
-await test_evaluated_worker_script();
+await test_evaluated_eval_worker_script();
+await test_evaluated_url_worker_script();
