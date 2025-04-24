647175ee0b8ca19d6f315216c879b1dc89ad2759
James M Snell <jasnell@gmail.com>
2025-04-24 07:50:22 -0700

    buffer: move SlowBuffer to EOL
    `SlowBuffer` has been deprecated for many years now. Let's remove it.
    
    PR-URL: https://github.com/nodejs/node/pull/58008
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/pummel/test-buffer-large-size-slowbuffer.js
+++ test/pummel/test-buffer-large-size-slowbuffer.js
@@ -5,18 +5,18 @@ const common = require('../common');
 common.skipIf32Bits();
 
 const assert = require('node:assert');
-const {
-  SlowBuffer,
-} = require('node:buffer');
+const { Buffer } = require('node:buffer');
 
 const size = 2 ** 31;
 
-// Test SlowBuffer with size larger than integer range
+// Test slow Buffer with size larger than integer range
 try {
-  assert.throws(() => SlowBuffer(size).toString('utf8'), { code: 'ERR_STRING_TOO_LONG' });
+  assert.throws(() => Buffer.allocUnsafeSlow(size).toString('utf8'), {
+    code: 'ERR_STRING_TOO_LONG',
+  });
 } catch (e) {
   if (e.code !== 'ERR_MEMORY_ALLOCATION_FAILED') {
     throw e;
   }
-  common.skip('insufficient space for SlowBuffer');
+  common.skip('insufficient space for slow Buffer allocation');
 }
