2f8444917d1705624ca80deb9c71a2972427d030
Dario Piotrowicz <dario@cloudflare.com>
2025-04-25 18:39:12 +0200

    Revert "readline: add stricter validation for functions called after closed"
    This reverts commit 8e7f32f968a723bc8811e9f146876a1061f7941b.

--- test/parallel/test-repl-no-terminal.js
+++ test/parallel/test-repl-no-terminal.js
@@ -1,12 +1,7 @@
 'use strict';
 const common = require('../common');
-const ArrayStream = require('../common/arraystream');
+
 const repl = require('repl');
-
-const stream = new ArrayStream();
-
-const replServer = repl.start({ terminal: false, input: stream, output: stream });
-
-replServer.setupHistory('/nonexistent/file', common.mustSucceed(() => {
-  replServer.close();
-}));
+const r = repl.start({ terminal: false });
+r.setupHistory('/nonexistent/file', common.mustSucceed());
+process.stdin.unref?.();
