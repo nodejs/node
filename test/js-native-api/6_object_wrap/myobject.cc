ecd081df82920017df62a3797afde7a58df72534
Chengzhong Wu <legendecas@gmail.com>
2025-04-26 00:22:30 +0200

    node-api: add nested object wrap and napi_ref test
    Test that an napi_ref can be nested inside another ObjectWrap. The test
    shows a critical case where a finalizer deletes an `napi_ref` whose
    finalizer is also scheduled.
    
    PR-URL: https://github.com/nodejs/node/pull/57981
    Reviewed-By: Vladimir Morozov <vmorozov@microsoft.com>
    Reviewed-By: Michael Dawson <midawson@redhat.com>

--- test/js-native-api/6_object_wrap/myobject.cc
+++ test/js-native-api/6_object_wrap/myobject.cc
@@ -1,7 +1,7 @@
+#include "myobject.h"
 #include "../common.h"
 #include "../entry_point.h"
 #include "assert.h"
-#include "myobject.h"
 
 typedef int32_t FinalizerData;
 
@@ -10,7 +10,9 @@ napi_ref MyObject::constructor;
 MyObject::MyObject(double value)
     : value_(value), env_(nullptr), wrapper_(nullptr) {}
 
-MyObject::~MyObject() { napi_delete_reference(env_, wrapper_); }
+MyObject::~MyObject() {
+  napi_delete_reference(env_, wrapper_);
+}
 
 void MyObject::Destructor(node_api_basic_env env,
                           void* nativeObject,
@@ -26,24 +28,36 @@ void MyObject::Destructor(node_api_basic_env env,
 
 void MyObject::Init(napi_env env, napi_value exports) {
   napi_property_descriptor properties[] = {
-    { "value", nullptr, nullptr, GetValue, SetValue, 0, napi_default, 0 },
-    { "valueReadonly", nullptr, nullptr, GetValue, nullptr, 0, napi_default,
-      0 },
-    DECLARE_NODE_API_PROPERTY("plusOne", PlusOne),
-    DECLARE_NODE_API_PROPERTY("multiply", Multiply),
+      {"value", nullptr, nullptr, GetValue, SetValue, 0, napi_default, 0},
+      {"valueReadonly",
+       nullptr,
+       nullptr,
+       GetValue,
+       nullptr,
+       0,
+       napi_default,
+       0},
+      DECLARE_NODE_API_PROPERTY("plusOne", PlusOne),
+      DECLARE_NODE_API_PROPERTY("multiply", Multiply),
   };
 
   napi_value cons;
-  NODE_API_CALL_RETURN_VOID(env, napi_define_class(
-      env, "MyObject", -1, New, nullptr,
-      sizeof(properties) / sizeof(napi_property_descriptor),
-      properties, &cons));
+  NODE_API_CALL_RETURN_VOID(
+      env,
+      napi_define_class(env,
+                        "MyObject",
+                        -1,
+                        New,
+                        nullptr,
+                        sizeof(properties) / sizeof(napi_property_descriptor),
+                        properties,
+                        &cons));
 
   NODE_API_CALL_RETURN_VOID(env,
-      napi_create_reference(env, cons, 1, &constructor));
+                            napi_create_reference(env, cons, 1, &constructor));
 
-  NODE_API_CALL_RETURN_VOID(env,
-      napi_set_named_property(env, exports, "MyObject", cons));
+  NODE_API_CALL_RETURN_VOID(
+      env, napi_set_named_property(env, exports, "MyObject", cons));
 }
 
 napi_value MyObject::New(napi_env env, napi_callback_info info) {
@@ -71,8 +85,12 @@ napi_value MyObject::New(napi_env env, napi_callback_info info) {
 
     obj->env_ = env;
     NODE_API_CALL(env,
-        napi_wrap(env, _this, obj, MyObject::Destructor,
-            nullptr /* finalize_hint */, &obj->wrapper_));
+                  napi_wrap(env,
+                            _this,
+                            obj,
+                            MyObject::Destructor,
+                            nullptr /* finalize_hint */,
+                            &obj->wrapper_));
 
     return _this;
   }
@@ -93,7 +111,7 @@ napi_value MyObject::New(napi_env env, napi_callback_info info) {
 napi_value MyObject::GetValue(napi_env env, napi_callback_info info) {
   napi_value _this;
   NODE_API_CALL(env,
-      napi_get_cb_info(env, info, nullptr, nullptr, &_this, nullptr));
+                napi_get_cb_info(env, info, nullptr, nullptr, &_this, nullptr));
 
   MyObject* obj;
   NODE_API_CALL(env, napi_unwrap(env, _this, reinterpret_cast<void**>(&obj)));
@@ -121,7 +139,7 @@ napi_value MyObject::SetValue(napi_env env, napi_callback_info info) {
 napi_value MyObject::PlusOne(napi_env env, napi_callback_info info) {
   napi_value _this;
   NODE_API_CALL(env,
-      napi_get_cb_info(env, info, nullptr, nullptr, &_this, nullptr));
+                napi_get_cb_info(env, info, nullptr, nullptr, &_this, nullptr));
 
   MyObject* obj;
   NODE_API_CALL(env, napi_unwrap(env, _this, reinterpret_cast<void**>(&obj)));
