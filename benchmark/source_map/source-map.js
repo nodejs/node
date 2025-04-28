b41dcedd538dc0edff984ca26f37032604ebb98e
Miguel Marcondes <miguelmarcondesfilho@gmail.com>
2025-04-28 18:10:11 -0300

    benchmark: expand operations and add performance tests for minified, sectioned, and large source maps

--- benchmark/source_map/source-map.js
+++ benchmark/source_map/source-map.js
@@ -9,7 +9,14 @@ const options = {
 const bench = common.createBenchmark(
   main,
   {
-    operation: ['parse', 'findEntry', 'findOrigin'],
+    operation: [
+      'parse',
+      'parse-minified',
+      'parse-sectioned',
+      'parse-large',
+      'findEntry',
+      'findOrigin',
+    ],
     n: [1e5],
   },
   options
@@ -23,10 +30,43 @@ function main({ operation, n }) {
     file: 'out.js',
     sourceRoot: '',
     sources: ['foo.js', 'bar.js'],
-    sourcesContent: ['console.log("foo");', 'console.log("bar");'],
+    sourcesContent: [null, null],
     names: ['src', 'maps', 'are', 'fun'],
     mappings: 'A,AAAB;;ABCDE;',
   };
+  const minifiedPayload = {
+    version: 3,
+    file: 'add.min.js',
+    sourceRoot: '',
+    sources: ['add.ts'],
+    names: ['add', 'x', 'y'],
+    mappings:
+      'AAAA,QAASA,EAAGC,EAAGC,CAClB,OAAOD,EAAGC,GACZC,QAAQC,IAAI,CAAC,CAAC,CAAC',
+  };
+  const sectionedPayload = {
+    version: 3,
+    file: 'app.js',
+    sections: [
+      {
+        offset: { line: 100, column: 10 },
+        map: {
+          version: 3,
+          file: 'section.js',
+          sources: ['foo.js', 'bar.js'],
+          names: ['src', 'maps', 'are', 'fun'],
+          mappings: 'AAAA,E;;ABCDE;',
+        },
+      },
+    ],
+  };
+  const largePayload = {
+    version: 3,
+    file: 'out.js',
+    sources: Array.from({ length: 1000 }, (_, i) => `source-${i}.js`),
+    names: Array.from({ length: 1000 }, (_, i) => `name-${i}`),
+    mappings: 'A,AAAB;;ABCDE;'.repeat(1000),
+  };
+
   const sourceMap = new SourceMap(samplePayload);
 
   switch (operation) {
@@ -34,6 +74,30 @@ function main({ operation, n }) {
       bench.start();
       for (let i = 0; i < n; i++) {
         new SourceMap(samplePayload);
+      }
+      bench.end(n);
+      break;
+
+    case 'parse-minified':
+      bench.start();
+      for (let i = 0; i < n; i++) {
+        new SourceMap(minifiedPayload);
+      }
+      bench.end(n);
+      break;
+
+    case 'parse-sectioned':
+      bench.start();
+      for (let i = 0; i < n; i++) {
+        new SourceMap(sectionedPayload);
+      }
+      bench.end(n);
+      break;
+
+    case 'parse-large':
+      bench.start();
+      for (let i = 0; i < n; i++) {
+        new SourceMap(largePayload);
       }
       bench.end(n);
       break;
