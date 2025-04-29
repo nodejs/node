ae8fb9079a63b8261a0c8ba50915df8f540315c0
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-04-29 22:55:33 +0200

    esm: avoid `import.meta` setup costs for unused properties
    PR-URL: https://github.com/nodejs/node/pull/57286
    Reviewed-By: Jacob Smith <jacob@frende.me>
    Reviewed-By: Edy Silva <edigleyssonsilva@gmail.com>

--- src/env_properties.h
+++ src/env_properties.h
@@ -124,6 +124,7 @@
   V(destroyed_string, "destroyed")                                             \
   V(detached_string, "detached")                                               \
   V(dh_string, "DH")                                                           \
+  V(dirname_string, "dirname")                                                 \
   V(divisor_length_string, "divisorLength")                                    \
   V(dns_a_string, "A")                                                         \
   V(dns_aaaa_string, "AAAA")                                                   \
@@ -327,6 +328,7 @@
     "export * from 'original'; export { default } from 'original'; export "    \
     "const __esModule = true;")                                                \
   V(require_string, "require")                                                 \
+  V(resolve_string, "resolve")                                                 \
   V(resource_string, "resource")                                               \
   V(result_string, "result")                                                   \
   V(retry_string, "retry")                                                     \
