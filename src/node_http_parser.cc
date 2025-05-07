d49fd29859e8501c9a8e7c8cd15e8f3f516a1287
Fedor Indutny <238531+indutny@users.noreply.github.com>
2025-05-07 07:21:52 -0700

    deps: update llhttp to 9.3.0
    llhttp@9.3.0 optimizes header value parsing on ARM Neon/WASM, and adds
    support for a protocol callback for use outside of the typical HTTP
    setting (RTSP/ICE).
    
    PR-URL: https://github.com/nodejs/node/pull/58144
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>

--- src/node_http_parser.cc
+++ src/node_http_parser.cc
@@ -1203,6 +1203,10 @@ void ConnectionsList::Expired(const FunctionCallbackInfo<Value>& args) {
 
 const llhttp_settings_t Parser::settings = {
     Proxy<Call, &Parser::on_message_begin>::Raw,
+
+    // on_protocol
+    nullptr,
+
     Proxy<DataCall, &Parser::on_url>::Raw,
     Proxy<DataCall, &Parser::on_status>::Raw,
 
@@ -1222,6 +1226,8 @@ const llhttp_settings_t Parser::settings = {
     Proxy<DataCall, &Parser::on_body>::Raw,
     Proxy<Call, &Parser::on_message_complete>::Raw,
 
+    // on_protocol_complete
+    nullptr,
     // on_url_complete
     nullptr,
     // on_status_complete
