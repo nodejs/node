5c8b8f6b2ecbe9ab459d17afb0e275bdf00cf129
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-08 19:36:25 +0100

    add tests ensuring worker threads cannot access internals

---
+++ test/parallel/test-worker-internal-modules.mjs
@@ -0,0 +1,35 @@
+import tmpdir from '../common/tmpdir.js';
+import assert from 'node:assert/strict';
+import { once } from 'node:events';
+import fs from 'node:fs/promises';
+import { describe, test, before } from 'node:test';
+import { Worker } from 'node:worker_threads';
+
+const accessInternalsSource = `
+import 'node:internal/freelist';
+`;
+
+function convertScriptSourceToDataUrl(script) {
+  return new URL(`data:text/javascript,${encodeURIComponent(script)}`);
+}
+
+describe("Worker threads should not be able to access internal modules", () => {
+  before(() => tmpdir.refresh());
+
+  test("worker instantiated with module file path", async () => {
+    const moduleFilepath = tmpdir.resolve('test-worker-internal-modules.mjs');
+    await fs.writeFile(moduleFilepath, accessInternalsSource);
+    const w = new Worker(moduleFilepath)
+    await assert.rejects(once(w, "exit"))
+  })
+
+  test("worker instantiated with module source", async () => {
+    const w = new Worker(accessInternalsSource, { eval: true })
+    await assert.rejects(once(w, "exit"))
+  })
+
+  test("worker instantiated with data: URL", async () => {
+    const w = new Worker(convertScriptSourceToDataUrl(accessInternalsSource))
+    await assert.rejects(once(w, "exit"))
+  })
+})
