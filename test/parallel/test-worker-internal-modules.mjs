775fe0299d08ea45fe7f8a559b1846a4a5609f5b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-08 19:47:48 +0100

    style: resolve linter complaints

--- test/parallel/test-worker-internal-modules.mjs
+++ test/parallel/test-worker-internal-modules.mjs
@@ -1,3 +1,4 @@
+import '../common/index.mjs';
 import tmpdir from '../common/tmpdir.js';
 import assert from 'node:assert/strict';
 import { once } from 'node:events';
@@ -13,23 +14,23 @@ function convertScriptSourceToDataUrl(script) {
   return new URL(`data:text/javascript,${encodeURIComponent(script)}`);
 }
 
-describe("Worker threads should not be able to access internal modules", () => {
+describe('Worker threads should not be able to access internal modules', () => {
   before(() => tmpdir.refresh());
 
-  test("worker instantiated with module file path", async () => {
+  test('worker instantiated with module file path', async () => {
     const moduleFilepath = tmpdir.resolve('test-worker-internal-modules.mjs');
     await fs.writeFile(moduleFilepath, accessInternalsSource);
-    const w = new Worker(moduleFilepath)
-    await assert.rejects(once(w, "exit"))
-  })
+    const w = new Worker(moduleFilepath);
+    await assert.rejects(once(w, 'exit'), { code: 'ERR_UNKNOWN_BUILTIN_MODULE' });
+  });
 
-  test("worker instantiated with module source", async () => {
-    const w = new Worker(accessInternalsSource, { eval: true })
-    await assert.rejects(once(w, "exit"))
-  })
+  test('worker instantiated with module source', async () => {
+    const w = new Worker(accessInternalsSource, { eval: true });
+    await assert.rejects(once(w, 'exit'), { code: 'ERR_UNKNOWN_BUILTIN_MODULE' });
+  });
 
-  test("worker instantiated with data: URL", async () => {
-    const w = new Worker(convertScriptSourceToDataUrl(accessInternalsSource))
-    await assert.rejects(once(w, "exit"))
-  })
-})
+  test('worker instantiated with data: URL', async () => {
+    const w = new Worker(convertScriptSourceToDataUrl(accessInternalsSource));
+    await assert.rejects(once(w, 'exit'), { code: 'ERR_UNKNOWN_BUILTIN_MODULE' });
+  });
+});
