71104b3352be4c3254d703679ffe3fae134884ed
Attila Szegedi <attila.szegedi@datadoghq.com>
2025-05-08 15:07:08 +0200

    Test for expected frame invariant too

--- test/async-hooks/test-async-local-storage-set-dc.js
+++ test/async-hooks/test-async-local-storage-set-dc.js
@@ -11,45 +11,48 @@ const { subscribe } = require('diagnostics_channel');
 const { AsyncLocalStorage, AsyncResource } = require('async_hooks');
 
 let frameSetCounter = 0;
-subscribe('async_context_frame.set', () => {
+let lastPublishedFrame = 0;
+subscribe('async_context_frame.set', (frame) => {
   frameSetCounter++;
+  lastPublishedFrame = frame;
 });
 
 const asyncLocalStorage = new AsyncLocalStorage();
 assert.strictEqual(frameSetCounter, 0);
 let lastObservedSetCounter = 0;
 
-function assertFrameWasSet() {
+function assertFrameWasSet(expectedStore) {
   assert(frameSetCounter > lastObservedSetCounter);
+  assert.strictEqual(lastPublishedFrame?.get(asyncLocalStorage), expectedStore);
   lastObservedSetCounter = frameSetCounter;
 }
 
 setImmediate(() => {
   // Entering an immediate callback sets the frame.
-  assertFrameWasSet();
+  assertFrameWasSet(undefined);
   const store = { foo: 'bar' };
   asyncLocalStorage.enterWith(store);
   // enterWith sets the frame.
-  assertFrameWasSet();
+  assertFrameWasSet(store);
   assert.strictEqual(asyncLocalStorage.getStore(), store);
 
   setTimeout(() => {
     // Entering a timeout callback sets the frame.
-    assertFrameWasSet();
+    assertFrameWasSet(store);
     assert.strictEqual(asyncLocalStorage.getStore(), store);
     const res = new AsyncResource('test');
     const store2 = { foo: 'bar2' };
     asyncLocalStorage.enterWith(store2);
     // enterWith sets the frame.
-    assertFrameWasSet();
+    assertFrameWasSet(store2);
     res.runInAsyncScope(() => {
       // runInAsyncScope sets the frame on entry.
-      assertFrameWasSet();
+      assertFrameWasSet(store);
       // AsyncResource was constructed before store2 assignment, so it should
       // keep reflecting the old store.
       assert.strictEqual(asyncLocalStorage.getStore(), store);
     });
     // runInAsyncScope sets the frame on exit.
-    assertFrameWasSet();
+    assertFrameWasSet(store2);
   }, 10);
 });
