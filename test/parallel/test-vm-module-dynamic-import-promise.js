25e03e6f637113300eb71c6faf718c98b4c575a4
Chengzhong Wu <cwu631@bloomberg.net>
2025-05-13 12:58:58 +0100

    fixup! vm: import call should return a promise in the current context

--- test/parallel/test-vm-module-dynamic-import-promise.js
+++ test/parallel/test-vm-module-dynamic-import-promise.js
@@ -38,6 +38,34 @@ async function testScript() {
   await ctx.__result;
 }
 
+async function testScriptImportFailed() {
+  const ctx = createContext();
+
+  const mod1 = new SourceTextModule('export const a = 1;', {
+    context: ctx,
+  });
+  // No import statements, so must not link statically.
+  await mod1.link(common.mustNotCall());
+
+  const script2 = new Script(`
+    const promise = import("mod1");
+    if (Object.getPrototypeOf(promise) !== Promise.prototype) {
+      throw new Error('Expected promise to be a Promise');
+    }
+    globalThis.__result = promise;
+  `, {
+    importModuleDynamically: common.mustCall((specifier, referrer) => {
+      throw new Error('import failed');
+    }),
+  });
+  script2.runInContext(ctx);
+
+  // Wait for the promise to reject.
+  await assert.rejects(ctx.__result, {
+    message: 'import failed',
+  });
+}
+
 async function testModule() {
   const ctx = createContext();
 
@@ -66,7 +94,41 @@ async function testModule() {
   await mod2.evaluate();
 }
 
+async function testModuleImportFailed() {
+  const ctx = createContext();
+
+  const mod1 = new SourceTextModule('export const a = 1;', {
+    context: ctx,
+  });
+  // No import statements, so must not link statically.
+  await mod1.link(common.mustNotCall());
+
+  const mod2 = new SourceTextModule(`
+    const promise = import("mod1");
+    if (Object.getPrototypeOf(promise) !== Promise.prototype) {
+      throw new Error('Expected promise to be a Promise');
+    }
+    await promise.then(() => {
+      throw new Error('Expected promise to be rejected');
+    }, (e) => {
+      if (e.message !== 'import failed') {
+        throw new Error('Expected promise to be rejected with "import failed"');
+      }
+    });
+  `, {
+    context: ctx,
+    importModuleDynamically: common.mustCall((specifier, referrer) => {
+      throw new Error('import failed');
+    }),
+  });
+  // No import statements, so must not link statically.
+  await mod2.link(common.mustNotCall());
+  await mod2.evaluate();
+}
+
 Promise.all([
   testScript(),
+  testScriptImportFailed(),
   testModule(),
+  testModuleImportFailed(),
 ]).then(common.mustCall());
