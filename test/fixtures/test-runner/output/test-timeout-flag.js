7cdda927fa8244f379c4d98f99af8d10f8d8c4a9
Pietro Marchini <pietro.marchini94@gmail.com>
2025-05-14 16:07:04 +0200

    test: fix test-timeout-flag after revert of auto subtest wait
    PR-URL: https://github.com/nodejs/node/pull/58282
    Fixes: https://github.com/nodejs/node/issues/58227
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: LiviaMedeiros <livia@cirno.name>

--- test/fixtures/test-runner/output/test-timeout-flag.js
+++ test/fixtures/test-runner/output/test-timeout-flag.js
@@ -1,19 +1,39 @@
-// Flags: --test-timeout=20
 'use strict';
-const { describe, test } = require('node:test');
-const { setTimeout } = require('node:timers/promises');
+const { describe, it, after } = require('node:test');
+const { setTimeout } = require('node:timers');
+
+const timeoutRefs = [];
 
 describe('--test-timeout is set to 20ms', () => {
-  test('should timeout after 20ms', async () => {
-    await setTimeout(200000, undefined, { ref: false });
-  });
-  test('should timeout after 5ms', { timeout: 5 }, async () => {
-    await setTimeout(200000, undefined, { ref: false });
-  });
-
-  test('should not timeout', { timeout: 50000 }, async () => {
-    await setTimeout(1);
+  it('should timeout after 20ms', async () => {
+    const { promise, resolve } = Promise.withResolvers();
+    timeoutRefs.push(setTimeout(() => {
+      resolve();
+    }, 20000));
+    await promise;
   });
 
-  test('should pass', async () => {});
+  it('should timeout after 5ms', { timeout: 5 }, async () => {
+    const { promise, resolve } = Promise.withResolvers();
+    timeoutRefs.push(setTimeout(() => {
+      resolve();
+    }, 20000));
+    await promise;
+  });
+
+  it('should not timeout', { timeout: 50000 }, async () => {
+    const { promise, resolve } = Promise.withResolvers();
+    timeoutRefs.push(setTimeout(() => {
+      resolve();
+    }, 1));
+    await promise;
+  });
+
+  it('should pass', async () => {});
+
+  after(() => {
+    for (const timeoutRef of timeoutRefs) {
+      clearTimeout(timeoutRef);
+    }
+  });
 });
