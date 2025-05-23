d5abfbf5825de940ad7ac2c49da1f487646f05e4
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2025-05-23 23:03:02 +0100

    lib: add module to use in module deprecation warnings
    PR-URL: https://github.com/nodejs/node/pull/58442
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Darshan Sen <raisinten@gmail.com>

--- test/parallel/test-warn-stream-transform-deprecation.js
+++ test/parallel/test-warn-stream-transform-deprecation.js
@@ -5,6 +5,6 @@ const common = require('../common');
 // _stream_transform is deprecated.
 
 common.expectWarning('DeprecationWarning',
-                     'The _stream_transform module is deprecated.', 'DEP0193');
+                     'The _stream_transform module is deprecated. Use `node:stream` instead.', 'DEP0193');
 
 require('_stream_transform');
--- test/parallel/test-warn-stream-wrap-deprecation.js
+++ test/parallel/test-warn-stream-wrap-deprecation.js
@@ -5,6 +5,6 @@ const common = require('../common');
 // _stream_wrap is deprecated.
 
 common.expectWarning('DeprecationWarning',
-                     'The _stream_wrap module is deprecated.', 'DEP0125');
+                     'The _stream_wrap module is deprecated. Use `node:stream` instead.', 'DEP0125');
 
 require('_stream_wrap');
