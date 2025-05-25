92ed1cf06c3bfe53f3a9f22144b65874174bc9ec
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2025-05-25 22:18:51 +0100

    fs: improve cpSync no-filter copyDir performance
    move the logic in `cpSync` that copies a directory from
    src to dest from JavaScript to C++ increasing its performance
    
    Note: this improvement is not applied if the filter option is
          provided, such optimization will be looked into separately

--- src/node_errors.h
+++ src/node_errors.h
@@ -78,7 +78,9 @@ void OOMErrorHandler(const char* location, const v8::OOMDetails& details);
   V(ERR_FS_CP_EINVAL, Error)                                                   \
   V(ERR_FS_CP_DIR_TO_NON_DIR, Error)                                           \
   V(ERR_FS_CP_NON_DIR_TO_DIR, Error)                                           \
+  V(ERR_FS_CP_SYMLINK_TO_SUBDIRECTORY, Error)                                  \
   V(ERR_FS_EISDIR, Error)                                                      \
+  V(ERR_FS_CP_EEXIST, Error)                                                   \
   V(ERR_FS_CP_SOCKET, Error)                                                   \
   V(ERR_FS_CP_FIFO_PIPE, Error)                                                \
   V(ERR_FS_CP_UNKNOWN, Error)                                                  \
