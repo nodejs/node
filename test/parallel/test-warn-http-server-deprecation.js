2103d6f19f5b44505c4bd02a8d6199115a75358f
Sebastian Beltran <bjohansebas@gmail.com>
2025-05-31 18:38:39 -0500

    Update test/parallel/test-warn-http-server-deprecation.js
    Co-authored-by: Dario Piotrowicz <dario.piotrowicz@gmail.com>

--- test/parallel/test-warn-http-server-deprecation.js
+++ test/parallel/test-warn-http-server-deprecation.js
@@ -2,7 +2,7 @@
 
 const common = require('../common');
 
-// _http_outgoing is deprecated.
+// _http_server is deprecated.
 
 common.expectWarning('DeprecationWarning',
                      'The _http_server module is deprecated. Use `node:http` instead.', 'DEP0195');
