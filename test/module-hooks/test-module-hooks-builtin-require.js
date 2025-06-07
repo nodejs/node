4b27d6a624f7021ee640399c9f8041a978ca0ced
Alex Yang <alex@runllama.ai>
2025-06-07 16:53:07 +0800

    module: convert schema-only core module on `convertCJSFilenameToURL`
    Co-authored-by: Joyee Cheung <joyeec9h3@gmail.com>

--- test/module-hooks/test-module-hooks-builtin-require.js
+++ test/module-hooks/test-module-hooks-builtin-require.js
@@ -1,39 +1,15 @@
 'use strict';
 
-const common = require('../common');
-const assert = require('assert');
-const { registerHooks } = require('module');
-
-// This tests that required builtins get null as source from default
-// step, and the source returned are ignored.
-// TODO(joyeecheung): this is to align with the module.register() behavior
-// but perhaps the load hooks should not be invoked for builtins at all.
-
-// Pick a builtin that's unlikely to be loaded already - like zlib.
-assert(!process.moduleLoadList.includes('NativeModule zlib'));
-let hook;
-
-hook = registerHooks({
-  load: common.mustCall(function load(url, context, nextLoad) {
-    assert.strictEqual(url, 'node:zlib');
-    const result = nextLoad(url, context);
-    assert.strictEqual(result.source, null);
-    return {
-      source: 'throw new Error("I should not be thrown")',
-      format: 'builtin',
-    };
-  }),
-});
-
-assert.strictEqual(typeof require('zlib').createGzip, 'function');
-
-hook.deregister();
-
 // This tests that when builtins that demand the `node:` prefix are
 // required, the URL returned by the default resolution step is still
 // prefixed and valid, and that gets passed to the load hook is still
 // the one with the `node:` prefix. The one with the prefix
 // stripped for internal lookups should not get passed into the hooks.
+
+const common = require('../common');
+const assert = require('assert');
+const { registerHooks } = require('module');
+
 const schemelessBlockList = new Set([
   'sea',
   'sqlite',
@@ -46,7 +22,13 @@ for (const mod of schemelessBlockList) {
   testModules.push(`node:${mod}`);
 }
 
-hook = registerHooks({
+const hook = registerHooks({
+  resolve: common.mustCall((specifier, context, nextResolve) => {
+    const result = nextResolve(specifier, context);
+    assert.match(result.url, /^node:/);
+    assert.strictEqual(schemelessBlockList.has(result.url.slice(5, result.url.length)), true);
+    return result;
+  }, testModules.length),
   load: common.mustCall(function load(url, context, nextLoad) {
     assert.match(url, /^node:/);
     assert.strictEqual(schemelessBlockList.has(url.slice(5, url.length)), true);
