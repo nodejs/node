fab0dbdf0d75097441a5d9413fbe43563938116d
James M Snell <jasnell@gmail.com>
2025-06-07 13:08:39 -0700

    Update test/parallel/test-crypto-ecdh-setpublickey-deprecation.js
    Co-authored-by: Filip Skokan <panva.ip@gmail.com>

--- test/parallel/test-crypto-ecdh-setpublickey-deprecation.js
+++ test/parallel/test-crypto-ecdh-setpublickey-deprecation.js
@@ -17,7 +17,7 @@ const ec = crypto.createECDH('secp256k1');
 try {
   // This will throw but we don't care about the error,
   // we just want to verify that the deprecation warning
-  // is emitter.
+  // is emitted.
   ec.setPublicKey(Buffer.from([123]));
 } catch {
   // Intentionally ignore the error
