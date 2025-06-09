5d805a1d4dacd33964c9cb8e496e4a3583bc279c
islandryu <shimaryuhei@gmail.com>
2025-06-09 22:53:43 +0900

    module: correctly detect top-level await in ambiguous contexts
    Fixes: https://github.com/nodejs/node/issues/58331

--- test/es-module/test-esm-detect-ambiguous.mjs
+++ test/es-module/test-esm-detect-ambiguous.mjs
@@ -427,22 +427,75 @@ describe('when working with Worker threads', () => {
   });
 });
 
-describe('cjs & esm ambiguous syntax case', () => {
-  it('should throw an ambiguous syntax error when using top-level await with require', async () => {
-    const { stderr, code, signal } = await spawnPromisified(
-      process.execPath,
-      [
-        '--input-type=module',
-        '--eval',
-        `await 1;\nconst fs = require('fs');`,
-      ]
-    );
+describe('maybe top-level await syntax errors that are not recognized as top-level await errors', () => {
+  const expressions = [
+    // string
+    { expression: '""' },
+    // number
+    { expression: '0' },
+    // boolean
+    { expression: 'true' },
+    // null
+    { expression: 'null' },
+    // undefined
+    { expression: 'undefined' },
+    // object
+    { expression: '{}' },
+    // array
+    { expression: '[]' },
+    // new
+    { expression: 'new Date()' },
+    // identifier
+    { initialize: 'const a = 2;', expression: 'a' },
+  ];
+  it('should not crash the process', async () => {
+    for (const { expression, initialize } of expressions) {
+      const wrapperExpressions = [
+        `function callAwait() {}; callAwait(await ${expression});`,
+        `if (await ${expression}) {}`,
+        `{ key: await ${expression} }`,
+        `[await ${expression}]`,
+        `(await ${expression})`,
+      ];
+      for (const wrapperExpression of wrapperExpressions) {
+        const { code, signal, stdout, stderr } = await spawnPromisified(process.execPath, [
+          '--eval',
+          `
+          ${initialize || ''}
+          ${wrapperExpression}
+          `,
+        ]);
 
-    match(
-      stderr,
-      /ReferenceError: Cannot determine intended module format because both require\(\) and top-level await are present\. If the code is intended to be CommonJS, wrap await in an async function\. If the code is intended to be an ES module, replace require\(\) with import\./
-    );
+        strictEqual(stderr, '');
+        strictEqual(stdout, '');
+        strictEqual(code, 0);
+        strictEqual(signal, null);
+      }
+    }
+  });
 
+  it('should crash when the expression is not valid', async () => {
+    let { code, signal, stdout, stderr } = await spawnPromisified(process.execPath, [
+      '--eval',
+      `
+      function callAwait() {}
+      callAwait(await "" ""); 
+      `,
+    ]);
+    match(stderr, /SyntaxError: missing \) after argument list/);
+    strictEqual(stdout, '');
+    strictEqual(code, 1);
+    strictEqual(signal, null);
+
+    ({ code, signal, stdout, stderr } = await spawnPromisified(process.execPath, [
+      '--eval',
+      `
+      function callAwait() {}
+      if (a "") {}
+      `,
+    ]));
+    match(stderr, /SyntaxError: Unexpected string/);
+    strictEqual(stdout, '');
     strictEqual(code, 1);
     strictEqual(signal, null);
   });
