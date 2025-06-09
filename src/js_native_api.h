5961ebfad9e9f2676cbd319c3e305b7396ee2159
Miguel Marcondes <miguelmarcondesfilho@gmail.com>
2025-06-09 11:35:01 -0300

    node-api: add platform-specific warning

--- src/js_native_api.h
+++ src/js_native_api.h
@@ -24,7 +24,13 @@
 #if defined(NAPI_EXPERIMENTAL) &&                                              \
     !defined(NODE_API_EXPERIMENTAL_NO_WARNING) &&                              \
     !defined(NODE_WANT_INTERNALS)
-#warning "NAPI_EXPERIMENTAL is enabled. Experimental features may be unstable."
+#ifdef _MSC_VER
+#pragma message("NAPI_EXPERIMENTAL is enabled. "                               \
+                "Experimental features may be unstable.")
+#else
+#warning "NAPI_EXPERIMENTAL is enabled. " \
+       "Experimental features may be unstable."
+#endif
 #endif
 
 #include "js_native_api_types.h"
