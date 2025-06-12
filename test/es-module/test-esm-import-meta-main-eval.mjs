2fc89892abaa43f372f80009212e458e71177a92
Marco Ippolito <marcoippolito54@gmail.com>
2025-06-12 13:21:54 +0200

    module: fix typescript import.meta.main
    PR-URL: https://github.com/nodejs/node/pull/58661
    Backport-PR-URL: https://github.com/nodejs/node/pull/58693
    Fixes: https://github.com/nodejs/node/issues/58660
    Reviewed-By: Xuguang Mei <meixuguang@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Jacob Smith <jacob@frende.me>
    Reviewed-By: Ethan Arrowood <ethan@arrowood.dev>
    Reviewed-By: Pietro Marchini <pietro.marchini94@gmail.com>
    Reviewed-By: Chemi Atlow <chemi@atlow.co.il>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- test/es-module/test-esm-import-meta-main-eval.mjs
+++ test/es-module/test-esm-import-meta-main-eval.mjs
@@ -3,17 +3,6 @@ import * as fixtures from '../common/fixtures.js';
 import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 
-const importMetaMainScript = `
-import assert from 'node:assert/strict';
-
-assert.strictEqual(import.meta.main, true, 'import.meta.main should evaluate true in main module');
-
-const { isMain: importedModuleIsMain } = await import(
-    ${JSON.stringify(fixtures.fileURL('es-modules/import-meta-main.mjs'))}
-);
-assert.strictEqual(importedModuleIsMain, false, 'import.meta.main should evaluate false in imported module');
-`;
-
 function wrapScriptInEvalWorker(script) {
   return `
   import { Worker } from 'node:worker_threads';
@@ -33,6 +22,17 @@ function wrapScriptInUrlWorker(script) {
 }
 
 describe('import.meta.main in evaluated scripts', () => {
+  const importMetaMainScript = `
+import assert from 'node:assert/strict';
+
+assert.strictEqual(import.meta.main, true, 'import.meta.main should evaluate true in main module');
+
+const { isMain: importedModuleIsMain } = await import(
+    ${JSON.stringify(fixtures.fileURL('es-modules/import-meta-main.mjs'))}
+);
+assert.strictEqual(importedModuleIsMain, false, 'import.meta.main should evaluate false in imported module');
+`;
+
   it('should evaluate true in evaluated script', async () => {
     const result = await spawnPromisified(
       process.execPath,
@@ -63,6 +63,68 @@ describe('import.meta.main in evaluated scripts', () => {
     const result = await spawnPromisified(
       process.execPath,
       ['--input-type=module', '--eval', wrapScriptInUrlWorker(importMetaMainScript)],
+    );
+    assert.deepStrictEqual(result, {
+      stderr: '',
+      stdout: '',
+      code: 0,
+      signal: null,
+    });
+  });
+});
+
+describe('import.meta.main in typescript scripts', () => {
+  const importMetaMainTSScript = `
+import assert from 'node:assert/strict';
+
+assert.strictEqual(import.meta.main, true, 'import.meta.main should evaluate true in main module');
+
+const { isMain: importedModuleIsMain } = await import(
+    ${JSON.stringify(fixtures.fileURL('es-modules/import-meta-main.ts'))}
+);
+assert.strictEqual(importedModuleIsMain, false, 'import.meta.main should evaluate false in imported module');
+`;
+
+  it('should evaluate true in evaluated script', async () => {
+    const result = await spawnPromisified(
+      process.execPath,
+      ['--experimental-strip-types',
+       '--input-type=module-typescript',
+       '--disable-warning=ExperimentalWarning',
+       '--eval', importMetaMainTSScript],
+    );
+    assert.deepStrictEqual(result, {
+      stderr: '',
+      stdout: '',
+      code: 0,
+      signal: null,
+    });
+  });
+
+  it('should evaluate true in worker instantiated with module source by evaluated script', async () => {
+    const result = await spawnPromisified(
+      process.execPath,
+      ['--experimental-strip-types',
+       '--input-type=module-typescript',
+       '--disable-warning=ExperimentalWarning',
+       '--eval', wrapScriptInEvalWorker(importMetaMainTSScript)],
+    );
+    assert.deepStrictEqual(result, {
+      stderr: '',
+      stdout: '',
+      code: 0,
+      signal: null,
+    });
+  });
+
+  it('should evaluate true in worker instantiated with `data:` URL by evaluated script', async () => {
+    const result = await spawnPromisified(
+      process.execPath,
+      ['--input-type=module',
+       '--experimental-strip-types',
+       '--input-type=module-typescript',
+       '--disable-warning=ExperimentalWarning',
+       '--eval', wrapScriptInUrlWorker(importMetaMainTSScript)],
     );
     assert.deepStrictEqual(result, {
       stderr: '',
