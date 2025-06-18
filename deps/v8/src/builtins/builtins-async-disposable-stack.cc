bf6629138260b84d26bc26b9972a18308ce0a95c
Michaël Zasso <targos@protonmail.com>
2025-06-18 08:48:13 +0200

    deps: V8: cherry-pick 1d7159580156
    Original commit message:
    
        [explicit-resource-management] Clear isolate internal exception
    
        This CL clears the isolate internal exception before rejecting
        the promise.
    
        Bug: 418103036
        Change-Id: If3748f7fc4b79b7b5be5009b9fff0d0267541a6f
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6594252
        Reviewed-by: Shu-yu Guo <syg@chromium.org>
        Commit-Queue: Rezvan Mahdavi Hezaveh <rezvan@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#100532}
    
    Refs: https://github.com/v8/v8/commit/1d71595801567c60eae0e8ae7157c1afd0aedce5
    PR-URL: https://github.com/nodejs/node/pull/58749
    Reviewed-By: Richard Lau <richard.lau@ibm.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- deps/v8/src/builtins/builtins-async-disposable-stack.cc
+++ deps/v8/src/builtins/builtins-async-disposable-stack.cc
@@ -87,10 +87,6 @@ BUILTIN(AsyncDisposeFromSyncDispose) {
               kMethod))),
       isolate);
 
-  v8::TryCatch try_catch(reinterpret_cast<v8::Isolate*>(isolate));
-  try_catch.SetVerbose(false);
-  try_catch.SetCaptureMessage(false);
-
   MaybeDirectHandle<Object> result =
       Execution::Call(isolate, sync_method, receiver, {});
 
@@ -105,7 +101,8 @@ BUILTIN(AsyncDisposeFromSyncDispose) {
       return {};
     }
     //        d. IfAbruptRejectPromise(result, promiseCapability).
-    DCHECK(try_catch.HasCaught());
+    isolate->clear_internal_exception();
+    isolate->clear_pending_message();
     JSPromise::Reject(promise, direct_handle(exception, isolate));
   }
 
