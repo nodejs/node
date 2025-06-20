68671f4314a11b4d3d3fa050f7c456e47f3b2f66
npm CLI robot <npm-cli+bot@github.com>
2025-06-19 21:39:59 -0700

    deps: upgrade npm to 11.4.2
    PR-URL: https://github.com/nodejs/node/pull/58696
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- deps/npm/lib/commands/stop.js
+++ deps/npm/lib/commands/stop.js
@@ -1,6 +1,6 @@
 const LifecycleCmd = require('../lifecycle-cmd.js')
 
-// This ends up calling run-script(['stop', ...args])
+// This ends up calling run(['stop', ...args])
 class Stop extends LifecycleCmd {
   static description = 'Stop a package'
   static name = 'stop'
