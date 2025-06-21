e0b562a66c852cfb2c5e10fc30fb5dd2a344f138
Hans Klunder <hans.klunder@gmail.com>
2025-06-21 19:42:44 +0000

    applied various changes

--- test/parallel/test-stream-readable-to-web-termination-byob.js
+++ test/parallel/test-stream-readable-to-web-termination-byob.js
@@ -1,11 +1,15 @@
 'use strict';
 require('../common');
 const { Readable } = require('stream');
+const assert = require('assert');
+const common = require('../common');
 {
   const r = Readable.from([]);
   // Cancelling reader while closing should not cause uncaught exceptions
-  r.on('close', () => reader.cancel());
+  r.on('close', common.mustCall(() => reader.cancel()));
 
   const reader = Readable.toWeb(r, { type: 'bytes' }).getReader({ mode: 'byob' });
-  reader.read(new Uint8Array(16));
+  reader.read(new Uint8Array(16)).then(common.mustCall((result) => {
+    assert.ok(result.done);
+  }));
 }
