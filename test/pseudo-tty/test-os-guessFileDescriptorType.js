8fdfea73e9074307b698c778c612a4c5e802bdb0
Vlad Frangu <me@vladfrangu.dev>
2025-06-22 13:57:14 +0300

    chore: use forEach and assert based on error code
    Co-authored-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Signed-off-by: Vlad Frangu <me@vladfrangu.dev>

--- test/pseudo-tty/test-os-guessFileDescriptorType.js
+++ test/pseudo-tty/test-os-guessFileDescriptorType.js
@@ -11,9 +11,18 @@ strictEqual(guessFileDescriptorType(2), 'TTY', 'stderr reported to not be a tty,
 strictEqual(guessFileDescriptorType(55555), 'UNKNOWN', '55555 reported to be a handle, but it is not');
 strictEqual(guessFileDescriptorType(2 ** 31 - 1), 'UNKNOWN', '2^31-1 reported to be a handle, but it is not');
 
-throws(() => guessFileDescriptorType(-1), /"fd" must be a positive integer/, '-1 reported to be a handle, but it is not');
-throws(() => guessFileDescriptorType(1.1), /"fd" must be a positive integer/, '1.1 reported to be a handle, but it is not');
-throws(() => guessFileDescriptorType('1'), /"fd" must be a positive integer/, '\'1\' reported to be a tty, but it is not');
-throws(() => guessFileDescriptorType({}), /"fd" must be a positive integer/, '{} reported to be a tty, but it is not');
-throws(() => guessFileDescriptorType(() => {}), /"fd" must be a positive integer/, '() => {} reported to be a tty, but it is not');
-throws(() => guessFileDescriptorType(2 ** 31), /"fd" must be a positive integer/, '2^31 reported to be a handle, but it is not (because the fd check rolls over the input to negative of it)');
+[
+    -1,
+    1.1,
+    '1',
+    [],
+    {},
+    () => {},
+    2 ** 31,
+    true,
+    false,
+    1n,
+    Symbol(),
+    undefined,
+    null,
+].forEach((val) => throws(() => guessFileDescriptorType(val), { code: 'ERR_INVALID_FD' }));
