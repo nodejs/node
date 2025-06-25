813626123978fb986ada07f22488b0c2f2576956
LiviaMedeiros <livia@cirno.name>
2025-06-26 00:01:01 +0800

    http,https,http2: make async disposers idempotent

--- test/parallel/test-http2-server-async-dispose.js
+++ test/parallel/test-http2-server-async-dispose.js
@@ -6,10 +6,17 @@ if (!common.hasCrypto)
   common.skip('missing crypto');
 
 const http2 = require('http2');
+const assert = require('node:assert');
 
 const server = http2.createServer();
 
 server.listen(0, common.mustCall(() => {
+  assert.strictEqual(server[Symbol.asyncDispose].name, '[Symbol.asyncDispose]');
   server.on('close', common.mustCall());
-  server[Symbol.asyncDispose]().then(common.mustCall());
+  server[Symbol.asyncDispose]().then(common.mustCall(() => {
+    assert.strictEqual(server._handle, null);
+
+    // Disposer must be idempotent, subsequent call must not throw
+    server[Symbol.asyncDispose]().then(common.mustCall());
+  }));
 }));
