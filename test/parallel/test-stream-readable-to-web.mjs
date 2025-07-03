3775dc9ac220f4378a0d3c3ea98fb183345bb5be
Ethan Arrowood <ethan@arrowood.dev>
2025-07-03 13:30:28 -0600

    test: dynamic import cypto

--- test/parallel/test-stream-readable-to-web.mjs
+++ test/parallel/test-stream-readable-to-web.mjs
@@ -1,69 +1,75 @@
 import { hasCrypto, skip } from '../common/index.mjs';
-if (!hasCrypto) { skip('missing crypto'); }
+if (!hasCrypto) {
+  skip('requires crypto');
+}
 
-import { Readable } from 'stream';
-import { memoryUsage } from 'process';
-import { randomBytes } from 'crypto';
-import assert from 'assert';
-import { setImmediate } from 'timers/promises';
+import { Readable } from 'node:stream';
+import { memoryUsage } from 'node:process';
+import assert from 'node:assert';
+import { setImmediate } from 'node:timers/promises';
+import { test } from 'node:test';
 
 // Based on: https://github.com/nodejs/node/issues/46347#issuecomment-1413886707
 // edit: make it cross-platform as /dev/urandom is not available on Windows
 
-const MAX_MEM = 256 * 1024 * 1024; // 256 MiB
+test('Stream Readable.toWeb() should not cause memory leak', async function() {
+  const { randomBytes } = await import('node:crypto');
 
-function checkMemoryUsage() {
-  assert(memoryUsage().arrayBuffers < MAX_MEM);
-}
+  const MAX_MEM = 256 * 1024 * 1024; // 256 MiB
 
-let end = false;
+  function checkMemoryUsage() {
+    assert(memoryUsage().arrayBuffers < MAX_MEM);
+  }
 
-const timeout = setTimeout(() => {
-  end = true;
-}, 5000);
+  let end = false;
 
-const randomNodeStream = new Readable({
-  read(size) {
-    if (end) {
-      this.push(null);
-      return;
-    }
+  const timeout = setTimeout(() => {
+    end = true;
+  }, 5000);
 
-    randomBytes(size, (err, buf) => {
-      if (err) {
-        this.destroy(err);
+  const randomNodeStream = new Readable({
+    read(size) {
+      if (end) {
+        this.push(null);
         return;
       }
 
-      this.push(buf);
-    });
+      randomBytes(size, (err, buf) => {
+        if (err) {
+          this.destroy(err);
+          return;
+        }
+
+        this.push(buf);
+      });
+    }
+  });
+
+  randomNodeStream.on('error', (err) => {
+    clearTimeout(timeout);
+    assert.fail(err);
+  });
+
+  // Before doing anything, make sure memory usage is okay
+  checkMemoryUsage();
+
+  // Create stream and check memory usage remains okay
+
+  const randomWebStream = Readable.toWeb(randomNodeStream);
+
+  checkMemoryUsage();
+
+  try {
+    // eslint-disable-next-line no-unused-vars
+    for await (const _ of randomWebStream) {
+      // Yield event loop to allow garbage collection
+      await setImmediate();
+      // consume the stream
+      // check memory usage remains okay
+      checkMemoryUsage();
+    }
+  } catch (err) {
+    clearTimeout(timeout);
+    assert.fail(err);
   }
 });
-
-randomNodeStream.on('error', (err) => {
-  clearTimeout(timeout);
-  assert.fail(err);
-});
-
-// Before doing anything, make sure memory usage is okay
-checkMemoryUsage();
-
-// Create stream and check memory usage remains okay
-
-const randomWebStream = Readable.toWeb(randomNodeStream);
-
-checkMemoryUsage();
-
-try {
-  // eslint-disable-next-line no-unused-vars
-  for await (const _ of randomWebStream) {
-    // Yield event loop to allow garbage collection
-    await setImmediate();
-    // consume the stream
-    // check memory usage remains okay
-    checkMemoryUsage();
-  }
-} catch (err) {
-  clearTimeout(timeout);
-  assert.fail(err);
-}
