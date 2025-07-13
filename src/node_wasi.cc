7e590e407882a2a928a4d8653319e0ebc9fb1527
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2025-07-13 21:23:11 +0100

    wasi: fix WasiFunction fast call signature

--- src/node_wasi.cc
+++ src/node_wasi.cc
@@ -266,7 +266,6 @@ inline void EinvalError() {}
 
 template <typename FT, FT F, typename R, typename... Args>
 R WASI::WasiFunction<FT, F, R, Args...>::FastCallback(
-    Local<Object> unused,
     Local<Object> receiver,
     Args... args,
     // NOLINTNEXTLINE(runtime/references) This is V8 api.
