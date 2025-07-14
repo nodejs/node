e7897e880b0cae51e8f1939ea3499289e4b3b26d
ishabi <ilyasshabi94@gmail.com>
2025-07-14 19:15:42 +0200

    keep packets unsent to have data in getters

--- benchmark/dgram/get-send-queue-info.js
+++ benchmark/dgram/get-send-queue-info.js
@@ -2,10 +2,13 @@
 
 const common = require('../common.js');
 const dgram = require('dgram');
+const assert = require('assert');
 
 const bench = common.createBenchmark(main, {
   n: [5e6],
   method: ['size', 'count'],
+}, {
+  flags: ['--test-udp-no-try-send'],
 });
 
 function main({ n, method }) {
@@ -14,20 +17,27 @@ function main({ n, method }) {
 
   server.bind(0, () => {
     client.connect(server.address().port, () => {
+      // warm-up: keep packets unsent using --test-udp-no-try-send flag
       for (let i = 0; i < 999; i++) client.send('Hello');
+
+      let size = 0;
+      let count = 0;
 
       bench.start();
 
       if (method === 'size') {
-        for (let i = 0; i < n; ++i) server.getSendQueueSize();
+        for (let i = 0; i < n; ++i) size += client.getSendQueueSize();
       } else {
-        for (let i = 0; i < n; ++i) server.getSendQueueCount();
+        for (let i = 0; i < n; ++i) count += client.getSendQueueCount();
       }
 
       bench.end(n);
 
       client.close();
       server.close();
+
+      assert.ok(size > 0);
+      assert.ok(count > 0);
     });
   });
 }
