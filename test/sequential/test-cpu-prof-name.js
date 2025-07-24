d5640ca58a3b1c7c8708c93fadda684642d35620
Haram Jeong <91401364+haramj@users.noreply.github.com>
2025-07-24 19:28:18 +0900

    cli: support `${pid}` placeholder in --cpu-prof-name
    PR-URL: https://github.com/nodejs/node/pull/59072
    Fixes: https://github.com/nodejs/node/issues/57418
    Reviewed-By: Daeyeon Jeong <daeyeon.dev@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/sequential/test-cpu-prof-name.js
+++ test/sequential/test-cpu-prof-name.js
@@ -8,6 +8,8 @@ const fixtures = require('../common/fixtures');
 common.skipIfInspectorDisabled();
 
 const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
 const { spawnSync } = require('child_process');
 
 const tmpdir = require('../common/tmpdir');
@@ -40,4 +42,37 @@ const {
   const profiles = getCpuProfiles(tmpdir.path);
   assert.deepStrictEqual(profiles, [file]);
   verifyFrames(output, file, 'fibonacci.js');
+}
+
+// --cpu-prof-name with ${pid} placeholder
+{
+  tmpdir.refresh();
+  // eslint-disable-next-line no-template-curly-in-string
+  const profName = 'CPU.${pid}.cpuprofile';
+  const dir = tmpdir.path;
+
+  const output = spawnSync(process.execPath, [
+    '--cpu-prof',
+    '--cpu-prof-interval',
+    kCpuProfInterval,
+    '--cpu-prof-name',
+    profName,
+    fixtures.path('workload', 'fibonacci.js'),
+  ], {
+    cwd: dir,
+    env
+  });
+
+  if (output.status !== 0) {
+    console.error(output.stderr.toString());
+  }
+
+  assert.strictEqual(output.status, 0);
+
+  const expectedFile = path.join(dir, `CPU.${output.pid}.cpuprofile`);
+  assert.ok(fs.existsSync(expectedFile), `Expected file ${expectedFile} not found.`);
+
+  verifyFrames(output, expectedFile, 'fibonacci.js');
+
+  fs.unlinkSync(expectedFile);
 }
