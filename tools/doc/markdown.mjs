3a0131ac40e52d8ff98cfde939553b476df8a387
avivkeller <me@aviv.sh>
2025-07-25 17:57:31 -0400

    doc: remove synopsis

--- tools/doc/markdown.mjs
+++ tools/doc/markdown.mjs
@@ -2,22 +2,13 @@ import { visit } from 'unist-util-visit';
 
 export const referenceToLocalMdFile = /^(?![+a-z]+:)([^#?]+)\.md(#.+)?$/i;
 
-export function replaceLinks({ filename, linksMapper }) {
+export default () => {
   return (tree) => {
-    const fileHtmlUrls = linksMapper[filename];
-
     visit(tree, (node) => {
       node.url &&= node.url.replace(
         referenceToLocalMdFile,
         (_, filename, hash) => `${filename}.html${hash || ''}`,
       );
-    });
-    visit(tree, 'definition', (node) => {
-      const htmlUrl = fileHtmlUrls?.[node.identifier];
-
-      if (htmlUrl && typeof htmlUrl === 'string') {
-        node.url = htmlUrl;
-      }
     });
   };
 }
