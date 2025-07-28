71d25f8f5b988ae2edd716e911478b2293c470c5
lionpaha12 <darwoshine@gmail.com>
2025-07-29 07:47:39 +0800

    Delete typings directory

--- typings/internalBinding/timers.d.ts
+++
@@ -1,8 +0,0 @@
-export interface TimersBinding {
-  getLibuvNow(): number;
-  setupTimers(immediateCallback: () => void, timersCallback: (now: number) => void): void;
-  scheduleTimer(msecs: number): void;
-  toggleTimerRef(value: boolean): void;
-  toggleImmediateRef(value: boolean): void;
-  immediateInfo: Uint32Array;
-}
