0ef363b35782f1f30cb3b7899db8934ce9992e15
Alex Yang <himself65@outlook.com>
2025-07-29 21:39:28 -0700

    fix: eliminate `for ... of` syntax

--- tools/eslint-rules/no-unsafe-array-iteration.js
+++ tools/eslint-rules/no-unsafe-array-iteration.js
@@ -9,11 +9,12 @@
 // Rule Definition
 //------------------------------------------------------------------------------
 
-const USE_FOR_LOOP = 'Use traditional for loop instead of for...of for array iteration to avoid relying on user-mutable Symbol.iterator.';
+const USE_FOR_LOOP = 'Use traditional for loop instead of for...of for ' +
+  'array iteration to avoid relying on user-mutable Symbol.iterator.';
 
 /**
  * Checks if a node represents an array-like expression
- * @param {Object} node - The AST node to check
+ * @param {object} node - The AST node to check
  * @returns {boolean} - True if the node appears to be an array
  */
 function isArrayLike(node) {
@@ -21,22 +22,18 @@ function isArrayLike(node) {
   if (node.type === 'ArrayExpression') {
     return true;
   }
-  
   // Variables/identifiers that might be arrays (we'll be conservative and flag all)
   if (node.type === 'Identifier') {
     return true;
   }
-  
   // Member expressions like obj.array, obj['array']
   if (node.type === 'MemberExpression') {
     return true;
   }
-  
   // Call expressions that might return arrays
   if (node.type === 'CallExpression') {
     return true;
   }
-  
   return false;
 }
 
@@ -50,7 +47,6 @@ module.exports = {
     fixable: null, // Not auto-fixable due to complexity of conversion
     schema: [],
   },
-  
   create(context) {
     return {
       ForOfStatement(node) {
