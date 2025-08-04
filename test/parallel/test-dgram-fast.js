b249db81bac31c9c7f8d5dc7766cd53d7cbd772f
ishabi <ilyasshabi94@gmail.com>
2025-08-04 13:11:05 +0200

    add fast api test

---
+++ test/parallel/test-dgram-fast.js
@@ -0,0 +1,38 @@
+// Flags: --expose-internals --no-warnings --allow-natives-syntax --test-udp-no-try-send
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const dgram = require('dgram');
+
+const { internalBinding } = require('internal/test/binding');
+
+function testFastDgram() {
+  const server = dgram.createSocket('udp4');
+  const client = dgram.createSocket('udp4');
+
+  server.bind(0, common.mustCall(() => {
+    client.connect(server.address().port, common.mustCall(() => {
+      // Send some packets to queue them up (using --test-udp-no-try-send flag)
+      client.send('Hello');
+      client.send('World');
+      
+      assert.strictEqual(typeof client.getSendQueueSize(), 'number');
+      assert.strictEqual(typeof client.getSendQueueCount(), 'number');
+      
+      client.close();
+      server.close();
+    }));
+  }));
+}
+
+eval('%PrepareFunctionForOptimization(testFastDgram)');
+testFastDgram();
+eval('%OptimizeFunctionOnNextCall(testFastDgram)');
+testFastDgram();
+
+if (common.isDebug) {
+  const { getV8FastApiCallCount } = internalBinding('debug');
+  assert.strictEqual(getV8FastApiCallCount('udp.getSendQueueSize'), 1);
+  assert.strictEqual(getV8FastApiCallCount('udp.getSendQueueCount'), 1);
+}
