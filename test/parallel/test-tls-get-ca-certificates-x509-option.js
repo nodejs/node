603967fa55002c34676321002327564ba133215a
haramjeong <04harams77@gmail.com>
2025-08-06 01:53:38 +0900

    test: expand getCACertificates() with 'as' option and invalid inputs

--- test/parallel/test-tls-get-ca-certificates-x509-option.js
+++ test/parallel/test-tls-get-ca-certificates-x509-option.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
@@ -7,7 +8,7 @@ const assert = require('assert');
 const tls = require('tls');
 const { X509Certificate } = require('crypto');
 
-function testX509Option() {
+{
   const certs = tls.getCACertificates({ type: 'default', as: 'x509' });
 
   assert.ok(Array.isArray(certs), 'should return an array');
@@ -15,7 +16,47 @@ function testX509Option() {
   assert.ok(certs[0] instanceof X509Certificate,
             'each cert should be instance of X509Certificate');
 
-  assert.match(certs[0].serialNumber, /^[0-9A-F]+$/i, 'serialNumber should be hex string');
+  assert.match(certs[0].serialNumber, /^[0-9A-F]+$/i,
+               'serialNumber should be hex string');
 }
 
-testX509Option();
+{
+  const certs = tls.getCACertificates({ type: 'default', as: 'buffer' });
+  assert.ok(Array.isArray(certs));
+  assert.ok(certs.length > 0);
+  assert.ok(Buffer.isBuffer(certs[0]));
+}
+
+{
+  const certs = tls.getCACertificates({ type: 'default' });
+  assert.ok(Array.isArray(certs));
+  assert.ok(certs.length > 0);
+  assert.ok(Buffer.isBuffer(certs[0]));
+}
+
+{
+  assert.throws(() => {
+    tls.getCACertificates({ type: 'default', as: 'invalid' });
+  }, {
+    name: 'TypeError',
+    code: 'ERR_INVALID_ARG_VALUE',
+    message: /must be one of/
+  });
+}
+
+{
+  const certs = tls.getCACertificates({ as: 'buffer' });
+  assert.ok(Array.isArray(certs));
+  assert.ok(certs.length > 0);
+  assert.ok(Buffer.isBuffer(certs[0]));
+}
+
+{
+  assert.throws(() => {
+    tls.getCACertificates({ type: 'invalid', as: 'buffer' });
+  }, {
+    name: 'TypeError',
+    code: 'ERR_INVALID_ARG_VALUE',
+    message: "The argument 'type' is invalid. Received 'invalid'"
+  });
+}
