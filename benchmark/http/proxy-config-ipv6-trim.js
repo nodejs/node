8188507739f0bbf6ccc3eb002c3f6cc43e478b9c
Krishnadas <pckrishnadas88@gmail.com>
2025-08-09 22:43:33 +0530

    http: optimize IPv6 hostname, suggested review changes
    1. Inline [,] removal added.
    1. removed private function.
    2. Updated benchmark file.

--- benchmark/http/proxy-config-ipv6-trim.js
+++ benchmark/http/proxy-config-ipv6-trim.js
@@ -1,60 +1,37 @@
 'use strict';
 const common = require('../common.js');
 
-// Test functions
-function regexTrim(hostname) {
-  return hostname.replace(/^\[|\]$/g, '');
-}
-
-function stringOpTrim(hostname) {
-  return hostname.startsWith('[') && hostname.endsWith(']') ?
-    hostname.slice(1, -1) :
-    hostname;
-}
-
-function normalizeProxyHostname(hostname) {
-  if (hostname.length > 2 && 
-      hostname[0] === '[' && 
-      hostname[hostname.length - 1] === ']') {
-    return hostname.slice(1, -1);
-  }
-  return hostname;
-}
-
 // Benchmark configuration
 const bench = common.createBenchmark(main, {
-  type: ['ipv6', 'ipv4'],
-  method: ['regex', 'stringOp', 'normalize'],
+  hostname: [
+    '[::]',
+    '127.0.0.1',
+    'localhost',
+    'www.example.proxy',
+  ],
   n: [1e6]
+}, {
+  flags: ['--expose-internals'],
 });
 
-function main({ type, method, n }) {
-  const hostname = type === 'ipv6' ? '[::1]' : '127.0.0.1';
-  let fn;
+function main({ hostname, n }) {
 
-  switch (method) {
-    case 'regex':
-      fn = regexTrim;
-      break;
-    case 'stringOp':
-      fn = stringOpTrim;
-      break;
-    case 'normalize':
-      fn = normalizeProxyHostname;
-      break;
-    default:
-      throw new Error('Invalid method');
+  const { parseProxyConfigFromEnv } = require('internal/http');
+
+  const protocol = 'https:';
+  const env = {
+    https_proxy: `https://${hostname}`,
   }
 
   // Warmup
   for (let i = 0; i < n; i++) {
-    fn(hostname);
+    parseProxyConfigFromEnv(env, protocol);
   }
 
-  // Benchmark
+  // // Benchmark
   bench.start();
   for (let i = 0; i < n; i++) {
-    fn(hostname);
+    parseProxyConfigFromEnv(env, protocol);
   }
   bench.end(n);
 }
