bf0948f867b7762921face85eef03a1942ec1fd0
Woohyun Sung <whsung0330@gmail.com>
2025-08-14 10:56:09 +0900

    typings: add missing URLBinding methods

--- typings/internalBinding/url.d.ts
+++ typings/internalBinding/url.d.ts
@@ -10,4 +10,6 @@ export interface URLBinding {
   format(input: string, fragment?: boolean, unicode?: boolean, search?: boolean, auth?: boolean): string;
   parse(input: string, base?: string): string | false;
   update(input: string, actionType: typeof urlUpdateActions, value: string): string | false;
+  getOrigin(input: string): string;
+  pathToFileURL(input: string, isWindows: boolean, hostname?: string): string;
 }
