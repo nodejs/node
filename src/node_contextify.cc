2a11d505266436c9c440a8fe5a1c089c8b1908f4
Shima Ryuhei <65934663+islandryu@users.noreply.github.com>
2025-08-16 07:10:17 +0900

    module: correctly detect top-level await in ambiguous contexts
    Fixes: https://github.com/nodejs/node/issues/58331
    PR-URL: https://github.com/nodejs/node/pull/58646
    Reviewed-By: Ethan Arrowood <ethan@arrowood.dev>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>

--- src/node_contextify.cc
+++ src/node_contextify.cc
@@ -1446,6 +1446,12 @@ static std::vector<std::string_view> throws_only_in_cjs_error_messages = {
     "await is only valid in async functions and "
     "the top level bodies of modules"};
 
+static const auto maybe_top_level_await_errors =
+    std::array<std::string_view, 2>{
+        "missing ) after argument list",  // example: `func(await 1);`
+        "SyntaxError: Unexpected"         // example: `if(await 1)`
+    };
+
 // If cached_data is provided, it would be used for the compilation and
 // the on-disk compilation cache from NODE_COMPILE_CACHE (if configured)
 // would be ignored.
@@ -1654,6 +1660,16 @@ bool ShouldRetryAsESM(Realm* realm,
       break;
     }
   }
+
+  for (const auto& error_message : maybe_top_level_await_errors) {
+    if (message_view.find(error_message) != std::string_view::npos) {
+      // If the error message is related to top-level await, we can try to
+      // compile it as ESM.
+      maybe_valid_in_esm = true;
+      break;
+    }
+  }
+
   if (!maybe_valid_in_esm) {
     return false;
   }
