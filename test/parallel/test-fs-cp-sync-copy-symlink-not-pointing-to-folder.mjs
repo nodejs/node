162ac9393c78e5e7255642d3067b70212256f7f5
Joyee Cheung <joyeec9h3@gmail.com>
2025-08-17 00:08:36 +0200

    test: use case-insensitive path checking on Windows in fs.cpSync tests
    In certain machine configurations on Windows, fs.readlinkSync() may
    return a path with upper case drive letter while the other paths may be
    constructed from a base path with a lower case drive letter (e.g.
    from process.cwd()). Checking path mismatch in a case-sensitive
    manner can lead to failure in some tests, specifically with the
    Windows machine configurations in the Jenkins CI. Since paths
    are case-insensitive on Windows anyway, compare them in a
    case-insensitive manner in the tests.
    
    PR-URL: https://github.com/nodejs/node/pull/59475
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>

--- test/parallel/test-fs-cp-sync-copy-symlink-not-pointing-to-folder.mjs
+++ test/parallel/test-fs-cp-sync-copy-symlink-not-pointing-to-folder.mjs
@@ -1,5 +1,5 @@
 // This tests that cpSync copies link if it does not point to folder in src.
-import { mustNotMutateObjectDeep } from '../common/index.mjs';
+import { mustNotMutateObjectDeep, isWindows } from '../common/index.mjs';
 import { nextdir } from '../common/fs.js';
 import assert from 'node:assert';
 import { cpSync, mkdirSync, symlinkSync, readlinkSync } from 'node:fs';
@@ -16,4 +16,11 @@ mkdirSync(join(dest, 'a'), mustNotMutateObjectDeep({ recursive: true }));
 symlinkSync(dest, join(dest, 'a', 'c'));
 cpSync(src, dest, mustNotMutateObjectDeep({ recursive: true }));
 const link = readlinkSync(join(dest, 'a', 'c'));
-assert.strictEqual(link, src);
+
+if (isWindows) {
+  // On Windows, readlinkSync() may return a path with uppercase drive letter,
+  // but paths are case-insensitive.
+  assert.strictEqual(link.toLowerCase(), src.toLowerCase());
+} else {
+  assert.strictEqual(link, src);
+}
