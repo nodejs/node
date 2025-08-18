1d4d95b329758c8a630d1c46cda3179fc8a25574
Filip Skokan <panva.ip@gmail.com>
2025-08-18 17:00:01 +0200

    fixup! crypto: add ChaCha20-Poly1305 Web Cryptography algorithm

--- test/parallel/test-webcrypto-encrypt-decrypt-chacha20-poly1305.js
+++ test/parallel/test-webcrypto-encrypt-decrypt-chacha20-poly1305.js
@@ -5,7 +5,7 @@ const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
 
-if (!process.features.openssl_is_boringssl)
+if (process.features.openssl_is_boringssl)
   common.skip('Skipping unsupported ChaCha20-Poly1305 test case');
 
 const assert = require('assert');
