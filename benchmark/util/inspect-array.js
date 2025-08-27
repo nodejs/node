d08167ba540d85038129c4bfadc6dfc5f1871410
Bruno Rodrigues <swe@brunocroh.com>
2025-08-27 23:19:30 +0100

    benchmark: calibrate util.inspect-array
    According to nodejs/performance#186
    this benchmark takes 1.3 minutes to run a single run.
    So using calibrate-n script it suggests reduce it from 5e3 to 10

--- benchmark/util/inspect-array.js
+++ benchmark/util/inspect-array.js
@@ -4,14 +4,9 @@ const common = require('../common');
 const util = require('util');
 
 const bench = common.createBenchmark(main, {
-  n: [5e3],
+  n: [10],
   len: [1e2, 1e5],
-  type: [
-    'denseArray',
-    'sparseArray',
-    'mixedArray',
-    'denseArray_showHidden',
-  ],
+  type: ['denseArray', 'sparseArray', 'mixedArray', 'denseArray_showHidden'],
 });
 
 function main({ n, len, type }) {
@@ -29,8 +24,7 @@ function main({ n, len, type }) {
     case 'sparseArray':
       break;
     case 'mixedArray':
-      for (let i = 0; i < n; i += 2)
-        arr[i] = i;
+      for (let i = 0; i < n; i += 2) arr[i] = i;
       break;
     default:
       throw new Error(`Unsupported type ${type}`);
