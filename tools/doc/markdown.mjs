85a00d3c40c00f27f809eca0e6f8350c749d35d4
avivkeller <me@aviv.sh>
2025-08-30 10:04:44 -0400

    fixup!

--- tools/doc/markdown.mjs
+++ tools/doc/markdown.mjs
@@ -2,7 +2,7 @@ import { visit } from 'unist-util-visit';
 
 export const referenceToLocalMdFile = /^(?![+a-z]+:)([^#?]+)\.md(#.+)?$/i;
 
-export default () => {
+export function replaceLinks({ filename, linksMapper }) {
   return (tree) => {
     visit(tree, (node) => {
       node.url &&= node.url.replace(
