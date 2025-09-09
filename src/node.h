234c26cca3bbd0bb9f4c0261dd3605056f284afb
Anna Henningsen <anna@addaleax.net>
2025-09-09 17:00:42 +0200

    src: store `Local` for `CallbackScope` on stack
    This is a requirement of the V8 API, but requires a separate
    semver-major change (as it is ABI-breaking) to address.
    
    (There's also a similar requirement for `napi_open_callback_scope`
    that would not be easily addressable without breaking ABI compatibility
    there as well. In real-world situations, it seems extremely unlikely
    that the `CallbackScope` would be the only reference to the resource
    object.)
    
    PR-URL: https://github.com/nodejs/node/pull/59705
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>

--- src/node.h
+++ src/node.h
@@ -1452,6 +1452,8 @@ class NODE_EXTERN CallbackScope {
   CallbackScope(CallbackScope&&) = delete;
 
  private:
+  void* reserved_;
+  v8::Local<v8::Object> resource_storage_;
   InternalCallbackScope* private_;
   v8::TryCatch try_catch_;
 };
