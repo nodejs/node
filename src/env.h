a7f7d10c067faa8c67ee75b30306de7e225d6f86
Aditi <62544124+Aditi-1400@users.noreply.github.com>
2025-09-12 16:30:39 +0530

    src: add an option to make compile cache portable
    Adds an option (NODE_COMPILE_CACHE_PORTABLE) for
    the built-in compile cache to encode the hashes with
    relative file paths. On enabling the option,
    the source directory along with cache directory can be
    bundled and moved, and the cache continues to work.
    
    When enabled, paths encoded in hash are relative to
    compile cache directory.
    
    PR-URL: https://github.com/nodejs/node/pull/58797
    Fixes: https://github.com/nodejs/node/issues/58755
    Refs: https://github.com/nodejs/node/issues/52696
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/env.h
+++ src/env.h
@@ -1023,7 +1023,8 @@ class Environment final : public MemoryRetainer {
   void InitializeCompileCache();
   // Enable built-in compile cache if it has not yet been enabled.
   // The cache will be persisted to disk on exit.
-  CompileCacheEnableResult EnableCompileCache(const std::string& cache_dir);
+  CompileCacheEnableResult EnableCompileCache(const std::string& cache_dir,
+                                              EnableOption option);
   void FlushCompileCache();
 
   void RunAndClearNativeImmediates(bool only_refed = false);
