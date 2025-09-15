626b5e344ba4d6a74f5b51db7b67856925452edb
Richard Lau <richard.lau@ibm.com>
2025-09-15 17:42:06 +0000

    fixup! tools: fix `tools/make-v8.sh` for clang

--- tools/make-v8.sh
+++ tools/make-v8.sh
@@ -21,16 +21,17 @@ if [ "$ARCH" = "s390x" ] || [ "$ARCH" = "ppc64le" ]; then
     TARGET_ARCH="ppc64"
   fi
   # Propagate ccache to gn.
+  # shellcheck disable=SC2154
   case "$CXX" in
     *ccache*) CC_WRAPPER="cc_wrapper=\"ccache\"" ;;
     *) ;;
   esac
 
+  # shellcheck disable=SC2154
   case "$CXX" in
     *clang*) GN_COMPILER_OPTS="is_clang=true clang_base_path=\"/usr\" clang_use_chrome_plugins=false treat_warnings_as_errors=false use_custom_libcxx=false" ;;
     *) GN_COMPILER_OPTS="treat_warnings_as_errors=false use_custom_libcxx=false" ;;
   esac
-  export PKG_CONFIG_PATH=$BUILD_TOOLS/pkg-config
   gn gen -v "out.gn/$BUILD_ARCH_TYPE" --args="$GN_COMPILER_OPTS is_component_build=false is_debug=false v8_target_cpu=\"$TARGET_ARCH\" target_cpu=\"$TARGET_ARCH\" v8_enable_backtrace=true $CC_WRAPPER"
   ninja -v -C "out.gn/$BUILD_ARCH_TYPE" "${JOBS_ARG}" d8 cctest inspector-test
 else
