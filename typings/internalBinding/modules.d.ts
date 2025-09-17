36b68db7f5a6690930ffae42764dab86cb9105a2
Michael Smith <smithr.michael1997@gmail.com>
2025-09-17 06:26:52 -0400

    src: reduce the nearest parent package JSON cache size
    PR-URL: https://github.com/nodejs/node/pull/59888
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Edy Silva <edigleyssonsilva@gmail.com>
    Reviewed-By: Ruben Bridgewater <ruben@bridgewater.de>

--- typings/internalBinding/modules.d.ts
+++ typings/internalBinding/modules.d.ts
@@ -23,7 +23,6 @@ export type SerializedPackageConfig = [
 export interface ModulesBinding {
   readPackageJSON(path: string): SerializedPackageConfig | undefined;
   getNearestParentPackageJSONType(path: string): PackageConfig['type']
-  getNearestParentPackageJSON(path: string): SerializedPackageConfig | undefined
   getPackageScopeConfig(path: string): SerializedPackageConfig | undefined
   getPackageType(path: string): PackageConfig['type'] | undefined
   enableCompileCache(path?: string): { status: number, message?: string, directory?: string }
