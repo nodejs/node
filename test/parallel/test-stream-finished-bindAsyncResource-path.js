d51ca0dcb2a117a2e00b3a17eb985a9d37bf429f
Amir Valizadeh <avalizadeh@cribl.io>
2025-09-18 15:28:55 -0700

    expanded on the unit test

--- test/parallel/test-stream-finished-bindAsyncResource-path.js
+++ test/parallel/test-stream-finished-bindAsyncResource-path.js
@@ -3,19 +3,33 @@
 
 const common = require('../common');
 const { Readable, finished } = require('stream');
-const { createHook } = require('async_hooks');
+const { createHook, executionAsyncId } = require('async_hooks');
 const { strictEqual } = require('assert');
 const internalAsyncHooks = require('internal/async_hooks');
 
 // This test verifies that when there are active async hooks, stream.finished() uses
 // the bindAsyncResource path
 
-createHook({}).enable();
+createHook({
+  init(asyncId, type, triggerAsyncId) {
+    if (type === 'STREAM_END_OF_STREAM') {
+      const parentContext = contextMap.get(triggerAsyncId);
+      contextMap.set(asyncId, parentContext);
+    }
+  }
+}).enable();
+
+const contextMap = new Map();
+const asyncId = executionAsyncId();
+contextMap.set(asyncId, 'abc-123');
 const readable = new Readable();
 
 finished(readable, common.mustCall(() => {
+  const currentAsyncId = executionAsyncId();
+  const ctx = contextMap.get(currentAsyncId);
   strictEqual(internalAsyncHooks.getHookArrays()[0].length > 0,
               true, 'Should have active user async hook');
+  strictEqual(ctx, 'abc-123', 'Context should be preserved');
 }));
 
 readable.destroy();
