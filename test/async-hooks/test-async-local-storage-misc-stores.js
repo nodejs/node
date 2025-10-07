7b5d633f660318ff777ddeb52c126d3f3ba9d3ab
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-10-07 23:36:30 +0200

    test: ensure assertions are reachable in `test/async-hooks`

--- test/async-hooks/test-async-local-storage-misc-stores.js
+++ test/async-hooks/test-async-local-storage-misc-stores.js
@@ -1,15 +1,15 @@
 'use strict';
-require('../common');
+const common = require('../common');
 const assert = require('assert');
 const { AsyncLocalStorage } = require('async_hooks');
 
 const asyncLocalStorage = new AsyncLocalStorage();
 
-asyncLocalStorage.run('hello node', () => {
+asyncLocalStorage.run('hello node', common.mustCall(() => {
   assert.strictEqual(asyncLocalStorage.getStore(), 'hello node');
-});
+}));
 
 const runStore = { hello: 'node' };
-asyncLocalStorage.run(runStore, () => {
+asyncLocalStorage.run(runStore, common.mustCall(() => {
   assert.strictEqual(asyncLocalStorage.getStore(), runStore);
-});
+}));
