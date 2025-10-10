f43c6434e258812df21d2139c6ebdee96648acbc
방진혁 <jinhyeokfang@gmail.com>
2025-10-11 07:37:01 +0900

    typings: add buffer internalBinding typing
    PR-URL: https://github.com/nodejs/node/pull/60163
    Reviewed-By: Daeyeon Jeong <daeyeon.dev@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- typings/globals.d.ts
+++ typings/globals.d.ts
@@ -1,6 +1,7 @@
 import { AsyncContextFrameBinding } from './internalBinding/async_context_frame';
 import { AsyncWrapBinding } from './internalBinding/async_wrap';
 import { BlobBinding } from './internalBinding/blob';
+import { BufferBinding } from './internalBinding/buffer';
 import { ConfigBinding } from './internalBinding/config';
 import { ConstantsBinding } from './internalBinding/constants';
 import { DebugBinding } from './internalBinding/debug';
@@ -31,6 +32,7 @@ interface InternalBindingMap {
   async_context_frame: AsyncContextFrameBinding;
   async_wrap: AsyncWrapBinding;
   blob: BlobBinding;
+  buffer: BufferBinding;
   config: ConfigBinding;
   constants: ConstantsBinding;
   debug: DebugBinding;
