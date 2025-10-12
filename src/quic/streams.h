4efd16af7eff8158de7ab81f757f93671912bcb9
Marten Richter <marten.richter@tu-berlin.de>
2025-10-12 19:26:43 +0200

    quic: for streams allow ReadableStream as body
    After the change the body parameter for all
    created QuicStreams can take a ReadableStream
    of Uint8Array, Arraybuffers etc..
    
    We introduce a DataQueueFeeder class, that may be
    also used for other related mechanisms.
    
    A locally created unidirectional Stream can not
    have a reader. Therefore the interface is changed
    to handle this case. (undercovered when writing
     the tests).
    
    Furthermore, a ResumeStream must be added After
    AddStream in QuicSession, as the ResumeStream
    beforehand triggered with set_outbound is a no-op,
    as Stream was not added to Session beforehand.
    
    Fixes: https://github.com/nodejs/node/issues/60234

--- src/quic/streams.h
+++ src/quic/streams.h
@@ -15,12 +15,17 @@
 #include "bindingdata.h"
 #include "data.h"
 
-namespace node::quic {
+namespace node {
+
+using v8::Global;
+using v8::Promise;
+namespace quic {
 
 class Session;
 class Stream;
 
 using Ngtcp2Source = bob::SourceImpl<ngtcp2_vec>;
+
 
 // When a request to open a stream is made before a Session is able to actually
 // open a stream (either because the handshake is not yet sufficiently complete
@@ -387,7 +392,58 @@ class Stream final : public AsyncWrap,
   void Schedule(Queue* queue);
   void Unschedule();
 };
+}  // namespace quic
+class DataQueueFeeder final: public AsyncWrap {
+ public:
+using Next = bob::Next<DataQueue::Vec>;
 
-}  // namespace node::quic
+  DataQueueFeeder(Environment* env,
+           v8::Local<v8::Object> object);
+
+  JS_CONSTRUCTOR(DataQueueFeeder);
+  JS_BINDING_INIT_BOILERPLATE();
+
+  static BaseObjectPtr<DataQueueFeeder> Create();
+
+  void setDataQueue(std::shared_ptr<DataQueue> queue) {
+    dataQueue_ = queue;
+  }
+
+  void tryWakePulls();
+  void DrainAndClose();
+
+  struct PendingPull {
+    Next next;
+    explicit PendingPull(Next next)
+        : next(std::move(next)) {}
+  };
+
+  void addPendingPull(PendingPull toAdd) {
+    pendingPulls_.emplace_back(std::move(toAdd));
+  }
+
+  bool Done() {return done;};
+
+  SET_NO_MEMORY_INFO()
+  SET_MEMORY_INFO_NAME(DataQueueFeeder)
+  SET_SELF_SIZE(DataQueueFeeder)
+
+  JS_METHOD(New);
+  JS_METHOD(Submit);
+  JS_METHOD(Error);
+  JS_METHOD(Ready);
+
+
+ private:
+  std::shared_ptr<DataQueue> dataQueue_;
+  Global<Promise::Resolver> readFinish_;
+
+
+
+  std::deque<PendingPull> pendingPulls_;
+  bool done = false;
+};
+
+}  // namespace node
 
 #endif  // defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
