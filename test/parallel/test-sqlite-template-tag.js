71a8a57d581696060816c74976b2f3d9cacf856e
islandryu <shimaryuhei@gmail.com>
2025-10-12 17:19:29 +0900

    sqlite: improve error reporting for prepared statements in SQLTagStore
    Fixes: https://github.com/nodejs/node/issues/60198

--- test/parallel/test-sqlite-template-tag.js
+++ test/parallel/test-sqlite-template-tag.js
@@ -100,3 +100,16 @@ test('TagStore capacity, size, and clear', () => {
 test('sql.db returns the associated DatabaseSync instance', () => {
   assert.strictEqual(sql.db, db);
 });
+
+test('failed prepares throw', () => {
+  assert.throws(() => {
+    sql.all`SELECT * FROM does_not_exist`; // eslint-disable-line no-unused-expressions
+  }, {
+    name: 'Error',
+    message: 'Failed to prepare statement',
+    code: 'ERR_SQLITE_ERROR',
+    errcode: 1,
+    errstr: 'no such table: does_not_exist',
+    errmsg: 'SQL logic error'
+  });
+});
