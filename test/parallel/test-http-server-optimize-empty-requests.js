4395fe14b9002171defd4fb896adbd2bf4d93d13
Rafael Gonzaga <rafael.nunu@hotmail.com>
2025-10-14 09:17:53 -0300

    http: add optimizeEmptyRequests server option
    Signed-off-by: RafaelGSS <rafael.nunu@hotmail.com>
    Co-Authored-By: RafaelGSS <rafael.nunu@hotmail.com>
    PR-URL: https://github.com/nodejs/node/pull/59778
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Tim Perry <pimterry@gmail.com>
    Reviewed-By: Gerhard Stöbich <deb2001-github@yahoo.de>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

---
+++ test/parallel/test-http-server-optimize-empty-requests.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const http = require('http');
+const net = require('net');
+
+let reqs = 0;
+let optimizedReqs = 0;
+const server = http.createServer({
+  optimizeEmptyRequests: true
+}, (req, res) => {
+  reqs++;
+  if (req._dumped) {
+    optimizedReqs++;
+    req.on('data', common.mustNotCall());
+    req.on('end', common.mustNotCall());
+
+    assert.strictEqual(req._dumped, true);
+    assert.strictEqual(req.readableEnded, true);
+    assert.strictEqual(req.destroyed, true);
+  }
+  res.writeHead(200);
+  res.end('ok');
+});
+
+server.listen(0, common.mustCall(async () => {
+  // GET request without Content-Length (should be optimized)
+  const getRequest = 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n';
+  await makeRequest(getRequest);
+
+  // HEAD request (should always be optimized regardless of headers)
+  const headRequest = 'HEAD / HTTP/1.1\r\nHost: localhost\r\n\r\n';
+  await makeRequest(headRequest);
+
+  // POST request without body headers (should be optimized)
+  const postWithoutBodyHeaders = 'POST / HTTP/1.1\r\nHost: localhost\r\n\r\n';
+  await makeRequest(postWithoutBodyHeaders);
+
+  // DELETE request without body headers (should be optimized)
+  const deleteWithoutBodyHeaders = 'DELETE / HTTP/1.1\r\nHost: localhost\r\n\r\n';
+  await makeRequest(deleteWithoutBodyHeaders);
+
+  // POST request with Content-Length header (should not be optimized)
+  const postWithContentLength = 'POST / HTTP/1.1\r\nHost: localhost\r\nContent-Length: 0\r\n\r\n';
+  await makeRequest(postWithContentLength);
+
+  // GET request with Content-Length header (should not be optimized)
+  const getWithContentLength = 'GET / HTTP/1.1\r\nHost: localhost\r\nContent-Length: 0\r\n\r\n';
+  await makeRequest(getWithContentLength);
+
+  // POST request with Transfer-Encoding header (should not be optimized)
+  const postWithTransferEncoding = 'POST / HTTP/1.1\r\nHost: localhost\r\nTransfer-Encoding: chunked\r\n\r\n';
+  await makeRequest(postWithTransferEncoding);
+
+  // GET request with Transfer-Encoding header (should not be optimized)
+  const getWithTransferEncoding = 'GET / HTTP/1.1\r\nHost: localhost\r\nTransfer-Encoding: chunked\r\n\r\n';
+  await makeRequest(getWithTransferEncoding);
+
+  server.close();
+
+  assert.strictEqual(reqs, 8, `Expected 8 requests but got ${reqs}`);
+  assert.strictEqual(optimizedReqs, 4, `Expected 4 optimized requests but got ${optimizedReqs}`);
+}));
+
+function makeRequest(str) {
+  return new Promise((resolve) => {
+    const client = net.connect({ port: server.address().port }, common.mustCall(() => {
+      client.on('data', () => {});
+      client.on('end', common.mustCall(() => {
+        resolve();
+      }));
+      client.write(str);
+      client.end();
+    }));
+  });
+}
