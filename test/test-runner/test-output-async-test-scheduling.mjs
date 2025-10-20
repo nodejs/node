72e27824ddcde9136e613777b309ea3d1cec6d7e
Joyee Cheung <joyeec9h3@gmail.com>
2025-10-21 01:34:01 +0200

    test: split test-runner-output
    This test has been flaky for months. As it squeezes too many
    independent test cases together in one file, split this up
    before marking any persistent flaky cases as flaky, to avoid
    masking regressions in other non-flaky cases.

---
+++ test/test-runner/test-output-async-test-scheduling.mjs
@@ -0,0 +1,13 @@
+// Flags: --expose-internals
+// Test that the output of test-runner/output/async-test-scheduling.mjs matches
+// test-runner/output/async-test-scheduling.snapshot
+import '../common/index.mjs';
+import * as fixtures from '../common/fixtures.mjs';
+import { spawnAndAssert, defaultTransform, ensureCwdIsProjectRoot } from '../common/assertSnapshot.js';
+
+ensureCwdIsProjectRoot();
+await spawnAndAssert(
+  fixtures.path('test-runner/output/async-test-scheduling.mjs'),
+  defaultTransform,
+  { flags: ['--test-reporter=tap'] },
+);
---
+++ test/test-runner/test-output-global-after-should-fail-the-test.mjs
@@ -0,0 +1,13 @@
+// Flags: --expose-internals
+// Test that the output of test-runner/output/global_after_should_fail_the_test.js matches
+// test-runner/output/global_after_should_fail_the_test.snapshot
+import '../common/index.mjs';
+import * as fixtures from '../common/fixtures.mjs';
+import { spawnAndAssert, defaultTransform, ensureCwdIsProjectRoot } from '../common/assertSnapshot.js';
+
+ensureCwdIsProjectRoot();
+await spawnAndAssert(
+  fixtures.path('test-runner/output/global_after_should_fail_the_test.js'),
+  defaultTransform,
+  { flags: ['--test-reporter=tap'] },
+);
