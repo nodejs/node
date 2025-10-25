12e9213a244855aa0c8d99caa5cc67e8cddff4fd
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-10-25 23:49:42 +0200

    test: ensure assertions are reachable in `test/sequential`
    PR-URL: https://github.com/nodejs/node/pull/60412
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- test/sequential/test-timers-block-eventloop.js
+++ test/sequential/test-timers-block-eventloop.js
@@ -6,14 +6,14 @@ const assert = require('assert');
 const { sleep } = require('internal/util');
 
 let called = false;
-const t1 = setInterval(() => {
+const t1 = setInterval(common.mustCallAtLeast(() => {
   assert(!called);
   called = true;
   setImmediate(common.mustCall(() => {
     clearInterval(t1);
     clearInterval(t2);
   }));
-}, 10);
+}), 10);
 
 const t2 = setInterval(() => {
   sleep(20);
