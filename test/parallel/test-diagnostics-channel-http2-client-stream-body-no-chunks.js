4ef2ba647a6b5178523ad18ca211d26ea8e34611
Darshan Sen <raisinten@gmail.com>
2025-10-27 18:59:23 +0530

    http2: add diagnostics channels for client stream request body
    These would allow inspection of HTTP/2 client stream request bodies.
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/60480
    Reviewed-By: Ilyas Shabi <ilyasshabi94@gmail.com>
    Reviewed-By: Stephen Belanger <admin@stephenbelanger.com>

---
+++ test/parallel/test-diagnostics-channel-http2-client-stream-body-no-chunks.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+
+// This test ensures that the built-in HTTP/2 diagnostics channels are reporting
+// the diagnostics messages for the 'http2.client.stream.bodyChunkSent' and
+// 'http2.client.stream.bodySent' channels when ClientHttp2Streams bodies are
+// being sent with no chunks.
+
+const assert = require('assert');
+const dc = require('diagnostics_channel');
+const http2 = require('http2');
+const { Duplex } = require('stream');
+
+dc.subscribe('http2.client.stream.bodyChunkSent', common.mustNotCall());
+
+dc.subscribe('http2.client.stream.bodySent', common.mustCall(({ stream }) => {
+  // Since ClientHttp2Stream is not exported from any module, this just checks
+  // if the stream is an instance of Duplex.
+  assert.ok(stream instanceof Duplex);
+  assert.strictEqual(stream.constructor.name, 'ClientHttp2Stream');
+}));
+
+const server = http2.createServer();
+server.on('stream', common.mustCall((stream) => {
+  stream.respond({}, { endStream: true });
+}));
+
+server.listen(0, common.mustCall(() => {
+  const port = server.address().port;
+  const client = http2.connect(`http://localhost:${port}`);
+
+  const stream = client.request({ [http2.constants.HTTP2_HEADER_METHOD]: 'POST' });
+  stream.end();
+
+  stream.on('response', common.mustCall(() => {
+    client.close();
+    server.close();
+  }));
+}, 1));
