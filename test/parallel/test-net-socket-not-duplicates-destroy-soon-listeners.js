9d081a1078f149be01ee57666f57d3ed76ea1209
arturgawlik <artur.gawlik@icloud.com>
2025-10-28 23:19:27 +0100

    remove third argument on `strictEqual` call

--- test/parallel/test-net-socket-not-duplicates-destroy-soon-listeners.js
+++ test/parallel/test-net-socket-not-duplicates-destroy-soon-listeners.js
@@ -13,13 +13,5 @@ socket.destroySoon();
 socket.destroySoon();
 const finishListenersCount = socket.listeners('finish').length;
 const connectListenersCount = socket.listeners('connect').length;
-assert.strictEqual(
-  finishListenersCount,
-  1,
-  '"finish" event listeners should not be duplicated for multiple "Socket.destroySoon" calls'
-);
-assert.strictEqual(
-  connectListenersCount,
-  1,
-  '"connect" event listeners should not be duplicated for multiple "Socket.destroySoon" calls'
-);
+assert.strictEqual(finishListenersCount, 1);
+assert.strictEqual(connectListenersCount, 1);
