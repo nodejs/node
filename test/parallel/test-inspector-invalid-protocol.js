e0216a1680b82782aa78abfd30be9b3a1b3aaf57
Shima Ryuhei <65934663+islandryu@users.noreply.github.com>
2025-10-29 20:20:29 +0900

    inspector: fix crash when receiving non json message
    PR-URL: https://github.com/nodejs/node/pull/60388
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Kohei Ueno <kohei.ueno119@gmail.com>

---
+++ test/parallel/test-inspector-invalid-protocol.js
@@ -0,0 +1,40 @@
+'use strict';
+const common = require('../common');
+common.skipIfInspectorDisabled();
+const { spawn } = require('node:child_process');
+const assert = require('node:assert');
+
+(async () => {
+  const child = spawn(
+    process.execPath,
+    ['--inspect-wait=0', '-e', "console.log('test');"],
+    {}
+  );
+
+  const url = await new Promise((resolve) => {
+    child.stderr.on('data', (data) => {
+      const msg = data.toString();
+      const match = msg.match(/ws:\/\/127\.0\.0\.1:(\d+)\/([a-f0-9-]+)/);
+      if (match) {
+        child.stderr.removeAllListeners('data');
+        return resolve(match[0]);
+      }
+    });
+  });
+
+  child.once('exit', (_, signal) => {
+    assert.strictEqual(signal, 'SIGTERM');
+  });
+
+  const socket = new WebSocket(url);
+
+  socket.addEventListener('open', common.mustCall(() => {
+    socket.send('This is not a valid protocol message');
+  }));
+
+  socket.addEventListener('message', common.mustCall((event) => {
+    assert.ok(Object.keys(JSON.parse(event.data)).includes('error'));
+    socket.close();
+    child.kill();
+  }));
+})().then(common.mustCall());
