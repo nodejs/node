40ef4553497d7d79680c05af2122ebd214bb5c5f
arturgawlik <artur.gawlik@icloud.com>
2025-10-31 22:39:21 +0100

    revert changes in `_final`

--- test/parallel/test-net-socket-not-duplicates-destroy-soon-listeners.js
+++ test/parallel/test-net-socket-not-duplicates-destroy-soon-listeners.js
@@ -3,20 +3,13 @@ require('../common');
 const { addresses } = require('../common/internet');
 
 const assert = require('assert');
-const net = require('net');
+const { Socket } = require('net');
 
-const socket = new net.Socket();
+const socket = new Socket();
 socket.on('error', () => {
   // noop
 });
-const connectOptions = { host: addresses.INVALID_HOST, port: 1234 };
-
-socket.connect(connectOptions);
-socket.destroySoon(); // Adds "connect" and "finish" event listeners when socket has "writable" state
-socket.destroy(); // Makes imideditly socket again "writable"
-
-socket.connect(connectOptions);
-socket.destroySoon(); // Should not duplicate "connect" and "finish" event listeners
-
+socket.connect({ host: addresses.INVALID_HOST, port: 1234 });
+socket.destroySoon();
+socket.destroySoon();
 assert.strictEqual(socket.listeners('finish').length, 1);
-assert.strictEqual(socket.listeners('connect').length, 1);
