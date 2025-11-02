7b5a01ab4af4e4ef4232ce8c049e2ed01fa8dadc
Artur Gawlik <artur.gawlik@icloud.com>
2025-11-02 23:34:42 +0100

    Update test/parallel/test-net-socket-not-duplicates-destroy-soon-listeners.js
    Co-authored-by: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-net-socket-not-duplicates-destroy-soon-listeners.js
+++ test/parallel/test-net-socket-not-duplicates-destroy-soon-listeners.js
@@ -5,10 +5,6 @@ const assert = require('assert');
 const { Socket } = require('net');
 
 const socket = new Socket();
-socket.on('error', () => {
-  // noop
-});
-socket.connect({ host: addresses.INVALID_HOST, port: 1234 });
 socket.destroySoon();
 socket.destroySoon();
 assert.strictEqual(socket.listeners('finish').length, 1);
