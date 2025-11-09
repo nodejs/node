9f14eb93b8248ae033eab40c285afd4515697bd5
Joyee Cheung <joyeec9h3@gmail.com>
2025-11-09 13:13:04 +0100

    src: build v8 tick processor as built-in source text modules
    Instead of polyfilling it with vm.SourceTextModule,
    use a built-in source text module loader so that we can
    also build the code cache for it at build tiem to
    embed the code cache for them in the binary.
    
    Drive-by: instead of inferring how to compile a particular
    built-in at run time, do the inferring at build time,
    so the function-based built-ins can be compiled using
    parameters quickly looked up from a static map, and
    the builtins that should be compiled as source text
    modules are known internally based on extension in
    the source code (at run time, the extensions are all
    removed).

--- src/env_properties.h
+++ src/env_properties.h
@@ -57,6 +57,7 @@
   V(onpskexchange_symbol, "onpskexchange")                                     \
   V(resource_symbol, "resource_symbol")                                        \
   V(trigger_async_id_symbol, "trigger_async_id_symbol")                        \
+  V(builtin_source_text_module_hdo, "builtin_source_text_module_hdo")          \
   V(source_text_module_default_hdo, "source_text_module_default_hdo")          \
   V(vm_context_no_contextify, "vm_context_no_contextify")                      \
   V(vm_dynamic_import_default_internal, "vm_dynamic_import_default_internal")  \
