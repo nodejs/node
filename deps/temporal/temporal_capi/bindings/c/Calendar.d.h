9790c852213918a5b0d99d73c1b3b89862443dbe
Chengzhong Wu <legendecas@gmail.com>
2025-11-13 00:13:55 +0000

    deps: add temporal_rs 0.1.0

---
+++ deps/temporal/temporal_capi/bindings/c/Calendar.d.h
@@ -0,0 +1,19 @@
+#ifndef Calendar_D_H
+#define Calendar_D_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "diplomat_runtime.h"
+
+
+
+
+
+typedef struct Calendar Calendar;
+
+
+
+
+#endif // Calendar_D_H
