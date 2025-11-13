9426ff6b08a652bedb028cfa95f768181c0173f4
Chengzhong Wu <cwu631@bloomberg.net>
2025-11-13 14:39:19 +0000

    test: add basic temporal presence check
    PR-URL: https://github.com/nodejs/node/pull/60703
    Refs: https://github.com/nodejs/node/issues/58730
    Refs: https://github.com/nodejs/node/pull/60693
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- test/common/index.js
+++ test/common/index.js
@@ -36,6 +36,17 @@ const bits = ['arm64', 'loong64', 'mips', 'mipsel', 'ppc64', 'riscv64', 's390x',
   .includes(process.arch) ? 64 : 32;
 const hasIntl = !!process.config.variables.v8_enable_i18n_support;
 
+// small-icu doesn't support non-English locales
+const hasFullICU = (() => {
+  try {
+    const january = new Date(9e8);
+    const spanish = new Intl.DateTimeFormat('es', { month: 'long' });
+    return spanish.format(january) === 'enero';
+  } catch {
+    return false;
+  }
+})();
+
 const {
   atob,
   btoa,
@@ -936,6 +947,7 @@ const common = {
   getBufferSources,
   getTTYfd,
   hasIntl,
+  hasFullICU,
   hasCrypto,
   hasQuic,
   hasInspector,
