4947af957f4f2dfa04a6150f079c280864180495
Chengzhong Wu <legendecas@gmail.com>
2025-11-13 00:13:55 +0000

    deps: add temporal_rs 0.1.0
    PR-URL: https://github.com/nodejs/node/pull/60703
    Refs: https://github.com/nodejs/node/issues/58730
    Refs: https://github.com/nodejs/node/pull/60693
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

---
+++ deps/temporal/temporal_capi/bindings/c/ArithmeticOverflow.h
@@ -0,0 +1,22 @@
+#ifndef ArithmeticOverflow_H
+#define ArithmeticOverflow_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "diplomat_runtime.h"
+
+
+#include "ArithmeticOverflow.d.h"
+
+
+
+
+
+
+
+
+
+
+#endif // ArithmeticOverflow_H
