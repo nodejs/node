700df39d939fe00a93d27b5b85ef185945392d2a
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2025-11-13 22:45:17 +0100

    node-api: support SharedArrayBuffer in napi_create_dataview
    PR-URL: https://github.com/nodejs/node/pull/60473
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- test/js-native-api/test_dataview/test.js
+++ test/js-native-api/test_dataview/test.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 // Testing api calls for arrays
 const test_dataview = require(`./build/${common.buildType}/test_dataview`);
 
-// Test for creating dataview
+// Test for creating dataview with ArrayBuffer
 {
   const buffer = new ArrayBuffer(128);
   const template = Reflect.construct(DataView, [buffer]);
@@ -15,9 +15,29 @@ const test_dataview = require(`./build/${common.buildType}/test_dataview`);
             `Expect ${theDataview} to be a DataView`);
 }
 
-// Test for creating dataview with invalid range
+// Test for creating dataview with SharedArrayBuffer
+{
+  const buffer = new SharedArrayBuffer(128);
+  const template = new DataView(buffer);
+
+  const theDataview = test_dataview.CreateDataViewFromJSDataView(template);
+  assert.ok(theDataview instanceof DataView,
+            `Expect ${theDataview} to be a DataView`);
+
+  assert.strictEqual(template.buffer, theDataview.buffer);
+}
+
+// Test for creating dataview with ArrayBuffer and invalid range
 {
   const buffer = new ArrayBuffer(128);
+  assert.throws(() => {
+    test_dataview.CreateDataView(buffer, 10, 200);
+  }, RangeError);
+}
+
+// Test for creating dataview with SharedArrayBuffer and invalid range
+{
+  const buffer = new SharedArrayBuffer(128);
   assert.throws(() => {
     test_dataview.CreateDataView(buffer, 10, 200);
   }, RangeError);
