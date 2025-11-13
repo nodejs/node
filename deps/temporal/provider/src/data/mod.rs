cb8d53bdb735181721c021a86a9207f779b310ef
Chengzhong Wu <legendecas@gmail.com>
2025-11-13 00:13:55 +0000

    deps: add temporal_rs 0.1.0
    PR-URL: https://github.com/nodejs/node/pull/60703
    Refs: https://github.com/nodejs/node/issues/58730
    Refs: https://github.com/nodejs/node/pull/60693
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Joyee Cheung <joyeec9h3@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

---
+++ deps/temporal/provider/src/data/mod.rs
@@ -0,0 +1,3 @@
+include!("iana_normalizer.rs.data");
+
+include!("compiled_zoneinfo_provider.rs.data");
---
+++ deps/temporal/temporal_capi/bindings/c/ParsedDate.d.h
@@ -0,0 +1,19 @@
+#ifndef ParsedDate_D_H
+#define ParsedDate_D_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "diplomat_runtime.h"
+
+
+
+
+
+typedef struct ParsedDate ParsedDate;
+
+
+
+
+#endif // ParsedDate_D_H
