96f0600df4bd25bb7ab9142a58005b0f0af9b453
Michaël Zasso <targos@protonmail.com>
2025-11-18 09:15:53 +0100

    fixup: config warning
    > [@stylistic/eslint-plugin]: You are using deprecated value(boolean) for "allowTemplateLiterals" in "quotes", please use "always"/"never" instead.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -326,7 +326,7 @@ export default [
         'error',
         { blankLine: 'always', prev: 'function', next: 'function' },
       ],
-      '@stylistic/js/quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+      '@stylistic/js/quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: 'always' }],
       '@stylistic/js/quote-props': ['error', 'consistent'],
       '@stylistic/js/rest-spread-spacing': 'error',
       '@stylistic/js/semi': 'error',
