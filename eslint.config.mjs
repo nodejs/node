78aef6c098b8ebad62eec437352d3c4cbb49a6cb
Michaël Zasso <targos@protonmail.com>
2025-11-18 10:19:16 +0100

    tools: remove deprecated ESLint plugins
    Syntax for explicit resource management and import attributes syntax
    are supported without it.
    
    PR-URL: https://github.com/nodejs/node/pull/60908
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Ulises Gascón <ulisesgascongonzalez@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,9 +18,6 @@ import nodeCore from './tools/eslint/eslint-plugin-node-core.js';
 const { globalIgnores } = await importEslintTool('eslint/config');
 const { default: js } = await importEslintTool('@eslint/js');
 const { default: babelEslintParser } = await importEslintTool('@babel/eslint-parser');
-const babelPluginProposalExplicitResourceManagement =
-  resolveEslintTool('@babel/plugin-proposal-explicit-resource-management');
-const babelPluginSyntaxImportAttributes = resolveEslintTool('@babel/plugin-syntax-import-attributes');
 const babelPluginSyntaxImportSource = resolveEslintTool('@babel/plugin-syntax-import-source');
 const { default: jsdoc } = await importEslintTool('eslint-plugin-jsdoc');
 const { default: markdown } = await importEslintTool('eslint-plugin-markdown');
@@ -106,8 +103,6 @@ export default [
         babelOptions: {
           parserOpts: { createImportExpressions: true },
           plugins: [
-            babelPluginProposalExplicitResourceManagement,
-            babelPluginSyntaxImportAttributes,
             babelPluginSyntaxImportSource,
           ],
         },
