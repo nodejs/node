35b8dac56619749b052982166ade9bdcb84d569e
root <root@PC-5075.localdomain>
2025-11-19 02:52:25 -0400

    child_process: add getMemoryUsage() method to ChildProcess
    Signed-off-by: root <root@PC-5075.localdomain>

--- src/env_properties.h
+++ src/env_properties.h
@@ -394,6 +394,7 @@
   V(contextify_global_template, v8::ObjectTemplate)                            \
   V(contextify_wrapper_template, v8::ObjectTemplate)                           \
   V(cpu_usage_template, v8::DictionaryTemplate)                                \
+  V(memory_usage_template, v8::DictionaryTemplate)                             \
   V(crypto_key_object_handle_constructor, v8::FunctionTemplate)                \
   V(env_proxy_template, v8::ObjectTemplate)                                    \
   V(env_proxy_ctor_template, v8::FunctionTemplate)                             \
