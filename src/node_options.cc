6caaf5f51d44128d80c048518f8d57ff5d2bc150
y-okt <yuki.work0512@gmail.com>
2025-11-22 23:42:56 +0900

    src: make --strip-types option false when compiled without amaro
    stripe-types option's default value is true, but we need to ensure that when compiled without amaro, the default vaue is false.
    
    Fixes: https://github.com/nodejs/node/issues/60640

--- src/node_options.cc
+++ src/node_options.cc
@@ -1092,7 +1092,12 @@ EnvironmentOptionsParser::EnvironmentOptionsParser() {
             "Type-stripping for TypeScript files.",
             &EnvironmentOptions::strip_types,
             kAllowedInEnvvar,
-            true);
+#if HAVE_AMARO
+            true
+#else
+            false
+#endif  // HAVE_AMARO
+);
   AddAlias("--experimental-strip-types", "--strip-types");
   AddOption("--experimental-transform-types",
             "enable transformation of TypeScript-only"
