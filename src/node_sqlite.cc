69cb3ab1eb93283b9dc39da9e3754b82cbbea1d7
Edy Silva <edigleyssonsilva@gmail.com>
2025-11-29 01:59:05 -0300

    sqlite: set thread mode to multi threaded

--- src/node_sqlite.cc
+++ src/node_sqlite.cc
@@ -474,6 +474,7 @@ class SQLiteAsyncTask : public ThreadPoolWork {
       after_(result_, resolver);
       Finalize();
     }
+    db_->ProcessNextAsyncTask();
   }
 
   void Finalize() { db_->RemoveAsyncTask(this); }
@@ -743,6 +744,25 @@ void Database::AddAsyncTask(ThreadPoolWork* async_task) {
 
 void Database::RemoveAsyncTask(ThreadPoolWork* async_task) {
   async_tasks_.erase(async_task);
+}
+
+void Database::ScheduleAsyncTask(ThreadPoolWork* work) {
+  if (has_running_task_) {
+    task_queue_.push(work);
+  } else {
+    has_running_task_ = true;
+    work->ScheduleWork();
+  }
+}
+
+void Database::ProcessNextAsyncTask() {
+  if (task_queue_.empty()) {
+    has_running_task_ = false;
+  } else {
+    ThreadPoolWork* next_work = task_queue_.front();
+    task_queue_.pop();
+    next_work->ScheduleWork();
+  }
 }
 
 void Database::DeleteSessions() {
@@ -1234,8 +1254,8 @@ Local<Promise::Resolver> MakeSQLiteAsyncWork(
   }
 
   auto* work = new SQLiteAsyncTask<T>(env, db, resolver, task, after);
-  work->ScheduleWork();
   db->AddAsyncTask(work);
+  db->ScheduleAsyncTask(work);
   return resolver;
 }
 
@@ -2609,6 +2629,8 @@ MaybeLocal<Promise::Resolver> StatementAsyncExecutionHelper::Get(
   Database* db = stmt->db_.get();
   auto task = [stmt]() -> std::tuple<int, int> {
     int r = sqlite3_step(stmt->statement_);
+    std::cout << "In Get Task\n";
+    std::cout << "Step result: " << r << "\n";
     if (r != SQLITE_ROW && r != SQLITE_DONE) {
       return std::make_tuple(r, 0);
     }
@@ -2619,6 +2641,10 @@ MaybeLocal<Promise::Resolver> StatementAsyncExecutionHelper::Get(
                                Local<Promise::Resolver> resolver) {
     Isolate* isolate = env->isolate();
     auto [r, num_cols] = task_result;
+
+    std::cout << "After Get\n";
+    std::cout << "r: " << r << ", num_cols: " << num_cols << "\n";
+
     if (r == SQLITE_DONE) {
       resolver->Resolve(env->context(), Undefined(isolate)).FromJust();
       return;
