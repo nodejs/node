fb02de73deaa1b914a1acb7505b3593a31ec2d95
DeveloperViraj <workwithvirajjadhav@gmail.com>
2025-12-01 15:55:34 +0530

    test_runner: revert EnableOptions typedef change and update AbortSignal.timeout test

--- test/parallel/test-mock-timers-abortsignal-timeout.js
+++ test/parallel/test-mock-timers-abortsignal-timeout.js
@@ -1,26 +1,23 @@
 'use strict';
 
-// eslint-disable-next-line no-unused-vars
-const common = require('../common');
+require('../common');
 const assert = require('assert');
-const { MockTimers } = require('internal/test_runner/mock/mock_timers');
-const { AbortSignal } = require('internal/abort_controller');
+const { mock } = require('node:test');
 
 {
-  const mock = new MockTimers();
-  mock.enable({ apis: ['AbortSignal.timeout'] });
+  mock.timers.enable({ apis: ['AbortSignal.timeout'] });
 
   try {
     const signal = AbortSignal.timeout(50);
 
     assert.strictEqual(signal.aborted, false);
 
-    mock.tick(49);
+    mock.timers.tick(49);
     assert.strictEqual(signal.aborted, false);
 
-    mock.tick(1);
+    mock.timers.tick(1);
     assert.strictEqual(signal.aborted, true);
   } finally {
-    mock.reset();
+    mock.timers.reset();
   }
 }
