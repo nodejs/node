21b84fcf7bb4a772130b6e3c74cc8391ecd71058
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-12-03 14:17:37 +0100

    tools: enforce trailing commas in `test/sequential`
    PR-URL: https://github.com/nodejs/node/pull/60892
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jake Yuesong Li <jake.yuesong@gmail.com>

--- test/eslint.config_partial.mjs
+++ test/eslint.config_partial.mjs
@@ -147,7 +147,6 @@ export default [
     files: [
       'test/es-module/**/*.{js,mjs}',
       'test/parallel/**/*.{js,mjs}',
-      'test/sequential/**/*.{js,mjs}',
     ],
     rules: {
       '@stylistic/js/comma-dangle': [
@@ -158,6 +157,23 @@ export default [
           functions: 'only-multiline',
           imports: 'always-multiline',
           objects: 'only-multiline',
+        },
+      ],
+    },
+  },
+  {
+    files: [
+      'test/sequential/**/*.{js,mjs}',
+    ],
+    rules: {
+      '@stylistic/js/comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'only-multiline',
+          imports: 'always-multiline',
+          objects: 'always-multiline',
         },
       ],
     },
