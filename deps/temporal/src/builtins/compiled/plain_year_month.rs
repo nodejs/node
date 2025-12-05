f2692c55349fbdd8a0080b6e3e716c381c3d8a42
Chengzhong Wu <cwu631@bloomberg.net>
2025-12-05 18:21:50 +0000

    deps: remove independent temporal
    Remove independent `temporal_rs` dependency and use the cargo vendored
    one.
    
    PR-URL: https://github.com/nodejs/node/pull/61072
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>

--- deps/temporal/src/builtins/compiled/plain_year_month.rs
+++
@@ -1,9 +0,0 @@
-use crate::{
-    builtins::TZ_PROVIDER, unix_time::EpochNanoseconds, PlainYearMonth, TemporalResult, TimeZone,
-};
-
-impl PlainYearMonth {
-    pub fn epoch_ns_for(&self, time_zone: TimeZone) -> TemporalResult<EpochNanoseconds> {
-        self.epoch_ns_for_with_provider(time_zone, &*TZ_PROVIDER)
-    }
-}
--- deps/temporal/tools/tzif-inspect/README.md
+++
@@ -1,9 +0,0 @@
-# tzif-inspect
-
-Simple utility tool to dump the data for a given tzdb entry
-
-To run:
-
-```sh
-cargo run -p tzif-inspect -- America/Los_Angeles
-```
