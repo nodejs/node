bd15577c018750cf03b29cf8caf3f35ed3f0d168
Chengzhong Wu <cwu631@bloomberg.net>
2025-12-05 18:21:50 +0000

    deps: remove independent temporal
    Remove independent `temporal_rs` dependency and use the cargo vendored
    one.

--- deps/temporal/provider/src/error.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-#[non_exhaustive]
-pub enum TimeZoneProviderError {
-    InstantOutOfRange,
-    Assert(&'static str),
-    Range(&'static str),
-}
--- deps/temporal/src/provider.rs
+++
@@ -1,9 +0,0 @@
-//! The `TimeZoneProvider` trait.
-
-pub use timezone_provider::provider::{
-    CandidateEpochNanoseconds, EpochNanosecondsAndOffset, GapEntryOffsets, NeverProvider,
-    ParseDirectionError, TimeZoneId, TimeZoneProvider, TransitionDirection, UtcOffsetSeconds,
-};
-
-#[cfg(feature = "compiled_data")]
-pub use crate::builtins::TZ_PROVIDER as COMPILED_TZ_PROVIDER;
