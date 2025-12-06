d6a9d6a9c7d84adb8bd720ac419a74a3eda6f0c5
Mert Can Altin <mertgold60@gmail.com>
2025-12-06 23:07:44 +0300

    fix: add missing commas in spawnSync options for consistency

--- test/es-module/test-require-module-transpiled.js
+++ test/es-module/test-require-module-transpiled.js
@@ -9,7 +9,7 @@ const result = spawnSync(process.execPath, [
   '--experimental-require-module',
   fixtures.path('es-modules', 'transpiled-cjs-require-module', 'dist', 'import-both.cjs'),
 ], {
-  encoding: 'utf8'
+  encoding: 'utf8',
 });
 if (!result.stdout.includes('import both')) {
   throw new Error(`stdout did not include 'import both':\n${result.stdout}`);
@@ -19,7 +19,7 @@ const resultNamed = spawnSync(process.execPath, [
   '--experimental-require-module',
   fixtures.path('es-modules', 'transpiled-cjs-require-module', 'dist', 'import-named.cjs'),
 ], {
-  encoding: 'utf8'
+  encoding: 'utf8',
 });
 if (!resultNamed.stdout.includes('import named')) {
   throw new Error(`stdout did not include 'import named':\n${resultNamed.stdout}`);
@@ -29,7 +29,7 @@ const resultDefault = spawnSync(process.execPath, [
   '--experimental-require-module',
   fixtures.path('es-modules', 'transpiled-cjs-require-module', 'dist', 'import-default.cjs'),
 ], {
-  encoding: 'utf8'
+  encoding: 'utf8',
 });
 if (!resultDefault.stdout.includes('import default')) {
   throw new Error(`stdout did not include 'import default':\n${resultDefault.stdout}`);
