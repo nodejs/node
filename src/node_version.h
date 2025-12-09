ec57c0e8ca7b7869f9e15345ad25133d9f9e4f0d
Michaël Zasso <targos@protonmail.com>
2025-12-09 09:14:15 +0100

    src: update NODE_MODULE_VERSION to 145
    Major V8 updates are usually API/ABI incompatible with previous
    versions. This commit adapts NODE_MODULE_VERSION for V8 14.4.
    
    Refs: https://github.com/nodejs/CTC/blob/master/meetings/2016-09-28.md

--- src/node_version.h
+++ src/node_version.h
@@ -95,7 +95,7 @@
 #if defined(NODE_EMBEDDER_MODULE_VERSION)
 #define NODE_MODULE_VERSION NODE_EMBEDDER_MODULE_VERSION
 #else
-#define NODE_MODULE_VERSION 144
+#define NODE_MODULE_VERSION 145
 #endif
 
 // The NAPI_VERSION supported by the runtime. This is the inclusive range of
