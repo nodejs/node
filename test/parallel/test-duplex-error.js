45bc784214cc4ce737527419049108ceec16f99f
aelhor <aelhor90@gmail.com>
2025-12-18 21:11:09 +0200

    lint: fix style issues in duplexpair and its tests

--- test/parallel/test-duplex-error.js
+++ test/parallel/test-duplex-error.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const common = require('../common');
 const assert = require('assert');
 const { duplexPair } = require('stream');
 
@@ -8,32 +9,22 @@ const [sideA, sideB] = duplexPair();
 let sideAErrorReceived = false;
 let sideBErrorReceived = false;
 
-// Add error handlers
-sideA.on('error', (err) => {
+// Use common.mustCall inside the listeners to ensure they trigger
+sideA.on('error', common.mustCall((err) => {
   sideAErrorReceived = true;
-});
-sideB.on('error', (err) => {
-  sideBErrorReceived = true;
-});
+}));
 
-// Ensure the streams are flowing
+sideB.on('error', common.mustCall((err) => {
+  sideBErrorReceived = true;
+}));
+
 sideA.resume();
 sideB.resume();
 
-// Destroy sideB with an error
 sideB.destroy(new Error('Simulated error'));
 
-// Wait for event loop to process error events
-setImmediate(() => {
-  assert.strictEqual(
-    sideAErrorReceived,
-    true,
-    'sideA should receive the error when sideB is destroyed with an error'
-  );
-  assert.strictEqual(
-    sideBErrorReceived,
-    true,
-    'sideB should emit its own error when destroyed'
-  );
-});
-
+// Wrap the callback in common.mustCall()
+setImmediate(common.mustCall(() => {
+  assert.strictEqual(sideAErrorReceived, true);
+  assert.strictEqual(sideBErrorReceived, true);
+}));
