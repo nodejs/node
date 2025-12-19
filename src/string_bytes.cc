aee5408dec4f887dedb029a9650f7b5b888beba3
Nikita Skovoroda <chalkerx@gmail.com>
2025-12-20 03:27:28 +0400

    src: improve StringBytes::Encode perf on UTF8

--- src/string_bytes.cc
+++ src/string_bytes.cc
@@ -539,6 +539,16 @@ MaybeLocal<Value> StringBytes::Encode(Isolate* isolate,
         return ExternOneByteString::NewFromCopy(isolate, buf, buflen);
       }
 
+      if (simdutf::validate_utf8(buf, buflen)) {
+        // We know that we are non-ASCII (and are unlikely Latin1), use 2-byte
+        // In the most likely case of valid UTF-8, we can use this fast impl
+        size_t u16size = simdutf::utf16_length_from_utf8(buf, buflen);
+        uint16_t* dst = node::UncheckedMalloc<uint16_t>(u16size);
+        size_t utf16len = simdutf::convert_valid_utf8_to_utf16(
+              buf, buflen, reinterpret_cast<char16_t*>(dst));
+        return ExternTwoByteString::New(isolate, dst, utf16len);
+      }
+
       val =
           String::NewFromUtf8(isolate, buf, v8::NewStringType::kNormal, buflen);
       Local<String> str;
