d2c8dd29ccad1d7b77579f096e4bf4be4403e0ea
Stefan Stojanovic <StefanStojanovic@users.noreply.github.com>
2025-12-24 10:53:23 +0100

    tools,doc: fix format-md files list
    Adds content of doc/contributing/maintaining to the list
    
    Fixes: https://github.com/nodejs/node/issues/55216
    PR-URL: https://github.com/nodejs/node/pull/61147
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Aviv Keller <me@aviv.sh>

--- vcbuild.bat
+++ vcbuild.bat
@@ -781,6 +781,11 @@ for /D %%D IN (doc\*) do (
   for %%F IN (%%D\*.md) do (
     set "lint_md_files="%%F" !lint_md_files!"
   )
+  for /D %%S IN (%%D\*) do (
+    for %%F IN (%%S\*.md) do (
+      set "lint_md_files="%%F" !lint_md_files!"
+    )
+  )
 )
 %node_exe% tools\lint-md\lint-md.mjs %lint_md_files%
 ENDLOCAL
@@ -795,6 +800,11 @@ set lint_md_files=
 for /D %%D IN (doc\*) do (
   for %%F IN (%%D\*.md) do (
     set "lint_md_files="%%F" !lint_md_files!"
+  )
+  for /D %%S IN (%%D\*) do (
+    for %%F IN (%%S\*.md) do (
+      set "lint_md_files="%%F" !lint_md_files!"
+    )
   )
 )
 %node_exe% tools\lint-md\lint-md.mjs --format %lint_md_files%
