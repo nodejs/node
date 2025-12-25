d552261ba4c74d2c79e7f7a98a73f22e7c8dfb02
Guilherme Araújo <arauujogui@gmail.com>
2025-12-25 13:10:53 -0300

    buffer: format cpp

--- src/node_buffer.cc
+++ src/node_buffer.cc
@@ -1484,9 +1484,8 @@ void StaticCopy(const FunctionCallbackInfo<Value>& args) {
   }
 
   if (source_start > source_byte_length || source_end > source_byte_length) {
-    return THROW_ERR_OUT_OF_RANGE(
-      env,
-      "sourceStart or sourceEnd is out of bounds");
+    return THROW_ERR_OUT_OF_RANGE(env,
+                                  "sourceStart or sourceEnd is out of bounds");
   }
 
   if (source_start >= source_end) {
