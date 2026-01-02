61f14431031ad3e47da92a8abbd416331460772d
aelhor <aelhor90@gmail.com>
2026-01-02 20:29:45 +0200

    stream: propagate destruction in duplexPair
    Ensure destroying one side of a duplexPair triggers destruction of the
    other side via process.nextTick().
    
    To avoid a breaking change and unhandled 'error' events (e.g., in HTTP
    tests), the error object is not propagated; only the destruction signal
    is sent.

--- test/parallel/test-duplex-error.js
+++ test/parallel/test-duplex-error.js
@@ -6,15 +6,27 @@ const { duplexPair } = require('stream');
 
 const [sideA, sideB] = duplexPair();
 
+// Side A should receive the error because we called .destroy(err) on it.
 sideA.on('error', common.mustCall((err) => {
   assert.strictEqual(err.message, 'Simulated error');
 }));
 
-sideB.on('error', common.mustCall((err) => {
-  assert.strictEqual(err.message, 'Simulated error');
+// Side B should NOT necessarily emit an error (to avoid crashing
+// existing code), but it MUST be destroyed.
+sideB.on('error', common.mustNotCall('Side B should not emit an error event'));
+
+sideB.on('close', common.mustCall(() => {
+  assert.strictEqual(sideB.destroyed, true);
 }));
 
 sideA.resume();
 sideB.resume();
 
-sideB.destroy(new Error('Simulated error'));
+// Trigger the destruction
+sideA.destroy(new Error('Simulated error'));
+
+// Check the state in the next tick to allow nextTick/microtasks to run
+setImmediate(common.mustCall(() => {
+  assert.strictEqual(sideA.destroyed, true);
+  assert.strictEqual(sideB.destroyed, true);
+}));
