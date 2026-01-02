d8fdffda65c5a0c0b09ac5dca98722d543a015b3
Milad Fa <mfarazma@ibm.com>
2026-01-02 13:27:37 +0000

    deps: V8: cherry-pick d58ce5ad2ee2
    Original commit message:
    
        Fix builds with no `__builtin_bswap64` support
    
        Currently getting `'kBitsPerByte' was not declared in this scope`.
    
        Change-Id: Ied391ac9e859f0f3548d88fd1cc57f490d99e39a
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7310534
        Reviewed-by: Clemens Backes <clemensb@chromium.org>
        Commit-Queue: Milad Farazmand <mfarazma@ibm.com>
        Cr-Commit-Position: refs/heads/main@{#104481}
    
    Refs: https://github.com/v8/v8/commit/d58ce5ad2ee2680f811414220cff18e024d152a5

--- deps/v8/src/base/bits.h
+++ deps/v8/src/base/bits.h
@@ -504,15 +504,13 @@ inline constexpr uint64_t ByteReverse64(uint64_t value) {
 #if V8_HAS_BUILTIN_BSWAP64
   return __builtin_bswap64(value);
 #else
-  size_t bits_of_v = sizeof(value) * kBitsPerByte;
-  return value << (bits_of_v - 8) |
-         ((value << (bits_of_v - 24)) & 0x00FF000000000000) |
-         ((value << (bits_of_v - 40)) & 0x0000FF0000000000) |
-         ((value << (bits_of_v - 56)) & 0x000000FF00000000) |
-         ((value >> (bits_of_v - 56)) & 0x00000000FF000000) |
-         ((value >> (bits_of_v - 40)) & 0x0000000000FF0000) |
-         ((value >> (bits_of_v - 24)) & 0x000000000000FF00) |
-         ((value >> (bits_of_v - 8)) & 0x00000000000000FF);
+  return value << 56 | ((value << 40) & 0x00FF000000000000) |
+         ((value << 24) & 0x0000FF0000000000) |
+         ((value << 8) & 0x000000FF00000000) |
+         ((value >> 8) & 0x00000000FF000000) |
+         ((value >> 24) & 0x0000000000FF0000) |
+         ((value >> 40) & 0x000000000000FF00) |
+         ((value >> 56) & 0x00000000000000FF);
 #endif
 }
 
