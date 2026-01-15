538aebfa5d7b28c9f569c0ea52fca10d15c8aa3c
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2026-01-15 22:55:01 +0100

    test_runner: support expecting a test-case to fail
    Co-Authored-By: Alejandro Espa <98526766+vespa7@users.noreply.github.com>
    PR-URL: https://github.com/nodejs/node/pull/60669
    Reviewed-By: Aviv Keller <me@aviv.sh>
    Reviewed-By: Pietro Marchini <pietro.marchini94@gmail.com>
    Reviewed-By: Moshe Atlow <moshe@atlow.co.il>
    Reviewed-By: Jordan Harband <ljharb@gmail.com>

--- test/fixtures/test-runner/output/output.js
+++ test/fixtures/test-runner/output/output.js
@@ -5,6 +5,30 @@ const assert = require('node:assert');
 const test = require('node:test');
 const util = require('util');
 
+test.expectFailure('sync expect fail (method)', () => {
+  throw new Error('should pass');
+});
+
+test('sync expect fail (options)', { expectFailure: true }, () => {
+  throw new Error('should pass');
+});
+
+test.expectFailure('async expect fail (method)', async () => {
+  throw new Error('should pass');
+});
+
+test('async expect fail (options)', { expectFailure: true }, async () => {
+  throw new Error('should pass');
+});
+
+test.todo('sync todo with expect fail', { expectFailure: true }, () => {
+  throw new Error('should not count as an expected failure');
+});
+
+test.skip('sync skip expect fail', { expectFailure: true }, () => {
+  throw new Error('should not fail');
+});
+
 test('sync pass todo', (t) => {
   t.todo();
 });
