d2586b7e4cbe9473fa265015925d28ad77f6cf45
Ryuhei Shima <65934663+islandryu@users.noreply.github.com>
2026-01-19 10:16:25 +0900

    child_process: treat ipc length header as unsigned uint32
    Fixes: https://github.com/nodejs/node/issues/61312
    PR-URL: https://github.com/nodejs/node/pull/61344
    Reviewed-By: Aviv Keller <me@aviv.sh>

---
+++ test/parallel/test-child-process-fork-advanced-header-serialization.js
@@ -0,0 +1,42 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const { fork } = require('child_process');
+const fs = require('fs');
+
+if (process.argv[2] === 'child-buffer') {
+  const v = process.argv[3];
+  const payload = Buffer.from([
+    (v >> 24) & 0xFF,
+    (v >> 16) & 0xFF,
+    (v >> 8) & 0xFF,
+    v & 0xFF,
+  ]);
+  const fd = process.channel?.fd;
+  if (fd === undefined) {
+    // skip test
+    process.exit(0);
+  }
+  fs.writeSync(fd, payload);
+  return;
+}
+
+const testCases = [
+  0x00000001,
+  0x7fffffff,
+  0x80000000,
+  0x80000001,
+  0xffffffff,
+];
+
+for (const size of testCases) {
+  const child = fork(__filename, ['child-buffer', size], {
+    serialization: 'advanced',
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+  });
+
+  child.on('exit', common.mustCall((code, signal) => {
+    assert.strictEqual(code, 0);
+    assert.strictEqual(signal, null);
+  }));
+}
