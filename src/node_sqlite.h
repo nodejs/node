4cc029d2ac3dffad2f6758b26e70d779d7dc2bc1
Mert Can Altin <mertgold60@gmail.com>
2026-01-22 10:09:16 +0300

    Update node_sqlite.h
    Co-authored-by: Bart Louwers <bart.louwers@gmail.com>

--- src/node_sqlite.h
+++ src/node_sqlite.h
@@ -110,7 +110,7 @@ class DatabaseOpenConfiguration {
   inline bool get_enable_defensive() const { return defensive_; }
 
   inline void set_initial_limit(int sqlite_limit_id, int value) {
-    initial_limits_[sqlite_limit_id] = value;
+    initial_limits_.at(sqlite_limit_id) = value;
   }
 
   inline const std::array<std::optional<int>, SQLITE_LIMIT_TRIGGER_DEPTH + 1>&
