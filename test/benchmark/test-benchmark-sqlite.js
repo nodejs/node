bb8982b6e303eb6ffa0199425c091b3537cd6f51
Bruno Rodrigues <swe@brunocroh.com>
2026-01-27 20:33:20 +0000

    feat: check if SQLite is available before its test benchmark

--- test/benchmark/test-benchmark-sqlite.js
+++ test/benchmark/test-benchmark-sqlite.js
@@ -1,6 +1,8 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
+if (!common.hasSQLite)
+  common.skip('missing SQLite');
 
 const runBenchmark = require('../common/benchmark');
 
