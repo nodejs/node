09ab6af339162cf416b829e09ab27f2534ee08ba
Edy Silva <edigleyssonsilva@gmail.com>
2026-01-28 23:53:06 -0300

    test_runner: fix test enqueue when test file has syntax error

---
+++ test/parallel/test-runner-enqueue-file-syntax-error.js
@@ -0,0 +1,22 @@
+// Flags: --no-warnings
+'use strict';
+const common = require('../common');
+const assert = require('node:assert');
+const { run } = require('node:test');
+const fixtures = require('../common/fixtures');
+
+const testFile = fixtures.path('test-runner', 'syntax-error-test.mjs');
+const stream = run({
+  files: [testFile],
+  isolation: 'none',
+});
+
+stream.on('test:enqueue', common.mustCall((data) => {
+  assert.ok(data.file, 'test:enqueue event should have file field');
+  assert.strictEqual(data.file, testFile);
+}));
+
+stream.on('test:fail', common.mustCall((data) => {
+  assert.ok(data.details.error);
+  assert.ok(/SyntaxError/.test(data.details.error.name));
+}));
