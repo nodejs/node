3017a5402fd524314928d76a7ea3d31f4bb05d88
Amol Yadav <amyssnipet@yahoo.com>
2026-02-02 03:23:31 -0800

    net: add `setTOS` and `getTOS` to `Socket`
    PR-URL: https://github.com/nodejs/node/pull/61503
    Fixes: https://github.com/nodejs/node/issues/61489
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/tcp_wrap.h
+++ src/tcp_wrap.h
@@ -74,6 +74,8 @@ class TCPWrap : public ConnectionWrap<TCPWrap, uv_tcp_t> {
   static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetNoDelay(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetKeepAlive(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void SetTypeOfService(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void GetTypeOfService(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Bind(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Bind6(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void Listen(const v8::FunctionCallbackInfo<v8::Value>& args);
