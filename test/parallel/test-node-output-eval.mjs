c2dc96910a8f95190d0814abe8fc0751afbe9911
Chengzhong Wu <cwu631@bloomberg.net>
2026-02-03 16:49:53 -0500

    test: unify assertSnapshot stacktrace transform
    The snapshotted stack frames should hide node internal stack frames so
    that general node core development does not need updating the snapshot.
    
    For userland stack frames, they are highly fixture related and any
    fixture change should reflect in a change of the snapshot. Additionally,
    the line and column number are highly relevant to the correctness of the
    snapshot, these should not be redacted. A change in node core that
    affects userland stack frames should be alarming and be reflected in the
    snapshots.
    
    Features like test runner and source map support both should snapshot
    userland stack frames to ensure that userland code locations are
    computed correctly.

--- test/parallel/test-node-output-eval.mjs
+++ test/parallel/test-node-output-eval.mjs
@@ -7,22 +7,6 @@ import * as snapshot from '../common/assertSnapshot.js';
 import { describe, it } from 'node:test';
 
 describe('eval output', { concurrency: true }, () => {
-  function normalize(str) {
-    return str
-      .replaceAll(/\d+:\d+/g, '*:*');
-  }
-
-  const defaultTransform = snapshot.transform(
-    normalize,
-    snapshot.basicTransform,
-    snapshot.transformProjectRoot(),
-    removeStackTraces,
-  );
-
-  function removeStackTraces(output) {
-    return output.replaceAll(/^ *at .+$/gm, '');
-  }
-
   const tests = [
     { name: 'eval/eval_messages.js' },
     { name: 'eval/stdin_messages.js' },
@@ -32,7 +16,7 @@ describe('eval output', { concurrency: true }, () => {
 
   for (const { name } of tests) {
     it(name, async () => {
-      await snapshot.spawnAndAssert(fixtures.path(name), defaultTransform);
+      await snapshot.spawnAndAssert(fixtures.path(name), snapshot.defaultTransform);
     });
   }
 });
