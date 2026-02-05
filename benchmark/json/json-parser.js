d4f4bd547de9626fe0be99bb9d00354dc45c1019
Guilherme Araújo <arauujogui@gmail.com>
2026-02-05 16:16:09 -0300

    benchmark: recreate benchmarks

--- benchmark/json/json-parser.js
+++ benchmark/json/json-parser.js
@@ -1,21 +1,22 @@
 'use strict';
 const { readFile } = require('node:fs/promises');
 const common = require('../common.js');
-const { parse, parseFromBuffer } = require('node:json');
+const { parse } = require('node:json');
 const path = require('node:path');
 
 const configs = {
   n: [1024],
-  encoding: ['utf-8', 'buffer'],
+  parser: ['native', 'node'],
+  input: ['simple-array', 'simple-object', 'nested-ascii', 'nested-unicode', 'twitter'],
 };
 
 const bench = common.createBenchmark(main, configs);
 
 async function main(conf) {
-  const fn = conf.encoding === 'buffer' ? parseFromBuffer : parse;
+  const fn = conf.parser === 'native' ? JSON.parse : parse;
 
-  const enconding = conf.encoding === 'buffer' ? undefined : conf.encoding;
-  const json = await readFile(path.join(__dirname, 'twitter.json'), enconding);
+  const filename = `${conf.input}.json`;
+  const json = await readFile(path.join(__dirname, filename), 'utf-8');
 
   bench.start();
 
