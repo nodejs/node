b17e5335b9c4707d843a473337cb82b162c5534c
Guilherme Araújo <arauujogui@gmail.com>
2026-02-05 13:22:19 -0300

    benchmark: add json buffer config

--- benchmark/json/json-parser.js
+++ benchmark/json/json-parser.js
@@ -1,22 +1,26 @@
 'use strict';
 const { readFile } = require('node:fs/promises');
 const common = require('../common.js');
-const { parse } = require('node:json');
+const { parse, parseFromBuffer } = require('node:json');
 const path = require('node:path');
 
 const configs = {
   n: [1024],
+  encoding: ['utf-8', 'buffer'],
 };
 
 const bench = common.createBenchmark(main, configs);
 
 async function main(conf) {
+  const fn = conf.encoding === 'buffer' ? parseFromBuffer : parse;
+
+  const enconding = conf.encoding === 'buffer' ? undefined : conf.encoding;
+  const json = await readFile(path.join(__dirname, 'twitter.json'), enconding);
+
   bench.start();
 
-  const json = await readFile(path.join(__dirname, 'twitter.json'), 'utf-8');
-
   for (let i = 0; i < conf.n; i++) {
-    parse(json);
+    fn(json);
   }
 
   bench.end(conf.n);
