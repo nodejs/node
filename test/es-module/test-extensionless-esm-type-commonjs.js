d14e1baf52d79074aadab58348f97e0ed16e7f14
inoway46 <inoueyuya416@gmail.com>
2026-02-07 01:11:35 +0900

    test: stop using no-experimental-detect-module in extensionless type test

--- test/es-module/test-extensionless-esm-type-commonjs.js
+++ test/es-module/test-extensionless-esm-type-commonjs.js
@@ -3,23 +3,16 @@
 const { spawnSyncAndAssert } = require('../common/child_process');
 const fixtures = require('../common/fixtures');
 
-const commonjsDir = fixtures.path('es-modules', 'extensionless-esm-commonjs');
-
-spawnSyncAndAssert(process.execPath, ['--no-experimental-detect-module', './script'], {
-  cwd: commonjsDir,
-  encoding: 'utf8',
-}, {
+spawnSyncAndAssert(process.execPath, [
+  fixtures.path('es-modules', 'extensionless-esm-commonjs', 'script'),
+], {
   status: 1,
-  stderr: /.+/,
-  trim: true,
+  stderr: /SyntaxError: Cannot use import statement outside a module/,
 });
 
-const moduleDir = fixtures.path('es-modules', 'extensionless-esm-module');
-
-spawnSyncAndAssert(process.execPath, ['--no-experimental-detect-module', './script'], {
-  cwd: moduleDir,
-  encoding: 'utf8',
-}, {
+spawnSyncAndAssert(process.execPath, [
+  fixtures.path('es-modules', 'extensionless-esm-module', 'script'),
+], {
   stdout: /script STARTED[\s\S]*v\d+\./,
   trim: true,
 });
