a756a12cbf4768568a457dccce984b4f661693bd
Alex Schwartz <alexschwartz01@gmail.com>
2026-02-06 13:36:33 -0500

    use temp dir

--- test/sea/test-build-sea-custom-argv0.js
+++ test/sea/test-build-sea-custom-argv0.js
@@ -7,20 +7,31 @@ const { skipIfBuildSEAIsNotSupported } = require('../common/sea');
 
 skipIfBuildSEAIsNotSupported();
 
+const tmpdir = require('../common/tmpdir');
 const { resolve } = require('path');
 const fixtures = require('../common/fixtures');
-const { rmSync } = require('fs');
+const { copyFileSync } = require('fs');
 
 const { spawnSyncAndAssert } = require('../common/child_process');
+
 const fixturesDir = fixtures.path('sea', 'basic');
+const tempDir = tmpdir.path;
+
+copyFileSync(
+  resolve(fixturesDir, 'sea-config.json'),
+  resolve(tempDir, 'sea-config.json')
+);
+
+copyFileSync(
+  resolve(fixturesDir, 'sea.js'),
+  resolve(tempDir, 'sea.js')
+);
 
 spawnSyncAndAssert(
   process.execPath,
-  ['--build-sea', resolve(fixturesDir, 'sea-config.json')], {
-    cwd: fixturesDir,
+  ['--build-sea', resolve(tempDir, 'sea-config.json')], {
+    cwd: tempDir,
     argv0: 'argv0',
   }, {
     stdout: /Generated single executable/,
   });
-
-rmSync(resolve(fixturesDir, 'sea'));
