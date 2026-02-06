8b62eac43cda3686a1caea316717176bf2e63c66
Alex Schwartz <alexschwartz01@gmail.com>
2026-02-06 11:49:31 -0500

    Add test

---
+++ test/sea/test-build-sea-custom-argv0.js
@@ -0,0 +1,23 @@
+'use strict';
+// This tests --build-sea with a custom argv0 value.
+
+require('../common');
+
+const { skipIfBuildSEAIsNotSupported } = require('../common/sea');
+
+skipIfBuildSEAIsNotSupported();
+
+const { resolve } = require('path');
+const fixtures = require('../common/fixtures');
+
+const { spawnSyncAndAssert } = require('../common/child_process');
+const fixturesDir = fixtures.path('sea', 'basic');
+
+spawnSyncAndAssert(
+  process.execPath,
+  ['--build-sea', resolve(fixturesDir, 'sea-config.json')], {
+    cwd: fixturesDir,
+    argv0: 'argv0',
+  }, {
+    stdout: /Generated single executable/,
+  });
