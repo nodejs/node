bf4a28dbaafdec764b251293c35a89cad5961d16
Hiroki Osame <hiroki.osame@gmail.com>
2026-02-15 12:02:51 +0900

    fixup! util: add fast path to stripVTControlCharacters

--- benchmark/util/strip-vt-control-characters.js
+++ benchmark/util/strip-vt-control-characters.js
@@ -6,25 +6,24 @@ const { stripVTControlCharacters } = require('node:util');
 const assert = require('node:assert');
 
 const bench = common.createBenchmark(main, {
-  input: ['plain-short', 'plain-long', 'ansi-short', 'ansi-heavy'],
+  input: ['noAnsi-short', 'noAnsi-long', 'ansi-short', 'ansi-long'],
   n: [1e6],
 });
 
 function main({ input, n }) {
   let str;
   switch (input) {
-    case 'plain-short':
-      str = 'Hello, World!';
+    case 'noAnsi-short':
+      str = 'This is a plain text string without any ANSI codes';
       break;
-    case 'plain-long':
-      str = 'a'.repeat(1000);
+    case 'noAnsi-long':
+      str = 'Long plain text without ANSI. '.repeat(100);
       break;
     case 'ansi-short':
       str = '\u001B[31mHello\u001B[39m';
       break;
-    case 'ansi-heavy':
-      str = '\u001B[1m\u001B[31m\u001B[4m' + 'x'.repeat(100) +
-            '\u001B[24m\u001B[39m\u001B[22m';
+    case 'ansi-long':
+      str = ('\u001B[31m' + 'colored text '.repeat(10) + '\u001B[39m').repeat(10);
       break;
   }
 
