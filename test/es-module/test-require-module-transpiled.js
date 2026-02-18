6d44cbc7856e80eb34cddce2756872a9c19f4872
Mert Can Altin <mertgold60@gmail.com>
2026-02-18 22:57:34 +0300

    revert

--- test/es-module/test-require-module-transpiled.js
+++ test/es-module/test-require-module-transpiled.js
@@ -1,36 +1,30 @@
 'use strict';
 require('../common');
+const { spawnSyncAndAssert } = require('../common/child_process');
 const fixtures = require('../common/fixtures');
 
 // This is a minimum integration test for CJS transpiled from ESM that tries to load real ESM.
 
-const { spawnSync } = require('child_process');
-const result = spawnSync(process.execPath, [
+spawnSyncAndAssert(process.execPath, [
   '--experimental-require-module',
   fixtures.path('es-modules', 'transpiled-cjs-require-module', 'dist', 'import-both.cjs'),
 ], {
-  encoding: 'utf8',
+  trim: true,
+  stdout: 'import both',
 });
-if (!result.stdout.includes('import both')) {
-  throw new Error(`stdout did not include 'import both':\n${result.stdout}`);
-}
 
-const resultNamed = spawnSync(process.execPath, [
+spawnSyncAndAssert(process.execPath, [
   '--experimental-require-module',
   fixtures.path('es-modules', 'transpiled-cjs-require-module', 'dist', 'import-named.cjs'),
 ], {
-  encoding: 'utf8',
+  trim: true,
+  stdout: 'import named',
 });
-if (!resultNamed.stdout.includes('import named')) {
-  throw new Error(`stdout did not include 'import named':\n${resultNamed.stdout}`);
-}
 
-const resultDefault = spawnSync(process.execPath, [
+spawnSyncAndAssert(process.execPath, [
   '--experimental-require-module',
   fixtures.path('es-modules', 'transpiled-cjs-require-module', 'dist', 'import-default.cjs'),
 ], {
-  encoding: 'utf8',
+  trim: true,
+  stdout: 'import default',
 });
-if (!resultDefault.stdout.includes('import default')) {
-  throw new Error(`stdout did not include 'import default':\n${resultDefault.stdout}`);
-}
