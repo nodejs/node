25e32d0e4c2bba7a03df3506ae6802a0a3af19a8
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-02-21 19:11:37 +0900

    test: migrate webstreams compression buffer-source test to node:test

--- test/parallel/test-webstreams-compression-buffer-source.js
+++ test/parallel/test-webstreams-compression-buffer-source.js
@@ -1,6 +1,7 @@
 'use strict';
-const common = require('../common');
+require('../common');
 const assert = require('assert');
+const test = require('node:test');
 const { DecompressionStream, CompressionStream } = require('stream/web');
 
 // Minimal gzip-compressed bytes for "hello"
@@ -9,7 +10,7 @@ const compressedGzip = new Uint8Array([
   203, 72, 205, 201, 201, 7, 0, 134, 166, 16, 54, 5, 0, 0, 0,
 ]);
 
-async function testDecompressionAcceptsArrayBuffer() {
+test('DecompressionStream accepts ArrayBuffer chunks', async () => {
   const ds = new DecompressionStream('gzip');
   const reader = ds.readable.getReader();
   const writer = ds.writable.getWriter();
@@ -27,15 +28,13 @@ async function testDecompressionAcceptsArrayBuffer() {
   await writePromise;
   const out = Buffer.concat(chunks.map((c) => Buffer.from(c)));
   assert.strictEqual(out.toString(), 'hello');
-}
+});
 
-async function testCompressionRoundTripWithArrayBuffer() {
+test('CompressionStream round-trip with ArrayBuffer input', async () => {
   const cs = new CompressionStream('gzip');
   const ds = new DecompressionStream('gzip');
 
   const csWriter = cs.writable.getWriter();
-  const csReader = cs.readable.getReader();
-  const dsWriter = ds.writable.getWriter();
   const dsReader = ds.readable.getReader();
 
   const input = new TextEncoder().encode('hello').buffer;
@@ -46,7 +45,7 @@ async function testCompressionRoundTripWithArrayBuffer() {
   await cs.readable.pipeTo(ds.writable);
 
   const out = [];
-  done = false;
+  let done = false;
   while (!done) {
     const { value, done: d } = await dsReader.read();
     if (value) out.push(value);
@@ -54,9 +53,4 @@ async function testCompressionRoundTripWithArrayBuffer() {
   }
   const result = Buffer.concat(out.map((c) => Buffer.from(c)));
   assert.strictEqual(result.toString(), 'hello');
-}
-
-Promise.all([
-  testDecompressionAcceptsArrayBuffer(),
-  testCompressionRoundTripWithArrayBuffer(),
-]).then(common.mustCall());
+});
