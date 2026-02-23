db651e346babf1aa2b2a1aa5c9d2623ac60a16df
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-02-23 12:47:46 +0100

    fixup! tools: automatically remove `lts-watch-*` labels

--- tools/actions/remove-lts-watch-label.sh
+++ tools/actions/remove-lts-watch-label.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+[ -n "$GITHUB_SERVER_URL" ] || {
+  echo "Required environment variable GITHUB_SERVER_URL not found" >&2
+  exit 1
+}
 [ -n "$GITHUB_REPOSITORY_OWNER" ] || {
   echo "Required environment variable GITHUB_REPOSITORY_OWNER not found" >&2
   exit 1
@@ -40,8 +44,8 @@ fetch_label_id () {
   fi
 }
 
-jq -r '.commits[].message' < "$GITHUB_EVENT_PATH" \
-| while IFS= read -r msg; do
+jq --raw-output0 '.commits[].message' < "$GITHUB_EVENT_PATH" \
+| while IFS= read -r -d '' msg; do
   pr_url=$(echo "$msg" | git interpret-trailers --parse | awk -F': ' '$1 == "PR-URL" { print $2; exit }')
   pr_number=${pr_url##"$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/pull/"}
 
@@ -52,33 +56,30 @@ jq -r '.commits[].message' < "$GITHUB_EVENT_PATH" \
   esac
 
   # shellcheck disable=SC2016
-  pr_id="$(gh api graphql -f query='
-    query($owner:String!, $repo:String!, $number:Int!, $label:String!) {
+  pr_id="$(LTS_WATCH_LABEL="$label" gh api graphql -f query='
+    query($owner:String!, $repo:String!, $number:Int!) {
       repository(owner:$owner, name:$repo) {
         pullRequest(number:$number) {
           id
-          labels(first: 1, query: $label) {
-            nodes { name }
-          }
+          labels(first: 100) { nodes { name } }
         }
       }
-    }' -f owner="$owner" -f repo="$repo" -F number="$pr_number" -f label="$label" \
-    --jq ' if (.data.repository.pullRequest.labels.nodes | length) then .data.repository.pullRequest.id end')"
+    }' -f owner="$owner" -f repo="$repo" -F number="$pr_number" \
+    --jq 'if (.data.repository.pullRequest.labels.nodes | any(.name == env.LTS_WATCH_LABEL)) then .data.repository.pullRequest.id else null end')"
 
-  if [ -z "$pr_id" ] || [ "$pr_id" = "null" ]; then
-    echo "PR #$pr_number not found or does not have the label" >&2
-    continue
-  fi
+  # Skip PRs that do not have the label
+  [ -n "$pr_id" ] || continue
 
+  # Lazy-load the label ID if not done already
   [ -n "$label_id" ] || fetch_label_id
 
   # shellcheck disable=SC2016
   gh api graphql -f query='
-    mutation($labelableId:ID!, $labelIds:[ID!]!) {
-      removeLabelsFromLabelable(input:{labelableId:$labelableId, labelIds:$labelIds}) {
+    mutation($labelableId:ID!, $labelId:ID!) {
+      removeLabelsFromLabelable(input:{labelableId:$labelableId, labelIds:[$labelId]}) {
         clientMutationId
       }
-    }' -f labelableId="$pr_id" -f labelIds="[$label_id]" >/dev/null
+    }' -f labelableId="$pr_id" -f labelId="$label_id"
 
   # gentle throttle
   sleep 0.2
