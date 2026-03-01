363f9a9d18b5606451538bdba2635d15b192d78d
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-03-01 11:58:04 +0100

    test: skip `test-url` on `--shared-ada` builds
    PR-URL: https://github.com/nodejs/node/pull/62019
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Richard Lau <richard.lau@ibm.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>

--- test/wpt/test-url.js
+++ test/wpt/test-url.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const { skip } = require('../common');
+
+if (process.config.variables.node_shared_ada) {
+  skip('Different versions of Ada affect the WPT tests');
+}
+
 const { WPTRunner } = require('../common/wpt');
 
 const runner = new WPTRunner('url');
