3c78a769bee841095f73fc8d85c530955641963e
Yagiz Nizipli <yagiz@nizipli.com>
2026-03-09 08:57:02 -0400

    fixup! deps: update merve to 1.2.0

--- src/node_cjs_lexer.cc
+++ src/node_cjs_lexer.cc
@@ -71,14 +71,14 @@ void Parse(const FunctionCallbackInfo<Value>& args) {
   // Convert exports to JS Set
   Local<Set> exports_set = Set::New(isolate);
   for (const auto& exp : analysis.exports) {
-    exports_set->Add(context, CreateString(isolate, exp)).ToLocalChecked();
+    exports_set->Add(context, CreateString(isolate, exp.name)).ToLocalChecked();
   }
 
   // Convert reexports to JS array using batch creation
   LocalVector<Value> reexports_vec(isolate);
   reexports_vec.reserve(analysis.re_exports.size());
   for (const auto& reexp : analysis.re_exports) {
-    reexports_vec.push_back(CreateString(isolate, reexp));
+    reexports_vec.push_back(CreateString(isolate, reexp.name));
   }
 
   // Create result array [exports (Set), reexports (Array)]
