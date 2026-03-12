ae1f30e1424cd45502c51dd02e22f59ead904b63
simon-id <simon.id@protonmail.com>
2026-03-12 14:23:06 +0100

    fix tests

--- test/parallel/test-diagnostics-channel-meta-channels.js
+++ test/parallel/test-diagnostics-channel-meta-channels.js
@@ -8,28 +8,37 @@ const testedChannel = dc.channel('test');
 const testedSubscription = () => {};
 const testedData = { foo: 'bar' };
 
-// should publish on meta channel for subscribe() on both inactive and active prototype
-dc.subscribe('diagnostics_channel.subscribe', common.mustCall(({ channel, subscription }) => {
-  assert.strictEqual(channel, testedChannel);
-  assert.strictEqual(subscription, testedSubscription);
-}, 2)); // called twice
-testedChannel.subscribe(testedSubscription); // inactive prototype
-testedChannel.subscribe(testedSubscription); // active prototype
+{
+  // should publish on meta channel for subscribe() on both inactive and active prototype
+  const subscribeHandler = common.mustCall(({ channel, subscription }) => {
+    assert.strictEqual(channel, testedChannel);
+    assert.strictEqual(subscription, testedSubscription);
+  }, 2); // called twice
+  dc.subscribe('diagnostics_channel.subscribe', subscribeHandler);
+  testedChannel.subscribe(testedSubscription); // inactive prototype
+  testedChannel.subscribe(testedSubscription); // active prototype
+  dc.unsubscribe('diagnostics_channel.subscribe', subscribeHandler);
+}
 
-// should publish on meta channel for publish()
-dc.subscribe('diagnostics_channel.publish', common.mustCall(({ channel, data }) => {
-  assert.strictEqual(channel, testedChannel);
-  assert.strictEqual(data, testedData);
-}));
-testedChannel.publish(testedData);
+{
+  // should publish on meta channel for publish()
+  const publishHandler = common.mustCall(({ channel, data }) => {
+    assert.strictEqual(channel, testedChannel);
+    assert.strictEqual(data, testedData);
+  });
+  dc.subscribe('diagnostics_channel.publish', publishHandler);
+  testedChannel.publish(testedData);
+  dc.unsubscribe('diagnostics_channel.publish', publishHandler);
+}
 
-// should publish on meta channel for unsubscribe() on both inactive and active prototype
-dc.subscribe('diagnostics_channel.unsubscribe', common.mustCall(({ channel, subscription }) => {
-  assert.strictEqual(channel, testedChannel);
-  assert.strictEqual(subscription, testedSubscription);
-}, 2)); // called twice
-testedChannel.unsubscribe(testedSubscription); // active prototype
-testedChannel.unsubscribe(testedSubscription); // inactive prototype
-
-
+{
+  // should publish on meta channel for unsubscribe() on both inactive and active prototype
+  const unsubscribeHandler = common.mustCall(({ channel, subscription }) => {
+    assert.strictEqual(channel, testedChannel);
+    assert.strictEqual(subscription, testedSubscription);
+  }, 2); // called twice
+  dc.subscribe('diagnostics_channel.unsubscribe', unsubscribeHandler);
+  testedChannel.unsubscribe(testedSubscription); // active prototype
+  testedChannel.unsubscribe(testedSubscription); // inactive prototype
+}
 // TODO: should it publish on inactive channels ?
