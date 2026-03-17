56dfeb06df33c1f9e5361c725031e7a6d92cfc16
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-03-17 17:36:20 +0100

    tools: fix timeout errors in `lint-nix` job
    PR-URL: https://github.com/nodejs/node/pull/62265
    Fixes: https://github.com/nodejs/node/issues/62219
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: René <contact.9a5d6388@renegade334.me.uk>

--- tools/nix/pkgs.nix
+++ tools/nix/pkgs.nix
@@ -8,3 +8,14 @@ let
   }) arg;
 in
 nixpkgs
+// {
+  nixfmt-tree = nixpkgs.nixfmt-tree.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      (nixpkgs.fetchpatch2 {
+        url = "https://github.com/numtide/treefmt/commit/b96016b4e38ffc76518087b3b0c9bbfa190d5225.patch?full_index=1";
+        revert = true;
+        hash = "sha256-DcxT2OGPX6Kmxhqa56DjZsSh2hoyhPyVmE17ULeryv8=";
+      })
+    ];
+  });
+}
