61fac1308697e12c20d31b22e9219cd40ca2a20e
Marco <marco.piraccini@gmail.com>
2026-03-17 17:02:55 +0100

    module: fix coverage of mocked CJS modules imported from ESM
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/62133
    Fixes: https://github.com/nodejs/node/issues/61709
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Zeyu "Alex" Yang <himself65@outlook.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Jacob Smith <jacob@frende.me>

---
+++ test/test-runner/test-output-coverage-with-mock-cjs.mjs
@@ -0,0 +1,22 @@
+import * as common from '../common/index.mjs';
+import * as fixtures from '../common/fixtures.mjs';
+import { spawnAndAssert, defaultTransform, ensureCwdIsProjectRoot } from '../common/assertSnapshot.js';
+
+if (!process.features.inspector) {
+  common.skip('inspector support required');
+}
+
+ensureCwdIsProjectRoot();
+await spawnAndAssert(
+  fixtures.path('test-runner/output/coverage-with-mock-cjs.mjs'),
+  defaultTransform,
+  {
+    flags: [
+      '--disable-warning=ExperimentalWarning',
+      '--test-reporter=tap',
+      '--experimental-test-module-mocks',
+      '--experimental-test-coverage',
+      '--test-coverage-exclude=!test/**',
+    ],
+  },
+);
