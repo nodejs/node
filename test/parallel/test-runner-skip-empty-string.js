58be01d6a8939741f41184a145831f85d68b4d65
Vaishnavi J K <upadyayavaishnavi10@gmail.com>
2026-03-20 17:34:05 +0530

    test: use common.mustCall() for skip empty string test

--- test/parallel/test-runner-skip-empty-string.js
+++ test/parallel/test-runner-skip-empty-string.js
@@ -1,15 +1,6 @@
 'use strict';
-require('../common');
 
+const common = require('../common');
 const { test } = require('node:test');
-const assert = require('assert');
 
-let ran = false;
-
-test('skip option empty string should not skip', { skip: '' }, () => {
-  ran = true;
-});
-
-process.on('exit', () => {
-  assert.ok(ran);
-});
+test('skip option empty string should not skip', { skip: '' }, common.mustCall());
