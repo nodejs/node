aa3a1e83ed664c3c408260d542346eec2c29d883
Guy Bedford <gbedford@cloudflare.com>
2026-03-23 16:26:13 -0700

    esm: fix source phase identity bug in loadCache eviction
    PR-URL: https://github.com/nodejs/node/pull/62415
    Reviewed-By: Yagiz Nizipli <yagiz@nizipli.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>

---
+++ test/fixtures/es-modules/test-wasm-source-phase-identity-parent.mjs
@@ -0,0 +1,6 @@
+import * as mod1 from './simple.wasm';
+import * as mod2 from './simple.wasm';
+import source mod3 from './simple.wasm';
+import source mod4 from './simple.wasm';
+
+export { mod1, mod2, mod3, mod4 };
