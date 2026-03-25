1730631eaa2ac0e3d245c32548d8567ac22dd2fa
Yuya Inoue <65857152+inoway46@users.noreply.github.com>
2026-03-26 06:22:41 +0900

    test: avoid flaky run wait in debugger restart test
    PR-URL: https://github.com/nodejs/node/pull/62112
    Refs: https://github.com/nodejs/node/issues/61762
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/parallel/test-debugger-run-after-quit-restart.js
+++ test/parallel/test-debugger-run-after-quit-restart.js
@@ -46,7 +46,7 @@ const path = require('path');
     .then(() => {
       assert.match(cli.output, /Use `run` to start the app again/);
     })
-    .then(() => cli.stepCommand('run'))
+    .then(() => cli.command('run'))
     .then(() => cli.waitForInitialBreak())
     .then(() => cli.waitForPrompt())
     .then(() => {
@@ -76,7 +76,7 @@ const path = require('path');
     .then(() => {
       assert.match(cli.output, /Use `run` to start the app again/);
     })
-    .then(() => cli.stepCommand('run'))
+    .then(() => cli.command('run'))
     .then(() => cli.waitForInitialBreak())
     .then(() => cli.waitForPrompt())
     .then(() => {
