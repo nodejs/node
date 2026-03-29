68bf4825fa2bbbd1104f2290043dc67388e40f1e
Robert Nagy <ronagy@icloud.com>
2026-03-29 12:26:42 +0200

    fixup

--- benchmark/buffers/buffer-copy.js
+++ benchmark/buffers/buffer-copy.js
@@ -4,12 +4,13 @@ const common = require('../common.js');
 const bench = common.createBenchmark(main, {
   bytes: [8, 128, 1024],
   partial: ['true', 'false'],
+  shared: ['true', 'false'],
   n: [6e6],
 });
 
-function main({ n, bytes, partial }) {
-  const source = Buffer.allocUnsafe(bytes);
-  const target = Buffer.allocUnsafe(bytes);
+function main({ n, bytes, partial, shared }) {
+  const source = shared === 'true' ? Buffer.from(new SharedArrayBuffer(bytes)) : Buffer.allocUnsafe(bytes);
+  const target = shared === 'true' ? Buffer.from(new SharedArrayBuffer(bytes)) : Buffer.allocUnsafe(bytes);
   const sourceStart = (partial === 'true' ? Math.floor(bytes / 2) : 0);
   bench.start();
   for (let i = 0; i < n; i++) {
