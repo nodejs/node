c1e61f14740ffb6ca1c1bb4dab6ccba2fe70dde2
haramjeong <04harams77@gmail.com>
2026-03-31 15:19:24 +0900

    tls: enhance test validation

--- test/parallel/test-tls-get-ca-certificates-x509-option.js
+++ test/parallel/test-tls-get-ca-certificates-x509-option.js
@@ -8,28 +8,41 @@ const assert = require('assert');
 const tls = require('tls');
 const { X509Certificate } = require('crypto');
 
+const expectedPems = tls.getCACertificates({ type: 'default', format: 'pem' });
+
 {
   const certs = tls.getCACertificates({ type: 'default', format: 'x509' });
-  assert.ok(Array.isArray(certs));
-  assert.ok(certs.length > 0);
-  for (const cert of certs) {
+  assert.strictEqual(certs.length, expectedPems.length);
+
+  for (let i = 0; i < certs.length; i++) {
+    const cert = certs[i];
+    const expected = new X509Certificate(expectedPems[i]);
+
     assert.ok(cert instanceof X509Certificate);
+    
+    assert.strictEqual(cert.fingerprint, expected.fingerprint);
+    assert.strictEqual(cert.serialNumber, expected.serialNumber);
+    assert.strictEqual(cert.subject, expected.subject);
+    assert.strictEqual(cert.raw.toString('hex'), expected.raw.toString('hex'));
   }
 }
 
 {
   const certs = tls.getCACertificates({ type: 'default', format: 'buffer' });
-  assert.ok(Array.isArray(certs));
-  assert.ok(certs.length > 0);
-  for (const cert of certs) {
+  assert.strictEqual(certs.length, expectedPems.length);
+
+  for (let i = 0; i < certs.length; i++) {
+    const cert = certs[i];
+    const expected = new X509Certificate(expectedPems[i]);
+
     assert.ok(Buffer.isBuffer(cert));
+    assert.strictEqual(cert.toString('hex'), expected.raw.toString('hex'));
   }
 }
 
 {
   const certs = tls.getCACertificates({ type: 'default' });
-  assert.ok(Array.isArray(certs));
-  assert.ok(certs.length > 0);
+  assert.strictEqual(certs.length, expectedPems.length);
   for (const cert of certs) {
     assert.strictEqual(typeof cert, 'string');
     assert.ok(cert.includes('-----BEGIN CERTIFICATE-----'));
@@ -61,6 +74,6 @@ const { X509Certificate } = require('crypto');
   }, {
     name: 'TypeError',
     code: 'ERR_INVALID_ARG_VALUE',
-    message: "The argument 'type' is invalid. Received 'invalid'"
+    message: /The argument 'type' is invalid/
   });
 }
