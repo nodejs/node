82428627a8bfa2808ab6f99c791f15781d841f39
池下 克彦 <kik@nvstl.com>
2026-03-31 14:04:13 +0900

    stream: reject TransformStream in addAbortSignal

--- test/parallel/test-stream-add-abort-signal.js
+++ test/parallel/test-stream-add-abort-signal.js
@@ -4,6 +4,7 @@
 require('../common');
 const assert = require('assert');
 const { addAbortSignal, Readable } = require('stream');
+const { TransformStream } = require('stream/web');
 const {
   addAbortSignalNoValidate,
 } = require('internal/streams/add-abort-signal');
@@ -16,6 +17,10 @@ const {
   const ac = new AbortController();
   assert.throws(() => {
     addAbortSignal(ac.signal, 'INVALID_STREAM');
+  }, /ERR_INVALID_ARG_TYPE/);
+
+  assert.throws(() => {
+    addAbortSignal(ac.signal, new TransformStream());
   }, /ERR_INVALID_ARG_TYPE/);
 }
 
