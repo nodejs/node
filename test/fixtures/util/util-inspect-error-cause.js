e00fa412f6970a1a21d03df1dbf47ba01313485f
Vansh Sharma <s.vansh1811@gmail.com>
2026-04-12 17:40:09 +0530

    test: add util-inspect-error-cause fixture

---
+++ test/fixtures/util/util-inspect-error-cause.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('../../common');
+
+const { inspect } = require('util');
+
+class FoobarError extends Error {
+  status = 'Feeling good';
+}
+
+const cause1 = new TypeError('Inner error');
+const cause2 = new FoobarError('Individual message', { cause: cause1 });
+cause2.extraProperties = 'Yes!';
+const cause3 = new Error('Stack causes', { cause: cause2 });
+
+const cause4 = new Error('Number error cause', { cause: 42 });
+const cause5 = new Error('Object cause', {
+  cause: {
+    message: 'Unique',
+    name: 'Error',
+    stack: 'Error: Unique\n' +
+      '    at Module._compile (node:internal/modules/cjs/loader:827:30)',
+  },
+});
+const cause6 = new Error('undefined cause', {
+  cause: undefined,
+});
+
+console.log(cause4);
+console.log(cause5);
+console.log(cause6);
+console.log(cause3);
+console.log(inspect(cause3, { compact: false, breakLength: Infinity }));
