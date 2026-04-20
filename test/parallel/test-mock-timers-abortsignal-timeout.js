9ffc42dd8217dcde071f3c757a5493dc17ddfd94
René <contact.9a5d6388@renegade334.me.uk>
2026-04-20 16:40:51 +0100

    Apply suggestions from code review
    Co-authored-by: René <contact.9a5d6388@renegade334.me.uk>

--- test/parallel/test-mock-timers-abortsignal-timeout.js
+++ test/parallel/test-mock-timers-abortsignal-timeout.js
@@ -4,20 +4,18 @@ require('../common');
 const assert = require('assert');
 const { mock } = require('node:test');
 
-{
-  mock.timers.enable({ apis: ['AbortSignal.timeout'] });
+const originalAbortSignalTimeout = AbortSignal.timeout;
 
-  try {
-    const signal = AbortSignal.timeout(50);
+mock.timers.enable({ apis: ['AbortSignal.timeout'] });
 
-    assert.strictEqual(signal.aborted, false);
+const signal = AbortSignal.timeout(50);
+assert.strictEqual(signal.aborted, false);
 
-    mock.timers.tick(49);
-    assert.strictEqual(signal.aborted, false);
+mock.timers.tick(49);
+assert.strictEqual(signal.aborted, false);
 
-    mock.timers.tick(1);
-    assert.strictEqual(signal.aborted, true);
-  } finally {
-    mock.timers.reset();
-  }
-}
+mock.timers.tick(1);
+assert.strictEqual(signal.aborted, true);
+
+mock.timers.reset();
+assert.strictEqual(AbortSignal.timeout, originalAbortSignalTimeout);
