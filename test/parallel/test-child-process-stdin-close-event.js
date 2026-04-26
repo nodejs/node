c0dcf42faa702fcee1d35af482f6f284c1c7652b
TseIan <tseianxwc@gmail.com>
2026-04-26 12:06:01 +0800

    child_process: add more unit test case

--- test/parallel/test-child-process-stdin-close-event.js
+++ test/parallel/test-child-process-stdin-close-event.js
@@ -8,20 +8,65 @@ if (common.isWindows) {
   common.skip('Not applicable on Windows');
 }
 
-const child = spawn(process.execPath, [
-  '-e',
-  'require("fs").closeSync(0); setTimeout(() => {}, 2000)',
-], { stdio: ['pipe', 'ignore', 'ignore'] });
+function spawnChild(script) {
+  return spawn(process.execPath, ['-e', script], {
+    stdio: ['pipe', 'ignore', 'ignore'],
+  });
+}
 
-const timeout = setTimeout(() => {
-  assert.fail('stdin close event was not emitted');
-}, 1000);
+function runTest({ script, setup }, done) {
+  const child = spawnChild(script);
+  const timeout = setTimeout(() => {
+    assert.fail('stdin close event was not emitted');
+  }, 2000);
 
-child.stdin.on('close', common.mustCall(() => {
-  clearTimeout(timeout);
-  child.kill();
-}));
+  let closed = false;
+  let exited = false;
+  function maybeDone() {
+    if (!closed || !exited) return;
+    clearTimeout(timeout);
+    done();
+  }
 
-child.on('exit', common.mustCall(() => {
-  clearTimeout(timeout);
+  child.stdin.once('close', common.mustCall(() => {
+    closed = true;
+    child.kill();
+    maybeDone();
+  }));
+
+  child.once('exit', common.mustCall(() => {
+    exited = true;
+    maybeDone();
+  }));
+
+  setup(child);
+}
+
+runTest({
+  script: 'setTimeout(() => require("fs").closeSync(0), 50); setTimeout(() => {}, 2000)',
+  setup: () => {},
+}, common.mustCall(() => {
+  runTest({
+    script: 'setTimeout(() => require("fs").closeSync(0), 200); setTimeout(() => {}, 2000)',
+    setup: (child) => {
+      const handle = child.stdin?._handle;
+      assert.strictEqual(typeof handle?.watchPeerClose, 'function');
+      handle.watchPeerClose(true, common.mustNotCall());
+    },
+  }, common.mustCall(() => {
+    runTest({
+      script: 'setTimeout(() => {}, 2000)',
+      setup: (child) => {
+        const handle = child.stdin?._handle;
+        assert.strictEqual(typeof handle?.watchPeerClose, 'function');
+
+        child.stdin.once('close', common.mustCall(() => {
+          assert.doesNotThrow(() => {
+            handle.watchPeerClose(true, common.mustNotCall());
+          });
+        }));
+        child.stdin.destroy();
+      },
+    }, common.mustCall(() => {}));
+  }));
 }));
