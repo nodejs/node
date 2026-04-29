655bda5464180807db17d7cff70d6eda34b64610
Matteo Collina <hello@matteocollina.com>
2026-04-29 13:14:54 +0200

    test: fix http2 shutdown lint regression

--- test/parallel/test-http2-server-shutdown-options-errors.js
+++ test/parallel/test-http2-server-shutdown-options-errors.js
@@ -58,7 +58,7 @@ server.listen(
   common.mustCall(() => {
     const client = http2.connect(`http://localhost:${server.address().port}`);
     const req = client.request();
-    req.on('error', common.mustCallAtLeast(() => {}, 0));
+    req.on('error', common.mustCallAtLeast());
     req.resume();
     req.on('close', common.mustCall(() => {
       client.close();
