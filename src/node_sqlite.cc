d129ed71dee34d6cd28fa9b4a01a45fe417f82a0
Michaël Zasso <targos@protonmail.com>
2026-04-30 08:19:48 +0200

    src: migrate from deprecated xxxV2 APIs
    - Set/GetPrototype
    - Holder
    
    Signed-Off-By: Michaël Zasso <targos@protonmail.com>

--- src/node_sqlite.cc
+++ src/node_sqlite.cc
@@ -873,7 +873,7 @@ Intercepted DatabaseSyncLimits::LimitsGetter(
   }
 
   DatabaseSyncLimits* limits;
-  ASSIGN_OR_RETURN_UNWRAP(&limits, info.HolderV2(), Intercepted::kNo);
+  ASSIGN_OR_RETURN_UNWRAP(&limits, info.Holder(), Intercepted::kNo);
 
   Environment* env = limits->env();
   Isolate* isolate = env->isolate();
@@ -906,7 +906,7 @@ Intercepted DatabaseSyncLimits::LimitsSetter(
   }
 
   DatabaseSyncLimits* limits;
-  ASSIGN_OR_RETURN_UNWRAP(&limits, info.HolderV2(), Intercepted::kNo);
+  ASSIGN_OR_RETURN_UNWRAP(&limits, info.Holder(), Intercepted::kNo);
 
   Environment* env = limits->env();
   Isolate* isolate = env->isolate();
@@ -3363,9 +3363,9 @@ BaseObjectPtr<StatementSyncIterator> StatementExecutionHelper::Iterate(
   }
 
   if (iter->object()
-          ->GetPrototypeV2()
+          ->GetPrototype()
           .As<Object>()
-          ->SetPrototypeV2(context, js_iterator_prototype)
+          ->SetPrototype(context, js_iterator_prototype)
           .IsNothing()) {
     return BaseObjectPtr<StatementSyncIterator>();
   }
