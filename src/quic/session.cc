0e13822516e0f3f4367464bc4c7299c630e7adc4
James M Snell <jasnell@gmail.com>
2026-05-07 10:21:10 -0700

    quic: remove unused binding variable in session.cc
    Signed-off-by: James M Snell <jasnell@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/63177
    Reviewed-By: Tim Perry <pimterry@gmail.com>
    Reviewed-By: Anna Henningsen <anna@addaleax.net>

--- src/quic/session.cc
+++ src/quic/session.cc
@@ -1358,7 +1358,6 @@ Session::Session(Endpoint* endpoint,
 
   MakeWeak();
   Debug(this, "Session created.");
-  auto& binding = BindingData::Get(env());
 
   JS_DEFINE_READONLY_PROPERTY(
       env(), object, env()->stats_string(), impl_->stats_.GetArrayBuffer());
