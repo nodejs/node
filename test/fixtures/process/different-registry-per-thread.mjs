b2737528b7948d712563e252845fde5685ce861f
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-05-09 14:51:12 +0200

    test: reduce flakiness of `different-registry-per-thread`
    There was an assumption that the `WeakRef` would be kept alive but
    it seems to not always be true on Windows. This commit makes sure
    it's kept alive.
    
    Signed-off-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/63244
    Reviewed-By: Richard Lau <richard.lau@ibm.com>
    Reviewed-By: Vinícius Lourenço Claro Cardoso <contact@viniciusl.com.br>
    Reviewed-By: Stefan Stojanovic <stefan.stojanovic@janeasystems.com>

--- test/fixtures/process/different-registry-per-thread.mjs
+++ test/fixtures/process/different-registry-per-thread.mjs
@@ -1,15 +1,26 @@
 import { isMainThread, Worker } from 'node:worker_threads';
 
+const registeredRefs = [];
+
+const ref = { foo: 'foo' };
+registeredRefs.push(ref);
+
 if (isMainThread) {
-  process.finalization.register({ foo: 'foo' }, () => {
+  process.finalization.register(ref, () => {
     process.stdout.write('shutdown on main thread\n');
   });
 
   const worker = new Worker(import.meta.filename);
 
+  worker.on('error', (err) => {
+    // Referencing `registeredRefs` here to avoid `ref` being GCed before the worker exits.
+    console.log(registeredRefs);
+    throw err;
+  });
+
   worker.postMessage('ping');
 } else {
-  process.finalization.register({ foo: 'bar' }, () => {
+  process.finalization.register(ref, () => {
     process.stdout.write('shutdown on worker\n');
   });
 }
