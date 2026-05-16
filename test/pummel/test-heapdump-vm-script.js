d5a8b7f37950f49abdee297cb235ef0b3eceaa2f
Joyee Cheung <joyeec9h3@gmail.com>
2026-05-16 02:08:30 +0200

    test: update test-heapdump-vm-script.js for new version of V8
    Signed-off-by: Joyee Cheung <joyeec9h3@gmail.com>

--- test/pummel/test-heapdump-vm-script.js
+++ test/pummel/test-heapdump-vm-script.js
@@ -7,7 +7,7 @@ const script = new vm.Script(source);
 const context = vm.createContext();
 
 validateByRetainingPath('Node / ContextifyScript', [
-  { node_name: '(shared function info)' },  // This is the UnboundScript referenced by ContextifyScript.
+  { node_name: 'system / SharedFunctionInfo' },  // This is the UnboundScript referenced by ContextifyScript.
   { edge_name: 'script' },
   { edge_name: 'source', node_type: 'string', node_name: source },
 ]);
