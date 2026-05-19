5658c631fab28530477d6953b4dd930ad510fdef
ikeyan <kik@nvstl.com>
2026-05-19 22:38:22 +0900

    stream: align `Readable.toWeb` termination with eos
    PR-URL: https://github.com/nodejs/node/pull/62394
    Backport-PR-URL: https://github.com/nodejs/node/pull/64675
    Reviewed-By: Mattias Buelens <mattias@buelens.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-stream-readable-to-web-termination.js
+++ test/parallel/test-stream-readable-to-web-termination.js
@@ -1,6 +1,8 @@
 'use strict';
-require('../common');
-const { Readable } = require('stream');
+const common = require('../common');
+const assert = require('assert');
+const { Duplex, Readable } = require('stream');
+const { setTimeout: delay } = require('timers/promises');
 
 {
   const r = Readable.from([]);
@@ -9,4 +11,34 @@ const { Readable } = require('stream');
 
   const reader = Readable.toWeb(r).getReader();
   reader.read();
+}
+
+{
+  const duplex = new Duplex({
+    read() {
+      this.push(Buffer.from('x'));
+      this.push(null);
+    },
+    write(_chunk, _encoding, callback) {
+      callback();
+    },
+  });
+
+  const reader = Readable.toWeb(duplex).getReader();
+
+  (async () => {
+    const result = await reader.read();
+    assert.deepStrictEqual(result, {
+      value: new Uint8Array(Buffer.from('x')),
+      done: false,
+    });
+
+    const closeResult = await Promise.race([
+      reader.read(),
+      delay(common.platformTimeout(100)).then(() => 'timeout'),
+    ]);
+
+    assert.notStrictEqual(closeResult, 'timeout');
+    assert.deepStrictEqual(closeResult, { value: undefined, done: true });
+  })().then(common.mustCall());
 }
