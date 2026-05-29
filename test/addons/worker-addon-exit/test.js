fcb0a4dc17fa6b91cccf2f5ea4b3f8c0081532d5
Anna Henningsen <anna@addaleax.net>
2026-05-29 20:38:18 +0200

    fixup! test: add regression test for using `ObjectWrap` in worker

--- test/addons/worker-addon-exit/test.js
+++ test/addons/worker-addon-exit/test.js
@@ -5,10 +5,6 @@ const { isMainThread, Worker } = require('worker_threads');
 if (!isMainThread) {
   const addon = require(`./build/${common.buildType}/binding`);
 
-  // Create some garbage
-  const arr = [];
-  for (let i = 0; i < 1e5; i++) arr.push(`${i}`.repeat(100));
-
   new addon.MyObject(10);
 
   // Should not segfault
