78fa8095adf52fce009ae080d58848a8d3912e53
waqar bin zafar <waqarniazi51@gmail.com>
2026-06-01 12:31:19 +0500

    test: avoid spawning repl import crash regression test
    Signed-off-by: waqar bin zafar <waqarniazi51@gmail.com>

--- test/parallel/test-repl-import-incomplete-crash.js
+++ test/parallel/test-repl-import-incomplete-crash.js
@@ -1,13 +1,10 @@
 'use strict';
-const common = require('../common');
-const child_process = require('child_process');
-const assert = require('assert');
+require('../common');
+const repl = require('repl');
 
 // Regression test for https://github.com/nodejs/node/issues/63551:
 // Typing a bare `import` keyword in the REPL must not crash the process.
-const proc = child_process.spawn(process.execPath, ['-i']);
-proc.on('error', common.mustNotCall());
-proc.on('exit', common.mustCall((code) => {
-  assert.strictEqual(code, 0);
-}));
-proc.stdin.write('import\n.exit\n');
+const replserver = new repl.REPLServer();
+
+replserver.emit('line', 'import');
+replserver.emit('line', '.exit');
