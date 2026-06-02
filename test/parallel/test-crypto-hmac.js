607055309a801606f79063d9a88be7a74bcb408a
theherrovn-sys <theherrovn-sys@github.com>
2026-06-02 19:30:57 +0700

    crypto: runtime-deprecate calling digest() on HMAC more than once
    Calling digest() on an already-finalized Hmac instance now emits a
    runtime deprecation warning with code DEP0206. Previously it silently
    returned an empty buffer, which is inconsistent with Hash behavior and
    could lead to security issues.
    
    Fixes: https://github.com/nodejs/node/issues/62838

--- test/parallel/test-crypto-hmac.js
+++ test/parallel/test-crypto-hmac.js
@@ -417,6 +417,12 @@ assert.strictEqual(
       '\u0010\u0041\u0052\u00c5\u00bf\u00dc\u00a0\u007b\u00c6\u0033' +
       '\u00ee\u00bd\u0046\u0019\u009f\u0002\u0055\u00c9\u00f4\u009d';
   {
+    common.expectWarning({
+      DeprecationWarning: [
+        ['Calling digest() on an already-finalized Hmac instance is deprecated.',
+         'DEP0206'],
+      ],
+    });
     const h = crypto.createHmac('sha1', 'key').update('data');
     assert.deepStrictEqual(h.digest('buffer'), Buffer.from(expected, 'latin1'));
     assert.deepStrictEqual(h.digest('buffer'), Buffer.from(''));
