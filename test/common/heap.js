nodejs/node

commit 6e60ab744e6a1371aa514cb5b6a5cedceb2a706b

src: annotate BaseObjects in the heap snapshots correctly
This fixes two issues in the BaseObject views in the heap snapshots:

1. BaseObjects are not conceptually roots when the environment and
   the realms are also showing up in the heap snapshot. Rather, they
   should be considered being held alive by the BaseObjectList in
   the realms, which are in turn held alive by Environment. The
   actual root from the containment view should be the Environment
   instead.
2. The concept of DOM detaching does not really apply to Node.js
   wrappers, and it's confusing to connect that with the weakness
   or detachment (native weakness) of BaseObjects. To avoid the
   confusion, just restore to the default detachedness for them.

PR-URL: https://github.com/nodejs/node/pull/57417
Reviewed-By: James M Snell <jasnell@gmail.com>
Reviewed-By: Chengzhong Wu <legendecas@gmail.com>

src/base_object.cc
@@ -161,10 +161,6 @@ Local<Object> BaseObject::WrappedObject() const {
   return object();
 }
 
-bool BaseObject::IsRootNode() const {
-  return !persistent_handle_.IsWeak();
-}
-
 bool BaseObject::IsNotIndicativeOfMemoryLeakAtExit() const {
   return IsWeakOrDetached();
 }

src/base_object-inl.h
@@ -125,11 +125,6 @@ bool BaseObject::IsWeakOrDetached() const {
   return pd->wants_weak_jsobj || pd->is_detached;
 }
 
-v8::EmbedderGraph::Node::Detachedness BaseObject::GetDetachedness() const {
-  return IsWeakOrDetached() ? v8::EmbedderGraph::Node::Detachedness::kDetached
-                            : v8::EmbedderGraph::Node::Detachedness::kUnknown;
-}
-
 template <int Field>
 void BaseObject::InternalFieldGet(
     const v8::FunctionCallbackInfo<v8::Value>& args) {

src/base_object.h
@@ -105,8 +105,6 @@ class BaseObject : public MemoryRetainer {
   // to it anymore.
   inline bool IsWeakOrDetached() const;
 
-  inline v8::EmbedderGraph::Node::Detachedness GetDetachedness() const override;
-
   // Utility to create a FunctionTemplate with one internal field (used for
   // the `BaseObject*` pointer) and a constructor that initializes that field
   // to `nullptr`.
@@ -192,7 +190,6 @@ class BaseObject : public MemoryRetainer {
 
  private:
   v8::Local<v8::Object> WrappedObject() const override;
-  bool IsRootNode() const override;
   void DeleteMe();
 
   // persistent_handle_ needs to be at a fixed offset from the start of the

commit d7df2628cbf5f25bc402a1d1f5a989215646aa1c

src: do not track weak `BaseObject`s as childrens of `Realm`s
Heap dumps are used for analyzing the relationship between
objects that keep each other alive, so that retainers of
memory can be detected and memory leaks fixed.

6e60ab744e6a13 broke this for a wide range of `BaseObject`
instances. Marking all `BaseObject`s, including weak ones,
as children of `Realm` and `Environment` instances gives
the incorrect impression that they are held alive by those
objects, effectively hiding the "real" set of strong roots
that keep other objects alive through GC.

An upcoming change in V8 will allow us to represent this
relationship between `Realm` and `Environment` and its
`BaseObject` and `CppgcMixin` instances properly.

Refs: https://github.com/v8/v8/commit/e37cadf1143a8c5bbe44c0408186b5a26cc23863
Refs: https://github.com/nodejs/node/pull/57417
Signed-off-by: Anna Henningsen <anna@addaleax.net>

src/base_object.cc
@@ -174,9 +174,15 @@ void BaseObjectList::Cleanup() {
   }
 }
 
-void BaseObjectList::MemoryInfo(node::MemoryTracker* tracker) const {
+void BaseObjectList::MemoryInfo(MemoryTracker* tracker) const {
   for (auto bo : *this) {
-    if (bo->IsDoneInitializing()) tracker->Track(bo);
+    if (bo->IsDoneInitializing()) {
+      // TODO(addaleax): Add weak edges instead of no edges once
+      // https://github.com/v8/v8/commit/e37cadf1143a8c5bbe44c0408186b5a26cc23863
+      // is available for us
+      tracker->Track(
+          bo, bo->persistent().IsWeak() ? MemoryTracker::KWeakEdge : nullptr);
+    }
   }
 }
 

src/cppgc_helpers.cc
@@ -16,7 +16,10 @@ void CppgcWrapperList::MemoryInfo(MemoryTracker* tracker) const {
   for (auto node : *this) {
     CppgcMixin* ptr = node->persistent.Get();
     if (ptr != nullptr) {
-      tracker->Track(ptr);
+      // TODO(addaleax): Add weak edges instead of no edges once
+      // https://github.com/v8/v8/commit/e37cadf1143a8c5bbe44c0408186b5a26cc23863
+      // is available for us
+      tracker->Track(ptr, MemoryTracker::KWeakEdge);
     }
   }
 }

test/common/heap.js
@@ -336,6 +336,19 @@ function validateByRetainingPath(...args) {
   return validateByRetainingPathFromNodes(nodes, ...args);
 }
 
+function getRetainingNodes(startingNode, filter) {
+  const seen = new Set();
+  function listNodes(node) {
+    if (!filter(node) || seen.has(node)) return;
+    seen.add(node);
+    for (const edge of node.incomingEdges) {
+      listNodes(edge.from);
+    }
+  }
+  listNodes(startingNode);
+  return [...seen];
+}
+
 module.exports = {
   recordState,
   validateSnapshotNodes,
@@ -343,4 +356,5 @@ module.exports = {
   validateByRetainingPathFromNodes,
   getHeapSnapshotOptionTests,
   createJSHeapSnapshot,
+  getRetainingNodes,
 };

test/pummel/test-heapdump-zlib.js
@@ -3,7 +3,7 @@
 
 const common = require('../common');
 const assert = require('assert');
-const { validateByRetainingPath, validateByRetainingPathFromNodes } = require('../common/heap');
+const { validateByRetainingPath, validateByRetainingPathFromNodes, getRetainingNodes } = require('../common/heap');
 const zlib = require('zlib');
 
 // Before zlib stream is created, no ZlibStream should be created.
@@ -25,6 +25,18 @@ const gzip = zlib.createGzip();
     { node_name: 'Node / zlib_memory', edge_name: 'zlib_memory' },
   ], true);
   assert.strictEqual(withMemory.length, 0);
+}
+
+{
+  // Assert that the `ZlibStream` has no unexpected connections
+  // to other `Node / ...` nodes.
+  const contexts = validateByRetainingPath('Node / ZlibContext', []);
+  assert.deepStrictEqual(
+    getRetainingNodes(contexts[0], (node) => node.name?.startsWith('Node /'))
+      .map((node) => node.name).sort(), [
+      'Node / ZlibContext',
+      'Node / ZlibStream',
+    ]);
 }
 
 // After zlib stream is written, zlib_memory should be created.