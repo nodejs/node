53214f350d0d6e376b2ebfec2eaca73790359dc0
SudhansuBandha <sudhansu9.vssut@gmail.com>
2026-06-14 14:57:44 +0530

    repl: fix dot command handling in multiline mode
    Update conditional logic to correctly handle dot commands
    in multiline REPL input
    
    Fixes: https://github.com/nodejs/node/issues/63864
    Refs: https://github.com/nodejs/node/pull/63889/
    Signed-off-by: SudhansuBandha <bandhasudhansu@gmail.com>

---
+++ test/parallel/test-repl-multiline-dot-commands-execution.js
@@ -0,0 +1,40 @@
+'use strict';
+const common = require('../common');
+const assert = require('assert');
+const { startNewREPLServer } = require('../common/repl');
+
+
+const dotCommandSyntaxError =
+  /Uncaught SyntaxError: Unexpected token '\.'/;
+
+
+function runDotCommand(command, validate) {
+  const { replServer, output } = startNewREPLServer();
+
+  replServer.on('exit', common.mustCall());
+  replServer.write('function a() {\n');
+  replServer.write(`${command}\n`);
+  validate(replServer, output);
+  replServer.write('arr = [1,\n');
+  replServer.write(`${command}\n`);
+  validate(replServer, output);
+  replServer.close();
+}
+
+runDotCommand('.break', common.mustCall((replServer, output) => {
+  replServer.write('1 + 1\n');
+  assert.doesNotMatch(output.accumulator, dotCommandSyntaxError);
+  assert.match(output.accumulator, /2\n/);
+}, 2));
+
+runDotCommand('.clear', common.mustCall((replServer, output) => {
+  replServer.write('1 + 1\n');
+  assert.doesNotMatch(output.accumulator, dotCommandSyntaxError);
+  assert.match(output.accumulator, /2\n/);
+}, 2));
+
+runDotCommand('.help', common.mustCall((replServer, output) => {
+  replServer.write('1 + 1\n');
+  assert.doesNotMatch(output.accumulator, dotCommandSyntaxError);
+  assert.match(output.accumulator, /2\n/);
+}, 2));
