e018f9a4a19f303e8d3c42efa54a57da2b6eeeeb
Chengzhong Wu <legendecas@gmail.com>
2026-06-19 17:50:27 -0400

    src: add perfetto trace agent
    Signed-off-by: Chengzhong Wu <cwu631@bloomberg.net>
    PR-URL: https://github.com/nodejs/node/pull/64565
    Refs: https://github.com/nodejs/diagnostics/issues/654
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Ryuhei Shima <shimaryuhei@gmail.com>

--- src/tracing/trace_event.h
+++ src/tracing/trace_event.h
@@ -1,9 +1,15 @@
 #ifndef SRC_TRACING_TRACE_EVENT_H_
 #define SRC_TRACING_TRACE_EVENT_H_
 
+#include "tracing/agent.h"
+
 #if defined(V8_USE_PERFETTO)
 
-#error "Perfetto is not supported"
+#define TRACING_CATEGORY_NODE "node"
+#define TRACING_CATEGORY_NODE1(one) TRACING_CATEGORY_NODE "." #one
+#define TRACING_CATEGORY_NODE2(one, two) TRACING_CATEGORY_NODE "." #one "." #two
+
+#include "tracing/trace_event_perfetto.h"
 
 #else  // defined(V8_USE_PERFETTO)
 
