91a707b2f150e8225c32c6bc9485a1b4ebd8d8d3
Marten Richter <marten.richter@tu-berlin.de>
2026-06-20 08:12:08 +0200

    Import nghttp3 webtransport PR

--- deps/ngtcp2/nghttp3/lib/nghttp3_conv.c
+++ deps/ngtcp2/nghttp3/lib/nghttp3_conv.c
@@ -79,6 +79,12 @@ const uint8_t *nghttp3_get_varint(int64_t *dest, const uint8_t *p) {
   return p;
 }
 
+const uint8_t *nghttp3_get_uint32be(uint32_t *dest, const uint8_t *p) {
+  memcpy(dest, p, sizeof(*dest));
+  *dest = ntohl(*dest);
+  return p + sizeof(*dest);
+}
+
 uint8_t *nghttp3_put_uint64be(uint8_t *p, uint64_t n) {
   n = nghttp3_htonl64(n);
   return nghttp3_cpymem(p, (const uint8_t *)&n, sizeof(n));
