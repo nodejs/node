25433a963ee8706b1e972368fd2900f93bc5dd52
Marten Richter <marten.richter@tu-berlin.de>
2026-06-20 11:39:17 +0200

    quic: add session id callback to test

--- test/parallel/test-quic-internal-setcallbacks.mjs
+++ test/parallel/test-quic-internal-setcallbacks.mjs
@@ -34,6 +34,7 @@ const callbacks = {
   onStreamReset() {},
   onStreamStopSending() {},
   onStreamHeaders() {},
+  onStreamSessionId() {},
   onStreamTrailers() {},
 };
 // Fail if any callback is missing
