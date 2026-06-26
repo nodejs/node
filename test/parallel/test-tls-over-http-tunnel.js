17099b21043272508c962572ee3b2de7357e50b3
Archkon <180910180+Archkon@users.noreply.github.com>
2026-06-26 23:32:16 +0800

    test: update CONNECT Host header expectation
    Signed-off-by: Archkon <180910180+Archkon@users.noreply.github.com>

--- test/parallel/test-tls-over-http-tunnel.js
+++ test/parallel/test-tls-over-http-tunnel.js
@@ -61,7 +61,7 @@ const proxy = net.createServer(common.mustCall((clientSocket) => {
                          `CONNECT localhost:${server.address().port} ` +
                          'HTTP/1.1\r\n' +
                          'Proxy-Connections: keep-alive\r\n' +
-                         `Host: localhost:${proxy.address().port}\r\n` +
+                         `Host: localhost:${server.address().port}\r\n` +
                          'Connection: keep-alive\r\n\r\n');
 
       console.log('PROXY: got CONNECT request');
