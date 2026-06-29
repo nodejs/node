9ad22d10c90c7b67d441c141a7012d9c8345d63b
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-06-29 14:07:18 +0200

    tools: add GHA benchmark runner
    Signed-off-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/60293
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>

--- tools/nix/benchmarkTools.nix
+++ tools/nix/benchmarkTools.nix
@@ -1,9 +1,11 @@
 {
   pkgs ? import ./pkgs.nix { },
+  withHttpBenchmarkDeps ? true,
 }:
 [
   pkgs.R
   pkgs.rPackages.ggplot2
   pkgs.rPackages.plyr
-  pkgs.wrk
 ]
+++ pkgs.lib.optional withHttpBenchmarkDeps pkgs.wrk
+++ pkgs.lib.optional pkgs.stdenv.buildPlatform.isLinux pkgs.glibcLocales
