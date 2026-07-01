821688aaa03592485900d428098a9602ffe52113
Matteo Collina <hello@matteocollina.com>
2026-07-01 15:59:54 +0200

    http: reject requests exceeding max header count
    PR-URL: https://github.com/nodejs-private/node-private/pull/922
    Refs: https://hackerone.com/reports/3564941
    CVE-ID: CVE-2026-58044

--- test/parallel/test-https-max-headers-count.js
+++ test/parallel/test-https-max-headers-count.js
@@ -24,19 +24,29 @@ for (let i = 0; i < N; ++i) {
   headers[`key${i}`] = i;
 }
 
-const maxAndExpected = [ // for server
-  [50, 50],
-  [1500, 102],
-  [0, N + 2], // Host and Connection
+function createRequestHeaders(count) {
+  const requestHeaders = {
+    host: 'example.com',
+  };
+  for (let i = 0; i < count; ++i) {
+    requestHeaders[`key${i}`] = i;
+  }
+  return requestHeaders;
+}
+
+const serverMaxAndExpected = [ // for server
+  [50, 50, 48],
+  [1500, 102, N],
+  [0, N + 2, N], // Host and Connection
 ];
-let max = maxAndExpected[requests][0];
-let expected = maxAndExpected[requests][1];
+let max = serverMaxAndExpected[requests][0];
+let expected = serverMaxAndExpected[requests][1];
 
 const server = https.createServer(serverOptions, common.mustCall((req, res) => {
   assert.strictEqual(Object.keys(req.headers).length, expected);
-  if (++requests < maxAndExpected.length) {
-    max = maxAndExpected[requests][0];
-    expected = maxAndExpected[requests][1];
+  if (++requests < serverMaxAndExpected.length) {
+    max = serverMaxAndExpected[requests][0];
+    expected = serverMaxAndExpected[requests][1];
     server.maxHeadersCount = max;
   }
   res.writeHead(200, { ...headers, 'Connection': 'close' });
@@ -45,22 +55,24 @@ const server = https.createServer(serverOptions, common.mustCall((req, res) => {
 server.maxHeadersCount = max;
 
 server.listen(0, common.mustCall(() => {
-  const maxAndExpected = [ // for client
+  const clientMaxAndExpected = [ // for client
     [20, 20],
     [1200, 104],
     [0, N + 4], // Host and Connection
   ];
   const doRequest = common.mustCall(() => {
-    const max = maxAndExpected[responses][0];
-    const expected = maxAndExpected[responses][1];
+    const max = clientMaxAndExpected[responses][0];
+    const expected = clientMaxAndExpected[responses][1];
+    const requestHeaders =
+      createRequestHeaders(serverMaxAndExpected[requests][2]);
     const req = https.request({
       port: server.address().port,
-      headers: headers,
+      headers: requestHeaders,
       rejectUnauthorized: false
     }, common.mustCall((res) => {
       assert.strictEqual(Object.keys(res.headers).length, expected);
       res.on('end', () => {
-        if (++responses < maxAndExpected.length) {
+        if (++responses < clientMaxAndExpected.length) {
           doRequest();
         } else {
           server.close();
