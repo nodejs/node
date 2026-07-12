17163ead3326a2eda3253c392474c777405a04c7
Archkon <180910180+Archkon@users.noreply.github.com>
2026-07-12 19:16:52 +0800

    http: do not emit socket errors after complete response
    Signed-off-by: Archkon <180910180+Archkon@users.noreply.github.com>
    PR-URL: https://github.com/nodejs/node/pull/64278
    Fixes: https://github.com/nodejs/node/issues/64272
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>

---
+++ test/parallel/test-http-client-complete-response-reset.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const http = require('http');
+
+const BODY = Buffer.alloc(2 * 1024 * 1024);
+
+const server = http.createServer(common.mustCall((request, response) => {
+  let received = 0;
+
+  request.on('data', function onData(chunk) {
+    received += chunk.length;
+    request.off('data', onData);
+    request.destroy();
+  });
+
+  response.writeHead(413, { 'content-length': 0 });
+  response.end();
+
+  request.on('end', common.mustNotCall());
+  request.on('close', common.mustCall(() => {
+    assert.strictEqual(request.complete, false);
+    assert.ok(received > 0);
+  }));
+}));
+
+server.listen(0, common.mustCall(() => {
+  let response;
+  const req = http.request({
+    method: 'POST',
+    port: server.address().port,
+    headers: {
+      'content-type': 'application/json',
+      'content-length': BODY.length,
+    },
+  }, common.mustCall((res) => {
+    response = res;
+    assert.strictEqual(res.statusCode, 413);
+    // Deliberately do not consume the response body. A response that has
+    // already been received should not be followed by a late ClientRequest
+    // socket error.
+    req.write(BODY);
+    req.end();
+  }));
+
+  req.on('error', common.mustNotCall());
+  req.on('close', common.mustCall(() => {
+    assert(response);
+    assert.strictEqual(response.complete, true);
+    server.close();
+  }));
+
+  req.flushHeaders();
+}));
