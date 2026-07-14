6b09d30a76d5f6f06e6510edc10e33bf9d6e31fe
Livia Medeiros <livia@cirno.name>
2026-07-14 21:30:56 +0800

    lib,tools: add `node-core/func-name-matching` lint rule
    And rename functions accordingly.
    
    Signed-off-by: LiviaMedeiros <livia@cirno.name>
    Co-authored-by: louiellan <louie.lou.llaneta@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/57901
    Refs: https://github.com/nodejs/node/issues/57899
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Ethan Arrowood <ethan@arrowood.dev>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -176,7 +176,6 @@ export default [
       'default-case-last': 'error',
       'dot-notation': 'error',
       'eqeqeq': ['error', 'smart'],
-      'func-name-matching': 'error',
       'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
       'no-constant-condition': ['error', { checkLoops: false }],
       'no-constructor-return': 'error',
@@ -388,6 +387,7 @@ export default [
       'node-core/no-duplicate-requires': 'error',
       'node-core/prefer-proto': 'error',
       'node-core/prefer-optional-chaining': 'error',
+      'node-core/func-name-matching': ['error', { considerPropertyDescriptor: true }],
     },
   },
   // #endregion
