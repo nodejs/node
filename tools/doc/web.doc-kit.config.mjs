38af496a070f7395d571d5163c1a85931afb5aab
avivkeller <me@aviv.sh>
2026-07-13 20:35:19 -0700

    fixup!

--- tools/doc/web.doc-kit.config.mjs
+++ tools/doc/web.doc-kit.config.mjs
@@ -2,13 +2,13 @@ import { totalmem } from 'node:os';
 import { join } from 'node:path';
 import { pathToFileURL } from 'node:url';
 
-const ALL_PAGE_MEMORY_THRESHOLD = 4 * 1024 ** 3; // 4 GiB
+const hasEnoughMemory = totalmem() > 6 * (1024 ** 3); // 6GB
 
 const fromRoot = (path) =>
   pathToFileURL(join(import.meta.dirname, '..', '..', path)).href;
 
 export default {
-  'target': ['web', 'legacy-json-all'],
+  'target': ['legacy-json-all', hasEnoughMemory && 'web'].filter(Boolean),
 
   'global': {
     input: ['doc/api/*.md'],
@@ -20,9 +20,5 @@ export default {
 
   'metadata': {
     typeMap: fromRoot('doc/type-map.json'),
-  },
-
-  'jsx-ast': {
-    generateAllPage: totalmem() > ALL_PAGE_MEMORY_THRESHOLD,
   },
 };
