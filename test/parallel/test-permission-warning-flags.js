bdad8d740529de69c0a3cf00fceff19fa3be2dad
David Evans <davidje13@users.noreply.github.com>
2026-07-21 23:41:19 +0100

    Add explicit test of disable-warning behaviour

--- test/parallel/test-permission-warning-flags.js
+++ test/parallel/test-permission-warning-flags.js
@@ -28,3 +28,17 @@ for (const flag of warnFlags) {
   assert.match(stderr.toString(), new RegExp(`\\[PERM\\d{4}\\] SecurityWarning: The flag ${RegExp.escape(flag)} must be used with extreme caution`));
   assert.strictEqual(status, 0);
 }
+
+const { status, stderr } = spawnSync(
+  process.execPath,
+  [
+    '--permission', '--allow-child-process', '--allow-wasi', '--disable-warning=PERM0002', '-e',
+    'setTimeout(() => {}, 1)',
+  ]
+);
+
+// Disabled warning does not appear
+assert.doesNotMatch(stderr.toString(), new RegExp(`The flag --allow-child-process must be used with extreme caution`));
+// But non-disabled warnings still appear
+assert.match(stderr.toString(), new RegExp(`The flag --allow-wasi must be used with extreme caution`));
+assert.strictEqual(status, 0);
