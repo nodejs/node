13f9c804416b0659e9855674626736ab62b8fc28
Efe Karasakal <hi@efe.dev>
2026-07-25 10:30:04 +0200

    http: update tests
    Signed-off-by: Efe Karasakal <hi@efe.dev>

--- test/parallel/test-http-server-invalid-headers-timeout-no-abort.js
+++ test/parallel/test-http-server-invalid-headers-timeout-no-abort.js
@@ -1,33 +1,19 @@
 'use strict';
 
-require('../common');
+const common = require('../common');
 const assert = require('assert');
-const { spawnSync } = require('child_process');
+const { createServer } = require('http');
 
-if (process.argv[2] === 'child') {
-  const { createServer } = require('http');
+const server = createServer({
+  connectionsCheckingInterval: 1,
+}, common.mustNotCall());
 
-  const server = createServer({
-    connectionsCheckingInterval: 1,
-  }, (_req, res) => {
-    res.end('ok');
-  });
+// Invalid headersTimeout should not crash the server
+server.headersTimeout = 'im-not-a-number';
+assert.strictEqual(server.headersTimeout, 'im-not-a-number');
 
-  server.headersTimeout = 'im-not-a-number';
-
-  server.listen(0, '127.0.0.1', () => {
-    setTimeout(() => {
-      server.close(() => process.exit(0));
-    }, 50);
-  });
-} else {
-  // Run the repro in a child so the native crash is observable without failing the whole run
-  const { signal, status, stderr } = spawnSync(
-    process.execPath,
-    [__filename, 'child'],
-    { encoding: 'utf8' },
-  );
-
-  assert.strictEqual(signal, null);
-  assert.strictEqual(status, 0, stderr);
-}
+server.listen(0, '127.0.0.1', common.mustCall(() => {
+  setTimeout(common.mustCall(() => {
+    server.close(common.mustCall());
+  }), common.platformTimeout(50));
+}));
