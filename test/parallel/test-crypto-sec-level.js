2d8547fbab2b05d88611cc1072b303508cfbb548
Filip Skokan <panva.ip@gmail.com>
2026-07-26 14:18:38 +0200

    test: drop OpenSSL 1.x-only coverage
    Delete test-crypto-ecb.js, which can no longer run anywhere: Blowfish
    is legacy-provider only on OpenSSL 3 and absent from BoringSSL.
    
    The addon and cctest version guards become OPENSSL_IS_BORINGSSL checks.
    
    Signed-off-by: Filip Skokan <panva.ip@gmail.com>

--- test/parallel/test-crypto-sec-level.js
+++ test/parallel/test-crypto-sec-level.js
@@ -11,7 +11,7 @@ const assert = require('assert');
 // are available by default. Different OpenSSL versions have different
 // default security levels and we use this value to adjust what a test
 // expects based on the security level. You can read more in
-// https://docs.openssl.org/1.1.1/man3/SSL_CTX_set_security_level/#default-callback-behaviour
+// https://docs.openssl.org/3.0/man3/SSL_CTX_set_security_level/#default-callback-behaviour
 // This test simply validates that we can get some value for the secLevel
 // when needed by tests.
 const secLevel = require('internal/crypto/util').getOpenSSLSecLevel();
