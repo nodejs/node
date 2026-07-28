6a64de237383be5e6b6481fed71401e26a69ab64
Filip Skokan <panva.ip@gmail.com>
2026-07-28 21:57:16 +0200

    fixup! deps: update ada to 4.0.0

--- test/benchmark/test-benchmark-url.js
+++ test/benchmark/test-benchmark-url.js
@@ -1,10 +1,6 @@
 'use strict';
 
-const { skip } = require('../common');
-
-if (process.config.variables.node_shared_ada) {
-  skip('WHATWG URL parsing is affected by different versions of Ada');
-}
+require('../common');
 
 const runBenchmark = require('../common/benchmark');
 
