50ee90fd1806179ed314aafa623a7c8a417e825c
Renegade334 <contact.9a5d6388@renegade334.me.uk>
2026-07-28 17:59:40 +0200

    fixup! deps: update ada to 4.0.0

--- test/benchmark/test-benchmark-url.js
+++ test/benchmark/test-benchmark-url.js
@@ -1,6 +1,10 @@
 'use strict';
 
-require('../common');
+const { skip } = require('../common');
+
+if (process.config.variables.node_shared_ada) {
+  skip('WHATWG URL parsing is affected by different versions of Ada');
+}
 
 const runBenchmark = require('../common/benchmark');
 
