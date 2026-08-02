240a86dac0392b94cc9aec1b2a69119a4d6d5ef8
David Evans <davidje13@users.noreply.github.com>
2026-08-02 14:54:33 +0100

    permission: add unique warning codes
    Adds unique warning codes of the form PERM0000 for all permissions
    related SecurityWarnings, so that they can be individually silenced
    if required.
    
    Fixes: https://github.com/nodejs/node/issues/59818
    Signed-off-by: David Evans <davidje13@users.noreply.github.com>
    PR-URL: https://github.com/nodejs/node/pull/64414
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Ulises Gascón <ulisesgascongonzalez@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-permission-warning-flags.js
+++ test/parallel/test-permission-warning-flags.js
@@ -25,6 +25,20 @@ for (const flag of warnFlags) {
     ]
   );
 
-  assert.match(stderr.toString(), new RegExp(`SecurityWarning: The flag ${RegExp.escape(flag)} must be used with extreme caution`));
+  assert.match(stderr.toString(), new RegExp(`\\[PERM\\d{4}\\] SecurityWarning: The flag ${RegExp.escape(flag)} must be used with extreme caution`));
   assert.strictEqual(status, 0);
 }
+
+const { status, stderr } = spawnSync(
+  process.execPath,
+  [
+    '--permission', '--allow-child-process', '--allow-wasi', '--disable-warning=PERM0002', '-e',
+    'setTimeout(() => {}, 1)',
+  ]
+);
+
+// Disabled warning does not appear
+assert.doesNotMatch(stderr.toString(), new RegExp(`The flag --allow-child-process must be used with extreme caution`));
+// But non-disabled warnings still appear
+assert.match(stderr.toString(), new RegExp(`The flag --allow-wasi must be used with extreme caution`));
+assert.strictEqual(status, 0);
