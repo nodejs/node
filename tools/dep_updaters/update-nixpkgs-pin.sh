f0140d7ebd2e98b3bb7b8b63c90ddb024d4254bf
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-08-04 18:34:32 +0200

    tools: store "default" OpenSSL version in `openssl-matrix.nix`
    To help with automating keeping in sync with the bundled version.
    
    Signed-off-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/64962
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Aviv Keller <me@aviv.sh>

--- tools/dep_updaters/update-nixpkgs-pin.sh
+++ tools/dep_updaters/update-nixpkgs-pin.sh
@@ -26,6 +26,14 @@ TMP_FILE=$(mktemp)
 sed "s/$CURRENT_VERSION_SHA1/$NEW_UPSTREAM_SHA1/;s/$CURRENT_TARBALL_HASH/$NEW_TARBALL_HASH/" "$NIXPKGS_PIN_FILE" > "$TMP_FILE"
 mv "$TMP_FILE" "$NIXPKGS_PIN_FILE"
 
+# === Update openssl-matrix.nix ===
+# When bumping the pin, we want to update the openssl-matrix.nix file to keep the list in sync nixpkgs
+# i.e. add newly added release lines, remove newly dropped release lines), and make sure the "openssl"
+# attribute still refers to the same release line as the bundled version in deps/openssl/.
+
+OPENSSL_MAJOR=$(awk -F= '/^MAJOR=[0-9]+$/ { print $2; exit }' "$BASE_DIR/deps/openssl/openssl/VERSION.dat")
+OPENSSL_MINOR=$(awk -F= '/^MINOR=[0-9]+$/ { print $2; exit }' "$BASE_DIR/deps/openssl/openssl/VERSION.dat")
+
 nix-instantiate -I "nixpkgs=$NIXPKGS_PIN_FILE" --eval --strict --json -E "
   let
     pkgs = import <nixpkgs> {};
@@ -33,15 +41,24 @@ nix-instantiate -I "nixpkgs=$NIXPKGS_PIN_FILE" --eval --strict --json -E "
       (n: builtins.match \"openssl_[0-9]+(_[0-9]+)?\" n != null)
       (builtins.attrNames pkgs);
     extraMatrixAttrs = [ \"boringssl\" ];
+    default = builtins.head (builtins.filter (n:
+      let
+        inherit (pkgs.lib) versions;
+        t = builtins.tryEval pkgs.\${n};
+        v = if t.success then builtins.tryEval t.value.version else t;
+        majorVersion = pkgs.lib.optionalString v.success (versions.major v.value);
+        minorVersion = pkgs.lib.optionalString v.success (versions.minor v.value);
+      in
+        majorVersion == ''$OPENSSL_MAJOR'' && minorVersion == ''$OPENSSL_MINOR'') opensslAttrs);
     attrs = builtins.filter
       (n:
         let t = builtins.tryEval pkgs.\${n}; in
-        t.success && (builtins.tryEval t.value.version).success
+        n != default && t.success && (builtins.tryEval t.value.version).success
       )
       (opensslAttrs ++ extraMatrixAttrs);
   in
   {
-    inherit attrs;
+    inherit attrs default;
     permittedInsecurePackages = builtins.map (attr: pkgs.\${attr}.name) (
       builtins.filter (attr: (pkgs.\${attr}.meta.insecure)) attrs
     );
@@ -53,6 +70,10 @@ nix-instantiate -I "nixpkgs=$NIXPKGS_PIN_FILE" --eval --strict --json -E "
 }:
 
 {
+  # "default" OpenSSL release line, should be kept in sync with the bundled version:
+  openssl = pkgs.\(.default);
+
+  # Other OpenSSL variants we want to test for:
   inherit (pkgs)
     \(.attrs | sort | join("\n    "))
     ;
