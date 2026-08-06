b4cff78644a107cb5a95d67ea2c1899949a07029
Aviv Keller <me@aviv.sh>
2026-08-05 21:34:39 -0400

    fixup!

--- test/benchmark/test-benchmark-repl.js
+++ test/benchmark/test-benchmark-repl.js
@@ -1,7 +1,8 @@
 'use strict';
 
-require('../common');
-
+const common = require('../common');
 const runBenchmark = require('../common/benchmark');
+
+common.skipIfInspectorDisabled();
 
 runBenchmark('repl', { NODEJS_BENCHMARK_ZERO_ALLOWED: 1 });
