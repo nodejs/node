d6d2a71beee138a2c5cf307e841af2dd09047977
Anshika Jain <anshikajain196872@gmail.com>
2026-08-08 21:24:54 +0530

    sqlite: validate maxSize argument in createTagStore()
    Signed-off-by: anshikakalpana <anshikajain196872@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/63792
    Fixes: https://github.com/nodejs/node/issues/63791
    Reviewed-By: Trivikram Kamat <trivikr.dev@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- test/parallel/test-sqlite-template-tag.js
+++ test/parallel/test-sqlite-template-tag.js
@@ -257,6 +257,40 @@ test('a finished iterator stays done and does not restart', () => {
   assert.strictEqual(iter.next().done, true);
 });
 
+test('createTagStore throws on invalid maxSize', () => {
+  const db = new DatabaseSync(':memory:');
+
+  assert.throws(() => db.createTagStore(0), {
+    code: 'ERR_OUT_OF_RANGE',
+    message: /maxSize/,
+  });
+
+  assert.throws(() => db.createTagStore(-1), {
+    code: 'ERR_OUT_OF_RANGE',
+    message: /maxSize/,
+  });
+
+  assert.throws(() => db.createTagStore(NaN), {
+    code: 'ERR_OUT_OF_RANGE',
+    message: /maxSize/,
+  });
+
+  assert.throws(() => db.createTagStore(1.5), {
+    code: 'ERR_OUT_OF_RANGE',
+    message: /maxSize/,
+  });
+
+  assert.throws(() => db.createTagStore('abc'), {
+    code: 'ERR_INVALID_ARG_TYPE',
+    message: /maxSize/,
+  });
+
+  assert.throws(() => db.createTagStore(Number.MAX_SAFE_INTEGER), {
+    code: 'ERR_OUT_OF_RANGE',
+    message: /maxSize/,
+  });
+});
+
 test('sql.db returns the associated DatabaseSync instance', () => {
   assert.strictEqual(sql.db, db);
 });
