cd337e7fc682be1727a1422d12aa9269ac3087ab
Milad Fa <mfarazma@ibm.com>
2026-08-14 16:50:14 +0000

    deps: V8: cherry-pick 811fe8b04203
    Original commit message:
    
        Skip regress-498816446-3 when there is not enough memory
    
        Currently causing a test failure on a machine with 8Gb of memory:
        ```
        regress-498816446-3.js:25: RangeError: Array buffer allocation failed
        ```
    
        Change-Id: I9e0396445c0b870d0070d666f9d6081b93e80f9c
        Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/8252113
        Commit-Queue: Milad Farazmand <mfarazma@ibm.com>
        Reviewed-by: Darius Mercadier <dmercadier@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#109426}
    
    Refs: https://github.com/v8/v8/commit/811fe8b0420359456f4cb8ea40b2df84a66033d9

--- deps/v8/test/mjsunit/maglev/regress-498816446-3.js
+++ deps/v8/test/mjsunit/maglev/regress-498816446-3.js
@@ -21,7 +21,19 @@ foo(ta);
 foo(ta);
 assertOptimized(foo);
 
-// Triggering deopt
-ta = new Int32Array(0xffffffff+1);
-assertThrows(() => foo(ta), RangeError, "Invalid array length");
-assertUnoptimized(foo);
+// Triggering deopt.
+var skip = false;
+try {
+  ta = new Int32Array(0xffffffff+1);
+} catch (e) {
+  if (/Array buffer allocation failed/.test(e.message)) {
+    skip = true;  // We don't have enough memory, just skip the test.
+  } else {
+    throw e;
+  }
+}
+
+if (!skip) {
+  assertThrows(() => foo(ta), RangeError, "Invalid array length");
+  assertUnoptimized(foo);
+}
