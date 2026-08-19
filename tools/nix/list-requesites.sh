da1641ac89500a42942e32257242c06aa562f1b2
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-08-19 17:58:24 +0200

    tools: improve nix-changes coverage
    Signed-off-by: Antoine du Hamel <duhamelantoine1995@gmail.com>

---
+++ tools/nix/list-requesites.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+set -ex
+
+OPTIONAL_FLAGS=$(nix-instantiate \
+  --eval --strict --json -E '
+    builtins.filter
+      (n: builtins.match "with[A-Z].+" n != null)
+      (builtins.attrNames (builtins.functionArgs (import ./shell.nix)))
+  ' | jq -r 'map("--arg \(.) true") | join(" ")')
+
+PIPEFAIL="$(mktemp)"
+
+cleanup () {
+  EXIT_CODE=$?
+  rm "$PIPEFAIL"
+  exit $EXIT_CODE
+}
+
+trap cleanup INT TERM EXIT
+
+{
+  # shellcheck disable=SC2086
+  nix-instantiate -I "nixpkgs=./tools/nix/pkgs.nix" shell.nix \
+    $OPTIONAL_FLAGS \
+    --arg sharedLibDeps '{
+      # Using an empty set as some build dependencies are required only in the absence of shared deps (e.g. Cargo).
+      # We pass the shared deps as devTools below so they are still accounted for.
+    }' \
+    --arg pkcs11 'import ./tools/nix/pkcs11.nix {
+      # Passing an import call rather than "true" to workaround sharedLibDeps being empty.
+    }' \
+    --arg devTools '
+      let
+        pkgs = import <nixpkgs> { };
+        sharedLibDepsFn = import ./tools/nix/sharedLibDeps.nix;
+      in
+      (import ./tools/nix/devTools.nix { })
+      ++ pkgs.lib.flatten (
+        with (pkgs.callPackage ./tools/nix/v8.nix { });
+        [
+          # We do not want to build V8 here, but still want to list its requisites.
+          buildInputs
+          nativeBuildInputs
+          propagatedBuildInputs
+          propagatedNativeBuildInputs
+          (pkgs.callPackage ./tools/nix/non-v8-deps-mock.nix { })
+        ]
+      )
+      ++ builtins.attrValues (
+        {
+          # Additional packages we are using across the codebase
+          inherit (pkgs) nixfmt-tree sccache;
+        }
+        // import ./tools/nix/openssl-matrix.nix { }
+        // sharedLibDepsFn (
+          pkgs.lib.filterAttrs (n: v: builtins.match "with[A-Z].+" n != null) (
+            builtins.functionArgs sharedLibDepsFn
+          )
+        )
+      )' || echo > "$PIPEFAIL"
+} | {
+  xargs nix-store --query --references || echo > "$PIPEFAIL"
+} | {
+  xargs nix-store --realise || echo > "$PIPEFAIL"
+} | {
+  xargs nix-store --query --requisites || echo > "$PIPEFAIL"
+} | {
+  sort -k1.45 || echo > "$PIPEFAIL"
+} && [ ! -s "$PIPEFAIL" ]
