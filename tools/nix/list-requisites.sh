efd2c98624d6862bea8dc2085c55a3c6f8cb4f81
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-08-21 19:05:15 +0200

    tools: improve nix-changes coverage
    Signed-off-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/65403
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>

---
+++ tools/nix/list-requisites.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+set -ex
+
+BASE_DIR=$(cd "$(dirname "$0")/../.." && pwd)
+
+OPTIONAL_FLAGS=$(nix-instantiate -I "nixpkgs=$BASE_DIR/tools/nix/pkgs.nix" --eval --strict --raw -E "
+  (import <nixpkgs> {}).lib.concatMapStrings
+    (n: ''--arg \${n} true '')
+    (builtins.filter
+      (n: builtins.match ''with[A-Z].+'' n != null)
+      (builtins.attrNames (builtins.functionArgs (import $BASE_DIR/shell.nix))))
+  "
+)
+
+DRV=$(
+  cd "$BASE_DIR"
+  # shellcheck disable=SC2016,SC2086
+  nix-instantiate -I "nixpkgs=./tools/nix/pkgs.nix" shell.nix \
+    $OPTIONAL_FLAGS \
+    --arg sharedLibDeps '{
+      # Using an empty set as some build dependencies are required only in the absence of shared deps (e.g. Cargo).
+      # We pass the shared deps as devTools below so they are still accounted for.
+    }' \
+    --arg pkcs11 'import ./tools/nix/pkcs11.nix {
+      # Passing an import call rather than "true" to workaround sharedLibDeps being empty.
+    }' \
+    --arg devTools '
+      let
+        pkgs = import <nixpkgs> { };
+        sharedLibDepsFn = import ./tools/nix/sharedLibDeps.nix;
+      in
+      (import ./tools/nix/devTools.nix { })
+      ++ pkgs.lib.flatten (
+        with (
+          let
+            # The pinned stdenv does not lazy-evaluates the src, making it complain about missing files (due to the sparse checkout).
+            # Since we are not building the derivation, only interested in its attributes, using a more modern stdenv should have no
+            # effect on the output (it adds some download and parse time, should be less than a non-sparse checkout).
+            repo = "https://github.com/NixOS/nixpkgs";
+            rev = "6062ba1a1b8b6281b12533c9075a2dbeb38f7e49";
+            nixpkgs_26_05 = import (builtins.fetchTarball {
+              url = "${repo}/archive/${rev}.tar.gz";
+              sha256 = "1dfmhgmairiq0k50vflzqfqaiz6v9dfvbf60yh6fhnkna312daiy";
+            }) { };
+          in
+          pkgs.callPackage ./tools/nix/v8.nix { inherit (nixpkgs_26_05) stdenv; }
+        );
+        [
+          # We do not want to build V8 here, but still want to list its requisites.
+          buildInputs
+          nativeBuildInputs
+          propagatedBuildInputs
+          propagatedNativeBuildInputs
+          (pkgs.callPackage ./tools/nix/non-v8-deps-mock.nix { })
+        ]
+      )
+      ++ builtins.attrValues (
+        {
+          # Additional packages we are using across the codebase
+          inherit (pkgs) nixfmt-tree sccache;
+        }
+        // import ./tools/nix/openssl-matrix.nix { }
+        // sharedLibDepsFn (
+          pkgs.lib.filterAttrs (n: v: builtins.match "with[A-Z].+" n != null) (
+            builtins.functionArgs sharedLibDepsFn
+          )
+        )
+      )'
+)
+REFS=$(nix-store --query --references "$DRV")
+STORE_PATHS=$(echo "$REFS" | xargs nix-store --realise)
+REQUISITES=$(echo "$STORE_PATHS" | xargs nix-store --query --requisites)
+
+echo "$REQUISITES" | sort -k1.45 | uniq
