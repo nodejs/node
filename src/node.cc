21f0f2786f02f930acd56c0665dc65a8bb1693f5
Olivier Flückiger <o-@users.noreply.github.com>
2026-08-21 09:48:38 +0200

    src: remove --harmony-import-attributes
    Import attributes have been enabled by default since V8 M123 and are now
    standard in JavaScript. Remove the hardcoded injection of the flag in
    Node.js so that the flag can be deprecated and removed upstream in V8.
    
    Refs: https://chromium-review.googlesource.com/c/v8/v8/+/8260695
    Signed-off-by: Olivier Flückiger <olivf@chromium.org>
    PR-URL: https://github.com/nodejs/node/pull/65396
    Refs: https://github.com/nodejs/node/pull/61898
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    Reviewed-By: Richard Lau <richard.lau@ibm.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>
    Reviewed-By: Marco Ippolito <marcoippolito54@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- src/node.cc
+++ src/node.cc
@@ -759,13 +759,6 @@ static ExitCode ProcessGlobalArgsInternal(std::vector<std::string>* args,
     return ExitCode::kInvalidCommandLineArgument2;
   }
 
-  // TODO(aduh95): remove this when the harmony-import-attributes flag
-  // is removed in V8.
-  if (std::ranges::find(v8_args, "--no-harmony-import-attributes") ==
-      v8_args.end()) {
-    v8_args.emplace_back("--harmony-import-attributes");
-  }
-
   if (!per_process::cli_options->per_isolate->max_old_space_size_percentage
            .empty()) {
     v8_args.emplace_back(
