b8071bd383a07412abb8badf867e8eb424e61690
Filip Skokan <panva.ip@gmail.com>
2026-08-22 13:31:02 +0200

    tools: update BoringSSL pin on v26.x
    Signed-off-by: Filip Skokan <panva.ip@gmail.com>

--- tools/nix/openssl-matrix.nix
+++ tools/nix/openssl-matrix.nix
@@ -4,13 +4,24 @@
   },
 }:
 
+let
+  boringssl = pkgs.boringssl.overrideAttrs (_: rec {
+    version = "0.20260803.0";
+    src = pkgs.fetchgit {
+      url = "https://boringssl.googlesource.com/boringssl";
+      tag = version;
+      hash = "sha256-GmaXG6I2euA+Q7naO2Oxu+P4mK37RbgwW5iM7ync6Gg=";
+    };
+    patches = [ ];
+  });
+in
 {
   # "default" OpenSSL release line, should be kept in sync with the bundled version:
   openssl = pkgs.openssl_3_5;
 
   # Other OpenSSL variants we want to test for:
+  inherit boringssl;
   inherit (pkgs)
-    boringssl
     openssl_1_1
     openssl_3
     openssl_3_6
