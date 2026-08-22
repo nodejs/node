abb03e6df97be9f7d707828e5a8e6465e7441f7d
Filip Skokan <panva.ip@gmail.com>
2026-08-22 13:31:02 +0200

    tools: update BoringSSL pin on v26.x
    Signed-off-by: Filip Skokan <panva.ip@gmail.com>

--- tools/nix/openssl-matrix.nix
+++ tools/nix/openssl-matrix.nix
@@ -8,9 +8,14 @@
   # "default" OpenSSL release line, should be kept in sync with the bundled version:
   openssl = pkgs.openssl_3_5;
 
+  # Using an updated version of BoringSSL as we do not support the old version anymore.
+  boringssl = pkgs.callPackage (builtins.fetchurl {
+    url = "https://github.com/NixOS/nixpkgs/raw/6062ba1a1b8b6281b12533c9075a2dbeb38f7e49/pkgs/by-name/bo/boringssl/package.nix";
+    sha256 = "01n2ga83ds5m9hz88z635g3q1c141ka2qs9k576c62v3fdpgcj6i";
+  }) { };
+
   # Other OpenSSL variants we want to test for:
   inherit (pkgs)
-    boringssl
     openssl_1_1
     openssl_3
     openssl_3_6
