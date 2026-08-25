070a5ecce3d715898d827c2a5571964111381afb
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-25 15:02:46 +0500

    worker: add Symbol.toStringTag to BroadcastChannel
    Signed-off-by: Lazizbek Ergashev <lazerg2@gmail.com>

--- test/parallel/test-messaging-string-tag.js
+++ test/parallel/test-messaging-string-tag.js
@@ -4,7 +4,7 @@ require('../common');
 
 const assert = require('assert');
 
-const classesToBeTested = [ MessageChannel, MessagePort ];
+const classesToBeTested = [ MessageChannel, MessagePort, BroadcastChannel ];
 
 classesToBeTested.forEach((cls) => {
   assert.strictEqual(cls.prototype[Symbol.toStringTag], cls.name);
@@ -15,3 +15,7 @@ classesToBeTested.forEach((cls) => {
 const channel = new MessageChannel();
 assert.strictEqual(Object.prototype.toString.call(channel), '[object MessageChannel]');
 assert.strictEqual(Object.prototype.toString.call(channel.port1), '[object MessagePort]');
+
+const broadcastChannel = new BroadcastChannel('string-tag');
+assert.strictEqual(Object.prototype.toString.call(broadcastChannel), '[object BroadcastChannel]');
+broadcastChannel.close();
