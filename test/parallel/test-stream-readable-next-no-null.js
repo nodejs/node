2342b2bf30accbbd7530a1bab98ecf7d7a44fa95
jakecastelli <38635403+jakecastelli@users.noreply.github.com>
2026-08-26 23:37:26 +0930

    test: cover Readable.from() sync iterator errors
    Signed-off-by: jakecastelli <38635403+jakecastelli@users.noreply.github.com>
    PR-URL: https://github.com/nodejs/node/pull/65515
    Reviewed-By: Jason Zhang <xzha4350@gmail.com>
    Reviewed-By: Daeyeon Jeong <daeyeon.dev@gmail.com>

--- test/parallel/test-stream-readable-next-no-null.js
+++ test/parallel/test-stream-readable-next-no-null.js
@@ -1,19 +1,87 @@
 'use strict';
-const { mustNotCall, expectsError } = require('../common');
+const common = require('../common');
+const assert = require('assert');
 const { Readable } = require('stream');
+const { finished } = require('stream/promises');
 
-async function* generate() {
-  yield null;
-}
-
-const stream = Readable.from(generate());
-
-stream.on('error', expectsError({
+const expectedError = {
   code: 'ERR_STREAM_NULL_VALUES',
   name: 'TypeError',
   message: 'May not write null values to stream'
-}));
+};
 
-stream.on('data', mustNotCall());
+async function rejectsNull(iterable, expectedChunks = []) {
+  const stream = Readable.from(iterable);
+  const chunks = [];
+  const completion = finished(stream);
 
-stream.on('end', mustNotCall());
+  stream.on('data', (chunk) => chunks.push(chunk));
+
+  await assert.rejects(completion, expectedError);
+  assert.deepStrictEqual(chunks, expectedChunks);
+  assert.strictEqual(stream.destroyed, true);
+}
+
+async function asyncIteratorYieldsNull() {
+  const cleanup = common.mustCall();
+
+  async function* generate() {
+    try {
+      yield null;
+    } finally {
+      cleanup();
+    }
+  }
+
+  await rejectsNull(generate());
+}
+
+async function syncIteratorYieldsNull() {
+  const cleanup = common.mustCall();
+
+  function* generate() {
+    try {
+      yield null;
+    } finally {
+      cleanup();
+    }
+  }
+
+  await rejectsNull(generate());
+}
+
+async function firstSyncValueResolvesToNull() {
+  const cleanup = common.mustCall();
+
+  function* generate() {
+    try {
+      yield Promise.resolve(null);
+    } finally {
+      cleanup();
+    }
+  }
+
+  await rejectsNull(generate());
+}
+
+async function laterSyncValueResolvesToNull() {
+  const cleanup = common.mustCall();
+
+  function* generate() {
+    try {
+      yield Promise.resolve('first');
+      yield Promise.resolve(null);
+    } finally {
+      cleanup();
+    }
+  }
+
+  await rejectsNull(generate(), ['first']);
+}
+
+Promise.all([
+  asyncIteratorYieldsNull(),
+  syncIteratorYieldsNull(),
+  firstSyncValueResolvesToNull(),
+  laterSyncValueResolvesToNull(),
+]).then(common.mustCall());
