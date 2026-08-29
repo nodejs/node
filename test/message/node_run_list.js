14b5dfc63d548adaa576f3ddb06d758a55602740
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-08-29 16:51:21 +0200

    test: remove `console.log` call in `node_run_list`
    Signed-off-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/65572
    Refs: https://github.com/nodejs/node/pull/65552/changes#r3862720795
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/message/node_run_list.js
+++ test/message/node_run_list.js
@@ -4,10 +4,9 @@ require('../common');
 const { spawnSyncAndExit } = require('../common/child_process');
 const fixtures = require('../common/fixtures');
 
-const { child } = spawnSyncAndExit(
+spawnSyncAndExit(
   process.execPath,
   [ '--no-warnings', '--run'],
-  { cwd: fixtures.path('run-script'), encoding: 'utf8' },
+  { cwd: fixtures.path('run-script'), encoding: 'utf8', stdio: ['ignore', 'ignore', 'inherit'] },
   { status: 9, signal: null },
 );
-console.log(child.stderr);
