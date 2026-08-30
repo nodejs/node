d2b6bf4f69ab5cee89b841942089019090ce4811
James M Snell <jasnell@gmail.com>
2026-08-30 20:11:50 +0000

    test: fix node:bench test timing
    Signed-off-by: James M Snell <jasnell@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/65606
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Robert Nagy <ronagy@icloud.com>
    Reviewed-By: Vinícius Lourenço Claro Cardoso <contact@viniciusl.com.br>
    Reviewed-By: Gürgün Dayıoğlu <hey@gurgun.day>

--- test/fixtures/bench-runner/ipc.cjs
+++ test/fixtures/bench-runner/ipc.cjs
@@ -5,7 +5,5 @@ const { bench } = require('node:bench');
 process.send?.({ type: 'user-message' });
 
 bench('user IPC', { samples: 1 }, (b) => {
-  b.start();
-  process.hrtime.bigint();
-  b.end(1);
+  b.record({ duration_ns: 1n, operations: 1 });
 });
