7e6a56c36717e1211601865fc52c7cc3d220b3ad
leah-1ee <selee3196@gmail.com>
2026-08-30 16:10:15 +0900

    typings: add fs_event_wrap internal binding types
    Add a FsEventWrapBinding declaration for
    internalBinding('fs_event_wrap') and wire it into InternalBindingMap.
    
    Signed-off-by: leah-1ee <selee3196@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/65661
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>

---
+++ typings/internalBinding/fs_event_wrap.d.ts
@@ -0,0 +1,28 @@
+import { owner_symbol } from './symbols';
+
+declare namespace InternalFsEventWrapBinding {
+  class FSEvent {
+    constructor();
+    [owner_symbol]?: object;
+    readonly initialized: boolean;
+    onchange?: (
+      status: number,
+      eventType: string,
+      filename: string | Uint8Array | null,
+    ) => void;
+    start(
+      filename: string,
+      persistent: boolean,
+      recursive: boolean,
+      encoding: string,
+    ): number;
+    close(callback?: () => void): void;
+    hasRef(): boolean;
+    ref(): void;
+    unref(): void;
+  }
+}
+
+export interface FsEventWrapBinding {
+  FSEvent: typeof InternalFsEventWrapBinding.FSEvent;
+}
