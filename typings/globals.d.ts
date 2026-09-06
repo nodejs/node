6357c8f56e77998c4789dea76bef3cdda0a05790
Seongeun Lee <selee3196@gmail.com>
2026-09-06 17:44:07 +0900

    typings: add stream_pipe internal binding types
    Add a StreamPipeBinding declaration for internalBinding('stream_pipe')
    and wire it into InternalBindingMap.
    
    Signed-off-by: leah-1ee <selee3196@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/65664
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>

--- typings/globals.d.ts
+++ typings/globals.d.ts
@@ -30,6 +30,7 @@ import { ProfilerBinding } from './internalBinding/profiler';
 import { SeaBinding } from './internalBinding/sea';
 import { SerdesBinding } from './internalBinding/serdes';
 import { SignalWrapBinding } from './internalBinding/signal_wrap';
+import { StreamPipeBinding } from './internalBinding/stream_pipe';
 import { StringDecoderBinding } from './internalBinding/string_decoder';
 import { SymbolsBinding } from './internalBinding/symbols';
 import { TimersBinding } from './internalBinding/timers';
@@ -76,6 +77,7 @@ interface InternalBindingMap {
   sea: SeaBinding;
   serdes: SerdesBinding;
   signal_wrap: SignalWrapBinding;
+  stream_pipe: StreamPipeBinding;
   string_decoder: StringDecoderBinding;
   symbols: SymbolsBinding;
   timers: TimersBinding;
