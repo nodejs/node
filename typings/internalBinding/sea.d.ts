3d9177b477a9b7f838925afb2d9bb35f6aa11827
이혜미 <103042868+hyemimi@users.noreply.github.com>
2026-09-08 11:34:31 +0900

    typings: add missing sea binding properties
    Signed-off-by: hyemimi <hyemi7375@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/65815
    Reviewed-By: Daeyeon Jeong <daeyeon.dev@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- typings/internalBinding/sea.d.ts
+++ typings/internalBinding/sea.d.ts
@@ -1,5 +1,8 @@
 export interface SeaBinding {
   getAsset(key: string): ArrayBuffer | undefined;
+  getAssetKeys(): string[];
   isExperimentalSeaWarningNeeded(): boolean;
   isSea(): boolean;
+  isVfsEnabled(): boolean;
+  mainCodePath?: string;
 }
