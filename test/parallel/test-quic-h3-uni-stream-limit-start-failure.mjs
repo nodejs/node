debe5632e726e725cac8c3d754d1ebe6665bf830
Tim Perry <1526883+pimterry@users.noreply.github.com>
2026-09-16 16:18:21 +0200

    Fix test comment
    Co-authored-by: James M Snell <jasnell@gmail.com>

--- test/parallel/test-quic-h3-uni-stream-limit-start-failure.mjs
+++ test/parallel/test-quic-h3-uni-stream-limit-start-failure.mjs
@@ -1,6 +1,6 @@
 // Flags: --experimental-quic --no-warnings
 
-// An HTTP/3 session must not cleanly fail if the peer advertises fewer than
+// An HTTP/3 session must cleanly fail if the peer advertises fewer than
 // the 3 unidirectional streams that HTTP/3 needs for control and QPACK.
 
 import { hasQuic, skip, mustNotCall } from '../common/index.mjs';
