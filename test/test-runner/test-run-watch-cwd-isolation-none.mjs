dda22c06d7c3a23af1e474f488cd0e73dbedd616
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-09-18 17:15:50 +0200

    test: deflake test-run-watch-cwd-isolation-none-*
    Signed-off-by: Antoine du Hamel <duhamelantoine1995@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/66035
    Refs: https://github.com/nodejs/node/issues/66056
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Luigi Pinca <luigipinca@gmail.com>

--- test/test-runner/test-run-watch-cwd-isolation-none.mjs
+++ test/test-runner/test-run-watch-cwd-isolation-none.mjs
@@ -1,27 +1,28 @@
 // Test run({ watch: true, cwd, isolation: 'none' }) runs with different cwd while in watch mode and isolation none
 import * as common from '../common/index.mjs';
+import * as fixtures from '../common/fixtures.mjs';
 import { run } from 'node:test';
-import tmpdir from '../common/tmpdir.js';
-import { refreshForTestRunnerWatch, skipIfNoWatch } from '../common/watch.js';
+import { skipIfNoWatch } from '../common/watch.js';
 
 skipIfNoWatch();
-refreshForTestRunnerWatch();
 
 const controller = new AbortController();
 const stream = run({
-  cwd: tmpdir.path,
+  cwd: fixtures.path('test-runner-watch'),
   watch: true,
   signal: controller.signal,
   isolation: 'none',
-}).on('data', function({ type }) {
-  if (type === 'test:watch:drained') {
-    stream.removeAllListeners('test:fail');
-    stream.removeAllListeners('test:pass');
-    controller.abort();
-  }
+}).on('data', ({ type }) => {
+  if (type !== 'test:watch:drained') return;
+
+  stream.removeAllListeners('test:fail');
+  stream.removeAllListeners('test:pass');
+  controller.abort();
 });
 
-stream.on('test:fail', common.mustNotCall());
-stream.on('test:pass', common.mustCall(1));
-// eslint-disable-next-line no-unused-vars
-for await (const _ of stream);
+stream.on('test:watch:restarted', common.mustNotCall('test:watch:restarted'));
+stream.on('test:fail', common.mustNotCall('test:fail'));
+stream.on('test:pass', common.mustCall());
+
+// eslint-disable-next-line no-empty-pattern
+for await (const {} of stream);
