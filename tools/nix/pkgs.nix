3adcbe072f53174fb514ea338bf2b6e9e2c455b6
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-09-20 13:08:28 +0200

    fixup! drop debug changes, pin sccache on x64 linux

--- tools/nix/pkgs.nix
+++ tools/nix/pkgs.nix
@@ -14,11 +14,9 @@ if builtins.currentSystem == "x86_64-darwin" then
   (import ./pkgs-26.05.nix arg)
 else
   nixpkgs
-  //
-    nixpkgs.lib.optionalAttrs (pkgs.stdenv.hostPlatform.isLinux && pkgs.stdenv.hostPlatform.isx86_64)
-      {
-        sccache = nixpkgs.callPackage (builtins.fetchurl {
-          url = "${repo}/raw/aff8a0b28396750446e5537a96461bc4facdb287/pkgs/by-name/sc/sccache/package.nix";
-          sha256 = "09dlc99sam9ld9jvzgcc00r4v4ls1cmbb0yim3mr895fpbs8b3qa";
-        }) { };
-      }
+  // nixpkgs.lib.optionalAttrs (with nixpkgs.stdenv.hostPlatform; isLinux && isx86_64) {
+    sccache = nixpkgs.callPackage (builtins.fetchurl {
+      url = "${repo}/raw/aff8a0b28396750446e5537a96461bc4facdb287/pkgs/by-name/sc/sccache/package.nix";
+      sha256 = "09dlc99sam9ld9jvzgcc00r4v4ls1cmbb0yim3mr895fpbs8b3qa";
+    }) { };
+  }
