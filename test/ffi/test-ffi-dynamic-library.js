f981fef5fc64eafc82198d784674afc25952b822
HoonDongKang <d159123@naver.com>
2026-09-24 22:24:18 +0900

    ffi: check only error codes in assertions
    Signed-off-by: HoonDongKang <d159123@naver.com>

--- test/ffi/test-ffi-dynamic-library.js
+++ test/ffi/test-ffi-dynamic-library.js
@@ -436,21 +436,18 @@ test('dynamic library APIs validate failures and bad signatures', () => {
       lib.getFunction('add_i32', { return: 1, arguments: [] });
     }, {
       code: 'ERR_INVALID_ARG_TYPE',
-      message: 'Return value type of function add_i32 must be a string',
     });
 
     assert.throws(() => {
       lib.getFunction('add_i32', { return: 'i32', arguments: 'i32' });
     }, {
       code: 'ERR_INVALID_ARG_TYPE',
-      message: 'Arguments list of function add_i32 must be an array',
     });
 
     assert.throws(() => {
       lib.getFunction('add_i32', { return: 'i32', arguments: [1] });
     }, {
       code: 'ERR_INVALID_ARG_TYPE',
-      message: 'Argument 0 of function add_i32 must be a string',
     });
 
     assert.throws(() => {
