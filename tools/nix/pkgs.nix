5d9228d5c3e3c5ba8a03ffb8d053fbee57e7956c
Tim Perry <1526883+pimterry@users.noreply.github.com>
2026-09-26 10:20:18 +0200

    tools: pin sccache on all platforms to fix benchmark CI job
    Signed-off-by: Tim Perry <pimterry@gmail.com>
    PR-URL: https://github.com/nodejs/node/pull/66284
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>

--- tools/nix/pkgs.nix
+++ tools/nix/pkgs.nix
@@ -14,7 +14,7 @@ if builtins.currentSystem == "x86_64-darwin" then
   (import ./pkgs-26.05.nix arg)
 else
   nixpkgs
-  // nixpkgs.lib.optionalAttrs (with nixpkgs.stdenv.hostPlatform; isLinux && isx86_64) {
+  // {
     # TODO: remove pin once https://github.com/mozilla/sccache/issues/2869 is resolved
     sccache = nixpkgs.callPackage (builtins.fetchurl {
       url = "${repo}/raw/aff8a0b28396750446e5537a96461bc4facdb287/pkgs/by-name/sc/sccache/package.nix";
